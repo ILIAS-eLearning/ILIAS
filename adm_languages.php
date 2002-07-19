cf5506da97a488aeac4cdcf83164c85e72280d9c
pgabriel <pgabriel>
2002-07-19 15:28:45 +0000

    language filesystem, howto use: at installation-time only en.lang exists then you can go to admin-languages and install languages from the masterfile
    later in the developmentprocess one will turn the generated files into the database, but now it is fast enough and:
    the smaller lang-files, e.g. "de.lang" can operate as intermediate file for a translator.
    If she/he has finished translation she/he will give it to a developer. he will merge the intermediate file to the master fil
    e then commit.
    okay?
    
    so press the generate from file-button in the admin-languages section and all languagefiles are created.
    
    the backmerging into the large languagesfile is not written yet
    
    
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@34 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- adm_languages.php
+++ adm_languages.php
@@ -1,6 +1,6 @@
 <?PHP
 /**
- * admin database
+ * admin languages
  * utils for updating the database and optimize it etc.
  *
  * @author Peter Gabriel <pgabriel@databay.de>
@@ -12,9 +12,20 @@ include("./include/inc.main.php");
 
 $lng = new Language($ilias->account->data["language"]);
 
-$tpl = new Template("tpl.adm_languages.html", false, false);
-
+$tpl = new Template("tpl.adm_languages.html", true, true);
 $tpl->setVariable("TXT_PAGEHEADLINE", $lng->txt("languages"));
+
+if ($_POST["func"]=="gen_langs")
+{
+	if ($lng->generateLanguageFiles($_POST["langfile"])==false)
+	{
+		$tpl->setCurrentBlock("message");
+		$tpl->setVariable("MSG", $lng->error);
+		$tpl->parseCurrentBlock();		
+	}
+}
+
+
 
 
 $langs = $lng->getAllLanguages();
@@ -24,6 +35,7 @@ $tplbtn->setCurrentBlock("btn_cell");
 $tplbtn->setVariable("BTN_LINK",".php");
 $tplbtn->setVariable("BTN_TXT", $lng->txt("refresh_list"));
 $tplbtn->parseCurrentBlock();
+
 $tplbtn->setCurrentBlock("btn_row");
 $tplbtn->parseCurrentBlock();
 
@@ -47,21 +59,25 @@ $tplbtn->setCurrentBlock("btn_row");
 $tplbtn->parseCurrentBlock();
 
 $tpl->setVariable("BUTTONS2",$tplbtn->get());
-$tpl->setVariable("LNG_AVAILLANG_TXT", $lng->txt("available_languages"));
-$tpl->setVariable("LNG_LANG_TXT", $lng->txt("language"));
-$tpl->setVariable("LNG_STATUS_TXT", $lng->txt("status"));
-$tpl->setVariable("LNG_LASTCHANGE_TXT", $lng->txt("last_change"));
+$tpl->setVariable("TXT_AVAILLANG", $lng->txt("available_languages"));
+$tpl->setVariable("TXT_LANG", $lng->txt("language"));
+$tpl->setVariable("TXT_STATUS", $lng->txt("status"));
+$tpl->setVariable("TXT_LASTCHANGE", $lng->txt("last_change"));
 
 foreach ($langs as $row)
 {
 	$i++;
 	$tpl->setCurrentBlock("language_row");
 	$tpl->setVariable("ROWCOL","tblrow".(($i % 2)+1));
-	$tpl->setVariable("LNG_TITLE",$row["title"]);
-	$tpl->setVariable("LNG_STATUS",$row["status"]);
-	$tpl->setVariable("LNG_DATE",$row["lastchange"]);
+	$tpl->setVariable("TITLE", $row["name"]);
+	$tpl->setVariable("STATUS", "_installed");
+	$tpl->setVariable("LASTCHANGE", " ");
 	$tpl->parseCurrentBlock();
 }
+
+$tpl->setVariable("TXT_GENERATE", "_generate from");
+$tpl->setVariable("LANGMASTERFILE", "languages.txt");
+$tpl->setVariable("TXT_SUBMIT", $lng->txt("submit"));
 
 $tplmain->setVariable("PAGECONTENT",$tpl->get());
 $tplmain->show();
