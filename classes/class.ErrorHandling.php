8920328ca09128227c9f6b18069bd8cf5858a191
Stefan Meyer <smeyer.ilias@gmx.de>
2002-08-02 13:23:29 +0000

    added Error Handler
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@72 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ classes/class.ErrorHandling.php
@@ -0,0 +1,38 @@
+<?php
+class ErrorHandling
+{
+	var $DEBUG_ENV;
+	var $FATAL = 1;
+	var $WARNING = 2;
+
+	function ErrorHandling()
+	{
+		$this->DEBUG_ENV = 1;
+	}
+	function errorHandler($a_error_obj)
+	{
+		if($a_error_obj->getCode() == $this->FATAL)
+		{
+			die ($a_error_obj->getMessage());
+		}
+		if($a_error_obj->getCode() == $this->WARNING)
+		{
+			if($this->DEBUG_ENV == 1)
+			{
+				$message = $a_error_obj->getMessage();
+			}
+			else
+			{
+				$message = "Under Construction";
+			}
+			$_SESSION["redirect"] = $_SESSION["referer"];
+			if($_SESSION["cmd"])
+			{
+				$_SESSION["redirect"] .= "&cmd=".$_POST["cmd"];
+			}
+			header("location: error.php?message=$message");
+			exit();
+		}
+	}
+}
+?>
