544d740089babf462b18a08a0f98379fd8bd20f9
pgabriel <pgabriel>
2002-08-07 14:20:48 +0000

    implemented search only usersearch at this time
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@95 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ classes/class.Search.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * search
+ * @author Peter Gabriel <pgabriel@databay.de>
+ * 
+ * @package ilias-core
+ * @version $Id$
+ */
+class Search extends PEAR
+{
+    /**
+     * database handler
+     *
+     * @var object DB
+     */	
+    var $db;
+
+    /**
+     * Constructor
+     *
+     * setup database
+     *
+     * @param object database handler
+     * @param string UserID
+     */
+    function Search(&$dbhandle)
+    {
+		// Initiate variables
+		$this->db =& $dbhandle;
+    }
+
+	/**
+     * search database with given values
+     *
+	 * execute() performs a search on the databasetables
+	 * 
+     * @access private
+     */
+
+     function execute()
+	 {
+		 $this->result = array();
+		
+		 if ($this->text == "")
+		 	return false;
+		 //now only user search and phrase search
+		 //search for login  firstname  surname email
+		 $w = "login LIKE '%".$this->text."%'";
+		 $w .= " OR firstname LIKE '%".$this->text."%'";
+		 $w .= " OR surname LIKE '%".$this->text."%'";
+		 $w .= " OR email LIKE '%".$this->text."%'";
+		 
+		 $query = "SELECT * FROM user_data WHERE ".$w;	
+		 $res = $this->db->query($query);
+		 
+		 if (DB::isError($res)) {
+			 die("<b>".$res->getMessage()."</b><br>Class: ".get_class($this)."<br>Script: ".__FILE__."<br>Line: ".__LINE__);
+		 }
+		 
+		 $this->hits = $res->numRows();
+
+		 if ($this->hits > 0)
+		 {
+			 while ($row = $res->fetchRow(DB_FETCHMODE_ASSOC))
+			 {
+				 $this->result[] = array(
+				 	"text" => "hello",
+					"link" => "search.php"
+				 );
+			 }
+			 return true;
+		 }
+		 else
+		 	return false;
+	 }
+	 
+
+	 /**
+	  * set options
+	  * @param array
+	  * @access public
+	  */
+	 function setOptions($ar)
+	 {
+		 $this->options = $ar;
+	 }
+
+	 /**
+	  * set area
+	  * @access public
+	  * @param string str
+	  */
+	 function setArea($str)
+	 {
+		 $this->area = $str;
+	 }
+
+	 /**
+	  * set searchtext
+	  * @access public
+	  * @param string 
+	  */
+	 function setText($str)
+	 {
+		 $this->text = trim($str);
+	 }
+	 
+} // END class user
+?>
