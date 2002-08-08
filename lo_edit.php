b6129acd18a776b98fc42139dae8c9b54f101ba7
pgabriel <pgabriel@svn.ilias.de>
2002-08-08 15:57:03 +0000

    added many language topics replaced languagevariables
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@121 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- lo_edit.php
+++ lo_edit.php
@@ -13,6 +13,18 @@ $tpl = new Template("tpl.lo_edit.html", false, false);
 
 $tpl->setVariable("TXT_PAGEHEADLINE", $lng->txt("lo_edit"));
 
+$tpl->setVariable("TXT_SEQUENCES", $lng->txt("sequences"));
+$tpl->setVariable("TXT_ONLINE_CHAPTER", $lng->txt("online_chapter"));
+$tpl->setVariable("TXT_VISIBLE_LAYERS", $lng->txt("visible_layers"));
+$tpl->setVariable("TXT_REFRESH", $lng->txt("refresh"));
+$tpl->setVariable("TXT_FAQ_EXERCISE", $lng->txt("faq_exercise"));
+$tpl->setVariable("TXT_CHAPTER_NUMBER", $lng->txt("chapter_number"));
+$tpl->setVariable("TXT_CHAPTER_TITLE", $lng->txt("chapter_title"));
+$tpl->setVariable("TXT_SEQUENCE", $lng->txt("sequence"));
+$tpl->setVariable("TXT_FORUM", $lng->txt("forum"));
+$tpl->setVariable("TXT_STARTPAGE", $lng->txt("startpage"));
+$tpl->setVariable("TXT_NO_TITLE", $lng->txt("no_title"));
+
 $tplmain->setVariable("PAGECONTENT",$tpl->get());
 $tplmain->show();
 
