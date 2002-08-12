5a3f7b007ab16af4e95cbabad3118bf0d2aca570
pgabriel <pgabriel@svn.ilias.de>
2002-08-12 15:51:11 +0000

    added lessons-tree view like admin-interface
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@135 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- admindex.php
+++ admindex.php
@@ -1,12 +1,13 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
 <html>
 <head>
-<title>ILIAS admin interface</title>
+	<title></title>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 </head>
 
 <frameset cols="175,*">
-<frame src="adm_menu.php?expand=1" name="tree" marginwidth="0" marginheight="0" scrolling="auto"/>
-<frame src="content.php" name="content" marginwidth="0" marginheight="0" scrolling="auto"/>
+<frame src="adm_menu.php?expand=1" name="tree" marginwidth="0" marginheight="0" scrolling="auto">
+<frame src="content.php" name="content" marginwidth="0" marginheight="0" scrolling="auto">
 <noframes>
 The administration interface requires a <b>frames-capable</b> web browser.
 </noframes>
---
+++ lesson_menu.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once "include/ilias_header.inc";
+include_once "classes/class.Explorer.php";
+
+$ilias =& new ILIAS;
+
+$expanded = explode('|',$_GET["expand"]);
+
+$tplContent = new Template("explorer.html",true,true);
+
+$explorer = new Explorer($ilias,1);
+$explorer->setOutput(1,0);
+
+$output = $explorer->getOutput();
+$tplContent->setVariable("EXPLORER",$output);
+
+include_once "include/ilias_footer.inc";
+?>
