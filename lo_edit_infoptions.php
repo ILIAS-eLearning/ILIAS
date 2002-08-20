af78e93951fef0a13e03ddf169732a8d9f5435c4
pgabriel <pgabriel@svn.ilias.de>
2002-08-20 15:33:45 +0000

    added language-topics
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@172 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- lo_edit_infoptions.php
+++ lo_edit_infoptions.php
@@ -13,9 +13,31 @@ $tpl = new Template("tpl.lo_edit_infoptions.html", false, false);
 
 $tpl->setVariable("TXT_PAGEHEADLINE", $lng->txt("information_abbr")."/".$lng->txt("options"));
 
+$tpl->setVariable("TXT_TITLE", $lng->txt("title"));
+$tpl->setVariable("TXT_UID", $lng->txt("uid"));
+$tpl->setVariable("TXT_AUTHORS", $lng->txt("authors"));
+$tpl->setVariable("TXT_MEMBERS", $lng->txt("members"));
+$tpl->setVariable("TXT_PUBLISHING_ORGANISATION", $lng->txt("publishing_organisation"));
+$tpl->setVariable("TXT_LANGUAGE", $lng->txt("language"));
+$tpl->setVariable("TXT_SUMMARY", $lng->txt("summary"));
+$tpl->setVariable("TXT_KEYWORDS", $lng->txt("keywords"));
+$tpl->setVariable("TXT_LO_CAT", $lng->txt("lo_categories"));
+$tpl->setVariable("TXT_LEVEL", $lng->txt("level"));
+$tpl->setVariable("TXT_STATUS", $lng->txt("status"));
+$tpl->setVariable("TXT_PUB_DATE", $lng->txt("publication_date"));
+$tpl->setVariable("TXT_PUB", $lng->txt("publication"));
+$tpl->setVariable("TXT_LASTCHANGE", $lng->txt("last_change"));
+
+$tpl->setVariable("TXT_FUNCTIONS", $lng->txt("functions"));
+$tpl->setVariable("TXT_CHANGE_METADATA", $lng->txt("change_metadata"));
+$tpl->setVariable("TXT_PRESENTATION_OPTIONS", $lng->txt("presentation_options"));
+$tpl->setVariable("TXT_CHANGE_LO_INFO", $lng->txt("change_lo_info"));
+$tpl->setVariable("TXT_ANNOUNCE_CHANGES", $lng->txt("announce_changes"));
+$tpl->setVariable("TXT_ASSIGN_LO_FORUM", $lng->txt("assign_lo_forum"));
+
+
 include("./include/inc.lo_buttons.php");
 
-$tpl->parseCurrentBlock();
 
 $tplmain->setVariable("PAGECONTENT",$tpl->get());
 $tplmain->show();
--- lo_edit_questions.php
+++ lo_edit_questions.php
@@ -13,6 +13,14 @@ $tpl = new Template("tpl.lo_edit_questions.html", false, false);
 
 $tpl->setVariable("TXT_PAGEHEADLINE", $lng->txt("list_of_questions"));
 
+$tpl->setVariable("TXT_ID", $lng->txt("id"));
+$tpl->setVariable("TXT_TITLE", $lng->txt("title"));
+$tpl->setVariable("TXT_QUESTION", $lng->txt("question"));
+$tpl->setVariable("TXT_TOTAL", $lng->txt("total"));
+$tpl->setVariable("TXT_LINKED_PAGES", $lng->txt("linked_pages"));
+$tpl->setVariable("TXT_SECTIONS", $lng->txt("sections"));
+$tpl->setVariable("TXT_SET", $lng->txt("set"));
+
 include("./include/inc.lo_buttons.php");
 
 $tpl->parseCurrentBlock();
