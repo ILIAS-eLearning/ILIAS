f73f59dd34e1800be879868ce46f56a185998b77
shofmann <shofmann>
2002-08-23 15:50:46 +0000

    - cleaned up class (removed obsolete functions) - added function switchColor() used for changing colors for multiple rows
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@206 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- classes/class.util.php
+++ classes/class.util.php
@@ -1,252 +1,203 @@
 <?php
 /**
- * util class
- * various functions, usage as namespace
- *
- * @author Sascha Hofmann <shofmann@databay.de>
- * @version $Id$
- * @package ilias-core
- */
+* util class
+* various functions, usage as namespace
+*
+* @author Sascha Hofmann <shofmann@databay.de>
+* @version $Id$
+* @package ilias-core
+*/
 class TUtil
 {
-    /**
-     * Properties
-	 */
-    var $ClassName = "TUtil";    // Eigentlich unn÷tig
-
-    
 	/**
-	 * Fetch system_roles and return them in array(role_id => role_name)
-	 */
-	function getRoles ()
-    {
-        global $ilias;
-        $db = $ilias->db;
-        
-        $res = $db->query("SELECT * FROM object_data
-                    WHERE type = 'role' ORDER BY title");
-        
-        if ($res->numRows() > 0)
-        {
-            while ($data = $res->fetchRow(DB_FETCHMODE_ASSOC))
-            {
-                $arr[$data["obj_id"]] = $data["title"];
-            }
-        }
-        else
-        {
-            return false;
-        }
-        
-        return $arr;
-    }
+	* Properties
+	*/
+	var $ClassName = "TUtil";	// Eigentlich unnötig
 	
 	/**
-	 * Fetch loaded modules or possible modules in context
-	 * @param string
-	 */
+	* Fetch system_roles and return them in array(role_id => role_name)
+	*/
+	function getRoles ()
+	{
+		global $ilias;
+		$db = $ilias->db;
+		
+		$res = $db->query("SELECT * FROM object_data
+					WHERE type = 'role' ORDER BY title");
+		
+		if ($res->numRows() > 0)
+		{
+			while ($data = $res->fetchRow(DB_FETCHMODE_ASSOC))
+			{
+				$arr[$data["obj_id"]] = $data["title"];
+			}
+		}
+		else
+		{
+			return false;
+		}
+		
+		return $arr;
+	}
+	
+	/**
+	* Fetch loaded modules or possible modules in context
+	* @param string
+	*/
 	function getModules ($ATypeList = "")
-    {
-        global $ilias;
+	{
+		global $ilias;
 
 		$rbacadmin = new RbacAdminH($ilias->db);
-        $db = $ilias->db;
-        
+		$db = $ilias->db;
+		
 		$arr = array();
 
-        if (empty($ATypeList))
-        {var_dump("<pre>","hallo","</pre");
-            $query = "SELECT * FROM object_data
-                      WHERE type = 'type'
-                      ORDER BY type";
-        }
-        else
-        {
-            $query = "SELECT * FROM object_data
-                      WHERE title IN ($ATypeList)
-                      AND type='type'";
-        }
+		if (empty($ATypeList))
+		{
+			$query = "SELECT * FROM object_data
+					  WHERE type = 'type'
+					  ORDER BY type";
+		}
+		else
+		{
+			$query = "SELECT * FROM object_data
+					  WHERE title IN ($ATypeList)
+					  AND type='type'";
+		}
 
-        $res = $db->query($query);
-        
+		$res = $db->query($query);
+		
 		$rolf_exist = false;
+		
 		if (count($rbacadmin->getRoleFolderOfObject($_GET["obj_id"])) > 0)
 		{
 			$rolf_exist = true;
 		}
 		
 		if ($res->numRows() > 0)
-        {
-            while ($data = $res->fetchRow(DB_FETCHMODE_ASSOC))
-            {
-                if (!$rolf_exist || ($data["title"] != "rolf"))
+		{
+			while ($data = $res->fetchRow(DB_FETCHMODE_ASSOC))
+			{
+				if (!$rolf_exist || ($data["title"] != "rolf"))
 				{
 					$arr[$data["title"]] = $data["description"];
 				}
-            }
-        }
-       
-        return $arr;
-    }
+			}
+		}
+	   
+		return $arr;
+	}
 
 	
 	/**
-	 * Fetch system_roles and return them in array(role_id => role_name)
-	 */
+	* Fetch system_roles and return them in array(role_id => role_name)
+	*/
 	function getGroups ()
-    {
-        global $ilias;
-        
-        $db = $ilias->db;
-        
-        $db->query("SELECT grp_id,grp_name FROM group_data ORDER BY grp_id");
-        
-        if ($db->num_rows())
-        {
-            while ($db->next_record())
-            {
-                $arr[$db->f("grp_id")] = $db->f("grp_name");
-            }
-        }
-        else
-        {
-            return false;
-        }
-        
-        return $arr;
-    }
-
-/*
-	// get Roles of a group
-    function getRoles ($AGroupId)
-    {
-        global $ilias;
-        
-        $db = $ilias->db;
-        
-        $db->query("SELECT rid,role_name FROM perm_groups WHERE gid=$AGroupId ORDER BY rid");
-        
-        if ($db->num_rows())
-        {
-            while ($db->next_record())
-            {
-                $arr[$db->f("rid")] = $db->f("role_name");
-            }
-        }
-        else
-        {
-            return false;
-        }
-        
-        return $arr;
-    }
-    */
-
-    /**
-	 * builds an array for access output
-	 * @param string
-	 * @param string
-	 */
-    function setAccessString ($ARights,$AVar)
-    {
-        $granted = "<font face=\"courier\" color=\"green\">o</font>";
-        $denied  = "<font face=\"courier\" color=\"red\">x</font>";
-
-        if ($ARights & 1) $arr[$AVar."1"] = $granted; else $arr[$AVar."1"] = $denied;
-        if ($ARights & 2) $arr[$AVar."2"] .= $granted; else $arr[$AVar."2"] .= $denied;
-        if ($ARights & 4) $arr[$AVar."3"] .= $granted; else $arr[$AVar."3"] .= $denied;
-        if ($ARights & 8) $arr[$AVar."4"] .= $granted; else $arr[$AVar."4"] .= $denied;
-        if ($ARights & 16) $arr[$AVar."5"] .= $granted; else $arr[$AVar."5"] .= $denied;
-        if ($ARights & 32) $arr[$AVar."6"] .= $granted; else $arr[$AVar."6"] .= $denied;
-        if ($ARights & 64) $arr[$AVar."7"] .= $granted; else $arr[$AVar."7"] .= $denied;
-        if ($ARights & 128) $arr[$AVar."8"] .= $granted; else $arr[$AVar."8"] .= $denied;
-
-        return $arr;
-    }
+	{
+		global $ilias;
+		
+		$db = $ilias->db;
+		
+		$db->query("SELECT grp_id,grp_name FROM group_data ORDER BY grp_id");
+		
+		if ($db->num_rows())
+		{
+			while ($db->next_record())
+			{
+				$arr[$db->f("grp_id")] = $db->f("grp_name");
+			}
+		}
+		else
+		{
+			return false;
+		}
+		
+		return $arr;
+	}
 	
-    /**
-	 * Builds aa select form field with options and shows the selected option first
-	 * @param string value to be selected
-	 * @param string variable name in formular
-	 * @param array array with $options
-	 * @param boolean
-	 */
-    function formSelect ($selected,$varname,$options,$multiple = false)
-    {
-
+	/**
+	* Builds aa select form field with options and shows the selected option first
+	* @param string value to be selected
+	* @param string variable name in formular
+	* @param array array with $options
+	* @param boolean
+	*/
+	function formSelect ($selected,$varname,$options,$multiple = false)
+	{
 		$multiple ? $multiple = "multiple" : "";
-        $str = "<select name=\"".$varname ."\" ".$multiple.">\n";
+		$str = "<select name=\"".$varname ."\" ".$multiple.">\n";
 
 		foreach ($options as $key => $val)
-        {
+		{
 			$str .= " <option value=\"".$key."\"";
 			
-            if ($selected == $key)
+			if ($selected == $key)
 			{
 				$str .= " selected";
 			}
-            
+			
 			$str .= ">".$val."</option>\n";
-        }
+		}
 
 		$str .= "</select>\n";
 		
 		return $str;
-    }
+	}
 
 	/**
-	 * ???
-* @param string
-* @param string	 
-	 */
-    function getSelectName ($selected,$values)
-    {
-        return($values[$selected]);
-    }
+	* ???
+	* @param string
+	* @param string	 
+	*/
+	function getSelectName ($selected,$values)
+	{
+		return($values[$selected]);
+	}
 
-    /**
-	 * ???
-* @param string	 
-* @param string	 
-* @param string	 
-	 */
-    function formCheckbox ($checked,$varname,$value)
-    {
-        $str = "<input type=\"checkbox\" name=\"".$varname."\"";
-        
+	/**
+	* ???
+	* @param string	 
+	* @param string	 
+	* @param string	 
+	*/
+	function formCheckbox ($checked,$varname,$value)
+	{
+		$str = "<input type=\"checkbox\" name=\"".$varname."\"";
+		
 		if ($checked == 1)
 		{
-		    $str .= " checked";
+			$str .= " checked";
 		}
-        
+		
 		$str .= " value=\"".$value."\">\n";
 		
 		return $str;
-    }
+	}
 
 	/**
-	 * ???
-* @param string	 
-* @param string	 
-* @param string	 
-	 */
-    function formRadioButton($checked,$varname,$value)
-    {
+	* ???
+	* @param string	 
+	* @param string	 
+	* @param string	 
+	*/
+	function formRadioButton($checked,$varname,$value)
+	{
 	$str = "<input type=\"radio\" name=\"".$varname."\"";
 		if ($checked == 1)
 		{
-		    $str .= " checked";
+			$str .= " checked";
 		}
-        
+		
 		$str .= " value=\"".$value."\">\n";
 		
 		return $str;
-    }
+	}
 
 	/**
-	 * ???
-* @param string	 
-	 */
+	* ???
+	* @param string	 
+	*/
 	function checkInput ($vars)
 	{
 		// TO DO:
@@ -254,34 +205,33 @@ class TUtil
 	}
 
 	/**
-	 * ???
-* @param string	 
-	 */
-    function setPathStr ($Apath)
+	* ???
+	* @param string	 
+	*/
+	function setPathStr ($a_path)
 	{
-        if ("" != $Apath && "/" != substr($Apath, -1))
-            $Apath .= "/";
+		if ("" != $a_path && "/" != substr($a_path, -1))
+			$a_path .= "/";
 			
-		return $Apath;
+		return $a_path;
 	}
 	
-	
 	/**
-	 * liefert den owner des objektes $Aobj_id als user_objekt zurück
-* @param string	 
-	 */
+	* liefert den owner des objektes $Aobj_id als user_objekt zurück
+	* @param string	 
+	*/
 	function getOwner ($Aobj_id)
 	{
-        global $ilias;
-        $db = $ilias->db;
+		global $ilias;
+		$db = $ilias->db;
 
-        $query = "SELECT owner FROM object_data
+		$query = "SELECT owner FROM object_data
 				  WHERE obj_id = '".$Aobj_id."'";
 
-        $res = $db->query($query);
-        
-        if ($res->numRows() == 1)
-        {
+		$res = $db->query($query);
+		
+		if ($res->numRows() == 1)
+		{
 			$row = $res->fetchRow(DB_FETCHMODE_ORDERED);
 			$owner_id = $row[0];
 			
@@ -292,15 +242,37 @@ class TUtil
 			}
 
 			$owner = new User($db,$owner_id);
+
 			return $owner;
-        }
-        else
-        {
+		}
+		else
+		{
 			// select liefert falsch row-anzahl oder nix
 			return false;
-        }	
+		}	
+	}
+
+	/**
+	* switches style sheets for each even $a_num
+	* (used for changing colors of different result rows)
+	* 
+	* @access	public
+	* @param	integer	$a_num	the counter
+	* @param	string	$a_css1	name of stylesheet 1
+	* @param	string	$a_css2	name of stylesheet 2
+	* @return	string	$a_css1 or $a_css2
+	*/
+	function switchColor ($a_num,$a_css1,$a_css2)
+	{
+		if (!($a_num % 2))
+		{
+			return $a_css1;	
+		}
+		else
+		{
+			return $a_css2;
+		}
 	}
 	
 } // END class.util
-
 ?>
