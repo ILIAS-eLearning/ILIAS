43f5a96ceba905298d056e95dbb86029f2b5f7aa
Stefan Meyer <smeyer.ilias@gmx.de>
2002-08-26 14:51:22 +0000

    fixed no path found bug for users
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@226 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- content_user.php
+++ content_user.php
@@ -43,7 +43,7 @@ if ($rbacsystem->checkAccess('read',$_GET["obj_id"],$_GET["parent"]))
 			$css_row = TUtil::switchColor($key,"row_high","row_low");
 
 			$node = "[<a href=\"".$SCRIPT_NAME."?obj_id=".$val["id"]."&parent=".$val["parent"]."\">".$val["title"]."</a>]";
-			$tplContent->setVariable("LINK_TARGET","object.php?obj_id=".$val["obj_id"]."&parent=$obj_id&cmd=edit");
+			$tplContent->setVariable("LINK_TARGET","object.php?obj_id=".$val["obj_id"]."&parent=$obj_id&parent_parent=$_GET[parent]&cmd=edit");
 			$tplContent->setVariable("OBJ_TITLE",$val["title"]);
 			$tplContent->setVariable("OBJ_LAST_UPDATE",$val["last_update"]);
 			$tplContent->setVariable("IMG_TYPE","icon_user_b.gif");
