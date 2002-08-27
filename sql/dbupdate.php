8b61c7c5d223c825a0d1543705221bfb47e70c29
shofmann <shofmann>
2002-08-27 14:29:37 +0000

    added new table 'user_pref'. Please update your db.
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@232 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- sql/dbupdate.php
+++ sql/dbupdate.php
@@ -1,2 +1,11 @@
 <#1>
 #intial release of database
+<#2>
+# add table for user preferences
+CREATE TABLE user_pref (
+usr_id INT NOT NULL,
+keyword CHAR(40) NOT NULL,
+value_str CHAR(40),
+value_int BIGINT,
+PRIMARY KEY (usr_id,keyword)
+);
