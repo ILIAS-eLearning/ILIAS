0da339b7355242c9f388e5c977c663eb524ac2d0
pgabriel <pgabriel>
2002-10-04 12:55:30 +0000

    modification in admin section
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@371 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- login.php
+++ login.php
@@ -18,9 +18,7 @@ if ($ilias->auth->getAuth())
 }
 
 //instantiate login template
-$tplContent = new Template("tpl.login.html", true, true);
-
-
+$tpl->addBlockFile("CONTENT", "content", "tpl.login.html");
 
 //language handling
 if ($_GET["lang"] != "")
@@ -39,37 +37,38 @@ $langs = $lng->getInstalledLanguages();
 
 foreach ($langs as $row)
 {
-	$tplContent->setCurrentBlock("languages");
-	$tplContent->setVariable("LANG_ID", $row["id"]);
-	$tplContent->setVariable("LANG_DESC", $row["name"]);
-	$tplContent->setVariable("LANG_IMG", "./lang/".$row["id"].".gif");
-	$tplContent->parseCurrentBlock();
+	$tpl->setCurrentBlock("languages");
+	$tpl->setVariable("LANG_ID", $row["id"]);
+	$tpl->setVariable("LANG_DESC", $row["name"]);
+	$tpl->setVariable("LANG_IMG", "./lang/".$row["id"].".gif");
+	$tpl->parseCurrentBlock();
 }
-$tplContent->setVariable("ILIAS_RELEASE", $ilias->getSetting("ilias_version"));
-$tplContent->setVariable("TXT_ILIAS_LOGIN", $lng->txt("login_to_ilias"));
-$tplContent->setVariable("FORMACTION", "login.php?lang=".$lang);
-$tplContent->setVariable("TXT_USERNAME", $lng->txt("username"));
-$tplContent->setVariable("TXT_PASSWORD", $lng->txt("password"));
-$tplContent->setVariable("TXT_SUBMIT", $lng->txt("submit"));
-$tplContent->setVariable("TXT_CHOOSE_LANGUAGE", $lng->txt("choose_language"));
+$tpl->setVariable("ILIAS_RELEASE", $ilias->getSetting("ilias_version"));
+$tpl->setVariable("TXT_ILIAS_LOGIN", $lng->txt("login_to_ilias"));
+$tpl->setVariable("FORMACTION", "login.php?lang=".$lang);
+$tpl->setVariable("TXT_USERNAME", $lng->txt("username"));
+$tpl->setVariable("TXT_PASSWORD", $lng->txt("password"));
+$tpl->setVariable("TXT_SUBMIT", $lng->txt("submit"));
+$tpl->setVariable("TXT_CHOOSE_LANGUAGE", $lng->txt("choose_language"));
 
 if (!empty($ilias->auth->status) && $ilias->auth->status == AUTH_EXPIRED)
 {
-	$tplContent->setVariable(TXT_MSG_LOGIN_FAILED, $lng->txt("err_session_expired"));
+	$tpl->setVariable(TXT_MSG_LOGIN_FAILED, $lng->txt("err_session_expired"));
 }
 else if (!empty($ilias->auth->status) && $ilias->auth->status == AUTH_IDLED)
 {
-	$tplContent->setVariable(TXT_MSG_LOGIN_FAILED, $lng->txt("err_idled"));
+	$tpl->setVariable(TXT_MSG_LOGIN_FAILED, $lng->txt("err_idled"));
 } 
 else if (!empty ($ilias->auth->status) && $ilias->auth->status == AUTH_WRONG_LOGIN)
 {
-	$tplContent->setVariable(TXT_MSG_LOGIN_FAILED, $lng->txt("err_wrong_login"));
+	$tpl->setVariable(TXT_MSG_LOGIN_FAILED, $lng->txt("err_wrong_login"));
 }
 
-$tplContent->setVariable(PHP_SELF,$_SERVER['PHP_SELF']);
-$tplContent->setVariable(USERNAME,$username);
+$tpl->setCurrentBlock("content");
+$tpl->setVariable(PHP_SELF,$_SERVER['PHP_SELF']);
+$tpl->setVariable(USERNAME,$username);
+$tpl->parseCurrentBlock();
 
-$tplmain->setVariable("PAGECONTENT", $tplContent->get());
-//$tplContent->show();
-$tplmain->show();
+//$tpl->show();
+$tpl->show();
 ?>
