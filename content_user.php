689d61b887c9a054f2e66a9e4f8b295fbe588ba5
pgabriel <pgabriel@svn.ilias.de>
2002-10-04 12:07:29 +0000

    modifications
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@368 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- content_user.php
+++ content_user.php
@@ -1,13 +1,9 @@
 <?php
 require_once "include/ilias_header.inc";
 
-// Template generieren
-$tplContent = new Template("content_user.html",true,true);
-
-//deprecated???
-$tplContent->setVariable("OBJ_SELF","content_user.php?obj_id=".$_GET["obj_id"]."&parent=".$_GET["parent"]);
-$tplContent->setVariable("OBJ_ID",$_GET["obj_id"]);
-$tplContent->setVariable("TPOS",$_GET["parent"]);
+$tpl->addBlockFile("CONTENT", "content", "tpl.adm_content.html");
+$tpl->addBlockFile("ADM_CONTENT", "adm_content", "tpl.adm_usr.html");
+$tpl->addBlockFile("LOCATOR", "locator", "tpl.adm_locator.html");
 
 //show tabs
 $o = array();
@@ -15,23 +11,28 @@ $o["LINK1"] = "content_user.php?obj_id=".$_GET["obj_id"]."&parent=".$_GET["paren
 $o["LINK2"] = "./object.php?obj_id=".$_GET["obj_id"]."&parent=".$_GET["parent"]."&cmd=edit";
 $o["LINK3"] = "./object.php?obj_id=".$_GET["obj_id"]."&parent=".$_GET["parent"]."&cmd=perm";
 $o["LINK4"] = "./object.php?obj_id=".$_GET["obj_id"]."&parent=".$_GET["parent"]."&cmd=owner";
-$tplContent->setVariable("TABS", TUtil::showTabs(1,$o));
+$tpl->setVariable("TABS", TUtil::showTabs(1,$o));
 
 
 // display path
 $path = $tree->showPath($tree->getPathFull(),"content.php");
-$tplContent->setVariable("TREEPATH",$path);
-$tplContent->setVariable("MESSAGE","<h5>Click on the name of a user to edit that user</h5>");
-$tplContent->setVariable("TYPE","user");
+
+$tpl->setCurrentBlock("locator");
+$tpl->setVariable("TREEPATH",$path);
+$tpl->setVariable("PATH", $lng->txt("path"));
+$tpl->parseCurrentBlock();
+
+$tpl->setVariable("TYPE","user");
+$tpl->setVariable("MESSAGE","<h5>Click on the name of a user to edit that user</h5>");
 
 // determine sort direction
 if(!$_GET["direction"] || $_GET["direction"] == 'ASC')
 {
-	$tplContent->setVariable("DIR",'DESC');
+	$tpl->setVariable("DIR",'DESC');
 }
 if($_GET["direction"] == 'DESC')
 {
-	$tplContent->setVariable("DIR",'ASC');
+	$tpl->setVariable("DIR",'ASC');
 }
 
 // set sort column
@@ -53,18 +54,18 @@ if ($rbacsystem->checkAccess('read',$_GET["obj_id"],$_GET["parent"]))
 
 			$node = "[<a href=\"".$SCRIPT_NAME."?obj_id=".$val["id"]."&parent=".$val["parent"]."\">".$val["title"]."</a>]";
 
-			$tplContent->setCurrentBlock("row");
-			$tplContent->setVariable("LINK_TARGET","object.php?obj_id=".$val["obj_id"]."&parent=".$_GET["obj_id"]."&parent_parent=".$_GET["parent"]."&cmd=edit");
-			$tplContent->setVariable("OBJ_TITLE",$val["title"]);
-			$tplContent->setVariable("OBJ_LAST_UPDATE",$val["last_update"]);
-			$tplContent->setVariable("IMG_TYPE","icon_user_b.gif");
-			$tplContent->setVariable("ALT_IMG_TYPE","user");
-			$tplContent->setVariable("CSS_ROW",$css_row);
-			$tplContent->setVariable("OBJ",$val["obj_id"]);
-			$tplContent->parseCurrentBlock("row");
+			$tpl->setCurrentBlock("row");
+			$tpl->setVariable("LINK_TARGET","object.php?obj_id=".$val["obj_id"]."&parent=".$_GET["obj_id"]."&parent_parent=".$_GET["parent"]."&cmd=edit");
+			$tpl->setVariable("OBJ_TITLE",$val["title"]);
+			$tpl->setVariable("OBJ_LAST_UPDATE",$val["last_update"]);
+			$tpl->setVariable("IMG_TYPE","icon_user_b.gif");
+			$tpl->setVariable("ALT_IMG_TYPE","user");
+			$tpl->setVariable("CSS_ROW",$css_row);
+			$tpl->setVariable("OBJ",$val["obj_id"]);
+			$tpl->parseCurrentBlock("row");
 		}
 		
-		$tplContent->touchBlock("options");
+		$tpl->touchBlock("options");
 	}
 }
 else
@@ -74,12 +75,12 @@ else
 
 if ($_GET["message"])
 {
-	$tplContent->setCurrentBlock("sys_message");
-	$tplContent->setVariable("ERROR_MESSAGE",stripslashes($_GET["message"]));
-	$tplContent->parseCurrentBlock();
+	$tpl->addBlockFile("MESSAGE", "message", "tpl.message.html");
+	$tpl->setCurrentBlock("message");
+	$tpl->setVariable("MSG",stripslashes($_GET["message"]));
+	$tpl->parseCurrentBlock();
 }
 
-$tpl->setVariable("CONTENT", $tplContent->get());	
 $tpl->show();
 
 ?>
