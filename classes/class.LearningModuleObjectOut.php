1dc551145ec44c82842c7836ee5300f4ad3a7649
shofmann <shofmann>
2002-12-10 17:00:09 +0000

    moved import dialog to out class of learningmodule object
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@455 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- classes/class.LearningModuleObjectOut.php
+++ classes/class.LearningModuleObjectOut.php
@@ -3,7 +3,7 @@
 * Class LearningModuleObjectOut
 *
 * @author Stefan Meyer <smeyer@databay.de> 
-* $Id$Id$
+* $Id$Id: class.LearningModuleObjectOut.php,v 1.1 2002/12/03 16:50:15 smeyer Exp $
 * 
 * @extends ObjectOut
 * @package ilias-core
@@ -20,6 +20,19 @@ class LearningModuleObjectOut extends ObjectOut
 	function LearningModuleObjectOut($a_data)
 	{
 		$this->ObjectOut($a_data);
+	}
+	
+	function importObject()
+	{
+		$this->getTemplateFile("import");
+		$this->tpl->setVariable("FORMACTION", "adm_object.php?cmd=upload&type=".$_GET["type"].
+						  "&obj_id=".$_GET["obj_id"]."&parent=".$_GET["parent"]."&parent_parent=".$_GET["parent_parent"]);
+		$this->tpl->setVariable("TXT_SAVE", $this->lng->txt("import"));
+	}
+	
+	function uploadObject()
+	{
+		//nada para mirar
 	}
 
 } // END class.LeraningObject
