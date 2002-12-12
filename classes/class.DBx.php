029e6ab98fcbe04e0bf7744f52a18e7e71fe8a0e
shofmann <shofmann@svn.ilias.de>
2002-12-12 12:18:16 +0000

    bugfix: I forgot the fetchmode-parameter :-)
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@460 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- classes/class.DBx.php
+++ classes/class.DBx.php
@@ -117,9 +117,9 @@ class DBx extends PEAR
 	* @param string
 	* @return object DB
 	*/
-	function getRow($sql)
+	function getRow($sql,$mode = "DB_FETCHMODE_OBJECT")
 	{
-		$r = $this->db->getrow($sql);
+		$r = $this->db->getrow($sql,$mode);
 		
 		if (DB::isError($r))
 		{
