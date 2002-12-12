ffd7abd40b467909d8aa3875df426b96b98bd5c1
shofmann <shofmann@svn.ilias.de>
2002-12-12 14:25:10 +0000

    cleaned up import function. import of LOs is now possible again
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@463 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- classes/class.LearningModuleObject.php
+++ classes/class.LearningModuleObject.php
@@ -13,6 +13,7 @@ class LearningModuleObject extends Object
 {
 	/**
 	* Constructor
+	* 
 	* @access public
 	*/
 	function LearningModuleObject()
@@ -22,12 +23,19 @@ class LearningModuleObject extends Object
 
 	function importObject()
 	{
-		//echo "import";
+		// nothing to do. just display the dialogue in Out
 	}
 	
 	/**
-	* additional to normal saveObject method,
-	* a tree entry for each LearnungModule is created
+	* extents inherited saveObject method by
+	* creating a tree entry for each LearningModule
+	* 
+	* @param	integer	object id of new object
+	* @param	integer	object id of parent object
+	* @param	string	object type of parent object
+	* @param	string	object type of new object
+	* @param	array	object data (title,description,owner)
+	* @access	public
 	*/
 	function saveObject($a_obj_id = '', $a_parent = '' ,$a_type = '' , $a_new_type = '' , $a_data = '')
 	{
@@ -38,6 +46,11 @@ class LearningModuleObject extends Object
 		$tree->addTree($obj_id);
 	}
 
+	/**
+	* uploads a complete LearningModule from a LO-XML file
+	* 
+	* @access	public
+	*/
 	function uploadObject()
 	{
 		global $HTTP_POST_FILES;
@@ -59,51 +72,22 @@ class LearningModuleObject extends Object
 			$this->ilias->raiseError("Wrong file type!",$this->ilias->error_obj->MESSAGE);
 		}
 		
-		//parse and/or validate document
-		//$xmldoc = new xml2sql();
+		// create domxml-handler
 		$domxml = new domxml();
 				
+		//get XML-file, parse and/or validate the document
 		$file = $HTTP_POST_FILES["xmldoc"]["name"];
-		//$xmldoc->loadDocument(basename($source),dirname($source),1,$_POST["parse_mode"]);
 		$domxml->loadDocument(basename($source),dirname($source),$_POST["parse_mode"]);
 
-		//$xmldoc->prepareXMLTree($xmldoc->domxml);
+		// remove empty text nodes
 		$domxml->trimDocument();
-		
-		//$lo_elements = $xmldoc->domxml->get_elements_by_tagname("LearningObject");
-		//$elements = $domxml->getElementsByTagname("LearningObject");
 
-##################### separating Learning Objects in one XML-File ####################
-		// step 1: Identify Leaf-LOS (LOs not containing other LOs)
-		// step 2: save Element-Id somewhere
-		// foreach Leaf-LO:		
-		// step 3: create new domxml2
-		// step 4: copy dom-tree of Leaf-LO to new domxml
-		// step 5: remove dom-tree from main domxml
-		// step 6: prepare domxml2
-		// step 7: insert domxml2 into db
-		// step 8: inser new node 'LearningObject' in main domxml with sql_id from removed leaf-LO as attribute
-		// end foreach
-		// start over with step 1 until all LOs are inserted into db
-		// step 9: insert LM-structure in tree!
-		
-		
-		
-		// step 1:
-		//$root = $xmldoc->document_element();
-
-		
-		// erstes element kann rausgelöscht werden, da immer ein LO alles umschliesst.
-
-		
-		
-		// step 9: Ich muss alle LOs wieder nach dem tag <LO> durchforsten, um rekursiv den Baum
-		// zu erzeugen.
-		// Brauche ich den Baum überhaupt?
-		
 		// step 1: Identify Leaf-LOS (LOs not containing other LOs)			
 		while (count($elements = $domxml->getElementsByTagname("LearningObject")) > 1)
 		{
+			// delete first element since its always the root LearningObject
+			array_shift($elements);
+			
 			foreach ($elements as $element)
 			{
 				if ($domxml->isLeafElement($element,"LearningObject",1))
@@ -117,92 +101,43 @@ class LearningModuleObject extends Object
 					// remove the LearningObject from main file
 					$element->unlink_node();
 					
-					// create a new DOM object containing the cutted LearningObject in $subtree
+					// create a new domDocument containing the isolated LearningObject in $subtree
 					$lo = new LearningObject();
-					$node  = $lo->domxml->doc->add_child($subtree);
+					$node  = $lo->domxml->appendChild($subtree);
 				
-					// write in file (TESTING)
-					//$lo->dump_file("c:/htdocs/ilias3/xml/file_".$n.".xml");
-					
 					// get LO informationen (title & description)
-					// a) object_data entry
 					$obj_data = $lo->getInfo();
 		
-					// insert LO into database
-					// b) get mysql_last_insert_id
+					// get unique obj_id of LO
 					$lo_id = createNewObject("lo",$obj_data);
 					
-					// c) insert xml_data into lo_db
-					//$xmltmp = new xml2sql();
-					//$lo->domxml->doc = $lo;
-					$lo->domxml->trimDocument(); 
+					// prepare LO for database insertion
+					$lo->domxml->trimDocument(); // maybe obsolete?
 					$lotree = $lo->domxml->buildTree();
 					
-					// create new xml2sql object for insertion functions
+					// insert LO into lo_database
 					$xml2sql = new xml2sql($lotree,$lo_id);
 					$xml2sql->insertDocument();
 					
-					// d) create a reference in main file with global obj_id of inserted LO
+					// create a reference in main file with global obj_id of inserted LO
 					$domxml->appendReferenceNodeForLO ($parent,$lo_id);
-					
-					// e) continue until only the root LO is left in main file
 				}
 			}
-		}
+		} // END: while. Continue until only the root LO is left in main file
 		
 		// write root LO to file (TESTING)
 		//$xmldoc->domxml->dump_file("c:/htdocs/ilias3/xml/file".$n.".xml");
-		$lo = new LearningObject();
-		$node = $lo->domxml->doc->add_child($domxml->doc->document_element());
+		
+		// insert the remaining root-LO into DB
+		$lo = new LearningObject($domxml->doc);
 		$obj_data = $lo->getInfo();
 		$lo_id = createNewObject("lo",$obj_data);
-		$lo->domxml->doc = $lo;
-		$lo->domxml->trimDocument(); 
 		$lotree = $lo->domxml->buildTree();
 		$xml2sql = new xml2sql($lotree,$lo_id);
 		$xml2sql->insertDocument();
 
-		//$this->displayStructure($xmldoc->xmltree);
-
 		// copying file to server if document is valid (soon...)
 		//move_uploaded_file($a_source,$path()."/".$a_obj_id."_".$a_name);
-					
-		exit;
-	}
-	
-	function displayStructure ($tree)
-	{
-		echo "<table border=\"1\">".
-			 "<tr>".
-			 "<th>id</th>".
-			 "<th>value</th>".
-			 "<th>name</th>".
-			 "<th>type</th>".
-			 "<th>depth</th>".
-			 "<th>parent</th>".
-			 "<th>first</th>".
-			 "<th>prev</th>".
-			 "<th>next</th>".
-			 "<th>left</th>".
-			 "<th>right</th>".
-			 "<th>db_id</th>".
-			 "</tr>";
-
-		foreach ($tree as $id => $node)
-		{	
-			echo "<tr>";
-			echo "<td>".$id."</td>";
-		
-			foreach ($node as $key => $value)
-			{
-				//if ($key != "content")
-				//{
-					echo "<td>".$value."</td>";
-				//}
-			}
-			echo "</tr>";
-		}
-		echo "</table>";	
 	}
 } // END class.LearningModuleObject
 ?>
--- classes/class.LearningObjectObjectOut.php
+++ classes/class.LearningObjectObjectOut.php
@@ -3,21 +3,60 @@
 * Class LearningObjectOut
 *
 * @author Stefan Meyer <smeyer@databay.de> 
+* @author Sascha Hofmann <shofmann@databay.de>
 * $Id$
 * 
 * @extends ObjectOut
 * @package ilias-core
 */
-
 class LearningObjectOut extends Object
 {
 	/**
 	* Constructor
+	*
 	* @access public
 	*/
 	function LearningObject($a_data)
 	{
 		$this->ObjectOut($a_data);
+	}
+
+	/**
+	* display tree structure of a LearningObject
+	* DEBUG function
+	* 
+	* @access	public
+	*/
+	function displayStructure ($a_tree)
+	{
+		echo "<table border=\"1\">".
+			 "<tr>".
+			 "<th>id</th>".
+			 "<th>value</th>".
+			 "<th>name</th>".
+			 "<th>type</th>".
+			 "<th>depth</th>".
+			 "<th>parent</th>".
+			 "<th>first</th>".
+			 "<th>prev</th>".
+			 "<th>next</th>".
+			 "<th>left</th>".
+			 "<th>right</th>".
+			 "<th>db_id</th>".
+			 "</tr>";
+
+		foreach ($a_tree as $id => $node)
+		{	
+			echo "<tr>";
+			echo "<td>".$id."</td>";
+		
+			foreach ($node as $key => $value)
+			{
+				echo "<td>".$value."</td>";
+			}
+			echo "</tr>";
+		}
+		echo "</table>";	
 	}
 } // END class.LeraningObject
 ?>
