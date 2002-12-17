cd7e526bfab41ba65ce2bc861839a53a9fa23e21
Stefan Meyer <smeyer.ilias@gmx.de>
2002-12-17 13:59:24 +0000

    reanimated selection of active roles minor bug fix in error handler
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@486 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- classes/class.UserObject.php
+++ classes/class.UserObject.php
@@ -150,31 +150,18 @@ class UserObject extends Object
 			$data["fields"]["default_role"] = $role;
 			$data["title"] = $user->data["Title"];
 			
-/*
-			if ($_GET["obj_id"] == $_SESSION["AccountId"])
+
+			// BEGIN ACTIVE ROLE
+			$assigned_roles = $rbacreview->assignedRoles($_GET["obj_id"]);
+			foreach ($assigned_roles as $key => $role)
 			{
-				// BEGIN ACTIVE ROLE
-				$assigned_roles = $rbacreview->assignedRoles($_GET["obj_id"]);
-				
-				foreach ($assigned_roles as $key => $role)
-				{
-					// BEGIN TABLE_ROLES
-					$tpl->setCurrentBlock("TABLE_ROLES");
-					$obj = getObject($role);
-					$tpl->setVariable("CSS_ROW_ROLE",$key % 2 ? 'tblrow1' : 'tblrow2');
-					$box = Tutil::formCheckBox(in_array($role,$_SESSION["RoleId"]),'active[]',$role);
-					$tpl->setVariable("CHECK_ROLE",$box);
-					$tpl->setVariable("ROLENAME",$obj["title"]);
-					$tpl->parseCurrentBlock();
-				}
-				
-				$tpl->setCurrentBlock("ACTIVE_ROLE");
-				$tpl->setVariable("ACTIVE_ROLE_OBJ_ID",$_GET["obj_id"]);
-				$tpl->setVariable("ACTIVE_ROLE_TPOS",$_GET["parent"]);
-				$tpl->setVariable("ACTIVE_ROLE_PAR",$_GET["parent_parent"]);
-				$tpl->parseCurrentBlock();
+			   // BEGIN TABLE_ROLES
+			   $obj = getObject($role);
+			   $box = Tutil::formCheckBox(in_array($role,$_SESSION["RoleId"]),'active[]',$role);
+			   $data["active_role"][$role]["checkbox"] = $box;
+			   $data["active_role"][$role]["title"] = $obj["title"];
 			}
-*/
+
 			return $data;
 		}
 		else
@@ -215,21 +202,14 @@ class UserObject extends Object
 	**/
 	function activeRoleSaveObject()
 	{
-		if ($_GET["obj_id"] == $_SESSION["AccountId"])
-		{
-			if (!count($_POST["active"]))
-			{
-				$this->ilias->raiseError("You must leave one active role",$this->ilias->error_obj->WARNING);
-			}
+	   if (!count($_POST["active"]))
+	   {
+		  $this->ilias->raiseError("You must leave one active role",$this->ilias->error_obj->MESSAGE);
+	   }
 
-			$_SESSION["RoleId"] = $_POST["active"];
-		}
-		else
-		{
-			$this->ilias->raiseError("You can only change your own account",$this->ilias->error_obj->WARNING);
-		}
+	   $_SESSION["RoleId"] = $_POST["active"];
 
-		return true;		
+	   return true;
 	}
 } //end class.UserObject
 ?>
