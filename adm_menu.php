ad898930248096afd8bf220b1bd4927781113ccd
Stefan Meyer <smeyer.ilias@gmx.de>
2003-01-10 10:12:21 +0000

    information about expanded nad compressed nodes in explorer view is now saved $_SESSION["expand"]
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@614 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- adm_menu.php
+++ adm_menu.php
@@ -2,12 +2,12 @@
 require_once "include/inc.header.php";
 require_once "classes/class.Explorer.php";
 
+
 $tpl->addBlockFile("CONTENT", "content", "tpl.explorer.html");
 
 $explorer = new Explorer("adm_object.php");
 
 $explorer->setExpand($_GET["expand"]);
-
 $explorer->setOutput(0);
 
 $output = $explorer->getOutput();
