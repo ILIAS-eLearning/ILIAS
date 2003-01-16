54218ffc853b9960746dfe5483508166ee329d47
Stefan Meyer <smeyer.ilias@gmx.de>
2003-01-16 14:09:48 +0000

    added overwritten deleteObject() method for RoleFolder objects
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@660 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- classes/class.RootFolderObject.php
+++ classes/class.RootFolderObject.php
@@ -3,7 +3,7 @@
 * Class RootFolder
 *
 * @author Stefan Meyer <smeyer@databay.de> 
-* @version $Id$Id$
+* @version $Id$Id: class.RootFolderObject.php,v 1.2 2002/12/23 23:45:00 smeyer Exp $
 * 
 * @extends Object
 * @package ilias-core
@@ -17,6 +17,25 @@ class RootFolderObject extends Object
 	function RootFolderObject()
 	{
 		$this->Object();
+	}
+
+	function deleteObject($a_obj_id,$a_parent,$a_tree_id = 1)
+	{
+		global $rbacadmin;
+
+		// GET ALL ROLES OF ROLE FOLDER
+		$all_roles = $rbacadmin->getRolesAssignedToFolder($a_obj_id);
+		
+		// FIRST DELETE THIS ROLES
+		foreach($all_roles as $role_id)
+		{
+			include_once("classes/class.RoleObject.php");
+
+			$role_obj = new RoleObject();
+			$role_obj->deleteObject($role_id,$a_obj_id);
+		}
+		// NOW DELETE ROLE FOLDER
+		parent::deleteObject($a_obj_id,$a_parent,$a_tree_id);
 	}
 } // END class.RootFolder
 ?>
