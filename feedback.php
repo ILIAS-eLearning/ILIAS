93df5b044c7eb0cd96c998415cf717e1e69c8dc9
shofmann <shofmann>
2003-01-22 17:29:00 +0000

    reanimated feedback
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@712 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- feedback.php
+++ feedback.php
@@ -9,15 +9,14 @@
 */
 require_once "./include/inc.header.php";
 
-$tpl = new Template("tpl.feedback.html", false, false);
-$tpl->setVariable("TXT_PAGEHEADLINE", $lng->txt("feedback"));
+$tpl->addBlockFile("CONTENT", "content", "tpl.feedback.html");
 
+$tpl->setCurrentBlock("content");
+$tpl->setVariable("TXT_PAGEHEADLINE", $lng->txt("feedback"));
 $tpl->setVariable("TXT_MSG_DEFAULT",  $lng->txt("type_your_message_here"));
 $tpl->setVariable("TXT_MSG_TO", $lng->txt("message_to"));
 $tpl->setVariable("TXT_YOUR_MSG",  $lng->txt("your_message"));
 $tpl->setVariable("TXT_SEND",  $lng->txt("send"));
-
-$tplmain->setVariable("PAGECONTENT",$tpl->get());
-$tplmain->show();
-session_unregister("Error_Message");
+$tpl->parseCurrentBlock();
+$tpl->show();
 ?>
