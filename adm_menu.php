6270632c1d4e29c8f441d847a84a6406c0a92278
shofmann <shofmann>
2003-02-06 13:34:13 +0000

    set explorer filter rules to display all
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@736 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- adm_menu.php
+++ adm_menu.php
@@ -8,6 +8,19 @@ $tpl->addBlockFile("CONTENT", "content", "tpl.explorer.html");
 $explorer = new Explorer("adm_object.php");
 
 $explorer->setExpand($_GET["expand"]);
+$explorer->addFilter("root");
+$explorer->addFilter("cat");
+$explorer->addFilter("grp");
+$explorer->addFilter("crs");
+$explorer->addFilter("le");
+$explorer->addFilter("frm");
+$explorer->addFilter("lo");
+$explorer->addFilter("rolf");
+$explorer->addFilter("adm");
+$explorer->addFilter("lngf");
+$explorer->addFilter("usrf");
+$explorer->addFilter("objf");
+$explorer->setFiltered(true);
 $explorer->setOutput(0);
 
 $output = $explorer->getOutput();
