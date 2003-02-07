03d6651ffbc19bd5b1ef752021370fe28b353771
mmaschke <mmaschke>
2003-02-07 13:49:11 +0000

    adapted some changes to notes.php
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@750 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- note_new.php
+++ note_new.php
@@ -16,11 +16,16 @@ $myNoteFolder = new NoteFolderObject($ilias->account->Id);
 
 if ($_GET["cmd"] == "edit")
 {
-	$myNotes = $myNoteFolder->getNotes($_GET["id"]);
+	$myNotes = $myNoteFolder->viewNote($_GET["id"]);
+	$obj_note = getObject($_GET["id"]);		//gets title out of object_data
 
-	$tpl->setVariable("LOTITLE", $myNotes->lo_title);
+	$tpl->setVariable("LOTITLE", $obj_note["title"]);
 	$tpl->setVariable("NOTETEXT", $myNotes->text);
 	$tpl->setVariable("LOID", $myNotes->lo_id);
+	if($myNotes->important =='y') $tpl->setVariable("IMPORTANT_CHECKED", "checked");
+	if($myNotes->good =='y') $tpl->setVariable("GOOD_CHECKED", "checked");
+	if($myNotes->bad =='y') $tpl->setVariable("BAD_CHECKED", "checked");
+	if($myNotes->question =='y') $tpl->setVariable("QUESTION_CHECKED", "checked");
 	$tpl->setVariable("FORMACTION", "notes.php?cmd=update&amp;id=".$_GET["id"]);
 }
 else
@@ -30,7 +35,7 @@ else
 $tpl->addBlockFile("CONTENT", "content", "tpl.note_new.html");
 $tpl->addBlockFile("BUTTONS", "buttons", "tpl.buttons.html");
 
-$tpl->setCurrentBlock("btn_row");
+	$tpl->setCurrentBlock("btn_row");
 $tpl->parseCurrentBlock();
 $tpl->setVariable("TXT_LOID", "Learning Object ID");
 $tpl->setVariable("TXT_LOTITLE", "Learning Object Title");
