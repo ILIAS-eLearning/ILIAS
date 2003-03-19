53798f967a5ac0f7b2908510d382f101b74062ba
shofmann <shofmann>
2003-03-19 20:34:50 +0000

    some improvements
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@879 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- feedback.php
+++ feedback.php
@@ -12,15 +12,29 @@ require_once "./include/inc.header.php";
 $tpl->addBlockFile("CONTENT", "content", "tpl.feedback.html");
 $tpl->addBlockFile("MESSAGE","message","tpl.message.html");
 
+$recipient = $ilias->getSetting("feedback_recipient");
+
+if (empty($recipient))
+{
+	$recipient = $ilias->getSetting("admin_email");
+}
+
 if ($_POST["msg_send"])
 {
-	if (mail($ilias->getSetting("feedback_recipient"), "ILIAS 3 Feedback", $_POST["msg_content"]))
+	if (empty($recipient))
 	{
-		$tpl->setVariable("MSG", $lng->txt("mail_not_sent"));	
+		$tpl->setVariable("MSG", $lng->txt("no_recipient")."<br/>".$lng->txt("mail_not_sent"));
 	}
 	else
 	{
-		$tpl->setVariable("MSG", $lng->txt("mail_sent"));
+		if (mail($recipient, "ILIAS 3 Feedback", $_POST["msg_content"]))
+		{
+			$tpl->setVariable("MSG", $lng->txt("mail_not_sent"));	
+		}
+		else
+		{
+			$tpl->setVariable("MSG", $lng->txt("mail_sent"));
+		}
 	}
 }
 
@@ -28,7 +42,7 @@ $tpl->setCurrentBlock("content");
 $tpl->setVariable("TXT_PAGEHEADLINE", $lng->txt("feedback"));
 $tpl->setVariable("TXT_MSG_DEFAULT",  $lng->txt("type_your_message_here"));
 $tpl->setVariable("TXT_MSG_TO", $lng->txt("message_to"));
-$tpl->setVariable("FEEDBACK_RECIPIENT", $ilias->getSetting("feedback_recipient"));
+$tpl->setVariable("FEEDBACK_RECIPIENT", $recipient);
 $tpl->setVariable("TXT_YOUR_MSG",  $lng->txt("your_message"));
 $tpl->setVariable("TXT_SEND",  $lng->txt("send"));
 $tpl->parseCurrentBlock();
