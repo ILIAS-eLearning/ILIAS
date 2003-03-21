f9b629487fbcb0582e55e7c06a14bf108029b62a
shofmann <shofmann>
2003-03-21 11:11:21 +0000

    peace
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@895 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<head>
+<title>ILIAS 3</title>
+</head>
+<body bgcolor="#ffffff">
+<table border="0" height="98%" width="100%" cellpadding="0" cellspacing="0" align="center">
+  <tr valign="middle">
+    <td height="600" valign="middle"> 
+      <div align="center">
+        <table border="0" cellpadding="0" cellspacing="0">
+
+          <tr> 
+            <td> <a href="login.php"> <img src="./images/peace.gif" alt="" border="0"></a>
+      </td>
+   </tr>
+</table>
+</body>
+</html>
+
 <?php
 /**
 * start page of ilias 
@@ -7,11 +25,12 @@
 *
 * @package ilias
 */
-
+/*
 require_once "include/inc.check_pear.php";
 require_once "include/inc.header.php";
 
 $start = $ilias->ini->readVariable("server", "start");
+
 if ($start == "")
 {
 	$start = "login.php";
@@ -19,4 +38,5 @@ if ($start == "")
 
 header("location: ".$start);
 exit();
+*/
 ?>
