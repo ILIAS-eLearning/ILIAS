c0c7fecffa35a638d4a6106abd28696c1264b2ce
Stefan Meyer <smeyer.ilias@gmx.de>
2003-03-21 18:22:58 +0000

    deleted mail button
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@916 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- include/inc.mail_buttons.php
+++ include/inc.mail_buttons.php
@@ -20,11 +20,6 @@ else
 $file_name = basename($_SERVER["SCRIPT_NAME"]);
 
 $tpl->setCurrentBlock("btn_cell");
-$tpl->setVariable("BTN_LINK", "mail.php?mobj_id=$_GET[mobj_id]");
-$tpl->setVariable("BTN_TXT", $lng->txt("mail"));
-$tpl->parseCurrentBlock();
-
-$tpl->setCurrentBlock("btn_cell");
 $tpl->setVariable("BTN_LINK", "mail_new.php?mobj_id=$_GET[mobj_id]&type=new");
 $tpl->setVariable("BTN_TXT", $lng->txt("compose"));
 $tpl->parseCurrentBlock();
