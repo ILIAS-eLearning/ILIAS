3d9ad546f7df449699d9e7f31d78ea7bfb4b5dd7
Stefan Meyer <smeyer.ilias@gmx.de>
2003-03-27 14:24:09 +0000

    added more options for addressbook
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@965 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- mail_addressbook.php
+++ mail_addressbook.php
@@ -10,7 +10,7 @@
 require_once "./include/inc.header.php";
 require_once "./include/inc.mail.php";
 require_once "classes/class.User.php";
-require_once "classes/class.Group.php";
+require_once "classes/class.Mail.php";
 require_once "classes/class.Addressbook.php";
 
 $tpl->addBlockFile("CONTENT", "content", "tpl.mail_addressbook.html");
@@ -23,6 +23,19 @@ include "./include/inc.mail_buttons.php";
 
 $abook = new Addressbook($_SESSION["AccountId"]);
 
+// ADD MAIL SENDER TO ADDRESSBOOK
+if($_GET["type"] == 'add')
+{
+	$umail = new Mail($_SESSION["AccountId"]);
+	$mail_data = $umail->getMail($_GET["mail_id"]);
+
+	$tmp_user = new User($mail_data["sender_id"]);
+	$abook->addEntry($tmp_user->getLogin(),
+					 $tmp_user->getFirstname(),
+					 $tmp_user->getLastname(),
+					 $tmp_user->getEmail());
+	sendInfo($lng->txt("mail_entry_added"));
+}
 // ACTIONS
 if(isset($_POST["cmd"]))
 {
@@ -33,6 +46,17 @@ if(isset($_POST["cmd"]))
 			header("location:mail_addressbook.php?mobj_id=$_GET[mobj_id]");
 			exit();
 
+		case 'edit':
+			if(!is_array($_POST["entry_id"]))
+			{
+				sendInfo($lng->txt("mail_select_one_entry"));
+			}
+			else
+			{
+				$tmp_abook = new Addressbook($_SESSION["AccountId"]);
+				$data = $tmp_abook->getEntry($_POST["entry_id"][0]);
+			}
+			break;
 		case 'delete':
 			if(isset($_POST["confirm"]))
 			{
@@ -40,7 +64,7 @@ if(isset($_POST["cmd"]))
 				{
 					sendInfo($lng->txt("mail_select_one_entry"));
 				}
-				else if($abbok->deleteEntries($_POST["mail_id"]))
+				else if($abook->deleteEntries($_POST["entry_id"]))
 				{
 					sendInfo($lng->txt("mail_deleted_entry"));
 				}
@@ -68,6 +92,30 @@ if(isset($_POST["cmd"]))
 				exit;
 			}
 			break;
+
+		case $lng->txt("change"):
+			if(!is_array($_POST["entry_id"]))
+			{
+				sendInfo($lng->txt("mail_select_one"));
+			}
+			else
+			{
+				$abook->updateEntry($_POST["entry_id"][0],
+									$_POST["login"],
+									$_POST["firstname"],
+									$_POST["lastname"],
+									$_POST["email"]);
+				unset($_POST["entry_id"]);
+				sendInfo($lng->txt("mail_entry_changed"));
+			}
+			break;
+
+		case $lng->txt("add"):
+			$abook->addEntry($_POST["login"],
+							 $_POST["firstname"],
+							 $_POST["lastname"],
+							 $_POST["email"]);
+			sendInfo($lng->txt("mail_entry_added"));
 	}
 }
 
@@ -105,6 +153,7 @@ $tpl->parseCurrentBlock();
 
 $entries = $abook->getEntries();
 
+// SHOW ENTRIES
 if($entries)
 {
 	$counter = 0;
@@ -112,6 +161,10 @@ if($entries)
 	foreach($entries as $entry)
 	{
 		$tpl->setVariable("CSSROW",++$counter%2 ? 'tblrow1' : 'tblrow2');
+		if(is_array($_POST["entry_id"]))
+		{
+			$tpl->setVariable("CHECKED",in_array($entry["addr_id"],$_POST["entry_id"]) ? 'checked' : '');
+		}
 		$tpl->setVariable("ENTRY_ID",$entry["addr_id"]);
 		$tpl->setVariable("LOGIN",$entry["login"]);
 		$tpl->setVariable("FIRSTNAME",$entry["firstname"]);
@@ -126,6 +179,23 @@ else
 	$tpl->setVariable("TXT_ADDR_NO",$lng->txt("mail_search_no"));
 	$tpl->parseCurrentBlock();
 }
+
+// SHOW EDIT FIELD
+$tpl->setVariable("CSSROW_LOGIN",'tblrow1');
+$tpl->setVariable("HEADER_LOGIN",$lng->txt("login"));
+$tpl->setVariable("VALUE_LOGIN",$data["login"]);
+$tpl->setVariable("CSSROW_FIRSTNAME",'tblrow2');
+$tpl->setVariable("HEADER_FIRSTNAME",$lng->txt("firstname"));
+$tpl->setVariable("VALUE_FIRSTNAME",$data["firstname"]);
+$tpl->setVariable("CSSROW_LASTNAME",'tblrow1');
+$tpl->setVariable("HEADER_LASTNAME",$lng->txt("lastname"));
+$tpl->setVariable("VALUE_LASTNAME",$data["lastname"]);
+$tpl->setVariable("CSSROW_EMAIL",'tblrow2');
+$tpl->setVariable("HEADER_EMAIL",$lng->txt("email"));
+$tpl->setVariable("VALUE_EMAIL",$data["email"]);
+
+// SUBMIT VALUE DEPENDS ON $_POST["cmd"]
+$tpl->setVariable("BUTTON_EDIT_ADD",$_POST["cmd"] == 'edit' ? $lng->txt("change") : $lng->txt("add"));
 
 $tpl->show();
 ?>
