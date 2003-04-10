fca6dfb529682da95e69d9491d5ec1703c5f413f
Alexander Killing <killing@leifos.de>
2003-04-10 21:27:51 +0000

    renamed class ilBookmarkFolderGUI -> ilBookmarkAdministrationGUI and removed unused scripts bookmark_new.php and bookmark_newfolder.php
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@1102 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- bookmark_frameset.php
+++ bookmark_frameset.php
@@ -1,7 +1,6 @@
 <?php
-
 /*
-* output bookmark frameset
+* bookmark frameset
 *
 * @author Alex Killing <alex.killing@gmx.de>
 * @package ilias-core
@@ -10,32 +9,8 @@
 
 require_once "./include/inc.header.php";
 
-//$startfilename = $ilias->tplPath.$ilias->account->getPref("skin")."/tpl.bookmark_frameset.html";
+// output frameset for personal bookmark administration
+$tpl = new ilTemplate("tpl.bookmark_frameset.html", false, false);
+$tpl->show();
 
-//if (file_exists($startfilename) and ($_SESSION["viewmode"] == "tree"))
-//{
-	$tpl = new ilTemplate("tpl.bookmark_frameset.html", false, false);
-/*
-	if(isset($_GET["target"]))
-	{
-		$tpl->setVariable("FRAME_RIGHT_SRC",urldecode($_GET["target"]));
-	}
-	else
-	{
-		$tpl->setVariable("FRAME_RIGHT_SRC","mail.php");
-	}*/
-	$tpl->show();
-//}
-/*
-else
-{
-	if(isset($_GET["target"]))
-	{
-		header("location: ".urldecode($_GET["target"]));
-	}
-	else
-	{
-		header("location: mail.php");
-	}
-}*/
 ?>
