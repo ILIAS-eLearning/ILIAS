616816fc8fc960dff2d3485153f2ee486e547863
bschuerm <bschuerm@svn.ilias.de>
2003-04-17 09:48:23 +0000

    Removed peace.gif file. The ILIAS CVS repository is not a suitable place for political statements. If political statements on behalf of the project are made they should be based on a community resolution. They should not just express the political attitude of an individual or parts of the ILIAS community.
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@1121 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- index.php
+++ index.php
@@ -1,21 +1,3 @@
-<head>
-<title>ILIAS 3</title>
-</head>
-<body bgcolor="#ffffff">
-<table border="0" height="98%" width="100%" cellpadding="0" cellspacing="0" align="center">
-  <tr valign="middle">
-    <td height="600" valign="middle"> 
-      <div align="center">
-        <table border="0" cellpadding="0" cellspacing="0">
-
-          <tr> 
-            <td> <a href="login.php"> <img src="./images/peace.gif" alt="" border="0"></a>
-      </td>
-   </tr>
-</table>
-</body>
-</html>
-
 <?php
 /**
 * start page of ilias 
