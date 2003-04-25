1f840836761b4fe67faf36e28ac760e245e604a4
Alexander Killing <killing@leifos.de>
2003-04-25 08:01:36 +0000

    learning module import 2 pre alpha
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@1145 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ classes/class.ilLearningModule.php
@@ -0,0 +1,62 @@
+<?php
+/*
+	+-----------------------------------------------------------------------------+
+	| ILIAS open source                                                           |
+	+-----------------------------------------------------------------------------+
+	| Copyright (c) 1998-2001 ILIAS open source, University of Cologne            |
+	|                                                                             |
+	| This program is free software; you can redistribute it and/or               |
+	| modify it under the terms of the GNU General Public License                 |
+	| as published by the Free Software Foundation; either version 2              |
+	| of the License, or (at your option) any later version.                      |
+	|                                                                             |
+	| This program is distributed in the hope that it will be useful,             |
+	| but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+	| GNU General Public License for more details.                                |
+	|                                                                             |
+	| You should have received a copy of the GNU General Public License           |
+	| along with this program; if not, write to the Free Software                 |
+	| Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
+	+-----------------------------------------------------------------------------+
+*/
+
+require_once("classes/class.ilMetaData.php");
+
+/**
+* Class ilLearningModule
+*
+* This class handles Learning Modules like ilObjLearningModule
+* , maybe they will be merged sometime. This class is only an
+* intermediate test class. All object_data storage and the like is done
+* by ilObjLearningModule. This class represents a LearningModule of ILIAS DTD.
+*
+* @author Alex Killing <alex.killing@gmx.de>
+* @version $Id$
+*
+* @package application
+*/
+class ilLearningModule
+{
+	var $ilias;
+	var $meta_data;
+
+	/**
+	* Constructor
+	* @access	public
+	*/
+	function ilLearningModule()
+	{
+		global $ilias;
+
+		$this->ilias =& $ilias;
+	}
+
+
+	function assignMetaData(&$a_meta_data)
+	{
+		$this->meta_data =& $a_meta_data;
+	}
+
+}
+?>
---
+++ classes/class.ilPageObject.php
@@ -0,0 +1,85 @@
+<?php
+/*
+	+-----------------------------------------------------------------------------+
+	| ILIAS open source                                                           |
+	+-----------------------------------------------------------------------------+
+	| Copyright (c) 1998-2001 ILIAS open source, University of Cologne            |
+	|                                                                             |
+	| This program is free software; you can redistribute it and/or               |
+	| modify it under the terms of the GNU General Public License                 |
+	| as published by the Free Software Foundation; either version 2              |
+	| of the License, or (at your option) any later version.                      |
+	|                                                                             |
+	| This program is distributed in the hope that it will be useful,             |
+	| but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+	| GNU General Public License for more details.                                |
+	|                                                                             |
+	| You should have received a copy of the GNU General Public License           |
+	| along with this program; if not, write to the Free Software                 |
+	| Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
+	+-----------------------------------------------------------------------------+
+*/
+
+require_once("classes/class.ilMetaData.php");
+
+/**
+* Class ilPageObject
+*
+* Handles PageObjects of ILIAS Learning Modules (see ILIAS DTD)
+*
+* @author Alex Killing <alex.killing@gmx.de>
+* @version $Id$
+*
+* @package application
+*/
+class ilPageObject
+{
+	var $ilias;
+	var $meta_data;
+	var $is_alias;
+	var $origin_id;
+
+	/**
+	* Constructor
+	* @access	public
+	*/
+	function ilPageObject()
+	{
+		global $ilias;
+
+		$this->ilias =& $ilias;
+
+		$this->is_alias = false;
+	}
+
+	/**
+	* set wether page object is an alias
+	*/
+	function setAlias($a_is_alias)
+	{
+		$this->is_alias = $a_is_alias;
+	}
+
+	function isAlias()
+	{
+		return $this->is_alias;
+	}
+
+	function setOriginID($a_id)
+	{
+		return $this->origin_id = $a_id;
+	}
+
+	function getOriginID()
+	{
+		return $this->origin_id;
+	}
+
+	function assignMetaData(&$a_meta_data)
+	{
+		$this->meta_data =& $a_meta_data;
+	}
+
+}
+?>
