6f6fc7081e0e50c963b08058909ca9761a04385a
Alexander Killing <killing@leifos.de>
2003-04-25 08:00:00 +0000

    added license header
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@1144 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- classes/class.ilBookmark.php
+++ classes/class.ilBookmark.php
@@ -1,11 +1,33 @@
 <?php
+/*
+	+-----------------------------------------------------------------------------+
+	| ILIAS open source                                                           |
+	+-----------------------------------------------------------------------------+
+	| Copyright (c) 1998-2001 ILIAS open source, University of Cologne            |
+	|                                                                             |
+	| This program is free software; you can redistribute it and/or               |
+	| modify it under the terms of the GNU General Public License                 |
+	| as published by the Free Software Foundation; either version 2              |
+	| of the License, or (at your option) any later version.                      |
+	|                                                                             |
+	| This program is distributed in the hope that it will be useful,             |
+	| but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+	| GNU General Public License for more details.                                |
+	|                                                                             |
+	| You should have received a copy of the GNU General Public License           |
+	| along with this program; if not, write to the Free Software                 |
+	| Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
+	+-----------------------------------------------------------------------------+
+*/
+
 /**
 * Class Bookmarks
 * Bookmark management
 *
-* @author Peter Gabriel <pgabriel@databay.de>
+* @author Alex Killing <alex.killing@gmx.de>
 * @version $Id$
-* 
+*
 * @package application
 */
 class ilBookmark
@@ -21,7 +43,7 @@ class ilBookmark
 	* ilias object
 	* @var object ilias
 	* @access public
-	*/	
+	*/
 	var $ilias;
 	var $tree;
 
