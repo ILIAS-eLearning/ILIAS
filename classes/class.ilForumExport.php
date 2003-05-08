e9dd356c95b623de7307a145e1b15e7fe99e6670
wmerkens <wmerkens@svn.ilias.de>
2003-05-08 16:23:50 +0000

    class for forum export
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@1186 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ classes/class.ilForumExport.php
@@ -0,0 +1,433 @@
+<?php
+/**
+* Class ForumExport
+* core export functions for forum
+*
+* @author Wolfgang Merkens <wmerkens@databay.de>
+* @version $Id$
+*
+* @package ilias-forum
+*/
+class ilForumExport
+{
+	/**
+	* ilias object
+	* @var object ilias
+	* @access public
+	*/
+	var $ilias;
+	
+	/**
+	* database table name
+	* @var string
+	* @see setDbTable(), getDbTable()
+	* @access private
+	*/
+	var $dbTable;
+	
+	/**
+	* class name
+	* @var string class name
+	* @access private
+	*/
+	var $className="ilForumExport";
+	
+	/**
+	* database table field for sorting the results
+	* @var string
+	* @see setOrderField()
+	* @access private
+	*/
+	var $orderField;
+	
+	var $whereCondition = "1";
+	
+	var $txtQuote1 = "[quote]"; 
+	var $txtQuote2 = "[/quote]"; 
+	var $replQuote1 = "<blockquote class=\"quote\"><hr size=\"1\" color=\"#000000\">"; 
+	var $replQuote2 = "<hr size=\"1\" color=\"#000000\"/></blockquote>"; 
+	
+	// max. datasets per page
+	var $pageHits = 20;
+	
+	// object id
+	var $id;
+
+	/**
+	* Constructor
+	* @access	public
+	*/
+	function ilForumExport()
+	{
+		global $ilias;
+
+		$this->ilias =& $ilias;
+	}
+	
+	
+	
+	/**
+	* set database field for sorting results
+	* @param	string	$orderField database field for sorting
+	* @see				$orderField
+	* @access	private
+	*/
+	function setOrderField($orderField)
+	{
+		if ($orderField == "")
+		{
+			die($this->className . "::setOrderField(): No orderField given.");			
+		}
+		else
+		{
+			$this->orderField = $orderField;
+		}
+	}
+	
+	/**
+	* get name of orderField
+	* @return	string	name of orderField
+	* @see				$orderField
+	* @access	public
+	*/
+	function getOrderField()
+	{
+		return $this->orderField;
+	}
+	
+	/**
+	* set database table
+	* @param	string	$dbTable database table
+	* @see				$dbTable
+	* @access	public
+	*/
+	function setDbTable($dbTable)
+	{
+		if ($dbTable == "")
+		{
+			die($this->className . "::setDbTable(): No database table given.");
+		}
+		else
+		{
+			$this->dbTable = $dbTable;
+		}
+	}
+
+	/**
+	* get name of database table
+	* @return	string	name of database table
+	* @see				$dbTable
+	* @access	public
+	*/
+	function getDbTable()
+	{
+		return $this->dbTable;
+	}
+	
+	/**
+	* set content of WHERE condition
+	* @param	string	$whereCondition 
+	* @see				$whereCondition
+	* @access	public
+	*/
+	function setWhereCondition($whereCondition = "1")
+	{
+		$this->whereCondition = $whereCondition;
+		return true;
+	}
+	
+	/**
+	* get content of whereCondition
+	* @return	string 
+	* @see				$whereCondition
+	* @access	public
+	*/
+	function getWhereCondition()
+	{
+		return $this->whereCondition;
+	}
+	
+	
+	/**
+	* get one topic-dataset by WhereCondition
+	* @return	array	$result dataset of the topic
+	* @access	public
+	*/
+	function getOneTopic()
+	{	
+		$query = "SELECT * FROM frm_data WHERE ( ".$this->whereCondition." )";
+		
+		$result = $this->ilias->db->getRow($query, DB_FETCHMODE_ASSOC);
+
+		$this->setWhereCondition("1");
+		
+		$result["top_name"] = trim(stripslashes($result["top_name"]));
+		$result["top_description"] = nl2br(stripslashes($result["top_description"]));
+		
+		return $result;	
+	}
+	
+	
+	
+	/**
+	* get one thread-dataset by WhereCondition
+	* @return	array	$result dataset of the thread
+	* @access	public
+	*/
+	function getOneThread()
+	{	
+		$query = "SELECT * FROM frm_threads WHERE ( ".$this->whereCondition." )";
+		
+		$result = $this->ilias->db->getRow($query, DB_FETCHMODE_ASSOC);
+
+		$this->setWhereCondition("1");
+		
+		$result["thr_subject"] = trim(stripslashes($result["thr_subject"]));
+				
+		return $result;	
+	}
+	
+	
+	
+	/**
+	* get data of the first node from frm_posts_tree and frm_posts
+	* @access	public
+	* @param	integer		tree id	
+	* @return	object		db result object
+	*/
+	function getFirstPostNode($tree_id)
+	{
+		$query = "SELECT * FROM frm_posts, frm_posts_tree ".
+				 "WHERE pos_pk = pos_fk ".				 
+				 "AND parent_pos = 0 ".
+				 "AND thr_fk = '".$tree_id."'";
+		$res = $this->ilias->db->query($query);
+		
+		$row = $res->fetchRow(DB_FETCHMODE_OBJECT);
+
+		return $this->fetchPostNodeData($row);
+	}
+	
+	
+	/**
+	* get all nodes in the subtree under specified node
+	* 
+	* @access	public
+	* @param	array		node_data
+	* @return	array		2-dim (int/array) key, node_data of each subtree node including the specified node
+	*/
+	function getPostTree($a_node)
+	{
+	    $subtree = array();
+	
+		$query = "SELECT * FROM frm_posts_tree ".
+				 "LEFT JOIN frm_posts ON frm_posts.pos_pk = frm_posts_tree.pos_fk ".
+				 "WHERE frm_posts_tree.lft BETWEEN '".$a_node["lft"]."' AND '".$a_node["rgt"]."' ".
+				 "AND thr_fk = '".$a_node["tree"]."'";
+		if ($this->orderField != "")
+			$query .= " ORDER BY ".$this->orderField." DESC";		 
+		
+		$res = $this->ilias->db->query($query);
+		
+		while ($row = $res->fetchRow(DB_FETCHMODE_OBJECT))
+		{
+			$subtree[] = $this->fetchPostNodeData($row);
+		}
+					
+		return $subtree;
+	}
+	
+	
+	
+	/**
+	* get data of parent node from frm_posts_tree and frm_posts
+	* @access	private
+ 	* @param	object	db	db result object containing node_data
+	* @return	array		2-dim (int/str) node_data
+	*/
+	function fetchPostNodeData($a_row)
+	{
+		$data = array(
+					"pos_pk"		=> $a_row->pos_pk,
+					"author"		=> $a_row->pos_usr_id,
+					"message"		=> $a_row->pos_message,
+					"date"			=> $a_row->date,
+					"create_date"	=> $a_row->pos_date,
+					"update"		=> $a_row->pos_update,					
+					"update_user"	=> $a_row->update_user,
+					"tree"			=> $a_row->thr_fk,					
+					"parent"		=> $a_row->parent_pos,
+					"lft"			=> $a_row->lft,
+					"rgt"			=> $a_row->rgt,
+					"depth"			=> $a_row->depth,
+					"id"			=> $a_row->fpt_pk		
+					);
+		
+		$data["message"] = stripslashes($data["message"]);
+		
+		return $data ? $data : array();
+	}
+	
+	
+	
+	/**
+	* get content of given user-ID
+	*
+	* @param	integer $a_user_id: user-ID
+	* @return	object	user object 
+	* @access	public
+   	*/
+	function getUser($a_user_id)
+	{
+		$userObj = new ilObjUser($a_user_id);
+
+		return $userObj;
+	}
+	
+	
+	
+	
+	/**
+    * converts the date format
+    * @param	string	$date 
+    * @return	string	formatted datetime
+    * @access	public
+    */
+    function convertDate($date)
+    {
+        global $lng;
+		
+		if ($date > date("Y-m-d H:i:s", mktime(0, 0, 0, date("m"), date("d"), date("Y"))))
+        {
+			return  $lng->txt("today").", ".ilFormat::formatDate($date,"time");
+		}
+		
+		return ilFormat::formatDate($date);
+    }
+	
+	
+	
+	/**
+   	* get number of articles from given user-ID
+	*
+	* @param	integer	$user: user-ID
+	* @return	integer
+	* @access	public
+   	*/
+	function countUserArticles($user)
+	{
+		$q = "SELECT * FROM frm_posts WHERE ";
+		$q .= "pos_usr_id ='".$user."'";
+				
+		$res = $this->ilias->db->query($q);			
+		
+		return $res->numRows();
+	}
+	
+	
+	
+	/**
+	* prepares given string
+	* @access	public
+	* @param	string	
+	* @param	integer	
+	* @return	string	
+	*/
+	function prepareText($text,$edit=0)
+	{		
+		global $lng;
+
+		if ($edit == 1)
+		{
+			$text = str_replace($this->txtQuote1, "", $text);		
+			$text = str_replace($this->txtQuote2, "", $text);		
+			$text = $this->txtQuote1.$text.$this->txtQuote2;
+		}
+		else
+		{		
+			// check for quotation		
+			$startZ = substr_count ($text, $this->txtQuote1);
+			$endZ = substr_count ($text, $this->txtQuote2);
+			
+			
+			if ($startZ > 0 || $endZ > 0)
+			{
+				if ($startZ > $endZ) 
+				{
+					$diff = $startZ - $endZ;
+
+					for ($i = 0; $i < $diff; $i++)
+					{
+						$text .= $this->txtQuote2;
+					}
+				}
+				elseif ($startZ < $endZ)
+				{
+					$diff = $endZ - $startZ;
+
+					for ($i = 0; $i < $diff; $i++)
+					{				
+						$text = $this->txtQuote1.$text;
+					}
+				}
+				
+				// only one txtQuote can exist...
+				if ($startZ > 1)
+				{
+					$start_firstPos = strpos($text, $this->txtQuote1);				
+					$text_s2 = str_replace($this->txtQuote1, "", substr($text, ($start_firstPos+strlen($this->txtQuote1))));
+					$text_s1 = substr($text, 0, ($start_firstPos+strlen($this->txtQuote1)));
+					$text = $text_s1.$text_s2;				
+				}
+				if ($endZ > 1)
+				{				
+					$end_firstPos = strrpos($text, $this->txtQuote2);				
+					$text_e1 = str_replace($this->txtQuote2, "", substr($text, 0, $end_firstPos));
+					$text_e2 = substr($text, $end_firstPos);
+					$text = $text_e1.$text_e2;					
+				}			
+				
+				if ($edit == 0)
+				{
+					$text = str_replace($this->txtQuote1, "<blockquote><span><b>".$lng->txt("quote")."</b>:</blockquote>".$this->replQuote1, $text);		
+					$text = str_replace($this->txtQuote2, $this->replQuote2, $text);					
+				}
+			}		
+		}
+		$text = stripslashes($text);
+		return $text;
+	}
+	
+	
+	
+	/**
+	* get one post-dataset 
+	* @param    integer post id 
+	* @return	array result dataset of the post
+	* @access	public
+	*/
+	function getOnePost($post)
+	{				
+		$q = "SELECT frm_posts.*, usr_data.lastname FROM frm_posts, usr_data WHERE ";		
+		$q .= "pos_pk = '".$post."' AND ";
+		$q .= "pos_usr_id = usr_id";		
+
+		$result = $this->ilias->db->getRow($q, DB_FETCHMODE_ASSOC);
+		
+		$ROW = $this->fetchPostNodeData($result);
+				
+		$result["create_date"] = $this->convertDate($result["pos_date"]);		
+		$result["message"] = nl2br(stripslashes($result["pos_message"]));
+		$result["author"] = nl2br(stripslashes($result["pos_usr_id"]));
+		$result["date"] = date;
+		$result["update"] = $result["pos_update"];		
+			
+				
+		return $result;
+	}
+	
+	
+	
+
+	
+} // END class.ForumExport
