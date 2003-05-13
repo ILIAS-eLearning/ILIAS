759829d70acd771fee825fab8b969ec066d2c60e
Alexander Killing <killing@leifos.de>
2003-05-13 20:05:33 +0000

    added some meta data fields
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@1262 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- sql/dbupdate.php
+++ sql/dbupdate.php
@@ -916,4 +916,10 @@ $res = $this->db->query($query);
 $query = "INSERT INTO rbac_pa ". 
          " VALUES('2','a:6:{i:0;s:1:\"1\";i:1;s:1:\"3\";i:2;s:2:\"11\";i:3;s:2:\"12\";i:4;s:1:\"2\";i:5;s:1:\"4\";}','".$ref_id."','".$ref_id."')";
 $res = $this->db->query($query);
+
 ?>
+
+<#37>
+ALTER TABLE meta_data ADD column language VARCHAR(200) NOT NULL DEFAULT '';
+ALTER TABLE meta_data ADD column keyword BLOB NOT NULL DEFAULT '';
+ALTER TABLE meta_data ADD column description BLOB NOT NULL DEFAULT '';
