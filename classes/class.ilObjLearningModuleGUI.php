f0e646ba216f1d11c2326298ded07aa9f81b4527
Stefan Meyer <smeyer.ilias@gmx.de>
2003-05-16 13:39:22 +0000

    added gpl header
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@1301 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- classes/class.ilObjLearningModuleGUI.php
+++ classes/class.ilObjLearningModuleGUI.php
@@ -1,10 +1,33 @@
 <?php
+/*
+	+-----------------------------------------------------------------------------+
+	| ILIAS open source                                                           |
+	+-----------------------------------------------------------------------------+
+	| Copyright (c) 1998-2001 ILIAS open source, University of Cologne            |
+	|                                                                             |
+	| This program is free software; you can redistribute it and/or               |
+	| modify it under the terms of the GNU General Public License                 |
+	| as published by the Free Software Foundation; either version 2              |
+	| of the License, or (at your option) any later version.                      |
+	|                                                                             |
+	| This program is distributed in the hope that it will be useful,             |
+	| but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+	| GNU General Public License for more details.                                |
+	|                                                                             |
+	| You should have received a copy of the GNU General Public License           |
+	| along with this program; if not, write to the Free Software                 |
+	| Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
+	+-----------------------------------------------------------------------------+
+*/
+
+
 /**
 * Class ilObjLearningModuleGUI
 *
 * @author Stefan Meyer <smeyer@databay.de>
 * @author Sascha Hofmann <shofmann@databay.de>
-* $Id$Id: class.ilObjLearningModuleGUI.php,v 1.9 2003/04/29 07:33:18 akill Exp $
+* $Id$Id: class.ilObjLearningModuleGUI.php,v 1.10 2003/05/14 17:16:56 shofmann Exp $
 * 
 * @extends ilObjectGUI
 * @package ilias-core
--- classes/class.ilObjTypeDefinitionGUI.php
+++ classes/class.ilObjTypeDefinitionGUI.php
@@ -1,9 +1,32 @@
 <?php
+/*
+	+-----------------------------------------------------------------------------+
+	| ILIAS open source                                                           |
+	+-----------------------------------------------------------------------------+
+	| Copyright (c) 1998-2001 ILIAS open source, University of Cologne            |
+	|                                                                             |
+	| This program is free software; you can redistribute it and/or               |
+	| modify it under the terms of the GNU General Public License                 |
+	| as published by the Free Software Foundation; either version 2              |
+	| of the License, or (at your option) any later version.                      |
+	|                                                                             |
+	| This program is distributed in the hope that it will be useful,             |
+	| but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+	| GNU General Public License for more details.                                |
+	|                                                                             |
+	| You should have received a copy of the GNU General Public License           |
+	| along with this program; if not, write to the Free Software                 |
+	| Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
+	+-----------------------------------------------------------------------------+
+*/
+
+
 /**
 * Class ilObjTypeDefinitionGUI
 *
 * @author Stefan Meyer <smeyer@databay.de>
-* $Id$Id: class.ilObjTypeDefinitionGUI.php,v 1.2 2003/03/28 10:30:36 shofmann Exp $
+* $Id$Id: class.ilObjTypeDefinitionGUI.php,v 1.3 2003/03/31 09:38:20 akill Exp $
 *
 * @extends ilObjectGUI
 * @package ilias-core
