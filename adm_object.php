fc37f31753effc2a22466fa802b8b2a6712f3467
Stefan Meyer <smeyer.ilias@gmx.de>
2003-05-16 13:24:51 +0000

    added gpl header
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@1300 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- adm_object.php
+++ adm_object.php
@@ -1,4 +1,27 @@
 <?php
+/*
+	+-----------------------------------------------------------------------------+
+	| ILIAS open source                                                           |
+	+-----------------------------------------------------------------------------+
+	| Copyright (c) 1998-2001 ILIAS open source, University of Cologne            |
+	|                                                                             |
+	| This program is free software; you can redistribute it and/or               |
+	| modify it under the terms of the GNU General Public License                 |
+	| as published by the Free Software Foundation; either version 2              |
+	| of the License, or (at your option) any later version.                      |
+	|                                                                             |
+	| This program is distributed in the hope that it will be useful,             |
+	| but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+	| GNU General Public License for more details.                                |
+	|                                                                             |
+	| You should have received a copy of the GNU General Public License           |
+	| along with this program; if not, write to the Free Software                 |
+	| Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
+	+-----------------------------------------------------------------------------+
+*/
+
+
 /**
 * adm_object
 * main script for administration console
@@ -85,7 +108,7 @@ $method = $cmd."Object";
 $class_name = $objDefinition->getClassName($obj_type);
 $class_constr = "ilObj".$class_name."GUI";
 require_once("./classes/class.ilObj".$class_name."GUI.php");
-//echo "$class_constr().$method<br>"; //exit;
+#echo "$class_constr().$method<br>";
 $obj = new $class_constr($data, $id, $call_by_reference);
 $obj->$method();
 
