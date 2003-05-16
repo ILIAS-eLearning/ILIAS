0a1882daf5d40cbda10f11df191be9fa62150519
Stefan Meyer <smeyer.ilias@gmx.de>
2003-05-16 13:39:22 +0000

    added gpl header
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@1301 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- classes/class.ilObjGroupGUI.php
+++ classes/class.ilObjGroupGUI.php
@@ -1,9 +1,32 @@
 <?php
+/*
+	+-----------------------------------------------------------------------------+
+	| ILIAS open source                                                           |
+	+-----------------------------------------------------------------------------+
+	| Copyright (c) 1998-2001 ILIAS open source, University of Cologne            |
+	|                                                                             |
+	| This program is free software; you can redistribute it and/or               |
+	| modify it under the terms of the GNU General Public License                 |
+	| as published by the Free Software Foundation; either version 2              |
+	| of the License, or (at your option) any later version.                      |
+	|                                                                             |
+	| This program is distributed in the hope that it will be useful,             |
+	| but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+	| GNU General Public License for more details.                                |
+	|                                                                             |
+	| You should have received a copy of the GNU General Public License           |
+	| along with this program; if not, write to the Free Software                 |
+	| Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
+	+-----------------------------------------------------------------------------+
+*/
+
+
 /**
 * Class ilObjGroupGUI
 *
 * @author Stefan Meyer <smeyer@databay.de> 
-* $Id$Id: class.ilObjGroupGUI.php,v 1.4 2003/04/28 15:02:27 mrus Exp $
+* $Id$Id: class.ilObjGroupGUI.php,v 1.5 2003/05/12 13:02:48 mmaschke Exp $
 * 
 * @extends ilObjectGUI
 * @package ilias-core
--- classes/class.ilObjRoleGUI.php
+++ classes/class.ilObjRoleGUI.php
@@ -1,9 +1,32 @@
 <?php
+/*
+	+-----------------------------------------------------------------------------+
+	| ILIAS open source                                                           |
+	+-----------------------------------------------------------------------------+
+	| Copyright (c) 1998-2001 ILIAS open source, University of Cologne            |
+	|                                                                             |
+	| This program is free software; you can redistribute it and/or               |
+	| modify it under the terms of the GNU General Public License                 |
+	| as published by the Free Software Foundation; either version 2              |
+	| of the License, or (at your option) any later version.                      |
+	|                                                                             |
+	| This program is distributed in the hope that it will be useful,             |
+	| but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+	| GNU General Public License for more details.                                |
+	|                                                                             |
+	| You should have received a copy of the GNU General Public License           |
+	| along with this program; if not, write to the Free Software                 |
+	| Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
+	+-----------------------------------------------------------------------------+
+*/
+
+
 /**
 * Class ilObjRoleGUI
 *
 * @author Stefan Meyer <smeyer@databay.de> 
-* $Id$Id: class.ilObjRoleGUI.php,v 1.21 2003/05/15 13:04:17 smeyer Exp $
+* $Id$Id: class.ilObjRoleGUI.php,v 1.22 2003/05/15 13:43:14 smeyer Exp $
 * 
 * @extends ilObjectGUI
 * @package ilias-core
