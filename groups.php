6e63cca80530c104dca19d4add69deb81b69eb24
neiken <neiken@svn.ilias.de>
2003-06-04 12:05:49 +0000

    *** empty log message ***
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@1360 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- groups.php
+++ groups.php
@@ -32,67 +32,13 @@
 */
 require_once "./include/inc.header.php";
 
-$grp_sys[] = array("name" => "Administrator",
-				"desc" => "System Administrators",
-				"owner" => "System Administrator [root]"
-			);
+require_once "./classes/class.ilObjGroupGUI.php";
 
-			
-$groups = array();
 
-//go through valid objects and filter out the lessons only
-$objects = $tree->getChilds(ROOT_FOLDER_ID,"title");
+infoPanel();
 
-if (count($objects) > 0)
-{
-	foreach ($objects as $key => $object)
-	{
-		if (($object["type"] == "cat" || $object["type"] == "grp") && $rbacsystem->checkAccess('visible',$object["child"]))
-		{
-			$groups[$key] = $object;
-		}
-	}
-}
+$ilObjGroupGUI = new ilObjGroupGUI(array(), $obj_id, true);
+$ilObjGroupGUI->listGroups();
 
-//TODO: maybe move the code above to this method
-//$groups = $ilias->account->getGroups();
-
-$tpl->addBlockFile("CONTENT", "content", "tpl.groups.html");
-$tpl->addBlockFile("BUTTONS", "buttons", "tpl.buttons.html");
-
-sendInfo("Not available in this release.");
-
-/*
-$tpl->setCurrentBlock("btn_cell");
-$tpl->setVariable("BTN_LINK","group_new.php");
-$tpl->setVariable("BTN_TXT", $lng->txt("new_group"));
-$tpl->parseCurrentBlock();
-$tpl->touchBlock("btn_row");
-
-$i=0;
-
-foreach ($grp_sys as $row)
-{
-	$i++;
-	$tpl->setCurrentBlock("group_row");
-	$tpl->setVariable("ROWCOL","tblrow".(($i%2)+1));
-	$tpl->setVariable("GRP_NAME", $row["name"]);
-	$tpl->setVariable("GRP_DESC", $row["desc"]);
-	$tpl->setVariable("GRP_OWNER", $row["owner"]);
-	$tpl->parseCurrentBlock();
-}
-
-$tpl->setCurrentBlock("group");
-$tpl->setVariable("TXT_GRP_TITLE", $lng->txt("system_groups"));
-$tpl->setVariable("TXT_NAME", $lng->txt("name"));
-$tpl->setVariable("TXT_DESC", $lng->txt("description"));
-$tpl->setVariable("TXT_OWNER", $lng->txt("owner"));
-
-$tpl->parseCurrentBlock("group");
-
-$tpl->setCurrentBlock("content");
-$tpl->setVariable("TXT_GROUPS", $lng->txt("groups"));
-$tpl->parseCurrentBlock();
-*/
 $tpl->show();
 ?>
