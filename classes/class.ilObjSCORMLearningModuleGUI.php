55edeb767bd884df407d6a8e0a1b9e7b531556cf
Alexander Killing <killing@leifos.de>
2003-06-22 22:06:00 +0000

    scorm package upload form, some other basics, doesnt do anything
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@1472 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- classes/class.ilObjSCORMLearningModuleGUI.php
+++ classes/class.ilObjSCORMLearningModuleGUI.php
@@ -49,5 +49,75 @@ class ilObjSCORMLearningModuleGUI extends ilObjectGUI
 
 	}
 
+	/**
+	* no manual SCORM creation, only import at the time
+	*/
+	function createObject()
+	{
+		$this->importObject();
+	}
+
+	/**
+	* display dialogue for importing SCORM package
+	*
+	* @access	public
+	*/
+	function importObject()
+	{
+		// display import form
+		$this->tpl->addBlockFile("ADM_CONTENT", "adm_content", "tpl.slm_import.html");
+		$this->tpl->setVariable("FORMACTION", "adm_object.php?&ref_id=".$_GET["ref_id"]."&cmd=gateway&new_type=slm");
+		$this->tpl->setVariable("BTN_NAME", "upload");
+		$this->tpl->setVariable("TXT_UPLOAD", $this->lng->txt("upload"));
+		$this->tpl->setVariable("TXT_IMPORT_SLM", $this->lng->txt("import_slm"));
+		$this->tpl->setVariable("TXT_SELECT_FILE", $this->lng->txt("select_file"));
+	}
+
+	/**
+	* display status information or report errors messages
+	* in case of error
+	*
+	* @access	public
+	*/
+	function uploadObject()
+	{
+		global $HTTP_POST_FILES, $rbacsystem;
+
+		// check if file was uploaded
+		$source = $HTTP_POST_FILES["scormfile"]["tmp_name"];
+		if (($source == 'none') || (!$source))
+		{
+			$this->ilias->raiseError("No file selected!",$this->ilias->error_obj->MESSAGE);
+		}
+		// check create permission
+		if (!$rbacsystem->checkAccess("create", $_GET["ref_id"], $_GET["new_type"]))
+		{
+			$this->ilias->raiseError($this->lng->txt("no_create_permission"), $this->ilias->error_obj->WARNING);
+		}
+
+		// create and insert object in objecttree
+		require_once("classes/class.ilObjSCORMLearningModule.php");
+		$newObj = new ilObjSCORMLearningModule();
+		$newObj->setType("slm");
+		$newObj->setTitle("temp title");				// should be set by
+		$newObj->setDescription("temp description");	// import parser
+		$newObj->create();
+		$newObj->createReference();
+		$newObj->putInTree($_GET["ref_id"]);
+		$newObj->setPermissions($_GET["ref_id"]);
+
+		// todo: create directory, copy file to directory
+
+		// start SCORM package parser
+		require_once ("content/classes/SCORM/class.ilSCORMPackageParser.php");
+		$slmParser = new ilSCORMPackageParser($newObj, $HTTP_POST_FILES["scormfile"]["tmp_name"]);
+		$slmParser->startParsing();
+
+		header("Location: adm_object.php?".$this->link_params);
+		exit();
+	}
+
+
+
 }
 ?>
