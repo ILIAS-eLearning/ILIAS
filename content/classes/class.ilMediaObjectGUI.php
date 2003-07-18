356c3572c9267b676a58c3e8b5b73e37165ccb4f
Alexander Killing <killing@leifos.de>
2003-07-18 07:33:19 +0000

    added helptext
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@1659 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- content/classes/class.ilMediaObjectGUI.php
+++ content/classes/class.ilMediaObjectGUI.php
@@ -70,6 +70,7 @@ class ilMediaObjectGUI extends ilPageContentGUI
 		$this->tpl->setVariable("TXT_STANDARD_VIEW", $this->lng->txt("cont_std_view"));
 		$this->tpl->setVariable("TXT_FILE", $this->lng->txt("cont_file"));
 		$this->tpl->setVariable("TXT_REFERENCE", $this->lng->txt("cont_reference"));
+		$this->tpl->setVariable("TXT_REF_HELPTEXT", $this->lng->txt("cont_ref_helptext"));
 		$this->tpl->setVariable("TXT_WIDTH", $this->lng->txt("cont_width"));
 		$this->tpl->setVariable("TXT_HEIGHT", $this->lng->txt("cont_height"));
 		$this->tpl->setVariable("TXT_ORIGINAL_SIZE", $this->lng->txt("cont_orig_size"));
