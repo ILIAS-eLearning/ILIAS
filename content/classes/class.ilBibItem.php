cd8f40ed3a5d0a11bde3890741cd7799371c9409
Aresch Yavari <ay@databay.de>
2003-07-25 13:27:10 +0000

    zum Sammeln der XML-DAten beim Import.
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@1712 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ content/classes/class.ilBibItem.php
@@ -0,0 +1,112 @@
+<?php
+/*
+	+-----------------------------------------------------------------------------+
+	| ILIAS open source                                                           |
+	+-----------------------------------------------------------------------------+
+	| Copyright (c) 1998-2001 ILIAS open source, University of Cologne            |
+	|                                                                             |
+	| This program is free software; you can redistribute it and/or               |
+	| modify it under the terms of the GNU General Public License                 |
+	| as published by the Free Software Foundation; either version 2              |
+	| of the License, or (at your option) any later version.                      |
+	|                                                                             |
+	| This program is distributed in the hope that it will be useful,             |
+	| but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+	| GNU General Public License for more details.                                |
+	|                                                                             |
+	| You should have received a copy of the GNU General Public License           |
+	| along with this program; if not, write to the Free Software                 |
+	| Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
+	+-----------------------------------------------------------------------------+
+*/
+
+require_once ("classes/class.ilMetaData.php");
+
+/**
+* Class ilBibItem
+*
+* Handles Bib-Items of ILIAS DigiLib-Books (see ILIAS DTD)
+*
+* @author Databay AG <ay@databay.de>
+* @version $Id$
+*
+* @package application
+*/
+class ilBibItem extends ilMetaData
+{
+
+	//fbo;
+	var $encoding;
+	//end fbo
+
+	
+	/**
+	* Constructor
+	* @access	public
+	*/
+	function ilBibItem($a_type = "", $a_id = 0)
+	{
+		global $ilias;
+
+		$this->ilias =& $ilias;
+
+		$this->import_id = array();
+		$this->title = "";
+		$this->language = array();
+		$this->description = array();
+		$this->keyword = array();
+		$this->technicals = array();	// technical sections
+		$this->coverage = "";
+		$this->structure = "";
+		$this->type = $a_type;
+		$this->id = $a_id;
+
+		if($a_id != 0)
+		{
+			$this->read();
+		}
+	}
+
+
+	//fbo:
+	
+	/**
+	* set xml content of BibItem, start with <BibItem...>,
+	* end with </BibItemt>, comply with ILIAS DTD, use utf-8!
+	*
+	* @param	string		$a_xml			xml content
+	* @param	string		$a_encoding		encoding of the content (here is no conversion done!
+	*										it must be already utf-8 encoded at the time)
+	*/
+	function setXMLContent($a_xml, $a_encoding = "UTF-8")
+	{
+		$this->encoding = $a_encoding;
+		$this->xml = $a_xml;
+	}
+
+
+	/**
+	* append xml content to BibItem
+	* setXMLContent must be called before and the same encoding must be used
+	*
+	* @param	string		$a_xml			xml content
+	*/
+	function appendXMLContent($a_xml)
+	{
+		$this->xml.= $a_xml;
+	}
+
+
+	/**
+	* get xml content of BibItem
+	*/
+	function getXMLContent()/*$a_incl_head = false*/
+	{
+
+        return $this->xml;
+	}
+
+
+}
+?>
