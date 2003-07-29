b499540a72738bb9b7e468e7da16609df6198c0f
shofmann <shofmann>
2003-07-29 10:13:46 +0000

    fixed session expiry issue. *sigh* setExpire is called twice. The first call inits the default expiry value (from ilias.ini), the second call adds the expiry time on every click.
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@1735 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- include/inc.header.php
+++ include/inc.header.php
@@ -104,8 +104,10 @@ if (DEBUG)
 }
 
 //authenticate & start session
+$ilias->auth->setExpire($ilias->ini->readVariable("session","expire"));
 $ilias->auth->setExpire($ilias->ini->readVariable("session","expire"),true);
 $ilias->auth->start();
+
 
 // start logging
 $log = new ilLog("ilias.log");
