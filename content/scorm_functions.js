ac6825e3fdb08d57f2634118d0505f8d8c018377
rbarthel <rbarthel>
2003-08-14 14:06:01 +0000

    new  version content starts in new  window
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@1887 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- content/scorm_functions.js
+++ content/scorm_functions.js
@@ -1,37 +1,44 @@
 function init(){
-  obj=window.document.APIAdapter;
+
+  API=this.frames[0].window.document.APIAdapter;
   
 }
 
 function LMSGetValue(dataModelElement){
- return obj.LMSGetValue(dataModelElement);
+
+ return API.LMSGetValue(dataModelElement);
 }
 
 function LMSSetValue(dataModelElement,elemValue){
- return obj.LMSSetValue(dataModelElement,elemValue);
+alert("called");
+retval=API.LMSSetValue(dataModelElement,elemValue);
+alert(retval);
+return retval;
+ //return obj.LMSSetValue(dataModelElement,elemValue);
 }
 
 function LMSInitialize(inString){
- return obj.LMSInitialize(inString);
+
+ return API.LMSInitialize(inString);
 }
 
 function LMSFinish(inString){
 
- return obj.LMSFinish(inString);
+ return API.LMSFinish(inString);
 }
 
 function LMSCommit(inString){
- return obj.LMSCommit(inString);
+ return API.LMSCommit(inString);
 }
 
 function LMSGetLastError(){
- return obj.LMSGetLastError();
+ return API.LMSGetLastError();
 }
 
 function LMSGetErrorString(errorNumber){
- return obj.LMSGetErrorString(errorNumber);
+ return API.LMSGetErrorString(errorNumber);
 }
 
 function LMSGetDiagnostics(inString){
- return obj.LMSGetDiagnostics(inString);
+ return API.LMSGetDiagnostics(inString);
 }
