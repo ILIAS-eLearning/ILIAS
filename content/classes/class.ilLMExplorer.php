7472c64b0b5867ca21897754294b60fcc1fa7492
Alexander Killing <killing@leifos.de>
2003-08-14 13:31:15 +0000

    simplified a lot of code
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@1884 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- content/classes/class.ilLMExplorer.php
+++ content/classes/class.ilLMExplorer.php
@@ -59,120 +59,11 @@ class ilLMExplorer extends ilExplorer
 		$this->root_id = $this->tree->readRootId();
 		$this->lm_obj =& $a_lm_obj;
 		$this->order_column = "";
+		$this->setSessionExpandVariable("lmexpand");
+		$this->checkPermissions(false);
+		$this->setExpandTarget("lm_edit.php?cmd=explorer&ref_id=".$this->lm_obj->getRefId());
 	}
 
-	/**
-	* Creates output
-	* overwritten method from class Explorer
-	* @access	public
-	* @return	string
-	*/
-	function getOutput()
-	{
-		$this->format_options[0]["tab"] = array();
-
-		$depth = $this->tree->getMaximumDepth();
-
-		for ($i=0;$i<$depth;++$i)
-		{
-			$this->createLines($i);
-		}
-
-		foreach ($this->format_options as $key => $options)
-		{
-			if ($options["visible"] and $key != 0)
-			{
-				$this->formatObject($options["child"],$options);
-			}
-			if($key == 0)
-			{
-				$this->formatHeader($options["child"],$options);
-			}
-		}
-
-		return implode('',$this->output);
-	}
-
-
-	/**
-	* Overwritten method from class.Explorer.php to avoid checkAccess selects
-	* recursive method
-	* @access	public
-	* @param	integer		parent_node_id where to start from (default=0, 'root')
-	* @param	integer		depth level where to start (default=1)
-	* @return	string
-	*/
-	function setOutput($a_parent, $a_depth = 1)
-	{
-		global $lng;
-		static $counter = 0;
-
-		if ($objects =  $this->tree->getChilds($a_parent, $this->order_column))
-		{
-			$tab = ++$a_depth - 2;
-
-			foreach ($objects as $key => $object)
-			{
-
-				if ($this->filtered == false || $this->checkFilter($object["type"])==true)
-				{
-
-					if ($object["child"] != $this->root_id)
-					{
-						//$data = $this->tree->getParentNodeData($object["child"]);
-						$parent_index = $this->getIndex($object);
-					}
-					$this->format_options["$counter"]["parent"] = $object["parent"];
-					$this->format_options["$counter"]["child"] = $object["child"];
-					$this->format_options["$counter"]["title"] = $object["title"];
-					//$this->format_options["$counter"]["type"] = $object["type"];
-					switch ($object["type"])
-					{
-						case "st":
-							$this->format_options["$counter"]["type"] = "cat";
-							break;
-
-						case "pg":
-							$this->format_options["$counter"]["type"] = "le";
-							break;
-					}
-
-					$this->format_options["$counter"]["depth"] = $tab;
-					$this->format_options["$counter"]["container"] = false;
-					$this->format_options["$counter"]["visible"]	  = true;
-					// Create prefix array
-					for ($i = 0; $i < $tab; ++$i)
-					{
-						$this->format_options["$counter"]["tab"][] = 'blank';
-					}
-					// only if parent is expanded and visible, object is visible
-					if ($object["child"] != $this->root_id  and (!in_array($object["parent"],$this->expanded)
-															or !$this->format_options["$parent_index"]["visible"]))
-					{
-						$this->format_options["$counter"]["visible"] = false;
-					}
-					// if object exists parent is container
-					if ($object["child"] != $this->root_id)
-					{
-						$this->format_options["$parent_index"]["container"] = true;
-
-						if (in_array($object["parent"],$this->expanded))
-						{
-							$this->format_options["$parent_index"]["tab"][($tab-2)] = 'minus';
-						}
-						else
-						{
-							$this->format_options["$parent_index"]["tab"][($tab-2)] = 'plus';
-						}
-					}
-					++$counter;
-
-					// Recursive
-					$this->setOutput($object["child"],$a_depth);
-				}
-			} //foreach
-		} //if
-	} //function
 
 	/**
 	* overwritten method from base class
@@ -188,8 +79,6 @@ class ilLMExplorer extends ilExplorer
 		$tpl = new ilTemplate("tpl.tree.html", true, true);
 
 		$tpl->setCurrentBlock("row");
-		//$tpl->setVariable("TYPE", $a_option["type"]);
-		//$tpl->setVariable("ICON_IMAGE" ,ilUtil::getImagePath("icon_".$a_option["type"].".gif"));
 		$tpl->setVariable("TITLE", $this->lm_obj->getTitle());
 		$tpl->setVariable("LINK_TARGET", $this->target);
 		$tpl->setVariable("TARGET", " target=\"".$this->frameTarget."\"");
@@ -198,47 +87,5 @@ class ilLMExplorer extends ilExplorer
 		$this->output[] = $tpl->get();
 	}
 
-	/**
-	* set the expand option
-	* this value is stored in a SESSION variable to save it different view (lo view, frm view,...)
-	* @access	private
-	* @param	string		pipe-separated integer
-	*/
-	function setExpand($a_node_id)
-	{
-		// IF ISN'T SET CREATE SESSION VARIABLE
-		if(!is_array($_SESSION["lmexpand"]))
-		{
-			$_SESSION["lmexpand"] = array();
-		}
-		// IF $_GET["expand"] is positive => expand this node
-		if($a_node_id > 0 && !in_array($a_node_id,$_SESSION["lmexpand"]))
-		{
-			array_push($_SESSION["lmexpand"],$a_node_id);
-		}
-		// IF $_GET["expand"] is negative => compress this node
-		if($a_node_id < 0)
-		{
-			$key = array_keys($_SESSION["lmexpand"],-(int) $a_node_id);
-			unset($_SESSION["lmexpand"][$key[0]]);
-		}
-		$this->expanded = $_SESSION["lmexpand"];
-	}
-	/**
-	* Creates Get Parameter
-	* @access	private
-	* @param	string
-	* @param	integer
-	* @return	string
-	*/
-	function createTarget($a_type,$a_child)
-	{
-		// SET expand parameter:
-		//     positive if object is expanded
-		//     negative if object is compressed
-		$a_child = $a_type == '+' ? $a_child : -(int) $a_child;
-
-		return $_SERVER["SCRIPT_NAME"]."?cmd=explorer&ref_id=".$this->lm_obj->getRefId()."&lmexpand=".$a_child;
-	}
-} // END class.ilMailExplorer
+} // END class ilLMExplorer
 ?>
