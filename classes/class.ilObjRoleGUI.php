4edbf51588e9d98dda63636d15930c996e566bb7
shofmann <shofmann@svn.ilias.de>
2003-08-19 12:21:45 +0000

    fixed a bug: if no permission is checked for one object type, permissions ere not revoked if 'change existing objects' is enabled.
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@1937 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- classes/class.ilObjRoleGUI.php
+++ classes/class.ilObjRoleGUI.php
@@ -26,7 +26,7 @@
 * Class ilObjRoleGUI
 *
 * @author Stefan Meyer <smeyer@databay.de> 
-* $Id$Id: class.ilObjRoleGUI.php,v 1.46 2003/08/18 12:42:14 shofmann Exp $
+* $Id$Id: class.ilObjRoleGUI.php,v 1.47 2003/08/19 11:01:23 shofmann Exp $
 * 
 * @extends ilObjectGUI
 * @package ilias-core
@@ -389,11 +389,10 @@ class ilObjRoleGUI extends ilObjectGUI
 				// GET ALL SUBNODES
 				$node_data = $this->tree->getNodeData($node_id);
 				$subtree_nodes = $this->tree->getSubTree($node_data);
-
+				
 				// GET ALL OBJECTS THAT CONTAIN A ROLE FOLDERS
 				$all_rolf_obj = $rbacreview->getObjectsWithStopedInheritance($this->object->getId());
-				
-				//var_dump("<pre>",$all_rolf_obj,"</pre>");exit;
+
 				// DELETE ACTUAL ROLE FOLDER FROM ARRAY
 				$key = array_keys($all_rolf_obj,$node_id);
 				unset($all_rolf_obj["$key[0]"]);
@@ -427,33 +426,23 @@ class ilObjRoleGUI extends ilObjectGUI
 						}
 					}
 				}
-				
-				// revoke all permissions for valid nodes if nothing was posted
-				if (empty($_POST["template_perm"]))
+
+				// NOW SET ALL PERMISSIONS
+				foreach ($_POST["template_perm"] as $type => $a_perm)
 				{
 					foreach ($valid_nodes as $node)
 					{
 						$rbacadmin->revokePermission($node["child"],$this->object->getId());
-					}				
-				}
-				else
-				{
-					// NOW SET ALL PERMISSIONS
-					foreach ($_POST["template_perm"] as $type => $a_perm)
-					{
-						foreach ($valid_nodes as $node)
+				
+						if ($type == $node["type"])
 						{
-							if ($type == $node["type"])
-							{
-								$rbacadmin->revokePermission($node["child"],$this->object->getId());
-								$rbacadmin->grantPermission($this->object->getId(),$a_perm,$node["child"]);
-							}
+							$rbacadmin->grantPermission($this->object->getId(),$a_perm,$node["child"]);
 						}
 					}
 				}
 			}// END IF RECURSIVE
 		}// END CHECK ACCESS
-	
+
 		sendinfo($this->lng->txt("saved_successfully"),true);
 
 		header("Location: adm_object.php?ref_id=".$_GET["ref_id"]."&obj_id=".$this->object->getId()."&cmd=perm");
