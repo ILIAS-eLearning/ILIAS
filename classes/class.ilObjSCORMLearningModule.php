0377f93197c55f0029bb50a37d9e16e33578b9a9
mrus <mrus>
2003-09-10 07:20:21 +0000

    added notify funtionality
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@2127 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- classes/class.ilObjSCORMLearningModule.php
+++ classes/class.ilObjSCORMLearningModule.php
@@ -162,5 +162,67 @@ class ilObjSCORMLearningModule extends ilObject
 		return "";
 	}
 
+	/**
+	* notifys an object about an event occured
+	* Based on the event happend, each object may decide how it reacts.
+	*
+	* @access	public
+	* @param	string	event
+	* @param	integer	reference id of object where the event occured
+	* @param	array	passes optional paramters if required
+	* @return	boolean
+	*/
+	function notify($a_event,$a_ref_id,$a_node_id,$a_params = 0)
+	{
+		global $tree;
+		
+		switch ($a_event)
+		{
+			case "link":
+				
+				//var_dump("<pre>",$a_params,"</pre>");
+				//echo "SCORMLearningModule ".$this->getRefId()." triggered by link event. Objects linked into target object ref_id: ".$a_ref_id;
+				//exit;
+				break;
+			
+			case "cut":
+				
+				//echo "SCORMLearningModule ".$this->getRefId()." triggered by cut event. Objects are removed from target object ref_id: ".$a_ref_id;
+				//exit;
+				break;
+				
+			case "copy":
+			
+				//var_dump("<pre>",$a_params,"</pre>");
+				//echo "SCORMLearningModule ".$this->getRefId()." triggered by copy event. Objects are copied into target object ref_id: ".$a_ref_id;
+				//exit;
+				break;
+
+			case "paste":
+				
+				//echo "SCORMLearningModule ".$this->getRefId()." triggered by paste (cut) event. Objects are pasted into target object ref_id: ".$a_ref_id;
+				//exit;
+				break;
+			
+			case "new":
+				
+				//echo "SCORMLearningModule ".$this->getRefId()." triggered by paste (new) event. Objects are applied to target object ref_id: ".$a_ref_id;
+				//exit;
+				break;
+		}
+		
+		// At the beginning of the recursive process it avoids second call of the notify function with the same parameter
+		if ($a_node_id==$a_ref_id)
+		{	
+			$parent_obj =& $this->ilias->obj_factory->getInstanceByRefId($a_node_id);
+			$parent_type = $parent_obj->getType();
+			if($parent_type == $this->getType())
+			{
+				$a_node_id = (int) $tree->getParentId($a_node_id);
+			}
+		}
+		
+		parent::notify($a_event,$a_ref_id,$a_node_id,$a_params);
+	}
 } // END class.ilObjSCORMLearningModule
 ?>
