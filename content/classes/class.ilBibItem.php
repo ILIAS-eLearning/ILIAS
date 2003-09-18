f62ed78ce9258be82d55ba1ab2d2c4aab178599f
Stefan Meyer <smeyer.ilias@gmx.de>
2003-09-18 14:35:00 +0000

    translation of bibliography.xsl
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@2159 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- content/classes/class.ilBibItem.php
+++ content/classes/class.ilBibItem.php
@@ -35,12 +35,18 @@ require_once ("classes/class.ilMetaData.php");
 */
 class ilBibItem
 {
+	var $nested_obj;
+	var $content_obj;
+	var $xml;
+
+	var $bibliography_attr;
+	var $abstract;
 
 	/**
 	* Constructor
 	* @access	public
 	*/
-	function ilBibItem($a_type = "", $a_id = 0)
+	function ilBibItem($content_obj = 0)
 	{
 		global $ilias;
 
@@ -54,12 +60,163 @@ class ilBibItem
 		$this->technicals = array();	// technical sections
 		$this->coverage = "";
 		$this->structure = "";
-		$this->type = $a_type;
-		$this->id = $a_id;
 
+		$this->content_obj =& $content_obj;
+		if(is_object($content_obj))
+		{
+			$this->readXML();
+		}
+	}
+
+	// SET METHODS
+	/**
+	* @param array e.g array("version" => 1,...)
+	* @see ilias_co.dtd
+	* @access	public
+	*/
+	function setBibliographyAttributes($a_data)
+	{
+		$this->bibliography_attr = $a_data;
+	}
+
+	function setAbstract($a_data)
+	{
+		$this->abstract = $a_data;
+	}
+
+	function setBibItemData($a_key,$a_value,$a_bib_item_nr)
+	{
+		$this->bib_item_data[$a_bib_item_nr]["$a_key"] = $a_value;
+		
+		return true;
+	}
+	function appendBibItemData($a_key,$a_value,$a_bib_item_nr)
+	{
+		$this->bib_item_data[$a_bib_item_nr]["$a_key"] = array_merge($this->bib_item_data[$a_bib_item_nr]["$a_key"],array($a_value));
+	}
+
+	// GET MEHODS
+	function getBibItemData()
+	{
+		return $this->bib_item_data;
+	}
+
+	/**
+	* @return array e.g array("version" => 1,...)
+	* @see ilias_co.dtd
+	* @access	public
+	*/
+	function getBibliographyAttributes()
+	{
+		return $this->bibliography_attr ? $this->bibliography_attr : array();
+	}
+	/**
+	* @return string
+	* @see ilias_co.dtd
+	* @access	public
+	*/
+	function getAbstract()
+	{
+		return $this->abstract;
+	}
+
+	function getTitle()
+	{
+		return $this->title;
+	}
+
+	function getXML()
+	{
+		return $this->xml;
 	}
 
 
+	function readXML()
+	{
+		if(!$this->__initNestedSet())
+		{
+			return false;
+		}
+		$this->xml = $this->nested_obj->export($this->content_obj->getId(),"bib");
+	}
+
+
+	function read()
+	{
+		if(!$this->__initNestedSet())
+		{
+			return false;
+		}
+		return;
+		/*
+		$this->dom = domxml_open_mem($this->nested_obj->export($this->content_obj->getId(),"bib"));
+		
+		// PARSE BIBLIOGRAPHY TAG
+		$root = $this->dom->document_element();
+		
+		// Bibliography attributes
+		foreach($root->attributes() as $key => $value)
+		{
+			$tmp_arr[$value->name] = $value->value;
+		}
+		$this->setBibliographyAttributes($tmp_arr);
+		
+		// READ Abstracts
+		if($abstract = $this->dom->get_elements_by_tagname("Abstract"))
+		{
+			$this->setAbstract($abstract[0]->get_content());
+		}
+
+		// READ ALL BibItems
+		$bib_arr = $this->dom->get_elements_by_tagname("BibItem");
+		if(!is_array($bib_arr))
+		{
+			return false;
+		}
+		$counter = 0;
+		foreach($bib_arr as $bib_elem)
+		{
+			// READ BIB ITEM ATTRIBUTES
+			foreach($bib_elem->attributes() as $value)
+			{
+				$this->setBibItemData($value->name,$value->value,$counter);
+			}
+			
+			// GET ALL CHILD ELEMENTS
+			$bib_child = $bib_elem->first_child();
+			while($bib_child)
+			{
+				switch($bib_child->tagname)
+				{
+					case "Identifier":
+						unset($tmp_arr);
+						foreach($bib_child->attributes() as $value)
+						{
+							$tmp_arr[$value->name] = $value->value;
+						}
+						$this->setBibItemData("Identifier",$tmp_arr,$counter);
+						break;
+
+					case "Language":
+						foreach($bib_child->attributes() as $value)
+						{
+							$value = $value->value;
+						}
+						$this->appendBibItemData("Language",$value,$counter);
+						break;
+				}
+
+				$bib_child = $bib_child->next_sibling();
+				++$counter;
+			}
+		}
+		*/
+
+		$res = $this->nested_obj->getFirstDomNode("//Bibliography/BibItem/Language");
+		#$this->title = $res[0]["value"];
+		#var_dump("<pre>",$res,"</pre");
+
+	}
 	//fbo:
 	
 	/**
@@ -98,6 +255,15 @@ class ilBibItem
         return $this->xml;
 	}
 
+	// PRIVATE METHODS
+	function __initNestedSet()
+	{
+		include_once("classes/class.ilNestedSetXML.php");
 
+		$this->nested_obj =& new ilNestedSetXML();
+		$this->nested_obj->init($this->content_obj->getId(),"bib");
+
+		return $this->nested_obj->initDom();
+	}
 }
 ?>
