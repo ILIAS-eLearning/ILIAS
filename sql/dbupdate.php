beff82913847d20f417c0f580554f23cd82e6fa7
mmaschke <mmaschke>
2003-10-10 10:41:40 +0000

    modified grpStatusClosedTemplate: revoked the permission to read
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@2329 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- sql/dbupdate.php
+++ sql/dbupdate.php
@@ -288,3 +288,6 @@ CREATE TABLE lo_access
 
 <#35>
 ALTER TABLE file_data ADD file_name CHAR(128) NOT NULL AFTER file_id;
+
+<#36>
+DELETE FROM rbac_templates WHERE rol_id='83' AND type='grp' AND ops_id=3 AND parent=8;
