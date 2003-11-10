78fddae37b20776e89f98e1d5ea9ceaa8e394d5c
aammerla <aammerla>
2003-11-10 09:00:36 +0000

    added new locator-in-a-frame, new templates & template changes
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@2654 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- templates/ingmedia/functions.js
+++ templates/ingmedia/functions.js
@@ -75,11 +75,11 @@ function ZweiFrames(URI1,F1,URI2,F2)
   Frame2=eval("parent."+F2);
   Frame1.location.href = URI1;
   Frame2.location.href = URI2;
-  
-  <!-- UpdateLocatorFrame(); -->
-  LocatorFrame=eval("parent.locator");
-  LocatorFrame.location.href = "./locator.php";
+}
 
-  alert("Hello world!");
+function UpdateLocatorFrame()
+{
+  Frame=eval("parent.locator");
+  Frame.location.href = "locator.php";
 }
 
