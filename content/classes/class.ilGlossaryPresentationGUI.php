96c89de6df017111672ea5841edf1e35da779c2f
Alexander Killing <killing@leifos.de>
2003-11-15 01:51:37 +0000

    fixed gender setting in personal profile
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@2696 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ content/classes/class.ilGlossaryPresentationGUI.php
@@ -0,0 +1,393 @@
+<?php
+/*
+	+-----------------------------------------------------------------------------+
+	| ILIAS open source                                                           |
+	+-----------------------------------------------------------------------------+
+	| Copyright (c) 1998-2001 ILIAS open source, University of Cologne            |
+	|                                                                             |
+	| This program is free software; you can redistribute it and/or               |
+	| modify it under the terms of the GNU General Public License                 |
+	| as published by the Free Software Foundation; either version 2              |
+	| of the License, or (at your option) any later version.                      |
+	|                                                                             |
+	| This program is distributed in the hope that it will be useful,             |
+	| but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+	| GNU General Public License for more details.                                |
+	|                                                                             |
+	| You should have received a copy of the GNU General Public License           |
+	| along with this program; if not, write to the Free Software                 |
+	| Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
+	+-----------------------------------------------------------------------------+
+*/
+
+require_once("classes/class.ilObjectGUI.php");
+require_once("classes/class.ilMetaDataGUI.php");
+require_once("content/classes/class.ilObjGlossary.php");
+require_once("content/classes/class.ilGlossaryTermGUI.php");
+require_once("content/classes/class.ilGlossaryDefinition.php");
+require_once("content/classes/class.ilTermDefinitionEditorGUI.php");
+require_once("content/classes/Pages/class.ilPCParagraph.php");
+
+/**
+* Class ilGlossaryPresentationGUI
+*
+* GUI class for glossary presentation
+*
+* @author Alex Killing <alex.killing@gmx.de>
+* @version $Id$
+*
+* @package content
+*/
+class ilGlossaryPresentationGUI
+{
+	var $admin_tabs;
+	var $glossary;
+	var $ilias;
+	var $tpl;
+	var $lng;
+
+	/**
+	* Constructor
+	* @access	public
+	*/
+	function ilGlossaryPresentationGUI()
+	{
+		global $lng, $ilias, $tpl;
+
+		$this->tpl =& $tpl;
+		$this->lng =& $lng;
+		$this->ilias =& $ilias;
+
+		// Todo: check lm id
+		$this->glossary =& $this->ilias->obj_factory->getInstanceByRefId($_GET["ref_id"]);
+
+	}
+
+
+	/**
+	* executer command ("listTerms" | "listDefinitions")
+	*/
+	function executeCommand()
+	{
+		$cmd = $_GET["cmd"];
+		if ($cmd != "listDefinitions")
+		{
+			$this->prepareOutput();
+		}
+		if($cmd == "")
+		{
+			$cmd = "listTerms";
+		}
+
+		$this->$cmd();
+
+		$this->tpl->show();
+	}
+
+	function prepareOutput()
+	{
+		$this->tpl->addBlockFile("CONTENT", "content", "tpl.adm_content.html");
+		$this->tpl->addBlockFile("STATUSLINE", "statusline", "tpl.statusline.html");
+		$title = $this->glossary->getTitle();
+
+		// catch feedback message
+		sendInfo();
+
+		if (!empty($title))
+		{
+			$this->tpl->setVariable("HEADER", $title);
+		}
+
+		$this->setAdminTabs();
+		$this->setLocator();
+	}
+
+
+
+	/**
+	* list glossary terms
+	*/
+	function listTerms()
+	{
+		$limit = (empty($_GET["limit"]))
+			? $limit = 20
+			: $_GET["limit"];
+
+		$this->lng->loadLanguageModule("meta");
+		include_once "./classes/class.ilTableGUI.php";
+
+		// load template for table
+		$this->tpl->addBlockfile("ADM_CONTENT", "adm_content", "tpl.table.html");
+
+		// load template for table content data
+		$this->tpl->addBlockfile("TBL_CONTENT", "tbl_content", "tpl.term_tbl_row.html", true);
+
+		$num = 0;
+
+		$obj_str = ($this->call_by_reference) ? "" : "&obj_id=".$this->obj_id;
+		$this->tpl->setVariable("FORMACTION", "glossary_edit.php?ref_id=".$this->ref_id."$obj_str&cmd=post");
+
+		// create table
+		$tbl = new ilTableGUI();
+
+		// title & header columns
+		$tbl->setTitle($this->lng->txt("cont_terms"));
+		//$tbl->setHelp("tbl_help.php","icon_help.gif",$this->lng->txt("help"));
+
+		$tbl->setHeaderNames(array("", $this->lng->txt("cont_term"),
+			 $this->lng->txt("language"), $this->lng->txt("cont_definitions")));
+
+		$cols = array("", "term", "language", "definitions", "id");
+		$header_params = array("ref_id" => $this->ref_id, "cmd" => "listTerms");
+		$tbl->setHeaderVars($cols, $header_params);
+		$tbl->setColumnWidth(array("1%","24%","15%","60%"));
+
+		// control
+		$tbl->setOrderColumn($_GET["sort_by"]);
+		$tbl->setOrderDirection($_GET["sort_order"]);
+		$tbl->setLimit($limit);
+		$tbl->setOffset($_GET["offset"]);
+		$tbl->setMaxCount($this->maxcount);
+
+		$this->tpl->setVariable("COLUMN_COUNTS", 4);
+
+		// footer
+		$tbl->setFooter("tblfooter",$this->lng->txt("previous"),$this->lng->txt("next"));
+		//$tbl->disable("footer");
+
+		$term_list = $this->glossary->getTermList();
+		$tbl->setMaxCount(count($term_list));
+
+		// sorting array
+		//$term_list = ilUtil::sortArray($term_list, $_GET["sort_by"], $_GET["sort_order"]);
+		$term_list = array_slice($term_list, $_GET["offset"], $limit);
+
+		// render table
+		$tbl->render();
+
+		if (count($term_list) > 0)
+		{
+			$i=1;
+			foreach($term_list as $key => $term)
+			{
+				$css_row = ilUtil::switchColor($i++,"tblrow1","tblrow2");
+				$defs = ilGlossaryDefinition::getDefinitionList($term["id"]);
+				for($j=0; $j<count($defs); $j++)
+				{
+					$def = $defs[$j];
+					$this->tpl->setCurrentBlock("definition");
+					$this->tpl->setVariable("DEF_LINK",
+						"glossary_edit.php?ref_id=".$_GET["ref_id"]."&cmd=view&def=".$def["id"]);
+					$this->tpl->setVariable("DEF_TEXT", $this->lng->txt("cont_definition")." ".($j + 1));
+					$short_str = ilPCParagraph::xml2output($def["short_text"]);
+					$short_str = str_replace("<", "&lt;", $short_str);
+					$short_str = str_replace(">", "&gt;", $short_str);
+					$this->tpl->setVariable("DEF_SHORT", $short_str);
+					$this->tpl->parseCurrentBlock();
+				}
+
+				$this->tpl->setVariable("CSS_ROW", $css_row);
+				$this->tpl->setVariable("TEXT_TERM", $term["term"]);
+				$this->tpl->setVariable("CHECKBOX_ID", $term["id"]);
+				$this->tpl->setVariable("TARGET_TERM", "glossary_edit.php?ref_id=".
+					$_GET["ref_id"]."&cmd=listDefinitions&term_id=".$term["id"]);
+				$this->tpl->setVariable("TEXT_LANGUAGE", $this->lng->txt("meta_l_".$term["language"]));
+				$this->tpl->setCurrentBlock("tbl_content");
+				$this->tpl->parseCurrentBlock();
+			}
+		} //if is_array
+		else
+		{
+			$this->tpl->setCurrentBlock("notfound");
+			$this->tpl->setVariable("TXT_OBJECT_NOT_FOUND", $this->lng->txt("obj_not_found"));
+			$this->tpl->setVariable("NUM_COLS", $num);
+			$this->tpl->parseCurrentBlock();
+		}
+	}
+
+	function listDefinitions()
+	{
+		require_once("content/classes/Pages/class.ilPageObjectGUI.php");
+
+		$this->tpl->setCurrentBlock("ContentStyle");
+		$this->tpl->setVariable("LOCATION_CONTENT_STYLESHEET",
+			ilObjStyleSheet::getContentStylePath(0));
+		$this->tpl->parseCurrentBlock();
+
+		//$this->admin_tabs[] = array("cont_definitions","listDefinitions");
+		//$this->admin_tabs[] = array("meta_data","editTerm");
+		$term =& new ilGlossaryTerm($_GET["term_id"]);
+
+		// load template for table
+		$this->tpl->addBlockfile("CONTENT", "def_list", "tpl.glossary_definition_list.html", true);
+		$this->tpl->addBlockfile("STATUSLINE", "statusline", "tpl.statusline.html");
+		$this->setLocator();
+		$this->setAdminTabs("term_edit");
+		$this->tpl->setVariable("TXT_HEADER",
+			$this->lng->txt("cont_term").": ".$term->getTerm());
+
+		$this->tpl->setVariable("FORMACTION", "glossary_edit.php?ref_id=".$_GET["ref_id"].
+			"&cmd=post&term_id=".$_GET["term_id"]);
+		$this->tpl->setVariable("TXT_ADD_DEFINITION",
+			$this->lng->txt("cont_add_definition"));
+		$this->tpl->setVariable("BTN_ADD", "addDefinition");
+
+		$defs = ilGlossaryDefinition::getDefinitionList($_GET["term_id"]);
+
+		$this->tpl->setVariable("TXT_TERM", $term->getTerm());
+
+		for($j=0; $j<count($defs); $j++)
+		{
+			$def = $defs[$j];
+			$page =& new ilPageObject("gdf", $def["id"]);
+			$page_gui =& new ilPageObjectGUI($page);
+			//$page_gui->setOutputMode("edit");
+			//$page_gui->setPresentationTitle($this->term->getTerm());
+			$page_gui->setTemplateOutput(false);
+			$output = $page_gui->preview();
+
+			if (count($defs) > 1)
+			{
+				$this->tpl->setCurrentBlock("definition_header");
+						$this->tpl->setVariable("TXT_DEFINITION",
+				$this->lng->txt("cont_definition")." ".($j+1));
+				$this->tpl->parseCurrentBlock();
+			}
+
+			if ($j > 0)
+			{
+				$this->tpl->setCurrentBlock("up");
+				$this->tpl->setVariable("TXT_UP", $this->lng->txt("up"));
+				$this->tpl->setVariable("LINK_UP",
+					"glossary_edit.php?ref_id=".$_GET["ref_id"]."&cmd=moveUp&def=".$def["id"]);
+				$this->tpl->parseCurrentBlock();
+			}
+
+			if ($j+1 < count($defs))
+			{
+				$this->tpl->setCurrentBlock("down");
+				$this->tpl->setVariable("TXT_DOWN", $this->lng->txt("down"));
+				$this->tpl->setVariable("LINK_DOWN",
+					"glossary_edit.php?ref_id=".$_GET["ref_id"]."&cmd=moveDown&def=".$def["id"]);
+				$this->tpl->parseCurrentBlock();
+			}
+
+			$this->tpl->setCurrentBlock("definition");
+			$this->tpl->setVariable("PAGE_CONTENT", $output);
+			$this->tpl->setVariable("TXT_EDIT", $this->lng->txt("edit"));
+			$this->tpl->setVariable("LINK_EDIT",
+				"glossary_edit.php?ref_id=".$_GET["ref_id"]."&cmd=view&def=".$def["id"]);
+			$this->tpl->setVariable("TXT_DELETE", $this->lng->txt("delete"));
+			$this->tpl->setVariable("LINK_DELETE",
+				"glossary_edit.php?ref_id=".$_GET["ref_id"]."&cmd=confirmDefinitionDeletion&def=".$def["id"]);
+			$this->tpl->parseCurrentBlock();
+		}
+		//$this->tpl->setCurrentBlock("def_list");
+		//$this->tpl->parseCurrentBlock();
+
+	}
+
+
+
+	/**
+	* set Locator
+	*
+	* @param	object	tree object
+	* @param	integer	reference id
+	* @access	public
+	*/
+	function setLocator($a_tree = "", $a_id = "")
+	{
+		global $ilias_locator;
+
+		// ### AA 03.11.10 added new locator GUI class ###
+		$i = 1;
+
+		$this->tpl->addBlockFile("LOCATOR", "locator", "tpl.locator.html");
+
+		if (!empty($_GET["term_id"]))
+		{
+			$this->tpl->touchBlock("locator_separator");
+		}
+
+		$this->tpl->setCurrentBlock("locator_item");
+		$this->tpl->setVariable("ITEM", $this->glossary->getTitle());
+		// TODO: SCRIPT NAME HAS TO BE VARIABLE!!!
+		$this->tpl->setVariable("LINK_ITEM", "glossary_presentation.php?ref_id=".$_GET["ref_id"]."&cmd=listTerms");
+		$this->tpl->parseCurrentBlock();
+
+		// ### AA 03.11.10 added new locator GUI class ###
+		// navigate locator
+		$ilias_locator->navigate($i++,$this->glossary->getTitle(),"glossary_presentation.php?ref_id=".$_GET["ref_id"]."&cmd=listTerms","bottom");
+
+		if (!empty($_GET["term_id"]))
+		{
+			$term =& new ilGlossaryTerm($_GET["term_id"]);
+			$this->tpl->setCurrentBlock("locator_item");
+			$this->tpl->setVariable("ITEM", $term->getTerm());
+			$this->tpl->setVariable("LINK_ITEM", "glossary_presentation.php?ref_id=".$_GET["ref_id"].
+				"&cmd=listDefinitions&term_id=".$term->getId());
+			$this->tpl->parseCurrentBlock();
+
+			// ### AA 03.11.10 added new locator GUI class ###
+			// navigate locator
+			$ilias_locator->navigate($i++,$term->getTerm(),"glossary_edit.php?ref_id=".$_GET["ref_id"].
+				"&cmd=listDefinitions&term_id=".$term->getId(),"bottom");
+		}
+
+		//$this->tpl->touchBlock("locator_separator");
+
+		$this->tpl->setCurrentBlock("locator");
+		$this->tpl->setVariable("TXT_LOCATOR",$debug.$this->lng->txt("locator"));
+		$this->tpl->parseCurrentBlock();
+
+	}
+
+	function setAdminTabs($mode = "std")
+	{
+		switch($mode)
+		{
+			case "std":
+				//$tabs[] = array("cont_definitions","listDefinitions");
+				//$tabs[] = array("properties","editTerm");
+				break;
+		}
+
+		if (!is_array($tabs))
+		{
+			return;
+		}
+
+		$this->tpl->addBlockFile("TABS", "tabs", "tpl.tabs.html");
+
+
+		foreach ($tabs as $row)
+		{
+			$i++;
+
+			if ($row[1] == $_GET["cmd"])
+			{
+				$tabtype = "tabactive";
+				$tab = $tabtype;
+			}
+			else
+			{
+				$tabtype = "tabinactive";
+				$tab = "tab";
+			}
+
+			$this->tpl->setCurrentBlock("tab");
+			$this->tpl->setVariable("TAB_TYPE", $tabtype);
+			$this->tpl->setVariable("TAB_TYPE2", $tab);
+			$this->tpl->setVariable("TAB_LINK", "glossary_edit.php?ref_id=".$_GET["ref_id"]."&def=".
+				$_GET["def"]."&term_id=".$_GET["term_id"]."&cmd=".$row[1]);
+			$this->tpl->setVariable("TAB_TEXT", $this->lng->txt($row[0]));
+			$this->tpl->parseCurrentBlock();
+		}
+
+	}
+
+}
+
+?>
