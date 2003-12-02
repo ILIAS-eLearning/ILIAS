df4c8a5c4501d537694cea3da3b9dc818b05e32f
shofmann <shofmann@svn.ilias.de>
2003-12-02 14:05:29 +0000

    various layout fixes
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@2904 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- setup/include/inc.client_tabs.php
+++ setup/include/inc.client_tabs.php
@@ -43,35 +43,35 @@ $tab = $this->cmd ? $this->cmd : "view";
 $this->tpl->setCurrentBlock("tab");
 $this->tpl->setVariable("TAB_TYPE",$tab == "view" ? "tabactive" : "tabinactive");
 $this->tpl->setVariable("TAB_LINK","setup.php?".$client_id."&cmd=view");
-$this->tpl->setVariable("TAB_TEXT",$this->lng->txt("overview"));
+$this->tpl->setVariable("TAB_TEXT",ucfirst($this->lng->txt("overview")));
 $this->tpl->parseCurrentBlock();
 
 // database
 $this->tpl->setCurrentBlock("tab");
 $this->tpl->setVariable("TAB_TYPE",$tab == "db" ? "tabactive" : "tabinactive");
 $this->tpl->setVariable("TAB_LINK","setup.php?".$client_id."&cmd=db");
-$this->tpl->setVariable("TAB_TEXT",$this->lng->txt("database"));
+$this->tpl->setVariable("TAB_TEXT",ucfirst($this->lng->txt("database")));
 $this->tpl->parseCurrentBlock();
 
 // languages
 $this->tpl->setCurrentBlock("tab");
 $this->tpl->setVariable("TAB_TYPE",$tab == "lang" ? "tabactive" : "tabinactive");
 $this->tpl->setVariable("TAB_LINK","setup.php?cmd=lang");
-$this->tpl->setVariable("TAB_TEXT",$this->lng->txt("languages"));
+$this->tpl->setVariable("TAB_TEXT",ucfirst($this->lng->txt("languages")));
 $this->tpl->parseCurrentBlock();
 
 // contact data
 $this->tpl->setCurrentBlock("tab");
 $this->tpl->setVariable("TAB_TYPE",$tab == "contact" ? "tabactive" : "tabinactive");
 $this->tpl->setVariable("TAB_LINK","setup.php?cmd=contact");
-$this->tpl->setVariable("TAB_TEXT",$this->lng->txt("contact"));
+$this->tpl->setVariable("TAB_TEXT",ucfirst($this->lng->txt("contact")));
 $this->tpl->parseCurrentBlock();
 
 // ilias-NIC
 $this->tpl->setCurrentBlock("tab");
 $this->tpl->setVariable("TAB_TYPE",$tab == "nic" ? "tabactive" : "tabinactive");
 $this->tpl->setVariable("TAB_LINK","setup.php?cmd=nic");
-$this->tpl->setVariable("TAB_TEXT",$this->lng->txt("ilias_nic"));
+$this->tpl->setVariable("TAB_TEXT",ucfirst($this->lng->txt("ilias_nic")));
 $this->tpl->parseCurrentBlock();
 
 // setup settings
@@ -88,7 +88,7 @@ if ($this->isAdmin())
 	$this->tpl->setCurrentBlock("tab");
 	$this->tpl->setVariable("TAB_TYPE",$tab == "delete" ? "tabactive" : "tabinactive");
 	$this->tpl->setVariable("TAB_LINK","setup.php?cmd=delete&lang=".$this->lang);
-	$this->tpl->setVariable("TAB_TEXT",$this->lng->txt("delete"));
+	$this->tpl->setVariable("TAB_TEXT",ucfirst($this->lng->txt("delete")));
 	$this->tpl->parseCurrentBlock();
 }
 ?>
