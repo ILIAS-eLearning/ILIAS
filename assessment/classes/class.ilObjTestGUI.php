d5809823197591875daa14a017c8d616e4f7021a
hschottm <hschottm>
2004-02-03 18:41:10 +0000

    Added repository integretion for test&assessment tool
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@3213 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ assessment/classes/class.ilObjTestGUI.php
@@ -0,0 +1,78 @@
+<?php
+/*
+	+-----------------------------------------------------------------------------+
+	| ILIAS open source                                                           |
+	+-----------------------------------------------------------------------------+
+	| Copyright (c) 1998-2001 ILIAS open source, University of Cologne            |
+	|                                                                             |
+	| This program is free software; you can redistribute it and/or               |
+	| modify it under the terms of the GNU General Public License                 |
+	| as published by the Free Software Foundation; either version 2              |
+	| of the License, or (at your option) any later version.                      |
+	|                                                                             |
+	| This program is distributed in the hope that it will be useful,             |
+	| but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+	| GNU General Public License for more details.                                |
+	|                                                                             |
+	| You should have received a copy of the GNU General Public License           |
+	| along with this program; if not, write to the Free Software                 |
+	| Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
+	+-----------------------------------------------------------------------------+
+*/
+
+
+/**
+* Class ilObjTestGUI
+*
+* @author Helmut Schottmüller <hschottm@tzi.de>
+* $Id$
+*
+* @extends ilObjectGUI
+* @package ilias-core
+* @package assessment
+*/
+
+require_once "classes/class.ilObjectGUI.php";
+
+class ilObjTestGUI extends ilObjectGUI
+{
+	/**
+	* Constructor
+	* @access public
+	*/
+	function ilObjTestGUI($a_data,$a_id,$a_call_by_reference)
+	{
+		$this->type = "tst";
+		$this->ilObjectGUI($a_data,$a_id,$a_call_by_reference, false);
+	}
+	
+	/**
+	* save object
+	* @access	public
+	*/
+	function saveObject()
+	{
+		global $rbacadmin;
+
+		// create and insert forum in objecttree
+		$newObj = parent::saveObject();
+
+		// setup rolefolder & default local roles
+		//$roles = $newObj->initDefaultRoles();
+
+		// ...finally assign role to creator of object
+		//$rbacadmin->assignUser($roles[0], $newObj->getOwner(), "y");
+
+		// put here object specific stuff
+			
+		// always send a message
+		sendInfo($this->lng->txt("object_added"),true);
+		
+		header("Location:".$this->getReturnLocation("save","adm_object.php?".$this->link_params));
+		exit();
+	}
+
+} // END class.ilObjTestGUI
+
+?>
