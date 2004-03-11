344378d1d02309b8336361c328bb97e873bc5e06
hschottm <hschottm>
2004-03-11 21:06:11 +0000

    Test&Assessment: Added a first output routine for "Tests already taken"
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@3506 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- assessment/tests_taken.php
+++ assessment/tests_taken.php
@@ -33,39 +33,79 @@
 define("ILIAS_MODULE", "assessment");
 chdir("..");
 require_once "./include/inc.header.php";
+require_once "./assessment/classes/class.ilObjTestGUI.php";
 
 // for security
 unset($id);
-
 global $tpl;
 global $lng;
+global $ilUser;
+global $ilDB;
 
 $lng->loadLanguageModule("assessment");
 $tpl->addBlockFile("CONTENT", "content", "tpl.adm_content.html");
 $tpl->addBlockFile("STATUSLINE", "statusline", "tpl.statusline.html");
-$title = "Not yet implemented";
 // catch feedback message
 sendInfo();
-setLocator();
-$tpl->setVariable("HEADER", $title);
-$tpl->addBlockFile("ADM_CONTENT", "adm_content", "tpl.il_as_tst_introduction.html", true);
+$ilias_locator = new ilLocatorGUI(false);
+$i = 1;
+$ilias_locator->navigate($i++, $lng->txt("personal_desktop"), ILIAS_HTTP_PATH . "/usr_personaldesktop.php", "bottom");
+$ilias_locator->navigate($i++, $lng->txt("tst_already_taken"), ILIAS_HTTP_PATH . "/assessment/tests_taken.php", "bottom");
+$ilias_locator->output();
+$tpl->setVariable("HEADER", $lng->txt("tst_already_taken"));
+$tpl->addBlockFile("ADM_CONTENT", "adm_content", "tpl.il_as_tst_taken.html", true);
+$q = sprintf("SELECT tst_active.tries, tst_active.test_fi, tst_tests.nr_of_tries, tst_tests.ref_fi FROM tst_active, tst_tests WHERE tst_active.user_fi = %s AND tst_active.test_fi = tst_tests.test_id",
+	$ilDB->quote($ilUser->id)
+);
+$result = $ilDB->query($q);
+$taken_array = array();
+while ($row = $result->fetchRow(DB_FETCHMODE_OBJECT)) {
+	$test = new ilObjTestGUI("", $row->ref_fi, true, false);
+	$test->object->load_from_db();
+	$array_result =& $test->object->get_test_result($ilUser->id);
+	$mark = $test->object->mark_schema->get_matching_mark(100*($array_result[0]["total_reached_points"]/$array_result[0]["total_max_points"]));
+	foreach ($array_result as $key => $value) {
+		$array_result[$key]["nr_of_tries"] = $row->nr_of_tries;
+		$array_result[$key]["used_tries"] = $row->tries;
+		$array_result[$key]["mark"] = $mark->get_official_name();
+	}
+	array_push($taken_array, $array_result);
+}
+
+$counter = 0;
+$classes = array("tblrow1", "tblrow2");
+$test_types = array(
+	"1" => "tt_assessment",
+	"2" => "tt_self_assessment",
+	"3" => "tt_navigation_controlling"
+);
+
+foreach ($taken_array as $key => $value) {
+	$tpl->setCurrentBlock("row");
+	$tpl->setVariable("COLOR_CLASS", $classes[$counter % 2]);
+	$tpl->setVariable("TEST_TITLE", $value[0]["test_title"]);
+	$tpl->setVariable("TEST_TYPE", $lng->txt($test_types[$value[0]["test_type"]]));
+	$status_image = "";
+	if ($value[0]["used_tries"] == 0) {
+		// test is not completed
+		$status_image = $lng->txt("tst_status_progress");
+	} elseif (($value[0]["nr_of_tries"] > 0) and ($value[0]["used_tries"] == $value[0]["nr_of_tries"])) {
+		// test is completed
+		$status_image = $lng->txt("tst_status_completed");
+	} else {
+		// test is completed but can be completed again
+		$status_image = $lng->txt("tst_status_completed_more_tries_possible");
+	}
+	$tpl->setVariable("TEST_STATUS", $status_image);
+	$tpl->setVariable("TEST_MARK", $value[0]["mark"]);
+	$tpl->parseCurrentBlock();
+}
+$tpl->setCurrentBlock("adm_content");
+$tpl->setVariable("TEST_TITLE", $lng->txt("title"));
+$tpl->setVariable("TEST_TYPE", $lng->txt("tst_type"));
+$tpl->setVariable("TEST_STATUS", $lng->txt("status"));
+$tpl->setVariable("TEST_MARK", $lng->txt("tst_mark"));
+
 $tpl->parseCurrentBlock();
 $tpl->show();
-
-	/**
-	* set Locator
-	*
-	* @access	public
-	*/
-	function setLocator()
-	{
-		global $lng;
-		
-	  $ilias_locator = new ilLocatorGUI(false);
-		$i = 1;
-		$ilias_locator->navigate($i++, $lng->txt("personal_desktop"), ILIAS_HTTP_PATH . "/usr_personaldesktop.php", "bottom");
-		$ilias_locator->navigate($i++, $lng->txt("tst_already_taken"), ILIAS_HTTP_PATH . "/assessment/tests_taken.php", "bottom");
-    $ilias_locator->output();
-	}
-
 ?>
