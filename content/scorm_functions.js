c8ee1f07daa42fb5dc31002edf3c6e2b6f36493c
dpetzelt <dpetzelt>
2004-04-19 11:32:13 +0000

    no message
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@3802 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- content/scorm_functions.js
+++ content/scorm_functions.js
@@ -1,72 +1,31 @@
-
-function LMSGetValue(dataModelElement){
- //alert("get");
- return window.document.APIAdapter.LMSGetValue(dataModelElement);
+function LMSGetValue(dataModelElement) {
+ 	return window.document.APIAdapter.LMSGetValue(dataModelElement);
 }
 
-
-
-function LMSSetValue(dataModelElement,elemValue){
-
-
-
-return window.document.APIAdapter.LMSSetValue(dataModelElement,elemValue);
-
-//alert(retval);
-
-//return retval;
-
- //return obj.LMSSetValue(dataModelElement,elemValue);
-
+function LMSSetValue(dataModelElement,elemValue) {
+	return window.document.APIAdapter.LMSSetValue(dataModelElement,elemValue);
 }
 
-
-
-function LMSInitialize(inString){
-
-
- return window.document.APIAdapter.LMSInitialize(inString);
-
+function LMSInitialize(inString) {		
+ 	return window.document.APIAdapter.LMSInitialize(inString);
 }
 
-
-
-function LMSFinish(inString){
-
-
-//alert("finish");
+function LMSFinish(inString) {
  return window.document.APIAdapter.LMSFinish(inString);
-
 }
 
-
-
-function LMSCommit(inString){
-
+function LMSCommit(inString) {
  return window.document.APIAdapter.LMSCommit(inString);
-
 }
 
-
-
-function LMSGetLastError(){
-
- return window.document.APIAdapter.LMSGetLastError();
-
+function LMSGetLastError() {
+ 	return window.document.APIAdapter.LMSGetLastError();
 }
 
-
-
-function LMSGetErrorString(errorNumber){
-
- return window.document.APIAdapter.LMSGetErrorString(errorNumber);
-
+function LMSGetErrorString(errorNumber) {
+ 	return window.document.APIAdapter.APIAdapter.LMSGetErrorString(errorNumber);
 }
 
-
-
-function LMSGetDiagnostics(inString){
-
-return window.document.APIAdapter.LMSGetDiagnostics(inString);
-
+function LMSGetDiagnostics(inString) {
+	return window.document.APIAdapter.LMSGetDiagnostics(inString);
 }
