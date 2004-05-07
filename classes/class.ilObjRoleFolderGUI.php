279a33eef9b92b7a41da17ef32ac87019c1e0ccf
shofmann <shofmann@svn.ilias.de>
2004-05-07 17:36:47 +0000

    you may delete local rulesets of anonymous role (to cancel stoppped inheritance) but not the instance in the main role folder
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@3945 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- classes/class.ilObjRoleFolderGUI.php
+++ classes/class.ilObjRoleFolderGUI.php
@@ -179,7 +179,7 @@ class ilObjRoleFolderGUI extends ilObjectGUI
 				$css_row = ilUtil::switchColor($i+1,"tblrow1","tblrow2");
 
 				// surpress checkbox for particular object types, the system role & anonymous role
-				if (!$this->objDefinition->hasCheckbox($ctrl["type"]) or $ctrl["obj_id"] == SYSTEM_ROLE_ID or $ctrl["obj_id"] == ANONYMOUS_ROLE_ID)
+				if (!$this->objDefinition->hasCheckbox($ctrl["type"]) or $ctrl["obj_id"] == SYSTEM_ROLE_ID or ($ctrl["obj_id"] == ANONYMOUS_ROLE_ID and $this->object->getRefId() == ROLE_FOLDER_ID))
 				{
 					$this->tpl->touchBlock("empty_cell");
 				}
