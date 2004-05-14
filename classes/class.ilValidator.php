d424b158cb6c046f5ce865a498f1a2bea4e8a2fa
shofmann <shofmann@svn.ilias.de>
2004-05-14 16:01:53 +0000

    disabled systemcheck for files. some language corrections
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@4034 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- classes/class.ilValidator.php
+++ classes/class.ilValidator.php
@@ -42,7 +42,7 @@ class ilValidator extends PEAR
 	* list of object types to exclude from recovering
 	* @var	array
 	*/
-	var $object_types_exclude = array("adm","root","ldap","mail","usrf","objf","lngf");
+	var $object_types_exclude = array("adm","root","ldap","mail","usrf","objf","lngf","file");
 	
 	/**
 	* set mode
