e64199765e9e49e3208f10236491861671c73bf8
Alexander Killing <killing@leifos.de>
2004-05-18 16:37:06 +0000

    scorm improvements
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@4059 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- content/apiadapter/IliasApiAdapterApplet.java
+++ content/apiadapter/IliasApiAdapterApplet.java
@@ -1,243 +1,246 @@
+/*
+ * SCORM-1.2 API-Adapter Java Applet for ILIAS
+ * Copyright (c) Matthai Kurian, Jan Gellweiler, Alexander Killing
+ *
+ * Made for ILIAS, the same license terms as for ILIAS itself apply.
+ *
+ * This Applet handles communication between ILIAS and SCORM-1.2
+ * Sharable Content Objects (SCOs). Most communication is via Liveconnect.
+ * CMI (Computer Managed Instruction) data is sent to ILIAS through http POST
+ * to an ILIAS server side PHP script. SCORM-1.2 runtime behavior and CMI
+ * datamodel management is done by the PfPLMS SCORM-1.2 API-Adapter Core. 
+ */
 
 import java.util.Hashtable;
+import java.util.Enumeration;
 import java.net.*;
+import java.io.*;
 
 public	class IliasApiAdapterApplet
 	extends java.applet.Applet
 	implements ch.ethz.pfplms.scorm.api.ApiAdapterInterface
 {
-	private	ch.ethz.pfplms.scorm.api.ApiAdapter api;
+	private	ch.ethz.pfplms.scorm.api.ApiAdapter core;
 
-	private String user_id;
-	private String ref_id;
-	private String student_id;
-	private String student_name;
+	private Hashtable IliasScoCmi  = new Hashtable();
+
+	private String  IliasStudentId;
+	private String  IliasStudentName;
+	private String  IliasRefId;
+	private String  IliasScoId;
+
+	private boolean IliasCredit = false;
+	private boolean isLaunched  = false;
+	private boolean isVerbose   = false;
 
 	public IliasApiAdapterApplet () {
-		api = new ch.ethz.pfplms.scorm.api.ApiAdapter ();
+		core = new ch.ethz.pfplms.scorm.api.ApiAdapter ();
 	}
 
-	public void init () {
-		ref_id  = getParameter("ref_id");
-		student_id = getParameter("student_id");
-		student_name = getParameter("student_name");
+	public	final void init () {
+		//if (getParameter("verbose") != null)
+		isVerbose = true;
+		IliasRefId       = getParameter ("ref_id");
+		IliasStudentId   = getParameter ("student_id");
+		IliasStudentName = getParameter ("student_name");
+		if (getParameter ("credit") != null) {
+			IliasCredit = true;
+			say ("cmi.core.credit=credit");
+		}
+		say ("cmi.core.student_id=" +IliasStudentId);
+		say ("cmi.core.student_name=" +IliasStudentName);
+	}
 
-		System.out.println ("IliasApiAdapterApplet ready.");
+	private final void say (String s) {
+		if (isVerbose) System.out.println (s);
+	}
 
-		System.out.println ("ref_id="+ref_id);
-		System.out.println ("student_id="+student_id);
-		System.out.println ("student_name="+student_name);
+	private final void IliasLaunchContent (String s) {
+		try {
+			getAppletContext().showDocument (
+				new URL(getCodeBase()+s),
+				"scorm_content"
+			);
+		} catch (Exception e) {}
+	}
 
+	public	final void IliasLaunchSco (String sco_id) {
+		if (sco_id == null) return;
+		if (isLaunched && sco_id.equals(IliasScoId)) {
+			say ("SCO " +sco_id +" is already running.");
+			return;
+		}
+		IliasScoCmi.clear();
+		say ("Launching sco " +sco_id);
+		if (isLaunched) say ("Sco "+IliasScoId +" will be unloaded.");
+		isLaunched = false;
+		IliasLaunchContent (
+			"../scorm_presentation.php?cmd=launchSco"
+			+"&sco_id=" + sco_id
+			+"&ref_id=" + IliasRefId
+		);
+		IliasScoId = sco_id;
+	}
+
+	public	final void IliasSetValue (String l, String r) {
+		say ("IliasSetValue("+l+"="+r+")");
+		if (l != null && r != null) IliasScoCmi.put (l, r);
+	}
+
+	private	final void IliasInitialize () {
+		core.sysPut ("cmi.core.student_id",   IliasStudentId);
+		core.sysPut ("cmi.core.student_name", IliasStudentName);
+		if (IliasCredit) {
+			core.sysPut ("cmi.core.credit", "credit");
+			core.sysPut ("cmi.core.lesson_mode", "normal");
+		}
+		core.sysPut (IliasScoCmi);
+		core.transBegin();
+		isLaunched = true;
+	}
+
+	private final String IliasCommit () {
+		core.transEnd();
+		StringBuffer P = new StringBuffer();
+		Hashtable ins = core.getTransNew ();
+		Hashtable mod = core.getTransMod ();
+		core.transBegin();
+
+		int i=0;
+		for (Enumeration e = ins.keys(); e.hasMoreElements(); i++) {
+			Object l = e.nextElement();
+			Object r = ins.get(l);
+			P.append("&iL["+i+"]="+l.toString());
+			P.append("&iR["+i+"]="+URLEncoder.encode(r.toString()));
+		}
+
+		int u=0;
+		for (Enumeration e = mod.keys(); e.hasMoreElements(); u++) {
+			Object l = e.nextElement();
+			Object r = mod.get(l);
+			P.append("&uL["+u+"]="+l.toString());
+			P.append("&uR["+u+"]="+URLEncoder.encode(r.toString()));
+		}
+
+		if (i == 0 && u == 0) {
+			say ("Nothing to do.");
+			return "true";
+		}
+
+		HttpURLConnection http;
+
+		try {
+			http = (HttpURLConnection) ( new java.net.URL (
+				getCodeBase().toString()
+				+ "../scorm_server.php"
+				+ "?cmd=store" 
+				+ "&api=2" 
+				+ "&user_id="+IliasStudentId
+				+ "&sco_id=" +IliasScoId
+			)).openConnection();
+
+			http.setRequestProperty (
+				"Content-Type",
+				"application/x-www-form-urlencoded"
+			);
+			http.setRequestProperty (
+				"Content-Length",
+				Integer.toString (P.length())
+			);
+			http.setDoOutput (true);
+			http.setUseCaches (false);
+			http.setRequestMethod ("POST");
+			http.setAllowUserInteraction (false);
+
+			OutputStream os = http.getOutputStream();
+			say ("post:" +P.toString());
+			os.write (P.toString().getBytes());
+			os.close ();
+			DataInputStream r = new DataInputStream(
+				http.getInputStream ()
+			);
+
+			try {
+				say (r.readUTF());
+				r.close ();
+			} catch (EOFException ok) {}
+
+			say (i +" inserted.");
+			say (u +" updated.");
+			return "true";
+
+		} catch (Exception e) {
+			say ("Ilias cmi storage failed.");
+			say (e.toString());
+			return "false";
+		}
+	}
+
+	private	final void IliasFinish () {
+		core.transEnd();
+		if (!isLaunched) return;
+		IliasLaunchContent (
+			"../scorm_presentation.php?cmd=view"
+			+"&ref_id=" + IliasRefId
+		);
+		isLaunched = false;
 	}
 
 	/*
 	 * Liveconnect interface methods for SCO
 	 */
-	public	String LMSInitialize (String s) {
 
-		String rv = api.LMSInitialize(s);
-
-		/*
-		 * Override or modify this method to load learners stored cmi
-		 * data from your RTE backend
-		 */
-		System.out.println ("LMSInitialize("+s+")="+rv);
-
+	public	final String LMSInitialize (String s) { 
+		core.reset();
+		String rv = core.LMSInitialize(s);
+		IliasInitialize ();
+		say ("LMSInitialize("+s+")="+rv);
 		return rv;
 	}
 
-	public	String LMSCommit (String s) {
-
-		String rv = api.LMSCommit(s);
-		/*
-		 * Override or modify this method to store/update learners
-		 * cmi data in your RTE backend
-		 */
-		System.out.println ("LMSCommit("+s+")="+rv);
-
+	public	final String LMSCommit (String s) {
+		String rv = core.LMSCommit(s);
+		if (rv.equals("false")) return rv;
+		rv = IliasCommit(); 
+		say ("LMSCommit("+s+")="+rv);
 		return rv;
 	}
 
-	public	String LMSFinish (String s) {
-
-		String rv = api.LMSFinish(s);
-		/*
-		 * Override or modify this method to store/update learners
-		 * cmi data in your RTE backend
-		 */
-		System.out.println ("LMSFinish("+s+")="+rv);
-
+	public	final String LMSFinish (String s) {
+		String rv = core.LMSFinish(s);
+		say ("LMSFinish("+s+")="+rv);
+		if (rv.equals("false")) return rv;
+		IliasFinish();
 		return rv;
 	}
 
-	/*
-	 * The following methods can be left as they are, they are 
-	 * related only to the internal state of the API adapter.
-	 */
-	public	String LMSGetDiagnostic (String e) {
-		// return LMSGetDiagnostic (e);
-		
-		String rv = api.LMSGetDiagnostic (e);
-		System.out.println ("LMSGetDiagnostic("+e+")="+rv);
+	public	final String LMSGetDiagnostic (String e) {
+		String rv = core.LMSGetDiagnostic (e);
+		say ("LMSGetDiagnostic("+e+")="+rv);
 		return rv;
 	}
 
-	public	String LMSGetErrorString (String e) {
-		//return api.LMSGetErrorString(e);
-		
-		String rv = api.LMSGetErrorString (e);
-		System.out.println ("LMSGetErrorString("+e+")="+rv);
+	public	final String LMSGetErrorString (String e) {
+		String rv = core.LMSGetErrorString (e);
+		say ("LMSGetErrorString("+e+")="+rv);
 		return rv;
 	}
 
-	public	String LMSGetLastError () {
-		// return api.LMSGetLastError();
-		
-		String rv = api.LMSGetLastError ();
-		System.out.println ("LMSLastError()="+rv);
+	public	final String LMSGetLastError () {
+		String rv = core.LMSGetLastError ();
+		say ("LMSLastError()="+rv);
 		return rv;
 	}
 
-	public	String LMSGetValue (String l) {
-		//return api.LMSGetValue(l);
-
-		String rv = api.LMSGetValue (l);
-		System.out.println ("LMSGetValue("+l+")="+rv);
+	public	final String LMSGetValue (String l) {
+		String rv = core.LMSGetValue (l);
+		say ("LMSGetValue("+l+")="+rv);
 		return rv;
 	}
 
-	public	String LMSSetValue (String l, String r) {
-		//return (api.LMSSetValue(l,r));
-
-		String rv = api.LMSSetValue (l, r);
-		System.out.println ("LMSSetValue("+l+"="+r+")="+rv);
+	public	final String LMSSetValue (String l, String r) {
+		String rv = core.LMSSetValue (l, r);
+		say ("LMSSetValue("+l+"="+r+")="+rv);
 		return rv;
 	}
-
-
-	/*
-	 * Methods to integrate into Ilias
-	 */
-
-	public	void IliasLaunchSco (String sco_id) {
-
-		reset ();
-
-		api.sysPut ("cmi.core.student_id",   student_id);
-		api.sysPut ("cmi.core.student_name", student_name);
-
-		try {
-			getAppletContext().showDocument(
-				new URL(
-					getCodeBase()
-					+"../scorm_presentation.php?cmd=launchSco"
-					+"&sco_id=" + sco_id
-					+"&ref_id=" + ref_id
-				),
-				"scorm_content"
-			);
-		} catch (Exception e) {
-		}
-		
-	}
-
-	public	void IliasSetValue (String l, String r) {
-		System.out.println ("IliasSetValue("+l+"="+r+")");
-		if (l != null && r != null) sysPut (l, r);
-	}
-
-	private void IliasStoreCmi () {
-
-		HttpURLConnection http;
-		try {
-			http = (HttpURLConnection) (
-					new java.net.URL (
-						getCodeBase().toString() + "../scorm_server.php"
-					)
-				).openConnection();
-
-			http.setRequestMethod("POST");
-			http.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
-			http.setDoOutput(true);
-			http.setUseCaches (false);
-
-		} catch (Exception e) {
-			System.out.println (e);
-			return;
-		}
-
-		/*
-		URL url = new URL (getCodeBase().toString() + "YourFormhandler.php");
-		HttpURLConnection http = (HttpURLConnection)url.openConnection();
-
-		byte [] data = "var0=Name".getBytes();
-		http.setRequestMethod("POST");
-		http.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
-		http.setDoOutput(true);
-		http.setUseCaches (false);
-
-		OutputStream os = http.getOutputStream();
-		os.write(data);
-		os.close();
-
-		StringBuffer contentBuffer = new StringBuffer();
-		PrintWriter out = new PrintWriter(http.getOutputStream ());
-		contentBuffer.append("your1DataParam=" + URLEncoder.encode(your1Data));
-		contentBuffer.append("&your2DataParam=" + URLEncoder.encode(your2Data));
-		String content = new String(contentBuffer.toString());
-		out.println(content);
-		out.flush ();
-		out.close ();
-		*/
-
-	}
-
-	/*
-	 * Extra methods needed for integration into RTE
-	 */
-	private	void	reset () {
-		api.reset();
-	}
-
-	private void sysPut (Hashtable cmi) {
-		api.sysPut (cmi);
-	}
-
-	private void sysPut (String l,  String r) {
-		api.sysPut (l, r);
-	}
-
-	private  String sysGet (String l) {
-		return api.sysGet (l);
-	}
-
-	private  Hashtable getCmiHash () {
-		return api.getCmiHash ();
-	}
-
-	/*
-	 * Incremental RTE backend storage support methods, use these methods
-	 * to minimize traffic and load to your RTE backend.
-	 */
-
-	private	void transBegin () {
-		// marks beginning of changes to current internal cmi data state
-		api.transBegin ();
-	}
-
-	private	void transEnd () {
-		// marks end of changes to internal cmi data state
-		api.transEnd ();
-	}
-
-	private	Hashtable getTransMod () {
-		// delivers modified cmi elements
-		return api.getTransMod ();
-	}
-
-	private	Hashtable getTransNew () {
-		// delivers inserted cmi elements
-		return api.getTransNew ();
-	}
-
 }
