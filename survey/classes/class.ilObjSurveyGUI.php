31b14601ec530e75ab53e708fc7c89c5a97d60d8
hschottm <hschottm>
2004-05-22 22:53:10 +0000

    Survey tool: Created code for question blocks Survey tool: Created survey properties form
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@4072 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- survey/classes/class.ilObjSurveyGUI.php
+++ survey/classes/class.ilObjSurveyGUI.php
@@ -35,6 +35,7 @@
 
 require_once "classes/class.ilObjectGUI.php";
 require_once "classes/class.ilMetaDataGUI.php";
+require_once "classes/class.ilUtil.php";
 
 class ilObjSurveyGUI extends ilObjectGUI
 {
@@ -79,5 +80,79 @@ class ilObjSurveyGUI extends ilObjectGUI
 		header("Location:".$this->getReturnLocation("save","questionpool.php?".$this->link_params));
 		exit();
 	}
+
+/**
+* Returns the GET parameters for the survey object URLs
+*
+* Returns the GET parameters for the survey object URLs
+*
+* @access public
+*/
+  function getAddParameter() 
+  {
+    return "?ref_id=" . $_GET["ref_id"] . "&cmd=" . $_GET["cmd"];
+  }
+	
+/**
+* Creates the properties form for the survey object
+*
+* Creates the properties form for the survey object
+*
+* @access public
+*/
+  function propertiesObject()
+  {
+		global $rbacsystem;
+		
+    $add_parameter = $this->getAddParameter();
+    if ($_POST["cmd"]["save"]) {
+			$this->updateObject();
+      sendInfo($this->lng->txt("msg_obj_modified"), true);
+			$path = $this->tree->getPathFull($this->object->getRefID());
+      header("location: ". $this->getReturnLocation("cancel","/ilias3/repository.php?ref_id=" . $path[count($path) - 2]["child"]));
+			exit();
+    }
+    if ($_POST["cmd"]["apply"]) {
+			$this->updateObject();
+      sendInfo($this->lng->txt("msg_obj_modified"));
+    }
+    if ($_POST["cmd"]["cancel"]) {
+      sendInfo($this->lng->txt("msg_cancel"), true);
+			$path = $this->tree->getPathFull($this->object->getRefID());
+      header("location: ". $this->getReturnLocation("cancel","/ilias3/repository.php?ref_id=" . $path[count($path) - 2]["child"]));
+      exit();
+    }
+
+		$this->tpl->addBlockFile("ADM_CONTENT", "adm_content", "tpl.il_svy_svy_properties.html", true);
+		$this->tpl->setCurrentBlock("adm_content");
+		$this->tpl->setVariable("TEXT_TITLE", $this->lng->txt("title"));
+		$this->tpl->setVariable("VALUE_TITLE", $this->object->getTitle());
+		$this->tpl->setVariable("TEXT_AUTHOR", $this->lng->txt("author"));
+//		$this->tpl->setVariable("VALUE_AUTHOR", $this->object->getAuthor());
+		$this->tpl->setVariable("TEXT_DESCRIPTION", $this->lng->txt("description"));
+		$this->tpl->setVariable("VALUE_DESCRIPTION", $this->object->getDescription());
+		$this->tpl->setVariable("TEXT_INTRODUCTION", $this->lng->txt("introduction"));
+//		$this->tpl->setVariable("VALUE_INTRODUCTION", $this->object->getIntroduction());
+		$this->tpl->setVariable("TEXT_STATUS", $this->lng->txt("status"));
+		$this->tpl->setVariable("TEXT_START_DATE", $this->lng->txt("start_date"));
+		$this->tpl->setVariable("VALUE_START_DATE", ilUtil::makeDateSelect("start_date", "", "", ""));
+		$this->tpl->setVariable("TEXT_END_DATE", $this->lng->txt("end_date"));
+		$this->tpl->setVariable("VALUE_END_DATE", ilUtil::makeDateSelect("end_date", "", "", ""));
+		$this->tpl->setVariable("TEXT_EVALUATION_ACCESS", $this->lng->txt("evaluation_access"));
+		$this->tpl->setVariable("VALUE_OFFLINE", $this->lng->txt("offline"));
+		$this->tpl->setVariable("VALUE_ONLINE", $this->lng->txt("online"));
+		$this->tpl->setVariable("TEXT_ENABLED", $this->lng->txt("enabled"));
+		$this->tpl->setVariable("VALUE_OFF", $this->lng->txt("off"));
+		$this->tpl->setVariable("VALUE_ON", $this->lng->txt("on"));
+
+		$this->tpl->setVariable("TXT_REQUIRED_FLD", $this->lng->txt("required_field"));
+    if ($rbacsystem->checkAccess('write', $this->ref_id)) {
+			$this->tpl->setVariable("APPLY", $this->lng->txt("apply"));
+			$this->tpl->setVariable("SAVE", $this->lng->txt("save"));
+			$this->tpl->setVariable("CANCEL", $this->lng->txt("cancel"));
+		}
+    $this->tpl->parseCurrentBlock();
+  }
+	
 } // END class.ilObjSurveyGUI
 ?>
