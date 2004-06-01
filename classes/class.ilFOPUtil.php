96dd7c9f559ef65d3995038812fcd64bc9a9f26b
Alexander Killing <killing@leifos.de>
2004-06-01 09:57:39 +0000

    avoid additional newlines in code paragraphs
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@4128 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- classes/class.ilFOPUtil.php
+++ classes/class.ilFOPUtil.php
@@ -48,24 +48,27 @@ class ilFOPUtil
 	*/
 	function makePDF($a_from, $a_to)
 	{
+		$saved = getenv("JAVACMD");        // save old value
+		putenv("JAVACMD=".PATH_TO_JAVA);
+
 		$fop_cmd = ilFOPUtil::getFOPCmd()." -fo ".
 			$a_from." -pdf ".$a_to;
-echo $fop_cmd."<br>";
-		echo ":";
+//echo $fop_cmd."<br>:";
 
 		//error_reporting(E_ALL);
 
-/* Add redirection so we can get stderr. */
-$handle = popen($fop_cmd.' 2>&1', 'r');
-echo "'$handle'; " . gettype($handle) . "\n";
-$read = fread($handle, 2096);
-echo $read;
-pclose($handle);
+		/* Add redirection so we can get stderr. */
+		$handle = popen($fop_cmd.' 2>&1', 'r');
+//echo "'$handle'; " . gettype($handle) . "\n";
+		$read = fread($handle, 2096);
+//echo $read;
+		pclose($handle);
 
 		$ret = exec($fop_cmd, $arr, $r2);
-		echo $ret.$r2;
-		echo ":";
-		var_dump($arr);
+//echo $ret.$r2;
+//echo ":";
+//var_dump($arr);
+		putenv("JAVACMD=$saved");
 	}
 
 } // END class.ilFOPUtil
