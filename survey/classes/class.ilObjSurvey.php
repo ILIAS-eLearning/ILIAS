54c82661aae3980b7c4736128353afedd4791ba6
hschottm <hschottm@svn.ilias.de>
2004-06-09 08:54:54 +0000

    Survey tool: Added copy mechanism for inserted survey questions Survey tool: Added missing phrase relations
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@4179 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- survey/classes/class.ilObjSurvey.php
+++ survey/classes/class.ilObjSurvey.php
@@ -421,6 +421,80 @@ class ilObjSurvey extends ilObject
 	}
 
 /**
+* Takes a question and creates a copy of the question for use in the survey
+*
+* Takes a question and creates a copy of the question for use in the survey
+*
+* @param integer $question_id The database id of the question
+* @result integer The database id of the copied question
+* @access public
+*/
+	function duplicateQuestionForSurvey($question_id)
+	{
+		global $ilUser;
+		
+		$query = sprintf("SELECT * FROM survey_question WHERE question_id = %s",
+			$this->ilias->db->quote($question_id)
+		);
+    $result = $this->ilias->db->query($query);
+		if ($result->numRows() == 0)
+		{
+			return 0;
+		}
+		$row = $result->fetchRow(DB_FETCHMODE_OBJECT);
+		$query = sprintf("INSERT INTO survey_question (question_id, subtype, questiontype_fi, ref_fi, owner_fi, title, description, author, questiontext, obligatory, complete, created, original_id, TIMESTAMP)" .
+			" VALUES (NULL, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, NULL)",
+			$this->ilias->db->quote("$row->subtype"),
+			$this->ilias->db->quote("$row->questiontype_fi"),
+			$this->ilias->db->quote("0"),
+			$this->ilias->db->quote("$ilUser->id"),
+			$this->ilias->db->quote("$row->title"),
+			$this->ilias->db->quote("$row->description"),
+			$this->ilias->db->quote("$row->author"),
+			$this->ilias->db->quote("$row->questiontext"),
+			$this->ilias->db->quote("$row->obligatory"),
+			$this->ilias->db->quote("$row->complete"),
+			$this->ilias->db->quote("$row->created"),
+			$this->ilias->db->quote("$row->question_id")
+		);
+    $result = $this->ilias->db->query($query);
+		$duplicate_id = $this->ilias->db->getLastInsertId();
+		
+		// copy question variables
+		$query = sprintf("SELECT * FROM survey_variable WHERE question_fi = %s",
+			$this->ilias->db->quote($question_id)
+		);
+    $result = $this->ilias->db->query($query);
+		while ($row = $result->fetchRow(DB_FETCHMODE_OBJECT))
+		{
+			$insertquery = sprintf("INSERT INTO survey_variable (variable_id, category_fi, question_fi, value1, value2, sequence, TIMESTAMP) VALUES (NULL, %s, %s, %s, %s, %s, NULL)",
+				$this->ilias->db->quote("$row->category_fi"),
+				$this->ilias->db->quote("duplicate_id"),
+				$this->ilias->db->quote("$row->value1"),
+				$this->ilias->db->quote("$row->value2"),
+				$this->ilias->db->quote("$row->sequence")
+			);
+	    $insertresult = $this->ilias->db->query($insertquery);
+		}
+
+		// copy question materials
+		$query = sprintf("SELECT * FROM survey_question_material WHERE question_fi = %s",
+			$this->ilias->db->quote($question_id)
+		);
+    $result = $this->ilias->db->query($query);
+		while ($row = $result->fetchRow(DB_FETCHMODE_OBJECT))
+		{
+			$insertquery = sprintf("INSERT INTO survey_question_material (material_id, question_fi, materials, materials_file) VALUES (NULL, %s, %s, %s)",
+				$this->ilias->db->quote("duplicate_id"),
+				$this->ilias->db->quote("$row->materials"),
+				$this->ilias->db->quote("$row->materials_file"),
+			);
+	    $insertresult = $this->ilias->db->query($insertquery);
+		}
+		return $duplicate_id;
+	}
+
+/**
 * Inserts a question in the survey and saves the relation to the database
 *
 * Inserts a question in the survey and saves the relation to the database
@@ -434,9 +508,10 @@ class ilObjSurvey extends ilObject
     );
     $result = $this->ilias->db->query($query);
     $sequence = $result->numRows();
+		$duplicate_id = $this->duplicateQuestionForSurvey($question_id);
     $query = sprintf("INSERT INTO survey_survey_question (survey_question_id, survey_fi, question_fi, sequence, TIMESTAMP) VALUES (NULL, %s, %s, %s, NULL)",
       $this->ilias->db->quote($this->getSurveyId()),
-      $this->ilias->db->quote($question_id),
+      $this->ilias->db->quote($duplicate_id),
       $this->ilias->db->quote($sequence)
     );
     $result = $this->ilias->db->query($query);
@@ -1327,6 +1402,35 @@ class ilObjSurvey extends ilObject
 		$this->questions = array_values(array_merge($part1, $move_questions, $part2));
 		$this->saveQuestionsToDb();
 	}
+	
+/**
+* Remove a question from the survey
+*
+* Remove a question from the survey
+*
+* @param integer $question_id The database id of the question
+* @access public
+*/
+	function removeQuestion($question_id)
+	{
+		// Delete the question from the question table (don't worry, it's just a copy)
+		$query = sprintf("DELETE FROM survey_question WHERE question_id = %s",
+			$this->ilias->db->quote($question_id)
+		);
+		$result = $this->ilias->db->query($query);
+		// Delete the materials link of the question
+		$query = sprintf("DELETE FROM survey_question_material WHERE question_fi = %s",
+			$this->ilias->db->quote($question_id)
+		);
+		$result = $this->ilias->db->query($query);
+		// Delete the variables of the question
+		$query = sprintf("DELETE FROM survey_variable WHERE question_fi = %s",
+			$this->ilias->db->quote($question_id)
+		);
+		$result = $this->ilias->db->query($query);
+		// Delete constraints of the question
+		$this->deleteConstraints($question_id);
+	}
 		
 /**
 * Remove questions from the survey
@@ -1345,7 +1449,7 @@ class ilObjSurvey extends ilObject
 			if (in_array($question_id, $remove_questions) or in_array($data["questionblock_id"], $remove_questionblocks))
 			{
 				unset($this->questions[array_search($question_id, $this->questions)]);
-				$this->deleteConstraints($question_id);
+				$this->removeQuestion($question_id);
 			}
 		}
 		foreach ($remove_questionblocks as $questionblock_id)
