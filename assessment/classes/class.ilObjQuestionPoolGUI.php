e99173e09e87f4fd0ce1df68f6e117c27c13e573
hschottm <hschottm@svn.ilias.de>
2004-09-10 15:41:19 +0000

    Fixed corrupt location bar targets in Assessment and Survey classes
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@4913 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- assessment/classes/class.ilObjQuestionPoolGUI.php
+++ assessment/classes/class.ilObjQuestionPoolGUI.php
@@ -1176,7 +1176,7 @@ class ilObjQuestionPoolGUI extends ilObjectGUI
 		{
 			foreach ($path as $key => $row)
 			{
-				$ilias_locator->navigate($i++, $row["title"], ILIAS_HTTP_PATH . "/adm_object.php?ref_id=".$row["child"],"bottom");
+				$ilias_locator->navigate($i++, $row["title"], ILIAS_HTTP_PATH . "/adm_object.php?ref_id=".$row["child"],"target=\"bottom\"");
 			}
 		}
 		else
@@ -1190,7 +1190,7 @@ class ilObjQuestionPoolGUI extends ilObjectGUI
 				if ($this->ref_id == $row["child"])
 				{
 					$param = "&cmd=questions";
-					$ilias_locator->navigate($i++, $row["title"], ILIAS_HTTP_PATH . "/assessment/questionpool.php" . "?ref_id=".$row["child"] . $param,"bottom");
+					$ilias_locator->navigate($i++, $row["title"], ILIAS_HTTP_PATH . "/assessment/questionpool.php" . "?ref_id=".$row["child"] . $param,"target=\"bottom\"");
 					switch ($_GET["cmd"])
 					{
 						case "question":
@@ -1201,21 +1201,21 @@ class ilObjQuestionPoolGUI extends ilObjectGUI
 							}
 							if ($question_title)
 							{
-								$ilias_locator->navigate($i++, $question_title, ILIAS_HTTP_PATH . "/assessment/questionpool.php" . "?ref_id=".$row["child"] . "&cmd=question&edit=$id","bottom");
+								$ilias_locator->navigate($i++, $question_title, ILIAS_HTTP_PATH . "/assessment/questionpool.php" . "?ref_id=".$row["child"] . "&cmd=question&edit=$id","target=\"bottom\"");
 							}
 							break;
 					}
 				}
 				else
 				{
-					$ilias_locator->navigate($i++, $row["title"], ILIAS_HTTP_PATH . "/" . $scriptname."?ref_id=".$row["child"],"bottom");
+					$ilias_locator->navigate($i++, $row["title"], ILIAS_HTTP_PATH . "/" . $scriptname."?ref_id=".$row["child"],"target=\"bottom\"");
 				}
 			}
 
 			if (isset($_GET["obj_id"]))
 			{
 				$obj_data =& $this->ilias->obj_factory->getInstanceByObjId($_GET["obj_id"]);
-				$ilias_locator->navigate($i++,$obj_data->getTitle(),$scriptname."?ref_id=".$_GET["ref_id"]."&obj_id=".$_GET["obj_id"],"bottom");
+				$ilias_locator->navigate($i++,$obj_data->getTitle(),$scriptname."?ref_id=".$_GET["ref_id"]."&obj_id=".$_GET["obj_id"],"target=\"bottom\"");
 			}
 		}
 		$ilias_locator->output(true);
