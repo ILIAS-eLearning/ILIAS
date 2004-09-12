12b0199ff4b1f23f4509849b96d30baa145ac0ba
hschottm <hschottm>
2004-09-12 15:01:48 +0000

    Survey Tool: Added default sort order for question browser
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@4921 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- survey/classes/class.ilObjSurveyQuestionPoolGUI.php
+++ survey/classes/class.ilObjSurveyQuestionPoolGUI.php
@@ -879,6 +879,11 @@ class ilObjSurveyQuestionPoolGUI extends ilObjectGUI
 		{
 			$startrow = $_GET["startrow"];
 		}
+		if (!$_GET["sort"])
+		{
+			// default sort order
+			$_GET["sort"] = array("title" => "ASC");
+		}
 		$table = $this->object->getQuestionsTable($_GET["sort"], $_POST["filter_text"], $_POST["sel_filter_type"], $startrow);
     $colors = array("tblrow1", "tblrow2");
     $counter = 0;
