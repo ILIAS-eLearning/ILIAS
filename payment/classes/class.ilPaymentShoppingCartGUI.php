32fb233e416bec9c4a77a4aef8f9c80e40577b65
Stefan Meyer <smeyer.ilias@gmx.de>
2004-09-27 14:54:51 +0000

    added new table in shopping cart
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@5056 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- payment/classes/class.ilPaymentShoppingCartGUI.php
+++ payment/classes/class.ilPaymentShoppingCartGUI.php
@@ -80,20 +80,111 @@ class ilPaymentShoppingCartGUI extends ilPaymentBaseGUI
 	{
 		$this->__initShoppingCartObject();
 
-		if(!count($this->psc_obj->getEntries()))
+		if(!count($items = $this->psc_obj->getEntries()))
 		{
 			sendInfo($this->lng->txt('pay_shopping_cart_empty'));
 		}
 
 		$this->tpl->addBlockfile('ADM_CONTENT','adm_content','tpl.pay_shopping_cart.html',true);
-		$this->tpl->setVariable("HALLO",'hallo');
 
+		$counter = 0;
+		foreach($items as $item)
+		{
+			$tmp_pobject =& new ilPaymentObject($this->user_obj,$item['pobject_id']);
+
+			$tmp_obj =& ilObjectFactory::getInstanceByRefId($tmp_pobject->getRefId());
+
+			$price_arr = ilPaymentPrices::_getPrice($item['price_id']);
+
+
+			$f_result[$counter][] = ilUtil::formCheckBox(0,'item[]',$item['pobject_id']);
+			$f_result[$counter][] = $tmp_obj->getTitle();
+			$f_result[$counter][] = $price_arr['duration'];
+
+			$price = $price_arr['unit_value'].' '.$this->lng->txt('paya_euro');
+
+			if($price_arr['sub_unit_value'])
+			{
+				$price .= ' '.$price_arr['sub_unit_value'].' '.$this->lng->txt('paya_cent');
+			}
+			$f_result[$counter][] = $price;
+
+			unset($tmp_obj);
+			unset($tmp_pobject);
+			
+			++$counter;
+		}
+			
+		return $this->__showItemsTable($f_result);
 	}
 	
+	function __showItemsTable($a_result_set)
+	{
+		$tbl =& $this->initTableGUI();
+		$tpl =& $tbl->getTemplateObject();
+
+		// SET FORMAACTION
+		$tpl->setCurrentBlock("tbl_form_header");
+
+		$tpl->setVariable("FORMACTION",$this->ctrl->getFormAction($this));
+		$tpl->parseCurrentBlock();
+
+		$tpl->setCurrentBlock("tbl_action_row");
+		$tpl->setCurrentBlock("plain_buttons");
+		$tpl->parseCurrentBlock();
+
+		$tpl->setVariable("COLUMN_COUNTS",4);
+		$tpl->setVariable("IMG_ARROW", ilUtil::getImagePath("arrow_downright.gif"));
+
+		$tpl->setCurrentBlock("tbl_action_button");
+		$tpl->setVariable("BTN_NAME","deleteItem");
+		$tpl->setVariable("BTN_VALUE",$this->lng->txt("delete"));
+		$tpl->parseCurrentBlock();
+		$tpl->setCurrentBlock("tbl_action_row");
+		$tpl->setVariable("TPLPATH",$this->tpl->tplPath);
+		$tpl->parseCurrentBlock();
+
+		$tbl->setTitle($this->lng->txt("paya_statistic"),"icon_pays_b.gif",$this->lng->txt("paya_statistic"));
+		$tbl->setHeaderNames(array($this->lng->txt(""),
+								   $this->lng->txt("title"),
+								   $this->lng->txt("paya_duration"),
+								   $this->lng->txt("paya_price")));
+
+		$tbl->setHeaderVars(array("",
+								  "title",
+								  "duration",
+								  "price"),
+							array("cmd" => "",
+								  "cmdClass" => "ilpaymentshoppingcartgui",
+								  "cmdNode" => $_GET["cmdNode"]));
+
+		$offset = $_GET["offset"];
+		$order = $_GET["sort_by"];
+		$direction = $_GET["sort_order"] ? $_GET['sort_order'] : 'desc';
+
+		$tbl->setOrderColumn($order,'title');
+		$tbl->setOrderDirection($direction);
+		$tbl->setOffset($offset);
+		$tbl->setLimit($_GET["limit"]);
+		$tbl->setMaxCount(count($a_result_set));
+		$tbl->setFooter("tblfooter",$this->lng->txt("previous"),$this->lng->txt("next"));
+		$tbl->setData($a_result_set);
+
+
+		$tbl->render();
+
+		$this->tpl->setVariable("ITEMS_TABLE",$tbl->tpl->get());
+
+		return true;
+	}
+		
+
 	function __initShoppingCartObject()
 	{
 		$this->psc_obj =& new ilPaymentShoppingCart($this->user_obj);
 	}
+
+	
 
 }
 ?>
