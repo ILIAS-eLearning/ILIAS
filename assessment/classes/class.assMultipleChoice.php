19ae97a50b6a6860ac7a6296887990c48ec8393e
hschottm <hschottm@svn.ilias.de>
2004-10-13 12:27:26 +0000

    merged fix 24 of Release_3_2_0_branch
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@5270 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- assessment/classes/class.assMultipleChoice.php
+++ assessment/classes/class.assMultipleChoice.php
@@ -280,7 +280,6 @@ class ASS_MultipleChoice extends ASS_Question
 		$qtiComment->append_child($qtiCommentText);
 		$qtiIdent->append_child($qtiComment);
 		// add estimated working time
-		$qtiIdent->append_child($qtiComment);
 		$qtiDuration = $this->domxml->create_element("duration");
 		$workingtime = $this->getEstimatedWorkingTime();
 		$qtiDurationText = $this->domxml->create_text_node(sprintf("P0Y0M0DT%dH%dM%dS", $workingtime["h"], $workingtime["m"], $workingtime["s"]));
