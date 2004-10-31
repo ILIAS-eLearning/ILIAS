a2ce2aadb11820bdf64c2cdedafbea7e6639440e
hschottm <hschottm@svn.ilias.de>
2004-10-31 15:57:44 +0000

    merged fix 15 for Release_3_2_1
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@5565 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- survey/classes/class.ilObjSurvey.php
+++ survey/classes/class.ilObjSurvey.php
@@ -4021,5 +4021,26 @@ class ilObjSurvey extends ilObject
 		return $result;
 	}
 	
+	/**
+	* update meta data only
+	*/
+	function updateMetaData()
+	{
+		$this->initMeta();
+		$this->meta_data->update();
+		if ($this->meta_data->section != "General")
+		{
+			$meta = $this->meta_data->getElement("Title", "General");
+			$this->meta_data->setTitle($meta[0]["value"]);
+			$meta = $this->meta_data->getElement("Description", "General");
+			$this->meta_data->setDescription($meta[0]["value"]);
+		}
+		else
+		{
+			$this->setTitle($this->meta_data->getTitle());
+			$this->setDescription($this->meta_data->getDescription());
+		}
+		parent::update();
+	}
 } // END class.ilObjSurvey
 ?>
