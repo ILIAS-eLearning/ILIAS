7e58804e8342bf31df723bb693810f884f88983b
hschottm <hschottm@svn.ilias.de>
2004-11-01 10:01:43 +0000

    Added test import form in test create form, so creating OR importing a test is available through the repository
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@5574 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- assessment/classes/class.ilObjQuestionPoolGUI.php
+++ assessment/classes/class.ilObjQuestionPoolGUI.php
@@ -783,7 +783,7 @@ class ilObjQuestionPoolGUI extends ilObjectGUI
 				$this->tpl->setCurrentBlock("pages");
 				if ($table["startrow"] == $i)
 				{
-					$this->tpl->setVariable("PAGE_NUMBER", "<strong>$counter</strong>");
+					$this->tpl->setVariable("PAGE_NUMBER", "<span class=\"inactivepage\">$counter</span>");
 				}
 				else
 				{
