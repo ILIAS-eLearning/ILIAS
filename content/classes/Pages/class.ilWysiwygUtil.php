4ce357e8855df2a067346d1fd13f37f8c79348ae
Aresch Yavari <ay@databay.de>
2004-11-04 13:32:32 +0000

    wysiwyg-paragraph-editor
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@5677 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ content/classes/Pages/class.ilWysiwygUtil.php
@@ -0,0 +1,133 @@
+<?php
+/*
+	+-----------------------------------------------------------------------------+
+	| ILIAS open source                                                           |
+	+-----------------------------------------------------------------------------+
+	| Copyright (c) 1998-2001 ILIAS open source, University of Cologne            |
+	|                                                                             |
+	| This program is free software; you can redistribute it and/or               |
+	| modify it under the terms of the GNU General Public License                 |
+	| as published by the Free Software Foundation; either version 2              |
+	| of the License, or (at your option) any later version.                      |
+	|                                                                             |
+	| This program is distributed in the hope that it will be useful,             |
+	| but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+	| GNU General Public License for more details.                                |
+	|                                                                             |
+	| You should have received a copy of the GNU General Public License           |
+	| along with this program; if not, write to the Free Software                 |
+	| Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
+	+-----------------------------------------------------------------------------+
+*/
+
+
+class ilWysiwygUtil
+{
+    
+    var $struct = array();
+    var $depth;
+    var $newXml;
+    function convertFromPost($content) {
+        
+        $xml_parser = xml_parser_create("UTF-8");
+        xml_parser_set_option($xml_parser, XML_OPTION_CASE_FOLDING, false);
+        xml_set_object($xml_parser,$this);
+        xml_set_element_handler($xml_parser, "startElement", "endElement");
+        xml_set_character_data_handler($xml_parser, "characterData");
+
+        $xml_data = "<xml>".$content."</xml>";
+        
+        $this->depth = 0;
+        $this->struct = array();
+        $this->$newXml = "";
+        
+        if (!xml_parse($xml_parser, $xml_data)) {
+            die(sprintf("XML error: %s at line %d",	xml_error_string(xml_get_error_code($xml_parser)),xml_get_current_line_number($xml_parser)));
+        }
+        xml_parser_free($xml_parser);
+        
+        $this->newXml = str_replace("<xml>","",$this->newXml);
+        $this->newXml = str_replace("</xml>","",$this->newXml);
+        
+        return($this->newXml);
+    }
+
+    
+    
+
+    //{{{
+    function startElement($parser, $name, $attrs) 
+	{
+        $new = array("name" => $name,
+                                "attrs" => $attrs,
+                                "convert" => "",
+                                "convert2" => ""
+                                );
+
+        $new["convert"] = "<".$name;                
+        if (is_array($attrs)) {
+            reset ($attrs);
+            while (list ($key, $val) = each ($attrs)) {
+                $new["convert"] .= " ".$key."=\"".$val."\"";
+            }
+        }
+        $new["convert"] .= ">";
+        $new["convert2"] = "</".$name.">";
+        
+        if ($attrs["style"] == "font-weight: bold;") {
+            $new["convert"] = "[str]";
+            $new["convert2"] = "[/str]";
+        }
+        if ($attrs["style"] == "color: rgb(0, 128, 0);") {
+            $new["convert"] = "[com]";
+            $new["convert2"] = "[/com]";
+        }
+        
+        if ($attrs["style"] == "color: rgb(165, 42, 42); font-style: italic;") {
+            $new["convert"] = "[quot]";
+            $new["convert2"] = "[/quot]";
+        }
+        
+        if ($name == "code") {
+            $new["convert"] = "[code]";
+            $new["convert2"] = "[/code]";
+        }
+        
+        //echo htmlspecialchars($new[convert]);
+                                
+        $this->struct[$this->depth] = $new;
+        $this->depth++;
+        
+        if ($name!="br") {
+            $this->newXml .= $new[convert];
+        } else {
+            $this->newXml .= "\n";
+        }
+        
+//        vd($name);
+//        vd($attrs);
+    }
+    
+    function characterData($parser, $data) 
+	{
+//        vd($data);
+        $this->newXml .= $data;
+    }
+    
+    function endElement($parser, $name)
+    {
+        $this->depth--;
+        
+        //$this->newXml .= "</".$name.">";
+        if ($name!="br") {
+            $this->newXml .= $this->struct[$this->depth]["convert2"];
+        }
+        
+//        vd($name);
+    }
+    // }}}  
+    
+}
+
+?>
