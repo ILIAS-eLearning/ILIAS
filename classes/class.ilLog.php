b52937256f35890e61739122d25aca45cab7b546
hschottm <hschottm@svn.ilias.de>
2004-11-11 17:37:48 +0000

    merged fix 12 for Release_3_2_2
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@5771 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- classes/class.ilLog.php
+++ classes/class.ilLog.php
@@ -47,6 +47,27 @@ class ilLog
 	var $filename;
 	var $tag;
 	var $log_format;
+ 
+	/**
+	* Log level 10: Log only fatal errors that could lead to serious problems
+	* @var		integer
+	* @access	public
+	*/
+	var $FATAL;
+
+	/**
+	* Log level 20: This is the standard log level that is set if no level is given
+	* @var		integer
+	* @access	public
+	*/
+	var $WARNING;
+
+	/**
+	* Log level 30: Logs messages and notices that are less important for system functionality like not translated language values
+	* @var		integer
+	* @access	public
+	*/
+	var $MESSAGE;
 
 	/**
 	* constructor
@@ -57,16 +78,66 @@ class ilLog
 	* @return	boolean
 	* @access	public
 	*/
-	function ilLog($a_log_path, $a_log_file, $a_tag = "", $a_enabled = true)
+	function ilLog($a_log_path, $a_log_file, $a_tag = "", $a_enabled = true, $a_log_level = NULL)
 	{
+		// init vars
+		$this->FATAL	 = 10;
+		$this->WARNING	 = 20;
+		$this->MESSAGE	 = 30;
+  
+        $this->default_log_level= $this->WARNING;
+        $this->current_log_level = $this->setLogLevel($a_log_level);
+
 		$this->path = ($a_log_path) ? $a_log_path : ILIAS_ABSOLUTE_PATH;
 		$this->filename = ($a_log_file) ? $a_log_file : "ilias.log";
 		$this->tag = ($a_tag == "") ? "unknown" : $a_tag;
 		$this->enabled = (bool) $a_enabled;
-		$this->setLogFormat(date("[y-m-d H:i] ")."[".$this->tag."] ");
+		$this->setLogFormat(date("[y-m-d H:i:s] ")."[".$this->tag."] ");
 
 	}
-	
+ 
+    /**
+    * set global log level
+    *
+    * @access    private
+    * @param     integer   log level
+    * @return    integer   log level
+    */
+    function setLogLevel($a_log_level)
+    {
+        switch (strtolower($a_log_level))
+        {
+            case "fatal":
+                return $this->FATAL;
+            case "warning":
+                return $this->WARNING;
+            case "message":
+                return $this->MESSAGE;
+            default:
+                return $this->default_log_level;
+        }
+    }
+
+    /**
+    * determine log level
+    *
+    * @access    private
+    * @param     integer   log level
+    * @return    integer   checked log level
+    */
+    function checkLogLevel($a_log_level)
+    {
+        if (empty($a_log_level))
+            return $this->default_log_level;
+
+        $level = (int) $a_log_level;
+        
+        if ($a_log_level != (int) $a_log_level)
+            return $this->default_log_level;
+        
+        return $level;
+    }
+
 	function setLogFormat($a_format)
 	{
 		$this->log_format = $a_format;
@@ -96,7 +167,7 @@ class ilLog
 	* special language checking routine
 	* 
 	* only add a log entry to the logfile
-	* if there isn't a logentry for the topic
+	* if there isn't a log entry for the topic
 	* 
 	* @param	string
 	* @access	public
@@ -105,7 +176,7 @@ class ilLog
 	{
 		//TODO: go through logfile and search for the topic
 		//only write the log if the error wasn't reported yet
-		$this->write("Language (".$a_lang_key."): topic -".$a_topic."- not present");
+		$this->write("Language (".$a_lang_key."): topic -".$a_topic."- not present",$this->MESSAGE);
 	}
 
 	/**
@@ -119,6 +190,14 @@ class ilLog
 		$this->write("WARNING: ".$a_message);
 	}
 	
+    /**
+    * this function is automatically called by class.ilErrorHandler in case of an error
+    * To log manually please use $this::write
+    * @access   public
+    * @param    integer  error level code from PEAR_Error
+    * @param    string   error message
+    * @see      this::write
+    */
 	function logError($a_code,$a_msg)
 	{
 		switch ($a_code)
@@ -147,24 +226,29 @@ class ilLog
 	/**
 	* logging 
 	* 
-	* this method logs anything you want. It appends a line to the given logfile:
-	* date: message
-	*
-	* @param string
-	* @access public
+	* this method logs anything you want. It appends a line to the given logfile.
+	* Datetime and client id is appended automatically
+	* You may set the log level in each call. Leave blank to use default log level
+    * specified in ilias.ini:
+    * [log]
+    * level = "<level>" possible values are fatal,warning,message
+    *
+    * @access   public
+    * @param    string   error message
+    * @param    integer  log level (optional)
 	*/
-	function write($msg)
+	function write($a_msg, $a_log_level = NULL)
 	{
-		if ($this->enabled)
+		if ($this->enabled and $this->current_log_level >= $this->checkLogLevel($a_log_level))
 		{
-			$fp = @fopen ($this->path."/".$this->filename, "a");
+            $fp = @fopen ($this->path."/".$this->filename, "a");
 
 			if ($fp == false)
 			{
 				die("Logfile: cannot open file. Please give Logfile Writepermissions.");
 			}
-	//var_dump($this->getLogFormat());exit;
-			if (fwrite($fp,$this->getLogFormat().$msg."\n") == -1)
+
+			if (fwrite($fp,$this->getLogFormat().$a_msg."\n") == -1)
 			{
 				die("Logfile: cannot write to file. Please give Logfile Writepermissions.");
 			}
