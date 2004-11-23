9215ff792328765f2d646032404455d25e9aaaec
Alexander Killing <killing@leifos.de>
2004-11-23 13:18:03 +0000

    fixed split to next page (php4 only)
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@5954 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- content/classes/Pages/class.ilPageObject.php
+++ content/classes/Pages/class.ilPageObject.php
@@ -1379,9 +1379,9 @@ class ilPageObject
 	function _moveContentAfterHierId(&$a_source_page, &$a_target_page, $a_hid)
 	{
 		$hier_ids = $a_source_page->getHierIds();
-		
+
 		$copy_ids = array();
-		
+
 		// iterate all hierarchical ids
 		foreach ($hier_ids as $hier_id)
 		{
@@ -1395,40 +1395,40 @@ class ilPageObject
 			}
 		}
 		asort($copy_ids);
-echo "-1";
+//echo "-1";
 		$parent_node =& $a_target_page->getContentNode("pg");
 		$target_dom =& $a_target_page->getDom();
 		$parent_childs =& $parent_node->child_nodes();
 		$first_child =& $parent_childs[0];
 		$cnt_parent_childs = count($parent_childs);
-echo "-2";		
+//echo "-2";
 		foreach($copy_ids as $copy_id)
 		{
-echo "-3";
+//echo "-3";
 			$source_node =& $a_source_page->getContentNode($copy_id);
-echo "-4";
+//echo "-4";
 			$new_node =& $source_node->clone_node(true);
 			$new_node->unlink_node($new_node);
-echo "-5";
+//echo "-5";
 			$source_node->unlink_node($source_node);
-			
+
 			if($cnt_parent_childs == 0)
 			{
-echo "-6";
+//echo "-6";
 				$new_node =& $parent_node->append_child($new_node);
 			}
 			else
 			{
-echo "-7";
-				$target_dom->importNode($new_node);
+//echo "-7";
+				//$target_dom->import_node($new_node);
 				$new_node =& $first_child->insert_before($new_node, $first_child);
 			}
-echo "-8";
+//echo "-8";
 			$parent_childs =& $parent_node->child_nodes();
-echo "-9";
+//echo "-9";
 			$cnt_parent_childs++;
 		}
-		
+
 		$a_target_page->update();
 		$a_source_page->update();
 
