70a5711f2066af000a2df87c4d275feb2733752f
hschottm <hschottm@svn.ilias.de>
2004-12-17 09:20:45 +0000

    Fixed special chars output in charts
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@6267 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- survey/displaychart.php
+++ survey/displaychart.php
@@ -2,5 +2,12 @@
 	require_once "classes/class.SurveyChart.php";
 	$arr1 = unserialize(base64_decode($_GET["arr"]));
 	$graphName = utf8_decode($_GET["grName"]);
-	$b1 = new SurveyChart($_GET["type"],400,250,$graphName,$_GET["x"],$_GET["y"],$arr1);
+	foreach ($arr1 as $key => $value)
+	{
+		foreach ($value as $key2 => $value2)
+		{
+			$arr1[$key][$key2] = utf8_decode($value2);
+		}
+	}
+	$b1 = new SurveyChart($_GET["type"],400,250,$graphName,utf8_decode($_GET["x"]),utf8_decode($_GET["y"]),$arr1);
 ?>
