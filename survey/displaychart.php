e0589f6db81ad1482de21ebfa83013dc888ddf12
hschottm <hschottm@svn.ilias.de>
2004-12-17 06:23:21 +0000

    Survey tool: Added chart visualization for survey evaluation Added german language strings
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@6256 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ survey/displaychart.php
@@ -0,0 +1,6 @@
+<?php
+	require_once "classes/class.SurveyChart.php";
+	$arr1 = unserialize(base64_decode($_GET["arr"]));
+	$graphName = $_GET["grName"];
+	$b1 = new SurveyChart($_GET["type"],400,250,$graphName,$_GET["x"],$_GET["y"],$arr1);
+?>
