b3b9c7337fdbd660b18de83de2eb3b25b38309d8
Stefan Meyer <smeyer.ilias@gmx.de>
2005-04-11 16:02:57 +0000

    added application classes for meta data Relation and subelements
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@7175 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ Services/MetaData/classes/class.ilMDDescription.php
@@ -0,0 +1,185 @@
+<?php
+/*
+	+-----------------------------------------------------------------------------+
+	| ILIAS open source                                                           |
+	+-----------------------------------------------------------------------------+
+	| Copyright (c) 1998-2001 ILIAS open source, University of Cologne            |
+	|                                                                             |
+	| This program is free software; you can redistribute it and/or               |
+	| modify it under the terms of the GNU General Public License                 |
+	| as published by the Free Software Foundation; either version 2              |
+	| of the License, or (at your option) any later version.                      |
+	|                                                                             |
+	| This program is distributed in the hope that it will be useful,             |
+	| but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+	| GNU General Public License for more details.                                |
+	|                                                                             |
+	| You should have received a copy of the GNU General Public License           |
+	| along with this program; if not, write to the Free Software                 |
+	| Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
+	+-----------------------------------------------------------------------------+
+*/
+
+
+/**
+* Meta Data class (element description)
+*
+* @package ilias-core
+* @version $Id$
+*/
+include_once 'class.ilMDBase.php';
+
+class ilMDDescription extends ilMDBase
+{
+	var $parent_obj = null;
+
+	function ilMDDescription(&$parent_obj,$a_id = null)
+	{
+		$this->parent_obj =& $parent_obj;
+
+		parent::ilMDBase($this->parent_obj->getRBACId(),
+						 $this->parent_obj->getObjId(),
+						 $this->parent_obj->getObjType(),
+						 'meta_description',
+						 $a_id);
+
+		$this->setParentType($this->parent_obj->getMetaType());
+		$this->setParentId($this->parent_obj->getMetaId());
+
+		if($a_id)
+		{
+			$this->read();
+		}
+	}
+
+	// SET/GET
+	function setDescription($a_description)
+	{
+		$this->description = $a_description;
+	}
+	function getDescription()
+	{
+		return $this->description;
+	}
+	function setDescriptionLanguage(&$lng_obj)
+	{
+		if(is_object($lng_obj))
+		{
+			$this->description_language = $lng_obj;
+		}
+	}
+	function &getDescriptionLanguage()
+	{
+		return is_object($this->description_language) ? $this->description_language : false;
+	}
+	function getDescriptionLanguageCode()
+	{
+		return is_object($this->description_language) ? $this->description_language->getLanguageCode() : false;
+	}
+
+	function save()
+	{
+		if($this->db->autoExecute('il_meta_description',
+								  $this->__getFields(),
+								  DB_AUTOQUERY_INSERT))
+		{
+			$this->setMetaId($this->db->getLastInsertId());
+
+			return $this->getMetaId();
+		}
+		return false;
+	}
+
+	function update()
+	{
+		if($this->getMetaId())
+		{
+			if($this->db->autoExecute('il_meta_description',
+									  $this->__getFields(),
+									  DB_AUTOQUERY_UPDATE,
+									  "meta_description_id = '".$this->getMetaId()."'"))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	function delete()
+	{
+		if($this->getMetaId())
+		{
+			$query = "DELETE FROM il_meta_description ".
+				"WHERE meta_description_id = '".$this->getMetaId()."'";
+			
+			$this->db->query($query);
+			
+			return true;
+		}
+		return false;
+	}
+			
+
+	function __getFields()
+	{
+		return array('rbac_id'	=> $this->getRBACId(),
+					 'obj_id'	=> $this->getObjId(),
+					 'obj_type'	=> ilUtil::prepareDBString($this->getObjType()),
+					 'parent_type' => $this->getParentType(),
+					 'parent_id' => $this->getParentId(),
+					 'description'	=> ilUtil::prepareDBString($this->getDescription()),
+					 'description_language' => ilUtil::prepareDBString($this->getDescriptionLanguageCode()));
+	}
+
+	function read()
+	{
+		include_once 'Services/MetaData/classes/class.ilMDLanguage.php';
+
+		if($this->getMetaId())
+		{
+			$query = "SELECT * FROM il_meta_description ".
+				"WHERE meta_description_id = '".$this->getMetaId()."'";
+
+			$res = $this->db->query($query);
+			while($row = $res->fetchRow(DB_FETCHMODE_OBJECT))
+			{
+				$this->setDescription($row->description);
+				$this->setDescriptionLanguage(new ilMDLanguage($row->description_language));
+			}
+		}
+		return true;
+	}
+				
+	/*
+	 * XML Export of all meta data
+	 * @param object (xml writer) see class.ilMD2XML.php
+	 * 
+	 */
+	function toXML(&$writer)
+	{
+		$writer->xmlElement('Description',array('Language' => $this->getDescriptionLanguageCode()),$this->getDescription());
+	}
+
+
+	// STATIC
+	function _getIds($a_rbac_id,$a_obj_id,$a_parent_id,$a_parent_type)
+	{
+		global $ilDB;
+
+		$query = "SELECT meta_description_id FROM il_meta_description ".
+			"WHERE rbac_id = '".$a_rbac_id."' ".
+			"AND obj_id = '".$a_obj_id."' ".
+			"AND parent_id = '".$a_parent_id."' ".
+			"AND parent_type = '".$a_parent_type."' ".
+			"ORDER BY meta_description_id";
+
+		$res = $ilDB->query($query);
+		while($row = $res->fetchRow(DB_FETCHMODE_OBJECT))
+		{
+			$ids[] = $row->meta_description_id;
+		}
+		return $ids ? $ids : array();
+	}
+}
+?>
---
+++ Services/MetaData/classes/class.ilMDRelation.php
@@ -0,0 +1,224 @@
+<?php
+/*
+	+-----------------------------------------------------------------------------+
+	| ILIAS open source                                                           |
+	+-----------------------------------------------------------------------------+
+	| Copyright (c) 1998-2001 ILIAS open source, University of Cologne            |
+	|                                                                             |
+	| This program is free software; you can redistribute it and/or               |
+	| modify it under the terms of the GNU General Public License                 |
+	| as published by the Free Software Foundation; either version 2              |
+	| of the License, or (at your option) any later version.                      |
+	|                                                                             |
+	| This program is distributed in the hope that it will be useful,             |
+	| but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+	| GNU General Public License for more details.                                |
+	|                                                                             |
+	| You should have received a copy of the GNU General Public License           |
+	| along with this program; if not, write to the Free Software                 |
+	| Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
+	+-----------------------------------------------------------------------------+
+*/
+
+
+/**
+* Meta Data class (element relation)
+*
+* @package ilias-core
+* @version $Id$
+*/
+include_once 'class.ilMDBase.php';
+
+class ilMDRelation extends ilMDBase
+{
+	var $parent_obj = null;
+
+	function ilMDRelation(&$parent_obj,$a_id = null)
+	{
+		$this->parent_obj =& $parent_obj;
+
+		parent::ilMDBase($this->parent_obj->getRBACId(),
+						 $this->parent_obj->getObjId(),
+						 $this->parent_obj->getObjType(),
+						 'meta_relation',
+						 $a_id);
+
+		if($a_id)
+		{
+			$this->read();
+		}
+	}
+
+	// METHODS OF CHILD OBJECTS (Taxon)
+	function &getIdentifier_Ids()
+	{
+		include_once 'Services/MetaData/classes/class.ilMDIdentifier_.php';
+
+		return ilMDIdentifier_::_getIds($this->getRBACId(),$this->getObjId(),$this->getMetaId());
+	}
+	function &getIdentifier_($a_identifier__id)
+	{
+		include_once 'Services/MetaData/classes/class.ilMDIdentifier_.php';
+
+		if(!$a_identifier__id)
+		{
+			return false;
+		}
+		return new ilMDIdentifier_($this,$a_identifier__id);
+	}
+	function &addIdentifier_()
+	{
+		include_once 'Services/MetaData/classes/class.ilMDIdentifier_.php';
+
+		return new ilMDIdentifier_($this);
+	}
+
+	function &getDescriptionIds()
+	{
+		include_once 'Services/MetaData/classes/class.ilMDDescription.php';
+
+		return ilMdDescription::_getIds($this->getRBACId(),$this->getObjId(),$this->getMetaId(),$this->getMetaType());
+	}
+	function &getDescription($a_description_id)
+	{
+		include_once 'Services/MetaData/classes/class.ilMDDescription.php';
+		
+		if(!$a_description_id)
+		{
+			return false;
+		}
+		return new ilMDDescription($this,$a_description_id);
+
+	}
+	function &addDescription()
+	{
+		include_once 'Services/MetaData/classes/class.ilMDDescription.php';
+		
+		return new ilMDDescription($this);
+	}
+	// SET/GET
+	function setKind($a_kind)
+	{
+		$this->kind = $a_kind;
+	}
+	function getKind()
+	{
+		return $this->kind;
+	}
+
+
+	function save()
+	{
+		if($this->db->autoExecute('il_meta_relation',
+								  $this->__getFields(),
+								  DB_AUTOQUERY_INSERT))
+		{
+			$this->setMetaId($this->db->getLastInsertId());
+
+			return $this->getMetaId();
+		}
+		return false;
+	}
+
+	function update()
+	{
+		if($this->getMetaId())
+		{
+			if($this->db->autoExecute('il_meta_relation',
+									  $this->__getFields(),
+									  DB_AUTOQUERY_UPDATE,
+									  "meta_relation_id = '".$this->getMetaId()."'"))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	function delete()
+	{
+		if($this->getMetaId())
+		{
+			$query = "DELETE FROM il_meta_relation ".
+				"WHERE meta_relation_id = '".$this->getMetaId()."'";
+			
+			$this->db->query($query);
+			
+			return true;
+		}
+		return false;
+	}
+			
+
+	function __getFields()
+	{
+		return array('rbac_id'	=> $this->getRBACId(),
+					 'obj_id'	=> $this->getObjId(),
+					 'obj_type'	=> ilUtil::prepareDBString($this->getObjType()),
+					 'kind'		=> ilUtil::prepareDBString($this->getKind()));
+	}
+
+	function read()
+	{
+		if($this->getMetaId())
+		{
+			$query = "SELECT * FROM il_meta_relation ".
+				"WHERE meta_relation_id = '".$this->getMetaId()."'";
+
+			$res = $this->db->query($query);
+			while($row = $res->fetchRow(DB_FETCHMODE_OBJECT))
+			{
+				$this->setKind($row->kind);
+			}
+		}
+		return true;
+	}
+
+	/*
+	 * XML Export of all meta data
+	 * @param object (xml writer) see class.ilMD2XML.php
+	 * 
+	 */
+	function toXML(&$writer)
+	{
+		$writer->xmlStartTag('Relation',array('Kind' => $this->getKind()));
+		$writer->xmlStartTag('Resource');
+
+		// Identifier_
+		foreach($this->getIdentifier_Ids() as $id)
+		{
+			$ide =& $this->getIdentifier_($id);
+			$ide->toXML($writer);
+		}
+		// Description
+		foreach($this->getDescriptionIds() as $id)
+		{
+			$des =& $this->getDescription($id);
+			$des->toXML($writer);
+		}
+		$writer->xmlEndTag('Resource');
+		$writer->xmlEndTag('Relation');
+	}
+
+				
+
+	// STATIC
+	function _getIds($a_rbac_id,$a_obj_id)
+	{
+		global $ilDB;
+
+		$query = "SELECT meta_relation_id FROM il_meta_relation ".
+			"WHERE rbac_id = '".$a_rbac_id."' ".
+			"AND obj_id = '".$a_obj_id."' ".
+			"ORDER BY meta_relation_id";
+
+		$res = $ilDB->query($query);
+		while($row = $res->fetchRow(DB_FETCHMODE_OBJECT))
+		{
+			$ids[] = $row->meta_relation_id;
+		}
+		return $ids ? $ids : array();
+	}
+}
+?>
