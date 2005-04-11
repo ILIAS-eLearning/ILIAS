52a21ec891a2096174675671a0c1d520bcd2fb2e
rkuester <rkuester@svn.ilias.de>
2005-04-11 12:32:57 +0000

    - allow remove pre tag from negative tags
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@7167 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- classes/class.ilUtil.php
+++ classes/class.ilUtil.php
@@ -1731,7 +1731,7 @@ class ilUtil
 			"colgroup,dd,del,dfn,dir,div,dl,dt,em,fieldset,font,form,frame,".
 			"frameset,h1,h2,h3,h4,h5,h6,head,hr,html,i,iframe,img,input,ins,isindex,kbd,".
 			"label,legend,li,link,map,menu,meta,noframes,noscript,object,ol,".
-			"optgroup,option,p,param,pre,q,s,samp,script,select,small,span,".
+			"optgroup,option,p,param,q,s,samp,script,select,small,span,".
 			"strike,strong,style,sub,sup,table,tbody,td,textarea,tfoot,th,thead,".
 			"title,tr,tt,u,ul,var";
 		$a_allow = strtolower ($a_allow);
