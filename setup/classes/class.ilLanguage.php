e2b238aae1e76255f2c4a002edb09496b1e3b218
bblackmoor <bblackmoor>
2005-04-12 20:33:03 +0000

    adding local language feature to admin menu; replacing indents with TAB characters
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@7220 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- setup/classes/class.ilLanguage.php
+++ setup/classes/class.ilLanguage.php
@@ -23,81 +23,81 @@
 
 
 /**
-* language handling for setup
-*
-* this class offers the language handling for an application.
-* the constructor is called with a small language abbreviation
-* e.g. $lng = new Language("en");
-* the constructor reads the single-languagefile en.lang and puts this into an array.
-* with 
-* e.g. $lng->txt("user_updated");
-* you can translate a lang-topic into the actual language
-*
-* @author Peter Gabriel <pgabriel@databay.de>
-* @version $Id$
-* 
-* @package application
-* 
-* @todo The DATE field is not set correctly on changes of a language (update, install, your stable).
-*  The format functions do not belong in class.Language. Those are also applicable elsewhere.
-*  Therefore, they would be better placed in class.Format
-*/
+ * language handling for setup
+ *
+ * this class offers the language handling for an application.
+ * the constructor is called with a small language abbreviation
+ * e.g. $lng = new Language("en");
+ * the constructor reads the single-languagefile en.lang and puts this into an array.
+ * with 
+ * e.g. $lng->txt("user_updated");
+ * you can translate a lang-topic into the actual language
+ *
+ * @author Peter Gabriel <pgabriel@databay.de>
+ * @version $Id$
+ * 
+ * @package application
+ * 
+ * @todo The DATE field is not set correctly on changes of a language (update, install, your stable).
+ *  The format functions do not belong in class.Language. Those are also applicable elsewhere.
+ *  Therefore, they would be better placed in class.Format
+ */
 class ilLanguage
 {
-   /**
-	* text elements
-	* @var array
-	* @access private
-	*/
+	/**
+	 * text elements
+	 * @var array
+	 * @access private
+	 */
 	var $text = array();
 	
-   /**
-	* indicator for the system language
-	* this language must not be deleted
-	* @var      string
-	* @access   private
-	*/
+	/**
+	 * indicator for the system language
+	 * this language must not be deleted
+	 * @var      string
+	 * @access   private
+	 */
 	var $lang_default = "en";
 
-   /**
-	* path to language files
-	* relative path is taken from ini file
-	* and added to absolute path of ilias
-	* @var      string
-	* @access   private
-	*/
+	/**
+	 * path to language files
+	 * relative path is taken from ini file
+	 * and added to absolute path of ilias
+	 * @var      string
+	 * @access   private
+	 */
 	var $lang_path;
 
-   /**
-	* language key in use by current user
-	* @var      string  languagecode (two characters), e.g. "de", "en", "in"
-	* @access   private
-	*/
+	/**
+	 * language key in use by current user
+	 * @var      string  languagecode (two characters), e.g. "de", "en", "in"
+	 * @access   private
+	 */
 	var $lang_key;
 
-   /**
-	* separator value between module, identifier, and value 
-	* @var      string
-	* @access   private
-	*/
+	/**
+	 * separator value between module, identifier, and value 
+	 * @var      string
+	 * @access   private
+	 */
 	var $separator = "#:#";
 	
-   /**
-	* separator value between the content and the comment of the lang entry
-	* @var      string
-	* @access   private
-	*/
+	/**
+	 * separator value between the content and the comment of the lang entry
+	 * @var      string
+	 * @access   private
+	 */
 	var $comment_separator = "###";
 
-   /**
-	* Constructor
-	* read the single-language file and put this in an array text.
-	* the text array is two-dimensional. First dimension is the language.
-	* Second dimension is the languagetopic. Content is the translation.
-	* @access   public
-	* @param    string      languagecode (two characters), e.g. "de", "en", "in"
-	* @return   boolean     false if reading failed
-	*/
+	/**
+	 * Constructor
+	 * read the single-language file and put this in an array text.
+	 * the text array is two-dimensional. First dimension is the language.
+	 * Second dimension is the languagetopic. Content is the translation.
+	 * @access   public
+	 * @param    string      languagecode (two characters), e.g. "de", "en", "in"
+	 * @return   boolean     false if reading failed
+	 */
 	function ilLanguage($a_lang_key)
 	{
 		$this->lang_key = ($a_lang_key) ? $a_lang_key : $this->lang_default;
@@ -125,14 +125,14 @@ class ilLanguage
 		return false;
 	}
 	
-   /**
-	* gets the text for a given topic
-	*
-	* if the topic is not in the list, the topic itself with "-" will be returned
-	* @access   public 
-	* @param    string  topic
-	* @return   string  text clear-text
-	*/
+	/**
+	 * gets the text for a given topic
+	 *
+	 * if the topic is not in the list, the topic itself with "-" will be returned
+	 * @access   public 
+	 * @param    string  topic
+	 * @return   string  text clear-text
+	 */
 	function txt($a_topic)
 	{
 		global $log;
@@ -164,13 +164,13 @@ class ilLanguage
 		}
 	}
 
-   /**
-	* get all setup languages in the system
-	*
-	* the functions looks for setup*.lang-files in the languagedirectory
-	* @access   public
-	* @return   array   langs
-	*/
+	/**
+	 * get all setup languages in the system
+	 *
+	 * the functions looks for setup*.lang-files in the languagedirectory
+	 * @access   public
+	 * @return   array   langs
+	 */
 	function getLanguages()
 	{
 		$d = dir($this->lang_path);
@@ -192,11 +192,12 @@ class ilLanguage
 		return $languages;
 	}
 
-   /**
-	* install languages
-	* @param    array   array with lang_keys of languages to install
-	* @return   boolean true on success
-	*/
+	/**
+	 * install languages
+	 * 
+	 * @param    array   array with lang_keys of languages to install
+	 * @return   boolean true on success
+	 */
 	function installLanguages($a_lang_keys, $a_local_keys)
 	{
 		if (empty($a_lang_keys))
@@ -236,11 +237,21 @@ class ilLanguage
 				// register language first time install
 				if (!array_key_exists($lang_key,$db_langs))
 				{
-					$q = "INSERT INTO object_data ".
-						 "(type,title,description,owner,create_date,last_update) ".
-						 "VALUES ".
-						 "('lng','".$lang_key."','installed','-1',now(),now())";
-					$this->db->query($q);
+					if (in_array($lang_key, $a_local_keys))
+					{
+						$query = "INSERT INTO object_data ".
+								"(type,title,description,owner,create_date,last_update) ".
+								"VALUES ".
+								"('lng','".$lang_key."','installed_local','-1',now(),now())";
+					}
+					else
+					{
+						$query = "INSERT INTO object_data ".
+								"(type,title,description,owner,create_date,last_update) ".
+								"VALUES ".
+								"('lng','".$lang_key."','installed','-1',now(),now())";
+					}
+					$this->db->query($query);
 				}
 			}
 			else
@@ -255,24 +266,34 @@ class ilLanguage
 			{
 				if (in_array($key,$a_lang_keys))
 				{
-					if ($val["status"] != "installed")
+					if (in_array($key, $a_local_keys))
 					{
-						$q = "UPDATE object_data SET ".
-							 "description = 'installed',last_update = now() ".
-							 "WHERE obj_id='".$val["obj_id"]."' ".
-							 "AND type='lng'";
-						$this->db->query($q);
+						$query = "UPDATE object_data SET " .
+								"description = 'installed_local', " .
+								"last_update = now() " .
+								"WHERE obj_id='".$val["obj_id"]."' " .
+								"AND type='lng'";
 					}
+					else
+					{
+						$query = "UPDATE object_data SET " .
+								"description = 'installed', " .
+								"last_update = now() " .
+								"WHERE obj_id='".$val["obj_id"]."' " .
+								"AND type='lng'";
+					}
+					$this->db->query($query);
 				}
 				else
 				{
-					if ($val["status"] == "installed")
+					if (substr($val["status"], 0, 9) == "installed")
 					{
-						$q = "UPDATE object_data SET ".
-							 "description = 'not_installed',last_update = now() ".
-							 "WHERE obj_id='".$val["obj_id"]."' ".
-							 "AND type='lng'";
-						$this->db->query($q);
+						$query = "UPDATE object_data SET " .
+								"description = 'not_installed', " .
+								"last_update = now() " .
+								"WHERE obj_id='" . $val["obj_id"] . "' " .
+								"AND type='lng'";
+						$this->db->query($query);
 					}
 				}
 			}
@@ -281,18 +302,19 @@ class ilLanguage
 		return ($err_lang) ? $err_lang : true;
 	}
 
-   /**
-	* get already installed languages (in db)
-	* @return   array   array with inforamtion about each installed language
-	*/
+	/**
+	 * get already installed languages (in db)
+	 * 
+	 * @return   array   array with inforamtion about each installed language
+	 */
 	function getInstalledLanguages()
 	{
 		$arr = array();
 
-		$q = "SELECT * FROM object_data ".
-			 "WHERE type = 'lng' ".
-			 "AND description = 'installed'";
-		$r = $this->db->query($q);
+		$query = "SELECT * FROM object_data ".
+				"WHERE type = 'lng' ".
+				"AND description LIKE 'installed%'";
+		$r = $this->db->query($query);
 
 		while ($row = $r->fetchRow(DB_FETCHMODE_OBJECT))
 		{
@@ -302,17 +324,39 @@ class ilLanguage
 		return $arr;
 	}
 	
-   /**
-	* get already registered languages (in db)
-	* @return   array   array with information about languages that has been registered in db
-	*/
+	/**
+	 * get already installed local languages (in db)
+	 * 
+	 * @return   array   array with inforamtion about each installed language
+	 */
+	function getInstalledLocalLanguages()
+	{
+		$arr = array();
+
+		$query = "SELECT * FROM object_data ".
+				"WHERE type = 'lng' ".
+				"AND description = 'installed_local'";
+		$r = $this->db->query($query);
+
+		while ($row = $r->fetchRow(DB_FETCHMODE_OBJECT))
+		{
+			$arr[] = $row->title;
+		}
+
+		return $arr;
+	}
+
+	/**
+	 * get already registered languages (in db)
+	 * @return   array   array with information about languages that has been registered in db
+	 */
 	function getAvailableLanguages()
 	{
 		$arr = array();
 
-		$q = "SELECT * FROM object_data ".
-			 "WHERE type = 'lng'";
-		$r = $this->db->query($q);
+		$query = "SELECT * FROM object_data ".
+				"WHERE type = 'lng'";
+		$r = $this->db->query($query);
 
 		while ($row = $r->fetchRow(DB_FETCHMODE_OBJECT))
 		{
@@ -323,18 +367,18 @@ class ilLanguage
 		return $arr;
 	}
 
-   /**
-	* validate the logical structure of a lang-file
-	*
-	* This function checks if a lang-file of a given lang_key exists,
-	* the file has a header and each lang-entry consist of exact three elements
-	* (module,identifier,value)
-	*
-	* @param    string      $a_lang_key     international language key (2 digits)
-	* @param    string      $scope          empty (global) or "local"
-	* @return   string      $info_text      message about results of check OR "1" if all checks successfully passed
-	*/
-	function checkLanguage($a_lang_key, $scope = "")
+	/**
+	 * validate the logical structure of a lang-file
+	 *
+	 * This function checks if a lang-file of a given lang_key exists, 
+	 * the file has a header, and each lang-entry consists of exactly 
+	 * three elements (module, identifier, value).
+	 *
+	 * @param    string      $a_lang_key     international language key (2 digits)
+	 * @param    string      $scope          empty (global) or "local"
+	 * @return   string      $info_text      message about results of check OR "1" if all checks successfully passed
+	 */
+	function checkLanguage($a_lang_key, $scope = '')
 	{
 		if (!empty($scope))
 		{
@@ -344,15 +388,15 @@ class ilLanguage
 			}
 			else
 			{
-				$scope = '.' . $scope;
+				$scopeExtension = '.' . $scope;
 			}
 		}
 
 		$tmpPath = getcwd();
-		chdir ($this->lang_path);
+		chdir($this->lang_path);
 
 		// compute lang-file name format
-		$lang_file = "ilias_" . $a_lang_key . ".lang" . $scope;
+		$lang_file = "ilias_" . $a_lang_key . ".lang" . $scopeExtension;
 
 		// file check
 		if (!is_file($lang_file))
@@ -371,7 +415,7 @@ class ilLanguage
 		// check (counting) elements of each lang-entry
 		foreach ($content as $key => $val)
 		{
-			$separated = explode ($this->separator,trim($val));
+			$separated = explode($this->separator, trim($val));
 			$num = count($separated);
 
 			if ($num != 3)
@@ -387,16 +431,16 @@ class ilLanguage
 		return true;
 	}
 
-   /**
-	* Remove *.lang header information from '$content'.
-	*
-	* This function seeks for a special keyword where the language information starts.
-	* If found it returns the plain language information; otherwise returns false.
-	*
-	* @param    string      $content    expect an ILIAS lang-file
-	* @return   string      $content    content without header info OR false if no valid header was found
-	* @access   private
-	*/
+	/**
+	 * Remove *.lang header information from '$content'.
+	 *
+	 * This function seeks for a special keyword where the language information starts.
+	 * If found it returns the plain language information; otherwise returns false.
+	 *
+	 * @param    string      $content    expect an ILIAS lang-file
+	 * @return   string      $content    content without header info OR false if no valid header was found
+	 * @access   private
+	 */
 	function cut_header($content)
 	{
 		foreach ($content as $key => $val)
@@ -410,24 +454,24 @@ class ilLanguage
 		return false;
 	}
 
-   /**
-	* remove all languagee from database
-	*/
+	/**
+	 * remove all languagee from database
+	 */
 	function flushLanguages()
 	{
-		$q = "DELETE FROM lng_data";
-		$this->db->query($q);
+		$query = "DELETE FROM lng_data";
+		$this->db->query($query);
 	}
 
 	//TODO: remove redundant checks here!
-   /**
-	* insert language data from file in database
-	*
-	* @param    string  $lang_key   international language key (2 digits)
-	* @param    string  $scope      empty (global) or "local"
-	* @return   void
-	*/
-	function insertLanguage($lang_key, $scope = "")
+	/**
+	 * insert language data from file in database
+	 *
+	 * @param    string  $lang_key   international language key (2 digits)
+	 * @param    string  $scope      empty (global) or "local"
+	 * @return   void
+	 */
+	function insertLanguage($lang_key, $scope = '')
 	{
 		if (!empty($scope))
 		{
@@ -437,14 +481,14 @@ class ilLanguage
 			}
 			else
 			{
-				$scope = '.' . $scope;
+				$scopeExtension = '.' . $scope;
 			}
 		}
 
 		$tmpPath = getcwd();
 		chdir($this->lang_path);
 
-		$lang_file = "ilias_" . $lang_key . ".lang" . $scope;
+		$lang_file = "ilias_" . $lang_key . ".lang" . $scopeExtension;
 
 		if ($lang_file)
 		{
@@ -466,23 +510,23 @@ class ilLanguage
 
 					if (empty($scope))
 					{
-						$q = "INSERT INTO lng_data ".
-								 "(module,identifier,lang_key,value) ".
-								 "VALUES ".
-								 "('".$separated[0]."','".$separated[1]."','".$lang_key."','".addslashes($separated[2])."')";
+						$query = "INSERT INTO lng_data ".
+								"(module,identifier,lang_key,value) ".
+								"VALUES ".
+								"('" . $separated[0] . "','" . $separated[1] . "','" . $lang_key . "','" . addslashes($separated[2]) . "')";
 					}
-					else
+					else if ($scope == 'local')
 					{
-						$q = "UPDATE lng_data SET ".
-								 "module = '" . $separated[0] . "', " .
-								 "identifier = '" . $separated[1] . "', " . 
-								 "lang_key = '" . $lang_key . "', " .
-								 "value = '" . addslashes($separated[2]) . "' " .
-								 "WHERE module = '" . $separated[0] . "' " .
-								 "AND identifier = '" . $separated[1] . "' " .
-								 "AND lang_key = '" . $lang_key . "'";
+						$query = "UPDATE lng_data SET ".
+								"module = '" . $separated[0] . "', " .
+								"identifier = '" . $separated[1] . "', " . 
+								"lang_key = '" . $lang_key . "', " .
+								"value = '" . addslashes($separated[2]) . "' " .
+								"WHERE module = '" . $separated[0] . "' " .
+								"AND identifier = '" . $separated[1] . "' " .
+								"AND lang_key = '" . $lang_key . "'";
 					}
-					$this->db->query($q);
+					$this->db->query($query);
 				}
 			}
 		}
@@ -490,11 +534,11 @@ class ilLanguage
 		chdir($tmpPath);
 	}
 	
-   /**
-	* Searches for the existence of *.lang.local files.
-	*
-	* return    $local_langs    array of language keys
-	*/
+	/**
+	 * Searches for the existence of *.lang.local files.
+	 *
+	 * return    $local_langs    array of language keys
+	 */
 	function getLocalLanguages()
 	{
 		$local_langs = array();
@@ -542,11 +586,11 @@ class ilLanguage
 		return $languages1;
 	}
 	
-   /**
-	* set db handler object
-	* @string   object      db handler
-	* @return   boolean     true on success
-	*/
+	/**
+	 * set db handler object
+	 * @string   object      db handler
+	 * @return   boolean     true on success
+	 */
 	function setDbHandler($a_db_handler)
 	{
 		if (empty($a_db_handler) or !is_object($a_db_handler))
