181d88a53d4e86419f406bd5ed01ab309b47da7d
Alexander Killing <killing@leifos.de>
2005-06-09 16:24:22 +0000

    introduced ilias.php and modules.xml
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@7816 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- setup/classes/class.ilCtrlStructureReader.php
+++ setup/classes/class.ilCtrlStructureReader.php
@@ -60,11 +60,12 @@ class ilCtrlStructureReader
 		// read module information
 		// not clear wheter this is a good place for module reading info
 		// or not
+/*
 		require_once("classes/class.ilModuleReader.php");
 		$module_reader =& new ilModuleReader();
 		$module_reader->clearTables();
 		$module_reader->startParsing();
-
+*/
 	}
 
 	/**
