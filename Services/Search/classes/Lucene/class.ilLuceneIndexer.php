c3182cce12290af43da463fbb13d195069039de2
Stefan Meyer <smeyer.ilias@gmx.de>
2005-07-01 11:58:34 +0000

    added lucene search classes
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@7944 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ Services/Search/classes/Lucene/class.ilLuceneIndexer.php
@@ -0,0 +1,138 @@
+<?php
+/*
+	+-----------------------------------------------------------------------------+
+	| ILIAS open source                                                           |
+	+-----------------------------------------------------------------------------+
+	| Copyright (c) 1998-2001 ILIAS open source, University of Cologne            |
+	|                                                                             |
+	| This program is free software; you can redistribute it and/or               |
+	| modify it under the terms of the GNU General Public License                 |
+	| as published by the Free Software Foundation; either version 2              |
+	| of the License, or (at your option) any later version.                      |
+	|                                                                             |
+	| This program is distributed in the hope that it will be useful,             |
+	| but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+	| GNU General Public License for more details.                                |
+	|                                                                             |
+	| You should have received a copy of the GNU General Public License           |
+	| along with this program; if not, write to the Free Software                 |
+	| Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
+	+-----------------------------------------------------------------------------+
+*/
+
+
+/**
+* Class for indexing hmtl ,pdf, txt files and htlm Learning modules.
+* This indexer is called by cron.php
+*
+* @author Stefan Meyer <smeyer@databay.de>
+* @version $Id$
+*
+* @package ilias
+*/
+
+class ilLuceneIndexer
+{
+	function ilLuceneIndexer()
+	{
+		global $ilLog,$ilDB;
+
+		$this->log =& $ilLog;
+		$this->db =& $ilDB;
+	}
+
+	function index()
+	{
+		// Todo check in settings which objects should be indexed
+		$this->__flushIndex();
+		$this->__indexFiles();
+		$this->__indexHTLMs();
+
+		return true;
+	}
+
+	// PRIVATE
+	function __indexFiles()
+	{
+		global $tree;
+
+		$query = "SELECT * FROM file_data ".
+			"WHERE file_type IN ('text/plain','application/pdf','text/html')";
+
+		$res = $this->db->query($query);
+
+		$counter = 0;
+		$files = array();
+		while($row = $res->fetchRow(DB_FETCHMODE_OBJECT))
+		{
+			++$counter;
+			$bname = ilUtil::getDataDir();
+			$bname .= ("/files/file_".$row->file_id);
+			if($row->version)
+			{
+				$bname .= ("/".sprintf("%03d", $row->version));
+			}
+			$files[$row->file_id] = $bname."/".$row->file_name;
+		}
+		$this->log->write('Lucene indexer: Found '.$counter.' files for indexing');
+
+		if(count($files))
+		{
+			// Send files to lucene rpc server
+			include_once './Services/Search/classes/Lucene/class.ilLuceneRPCAdapter.php';
+
+			$rpc_adapter =& new ilLuceneRPCAdapter();
+			$rpc_adapter->setMode('file');
+			$rpc_adapter->setFiles($files);
+			if($rpc_adapter->send())
+			{
+				$this->log->write('Lucene indexer: files sent');
+			}
+			return true;
+		}
+	}
+	function __indexHTLMs()
+	{
+		global $ilias;
+
+		$query = "SELECT * FROM object_data WHERE type = 'htlm'";
+		$res = $this->db->query($query);
+		$counter = 0;
+		while($row = $res->fetchRow(DB_FETCHMODE_OBJECT))
+		{
+			++$counter;
+			$lms[$row->obj_id] = ILIAS_ABSOLUTE_PATH.'/'.ILIAS_WEB_DIR.'/'.CLIENT_ID.'/lm_data/lm_'.$row->obj_id;
+
+		}
+		$this->log->write('Lucene indexer: Found '.$counter.' html learning modules for indexing');
+
+		if(count($lms))
+		{
+			// Send files to lucene rpc server
+			include_once './Services/Search/classes/Lucene/class.ilLuceneRPCAdapter.php';
+
+			$rpc_adapter =& new ilLuceneRPCAdapter();
+			$rpc_adapter->setMode('htlm');
+			$rpc_adapter->setHTLMs($lms);
+			if($rpc_adapter->send())
+			{
+				$this->log->write('Lucene indexer: files sent');
+			}
+			return true;
+		}
+	}
+	function __flushIndex()
+	{
+		include_once './Services/Search/classes/Lucene/class.ilLuceneRPCAdapter.php';
+
+		$rpc_adapter =& new ilLuceneRPCAdapter();
+		$rpc_adapter->setMode('flush');
+		if($rpc_adapter->send())
+		{
+			$this->log->write('Lucene indexer: deleted index');
+		}
+		return true;
+	}		
+}
+?>
---
+++ Services/Search/classes/Lucene/class.ilLuceneRPCAdapter.php
@@ -0,0 +1,152 @@
+<?php
+/*
+	+-----------------------------------------------------------------------------+
+	| ILIAS open source                                                           |
+	+-----------------------------------------------------------------------------+
+	| Copyright (c) 1998-2001 ILIAS open source, University of Cologne            |
+	|                                                                             |
+	| This program is free software; you can redistribute it and/or               |
+	| modify it under the terms of the GNU General Public License                 |
+	| as published by the Free Software Foundation; either version 2              |
+	| of the License, or (at your option) any later version.                      |
+	|                                                                             |
+	| This program is distributed in the hope that it will be useful,             |
+	| but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+	| GNU General Public License for more details.                                |
+	|                                                                             |
+	| You should have received a copy of the GNU General Public License           |
+	| along with this program; if not, write to the Free Software                 |
+	| Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
+	+-----------------------------------------------------------------------------+
+*/
+
+
+/**
+* Adapter class for communication between ilias and ilRPCServer
+*
+* @author Stefan Meyer <smeyer@databay.de>
+* @version $Id$
+*
+* @package ilias
+*/
+include_once 'Services/WebServices/RPC/classes/class.ilRPCServerAdapter.php';
+
+class ilLuceneRPCAdapter extends ilRPCServerAdapter
+{
+	var $mode = '';
+	var $files = array();
+	var $query_str = '';
+
+	function ilLuceneRPCAdapter()
+	{
+		parent::ilRPCServerAdapter();
+	}
+
+	function setMode($a_mode)
+	{
+		$this->mode = $a_mode;
+	}
+	function getMode()
+	{
+		return $this->mode;
+	}
+	function setFiles(&$files)
+	{
+		$this->files =& $files;
+	}
+	function &getFiles()
+	{
+		return $this->files ? $this->files : array();
+	}
+	function setHTLMs(&$htlms)
+	{
+		$this->htlms = $htlms;
+	}
+	function &getHTLMs()
+	{
+		return $this->htlms;
+	}
+
+	function setQueryString($a_str)
+	{
+		$this->query_str = $a_str;
+	}
+	function getQueryString()
+	{
+		return $this->query_str;
+	}
+
+
+	function send()
+	{
+		$this->__initClient();
+		switch($this->getMode())
+		{
+			case 'file':
+				$this->__prepareIndexFileParams();
+				break;
+
+			case 'query':
+				$this->__prepareQueryParams();
+				break;
+
+			case 'htlm':
+				$this->__prepareIndexHTLMParams();
+				break;
+
+			case 'flush':
+				$this->__prepareFlushIndex();
+				break;
+
+			default:
+				$this->log->write('ilLuceneRPCHandler(): No valid mode given');
+				return false;
+
+		}
+		return parent::send();
+	}
+	// PRIVATE
+	function __prepareQueryParams()
+	{
+		$this->__initMessage('Searcher.ilSearch',array(new XML_RPC_Value(CLIENT_ID,"string"),
+													   new XML_RPC_Value($this->getQueryString(),"string")));
+
+		return true;
+	}
+
+	function __prepareIndexFileParams()
+	{
+		foreach($this->getFiles() as $obj_id => $fname)
+		{
+			$struct[$obj_id] = new XML_RPC_Value($fname,"string");
+		}
+		$params = array(new XML_RPC_Value(CLIENT_ID,"string"),
+						new XML_RPC_Value($struct,"struct"));
+
+		$this->__initMessage('Indexer.ilFileIndexer',$params);
+
+		return true;
+	}
+
+	function __prepareIndexHTLMParams()
+	{
+		foreach($this->getHTLMs() as $obj_id => $fname)
+		{
+			$struct[$obj_id] = new XML_RPC_Value($fname,"string");
+		}
+
+		$this->__initMessage('Indexer.ilHTLMIndexer',array(new XML_RPC_Value(CLIENT_ID,"string"),
+														   new XML_RPC_Value($struct,"struct")));
+
+		return true;
+	}
+	function __prepareFlushIndex()
+	{
+
+		$this->__initMessage('Indexer.ilClearIndex',array(new XML_RPC_Value(CLIENT_ID,"string")));
+
+		return true;
+	}
+}
+?>
