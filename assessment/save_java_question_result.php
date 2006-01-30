0307f02ff02efe501273d6c195f22216a0d27cd2
hschottm <hschottm@svn.ilias.de>
2006-01-30 09:03:33 +0000

    merged fix 104 for Release_3_6_0
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@9702 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- assessment/save_java_question_result.php
+++ assessment/save_java_question_result.php
@@ -1,5 +1,30 @@
 <?php
 chdir("..");
+include_once "./webservice/soap/include/inc.soap_functions.php";
+$results = array();
+foreach ($_POST as $key => $value)
+{
+	if (preg_match("/value_(\d+)_1/", $key, $matches))
+	{
+		array_push($results, $_POST["value_" . $matches[1] . "_1"]);
+		array_push($results, $_POST["value_" . $matches[1] . "_2"]);
+		array_push($results, $_POST["points_" . $matches[1]]);
+	}
+}
+$res = saveQuestionResult($_POST["session_id"]."::".$_POST["client"],$_POST["user_id"], $_POST["test_id"], $_POST["question_id"], $_POST["pass"], $results);
+if ($res === true)
+{
+	global $lng;
+	$lng->loadLanguageModule("assessment");
+	echo $lng->txt("javaapplet_successful_saved");
+}
+else
+{
+	echo $lng->txt("javaapplet_unsuccessful_saved");
+	//echo print_r($res, true);
+}
+/*
+chdir("..");
 include_once "./include/inc.header.php";
 if ($_POST["test_id"] > 0)
 {
@@ -35,5 +60,5 @@ else
 {
 	echo $lng->txt("javaapplet_unsuccessful_saved");
 }
-
+*/
 ?>
