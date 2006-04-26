12bebe0631e3d3a6d652a3707f50982dbbe3fff8
agonzalez <agonzalez@svn.ilias.de>
2006-04-26 15:39:58 +0000

    - improved exercise user interface. - added "remaining working time" feature for exercise.
    (please adapt delos skin colors to your needs!).
    
    
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@10692 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- classes/class.ilUtil.php
+++ classes/class.ilUtil.php
@@ -2831,5 +2831,90 @@ class ilUtil
 		return $a_text;
 	}
 
+
+  /**
+      * Return an array of date segments.
+      *
+      * @param        int $seconds Number of seconds to be parsed
+      * @return       mixed An array containing named segments
+      */
+     function int2array ($seconds, $periods = null)
+     {        
+         // Define time periods
+         if (!is_array($periods)) {
+             $periods = array (
+                     'years'     => 31556926,
+                     'months'    => 2629743,
+                     'weeks'     => 604800,
+                     'days'      => 86400,
+                     'hours'     => 3600,
+                     'minutes'   => 60,
+                     'seconds'   => 1
+                     );
+         }
+  
+         // Loop
+         $seconds = (float) $seconds;
+         foreach ($periods as $period => $value) {
+             $count = floor($seconds / $value);
+  
+             if ($count == 0) {
+                 continue;
+             }
+  
+             $values[$period] = $count;
+             $seconds = $seconds % $value;
+         }
+  
+         // Return
+         if (empty($values)) {
+             $values = null;
+         }
+  
+         return $values;
+     }
+
+     /**
+      * Return a string of time periods.
+      *
+      * @package      Duration
+      * @param        mixed $duration An array of named segments
+      * @return       string
+      */
+     function timearray2string ($duration)
+     {
+
+       global $lng;
+
+       if (!is_array($duration)) {
+	 return false;
+       }
+       
+       foreach ($duration as $key => $value) {
+	 $segment_name = substr($key, 0, -1);
+	 $segment_name = $lng->txt($segment_name);
+	 $segment = $value . ' ' . $segment_name; 
+	 
+	 // Plural
+	 if ($value != 1) {
+	   $segment .= 's';
+	 }
+	 
+	 $array[] = $segment;
+       }
+       $len = count($array);
+       if ($len>=3) {
+	 $array=array_slice($array,0,(3-$len));
+       }
+       
+       $str = implode(', ', $array);
+       
+       return $str;
+     }
+     
+     
+     
 } // END class.ilUtil
+
+
 ?>
