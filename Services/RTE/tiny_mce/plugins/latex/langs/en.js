b5adfe3ac870ba853f5b16731eba8b72188bf544
hschottm <hschottm>
2006-07-06 12:09:58 +0000

    Removed tinymce editor from CVS
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@11335 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/RTE/tiny_mce/plugins/latex/langs/en.js
+++
@@ -1,8 +0,0 @@
-// UK lang variables
-
-/* Remember to namespace the language parameters lang_<your plugin>_<some name> */
-
-tinyMCE.addToLang('',{
-latex_title : 'LaTeX markup',
-latex_desc : 'This command marks the selected text as LaTeX markup'
-});
