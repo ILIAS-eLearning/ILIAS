6c89a040869749adcd3ad64207665ebd2f24794b
jconze <jconze@svn.ilias.de>
2006-07-12 09:31:16 +0000

    no message
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@11399 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- payment/classes/class.ilPaymentBuyedObjectsGUI.php
+++ payment/classes/class.ilPaymentBuyedObjectsGUI.php
@@ -171,7 +171,7 @@ class ilPaymentBuyedObjectsGUI extends ilPaymentBaseGUI
 		$tpl->parseCurrentBlock();
 
 
-		$tbl->setTitle($this->lng->txt("paya_statistic"),"icon_pays.gif",$this->lng->txt("paya_statistic"));
+		$tbl->setTitle($this->lng->txt("paya_buyed_objects"),"icon_pays_access.gif",$this->lng->txt("paya_statistic"));
 		$tbl->setHeaderNames(array($this->lng->txt("paya_transaction"),
 								   $this->lng->txt("title"),
 								   $this->lng->txt("paya_vendor"),
