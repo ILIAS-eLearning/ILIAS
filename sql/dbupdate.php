7fd8c38963bf1818438ed09d34fa3c483902a3b7
jconze <jconze@svn.ilias.de>
2006-08-03 11:18:58 +0000

    Courses: Add field "show members"
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@11764 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- sql/dbupdate.php
+++ sql/dbupdate.php
@@ -11479,3 +11479,5 @@ ALTER TABLE mail ADD INDEX folder_id (folder_id);
 ALTER TABLE mail ADD INDEX m_status (m_status);
 <#811>
 ALTER TABLE frm_posts ADD INDEX pos_date (pos_date);
+<#812>
+ALTER TABLE `crs_settings` ADD `show_members` TINYINT NOT NULL DEFAULT '1';
