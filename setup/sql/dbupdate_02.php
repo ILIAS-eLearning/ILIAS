ef7cc94ca1fae4851547444cc5c71b6da9ea334d
Stefan Meyer <smeyer.ilias@gmx.de>
2006-12-07 13:38:06 +0000

    Added ldap tables
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@12789 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- setup/sql/dbupdate_02.php
+++ setup/sql/dbupdate_02.php
@@ -38,3 +38,57 @@ if ($attribute_visibility == FALSE)
 ?>
 <#870>
 ALTER TABLE `survey_survey_question` CHANGE `heading` `heading` TEXT NULL DEFAULT NULL;
+
+<#871>
+CREATE TABLE IF NOT EXISTS `ldap_attribute_mapping` (
+  `server_id` int(11) NOT NULL default '0',
+  `keyword` varchar(32)   NOT NULL default '',
+  `value` varchar(255)   NOT NULL default '',
+  `perform_update` tinyint(1) NOT NULL default '0',
+  PRIMARY KEY  (`server_id`,`keyword`),
+  KEY `server_id` (`server_id`)
+) TYPE=MyISAM;
+
+
+CREATE TABLE IF NOT EXISTS `ldap_role_group_mapping` (
+  `mapping_id` int(11) NOT NULL auto_increment,
+  `server_id` int(3) NOT NULL default '0',
+  `dn` varchar(255)   NOT NULL default '',
+  `member_attribute` varchar(64)   NOT NULL default '',
+  `member_isdn` tinyint(1) NOT NULL default '0',
+  `role` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`mapping_id`)
+) TYPE=MyISAM;
+
+
+CREATE TABLE IF NOT EXISTS `ldap_server_settings` (
+  `server_id` int(2) NOT NULL auto_increment,
+  `active` int(1) NOT NULL default '0',
+  `name` varchar(32)   NOT NULL default '',
+  `url` varchar(255)   NOT NULL default '',
+  `version` int(1) NOT NULL default '0',
+  `base_dn` varchar(255)   NOT NULL default '',
+  `referrals` int(1) NOT NULL default '0',
+  `tls` int(1) NOT NULL default '0',
+  `bind_type` int(1) NOT NULL default '0',
+  `bind_user` varchar(255)   NOT NULL default '',
+  `bind_pass` varchar(32)   NOT NULL default '',
+  `search_base` varchar(255)   NOT NULL default '',
+  `user_scope` tinyint(1) NOT NULL default '0',
+  `user_attribute` varchar(255)   NOT NULL default '',
+  `filter` varchar(255)   NOT NULL default '',
+  `group_dn` varchar(255)   NOT NULL default '',
+  `group_scope` tinyint(1) NOT NULL default '0',
+  `group_filter` varchar(255)   NOT NULL default '',
+  `group_member` varchar(255)   NOT NULL default '',
+  `group_memberisdn` tinyint(1) NOT NULL default '0',
+  `group_name` varchar(255)   NOT NULL default '',
+  `group_attribute` varchar(64)   NOT NULL default '',
+  `sync_on_login` tinyint(1) NOT NULL default '0',
+  `sync_per_cron` tinyint(1) NOT NULL default '0',
+  `role_sync_active` tinyint(1) NOT NULL default '0',
+  `role_bind_dn` varchar(255)   NOT NULL default '',
+  `role_bind_pass` varchar(32)   NOT NULL default '',
+  PRIMARY KEY  (`server_id`)
+) TYPE=MyISAM;
+
