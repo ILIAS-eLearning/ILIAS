646e43575e7127b4d9b11f181dc80204e02f29e6
Stefan Meyer <smeyer.ilias@gmx.de>
2006-12-15 13:36:31 +0000

    Movesd chat in Modules dir
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@12853 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ Modules/Chat/classes/class.ilChatRecording.php
@@ -0,0 +1,302 @@
+<?php
+/*
+	+-----------------------------------------------------------------------------+
+	| ILIAS open source                                                           |
+	+-----------------------------------------------------------------------------+
+	| Copyright (c) 1998-2001 ILIAS open source, University of Cologne            |
+	|                                                                             |
+	| This program is free software; you can redistribute it and/or               |
+	| modify it under the terms of the GNU General Public License                 |
+	| as published by the Free Software Foundation; either version 2              |
+	| of the License, or (at your option) any later version.                      |
+	|                                                                             |
+	| This program is distributed in the hope that it will be useful,             |
+	| but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+	| GNU General Public License for more details.                                |
+	|                                                                             |
+	| You should have received a copy of the GNU General Public License           |
+	| along with this program; if not, write to the Free Software                 |
+	| Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
+	+-----------------------------------------------------------------------------+
+*/
+
+/**
+* Class ilChatRecording
+* 
+* @author Jens Conze 
+* @version $Id$
+*
+*/
+
+class ilChatRecording
+{
+	var $ilias;
+	var $lng;
+
+	var $error_msg;
+
+	var $obj_id = 0; // OF CHAT OBJECT
+	var $moderator_id = 0;
+	var $room_id = 0;
+	var $record_id = 0;
+
+	var $data = array();
+
+	/**
+	* Constructor
+	* @access	public
+	* @param	integer	reference_id or object_id
+	* @param	boolean	treat the id as reference_id (true) or object_id (false)
+	*/
+	function ilChatRecording($a_id = 0)
+	{
+		global $ilias,$lng;
+
+		define(MAX_TIME,60*60*24);
+
+		$this->ilias =& $ilias;
+		$this->lng =& $lng;
+
+		if ($a_id > 0)
+		{
+			$this->setObjId($a_id);
+		}
+	}
+
+	// SET/GET
+	function getErrorMessage()
+	{
+		return $this->error_msg;
+	}
+
+	function setRoomId($a_id)
+	{
+		$this->room_id = $a_id;
+	}
+	function getRoomId()
+	{
+		return $this->room_id;
+	}
+
+	function setObjId($a_id)
+	{
+		$this->obj_id = $a_id;
+	}
+	function getObjId()
+	{
+		return $this->obj_id;
+	}
+
+	function setRecordId($a_id)
+	{
+		$this->record_id = $a_id;
+	}
+	function getRecordId()
+	{
+		return $this->record_id;
+	}
+
+	function getStartTime()
+	{
+		return $this->data["start_time"];
+	}
+	function getEndTime()
+	{
+		return $this->data["end_time"];
+	}
+
+	function setModeratorId($a_id)
+	{
+		$this->moderator_id = $a_id;
+	}
+	function getModeratorId()
+	{
+		return $this->moderator_id;
+	}
+
+	function setRecord($a_data)
+	{
+		$this->data = $a_data;
+	}
+	function getRecord($a_id = 0)
+	{
+		if ($a_id != 0)
+		{
+			$this->setRecordId($a_id);
+		}
+
+		$query = "SELECT * FROM chat_records WHERE 
+					record_id = '" . addslashes($this->getRecordId()) . "'";
+		$res = $this->ilias->db->query($query);
+		if (DB::isError($res)) die("ilChatRecording::getRecord(): " . $res->getMessage() . "<br>SQL-Statement: ".$q);
+
+		$data = array();
+		$status = false;
+		if ($res->numRows() > 0)
+		{
+			$data = $res->fetchRow(DB_FETCHMODE_ASSOC);
+			$status = true;
+		}
+
+		$this->setRecord($data);
+		return $status;
+	}
+
+	function setTitle($a_title)
+	{
+		$this->data["title"] = $a_title;
+	}
+	function getTitle()
+	{
+		return $this->data["title"];
+	}
+
+	function setDescription($a_description)
+	{
+		$this->data["description"] = $a_description;
+	}
+	function getDescription()
+	{
+		return $this->data["description"];
+	}
+
+	function startRecording($a_title = "")
+	{
+		$query = "INSERT INTO chat_records SET 
+					moderator_id = '" . $this->getModeratorId() . "', 
+					chat_id = '" . $this->getObjId() . "', 
+					room_id = '" . $this->getRoomId() . "', 
+					title = '" . (($a_title == "") ? "-N/A-" : addslashes($a_title)) . "', 
+					start_time = '" . time() . "'";
+		$res = $this->ilias->db->query($query);
+		if (DB::isError($res)) 
+			die("ilChatRecording::startRecording(): " . $res->getMessage() . "<br>SQL-Statement: ".$q);
+
+		$query = "SELECT LAST_INSERT_ID()";
+		$res = $this->ilias->db->query($query);
+		if (DB::isError($res)) 
+			die("ilChatRecording::startRecording(): " . $res->getMessage() . "<br>SQL-Statement: ".$q);
+
+		if ($res->numRows() > 0)
+		{
+			$lastId = $res->fetchRow();
+			$this->setRecordId($lastId[0]);
+
+			$this->getRecord();
+		}
+	}
+
+	function stopRecording()
+	{
+		$query = "UPDATE chat_records SET 
+					end_time = '" . time() . "' WHERE 
+					chat_id = '" . $this->getObjId() . "' AND 
+					room_id = '" . $this->getRoomId() . "'";
+		$res = $this->ilias->db->query($query);
+		if (DB::isError($res)) die("ilChatRecording::stopRecording(): " . $res->getMessage() . "<br>SQL-Statement: ".$q);
+
+		$this->setRecordId(0);
+
+		$data = array();
+		$this->setRecord($data);
+	}
+
+	function isRecording()
+	{
+		$query = "SELECT record_id FROM chat_records WHERE 
+					chat_id = '" . $this->getObjId() . "' AND 
+					room_id = '" . $this->getRoomId() . "' AND 
+					start_time > 0 AND 
+					end_time = 0";
+		$res = $this->ilias->db->query($query);
+		if (DB::isError($res)) die("ilChatRecording::isRecording(): " . $res->getMessage() . "<br>SQL-Statement: ".$q);
+
+		if ($res->numRows() > 0)
+		{
+			$id = $res->fetchRow(DB_FETCHMODE_ASSOC);
+			$this->setRecordId($id["record_id"]);
+
+			$this->getRecord();
+			return true;
+		}
+		
+		return false;
+	}
+
+	function getRecordings()
+	{
+		$query = "SELECT * FROM chat_records WHERE 
+					chat_id = '" . $this->getObjId() . "'";
+		$res = $this->ilias->db->query($query);
+		if (DB::isError($res)) die("ilChatRecording::getRecordings(): " . $res->getMessage() . "<br>SQL-Statement: ".$q);
+
+		if (($num = $res->numRows()) > 0)
+		{
+			for ($i = 0; $i < $num; $i++)
+			{
+				$data[] = $res->fetchRow(DB_FETCHMODE_ASSOC);
+			}
+			return $data;
+		}
+		
+		return false;
+	}
+
+	function getModerator($a_id = 0)
+	{
+		if ($a_id == 0)
+		{
+			$a_id = $this->getModeratorId();
+		}
+
+		return ilObjUser::_lookupLogin($a_id);
+	}
+
+	function delete($a_id = 0)
+	{
+		if ($a_id == 0 ||
+			$a_id == $this->getRecordId())
+		{
+			$a_id = $this->getRecordId();
+			$this->setRecordId(0);
+
+			$data = array();
+			$this->setRecord($data);
+		}
+
+		$query = "DELETE FROM chat_records WHERE 
+					record_id = '" . $a_id . "'";
+		$res = $this->ilias->db->query($query);
+		if (DB::isError($res)) die("ilChatRecording::delete(): " . $res->getMessage() . "<br>SQL-Statement: ".$q);
+
+		$query = "DELETE FROM chat_record_data WHERE 
+					record_id = '" . $a_id . "'";
+		$res = $this->ilias->db->query($query);
+		if (DB::isError($res)) die("ilChatRecording::delete(): " . $res->getMessage() . "<br>SQL-Statement: ".$q);
+	}
+
+	function exportMessages()
+	{
+		$query = "SELECT message FROM chat_record_data WHERE 
+					record_id = '" . $this->getRecordId() . "' ORDER BY 
+					msg_time ASC";
+		$res = $this->ilias->db->query($query);
+		if (DB::isError($res)) die("ilChatRecording::exportMessages(): " . $res->getMessage() . "<br>SQL-Statement: ".$q);
+
+		$html = "";
+		if (($num = $res->numRows()) > 0)
+		{
+			$html = "";
+			for ($i = 0; $i < $num; $i++)
+			{
+				$data = $res->fetchRow(DB_FETCHMODE_ASSOC);
+				$html .= $data["message"] . "<br />\n";
+			}
+		}
+		
+		return $html;
+	}
+
+} // END class.ilChatRecording
+?>
---
+++ Modules/Chat/classes/class.ilChatServerCommunicator.php
@@ -0,0 +1,311 @@
+<?php
+/*
+	+-----------------------------------------------------------------------------+
+	| ILIAS open source                                                           |
+	+-----------------------------------------------------------------------------+
+	| Copyright (c) 1998-2001 ILIAS open source, University of Cologne            |
+	|                                                                             |
+	| This program is free software; you can redistribute it and/or               |
+	| modify it under the terms of the GNU General Public License                 |
+	| as published by the Free Software Foundation; either version 2              |
+	| of the License, or (at your option) any later version.                      |
+	|                                                                             |
+	| This program is distributed in the hope that it will be useful,             |
+	| but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+	| GNU General Public License for more details.                                |
+	|                                                                             |
+	| You should have received a copy of the GNU General Public License           |
+	| along with this program; if not, write to the Free Software                 |
+	| Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
+	+-----------------------------------------------------------------------------+
+*/
+
+/**
+* Class ilChatServerCommunicator
+* 
+* @author Stefan Meyer 
+* @version $Id$
+*
+*/
+
+class ilChatServerCommunicator
+{
+	var $chat;
+
+	var $message;
+	var $socket_p;
+	var $type;
+	var $rcp_id;
+	var $rcp_login;
+	var $kicked_user;
+	/**
+	* Constructor
+	* @access	public
+	* @param	integer	reference_id or object_id
+	* @param	boolean	treat the id as reference_id (true) or object_id (false)
+	*/
+	function ilChatServerCommunicator(&$chat_obj)
+	{
+		define(TIMEOUT,2);
+
+		$this->chat =& $chat_obj;
+	}
+
+	// SET/GET
+	function setRecipientId($a_id)
+	{
+		$this->rcp_id = $a_id;
+		
+		if($this->rcp_id)
+		{
+			$tmp_user =& new ilObjUser($this->rcp_id);
+			$this->setRecipientLogin($tmp_user->getLogin());
+			unset($tmp_user);
+		}
+	}
+	function getRecipientId()
+	{
+		return $this->rcp_id;
+	}
+	function setRecipientLogin($a_login)
+	{
+		$this->rcp_login = $a_login;
+	}
+	function getRecipientLogin()
+	{
+		return $this->rcp_login;
+	}
+	function setKickedUser($k_user)
+	{
+		$this->kicked_user = $k_user;
+	}
+	function getKickedUser()
+	{
+		return $this->kicked_user;
+	}
+	function setMessage($a_message)
+	{
+		$this->message = $a_message;
+	}
+	function getMessage()
+	{
+		return $this->message;
+	}
+	function setType($a_type)
+	{
+		$this->type = $a_type;
+	}
+	function getType()
+	{
+		return $this->type ? $this->type : 'normal';
+	}
+	function getHtml()
+	{
+		return $this->html;
+	}
+
+	function send()
+	{
+		if(!$this->__openSocket())
+		{
+			return false;
+		}
+		fputs($this->socket_p,$this->__formatMessage());
+		fclose($this->socket_p);
+
+		return true;
+	}
+	
+	function getServerFrameSource()
+	{		
+		return sprintf("http".($this->chat->server_conf->getSSLStatus() && $this->chat->server_conf->getSSLPort() ? "s" : "")."://%s:%s/%s%s",
+                       $this->chat->server_conf->getExternalIp(),
+					   ($this->chat->server_conf->getSSLStatus() && $this->chat->server_conf->getSSLPort() ? $this->chat->server_conf->getSSLPort() : $this->chat->server_conf->getPort()),
+					   $this->__formatLogin($this->chat->chat_user->getLogin()),
+					   $this->__getFormattedChatroom());
+	}
+
+	function isAlive()
+	{
+		$this->setType("test");
+		return $this->send();
+	}
+
+	// PRIVATE
+	function __getFormattedChatroom()
+	{
+		$nic = $this->chat->server_conf->getNic();
+
+		return $nic.$this->chat->chat_room->getInternalName().
+			substr("______________",0,14-strlen($this->chat->chat_room->getInternalName()));
+	}
+	function __formatLogin($a_login)
+	{
+		$nic = $this->chat->server_conf->getNic();
+
+		return substr($nic.md5($a_login),0,32);
+	}
+	function __formatMessage()
+	{
+		$this->__emoticons();
+
+		switch($this->getType())
+		{
+			case 'private':
+				// STORE MESSAGE IN DB
+				return "|".$this->__formatLogin($this->getRecipientLogin()).
+					$this->__formatLogin($this->chat->chat_user->getLogin()).
+					$this->__getFormattedChatroom().$this->getMessage()."<br />";
+			
+			case 'address':
+				// STORE MESSAGE IN DB
+				$this->chat->chat_room->appendMessageToDb($this->getMessage());
+				return ">".$this->__getFormattedChatroom().$this->getMessage()."<br />";
+
+			case 'normal':
+				// STORE MESSAGE IN DB
+				$this->chat->chat_room->appendMessageToDb($this->getMessage());
+				return ">".$this->__getFormattedChatroom().$this->getMessage()."<br />";
+
+			case 'login':
+				return "!".$this->__formatLogin($this->chat->chat_user->getLogin()).$_SERVER["REMOTE_ADDR"];
+
+			case "logout":
+				return "-".$this->__formatLogin($this->chat->chat_user->getLogin());
+
+			case "kick":
+				return "GET /moderate?".$this->chat->server_conf->getModeratorPassword().
+					"&kick&".$this->__formatLogin($this->getKickedUser()).
+					"&".$this->__getFormattedChatroom();
+
+			case "delete":
+				return "GET /moderate?".$this->chat->server_conf->getModeratorPassword().
+					"&delete&".$this->__getFormattedChatroom();
+
+			case "empty":
+				return "GET /moderate?".$this->chat->server_conf->getModeratorPassword().
+					"&clear&".$this->__getFormattedChatroom();
+
+			case "test":
+				return "GET /Version";
+
+			default:
+				return "GET /Version";
+
+		}
+	}
+
+	function __emoticons()
+	{
+		$str = $this->getMessage();
+		$str = str_replace(":)", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_smile.gif\" border=0>", $str);
+		$str = str_replace(":-)", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_smile.gif\" border=0>", $str);
+		$str = str_replace(":smile:", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_smile.gif\" border=0>", $str);
+		$str = str_replace(";)", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_wink.gif\" border=0>", $str);
+		$str = str_replace(";-)", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_wink.gif\" border=0>", $str);
+		$str = str_replace(":wink:", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_wink.gif\" border=0>", $str);
+		$str = str_replace(":D", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_laugh.gif\" border=0>", $str);
+		$str = str_replace(":-D", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_laugh.gif\" border=0>", $str);
+		$str = str_replace(":laugh:", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_laugh.gif\" border=0>", $str);
+		$str = str_replace(":grin:", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_laugh.gif\" border=0>", $str);
+		$str = str_replace(":biggrin:", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_laugh.gif\" border=0>", $str);
+		$str = str_replace(":(", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_sad.gif\" border=0>", $str);
+		$str = str_replace(":-(", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_sad.gif\" border=0>", $str);
+		$str = str_replace(":sad:", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_sad.gif\" border=0>", $str);
+		$str = str_replace(":o", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_shocked.gif\" border=0>", $str);
+		$str = str_replace(":-o", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_shocked.gif\" border=0>", $str);
+		$str = str_replace(":shocked:", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_smile.gif\" border=0>", $str);
+		$str = str_replace(":p", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_tongue.gif\" border=0>", $str);
+		$str = str_replace(":-p", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_tongue.gif\" border=0>", $str);
+		$str = str_replace(":tongue:", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_smile.gif\" border=0>", $str);
+		$str = str_replace(":cool:", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_cool.gif\" border=0>", $str);
+		$str = str_replace(":eek:", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_eek.gif\" border=0>", $str);
+		$str = str_replace(":||", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_angry.gif\" border=0>", $str);
+		$str = str_replace(":-||", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_angry.gif\" border=0>", $str);
+		$str = str_replace(":angry:", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_angry.gif\" border=0>", $str);
+		$str = str_replace(":flush:", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_flush.gif\" border=0>", $str);
+		$str = str_replace(":idea:", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_idea.gif\" border=0>", $str);
+		$str = str_replace(":thumbup:", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_thumbup.gif\" border=0>", $str);
+		$str = str_replace(":thumbdown:", "<img src=\"" . ILIAS_HTTP_PATH . 
+						   "/templates/default/images/emoticons/icon_thumbdown.gif\" border=0>", $str);
+		$this->setMessage($str);
+	}
+
+	function __openSocket()
+	{
+        $this->socket_p = @fsockopen($this->chat->server_conf->getInternalIp(), 
+									 $this->chat->server_conf->getPort(), $errno, $errstr, TIMEOUT);
+
+		return $this->socket_p == null ? false : true;
+	}
+
+	// STATIC
+	function _initObject()
+	{
+		global $ilias;
+		
+		$query = "SELECT ref_id FROM object_data NATURAL JOIN object_reference ".
+			"WHERE type = 'chac'";
+		
+		$res = $ilias->db->query($query);
+		while($row = $res->fetchRow(DB_FETCHMODE_OBJECT))
+		{
+			$ref_id = $row->ref_id;
+		}
+		
+		// GET INSTANCE
+		return new ilChatServerCommunicator($tmp =& ilObjectFactory::getInstanceByRefId($ref_id));
+	}		
+
+	function _login()
+	{
+		$obj =& ilChatServerCommunicator::_initObject();
+
+		// CALLED BY login.php
+		$obj->setType("login");
+		$obj->chat->chat_user->setUserId($_SESSION["AccountId"]);
+		$obj->send();
+	}
+
+	function _logout()
+	{
+		$obj =& ilChatServerCommunicator::_initObject();
+
+		// CALLED BY login.php
+		$obj->setType("logout");
+		$obj->chat->chat_user->setUserId($_SESSION["AccountId"]);
+		$obj->send();
+	}
+		
+
+} // END class.ilChatServerCommunicator
+?>
