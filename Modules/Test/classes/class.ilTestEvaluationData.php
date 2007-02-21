8d48206486815837d6e860a1e3b428bee25c547d
hschottm <hschottm@svn.ilias.de>
2007-02-21 12:05:54 +0000

    Added classes to build a complete evaluation of a test. Started using this evaluation class for the detailed evaluation output
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@13292 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ Modules/Test/classes/class.ilTestEvaluationData.php
@@ -0,0 +1,122 @@
+<?php
+/*
+	+-----------------------------------------------------------------------------+
+	| ILIAS open source                                                           |
+	+-----------------------------------------------------------------------------+
+	| Copyright (c) 1998-2001 ILIAS open source, University of Cologne            |
+	|                                                                             |
+	| This program is free software; you can redistribute it and/or               |
+	| modify it under the terms of the GNU General Public License                 |
+	| as published by the Free Software Foundation; either version 2              |
+	| of the License, or (at your option) any later version.                      |
+	|                                                                             |
+	| This program is distributed in the hope that it will be useful,             |
+	| but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+	| GNU General Public License for more details.                                |
+	|                                                                             |
+	| You should have received a copy of the GNU General Public License           |
+	| along with this program; if not, write to the Free Software                 |
+	| Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
+	+-----------------------------------------------------------------------------+
+*/
+
+/**
+* Class ilTestEvaluationData
+*
+* @author		Helmut Schottmüller <helmut.schottmueller@mac.com>
+* @version $Id$
+*
+* @defgroup ModulesTest Modules/Test
+* @extends ilObject
+*/
+
+class ilTestEvaluationData
+{
+	/**
+	* Question titles
+	*
+	* @var array
+	*/
+	var $questionTitles;
+
+	/**
+	* Participants
+	*
+	* @var array
+	*/
+	var $participants;
+
+	/**
+	* Statistical data
+	*
+	* @var object
+	*/
+	var $statistics;
+
+	/**
+	* Constructor
+	*
+	* @access	public
+	*/
+	function ilTestEvaluationData($test_id, $withStatistics = TRUE)
+	{
+		$this->participants = array();
+		$this->questionTitles = array();
+		if ($withStatistics)
+		{
+			include_once "./Modules/Test/classes/class.ilTestStatistics.php";
+			$this->statistics = new ilTestStatistics($test_id);
+		}
+	}
+	
+	function addQuestionTitle($question_id, $question_title)
+	{
+		$this->questionTitles[$question_id] = $question_title;
+	}
+	
+	function getQuestionTitles()
+	{
+		return $this->questionTitles;
+	}
+	
+	function getQuestionTitle($question_id)
+	{
+		if (array_key_exists($question_id, $this->questionTitles))
+		{
+			return $this->questionTitles[$question_id];
+		}
+		else
+		{
+			return "";
+		}
+	}
+	
+	function getParticipants()
+	{
+		return $this->participants;
+	}
+	
+	function addParticipant($active_id, $participant)
+	{
+		$this->participants[$active_id] = $participant;
+	}
+	
+	function &getParticipant($active_id)
+	{
+		return $this->participants[$active_id];
+	}
+	
+	function participantExists($active_id)
+	{
+		return array_key_exists($active_id, $this->participants);
+	}
+	
+	function &getStatistics()
+	{
+		return $this->statistics;
+	}
+
+} // END ilTestEvaluationData
+
+?>
