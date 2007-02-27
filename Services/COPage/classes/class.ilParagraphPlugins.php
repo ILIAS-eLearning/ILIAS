68e977dc7159f8b4522f5117e37b11caca48f83c
rkuester <rkuester@svn.ilias.de>
2007-02-27 23:42:15 +0000

    - fixed paragraphplugins due to new structure
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@13369 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/COPage/classes/class.ilParagraphPlugins.php
+++ Services/COPage/classes/class.ilParagraphPlugins.php
@@ -37,38 +37,38 @@ class ilParagraphPlugins {
 	 * keys are equal to the serialization string of a plugin
 	 */
 	var $plugins;
-	
+
 	/**
 	 * contains the plugins directory
 	 */
 	var $pluginDirectory;
 	/**
-	 * array which contains all directories which should not be parsed 
+	 * array which contains all directories which should not be parsed
 	 * within the plugins directory, by default the sub directories
 	 * resources, CVS and classes are skipped
 	 */
 	var $skipDirectories;
-	
+
 	/**
 	 * constructor initializes skip Directories
 	 */
 	function ilParagraphPlugins () {
 		$this->plugins = array();
-		$this->pluginDirectory = ILIAS_ABSOLUTE_PATH."/content/plugins";
+		$this->pluginDirectory = ILIAS_ABSOLUTE_PATH."/Services/COPage/plugins";
 		$this->skipDirectories = array ();
-		$this->skipDirectories [$this->pluginDirectory."/classes"] = "skip"; 
+		$this->skipDirectories [$this->pluginDirectory."/classes"] = "skip";
 		$this->skipDirectories [$this->pluginDirectory."/resources"]= "skip";
 		$this->skipDirectories [$this->pluginDirectory."/CVS"]= "skip";
 	}
-	
+
 	/**
 	 * getPluginArray
 	 */
 	function getRegisteredPluginsAsArray () {
-		return $this->plugins;		
+		return $this->plugins;
 	}
-	
-	
+
+
 	/**
 	 * get a specific plugin by its directory name (since directories are unique)
 	 *
@@ -76,12 +76,12 @@ class ilParagraphPlugins {
 	 */
 	function getParagraphPlugin ($pluginDir) {
 	    foreach ($this->plugins as $plugin) {
-	        if (strcasecmp($pluginDir, $plugin->getDirectory()) == 0) 
+	        if (strcasecmp($pluginDir, $plugin->getDirectory()) == 0)
 	           return $plugin;
 	    }
 	    return null;
 	}
-	
+
 	/**
 	 * register plugin
 	 */
@@ -89,20 +89,28 @@ class ilParagraphPlugins {
 		//echo "registered Plugin ".$plugin->getTitle();
 		$this->plugins[$plugin->serializeToString()] = $plugin;
 	}
-	
+
+
+	function isRegistered ($pluginClassname) {
+	    foreach ($this->plugins as $plugin) {
+	        if (strcasecmp(get_class($plugin), $pluginClassname) == 0)
+	           return true;
+	    }
+	    return false;
+	}
 	/**
 	 * serializes all plugin to one string
 	 * format filetype#title#link#image|filetype#title#link#image|...
 	 */
 	function serializeToString (){
-		return implode ("|", array_keys($this->plugins));		
+		return implode ("|", array_keys($this->plugins));
 	}
-	
+
 	/**
 	 * parses plugin subdirectory to determine registered plugins
 	 */
-	function initialize () {		
-		if (file_exists($this->pluginDirectory)) 
+	function initialize () {
+		if (file_exists($this->pluginDirectory))
 		{
 		    $pluginDirs = glob($this->pluginDirectory."/*",GLOB_ONLYDIR);
 		    if (is_array($pluginDirs))
@@ -112,71 +120,73 @@ class ilParagraphPlugins {
     				if (array_key_exists($pluginDir,$this->skipDirectories) || !file_exists($pluginDir."/plugin.xml")) {
     					continue;
     				}
-                    
-    				
+
+
     				// load plugin xml, to retrieve plugin node (see dtd)
     				$pluginDOM = new ilDOMXML();
     				$pluginDOM->loadDocument("plugin.xml", $pluginDir, false);
     				$pluginNodes = $pluginDOM->getElementsByTagname("plugin");
-    				
-    				if ( count ($pluginNodes)>0 ) 
+
+    				if ( count ($pluginNodes)>0 )
     				{
     				    // found plugin node
     				    $pluginNode = $pluginNodes[0];
     				    //print_r($pluginNode) ;
         				//this is the subdirectory of the plugin
     				    $pluginSubDir = str_replace($this->pluginDirectory."/","",$pluginDir);
-    
+
         				// class file containing class which inherits from paragraph plugin
     				    $classfile = $pluginNode->get_attribute ("classfile");
     				    // according classname
         				$classname = $pluginNode->get_attribute ("classname");
-        				
+
         				// filter filetype, refers to sourcecode directory, hfile, e.g. java122 affects, that this plugin is for this paragraph type only
         				$filetype  = $pluginNode->get_attribute ("filetype");
-        				
+
         				// enable/disable plugin
         				$active    = strcasecmp($pluginNode->get_attribute ("active"),"true") == 0;
-        
+
         				// title is alt text for image
         				$title       = $this->getTextContent($pluginNode,"title");
-        				
-        				// link    				
+
+        				// link
         				$link        = $this->getTextContent($pluginNode,"link");
-        				
+
         				// description, not shown at the momemnt
         				$description = $this->getTextContent($pluginNode,"description");
-                        
-        				
+
+
         				// prepare class file for include, must reside in classes directory
         				$classfile = $pluginDir . "/classes/".$classfile;
-        				
-       /* 				echo $classfile."<br>";
+
+      			/*	      echo $classfile."<br>";
         				echo $classname."<br>";
         				echo $filetype."<br>";
         				echo $active."<br>";
         				echo $title."<br>";
-        				echo $description."<br>";
-       */ 				
-        				if (file_exists($classfile) && $active == TRUE && !class_exists($classname)) {
-        					include ($classfile);
+        				echo $description."<br>";*/
+
+        				if (file_exists($classfile) && $active && !$this->isRegistered($classname))
+        				{
+        					include_once ($classfile);
         					$plugin = new $classname($pluginSubDir, $title, $filetype, $link, $description, $active);
-        					
+
         					//print_r($plugin);
-        					
+
         					if (is_a($plugin,"ilParagraphPlugin") && $plugin->isActive()) {
         						$this->registerPlugin($plugin);
+
         						unset ($plugin);
         					}
         				}
-    				    
-    				}	
+
+    				}
     			}
-			}	
+			}
 		}
 	}
-	
-	
+
+
 	/**
 	 * get Text content from a child node (unique by name), otherwise first will be taken.
 	 *
