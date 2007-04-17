5f6ead334fa8fcd28deac36e01d4bc939bd59253
hschottm <hschottm>
2007-04-17 15:24:42 +0000

    Added viewport check for filter menu
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@13659 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/TestQuestionPool/templates/default/tpl.question_pool_menu.js
+++ Modules/TestQuestionPool/templates/default/tpl.question_pool_menu.js
@@ -9,10 +9,8 @@
 		if (filtericon != null) 
 		{
 			filtericon.style.visibility = 'visible';
-			xPos = YAHOO.util.Dom.getX(filtericon);
-			yPos = YAHOO.util.Dom.getY(filtericon) + 20;
 		}
-		qpMenu = new YAHOO.widget.Menu("qpMenu", { x : xPos, y : yPos, clicktohide: true, hidedelay: 100, zIndex: 1000, maxheight: 500 });
+		qpMenu = new YAHOO.widget.Menu("qpMenu", { x : xPos, y : yPos, clicktohide: true, hidedelay: 100, maxheight: 500, constraintoviewport: true });
 		qpMenu.addItems([
 <!-- BEGIN menuitem -->			{ text:"{ITEM_TEXT}", url:"{ITEM_URL}"<!-- BEGIN selected -->, selected: true, checked: true<!-- END selected --> },<!-- END menuitem -->
 		]);
@@ -28,6 +26,15 @@
 		if (element != null)
 		{
 			xPos = YAHOO.util.Dom.getX(element);
+			//if (xPos + qpMenu.)
+			var menu = document.getElementById("qpMenu");
+			if (menu != null)
+			{
+				var region = YAHOO.util.Dom.getRegion(menu);
+				var menuwidth = region.right - region.left;
+				if (xPos + menuwidth > YAHOO.util.Dom.getViewportWidth()) xPos = YAHOO.util.Dom.getViewportWidth() - menuwidth;
+				if (xPos < 0) xPos = 0;
+			}
 			yPos = YAHOO.util.Dom.getY(element) + 20;
 		}
 		qpMenu.moveTo(xPos, yPos);
