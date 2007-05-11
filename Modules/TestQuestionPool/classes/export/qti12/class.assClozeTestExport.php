89b58e17d28fdbb5aae81f22a38d8d03ee35079a
hschottm <hschottm@svn.ilias.de>
2007-05-11 21:27:25 +0000

    Added question export classes
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@13873 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ Modules/TestQuestionPool/classes/export/qti12/class.assClozeTestExport.php
@@ -0,0 +1,435 @@
+<?php
+ /*
+   +----------------------------------------------------------------------------+
+   | ILIAS open source                                                          |
+   +----------------------------------------------------------------------------+
+   | Copyright (c) 1998-2001 ILIAS open source, University of Cologne           |
+   |                                                                            |
+   | This program is free software; you can redistribute it and/or              |
+   | modify it under the terms of the GNU General Public License                |
+   | as published by the Free Software Foundation; either version 2             |
+   | of the License, or (at your option) any later version.                     |
+   |                                                                            |
+   | This program is distributed in the hope that it will be useful,            |
+   | but WITHOUT ANY WARRANTY; without even the implied warranty of             |
+   | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              |
+   | GNU General Public License for more details.                               |
+   |                                                                            |
+   | You should have received a copy of the GNU General Public License          |
+   | along with this program; if not, write to the Free Software                |
+   | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. |
+   +----------------------------------------------------------------------------+
+*/
+
+include_once "./Modules/TestQuestionPool/classes/export/qti12/class.assQuestionExport.php";
+
+/**
+* Class for cloze question exports
+*
+* assClozeTestExport is a class for cloze question exports
+*
+* @author		Helmut Schottmüller <helmut.schottmueller@mac.com>
+* @version	$Id$
+* @ingroup ModulesTestQuestionPool
+*/
+class assClozeTestExport extends assQuestionExport
+{
+	/**
+	* Returns a QTI xml representation of the question
+	*
+	* Returns a QTI xml representation of the question and sets the internal
+	* domxml variable with the DOM XML representation of the QTI xml representation
+	*
+	* @return string The QTI xml representation of the question
+	* @access public
+	*/
+	function toXML($a_include_header = true, $a_include_binary = true, $a_shuffle = false, $test_output = false, $force_image_references = false)
+	{
+		global $ilias;
+		
+		include_once "./Services/Math/classes/class.EvalMath.php";
+		$eval = new EvalMath();
+		$eval->suppress_errors = TRUE;
+		include_once("./classes/class.ilXmlWriter.php");
+		$a_xml_writer = new ilXmlWriter;
+		// set xml header
+		$a_xml_writer->xmlHeader();
+		$a_xml_writer->xmlStartTag("questestinterop");
+		$attrs = array(
+			"ident" => "il_".IL_INST_ID."_qst_".$this->object->getId(),
+			"title" => $this->object->getTitle()
+		);
+		$a_xml_writer->xmlStartTag("item", $attrs);
+		// add question description
+		$a_xml_writer->xmlElement("qticomment", NULL, $this->object->getComment());
+		// add estimated working time
+		$workingtime = $this->object->getEstimatedWorkingTime();
+		$duration = sprintf("P0Y0M0DT%dH%dM%dS", $workingtime["h"], $workingtime["m"], $workingtime["s"]);
+		$a_xml_writer->xmlElement("duration", NULL, $duration);
+		// add ILIAS specific metadata
+		$a_xml_writer->xmlStartTag("itemmetadata");
+		$a_xml_writer->xmlStartTag("qtimetadata");
+		$a_xml_writer->xmlStartTag("qtimetadatafield");
+		$a_xml_writer->xmlElement("fieldlabel", NULL, "ILIAS_VERSION");
+		$a_xml_writer->xmlElement("fieldentry", NULL, $ilias->getSetting("ilias_version"));
+		$a_xml_writer->xmlEndTag("qtimetadatafield");
+		$a_xml_writer->xmlStartTag("qtimetadatafield");
+		$a_xml_writer->xmlElement("fieldlabel", NULL, "QUESTIONTYPE");
+		$a_xml_writer->xmlElement("fieldentry", NULL, CLOZE_TEST_IDENTIFIER);
+		$a_xml_writer->xmlEndTag("qtimetadatafield");
+		$a_xml_writer->xmlStartTag("qtimetadatafield");
+		$a_xml_writer->xmlElement("fieldlabel", NULL, "AUTHOR");
+		$a_xml_writer->xmlElement("fieldentry", NULL, $this->object->getAuthor());
+		$a_xml_writer->xmlEndTag("qtimetadatafield");
+		$a_xml_writer->xmlStartTag("qtimetadatafield");
+		$a_xml_writer->xmlElement("fieldlabel", NULL, "textgaprating");
+		$a_xml_writer->xmlElement("fieldentry", NULL, $this->object->getTextgapRating());
+		$a_xml_writer->xmlEndTag("qtimetadatafield");
+		$a_xml_writer->xmlStartTag("qtimetadatafield");
+		$a_xml_writer->xmlElement("fieldlabel", NULL, "fixedTextLength");
+		$a_xml_writer->xmlElement("fieldentry", NULL, $this->object->getFixedTextLength());
+		$a_xml_writer->xmlEndTag("qtimetadatafield");
+		$a_xml_writer->xmlEndTag("qtimetadata");
+		$a_xml_writer->xmlEndTag("itemmetadata");
+		
+		// PART I: qti presentation
+		$attrs = array(
+			"label" => $this->object->getTitle()
+		);
+		$a_xml_writer->xmlStartTag("presentation", $attrs);
+		// add flow to presentation
+		$a_xml_writer->xmlStartTag("flow");
+		$text_parts = preg_split("/\[gap.*?\[\/gap\]/", $this->object->getClozeText());
+		// add material with question text to presentation
+		for ($i = 0; $i <= $this->object->getGapCount(); $i++)
+		{
+			// n-th text part
+			if ($i == 0)
+			{
+				$this->object->addQTIMaterial($a_xml_writer, $text_parts[$i]);
+			}
+			else
+			{
+				$this->object->addQTIMaterial($a_xml_writer, $text_parts[$i], TRUE, FALSE);
+			}
+
+			if ($i < $this->object->getGapCount())
+			{
+				// add gap
+				$gap = $this->object->getGap($i);
+				switch ($gap->getType())
+				{
+					case CLOZE_SELECT:
+						// comboboxes
+						$attrs = array(
+							"ident" => "gap_$i",
+							"rcardinality" => "Single"
+						);
+						$a_xml_writer->xmlStartTag("response_str", $attrs);
+						$solution = $this->object->getSuggestedSolution($i);
+						if (count($solution))
+						{
+							if (preg_match("/il_(\d*?)_(\w+)_(\d+)/", $solution["internal_link"], $matches))
+							{
+								$attrs = array(
+									"label" => "suggested_solution"
+								);
+								$a_xml_writer->xmlStartTag("material", $attrs);
+								$intlink = "il_" . IL_INST_ID . "_" . $matches[2] . "_" . $matches[3];
+								if (strcmp($matches[1], "") != 0)
+								{
+									$intlink = $solution["internal_link"];
+								}
+								$a_xml_writer->xmlElement("mattext", NULL, $intlink);
+								$a_xml_writer->xmlEndTag("material");
+							}
+						}
+
+						$attrs = array("shuffle" => ($gap->getShuffle() ? "Yes" : "No"));
+						$a_xml_writer->xmlStartTag("render_choice", $attrs);
+
+						// add answers
+						foreach ($gap->getItems() as $answeritem)
+						{
+							$attrs = array(
+								"ident" => $answeritem->getOrder()
+							);
+							$a_xml_writer->xmlStartTag("response_label", $attrs);
+							$a_xml_writer->xmlStartTag("material");
+							$a_xml_writer->xmlElement("mattext", NULL, $answeritem->getAnswertext());
+							$a_xml_writer->xmlEndTag("material");
+							$a_xml_writer->xmlEndTag("response_label");
+						}
+						$a_xml_writer->xmlEndTag("render_choice");
+						$a_xml_writer->xmlEndTag("response_str");
+						break;
+					case CLOZE_TEXT:
+						// text fields
+						$attrs = array(
+							"ident" => "gap_$i",
+							"rcardinality" => "Single"
+						);
+						$a_xml_writer->xmlStartTag("response_str", $attrs);
+						$solution = $this->object->getSuggestedSolution($i);
+						if (count($solution))
+						{
+							if (preg_match("/il_(\d*?)_(\w+)_(\d+)/", $solution["internal_link"], $matches))
+							{
+								$attrs = array(
+									"label" => "suggested_solution"
+								);
+								$a_xml_writer->xmlStartTag("material", $attrs);
+								$intlink = "il_" . IL_INST_ID . "_" . $matches[2] . "_" . $matches[3];
+								if (strcmp($matches[1], "") != 0)
+								{
+									$intlink = $solution["internal_link"];
+								}
+								$a_xml_writer->xmlElement("mattext", NULL, $intlink);
+								$a_xml_writer->xmlEndTag("material");
+							}
+						}
+						$attrs = array(
+							"fibtype" => "String",
+							"prompt" => "Box",
+							"columns" => $gap->getMaxWidth()
+						);
+						$a_xml_writer->xmlStartTag("render_fib", $attrs);
+						$a_xml_writer->xmlEndTag("render_fib");
+						$a_xml_writer->xmlEndTag("response_str");
+						break;
+					case CLOZE_NUMERIC:
+						// numeric fields
+						$attrs = array(
+							"ident" => "gap_$i",
+							"numtype" => "Decimal",
+							"rcardinality" => "Single"
+						);
+						$a_xml_writer->xmlStartTag("response_num", $attrs);
+						$solution = $this->object->getSuggestedSolution($i);
+						if (count($solution))
+						{
+							if (preg_match("/il_(\d*?)_(\w+)_(\d+)/", $solution["internal_link"], $matches))
+							{
+								$attrs = array(
+									"label" => "suggested_solution"
+								);
+								$a_xml_writer->xmlStartTag("material", $attrs);
+								$intlink = "il_" . IL_INST_ID . "_" . $matches[2] . "_" . $matches[3];
+								if (strcmp($matches[1], "") != 0)
+								{
+									$intlink = $solution["internal_link"];
+								}
+								$a_xml_writer->xmlElement("mattext", NULL, $intlink);
+								$a_xml_writer->xmlEndTag("material");
+							}
+						}
+						$answeritem = $gap->getItem(0);
+						$attrs = array(
+							"fibtype" => "Decimal",
+							"prompt" => "Box",
+							"columns" => $gap->getMaxWidth()
+						);
+						if (is_object($answeritem))
+						{
+							if ($eval->e($answeritem->getLowerBound()) !== FALSE)
+							{
+								$attrs["minnumber"] = $answeritem->getLowerBound();
+							}
+							if ($eval->e($answeritem->getUpperBound()) !== FALSE)
+							{
+								$attrs["maxnumber"] = $answeritem->getUpperBound();
+							}
+						}
+						$a_xml_writer->xmlStartTag("render_fib", $attrs);
+						$a_xml_writer->xmlEndTag("render_fib");
+						$a_xml_writer->xmlEndTag("response_num");
+						break;
+				}
+			}
+		}
+		$a_xml_writer->xmlEndTag("flow");
+		$a_xml_writer->xmlEndTag("presentation");
+
+		// PART II: qti resprocessing
+		$a_xml_writer->xmlStartTag("resprocessing");
+		$a_xml_writer->xmlStartTag("outcomes");
+		$a_xml_writer->xmlStartTag("decvar");
+		$a_xml_writer->xmlEndTag("decvar");
+		$a_xml_writer->xmlEndTag("outcomes");
+
+		// add response conditions
+		for ($i = 0; $i < $this->object->getGapCount(); $i++)
+		{
+			$gap = $this->object->getGap($i);
+			switch ($gap->getType())
+			{
+				case CLOZE_SELECT:
+					foreach ($gap->getItems() as $answer)
+					{
+						$attrs = array(
+							"continue" => "Yes"
+						);
+						$a_xml_writer->xmlStartTag("respcondition", $attrs);
+						// qti conditionvar
+						$a_xml_writer->xmlStartTag("conditionvar");
+
+						$attrs = array(
+							"respident" => "gap_$i"
+						);
+						$a_xml_writer->xmlElement("varequal", $attrs, $answer->getAnswertext());
+						$a_xml_writer->xmlEndTag("conditionvar");
+						// qti setvar
+						$attrs = array(
+							"action" => "Add"
+						);
+						$a_xml_writer->xmlElement("setvar", $attrs, $answer->getPoints());
+						// qti displayfeedback
+						$linkrefid = "";
+						$linkrefid = "$i" . "_Response_" . $answer->getOrder();
+						$attrs = array(
+							"feedbacktype" => "Response",
+							"linkrefid" => $linkrefid
+						);
+						$a_xml_writer->xmlElement("displayfeedback", $attrs);
+						$a_xml_writer->xmlEndTag("respcondition");
+					}
+					break;
+				case CLOZE_TEXT:
+					foreach ($gap->getItems() as $answer)
+					{
+						$attrs = array(
+							"continue" => "Yes"
+						);
+						$a_xml_writer->xmlStartTag("respcondition", $attrs);
+						// qti conditionvar
+						$a_xml_writer->xmlStartTag("conditionvar");
+						$attrs = array(
+							"respident" => "gap_$i"
+						);
+						$a_xml_writer->xmlElement("varequal", $attrs, $answer->getAnswertext());
+						$a_xml_writer->xmlEndTag("conditionvar");
+						// qti setvar
+						$attrs = array(
+							"action" => "Add"
+						);
+						$a_xml_writer->xmlElement("setvar", $attrs, $answer->getPoints());
+						// qti displayfeedback
+						$linkrefid = "$i" . "_Response_" . $answer->getOrder();
+						$attrs = array(
+							"feedbacktype" => "Response",
+							"linkrefid" => $linkrefid
+						);
+						$a_xml_writer->xmlElement("displayfeedback", $attrs);
+						$a_xml_writer->xmlEndTag("respcondition");
+					}
+					break;
+				case CLOZE_NUMERIC:
+					foreach ($gap->getItems() as $answer)
+					{
+						$attrs = array(
+							"continue" => "Yes"
+						);
+						$a_xml_writer->xmlStartTag("respcondition", $attrs);
+						// qti conditionvar
+						$a_xml_writer->xmlStartTag("conditionvar");
+						$attrs = array(
+							"respident" => "gap_$i"
+						);
+						$a_xml_writer->xmlElement("varequal", $attrs, $answer->getAnswertext());
+						$a_xml_writer->xmlEndTag("conditionvar");
+						// qti setvar
+						$attrs = array(
+							"action" => "Add"
+						);
+						$a_xml_writer->xmlElement("setvar", $attrs, $answer->getPoints());
+						// qti displayfeedback
+						$linkrefid = "$i" . "_Response_" . $answer->getOrder();
+						$attrs = array(
+							"feedbacktype" => "Response",
+							"linkrefid" => $linkrefid
+						);
+						$a_xml_writer->xmlElement("displayfeedback", $attrs);
+						$a_xml_writer->xmlEndTag("respcondition");
+					}
+					break;
+			}
+		}
+		$a_xml_writer->xmlEndTag("resprocessing");
+
+		// PART III: qti itemfeedback
+		for ($i = 0; $i < $this->object->getGapCount(); $i++)
+		{
+			$gap = $this->object->getGap($i);
+			switch ($gap->getType())
+			{
+				case CLOZE_SELECT:
+					foreach ($gap->getItems() as $answer)
+					{
+						$linkrefid = "$i" . "_Response_" . $answer->getOrder();
+						$attrs = array(
+							"ident" => $linkrefid,
+							"view" => "All"
+						);
+						$a_xml_writer->xmlStartTag("itemfeedback", $attrs);
+						// qti flow_mat
+						$a_xml_writer->xmlStartTag("flow_mat");
+						$a_xml_writer->xmlStartTag("material");
+						$a_xml_writer->xmlElement("mattext");
+						$a_xml_writer->xmlEndTag("material");
+						$a_xml_writer->xmlEndTag("flow_mat");
+						$a_xml_writer->xmlEndTag("itemfeedback");
+					}
+					break;
+				case CLOZE_TEXT:
+					foreach ($gap->getItems() as $answer)
+					{
+						$linkrefid = "$i" . "_Response_" . $answer->getOrder();
+						$attrs = array(
+							"ident" => $linkrefid,
+							"view" => "All"
+						);
+						$a_xml_writer->xmlStartTag("itemfeedback", $attrs);
+						// qti flow_mat
+						$a_xml_writer->xmlStartTag("flow_mat");
+						$a_xml_writer->xmlStartTag("material");
+						$a_xml_writer->xmlElement("mattext");
+						$a_xml_writer->xmlEndTag("material");
+						$a_xml_writer->xmlEndTag("flow_mat");
+						$a_xml_writer->xmlEndTag("itemfeedback");
+					}
+					break;
+				case CLOZE_NUMERIC:
+					foreach ($gap->getItems() as $answer)
+					{
+						$linkrefid = "$i" . "_Response_" . $answer->getOrder();
+						$attrs = array(
+							"ident" => $linkrefid,
+							"view" => "All"
+						);
+						$a_xml_writer->xmlStartTag("itemfeedback", $attrs);
+						// qti flow_mat
+						$a_xml_writer->xmlStartTag("flow_mat");
+						$a_xml_writer->xmlStartTag("material");
+						$a_xml_writer->xmlElement("mattext");
+						$a_xml_writer->xmlEndTag("material");
+						$a_xml_writer->xmlEndTag("flow_mat");
+						$a_xml_writer->xmlEndTag("itemfeedback");
+					}
+					break;
+			}
+		}
+		
+		$a_xml_writer->xmlEndTag("item");
+		$a_xml_writer->xmlEndTag("questestinterop");
+
+		$xml = $a_xml_writer->xmlDumpMem(FALSE);
+		if (!$a_include_header)
+		{
+			$pos = strpos($xml, "?>");
+			$xml = substr($xml, $pos + 2);
+		}
+		return $xml;
+	}
+}
+
+?>
---
+++ Modules/TestQuestionPool/classes/export/qti12/class.assMatchingQuestionExport.php
@@ -0,0 +1,318 @@
+<?php
+ /*
+   +----------------------------------------------------------------------------+
+   | ILIAS open source                                                          |
+   +----------------------------------------------------------------------------+
+   | Copyright (c) 1998-2001 ILIAS open source, University of Cologne           |
+   |                                                                            |
+   | This program is free software; you can redistribute it and/or              |
+   | modify it under the terms of the GNU General Public License                |
+   | as published by the Free Software Foundation; either version 2             |
+   | of the License, or (at your option) any later version.                     |
+   |                                                                            |
+   | This program is distributed in the hope that it will be useful,            |
+   | but WITHOUT ANY WARRANTY; without even the implied warranty of             |
+   | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              |
+   | GNU General Public License for more details.                               |
+   |                                                                            |
+   | You should have received a copy of the GNU General Public License          |
+   | along with this program; if not, write to the Free Software                |
+   | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. |
+   +----------------------------------------------------------------------------+
+*/
+
+include_once "./Modules/TestQuestionPool/classes/export/qti12/class.assQuestionExport.php";
+
+/**
+* Class for matching question exports
+*
+* assMatchingQuestionExport is a class for matching question exports
+*
+* @author		Helmut Schottmüller <helmut.schottmueller@mac.com>
+* @version	$Id$
+* @ingroup ModulesTestQuestionPool
+*/
+class assMatchingQuestionExport extends assQuestionExport
+{
+	/**
+	* Returns a QTI xml representation of the question
+	*
+	* Returns a QTI xml representation of the question and sets the internal
+	* domxml variable with the DOM XML representation of the QTI xml representation
+	*
+	* @return string The QTI xml representation of the question
+	* @access public
+	*/
+	function toXML($a_include_header = true, $a_include_binary = true, $a_shuffle = false, $test_output = false, $force_image_references = false)
+	{
+		global $ilias;
+		
+		include_once("./classes/class.ilXmlWriter.php");
+		$a_xml_writer = new ilXmlWriter;
+		// set xml header
+		$a_xml_writer->xmlHeader();
+		$a_xml_writer->xmlStartTag("questestinterop");
+		$attrs = array(
+			"ident" => "il_".IL_INST_ID."_qst_".$this->object->getId(),
+			"title" => $this->object->getTitle()
+		);
+		$a_xml_writer->xmlStartTag("item", $attrs);
+		// add question description
+		$a_xml_writer->xmlElement("qticomment", NULL, $this->object->getComment());
+		// add estimated working time
+		$workingtime = $this->object->getEstimatedWorkingTime();
+		$duration = sprintf("P0Y0M0DT%dH%dM%dS", $workingtime["h"], $workingtime["m"], $workingtime["s"]);
+		$a_xml_writer->xmlElement("duration", NULL, $duration);
+		// add ILIAS specific metadata
+		$a_xml_writer->xmlStartTag("itemmetadata");
+		$a_xml_writer->xmlStartTag("qtimetadata");
+		$a_xml_writer->xmlStartTag("qtimetadatafield");
+		$a_xml_writer->xmlElement("fieldlabel", NULL, "ILIAS_VERSION");
+		$a_xml_writer->xmlElement("fieldentry", NULL, $ilias->getSetting("ilias_version"));
+		$a_xml_writer->xmlEndTag("qtimetadatafield");
+		$a_xml_writer->xmlStartTag("qtimetadatafield");
+		$a_xml_writer->xmlElement("fieldlabel", NULL, "QUESTIONTYPE");
+		$a_xml_writer->xmlElement("fieldentry", NULL, MATCHING_QUESTION_IDENTIFIER);
+		$a_xml_writer->xmlEndTag("qtimetadatafield");
+		$a_xml_writer->xmlStartTag("qtimetadatafield");
+		$a_xml_writer->xmlElement("fieldlabel", NULL, "AUTHOR");
+		$a_xml_writer->xmlElement("fieldentry", NULL, $this->object->getAuthor());
+		$a_xml_writer->xmlEndTag("qtimetadatafield");
+		$a_xml_writer->xmlStartTag("qtimetadatafield");
+		$a_xml_writer->xmlElement("fieldlabel", NULL, "shuffle");
+		$a_xml_writer->xmlElement("fieldentry", NULL, $this->object->getShuffle());
+		$a_xml_writer->xmlEndTag("qtimetadatafield");
+		$a_xml_writer->xmlEndTag("qtimetadata");
+		$a_xml_writer->xmlEndTag("itemmetadata");
+
+		// PART I: qti presentation
+		$attrs = array(
+			"label" => $this->object->getTitle()
+		);
+		$a_xml_writer->xmlStartTag("presentation", $attrs);
+		// add flow to presentation
+		$a_xml_writer->xmlStartTag("flow");
+		// add material with question text to presentation
+		$this->object->addQTIMaterial($a_xml_writer, $this->object->getQuestion());
+		// add answers to presentation
+		$attrs = array();
+		if ($this->object->getMatchingType() == MT_TERMS_PICTURES)
+		{
+			$attrs = array(
+				"ident" => "MQP",
+				"rcardinality" => "Multiple"
+			);
+		}
+		else
+		{
+			$attrs = array(
+				"ident" => "MQT",
+				"rcardinality" => "Multiple"
+			);
+		}
+		if ($this->object->getOutputType() == OUTPUT_JAVASCRIPT)
+		{
+			$attrs["output"] = "javascript";
+		}
+		$a_xml_writer->xmlStartTag("response_grp", $attrs);
+		$solution = $this->object->getSuggestedSolution(0);
+		if (count($solution))
+		{
+			if (preg_match("/il_(\d*?)_(\w+)_(\d+)/", $solution["internal_link"], $matches))
+			{
+				$a_xml_writer->xmlStartTag("material");
+				$intlink = "il_" . IL_INST_ID . "_" . $matches[2] . "_" . $matches[3];
+				if (strcmp($matches[1], "") != 0)
+				{
+					$intlink = $solution["internal_link"];
+				}
+				$attrs = array(
+					"label" => "suggested_solution"
+				);
+				$a_xml_writer->xmlElement("mattext", $attrs, $intlink);
+				$a_xml_writer->xmlEndTag("material");
+			}
+		}
+		// shuffle output
+		$attrs = array();
+		if ($this->object->getShuffle())
+		{
+			$attrs = array(
+				"shuffle" => "Yes"
+			);
+		}
+		else
+		{
+			$attrs = array(
+				"shuffle" => "No"
+			);
+		}
+		$a_xml_writer->xmlStartTag("render_choice", $attrs);
+		// add answertext
+		$matchingtext_orders = array();
+		foreach ($this->object->getMatchingPairs() as $index => $matchingpair)
+		{
+			array_push($matchingtext_orders, $matchingpair->getTermId());
+		}
+
+		// shuffle it
+		$pkeys = array_keys($this->object->getMatchingPairs());
+		if ($this->object->getShuffle() && $a_shuffle)
+		{
+			$pkeys = $this->object->pcArrayShuffle($pkeys);
+		}
+		// add answers
+		foreach ($pkeys as $index)
+		{
+			$matchingpair = $this->object->getMatchingPair($index);
+			$attrs = array(
+				"ident" => $matchingpair->getDefinitionId(),
+				"match_max" => "1",
+				"match_group" => join($matchingtext_orders, ",")
+			);
+			$a_xml_writer->xmlStartTag("response_label", $attrs);
+			if ($this->object->getMatchingType() == MT_TERMS_PICTURES)
+			{
+				$a_xml_writer->xmlStartTag("material");
+				if ($force_image_references)
+				{
+					$attrs = array(
+						"imagtype" => "image/jpeg",
+						"label" => $matchingpair->getPicture(),
+						"uri" => $this->object->getImagePathWeb() . $matchingpair->getPicture()
+					);
+					$a_xml_writer->xmlElement("matimage", $attrs);
+				}
+				else
+				{
+					$imagepath = $this->object->getImagePath() . $matchingpair->getPicture();
+					$fh = @fopen($imagepath, "rb");
+					if ($fh != false)
+					{
+						$imagefile = fread($fh, filesize($imagepath));
+						fclose($fh);
+						$base64 = base64_encode($imagefile);
+						$attrs = array(
+							"imagtype" => "image/jpeg",
+							"label" => $matchingpair->getPicture(),
+							"embedded" => "base64"
+						);
+						$a_xml_writer->xmlElement("matimage", $attrs, $base64, FALSE, FALSE);
+					}
+				}
+				$a_xml_writer->xmlEndTag("material");
+			}
+			else
+			{
+				$a_xml_writer->xmlStartTag("material");
+				$this->object->addQTIMaterial($a_xml_writer, $matchingpair->getDefinition(), TRUE, FALSE);
+				$a_xml_writer->xmlEndTag("material");
+			}
+			$a_xml_writer->xmlEndTag("response_label");
+		}
+		// shuffle again to get another order for the terms or pictures
+		if ($this->object->getShuffle() && $a_shuffle)
+		{
+			$pkeys = $this->object->pcArrayShuffle($pkeys);
+		}
+		// add matchingtext
+		foreach ($pkeys as $index)
+		{
+			$matchingpair = $this->object->getMatchingPair($index);
+			$attrs = array(
+				"ident" => $matchingpair->getTermId()
+			);
+			$a_xml_writer->xmlStartTag("response_label", $attrs);
+			$a_xml_writer->xmlStartTag("material");
+			$attrs = array(
+				"texttype" => "text/plain"
+			);
+			if ($this->object->isHTML($matchingpair->getTerm()))
+			{
+				$attrs["texttype"] = "text/xhtml";
+			}
+			$a_xml_writer->xmlElement("mattext", $attrs, $matchingpair->getTerm());
+			$a_xml_writer->xmlEndTag("material");
+			$a_xml_writer->xmlEndTag("response_label");
+		}
+		$a_xml_writer->xmlEndTag("render_choice");
+		$a_xml_writer->xmlEndTag("response_grp");
+		$a_xml_writer->xmlEndTag("flow");
+		$a_xml_writer->xmlEndTag("presentation");
+
+		// PART II: qti resprocessing
+		$a_xml_writer->xmlStartTag("resprocessing");
+		$a_xml_writer->xmlStartTag("outcomes");
+		$a_xml_writer->xmlStartTag("decvar");
+		$a_xml_writer->xmlEndTag("decvar");
+		$a_xml_writer->xmlEndTag("outcomes");
+		// add response conditions
+		foreach ($this->object->getMatchingPairs() as $index => $matchingpair)
+		{
+			$attrs = array(
+				"continue" => "Yes"
+			);
+			$a_xml_writer->xmlStartTag("respcondition", $attrs);
+			// qti conditionvar
+			$a_xml_writer->xmlStartTag("conditionvar");
+			$attrs = array();
+			if ($this->object->getMatchingType() == MT_TERMS_PICTURES)
+			{
+				$attrs = array(
+					"respident" => "MQP"
+				);
+			}
+				else
+			{
+				$attrs = array(
+					"respident" => "MQT"
+				);
+			}
+			$a_xml_writer->xmlElement("varsubset", $attrs, $matchingpair->getTermId() . "," . $matchingpair->getDefinitionId());
+			$a_xml_writer->xmlEndTag("conditionvar");
+
+			// qti setvar
+			$attrs = array(
+				"action" => "Add"
+			);
+			$a_xml_writer->xmlElement("setvar", $attrs, $matchingpair->getPoints());
+			// qti displayfeedback
+			$attrs = array(
+				"feedbacktype" => "Response",
+				"linkrefid" => "correct_" . $matchingpair->getTermId() . "_" . $matchingpair->getDefinitionId()
+			);
+			$a_xml_writer->xmlElement("displayfeedback", $attrs);
+			$a_xml_writer->xmlEndTag("respcondition");
+		}
+		$a_xml_writer->xmlEndTag("resprocessing");
+
+		// PART III: qti itemfeedback
+		foreach ($this->object->getMatchingPairs() as $index => $matchingpair)
+		{
+			$attrs = array(
+				"ident" => "correct_" . $matchingpair->getTermId() . "_" . $matchingpair->getDefinitionId(),
+				"view" => "All"
+			);
+			$a_xml_writer->xmlStartTag("itemfeedback", $attrs);
+			// qti flow_mat
+			$a_xml_writer->xmlStartTag("flow_mat");
+			$a_xml_writer->xmlStartTag("material");
+			$a_xml_writer->xmlElement("mattext");
+			$a_xml_writer->xmlEndTag("material");
+			$a_xml_writer->xmlEndTag("flow_mat");
+			$a_xml_writer->xmlEndTag("itemfeedback");
+		}
+		$a_xml_writer->xmlEndTag("item");
+		$a_xml_writer->xmlEndTag("questestinterop");
+
+		$xml = $a_xml_writer->xmlDumpMem(FALSE);
+		if (!$a_include_header)
+		{
+			$pos = strpos($xml, "?>");
+			$xml = substr($xml, $pos + 2);
+		}
+		return $xml;
+	}
+}
+
+?>
