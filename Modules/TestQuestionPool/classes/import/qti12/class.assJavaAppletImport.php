b179adb791c650b4577254943e96fc4f986ff441
hschottm <hschottm@svn.ilias.de>
2007-05-11 21:26:27 +0000

    Added question import classes
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@13872 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ Modules/TestQuestionPool/classes/import/qti12/class.assJavaAppletImport.php
@@ -0,0 +1,197 @@
+<?php
+ /*
+   +----------------------------------------------------------------------------+
+   | ILIAS open source                                                          |
+   +----------------------------------------------------------------------------+
+   | Copyright (c) 1998-2001 ILIAS open source, University of Cologne           |
+   |                                                                            |
+   | This program is free software; you can redistribute it and/or              |
+   | modify it under the terms of the GNU General Public License                |
+   | as published by the Free Software Foundation; either version 2             |
+   | of the License, or (at your option) any later version.                     |
+   |                                                                            |
+   | This program is distributed in the hope that it will be useful,            |
+   | but WITHOUT ANY WARRANTY; without even the implied warranty of             |
+   | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              |
+   | GNU General Public License for more details.                               |
+   |                                                                            |
+   | You should have received a copy of the GNU General Public License          |
+   | along with this program; if not, write to the Free Software                |
+   | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. |
+   +----------------------------------------------------------------------------+
+*/
+
+include_once "./Modules/TestQuestionPool/classes/import/qti12/class.assQuestionImport.php";
+
+/**
+* Class for java applet question imports
+*
+* assJavaAppletImport is a class for java applet question imports
+*
+* @author		Helmut Schottmüller <helmut.schottmueller@mac.com>
+* @version	$Id$
+* @ingroup ModulesTestQuestionPool
+*/
+class assJavaAppletImport extends assQuestionImport
+{
+	/**
+	* Creates a question from a QTI file
+	*
+	* Receives parameters from a QTI parser and creates a valid ILIAS question object
+	*
+	* @param object $item The QTI item object
+	* @param integer $questionpool_id The id of the parent questionpool
+	* @param integer $tst_id The id of the parent test if the question is part of a test
+	* @param object $tst_object A reference to the parent test object
+	* @param integer $question_counter A reference to a question counter to count the questions of an imported question pool
+	* @param array $import_mapping An array containing references to included ILIAS objects
+	* @access public
+	*/
+	function fromXML(&$item, $questionpool_id, &$tst_id, &$tst_object, &$question_counter, &$import_mapping)
+	{
+		global $ilUser;
+
+		// empty session variable for imported xhtml mobs
+		unset($_SESSION["import_mob_xhtml"]);
+		$presentation = $item->getPresentation(); 
+		$duration = $item->getDuration();
+		$now = getdate();
+		$applet = NULL;
+		$maxpoints = 0;
+		$javacode = "";
+		$javacodebase = "";
+		$javaarchive = "";
+		$params = array();
+		$created = sprintf("%04d%02d%02d%02d%02d%02d", $now['year'], $now['mon'], $now['mday'], $now['hours'], $now['minutes'], $now['seconds']);
+		$answers = array();
+		foreach ($presentation->order as $entry)
+		{
+			switch ($entry["type"])
+			{
+				case "material":
+					$material = $presentation->material[$entry["index"]];
+					for ($i = 0; $i < $material->getMaterialCount(); $i++)
+					{
+						$mat = $material->getMaterial($i);
+						if (strcmp($mat["type"], "mattext") == 0)
+						{
+							$mattext = $mat["material"];
+							if ((strlen($mattext->getLabel()) == 0) && (strlen($this->object->QTIMaterialToString($item->getQuestiontext())) == 0))
+							{
+								$item->setQuestiontext($mattext->getContent());
+							}
+							if (strcmp($mattext->getLabel(), "points") == 0)
+							{
+								$maxpoints = $mattext->getContent();
+							}
+							else if (strcmp($mattext->getLabel(), "java_code") == 0)
+							{
+								$javacode = $mattext->getContent();
+							}
+							else if (strcmp($mattext->getLabel(), "java_codebase") == 0)
+							{
+								$javacodebase = $mattext->getContent();
+							}
+							else if (strcmp($mattext->getLabel(), "java_archive") == 0)
+							{
+								$javaarchive = $mattext->getContent();
+							}
+							else if (strlen($mattext->getLabel()) > 0)
+							{
+								array_push($params, array("key" => $mattext->getLabel(), "value" => $mattext->getContent()));
+							}
+						}
+						elseif (strcmp($mat["type"], "matapplet") == 0)
+						{
+							$applet = $mat["material"];
+						}
+					}
+					break;
+			}
+		}
+
+		$this->object->setTitle($item->getTitle());
+		$this->object->setComment($item->getComment());
+		$this->object->setAuthor($item->getAuthor());
+		$this->object->setOwner($ilUser->getId());
+		$this->object->setQuestion($this->object->QTIMaterialToString($item->getQuestiontext()));
+		$this->object->setObjId($questionpool_id);
+		$this->object->setEstimatedWorkingTime($duration["h"], $duration["m"], $duration["s"]);
+		$this->object->setJavaAppletFilename($applet->getUri());
+		$this->object->setJavaWidth($applet->getWidth());
+		$this->object->setJavaHeight($applet->getHeight());
+		$this->object->setJavaCode($javacode);
+		$this->object->setJavaCodebase($javacodebase);
+		$this->object->setJavaArchive($javaarchive);
+		$this->object->setPoints($maxpoints);
+		foreach ($params as $pair)
+		{
+			$this->object->addParameter($pair["key"], $pair["value"]);
+		}
+		$this->object->saveToDb();
+		if (count($item->suggested_solutions))
+		{
+			foreach ($item->suggested_solutions as $suggested_solution)
+			{
+				$this->object->setSuggestedSolution($suggested_solution["solution"]->getContent(), $suggested_solution["gap_index"], true);
+			}
+			$this->object->saveToDb();
+		}
+		$javaapplet =& base64_decode($applet->getContent());
+		$javapath = $this->object->getJavaPath();
+		if (!file_exists($javapath))
+		{
+			include_once "./Services/Utilities/classes/class.ilUtil.php";
+			ilUtil::makeDirParents($javapath);
+		}
+		$javapath .=  $this->object->getJavaAppletFilename();
+		$fh = fopen($javapath, "wb");
+		if ($fh == false)
+		{
+//									global $ilErr;
+//									$ilErr->raiseError($this->object->lng->txt("error_save_image_file") . ": $php_errormsg", $ilErr->MESSAGE);
+//									return;
+		}
+		else
+		{
+			$javafile = fwrite($fh, $javaapplet);
+			fclose($fh);
+		}
+		// handle the import of media objects in XHTML code
+		if (is_array($_SESSION["import_mob_xhtml"]))
+		{
+			include_once "./Services/MediaObjects/classes/class.ilObjMediaObject.php";
+			include_once "./Services/RTE/classes/class.ilRTE.php";
+			foreach ($_SESSION["import_mob_xhtml"] as $mob)
+			{
+				if ($tst_id > 0)
+				{
+					include_once "./Modules/Test/classes/class.ilObjTest.php";
+					$importfile = ilObjTest::_getImportDirectory() . "/" . $_SESSION["tst_import_subdir"] . "/" . $mob["uri"];
+				}
+				else
+				{
+					include_once "./Modules/TestQuestionPool/classes/class.ilObjQuestionPool.php";
+					$importfile = ilObjQuestionPool::_getImportDirectory() . "/" . $_SESSION["qpl_import_subdir"] . "/" . $mob["uri"];
+				}
+				$media_object =& ilObjMediaObject::_saveTempFileAsMediaObject(basename($importfile), $importfile, FALSE);
+				ilObjMediaObject::_saveUsage($media_object->getId(), "qpl:html", $this->object->getId());
+				$this->object->setQuestion(ilRTE::_replaceMediaObjectImageSrc(str_replace("src=\"" . $mob["mob"] . "\"", "src=\"" . "il_" . IL_INST_ID . "_mob_" . $media_object->getId() . "\"", $this->object->getQuestion()), 1));
+			}
+			$this->object->saveToDb();
+		}
+		if ($tst_id > 0)
+		{
+			$q_1_id = $this->object->getId();
+			$question_id = $this->object->duplicate(true);
+			$tst_object->questions[$question_counter++] = $question_id;
+			$import_mapping[$item->getIdent()] = array("pool" => $q_1_id, "test" => $question_id);
+		}
+		else
+		{
+			$import_mapping[$item->getIdent()] = array("pool" => $this->object->getId(), "test" => 0);
+		}
+	}
+}
+
+?>
