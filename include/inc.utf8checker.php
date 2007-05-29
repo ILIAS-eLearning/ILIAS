ddae9fb37529b29c682f88a88901eaea401992cd
hholtmann <hholtmann>
2007-05-29 15:17:18 +0000

    Changed utf8checker from preg_match to iconv-implementation, fix for apache-crash when importing some SCORM courses
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@13997 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- include/inc.utf8checker.php
+++ include/inc.utf8checker.php
@@ -26,38 +26,13 @@
 *
 * @package ilias-develop
 *
-* There is no way in the standard PHP 4.x installation to check wether a file is in UTF8 or not.
-* This code is grabbed from the comments at http://www.phpbuilder.com/lists/php-i18n/2003051/0020.php
-* posted by Cestmir Hybl.
+* requires PHP 5 and iconv (which is enabled by default under PHP5)
 * It tries to find out that a string $AStr is in UTF8 or not.
 *
 **/
 
-function isUTF8 ($aStr) {
-	$field = "/^(";
-	$field .="[\x09|\x0A|\x0D]|[\x20-\x7E]|";
-	$field .="[\xC2-\xDF][\x80-\xBF]|";
-	$field .="\xE0[\xA0-\xBF][\x80-\xBF]|";
-	$field .="[\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}|";
-	$field .="\xED[\x80-\x9F][\x80-\xBF]|";
-	$field .="\xF0[\x90-\xBF][\x80-\xBF]{2}|";
-	$field .="[\xF1-\xF3][\x80-\xBF]{3}|";
-	$field .="\xF4[\x80-\x8F][\x80-\xBF]{2}";
-	$field .=")*$/s";
-	return preg_match($field, $AStr);
-}
-
-function seems_not_utf8($AStr)
-{
-
-	$ptrASCII = '[\x00-\x7F]';
-	$ptr2Octet = '[\xC2-\xDF][\x80-\xBF]';
-	$ptr3Octet = '[\xE0-\xEF][\x80-\xBF]{2}';
-	$ptr4Octet = '[\xF0-\xF4][\x80-\xBF]{3}';
-	$ptr5Octet = '[\xF8-\xFB][\x80-\xBF]{4}';
-	$ptr6Octet = '[\xFC-\xFD][\x80-\xBF]{5}';
-
-	if (preg_match("/^($ptrASCII|$ptr2Octet|$ptr3Octet|$ptr4Octet|$ptr5Octet|$ptr6Octet)*$/s", $AStr))
+function isUTF8 ($AStr) {
+	if (@iconv('UTF-8', 'UTF-8',$AStr) == $AStr) 
 	{
 		return true;
 	}
@@ -66,6 +41,20 @@ function seems_not_utf8($AStr)
 		return false;
 	}
 }
+
+
+function seems_not_utf8($AStr)
+{
+	if (!@iconv('UTF-8', 'UTF-8',$AStr) == $AStr) 
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+ }
+
 
 function getUTF8String($aStr) {
 	if (!isUTF8($aStr)) {	
