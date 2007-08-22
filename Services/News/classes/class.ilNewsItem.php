e4e70c28a9b30df1a1f8c5d1f302f1164bd7f8d1
Alexander Killing <killing@leifos.de>
2007-08-22 10:17:41 +0000

    fixed rss feeds
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@14565 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/News/classes/class.ilNewsItem.php
+++ Services/News/classes/class.ilNewsItem.php
@@ -266,7 +266,10 @@ class ilNewsItem extends ilNewsItemGen
 			$ref_ids[] = $node["child"];
 		}
 		$ilObjDataCache->preloadReferenceCache($ref_ids);
-		$ilAccess->preloadActivationTimes($ref_ids);
+		if (!$a_only_public)
+		{
+			$ilAccess->preloadActivationTimes($ref_ids);
+		}
 		$ilBench->stop("News", "getAggregatedNewsData_getSubTree");
 		
 		// get news for all subtree nodes
