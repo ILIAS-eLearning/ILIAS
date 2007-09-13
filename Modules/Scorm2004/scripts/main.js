99f7bdadbc5d5211b1744c79acf3e69edec2a851
hholtmann <hholtmann@svn.ilias.de>
2007-09-13 16:30:41 +0000

    Navigation Fixes, path fixes for IE
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@14694 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Scorm2004/scripts/main.js
+++ Modules/Scorm2004/scripts/main.js
@@ -1215,7 +1215,7 @@ function updateControls(controlState)
 
 function setResource(id, url, base) 
 {
-	if(!(adlnavreq)) 
+	if(!(adlnavreq) || isIE()) 
 	{
 		url = base + url;
 	}
@@ -1248,6 +1248,11 @@ function setResource(id, url, base)
 	guiItem = all(ITEM_PREFIX + id);
 	if (guiItem)
 	{
+		removeClass(guiItem,"not_attempted",1);
+		removeClass(guiItem,"incomplete",1);
+		removeClass(guiItem,"completed",1);
+		removeClass(guiItem,"failed",1);
+		removeClass(guiItem,"passed",1);
 		addClass(guiItem, "current");
 		addClass(guiItem,"running");
 	}
@@ -1269,7 +1274,7 @@ function removeResource(callback)
 		elm.innerHTML = '<div>'+translate('resource_undelivered')+'</div>';
 	}
 	*/
-	open('about:blank', RESOURCE_NAME);
+	//open('about:blank', RESOURCE_NAME);
 	if (typeof(callback) === 'function') 
 	{
 		callback();
@@ -2232,10 +2237,11 @@ function onItemDeliver(item) // onDeliver called from sequencing process (delive
 	}
 	// deliver resource (sco)
 	scoStartTime = currentTime();
-	setResource(item.id, item.href+"?"+item.parameters, this.config.package_url);
 	// customize GUI
 	updateNav();
 	updateControls();
+	setResource(item.id, item.href+"?"+item.parameters, this.config.package_url);
+
 }
 
 function syncCMIADLTree(){
@@ -2456,7 +2462,7 @@ function onTerminate(data)
 	//	alert(navReq.type, navReq.target);
 
 		if (navReq.type!="suspend") {
-			//adlnavreq=true; 
+			adlnavreq=true; 
 			//TODO fix for Unix
 			if (navReq.type=="Choice") {
 				launchTarget(navReq.target);
@@ -2600,22 +2606,24 @@ function updateNav() {
 			if (node_stat_completion==null || node_stat_completion=="not attempted") {
 				toggleClass(elm,"not_attempted",1);
 			}
-			
-			//just in case-support not required due to spec
-			if (node_stat_completion=="browsed") {
-				toggleClass(elm,"browsed",1);
-			}
-			
+		
 			//incomplete
 			if (node_stat_completion=="unknown" || node_stat_completion=="incomplete") {
 				removeClass(elm,"not_attempted",1);
 				toggleClass(elm,"incomplete",1);	
 			}
 			
+			//just in case-support not required due to spec
+			if (node_stat_completion=="browsed") {
+					removeClass(elm,"not_attempted",1);
+					toggleClass(elm,"browsed",1);
+			}
+			
 			//completed
 			if (node_stat_completion=="completed") {
 				removeClass(elm,"not_attempted",1);
 				removeClass(elm,"incomplete",1);
+				removeClass(elm,"browsed",1);
 				toggleClass(elm,"completed",1);	
 			}
 			
@@ -2645,6 +2653,40 @@ function updateNav() {
 	}
 }
 
+function isIE(versionNumber) {
+var detect = navigator.userAgent.toLowerCase();
+if(!(navigator && navigator.userAgent && navigator.userAgent.toLowerCase)) {
+  	        return false;
+  	    } else {
+  	        if(detect.indexOf('msie') + 1) {
+  	            // browser is internet explorer
+  	            var ver = function() {
+  	                // http://msdn.microsoft.com/workshop/author/dhtml/overview/browserdetection.asp
+  	                // Returns the version of Internet Explorer or a -1
+  	                // (indicating the use of another browser).
+  	                var rv = -1; // Return value assumes failure
+  	                if (navigator.appName == 'Microsoft Internet Explorer') {
+  	                    var ua = navigator.userAgent;
+  	                    var re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+  	                    if (re.exec(ua) != null) {
+  	                        rv = parseFloat( RegExp.$1 );
+  	                    }
+  	                }
+  	                return rv;
+  	            };
+  	            var valid = true;
+  	            // if the version can be found and the version is less than our version number it is invalid
+  	            if ((ver > -1) && (ver < versionNumber)) {
+  	                valid = false;
+  	            }
+  	            return valid;
+  	        } else {
+  	            return false
+  	        }
+  	    }
+}
+
+
 // Server related Variables
 var remoteMapping = null; // mapping of userdata from client to server representation
 var remoteInsertId = 0; // pseudo IDs for newly generated data rows (will be prefixed by "$")
@@ -2661,8 +2703,7 @@ var sharedObjectives = new Object(); // global objectives by objective identifie
 var msequencer=new ADLSequencer();
 var mlaunch=null;
 var adlnavreq=null;
-//var mlaunch = new ADLLaunch();
-
+var treeYUI=null;
 
 // GUI constants
 var ITEM_PREFIX = "itm";
@@ -2700,7 +2741,6 @@ var scoStartTime = null;
 
 //remove later
 var pubAPI=null;
-var treeYUI=null;
 // Public interface
 window.scorm_init = init;
 
