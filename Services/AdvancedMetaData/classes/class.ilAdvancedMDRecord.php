2b8a58a91b889758555ebf031396a6a0dc9274c8
Stefan Meyer <smeyer.ilias@gmx.de>
2007-09-27 10:42:25 +0000

    Advanced meta data (category)
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@14878 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/AdvancedMetaData/classes/class.ilAdvancedMDRecord.php
+++ Services/AdvancedMetaData/classes/class.ilAdvancedMDRecord.php
@@ -112,7 +112,7 @@ class ilAdvancedMDRecord
 	 */
 	public static function _getAssignableObjectTypes()
 	{
-	 	return array('crs');
+	 	return array('crs','cat');
 	 	return array('crs','rcrs');
 	}
 	
