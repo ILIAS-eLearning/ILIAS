29b1dcf7b71b25fdccb1836640c57872c1df3999
rkuester <rkuester@svn.ilias.de>
2007-12-11 19:13:00 +0000

    - added missing ilFileUtilsException - refactored cloneAllObject from ilContainerGUI to ilContainer
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@15541 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ Services/Utilities/classes/class.ilFileUtilsException.php
@@ -0,0 +1,28 @@
+<?php
+/**
+*
+* Class to report exception
+*
+*
+*/
+
+include_once 'Services/Exceptions/classes/class.ilException.php';
+
+class ilFileUtilsException extends ilException
+{
+    public static $BROKEN_FILE = 0;
+    public static $INFECTED_FILE = 1;
+    public static $DOUBLETTES_FOUND = 2;
+    /**
+	 * A message isn't optional as in build in class Exception
+	 *
+	 * @access public
+	 *
+	 */
+	public function __construct($a_message,$a_code = 0)
+	{
+	 	parent::__construct($a_message,$a_code);
+	}
+}
+
+?>
