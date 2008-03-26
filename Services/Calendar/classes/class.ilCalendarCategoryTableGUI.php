07c07adf53687f9eb68ab1439f1209dd4e3a3519
Stefan Meyer <smeyer.ilias@gmx.de>
2008-03-26 17:27:37 +0000

    Minor bugfix
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@16294 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Calendar/classes/class.ilCalendarCategoryTableGUI.php
+++ Services/Calendar/classes/class.ilCalendarCategoryTableGUI.php
@@ -50,8 +50,8 @@ class ilCalendarCategoryTableGUI extends ilTable2GUI
 	 	$this->ctrl = $ilCtrl;
 	 	
 		parent::__construct($a_parent_obj,'showCategories');
-	 	$this->addColumn('','f',"1px");
-	 	$this->addColumn($this->lng->txt('title'),'title',"99%");
+	 	$this->addColumn('','f',"1");
+	 	$this->addColumn($this->lng->txt('title'),'title',"100%");
 	 	
 		$this->setFormAction($this->ctrl->getFormAction($a_parent_obj));
 		$this->setRowTemplate("tpl.show_category_row.html","Services/Calendar");
