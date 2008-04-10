56cfd60cff9adc1f7389057844f9fed734ae17cb
Stefan Meyer <smeyer.ilias@gmx.de>
2008-04-10 10:30:55 +0000

    Merged ldap, radius role assignent fixes.
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@16360 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Radius/classes/class.ilAuthRadius.php
+++ Services/Radius/classes/class.ilAuthRadius.php
@@ -107,7 +107,7 @@ class ilAuthRadius extends Auth
 					$_SESSION['tmp_auth_mode'] = 'radius';
 					$_SESSION['tmp_external_account'] = $a_username;
 					$_SESSION['tmp_pass'] = $_POST['password'];
-					$_SESSION['tmp_role'] = $this->radius_settings->getDefaultRole();
+					$_SESSION['tmp_roles'] = array(0 => $this->radius_settings->getDefaultRole());
 				
 					ilUtil::redirect('ilias.php?baseClass=ilStartUpGUI&cmd=showAccountMigration');
 				}
