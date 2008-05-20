c956ff3c4f72758a1032d4739fa351afdb422ee9
Stefan Meyer <smeyer.ilias@gmx.de>
2008-05-20 07:59:41 +0000

    Fixed function redeclaration (codepointToUtf8).
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@16659 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Utilities/classes/class.ilMWParserAdapter.php
+++ Services/Utilities/classes/class.ilMWParserAdapter.php
@@ -13,21 +13,24 @@ if (!function_exists("wfProfileOut"))
 	}
 }
 
-// From include/normal/UtfNormalUtil.php
-function codepointToUtf8( $codepoint ) {
-	if($codepoint <		0x80) return chr($codepoint);
-	if($codepoint <    0x800) return chr($codepoint >>	6 & 0x3f | 0xc0) .
-									 chr($codepoint		  & 0x3f | 0x80);
-	if($codepoint <  0x10000) return chr($codepoint >> 12 & 0x0f | 0xe0) .
-									 chr($codepoint >>	6 & 0x3f | 0x80) .
-									 chr($codepoint		  & 0x3f | 0x80);
-	if($codepoint < 0x110000) return chr($codepoint >> 18 & 0x07 | 0xf0) .
-									 chr($codepoint >> 12 & 0x3f | 0x80) .
-									 chr($codepoint >>	6 & 0x3f | 0x80) .
-									 chr($codepoint		  & 0x3f | 0x80);
-
-	echo "Asked for code outside of range ($codepoint)\n";
-	die( -1 );
+if(!function_exists('codepointToUtf8'))
+{
+	// From include/normal/UtfNormalUtil.php
+	function codepointToUtf8( $codepoint ) {
+		if($codepoint <		0x80) return chr($codepoint);
+		if($codepoint <    0x800) return chr($codepoint >>	6 & 0x3f | 0xc0) .
+										 chr($codepoint		  & 0x3f | 0x80);
+		if($codepoint <  0x10000) return chr($codepoint >> 12 & 0x0f | 0xe0) .
+										 chr($codepoint >>	6 & 0x3f | 0x80) .
+										 chr($codepoint		  & 0x3f | 0x80);
+		if($codepoint < 0x110000) return chr($codepoint >> 18 & 0x07 | 0xf0) .
+										 chr($codepoint >> 12 & 0x3f | 0x80) .
+										 chr($codepoint >>	6 & 0x3f | 0x80) .
+										 chr($codepoint		  & 0x3f | 0x80);
+	
+		echo "Asked for code outside of range ($codepoint)\n";
+		die( -1 );
+	}
 }
 
 // From includes/normal/UtfNormal.php
