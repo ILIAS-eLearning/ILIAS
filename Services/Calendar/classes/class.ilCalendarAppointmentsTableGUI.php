c24114e0cd5a1ec16b6ba3b52a7741518bf32d55
Stefan Meyer <smeyer.ilias@gmx.de>
2008-11-04 14:45:51 +0000

    Fixed mantis #0004004: Fullday events are displayed with duration "2 Days" in inbox
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@17823 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Calendar/classes/class.ilCalendarAppointmentsTableGUI.php
+++ Services/Calendar/classes/class.ilCalendarAppointmentsTableGUI.php
@@ -177,8 +177,9 @@ class ilCalendarAppointmentsTableGUI extends ilTable2GUI
 			$tmp_arr['fullday'] = $entry->isFullday();
  			$tmp_arr['begin'] = $entry->getStart()->get(IL_CAL_UNIX);
  			$tmp_arr['end'] = $entry->getEnd()->get(IL_CAL_UNIX);
- 			$tmp_arr['duration'] = ($dur = $tmp_arr['end'] - $tmp_arr['begin']) ? $dur : 60 * 60 * 24;
  			
+ 			#$tmp_arr['duration'] = ($dur = $tmp_arr['end'] - $tmp_arr['begin']) ? $dur : 60 * 60 * 24;
+ 			$tmp_arr['duration'] = $tmp_arr['end'] - $tmp_arr['begin'];
  			if($tmp_arr['fullday'])
  			{
  				$tmp_arr['duration'] += (60 * 60 * 24);
