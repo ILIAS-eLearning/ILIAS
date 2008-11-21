68d5db035a80df0f02ad6e6cd793e1dcb95f8324
Nadia Ahmad <nahmad@databay.de>
2008-11-21 16:02:49 +0000

    Added features for webshop
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@18034 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Payment/classes/class.ilShopPurchaseGUI.php
+++ Services/Payment/classes/class.ilShopPurchaseGUI.php
@@ -59,6 +59,14 @@ class ilShopPurchaseGUI extends ilObjectGUI
 		$this->ref_id = $a_ref_id;
 
 		$this->object =& ilObjectFactory::getInstanceByRefId($this->ref_id);
+		
+		global $ilTabs;
+				
+		$ilTabs->clearTargets();
+		$ilTabs->addTarget('buy', $this->ctrl->getLinkTarget($this, 'showDetails').'&purchasetype=buy');
+		$ilTabs->addTarget('payment_demo', $this->ctrl->getLinkTarget($this, 'showDemoVersion').'&purchasetype=demo');
+
+		$this->ctrl->setParameter($this, 'purchasetype', ($_GET['purchasetype'] == 'demo' ? 'demo' : 'buy'));
 	}
 
 	/**
@@ -79,6 +87,12 @@ class ilShopPurchaseGUI extends ilObjectGUI
 			default:
 				$cmd = $this->ctrl->getCmd();
 				$this->__buildHeader();
+				
+				if(!in_array($cmd, array('showDemoVersion', 'showDetails', 'addToShoppingCart')))
+				{
+					$cmd = ($_GET['purchasetype'] == 'demo' ? 'showDemoVersion' : 'showDetails');
+				}
+								
 				if(!$cmd || !method_exists($this, $cmd))
 				{
 					$cmd = 'showDetails';
@@ -87,16 +101,266 @@ class ilShopPurchaseGUI extends ilObjectGUI
 				break;
 		}
 	}
+	
+	public function showDemoVersion()
+	{
+		global $ilMainMenu, $ilTabs, $tpl;
+		
+		$this->__initPaymentObject();
+		$this->__initPricesObject();
+		$this->__initShoppingCartObject();
+		
+		$this->tpl->addBlockfile('BUTTONS', 'buttons', 'tpl.buttons.html');
+		$this->tpl->setCurrentBlock('btn_cell');
+		$this->tpl->setVariable('BTN_LINK', 'ilias.php?baseClass=ilShopController');
+		$this->tpl->setVariable('BTN_TXT', $this->lng->txt('payment_back_to_shop'));
+		$this->tpl->parseCurrentBlock();
+		
+		
+		$ilTabs->setTabActive('payment_demo');
+		$ilMainMenu->setActive('shop');
+		$this->tpl->addBlockFile('ADM_CONTENT', 'adm_content', 'tpl.shop_abstract_details.html', 'Services/Payment');
+		
+		$this->tpl->setVariable("DETAILS_FORMACTION",$this->ctrl->getFormAction($this));
+		$this->tpl->setVariable("TYPE_IMG",ilUtil::getImagePath('icon_'.$this->object->getType().'_b.gif'));
+		$this->tpl->setVariable("ALT_IMG",$this->lng->txt('obj_'.$this->object->getType()));
+		$this->tpl->setVariable("TITLE",$this->object->getTitle());
+		
+		// abstracts
+		if(($abstract_html = $this->__getAbstractHTML($this->pobject->getPobjectId())) != '')
+		{			
+			$this->tpl->setCurrentBlock('abstract_block');
+			$this->tpl->setVariable('TXT_ABSTRACT', $this->lng->txt('pay_abstract'));
+			$this->tpl->setVariable('ABSTRACT_HTML', $abstract_html.$output);
+			$this->tpl->parseCurrentBlock();
+		}	
+		
+		// public content ilias lm
+		global $ilObjDataCache;
+		if($ilObjDataCache->lookupType($ilObjDataCache->lookupObjId($this->pobject->getRefId())) == 'lm')
+		{			
+			include_once  'Modules/LearningModule/classes/class.ilShopPublicSectionSelector.php';
+			$exp = new ilShopPublicSectionSelector($this->ctrl->getLinkTarget($this, 'layout'),
+				ilObjectFactory::getInstanceByRefId($this->pobject->getRefId()), get_class($this));	
+			$exp->setTargetGet('obj_id');
+			$exp->setOutput(0);
+			$output = $exp->getOutput();
+			
+			if(trim($output) != '')
+			{	
+					
+				$this->tpl->setCurrentBlock('public_content_block');
+				$this->tpl->setVariable('TXT_CONTENT', $this->lng->txt('content'));
+				$this->tpl->setVariable('PUBLIC_CONTENT_HTML', $output);
+				$this->tpl->parseCurrentBlock();
+			}
+		}
+		else if($ilObjDataCache->lookupType($ilObjDataCache->lookupObjId($this->pobject->getRefId())) == 'crs')
+		{
+			$oCourse = ilObjectFactory::getInstanceByRefId($this->pobject->getRefId());
+			$items = $oCourse->getSubItems();
+			$this->__getCourseItemsHTML($items);	
+		}		
+	}
+	
+	private function getItemsByObjType($items, $type_group)
+	{
+		return is_array($items[$type_group]) ? $items[$type_group] : array();
+	}
+	
+	private function __getCourseItemsHTML($container_items)
+	{
+		global $ilUser, $rbacsystem, $objDefinition, $ilSetting, $ilObjDataCache;
+		
+		$output = false;
+		
+		$tpl_sub_items = new ilTemplate('tpl.pay_purchase_demo_list_block.html', true, true, 'Services/Payment');
+								
+		$objtype_groups = $objDefinition->getGroupedRepositoryObjectTypes(
+			array('cat', 'crs', 'grp', 'fold')
+		);
+
+		foreach($objtype_groups as $grp => $grpdata)
+		{
+			$title = $this->lng->txt('objs_'.$grp);
+			$items = $this->getItemsByObjType($container_items, $grp);
+			
+			$item_html = array();
+			$rel_header = 'th_'.$grp;
+
+			if(count($items) > 0)
+			{				
+				foreach($items as $item)
+				{
+					if($item['title'] != '')
+					{
+						$item_html[] = array(
+							'html' => $item['title'], 
+							'item_ref_id' => $item['ref_id'],
+							'item_obj_id' => $item['obj_id']
+						);
+					}
+				}
+				
+				// output block for resource type
+				if(count($item_html) > 0)
+				{
+					$output = true;
+					
+					// add a header for each resource type
+					if($ilSetting->get('icon_position_in_lists') == 'item_rows')
+					{
+						$this->addHeaderRow($tpl_sub_items, $grp, false);
+					}
+					else
+					{
+						$this->addHeaderRow($tpl_sub_items, $grp);
+					}
+					$this->resetRowType();
+					
+					// content row
+					foreach($item_html as $item)
+					{
+						if($ilSetting->get('icon_position_in_lists') == 'item_rows')
+						{
+							// BEGIN WebDAV: Use $item_list_gui to determine icon image type
+							$this->addStandardRow($tpl_sub_items, $item['html'], $item['item_ref_id'], $item['item_obj_id'], 
+								$item['item_icon_image_type'], 
+								$rel_header);
+							// END WebDAV: Use $item_list_gui to determine icon image type
+						}
+						else
+						{
+							$this->addStandardRow($tpl_sub_items, $item['html'], $item['item_ref_id'], $item['item_obj_id'], '', $rel_header);
+						}
+					}
+					
+					
+				}
+			}
+		}
+
+		if($output == true)
+		{
+			$this->tpl->setCurrentBlock('public_content_block');
+			$this->tpl->setVariable('TXT_CONTENT', $this->lng->txt('content'));
+			$this->tpl->setVariable('PUBLIC_CONTENT_HTML', $tpl_sub_items->get());
+			$this->tpl->parseCurrentBlock();
+		}		
+	}
+	
+	private function resetRowType()
+	{
+		$this->cur_row_type = "";
+	}
+	
+/**
+	* adds a header row to a block template
+	*
+	* @param	object		$a_tpl		block template
+	* @param	string		$a_type		object type
+	* @access	private
+	*/
+	private function addHeaderRow($a_tpl, $a_type, $a_show_image = true)
+	{
+		$icon = ilUtil::getImagePath("icon_".$a_type.".gif");
+		$title = $this->lng->txt("objs_".$a_type);
+		$header_id = "th_".$a_type;
+
+		if ($a_show_image)
+		{
+			$a_tpl->setCurrentBlock("container_header_row_image");
+			$a_tpl->setVariable("HEADER_IMG", $icon);
+			$a_tpl->setVariable("HEADER_ALT", $title);
+		}
+		else
+		{
+			$a_tpl->setCurrentBlock("container_header_row");
+		}
+		
+		$a_tpl->setVariable("BLOCK_HEADER_CONTENT", $title);
+		$a_tpl->setVariable("BLOCK_HEADER_ID", $header_id);
+		$a_tpl->parseCurrentBlock();
+		$a_tpl->touchBlock("container_row");
+	}	
+	
+	/**
+	* adds a standard row to a block template
+	*
+	* @param	object		$a_tpl		block template
+	* @param	string		$a_html		html code
+	* @access	private
+	*/
+	private function addStandardRow(&$a_tpl, $a_html, $a_item_ref_id = "", $a_item_obj_id = "",
+	$a_image_type = "", $a_related_header = "")
+	{
+		global $ilSetting;
+		
+		$this->cur_row_type = ($this->cur_row_type == "row_type_1")
+		? "row_type_2"
+		: "row_type_1";
+		$a_tpl->touchBlock($this->cur_row_type);
+		
+		if ($a_image_type != "")
+		{
+			if (!is_array($a_image_type) && !in_array($a_image_type, array("lm", "dbk", "htlm", "sahs")))
+			{
+				$icon = ilUtil::getImagePath("icon_".$a_image_type.".gif");
+				$title = $this->lng->txt("obj_".$a_image_type);
+			}
+			else
+			{
+				$icon = ilUtil::getImagePath("icon_lm.gif");
+				$title = $this->lng->txt("learning_resource");
+			}
+			
+			// custom icon
+			if ($ilSetting->get("custom_icons") &&
+			in_array($a_image_type, array("cat","grp","crs")))
+			{
+				require_once("./Services/Container/classes/class.ilContainer.php");
+				if (($path = ilContainer::_lookupIconPath($a_item_obj_id, "small")) != "")
+				{
+					$icon = $path;
+				}
+			}
+			
+			$a_tpl->setCurrentBlock("block_row_image");
+			$a_tpl->setVariable("ROW_IMG", $icon);
+			$a_tpl->setVariable("ROW_ALT", $title);
+			$a_tpl->parseCurrentBlock();
+		}
+		else
+		{
+			$a_tpl->setVariable("ROW_NBSP", "&nbsp;");
+		}
+		$a_tpl->setCurrentBlock("container_standard_row");
+		$a_tpl->setVariable("BLOCK_ROW_CONTENT", $a_html);
+		$rel_headers = ($a_related_header != "")
+		? "th_selected_items ".$a_related_header
+		: "th_selected_items";
+		$a_tpl->setVariable("BLOCK_ROW_HEADERS", $rel_headers);
+		$a_tpl->parseCurrentBlock();
+		$a_tpl->touchBlock("container_row");
+	}
 
 	public function showDetails()
 	{
-		global $ilMainMenu;
+		global $ilMainMenu, $ilTabs;
+		
+		$ilTabs->setTabActive('buy');
 		
 		$ilMainMenu->setActive('shop');
 		
 		$this->__initPaymentObject();
 		$this->__initPricesObject();
 		$this->__initShoppingCartObject();
+		
+		$this->tpl->addBlockfile('BUTTONS', 'buttons', 'tpl.buttons.html');
+		$this->tpl->setCurrentBlock('btn_cell');
+		$this->tpl->setVariable('BTN_LINK', 'ilias.php?baseClass=ilShopController');
+		$this->tpl->setVariable('BTN_TXT', $this->lng->txt('payment_back_to_shop'));
+		$this->tpl->parseCurrentBlock();
 
 		$this->tpl->addBlockFile('ADM_CONTENT', 'adm_content', 'tpl.pay_purchase_details.html', 'Services/Payment');
 
@@ -237,36 +501,7 @@ class ilShopPurchaseGUI extends ilObjectGUI
 #					$this->tpl->setVariable("PRICE",ilPaymentPrices::_getPriceString($price['price_id']));
 #				}
 #			}
-		}
-		
-		// abstracts
-		if(($abstract_html = $this->__getAbstractHTML($this->pobject->getPobjectId())) != '')
-		{			
-			$this->tpl->setCurrentBlock('abstract_block');
-			$this->tpl->setVariable('TXT_ABSTRACT', $this->lng->txt('pay_abstract'));
-			$this->tpl->setVariable('ABSTRACT_HTML', $abstract_html.$output);
-			$this->tpl->parseCurrentBlock();
-		}	
-		
-		// public content ilias lm
-		global $ilObjDataCache;
-		if($ilObjDataCache->lookupType($ilObjDataCache->lookupObjId($this->pobject->getRefId())) == 'lm')
-		{			
-			include_once  'Modules/LearningModule/classes/class.ilShopPublicSectionSelector.php';
-			$exp = new ilShopPublicSectionSelector($this->ctrl->getLinkTarget($this, 'layout'),
-				ilObjectFactory::getInstanceByRefId($this->pobject->getRefId()), get_class($this));	
-			$exp->setTargetGet('obj_id');
-			$exp->setOutput(0);
-			$output = $exp->getOutput();
-			
-			if(trim($output) != '')
-			{			
-				$this->tpl->setCurrentBlock('public_content_block');
-				$this->tpl->setVariable('TXT_CONTENT', $this->lng->txt('content'));
-				$this->tpl->setVariable('PUBLIC_CONTENT_HTML', $output);
-				$this->tpl->parseCurrentBlock();
-			}
-		}
+		}		
 	}	
 	
 	private function __getAbstractHTML($a_payment_object_id)
@@ -302,7 +537,9 @@ class ilShopPurchaseGUI extends ilObjectGUI
 
 	public function addToShoppingCart()
 	{
-		global $ilUser;		
+		global $ilUser, $ilTabs;
+		
+		$ilTabs->setTabActive('buy');		
 		
 		if(ANONYMOUS_USER_ID == $ilUser->getId())
 		{
@@ -402,8 +639,5 @@ class ilShopPurchaseGUI extends ilObjectGUI
 	{
 		$this->tpl->setVariable("LOCATION_STYLESHEET",ilUtil::getStyleSheetLocation());
 	}
-
-	
-
 }
 ?>
