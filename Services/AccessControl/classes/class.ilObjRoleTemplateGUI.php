86ba10a2696e7cf4f1f04d90301f60202991395f
Stefan Meyer <smeyer.ilias@gmx.de>
2008-11-24 15:52:21 +0000

    Added fix for "mantis 0004133: Newly created role templates cannot be opened " if rbac database structure is currupted.
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@18055 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/AccessControl/classes/class.ilObjRoleTemplateGUI.php
+++ Services/AccessControl/classes/class.ilObjRoleTemplateGUI.php
@@ -219,8 +219,9 @@ class ilObjRoleTemplateGUI extends ilObjectGUI
 
 		// build array with all rbac object types
 		$q = "SELECT ta.typ_id,obj.title,ops.ops_id,ops.operation FROM rbac_ta AS ta ".
-			 "LEFT JOIN object_data AS obj ON obj.obj_id=ta.typ_id ".
-			 "LEFT JOIN rbac_operations AS ops ON ops.ops_id=ta.ops_id";
+			 "JOIN object_data AS obj ON obj.obj_id=ta.typ_id ".
+			 "JOIN rbac_operations AS ops ON ops.ops_id=ta.ops_id";
+
 		$r = $this->ilias->db->query($q);
 
 		while ($row = $r->fetchRow(DB_FETCHMODE_OBJECT))
