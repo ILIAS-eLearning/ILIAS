5281f98f9fc137e47fdee96f000fa2ed44cf3d64
hschottm <hschottm>
2008-11-26 14:21:07 +0000

    Added import/export for flash questions
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@18095 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/TestQuestionPool/classes/export/qti12/class.assQuestionExport.php
+++ Modules/TestQuestionPool/classes/export/qti12/class.assQuestionExport.php
@@ -52,6 +52,94 @@ class assQuestionExport
 	{
 		$this->object =& $a_object;
 	}
+	
+	function exportFeedbackOnly($a_xml_writer)
+	{
+		$feedback_allcorrect = $this->object->getFeedbackGeneric(1);
+		$feedback_onenotcorrect = $this->object->getFeedbackGeneric(0);
+		if (strlen($feedback_allcorrect . $feedback_onenotcorrect))
+		{
+			$a_xml_writer->xmlStartTag("resprocessing");
+			$a_xml_writer->xmlStartTag("outcomes");
+			$a_xml_writer->xmlStartTag("decvar");
+			$a_xml_writer->xmlEndTag("decvar");
+			$a_xml_writer->xmlEndTag("outcomes");
+
+			if (strlen($feedback_allcorrect))
+			{
+				$attrs = array(
+					"continue" => "Yes"
+				);
+				$a_xml_writer->xmlStartTag("respcondition", $attrs);
+				// qti conditionvar
+				$a_xml_writer->xmlStartTag("conditionvar");
+				$attrs = array(
+					"respident" => "points"
+				);
+				$a_xml_writer->xmlElement("varequal", $attrs, $this->object->getPoints());
+				$a_xml_writer->xmlEndTag("conditionvar");
+				// qti displayfeedback
+				$attrs = array(
+					"feedbacktype" => "Response",
+					"linkrefid" => "response_allcorrect"
+				);
+				$a_xml_writer->xmlElement("displayfeedback", $attrs);
+				$a_xml_writer->xmlEndTag("respcondition");
+			}
+
+			if (strlen($feedback_onenotcorrect))
+			{
+				$attrs = array(
+					"continue" => "Yes"
+				);
+				$a_xml_writer->xmlStartTag("respcondition", $attrs);
+				// qti conditionvar
+				$a_xml_writer->xmlStartTag("conditionvar");
+				$a_xml_writer->xmlStartTag("not");
+				$attrs = array(
+					"respident" => "points"
+				);
+				$a_xml_writer->xmlElement("varequal", $attrs, $this->object->getPoints());
+				$a_xml_writer->xmlEndTag("not");
+				$a_xml_writer->xmlEndTag("conditionvar");
+				// qti displayfeedback
+				$attrs = array(
+					"feedbacktype" => "Response",
+					"linkrefid" => "response_onenotcorrect"
+				);
+				$a_xml_writer->xmlElement("displayfeedback", $attrs);
+				$a_xml_writer->xmlEndTag("respcondition");
+			}
+			$a_xml_writer->xmlEndTag("resprocessing");
+		}
+
+		if (strlen($feedback_allcorrect))
+		{
+			$attrs = array(
+				"ident" => "response_allcorrect",
+				"view" => "All"
+			);
+			$a_xml_writer->xmlStartTag("itemfeedback", $attrs);
+			// qti flow_mat
+			$a_xml_writer->xmlStartTag("flow_mat");
+			$this->object->addQTIMaterial($a_xml_writer, $feedback_allcorrect);
+			$a_xml_writer->xmlEndTag("flow_mat");
+			$a_xml_writer->xmlEndTag("itemfeedback");
+		}
+		if (strlen($feedback_onenotcorrect))
+		{
+			$attrs = array(
+				"ident" => "response_onenotcorrect",
+				"view" => "All"
+			);
+			$a_xml_writer->xmlStartTag("itemfeedback", $attrs);
+			// qti flow_mat
+			$a_xml_writer->xmlStartTag("flow_mat");
+			$this->object->addQTIMaterial($a_xml_writer, $feedback_onenotcorrect);
+			$a_xml_writer->xmlEndTag("flow_mat");
+			$a_xml_writer->xmlEndTag("itemfeedback");
+		}
+	}
 
 	/**
 	* Returns a QTI xml representation of the question
