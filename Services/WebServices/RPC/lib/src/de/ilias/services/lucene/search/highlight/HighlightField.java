c061287077171cc581092a7e3c4c0a5fe8097003
Stefan Meyer <smeyer.ilias@gmx.de>
2009-01-19 21:29:39 +0000

    Merged performance fix for index searcher.
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@18597 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ Services/WebServices/RPC/lib/src/de/ilias/services/lucene/search/highlight/HighlightField.java
@@ -0,0 +1,74 @@
+/*
+        +-----------------------------------------------------------------------------+
+        | ILIAS open source                                                           |
+        +-----------------------------------------------------------------------------+
+        | Copyright (c) 1998-2001 ILIAS open source, University of Cologne            |
+        |                                                                             |
+        | This program is free software; you can redistribute it and/or               |
+        | modify it under the terms of the GNU General Public License                 |
+        | as published by the Free Software Foundation; either version 2              |
+        | of the License, or (at your option) any later version.                      |
+        |                                                                             |
+        | This program is distributed in the hope that it will be useful,             |
+        | but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+        | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+        | GNU General Public License for more details.                                |
+        |                                                                             |
+        | You should have received a copy of the GNU General Public License           |
+        | along with this program; if not, write to the Free Software                 |
+        | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
+        +-----------------------------------------------------------------------------+
+*/
+
+package de.ilias.services.lucene.search.highlight;
+
+import org.apache.log4j.Logger;
+
+/**
+ * 
+ *
+ * @author Stefan Meyer <smeyer.ilias@gmx.de>
+ * @version $Id$
+ */
+public class HighlightField {
+
+	protected static Logger logger = Logger.getLogger(HighlightField.class);
+	
+	private String name;
+	private String highlight;
+	
+	/**
+	 * 
+	 */
+	public HighlightField() {
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param highlight the highlight to set
+	 */
+	public void setHighlight(String highlight) {
+		this.highlight = highlight;
+	}
+
+	/**
+	 * @return the highlight
+	 */
+	public String getHighlight() {
+		return highlight;
+	}
+
+}
