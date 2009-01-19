d26f1d1393c18e14d74c95e6d113231a7c24ac1b
hschottm <hschottm@svn.ilias.de>
2009-01-19 08:38:01 +0000

    Merged rev. 18543
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@18544 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/TestQuestionPool/classes/class.assQuestionGUI.php
+++ Modules/TestQuestionPool/classes/class.assQuestionGUI.php
@@ -851,7 +851,7 @@ class assQuestionGUI
 				$href = assQuestion::_getInternalLinkHref($solution_array["internal_link"]);
 				$template->setCurrentBlock("preview");
 				$template->setVariable("TEXT_SOLUTION", $this->lng->txt("solution_hint"));
-				$template->setVariable("VALUE_SOLUTION", " <a href=\"$href\" target=\"content\">" . $this->lng->txt("link"). "</a> ");
+				$template->setVariable("VALUE_SOLUTION", " <a href=\"$href\" target=\"content\">" . $this->lng->txt("download"). "</a> ");
 				$template->parseCurrentBlock();
 			}
 			elseif ((strcmp($solution_array["type"], "file") == 0) && (is_array($solution_array["value"])))
