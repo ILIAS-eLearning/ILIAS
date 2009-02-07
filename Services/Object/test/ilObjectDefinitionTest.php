70fdc886386c5e1620f13084cbcc4e8ee78d76d4
Alexander Killing <killing@leifos.de>
2009-02-07 22:15:26 +0000

    abstraction of il_object_group, il_object_subobj
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@18901 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ Services/Object/test/ilObjectDefinitionTest.php
@@ -0,0 +1,47 @@
+<?php
+/*
+	+-----------------------------------------------------------------------------+
+	| ILIAS open source                                                           |
+	+-----------------------------------------------------------------------------+
+	| Copyright (c) 1998-2009 ILIAS open source, University of Cologne            |
+	|                                                                             |
+	| This program is free software; you can redistribute it and/or               |
+	| modify it under the terms of the GNU General Public License                 |
+	| as published by the Free Software Foundation; either version 2              |
+	| of the License, or (at your option) any later version.                      |
+	|                                                                             |
+	| This program is distributed in the hope that it will be useful,             |
+	| but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+	| GNU General Public License for more details.                                |
+	|                                                                             |
+	| You should have received a copy of the GNU General Public License           |
+	| along with this program; if not, write to the Free Software                 |
+	| Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
+	+-----------------------------------------------------------------------------+
+*/
+
+class ilSettingTest extends PHPUnit_Framework_TestCase
+{
+	protected $backupGlobals = FALSE;
+
+	protected function setUp()
+	{
+		include_once("./Services/PHPUnit/classes/class.ilUnitUtil.php");
+		ilUnitUtil::performInitialisation();
+	}
+	
+	public function testObjDefinitionReader()
+	{
+		
+		include_once("./setup/classes/class.ilObjDefReader.php");
+		$def_reader = new ilObjDefReader("./Services/Object/test/testmodule.xml",
+			"DefinitionTest", "Modules");
+		
+		$def_reader->deleteObjectDefinition("xx1");
+		$def_reader->startParsing();
+		
+		$this->assertEquals("", $result);
+	}
+}
+?>
