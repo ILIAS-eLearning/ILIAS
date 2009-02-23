8bd3539bc1c3c1eb2954c2206b67a196cc269fd8
Alexander Killing <killing@leifos.de>
2009-02-23 13:28:01 +0000

    updated template
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@19118 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Database/classes/class.ilDBGenerator.php
+++ Services/Database/classes/class.ilDBGenerator.php
@@ -381,7 +381,7 @@ echo "<br>4"; flush();
 			$types_str = "array(".implode($types, ",").")";
 			$values_str = "array(".implode($values, ",").")";
 			$ins_st = "\n".'$ilDB->insert("'.$a_table.'", array('."\n";
-			$ins_st.= implode($i_str, ", ").");\n";
+			$ins_st.= implode($i_str, ", ")."));\n";
 			//$ins_st.= "\t".$fields_str."\n";
 			//$ins_st.= "\t".'VALUES '."(%s".str_repeat(",%s", count($fields) - 1).')"'.",\n";
 			//$ins_st.= "\t".$types_str.','.$values_str.');'."\n";
