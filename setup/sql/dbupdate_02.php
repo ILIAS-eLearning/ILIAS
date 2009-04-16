afa24dbcc7e2fe83318178f93b8a52f493c14184
Alexander Killing <killing@leifos.de>
2009-04-16 15:17:28 +0000

    added accessibility settings
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@19707 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- setup/sql/dbupdate_02.php
+++ setup/sql/dbupdate_02.php
@@ -11111,3 +11111,53 @@ $ilMySQLAbstraction->performAbstraction('chat_records');
 <?php
 	$ilDB->modifyTableColumn('il_meta_format', 'format', array('type' => 'text','length' => 255, 'notnull' => false));
 ?>
+<#2307>
+<?php
+
+// accessibility settings
+
+// register new object type 'accs' for accessibility settings
+$id = $ilDB->nextId("object_data");
+$ilDB->manipulateF("INSERT INTO object_data (obj_id, type, title, description, owner, create_date, last_update) ".
+		"VALUES (%s, %s, %s, %s, %s, %s, %s)",
+		array("integer", "text", "text", "text", "integer", "timestamp", "timestamp"),
+		array($id, "typ", "accs", "Accessibility settings", -1, ilUtil::now(), ilUtil::now()));
+$typ_id = $id;
+
+// create object data entry
+$id = $ilDB->nextId("object_data");
+$ilDB->manipulateF("INSERT INTO object_data (obj_id, type, title, description, owner, create_date, last_update) ".
+		"VALUES (%s, %s, %s, %s, %s, %s, %s)",
+		array("integer", "text", "text", "text", "integer", "timestamp", "timestamp"),
+		array($id, "accs", "__AccessibilitySettings", "Accessibility Settings", -1, ilUtil::now(), ilUtil::now()));
+
+// create object reference entry
+$ref_id = $ilDB->nextId('object_reference');
+$res = $ilDB->manipulateF("INSERT INTO object_reference (ref_id, obj_id) VALUES (%s, %s)",
+	array("integer", "integer"),
+	array($ref_id, $id));
+
+// put in tree
+$tree = new ilTree(ROOT_FOLDER_ID);
+$tree->insertNode($ref_id, SYSTEM_FOLDER_ID);
+
+// add rbac operations
+// 1: edit_permissions, 2: visible, 3: read, 4:write
+$ilDB->manipulateF("INSERT INTO rbac_ta (typ_id, ops_id) VALUES (%s, %s)",
+	array("integer", "integer"),
+	array($typ_id, 1));
+$ilDB->manipulateF("INSERT INTO rbac_ta (typ_id, ops_id) VALUES (%s, %s)",
+	array("integer", "integer"),
+	array($typ_id, 2));
+$ilDB->manipulateF("INSERT INTO rbac_ta (typ_id, ops_id) VALUES (%s, %s)",
+	array("integer", "integer"),
+	array($typ_id, 3));
+$ilDB->manipulateF("INSERT INTO rbac_ta (typ_id, ops_id) VALUES (%s, %s)",
+	array("integer", "integer"),
+	array($typ_id, 4));
+?>
+<#2308>
+<?php
+$ilCtrlStructureReader->getStructure();
+?>
+
