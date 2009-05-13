c36197dcc9cbe082256d2ee12864f23620ade462
hschottm <hschottm@svn.ilias.de>
2009-05-13 11:07:33 +0000

    Added QTI import/export for file upload questions
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@19986 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ Modules/TestQuestionPool/classes/export/qti12/class.assFileUploadExport.php
@@ -0,0 +1,123 @@
+<?php
+ /*
+   +----------------------------------------------------------------------------+
+   | ILIAS open source                                                          |
+   +----------------------------------------------------------------------------+
+   | Copyright (c) 1998-2001 ILIAS open source, University of Cologne           |
+   |                                                                            |
+   | This program is free software; you can redistribute it and/or              |
+   | modify it under the terms of the GNU General Public License                |
+   | as published by the Free Software Foundation; either version 2             |
+   | of the License, or (at your option) any later version.                     |
+   |                                                                            |
+   | This program is distributed in the hope that it will be useful,            |
+   | but WITHOUT ANY WARRANTY; without even the implied warranty of             |
+   | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              |
+   | GNU General Public License for more details.                               |
+   |                                                                            |
+   | You should have received a copy of the GNU General Public License          |
+   | along with this program; if not, write to the Free Software                |
+   | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. |
+   +----------------------------------------------------------------------------+
+*/
+
+include_once "./Modules/TestQuestionPool/classes/export/qti12/class.assQuestionExport.php";
+
+/**
+* Class for file upload question exports
+*
+* @author		Helmut Schottmüller <helmut.schottmueller@mac.com>
+* @version	$Id$
+* @ingroup ModulesTestQuestionPool
+*/
+class assFileUploadExport extends assQuestionExport
+{
+	/**
+	* Returns a QTI xml representation of the question
+	*
+	* Returns a QTI xml representation of the question and sets the internal
+	* domxml variable with the DOM XML representation of the QTI xml representation
+	*
+	* @return string The QTI xml representation of the question
+	* @access public
+	*/
+	function toXML($a_include_header = true, $a_include_binary = true, $a_shuffle = false, $test_output = false, $force_image_references = false)
+	{
+		global $ilias;
+		
+		include_once("./classes/class.ilXmlWriter.php");
+		$a_xml_writer = new ilXmlWriter;
+		// set xml header
+		$a_xml_writer->xmlHeader();
+		$a_xml_writer->xmlStartTag("questestinterop");
+		$attrs = array(
+			"ident" => "il_".IL_INST_ID."_qst_".$this->object->getId(),
+			"title" => $this->object->getTitle()
+		);
+		$a_xml_writer->xmlStartTag("item", $attrs);
+		// add question description
+		$a_xml_writer->xmlElement("qticomment", NULL, $this->object->getComment());
+		// add estimated working time
+		$workingtime = $this->object->getEstimatedWorkingTime();
+		$duration = sprintf("P0Y0M0DT%dH%dM%dS", $workingtime["h"], $workingtime["m"], $workingtime["s"]);
+		$a_xml_writer->xmlElement("duration", NULL, $duration);
+		// add ILIAS specific metadata
+		$a_xml_writer->xmlStartTag("itemmetadata");
+		$a_xml_writer->xmlStartTag("qtimetadata");
+		$a_xml_writer->xmlStartTag("qtimetadatafield");
+		$a_xml_writer->xmlElement("fieldlabel", NULL, "ILIAS_VERSION");
+		$a_xml_writer->xmlElement("fieldentry", NULL, $ilias->getSetting("ilias_version"));
+		$a_xml_writer->xmlEndTag("qtimetadatafield");
+		$a_xml_writer->xmlStartTag("qtimetadatafield");
+		$a_xml_writer->xmlElement("fieldlabel", NULL, "QUESTIONTYPE");
+		$a_xml_writer->xmlElement("fieldentry", NULL, $this->object->getQuestionType());
+		$a_xml_writer->xmlEndTag("qtimetadatafield");
+		$a_xml_writer->xmlStartTag("qtimetadatafield");
+		$a_xml_writer->xmlElement("fieldlabel", NULL, "AUTHOR");
+		$a_xml_writer->xmlElement("fieldentry", NULL, $this->object->getAuthor());
+		$a_xml_writer->xmlEndTag("qtimetadatafield");
+		$a_xml_writer->xmlStartTag("qtimetadatafield");
+		$a_xml_writer->xmlElement("fieldlabel", NULL, "points");
+		$a_xml_writer->xmlElement("fieldentry", NULL, $this->object->getPoints());
+		$a_xml_writer->xmlEndTag("qtimetadatafield");
+		$a_xml_writer->xmlStartTag("qtimetadatafield");
+		$a_xml_writer->xmlElement("fieldlabel", NULL, "maxsize");
+		$a_xml_writer->xmlElement("fieldentry", NULL, $this->object->getMaxSize());
+		$a_xml_writer->xmlEndTag("qtimetadatafield");
+		$a_xml_writer->xmlStartTag("qtimetadatafield");
+		$a_xml_writer->xmlElement("fieldlabel", NULL, "allowedextensions");
+		$a_xml_writer->xmlElement("fieldentry", NULL, $this->object->getAllowedExtensions());
+		$a_xml_writer->xmlEndTag("qtimetadatafield");
+		$a_xml_writer->xmlEndTag("qtimetadata");
+		$a_xml_writer->xmlEndTag("itemmetadata");
+
+		// PART I: qti presentation
+		$attrs = array(
+			"label" => $this->object->getTitle()
+		);
+		$a_xml_writer->xmlStartTag("presentation", $attrs);
+		// add flow to presentation
+		$a_xml_writer->xmlStartTag("flow");
+		// add material with question text to presentation
+		$this->object->addQTIMaterial($a_xml_writer, $this->object->getQuestion());
+		// add answers to presentation
+		$a_xml_writer->xmlEndTag("flow");
+		$a_xml_writer->xmlEndTag("presentation");
+		
+		$this->exportFeedbackOnly($a_xml_writer);
+		
+		$a_xml_writer->xmlEndTag("item");
+		$a_xml_writer->xmlEndTag("questestinterop");
+
+		$xml = $a_xml_writer->xmlDumpMem(FALSE);
+		if (!$a_include_header)
+		{
+			$pos = strpos($xml, "?>");
+			$xml = substr($xml, $pos + 2);
+		}
+		return $xml;
+	}
+
+}
+
+?>
