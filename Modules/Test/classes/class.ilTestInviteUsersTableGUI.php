c74d150f43921ffd2a313d3812ddcfc67ab8449d
hschottm <hschottm>
2009-06-27 17:43:20 +0000

    Changed participants invitation for tests to ilTable2GUI
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@20361 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ Modules/Test/classes/class.ilTestInviteUsersTableGUI.php
@@ -0,0 +1,96 @@
+<?php
+/*
+        +-----------------------------------------------------------------------------+
+        | ILIAS open source                                                           |
+        +-----------------------------------------------------------------------------+
+        | Copyright (c) 1998-2006 ILIAS open source, University of Cologne            |
+        |                                                                             |
+        | This program is free software; you can redistribute it and/or               |
+        | modify it under the terms of the GNU General Public License                 |
+        | as published by the Free Software Foundation; either version 2              |
+        | of the License, or (at your option) any later version.                      |
+        |                                                                             |
+        | This program is distributed in the hope that it will be useful,             |
+        | but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+        | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+        | GNU General Public License for more details.                                |
+        |                                                                             |
+        | You should have received a copy of the GNU General Public License           |
+        | along with this program; if not, write to the Free Software                 |
+        | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
+        +-----------------------------------------------------------------------------+
+*/
+
+include_once('./Services/Table/classes/class.ilTable2GUI.php');
+
+/**
+*
+* @author Helmut Schottmüller <ilias@aurealis.de>
+* @version $Id$
+*
+* @ingroup ModulesTest
+*/
+
+class ilTestInviteUsersTableGUI extends ilTable2GUI
+{
+	/**
+	 * Constructor
+	 *
+	 * @access public
+	 * @param
+	 * @return
+	 */
+	public function __construct($a_parent_obj, $a_parent_cmd)
+	{
+		parent::__construct($a_parent_obj, $a_parent_cmd);
+
+		global $lng, $ilCtrl;
+
+		$this->lng = $lng;
+		$this->ctrl = $ilCtrl;
+		$this->counter = 1;
+		
+		$this->setFormName('inviteusers');
+		$this->setStyle('table', 'fullwidth');
+
+		$this->addColumn('','f','1%');
+		$this->addColumn($this->lng->txt("login"),'login', '');
+		$this->addColumn($this->lng->txt("firstname"),'firstname', '');
+		$this->addColumn($this->lng->txt("lastname"),'lastname', '');
+		$this->addColumn($this->lng->txt("clientip"),'clientip', '');
+	
+		$this->setTitle($this->lng->txt('search_users'), 'icon_usr.gif', $this->lng->txt('usr'));
+	
+		$this->setRowTemplate("tpl.il_as_tst_invite_users_row.html", "Modules/Test");
+
+		$this->addMultiCommand('addParticipants', $this->lng->txt('add'));
+
+		$this->setFormAction($this->ctrl->getFormAction($a_parent_obj, $a_parent_cmd));
+
+		$this->setDefaultOrderField("login");
+		$this->setDefaultOrderDirection("asc");
+		$this->setPrefix('user_select');
+		$this->setSelectAllCheckbox('user_select');
+		
+		$this->enable('header');
+		$this->enable('sort');
+		$this->enable('select_all');
+	}
+
+	/**
+	 * fill row 
+	 *
+	 * @access public
+	 * @param
+	 * @return
+	 */
+	public function fillRow($data)
+	{
+		$this->tpl->setVariable("USER_ID", $data['usr_id']);
+		$this->tpl->setVariable("LOGIN", $data['login']);
+		$this->tpl->setVariable("FIRSTNAME", $data['firstname']);
+		$this->tpl->setVariable("LASTNAME", $data['lastname']);
+		$this->tpl->setVariable("CLIENT_IP", $data['clientip']);
+	}
+}
+?>
