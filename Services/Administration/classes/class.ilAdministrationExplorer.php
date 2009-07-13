6c6bd7aa99c099e05a560491e3fdf5c7da4155d0
Alexander Killing <killing@leifos.de>
2009-07-13 05:11:54 +0000

    small fix
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@20536 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Administration/classes/class.ilAdministrationExplorer.php
+++ Services/Administration/classes/class.ilAdministrationExplorer.php
@@ -1,25 +1,5 @@
 <?php
-/*
-	+-----------------------------------------------------------------------------+
-	| ILIAS open source                                                           |
-	+-----------------------------------------------------------------------------+
-	| Copyright (c) 1998-2005 ILIAS open source, University of Cologne            |
-	|                                                                             |
-	| This program is free software; you can redistribute it and/or               |
-	| modify it under the terms of the GNU General Public License                 |
-	| as published by the Free Software Foundation; either version 2              |
-	| of the License, or (at your option) any later version.                      |
-	|                                                                             |
-	| This program is distributed in the hope that it will be useful,             |
-	| but WITHOUT ANY WARRANTY; without even the implied warranty of              |
-	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
-	| GNU General Public License for more details.                                |
-	|                                                                             |
-	| You should have received a copy of the GNU General Public License           |
-	| along with this program; if not, write to the Free Software                 |
-	| Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
-	+-----------------------------------------------------------------------------+
-*/
+/* Copyright (c) 1998-2009 ILIAS open source, Extended GPL, see docs/LICENSE */
 
 /*
 * Administration Explorer
@@ -168,6 +148,11 @@ class ilAdministrationExplorer extends ilExplorer
 	function isVisible($a_ref_id,$a_type)
 	{
 		global $rbacsystem, $ilBench;
+
+		if ($this->tree->getParentId($a_ref_id) == ROOT_FOLDER_ID && $a_type != "adm")
+		{
+			return false;
+		}
 
 		// these objects may exist due to test cases that didnt clear
 		// data properly
