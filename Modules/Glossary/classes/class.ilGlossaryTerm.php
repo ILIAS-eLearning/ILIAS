30174d2b8005d58e898a2a67c005a885892e9188
Alexander Killing <killing@leifos.de>
2009-07-17 18:43:04 +0000

    glossary term fix
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@20626 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Glossary/classes/class.ilGlossaryTerm.php
+++ Modules/Glossary/classes/class.ilGlossaryTerm.php
@@ -1,26 +1,5 @@
 <?php
-/*
-	+-----------------------------------------------------------------------------+
-	| ILIAS open source                                                           |
-	+-----------------------------------------------------------------------------+
-	| Copyright (c) 1998-2009 ILIAS open source, University of Cologne            |
-	|                                                                             |
-	| This program is free software; you can redistribute it and/or               |
-	| modify it under the terms of the GNU General Public License                 |
-	| as published by the Free Software Foundation; either version 2              |
-	| of the License, or (at your option) any later version.                      |
-	|                                                                             |
-	| This program is distributed in the hope that it will be useful,             |
-	| but WITHOUT ANY WARRANTY; without even the implied warranty of              |
-	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
-	| GNU General Public License for more details.                                |
-	|                                                                             |
-	| You should have received a copy of the GNU General Public License           |
-	| along with this program; if not, write to the Free Software                 |
-	| Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
-	+-----------------------------------------------------------------------------+
-*/
-
+/* Copyright (c) 1998-2009 ILIAS open source, Extended GPL, see docs/LICENSE */
 
 /**
 * Class ilGlossaryTerm
@@ -93,8 +72,13 @@ class ilGlossaryTerm
 	{
 		global $ilDB;
 		
+		if ($a_import_id == "")
+		{
+			return 0;
+		}
+		
 		$q = "SELECT * FROM glossary_term WHERE import_id = ".
-			$ilDB->quote($a_import_id, "integer").
+			$ilDB->quote($a_import_id, "text").
 			" ORDER BY create_date DESC";
 		$term_set = $ilDB->query($q);
 		while ($term_rec = $ilDB->fetchAssoc($term_set))
