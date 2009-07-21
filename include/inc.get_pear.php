ab17b33f7c0450edd15720b37eb85de4cf30420c
Stefan Meyer <smeyer.ilias@gmx.de>
2009-07-21 19:55:25 +0000

    Fixed include path.
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@20678 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- include/inc.get_pear.php
+++ include/inc.get_pear.php
@@ -1,12 +1,10 @@
 <?php
 
 // First use local pear
-ini_set('include_path',"./Services/PEAR/lib:".ini_get('include_path'));
-
+set_include_path("./Services/PEAR/lib".PATH_SEPARATOR.ini_get('include_path'));
 // look for embedded pear
 if (is_dir("./pear"))
 {
-	ini_set("include_path", "./pear:".ini_get("include_path"));
+	set_include_path("./pear".PATH_SEPARATOR.ini_get('include_path'));
 }
-
 ?>
