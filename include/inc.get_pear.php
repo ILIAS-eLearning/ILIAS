64d5b4cd26a113863e91985a54ec34fece01c00c
rkuester <rkuester>
2009-08-08 11:26:03 +0000

    - fixed typo in class ObjUserFolder: getStandardUserFields -> getStandardFields
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@20959 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- include/inc.get_pear.php
+++ include/inc.get_pear.php
@@ -1,10 +1,12 @@
 <?php
-
+ini_set("error_reporting", E_ALL  & ~E_NOTICE);
 // First use local pear
-set_include_path("./Services/PEAR/lib".PATH_SEPARATOR.ini_get('include_path'));
+ini_set('include_path',"./Services/PEAR/lib;".ini_get('include_path'));
+
 // look for embedded pear
 if (is_dir("./pear"))
 {
-	set_include_path("./pear".PATH_SEPARATOR.ini_get('include_path'));
+	ini_set("include_path", "./pear;".ini_get("include_path"));
 }
+
 ?>
