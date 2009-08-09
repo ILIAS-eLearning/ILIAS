efd66ac69f16f4e5b6050ad865b3ce1c381b5640
Alexander Killing <killing@leifos.de>
2009-08-09 14:34:16 +0000

    added repository cache time setting in administration
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@20963 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Object/classes/class.ilListItemAccessCache.php
+++ Services/Object/classes/class.ilListItemAccessCache.php
@@ -12,6 +12,8 @@ include_once("./Services/Cache/classes/class.ilCache.php");
  */
 class ilListItemAccessCache extends ilCache
 {
+	static $disabled = false;
+	
 	/**
 	 * Constructor
 	 *
@@ -20,8 +22,46 @@ class ilListItemAccessCache extends ilCache
 	 */
 	function __construct()
 	{
+		global $ilSetting;
 		parent::__construct("ServicesObject", "CheckAccess", false);
-		$this->setExpiresAfter(1800);
+		$this->setExpiresAfter($ilSetting->get("rep_cache") * 60);
+		if ((int) $ilSetting->get("rep_cache") == 0)
+		{
+			self::$disabled = true;
+		}
+	}
+	
+	/**
+	 * Read an entry
+	 */
+	function readEntry($a_id)
+	{
+		if (!self::$disabled)
+		{
+			return parent::readEntry($a_id);
+		}
+		return false;
+	}
+	
+	
+	/**
+	 * Id is user_id:ref_id, we store ref_if additionally
+	 */
+	function storeEntry($a_id, $a_value, $a_ref_id = 0)
+	{
+		global $ilSetting;
+		if (!self::$disabled)
+		{
+			parent::storeEntry($a_id, $a_value, $a_ref_id);
+		}
+	}
+
+	/**
+	 * This one can be called, e.g. 
+	 */
+	function deleteByRefId($a_ref_id)
+	{
+		parent::deleteByAdditionalKeys($a_ref_id);
 	}
 }
 ?>
