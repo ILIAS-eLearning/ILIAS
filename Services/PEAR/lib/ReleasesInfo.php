79ac1999d1ba73cafa0f33c793ecfaf9daf916ee
Alexander Killing <killing@leifos.de>
2009-08-13 14:22:40 +0000

    added HTML_Template_IT and MDB2 to PEAR service
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@21054 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/PEAR/lib/ReleasesInfo.php
+++ Services/PEAR/lib/ReleasesInfo.php
@@ -1,9 +1,12 @@
 <?php /*
 
-Log			1.11.4
-Auth		1.6.1
-Auth_HTTP	2.1.6
-XML_RPC2	1.0.5
-
+Log					1.11.4
+Auth				1.6.1
+Auth_HTTP			2.1.6
+XML_RPC2			1.0.5
+MDB2				2.4.1
+MDB2_Driver_mysql	1.4.1
+MDB2_Driver_oci8	1.4.1
+HTML_Template_IT	1.2.1
 */
 ?>
