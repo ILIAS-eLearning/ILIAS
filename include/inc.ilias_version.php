d3e83031f9148fe25f8b28fc36ae40ba321a53cc
Alexander Killing <killing@leifos.de>
2009-08-19 17:48:35 +0000

    updated version and docs
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@21166 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- include/inc.ilias_version.php
+++ include/inc.ilias_version.php
@@ -9,6 +9,6 @@
 *
 * @package ilias-core
 */
-define("ILIAS_VERSION", "4.0.0 Beta1 2009-07-24");
+define("ILIAS_VERSION", "4.0.0 Beta2 2009-08-19");
 define("ILIAS_VERSION_NUMERIC", "4.0.0");			// must be always x.y.z: x, y and z are numbers
 ?>
