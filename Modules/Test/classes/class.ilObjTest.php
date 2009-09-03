163465e0f80e41ec314fcbe36a560744a72c44ef
hschottm <hschottm@svn.ilias.de>
2009-09-03 17:08:15 +0000

    Fixed Mantis bug #5062: Can´t save date by access to test results
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@21450 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Test/classes/class.ilObjTest.php
+++ Modules/Test/classes/class.ilObjTest.php
@@ -7778,6 +7778,7 @@ function loadQuestions($active_id = "", $pass = NULL)
 	function canViewResults()
 	{
 		$result = true;
+		if ($this->getScoreReporting() == 4) return false;
 		if ($this->getReportingDate())
 		{
 			if (preg_match("/(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/", $this->getReportingDate(), $matches))
