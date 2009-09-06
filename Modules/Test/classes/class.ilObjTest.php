d240249a8c9c375196d54177c23e2d5fcefdac11
hschottm <hschottm@svn.ilias.de>
2009-09-06 14:48:57 +0000

    Fixed shuffle function in tests
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@21476 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Test/classes/class.ilObjTest.php
+++ Modules/Test/classes/class.ilObjTest.php
@@ -3994,23 +3994,14 @@ function loadQuestions($active_id = "", $pass = NULL)
 	*/
 	function pcArrayShuffle($array)
 	{
-		mt_srand((double)microtime()*1000000);
-		$i = count($array);
-		if ($i > 0)
+		$keys = array_keys($array);
+		shuffle($keys);
+		$result = array();
+		foreach ($keys as $key)
 		{
-			while(--$i)
-			{
-				$j = mt_rand(0, $i);
-				if ($i != $j)
-				{
-					// swap elements
-					$tmp = $array[$j];
-					$array[$j] = $array[$i];
-					$array[$i] = $tmp;
-				}
-			}
+			$result[$key] = $array[$key];
 		}
-		return $array;
+		return $result;
 	}
 	
 	/**
