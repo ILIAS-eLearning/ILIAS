f5735e6da8b2adc74b59a3b05a0d819af2c077ba
Björn Heyser <bheyser@databay.de>
2009-09-10 12:08:10 +0000

    fixed calling of method in ilCronValidator that has gone away.
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@21612 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- cron/classes/class.ilCronValidator.php
+++ cron/classes/class.ilCronValidator.php
@@ -10,18 +10,14 @@
 		
 		public function check()
 		{
-			global $ilUser,$ilLog;
-			
-			$old_logfilename = $ilLog->getFilename();
-			$ilLog->setFilename('ilias_cron_systemcheck.log');
+			global $ilUser,$rbacsystem,$lng;
 			
 			$count_limit = (bool)$ilUser->getPref('systemcheck_count_limit');
 			$age_limit = (bool)$ilUser->getPref('systemcheck_age_limit');
 			$type_limit = $ilUser->getPref('systemcheck_type_limit');
-			$logging = (bool)$ilUser->getPref('systemcheck_log_scan');
 
 			include_once "classes/class.ilValidator.php";
-			$validator = new ilValidator($logging);
+			$validator = new ilValidator();
 	
 			$modes = array();
 			$possible_modes = $validator->getPossibleModes();
@@ -31,10 +27,7 @@
 				$modes[$possible_mode] = (bool)$ilUser->getPref($pref_key);
 			}
 			
-			echo "hier";
 			ob_start();
-			
-			global $rbacsystem,$lng;
 	
 			/*if (!$rbacsystem->checkAccess("visible,read",$this->object->getRefId()))
 			{
@@ -50,7 +43,6 @@
 				$used_modes[] = $mode.'='.$value;
 			}
 	
-			echo "Hallo\n";
 			$scan_log = $validator->validate();
 	
 			$mode = $lng->txt("scan_modes").": ".implode(', ',$used_modes);
@@ -68,13 +60,11 @@
 			
 			$echo = ob_get_contents();
 			ob_end_clean();
-			//echo $echo;
+			
 			$echo = preg_replace("/<br\/>/","\n",$echo);
 			$echo = preg_replace("/<br \/>/","\n",$echo);
 			$echo = preg_replace("/<br>/","\n",$echo);
 			echo $echo;
-			
-			$ilLog->setFilename($old_logfilename);
 		}
 	}
 
