9c81375bec1568258e4d624b2b9a0400f2b3423e
Michael Jansen <mjansen@databay.de>
2009-09-10 09:57:53 +0000

    Fixed forum and mail notification cron scripts
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@21607 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- cron/classes/class.ilCronMailNotification.php
+++ cron/classes/class.ilCronMailNotification.php
@@ -1,106 +1,54 @@
 <?php
-/*
-	+-----------------------------------------------------------------------------+
-	| ILIAS open source                                                           |
-	+-----------------------------------------------------------------------------+
-	| Copyright (c) 1998-2001 ILIAS open source, University of Cologne            |
-	|                                                                             |
-	| This program is free software; you can redistribute it and/or               |
-	| modify it under the terms of the GNU General Public License                 |
-	| as published by the Free Software Foundation; either version 2              |
-	| of the License, or (at your option) any later version.                      |
-	|                                                                             |
-	| This program is distributed in the hope that it will be useful,             |
-	| but WITHOUT ANY WARRANTY; without even the implied warranty of              |
-	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
-	| GNU General Public License for more details.                                |
-	|                                                                             |
-	| You should have received a copy of the GNU General Public License           |
-	| along with this program; if not, write to the Free Software                 |
-	| Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
-	+-----------------------------------------------------------------------------+
-*/
-
+/* Copyright (c) 1998-2009 ILIAS open source, Extended GPL, see docs/LICENSE */
 
 /**
-* 
 *
 * @author Michael Jansen <mjansen@databay.de>
-*
+* @version $Id:
 * @package ilias
 */
-
 class ilCronMailNotification
-{
-	function ilCronMailNotification()
+{	
+	public function sendNotifications()
 	{
-		global $ilLog,$ilDB;
-
-		$this->log =& $ilLog;
-		$this->db =& $ilDB;
-	}
-
-	function sendMails($res)
-	{
-		global $ilias, $rbacsystem;
-
-	}
-
-	function sendNotifications()
-	{
-		global $ilias;
+		global $ilias, $ilDB, $ilLog;
 		
-		include_once "Services/Mail/classes/class.ilMail.php";
-		include_once './Services/User/classes/class.ilObjUser.php';
-		include_once "./Services/Language/classes/class.ilLanguage.php";
+		include_once 'Services/Mail/classes/class.ilMail.php';
+		include_once 'Services/User/classes/class.ilObjUser.php';
+		include_once 'Services/Language/classes/class.ilLanguage.php';
+		include_once 'Services/Mail/classes/class.ilMimeMail.php';
 
-/*		$query = "SELECT mail.* " 
-				 ."FROM mail_options "
-				 ."INNER JOIN mail ON mail.user_id = mail_options.user_id "
-				 ."INNER JOIN mail_obj_data ON mail_obj_data.obj_id = mail.folder_id "
-				 ."WHERE cronjob_notification = '1' "
-				 ."AND send_time >= '" . date("Y-m-d H:i:s", time() - 60 * 60 * 24). "' "
-				 ."AND mail_obj_data.type = 'inbox' "
-				 ."AND m_status = 'unread' "
-				 ." ";
-				 		
-		$res = $this->db->query($query);
-*/
-		$res = queryF('SELECT mail.* FROM mail_options
+		$res = $ilDB->queryF('SELECT mail.* FROM mail_options
 						INNER JOIN mail ON mail.user_id = mail_options.user_id 
 						INNER JOIN mail_obj_data ON mail_obj_data.obj_id = mail.folder_id
 						WHERE cronjob_notification = %s 
 						AND send_time >= %s
 						AND mail_obj_data.m_type = %s
 						AND m_status = %s',
-				array('integer', 'timestamp', 'text', 'text'),
-				array('1', date("Y-m-d H:i:s", time() - 60 * 60 * 24), 'inbox', 'unread')
+			array('integer', 'timestamp', 'text', 'text'),
+			array(1, date('Y-m-d H:i:s', time() - 60 * 60 * 24), 'inbox', 'unread')
 		);
 				
 		$users = array();
 		
 		$user_id = 0;
-		while($row = $res->fetchRow(DB_FETCHMODE_ASSOC))
+		while($row = $ilDB->fetchAssoc($res))
 		{
-			if ($user_id == 0 || $row['user_id'] != $user_id) $user_id = $row['user_id'];
-
+			if($user_id == 0 || $row['user_id'] != $user_id) $user_id = $row['user_id'];
 			$users[$user_id][] = $row;				 
 		}
 		
 		$numRows = 0;
-		foreach ($users as $user_id => $mail_data)
+		foreach($users as $user_id => $mail_data)
 		{
-			$tmp_mail_obj = new ilMail($user_id);			
-			
-			include_once "Services/Mail/classes/class.ilMimeMail.php";
+			$tmp_mail_obj = new ilMail($user_id);		
 
 			$mmail = new ilMimeMail();
 			$mmail->autoCheck(false);
 			$mmail->From('noreply');
 			$mmail->To(ilObjUser::_lookupEmail($user_id));					
 			$mmail->Subject($tmp_mail_obj->formatNotificationSubject());
-			$mmail->Body($tmp_mail_obj->formatNotificationMessage($user_id, $mail_data));
-			
+			$mmail->Body($tmp_mail_obj->formatNotificationMessage($user_id, $mail_data));			
 			$mmail->Send();
 			
 			unset($tmp_mail_obj);
@@ -108,7 +56,7 @@ class ilCronMailNotification
 			++$numRows;
 		}
 		
-		$this->log->write(__METHOD__.': Send '.$numRows.' messages.');	
+		$ilLog->write(__METHOD__.': Send '.$numRows.' messages.');	
 
 		return true;
 	}
