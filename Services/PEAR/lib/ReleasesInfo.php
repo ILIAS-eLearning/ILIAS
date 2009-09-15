b20bc9da1cab451c004ab5f948eab6ba8acd0223
Stefan Meyer <smeyer.ilias@gmx.de>
2009-09-15 14:13:15 +0000

    Added HTTP_Request dependencies.
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@21687 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/PEAR/lib/ReleasesInfo.php
+++ Services/PEAR/lib/ReleasesInfo.php
@@ -2,14 +2,31 @@
 
 Versions:
 Log					1.11.5
+
 Auth				1.6.1
+
+
 Auth_HTTP			2.1.6
+	Depends:
+	Auth			1.2.0
+	
 XML_RPC2			1.0.5
+
 MDB2				2.4.1
+
 MDB2_Driver_mysql	1.4.1
+	Depends:
+	MDB2			2.4.1
+
 MDB2_Driver_oci8	1.4.1
+	Depends:
+	MDB2			2.4.1
 HTML_Template_IT	1.2.1
+
 HTTP_Request		1.4.4
+	Depends:
+	Net_URL			>= 1.0.12
+	Net_Socket		>= 1.0.7		 
 
 Patches:
 MDB2/Driver/oci8.php, line 1398:
