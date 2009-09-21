50b9adccefd9057d827006e9d2930c136296a0da
Stefan Meyer <smeyer.ilias@gmx.de>
2009-09-21 20:19:15 +0000

    Fixed 0004983: Session needs own lang variable for join
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@21787 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Session/classes/class.ilObjSessionAccess.php
+++ Modules/Session/classes/class.ilObjSessionAccess.php
@@ -46,7 +46,7 @@ class ilObjSessionAccess
 		$commands = array
 		(
 			array("permission" => "read", "cmd" => "infoScreen", "lang_var" => "info_short", "default" => true),
-			array("permission" => "read", "cmd" => "register", "lang_var" => "join"),
+			array("permission" => "read", "cmd" => "register", "lang_var" => "join_session"),
 			array("permission" => "read", "cmd" => "unregister", "lang_var" => "event_unregister"),
 			array("permission" => "write", "cmd" => "edit", "lang_var" => "edit")
 		);
