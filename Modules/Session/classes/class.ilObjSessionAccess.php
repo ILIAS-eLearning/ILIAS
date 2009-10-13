4f206fbdc30279d6d35ceb1ce9eb39beb04cbf78
Stefan Meyer <smeyer.ilias@gmx.de>
2009-10-13 09:53:08 +0000

    Added "Edit Participants" command in session list guis.
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@22095 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Session/classes/class.ilObjSessionAccess.php
+++ Modules/Session/classes/class.ilObjSessionAccess.php
@@ -48,7 +48,8 @@ class ilObjSessionAccess
 			array("permission" => "read", "cmd" => "infoScreen", "lang_var" => "info_short", "default" => true),
 			array("permission" => "read", "cmd" => "register", "lang_var" => "join_session"),
 			array("permission" => "read", "cmd" => "unregister", "lang_var" => "event_unregister"),
-			array("permission" => "write", "cmd" => "edit", "lang_var" => "edit")
+			array("permission" => "write", "cmd" => "edit", "lang_var" => "edit"),
+			array('permission' => 'write', 'cmd' => 'members', 'lang_var' => 'event_edit_members')
 		);
 		
 		return $commands;
