f599dcad99a6312b8c908bb857a1e8df28f79682
hholtmann <hholtmann>
2009-10-30 12:02:47 +0000

    Fixed Mantis #5311
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@22255 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Scorm2004/classes/class.ilSCORM2004Sco.php
+++ Modules/Scorm2004/classes/class.ilSCORM2004Sco.php
@@ -288,6 +288,7 @@ class ilSCORM2004Sco extends ilSCORM2004Node
 			<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 			<head>
 				<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+				<meta http-equiv="X-UA-Compatible" content="IE=7" />
 				<link rel="stylesheet" type="text/css" href="./css/system.css" />
 				<link rel="stylesheet" type="text/css" href="./css/style.css" />
 				<link rel="stylesheet" type="text/css" href="./css/accordion.css" />
