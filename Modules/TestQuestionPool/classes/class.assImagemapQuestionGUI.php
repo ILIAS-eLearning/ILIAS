2da302523be9a3a807f812297ba812b4e031d496
hschottm <hschottm@svn.ilias.de>
2009-12-17 14:11:47 +0000

    Fixed Mantis bug #5576: Multiple choice question produces fatal error on feedback
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@22607 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/TestQuestionPool/classes/class.assImagemapQuestionGUI.php
+++ Modules/TestQuestionPool/classes/class.assImagemapQuestionGUI.php
@@ -724,7 +724,7 @@ class assImagemapQuestionGUI extends assQuestionGUI
 		global $ilAccess;
 		if ($ilAccess->checkAccess("write", "", $_GET['ref_id']))
 		{
-			$template->setVariable("SAVE", $this->lng->txt("save"));
+			$this->tpl->setVariable("SAVE", $this->lng->txt("save"));
 		}
 		$this->tpl->setVariable("FORMACTION", $this->ctrl->getFormAction($this));
 
