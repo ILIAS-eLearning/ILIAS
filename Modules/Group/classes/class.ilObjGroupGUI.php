1c30c8f453ae99f685e1280816b4b0df1b9cd281
Stefan Meyer <smeyer.ilias@gmx.de>
2010-03-09 21:09:28 +0000

    Fixed mantis 0004333: Last access date sorting wrong Vol.3
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@23152 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Group/classes/class.ilObjGroupGUI.php
+++ Modules/Group/classes/class.ilObjGroupGUI.php
@@ -962,7 +962,7 @@ class ilObjGroupGUI extends ilContainerGUI
 				{
 					$tmp_data['access_time'] = ilDatePresentation::formatDate(
 						$tmp_date = new ilDateTime($progress[$usr_id]['ts'],IL_CAL_UNIX));
-					$tmp_data['access_time_unix'] = $tmp_date->get('IL_CAL_UNIX');
+					$tmp_data['access_time_unix'] = $tmp_date->get(IL_CAL_UNIX);
 				}
 				else
 				{
