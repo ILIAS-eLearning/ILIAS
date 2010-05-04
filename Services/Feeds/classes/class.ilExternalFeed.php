590f93fec135e340332d40ff92aa2bce3ab28875
Alexander Killing <killing@leifos.de>
2010-05-04 13:26:46 +0000

    webfeed fix
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@23703 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Feeds/classes/class.ilExternalFeed.php
+++ Services/Feeds/classes/class.ilExternalFeed.php
@@ -112,6 +112,11 @@ class ilExternalFeed
 				define('IL_FEED_PROXY_HOST', ilProxySettings::_getInstance()->getHost());
 				define('IL_FEED_PROXY_PORT', ilProxySettings::_getInstance()->getPort());
 			}
+			else
+			{
+				define('IL_FEED_PROXY_HOST', "");
+				define('IL_FEED_PROXY_PORT', "");
+			}
 		}
 		
 		$feed = @fetch_rss($a_url);
@@ -234,6 +239,11 @@ class ilExternalFeed
 			{
 				define('IL_FEED_PROXY_HOST', ilProxySettings::_getInstance()->getHost());
 				define('IL_FEED_PROXY_PORT', ilProxySettings::_getInstance()->getPort());
+			}
+			else
+			{
+				define('IL_FEED_PROXY_HOST', "");
+				define('IL_FEED_PROXY_PORT', "");
 			}
 		}
 
