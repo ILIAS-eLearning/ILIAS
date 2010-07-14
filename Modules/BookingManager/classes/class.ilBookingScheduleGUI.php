b11de5ba25ee662714ff236604eb13b1033be6d4
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2010-07-14 14:58:22 +0000

    booking schedule
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@24622 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/BookingManager/classes/class.ilBookingScheduleGUI.php
+++ Modules/BookingManager/classes/class.ilBookingScheduleGUI.php
@@ -78,7 +78,7 @@ class ilBookingScheduleGUI
 		$ilTabs->clearTargets();
 		$ilTabs->setBackTarget($lng->txt('book_back_to_list'), $ilCtrl->getLinkTarget($this, 'render'));
 
-		$form = $this->initForm('edit', (int)$_GET['type_id']);
+		$form = $this->initForm('edit', (int)$_GET['schedule_id']);
 		$tpl->setContent($form->getHTML());
 	}
 
@@ -92,6 +92,8 @@ class ilBookingScheduleGUI
 	{
 		global $lng, $ilCtrl;
 
+		$lng->loadLanguageModule("dateplaner");
+
 		include_once("Services/Form/classes/class.ilPropertyFormGUI.php");
 
 		$form_gui = new ilPropertyFormGUI();
@@ -102,16 +104,108 @@ class ilBookingScheduleGUI
 		$title->setMaxLength(120);
 		$form_gui->addItem($title);
 
+		$type = new ilRadioGroupInputGUI($lng->txt("book_schedule_type"), "type");
+		$type->setRequired(true);
+		$form_gui->addItem($type);
+		$fix = new ilRadioOption($lng->txt("book_schedule_type_fix"), "fix");
+		$fix->setInfo($lng->txt("book_schedule_type_fix_info"));
+		$type->addOption($fix);
+		$flex = new ilRadioOption($lng->txt("book_schedule_type_flexible"), "flexible");
+		$flex->setInfo($lng->txt("book_schedule_type_flex_info"));
+		$type->addOption($flex);
+
+		$raster = new ilNumberInputGUI($lng->txt("book_raster"), "raster");
+		$raster->setRequired(true);
+		$raster->setInfo($lng->txt("book_raster_info"));
+		$raster->setMinValue(1);
+		$raster->setSize(3);
+		$raster->setMaxLength(3);
+		$raster->setSuffix($lng->txt("book_minutes"));
+		$flex->addSubItem($raster);
+
+		$rent_min = new ilNumberInputGUI($lng->txt("book_rent_min"), "rent_min");
+		$rent_min->setInfo($lng->txt("book_rent_info"));
+		$rent_min->setMinValue(1);
+		$rent_min->setSize(3);
+		$rent_min->setMaxLength(3);
+		$flex->addSubItem($rent_min);
+
+		$rent_max = new ilNumberInputGUI($lng->txt("book_rent_max"), "rent_max");
+		$rent_max->setInfo($lng->txt("book_rent_info"));
+		$rent_max->setMinValue(1);
+		$rent_max->setSize(3);
+		$rent_max->setMaxLength(3);
+		$flex->addSubItem($rent_max);
+
+		$break = new ilNumberInputGUI($lng->txt("book_break"), "break");
+		$break->setInfo($lng->txt("book_break_info"));
+		$break->setMinValue(1);
+		$break->setSize(3);
+		$break->setMaxLength(3);
+		$flex->addSubItem($break);
+
+		$definition = new ilCheckboxGroupInputGUI($lng->txt("book_days"), "days");
+		$definition->setRequired(true);
+		$form_gui->addItem($definition);
+
+		$days = array('mo', 'tu', 'we', 'th', 'fr', 'sa', 'su');
+		foreach($days as $day_id)
+		{
+			$day = new ilCheckboxOption($lng->txt(ucfirst($day_id)."_long"), $day_id);
+			$definition->addOption($day);
+			
+			for($loop = 1; $loop < 5; $loop++)
+		    {
+				$hours[$day_id][$loop] = new ilTextInputGUI($lng->txt("book_slot")." ".$loop, $day_id."_slot[]");
+				$hours[$day_id][$loop]->setSize(14);
+				$hours[$day_id][$loop]->setMaxLength(14);
+				$day->addSubItem($hours[$day_id][$loop]);
+			}
+		}
+
+		$deadline = new ilNumberInputGUI($lng->txt("book_deadline"), "deadline");
+		$deadline->setInfo($lng->txt("book_deadline_info"));
+		$deadline->setSuffix($lng->txt("book_minutes"));
+		$deadline->setMinValue(0);
+		$deadline->setSize(3);
+		$deadline->setMaxLength(3);
+		$form_gui->addItem($deadline);
+	
 		if ($a_mode == "edit")
 		{
-			$item = new ilHiddenInputGUI('type_id');
+			$form_gui->setTitle($lng->txt("book_edit_schedule"));
+
+			$item = new ilHiddenInputGUI('schedule_id');
 			$item->setValue($id);
 			$form_gui->addItem($item);
-			include_once 'Modules/BookingManager/classes/class.ilBookingSchedule.php';
-			$type = new ilBookingSchedule($id);
 
-			$form_gui->setTitle($lng->txt("book_edit_schedule"));
-			$title->setValue($type->getTitle());
+			include_once 'Modules/BookingManager/classes/class.ilBookingSchedule.php';
+			$schedule = new ilBookingSchedule($id);
+			$title->setValue($schedule->getTitle());
+			$deadline->setValue($schedule->getDeadline());
+			if($schedule->getRaster())
+			{
+				$type->setValue("flexible");
+				$raster->setValue($schedule->getRaster());
+				$rent_min->setValue($schedule->getMinRental());
+				$rent_max->setValue($schedule->getMaxRental());
+				$break->setValue($schedule->getAutoBreak());
+			}
+			else
+			{
+				$type->setValue("fix");
+			}
+
+			$def = $schedule->getDefinition();
+			$definition->setValue(array_keys($def));
+			foreach($def as $day_id => $slots)
+			{
+				foreach($slots as $idx => $slot)
+				{
+					$hours[$day_id][$idx+1]->setValue($slot);
+				}
+			}
+
 			$form_gui->addCommandButton("update", $lng->txt("save"));
 		}
 		else
@@ -130,15 +224,14 @@ class ilBookingScheduleGUI
 	 */
 	function save()
 	{
-		global $tpl, $ilObjDataCache, $ilCtrl, $lng;
+		global $tpl, $lng;
 
 		$form = $this->initForm();
 		if($form->checkInput())
 		{
 			include_once 'Modules/BookingManager/classes/class.ilBookingSchedule.php';
 			$obj = new ilBookingSchedule;
-			$obj->setTitle($form->getInput("title"));
-			$obj->setPoolId($ilObjDataCache->lookupObjId($this->ref_id));
+			$this->formToObject($form, $obj);
 			$obj->save();
 
 			ilUtil::sendSuccess($lng->txt("book_schedule_added"));
@@ -156,15 +249,14 @@ class ilBookingScheduleGUI
 	 */
 	function update()
 	{
-		global $tpl, $ilObjDataCache, $lng;
+		global $tpl, $lng;
 
-		$form = $this->initForm('edit', (int)$_POST['type_id']);
+		$form = $this->initForm('edit', (int)$_POST['schedule_id']);
 		if($form->checkInput())
 		{
 			include_once 'Modules/BookingManager/classes/class.ilBookingSchedule.php';
-			$obj = new ilBookingSchedule((int)$_POST['type_id']);
-			$obj->setTitle($form->getInput("title"));
-			$obj->setPoolId($ilObjDataCache->lookupObjId($this->ref_id));
+			$obj = new ilBookingSchedule((int)$_POST['schedule_id']);
+			$this->formToObject($form, $obj);
 			$obj->update();
 
 			ilUtil::sendSuccess($lng->txt("book_schedule_updated"));
@@ -175,6 +267,100 @@ class ilBookingScheduleGUI
 			$form->setValuesByPost();
 			$tpl->setContent($form->getHTML());
 		}
+	}
+
+	/**
+	 * Convert incoming form data to schedule object
+	 * @param	object	$form
+	 * @param	object	$schedule
+	 */
+	protected function formToObject($form, $schedule)
+	{
+		global $ilObjDataCache;
+		
+		$schedule->setTitle($form->getInput("title"));
+		$schedule->setPoolId($ilObjDataCache->lookupObjId($this->ref_id));
+		$schedule->setDeadline($form->getInput("deadline"));
+
+		if($form->getInput("type") == "flexible")
+		{
+			$schedule->setRaster($form->getInput("raster"));
+			$schedule->setMinRental($form->getInput("rent_min"));
+			$schedule->setMaxRental($form->getInput("rent_max"));
+			$schedule->setAutoBreak($form->getInput("break"));
+		}
+		else
+		{
+			$schedule->setRaster(NULL);
+			$schedule->setMinRental(NULL);
+			$schedule->setMaxRental(NULL);
+			$schedule->setAutoBreak(NULL);
+		}
+
+		$definition = array();
+		foreach($form->getInput("days") as $day_id)
+		{
+			$day_slots = array();
+			foreach($form->getInput($day_id."_slot") as $slot)
+			{
+				if(trim($slot))
+				{
+					$fromto = explode("-", $slot);
+					if(sizeof($fromto) == 2)
+					{
+						$from = $this->parseTime($fromto[0]);
+						$to = $this->parseTime($fromto[1]);
+						$definition[$day_id][] = $from."-".$to;
+					}
+				}
+			}
+			if(!sizeof($definition[$day_id]))
+			{
+				$definition[$day_id] = array("00:00-23:59");
+			}
+		}
+		$schedule->setDefinition($definition);
+	}
+
+	/**
+	 * Parse/normalize incoming time values
+	 * @param	string	$raw
+	 */
+	protected function parseTime($raw)
+    {
+		$raw = strtolower(trim($raw));
+		$am = $pm = false;
+		$min = 0;
+		if(substr($raw, -2) == 'pm')
+		{
+			$pm = true;
+			$raw = substr($raw, 0, -2);
+		}
+		if(substr($raw, -2) == 'am')
+		{
+			$am = true;
+			$raw = substr($raw, 0, -2);
+		}
+		if($colon = strpos($raw, ':'))
+		{
+			$min = (int)substr($raw, $colon);
+			$raw = substr($raw, 0, $colon);
+		}
+		$hours = (int)$raw;
+		if(!$min)
+		{
+			$min = "0";
+		}
+		if($pm && $hours < 12)
+		{
+			$hours += 12;
+		}
+		else if($am && $hours == 12)
+		{
+			$hours -= 12;
+		}
+		return str_pad($hours, 2, "0", STR_PAD_LEFT).":".
+			str_pad($min, 2, "0", STR_PAD_LEFT);
 	}
 }
 
