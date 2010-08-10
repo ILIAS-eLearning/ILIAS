af6690cc50b03ef1eb0bd72970c3576c22262e8e
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2010-08-10 13:56:36 +0000

    booking pool: removing calendar entries
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@25059 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Calendar/classes/ConsultationHours/class.ilConsultationHourAppointments.php
+++ Services/Calendar/classes/ConsultationHours/class.ilConsultationHourAppointments.php
@@ -18,17 +18,26 @@ class ilConsultationHourAppointments
 	/**
 	 * Get all appointment ids
 	 * @param object $a_user_id
+	 * @param int $a_context_id
+	 * @param string $a_start
+	 * @param int $a_type
 	 * @return 
 	 */
-	public static function getAppointmentIds($a_user_id, $a_context_id = NULL, $a_start = NULL)
+	public static function getAppointmentIds($a_user_id, $a_context_id = NULL, $a_start = NULL, $a_type = NULL)
 	{
 		global $ilDB;
+
+		if(!$a_type)
+		{
+			include_once './Services/Calendar/classes/class.ilCalendarCategory.php';
+			$a_type = ilCalendarCategory::TYPE_CH;
+		}
 		
 		$query = "SELECT ce.cal_id FROM cal_entries ce ".
 			"JOIN cal_cat_assignments cca ON ce.cal_id = cca.cal_id ".
 			"JOIN cal_categories cc ON cca.cat_id = cc.cat_id ".
 			"WHERE obj_id = ".$ilDB->quote($a_user_id,'integer')." ".
-			"AND type = ".$ilDB->quote(ilCalendarCategory::TYPE_CH);
+			"AND type = ".$ilDB->quote($a_type);
 
 		if($a_context_id)
 		{
