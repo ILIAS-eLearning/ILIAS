bab7ba2bbe579029958e21c4a0f3b0271317809c
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2010-09-20 12:46:35 +0000

    lp: use default mode when undefined
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@25733 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Tracking/classes/class.ilLPStatusFactory.php
+++ Services/Tracking/classes/class.ilLPStatusFactory.php
@@ -16,11 +16,16 @@
 */
 class ilLPStatusFactory
 {
-	function _getClassById($a_obj_id)
+	function _getClassById($a_obj_id, $a_mode = NULL)
 	{
 		include_once 'Services/Tracking/classes/class.ilLPObjSettings.php';
 
-		switch(ilLPObjSettings::_lookupMode($a_obj_id))
+		if($a_mode === NULL)
+		{
+			$a_mode = ilLPObjSettings::_lookupMode($a_obj_id);
+		}
+
+		switch($a_mode)
 		{
 			case LP_MODE_VISITS:
 				include_once 'Services/Tracking/classes/class.ilLPStatusVisits.php';
@@ -74,6 +79,11 @@ class ilLPStatusFactory
 				include_once('./Services/Tracking/classes/class.ilLPStatusEvent.php');
 				return 'ilLPStatusEvent';
 
+			case LP_MODE_UNDEFINED:
+				$type = ilObject::_lookupType($a_obj_id);
+				$mode = ilLPObjSettings::__getDefaultMode($a_obj_id, $type);
+				return self::_getClassById($a_obj_id, $mode);
+
 			default:
 				echo "ilLPStatusFactory: unknown type ".ilLPObjSettings::_lookupMode($a_obj_id);
 				exit;
@@ -95,11 +105,16 @@ class ilLPStatusFactory
 		}
 	}
 
-	function &_getInstance($a_obj_id)
+	function &_getInstance($a_obj_id, $a_mode = NULL)
 	{
 		include_once 'Services/Tracking/classes/class.ilLPObjSettings.php';
+
+		if($a_mode === NULL)
+		{
+			$a_mode = ilLPObjSettings::_lookupMode($a_obj_id);
+		}
 		
-		switch(ilLPObjSettings::_lookupMode($a_obj_id))
+		switch($a_mode)
 		{
 			case LP_MODE_VISITS:
 				include_once 'Services/Tracking/classes/class.ilLPStatusVisits.php';
@@ -111,49 +126,44 @@ class ilLPStatusFactory
 
 			case LP_MODE_TLT:
 				include_once 'Services/Tracking/classes/class.ilLPStatusTypicalLearningTime.php';
-
 				return new ilLPStatusTypicalLearningTime($a_obj_id);
 
 			case LP_MODE_SCORM:
 				include_once 'Services/Tracking/classes/class.ilLPStatusSCORM.php';
-				
 				return new ilLPStatusSCORM($a_obj_id);
 
 			case LP_MODE_TEST_FINISHED:
 				include_once 'Services/Tracking/classes/class.ilLPStatusTestFinished.php';
-				
 				return new ilLPStatusTestFinished($a_obj_id);
 
 			case LP_MODE_TEST_PASSED:
 				include_once 'Services/Tracking/classes/class.ilLPStatusTestPassed.php';
-				
 				return new ilLPStatusTestPassed($a_obj_id);
 
 			case LP_MODE_MANUAL:
 				include_once 'Services/Tracking/classes/class.ilLPStatusManual.php';
-				
 				return new ilLPStatusManual($a_obj_id);
 
 			case LP_MODE_MANUAL_BY_TUTOR:
 				include_once 'Services/Tracking/classes/class.ilLPStatusManualByTutor.php';
-				
 				return new ilLPStatusManualByTutor($a_obj_id);
 
 			case LP_MODE_EXERCISE_RETURNED:
 				include_once 'Services/Tracking/classes/class.ilLPStatusExerciseReturned.php';
-
 				return new ilLPStatusExerciseReturned($a_obj_id);
 
 			case LP_MODE_OBJECTIVES:
 				include_once 'Services/Tracking/classes/class.ilLPStatusObjectives.php';
-
 				return new ilLPStatusObjectives($a_obj_id);
 				
 			case LP_MODE_EVENT:
 				include_once 'Services/Tracking/classes/class.ilLPStatusEvent.php';
-
 				return new ilLPStatusEvent($a_obj_id);
 				
+			case LP_MODE_UNDEFINED:
+				$type = ilObject::_lookupType($a_obj_id);
+				$mode = ilLPObjSettings::__getDefaultMode($a_obj_id, $type);
+				return self::_getInstance($a_obj_id, $mode);
 
 			default:
 				echo "ilLPStatusFactory: unknown type ".ilLPObjSettings::_lookupMode($a_obj_id);
