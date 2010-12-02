ed84c96287c71d186b703207d2594b6476bae05f
Alexander Killing <killing@leifos.de>
2010-12-02 11:13:52 +0000

    fixed bug #6883: Formatting question in Learning modules
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@26808 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/TestQuestionPool/classes/class.assClozeTest.php
+++ Modules/TestQuestionPool/classes/class.assClozeTest.php
@@ -1432,7 +1432,7 @@ class assClozeTest extends assQuestion
 		$result['id'] = (int) $this->getId();
 		$result['type'] = (string) $this->getQuestionType();
 		$result['title'] = (string) $this->getTitle();
-		$result['question'] =  (string) ilRTE::_replaceMediaObjectImageSrc($this->getQuestion(), 0);
+		$result['question'] =  (string) nl2br(ilRTE::_replaceMediaObjectImageSrc($this->getQuestion(), 0));
 		$result['nr_of_tries'] = (int) $this->getNrOfTries();
 		$result['shuffle'] = (bool) $this->getShuffle();
 		$result['feedback'] = array(
