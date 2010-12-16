69a45494ac672a47f4df2455be6f1e3d89265930
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2010-12-16 11:48:05 +0000

    scorm self-assesment: exporting thumbnail images, importing images vs. multi-line answers
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@26975 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/TestQuestionPool/classes/import/qti12/class.assSingleChoiceImport.php
+++ Modules/TestQuestionPool/classes/import/qti12/class.assSingleChoiceImport.php
@@ -269,6 +269,10 @@ class assSingleChoiceImport extends assQuestionImport
 		$this->object->setThumbSize($item->getMetadataEntry("thumb_size"));
 		foreach ($answers as $answer)
 		{
+			if (is_array($answer["imagefile"]) && (count($answer["imagefile"]) > 0))
+			{
+				$this->object->isSingleline = true;
+			}
 			$this->object->addAnswer($answer["answertext"], $answer["points"], $answer["answerorder"], $answer["imagefile"]["label"]);
 		}
 		$this->object->saveToDb();
