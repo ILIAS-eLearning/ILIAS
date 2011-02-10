0d8bb8f435a7e58567cd8f0318d1080288813463
Alexander Killing <killing@leifos.de>
2011-02-10 14:20:33 +0000

    cleaner way to create assets
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@27705 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Scorm2004/classes/class.ilSCORM2004Asset.php
+++ Modules/Scorm2004/classes/class.ilSCORM2004Asset.php
@@ -25,5 +25,20 @@ class ilSCORM2004Asset extends ilSCORM2004Sco
 		parent::ilSCORM2004Node($a_slm_object, $a_id);
 		$this->setType("ass");
 	}
+
+	/**
+	 * Create asset
+	 */
+	function create($a_upload = false, $a_template = false)
+	{
+		include_once("./Modules/Scorm2004/classes/seq_editor/class.ilSCORM2004Item.php");
+		include_once("./Modules/Scorm2004/classes/seq_editor/class.ilSCORM2004Objective.php");
+		parent::create($a_upload, false, true);
+		if (!$a_template) {
+			$seq_item = new ilSCORM2004Item($this->getId());
+			$seq_item->insert();
+		}
+	}
+
 }
 ?>
