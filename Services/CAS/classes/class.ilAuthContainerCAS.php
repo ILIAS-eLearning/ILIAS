6c85d22482a228b54f57563a0ef0f6749b38b4f0
Stefan Meyer <smeyer.ilias@gmx.de>
2011-02-22 19:36:58 +0000

    Fixed CAS with ldap data source
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@27833 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/CAS/classes/class.ilAuthContainerCAS.php
+++ Services/CAS/classes/class.ilAuthContainerCAS.php
@@ -54,9 +54,15 @@ class ilAuthContainerCAS extends Auth_Container
 		global $ilias, $rbacadmin, $ilSetting,$ilLog,$PHPCAS_CLIENT;
 		
 		$ilLog->write(__METHOD__.': Successful CAS login.');
+
+		// Radius with ldap as data source
+		include_once './Services/LDAP/classes/class.ilLDAPServer.php';
+		if(ilLDAPServer::isDataSourceActive(AUTH_CAS))
+		{
+			return $this->handleLDAPDataSource($a_auth,$a_username);
+		}
 		
 		include_once("./Services/CAS/lib/CAS.php");
-		
 		if ($PHPCAS_CLIENT->getUser() != "")
 		{
 			$username = $PHPCAS_CLIENT->getUser();
@@ -142,6 +148,54 @@ class ilAuthContainerCAS extends Auth_Container
 		}
 		return false;
     }
+
+	/**
+	 * Handle ldap as data source
+	 * @param Auth $auth
+	 * @param string $ext_account
+	 */
+	protected function handleLDAPDataSource($a_auth,$ext_account)
+	{
+		include_once './Services/LDAP/classes/class.ilLDAPServer.php';
+		$server = ilLDAPServer::getInstanceByServerId(
+			ilLDAPServer::getDataSource(AUTH_CAS)
+		);
+
+		$GLOBALS['ilLog']->write(__METHOD__.' Using ldap data source for user: '.$ext_account);
+
+		include_once './Services/LDAP/classes/class.ilLDAPUserSynchronisation.php';
+		$sync = new ilLDAPUserSynchronisation('cas', $server->getServerId());
+		$sync->setExternalAccount($ext_account);
+		$sync->setUserData(array());
+		#$sync->forceCreation($this->force_creation);
+		// TODO: Check this
+		$sync->forceCreation(true);
+
+		try {
+			$internal_account = $sync->sync();
+		}
+		catch(UnexpectedValueException $e) {
+			$GLOBALS['ilLog']->write(__METHOD__.': Login failed with message: '. $e->getMessage());
+			$a_auth->status = AUTH_WRONG_LOGIN;
+			$a_auth->logout();
+			return false;
+		}
+		catch(ilLDAPSynchronisationForbiddenException $e) {
+			// No syncronisation allowed => create Error
+			$GLOBALS['ilLog']->write(__METHOD__.': Login failed with message: '. $e->getMessage());
+			$a_auth->status = AUTH_CAS_NO_ILIAS_USER;
+			$a_auth->logout();
+			return false;
+		}
+		catch(ilLDAPAccountMigrationRequiredException $e) {
+			$GLOBALS['ilLog']->write(__METHOD__.': Starting account migration.');
+			$a_auth->logout();
+			ilUtil::redirect('ilias.php?baseClass=ilStartUpGUI&cmdClass=ilstartupgui&cmd=showAccountMigration');
+		}
+		$a_auth->setAuth($internal_account);
+		return true;
+	}
+
 	
 	
 	/**
