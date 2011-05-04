ab687e4052d159158a44d9f7c6e99da1ee364ab0
hschottm <hschottm@svn.ilias.de>
2011-05-04 16:56:17 +0000

    Merged r28819
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@28820 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Survey/classes/class.ilObjSurveyGUI.php
+++ Modules/Survey/classes/class.ilObjSurveyGUI.php
@@ -2091,7 +2091,7 @@ class ilObjSurveyGUI extends ilObjectGUI
 					'id' => $user_data["active_id"],
 					'name' => $user_data["sortname"],
 					'login' => $user_data["login"],
-					'last_access' => ilDatePresentation::formatDate(new ilDateTime($last_access,IL_CAL_UNIX))
+					'last_access' => $last_access
 				));
 			}
 		}
@@ -2121,7 +2121,7 @@ class ilObjSurveyGUI extends ilObjectGUI
 				'id' => $user_data["active_id"],
 				'name' => $user_data["sortname"],
 				'login' => $user_data["login"],
-				'last_access' => ilDatePresentation::formatDate(new ilDateTime($last_access,IL_CAL_UNIX))
+				'last_access' => $last_access
 			));
 		}
 		$table_gui->setData($data);
