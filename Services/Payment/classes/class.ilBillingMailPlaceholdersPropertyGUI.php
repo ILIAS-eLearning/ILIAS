4fa15903827dae8d0df769a60843ed47b7a851b7
Nadia Ahmad <nahmad@databay.de>
2011-05-11 08:04:20 +0000

    added missing function
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@28886 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Payment/classes/class.ilBillingMailPlaceholdersPropertyGUI.php
+++ Services/Payment/classes/class.ilBillingMailPlaceholdersPropertyGUI.php
@@ -44,13 +44,41 @@ class ilBillingMailPlaceholdersPropertyGUI extends ilFormPropertyGUI
 	}
 
 
-	public function replaceBillingMailPlaceholders()
+	public static function replaceBillingMailPlaceholders($a_message, $a_user_id)
 	{
+		global $lng;
 
+		$user = new ilObjUser($a_user_id);
+
+		// determine salutation
+		switch ($user->getGender())
+		{
+			case 'f':	$gender_salut = $lng->txt('salutation_f');
+						break;
+			case 'm':	$gender_salut = $lng->txt('salutation_m');
+						break;
+        }
+
+		$a_message = str_replace('[MAIL_SALUTATION]', $gender_salut, $a_message);
+		$a_message = str_replace('[LOGIN]', $user->getLogin(), $a_message);
+		$a_message = str_replace('[FIRST_NAME]', $user->getFirstname(), $a_message);
+		$a_message = str_replace('[LAST_NAME]', $user->getLastname(), $a_message);
+		$a_message = str_replace('[ILIAS_URL]', ILIAS_HTTP_PATH.'/login.php?client_id='.CLIENT_ID, $a_message);
+		$a_message = str_replace('[CLIENT_NAME]', CLIENT_NAME, $a_message);
+
+
+		include_once './Services/Payment/classes/class.ilShopLinkBuilder.php';
+		$shopLB = new ilShopLinkBuilder();
+		$bought_objects_url = $shopLB->buildLink('ilShopBoughtObjectsGUI');
+		$shop_url = $shopLB->buildLink('ilShopGUI');
+
+		$a_message = str_replace('[SHOP_BOUGHT_OBJECTS_URL]', $bought_objects_url, $a_message);
+		$a_message = str_replace('[SHOP_URL]', $shop_url, $a_message);
+
+		unset($user);
+
+		return $a_message;
 	}
-
-
-
 }
 
 ?>
