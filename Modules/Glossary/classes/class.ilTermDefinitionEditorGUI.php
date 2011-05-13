3830b5da16a2f256faef50924cbecb5ad97a8a74
Alexander Killing <killing@leifos.de>
2011-05-13 13:59:29 +0000

    fixed bug 7220: couldnt set internal link into glossary
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@29002 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Glossary/classes/class.ilTermDefinitionEditorGUI.php
+++ Modules/Glossary/classes/class.ilTermDefinitionEditorGUI.php
@@ -142,8 +142,10 @@ class ilTermDefinitionEditorGUI
 				$page_gui->setTemplateTargetVar("ADM_CONTENT");
 				$page_gui->setOutputMode("edit");
 				$page_gui->setLocator($gloss_loc);
+				$page_gui->setEnabledInternalLinks(true);
 				$page_gui->setIntLinkHelpDefault("GlossaryItem", $_GET["ref_id"]);
-				$page_gui->setIntLinkReturn($this->ctrl->getLinkTargetByClass("ilobjglossarygui", "quickList"));
+				$page_gui->setIntLinkReturn($this->ctrl->getLinkTargetByClass("ilobjglossarygui", "quickList",
+					"", false, false));
 				$page_gui->setPageBackTitle($this->lng->txt("cont_definition"));
 				$page_gui->setLinkParams("ref_id=".$_GET["ref_id"]);
 				$page_gui->setHeader($this->term->getTerm());
