8bf5cbfd94bce25b202e2d49be853928a6cb2b65
Alexander Killing <killing@leifos.de>
2011-07-06 20:59:53 +0000

    keep last visited in db
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@29779 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Navigation/classes/class.ilNavigationHistory.php
+++ Services/Navigation/classes/class.ilNavigationHistory.php
@@ -1,25 +1,6 @@
 <?php
-/*
-	+-----------------------------------------------------------------------------+
-	| ILIAS open source                                                           |
-	+-----------------------------------------------------------------------------+
-	| Copyright (c) 1998-2006 ILIAS open source, University of Cologne            |
-	|                                                                             |
-	| This program is free software; you can redistribute it and/or               |
-	| modify it under the terms of the GNU General Public License                 |
-	| as published by the Free Software Foundation; either version 2              |
-	| of the License, or (at your option) any later version.                      |
-	|                                                                             |
-	| This program is distributed in the hope that it will be useful,             |
-	| but WITHOUT ANY WARRANTY; without even the implied warranty of              |
-	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
-	| GNU General Public License for more details.                                |
-	|                                                                             |
-	| You should have received a copy of the GNU General Public License           |
-	| along with this program; if not, write to the Free Software                 |
-	| Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
-	+-----------------------------------------------------------------------------+
-*/
+
+/* Copyright (c) 1998-2011 ILIAS open source, Extended GPL, see docs/LICENSE */
 
 
 /**
@@ -56,8 +37,13 @@ class ilNavigationHistory
 	* Add an item to the stack. If ref_id is already used,
 	* the item is moved to the top.
 	*/
-	public function addItem($a_ref_id, $a_link, $a_type, $a_title = "", $a_sub_obj_id = "")
+	public function addItem($a_ref_id, $a_link, $a_type, $a_title = "", $a_sub_obj_id = "",
+		$a_goto_link = "")
 	{
+		global $ilUser, $ilDB;
+		
+		$a_sub_obj_id = $a_sub_obj_id."";
+		
 		if ($a_title == "" && $a_ref_id > 0)
 		{
 			$obj_id = ilObject::_lookupObjId($a_ref_id);
@@ -69,7 +55,7 @@ class ilNavigationHistory
 
 		$id = $a_ref_id.":".$a_sub_obj_id;
 
-		// remove ref id from stack, if existing
+		// remove id from stack, if existing
 		foreach($this->items as $key => $item)
 		{
 			if ($item["id"] == $id)
@@ -78,13 +64,54 @@ class ilNavigationHistory
 				break;
 			}
 		}
+		// same in db
+		$ilDB->manipulate($q = "DELETE FROM last_visited WHERE ".
+			" user_id = ".$ilDB->quote($ilUser->getId(), "integer").
+			" AND ref_id = ".$ilDB->quote($a_ref_id, "integer").
+			" AND ".$ilDB->equals("sub_obj_id", $a_sub_obj_id, "text", true)
+			);
 		
+		// put items in session
 		$this->items = array_merge(
 			array(array("id" => $id,"ref_id" => $a_ref_id, "link" => $a_link, "title" => $a_title,
 			"type" => $a_type)), $this->items);
-		
 		$items  = serialize($this->items);
 		$_SESSION["il_nav_history"] = $items;
+		
+		// and into database
+		$db_entries[] = array("user_id" => $ilUser->getId(), "nr" => 1,
+			"ref_id" => $a_ref_id, "type" => $a_type, "sub_obj_id" => $a_sub_obj,
+			"goto_link" => $a_goto_link);
+		$set = $ilDB->query("SELECT * FROM last_visited ".
+			" WHERE user_id = ".$ilDB->quote($ilUser->getId(), "integer").
+			" ORDER BY nr ASC"
+			);
+		$cnt = 1;
+		while ($rec = $ilDB->fetchAssoc($set))
+		{
+			$cnt++;
+			$rec["nr"] = $cnt;
+			$db_entries[] = $rec;
+		}
+		// same in db
+		$ilDB->manipulate("DELETE FROM last_visited WHERE ".
+			" user_id = ".$ilDB->quote($ilUser->getId(), "integer")
+			);
+		foreach ($db_entries as $e)
+		{
+			if ($e["nr"] <= 10)
+			{
+				$ilDB->manipulate("INSERT INTO last_visited ".
+					"(user_id, nr, ref_id, type, sub_obj_id, goto_link) VALUES (".
+					$ilDB->quote($ilUser->getId(), "integer").",".
+					$ilDB->quote($e["nr"], "integer").",".
+					$ilDB->quote($e["ref_id"], "integer").",".
+					$ilDB->quote($e["type"], "text").",".
+					$ilDB->quote($e["sub_obj_id"], "text").",".
+					$ilDB->quote($e["goto_link"], "text").
+					")");
+			}
+		}
 	}
 	
 	/**
@@ -92,7 +119,7 @@ class ilNavigationHistory
 	*/
 	public function getItems()
 	{
-		global $tree;
+		global $tree, $ilDB, $ilUser;
 		
 		$items = array();
 		
@@ -100,7 +127,33 @@ class ilNavigationHistory
 		{
 			if ($tree->isInTree($it["ref_id"]))
 			{
-				$items[] = $it;
+				$items[$it["ref_id"].":".$it["sub_obj_id"]] = $it;
+			}
+		}
+		
+		// less than 10? -> get items from db
+		if (count($items) < 10)
+		{
+			$set = $ilDB->query("SELECT * FROM last_visited ".
+				" WHERE user_id = ".$ilDB->quote($ilUser->getId(), "integer").
+				" ORDER BY nr ASC"
+				);
+			$cnt = count($items);
+			while ($rec = $ilDB->fetchAssoc($set))
+			{
+				include_once("./classes/class.ilLink.php");
+				
+				if ($cnt <= 10 && ! isset($items[$rec["ref_id"].":".$rec["sub_obj_id"]]))
+				{
+					$link = $rec["goto_link"] != ""
+						? $rec["goto_link"]
+						: ilLink::_getLink($rec["ref_id"]);
+					$title = ilObject::_lookupTitle(ilObject::_lookupObjId($rec["ref_id"]));
+					$items[] = array("id" => $rec["ref_id"].":".$rec["sub_obj_id"],
+						"ref_id" => $rec["ref_id"], "link" => $link, "title" => $title,
+						"type" => $rec["type"]);
+					$cnt++;
+				}
 			}
 		}
 		
