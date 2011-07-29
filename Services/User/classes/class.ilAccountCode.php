6c54411b404dd1ef5b7cee0bac979a869dc61423
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2011-07-29 10:20:24 +0000

    user accounts: codes
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@30082 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ Services/User/classes/class.ilAccountCode.php
@@ -0,0 +1,204 @@
+<?php
+
+/* Copyright (c) 1998-2009 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+/**
+* Class ilAccountCode
+*
+* @author Jörg Lützenkirchen <luetzenkirchen@leifos.com>
+* @version $Id: class.ilRegistrationSettingsGUI.php 23797 2010-05-07 15:54:03Z jluetzen $
+*
+* @ingroup ServicesUser
+*/
+class ilAccountCode
+{
+	const DB_TABLE = 'usr_account_codes';
+	const CODE_LENGTH = 10;
+	
+	public static function create($valid_until, $stamp)
+	{
+		global $ilDB;
+		
+		$id = $ilDB->nextId(self::DB_TABLE);
+		
+		// create unique code
+		$found = true;
+		while ($found)
+		{
+			$code = self::generateRandomCode();
+ 			$chk = $ilDB->queryF("SELECT code_id FROM ".self::DB_TABLE." WHERE code = %s", array("text"), array($code));
+			$found = (bool)$ilDB->numRows($chk);
+		}
+		
+		$data = array(
+			'code_id' => array('integer', $id),
+			'code' => array('text', $code),
+			'generated' => array('integer', $stamp),
+			'valid_until' => array('text', $valid_until)
+			);
+
+		$ilDB->insert(self::DB_TABLE, $data);
+		return $id;
+	}
+	
+	protected static function generateRandomCode()
+	{
+		// missing : 01iloO
+		$map = "23456789abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";
+		
+		$code = "";
+		$max = strlen($map)-1;
+		for($loop = 1; $loop <= self::CODE_LENGTH; $loop++)
+		{
+		  $code .= $map[mt_rand(0, $max)];
+		}
+		return $code;
+	}
+	
+	public static function getCodesData($order_field, $order_direction, $offset, $limit, $filter_code, $filter_valid_until, $filter_generated)
+	{
+		global $ilDB;
+		
+		// filter
+		$where = self::filterToSQL($filter_code, $filter_valid_until, $filter_generated);
+
+		// count query
+		$set = $ilDB->query("SELECT COUNT(*) AS cnt FROM ".self::DB_TABLE.$where);
+		$cnt = 0;
+		if ($rec = $ilDB->fetchAssoc($set))
+		{
+			$cnt = $rec["cnt"];
+		}
+		
+		$sql = "SELECT * FROM ".self::DB_TABLE.$where;
+		if($order_field)
+		{
+			$sql .= " ORDER BY ".$order_field." ".$order_direction;
+		}
+		
+		// set query
+		$ilDB->setLimit((int)$limit, (int)$offset);
+		$set = $ilDB->query($sql);
+		$result = array();
+		while($rec = $ilDB->fetchAssoc($set))
+		{
+			$result[] = $rec;
+		}
+		return array("cnt" => $cnt, "set" => $result);
+	}
+	
+	public static function loadCodesByIds(array $ids)
+	{
+		global $ilDB;
+
+		$set = $ilDB->query("SELECT * FROM ".self::DB_TABLE." WHERE ".$ilDB->in("code_id", $ids, false, "integer"));
+		$result = array();
+		while($rec = $ilDB->fetchAssoc($set))
+		{
+			$result[] = $rec;
+		}
+		return $result;
+	}
+	
+	public static function deleteCodes(array $ids)
+	{
+		global $ilDB;
+
+		if(sizeof($ids))
+		{
+			return $ilDB->manipulate("DELETE FROM ".self::DB_TABLE." WHERE ".$ilDB->in("code_id", $ids, false, "integer"));
+		}
+		return false;
+	}
+	
+	public static function getGenerationDates()
+	{
+		global $ilDB;
+		
+		$set = $ilDB->query("SELECT DISTINCT(generated) AS generated FROM ".self::DB_TABLE." ORDER BY generated");
+		$result = array();
+		while($rec = $ilDB->fetchAssoc($set))
+		{
+			$result[] = $rec["generated"];
+		}
+		return $result;
+	}
+	
+	protected static function filterToSQL($filter_code, $filter_valid_until, $filter_generated)
+	{
+		global $ilDB;
+
+		$where = array();
+		if($filter_code)
+		{
+			$where[] = $ilDB->like("code", "text", "%".$filter_code."%");
+		}
+		if($filter_valid_until)
+		{
+			$where[] ="valid_until = ".$ilDB->quote($filter_valid_until, "text");
+		}
+		if($filter_generated)
+		{
+			$where[] ="generated = ".$ilDB->quote($filter_generated, "text");
+		}
+		if(sizeof($where))
+		{
+			return " WHERE ".implode(" AND ", $where);
+		}
+		else
+		{
+			return "";
+		}
+	}
+	
+	public static function getCodesForExport($filter_code, $filter_valid_until, $filter_generated)
+	{
+		global $ilDB;
+
+		// filter
+		$where = self::filterToSQL($filter_code, $filter_valid_until, $filter_generated);
+
+		// set query
+		$set = $ilDB->query("SELECT code FROM ".self::DB_TABLE.$where." ORDER BY code_id");
+		$result = array();
+		while($rec = $ilDB->fetchAssoc($set))
+		{
+			$result[] = $rec["code"];
+		}
+		return $result;
+	}
+	
+	public static function isUnusedCode($code)
+	{
+		global $ilDB;
+		
+		$set = $ilDB->query("SELECT used FROM ".self::DB_TABLE." WHERE code = ".$ilDB->quote($code, "text"));
+		$set = $ilDB->fetchAssoc($set);
+		if($set && !$set["used"])
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	public static function useCode($code)
+	{
+		global $ilDB;
+
+		return (bool)$ilDB->update(self::DB_TABLE, array("used"=>array("timestamp", time())), array("code"=>array("text", $code)));
+	}
+
+	public static function getCodeValidUntil($code)
+    {
+		global $ilDB;
+
+		$set = $ilDB->query("SELECT valid_until FROM ".self::DB_TABLE." WHERE code = ".$ilDB->quote($code, "text"));
+		$row = $ilDB->fetchAssoc($set);
+		if(isset($row["valid_until"]))
+		{
+			return $row["valid_until"];
+		}
+	}
+}
+
+?>
---
+++ Services/User/classes/class.ilAccountCodesGUI.php
@@ -0,0 +1,220 @@
+<?php
+/* Copyright (c) 1998-2009 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+/**
+* GUI class for account codes
+*
+* @author Jörg Lützenkirchen <luetzenkirchen@leifos.com>
+* @version $Id$
+*
+* @ilCtrl_Calls ilAccountCodesGUI:
+* @ingroup ServicesUser
+*/
+class ilAccountCodesGUI 
+{	
+	protected $ref_id; // [int]
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param int $a_ref_id
+	 */
+	function __construct($a_ref_id)
+	{
+		global $lng;
+
+		$this->ref_id = $a_ref_id;
+		$lng->loadLanguageModule("user");
+	}
+	
+	function executeCommand()
+	{
+		global $ilCtrl;
+		
+		$next_class = $ilCtrl->getNextClass($this);
+		$cmd = $ilCtrl->getCmd();
+		
+		switch($next_class)
+		{
+			default:
+				if(!$cmd)
+				{
+					$cmd = "listCodes";
+				}
+				$this->$cmd();
+				break;
+		}
+		
+		return true;
+	}
+	
+	function listCodes()
+	{
+		global $ilAccess, $ilErr, $ilCtrl, $ilToolbar, $lng, $tpl;
+
+		if(!$ilAccess->checkAccess('read','',$this->ref_id))
+		{
+			$ilErr->raiseError($lng->txt("msg_no_perm_read"),$ilErr->MESSAGE);
+		}
+
+		$ilToolbar->addButton($lng->txt("account_codes_add"),
+			$ilCtrl->getLinkTarget($this, "addCodes"));
+
+		include_once("./Services/User/classes/class.ilAccountCodesTableGUI.php");
+		$ctab = new ilAccountCodesTableGUI($this, "listCodes");
+		$tpl->setContent($ctab->getHTML());
+	}
+	
+	function initAddCodesForm()
+	{
+		global $ilCtrl, $lng;
+		
+		include_once 'Services/Form/classes/class.ilPropertyFormGUI.php';
+
+		$this->form_gui = new ilPropertyFormGUI();
+		$this->form_gui->setFormAction($ilCtrl->getFormAction($this, 'createCodes'));
+		$this->form_gui->setTitle($lng->txt('account_codes_edit_header'));
+		
+		$count = new ilNumberInputGUI($lng->txt('account_codes_number'), 'acc_codes_number');
+		$count->setSize(4);
+		$count->setMaxLength(4);
+		$count->setMinValue(1);
+		$count->setMaxValue(1000);
+		$count->setRequired(true);
+		$this->form_gui->addItem($count);
+
+		$this->form_gui->addCommandButton('createCodes', $lng->txt('create'));
+		$this->form_gui->addCommandButton('listCodes', $lng->txt('cancel'));
+	}
+	
+	function addCodes()
+	{
+		global $ilAccess, $ilErr, $tpl, $lng;
+
+		if(!$ilAccess->checkAccess('write', '', $this->ref_id))
+		{
+			$ilErr->raiseError($lng->txt("msg_no_perm_write"), $ilErr->MESSAGE);
+		}
+	
+		$this->initAddCodesForm();
+		$tpl->setContent($this->form_gui->getHTML());
+	}
+	
+	function createCodes()
+	{
+		global $ilAccess, $ilErr, $lng, $tpl;
+
+		if(!$ilAccess->checkAccess('write', '', $this->ref_id))
+		{
+			$ilErr->raiseError($lng->txt("msg_no_perm_write"), $ilErr->MESSAGE);
+		}
+		
+		$this->initAddCodesForm();
+		if($this->form_gui->checkInput())
+		{
+			$number = $this->form_gui->getInput('acc_codes_number');
+			
+			include_once './Services/User/classes/class.ilAccountCode.php';
+			
+			$stamp = time();
+			for($loop = 1; $loop <= $number; $loop++)
+			{
+				ilAccountCode::create($role, $stamp);
+			}
+			
+			ilUtil::sendSuccess($lng->txt('saved_successfully'), true);
+			$this->ctrl->redirect($this, "listCodes");
+		}
+		else
+		{
+			$this->form_gui->setValuesByPost();
+			$tpl->setContent($this->form_gui->getHtml());
+		}
+	}
+	
+	function deleteCodes()
+	{
+		global $lng, $ilCtrl;
+		
+		include_once './Services/User/classes/class.ilAccountCode.php';
+		ilAccountCode::deleteCodes($_POST["id"]);
+		
+		ilUtil::sendSuccess($lng->txt('info_deleted'), true);
+		$ilCtrl->redirect($this, "listCodes");
+	}
+
+	function deleteConfirmation()
+	{
+		global $ilErr, $lng, $ilCtrl, $tpl;
+
+		if(!isset($_POST["id"]))
+		{
+			$ilErr->raiseError($lng->txt("no_checkbox"), $ilErr->MESSAGE);
+		}
+	
+		include_once './Services/Utilities/classes/class.ilConfirmationGUI.php';
+		$gui = new ilConfirmationGUI();
+		$gui->setHeaderText($lng->txt("info_delete_sure"));
+		$gui->setCancel($lng->txt("cancel"), "listCodes");
+		$gui->setConfirm($lng->txt("confirm"), "deleteCodes");
+		$gui->setFormAction($ilCtrl->getFormAction($this, "deleteCodes"));
+		
+		include_once './Services/User/classes/class.ilAccountCode.php';
+		$data = ilAccountCode::loadCodesByIds($_POST["id"]);
+		foreach($data as $code)
+		{
+			$gui->addItem("id[]", $code["code_id"], $code["code"]);
+		}
+
+		$tpl->setContent($gui->getHTML());
+	}
+	
+	function resetCodesFilter()
+	{
+		include_once("./Services/User/classes/class.ilAccountCodesTableGUI.php");
+		$utab = new ilAccountCodesTableGUI($this, "listCodes");
+		$utab->resetOffset();
+		$utab->resetFilter();
+		
+		$this->listCodes();
+	}
+	
+	function applyCodesFilter()
+	{
+		include_once("./Services/User/classes/class.ilAccountCodesTableGUI.php");
+		$utab = new ilAccountCodesTableGUI($this, "listCodes");
+		$utab->resetOffset();
+		$utab->writeFilterToSession();
+		
+		$this->listCodes();
+	}
+	
+	function exportCodes()
+	{
+		global $ilAccess, $ilErr, $lng;
+
+		if(!$ilAccess->checkAccess('read', '', $this->ref_id))
+		{
+			$ilErr->raiseError($lng->txt("msg_no_perm_read"), $ilErr->MESSAGE);
+		}
+		
+		include_once("./Services/User/classes/class.ilAccountCodesTableGUI.php");
+		$utab = new ilAccountCodesTableGUI($this, "listCodes");
+		
+		include_once './Services/User/classes/class.ilAccountCode.php';
+		$codes = ilAccountCode::getCodesForExport($utab->filter["code"], $utab->filter["valid_until"], $utab->filter["generated"]);
+
+		if(sizeof($codes))
+		{
+			// :TODO: add url/link to login?!
+			ilUtil::deliverData(implode("\n", $codes), "ilias_account_codes_".date("d-m-Y").".txt","text/plain");
+		}
+		else
+		{
+			ilUtil::sendFailure($this->lng->txt("account_export_codes_no_data"));
+			$this->listCodes();
+		}
+	}
+}
+
+?>
