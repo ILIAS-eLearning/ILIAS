1fb8385ab998a1d2f6d5053066ad2a9971bd5665
Uwe Kohnle <kohnle@internetlehrer-gmbh.de>
2011-08-03 10:19:32 +0000

    SCORM1.2 RTE fix some problems and add debugger
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@30129 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/ScormAicc/scripts/SCORM1_2standard.js
+++ Modules/ScormAicc/scripts/SCORM1_2standard.js
@@ -1,20 +1,14 @@
 var errorCode=0,
 	sco_id=0,
 	diag='',
-	totatTimeAtInitialize='00:00:00';
+	entryAtInitialize='',
+	totalTimeAtInitialize='',
+	b_scoCredit=true;
 	
-/*	
-	b_commitTerminated=true,
-	b_checkSetValues=true,
-	b_storeObjectives=true,
-	b_storeInteractions=true,
-	b_readInteractions=false,
-	c_storeSessionTime='n';//n=no store, s=session_time by sco, i=session_time by LMS
-//autocommit bei ping oder ping wenn keine Daten; Zeit beachten
-*/
-
+//var noCreditElements='cmi.
 
 // DataModel
+// rx = regular Expression; ac = accessibility; dv = default value
 var rx={
 	CMIString255:	'^[\\u0000-\\uffff]{0,255}$',
 	CMIString4096:	'^[\\u0000-\\uffff]{0,4096}$',
@@ -56,7 +50,7 @@ var model = {
 					dv: ''
 				},
 				lesson_mode:{
-					rx: '^browse$|^normal$|^review$',
+					rx: '^browse$|^normal$|^review$',// specs 3-31: "Right now there is no SCORM defined way to signify that learning content can be taken in dfferent modes. Implementation will therfore be LMS specific."
 					ac: 'r',
 					dv: 'browse'
 				},
@@ -355,11 +349,12 @@ function AddTime(first, second) {
 
 function LMSInitialize(param){
 	function setreturn(thisErrorCode,thisDiag){
-		//if Log ...
 		errorCode=thisErrorCode;
 		diag=thisDiag;
-		if(errorCode==0) return "true";
-		else return "false";
+		var s_return="false";
+		if(errorCode==0) s_return="true";
+		showCalls('LMSInitialize("'+param+'")',s_return,errorCode,diag);
+		return s_return;
 	}
 	if (param!=="") return setreturn(201,"param must be empty string");
 	if (Initialized) return setreturn(101,"already initialized");
@@ -367,34 +362,55 @@ function LMSInitialize(param){
 	errorCode=0;
 	diag='';
 	sco_id=iv.launchId;
+	//to avoid additional commits at LMSFinish, values for elements entry and total_time are stored separatly
+	totalTimeAtInitialize=getValueIntern(sco_id,'cmi.core.total_time');
+	if (totalTimeAtInitialize==null) totalTimeAtInitialize=model.cmi.core.total_time.dv;
+
+	entryAtInitialize="";
+	if (getValueIntern(sco_id,'cmi.core.entry') == null && getValueIntern(sco_id,'cmi.core.exit') == null) {
+		entryAtInitialize=model.cmi.core.entry.dv;
+		setValueIntern(sco_id,'cmi.core.entry',"",true);
+	}
 	if (getValueIntern(sco_id,'cmi.core.exit') == 'suspend'){
-		setValueIntern(sco_id,'cmi.core.entry','resume',false);
-//	} else {
-//		save total_time
+		entryAtInitialize='resume';
+		setValueIntern(sco_id,'cmi.core.entry','resume',true);
+//	} else { //some other than 1.2
+//		save total_time ...
 //		data[sco_id]=new Object();
 	}
+
 	var mode=iv.lesson_mode;
 	if (iv.b_autoReview==true) {
 		var st=getValueIntern(sco_id,'cmi.core.lesson_status');
-		if (st=="completed" || st=="passed" || st=="failed") mode='review';
+		if (st=="completed" || st=="passed" || st=="failed") {
+			mode='review';
+			entryAtInitialize=""; //specs 3-26
+			setValueIntern(sco_id,'cmi.core.entry',"",true);
+		}
 	}
-	setValueIntern(sco_id,'cmi.core.lesson_mode',mode,false);
-	if (mode == 'normal') setValueIntern(sco_id,'cmi.core.credit',iv.credit,false);
-	else setValueIntern(sco_id,'cmi.core.credit','no-credit',false);
-	
-	var totatTimeAtInitialize=getValueInternDecoded(sco_id,'cmi.core.total_time');
-	if (totatTimeAtInitialize==null) totatTimeAtInitialize=model.cmi.core.total_time.dv;
-	
+	setValueIntern(sco_id,'cmi.core.lesson_mode',mode,true);
+
+	b_scoCredit=false;
+	if (mode == 'normal') {
+		setValueIntern(sco_id,'cmi.core.credit',iv.credit,true);
+		if (iv.credit == 'credit') b_scoCredit=true;
+	} else {
+		setValueIntern(sco_id,'cmi.core.credit','no-credit',true);
+	}
+
+	initDebug();
+
 	return setreturn(0,"");
 }
 
 function LMSCommit(param) {
 	function setreturn(thisErrorCode,thisDiag){
-		//if Log ...
 		errorCode=thisErrorCode;
 		diag=thisDiag;
-		if(errorCode==0) return "true";
-		else return "false";
+		var s_return="false";
+		if(errorCode==0) s_return="true";
+		showCalls('LMSCommit("'+param+'")',s_return,errorCode,diag);
+		return s_return;
 	}
 	if (param!=="") return setreturn(201,"param must be empty string");
 	if (!Initialized) return setreturn(301,"");
@@ -405,14 +421,18 @@ function LMSCommit(param) {
 
 function LMSFinish(param){
 	function setreturn(thisErrorCode,thisDiag){
-		//if Log ...
 		errorCode=thisErrorCode;
 		diag=thisDiag;
-		if(errorCode==0) return "true";
-		else return "false";
+		var s_return="false";
+		if(errorCode==0) s_return="true";
+		showCalls('LMSFinish("'+param+'")',s_return,errorCode,diag);
+		return s_return;
 	}
 	if (param!=="") return setreturn(201, "param must be empty string");
 	if (!Initialized) return setreturn(301,"");
+//	if (getValueIntern(sco_id,'cmi.core.exit') == null && getValueIntern(sco_id,'cmi.core.entry') != 'resume') {
+//		setValueIntern(sco_id,'cmi.core.entry',"",true);
+//	}
 	if (IliasCommit()==false) return setreturn(101,"LMSFinish was not successful because of failure with implicit LMSCommit");
 	Initialized=false;
 	IliasLaunchAfterFinish();
@@ -420,10 +440,11 @@ function LMSFinish(param){
 }
 
 function LMSGetLastError() {
+	showCalls('LMSGetLastError()',""+errorCode);
 	return ""+errorCode;
 }
 
-function LMSGetErrorString(ec){
+function getErrorStringIntern(ec){
 	var s_error="";
 	ec=""+ec;
 	if (ec!=""){
@@ -442,27 +463,35 @@ function LMSGetErrorString(ec){
 			case "405"	: s_error = 'Incorrect Data Type';break;
 		}
 	}
-	// showCalls("LMSGetErrorString("+ec+")",s_error,"");
 	return s_error;
+}
+function LMSGetErrorString(ec){
+	s_err=getErrorStringIntern(ec);
+	showCalls('LMSGetErrorString("'+ec+'")',s_err);
+	return s_err;
 }
 
 function LMSGetDiagnostic(param){
+	var s_return="";
 	if (param==""){
-		if (diag=="") return 'no additional info for last error with error code '+errorCode;
-		else return 'additional info for last error with error code '+errorCode+': '+diag;
+		if (diag=="") s_return='no additional info for last error with error code '+errorCode;
+		else s_return='additional info for last error with error code '+errorCode+': '+diag;
 	} else {
-		return 'no additional info for error code '+param;
+		s_return='no additional info for last error with error code '+param;
 	}
+	showCalls('LMSGetDiagnostic("'+param+'")',s_return);
+	return s_return;
 }
 
 
 function LMSGetValue(s_el){
 	function setreturn(thisErrorCode,thisDiag,value){
-		//if Log ...
 		errorCode=thisErrorCode;
 		diag=thisDiag;
-		if(errorCode==0) return value;
-		else return "";
+		var s_return="";
+		if(errorCode==0) s_return=value;
+		showCalls('LMSGetValue("'+s_el+'")',s_return,errorCode,diag);
+		return s_return;
 	}
 	var value="";
 	s_el=""+s_el;
@@ -472,49 +501,62 @@ function LMSGetValue(s_el){
 	var o_elmod=getElementModel(s_el);
 	if (o_elmod==null){
 		var a_el=s_el.split('.');
-		if (a_el[a_el.length-1]=="_children") return setreturn(202,"element cannot have children");
-		if (a_el[a_el.length-1]=="_count") return setreturn(203,"element cannot have count");
+		if (a_el[a_el.length-1]=="_children") return setreturn(202,"");
+		if (a_el[a_el.length-1]=="_count") return setreturn(203,"");
 		return setreturn(201,"element not exists");
 	}
 	//check if writeable
-	if (o_elmod['ac'] == "w") return setreturn(404,"element is WriteOnly");
-	if (s_el=='cmi.core.total_time') value=totatTimeAtInitialize;
+	if (o_elmod['ac'] == "w") return setreturn(404,"");
+	if (s_el=='cmi.core.total_time') value=totalTimeAtInitialize;
+	else if (s_el=='cmi.core.entry') value=entryAtInitialize;
 	else value=getValueIntern(sco_id,s_el);
-	if (value != null) return setreturn(0,"",decodeURIComponent(value));
-	if (typeof o_elmod['dv'] == "undefined") return setreturn(0,"not set","");
+	if (value != null) return setreturn(0,"",value);
+	if (typeof o_elmod['dv'] == "undefined" || o_elmod['dv'] == null) return setreturn(0,"not set","");
 	else return setreturn(0,"",o_elmod['dv']);
 	return setreturn(101,"");
 }
 
 function LMSSetValue(s_el,value){
 	function setreturn(thisErrorCode,thisDiag){
-		//if Log ...
 		errorCode=thisErrorCode;
 		diag=thisDiag;
-		if(errorCode==0) return "true";
-		else return "false";
+		var s_return="false";
+		if(errorCode==0) s_return="true";
+		showCalls('LMSSetValue("'+s_el+'","'+value+'")',s_return,errorCode,diag);
+		return s_return;
 	}
 	s_el=""+s_el;
 	if (value==null) value="";
 	value=""+value;
 	if (!Initialized) return setreturn(301,"");
-	if (s_el=="" || s_el==null) return setreturn(201,"");
+	if (s_el=="" || s_el==null) return setreturn(201,"LMSSetValue without element");
+	//check if keyword
+	if (s_el.indexOf('_children')>-1 || s_el.indexOf('_count')>-1) return setreturn(402,"");
 	//check if model exists
 	var o_elmod=getElementModel(s_el);
 	if (o_elmod==null) return setreturn(201,"");
 	//check if writeable
-	if (o_elmod['ac'] == "r") return setreturn(201,"");//falsch writerror
+	if (o_elmod['ac'] == "r") return setreturn(403,"");
 	//Format-/Range-Checker
 	if(iv.b_checkSetValues){
 		var trx = new RegExp(o_elmod['rx']);
-		if (value.match(trx) == null) return setreturn(201,"");//falsch? writerror
+		if (value.match(trx) == null) return setreturn(405,"");
 	}
 	//store
-	var b_result=setValueIntern(sco_id,s_el,value,true);
+	var b_storeDB=true;
+	if (iv.b_storeInteractions==false && s_el.indexOf("cmi.interactions")>-1) b_storeDB=false;
+	else if (iv.b_storeObjectives==false && s_el.indexOf("cmi.objectives")>-1) b_storeDB=false;
+	if (b_scoCredit==false && (s_el.indexOf("score")>-1 || s_el.indexOf("status")>-1)) b_storeDB=false;
+
+	var b_result=setValueIntern(sco_id,s_el,value,b_storeDB);
 	if (b_result==false) return setreturn(201,"out of order");
 	if (s_el=='cmi.core.session_time'){
-		var ttime = AddTime(totatTimeAtInitialize, value);
+		var ttime = AddTime(totalTimeAtInitialize, value);
 		b_result=setValueIntern(sco_id,'cmi.core.total_time',ttime,true);
+	}
+	if (s_el=='cmi.core.exit'){
+		if (value=='suspend') b_result=setValueIntern(sco_id,'cmi.core.entry',"resume",true);
+		else b_result=setValueIntern(sco_id,'cmi.core.entry',"",true);
 	}
 	return setreturn(0,"");
 }
@@ -524,7 +566,7 @@ function init(){
 	model.cmi.core.student_name.dv=iv.studentName;
 	model.cmi.core.student_login.dv=iv.studentLogin;
 	model.cmi.core.student_ou.dv=iv.studentOu;
-	model.cmi.core.credit=iv.credit;
+	model.cmi.core.credit.dv=iv.credit;
 }
 
 this.LMSInitialize=LMSInitialize;
