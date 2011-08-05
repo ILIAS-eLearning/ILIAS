5ab82648f554800ce8d811372352595952aaae1e
Stefan Meyer <smeyer.ilias@gmx.de>
2011-08-05 14:14:17 +0000

    new ilias version
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@30162 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- include/inc.ilias_version.php
+++ include/inc.ilias_version.php
@@ -9,6 +9,6 @@
 *
 * @package ilias-core
 */
-define("ILIAS_VERSION", "4.2.0 Alpha3");
+define("ILIAS_VERSION", "4.2.0 Beta");
 define("ILIAS_VERSION_NUMERIC", "4.2.0");			// must be always x.y.z: x, y and z are numbers
 ?>
