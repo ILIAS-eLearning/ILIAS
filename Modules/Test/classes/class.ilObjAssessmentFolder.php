2edd10018b877282509e4aa895e355c377d1a3f2
hschottm <hschottm@svn.ilias.de>
2011-08-17 09:50:54 +0000

    Fixed: Wrong parameter in Test log method Fixed: Shuffle questions setting wasn't saved when test is copied
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@30310 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Test/classes/class.ilObjAssessmentFolder.php
+++ Modules/Test/classes/class.ilObjAssessmentFolder.php
@@ -318,7 +318,7 @@ class ilObjAssessmentFolder extends ilObject
 	* @param integer $original_id The database id of the original of a modified question (optional)
 	* @return array Array containing the datasets between $ts_from and $ts_to for the test with the id $test_id
 	*/
-	function _addLog($user_id, $object_id, $logtext, $question_id = "", $original_id = "", $test_only = FALSE, $ref_id = NULL)
+	function _addLog($user_id, $object_id, $logtext, $question_id = "", $original_id = "", $test_only = FALSE, $test_ref_id = NULL)
 	{
 		global $ilUser, $ilDB;
 		if (strlen($question_id) == 0) $question_id = NULL;
