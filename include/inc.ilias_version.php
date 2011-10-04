81fbcc7b504d52f371b5d3953f65be0710ca1793
Alexander Killing <killing@leifos.de>
2011-10-04 14:45:35 +0000

    increased version number
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@30981 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- include/inc.ilias_version.php
+++ include/inc.ilias_version.php
@@ -9,6 +9,6 @@
 *
 * @package ilias-core
 */
-define("ILIAS_VERSION", "4.2.0 Beta 4");
+define("ILIAS_VERSION", "4.2.0 2011-10-04");
 define("ILIAS_VERSION_NUMERIC", "4.2.0");			// must be always x.y.z: x, y and z are numbers
 ?>
