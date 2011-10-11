67c6b3c617143256a28ac3eee1623e717b69f2cc
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2011-10-11 10:28:58 +0000

    #7971 - blog/portfolio administration: wording
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@31075 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Portfolio/classes/class.ilObjPortfolioAdministrationGUI.php
+++ Services/Portfolio/classes/class.ilObjPortfolioAdministrationGUI.php
@@ -100,11 +100,18 @@ class ilObjPortfolioAdministrationGUI extends ilObjectGUI
 	*/
 	public function editSettings($a_form = null)
 	{
-		global $lng;
+		global $lng, $ilSetting;
 		
 		$this->tabs_gui->setTabActive('settings');	
 		
-		ilUtil::sendInfo($lng->txt("prtf_admin_toggle_info"));
+		if ($ilSetting->get('user_portfolios'))
+		{
+			ilUtil::sendInfo($lng->txt("prtf_admin_toggle_info"));
+		}
+		else
+		{
+			ilUtil::sendInfo($lng->txt("prtf_admin_inactive_info"));
+		}
 		
 		if(!$a_form)
 		{
