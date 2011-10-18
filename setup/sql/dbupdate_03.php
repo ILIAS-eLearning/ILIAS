02e83b531cad0694334eb9ed54af19d68fa89b28
Alexander Killing <killing@leifos.de>
2011-10-18 06:22:23 +0000

    fixed db update
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@31153 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- setup/sql/dbupdate_03.php
+++ setup/sql/dbupdate_03.php
@@ -8303,33 +8303,33 @@ if(!$ilDB->tableExists('note_settings'))
 ?>
 <#3493>
 <?php
-	$ilDB->addIndex('object_data', array('import_id'), "import_idx");
+	$ilDB->addIndex('object_data', array('import_id'), "i4");
 ?>
 <#3494>
 <?php
-	$ilDB->addIndex('usr_data', array('ext_account', 'auth_mode'), "ext_idx");
+	$ilDB->addIndex('usr_data', array('ext_account', 'auth_mode'), "i2");
 ?>
 <#3495>
 <?php
-	$ilDB->addIndex('event', array('obj_id'), "objid_idx");
+	$ilDB->addIndex('event', array('obj_id'), "i1");
 ?>
 <#3496>
 <?php
-	$ilDB->addIndex('conditions', array('target_obj_id'), "t_objid_idx");
+	$ilDB->addIndex('conditions', array('target_obj_id'), "i1");
 ?>
 <#3497>
 <?php
-	$ilDB->addIndex('mob_usage', array('usage_id'), "usgid_idx");
+	$ilDB->addIndex('mob_usage', array('usage_id'), "i1");
 ?>
 <#3498>
 <?php
-	$ilDB->addIndex('lo_access', array('usr_id'), "uid_idx");
+	$ilDB->addIndex('lo_access', array('usr_id'), "i1");
 ?>
 <#3499>
 <?php
-	$ilDB->addIndex('cal_categories', array('type'), "typeid_idx");
+	$ilDB->addIndex('cal_categories', array('type'), "i3");
 ?>
 <#3500>
 <?php
-	$ilDB->addIndex('tree', array('lft'), "lft_idx");
+	$ilDB->addIndex('tree', array('lft'), "i4");
 ?>
