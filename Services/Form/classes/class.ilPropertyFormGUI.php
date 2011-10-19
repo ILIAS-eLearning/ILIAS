6e1f733a864a7f311d3192bb21250c5af8b9fceb
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2011-10-19 13:25:41 +0000

    #8049 - file upload notice
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@31192 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Form/classes/class.ilPropertyFormGUI.php
+++ Services/Form/classes/class.ilPropertyFormGUI.php
@@ -964,50 +964,55 @@ class ilPropertyFormGUI extends ilFormGUI
 			if(is_dir($temp_path) && $user_id && $user_id != ANONYMOUS_USER_ID)
 			{
 				$reload = array();
-				foreach(glob($temp_path."/".$ilUser->getId()."~~".$_POST["ilfilehash"]."~~*") as $full_file)
+				
+				$temp_files = glob($temp_path."/".$ilUser->getId()."~~".$_POST["ilfilehash"]."~~*");
+				if(sizeof($temp_files))
 				{
-					$file = explode("~~", basename($full_file));
-					$field = $file[2];
-					$idx = $file[3];
-					$idx2 = $file[4];
-					$type = $file[5]."/".$file[6];
-					$name = $file[7];
+					foreach($temp_files as $full_file)
+					{
+						$file = explode("~~", basename($full_file));
+						$field = $file[2];
+						$idx = $file[3];
+						$idx2 = $file[4];
+						$type = $file[5]."/".$file[6];
+						$name = $file[7];
 
-					if($idx2 != "")
-					{
-						if(!$_FILES[$field]["tmp_name"][$idx][$idx2])
+						if($idx2 != "")
 						{
-							$reload[$field]["tmp_name"][$idx][$idx2] = $full_file;
-							$reload[$field]["name"][$idx][$idx2] = $name;
-							$reload[$field]["type"][$idx][$idx2] = $type;
-							$reload[$field]["error"][$idx][$idx2] = 0;
-							$reload[$field]["size"][$idx][$idx2] = filesize($full_file);								
+							if(!$_FILES[$field]["tmp_name"][$idx][$idx2])
+							{
+								$reload[$field]["tmp_name"][$idx][$idx2] = $full_file;
+								$reload[$field]["name"][$idx][$idx2] = $name;
+								$reload[$field]["type"][$idx][$idx2] = $type;
+								$reload[$field]["error"][$idx][$idx2] = 0;
+								$reload[$field]["size"][$idx][$idx2] = filesize($full_file);								
+							}
 						}
-					}
-					else if($idx != "")
-					{
-						if(!$_FILES[$field]["tmp_name"][$idx])
+						else if($idx != "")
 						{
-							$reload[$field]["tmp_name"][$idx] = $full_file;
-							$reload[$field]["name"][$idx] = $name;
-							$reload[$field]["type"][$idx] = $type;
-							$reload[$field]["error"][$idx] = 0;
-							$reload[$field]["size"][$idx] = filesize($full_file);								
-						}	
-					}
-					else
-					{
-						if(!$_FILES[$field]["tmp_name"])
-						{
-							$reload[$field]["tmp_name"] = $full_file;
-							$reload[$field]["name"] = $name;
-							$reload[$field]["type"] = $type;
-							$reload[$field]["error"] = 0;
-							$reload[$field]["size"] = filesize($full_file);								
+							if(!$_FILES[$field]["tmp_name"][$idx])
+							{
+								$reload[$field]["tmp_name"][$idx] = $full_file;
+								$reload[$field]["name"][$idx] = $name;
+								$reload[$field]["type"][$idx] = $type;
+								$reload[$field]["error"][$idx] = 0;
+								$reload[$field]["size"][$idx] = filesize($full_file);								
+							}	
 						}
-					}						
+						else
+						{
+							if(!$_FILES[$field]["tmp_name"])
+							{
+								$reload[$field]["tmp_name"] = $full_file;
+								$reload[$field]["name"] = $name;
+								$reload[$field]["type"] = $type;
+								$reload[$field]["error"] = 0;
+								$reload[$field]["size"] = filesize($full_file);								
+							}
+						}						
+					}
 				}
-
+				
 				$this->reloaded_files = $reload;
 			}
 		}
