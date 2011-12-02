dacfea3e92e714b942a8c0520ef6a688087c5252
Michael Jansen <mjansen@databay.de>
2011-12-02 16:50:01 +0000

    Fixed infinit redirects if both, password change and user profile completion are necessary
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@31966 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/PersonalDesktop/classes/class.ilPersonalDesktopGUI.php
+++ Services/PersonalDesktop/classes/class.ilPersonalDesktopGUI.php
@@ -69,13 +69,6 @@ class ilPersonalDesktopGUI
 
 		$this->tpl->addCss(ilUtil::getStyleSheetLocation('filesystem','delos.css','Services/Calendar'));
 		
-		
-		// check whether personal profile of user is incomplete
-		if ($ilUser->getProfileIncomplete() && $next_class != "ilpersonalprofilegui")
-		{
-			$this->ctrl->redirectByClass("ilpersonalprofilegui");
-		}
-
 		// check whether password of user have to be changed
 		// due to first login or password of user is expired
 		if( ($ilUser->isPasswordChangeDemanded() || $ilUser->isPasswordExpired())
@@ -83,6 +76,12 @@ class ilPersonalDesktopGUI
 		)
 		{
 			$this->ctrl->redirectByClass("ilpersonalsettingsgui");
+		}
+		
+		// check whether personal profile of user is incomplete
+		if ($ilUser->getProfileIncomplete() && $next_class != "ilpersonalprofilegui" && !($ilUser->isPasswordChangeDemanded() || $ilUser->isPasswordExpired()))
+		{
+			$this->ctrl->redirectByClass("ilpersonalprofilegui");
 		}
 
 		// read last active subsection
