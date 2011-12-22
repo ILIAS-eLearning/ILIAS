7a7358c75ab08cd3c72a16ead310b3317f3efdf7
Alexander Killing <killing@leifos.de>
2011-12-22 17:26:17 +0000

    fixed bug #8346: ilSkillTree::insertNode(): Parents mismatch
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@32298 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Skill/classes/class.ilSkillTreeNode.php
+++ Services/Skill/classes/class.ilSkillTreeNode.php
@@ -549,7 +549,7 @@ class ilSkillTreeNode
 				$cid = ilSkillTreeNode::pasteTree($skill["id"], $parent_id, $target,
 					$skill["insert_time"], $copied_nodes,
 					(ilEditClipboard::getAction() == "copy"), true);
-				$target = $cid;
+//				$target = $cid;
 			}
 		}
 
@@ -591,7 +591,7 @@ class ilSkillTreeNode
 		&$a_copied_nodes, $a_as_copy = false, $a_add_suffix = false)
 	{
 		global $ilUser, $ilias, $ilLog, $lng;
-
+if ($a_target == 6) mk();
 		$item_type = ilSkillTreeNode::_lookupType($a_item_id);
 
 		if ($item_type == "scat")
