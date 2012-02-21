3ae5580051588fc6e3728db1d54d747d8fd25783
Stefan Meyer <smeyer.ilias@gmx.de>
2012-02-21 15:38:57 +0000

    Removed debug
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@33279 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- webservice/soap/classes/class.ilNusoapUserAdministrationAdapter.php
+++ webservice/soap/classes/class.ilNusoapUserAdministrationAdapter.php
@@ -48,7 +48,7 @@ class ilNusoapUserAdministrationAdapter
 		define('SERVICE_NAMESPACE','urn:ilUserAdministration');
 		define('SERVICE_STYLE','rpc');
 		define('SERVICE_USE','encoded');
-		global $debug; $debug = true;
+		#global $debug; $debug = true;
 		$this->server = new soap_server();
 		$this->server->decode_utf8 = false;
 		$this->server->class = "ilSoapFunctions";
