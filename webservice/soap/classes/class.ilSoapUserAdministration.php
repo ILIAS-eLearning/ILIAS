20d0843ac91fb11d31f4583d271bccc753853a03
Stefan Meyer <smeyer.ilias@gmx.de>
2012-03-02 13:18:14 +0000

    0008589: hasNewMail Soap Function does not work
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@33474 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- webservice/soap/classes/class.ilSoapUserAdministration.php
+++ webservice/soap/classes/class.ilSoapUserAdministration.php
@@ -1302,6 +1302,8 @@ class ilSoapUserAdministration extends ilSoapAdministration
 			return $this->__raiseError($this->__getMessage(),$this->__getMessageCode());
 		}
 
+		global $ilUser;
+
 		include_once 'Services/Mail/classes/class.ilMailGlobalServices.php';
 		if(ilMailGlobalServices::getNumberOfNewMailsByUserId($ilUser->getId()) > 0)
 		{
