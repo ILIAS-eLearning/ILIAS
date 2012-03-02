11b78f4964e05837ca16b04677d6bf5fbb24dd20
Stefan Meyer <smeyer.ilias@gmx.de>
2012-03-02 12:52:14 +0000

    Merged 0008655: category reference doesnt work
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@33471 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/CategoryReference/classes/class.ilObjCategoryReferenceAccess.php
+++ Modules/CategoryReference/classes/class.ilObjCategoryReferenceAccess.php
@@ -54,14 +54,14 @@ class ilObjCategoryReferenceAccess extends ilContainerReferenceAccess
 	function _getCommands($a_ref_id)
 	{
 		global $ilAccess;
-		
+
 		if($ilAccess->checkAccess('write','',$a_ref_id))
 		{
 			// Only local (reference specific commands)
 			$commands = array
 			(
 				array("permission" => "visible", "cmd" => "", "lang_var" => "show","default" => true),
-				array("permission" => "write", "cmd" => "edit", "lang_var" => "edit")
+				array("permission" => "write", "cmd" => "editReference", "lang_var" => "edit")
 			);
 		}
 		else
