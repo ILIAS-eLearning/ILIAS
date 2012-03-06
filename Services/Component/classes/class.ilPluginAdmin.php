c30bd565d6afe97f11c6dce917200134f0beba6d
Alexander Killing <killing@leifos.de>
2012-03-06 09:21:26 +0000

    removed autoload; added static includeClass function
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@33550 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Component/classes/class.ilPluginAdmin.php
+++ Services/Component/classes/class.ilPluginAdmin.php
@@ -319,6 +319,29 @@ class ilPluginAdmin
 		}
 		return self::$plugin_objects[$a_ctype][$a_cname][$a_slot_id][$a_pname];
 	}
+	
+   /**
+	* Get Plugin Object
+	*
+	* @param	string	$a_ctype	Component Type
+	* @param	string	$a_cname	Component Name
+	* @param	string	$a_slot_id	Slot ID
+	* @param	string	$a_pname	Plugin Name
+	 * @return ilPlugin the plugin
+	*/
+	static function includeClass($a_ctype, $a_cname, $a_slot_id, $a_pname,
+		$a_class_file_name)
+	{
+		// cache the plugin objects
+		if (!isset(self::$plugin_objects[$a_ctype][$a_cname][$a_slot_id][$a_pname]))
+		{
+			self::$plugin_objects[$a_ctype][$a_cname][$a_slot_id][$a_pname] = 
+				ilPlugin::getPluginObject($a_ctype, $a_cname, $a_slot_id, $a_pname);
+		}
+		$pl = self::$plugin_objects[$a_ctype][$a_cname][$a_slot_id][$a_pname];
+		$pl->includeClass($a_class_file_name);
+	}
+
 }
 
 ?>
