2091bc6699eea6ddb1b8e51be15a8781f2ec6efb
Michael Jansen <mjansen@databay.de>
2012-03-09 21:21:15 +0000

    First steps of session reminder refactoring
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@33656 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ Services/Authentication/classes/class.ilSessionReminderCheck.php
@@ -0,0 +1,97 @@
+<?php
+/* Copyright (c) 1998-2012 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+/**
+ * @author Michael Jansen <mjansen@databay.de>
+ * @version $Id$
+ * @ingroup ServicesAuthentication
+ */
+class ilSessionReminderCheck
+{
+	public function __construct()
+	{
+	}
+
+	public function getJsonResponse($sessionId, $secondsUntilReminder)
+	{
+		/**
+		 * @var $ilDB ilDB
+		 * @var $ilUser ilObjUser
+		 */
+		global $ilDB, $ilUser;
+
+		include_once 'Services/JSON/classes/class.ilJsonUtil.php';
+
+		// Define response array
+		$response = array('remind' => false);
+
+		$ilDB->setLimit(1);
+		$res = $ilDB->queryF('
+			SELECT data, last_remind_ts, user_id
+			FROM usr_session
+			WHERE session_id = %s
+			ORDER BY expires DESC',
+			array('text'),
+			array(ilUtil::stripSlashes($sessionId))
+		);
+		$data = $ilDB->fetchAssoc($res);
+
+		if(!$data || !$data['data'] || !$data['user_id'] || $data['user_id'] == ANONYMOUS_USER_ID)
+		{
+			// No data found in database, of the current user is anonymous
+			return ilJsonUtil::encode($response);
+		}
+
+		if($data['last_remind_ts'] > time() - $secondsUntilReminder)
+		{
+			/* Reminder not necessary: There was a request (which extends the session) between the
+			   start of the javascript countdown and the current time */
+			return ilJsonUtil::encode($response);
+		}
+
+		// Unserialize the session
+		$session = ilUtil::unserializeSession($data['data']);
+		$idletime = null;
+		foreach((array)$session as $key => $entry)
+		{
+			if(strpos($key, '_auth__') === 0)
+			{
+				$idletime = $entry['idle'];
+				break;
+			}
+		}
+
+		if(null === $idletime)
+		{
+			// No idle value found
+			return ilJsonUtil::encode($response);
+		}
+
+		$expirestime = $idletime + ilSession::getIdleValue();
+
+		if($expirestime < time())
+		{
+			// Session is already expired
+			return ilJsonUtil::encode($response);
+		}
+
+		/**
+		 * @var $user ilObjUser
+		 */
+		$ilUser = ilObjectFactory::getInstanceByObjId($data['user_id']);
+
+		$ilDB->manipulateF('
+			UPDATE usr_session SET last_remind_ts = %s WHERE session_id = %s',
+			array('integer', 'text'),
+			array(time(), $sessionId)
+		);
+
+		$response = array(
+			'remind' => true,
+			'seconds_until_expiration' => ilFormat::_secondsToString((float)$ilUser->getPref('session_reminder_lead_time') * 60, true),
+			'current_time_string' => ilDatePresentation::formatDate(new ilDateTime(time(), IL_CAL_UNIX)),
+		);
+
+		return ilJsonUtil::encode($response);
+	}
+}
--- sessioncheck.php
+++ sessioncheck.php
@@ -2,9 +2,9 @@
 /* Copyright (c) 1998-2009 ILIAS open source, Extended GPL, see docs/LICENSE */
 
 // jump to setup if ILIAS is not installed
-if(!file_exists(getcwd().'/ilias.ini.php'))
+if(!file_exists(getcwd() . '/ilias.ini.php'))
 {
-    header('Location: ./setup/setup.php');
+	header('Location: ./setup/setup.php');
 	exit();
 }
 
@@ -14,69 +14,10 @@ ilContext::init(ilContext::CONTEXT_SESSION_REMINDER);
 require_once("Services/Init/classes/class.ilInitialisation.php");
 ilInitialisation::initILIAS();
 
-include_once 'Services/JSON/classes/class.ilJsonUtil.php';
-
-global $ilDB;
-
-$ilDB->setLimit(1);
-$res = $ilDB->queryF('
-	SELECT data, last_remind_ts FROM usr_session 
-	WHERE session_id = %s ORDER BY expires DESC',
-    array('text'),
-    array($_GET['session_id']));
-$oRow = $ilDB->fetchObject($res);
-
-$response_data = array('remind' => false);
-$currentTime = time();
-
-if(is_object($oRow))
-{			    
-	$data = $oRow->data;
-		
-	$expiresTime = null;
-	$pattern = "idle\";i:";
-	if(($lft_pos = strpos($data, $pattern)) !== false)
-	{
-		$substr = substr($data, $lft_pos + strlen($pattern));
-		$pattern = ";";
-		if(($rgt_pos = strpos($substr, $pattern)) !== false)
-		{
-			$expiresTime = (int)substr($substr, 0, $rgt_pos);
-		}
-	}
-	
-	if($expiresTime === null)
-	{
-		echo ilJsonUtil::encode($response_data);
-		exit();
-	}
-	else
-	{			
-		$leadTime = $_GET['lead_time'];			
-		$expiresTime += $ilClientIniFile->readVariable('session', 'expire');
-
-		if($expiresTime >= $currentTime &&
-		   $oRow->last_remind_ts <= $currentTime - $_GET['countDownTime'])
-		{
-			include_once 'Services/Calendar/classes/class.ilDate.php';
-			$date = new ilDateTime(time(),IL_CAL_UNIX);
-			$currentTimeTxt = $date->get(IL_CAL_FKT_DATE,'H:i:s', urldecode($_GET['timezone']));
-			
-			$ilDB->manipulateF('
-				UPDATE usr_session SET last_remind_ts = %s WHERE session_id = %s',
-			    array('integer', 'text'),
-			    array($currentTime, $_GET['session_id'])); 
-			
-			$response_data = array(
-				'remind' => true,
-				'expiresInTimeX' => ilFormat::_secondsToString($leadTime, true),
-				'currentTime' => $currentTimeTxt,
-			);	
-		}
-		
-	}	
-}
-
-echo ilJsonUtil::encode($response_data);
+include_once 'Services/Authentication/classes/class.ilSessionReminderCheck.php';
+$session_reminder_check = new ilSessionReminderCheck();
+echo $session_reminder_check->getJsonResponse(
+	ilUtil::stripSlashes($_GET['session_id']),
+	(int)ilUtil::stripSlashes($_GET['countdown'])
+);
 exit();
-?>
