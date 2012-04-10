f3e99c5f46fed13b0c46eee3323b0036844d0558
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2012-04-10 12:11:20 +0000

    autocomplete: yui to jquery
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@34097 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Search/classes/class.ilMainMenuSearchGUI.php
+++ Services/Search/classes/class.ilMainMenuSearchGUI.php
@@ -63,16 +63,17 @@ class ilMainMenuSearchGUI
 		{
 //			return '';
 		}
-		
-		include_once("./Services/YUI/classes/class.ilYuiUtil.php");
-		ilYuiUtil::initAutocomplete();
+
+		include_once "Services/jQuery/classes/class.iljQueryUtil.php";
+		iljQueryUtil::initjQuery();
+		iljQueryUtil::initjQueryUI();
 		$this->tpl = new ilTemplate('tpl.main_menu_search.html',true,true,'Services/Search');
 		$this->tpl->setVariable('FORMACTION','ilias.php?baseClass=ilSearchController&cmd=post'.
 			'&rtoken='.$ilCtrl->getRequestToken().'&fallbackCmd=remoteSearch');
 		$this->tpl->setVariable('BTN_SEARCH',$this->lng->txt('search'));
 		$this->tpl->setVariable('CONT_REF_ID',ROOT_FOLDER_ID);
-		$this->tpl->setVariable('ID_AUTOCOMPLETE', "mm_sr_auto");
-		$this->tpl->setVariable('YUI_DATASOURCE', "ilias.php?baseClass=ilSearchController&cmd=autoComplete");
+		// $this->tpl->setVariable('ID_AUTOCOMPLETE', "mm_sr_auto");
+		$this->tpl->setVariable('AC_DATASOURCE', "ilias.php?baseClass=ilSearchController&cmd=autoComplete");
 		
 		$this->tpl->setVariable('IMG_MM_SEARCH', ilUtil::img(ilUtil::getImagePath("icon_seas_s.gif")));
 		
