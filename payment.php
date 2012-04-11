1b7b5dfbcc18574f5151bf9220efb6599ccc1379
Nadia Ahmad <nahmad@databay.de>
2012-04-11 09:56:56 +0000

    cleanup payment script
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@34124 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- payment.php
+++ payment.php
@@ -3,7 +3,9 @@
 
 
 /**
-* base script for payment_shopping_cart and buyed objects
+* THIS IS ONLY USED BUY BMF-PAYMENTSYSTEM!!
+* 
+* base script for payment_shopping_cart and buyed objects 
 *
 * @author Stefan Meyer <smeyer@databay.de>
 * @version $Id: payment.php 5054 2004-09-27 14:00:01Z smeyer $
@@ -15,50 +17,22 @@ define('ILIAS_MODULE','payment');
 require_once "include/inc.header.php";
 
 switch ($_GET["view"])
-{/* not in use
-	case "payment_admin"	:	require_once "./Services/Payment/classes/class.ilPaymentAdminGUI.php";
-
-								$ilCtrl->setTargetScript("payment.php");
-								$ilCtrl->setParameterByClass("ilpaymentadmingui", "view", "payment_admin");
-								$ilCtrl->getCallStructure("ilpaymentadmingui");
-
-								$pa =& new ilPaymentAdminGUI($ilias->account);
-								$ilCtrl->forwardCommand($pa);
-								break;
-	case "start_purchase"	:	require_once "./Services/Payment/classes/class.ilPaymentPurchaseGUI.php";
-
-								$ilCtrl->setTargetScript("payment.php");
-								$ilCtrl->setParameterByClass("ilpaymentpurchasegui", "view", "start_purchase");
-								$ilCtrl->getCallStructure("ilpaymentpurchasegui");
-
-								$pa =& new ilPaymentPurchaseGUI((int) $_GET['ref_id']);
-								$ilCtrl->forwardCommand($pa);
-								break;
-								*/
+{
 	case "start_bmf"		:	require_once "./Services/Payment/classes/class.ilPurchaseBMFGUI.php";
 
 								$ilCtrl->setTargetScript("payment.php");
 								$ilCtrl->setParameterByClass("ilpurchasebmfgui", "view", "start_bmf");
 								$ilCtrl->getCallStructure("ilpurchasebmfgui");
 
-								$pa =& new ilPurchaseBMFGUI($ilias->account);
+								$pa = new ilPurchaseBMFGUI($ilias->account);
 								$ilCtrl->forwardCommand($pa);
 								break;
-	case "conditions"		:	require_once "./Services/Payment/classes/class.ilTermsCondition.php";
-
-								$pa =& new ilTermsCondition($ilias->account);
+							
+	case "conditions"		:	
+	default					:	require_once "./Services/Payment/classes/class.ilTermsCondition.php";
+								$pa = new ilTermsCondition($ilias->account);
 								$pa->show();
 								break;
-	default					:	require_once "./Services/Payment/classes/class.ilPaymentGUI.php";
-
-								$ilCtrl->setTargetScript("payment.php");
-								$ilCtrl->setParameterByClass("ilpaymentgui", "view", "payment");
-								$ilCtrl->getCallStructure("ilpaymentgui");
-
-								$pa =& new ilPaymentGUI($ilias->account);
-								$ilCtrl->forwardCommand($pa);
-								break;
-								
 }
 
 $tpl->show();
