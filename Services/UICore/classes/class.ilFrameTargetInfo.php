b418ddda355682d430cd9cafe3d567bd19a235bc
Michael Jansen <mjansen@databay.de>
2012-04-12 07:43:21 +0000

    Added static keyword for statically used method
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@34129 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/UICore/classes/class.ilFrameTargetInfo.php
+++ Services/UICore/classes/class.ilFrameTargetInfo.php
@@ -1,48 +1,48 @@
 <?php
 /* Copyright (c) 1998-2009 ILIAS open source, Extended GPL, see docs/LICENSE */
 
-
 /**
-* ilFrameTargetInfo
-*
-* @author	Alex Killing <alex.killing@gmx.de>
-* @version	$Id$
-*/
+ * ilFrameTargetInfo
+ * @author	 Alex Killing <alex.killing@gmx.de>
+ * @version	$Id$
+ */
 class ilFrameTargetInfo
 {
 	/**
-	* get content frame name
-	*/
-	function _getFrame($a_class, $a_type = "")
+	 * Get content frame name
+	 * @static
+	 * @param string $a_class
+	 * @param string $a_type
+	 * @return string
+	 */
+	public static function _getFrame($a_class, $a_type = '')
 	{
 		switch($a_type)
 		{
 			default:
 				switch($a_class)
 				{
-					case "RepositoryContent":
-						if ($_SESSION["il_rep_mode"] == "flat" or !isset($_SESSION['il_rep_mode']))
+					case 'RepositoryContent':
+						if($_SESSION['il_rep_mode'] == 'flat' or !isset($_SESSION['il_rep_mode']))
 						{
-							//return "bottom";
-							return "_top";
+							//return 'bottom';
+							return '_top';
 						}
 						else
 						{
-							return "rep_content";
+							return 'rep_content';
 						}
-						
-					case "MainContent":
-						//return "bottom";
-						return "_top";
-						
+
+					case 'MainContent':
+						//return 'bottom';
+						return '_top';
+
 					// frame for external content (e.g. web bookmarks, external links) 
-					case "ExternalContent":
-						return "_blank";
+					case 'ExternalContent':
+						return '_blank';
 				}
 		}
-		
-		return "";
-	}
 
-} // END class.ilFrameTargetInfo
-?>
+		return '';
+	}
+}
