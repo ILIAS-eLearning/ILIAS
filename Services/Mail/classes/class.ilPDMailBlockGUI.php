27050dd544d79726c13b95e51f68189f995d87b9
Michael Jansen <mjansen@databay.de>
2012-04-17 10:18:10 +0000

    Fixed url parameter
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@34228 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Mail/classes/class.ilPDMailBlockGUI.php
+++ Services/Mail/classes/class.ilPDMailBlockGUI.php
@@ -240,10 +240,10 @@ class ilPDMailBlockGUI extends ilBlockGUI
 		if($_GET["mail_mode"] != "system")
 		{
 			$content_block->addBlockCommand("ilias.php?baseClass=ilMailGUI&mail_id=" .
-					$_GET["mail_id"] . "&mobj_id" . $_GET["mobj_id"] . "&type=reply",
+					$_GET["mail_id"] . "&mobj_id=" . $_GET["mobj_id"] . "&type=reply",
 				$lng->txt("reply"));
 			$content_block->addBlockCommand("ilias.php?baseClass=ilMailGUI&mail_id=" .
-					$_GET["mail_id"] . "&mobj_id" . $_GET["mobj_id"] . "&type=read",
+					$_GET["mail_id"] . "&mobj_id=" . $_GET["mobj_id"] . "&type=read",
 				$lng->txt("inbox"));
 
 			$ilCtrl->setParameter($this, 'mail_id', (int)$_GET['mail_id']);
