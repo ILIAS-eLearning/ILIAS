bdf569ccc8f18d5c969c2af4de9dcc0df2ce0e19
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2012-05-15 08:43:36 +0000

    form: numeric input
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@34654 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Form/js/ServiceForm.js
+++ Services/Form/js/ServiceForm.js
@@ -222,3 +222,34 @@ function ilFormShowSubForm(id, cont_id, cb)
 	}
 }
 
+
+function ilFormInitNumericCheck(id)
+{
+	$('#'+id).keydown(function(event) {		
+		// decimal point is only allowed once
+		if (event.keyCode == 190)
+		{
+			var current = $('#'+id).val();
+			if (current.indexOf('.') > -1) 
+			{
+				event.preventDefault(); 
+			}
+		}
+        // Allow: backspace, delete, tab, escape, and enter
+        else if (event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || event.keyCode == 13 || 
+             // Allow: Ctrl+A
+            (event.keyCode == 65 && event.ctrlKey === true) || 
+             // Allow: home, end, left, right
+            (event.keyCode >= 35 && event.keyCode <= 39)) {
+                 // let it happen, don't do anything
+                 return;
+        }		
+        else {
+            // Ensure that it is a number and stop the keypress
+            if (event.shiftKey || (event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+                event.preventDefault(); 
+            }   
+        }
+    });
+
+}
