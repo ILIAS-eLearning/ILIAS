c84a1abd6bd18c8336e4c4d4a44a21784a98e104
Alexander Killing <killing@leifos.de>
2012-06-13 15:54:23 +0000

    fixed common action dispatcher forwarding
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@35056 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/LearningModule/classes/class.ilObjDlBookGUI.php
+++ Modules/LearningModule/classes/class.ilObjDlBookGUI.php
@@ -30,7 +30,7 @@
 *
 * @ilCtrl_Calls ilObjDlBookGUI: ilLMPageObjectGUI, ilStructureObjectGUI, ilObjStyleSheetGUI, ilMDEditorGUI
 * @ilCtrl_Calls ilObjDlBookGUI: ilLearningProgressGUI, ilPermissionGUI, ilInfoScreenGUI
-* @ilCtrl_Calls ilObjDlBookGUI: ilExportGUI
+* @ilCtrl_Calls ilObjDlBookGUI: ilExportGUI, ilCommonActionDispatcherGUI
 *
 * @ingroup ModulesIliasLearningModule
 */
