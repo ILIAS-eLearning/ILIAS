60cf20ca8c1a53d65970b9c9e5bc8ce82b71ff46
Alexander Killing <killing@leifos.de>
2012-06-22 10:36:46 +0000

    fixed handling of deleted chapters
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@35183 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/LearningModule/classes/class.ilStructureObject.php
+++ Modules/LearningModule/classes/class.ilStructureObject.php
@@ -1,25 +1,6 @@
 <?php
-/*
-	+-----------------------------------------------------------------------------+
-	| ILIAS open source                                                           |
-	+-----------------------------------------------------------------------------+
-	| Copyright (c) 1998-2009 ILIAS open source, University of Cologne            |
-	|                                                                             |
-	| This program is free software; you can redistribute it and/or               |
-	| modify it under the terms of the GNU General Public License                 |
-	| as published by the Free Software Foundation; either version 2              |
-	| of the License, or (at your option) any later version.                      |
-	|                                                                             |
-	| This program is distributed in the hope that it will be useful,             |
-	| but WITHOUT ANY WARRANTY; without even the implied warranty of              |
-	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
-	| GNU General Public License for more details.                                |
-	|                                                                             |
-	| You should have received a copy of the GNU General Public License           |
-	| along with this program; if not, write to the Free Software                 |
-	| Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
-	+-----------------------------------------------------------------------------+
-*/
+
+/* Copyright (c) 1998-2012 ILIAS open source, Extended GPL, see docs/LICENSE */
 
 require_once("./Modules/LearningModule/classes/class.ilLMObject.php");
 
@@ -59,6 +40,10 @@ class ilStructureObject extends ilLMObject
 	*/
 	function delete($a_delete_meta_data = true)
 	{
+		// only relevant for online help authoring
+		include_once("./Services/Help/classes/class.ilHelpMapping.php");
+		ilHelpMapping::removeScreenIdsOfChapter($this->getId());
+
 		$this->tree = new ilTree($this->getLmId());
 		$this->tree->setTableNames('lm_tree', 'lm_data');
 		$this->tree->setTreeTablePK("lm_id");
