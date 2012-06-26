4bf38b304826b0e5b7d6e5565c436c8632860f4b
Björn Heyser <bheyser@databay.de>
2012-06-26 09:29:20 +0000

    enhanced debug functions library
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@35217 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- include/inc.debug.php
+++ include/inc.debug.php
@@ -22,21 +22,27 @@
 */
 
 /**
-* debugging functions
-* 
-* @author Sascha Hofmann <shofmann@databay.de>
-* @version $Id$
-*
-* @package ilias-develop
-*/
+ * debugging functions
+ * 
+ * @author Björn Heyser <bheyser@databay.de>
+ * @author Sascha Hofmann <shofmann@databay.de>
+ * @version $Id$
+ *
+ * @package ilias-develop
+ */
 
 /**
-* shortcut for var_dump
-* @access	public
-* @param	mixed	any number of parameters
-*/
+ * shortcut for var_dump with enhanced debug information
+ * 
+ * @author Björn Heyser <bheyser@databay.de>
+ * @author Sascha Hofmann <shofmann@databay.de>
+ * @access	public
+ * @param	mixed	any number of parameters
+ */
 function vd()
 {
+	list($file, $func) = getPhpSourceCodePositionInfo(1);
+	
 	$numargs = func_num_args();
 
 	if ($numargs == 0)
@@ -47,12 +53,31 @@ function vd()
 	$arg_list = func_get_args();
 	$num = 1;
 
+	include_once 'Services/Authentication/classes/class.ilAuthFactory.php';
+	if( ilAuthFactory::getContext() == ilAuthFactory::CONTEXT_CRON )
+			$cli = true;
+	else	$cli = false;
 	
 	foreach ($arg_list as $arg)
 	{
-		echo "<pre>variable ".$num.":<br/>";
+		if(!$cli)
+		{
+			$printbefore = "<pre style=\"text-align:left;\">";
+			$printbefore .= "$file - $func - variable $num:<br/>";
+			$printafter = "</pre><br/>";
+		}
+		else
+		{
+			$printbefore = "\n\n_________________________________________________".
+							"_________________________________________________\n";
+			$printbefore .= "$file - $func - variable $num:\n\n";
+			$printafter = "_________________________________________________".
+							"_________________________________________________\n\n";
+		}
+
+		echo $printbefore;
 		var_dump($arg);
-		echo "</pre><br/>";
+		echo $printafter;
 		$num++;
 	}
 	
@@ -61,12 +86,85 @@ function vd()
 	flush(); ob_flush();
 }
 
+/**
+ * shortcut for print_r 
+ * 
+ * @author Björn Heyser <bheyser@databay.de>
+ * @access	public
+ * @param	mixed	any number of parameters
+ * @param	string	name of variable (optional)
+ */
 function pr($var,$name = '')
 {
 	if($name != '') $name .= ' = ';
-	echo '<pre>'.$name.print_r($var,true).'</pre>';
-
+	$print = $name.print_r($var,true);
+	
+	if( ilAuthFactory::getContext() == ilAuthFactory::CONTEXT_CRON )
+	{
+		$hr = "\n---------------------------------------------------------------\n";
+		echo $hr.$print.$hr;
+	}
+	else
+	{
+		echo '<pre>'.$print.'</pre>';
+	}
+	
+	// BH: php 5.3 seems to not flushing the output consequently so following redirects are still performed
+	// and the output of vd() would be lost in nirvana if we not flush the output manualy
 	flush(); ob_flush();
 }
 
-?>
+/**
+ * prints an information about the function that called
+ * the function that invoked this function
+ * (the optional backjump param conrols the level in backtrace)
+ * 
+ * @author Björn Heyser <bheyser@databay.de>
+ * @access	public
+ * @param integer $backjumps
+ */
+function cf($backjumps = 1)
+{
+	list($fileC, $funcC) = getPhpSourceCodePositionInfo( ($backjumps - 1) + 1 );
+	list($fileF, $funcF) = getPhpSourceCodePositionInfo( ($backjumps) + 1 );
+
+	echo "<pre style=\"text-align:left;\">$fileC - $funcC\nIS CALLED FROM: $fileF - $funcF";
+
+	// BH: php 5.3 seems to not flushing the output consequently so following redirects are still performed
+	// and the output of vd() would be lost in nirvana if we not flush the output manualy
+	flush(); ob_flush();
+}
+
+/**
+ * returns an array containing function information from backtrace
+ * (the optional backjump param conrols the level in backtrace)
+ * 
+ * @author Björn Heyser <bheyser@databay.de>
+ * @access	public
+ * @param integer $backjumps
+ */
+function getPhpSourceCodePositionInfo($backjumps = 0)
+{
+	$i = $backjumps;
+	$j = $backjumps + 1;
+
+	$e = new Exception();
+	$trace = $e->getTrace();
+
+	$file = basename($trace[$i]['file']).':'.$trace[$i]['line'];
+
+	$func = '';
+
+	if( isset($trace[$j]['class']) && strlen($trace[$j]['class']) )
+	{
+		$func = $trace[$j]['class'];
+
+		if( isset($trace[$j]['class']) && strlen($trace[$j]['class']) )
+				$func .= $trace[$j]['type'];
+		else	$func .= '::';
+	}
+
+	$func .= $trace[$j]['function'].'()';
+
+	return array($file, $func);
+}
