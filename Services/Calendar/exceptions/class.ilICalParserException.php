8f955490d1cf2da5fe4fbaba6ff8bb458c2f414c
Stefan Meyer <smeyer.ilias@gmx.de>
2012-07-09 07:20:07 +0000

    Added missing esceptions
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@35426 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ Services/Calendar/exceptions/class.ilICalParserException.php
@@ -0,0 +1,16 @@
+<?php
+
+/* Copyright (c) 1998-2010 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+/**
+ * ICal parser exception
+ *
+ * @author Stefan Meyer <smeyer.ilias@gmx.de>
+ * @version $Id$
+ *
+ */
+class ilICalParserException extends ilException
+{
+	
+}
+?>
