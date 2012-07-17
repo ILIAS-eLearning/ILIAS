7b1609f2e0eb8e9f79180fc066127c00a8067e27
Maximilian Becker <mbecker@databay.de>
2012-07-17 15:26:31 +0000

    Enhanced matching question type to support answer specific feedback.
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@35594 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/TestQuestionPool/classes/class.assMatchingQuestionGUI.php
+++ Modules/TestQuestionPool/classes/class.assMatchingQuestionGUI.php
@@ -525,9 +525,26 @@ class assMatchingQuestionGUI extends assQuestionGUI
 		{
 			$template->setVariable("QUESTIONTEXT", $this->object->prepareTextareaOutput($questiontext, TRUE));
 		}
+		
+		if ($show_feedback)
+		{
+			$feedback .= '<br /><br /><table><tbody>';
+			
+				foreach ($this->object->getMatchingPairs() as $idx => $ans)
+				{
+					$feedback .= '<tr><td><b><i>' . $ans->definition->text . '</i></b></td><td>'. $this->lng->txt("matches") . '&nbsp;';
+					$feedback .= '</td><td><b><i>' . $ans->term->text . '</i></b></td><td>';
+					$feedback .= $this->object->getFeedbackSingleAnswer($idx) . '</td> </tr>';
+				}
+
+			$feedback .= '</tbody></table>';
+		}
+		
 		$questionoutput = $template->get();
-		$feedback = ($show_feedback) ? $this->getAnswerFeedbackOutput($active_id, $pass) : "";
+		
+		$feedback .= ($show_feedback) ? '<br />' . $this->getAnswerFeedbackOutput($active_id, $pass) : "";
 		if (strlen($feedback)) $solutiontemplate->setVariable("FEEDBACK", $feedback);
+		
 		$solutiontemplate->setVariable("SOLUTION_OUTPUT", $questionoutput);
 
 		$solutionoutput = $solutiontemplate->get(); 
@@ -1201,6 +1218,10 @@ class assMatchingQuestionGUI extends assQuestionGUI
 		$errors = $this->feedback(true);
 		$this->object->saveFeedbackGeneric(0, $_POST["feedback_incomplete"]);
 		$this->object->saveFeedbackGeneric(1, $_POST["feedback_complete"]);
+		foreach ($this->object->getMatchingPairs() as $index => $answer)
+		{
+			$this->object->saveFeedbackSingleAnswer($index, $_POST["feedback_answer_$index"]);
+		}		
 		$this->object->cleanupMediaObjectUsage();
 		parent::saveFeedback();
 	}
@@ -1310,5 +1331,96 @@ class assMatchingQuestionGUI extends assQuestionGUI
 			$ilTabs->setBackTarget($this->lng->txt("qpl"), $this->ctrl->getLinkTargetByClass("ilobjquestionpoolgui", "questions"));
 		}
 	}
+
+		
+	/**
+	* Creates the output of the feedback page for a single choice question
+	*
+	* @access public
+	*/
+	function feedback($checkonly = false)
+	{
+		$save = (strcmp($this->ctrl->getCmd(), "saveFeedback") == 0) ? TRUE : FALSE;
+		include_once("./Services/Form/classes/class.ilPropertyFormGUI.php");
+		$form = new ilPropertyFormGUI();
+		$form->setFormAction($this->ctrl->getFormAction($this));
+		$form->setTitle($this->lng->txt('feedback_answers'));
+		$form->setTableWidth("98%");
+		$form->setId("feedback");
+
+		$complete = new ilTextAreaInputGUI($this->lng->txt("feedback_complete_solution"), "feedback_complete");
+		$complete->setValue($this->object->prepareTextareaOutput($this->object->getFeedbackGeneric(1)));
+		$complete->setRequired(false);
+		$complete->setRows(10);
+		$complete->setCols(80);
+		if (!$this->getPreventRteUsage())
+		{
+			$complete->setUseRte(true);
+		}
+		include_once "./Services/AdvancedEditing/classes/class.ilObjAdvancedEditing.php";
+		$complete->setRteTags(ilObjAdvancedEditing::_getUsedHTMLTags("assessment"));
+		$complete->addPlugin("latex");
+		$complete->addButton("latex");
+		$complete->addButton("pastelatex");
+		$complete->setRTESupport($this->object->getId(), "qpl", "assessment");
+		$form->addItem($complete);
+
+		$incomplete = new ilTextAreaInputGUI($this->lng->txt("feedback_incomplete_solution"), "feedback_incomplete");
+		$incomplete->setValue($this->object->prepareTextareaOutput($this->object->getFeedbackGeneric(0)));
+		$incomplete->setRequired(false);
+		$incomplete->setRows(10);
+		$incomplete->setCols(80);
+		if (!$this->getPreventRteUsage())
+		{
+			$incomplete->setUseRte(true);
+		}
+		include_once "./Services/AdvancedEditing/classes/class.ilObjAdvancedEditing.php";
+		$incomplete->setRteTags(ilObjAdvancedEditing::_getUsedHTMLTags("assessment"));
+		$incomplete->addPlugin("latex");
+		$incomplete->addButton("latex");
+		$incomplete->addButton("pastelatex");
+		$incomplete->setRTESupport($this->object->getId(), "qpl", "assessment");
+		$form->addItem($incomplete);
+	
+		if (!$this->getSelfAssessmentEditingMode())
+		{
+			foreach ($this->object->getMatchingPairs() as $index => $answer)
+			{
+				$caption = $ordinal = $index+1;
+				$caption .= '. <br />"' . $answer->term->text . '" =&gt; ';
+				$caption .= '"' . $answer->definition->text . '"';
+				$caption .= '</i>';
+				
+				$answerobj = new ilTextAreaInputGUI($this->object->prepareTextareaOutput($caption, true), "feedback_answer_$index");
+				$answerobj->setValue($this->object->prepareTextareaOutput($this->object->getFeedbackSingleAnswer($index)));
+				$answerobj->setRequired(false);
+				$answerobj->setRows(10);
+				$answerobj->setCols(80);
+				$answerobj->setUseRte(true);
+				include_once "./Services/AdvancedEditing/classes/class.ilObjAdvancedEditing.php";
+				$answerobj->setRteTags(ilObjAdvancedEditing::_getUsedHTMLTags("assessment"));
+				$answerobj->addPlugin("latex");
+				$answerobj->addButton("latex");
+				$answerobj->addButton("pastelatex");
+				$answerobj->setRTESupport($this->object->getId(), "qpl", "assessment");
+				$form->addItem($answerobj);
+			}
+		}
+
+		global $ilAccess;
+		if ($ilAccess->checkAccess("write", "", $_GET['ref_id']) || $this->getSelfAssessmentEditingMode())
+		{
+			$form->addCommandButton("saveFeedback", $this->lng->txt("save"));
+		}
+		if ($save)
+		{
+			$form->setValuesByPost();
+			$errors = !$form->checkInput();
+			$form->setValuesByPost(); // again, because checkInput now performs the whole stripSlashes handling and we need this if we don't want to have duplication of backslashes
+		}
+		if (!$checkonly) $this->tpl->setVariable("ADM_CONTENT", $form->getHTML());
+		return $errors;
+	}
+
 }
 ?>
