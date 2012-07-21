6e074b2b2114926564a0c527b1fe72b7e98b0923
Michael Jansen <mjansen@databay.de>
2012-07-21 17:41:34 +0000

    PHPDoc, license and code formatting
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@35698 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/PHPUnit/classes/class.ilUnitUtil.php
+++ Services/PHPUnit/classes/class.ilUnitUtil.php
@@ -1,58 +1,47 @@
 <?php
-/*
-	+-----------------------------------------------------------------------------+
-	| ILIAS open source                                                           |
-	+-----------------------------------------------------------------------------+
-	| Copyright (c) 1998-2006 ILIAS open source, University of Cologne            |
-	|                                                                             |
-	| This program is free software; you can redistribute it and/or               |
-	| modify it under the terms of the GNU General Public License                 |
-	| as published by the Free Software Foundation; either version 2              |
-	| of the License, or (at your option) any later version.                      |
-	|                                                                             |
-	| This program is distributed in the hope that it will be useful,             |
-	| but WITHOUT ANY WARRANTY; without even the implied warranty of              |
-	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
-	| GNU General Public License for more details.                                |
-	|                                                                             |
-	| You should have received a copy of the GNU General Public License           |
-	| along with this program; if not, write to the Free Software                 |
-	| Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
-	+-----------------------------------------------------------------------------+
-*/
+/* Copyright (c) 1998-2012 ILIAS open source, Extended GPL, see docs/LICENSE */
 
 /**
-* Utilities for Unit Testing
-*
-* @author Alex Killing <alex.killing@gmx.de>
-* @version $Id: class.ilSetting.php 15697 2008-01-08 20:04:33Z hschottm $
-*/
+ * Utilities for Unit Testing
+ * @author Alex Killing <alex.killing@gmx.de>
+ * @version $Id: class.ilSetting.php 15697 2008-01-08 20:04:33Z hschottm $
+ */
 class ilUnitUtil
 {
-	function performInitialisation()
+	/**
+	 * @static
+	 */
+	public static function performInitialisation()
 	{
+		/**
+		 * @var $ilErr ilErrorHandling
+		 */
 		global $ilErr;
-		
-		define("IL_PHPUNIT_TEST", true);
-		
-		session_id("phpunittest");
+
+		define('IL_PHPUNIT_TEST', true);
+
+		session_id('phpunittest');
 		$_SESSION = array();
-				
-		include("./Services/PHPUnit/config/cfg.phpunit.php");
-		
-		include_once "Services/Context/classes/class.ilContext.php";
+
+		include 'Services/PHPUnit/config/cfg.phpunit.php';
+
+		include_once 'Services/Context/classes/class.ilContext.php';
 		ilContext::init(ilContext::CONTEXT_UNITTEST);
-		
-		include_once("Services/Init/classes/class.ilInitialisation.php");
+
+		include_once('Services/Init/classes/class.ilInitialisation.php');
 		ilInitialisation::initILIAS();
 		ilInitialisation::initUserAccount();
-		
-		$ilUnitUtil = new ilUnitUtil;
-		$ilErr->setErrorHandling(PEAR_ERROR_CALLBACK, array($ilUnitUtil, "errorHandler"));	
-		PEAR::setErrorHandling(PEAR_ERROR_CALLBACK, array($ilUnitUtil, "errorHandler"));
+
+		$ilUnitUtil = new self();
+		$ilErr->setErrorHandling(PEAR_ERROR_CALLBACK, array($ilUnitUtil, 'errorHandler'));
+		PEAR::setErrorHandling(PEAR_ERROR_CALLBACK, array($ilUnitUtil, 'errorHandler'));
 	}
-	
-	function errorHandler($a_error_obj)
+
+	/**
+	 * @param $a_error_obj
+	 * @throws Exception
+	 */
+	public function errorHandler($a_error_obj)
 	{
 		echo "Error occured: ".get_class($a_error_obj)."\n";
 		try
@@ -63,9 +52,6 @@ class ilUnitUtil
  		{
 	 		$this->write($e->getTraceAsString());
  		}
-
-		//var_dump($a_error_obj);
-		exit;
+		exit();
 	}
 }
-?>
