f940a48cf47944fee81ea6ddbdd5ec74b764911c
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2012-07-27 13:31:55 +0000

    client_id escaping
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@35837 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Init/classes/class.ilInitialisation.php
+++ Services/Init/classes/class.ilInitialisation.php
@@ -250,10 +250,11 @@ class ilInitialisation
 		{
 			self::abortAndDie("Fatal Error: ilInitialisation::determineClient called without initialisation of ILIAS ini file object.");
 		}
-
+				
 		// set to default client if empty
 		if ($_GET["client_id"] != "")
 		{
+			$_GET["client_id"] = ilUtil::stripSlashes($_GET["client_id"]);
 			if (!defined("IL_PHPUNIT_TEST"))
 			{
 				ilUtil::setCookie("ilClientId", $_GET["client_id"]);
