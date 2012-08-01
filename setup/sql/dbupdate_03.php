991957647023bb5031542c394873b8534f64366a
Maximilian Becker <mbecker@databay.de>
2012-08-01 07:24:56 +0000

    dbupdate step for answer specific feedback in essay question
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@35889 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- setup/sql/dbupdate_03.php
+++ setup/sql/dbupdate_03.php
@@ -11799,3 +11799,40 @@ if(!$ilDB->tableExists('qpl_a_essay'))
 		);
 	}
 ?>
+<#3686>
+<?php
+	if (!$ilDB->tableExists('qpl_fb_essay'))
+	{
+		$fields = array(
+			'feedback_id' => array(
+				'type' => 'integer',
+				'length' => 4,
+				'notnull' => true
+			),
+			'question_fi' => array(
+				'type' => 'integer',
+				'length' => 4,
+				'notnull' => true
+			),
+			'answer' => array(
+				'type' => 'integer',
+				'length' => 4,
+				'notnull' => true
+			),
+			'feedback' => array(
+				'type' => 'text',
+				'length' => 4000,
+				'notnull' => false
+			),
+			'tstamp' => array(
+				'type' => 'integer',
+				'length' => 4,
+				'notnull' => true
+			)
+		);
+
+		$ilDB->createTable('qpl_fb_essay', $fields);
+		$ilDB->addIndex('qpl_fb_essay', array('question_fi'), 'i1');
+		$ilDB->createSequence('qpl_fb_essay');
+	}
+?>
