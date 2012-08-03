ee0dd68f0cac4ae419e02889d2428f5f86cb3ccf
Maximilian Becker <mbecker@databay.de>
2012-08-03 15:43:13 +0000

    Unit tests
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@35984 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/TestQuestionPool/test/assErrorTextTest.php
+++ Modules/TestQuestionPool/test/assErrorTextTest.php
@@ -55,28 +55,116 @@ class assErrorTextTest extends PHPUnit_Framework_TestCase
 		$instance = new assErrorText();
 		
 		// Assert
-		$this->assertTrue(TRUE);
+		$this->assertNotNull($instance);
 	}
-	
-	public function test_getErrorData()
+
+	public function test_getErrorsFromText()
 	{
-		$this->markTestIncomplete('Incomplete.');
-		return;
 		// Arrange
 		require_once './Modules/TestQuestionPool/classes/class.assErrorText.php';
+		$instance = new assErrorText();
+		
+		$errortext = '
+			Eine ((Kündigung)) kommt durch zwei gleichlautende Willenserklärungen zustande.
+			Ein Vertrag kommt durch ((drei gleichlaute)) Willenserklärungen zustande.
+			Ein Kaufvertrag an der Kasse im Supermarkt kommt durch das legen von Ware auf das 
+			Kassierband und den Kassiervorgang zustande. Dies nennt man ((konsequentes)) Handeln.';
+		
+		$expected = array( 0 => 'Kündigung', 1 => 'drei gleichlaute', 2 => 'konsequentes' );
+		
+		// Act
+		$actual = $instance->getErrorsFromText($errortext);
+		
+		// Assert
+		$this->assertEquals($expected, $actual);
+	}
+
+	public function test_getErrorsFromText_noMatch()
+	{
+		// Arrange
+		require_once './Modules/TestQuestionPool/classes/class.assErrorText.php';
+		$instance = new assErrorText();
+
+		$errortext = '
+			Eine Kündigung)) kommt durch zwei gleichlautende (Willenserklärungen) zustande.
+			Ein Vertrag kommt durch (drei gleichlaute) Willenserklärungen zustande.
+			Ein Kaufvertrag an der Kasse im Supermarkt [kommt] durch das #legen von Ware auf das 
+			Kassierband und den [[Kassiervorgang]] zustande. Dies nennt man *konsequentes Handeln.';
+
+		$expected = array();
 
 		// Act
-		$instance = new assErrorText
-		(
-			'A Test Question',
-			'A Comment',
-			'root user',
-			'6',
-			'Correct the following text:'
-		);
-		
-		$instance->setErrorText('An ((error text)) with ((errors)) in it.');
-		$instance->setErrorData($error_data);
-		$errdata = $instance->getErrorData();
+		$actual = $instance->getErrorsFromText($errortext);
+
+		// Assert
+		$this->assertEquals($expected, $actual);
 	}
+
+	public function test_getErrorsFromText_emptyArgShouldPullInternal()
+	{
+		// Arrange
+		require_once './Modules/TestQuestionPool/classes/class.assErrorText.php';
+		$instance = new assErrorText();
+
+		$errortext = '
+			Eine ((Kündigung)) kommt durch zwei gleichlautende Willenserklärungen zustande.
+			Ein Vertrag kommt durch ((drei gleichlaute)) Willenserklärungen zustande.
+			Ein Kaufvertrag an der Kasse im Supermarkt kommt durch das legen von Ware auf das 
+			Kassierband und den Kassiervorgang zustande. Dies nennt man ((konsequentes)) Handeln.';
+
+		$expected = array( 0 => 'Kündigung', 1 => 'drei gleichlaute', 2 => 'konsequentes' );
+
+		// Act
+		$instance->setErrorText($errortext);
+		$actual = $instance->getErrorsFromText('');
+
+		// Assert
+		$this->assertEquals($expected, $actual);
+	}
+	
+	public function test_setErrordata_newError()
+	{
+		// Arrange
+		require_once './Modules/TestQuestionPool/classes/class.assErrorText.php';
+		$instance = new assErrorText();
+		
+		$errordata = array ('error1');
+		require_once "./Modules/TestQuestionPool/classes/class.assAnswerErrorText.php";
+		$expected = new assAnswerErrorText($errordata[0], '', 0.0);
+		
+		// Act
+		$instance->setErrorData($errordata);
+		
+		$all_errors = $instance->getErrorData();
+		$actual = $all_errors[0];
+		
+		// Assert
+		$this->assertEquals($expected, $actual);		
+	}
+	
+	public function test_setErrordata_oldErrordataPresent()
+	{
+		$this->markTestIncomplete('No good way to prepopulate errordata to make this test meaningful.');
+		return;
+		
+		// Arrange
+		require_once './Modules/TestQuestionPool/classes/class.assErrorText.php';
+		$instance = new assErrorText();
+
+		$errordata = array ('error1');
+		require_once "./Modules/TestQuestionPool/classes/class.assAnswerErrorText.php";
+		$expected = new assAnswerErrorText($errordata[0], 'correct1', 10.0);
+		$instance->errordata = $expected;
+		
+		// Act
+		$instance->setErrorData($errordata);
+
+		$all_errors = $instance->getErrorData();
+		$actual = $all_errors[0];
+
+		// Assert
+		$this->assertEquals($expected, $actual);
+	}	
+	
+
 }
