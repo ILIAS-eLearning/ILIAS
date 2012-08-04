fcd4ce74243a804021ea242458fca9d6e9f1ec89
Michael Jansen <mjansen@databay.de>
2012-08-04 15:54:29 +0000

    Fixed context problems of permission screens: Chat Administration vs. Public Chat Fixed chat tab activation Code style refactoring
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@35989 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Chatroom/classes/class.ilObjChatroomAdminGUI.php
+++ Modules/Chatroom/classes/class.ilObjChatroomAdminGUI.php
@@ -1,33 +1,26 @@
 <?php
+/* Copyright (c) 1998-2012 ILIAS open source, Extended GPL, see docs/LICENSE */
 
-/* Copyright (c) 1998-2010 ILIAS open source, Extended GPL, see docs/LICENSE */
-
-require_once "./Services/Object/classes/class.ilObjectGUI.php";
-require_once "./Modules/Chatroom/classes/class.ilObjChatroom.php";
-require_once "./Modules/Chatroom/classes/class.ilObjChatroomAccess.php";
+require_once 'Services/Object/classes/class.ilObjectGUI.php';
+require_once 'Modules/Chatroom/classes/class.ilObjChatroom.php';
+require_once 'Modules/Chatroom/classes/class.ilObjChatroomAccess.php';
 require_once 'Modules/Chatroom/lib/DatabayHelper/databayHelperLoader.php';
 
 /**
  * Class ilObjChatroomAdminGUI
- *
  * GUI class for chatroom objects.
- *
- * @author Jan Posselt <jposselt at databay.de>
- * @version $Id$
- *
- * @ilCtrl_Calls ilObjChatroomAdminGUI: ilMDEditorGUI, ilInfoScreenGUI, ilPermissionGUI, ilObjectCopyGUI
- * @ilCtrl_Calls ilObjChatroomAdminGUI: ilExportGUI
+ * @author            Jan Posselt <jposselt at databay.de>
+ * @version           $Id$
+ * @ilCtrl_Calls      ilObjChatroomAdminGUI: ilMDEditorGUI, ilInfoScreenGUI, ilPermissionGUI, ilObjectCopyGUI
+ * @ilCtrl_Calls      ilObjChatroomAdminGUI: ilExportGUI
  * @ilCtrl_IsCalledBy ilObjChatroomAdminGUI: ilRepositoryGUI, ilAdministrationGUI
- *
- * @ingroup ModulesChatroom
+ * @ingroup           ModulesChatroom
  */
 class ilObjChatroomAdminGUI extends ilDBayObjectGUI
 {
-
 	/**
 	 * Constructor
-	 *
-	 * @param array $a_data
+	 * @param array   $a_data
 	 * @param integer $a_id
 	 * @param boolean $a_call_by_reference
 	 */
@@ -35,39 +28,36 @@ class ilObjChatroomAdminGUI extends ilDBayObjectGUI
 	{
 		global $lng;
 
-		$lng->loadLanguageModule( 'chatroom_adm' );
+		$lng->loadLanguageModule('chatroom_adm');
 
-		if( $a_data == null )
+		if($a_data == null)
 		{
-			if( $_GET['serverInquiry'] )
+			if($_GET['serverInquiry'])
 			{
-				require_once dirname( __FILE__ ) . '/class.ilChatroomServerHandler.php';
+				require_once dirname(__FILE__) . '/class.ilChatroomServerHandler.php';
 				new ilChatroomServerHandler();
 				return;
 			}
 		}
 
 		$this->type = 'chta';
-		$this->ilObjectGUI( $a_data, $a_id, $a_call_by_reference, false );
-
+		parent::__construct($a_data, $a_id, $a_call_by_reference, false);
 	}
 
 	/**
 	 * Returns object definition by calling getDefaultDefinitionWithCustomTaskPath
 	 * method in ilDBayObjectDefinition.
-	 *
 	 * @return ilDBayObjectDefinition
 	 */
 	protected function getObjectDefinition()
 	{
 		return ilDBayObjectDefinition::getDefaultDefinitionWithCustomTaskPath(
-				'Chatroom', 'admintasks'
-				);
+			'Chatroom', 'admintasks'
+		);
 	}
 
 	/**
 	 * Returns empty array.
-	 *
 	 * @return array
 	 */
 	public function _forwards()
@@ -77,39 +67,38 @@ class ilObjChatroomAdminGUI extends ilDBayObjectGUI
 
 	/**
 	 * Dispatches the command to the related executor class.
-	 *
-	 * @global ilCtrl2 $ilCtrl
 	 */
 	public function executeCommand()
 	{
-		//global $ilAccess, $ilNavigationHistory, $ilCtrl, $ilUser, $ilTabs;
+		/**
+		 * @var $ilCtrl ilCtrl
+		 */
 		global $ilCtrl;
 
 		$next_class = $ilCtrl->getNextClass();
 
 		require_once 'Modules/Chatroom/classes/class.ilChatroomTabFactory.php';
 
-		$tabFactory = new ilChatroomTabFactory( $this );
-		$tabFactory->getAdminTabsForCommand( $ilCtrl->getCmd() );
+		$tabFactory = new ilChatroomTabFactory($this);
+		$tabFactory->getAdminTabsForCommand($ilCtrl->getCmd());
 
 		switch($next_class)
 		{
 			case 'ilpermissiongui':
-				include_once("Services/AccessControl/classes/class.ilPermissionGUI.php");
+				include_once 'Services/AccessControl/classes/class.ilPermissionGUI.php';
 				$this->prepareOutput();
-				$perm_gui = & new ilPermissionGUI( $this );
-				$ret = & $this->ctrl->forwardCommand( $perm_gui );
+				$perm_gui = new ilPermissionGUI($this);
+				$ilCtrl->forwardCommand($perm_gui);
 				break;
 
 			default:
-				$res = split( '-', $ilCtrl->getCmd(), 2 );
-				$this->dispatchCall( $res[0], $res[1] ? $res[1] : '' );
+				$res = explode('-', $ilCtrl->getCmd(), 2);
+				$this->dispatchCall($res[0], $res[1] ? $res[1] : '');
 		}
 	}
 
 	/**
 	 * Returns connector.
-	 *
 	 * @return ilChatroomServerConnector
 	 */
 	public function getConnector()
@@ -117,40 +106,28 @@ class ilObjChatroomAdminGUI extends ilDBayObjectGUI
 		require_once 'Modules/Chatroom/classes/class.ilChatroomServerConnector.php';
 		require_once 'Modules/Chatroom/classes/class.ilChatroomServerSettings.php';
 
-		$settings	= ilChatroomServerSettings::loadDefault();
-		$connector	= new ilChatroomServerConnector( $settings );
+		$settings  = ilChatroomServerSettings::loadDefault();
+		$connector = new ilChatroomServerConnector($settings);
 
 		return $connector;
 	}
 
 	/**
 	 * Overwrites $_GET['ref_id'] with given $ref_id.
-	 *
-	 * @global ilCtrl2 $ilCtrl
-	 * @param integer $ref_id
+	 * @param integer  $ref_id
 	 */
 	public static function _goto($ref_id)
 	{
-		//global $ilCtrl;
-		//$ilCtrl->setParameter($this, 'cmd', 'view');
-
-		include_once("./Services/Object/classes/class.ilObjectGUI.php");
-		ilObjectGUI::_gotoRepositoryNode($ref_id, "view");
-		/*$_GET['cmd'] = 'view';
-		$_GET['ref_id'] = $ref_id;
-		require 'repository.php';*/
+		include_once 'Services/Object/classes/class.ilObjectGUI.php';
+		ilObjectGUI::_gotoRepositoryNode($ref_id, 'view');
 	}
 
 	/**
 	 * Returns RefId.
-	 *
 	 * @return integer
 	 */
 	public function getRefId()
 	{
 		return $this->object->getRefId();
 	}
-
 }
-
-?>
