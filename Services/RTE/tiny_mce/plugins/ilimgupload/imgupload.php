5ac151ad4b2e1868e8f760cd8bc8ae75b06133b0
Michael Jansen <mjansen@databay.de>
2012-08-10 14:28:00 +0000

    TinyMCE: Internal image manager part I
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@36126 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/RTE/tiny_mce/plugins/ilimgupload/imgupload.php
+++ Services/RTE/tiny_mce/plugins/ilimgupload/imgupload.php
@@ -1,24 +1,40 @@
 <?php
-/* Copyright (c) 1998-2010 ILIAS open source, Extended GPL, see docs/LICENSE */
+/* Copyright (c) 1998-2012 ILIAS open source, Extended GPL, see docs/LICENSE */
 
-include_once "../../../../../Services/Init/classes/class.ilIniFile.php";
-$file = "../../../../../ilias.ini.php";
-$ini = new ilIniFile($file);
-$ini->read();
-$htdocs=$ini->readVariable("server", "absolute_path") . "/";
-$weburl=$ini->readVariable("server", "http_path") . "/";
-$installpath=$htdocs;
+chdir('../../../../../');
+
+require_once 'Services/Init/classes/class.ilInitialisation.php';
+ilInitialisation::initILIAS();
+
+/**
+ * @var $ilIliasIniFile ilIniFile
+ */
+global $ilIliasIniFile;
+
+$htdocs      = $ilIliasIniFile->readVariable('server', 'absolute_path') . '/';
+$weburl      = $ilIliasIniFile->readVariable('server', 'http_path') . '/';
+$installpath = $htdocs;
 
 // directory where tinymce files are located
-$iliasMobPath = "data/" . $_GET["client_id"] . "/mobs/";
+$iliasMobPath      = 'data/' . CLIENT_ID . '/mobs/';
 $iliasAbsolutePath = $htdocs;
-$iliasHttpPath = $weburl;
+$iliasHttpPath     = $weburl;
+if($iliasHttpPath)
+{
+	/**
+	 * @var $https ilHttps
+	 */
+	global $https;
+
+	if(strpos($iliasHttpPath, 'https://') === false && $https->isDetected())
+	{
+		$iliasHttpPath = str_replace('http://', 'https://', $iliasHttpPath);
+	}
+}
+
 // base url for images
-$tinyMCE_base_url = "$weburl";
-
-$tinyMCE_DOC_url = "$installpath";
-
-// image library related config
+$tinyMCE_base_url = $weburl;
+$tinyMCE_DOC_url  = $installpath;
 
 // allowed extentions for uploaded image files
 $tinyMCE_valid_imgs = array('gif', 'jpg', 'jpeg', 'png');
@@ -29,102 +45,89 @@ $tinyMCE_upload_allowed = true;
 // allow delete in image library
 $tinyMCE_img_delete_allowed = false;
 
-$_GET["client_id"] = str_replace("..", "", $_GET["client_id"]);
-
-$dir = getcwd();
-chdir("../../../../../");
-include_once "webservice/soap/include/inc.soap_functions.php";
-$mobs = ilSoapFunctions::getMobsOfObject($_GET["session_id"]."::".$_GET["client_id"], $_GET["obj_type"].":html", $_GET["obj_id"]);
-chdir($dir);
-$preview = "";
-$arr_tinyMCE_image_files = array();
-$request_uri = urldecode(empty($_POST['request_uri'])?(empty($_GET['request_uri'])?'':$_GET['request_uri']):$_POST['request_uri']);
-$img = isset($_POST['imglist'])? $_POST['imglist'] : '';
-$_root = $installpath;
-$errors = array();
+include_once 'webservice/soap/include/inc.soap_functions.php';
+$mobs        = ilSoapFunctions::getMobsOfObject(session_id() . '::' . CLIENT_ID, $_GET['obj_type'] . ':html', (int)$_GET['obj_id']);
+$preview     = '';
+$mob_details = array();
+$img         = isset($_POST['imglist']) ? $_POST['imglist'] : '';
+$_root       = $installpath;
 
 // upload images
 $uploadedFile = false;
-if (isset($_FILES['img_file']['size']) && $_FILES['img_file']['size']>0)
+if(isset($_FILES['img_file']['size']) && $_FILES['img_file']['size'] > 0)
 {
-	$dir = getcwd();
-	chdir("../../../../../");
-	include_once "webservice/soap/include/inc.soap_functions.php";
-	$safefilename = preg_replace("/[^a-zA-z0-9_\.]/", "", $_FILES["img_file"]["name"]);
-	$media_object = ilSoapFunctions::saveTempFileAsMediaObject($_GET["session_id"]."::".$_GET["client_id"], $safefilename, $_FILES["img_file"]["tmp_name"]);
-	if (file_exists($iliasAbsolutePath.$iliasMobPath."mm_".$media_object->getId() . "/" . $media_object->getTitle()))
+	include_once 'webservice/soap/include/inc.soap_functions.php';
+	$safefilename = preg_replace('/[^a-zA-z0-9_\.]/', '', $_FILES['img_file']['name']);
+	$media_object = ilSoapFunctions::saveTempFileAsMediaObject(session_id() . '::' . CLIENT_ID, $safefilename, $_FILES['img_file']['tmp_name']);
+	if(file_exists($iliasAbsolutePath . $iliasMobPath . 'mm_' . $media_object->getId() . '/' . $media_object->getTitle()))
 	{
 		// only save usage if the file was uploaded
-		$media_object->_saveUsage($media_object->getId(), $_GET["obj_type"].":html", $_GET["obj_id"]);
-	}
-	chdir($dir);
+		$media_object->_saveUsage($media_object->getId(), $_GET['obj_type'] . ':html', (int)$_GET['obj_id']);
+		
+		// Append file to array of existings mobs of this context (obj_type and obj_id)
+		$mobs[$media_object->getId()] = $media_object->getId();
 
-	$preview = $iliasHttpPath.$iliasMobPath."mm_".$media_object->getId() . "/" . $media_object->getTitle();
-	$mobs[$media_object->getId()] = $media_object->getId();
-	
-	$uploadedFile = true;	
+		$uploadedFile   = $media_object->getId();
+		$_GET['update'] = 1;
+	}
+}
+$mob_details = array();
+foreach($mobs as $mob)
+{
+	$mobdir = $iliasAbsolutePath . $iliasMobPath . 'mm_' . $mob . '/';
+	$d      = @dir($mobdir);
+	if($d)
+	{
+		$i = 0;
+		while(false !== ($entry = $d->read()))
+		{
+			$ext = strtolower(substr(strrchr($entry, '.'), 1));
+			if(is_file($mobdir . $entry) && in_array($ext, $tinyMCE_valid_imgs))
+			{
+				$mob_details[$uploadedFile]['file_name'] = $entry;
+				$mob_details[$uploadedFile]['file_dir']  = $mobdir;
+				$mob_details[$uploadedFile]['http_dir']  = $iliasHttpPath . $iliasMobPath . 'mm_' . $mob . '/';
+			}
+		}
+		$d->close();
+	}
 }
 
-$tpl = new HTML_Template_ITX();
-$tpl->loadTemplatefile("tpl.img_upload.html", true, true);
+$tpl = new ilTemplate(dirname(__FILE__) . "/tpl.img_upload.html", true, true);
 
-$tpl->setVariable("REQUEST_URI", $_GET["request_uri"]);
-$tpl->setVariable("VALUE_REQUEST_URI", $request_uri);
-$tpl->setVariable("OBJ_ID", $_GET["obj_id"]);
+$tpl->setVariable("OBJ_ID", (int)$_GET["obj_id"]);
 $tpl->setVariable("OBJ_TYPE", $_GET["obj_type"]);
-$tpl->setVariable("SESSION_ID", $_GET["session_id"]);
-$tpl->setVariable("CLIENT_ID", $_GET["client_id"]);
-$tpl->setVariable("VALUE_UPDATE", $_GET["update"]);
+$tpl->setVariable("VALUE_UPDATE", (int)$_GET["update"]);
 $tpl->setVariable("ILIAS_INST_PATH", $iliasHttpPath);
-if ($_GET["update"] == 1)
-{	
-	$tpl->setVariable("INSERT_TYPE", "button");
-	$tpl->setVariable("INSERT_COMMAND", "{#update}");
-	$tpl->touchBlock('src');
-	$tpl->touchBlock('preview');
+$tpl->setVariable('IS_UPDATE', (int)$_GET["update"]);
+if($ilUser->getLanguage() == 'de')
+{
+	$tpl->touchBlock('validation_engine_lang_de');
 }
 else
 {
-	$tpl->setVariable("INSERT_TYPE", "submit");
-	$tpl->setVariable("INSERT_COMMAND", "{#ilimgupload.upload}");
-	$tpl->touchBlock('upload');
+	$tpl->touchBlock('validation_engine_lang_default');
 }
 
-$error_messages = "";
-if(!empty($errors))
+if($_GET["update"] == 1)
 {
-	$error_messages .= '<span class="error">';
-	foreach ($errors as $err)
-  	{
-		$error_messages .= $err . "<br />";
-	}
-  	$error_messages .= "</span>";
+	$tpl->setVariable("IMG_FROM_URL_TAB_DESC", "{#ilimgupload.edit_image}");
+	$tpl->setVariable("IMG_FROM_URL_DESC", "{#ilimgupload.edit_image_desc}");
+	$tpl->setVariable("INSERT_COMMAND", "{#update}");
 }
-else if($uploadedFile)
+else
 {
-	$tpl->setVariable('WIDTH', $_POST['width']);
-	$tpl->setVariable('HEIGHT', $_POST['height']);
-	$tpl->setVariable('SRC', $preview);
-	$tpl->setVariable('ALT', $_POST['alt']);
-	$tpl->touchBlock('adoptimage');
+	$tpl->setVariable("IMG_FROM_URL_TAB_DESC", "{#ilimgupload.upload_image_from_url}");
+	$tpl->setVariable("IMG_FROM_URL_DESC", "{#ilimgupload.upload_image_from_url_desc}");
+	$tpl->setVariable("INSERT_COMMAND", "{#ilimgupload.insert}");
 }
 
-
-$tpl->setVariable("ERROR_MESSAGES", $error_messages);
+if($uploadedFile && $mob_details[$uploadedFile])
+{
+	$img_size = getimagesize($mob_details[$uploadedFile]['file_dir'] . $mob_details[$uploadedFile]['file_name']);
+	$tpl->setVariable('UPLOADED_FILE_WIDTH', (int)$img_size[0]);
+	$tpl->setVariable('UPLOADED_FILE_HEIGHT', (int)$img_size[1]);
+	$tpl->setVariable('UPLOADED_FILE_SRC', $mob_details[$uploadedFile]['http_dir'] . $mob_details[$uploadedFile]['file_name']);
+}
 
 $tpl->show();
-
-function filesize_h($size, $dec = 1)
-{
-	$sizes = array('byte(s)', 'kb', 'mb', 'gb');
-	$count = count($sizes);
-	$i = 0;
-
-	while ($size >= 1024 && ($i < $count - 1)) {
-		$size /= 1024;
-		$i++;
-	}
-
-	return round($size, $dec) . ' ' . $sizes[$i];
-}
-?>
