90f2383f70e2fbae796099a53ace9bce6f92c788
Fabian Schmid <fs@studer-raimann.ch>
2012-08-23 12:16:22 +0000

    [DataCollection] Add Icon Fix
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@36451 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/DataCollection/classes/class.ilDataCollectionFieldListTableGUI.php
+++ Modules/DataCollection/classes/class.ilDataCollectionFieldListTableGUI.php
@@ -47,7 +47,7 @@ class ilDataCollectionFieldListTableGUI  extends ilTable2GUI
         
         $img = "<img src='".ilUtil::getImagePath("cmd_add_s.png")."' /> "; // Wirklich hässlich, doch leider wird der Text, der addHeaderCommand mitgeben wird, nicht mehr angezeigt, sobald man ein Bild mitsendet...
         
-        $this->addHeaderCommand($ilCtrl->getLinkTargetByClass("ildatacollectionfieldeditgui", "create"), $img.$lng->txt("dcl_add_new_field"));
+        $this->addHeaderCommand($ilCtrl->getLinkTargetByClass("ildatacollectionfieldeditgui", "create"), $lng->txt("dcl_add_new_field").$img);
         $this->setFormAction($ilCtrl->getFormActionByClass("ildatacollectionfieldlistgui"));
         $this->addCommandButton("save", $lng->txt("dcl_save"));
 
