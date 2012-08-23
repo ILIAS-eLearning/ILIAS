caccf5fa73bbeb02b583627b93bbdb2915c34993
Fabian Schmid <fs@studer-raimann.ch>
2012-08-23 12:42:41 +0000

    [DataCollection]
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@36454 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/DataCollection/classes/class.ilDataCollectionRecordField.php
+++ Modules/DataCollection/classes/class.ilDataCollectionRecordField.php
@@ -15,25 +15,40 @@ class ilDataCollectionRecordField
     private $field;
     private $record;
     private $value;
-
+    
+    /*
+     * __construct
+     */
     function __construct(ilDataCollectionRecord $record, ilDataCollectionField $field)
     {
         $this->record = $record;
         $this->field = $field;
         $this->doRead();
     }
-
-    private function doRead(){
+    
+    /*
+     * doRead
+     */
+    private function doRead()
+    {
         global $ilDB;
+        
         $query = "SELECT * FROM il_dcl_record_field WHERE field_id LIKE '".$this->field->getId()."' AND record_id = ".$this->record->getId();
         $set = $ilDB->query($query);
         $rec = $ilDB->fetchAssoc($set);
         $this->id = $rec['id'];
-        if($this->id == Null)
-            $this->doCreate();
+        
+        if($this->id == NULL)
+        {
+	        $this->doCreate();
+        }
     }
-
-    private function doCreate(){
+    
+    /*
+     * doCreate
+     */
+    private function doCreate()
+    {
         global $ilDB;
 
         $id = $ilDB->nextId("il_dcl_record_field");
@@ -41,23 +56,57 @@ class ilDataCollectionRecordField
         $ilDB->manipulate($query);
         $this->id = $id;
     }
-
-    function getValue(){
-        $this->loadValue();
-		return $this->value?$this->value:"-";
-    }
-
-    function delete(){
+    
+    /*
+     * doUpdate
+     */
+    public function doUpdate()
+    {
+		global $ilDB;
+		
+		$this->loadValue();
         $datatype = $this->field->getDatatype();
+        $query = "DELETE FROM il_dcl_stloc".$datatype->getStorageLocation()."_value WHERE record_field_id = ".$this->id;
+        $ilDB->manipulate($query);
+		$next_id = $ilDB->nextId("il_dcl_stloc".$datatype->getStorageLocation()."_value");
+        $ilDB->insert("il_dcl_stloc".$datatype->getStorageLocation()."_value",
+            array("value" => array($datatype->getDbType(), $this->value),
+            "record_field_id " => array("integer", $this->id),
+			"id" => array("integer", $next_id))
+        );
+    }
+    
+    /*
+     * delete
+     */
+    public function delete()
+    {
         global $ilDB;
+        
+        $datatype = $this->field->getDatatype();
         $query = "DELETE FROM il_dcl_stloc".$datatype->getStorageLocation()."_value WHERE record_field_id = ".$this->id;
         $ilDB->manipulate($query);
 
         $query2 = "DELETE FROM il_dcl_record_field WHERE id = ".$this->id;
         $ilDB->manipulate($query2);
     }
+    
+    /*
+     * getValue
+     */
+    public function getValue()
+    {
+        $this->loadValue();
+        
+		return $this->value ? $this->value : "-";
+    }
 
-    function setValue($value){
+    
+    /*
+     * setValue
+     */
+    public function setValue($value)
+    {
         $type = $this->field->getDatatype()->getId();
         $this->loadValue();
 		$tmp = $this->field->getDatatype()->parseValue($value);
@@ -72,33 +121,42 @@ class ilDataCollectionRecordField
 		}
     }
 
-
-    function getFormInput(){
+    /*
+     * getFormInput
+     */
+    public function getFormInput()
+    {
         $datatype = $this->field->getDatatype();
         return $datatype->parseFormInput($this->getValue());
     }
-
-    function getHTML(){
+    
+    /*
+     * getExportValue
+     */
+    public function getExportValue()
+    {
+        $datatype = $this->field->getDatatype();
+        
+        return $datatype->parseExportValue($this->getValue());
+    }
+    
+    
+    /*
+     * getHTML
+     */
+    public function getHTML()
+    {
         $datatype = $this->field->getDatatype();
         return $datatype->parseHTML($this->getValue(), $this);
     }
 
-    function doUpdate(){
-		$this->loadValue();
-        $datatype = $this->field->getDatatype();
-        global $ilDB;
-        $query = "DELETE FROM il_dcl_stloc".$datatype->getStorageLocation()."_value WHERE record_field_id = ".$this->id;
-        $ilDB->manipulate($query);
-		$next_id = $ilDB->nextId("il_dcl_stloc".$datatype->getStorageLocation()."_value");
-        $ilDB->insert("il_dcl_stloc".$datatype->getStorageLocation()."_value",
-            array("value" => array($datatype->getDbType(), $this->value),
-            "record_field_id " => array("integer", $this->id),
-			"id" => array("integer", $next_id))
-        );
-    }
-
-    private function loadValue(){
-        if($this->value == Null){
+    /*
+     * loadValue
+     */
+    private function loadValue()
+    {
+        if($this->value == NULL)
+        {
             global $ilDB;
             $datatype = $this->field->getDatatype();
             $query = "SELECT * FROM il_dcl_stloc".$datatype->getStorageLocation()."_value WHERE record_field_id = ".$this->id;
@@ -107,15 +165,28 @@ class ilDataCollectionRecordField
             $this->value = $rec['value'];
         }
     }
-
-	public function getField(){
+    
+    /*
+     * getField
+     */
+	public function getField()
+	{
 		return $this->field;
 	}
-
-	public function getId(){
+	
+	/*
+	 * getId
+	 */
+	public function getId()
+	{
 		return $this->id;
 	}
-	public function getRecord(){
+	
+	/*
+	 * getRecord
+	 */
+	public function getRecord()
+	{
 		return $this->record;
 	}
 
