6906708b064adf45eaf1b768b34d60a9f5f71161
Stefan Meyer <smeyer.ilias@gmx.de>
2012-08-28 15:46:23 +0000

    Fixed include
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@36590 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/WebServices/Rest/classes/class.ilRestFileStorage.php
+++ Services/WebServices/Rest/classes/class.ilRestFileStorage.php
@@ -1,7 +1,7 @@
 <?php
 /* Copyright (c) 1998-2009 ILIAS open source, Extended GPL, see docs/LICENSE */
 
-include_once './Services/FileSystemStorage/classes/class.ilFileSystemStorage.php';
+include_once './Services/FileSystem/classes/class.ilFileSystemStorage.php';
 
 /**
  * File storage handling
