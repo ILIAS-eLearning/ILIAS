1a87b89e3e32503f8b347e07ac00e127d3247811
Alexander Killing <killing@leifos.de>
2012-09-23 17:05:15 +0000

    make glossary definition keywords appear in search autocomplete
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@37124 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Search/classes/class.ilSearchAutoComplete.php
+++ Services/Search/classes/class.ilSearchAutoComplete.php
@@ -21,7 +21,7 @@ class ilSearchAutoComplete
 		$settings = new ilSearchSettings();
 		
 		$object_types = array('cat','dbk','crs','fold','frm','grp','lm','sahs','glo','mep','htlm','exc','file','qpl','tst','svy','spl',
-			'chat','icrs','icla','webr','mcst','sess','pg','st','wiki');
+			'chat','icrs','icla','webr','mcst','sess','pg','st','gdf','wiki');
 
 		$set = $ilDB->query("SELECT title, obj_id FROM object_data WHERE "
 			.$ilDB->like('title', 'text', $a_str."%")." AND "
