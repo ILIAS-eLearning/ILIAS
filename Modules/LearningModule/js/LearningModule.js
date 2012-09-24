14cf2f306a01cd98a2ce71c806df3e969eeac892
Alexander Killing <killing@leifos.de>
2012-09-24 10:52:49 +0000

    close extra content area
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@37132 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/LearningModule/js/LearningModule.js
+++ Modules/LearningModule/js/LearningModule.js
@@ -6,11 +6,20 @@ il.LearningModule = {
 	loadContentFrame: function (href) {
 		var faqt = $("#bot_center_area");
 		if (faqt.length == 0) {
-			$('body').append('<div id="bot_center_area" class="ilBotCenterArea"><iframe /></div>');
+			$('body').append('<div id="bot_center_area" class="ilBotCenterArea"><img class="ilAreaClose" /><iframe /></div>');
 		}
+		$("img.ilAreaClose").click(function () {
+			il.LearningModule.closeContentFrame();
+			});
 		$("#bot_center_area > iframe").attr("src", href);
 		il.UICore.refreshLayout();
 		return false;
+	},
+	
+	closeContentFrame: function () {
+		//alert("close!");
+		$("#bot_center_area").remove();
+		il.UICore.refreshLayout();
 	}
 }
 
