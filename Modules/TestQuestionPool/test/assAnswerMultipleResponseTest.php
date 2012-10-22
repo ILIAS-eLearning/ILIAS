a32ad4c4b86017798e255c9d49e65d7559167797
Maximilian Becker <mbecker@databay.de>
2012-10-22 09:55:09 +0000

    Unit tests for assAnswerMultipleResponse
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@37757 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/TestQuestionPool/test/assAnswerMultipleResponseTest.php
+++ Modules/TestQuestionPool/test/assAnswerMultipleResponseTest.php
@@ -45,4 +45,61 @@ class assAnswerMultipleResponseTest extends PHPUnit_Framework_TestCase
 			chdir('../../../');
 		}
 	}
+
+	public function test_instantiateObjectSimple()
+	{
+		// Arrange
+		require_once './Modules/TestQuestionPool/classes/class.assAnswerMultipleResponse.php';
+
+		// Act
+		$instance = new ASS_AnswerMultipleResponse();
+
+		// Assert
+		$this->assertInstanceOf('ASS_AnswerMultipleResponse', $instance);
+	}
+
+	public function test_setGetPointsUnchecked()
+	{
+		// Arrange
+		require_once './Modules/TestQuestionPool/classes/class.assAnswerMultipleResponse.php';
+		$instance = new ASS_AnswerMultipleResponse();
+		$expected = 1;
+
+		// Act
+		$instance->setPointsUnchecked($expected);
+		$actual = $instance->getPointsUnchecked();
+
+		// Assert
+		$this->assertEquals($expected, $actual);
+	}
+
+	public function test_setGetPointsUnchecked_InvalidPointsBecomeZero()
+	{
+		// Arrange
+		require_once './Modules/TestQuestionPool/classes/class.assAnswerMultipleResponse.php';
+		$instance = new ASS_AnswerMultipleResponse();
+		$expected = 0;
+
+		// Act
+		$instance->setPointsUnchecked('Günther');
+		$actual = $instance->getPointsUnchecked();
+
+		// Assert
+		$this->assertEquals($expected, $actual);
+	}
+
+	public function test_setGetPointsChecked()
+	{
+		// Arrange
+		require_once './Modules/TestQuestionPool/classes/class.assAnswerMultipleResponse.php';
+		$instance = new ASS_AnswerMultipleResponse();
+		$expected = 2;
+
+		// Act
+		$instance->setPointsChecked($expected);
+		$actual = $instance->getPointsChecked();
+
+		// Assert
+		$this->assertEquals($expected, $actual);
+	}
 }
