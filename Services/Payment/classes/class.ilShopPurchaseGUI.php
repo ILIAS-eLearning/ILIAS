58255fb374c0924c92447e27bbb73dfec4bd9a74
Alexander Killing <killing@leifos.de>
2012-10-27 11:53:39 +0000

    removed repository.php
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@37909 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Payment/classes/class.ilShopPurchaseGUI.php
+++ Services/Payment/classes/class.ilShopPurchaseGUI.php
@@ -620,21 +620,6 @@ class ilShopPurchaseGUI extends ilObjectGUI
 //		$this->__buildLocator();
 //	}	
 
-//	function __buildLocator()
-//	{
-//		$this->tpl->addBlockFile("LOCATOR", "locator", "tpl.locator.html", "Services/Locator");
-//		$this->tpl->setVariable("TXT_LOCATOR",$this->lng->txt("locator"));
-//
-//		$this->tpl->setCurrentBlock("locator_item");
-//		$this->tpl->setVariable("ITEM", $this->lng->txt("repository"));
-//		$this->tpl->setVariable("LINK_ITEM", "../repository.php?getlast=true");
-//		$this->tpl->parseCurrentBlock();
-//
-//		// CHECK for new mail and info
-//		ilUtil::sendInfo();
-//
-//		return true;
-//	}
 //
 //	function __buildStylesheet()
 //	{
