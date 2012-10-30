ef1fb4efd04b09acdc0627142429f5ee8a1846d5
Stefan Meyer <smeyer.ilias@gmx.de>
2012-10-30 14:55:22 +0000

    Fixed  0010026: Missing description under forum title
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@37953 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/UICore/classes/class.ilTemplate.php
+++ Services/UICore/classes/class.ilTemplate.php
@@ -1662,7 +1662,10 @@ class ilTemplate extends ilTemplateX
 			foreach($this->title_alerts as $alert)
 			{
 				$this->setCurrentBlock('header_alert');
-				$this->setVariable('H_PROP', $alert['property'].':');
+				if(!($alert['propertyNameVisible'] === false))
+				{
+					$this->setVariable('H_PROP', $alert['property'].':');
+				}
 				$this->setVariable('H_VALUE', $alert['value']);
 				$this->parseCurrentBlock();
 			}
