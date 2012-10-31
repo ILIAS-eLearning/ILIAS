c9c9fd4fd52234ede398d582f8faf8603982a5f6
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2012-10-31 15:10:22 +0000

    #10043 - blog, data collection: add to desktop
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@37971 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Blog/classes/class.ilObjBlogListGUI.php
+++ Modules/Blog/classes/class.ilObjBlogListGUI.php
@@ -22,7 +22,7 @@ class ilObjBlogListGUI extends ilObjectListGUI
 		$this->copy_enabled = true;
 		$this->delete_enabled = true;
 		$this->cut_enabled = true;
-		$this->subscribe_enabled = false;
+		$this->subscribe_enabled = true;
 		$this->link_enabled = false;
 		$this->payment_enabled = false;
 		$this->info_screen_enabled = true;
