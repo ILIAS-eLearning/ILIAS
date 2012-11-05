2b327d98a3919cf09ecfab8fba8962c4d06f850c
Oskar Truffer <ot@studer-raimann.ch>
2012-11-05 12:42:21 +0000

    [DataCollection] added empty export.
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@38024 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ Modules/DataCollection/classes/class.ilDataCollectionExporter.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: oskar
+ * Date: 11/5/12
+ * Time: 1:11 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+include_once ("./trunk/Services/Export/classes/class.ilExport.php");
+
+class ilDataCollectionExporter extends ilXmlExporter {
+
+    public function init(){
+    }
+
+    public function getValidSchemaVersions($entity){
+    }
+
+    public function getXmlRepresentation($entity, $schema_version, $id){
+    }
+}
+
+?>
