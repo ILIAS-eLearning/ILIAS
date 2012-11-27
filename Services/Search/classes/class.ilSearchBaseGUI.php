15c0f2a1cd8443aff0cdacce96f1b23c5ce8f5eb
Alexander Killing <killing@leifos.de>
2012-11-27 23:10:40 +0000

    search gui usability
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@38511 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Search/classes/class.ilSearchBaseGUI.php
+++ Services/Search/classes/class.ilSearchBaseGUI.php
@@ -110,11 +110,11 @@ class ilSearchBaseGUI implements ilDesktopItemHandling, ilAdministrationCommandH
 		
 		// alex, 15.8.2012: Added the following lines to get the value
 		// from the main menu top right input search form
-		if (isset($_POST["root_id"]))
+/*		if (isset($_POST["root_id"]))
 		{
 			$ti->setValue($_POST["root_id"]);
 			$ti->writeToSession();
-		}
+		}*/
 
 		
 		if(ilSearchSettings::getInstance()->isLuceneItemFilterEnabled())
