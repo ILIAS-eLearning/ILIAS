db9055654e4d66e924952701524b60f55550f4f1
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2012-11-28 11:03:00 +0000

    #9623 - blog/portfolio: web access checker
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@38523 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Context/classes/class.ilContextWebAccessCheck.php
+++ Services/Context/classes/class.ilContextWebAccessCheck.php
@@ -51,7 +51,7 @@ class ilContextWebAccessCheck extends ilContextBase
 	 */
 	public static function hasHTML()
 	{
-		return false;
+		return true;
 	}
 	
 	/**
@@ -61,7 +61,7 @@ class ilContextWebAccessCheck extends ilContextBase
 	 */
 	public static function usesTemplate()
 	{
-		return false;
+		return true;
 	}
 	
 	/**
