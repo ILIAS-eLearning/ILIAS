ddf9b2769d49621863adac59657a9198048d4476
Alexander Killing <killing@leifos.de>
2012-12-29 12:38:52 +0000

    fixed bug #10321: Error message when remains less than one minute to finish an exercise
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@39053 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Exercise/classes/class.ilExAssignmentGUI.php
+++ Modules/Exercise/classes/class.ilExAssignmentGUI.php
@@ -466,7 +466,6 @@ class ilExAssignmentGUI
 		else
 		{
 			$time_diff = ilUtil::int2array($a_deadline - time(),null);
-			unset($time_diff['seconds']);
 			if (isset($time_diff['days']))
 			{
 				unset($time_diff['minutes']);
@@ -474,6 +473,10 @@ class ilExAssignmentGUI
 			if (isset($time_diff['months']))
 			{
 				unset($time_diff['hours']);
+			}
+			if (isset($time_diff['minutes']))
+			{
+				unset($time_diff['seconds']);
 			}
 			$time_str = ilUtil::timearray2string($time_diff);
 		}
