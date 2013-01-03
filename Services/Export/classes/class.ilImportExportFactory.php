556e5bed270c421e1833ea6b7c3dbe1c792d27bf
Michael Jansen <mjansen@databay.de>
2013-01-03 09:22:28 +0000

    Added plugin support for ilExportGUI
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@39063 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Export/classes/class.ilImportExportFactory.php
+++ Services/Export/classes/class.ilImportExportFactory.php
@@ -12,20 +12,36 @@ class ilImportExportFactory
 
 	public static function getExporterClass($a_type)
 	{
+		/**
+		 * @var $objDefinition ilObjectDefinition
+		 */
 		global $objDefinition;
 
-		$comp = $objDefinition->getComponentForType($a_type);
-		$c = explode("/", $comp);
-		$class = "il".$c[1]."Exporter";
-
-		// the next line had a "@" in front of the include_once
-		// I removed this because it tages ages to track down errors
-		// if the include class contains parse errors.
-		// Alex, 20 Jul 2012
-		if(include_once "./".$comp."/classes/class.".$class.".php")
+		if($objDefinition->isPlugin($a_type))
 		{
-			return $class;
+			$classname = 'il'.$objDefinition->getClassName($a_type).'Exporter';
+			$location = $objDefinition->getLocation($a_type);
+			if(include_once $location.'/class.'.$classname.'.php')
+			{
+				return $classname;
+			}
 		}
+		else
+		{
+			$comp = $objDefinition->getComponentForType($a_type);
+			$c = explode("/", $comp);
+			$class = "il".$c[1]."Exporter";
+	
+			// the next line had a "@" in front of the include_once
+			// I removed this because it tages ages to track down errors
+			// if the include class contains parse errors.
+			// Alex, 20 Jul 2012
+			if(include_once "./".$comp."/classes/class.".$class.".php")
+			{
+				return $class;
+			}
+		}
+
 		throw InvalidArgumentException('Invalid exporter type given');
 	}
 }
