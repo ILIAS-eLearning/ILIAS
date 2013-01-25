e216e6e222aa8267b2a44018a38b94c82505e552
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2013-01-25 10:55:40 +0000

    #10495 - ownership management: export
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@39442 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Object/classes/class.ilObjectOwnershipManagementGUI.php
+++ Services/Object/classes/class.ilObjectOwnershipManagementGUI.php
@@ -122,11 +122,39 @@ class ilObjectOwnershipManagementGUI
 		global $ilCtrl, $tree, $objDefinition;
 			
 		$node = $tree->getNodeData($a_ref_id);
-		$gui_class = "ilObj".$objDefinition->getClassName($node["type"])."GUI";
+		$gui_class = "ilObj".$objDefinition->getClassName($node["type"])."GUI";				
+		$path = array("ilRepositoryGUI", $gui_class, $a_class);
 		
-		$ilCtrl->setParameterByClass("ilRepositoryGUI", "ref_id", $a_ref_id);	
-		$ilCtrl->setParameterByClass("ilRepositoryGUI", "cmd", $a_cmd);
-		$ilCtrl->redirectByClass(array("ilRepositoryGUI", $gui_class, $a_class));	
+		// #10495 - check if object type supports ilexportgui "directly"
+		if($a_class == "ilExportGUI")
+		{							
+			try
+			{
+				$ilCtrl->getLinkTargetByClass($path);
+			}
+			catch(Exception $e)
+			{
+				switch($node["type"])
+				{
+					case "glo":
+						$cmd = "exportList";
+						$path = array("ilRepositoryGUI", "ilGlossaryEditorGUI", $gui_class);
+						break;
+
+					default:
+						$cmd = "export";
+						$path = array("ilRepositoryGUI", $gui_class);
+						break;
+				}						
+				$ilCtrl->setParameterByClass($gui_class, "ref_id", $a_ref_id);	
+				$ilCtrl->setParameterByClass($gui_class, "cmd", $cmd);
+				$ilCtrl->redirectByClass($path);	
+			}
+		}
+						
+		$ilCtrl->setParameterByClass($a_class, "ref_id", $a_ref_id);	
+		$ilCtrl->setParameterByClass($a_class, "cmd", $a_cmd);
+		$ilCtrl->redirectByClass($path);				
 	}
 	
 	function delete()
