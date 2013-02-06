f9dfa2174b656bcb75eb08dbe1fc886a7ad9c019
Stefan Meyer <smeyer.ilias@gmx.de>
2013-02-06 15:53:14 +0000

    Fixed ecs update task for parallel group scenario ii
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/Release_4_3_x_branch@39740 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/WebServices/ECS/classes/Course/class.ilECSCourseCreationHandler.php
+++ Services/WebServices/ECS/classes/Course/class.ilECSCourseCreationHandler.php
@@ -102,7 +102,15 @@ class ilECSCourseCreationHandler
 		{
 			// do update
 			$GLOBALS['ilLog']->write(__METHOD__.' Performing update of already imported course.');
-			return $this->updateCourseData($course,$obj_id);
+			
+			$refs = ilObject::_getAllReferences($obj_id);
+			$ref = end($refs);
+			
+			return $this->doSync(
+				$a_content_id, 
+				$course,
+				ilObject::_lookupObjId($GLOBALS['tree']->getParentId($ref))
+			);
 		}
 		
 		// Get all rules
@@ -266,13 +274,29 @@ class ilECSCourseCreationHandler
 		if($obj_id)
 		{
 			// update multiple courses/groups according to parallel scenario
+			$GLOBALS['ilLog']->write(__METHOD__.': '.$course->basicData->parallelGroupScenario);
+			include_once './Services/WebServices/ECS/classes/Mapping/class.ilECSMappingUtils.php';
+			switch((int) $course->basicData->parallelGroupScenario)
+			{
+
+				case ilECSMappingUtils::PARALLEL_GROUPS_IN_COURSE:
+					$GLOBALS['ilLog']->write(__METHOD__.': Performing update for parallel groups in course.');
+					$this->updateParallelGroups($course,$obj_id);
+					break;
+				
+				case ilECSMappingUtils::PARALLEL_ONE_COURSE:
+				default:
+					// nothing to do
+					break;
+				
+			}
 			
 			// do update
 			return $this->updateCourseData($course,$obj_id);
 		}
 		else
 		{
-			switch((int) $course->parallelGroupScenario)
+			switch((int) $course->basicData->parallelGroupScenario)
 			{
 				case ilECSMappingUtils::PARALLEL_GROUPS_IN_COURSE:
 					$GLOBALS['ilLog']->write(__METHOD__.': Parallel scenario "groups in courses".');
@@ -281,8 +305,7 @@ class ilECSCourseCreationHandler
 					$this->setImported($course_id, $crs);
 
 					// Create parallel groups under crs
-					$this->createParallelGroups($course,$crs);
-					
+					$this->createParallelGroups($course,$crs->getRefId());
 					break;
 				
 					
@@ -294,6 +317,8 @@ class ilECSCourseCreationHandler
 
 				case ilECSMappingUtils::PARALLEL_ALL_COURSES:
 					$GLOBALS['ilLog']->write(__METHOD__.': Parallel scenario "Many courses".');
+					
+					
 				default:
 				case ilECSMappingUtils::PARALLEL_ONE_COURSE:
 					$GLOBALS['ilLog']->write(__METHOD__.': Parallel scenario "One Course".');
@@ -314,19 +339,63 @@ class ilECSCourseCreationHandler
 	 * @param type $course
 	 * @param ilObjCourse
 	 */
-	protected function createParallelGroups($course, $crs_obj)
+	protected function createParallelGroups($course, $parent_ref)
 	{
 		foreach((array) $course->parallelGroups as $group)
 		{
-			include_once './Modules/Group/classes/class.ilObjGroup.php';
-			$group_obj = new ilObjGroup();
-			$group_obj->setTitle($group->title);
-			$group_obj->create();
-			$group_obj->createReference();
-			$group_obj->putInTree($crs_obj->getRefId());
-			$group_obj->setPermissions($crs_obj->getRefId());
-			$group_obj->initGroupStatus(GRP_TYPE_CLOSED);
-			$this->setImported($course->basicData->id, $group_obj, $group->id);
+			$this->createParallelGroup($course, $group, $parent_ref);
+		}
+	}
+
+	/**
+	 * Create parallel group
+	 * @param type $course
+	 * @param type $group
+	 */
+	protected function createParallelGroup($course, $group, $parent_ref)
+	{
+		include_once './Modules/Group/classes/class.ilObjGroup.php';
+		$group_obj = new ilObjGroup();
+		$group_obj->setTitle($group->title);
+		$group_obj->setMaxMembers((int) $group->maxParticipants);
+		$group_obj->create();
+		$group_obj->createReference();
+		$group_obj->putInTree($parent_ref);
+		$group_obj->setPermissions($parent_ref);
+		$group_obj->initGroupStatus(GRP_TYPE_CLOSED);
+		$this->setImported($course->basicData->id, $group_obj, $group->id);
+	}
+
+
+	/**
+	 * Update parallel group data
+	 * @param type $course
+	 * @param type $parent_obj
+	 */
+	protected function updateParallelGroups($course,$parent_obj)
+	{
+		$parent_refs = ilObject::_getAllReferences($parent_obj);
+		$parent_ref = end($parent_refs);
+		
+		foreach((array) $course->parallelGroups as $group)
+		{
+			$obj_id = $this->getImportId($course->basicData->id, $group->id);
+			$GLOBALS['ilLog']->write(__METHOD__.': Imported obj id is ' .$obj_id);
+			if(!$obj_id)
+			{
+				$this->createParallelGroup($course, $group, $parent_ref);
+			}
+			else
+			{
+				$group_obj = ilObjectFactory::getInstanceByObjId($obj_id,false);
+				if($group_obj instanceof ilObjGroup)
+				{
+					$GLOBALS['ilLog']->write(__METHOD__.': New title is '. $group->title);
+					$group_obj->setTitle($group->title);
+					$group_obj->setMaxMembers((int) $group->maxParticipants);
+					$group_obj->update();
+				}
+			}
 		}
 	}
 	
@@ -336,13 +405,14 @@ class ilECSCourseCreationHandler
 	 * @param type $a_content_id
 	 * @return type
 	 */
-	protected function getImportId($a_content_id)
+	protected function getImportId($a_content_id,$a_sub_id = NULL)
 	{
 		include_once './Services/WebServices/ECS/classes/class.ilECSImport.php';
 		return ilECSImport::_isImported(
 				$this->getServer()->getServerId(),
 				$a_content_id,
-				$this->getMid()
+				$this->getMid(),
+				$a_sub_id
 		);
 	}
 	
@@ -409,7 +479,7 @@ class ilECSCourseCreationHandler
 	 * @param int $a_content_id
 	 * @param ilObjCourse $crs
 	 */
-	protected function setImported($a_content_id, $object, $a_sub_id)
+	protected function setImported($a_content_id, $object, $a_sub_id = 0)
 	{
 		include_once './Services/WebServices/ECS/classes/class.ilECSImport.php';
 		$import = new ilECSImport(
