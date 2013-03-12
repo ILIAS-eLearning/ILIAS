a427d2e91725c8fd28be2b8c3993d93b1a568494
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2013-03-12 12:32:26 +0000

    user accounts: send message
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@40550 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/User/classes/class.ilObjUserFolderGUI.php
+++ Services/User/classes/class.ilObjUserFolderGUI.php
@@ -2803,6 +2803,11 @@ class ilObjUserFolderGUI extends ilObjectGUI
 		$umail = new ilFormatMail($ilUser->getId());		
 		$mail_data = $umail->getSavedData();		
 		
+		if(!is_array($mail_data))
+		{
+			$mail_data = array("user_id" => $ilUser->getId());
+		}
+				
 		// ???
 		// $mail_data = $umail->appendSearchResult(array('#il_ml_'.$list_id), 'to');
 		
