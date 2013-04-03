5e9e7e33f162745839ccd2c77104e93cf4a4bbec
Fabian Schmid <fs@studer-raimann.ch>
2013-04-03 13:29:26 +0000

    http://ilias.de/mantis/view.php?id=10662
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@41063 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- shib_login.php
+++ shib_login.php
@@ -2,33 +2,27 @@
 /* Copyright (c) 1998-2009 ILIAS open source, Extended GPL, see docs/LICENSE */
 
 /**
-* Shibboleth login script for ilias
-*
-* $Id$
-* @author Lukas Haemmerle <haemmerle@switch.ch>
-* @package ilias-layout
-*/
+ * Shibboleth login script for ilias
+ *
+ * $Id$
+ * @author Lukas Haemmerle <haemmerle@switch.ch>
+ * @package ilias-layout
+ */
 
 // Load all the IILIAS stuff
 require_once "include/inc.header.php";
 
-if (!$_SERVER['HTTP_SHIB_APPLICATION_ID'] && !$_SERVER['Shib-Application-ID'])
-{
+if (! $_SERVER['HTTP_SHIB_APPLICATION_ID'] && ! $_SERVER['Shib-Application-ID']) {
 	$message = "This file must be protected by Shibboleth, otherwise you cannot use Shibboleth authentication! Consult the <a href=\"Services/AuthShibboleth/README.SHIBBOLETH.txt\">documentation</a> on how to configure Shibboleth authentication properly.";
-	$ilias->raiseError($message,$ilias->error_obj->WARNING);
+	$ilias->raiseError($message, $ilias->error_obj->WARNING);
 }
 
 // Check if all the essential attributes are available
-if (
-		!$_SERVER[$ilias->getSetting('shib_login')]
-		|| !$_SERVER[$ilias->getSetting('shib_firstname')]
-		|| !$_SERVER[$ilias->getSetting('shib_lastname')]
-		|| !$_SERVER[$ilias->getSetting('shib_email')]
-   )
-{
-	$message =  "ILIAS needs at least the attributes '".$ilias->getSetting('shib_login')."', '".$ilias->getSetting('shib_firstname')."', '".$ilias->getSetting('shib_lastname')."' and '".$ilias->getSetting('shib_email')."' to work properly !\n<br>Please consult the <a href=\"README.SHIBBOLETH.txt\">documentation</a> on how to configure Shibboleth authentication properly.";
-	
-	$ilias->raiseError($message,$ilias->error_obj->WARNING);
+if (! $_SERVER[$ilias->getSetting('shib_login')] || ! $_SERVER[$ilias->getSetting('shib_firstname')] || ! $_SERVER[$ilias->getSetting('shib_lastname')] || ! $_SERVER[$ilias->getSetting('shib_email')]
+) {
+	$message = "ILIAS needs at least the attributes '" . $ilias->getSetting('shib_login') . "', '" . $ilias->getSetting('shib_firstname') . "', '" . $ilias->getSetting('shib_lastname') . "' and '" . $ilias->getSetting('shib_email') . "' to work properly !\n<br>Please consult the <a href=\"README.SHIBBOLETH.txt\">documentation</a> on how to configure Shibboleth authentication properly.";
+
+	$ilias->raiseError($message, $ilias->error_obj->WARNING);
 }
 
 #global $ilAuth;
