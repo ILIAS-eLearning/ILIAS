fa342618ef322b38add892c2d806250693d6e285
Stefan Meyer <smeyer.ilias@gmx.de>
2013-04-23 08:39:22 +0000

    Fixed default value of course member mail notification
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@41651 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Course/classes/class.ilCourseMembershipMailNotification.php
+++ Modules/Course/classes/class.ilCourseMembershipMailNotification.php
@@ -65,7 +65,7 @@ class ilCourseMembershipMailNotification extends ilMailNotification
 			case self::TYPE_ADMISSION_MEMBER:
 				
 				// automatic mails about status change disabled
-				if(!$ilSetting->get('mail_crs_member_notification',false))
+				if(!$ilSetting->get('mail_crs_member_notification',true))
 				{
 					return;
 				}
@@ -170,7 +170,7 @@ class ilCourseMembershipMailNotification extends ilMailNotification
 			case self::TYPE_DISMISS_MEMBER:
 
 				// automatic mails about status change disabled
-				if(!$ilSetting->get('mail_crs_member_notification',false))
+				if(!$ilSetting->get('mail_crs_member_notification',true))
 				{
 					return;
 				}
