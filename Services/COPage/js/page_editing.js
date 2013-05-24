90272d60a5b3fdbc1d8866c8f63f388b95024a4b
Alexander Killing <killing@leifos.de>
2013-05-24 14:41:48 +0000

    added comment
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/Release_4_3_x_branch@42365 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/COPage/js/page_editing.js
+++ Services/COPage/js/page_editing.js
@@ -1051,7 +1051,8 @@ if (add_final_spacer)
 		
 		// force redraw for webkit based browsers (ILIAS chrome bug #0010871)
 		// http://stackoverflow.com/questions/3485365/how-can-i-force-webkit-to-redraw-repaint-to-propagate-style-changes
-		// no feature detection here since we are fixing a webkit bug
+		// no feature detection here since we are fixing a webkit bug and IE does not like this patch (starts flickering
+		// on "short" pages)
 		var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
 		var isSafari = /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor);
 		if (isChrome || isSafari) {
