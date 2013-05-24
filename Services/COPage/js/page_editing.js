f008912ce5f43fda0735bf6277896d26e48f8592
Alexander Killing <killing@leifos.de>
2013-05-24 14:21:28 +0000

    temp test
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/Release_4_3_x_branch@42363 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/COPage/js/page_editing.js
+++ Services/COPage/js/page_editing.js
@@ -1051,9 +1051,9 @@ if (add_final_spacer)
 		
 		// force redraw for webkit based browsers (ILIAS chrome bug #0010871)
 		// http://stackoverflow.com/questions/3485365/how-can-i-force-webkit-to-redraw-repaint-to-propagate-style-changes
-		back_el.style.display='none';
+		/*back_el.style.display='none';
 		dummy = back_el.offsetHeight;
-		back_el.style.display='';
+		back_el.style.display='';*/
 	},
 
 	autoResize: function(ed)
