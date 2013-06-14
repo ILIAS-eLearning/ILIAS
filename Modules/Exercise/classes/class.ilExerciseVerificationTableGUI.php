50adbdfa9773376e6bcc5bc75ae07d27bfb1e177
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2013-06-14 08:51:11 +0000

    #11210 - workspace: test/exercise verification vs. certificate settings
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@42723 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Exercise/classes/class.ilExerciseVerificationTableGUI.php
+++ Modules/Exercise/classes/class.ilExerciseVerificationTableGUI.php
@@ -44,13 +44,24 @@ class ilExerciseVerificationTableGUI extends ilTable2GUI
 		global $ilUser;
 
 		include_once "Modules/Exercise/classes/class.ilObjExercise.php";
+		include_once "./Modules/Exercise/classes/class.ilExerciseCertificateAdapter.php";
+		include_once "./Services/Certificate/classes/class.ilCertificate.php";
 
 		$data = array();
 		foreach(ilObjExercise::_lookupFinishedUserExercises($ilUser->getId()) as $exercise_id => $passed)
-		{
-			$data[] = array("id" => $exercise_id,
-				"title" => ilObject::_lookupTitle($exercise_id),
-				"passed" => $passed);
+		{			
+			// #11210 - only available certificates!
+			$exc = new ilObjExercise($exercise_id, false);				
+			if($exc->hasUserCertificate($ilUser->getId()))
+			{						
+				$adapter = new ilExerciseCertificateAdapter($exc);
+				if(ilCertificate::_isComplete($adapter))
+				{							
+					$data[] = array("id" => $exercise_id,
+						"title" => ilObject::_lookupTitle($exercise_id),
+						"passed" => $passed);
+				}
+			}
 		}
 
 		$this->setData($data);
