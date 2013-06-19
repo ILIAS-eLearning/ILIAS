babb4b314b3df9761dab9056254e0ffbab4a914e
Alexander Killing <killing@leifos.de>
2013-06-19 09:07:04 +0000

    fixed full group by
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/Release_4_3_x_branch@42796 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Wiki/classes/class.ilWikiPage.php
+++ Modules/Wiki/classes/class.ilWikiPage.php
@@ -474,11 +474,10 @@ class ilWikiPage extends ilPageObject
 					$ids[] = $source["id"];
 				}
 			}
-			// delete record of table il_wiki_data
-			$query = "SELECT count(*) AS cnt FROM il_wiki_page".
+			$query = "SELECT count(*) cnt FROM il_wiki_page".
 				" WHERE ".$ilDB->in("id", $ids, false, "integer").
 				" AND wiki_id = ".$ilDB->quote($a_wiki_id, "integer").
-				" ORDER BY title";
+				" GROUP BY wiki_id";
 			$set = $ilDB->query($query);
 			$rec = $ilDB->fetchAssoc($set);
 			if ($rec["cnt"] == 0 &&
