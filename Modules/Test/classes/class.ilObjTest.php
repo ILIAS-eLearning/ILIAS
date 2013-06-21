621df45f8d4086234cfb28417d97b099e40dddd7
Björn Heyser <bheyser@databay.de>
2013-06-21 11:43:16 +0000

    fixed question ids in statistics / was original id instead of duplicate id --> for historical reasons
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@42883 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Test/classes/class.ilObjTest.php
+++ Modules/Test/classes/class.ilObjTest.php
@@ -5169,7 +5169,7 @@ function getAnswerFeedbackPoints()
 			$data	->getParticipant($row["active_fi"])
 					->getPass($row["pass"])
 					->addAnsweredQuestion(
-						$row["original_id"] ? $row["original_id"] : $row["question_fi"],
+						$row["question_fi"],
 						$row["maxpoints"],
 						$row["points"],
 						$row['answered']
@@ -5198,8 +5198,8 @@ function getAnswerFeedbackPoints()
 						while ($row = $ilDB->fetchAssoc($result))
 						{
 							$tpass = array_key_exists("pass", $row) ? $row["pass"] : 0;
-							$data->getParticipant($active_id)->addQuestion($row["original_id"] ? $row["original_id"] : $row["question_fi"], $row["question_fi"], $row["points"], $row["sequence"], $tpass);
-							$data->addQuestionTitle($row["original_id"] ? $row["original_id"] : $row["question_fi"], $row["title"]);
+							$data->getParticipant($active_id)->addQuestion($row["original_id"], $row["question_fi"], $row["points"], $row["sequence"], $tpass);
+							$data->addQuestionTitle($row["question_fi"], $row["title"]);
 						}
 					}
 				}
@@ -5233,9 +5233,8 @@ function getAnswerFeedbackPoints()
 						$questionsequence = unserialize($seqrow["sequence"]);
 						foreach ($questionsequence as $sidx => $seq)
 						{
-							$qsid = $questionsbysequence[$seq]["original_id"] ? $questionsbysequence[$seq]["original_id"] : $questionsbysequence[$seq]["question_fi"];
-							$data->getParticipant($active_id)->addQuestion($qsid, $questionsbysequence[$seq]["question_fi"], $questionsbysequence[$seq]["points"], $sidx + 1, $seqrow["pass"]);
-							$data->addQuestionTitle($qsid, $questionsbysequence[$seq]["title"]);
+							$data->getParticipant($active_id)->addQuestion($questionsbysequence[$seq]["original_id"], $questionsbysequence[$seq]["question_fi"], $questionsbysequence[$seq]["points"], $sidx + 1, $seqrow["pass"]);
+							$data->addQuestionTitle($questionsbysequence[$seq]["question_fi"], $questionsbysequence[$seq]["title"]);
 						}
 					}
 				}
