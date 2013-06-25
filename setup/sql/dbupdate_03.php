ada79db8a0f3011c285601ddf6c8563056fadc8d
Timon Amstutz <timon.amstutz@bluewin.ch>
2013-06-25 14:39:39 +0000

    [cld][tax]: dbupdate for cloud-module added
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@42971 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- setup/sql/dbupdate_03.php
+++ setup/sql/dbupdate_03.php
@@ -16033,3 +16033,92 @@ ilDBUpdateNewObjectType::addRBACOperations($sysc_type_id, $rbac_ops);
 <?php
 	$ilCtrlStructureReader->getStructure();
 ?>
+<#3925>
+<?php
+$fields = array(
+    'id'            => array(
+        'type'    => 'integer',
+        'length'  => 4,
+        'notnull' => true
+    ),
+    'is_online'     => array(
+        'type'    => 'integer',
+        'length'  => 1,
+        'notnull' => false
+    ),
+    'service'       => array(
+        'type'    => 'text',
+        'length'  => 255,
+        'fixed'   => false,
+        'notnull' => false
+    ),
+    'root_folder'   => array(
+        'type'    => 'text',
+        'length'  => 255,
+        'fixed'   => false,
+        'notnull' => false
+    ),
+    'root_id'       => array(
+        'type'    => 'text',
+        'length'  => 255,
+        'fixed'   => false,
+        'notnull' => false
+    ),
+    'owner_id'      => array(
+        'type'    => 'integer',
+        'length'  => 8,
+        'notnull' => true
+    ),
+    'auth_complete' => array(
+        'type'    => 'integer',
+        'length'  => 1,
+        'notnull' => false
+    ),
+);
+
+$ilDB->createTable("il_cld_data", $fields);
+$ilDB->addPrimaryKey("il_cld_data", array("id"));
+?>
+
+<#3926>
+<?php
+$setting = new ilSetting();
+$setting->set("obj_dis_creation_cld", 1);
+?>
+<#3927>
+<?php
+include_once('./Services/Migration/DBUpdate_3560/classes/class.ilDBUpdateNewObjectType.php');
+
+$cld_type_id = ilDBUpdateNewObjectType::addNewType('cld', 'Cloud Folder');
+
+$rbac_ops = array(
+    ilDBUpdateNewObjectType::RBAC_OP_EDIT_PERMISSIONS,
+    ilDBUpdateNewObjectType::RBAC_OP_VISIBLE,
+    ilDBUpdateNewObjectType::RBAC_OP_READ,
+    ilDBUpdateNewObjectType::RBAC_OP_WRITE,
+    ilDBUpdateNewObjectType::RBAC_OP_DELETE
+);
+ilDBUpdateNewObjectType::addRBACOperations($cld_type_id, $rbac_ops);
+
+$parent_types = array('root', 'cat', 'crs', 'fold', 'grp');
+ilDBUpdateNewObjectType::addRBACCreate('create_cld', 'Create Cloud Folder', $parent_types);
+
+$ops_id = ilDBUpdateNewObjectType::addCustomRBACOperation('upload', 'Upload Items', 'object', 3200);
+ilDBUpdateNewObjectType::addRBACOperation($cld_type_id, $ops_id);
+$ops_id = ilDBUpdateNewObjectType::addCustomRBACOperation('delete_files', 'Delete Files', 'object', 3200);
+ilDBUpdateNewObjectType::addRBACOperation($cld_type_id, $ops_id);
+$ops_id = ilDBUpdateNewObjectType::addCustomRBACOperation('delete_folders', 'Delete Folders', 'object', 3200);
+ilDBUpdateNewObjectType::addRBACOperation($cld_type_id, $ops_id);
+$ops_id = ilDBUpdateNewObjectType::addCustomRBACOperation('download', 'Download Items', 'object', 3200);
+ilDBUpdateNewObjectType::addRBACOperation($cld_type_id, $ops_id);
+$ops_id = ilDBUpdateNewObjectType::addCustomRBACOperation('files_visible', 'Files are visible', 'object', 3200);
+ilDBUpdateNewObjectType::addRBACOperation($cld_type_id, $ops_id);
+$ops_id = ilDBUpdateNewObjectType::addCustomRBACOperation('folders_visible', 'Folders are visible', 'object', 3200);
+ilDBUpdateNewObjectType::addRBACOperation($cld_type_id, $ops_id);
+$ops_id = ilDBUpdateNewObjectType::addCustomRBACOperation('folders_create', 'Folders may be created', 'object', 3200);
+ilDBUpdateNewObjectType::addRBACOperation($cld_type_id, $ops_id);
+?>
+<#3928>
+<?php
+$ilCtrlStructureReader->getStructure();
+?>
