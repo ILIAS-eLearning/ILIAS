c3bb48decd2674ab5fb2f2b65a536f6ab59c1641
Timon Amstutz <timon.amstutz@bluewin.ch>
2013-06-26 13:32:48 +0000

    [Cloud][Tax]: added call for ctrl structure
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@43022 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Administration/classes/class.ilAdministrationGUI.php
+++ Services/Administration/classes/class.ilAdministrationGUI.php
@@ -32,6 +32,7 @@ include_once("./Services/Table/classes/class.ilTableGUI.php");
 * @ilCtrl_Calls ilAdministrationGUI: ilObjForumAdministrationGUI, ilObjBlogGUI, ilObjPollGUI, ilObjDataCollectionGUI
 * @ilCtrl_Calls ilAdministrationGUI: ilObjRemoteCategoryGUI, ilObjRemoteWikiGUI, ilObjRemoteLearningModuleGUI
 * @ilCtrl_Calls ilAdministrationGUI: ilObjRemoteGlossaryGUI, ilObjRemoteFileGUI, ilObjRemoteGroupGUI, ilObjECSSettingsGUI
+* @ilCtrl_Calls ilAdministrationGUI: ilObjCloudGUI
 * // BEGIN WebDAV
 * @ilCtrl_Calls ilAdministrationGUI: ilObjFileAccessSettingsGUI, ilPermissionGUI, ilObjRemoteTestGUI
 * // END WebDAV
