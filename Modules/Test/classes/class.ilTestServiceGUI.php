4500aff41b939c15338de9e6548d5abda64abe11
Björn Heyser <bheyser@databay.de>
2013-08-02 08:29:46 +0000

    merged r43864 from trunk/: fixed mantis #0011465: Error in Test-Statistics > Results by Question
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/Release_4_3_x_branch@43865 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Test/classes/class.ilTestServiceGUI.php
+++ Modules/Test/classes/class.ilTestServiceGUI.php
@@ -1007,7 +1007,7 @@ class ilTestServiceGUI
 			$resultpass = $this->object->_getResultPass($active_id);
 			for ($i = 0; $i < count($passes); $i++)
 			{
-				if (($resultpass != null) && ($resultpass == $passes[$i]["pass"]))
+				if (($resultpass !== null) && ($resultpass == $passes[$i]["pass"]))
 				{
 					$question_gui =& $this->object->createQuestionGUI("", $passes[$i]["qid"]);
 					$output .= $this->getResultsHeadUserAndPass($active_id, $resultpass+1);
