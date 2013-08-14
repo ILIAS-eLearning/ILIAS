0ec97afdc05ad4444590c4a3794132fe337f80aa
Martin Studer <ms@studer-raimann.ch>
2013-08-14 14:18:16 +0000

    0011084: No Need for Seconds in DC
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@44152 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/DataCollection/classes/class.ilDataCollectionRecord.php
+++ Modules/DataCollection/classes/class.ilDataCollectionRecord.php
@@ -57,7 +57,6 @@ class ilDataCollectionRecord
 		
 		$ilDB->update("il_dcl_record", array(
 			"table_id" => array("integer", $this->getTableId()),
-			"create_date" => array("date", $this->getCreateDate()),
 			"last_update" => array("date", $this->getLastUpdate()),
 			"owner" => array("text", $this->getOwner()),
 			"last_edit_by" => array("text", $this->getLastEditBy())
@@ -90,6 +89,8 @@ class ilDataCollectionRecord
 		$this->setTableId($rec["table_id"]);
 		$this->setCreateDate($rec["create_date"]);
 		$this->setLastUpdate($rec["last_update"]);
+
+
 		$this->setOwner($rec["owner"]);
 		$this->setLastEditBy($rec["last_edit_by"]);
 	}
@@ -456,7 +457,11 @@ class ilDataCollectionRecord
 				return ilUserUtil::getNamePresentation($this->getOwner());
 			case 'last_edit_by':
                 return ilUserUtil::getNamePresentation($this->getLastEditBy());
-		}
+            case 'last_update':
+                return ilDatePresentation::formatDate(new ilDateTime($this->getLastUpdate(),IL_CAL_DATETIME));
+            case 'create_date':
+                return ilDatePresentation::formatDate(new ilDateTime($this->getCreateDate(),IL_CAL_DATETIME));
+        }
 		
 		return $this->$field_id;
 	}
