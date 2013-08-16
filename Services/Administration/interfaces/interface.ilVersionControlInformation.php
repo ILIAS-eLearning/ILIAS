299b3317fb01014317b57f91b6b1b0d0d8239ca2
Michael Jansen <mjansen@databay.de>
2013-08-16 09:04:00 +0000

    Added version control information to server info screen in administration
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@44220 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ Services/Administration/interfaces/interface.ilVersionControlInformation.php
@@ -0,0 +1,14 @@
+<?php
+/* Copyright (c) 1998-2013 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+/**
+ * Class ilVersionControlInformation
+ * @author Michael Jansen <mjansen@databay.de>
+ */
+interface ilVersionControlInformation
+{
+	/**
+	 * @return string
+	 */
+	public function getInformationAsHtml();
+} 
