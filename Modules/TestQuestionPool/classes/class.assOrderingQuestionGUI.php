c65671a4c92ff5821c7e0b245a4916bee303b69e
Björn Heyser <bheyser@databay.de>
2013-08-22 13:52:14 +0000

    fixed mantis #0011594: Anordnungsfrage: Punktevergabe: nur ganze Punkte möglich.
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@44383 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/TestQuestionPool/classes/class.assOrderingQuestionGUI.php
+++ Modules/TestQuestionPool/classes/class.assOrderingQuestionGUI.php
@@ -436,6 +436,7 @@ class assOrderingQuestionGUI extends assQuestionGUI
 		}
 		// points
 		$points = new ilNumberInputGUI($this->lng->txt("points"), "points");
+		$points->allowDecimals(true);
 		$points->setValue($this->object->getPoints());
 		$points->setRequired(TRUE);
 		$points->setSize(3);
