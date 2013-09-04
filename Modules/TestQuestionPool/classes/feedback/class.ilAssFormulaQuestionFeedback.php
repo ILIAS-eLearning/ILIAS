34f216d8c8a5a4eee54da9a9765f5117402d2e65
Maximilian Becker <mbecker@databay.de>
2013-09-04 14:28:18 +0000

    Removing "novel of the year"-copyright notices from formula question files.
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@44604 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/TestQuestionPool/classes/feedback/class.ilAssFormulaQuestionFeedback.php
+++ Modules/TestQuestionPool/classes/feedback/class.ilAssFormulaQuestionFeedback.php
@@ -1,5 +1,4 @@
 <?php
-
 /* Copyright (c) 1998-2013 ILIAS open source, Extended GPL, see docs/LICENSE */
 
 require_once 'Modules/TestQuestionPool/classes/feedback/class.ilAssSingleOptionQuestionFeedback.php';
