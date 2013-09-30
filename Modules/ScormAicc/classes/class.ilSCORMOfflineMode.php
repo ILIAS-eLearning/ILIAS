644d457e2680351479b37f53470549c02f0acb40
Uwe Kohnle <kohnle@internetlehrer-gmbh.de>
2013-09-30 15:14:05 +0000

    improved import for SOP
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@45107 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/ScormAicc/classes/class.ilSCORMOfflineMode.php
+++ Modules/ScormAicc/classes/class.ilSCORMOfflineMode.php
@@ -33,90 +33,129 @@ class ilSCORMOfflineMode
 		$this->obj_id = ilObject::_lookupObjectId($_GET['ref_id']);
 		include_once "./Modules/ScormAicc/classes/class.ilObjSAHSLearningModule.php";
 		$this->type = ilObjSAHSLearningModule::_lookupSubType($this->obj_id);
+		$this->read();
 	}
-	/**
-	 * execute command
-	 */
-	// function &executeCommand()
-	// {
-		// global $ilAccess, $ilLog, $ilUser, $lng, $ilias;
-
-		// $cmd = $_GET["cmd"];
-
-		// if (!$ilAccess->checkAccess("read", "", $_GET["ref_id"]))
-		// {
-			// $ilias->raiseError($lng->txt("permission_denied"), $ilias->error_obj->WARNING);
-		// }
-// var_dump($cmd);
-		// // switch($cmd){
-		// // }
-	// }
 
 	function il2sop() {
 		global $ilUser;
 		$this->setOfflineMode("il2sop");
 		header('Content-Type: text/javascript; charset=UTF-8');
-		
-		// $result = array(
-			// 'lm' => $this->getDataForLm(),
-			// 'schema' => array(), 
-			// 'data' => array()
-		// );
 
 		include_once "./Modules/ScormAicc/classes/class.ilObjSAHSLearningModule.php";
 		$ob = new ilObjSAHSLearningModule($this->id);
 		$module_version = $ob->getModuleVersion();
+		$sahs_user = $this->il2sopSahsUser();
+		$support_mail = "";//TODO
+		$scorm_version = "1.2";
+		if ($this->type == "scorm2004") $scorm_version = "2004";
 
 		if ($this->type == 'scorm2004') {
 			include_once "./Modules/Scorm2004/classes/ilSCORM13Player.php";
 			$ob2004 = new ilSCORM13Player();
 			$init_data = $ob2004->getConfigForPlayer();
 			$resources = json_decode($ob2004->getCPDataInit());
+			$max_attempt = $ob2004->get_max_attempts();
+			if ($max_attempt == null) $max_attempt = 0;
 			$result = array(
+				'client_data' => array(
+					$support_mail
+				),
+				'user_data' => $this->il2sopUserData(),
 				'lm' => array(
 					ilObject::_lookupTitle($this->obj_id),
 					ilObject::_lookupDescription($this->obj_id),
-					$this->type,
+					$scorm_version,
 					1,//active
 					$init_data,
 					$resources,
 					"",
 					$module_version,
-					"" //offline_zip_created!!!!!!!!
+					"", //offline_zip_created!!!!!!!!
+					1,//learning_progress_enabled
+					1,//certificate_enabled
+					$max_attempt
 				),
-				'data' => $ob2004->getCMIData($ilUser->getID(), $this->obj_id)
+				'sahs_user' => $sahs_user,
+				'cmi' => $ob2004->getCMIData($ilUser->getID(), $this->obj_id)
 			);
 		}
-
-
-
 		print(json_encode($result));
 	}
-	function getDataForLm() {
-//		global  $ilias;
-		include_once "./Modules/ScormAicc/classes/class.ilObjSAHSLearningModule.php";
-		$ob = new ilObjSAHSLearningModule($this->id);
-		$module_version = $ob->getModuleVersion();
-
-		if ($this->type == 'scorm2004') {
-			include_once "./Modules/Scorm2004/classes/ilSCORM13Player.php";
-			$ob2004 = new ilSCORM13Player();
-			$init_data = $ob2004->getConfigForPlayer();
-			$resources = json_decode($ob2004->getCPDataInit());
-		}
-		$result = array(
-			ilObject::_lookupTitle($this->obj_id),
-			ilObject::_lookupDescription($this->obj_id),
-			$this->type,
-			0,//active
-			$init_data,
-			$resources,
-			"",
-			$module_version,
-			"" //offline_zip_created!!!!!!!!
-		);
-		return $result;
+	
+	function getClientIdSop() {
+		$iliasDomain = substr(ILIAS_HTTP_PATH,7);
+		if (substr($iliasDomain,0,1) == "\/") $iliasDomain = substr($iliasDomain,1);
+		if (substr($iliasDomain,0,4) == "www.") $iliasDomain = substr($iliasDomain,4);
+		return $iliasDomain.';'.CLIENT_ID;
 	}
+	
+	function il2sopUserData() {
+		global $ilUser;
+		return array(
+			$ilUser->getLogin(),
+			"",
+			$ilUser->getFirstname(),
+			$ilUser->getLastname(),
+			$ilUser->getUTitle(),
+			$ilUser->getGender(),
+			$ilUser->getID()
+			);
+	}
+	function il2sopSahsUser() {
+		global $ilDB,$ilUser;
+		$package_attempts	= 0;
+		$module_version		= 1;//if module_version in sop is different...
+		$last_visited		= "";
+		$first_access		= "";
+		$last_access		= "";
+		$last_status_change	= "";
+		$total_time_sec		= 0;
+		$sco_total_time_sec	= 0;
+		$status				= 0;
+		$percentage_completed = 0;
+		$user_data			= "";
+		
+		global $ilDB,$ilUser;
+		$res = $ilDB->queryF('SELECT * FROM sahs_user WHERE obj_id=%s AND user_id=%s',
+			array('integer','integer'),
+			array($this->obj_id,$ilUser->getID())
+		);
+		while($row = $ilDB->fetchAssoc($res))
+		{
+			$module_version = $row['module_version'];
+			$last_visited = $row['last_visited'];
+			$last_access = $row['last_access'];
+			$total_time_sec = $row['total_time_sec'];
+			$sco_total_time_sec = $row['sco_total_time_sec'];
+			$status = $row['status'];
+			$percentage_completed = $row['percentage_completed'];
+		}
+		return array($package_attempts, $module_version, $last_visited, $first_access, $last_access, $last_status_change, $total_time_sec, $sco_total_time_sec, $status, $percentage_completed, $user_data);
+	}
+
+	function sop2il() {
+		sleep(5);
+		$in = file_get_contents("php://input");
+		$ret = array('msg'=>array(),'err'=>array());
+		
+		if (!$in || $in == "") {
+			$ret['err'][] = "no post data recieved";
+			print(json_encode($ret));
+			exit;
+		}
+		
+		$obj = json_decode($in,true);
+		if (!is_array($obj)) {
+			$ret['err'][] = "invalid post data recieved";
+			print(json_encode($ret));
+			exit;
+		}
+		$ret['msg'][]  = "post data recieved";
+		header('Content-Type: text/plain; charset=UTF-8');
+		print json_encode($ret);
+		exit;
+	}
+
 	//offlineMode: offline, online, il2sop, sop2il
 	function setOfflineMode($a_mode) {
 		global $ilDB,$ilUser;
