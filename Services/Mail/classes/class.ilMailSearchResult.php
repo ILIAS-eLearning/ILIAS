ee94831a2a77262838f8beb3f474a53eb3f16a65
Michael Jansen <mjansen@databay.de>
2013-09-30 17:23:41 +0000

    Merged mail quick filter from hsuhh branch
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@45114 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ Services/Mail/classes/class.ilMailSearchResult.php
@@ -0,0 +1,53 @@
+<?php
+/* Copyright (c) 1998-2013 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+/**
+ * @author  Michael Jansen <mjansen@databay.de>
+ * @version $Id$
+ * @ingroup ServicesMail
+ */
+class ilMailSearchResult
+{
+	/**
+	 * @var array
+	 */
+	protected $result = array();
+
+	/**
+	 *
+	 */
+	public function __construct()
+	{
+	}
+
+	/**
+	 * @param array $item
+	 */
+	public function addItem($id, array $fields)
+	{
+		$this->result[$id] = $fields;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getIds()
+	{
+		return array_keys($this->result);
+	}
+
+	/**
+	 * @param integer $id
+	 * @return array
+	 * @throws OutOfBoundsException
+	 */
+	public function getFields($id)
+	{
+		if(!isset($this->result[$id]))
+		{
+			throw new OutOfBoundsException('mail_missing_result_fields');
+		}
+		
+		return $this->result[$id];
+	}
+}
