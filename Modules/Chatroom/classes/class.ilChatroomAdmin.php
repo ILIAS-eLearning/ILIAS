4c82cf675698dd9ceb8e2e91ad3e19ff48e9ea7b
Michael Jansen <mjansen@databay.de>
2013-10-03 14:25:42 +0000

    Code formatting (this class is a little bit confusing, maybe we should revise this)
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@45201 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Chatroom/classes/class.ilChatroomAdmin.php
+++ Modules/Chatroom/classes/class.ilChatroomAdmin.php
@@ -1,99 +1,92 @@
 <?php
 /* Copyright (c) 1998-2009 ILIAS open source, Extended GPL, see docs/LICENSE */
 
-
 /**
  * Class ilChatroomAdmin
- *
- * @author Jan Posselt <jposselt@databay.de>
+ * @author  Jan Posselt <jposselt@databay.de>
  * @version $Id$
- *
  * @ingroup ModulesChatroom
  */
 class ilChatroomAdmin
 {
-
+	/**
+	 * @var string
+	 */
 	private static $settingsTable = 'chatroom_admconfig';
+
+	/**
+	 * @var stdClass
+	 */
 	private $settings;
+
+	/**
+	 * @var int
+	 */
 	private $config_id;
 
 	/**
 	 * Constructor
-	 *
 	 * Sets $this->config_id and $this->settings using given $config_id
 	 * and $settings
-	 *
-	 * @param integer $config_id
+	 * @param integer  $config_id
 	 * @param stdClass $settings
 	 */
 	public function __construct($config_id, stdClass $settings = null)
 	{
-		$this->config_id	= $config_id;
-		$this->settings		= $settings;
+		$this->config_id = $config_id;
+		$this->settings  = $settings;
 	}
 
 	/**
 	 * Instantiates and returns ilChatroomAdmin object using instance_id and settings
 	 * from settingsTable.
-	 *
-	 * @global ilDBMySQL $ilDB
-	 * @return ilChatroomAdmin
+	 * @return self
 	 */
 	public static function getDefaultConfiguration()
 	{
+		/**
+		 * @var $ilDB ilDB
+		 */
 		global $ilDB;
 
-		$ilDB->setLimit( 1 );
-		$query = 'SELECT * FROM ' . self::$settingsTable; // . ' WHERE default_config = 1';
+		$ilDB->setLimit(1);
+		$query = 'SELECT * FROM ' . self::$settingsTable;
 
-		$rset = $ilDB->query( $query );
-
-		if( $row = $ilDB->fetchObject( $rset ) )
+		$rset = $ilDB->query($query);
+		if($row = $ilDB->fetchObject($rset))
 		{
-			$obj = new ilChatroomAdmin( $row->instance_id, $row );
-			return $obj;
+			return new self((int)$row->instance_id, $row);
 		}
 	}
 
 	/**
 	 * Instantiates ilChatroomServerSettings object, sets data using
 	 * $this->settings->server_settings and returns object.
-	 *
 	 * @return ilChatroomServerSettings
 	 */
 	public function getServerSettings()
 	{
 		require_once 'Modules/Chatroom/classes/class.ilChatroomServerSettings.php';
 		return ilChatroomServerSettings::loadDefault();
-		/*$data = json_decode( $this->settings->server_settings );
-
-		$settings = new ilChatroomServerSettings();
-
-		$settings->setDomain( $data->address );
-		$settings->setPort( $data->port );
-		$settings->setProtocol( $data->protocol );
-		$settings->setInstance( $data->instance );
-
-		return $settings;*/
 	}
 
 	/**
 	 * Saves given $settings into settingsTable.
-	 *
-	 * @global ilDBMySQL $ilDB
 	 * @param stdClass $settings
 	 */
 	public function saveGeneralSettings(stdClass $settings)
 	{
+		/**
+		 * @var $ilDB ilDB
+		 */
 		global $ilDB;
 
 		$res = $ilDB->queryF("
 				SELECT 	* 
 				FROM 	chatroom_admconfig
 				WHERE	instance_id = %s",
-
-		array('integer'),
-		array($this->config_id)
+			array('integer'),
+			array($this->config_id)
 		);
 
 		$row = $ilDB->fetchAssoc($res);
@@ -102,9 +95,8 @@ class ilChatroomAdmin
 			DELETE 
 			FROM 	chatroom_admconfig
 			WHERE	instance_id = %s",
-
-		array('integer'),
-		array($this->config_id)
+			array('integer'),
+			array($this->config_id)
 		);
 
 		$row['default_config'] !== null ? $def_conf = $row['default_config'] : $def_conf = "{}";
@@ -114,30 +106,28 @@ class ilChatroomAdmin
 			INSERT INTO		chatroom_admconfig
 							(instance_id, server_settings, default_config, client_settings)
 			VALUES			(%s, %s, %s, %s)",
-
-		array('integer', 'text', 'integer', 'text'),
-		array($this->config_id, json_encode( $settings ), $def_conf, $clnt_set)
+			array('integer', 'text', 'integer', 'text'),
+			array($this->config_id, json_encode($settings), $def_conf, $clnt_set)
 		);
 	}
 
-
 	/**
 	 * Saves given client $settings into settingsTable.
-	 *
-	 * @global ilDBMySQL $ilDB
 	 * @param stdClass $settings
 	 */
 	public function saveClientSettings(stdClass $settings)
 	{
+		/**
+		 * @var $ilDB
+		 */
 		global $ilDB;
 
 		$res = $ilDB->queryF("
 				SELECT 	* 
 				FROM 	chatroom_admconfig
 				WHERE	instance_id = %s",
-
-		array('integer'),
-		array($this->config_id)
+			array('integer'),
+			array($this->config_id)
 		);
 
 		$row = $ilDB->fetchAssoc($res);
@@ -146,9 +136,8 @@ class ilChatroomAdmin
 			DELETE 
 			FROM 	chatroom_admconfig
 			WHERE	instance_id = %s",
-
-		array('integer'),
-		array($this->config_id)
+			array('integer'),
+			array($this->config_id)
 		);
 
 		$row['default_config'] !== null ? $def_conf = $row['default_config'] : $def_conf = "{}";
@@ -158,28 +147,37 @@ class ilChatroomAdmin
 			INSERT INTO		chatroom_admconfig
 							(instance_id, server_settings, default_config, client_settings)
 			VALUES			(%s, %s, %s, %s)",
-
-		array('integer', 'text', 'integer', 'text'),
-		array($this->config_id, $srv_set, $def_conf, json_encode( $settings ))
+			array(
+				'integer',
+				'text',
+				'integer',
+				'text'
+			),
+			array(
+				$this->config_id,
+				$srv_set,
+				$def_conf,
+				json_encode($settings)
+			)
 		);
 	}
 
 	/**
 	 * Returns an array containing server settings from settingsTable.
-	 *
-	 * @global ilDBMySQL $ilDB
-	 * @return mixed
+	 * @return null|stdClass
 	 */
 	public function loadGeneralSettings()
 	{
+		/**
+		 * @var $ilDB ilDB
+		 */
 		global $ilDB;
 
-		$query = 'SELECT * FROM ' . self::$settingsTable . ' WHERE instance_id = ' .
-		$ilDB->quote( $this->config_id, 'integer' );
+		$query = 'SELECT * FROM ' . self::$settingsTable . ' WHERE instance_id = ' . $ilDB->quote($this->config_id, 'integer');
 
-		if( ($row = $ilDB->fetchAssoc( $ilDB->query( $query ) )) && $row['server_settings'] )
+		if(($row = $ilDB->fetchAssoc($ilDB->query($query))) && $row['server_settings'])
 		{
-			return json_decode( $row['server_settings'] );
+			return json_decode($row['server_settings']);
 		}
 
 		return null;
@@ -187,25 +185,21 @@ class ilChatroomAdmin
 
 	/**
 	 * Returns an array containing client settings from settingsTable.
-	 *
-	 * @global ilDBMySQL $ilDB
-	 * @return mixed
+	 * @return null|stdClass
 	 */
 	public function loadClientSettings()
 	{
+		/**
+		 * @var $ilDB ilDB
+		 */
 		global $ilDB;
 
-		$query = 'SELECT * FROM ' . self::$settingsTable . ' WHERE instance_id = ' .
-		$ilDB->quote( $this->config_id, 'integer' );
-
-		if( ($row = $ilDB->fetchAssoc( $ilDB->query( $query ) )) && $row['client_settings'] )
+		$query = 'SELECT * FROM ' . self::$settingsTable . ' WHERE instance_id = ' . $ilDB->quote($this->config_id, 'integer');
+		if(($row = $ilDB->fetchAssoc($ilDB->query($query))) && $row['client_settings'])
 		{
-			return json_decode( $row['client_settings'] );
+			return json_decode($row['client_settings']);
 		}
 
 		return null;
 	}
-
 }
-
-?>
