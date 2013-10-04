cdb8b699be1032b04a6a1c93683d9191172a21a1
Björn Heyser <bheyser@databay.de>
2013-10-04 13:30:31 +0000

    trt: random question fetching from stage // check + strore with amount per pool
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@45242 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Test/classes/class.ilTestRandomQuestionSetBuilderWithAmountPerTest.php
+++ Modules/Test/classes/class.ilTestRandomQuestionSetBuilderWithAmountPerTest.php
@@ -11,111 +11,29 @@ require_once 'Modules/Test/classes/class.ilTestRandomQuestionSetBuilder.php';
  */
 class ilTestRandomQuestionSetBuilderWithAmountPerTest extends ilTestRandomQuestionSetBuilder
 {
-	private $questionStage = array();
-
-	private $questionSet = array();
-
 	public function checkBuildable()
 	{
-		$this->buildQuestionStage();
+		$questionStage = $this->getQuestionStageForSourcePoolDefinitionList($this->sourcePoolDefinitionList);
 
-		return $this->isQuestionSetFetchable();
+		return $this->isQuestionSetFetchable($questionStage);
 	}
 
 	public function performBuild(ilTestSession $testSession)
 	{
-		$this->buildQuestionStage();
-		$this->fetchQuestionSet();
-		$this->storeQuestionSet($testSession);
+		$questionStage = $this->getQuestionStageForSourcePoolDefinitionList($this->sourcePoolDefinitionList);
+
+		$questionSet = $this->fetchQuestionsFromStageRandomly(
+			$questionStage, $this->questionSetConfig->getQuestionAmountPerTest()
+		);
+
+		$this->storeQuestionSet($testSession, $questionSet);
 	}
 
-	private function buildQuestionStage()
-	{
-		foreach($this->sourcePoolDefinitionList as $definition)
-		{
-			/** @var ilTestRandomQuestionSetSourcePoolDefinition $definition */
-
-			$this->stagingPoolQuestionList->resetQuestions();
-
-			$this->stagingPoolQuestionList->setTestId( $this->testOBJ->getTestId() );
-			$this->stagingPoolQuestionList->setPoolId( $definition->getPoolId() );
-
-			if( $this->hasTaxonomyFilter($definition) )
-			{
-				$this->stagingPoolQuestionList->addTaxonomyFilter(
-					$definition->getMappedFilterTaxId(), array($definition->getMappedFilterTaxNodeId())
-				);
-			}
-
-			$this->stagingPoolQuestionList->loadQuestions();
-
-			$this->questionStage = array_merge(
-				$this->questionStage, $this->stagingPoolQuestionList->getQuestions()
-			);
-		}
-
-		array_unique($this->questionStage);
-	}
-
-	private function hasTaxonomyFilter(ilTestRandomQuestionSetSourcePoolDefinition $definition)
-	{
-		if( !(int)$definition->getMappedFilterTaxId() )
-		{
-			return false;
-		}
-
-		if( !(int)$definition->getMappedFilterTaxNodeId() )
-		{
-			return false;
-		}
-
-		return true;
-	}
-
-	private function isQuestionSetFetchable()
+	private function isQuestionSetFetchable($questionStage)
 	{
 		$requiredAmount = $this->questionSetConfig->getQuestionAmountPerTest();
-		$possibleAmount = count($this->questionStage);
+		$possibleAmount = count($questionStage);
 
 		return ( $possibleAmount >= $requiredAmount );
-	}
-
-	private function fetchQuestionSet()
-	{
-		$requiredAmount = $this->questionSetConfig->getQuestionAmountPerTest();
-
-		$randomKeys = array_rand($this->questionStage, $requiredAmount);
-
-		foreach($randomKeys as $randomKey)
-		{
-			$this->questionSet[] = $this->questionStage[$randomKey];
-		}
-
-		if( $this->testOBJ->getShuffleQuestions() )
-		{
-			shuffle($this->questionSet);
-		}
-	}
-
-	private function storeQuestionSet(ilTestSession $testSession)
-	{
-		foreach($this->questionSet as $sequencePosition => $questionId)
-		{
-			$this->storeQuestion($testSession, $questionId, $sequencePosition);
-		}
-	}
-
-	private function storeQuestion(ilTestSession $testSession, $questionId, $sequencePosition)
-	{
-		$nextId = $this->db->nextId('tst_test_rnd_qst');
-
-		$this->db->insert('tst_test_rnd_qst', array(
-			'test_random_question_id' => array('integer', $nextId),
-			'active_fi' => array('integer', $testSession->getActiveId()),
-			'question_fi' => array('integer', $questionId),
-			'sequence' => array('integer', $sequencePosition),
-			'pass' => array('integer', $testSession->getPass()),
-			'tstamp' => array('integer', time())
-		));
 	}
 }
