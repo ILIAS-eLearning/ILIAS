0b9fbfcfea093d5a8f0e2c57fe0e584bbf38dccf
Michael Jansen <mjansen@databay.de>
2013-10-09 15:14:51 +0000

    Switched ilCaptchaInputGUI to new version of Securimage
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@45332 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Captcha/classes/class.ilSecurImageUtil.php
+++ Services/Captcha/classes/class.ilSecurImageUtil.php
@@ -11,7 +11,7 @@
  */
 class ilSecurImageUtil
 {
-	private static $ver = "3_0_1";
+	private static $ver = "3_5_1";
 	
 	/**
 	 * Get directory
@@ -38,7 +38,7 @@ class ilSecurImageUtil
 	 */
 	public static function getAudioScript()
 	{
-		return self::getDirectory() . '/securimage_play.swf?audio_file=' . self::getDirectory(). '/il_securimage_play.php';
+		return self::getDirectory() . '/securimage_play.swf?audio_file=' . self::getDirectory(). '/il_securimage_play.php&amp;icon_file=' . ilUtil::getImagePath('amarok.png');
 	}
 	
 	/**
