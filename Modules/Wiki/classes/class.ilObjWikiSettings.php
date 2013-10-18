0996a63f00b8873222b5ccb0b70b3d0807d6e5a7
Alexander Killing <killing@leifos.de>
2013-10-18 08:04:33 +0000

    back to object2
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@45614 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Wiki/classes/class.ilObjWikiSettings.php
+++ Modules/Wiki/classes/class.ilObjWikiSettings.php
@@ -2,7 +2,7 @@
 
 /* Copyright (c) 1998-2013 ILIAS open source, Extended GPL, see docs/LICENSE */
 
-require_once "./Services/Object/classes/class.ilObject.php";
+require_once "./Services/Object/classes/class.ilObject2.php";
 
 /**
  * Wiki settings application class
@@ -12,18 +12,18 @@ require_once "./Services/Object/classes/class.ilObject.php";
  *
  * @ingroup ModulesWiki
  */
-class ilObjWikiSettings extends ilObject
+class ilObjWikiSettings extends ilObject2
 {
 	/**
-	 * Constructor
+	 * Get type
 	 *
-	 * @param	integer	reference_id or object_id
-	 * @param	boolean	treat the id as reference_id (true) or object_id (false)
+	 * @param
+	 * @return
 	 */
-	public function __construct($a_id = 0, $a_call_by_reference = true)
+	function initType()
 	{
-		$this->type = 'wiks';
-		parent::__construct($a_id, $a_call_by_reference);
+		$this->type = "wiks";
 	}
+	
 }
 ?>
