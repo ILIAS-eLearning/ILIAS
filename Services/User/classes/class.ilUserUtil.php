1a00e9c4ca39bfec87643a4dded886d169635d17
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2013-11-20 10:35:48 +0000

    #10122 - header logo: personal starting point
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@46316 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/User/classes/class.ilUserUtil.php
+++ Services/User/classes/class.ilUserUtil.php
@@ -241,7 +241,7 @@ class ilUserUtil
 	 */
 	public static function getStartingPoint()
 	{
-		global $ilSetting;
+		global $ilSetting, $ilUser;
 				
 		$valid = array_keys(self::getPossibleStartingPoints());		
 		$current = $ilSetting->get("usr_starting_point");	
@@ -262,6 +262,10 @@ class ilUserUtil
 			}						
 			
 			self::setStartingPoint($current);
+		}
+		if($ilUser->getId() == ANONYMOUS_USER_ID)
+		{
+			$current = self::START_REPOSITORY;
 		}
 		return $current;
 	}	
