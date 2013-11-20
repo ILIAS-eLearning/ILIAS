176d620f47319df02435751a5603299bd3b7a6ed
Stefan Meyer <smeyer.ilias@gmx.de>
2013-11-20 11:43:19 +0000

    Fixed role import/export
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@46324 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/AccessControl/classes/class.ilRoleXmlExport.php
+++ Services/AccessControl/classes/class.ilRoleXmlExport.php
@@ -121,7 +121,8 @@ class ilRoleXmlExport extends ilXmlWriter
 		
 		$attributes = array(
 			'type'	=> ilObject::_lookupType($a_role_id),
-			'id'	=> 'il_'.IL_INST_ID.'_'.ilObject::_lookupType($a_role_id).'_'.$a_role_id
+			'id'	=> 'il_'.IL_INST_ID.'_'.ilObject::_lookupType($a_role_id).'_'.$a_role_id,
+			'protected' => ($GLOBALS['rbacreview']->isProtected($a_rolf,$a_role_id) ? 1 : 0)
 		);
 
 		$this->xmlStartTag('role',$attributes);
