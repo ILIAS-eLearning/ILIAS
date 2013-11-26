3142a1b46564aa6bb289b55e73c1ee22660ad5b2
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2013-11-26 08:48:48 +0000

    #11794 - number input: disabled js validation
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@46408 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Form/classes/class.ilNumberInputGUI.php
+++ Services/Form/classes/class.ilNumberInputGUI.php
@@ -384,7 +384,9 @@ class ilNumberInputGUI extends ilSubEnabledFormPropertyGUI
 				" disabled=\"disabled\"");
 		}
 		
+		/*
 		$tpl->setVariable("JS_DECIMALS_ALLOWED", (int)$this->areDecimalsAllowed());
+		*/ 
 		
 		// constraints
 		if ($this->areDecimalsAllowed() && $this->getDecimals() > 0)
