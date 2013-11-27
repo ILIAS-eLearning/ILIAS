1137f9c81965d423d7e2e6d9c855a669dfa0bef9
Michael Jansen <mjansen@databay.de>
2013-11-27 09:46:37 +0000

    Fix for mantis bug report #12195
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@46432 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Administration/classes/class.ilObjExternalToolsSettingsGUI.php
+++ Services/Administration/classes/class.ilObjExternalToolsSettingsGUI.php
@@ -28,62 +28,9 @@ class ilObjExternalToolsSettingsGUI extends ilObjectGUI
 		$this->type = "extt";
 		$this->ilObjectGUI($a_data,$a_id,$a_call_by_reference,false);
 		
-		define ("ILINC_DEFAULT_HTTP_PORT",80);
-		define ("ILINC_DEFAULT_SSL_PORT",443);
-		define ("ILINC_DEFAULT_TIMEOUT",30);
 		$lng->loadLanguageModule("delic");
 		$lng->loadLanguageModule("gmaps");
 		$lng->loadLanguageModule("mathjax");
-	}
-	
-	/**
-	* display settings menu
-	* 
-	* @access	public
-	*/
-	function viewObject()
-	{
-		$this->editiLincObject();
-		
-		
-//		global $rbacsystem;
-//		
-//		if (!$rbacsystem->checkAccess("visible,read",$this->object->getRefId()))
-//		{
-//			$this->ilias->raiseError($this->lng->txt("permission_denied"),$this->ilias->error_obj->MESSAGE);
-//		}
-//		
-//		$this->__initSubTabs("view");
-//		
-//		$this->tpl->addBlockFile("ADM_CONTENT", "adm_content", "tpl.extt_general.html",
-//			"Services/Administration");
-//		
-//		$this->tpl->setVariable("FORMACTION",
-//			$this->ctrl->getFormAction($this));
-//		$this->tpl->setVariable("TXT_EXTT_TITLE", $this->lng->txt("extt_title_configure"));
-//
-//		$this->tpl->setVariable("TXT_EXTT_NAME", $this->lng->txt("extt_name"));
-//		$this->tpl->setVariable("TXT_EXTT_ACTIVE", $this->lng->txt("active")."?");
-//		$this->tpl->setVariable("TXT_EXTT_DESC", $this->lng->txt("description"));
-//
-//		$this->tpl->setVariable("TXT_CONFIGURE", $this->lng->txt("extt_configure"));
-//		$this->tpl->setVariable("TXT_EXTT_REMARK", $this->lng->txt("extt_remark"));
-//
-//		// ilinc
-//		$this->tpl->setVariable("TXT_EXTT_ILINC_NAME", $this->lng->txt("extt_ilinc"));
-//		$this->tpl->setVariable("TXT_EXTT_ILINC_DESC", $this->lng->txt("extt_ilinc_desc"));
-//
-//	
-//		// icon handlers
-//		$icon_ok = "<img src=\"".ilUtil::getImagePath("icon_ok.png")."\" alt=\"".$this->lng->txt("enabled")."\" title=\"".$this->lng->txt("enabled")."\" border=\"0\" vspace=\"0\"/>";
-//		$icon_not_ok = "<img src=\"".ilUtil::getImagePath("icon_not_ok.png")."\" alt=\"".$this->lng->txt("disabled")."\" title=\"".$this->lng->txt("disabled")."\" border=\"0\" vspace=\"0\"/>";
-//
-//		$this->tpl->setVariable("EXTT_ILINC_ACTIVE", $this->ilias->getSetting('ilinc_active') ? $icon_ok : $icon_not_ok);
-	}
-	
-	function cancelObject()
-	{
-		$this->ctrl->redirect($this, "editiLinc");
 	}
 
 	function getAdminTabs(&$tabs_gui)
@@ -106,10 +53,10 @@ class ilObjExternalToolsSettingsGUI extends ilObjectGUI
 		{
 			$tabs_gui->addTarget("settings",
 //				$this->ctrl->getLinkTarget($this, "view"), 
-//				array("view","editiLinc","editDelicious", "editGoogleMaps","editMathJax", ""), "", "");
+//				array("view","editDelicious", "editGoogleMaps","editMathJax", ""), "", "");
 			
 				$this->ctrl->getLinkTarget($this, "editSocialBookmarks"),
-				array("editiLinc","editDelicious", "editGoogleMaps","editMathJax", ""), "", "");
+				array("editDelicious", "editGoogleMaps","editMathJax", ""), "", "");
 			$this->lng->loadLanguageModule('ecs');
 		}
 
@@ -119,187 +66,7 @@ class ilObjExternalToolsSettingsGUI extends ilObjectGUI
 				$this->ctrl->getLinkTargetByClass(array(get_class($this),'ilpermissiongui'), "perm"), array("perm","info","owner"), 'ilpermissiongui');
 		}
 	}
-	
-	/**
-	* Configure iLinc settings
-	* 
-	* @access	public
-	*/
-	function editiLincObject()
-	{
-		global $rbacsystem, $rbacreview;
-		
-		if (!$rbacsystem->checkAccess("write",$this->object->getRefId()))
-		{
-			$this->ilias->raiseError($this->lng->txt("permission_denied"),$this->ilias->error_obj->MESSAGE);
-		}
-		
-		ilUtil::sendFailure($this->lng->txt('ilinc_abandoned_info'));
-		
-		$this->__initSubTabs("editiLinc");
-		
-		if ($_SESSION["error_post_vars"])
-		{
-			if ($_SESSION["error_post_vars"]["ilinc"]["active"] == "1")
-			{
-				$this->tpl->setVariable("CHK_ILINC_ACTIVE", "checked=\"checked\"");
-			}
-			
-			if ($_SESSION["error_post_vars"]["ilinc"]["akclassvalues_active"] == "1")
-			{
-				$this->tpl->setVariable("CHK_ILINC_AKCLASSVALUES_ACTIVE", "checked=\"checked\"");
-			}
-			
-			if ($_SESSION["error_post_vars"]["ilinc"]["akclassvalues_required"] == "1")
-			{
-				$this->tpl->setVariable("CHK_ILINC_AKCLASSVALUES_REQUIRED", "checked=\"checked\"");
-			}
-			
-			$this->tpl->setVariable("ILINC_SERVER", $_SESSION["error_post_vars"]["ilinc"]["server"]);
-			$this->tpl->setVariable("ILINC_REGISTRAR_LOGIN", $_SESSION["error_post_vars"]["ilinc"]["registrar_login"]);
-			$this->tpl->setVariable("ILINC_REGISTRAR_PASSWD", $_SESSION["error_post_vars"]["ilinc"]["registrar_passwd"]);
-			$this->tpl->setVariable("ILINC_CUSTOMER_ID", $_SESSION["error_post_vars"]["ilinc"]["customer_id"]);
-		}
-		else
-		{
-			// set already saved data or default value for port
-			$settings = $this->ilias->getAllSettings();
 
-			if ($settings["ilinc_active"] == "1")
-			{
-				$this->tpl->setVariable("CHK_ILINC_ACTIVE", "checked=\"checked\"");
-			}
-
-			$this->tpl->setVariable("ILINC_SERVER", $settings["ilinc_server"].$settings["ilinc_path"]);
-			$this->tpl->setVariable("ILINC_REGISTRAR_LOGIN", $settings["ilinc_registrar_login"]);
-			$this->tpl->setVariable("ILINC_REGISTRAR_PASSWD", $settings["ilinc_registrar_passwd"]);
-			$this->tpl->setVariable("ILINC_CUSTOMER_ID", $settings["ilinc_customer_id"]);
-			
-			if (empty($settings["ilinc_port"]))
-			{
-				$this->tpl->setVariable("ILINC_PORT", ILINC_DEFAULT_HTTP_PORT);
-			}
-			else
-			{
-				$this->tpl->setVariable("ILINC_PORT", $settings["ilinc_port"]);			
-			}
-
-			if ($settings["ilinc_protocol"] == "https")
-			{
-				$this->tpl->setVariable("ILINC_PROTOCOL_SSL_SEL", "selected=\"selected\"");
-			}
-			else
-			{
-				$this->tpl->setVariable("ILINC_PROTOCOL_HTTP_SEL", "selected=\"selected\"");		
-			}
-			
-			if (empty($settings["ilinc_timeout"]))
-			{
-				$this->tpl->setVariable("ILINC_TIMEOUT", ILINC_DEFAULT_TIMEOUT);
-			}
-			else
-			{
-				$this->tpl->setVariable("ILINC_TIMEOUT", $settings["ilinc_timeout"]);			
-			}
-
-			if ($settings["ilinc_akclassvalues_active"] == "1")
-			{
-				$this->tpl->setVariable("CHK_ILINC_AKCLASSVALUES_ACTIVE", "checked=\"checked\"");
-			}
-
-			if ($settings["ilinc_akclassvalues_required"] == "1")
-			{
-				$this->tpl->setVariable("CHK_ILINC_AKCLASSVALUES_REQUIRED", "checked=\"checked\"");
-			}	
-		}
-
-		$this->tpl->addBlockFile("ADM_CONTENT", "adm_content", "tpl.extt_ilinc.html",
-			"Services/Administration");
-		
-		$this->tpl->setVariable("FORMACTION", $this->ctrl->getFormAction($this));
-		$this->tpl->setVariable("TXT_ILINC_TITLE", $this->lng->txt("extt_ilinc_configure"));
-		$this->tpl->setVariable("TXT_ILINC_ACTIVE", $this->lng->txt("extt_ilinc_enable"));
-		$this->tpl->setVariable("TXT_ILINC_CONNECTION_DATA", $this->lng->txt("extt_ilinc_connection_data"));
-		$this->tpl->setVariable("TXT_ILINC_ADDITIONAL_OPTIONS", $this->lng->txt("extt_ilinc_additional_options"));
-		$this->tpl->setVariable("TXT_ILINC_SERVER", $this->lng->txt("extt_ilinc_server"));
-		$this->tpl->setVariable("TXT_ILINC_PROTOCOL_PORT", $this->lng->txt("extt_ilinc_protocol_port"));
-		$this->tpl->setVariable("TXT_ILINC_TIMEOUT", $this->lng->txt("extt_ilinc_timeout"));
-		$this->tpl->setVariable("ILINC_DEFAULT_HTTP_PORT", ILINC_DEFAULT_HTTP_PORT);
-		$this->tpl->setVariable("ILINC_DEFAULT_SSL_PORT", ILINC_DEFAULT_SSL_PORT);
-		$this->tpl->setVariable("TXT_HTTP", $this->lng->txt('http'));
-		$this->tpl->setVariable("TXT_SSL", $this->lng->txt('ssl'));
-		
-		$this->tpl->setVariable("TXT_SECONDS", $this->lng->txt("seconds"));
-		$this->tpl->setVariable("TXT_ILINC_REGISTRAR_LOGIN", $this->lng->txt("extt_ilinc_registrar_login"));
-		$this->tpl->setVariable("TXT_ILINC_REGISTRAR_PASSWD", $this->lng->txt("extt_ilinc_registrar_passwd"));
-		$this->tpl->setVariable("TXT_ILINC_CUSTOMER_ID", $this->lng->txt("extt_ilinc_customer_id"));
-		
-		$this->tpl->setVariable("TXT_ILINC_AKCLASSVALUES_ACTIVE", $this->lng->txt("extt_ilinc_akclassvalues_active"));
-		$this->tpl->setVariable("TXT_ILINC_AKCLASSVALUES_ACTIVE_INFO", $this->lng->txt("extt_ilinc_akclassvalues_active_info"));
-		$this->tpl->setVariable("TXT_ILINC_AKCLASSVALUES_REQUIRED", $this->lng->txt("extt_ilinc_akclassvalues_required"));
-		$this->tpl->setVariable("TXT_ILINC_AKCLASSVALUES_REQUIRED_INFO", $this->lng->txt("extt_ilinc_akclassvalues_required_info"));
-
-		$this->tpl->setVariable("TXT_REQUIRED_FLD", $this->lng->txt("required_field"));
-		$this->tpl->setVariable("TXT_CANCEL", $this->lng->txt("cancel"));
-		$this->tpl->setVariable("TXT_SUBMIT", $this->lng->txt("save"));
-		$this->tpl->setVariable("CMD_SUBMIT", "saveiLinc");
-	}
-
-	/**
-	* validates all input data, save them to database if correct and active chosen extt mode
-	* 
-	* @access	public
-	*/
-	function saveiLincObject()
-	{
-         global $ilUser;
-
-        // validate required data 
-		if (!$_POST["ilinc"]["server"] or !$_POST["ilinc"]["port"] or !$_POST["ilinc"]["registrar_login"] or !$_POST["ilinc"]["registrar_passwd"] or !$_POST["ilinc"]["customer_id"])
-		{
-			$this->ilias->raiseError($this->lng->txt("fill_out_all_required_fields"),$this->ilias->error_obj->MESSAGE);
-		}
-		
-		// validate port
-		if ((preg_match("/^[0-9]{0,5}$/",$_POST["ilinc"]["port"])) == false)
-		{
-			$this->ilias->raiseError($this->lng->txt("err_invalid_port"),$this->ilias->error_obj->MESSAGE);
-		}
-		
-		if (substr($_POST["ilinc"]["server"],0,8) != "https://" and substr($_POST["ilinc"]["server"],0,7) != "http://")
-		{
-			$_POST["ilinc"]["server"] = $_POST["ilinc"]["protocol"]."://".$_POST["ilinc"]["server"];
-		}
-		
-		$url = parse_url($_POST["ilinc"]["server"]);
-		
-		if (!ilUtil::isIPv4($url["host"]) and !ilUtil::isDN($url["host"]))
-		{
-			$this->ilias->raiseError($this->lng->txt("err_invalid_server"),$this->ilias->error_obj->MESSAGE);
-		}
-		
-		if (is_numeric($_POST["ilinc"]["timeout"]))
-		{
-			$this->ilias->setSetting("ilinc_timeout", $_POST["ilinc"]["timeout"]);
-		}
-
-		// all ok. save settings
-		$this->ilias->setSetting("ilinc_server", $url["host"]);
-		$this->ilias->setSetting("ilinc_path", $url["path"]);
-		$this->ilias->setSetting("ilinc_protocol", $_POST["ilinc"]["protocol"]);
-		$this->ilias->setSetting("ilinc_port", $_POST["ilinc"]["port"]);
-		$this->ilias->setSetting("ilinc_active", $_POST["ilinc"]["active"]);
-		$this->ilias->setSetting("ilinc_registrar_login", $_POST["ilinc"]["registrar_login"]);
-		$this->ilias->setSetting("ilinc_registrar_passwd", $_POST["ilinc"]["registrar_passwd"]);
-		$this->ilias->setSetting("ilinc_customer_id", $_POST["ilinc"]["customer_id"]);
-		
-		$this->ilias->setSetting("ilinc_akclassvalues_active", $_POST["ilinc"]["akclassvalues_active"]);
-		$this->ilias->setSetting("ilinc_akclassvalues_required", $_POST["ilinc"]["akclassvalues_required"]);
-
-		ilUtil::sendSuccess($this->lng->txt("extt_ilinc_settings_saved"),true);
-		$this->ctrl->redirect($this,'editiLinc');
-	}
-	
 	function addSocialBookmarkObject()
 	{
 		global $ilAccess, $rbacreview, $lng, $ilCtrl;
@@ -745,7 +512,6 @@ class ilObjExternalToolsSettingsGUI extends ilObjectGUI
 	// init sub tabs
 	function __initSubTabs($a_cmd)
 	{
-		$ilinc = ($a_cmd == 'editiLinc') ? true : false;
 //		$overview = ($a_cmd == 'view' or $a_cmd == '') ? true : false;
 		//$delicious = ($a_cmd == 'editDelicious') ? true : false;
 		
@@ -767,8 +533,6 @@ class ilObjExternalToolsSettingsGUI extends ilObjectGUI
 											"", "", "", $mathjax);
 		$this->tabs_gui->addSubTabTarget("gmaps_extt_gmaps", $this->ctrl->getLinkTarget($this, "editGoogleMaps"),
 										 "", "", "", $gmaps);
-		$this->tabs_gui->addSubTabTarget("extt_ilinc", $this->ctrl->getLinkTarget($this, "editiLinc"),
-										 "", "", "", $ilinc);
 	}
 	
 	function &executeCommand()
