6f1cf161b22c5dad14dc36a5b81fa4ab6d601e08
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2013-12-03 09:46:37 +0000

    course: verification
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@46552 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Course/classes/Verification/class.ilObjCourseVerificationListGUI.php
+++ Modules/Course/classes/Verification/class.ilObjCourseVerificationListGUI.php
@@ -3,7 +3,7 @@
 /* Copyright (c) 1998-2009 ILIAS open source, Extended GPL, see docs/LICENSE */
 
 /**
-* Class ilObjExerciseVerificationListGUI
+* Class ilObjCourseVerificationListGUI
 *
 * @author Jörg Lützenkirchen <luetzenkirchen@leifos.com>
 * $Id: class.ilObjFolderListGUI.php 26089 2010-10-20 08:08:05Z smeyer $
@@ -13,7 +13,7 @@
 
 include_once "Services/Object/classes/class.ilObjectListGUI.php";
 
-class ilObjExerciseVerificationListGUI extends ilObjectListGUI
+class ilObjCourseVerificationListGUI extends ilObjectListGUI
 {
 	/**
 	* initialisation
@@ -27,12 +27,12 @@ class ilObjExerciseVerificationListGUI extends ilObjectListGUI
 		$this->link_enabled = false;
 		$this->payment_enabled = false;
 		$this->info_screen_enabled = false;
-		$this->type = "excv";
-		$this->gui_class_name = "ilobjexerciseverificationgui";
+		$this->type = "crsv";
+		$this->gui_class_name = "ilobjcourseverificationgui";
 
 		// general commands array
-		include_once('./Modules/Exercise/classes/class.ilObjExerciseVerificationAccess.php');
-		$this->commands = ilObjExerciseVerificationAccess::_getCommands();
+		include_once('./Modules/Course/classes/Verification/class.ilObjCourseVerificationAccess.php');
+		$this->commands = ilObjCourseVerificationAccess::_getCommands();
 	}
 	
 	function getProperties()
@@ -41,10 +41,9 @@ class ilObjExerciseVerificationListGUI extends ilObjectListGUI
 		
 		return array(
 			array("alert" => false, "property" => $lng->txt("type"),
-				"value" => $lng->txt("wsp_list_excv"))			
+				"value" => $lng->txt("wsp_list_crsv"))			
 		);
 	}
 	
-} 
-
+} // END class.ilObjTestVerificationListGUI
 ?>
