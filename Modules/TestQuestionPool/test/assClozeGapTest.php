2f9924c20df00f89d1b5ffcbc4bfc0f27099b2b2
Maximilian Becker <mbecker@databay.de>
2013-12-06 08:22:11 +0000

    A test. (A test a day keeps the bugs away.)
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@46609 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/TestQuestionPool/test/assClozeGapTest.php
+++ Modules/TestQuestionPool/test/assClozeGapTest.php
@@ -134,7 +134,7 @@ class assClozeGapTest extends PHPUnit_Framework_TestCase
 	public function test_addGetItem_shouldReturnValueUnchangedMultiplePlus()
 	{
 		$this->markTestIncomplete('SUT defective. Please check the inappropriate use of array_push vs. order-indices.');
-
+		// @TODO: Investigate addItem-Method.
 		// Arrange
 		require_once './Modules/TestQuestionPool/classes/class.assClozeGap.php';
 		$instance = new assClozeGap(0); // 0 - text gap
@@ -150,6 +150,113 @@ class assClozeGapTest extends PHPUnit_Framework_TestCase
 		$instance->addItem($answer3);
 		$instance->addItem($expected);
 		$actual = $instance->getItem(0);
+
+		// Assert
+		$this->assertEquals($expected, $actual);
+	}
+
+	public function test_getItems_shouldReturnItemsAdded()
+	{
+		// Arrange
+		require_once './Modules/TestQuestionPool/classes/class.assClozeGap.php';
+		$instance = new assClozeGap(0); // 0 - text gap
+		require_once './Modules/TestQuestionPool/classes/class.assAnswerCloze.php';
+		$item1 = new assAnswerCloze('Bert', 1.0, 0);
+		$item2 = new assAnswerCloze('Fred', 1.0, 2);
+		$item3 = new assAnswerCloze('Karl', 1.0, 1);
+		$item4 = new assAnswerCloze('Esther', 1.0, 3);
+
+		// Act
+		$instance->addItem($item1);
+		$instance->addItem($item2);
+		$instance->addItem($item3);
+		$instance->addItem($item4);
+		$actual = $instance->getItems();
+
+		// I have the feeling, that the order of the items in the return value is broken.
+		// @TODO: Investigate addItem-Method.
+		// Assert
+		$this->assertTrue(is_array($actual));
+		$this->assertTrue(in_array($item1, $actual));
+		$this->assertTrue(in_array($item2, $actual));
+		$this->assertTrue(in_array($item3, $actual));
+		$this->assertTrue(in_array($item4, $actual));
+	}
+
+	public function test_getItemsWithShuffle_shouldReturnItemsAddedShuffled()
+	{
+		// Arrange
+		require_once './Modules/TestQuestionPool/classes/class.assClozeGap.php';
+		$instance = new assClozeGap(0); // 0 - text gap
+		$instance->setShuffle(true);
+		require_once './Modules/TestQuestionPool/classes/class.assAnswerCloze.php';
+		$item1 = new assAnswerCloze('Bert', 1.0, 0);
+		$item2 = new assAnswerCloze('Fred', 1.0, 2);
+		$item3 = new assAnswerCloze('Karl', 1.0, 1);
+		$item4 = new assAnswerCloze('Esther', 1.0, 3);
+
+		// Act
+		$instance->addItem($item1);
+		$instance->addItem($item2);
+		$instance->addItem($item3);
+		$instance->addItem($item4);
+		$actual = $instance->getItems();
+
+		// I have the feeling, that the order of the items in the return value is broken.
+		// @TODO: Investigate addItem-Method.
+		// Assert
+		$this->assertTrue(is_array($actual));
+		$this->assertTrue(in_array($item1, $actual));
+		$this->assertTrue(in_array($item2, $actual));
+		$this->assertTrue(in_array($item3, $actual));
+		$this->assertTrue(in_array($item4, $actual));
+	}
+
+	public function test_getItemsRaw_shouldReturnItemsAdded()
+	{
+		// Arrange
+		require_once './Modules/TestQuestionPool/classes/class.assClozeGap.php';
+		$instance = new assClozeGap(0); // 0 - text gap
+		require_once './Modules/TestQuestionPool/classes/class.assAnswerCloze.php';
+		$item1 = new assAnswerCloze('Bert', 1.0, 0);
+		$item2 = new assAnswerCloze('Fred', 1.0, 2);
+		$item3 = new assAnswerCloze('Karl', 1.0, 1);
+		$item4 = new assAnswerCloze('Esther', 1.0, 3);
+
+		// Act
+		$instance->addItem($item1);
+		$instance->addItem($item2);
+		$instance->addItem($item3);
+		$instance->addItem($item4);
+		$actual = $instance->getItemsRaw();
+
+		// I have the feeling, that the order of the items in the return value is broken.
+		// @TODO: Investigate addItem-Method.
+		// Assert
+		$this->assertTrue(is_array($actual));
+		$this->assertTrue(in_array($item1, $actual));
+		$this->assertTrue(in_array($item2, $actual));
+		$this->assertTrue(in_array($item3, $actual));
+		$this->assertTrue(in_array($item4, $actual));
+	}
+
+	public function test_getItemCount_shouldReturnCorrectCount()
+	{
+		// Arrange
+		require_once './Modules/TestQuestionPool/classes/class.assClozeGap.php';
+		$instance = new assClozeGap(0); // 0 - text gap
+		require_once './Modules/TestQuestionPool/classes/class.assAnswerCloze.php';
+		$item1 = new assAnswerCloze('Bert', 1.0, 0);
+		$item2 = new assAnswerCloze('Fred', 1.0, 2);
+		$item3 = new assAnswerCloze('Karl', 1.0, 1);
+		$item4 = new assAnswerCloze('Esther', 1.0, 3);
+		$expected = 4;
+		// Act
+		$instance->addItem($item1);
+		$instance->addItem($item2);
+		$instance->addItem($item3);
+		$instance->addItem($item4);
+		$actual = $instance->getItemCount();
 
 		// Assert
 		$this->assertEquals($expected, $actual);
