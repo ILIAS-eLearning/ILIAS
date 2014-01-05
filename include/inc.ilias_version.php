fafea7f6302e85d5c41880e72cf406f3fa5869f6
Alexander Killing <killing@leifos.de>
2014-01-05 16:53:40 +0000

    updated languages and version number
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/Release_4_3_x_branch@47029 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- include/inc.ilias_version.php
+++ include/inc.ilias_version.php
@@ -9,6 +9,6 @@
 *
 * @package ilias-core
 */
-define("ILIAS_VERSION", "4.3.5 2013-10-08");
-define("ILIAS_VERSION_NUMERIC", "4.3.5");			// must be always x.y.z: x, y and z are numbers
+define("ILIAS_VERSION", "4.3.6 2014-01-05");
+define("ILIAS_VERSION_NUMERIC", "4.3.6");			// must be always x.y.z: x, y and z are numbers
 ?>
