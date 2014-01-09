8f2144b12bf93f6f8f1f7b95a8f0b47554a5d76c
Stefan Wanzenried <sw@studer-raimann.ch>
2014-01-09 14:26:51 +0000

    #12446 - Revert commit 45684
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/Release_4_3_x_branch@47153 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/DataCollection/classes/class.ilDataCollectionRecordField.php
+++ Modules/DataCollection/classes/class.ilDataCollectionRecordField.php
@@ -181,15 +181,15 @@ class ilDataCollectionRecordField
 	 */
 	protected function loadValue()
 	{
-		//if($this->value === NULL)
-		//{
+		if($this->value === NULL)
+		{
 			global $ilDB;
 			$datatype = $this->field->getDatatype();
 			$query = "SELECT * FROM il_dcl_stloc".$datatype->getStorageLocation()."_value WHERE record_field_id = ".$ilDB->quote($this->id, "integer");
 			$set = $ilDB->query($query);
 			$rec = $ilDB->fetchAssoc($set);
 			$this->value = $rec['value'];
-		//}
+		}
 	}
 	
 	/*
