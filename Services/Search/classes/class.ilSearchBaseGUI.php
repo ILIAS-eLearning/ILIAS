2d86a28983b69229872670565e9a2da3aed1533f
Alexander Killing <killing@leifos.de>
2014-01-16 14:16:26 +0000

    partly fixed bug #11606: Filter by type does not work anymore: do not show type selection, if drop down is empty
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/Release_4_3_x_branch@47312 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Search/classes/class.ilSearchBaseGUI.php
+++ Services/Search/classes/class.ilSearchBaseGUI.php
@@ -79,7 +79,7 @@ class ilSearchBaseGUI implements ilDesktopItemHandling, ilAdministrationCommandH
 	public function initStandardSearchForm($a_mode)
 	{
 		global $lng, $ilCtrl;
-	
+
 		include_once("Services/Form/classes/class.ilPropertyFormGUI.php");
 		$this->form = new ilPropertyFormGUI();
 		$this->form->setOpenTag(false);
