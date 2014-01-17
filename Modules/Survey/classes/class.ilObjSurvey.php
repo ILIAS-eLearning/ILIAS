c7c7a272e696eba14324e039cc60996f67369903
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2014-01-17 14:49:26 +0000

    #12420 - survey: obligatory
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@47337 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Survey/classes/class.ilObjSurvey.php
+++ Modules/Survey/classes/class.ilObjSurvey.php
@@ -2417,6 +2417,11 @@ class ilObjSurvey extends ilObject
 				array('integer','integer','integer','text','integer'),
 				array($next_id, $this->getSurveyId(), $question_fi, (strlen($obligatory)) ? $obligatory : 0, time())
 			);
+			
+			// #12420
+			$ilDB->manipulate("UPDATE svy_question".
+				" SET obligatory = ".$ilDB->quote($obligatory, "integer").
+				" WHERE question_id = ".$ilDB->quote($question_fi, "integer"));
 		}
 	}
 	
