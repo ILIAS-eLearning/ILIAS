b0af3568621af2dc023dd85224cf6e0388c8e9dc
Alexander Killing <killing@leifos.de>
2014-01-25 08:16:52 +0000

    removed wrongly committed trunk dir
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/Release_4_4_x_branch@47517 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- trunk/Modules/ScormAicc/sop/sop.update.php
+++
@@ -1,5 +0,0 @@
-<?php
-/* Copyright (c) 1998-2009 ILIAS open source, Extended GPL, see docs/LICENSE */
-header("Content-Type: application/rdf+xml");
-readfile('sop.update.rdf');
-?>
--- trunk/Services/WebServices/Rest/lib/ReleaseInfo.php
+++
@@ -1,7 +0,0 @@
-<?php 
-/*
-
-Current Version: Slim-1.5.1.5-0
-
-*/
-?>
