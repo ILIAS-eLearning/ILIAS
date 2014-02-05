6b0d1056d1d65cadbd9831c25f903d9272bfa9c7
Maximilian Becker <mbecker@databay.de>
2014-02-05 15:41:23 +0000

    Answer specific feedback for horizontal ordering question.
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@47730 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/TestQuestionPool/classes/feedback/class.ilAssOrderingHorizontalFeedback.php
+++ Modules/TestQuestionPool/classes/feedback/class.ilAssOrderingHorizontalFeedback.php
@@ -1,7 +1,7 @@
 <?php
 /* Copyright (c) 1998-2013 ILIAS open source, Extended GPL, see docs/LICENSE */
 
-require_once 'Modules/TestQuestionPool/classes/feedback/class.ilAssSingleOptionQuestionFeedback.php';
+require_once 'Modules/TestQuestionPool/classes/feedback/class.ilAssMultiOptionQuestionFeedback.php';
 
 /**
  * feedback class for assOrderingHorizontal questions
@@ -11,6 +11,26 @@ require_once 'Modules/TestQuestionPool/classes/feedback/class.ilAssSingleOptionQ
  * 
  * @package		Modules/TestQuestionPool
  */
-class ilAssOrderingHorizontalFeedback extends ilAssSingleOptionQuestionFeedback
+class ilAssOrderingHorizontalFeedback extends ilAssMultiOptionQuestionFeedback
 {
+	/**
+	 * returns the answer options mapped by answer index
+	 * (can be overwritten by concrete question type class)
+	 *
+	 * @return array $answerOptionsByAnswerIndex
+	 */
+	protected function getAnswerOptionsByAnswerIndex()
+	{
+		if (strpos($this->questionOBJ->ordertext, '::'))
+		{
+		return explode('::', $this->questionOBJ->ordertext);
+		}
+		return explode(' ', $this->questionOBJ->ordertext);
+	}
+
+	protected function buildAnswerOptionLabel( $index, $answer )
+	{
+		return trim($answer);
+	}
+
 }
