8a552c0af77b78cd4718cabf9bd81d3afcc91ff0
Gabriel Comte <gc@studer-raimann.ch>
2014-02-13 10:55:55 +0000

    Now also support ISO-8859-1 besides UTF-8 as charset of BIBL and RIS Files
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@47909 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Bibliographic/classes/class.ilObjBibliographic.php
+++ Modules/Bibliographic/classes/class.ilObjBibliographic.php
@@ -303,6 +303,8 @@ class ilObjBibliographic extends ilObject2
 
     static function __readRisFile($full_filename){
 
+	self::__setCharsetToUtf8($full_filename);
+
         require_once "./Modules/Bibliographic/lib/LibRIS/src/LibRIS/RISReader.php";
 
         $ris_reader = new RISReader();
@@ -311,6 +313,8 @@ class ilObjBibliographic extends ilObject2
     }
 
     static function __readBibFile($full_filename){
+
+	self::__setCharsetToUtf8($full_filename);
 
         require_once 'Modules/Bibliographic/lib/PEAR_BibTex_1.0.0RC5/Structures/BibTex.php';
 
@@ -346,6 +350,18 @@ class ilObjBibliographic extends ilObject2
         return $bibtex_reader->data;
     }
 
+
+    static function __setCharsetToUtf8($full_filename){
+        //If file charset does not seem to be Unicode, we assume that it is ISO-8859-1, and convert it to UTF-8.
+        $filedata = file_get_contents($full_filename);
+        if(strlen($filedata) == strlen(utf8_decode($filedata)))
+        {
+            // file charset is not UTF-8
+            $filedata = mb_convert_encoding($filedata, 'UTF-8', 'ISO-8859-1');
+            file_put_contents($full_filename, $filedata);
+        }
+
+    }
 
 	/**
 	 * Clone BIBL
