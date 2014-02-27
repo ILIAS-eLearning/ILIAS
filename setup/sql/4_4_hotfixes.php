4e7736fbe0052a117670b7f3bc1022393879f991
Stefan Meyer <smeyer.ilias@gmx.de>
2014-02-27 09:43:35 +0000

    Added 4.4 hotfix; increased object_data title length
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/Release_4_4_x_branch@48215 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ setup/sql/4_4_hotfixes.php
@@ -0,0 +1,28 @@
+<?php
+// IMPORTANT: Inform the lead developer, if you want to add any steps here.
+//
+// This is the hotfix file for ILIAS 4.4.x DB fixes
+// This file should be used, if bugfixes need DB changes, but the
+// main db update script cannot be used anymore, since it is
+// impossible to merge the changes with the trunk.
+//
+// IMPORTANT: The fixes done here must ALSO BE reflected in the trunk.
+// The trunk needs to work in both cases !!!
+// 1. If the hotfixes have been applied.
+// 2. If the hotfixes have not been applied.
+?>
+<#1>
+<?php
+
+$ilDB->modifyTableColumn(
+		'object_data', 
+		'title',
+		array(
+			"type" => "text", 
+			"length" => 255, 
+			"notnull" => false,
+			'fixed' => true
+		)
+	);
+?>
+
