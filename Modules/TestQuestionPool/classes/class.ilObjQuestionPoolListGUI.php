268268dd56d4582cc55a46c0e85153e0ad323fd8
Björn Heyser <bheyser@databay.de>
2014-03-10 11:32:15 +0000

    fixed mantis #0012938: Fatal error when trying to enter Settings of a QPL from ListGUI via Actions
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@48441 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/TestQuestionPool/classes/class.ilObjQuestionPoolListGUI.php
+++ Modules/TestQuestionPool/classes/class.ilObjQuestionPoolListGUI.php
@@ -120,9 +120,25 @@ class ilObjQuestionPoolListGUI extends ilObjectListGUI
 	*/
 	function getCommandLink($a_cmd)
 	{
-		// separate method for this line
-		//$cmd_link = "assessment/questionpool.php?ref_id=".$this->ref_id."&cmd=$a_cmd";
-		$cmd_link = "ilias.php?baseClass=ilObjQuestionPoolGUI&amp;ref_id=".$this->ref_id."&amp;cmd=$a_cmd";
+		global $ilCtrl;
+
+		$a_cmd = explode('::', $a_cmd);
+
+		if( count($a_cmd) == 2 )
+		{
+			$cmd_link = $ilCtrl->getLinkTargetByClass(array('ilRepositoryGUI', 'ilObjQuestionPoolGUI', $a_cmd[0]), $a_cmd[1]);
+		}
+		else
+		{
+			$cmd_link = $ilCtrl->getLinkTargetByClass('ilObjQuestionPoo', $a_cmd[0]);
+		}
+
+		$params = array_merge(array('ref_id' => $this->ref_id), $this->command_link_params);
+
+		foreach($params as $param => $value)
+		{
+			$cmd_link = ilUtil::appendUrlParameterString($cmd_link, "$param=$value", true);
+		}
 
 		return $cmd_link;
 	}
