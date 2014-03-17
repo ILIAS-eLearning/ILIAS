e06c1deb15d81749be1816c365bf9326b27f89fa
Michael Jansen <mjansen@databay.de>
2014-03-17 07:42:51 +0000

    Merged bugfix from 4.3.x branch (mantis bug report 12995)
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@48639 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Forum/classes/class.ilForumXMLWriter.php
+++ Modules/Forum/classes/class.ilForumXMLWriter.php
@@ -5,6 +5,7 @@
 include_once "./Services/Xml/classes/class.ilXmlWriter.php";
 include_once "./Modules/Forum/classes/class.ilFileDataForum.php";
 include_once "Services/MediaObjects/classes/class.ilObjMediaObject.php";
+include_once "Services/RTE/classes/class.ilRTE.php";
 
 /**
 * XML writer class
