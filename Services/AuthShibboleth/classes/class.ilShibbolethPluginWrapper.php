686e5ee3b00f0ec6242240125d20f6c8da836b55
Fabian Schmid <fs@studer-raimann.ch>
2014-03-19 12:53:46 +0000

    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@48748 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/AuthShibboleth/classes/class.ilShibbolethPluginWrapper.php
+++ Services/AuthShibboleth/classes/class.ilShibbolethPluginWrapper.php
@@ -70,7 +70,7 @@ class ilShibbolethPluginWrapper implements ilShibbolethAuthenticationPluginInt {
 	 * @return ilObjUser
 	 */
 	public function beforeLogin(ilObjUser $user) {
-		$this->log->write('ilShibbolethPluginWrapper::beforeLogin');
+//		$this->log->write('ilShibbolethPluginWrapper::beforeLogin');
 		foreach ($this->getPluginObjects() as $pl) {
 			$user = $pl->beforeLogin($user);
 		}
