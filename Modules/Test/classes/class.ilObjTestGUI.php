cdca45f52ed5bd5a64b7a3dad004e48540a87193
Michael Jansen <mjansen@databay.de>
2014-03-21 17:33:27 +0000

    Possible fix for mantis bug report 12590
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/Release_4_4_x_branch@48843 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Test/classes/class.ilObjTestGUI.php
+++ Modules/Test/classes/class.ilObjTestGUI.php
@@ -2317,6 +2317,9 @@ class ilObjTestGUI extends ilObjectGUI
 		global $ilAccess, $ilTabs;
 
 		$ilTabs->activateTab('assQuestions');
+		
+		// #12590
+		$this->ctrl->setParameter($this, 'test_express_mode', '');
 
 		if (!$ilAccess->checkAccess("write", "", $this->ref_id)) 
 		{
