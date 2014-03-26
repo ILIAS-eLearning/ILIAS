84030ec32e7d93b800080f6c76da86232ca4d4ab
Michael Jansen <mjansen@databay.de>
2014-03-26 10:50:41 +0000

    Fixed mantis bug report 12614 (merged from 4.4.x branch)
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/Release_4_3_x_branch@49001 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/TestQuestionPool/classes/class.ilObjQuestionPoolGUI.php
+++ Modules/TestQuestionPool/classes/class.ilObjQuestionPoolGUI.php
@@ -375,10 +375,11 @@ class ilObjQuestionPoolGUI extends ilObjectGUI
 	*/
 	function uploadQplObject($questions_only = false)
 	{
+		$this->ctrl->setParameter($this, 'new_type', $_REQUEST['new_type']);
 		if ($_FILES["xmldoc"]["error"] > UPLOAD_ERR_OK)
 		{
-			ilUtil::sendFailure($this->lng->txt("error_upload"));
-			$this->importObject();
+			ilUtil::sendFailure($this->lng->txt("error_upload"), true);
+			$this->ctrl->redirect($this, 'create');
 			return;
 		}
 		// create import directory
@@ -421,8 +422,8 @@ class ilObjQuestionPoolGUI extends ilObjectGUI
 			// delete import directory
 			ilUtil::delDir($basedir);
 
-			ilUtil::sendInfo($this->lng->txt("qpl_import_no_items"));
-			$this->importObject();
+			ilUtil::sendFailure($this->lng->txt("qpl_import_no_items"), true);
+			$this->ctrl->redirect($this, 'create');
 			return;
 		}
 		
@@ -445,8 +446,8 @@ class ilObjQuestionPoolGUI extends ilObjectGUI
 			// delete import directory
 			ilUtil::delDir($basedir);
 
-			ilUtil::sendInfo($this->lng->txt("qpl_import_non_ilias_files"));
-			$this->importObject();
+			ilUtil::sendFailure($this->lng->txt("qpl_import_non_ilias_files"), true);
+			$this->ctrl->redirect($this, 'create');
 			return;
 		}
 		
