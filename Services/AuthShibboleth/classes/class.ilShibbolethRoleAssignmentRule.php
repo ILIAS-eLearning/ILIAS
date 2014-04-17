d38e03daf0ae254f57ccd4447283ce5b645d788b
Fabian Schmid <fs@studer-raimann.ch>
2014-04-17 07:46:19 +0000

    Shibboleth Fix http://ilias.de/mantis/view.php?id=12921
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/Release_4_3_x_branch@49456 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/AuthShibboleth/classes/class.ilShibbolethRoleAssignmentRule.php
+++ Services/AuthShibboleth/classes/class.ilShibbolethRoleAssignmentRule.php
@@ -258,8 +258,7 @@ class ilShibbolethRoleAssignmentRule {
 	 * @return bool
 	 */
 	public function delete() {
-		$query = 'DELETE FROM ' . self::TABLE_NAME . ' '
-			. 'WHERE rule_id = ' . $this->db->quote($this->getRuleId(), 'integer');
+		$query = 'DELETE FROM ' . self::TABLE_NAME . ' ' . 'WHERE rule_id = ' . $this->db->quote($this->getRuleId(), 'integer');
 		$this->db->manipulate($query);
 
 		return true;
@@ -271,12 +270,10 @@ class ilShibbolethRoleAssignmentRule {
 	 */
 	public function add() {
 		$next_id = $this->db->nextId(self::TABLE_NAME);
-		$query = 'INSERT INTO ' . self::TABLE_NAME
-			. ' (rule_id,role_id,name,value,plugin,plugin_id,add_on_update,remove_on_update ) ' . 'VALUES( '
+		$query = 'INSERT INTO ' . self::TABLE_NAME . ' (rule_id,role_id,name,value,plugin,plugin_id,add_on_update,remove_on_update ) ' . 'VALUES( '
 			. $this->db->quote($next_id, 'integer') . ', ' . $this->db->quote($this->getRoleId(), 'integer') . ', '
 			. $this->db->quote($this->getName(), 'text') . ', ' . $this->db->quote($this->getValue(), 'text') . ', '
-			. $this->db->quote((int)$this->isPluginActive(), 'integer') . ', '
-			. $this->db->quote((int)$this->getPluginId(), 'integer') . ', '
+			. $this->db->quote((int)$this->isPluginActive(), 'integer') . ', ' . $this->db->quote((int)$this->getPluginId(), 'integer') . ', '
 			. $this->db->quote((int)$this->isAddOnUpdateEnabled(), 'integer') . ', '
 			. $this->db->quote((int)$this->isRemoveOnUpdateEnabled(), 'integer') . ') ';
 		$this->db->manipulate($query);
@@ -290,9 +287,8 @@ class ilShibbolethRoleAssignmentRule {
 	 * @return bool
 	 */
 	public function update() {
-		$query = 'UPDATE ' . self::TABLE_NAME . ' ' . 'SET role_id = ' . $this->db->quote($this->getRoleId(), 'integer')
-			. ', ' . 'name = ' . $this->db->quote($this->getName(), 'text') . ', ' . 'value = '
-			. $this->db->quote($this->getValue(), 'text') . ', ' . 'plugin = '
+		$query = 'UPDATE ' . self::TABLE_NAME . ' ' . 'SET role_id = ' . $this->db->quote($this->getRoleId(), 'integer') . ', ' . 'name = '
+			. $this->db->quote($this->getName(), 'text') . ', ' . 'value = ' . $this->db->quote($this->getValue(), 'text') . ', ' . 'plugin = '
 			. $this->db->quote((int)$this->isPluginActive(), 'integer') . ', ' . 'plugin_id = '
 			. $this->db->quote((int)$this->getPluginId(), 'integer') . ', ' . 'add_on_update = '
 			. $this->db->quote((int)$this->isAddOnUpdateEnabled(), 'integer') . ', ' . 'remove_on_update = '
@@ -337,7 +333,8 @@ class ilShibbolethRoleAssignmentRule {
 		$pattern = str_replace('*', '.*?', $a_str1);
 		$GLOBALS['ilLog']->write(__METHOD__ . ': Replace pattern:' . $pattern . ' => ' . $a_str2);
 
-		return (bool)preg_match('/^' . $pattern . '$/i', $a_str2);
+		return (bool)preg_match("/" . $pattern . "/us", $a_str2);
+		//		return (bool)preg_match('/^' . $pattern . '$/i', $a_str2);
 	}
 
 
@@ -348,8 +345,7 @@ class ilShibbolethRoleAssignmentRule {
 		if (! $this->getRuleId()) {
 			return true;
 		}
-		$query = 'SELECT * FROM ' . self::TABLE_NAME . ' '
-			. 'WHERE rule_id = ' . $this->db->quote($this->getRuleId(), 'integer');
+		$query = 'SELECT * FROM ' . self::TABLE_NAME . ' ' . 'WHERE rule_id = ' . $this->db->quote($this->getRuleId(), 'integer');
 		$res = $this->db->query($query);
 		while ($row = $res->fetchRow(DB_FETCHMODE_OBJECT)) {
 			$this->setRoleId($row->role_id);
