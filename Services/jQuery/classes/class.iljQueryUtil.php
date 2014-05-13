e18491863bea470964183e981d35a680e2b3fa7f
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2014-05-13 14:05:07 +0000

    jQuery: 1.11.1
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@50008 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/jQuery/classes/class.iljQueryUtil.php
+++ Services/jQuery/classes/class.iljQueryUtil.php
@@ -9,7 +9,7 @@
  */
 class iljQueryUtil
 {		
-	private static $ver = "1_11_0"; 
+	private static $ver = "1_11_1"; 
 	private static $ui_ver = "1_10_4";
 	private static $maphilight_ver = "14_03_20";
 	private static $min = "-min";
