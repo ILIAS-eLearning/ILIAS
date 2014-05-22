6b2060e61e0021d163d89162c371d2cfb6f0995b
Richard Klees <richard.klees@concepts-and-training.de>
2014-05-22 13:14:39 +0000

    gev: Missing utils file.
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali@50264 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ Services/GEV/Utils/classes/class.gevObjectUtils.php
@@ -0,0 +1,25 @@
+<?php
+
+/* Copyright (c) 1998-2014 ILIAS open source, Extended GPL, see docs/LICENSE */#
+
+/**
+* Utilities for objects.
+*
+* @author	Richard Klees <richard.klees@concepts-and-training.de>
+* @version	$Id$
+*/
+
+class gevObjectUtils {
+	static protected $instances = array();
+
+	static public function getRefId($a_obj_id) {
+		global $ilDB;
+		$res = $ilDB->query("SELECT ref_id FROM object_reference WHERE obj_id = ".$ilDB->quote($a_obj_id));
+		if ($ret = $ilDB->fetchAssoc($res)) {
+			return $ret["ref_id"];
+		}
+		return null;
+	}
+}
+
+?>
