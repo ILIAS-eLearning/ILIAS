e14e1cf2ce4fe94c64106f44b0035b56f12f2430
Richard Klees <richard.klees@concepts-and-training.de>
2014-05-26 13:37:20 +0000

    gev: added org units util.
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali@50347 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ Services/GEV/Utils/classes/class.gevOrgUnitUtils.php
@@ -0,0 +1,18 @@
+<?php
+
+/* Copyright (c) 1998-2014 ILIAS open source, Extended GPL, see docs/LICENSE */#
+
+/**
+* Utilities for AdvancedMetadata of Generali.
+*
+* @author	Richard Klees <richard.klees@concepts-and-training.de>
+* @author	Fabian Kochem <fabian.kochem@concepts-and-training.de>
+* @version	$Id$
+*/
+
+class gevOrgUnitUtils {
+	protected function __construct() {
+	}
+}
+
+?>
