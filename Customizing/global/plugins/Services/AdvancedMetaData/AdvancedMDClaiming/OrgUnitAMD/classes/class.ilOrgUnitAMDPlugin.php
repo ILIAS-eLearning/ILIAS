6c98f3c696873ea905f52c9ae55b7716f3181b6d
Richard Klees <richard.klees@concepts-and-training.de>
2014-05-28 13:05:12 +0000

    gev: amd for orgtypes.
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali@50389 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ Customizing/global/plugins/Services/AdvancedMetaData/AdvancedMDClaiming/OrgUnitAMD/classes/class.ilOrgUnitAMDPlugin.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once("Services/AdvancedMetaData/classes/class.ilAdvancedMDClaimingPlugin.php");
+
+ 
+/**
+* Advanced MD claiming test plugin
+*
+* @author Jörg Lützenkirchen <luetzenkirchen@leifos.com>
+* @version $Id$
+*/
+class ilOrgUnitAMDPlugin extends ilAdvancedMDClaimingPlugin
+{
+	public function getPluginName()
+	{
+		return "OrgUnitAMD";
+	}
+
+	public function checkPermission($a_user_id, $a_context_type, $a_context_id, $a_action_id, $a_action_sub_id)
+	{
+		return false;
+	}
+}
+
+?>
---
+++ Customizing/global/plugins/Services/AdvancedMetaData/AdvancedMDClaiming/OrgUnitAMD/sql/dbupdate.php
@@ -0,0 +1,176 @@
+<#1>
+<?php
+
+require_once("Services/AdvancedMetaData/classes/class.ilAdvancedMDFieldDefinition.php");
+require_once("Services/GEV/Utils/classes/class.gevAMDUtils.php");
+require_once("Services/GEV/Utils/classes/class.gevSettings.php");
+require_once("Services/GEV/Utils/classes/class.gevOrgUnitUtils.php");
+
+$tselect = ilAdvancedMDFieldDefinition::TYPE_SELECT;
+$ttext = ilAdvancedMDFieldDefinition::TYPE_TEXT;
+$tdate = ilAdvancedMDFieldDefinition::TYPE_DATE;
+$tdatetime = ilAdvancedMDFieldDefinition::TYPE_DATETIME;
+$tinteger = ilAdvancedMDFieldDefinition::TYPE_INTEGER;
+$tfloat = ilAdvancedMDFieldDefinition::TYPE_FLOAT;
+$tlocation = ilAdvancedMDFieldDefinition::TYPE_LOCATION;
+$tmultiselect = ilAdvancedMDFieldDefinition::TYPE_MULTI_SELECT;
+
+$gev_set = gevSettings::getInstance();
+
+$records_org = 
+array( "Adresse"
+		=> 	array( "Adressdaten der Organisationseinheit", 
+			array( "Straße" => 
+						array( gevSettings::ORG_AMD_STREET			# 0 to save in settings
+							 , null									# 1 description
+							 , true 								# 2 searchable
+							 , null 								# 3 definition
+							 , $ttext 								# 4 type
+							 )
+				 , "Hausnummer" =>
+				 		array( gevSettings::ORG_AMD_HOUSE_NUMBER
+				 			 , null
+				 			 , true
+				 			 , null
+				 			 , $ttext
+				 			 )
+				 , "Postleitzahl" =>
+				 		array( gevSettings::ORG_AMD_ZIPCODE
+				 			 , null
+				 			 , true
+				 			 , null
+				 			 , $ttext
+				 			 )
+				 , "Stadt" =>
+				 		array( gevSettings::ORG_AMD_ZIPCODE
+				 			 , null
+				 			 , true
+				 			 , null
+				 			 , $ttext
+				 			 )
+				 ))
+	 , "Kontaktdaten" 
+		=> array( "Ansprechpartner in der Organisationseinheit",
+		   array( "Name" =>
+				 		array( gevSettings::ORG_AMD_CONTACT_NAME
+				 			 , null
+				 			 , true
+				 			 , null
+				 			 , $ttext
+				 			 )
+				, "Telefon" =>
+						array( gevSettings::ORG_AMD_CONTACT_PHONE
+							 , null
+							 , true
+							 , null
+							 , $ttext
+							 )
+				, "Fax" =>
+						array( gevSettings::ORG_AMD_CONTACT_FAX
+							 , null
+							 , true
+							 , null
+							 , $ttext
+							 )
+				, "E-Mail" =>
+						array( gevSettings::ORG_AMD_CONTACT_EMAIL
+							 , null
+							 , true
+							 , null
+							 , $ttext
+							 )
+				, "Homepage" =>
+						array( gevSettings::ORG_AMD_HOMEPAGE
+							 , null
+							 , true
+							 , null
+							 , $ttext
+							 )
+				))
+	);
+
+$records_venue = 
+array("Ort"
+	 	=> array(null,
+	 	   array( "Ort" =>
+	 	   				array( gevSettings::VENUE_AMD_LOCATION
+	 	   					 , null
+	 	   					 , false
+	 	   					 , null
+	 	   					 , $tlocation
+	 	   					 )
+	 	   		))
+	 , "Preise"
+		=> array( "Übernachtungs- und Verpflegungspreise",
+		   array( "Kosten je Übernachtung" => 
+		   				array( gevSettings::VENUE_AMD_COSTS_PER_ACCOM
+		   					 , null
+		   					 , true
+		   					 , array( "min" => 0
+		   					 		, "decimals" => 2
+		   					 		)
+		   					 , $tfloat
+		   					 )
+				, "Pauschale Frühstück" => 
+		   				array( gevSettings::VENUE_AMD_COSTS_BREAKFAST
+		   					 , null
+		   					 , true
+		   					 , array( "min" => 0
+		   					 		, "decimals" => 2
+		   					 		)
+		   					 , $tfloat
+		   					 )
+				, "Pauschale Mittagessen" => 
+		   				array( gevSettings::VENUE_AMD_COSTS_LUNCH
+		   					 , null
+		   					 , true
+		   					 , array( "min" => 0
+		   					 		, "decimals" => 2
+		   					 		)
+		   					 , $tfloat
+		   					 )
+				, "Nachmittagspauschale" => 
+		   				array( gevSettings::VENUE_AMD_COSTS_COFFEE
+		   					 , null
+		   					 , true
+		   					 , array( "min" => 0
+		   					 		, "decimals" => 2
+		   					 		)
+		   					 , $tfloat
+		   					 )
+				, "Pauschale Abendessen" => 
+		   				array( gevSettings::VENUE_AMD_COSTS_DINNER
+		   					 , null
+		   					 , true
+		   					 , array( "min" => 0
+		   					 		, "decimals" => 2
+		   					 		)
+		   					 , $tfloat
+		   					 )
+				, "Pauschale Tagesverpflegung" => 
+		   				array( gevSettings::VENUE_AMD_COSTS_FOOD
+		   					 , null
+		   					 , true
+		   					 , array( "min" => 0
+		   					 		, "decimals" => 2
+		   					 		)
+		   					 , $tfloat
+		   					 )
+		   ))
+	);
+
+$records_org_ids = gevAMDUtils::createAMDRecords($records_org, array(array("orgu", "orgu_type")));
+$records_venue_ids = gevAMDUtils::createAMDRecords($records_venue, array(array("orgu", "orgu_type")));
+
+require_once("Customizing/global/plugins/Modules/OrgUnit/OrgUnitTypeHook/GEVOrgTypes/classes/class.ilGEVOrgTypesPlugin.php");
+
+// This is hacky!
+ilGEVOrgTypesPlugin::$allow = true;
+
+gevOrgUnitUtils::assignAMDRecordsToOrgUnitType(gevSettings::ORG_TYPE_VENUE, $records_org_ids);
+gevOrgUnitUtils::assignAMDRecordsToOrgUnitType(gevSettings::ORG_TYPE_VENUE, $records_venue_ids);
+gevOrgUnitUtils::assignAMDRecordsToOrgUnitType(gevSettings::ORG_TYPE_PROVIDER, $records_org_ids);
+gevOrgUnitUtils::assignAMDRecordsToOrgUnitType(gevSettings::ORG_TYPE_DEFAULT, $records_org_ids);
+
+ilGEVOrgTypesPlugin::$allow = false;
+?>
