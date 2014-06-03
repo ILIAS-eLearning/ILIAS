8a76527bd0470b990d49014eeaf43678fb4bec96
Björn Heyser <bheyser@databay.de>
2014-06-03 11:52:47 +0000

    merged r50498 from trunk: tst: fixed escaped html in presentation of user solution without making xss possible (-> purifier instead if entities)
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/Release_4_4_x_branch@50499 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/TestQuestionPool/classes/class.assQuestion.php
+++ Modules/TestQuestionPool/classes/class.assQuestion.php
@@ -4187,4 +4187,13 @@ abstract class assQuestion
 			$listener->notifyQuestionDeleted($this);
 		}
 	}
+
+	/**
+	 * @return ilAssHtmlUserSolutionPurifier
+	 */
+	public function getHtmlUserSolutionPurifier()
+	{
+		require_once 'Services/Html/classes/class.ilHtmlPurifierFactory.php';
+		return ilHtmlPurifierFactory::_getInstanceByType('qpl_usersolution');
+	}
 }
