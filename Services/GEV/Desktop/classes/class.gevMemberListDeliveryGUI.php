d532e9d8b1fc2041f3e0088d7ce12135cbe9432a
Richard Klees <richard.klees@concepts-and-training.de>
2014-06-12 12:08:51 +0000

    gev: forgot to add base class for member list delivery.
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali@50703 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ Services/GEV/Desktop/classes/class.gevMemberListDeliveryGUI.php
@@ -0,0 +1,41 @@
+<?php
+
+
+class gevMemberListDeliveryGUI {
+	public function __construct() {
+		global $ilCtrl, $ilAccess;
+		
+		$this->ctrl = &$ilCtrl;
+		$this->access = &$ilAccess;
+	}
+	
+	public function executeCommand() {
+		require_once("Services/GEV/Utils/classes/class.gevCourseUtils.php");
+		require_once("Services/GEV/Utils/classes/class.gevObjectUtils.php");
+
+		$cmd = $this->ctrl->getCmd();
+		$ref_id = intval($_GET["ref_id"]);
+		$obj_id = gevObjectUtils::getObjId($ref_id);
+		
+		if (!$this->access->checkAccess("write", "", $ref_id, "crs", $obj_id)) {
+			$this->ctrl->redirectByClass("gevDesktopGUI");
+			return;
+		}
+		
+		$utils = gevCourseUtils::getInstance($obj_id);
+		
+		switch($cmd) {
+			case "hotel":
+				$utils->deliverMemberList(true);
+				return;
+			case "trainer":
+				$utils->deliverMemberList(false);
+				return;
+			default:
+				$this->ctrl->redirectByClass("gevDesktopGUI");
+				return;
+		}
+	}
+}
+
+?>
