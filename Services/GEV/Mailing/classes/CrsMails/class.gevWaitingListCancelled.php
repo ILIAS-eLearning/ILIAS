8e11631debf6833407634c09784ee850e5fea386
Richard Klees <richard.klees@concepts-and-training.de>
2014-06-18 07:59:27 +0000

    gev: forgot to add auto mail file.
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali@50808 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ Services/GEV/Mailing/classes/CrsMails/class.gevWaitingListCancelled.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once("Services/GEV/Mailing/classes/class.gevCrsAutoMail.php");
+
+class gevWaitingListCancelled extends gevCrsAutoMail {
+	public function getTitle() {
+		return "Info Teilnehmer";
+	}
+	
+	public function _getDescription() {
+		return "Warteliste wird automatisch abgesagt";
+	}
+	
+	public function getScheduledFor() {
+		return null;
+	}
+	
+	public function getTemplateCategory() {
+		return "C10";
+	}
+	
+	public function getRecipientUserIDs() {
+		return $this->getCourseCancelledWithoutCostsMembers();
+	}
+	
+	public function getCC($a_recipient) {
+		return array();
+	}
+}
+
+?>
