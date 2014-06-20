a18715d0c23710d1d1ce088a8e692948f68fdcba
Richard Klees <richard.klees@concepts-and-training.de>
2014-06-20 08:10:23 +0000

    gev: typo in mail placeholder endzeit.
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali@50838 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/GEV/Mailing/classes/class.gevCrsMailTypeAdapter.php
+++ Services/GEV/Mailing/classes/class.gevCrsMailTypeAdapter.php
@@ -37,7 +37,7 @@ class gevCrsMailTypeAdapter extends ilMailTypeAdapter {
 				, array("Startdatum"				, "Beginndatum des Trainings")
 				, array("Startzeit"					, "Uhrzeit des Beginns des Trainings")
 				, array("Enddatum"					, "Enddatum des Trainings")
-				, array("Endzeit"					, "Uhrzteit des Ende des Trainings")
+				, array("Endzeit"					, "Uhrzeit des Ende des Trainings")
 				, array("TV-Name"					, "Name des Themenverantwortlichen des Trainings")
 				, array("TV-Telefon"				, "Telefonnummer des Themenverantwortlichen")
 				, array("TV-Email"					, "Emailadresse des Themenverantwortlichen")
