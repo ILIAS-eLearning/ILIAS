bc85cfa0bb6ee1100a89e0ab1864a60dec25c2eb
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2014-07-11 14:53:03 +0000

    #13515 - error page: link to repository
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@51368 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- error.php
+++ error.php
@@ -6,6 +6,14 @@ ilInitialisation::initILIAS();
 
 $tpl->addBlockFile("CONTENT", "content", "tpl.error.html");
 
+// #13515 - link back to "system" [see ilWebAccessChecker::sendError()]
+$nd = $tree->getNodeData(ROOT_FOLDER_ID);
+$txt = $nd['title'] == 'ILIAS' ? $lng->txt('repository') : $nd['title'];
+$tpl->SetCurrentBlock("ErrorLink");
+$tpl->SetVariable("TXT_LINK", $txt);
+$tpl->SetVariable("LINK", ILIAS_HTTP_PATH. '/ilias.php?baseClass=ilRepositoryGUI&amp;client_id='.CLIENT_ID);
+$tpl->ParseCurrentBlock();
+
 $tpl->setCurrentBlock("content");
 $tpl->setVariable("ERROR_MESSAGE",($_SESSION["failure"]));
 $tpl->setVariable("SRC_IMAGE", ilUtil::getImagePath("mess_failure.png"));
