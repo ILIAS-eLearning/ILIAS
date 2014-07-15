845d702bffd5c67cc578f735707af615d759ff68
Nadia Ahmad <nahmad@databay.de>
2014-07-15 09:10:51 +0000

    Bugfix 13570 activate/deactivate forum notification
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@51434 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Forum/classes/class.ilObjForumGUI.php
+++ Modules/Forum/classes/class.ilObjForumGUI.php
@@ -760,20 +760,24 @@ class ilObjForumGUI extends ilObjectGUI implements ilDesktopItemHandling
 
 		$ilTabs->setTabActive('settings');
 		$ilTabs->addSubTabTarget('basic_settings', $this->ctrl->getLinkTarget($this, 'edit'), 'edit', get_class($this), '', $_GET['cmd']=='edit'? true : false );
-		// member tab
-		// check if there a parent-node is a grp or crs
-		$grp_ref_id = $tree->checkForParentType($this->object->getRefId(), 'grp');
-		$crs_ref_id = $tree->checkForParentType($this->object->getRefId(), 'crs');
 
-		if((int)$grp_ref_id > 0 || (int)$crs_ref_id > 0 )
+		// notification tab
+		if($this->ilias->getSetting('forum_notification') > 0)
 		{
-			#show member-tab for notification if forum-notification is enabled in administration
-			if($ilAccess->checkAccess('edit_permission', '', $this->ref_id) && $this->ilias->getSetting('forum_notification') > 0)
+			// check if there a parent-node is a grp or crs
+			$grp_ref_id = $tree->checkForParentType($this->object->getRefId(), 'grp');
+			$crs_ref_id = $tree->checkForParentType($this->object->getRefId(), 'crs');
+	
+			if((int)$grp_ref_id > 0 || (int)$crs_ref_id > 0 )
 			{
-				$mem_active = array('showMembers', 'forums_notification_settings');
-				(in_array($_GET['cmd'],$mem_active)) ? $force_mem_active = true : $force_mem_active = false;
-
-				$ilTabs->addSubTabTarget('notifications', $this->ctrl->getLinkTarget($this, 'showMembers'), $_GET['cmd'], get_class($this), '', $force_mem_active);
+				#show member-tab for notification if forum-notification is enabled in administration
+				if($ilAccess->checkAccess('write', '', $this->ref_id))
+				{
+					$mem_active = array('showMembers', 'forums_notification_settings');
+					(in_array($_GET['cmd'],$mem_active)) ? $force_mem_active = true : $force_mem_active = false;
+	
+					$ilTabs->addSubTabTarget('notifications', $this->ctrl->getLinkTarget($this, 'showMembers'), $_GET['cmd'], get_class($this), '', $force_mem_active);
+				}
 			}
 		}
 		return true;
@@ -4215,13 +4219,7 @@ class ilObjForumGUI extends ilObjectGUI implements ilDesktopItemHandling
 
 		if($lg instanceof ilObjForumListGUI)
 		{
-			include_once 'Modules/Forum/classes/class.ilForumNotification.php';
-			$frm_noti = new ilForumNotification((int) $_GET['ref_id']);
-			$frm_noti->setUserId($ilUser->getId());
-			$user_toggle = (int)$frm_noti->isUserToggleNotification();
-			$admin_forced_noti = (int)$frm_noti->isAdminForceNotification();
-			
-			$is_user_allowed_to_deactivate_notification = ($admin_forced_noti == 1 && $user_toggle == 0);
+			$is_user_allowed_to_deactivate_notification = $this->isUserAllowedToDeactivateNotification();
 			
 			// Notification button
 			$frm_notificiation_enabled = false;
@@ -4292,6 +4290,37 @@ class ilObjForumGUI extends ilObjectGUI implements ilDesktopItemHandling
 		}
 
 		return $lg;
+	}
+	
+	public function isUserAllowedToDeactivateNotification()
+	{
+		if($this->objProperties->getNotificationType() == 'default')
+		{
+			return true;
+		}
+		
+		if($this->objProperties->isUserToggleNoti() ==  0)
+		{
+			return true;
+		}
+		
+		if($this->isParentObjectCrsOrGrp());
+		{	
+			global $ilUser;
+
+			include_once 'Modules/Forum/classes/class.ilForumNotification.php';
+
+			$frm_noti = new ilForumNotification((int) $_GET['ref_id']);
+			$frm_noti->setUserId($ilUser->getId());
+			
+			$user_toggle = (int)$frm_noti->isUserToggleNotification();
+			if($user_toggle == 0) 
+			{	
+				return true;
+			}
+		}
+		
+		return false;
 	}
 	
 	private function isParentObjectCrsOrGrp()
