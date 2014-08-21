6aebe0373d3de588f91a9e6d4f026967eb12573f
nhaagen <nhaagen>
2014-08-21 16:26:59 +0000

    countdown
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali@52483 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- index.php
+++ index.php
@@ -13,10 +13,11 @@
 
 
 include('./_launchDate.php');
-if( $delta > 0 
-	&& $_SERVER['SERVER_NAME'] != 'generali.test.cat06.de' 
+if( 
+	($delta > 0 
+	&& $_SERVER['SERVER_NAME'] != 'generali.test.cat06.de')
 	//&& $_SERVER['SERVER_NAME'] != 'localhost' 
-	&& $_GET['countdown'] != 1 
+	|| $_GET['countdown'] != 1 
 	){
     header('Location: ./countdown.php');
     die();
