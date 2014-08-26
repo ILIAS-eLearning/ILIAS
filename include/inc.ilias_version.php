966bb7e32a40b55193dd6103b0b87c11aa59db32
Alexander Killing <killing@leifos.de>
2014-08-26 06:34:56 +0000

    update langs, templates and version number
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/Release_4_3_x_branch@52601 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- include/inc.ilias_version.php
+++ include/inc.ilias_version.php
@@ -9,6 +9,6 @@
 *
 * @package ilias-core
 */
-define("ILIAS_VERSION", "4.3.8 2014-07-14");
-define("ILIAS_VERSION_NUMERIC", "4.3.8");			// must be always x.y.z: x, y and z are numbers
+define("ILIAS_VERSION", "4.3.9 2014-08-26");
+define("ILIAS_VERSION_NUMERIC", "4.3.9");			// must be always x.y.z: x, y and z are numbers
 ?>
