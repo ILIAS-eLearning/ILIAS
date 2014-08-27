32d2861f38cf1df990cb7e9b0d685ea988e33584
nhaagen <nhaagen>
2014-08-27 10:12:14 +0000

    Report AtttendanceByuser
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali@52690 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/GEV/Reports/classes/class.gevAttendanceByEmployeeGUI.php
+++ Services/GEV/Reports/classes/class.gevAttendanceByEmployeeGUI.php
@@ -135,106 +135,7 @@ class gevAttendanceByEmployeeGUI {
 	}
 	*/
 
-/*
-	protected function renderAcademyPoints($tpl) {
-		require_once("Services/Calendar/classes/class.ilDatePresentation.php");
-		
-		$tpl->setVariable("ACADEMY_SUM_TITLE", $this->lng->txt("gev_points_in_academy"));
-		$tpl->setVariable("ACADEMY_SUM_FIVE_YEAR_TITLE", $this->lng->txt("gev_points_in_five_years"));
-		
-		$start_date = $this->start_date->get(IL_CAL_FKT_GETDATE);
-		$fy_start = new ilDate($start_date["year"]."-01-01", IL_CAL_DATE); 
-		$fy_end = new ilDate($start_date["year"]."-12-31", IL_CAL_DATE);
-		$fy_end->increment(ilDateTime::YEAR, 4);
 
-		$tpl->setVariable("ACADEMY_FIVE_YEAR", ilDatePresentation::formatPeriod($fy_start, $fy_end));
-	
-		$query = $this->academyQuery($this->start_date, $this->end_date);
-		$res = $this->db->query($query);
-		if ($rec = $this->db->fetchAssoc($res)) {
-			$tpl->setVariable("ACADEMY_SUM", $rec["sum"] ? $rec["sum"] : 0);
-		}
-	
-		$query = $this->academyQuery($fy_start, $fy_end);
-		$res = $this->db->query($query);
-		if ($rec = $this->db->fetchAssoc($res)) {
-			$tpl->setVariable("ACADEMY_SUM_FIVE_YEAR", $rec["sum"] ? $rec["sum"] : 0);
-		}
-	}
-	
-	protected function academyQuery(ilDate $start, ilDate $end) {
-		return   "SELECT SUM(usrcrs.credit_points) sum "
-				.$this->queryFrom()
-				.$this->queryWhere($start, $end)
-				." AND usrcrs.participation_status = 'teilgenommen'"
-				." AND crs.crs_id > 0" // only academy points
-				;
-	}
-	
-	protected function renderWBDPoints($tpl) {
-		require_once("Services/Calendar/classes/class.ilDatePresentation.php");
-		$user_utils = gevUserUtils::getInstance($this->target_user_id);
-
-		$tpl->setVariable("WBD_SUM_TITLE", $this->lng->txt("gev_points_in_wbd"));
-		$tpl->setVariable("WBD_SUM_CERT_PERIOD_TITLE", $this->lng->txt("gev_points_in_wbd_cert_period"));
-		$tpl->setVariable("WBD_SUM_CUR_YEAR_TITLE", $this->lng->txt("gev_points_in_wbd_cert_year"));
-		$tpl->setVariable("WBD_SUM_CUR_YEAR_PRED_TITLE", $this->lng->txt("gev_points_at_end_of_cert_year"));
-		
-		$cy_start = $user_utils->getStartOfCurrentCertificationYear();
-		$cy_end = $user_utils->getStartOfCurrentCertificationYear();
-		$cy_end->increment(ilDateTime::YEAR, 1);
-		
-		$cp_start = $user_utils->getStartOfCurrentCertificationPeriod();
-		$cp_end = $user_utils->getStartOfCurrentCertificationPeriod();
-		$cp_end->increment(ilDateTime::YEAR, 5);
-		
-		$tpl->setVariable("WBD_CERT_PERIOD", ilDatePresentation::formatPeriod($cp_start, $cp_end));
-		$tpl->setVariable("WBD_CERT_YEAR", ilDatePresentation::formatPeriod($cy_start, $cy_end));
-		
-		$query = $this->wbdQuery($this->start_date, $this->end_date);
-		$res = $this->db->query($query);
-		if ($rec = $this->db->fetchAssoc($res)) {
-			$tpl->setVariable("WBD_SUM", $rec["sum"] ? $rec["sum"] : 0);
-		}
-		
-		$query = $this->wbdQuery($cy_start, $cy_end);
-		$res = $this->db->query($query);
-		if ($rec = $this->db->fetchAssoc($res)) {
-			$tpl->setVariable("WBD_SUM_CUR_YEAR", $rec["sum"] ? $rec["sum"] : 0);
-		}
-		
-		$query = $this->wbdQuery($cp_start, $cp_end);
-		$res = $this->db->query($query);
-		if ($rec = $this->db->fetchAssoc($res)) {
-			$tpl->setVariable("WBD_SUM_CERT_PERIOD", $rec["sum"] ? $rec["sum"] : 0);
-		}
-		
-		$query = "SELECT SUM(usrcrs.credit_points) sum "
-				.$this->queryFrom()
-				.$this->queryWhere($cy_start, $cy_end)
-				." AND usrcrs.booking_status = 'gebucht'"
-				." AND ".$this->db->in("usrcrs.participation_status", array("teilgenommen", "nicht gesetzt"), false, "text")
-				." AND (".$this->db->in("usrcrs.okz", array("OKZ1", "OKZ2", "OKZ3"), false, "text")
-				."      OR crs.crs_id < 0 "
-				."     )"
-				;
-		$res = $this->db->query($query);
-		if ($rec = $this->db->fetchAssoc($res)) {
-			$tpl->setVariable("WBD_SUM_CUR_YEAR_PRED", $rec["sum"] ? $rec["sum"] : 0);
-		}
-	}
-	
-	protected function wbdQuery(ilDate $start, ilDate $end) {
-		return   "SELECT SUM(usrcrs.credit_points) sum "
-				.$this->queryFrom()
-				.$this->queryWhere($start, $end)
-				." AND usrcrs.participation_status = 'teilgenommen'"
-				." AND (".$this->db->in("usrcrs.okz", array("OKZ1", "OKZ2", "OKZ3"), false, "text")
-				."      OR crs.crs_id < 0 "
-				."     )"
-				;
-	}
-*/	
 	public function renderTable() {
 		require_once("Services/CaTUIComponents/classes/class.catTableGUI.php");
 		require_once("Services/GEV/Utils/classes/class.gevCourseUtils.php");
@@ -250,8 +151,8 @@ class gevAttendanceByEmployeeGUI {
 		$table_cols = array(
 			array("lastname", "lastname"),
 			array("firstname", "firstname"),
-			array("gdv_id", "gdv_id"),
-			array("gdv_key", "gdv_key"),
+			array("gdv_id", "bwv_id"),
+			array("kennung", "position_key"),
 			array("gender", "gender"),
 			array("org_unit", "org_unit"),
 			array("title", "title"),
@@ -260,7 +161,7 @@ class gevAttendanceByEmployeeGUI {
 			array("gev_provider", "provider"),
 			array("gev_learning_type", "type"),
 			array("booking_status", "booking_status"),
-			array("attdnc_status", "attdnc_status")
+			array("participation_status", "participation_status")
 		);
 
 		$table->addColumn("", "blank", "0px", false);
@@ -272,11 +173,9 @@ class gevAttendanceByEmployeeGUI {
 		
 
 		//get data
-
-
 		$query =	 "SELECT usrcrs.usr_id, usrcrs.crs_id, "
 					."		 usrcrs.booking_status, usrcrs.participation_status, usrcrs.okz, usrcrs.org_unit,"
-					."		 usr.firstname, usr.lastname, usr.gender, "
+					."		 usr.firstname, usr.lastname, usr.gender, usr.bwv_id, usr.position_key,"
 					."		 crs.custom_id, crs.title, crs.type, crs.venue, crs.provider "
 
  					."  FROM hist_usercoursestatus usrcrs "
@@ -290,7 +189,7 @@ class gevAttendanceByEmployeeGUI {
 					."  ORDER BY usr.lastname ASC";
 
 
-		print $query;
+		//print $query;
 
 		$res = $this->db->query($query);
 		
@@ -299,60 +198,10 @@ class gevAttendanceByEmployeeGUI {
 		
 		$data = array();
 		while($rec = $this->db->fetchAssoc($res)) {
-/*			
-			$rec["fee"] = (($rec["bill_id"] != -1 || $user_utils->paysFees())&& $rec["fee"] != -1)
-						? $rec["fee"] = gevCourseUtils::formatFee($rec["fee"])." &euro;"
-						: $rec["fee"] == "-empty-";
-						
-			if ($rec["participation_status"] == "teilgenommen") {
-				$rec["status"] = $this->success_img;
-			}
-			else if (in_array($rec["participation_status"], array("fehlt entschuldigt", "fehlt ohne Absage"))
-				 ||  in_array($rec["booking_status"], array("kostenpflichtig storniert", "kostenfrei storniert"))
-				) {
-				$rec["status"] = $this->failed_img;
-			}
-			else {
-				$rec["status"] = $this->in_progress_img;
-			}
+			/*	
+				modify record-entries here.
+			*/			
 
-			if ($rec["begin_date"] == "0000-00-00" && $rec["end_date"] == "0000-00-00") {
-				$rec["date"] = $no_entry;
-			}
-			else if ($rec["end_date"] == "0000-00-00") {
-				$dt = new ilDate($rec["begin_date"], IL_CAL_DATE);
-				$rec["date"] = $this->lng->txt("gev_from")." ".ilDatePresentation::formatDate($dt);
-			}
-			else if ($rec["begin_date"] == "0000-00-00") {
-				$dt = new ilDate($rec["end_date"], IL_CAL_DATE);
-				$rec["date"] = $this->lng->txt("gev_until")." ".ilDatePresentation::formatDate($dt);
-			}
-			else {
-				$start = new ilDate($rec["begin_date"], IL_CAL_DATE);
-				$end = new ilDate($rec["end_date"], IL_CAL_DATE);
-				$rec["date"] = ilDatePresentation::formatDate($start)." - <br/>".ilDatePresentation::formatDate($end);
-			}
-			
-			$rec["wbd"] = in_array($rec["okz"], array("OKZ1", "OKZ2", "OKZ3"))
-						? $this->lng->txt("yes")
-						: $this->lng->txt("no");
-			
-			$rec["action"] = "";
-			if ($rec["bill_id"] != -1) {
-				$this->ctrl->setParameter($this, "bill_id", $rec["bill_id"]);
-				$this->ctrl->setParameter($this, "target_user_id", $this->target_user_id);
-				$rec["action"] = "<a href='".$this->ctrl->getLinkTarget($this, "getBill")."'>"
-							   . $this->get_bill_img."</a>";
-				$this->ctrl->clearParameters($this);
-			}
-			if ($rec["certificate"] != -1) {
-				$this->ctrl->setParameter($this, "cert_id", $rec["certificate"]);
-				$this->ctrl->setParameter($this, "target_user_id", $this->target_user_id);
-				$rec["action"] .= "<a href='".$this->ctrl->getLinkTarget($this, "getCertificate")."'>"
-							   . $this->get_cert_img."</a>";
-				$this->ctrl->clearParameters($this);
-			}
-*/			
 			foreach ($rec as $key => $value) {
 				if ($value == '-empty-' || $value == -1) {
 					$rec[$key] = $no_entry;
@@ -363,9 +212,11 @@ class gevAttendanceByEmployeeGUI {
 			$data[] = $rec;
 		}
 
-		//print '<hr><pre>';
-		//print_r($data);
-		//print '</pre><hr>';
+		/*
+		print '<hr><pre>';
+		print_r($data);
+		print '</pre><hr>';
+		*/
 
 		$table->setData($data);
 		return $table->getHTML();
