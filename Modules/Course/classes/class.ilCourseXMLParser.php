0d301900caf390167dee77ebdd96ed40c8d1f15e
Fabian Wolf <wolf@leifos.com>
2014-08-27 07:44:06 +0000

    Mantis: 13810 Fixed subscription membership limitation flag for Course Merged from Release_4_3_x_branch:52675
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/Release_4_4_x_branch@52678 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Course/classes/class.ilCourseXMLParser.php
+++ Modules/Course/classes/class.ilCourseXMLParser.php
@@ -271,7 +271,9 @@ class ilCourseXMLParser extends ilMDSaxParser implements ilSaxSubsetParser
 						$this->course_obj->setSubscriptionType(IL_CRS_SUBSCRIPTION_PASSWORD);
 						break;
 				}
+
 				$this->course_obj->setSubscriptionMaxMembers((int) $a_attribs['maxMembers']);
+				$this->course_obj->enableSubscriptionMembershipLimitation($this->course_obj->getSubscriptionMaxMembers() > 0);
 				$this->course_obj->setSubscriptionNotify($a_attribs['notification'] == 'Yes' ? true : false);
 				$this->course_obj->enableWaitingList($a_attribs['waitingList'] == 'Yes' ? true : false);
 				break;
