6230a830c78c6a71d730ab4188e2d1970d3243fe
Stefan Wanzenried <sw@studer-raimann.ch>
2014-08-28 09:45:25 +0000

    Merge rev. 52442, 51003
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@52743 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/OrgUnit/classes/class.ilOrgUnitExplorerGUI.php
+++ Modules/OrgUnit/classes/class.ilOrgUnitExplorerGUI.php
@@ -11,7 +11,16 @@ require_once("./Services/UIComponent/Explorer2/classes/class.ilTreeExplorerGUI.p
 class ilOrgUnitExplorerGUI extends ilTreeExplorerGUI
 {
 
-	protected $stay_with_command = array("", "render", "view", "infoScreen", "showStaff", "performPaste");
+    protected $stay_with_command = array("", "render", "view", "infoScreen", "showStaff", "performPaste");
+
+    /**
+     * Constructor
+     */
+    public function __construct($a_expl_id, $a_parent_obj, $a_parent_cmd, $a_tree)
+    {
+        parent::__construct($a_expl_id, $a_parent_obj, $a_parent_cmd, $a_tree);
+        $this->setAjax(true);
+    }
 
 		public function getNodeContent($node){
 			global $lng;
