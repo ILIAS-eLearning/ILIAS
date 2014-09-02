9c66421a48f9b4b446522291008a3b792068846c
Richard Klees <richard.klees@concepts-and-training.de>
2014-09-02 07:10:54 +0000

    gev: new reminder mail.
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali@52858 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/GEV/Mailing/classes/CrsMails/class.gevReminderParticipants.php
+++ Services/GEV/Mailing/classes/CrsMails/class.gevReminderParticipants.php
@@ -1,8 +1,47 @@
 <?php
 
-require_once("Services/GEV/Mailing/classes/class.gevCrsAutoMail.php");
+require_once("Services/GEV/Mailing/classes/CrsMails/class.gevInvitation.php");
 
-class gevReminderParticipants extends gevCrsAutoMail {
+class gevReminderParticipants extends gevInvitation {
+	const DAYS_BEFORE_COURSE_START = 3;
+
+	public function getTitle() {
+		return "Erinnerung Teilnehmer (Einladungsmail)";
+	}
+
+	public function _getDescription() {
+		return self::DAYS_BEFORE_COURSE_START." Tage vor Trainingsbeginn";
+	}
+
+	public function getScheduledFor() {
+		$date = $this->getCourseUtils()->getStartDate();
+		if ($date) {
+			$date->increment(IL_CAL_DAY, -1 * self::DAYS_BEFORE_COURSE_START);
+		}
+		return $date;
+	}
+
+	public function getMail($a_recipient) {
+		if (!$this->checkUserID($a_recipient)) {
+			throw new Exception("GEV-Invitation-Mails will only work for ILIAS-Users.");
+		}
+		
+		$function = $this->getUserFunction($a_recipient);
+
+		// function will be null if user is not member of the course. Fall back to
+		// standard mail.
+		if ($function === null) {
+			$function = "standard";
+		}
+
+		$mail = $this->getMailFor($function, $a_recipient);
+		
+		$mail["subject"] = "Reminder: ".$mail["subject"];
+		return $mail;
+	}
+}
+
+/*class gevReminderParticipants extends gevCrsAutoMail {
 	const DAYS_BEFORE_COURSE_START = 3;
 	
 	public function getTitle() {
@@ -32,6 +71,6 @@ class gevReminderParticipants extends gevCrsAutoMail {
 	public function getCC($a_recipient) {
 		return array();
 	}
-}
+}*/
 
 ?>
