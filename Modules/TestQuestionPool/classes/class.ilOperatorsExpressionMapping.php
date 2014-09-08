385dbfb796398b0900b1cff76394683d866c7e10
Thomas Joußen <tjoussen@databay.de>
2014-09-08 08:11:10 +0000

    Added operators expression mapping file from 4.4.x optes branch
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@53120 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ Modules/TestQuestionPool/classes/class.ilOperatorsExpressionMapping.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once "./Modules/TestQuestionPool/interfaces/interface.iQuestionCondition.php";
+
+/**
+ * Class ilOperatorsExpressionMapping
+ *
+ * Date: 03.12.13
+ * Time: 13:16
+ * @author Thomas Joußen <tjoussen@databay.de>
+ */ 
+abstract class ilOperatorsExpressionMapping {
+
+	/**
+	 * @var array
+	 */
+	private static $mappings = array(
+		iQuestionCondition::PercentageResultExpression => array("<", "<=", "=", ">=", ">", "<>"),
+		iQuestionCondition::NumericResultExpression => array("<", "<=", "=", ">=", ">", "<>"),
+		iQuestionCondition::StringResultExpression => array("=", "<>"),
+		iQuestionCondition::MatchingResultExpression => array("=", "<>"),
+		iQuestionCondition::OrderingResultExpression => array("=", "<>"),
+	);
+
+	/**
+	 * @param string $expression
+	 *
+	 * @return array
+	 */
+	public static function getOperatorsByExpression($expression)
+	{
+		return self::$mappings[$expression];
+	}
+
+	public static function getAll()
+	{
+		return self::$mappings;
+	}
+}
+ 
