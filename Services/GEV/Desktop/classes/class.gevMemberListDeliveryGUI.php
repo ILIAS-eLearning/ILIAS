6122fdc28378c7e020e6b24270ff5d2be6f5a429
Richard Klees <richard.klees@concepts-and-training.de>
2014-09-12 12:08:53 +0000

    gev: trainers have access to member list of their trainings.
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali@53325 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/GEV/Desktop/classes/class.gevMemberListDeliveryGUI.php
+++ Services/GEV/Desktop/classes/class.gevMemberListDeliveryGUI.php
@@ -3,10 +3,11 @@
 
 class gevMemberListDeliveryGUI {
 	public function __construct() {
-		global $ilCtrl, $ilAccess;
+		global $ilCtrl, $ilAccess, $ilUser;
 		
 		$this->ctrl = &$ilCtrl;
 		$this->access = &$ilAccess;
+		$this->user_id = $ilUser->getId();
 	}
 	
 	public function executeCommand() {
@@ -16,13 +17,14 @@ class gevMemberListDeliveryGUI {
 		$cmd = $this->ctrl->getCmd();
 		$ref_id = intval($_GET["ref_id"]);
 		$obj_id = gevObjectUtils::getObjId($ref_id);
+		$utils = gevCourseUtils::getInstance($obj_id);
 		
-		if (!$this->access->checkAccess("write", "", $ref_id, "crs", $obj_id)) {
+		
+		if (!$this->access->checkAccess("write", "", $ref_id, "crs", $obj_id)
+			&& !$utils->hasTrainer($this->user_id)) {
 			$this->ctrl->redirectByClass("gevDesktopGUI");
 			return;
 		}
-		
-		$utils = gevCourseUtils::getInstance($obj_id);
 		
 		switch($cmd) {
 			case "hotel":
