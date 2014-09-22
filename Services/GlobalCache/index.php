587fa3e94d926dd37239f661586c2e2d39c2c453
Fabian Schmid <fs@studer-raimann.ch>
2014-09-22 09:09:30 +0000

    GlobalCache: Deleted Bedugging-Info
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@53636 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/GlobalCache/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-chdir(strstr(__FILE__, 'Services', true));
-require_once('./include/inc.header.php');
-require_once('./Services/GlobalCache/classes/class.ilGlobalCache.php');
-$ilGlobalCache = ilGlobalCache::getInstance(ilGlobalCache::COMP_CLNG);
-var_dump($ilGlobalCache->isCacheServiceInstallable()); // FSX
-var_dump($ilGlobalCache->isActive()); // FSX
-echo '<pre>' . print_r($ilGlobalCache->getInfo(), 1) . '</pre>';
-?>
