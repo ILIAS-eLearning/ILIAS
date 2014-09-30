a7c133bae3584ffb59a138f7ffeb9ef6ca50080a
nhaagen <nhaagen>
2014-09-30 09:02:29 +0000

    get fragments from deleted courses
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali@53956 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/GEV/debug/gevDebug.php
+++ Services/GEV/debug/gevDebug.php
@@ -62,17 +62,39 @@ class gevDebug {
 			Courses that are in historizing, but not in objects
 		*/
 		$query = "
-			SELECT * 
-			FROM 
+			SELECT * FROM 
 				hist_course 
 			WHERE 
 				crs_id 
 			NOT IN 
 				(
-					SELECT 
-						obj_id 
-					FROM 
-						object_data
+					SELECT obj_id FROM object_data
+				)
+			AND
+				hist_historic=0
+		";
+
+		print $query;
+		$ret = array();
+		$res = $this->db->query($query);
+		while($rec = $this->db->fetchAssoc($res)) {
+			$ret[] = $rec;
+		}
+		return $ret;
+	}
+
+	public function getDeletedCoursesBookings() {
+		/*
+			Courses that are in historizing, but not in objects
+		*/
+		$query = "
+			SELECT * FROM 
+				hist_usercoursestatus
+			WHERE 
+				crs_id 
+			NOT IN 
+				(
+					SELECT obj_id FROM object_data
 				)
 			AND
 				hist_historic=0
@@ -88,15 +110,96 @@ class gevDebug {
 	}
 
 
+	public function getDeletedCoursesOtherFragments() {
+		$ret = array(
+			 'crs_book' => array()
+			,'crs_settings' => array()
+			,'crs_acco' => array()
+			,'crs_waiting_list' => array()
+			//,'crs_items' = array()
+		);
+
+		//crs_book.crs_id, crs_book.user_id
+		//crs_acco.crs_id, crs_acco.user_id,
+		//crs_waiting_list.obj_id, crs_waiting_list.usr_id, 
+		//crs_settings.obj_id
+		//crs_items.parent_id (crs_items.obj_id)
+
+		$queries = array();
+		$queries['crs_book'] = "
+			SELECT * FROM 
+				crs_book
+			WHERE 
+				crs_id 
+			NOT IN 
+				(
+					SELECT obj_id FROM object_data
+				)
+		";
+		$queries['crs_acco'] = "
+			SELECT * FROM 
+				crs_acco
+			WHERE 
+				crs_id 
+			NOT IN 
+				(
+					SELECT obj_id FROM object_data
+				)
+		";
+		$queries['crs_waiting_list'] = "
+			SELECT * FROM 
+				crs_waiting_list
+			WHERE 
+				obj_id 
+			NOT IN 
+				(
+					SELECT obj_id FROM object_data
+				)
+		";
+
+		$queries['crs_settings'] = "
+			SELECT * FROM 
+				crs_settings
+			WHERE 
+				obj_id 
+			NOT IN 
+				(
+					SELECT obj_id FROM object_data
+				)
+		";
+
+
+		foreach ($queries as $table => $query) {
+			print $query;
+			print '<br>';
+			$res = $this->db->query($query);
+			while($rec = $this->db->fetchAssoc($res)) {
+				$ret[$table][] = $rec;
+			}
+			
+		}
+
+		return $ret;
+	}
+
+
 }
 
 
-
-
-
+//crs_book.crs_id, crs_book.user_id
+//crs_settings.obj_id
+//crs_acco.crs_id, crs_acco.user_id,
+//crs_waiting_list.obj_id, crs_waiting_list.usr_id, 
+//crs_items.parent_id (crs_items.obj_id)
 
 $debug = new gevDebug();
-printToTable($debug->getDeletedCourses());
+//printToTable($debug->getDeletedCourses());
+//printToTable($debug->getDeletedCoursesBookings());
+$fragments = $debug->getDeletedCoursesOtherFragments();
+foreach ($fragments as $table => $res) {
+	print "<h1>$table</h1>";
+	printToTable($res);
+}
 
 
 ?>
