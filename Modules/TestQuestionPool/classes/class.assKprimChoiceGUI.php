d6c2376da81afd78815e9cdb3d406733e6ab8344
Björn Heyser <bheyser@databay.de>
2014-10-17 08:47:46 +0000

    kprim: fixed non affecting css margin on question instruction
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@54354 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/TestQuestionPool/classes/class.assKprimChoiceGUI.php
+++ Modules/TestQuestionPool/classes/class.assKprimChoiceGUI.php
@@ -188,19 +188,19 @@ class assKprimChoiceGUI extends assQuestionGUI implements ilGuiQuestionScoringAd
 		}
 		$form->addItem($optionLabel);
 		
-		// points
-		$points = new ilNumberInputGUI($this->lng->txt('points'), 'points');
-		$points->setRequired(true);
-		$points->setSize(3);
-		$points->allowDecimals(true);
-		$points->setMinValue(0);
-		$points->setMinvalueShouldBeGreater(true);
-		$points->setValue($this->object->getPoints());
-		$form->addItem($points);
-		
-		// score partial solution
 		if( !$this->object->getSelfAssessmentEditingMode() )
 		{
+			// points
+			$points = new ilNumberInputGUI($this->lng->txt('points'), 'points');
+			$points->setRequired(true);
+			$points->setSize(3);
+			$points->allowDecimals(true);
+			$points->setMinValue(0);
+			$points->setMinvalueShouldBeGreater(true);
+			$points->setValue($this->object->getPoints());
+			$form->addItem($points);
+
+			// score partial solution
 			$scorePartialSolution = new ilCheckboxInputGUI($this->lng->txt('score_partsol_enabled'), 'score_partsol_enabled');
 			$scorePartialSolution->setInfo($this->lng->txt('score_partsol_enabled_info'));
 			$scorePartialSolution->setChecked( $this->object->isScorePartialSolutionEnabled() );
