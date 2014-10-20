a18d4075f877a13396b6f3352d6c7de96a4c0aef
Fabian Schmid <fs@studer-raimann.ch>
2014-10-20 14:42:24 +0000

    Fix #0012440
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@54401 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/OrgUnit/classes/SimpleUserImport/class.ilOrgUnitSimpleUserImportGUI.php
+++ Modules/OrgUnit/classes/SimpleUserImport/class.ilOrgUnitSimpleUserImportGUI.php
@@ -1,11 +1,13 @@
 <?php
 /* Copyright (c) 1998-2013 ILIAS open source, Extended GPL, see docs/LICENSE */
-require_once("class.ilOrgUnitSimpleUserImport.php");
+require_once('class.ilOrgUnitSimpleUserImport.php');
+
 /**
  * Class ilOrgUnitSimpleUserImportGUI
  *
  * @author            Oskar Truffer <ot@studer-raimann.ch>
  * @author            Martin Studer <ms@studer-raimann.ch>
+ * @author            Fabian Schmid <fs@studer-raimann.ch>
  */
 class ilOrgUnitSimpleUserImportGUI {
 
@@ -43,7 +45,7 @@ class ilOrgUnitSimpleUserImportGUI {
 	 * @param $parent_gui
 	 */
 	function __construct($parent_gui) {
-		global $tpl, $ilCtrl, $ilTabs, $ilToolbar, $lng, $ilAccess;
+		global $tpl, $ilCtrl, $ilToolbar, $lng, $ilAccess, $log;
 		$this->tpl = $tpl;
 		$this->ctrl = $ilCtrl;
 		$this->parent_gui = $parent_gui;
@@ -51,18 +53,19 @@ class ilOrgUnitSimpleUserImportGUI {
 		$this->tabs_gui = $this->parent_gui->tabs_gui;
 		$this->toolbar = $ilToolbar;
 		$this->lng = $lng;
+		$this->ilLog = $log;
 		$this->ilAccess = $ilAccess;
 		$this->lng->loadLanguageModule('user');
-		if (! $this->ilAccess->checkaccess("write", "", $this->parent_gui->object->getRefId())) {
-			ilUtil::sendFailure($this->lng->txt("permission_denied"), true);
+		if (!$this->ilAccess->checkaccess('write', '', $this->parent_gui->object->getRefId())) {
+			ilUtil::sendFailure($this->lng->txt('permission_denied'), true);
 		}
 	}
+
 
 	/**
 	 * @return bool
 	 */
-	public function executeCommand()
-	{
+	public function executeCommand() {
 		$cmd = $this->ctrl->getCmd();
 
 		switch ($cmd) {
@@ -73,8 +76,10 @@ class ilOrgUnitSimpleUserImportGUI {
 				$this->startImport();
 				break;
 		}
+
 		return true;
 	}
+
 
 	public function userImportScreen() {
 		$form = $this->initForm();
@@ -82,57 +87,78 @@ class ilOrgUnitSimpleUserImportGUI {
 	}
 
 
+	/**
+	 * @description FSX Can be deleted; Just for a single Test of a UserImport
+	 */
+	protected function testImport() {
+		return false;
+		$importer = new ilOrgUnitSimpleUserImport();
+		$string = '<Assignment action=\'add\'>
+						<User id_type=\'ilias_login\'>root</User>
+                        <OrgUnit id_type=\'external_id\'>imported_001</OrgUnit>
+						<Role>superior</Role>
+                    </Assignment>';
+
+		$xml = new SimpleXMLElement($string);
+		$importer->simpleUserImportElement($xml);
+		ilUtil::sendInfo('<pre>' . print_r($importer->getErrors(), 1) . '</pre>');
+	}
+
+
 	protected function initForm() {
 		$form = new ilPropertyFormGUI();
-		$input = new ilFileInputGUI($this->lng->txt("import_xml_file"), "import_file");
+		$input = new ilFileInputGUI($this->lng->txt('import_xml_file'), 'import_file');
 		$input->setRequired(true);
 		$form->addItem($input);
 		$form->setFormAction($this->ctrl->getFormAction($this));
-		$form->addCommandButton("startImport", $this->lng->txt("import"));
+		$form->addCommandButton('startImport', $this->lng->txt('import'));
 
 		return $form;
 	}
 
+
 	public function startImport() {
 		$form = $this->initForm();
-		if (! $form->checkInput()) {
+		if (!$form->checkInput()) {
 			$this->tpl->setContent($form->getHTML());
 		} else {
-			$file = $form->getInput("import_file");
+			$file = $form->getInput('import_file');
 			$importer = new ilOrgUnitSimpleUserImport();
 			try {
-				$importer->simpleUserImport($file["tmp_name"]);
+				$importer->simpleUserImport($file['tmp_name']);
 			} catch (Exception $e) {
-				$this->ilLog->wirte($e->getMessage() . " - " . $e->getTraceAsString());
-				ilUtil::sendFailure($lng->txt("import_failed"), true);
-				$this->ctrl->redirect($this, "render");
+				$this->ilLog->write($e->getMessage() . ' - ' . $e->getTraceAsString());
+				ilUtil::sendFailure($this->lng->txt('import_failed'), true);
+				$this->ctrl->redirect($this, 'render');
 			}
 			$this->displayImportResults($importer);
 		}
 	}
 
+
 	/**
 	 * @param $importer ilOrgUnitImporter
 	 */
 	public function displayImportResults($importer) {
-		if (! $importer->hasErrors() && ! $importer->hasWarnings()) {
+		if (!$importer->hasErrors() AND !$importer->hasWarnings()) {
 			$stats = $importer->getStats();
-			ilUtil::sendSuccess(sprintf($this->lng->txt("user_import_successful"), $stats["created"], $stats["removed"]), true);
+			ilUtil::sendSuccess(sprintf($this->lng->txt('user_import_successful'), $stats['created'], $stats['removed']), true);
 		}
 		if ($importer->hasWarnings()) {
-			$msg = $this->lng->txt("import_terminated_with_warnings") . "<br>";
+			$msg = $this->lng->txt('import_terminated_with_warnings') . '<br>';
 			foreach ($importer->getWarnings() as $warning) {
-				$msg .= "-" . $this->lng->txt($warning["lang_var"]) . " (Import ID: " . $warning["import_id"] . ")<br>";
+				$msg .= '-' . $this->lng->txt($warning['lang_var']) . ' (Import ID: ' . $warning['import_id'] . ')<br>';
 			}
 			ilUtil::sendInfo($msg, true);
 		}
 		if ($importer->hasErrors()) {
-			$msg = $this->lng->txt("import_terminated_with_errors") . "<br>";
+			$msg = $this->lng->txt('import_terminated_with_errors') . '<br>';
 			foreach ($importer->getErrors() as $warning) {
-				$msg .= "- " . $this->lng->txt($warning["lang_var"]) . " (Import ID: " . $warning["import_id"] . ")<br>";
+				$msg .= '- ' . $this->lng->txt($warning['lang_var']) . ' (Import ID: ' . $warning['import_id'] . ')<br>';
 			}
 			ilUtil::sendFailure($msg, true);
 		}
 	}
 }
+
 ?>
--- Modules/OrgUnit/classes/class.ilOrgUnitImporter.php
+++ Modules/OrgUnit/classes/class.ilOrgUnitImporter.php
@@ -1,102 +1,135 @@
 <?php
 /* Copyright (c) 1998-2013 ILIAS open source, Extended GPL, see docs/LICENSE */
-require_once("./Services/Export/classes/class.ilXmlImporter.php");
+require_once('./Services/Export/classes/class.ilXmlImporter.php');
+
 /**
  * Class ilOrgUnitImporter
  *
- * @author: Oskar Truffer <ot@studer-raimann.ch>
- * @author: Martin Studer <ms@studer-raimann.ch>
- *
+ * @author  Oskar Truffer <ot@studer-raimann.ch>
+ * @author  Martin Studer <ms@studer-raimann.ch>
+ * @author  Fabian Schmid <fs@studer-raimann.ch>
  */
 class ilOrgUnitImporter extends ilXmlImporter {
 
-	/** @var  array lang_var => language variable, import_id => the reference or import id, depending on the ou_id_type */
+	/**
+	 * @var  array lang_var => language variable, import_id => the reference or import id, depending on the ou_id_type
+	 */
 	public $errors;
-
-	/** @var  array lang_var => language variable, import_id => the reference or import id, depending on the ou_id_type */
+	/**
+	 * @var  array lang_var => language variable, import_id => the reference or import id, depending on the ou_id_type
+	 */
 	public $warnings;
-
-	/** @var array keys in {updated, edited, deleted} */
+	/**
+	 * @var array keys in {updated, edited, deleted}
+	 */
 	public $stats;
 
 
-	protected function buildRef($id, $type){
-		if($type == "reference_id")
-		{
-			if(!ilObjOrgUnit::_exists($id, true))
-			{
+	/**
+	 * @param $id
+	 * @param $type
+	 *
+	 * @return bool|int
+	 */
+	protected function buildRef($id, $type) {
+		if ($type == 'reference_id') {
+			if (!ilObjOrgUnit::_exists($id, true)) {
 				return false;
 			}
+
 			return $id;
-		}
-		elseif($type == "external_id")
-		{
+		} elseif ($type == 'external_id') {
 			$obj_id = ilObject::_lookupObjIdByImportId($id);
-			$ref_ids = ilObject::_getAllReferences($obj_id);
-			if(!count($ref_ids))
-			{
+
+			if (!ilObject::_hasUntrashedReference($id)) {
 				return false;
 			}
-			if(count($ref_ids))
-			{
+
+			$ref_ids = ilObject::_getAllReferences($obj_id);
+			if (!count($ref_ids)) {
+				return false;
+			}
+			if (count($ref_ids)) {
 				return array_shift($ref_ids);
 			}
-		}
-		else
-		{
+		} else {
 			return false;
 		}
 	}
 
-	public function hasErrors(){
-		return count($this->errors)!=0;
+
+	/**
+	 * @return bool
+	 */
+	public function hasErrors() {
+		return count($this->errors) != 0;
 	}
 
-	public function hasWarnings(){
-		return count($this->warnings)!=0;
+
+	/**
+	 * @return bool
+	 */
+	public function hasWarnings() {
+		return count($this->warnings) != 0;
 	}
 
-	public function addWarning($lang_var, $import_id, $action = null){
-		$this->warnings[] = array("lang_var" => $lang_var, "import_id" => $import_id, "action" => $action);
+
+	/**
+	 * @param      $lang_var
+	 * @param      $import_id
+	 * @param null $action
+	 */
+	public function addWarning($lang_var, $import_id, $action = NULL) {
+		$this->warnings[] = array( 'lang_var' => $lang_var, 'import_id' => $import_id, 'action' => $action );
 	}
 
-	public function addError($lang_var, $import_id, $action = null){
-		$this->errors[] = array("lang_var" => $lang_var, "import_id" => $import_id, "action" => $action);
+
+	/**
+	 * @param      $lang_var
+	 * @param      $import_id
+	 * @param null $action
+	 */
+	public function addError($lang_var, $import_id, $action = NULL) {
+		$this->errors[] = array( 'lang_var' => $lang_var, 'import_id' => $import_id, 'action' => $action );
 	}
+
 
 	/**
 	 * @return array
 	 */
-	public function getErrors()
-	{
+	public function getErrors() {
 		return $this->errors;
 	}
 
+
 	/**
 	 * @return array
 	 */
-	public function getWarnings()
-	{
+	public function getWarnings() {
 		return $this->warnings;
 	}
 
+
 	/**
 	 * @return array
 	 */
-	public function getStats()
-	{
+	public function getStats() {
 		return $this->stats;
 	}
 
-	/**
-	 * Import XML
-	 *
-	 * @param
-	 * @return
-	 */
-	function importXmlRepresentation($a_entity, $a_id, $a_xml, $a_mapping)
-	{
 
+	/**
+	 * @param $a_entity
+	 * @param $a_id
+	 * @param $a_xml
+	 * @param $a_mapping
+	 *
+	 * @return string|void
+	 *
+	 * @deprecated
+	 */
+	public function importXmlRepresentation($a_entity, $a_id, $a_xml, $a_mapping) {
 	}
 }
+
 ?>
