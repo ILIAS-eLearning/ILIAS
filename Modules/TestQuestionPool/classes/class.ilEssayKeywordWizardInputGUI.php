aa00e031bee67ad4e96d3cfb385cb7b4b294ad86
Björn Heyser <bheyser@databay.de>
2014-11-05 08:46:08 +0000

    changed icons for ilEssayKeywordWizardInputGUI
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@54807 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/TestQuestionPool/classes/class.ilEssayKeywordWizardInputGUI.php
+++ Modules/TestQuestionPool/classes/class.ilEssayKeywordWizardInputGUI.php
@@ -273,8 +273,8 @@ class ilEssayKeywordWizardInputGUI extends ilSingleChoiceWizardInputGUI
 			{
 				$tpl->setVariable( "DISABLED_POINTS", " disabled=\"disabled\"" );
 			}
-			$tpl->setVariable( "ADD_BUTTON", ilUtil::getImagePath( 'edit_add.png' ) );
-			$tpl->setVariable( "REMOVE_BUTTON", ilUtil::getImagePath( 'edit_remove.png' ) );
+			$tpl->setVariable( "ADD_BUTTON", ilGlyphGUI::get(ilGlyphGUI::ADD) );
+			$tpl->setVariable( "REMOVE_BUTTON", ilGlyphGUI::get(ilGlyphGUI::REMOVE) );
 			$tpl->parseCurrentBlock();
 			$i++;
 		}
