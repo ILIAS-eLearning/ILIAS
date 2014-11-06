41e2aefb0fd2e916a7eb5b66defbbaa74bbaaabc
Björn Heyser <bheyser@databay.de>
2014-11-06 07:24:45 +0000

    merged r54839 from trunk: fixed mantis 0014479: Creating a question with paged-editor: html-tags are shown
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/Release_4_4_x_branch@54841 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Utilities/classes/class.ilUtil.php
+++ Services/Utilities/classes/class.ilUtil.php
@@ -4624,9 +4624,6 @@ class ilUtil
 			$result = str_replace("\\", "&#92;", $result);
 		}
 		
-		// if textarea output is floating anything mantis 14069
-		$result .= '<div style="clear: both;"> </div>';
-		
 		return $result;
 	}
 
