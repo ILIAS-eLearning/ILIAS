e9d2d578eec8da537d3bd2d5cffe10c121997834
Stefan Meyer <smeyer.ilias@gmx.de>
2014-11-25 10:50:35 +0000

    temp explorer image fix
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@55487 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/WebServices/ECS/classes/Mapping/class.ilECSNodeMappingLocalExplorer.php
+++ Services/WebServices/ECS/classes/Mapping/class.ilECSNodeMappingLocalExplorer.php
@@ -180,7 +180,7 @@ class ilECSNodeMappingLocalExplorer extends ilExplorer
 		if ($this->output_icons)
 		{
 			$tpl->setCurrentBlock("icon");
-			$tpl->setVariable("ICON_IMAGE" , $this->getImage("icon_".$a_option["type"]."_s.png", $a_option["type"], $a_obj_id));
+			$tpl->setVariable("ICON_IMAGE" , $this->getImage("icon_".$a_option["type"].".svg", $a_option["type"], $a_obj_id));
 
 			$tpl->setVariable("TARGET_ID" , "iconid_".$a_node_id);
 			$this->iconList[] = "iconid_".$a_node_id;
