0fa04f817721a674a0e0c1f3435b4edce59987be
Michael Jansen <mjansen@databay.de>
2014-12-01 13:55:28 +0000

    Fixed mantis bug report #14467
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/Release_4_4_x_branch@55781 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Test/classes/class.ilObjTest.php
+++ Modules/Test/classes/class.ilObjTest.php
@@ -6898,6 +6898,7 @@ function getAnswerFeedbackPoints()
 		$testQuestionSetConfigFactory->getQuestionSetConfig()->cloneQuestionSetRelatedData($newObj);
 
 		$newObj->saveToDb();
+		$newObj->updateMetaData();// #14467
 		return $newObj;
 	}
 
