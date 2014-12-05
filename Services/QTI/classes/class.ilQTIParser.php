964fe6f507e1159978fb2e9c48f14053e3423a80
Björn Heyser <bheyser@databay.de>
2014-12-05 16:16:33 +0000

    merged r56057 from trunk: fixed mantis #0010757: The QTI <matbreak> element is not supported by ILIAS test import
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/Release_4_3_x_branch@56059 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/QTI/classes/class.ilQTIParser.php
+++ Services/QTI/classes/class.ilQTIParser.php
@@ -1329,6 +1329,13 @@ class ilQTIParser extends ilSaxParser
 				}
 				$this->matimage = NULL;
 				break;
+			// add support for matbreak element
+			case "matbreak":
+				$this->mattext = new ilQTIMattext();
+				$this->mattext->setContent('<br />');
+				$this->material->addMattext($this->mattext);
+				$this->mattext = NULL;
+				break;
 			case "resprocessing":
 				if ($this->item != NULL)
 				{
