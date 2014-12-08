9094fafa3d2b58dc18c9875872354b8e6d88cb6c
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2014-12-08 11:05:22 +0000

    #14922 - survey: import
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@56078 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Survey/classes/class.SurveyImportParser.php
+++ Services/Survey/classes/class.SurveyImportParser.php
@@ -570,13 +570,13 @@ class SurveyImportParser extends ilSaxParser
 						$this->spl_id > 0)
 					{
 						
-						$question_id = $this->activequestion->duplicate(TRUE);
-						$this->survey->addQuestion($question_id);						
+						$question_id = $this->activequestion->duplicate(TRUE);										
 					}
 					else
 					{
 						$question_id = $this->activequestion->getId();
 					}
+					$this->survey->addQuestion($question_id);		
 					$this->questions[$this->original_question_id] = $question_id;
 					$this->activequestion = NULL;
 				}
