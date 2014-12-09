e0310cfbc30f5bca219005a3d20aa6fde6ca3116
Richard Klees <richard.klees@concepts-and-training.de>
2014-12-09 07:32:07 +0000

    Fixed duplicate key in custom copy_mappings. It's odd...
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali2@56096 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/CopyWizard/classes/class.ilCopyWizardOptions.php
+++ Services/CopyWizard/classes/class.ilCopyWizardOptions.php
@@ -489,12 +489,13 @@ class ilCopyWizardOptions
 		
 		// For some unknown reasons the mapping 1 -> 1 is always appended. Just
 		// ignore it...
-		if ($a_target_id !== 1 && $a_source_id !== 1 && $a_target_id != $a_source_id) {
-			$ilDB->insert('copy_mappings', array(
-				'target_ref_id'	=> array('integer', $a_target_id),
-				'source_ref_id'	=> array('integer', $a_source_id)
-			));
-		}
+		$ilDB->manipulate("INSERT INTO copy_mappings (target_ref_id, source_ref_id) "
+						 ."VALUES (".$ilDB->quote($a_target_id, "integer")
+						 ."       ,".$ilDB->quote($a_source_id, "integer")
+						 ."       )"
+						 ." ON DUPLICATE KEY UPDATE "
+						 ."       source_ref_id = ".$ilDB->quote($a_source_id, "integer")
+						 );
 		// gev-patch end
 		
 		return true;				
