f226d010e9da1a96f754ad8a263aff13cec0789c
Richard Klees <richard.klees@concepts-and-training.de>
2014-12-10 07:59:29 +0000

    gev2: fixed #786
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali2@56179 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/GEV/Reports/classes/class.gevEmployeeBookingsGUI.php
+++ Services/GEV/Reports/classes/class.gevEmployeeBookingsGUI.php
@@ -67,6 +67,8 @@ class gevEmployeeBookingsGUI extends catBasicReportGUI{
 							->on("usr.user_id = usrcrs.usr_id AND usr.hist_historic = 0")
 						->join("hist_course crs")
 							->on("crs.crs_id = usrcrs.crs_id AND crs.hist_historic = 0")
+						->join("object_reference oref")
+							->on("crs.crs_id = oref.obj_id AND oref.deleted IS NULL")
 						->compile()
 						;
 
@@ -90,6 +92,14 @@ class gevEmployeeBookingsGUI extends catBasicReportGUI{
 				return null;
 		}
 	}
+	
+	protected function renderView() {
+		if (count($this->getData()) == 0) {
+			return $this->lng->txt("gev_no_employee_bookings");
+		}
+		return parent::renderView();
+	}
+
 
 	protected function transformResultRow($rec) {
 		//date
@@ -130,7 +140,7 @@ class gevEmployeeBookingsGUI extends catBasicReportGUI{
 		$this->loadCourseAndTargetUserId();
 		$this->checkIfUserIsAllowedToCancelCourseForOtherUser();
 		
-		$o_status = $this->crs_utils->getBookingStatusOf($this->target_user);
+		$o_status = $this->crs_utils->getBookingStatusOf($this->target_user_id);
 		
 		if (!$this->crs_utils->cancelBookingOf($this->target_user_id)) {
 			$this->log->write("gevEmployeeBookingsGUI::finalizeCancellation: ".
@@ -139,7 +149,7 @@ class gevEmployeeBookingsGUI extends catBasicReportGUI{
 			return $this->render();
 		}
 		
-		$n_status = $this->crs_utils->getBookingStatusOf($this->target_user);
+		$n_status = $this->crs_utils->getBookingStatusOf($this->target_user_id);
 		
 		if (!in_array($n_status, array(ilCourseBooking::STATUS_CANCELLED_WITH_COSTS
 									  , ilCourseBooking::STATUS_CANCELLED_WITHOUT_COSTS))) {
