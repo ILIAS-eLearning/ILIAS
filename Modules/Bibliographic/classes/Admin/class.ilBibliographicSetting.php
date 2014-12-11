43b4ba5dea456fbe23dbaa6e56df37751334c303
Fabian Schmid <fs@studer-raimann.ch>
2014-12-11 12:59:47 +0000

    Fix Bibl Settings
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@56277 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Bibliographic/classes/Admin/class.ilBibliographicSetting.php
+++ Modules/Bibliographic/classes/Admin/class.ilBibliographicSetting.php
@@ -1,6 +1,8 @@
 <?php
 /* Copyright (c) 1998-2009 ILIAS open source, Extended GPL, see docs/LICENSE */
-require_once('./Services/ActiveRecord/class.ActiveRecord.php');
+if (class_exists('ActiveRecord') != true) {
+	require_once('./Services/ActiveRecord/class.ActiveRecord.php');
+}
 
 /**
  * Class ilBibliographicSetting
