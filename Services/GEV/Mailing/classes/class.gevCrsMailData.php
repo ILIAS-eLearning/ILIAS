2429b38cd33c666ae3ada80203cbcb3f567c4cb7
Richard Klees <richard.klees@concepts-and-training.de>
2014-12-30 16:02:27 +0000

    gev2: new placeholder for course mails.
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali2@56716 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/GEV/Mailing/classes/class.gevCrsMailData.php
+++ Services/GEV/Mailing/classes/class.gevCrsMailData.php
@@ -222,6 +222,8 @@ class gevCrsMailData extends ilMailData {
 			case "VO-TELEFON":
 				$val = $this->crs_utils->getVenuePhone();
 				break;
+			case "VO-INTERNET":
+				$val = $this->crs_utils->getVenueHomepage();
 			case "WEBINAR-LINK":
 				$val = $this->crs_utils->getWebExLink();
 				break;
