b491d1b3d932224f109abac2f331973f3901eb46
Fabian Schmid <fs@studer-raimann.ch>
2015-01-14 16:42:40 +0000

    commit from github
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/Release_4_4_x_branch@57127 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/DataCollection/classes/class.ilDataCollectionDatatype.php
+++ Modules/DataCollection/classes/class.ilDataCollectionDatatype.php
@@ -14,16 +14,16 @@ require_once "./Services/Form/classes/class.ilImageFileInputGUI.php";
 
 
 /**
-* Class ilDataCollectionDatatype
-*
-* @author Martin Studer <ms@studer-raimann.ch>
-* @author Marcel Raimann <mr@studer-raimann.ch>
-* @author Fabian Schmid <fs@studer-raimann.ch>
-* @author Oskar Truffer <ot@studer-raimann.ch>
-* @version $Id: 
-*
-* @ingroup ModulesDataCollection
-*/
+ * Class ilDataCollectionDatatype
+ *
+ * @author Martin Studer <ms@studer-raimann.ch>
+ * @author Marcel Raimann <mr@studer-raimann.ch>
+ * @author Fabian Schmid <fs@studer-raimann.ch>
+ * @author Oskar Truffer <ot@studer-raimann.ch>
+ * @version $Id: 
+ *
+ * @ingroup ModulesDataCollection
+ */
 class ilDataCollectionDatatype
 {
 	protected $id; // [int]
