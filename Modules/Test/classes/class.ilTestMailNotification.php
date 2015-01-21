531cbc37c32922bf0d65b406db7ff103dae94105
Nadia Ahmad <nahmad@databay.de>
2015-01-21 09:30:16 +0000

    0011264: Mysterious Message
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@57274 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ Modules/Test/classes/class.ilTestMailNotification.php
@@ -0,0 +1,93 @@
+<?php
+/* Copyright (c) 1998-2015 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+include_once './Services/Mail/classes/class.ilMailNotification.php';
+
+/**
+ * Class ilTestMailNotification
+ * @author Nadia Ahmad <nahmad@databay.de>
+ *         
+ * @version	$Id:$
+ * @ingroup ModulesTest
+ */
+class ilTestMailNotification extends ilMailNotification
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Send a simple notification to the owner of the test
+	 * 
+	 * @param int $owner_id
+	 * @param string $title
+	 * @param string $usr_data
+	 */
+	public function sendSimpleNotification($owner_id, $title, $usr_data)
+	{
+		$this->initLanguage($owner_id);
+		$this->language->loadLanguageModule('assessment');
+		$this->initMail();
+		$this->setSubject(sprintf($this->language->txt('tst_user_finished_test'), $title));
+		$this->setBody(ilMail::getSalutation($owner_id, $this->getLanguage()));
+		$this->appendBody("\n\n");
+		$this->appendBody($this->language->txt('user_has_finished_a_test'));
+		$this->appendBody("\n\n");
+		
+		$this->appendBody($this->language->txt('title').': '. $title);
+		$this->appendBody("\n");
+		$this->appendBody($this->language->txt('username').': '. $usr_data);
+		$this->appendBody("\n");
+		
+		ilDatePresentation::setUseRelativeDates(false);
+		$this->appendBody($this->language->txt('tst_finished').': '. ilDatePresentation::formatDate(new ilDateTime(time(), IL_CAL_UNIX)));
+		$this->appendBody("\n\n");
+		
+		$this->appendBody($this->language->txt('tst_notification_explanation_admin'));
+		$this->appendBody("\n");
+		$this->getMail()->appendInstallationSignature(true);
+
+		$this->sendMail(array($owner_id),array('system'));
+	}
+
+	/**
+	 * send an advanced notification to the owner of the test
+	 * @param int $owner_id
+	 * @param string $title
+	 * @param sting $usr_data
+	 * @param array $file_names
+	 */
+	public function sendAdvancedNotification($owner_id, $title, $usr_data, $file_names)
+	{
+		$this->initLanguage($owner_id);
+		$this->language->loadLanguageModule('assessment');
+
+		$this->initMail();
+		$this->setSubject(sprintf($this->language->txt('tst_user_finished_test'), $title));
+		$this->setBody(ilMail::getSalutation($owner_id, $this->getLanguage()));
+		$this->appendBody("\n\n");
+		$this->appendBody($this->language->txt('user_has_finished_a_test'));
+		$this->appendBody("\n\n");
+
+		$this->appendBody($this->language->txt('title').': '. $title);
+		$this->appendBody("\n");
+		$this->appendBody($this->language->txt('username').': '. $usr_data);
+		$this->appendBody("\n");
+
+		ilDatePresentation::setUseRelativeDates(false);
+		$this->appendBody($this->language->txt('tst_finished').': '. ilDatePresentation::formatDate(new ilDateTime(time(), IL_CAL_UNIX)));
+		$this->appendBody("\n\n");
+
+		$this->appendBody($this->language->txt('tst_attached_xls_file'));
+		$this->appendBody("\n\n");
+		
+		$this->appendBody($this->language->txt('tst_notification_explanation_admin'));
+		$this->appendBody("\n");
+		
+		$this->setAttachments($file_names);
+		$this->getMail()->appendInstallationSignature(true);
+
+		$this->sendMail(array($owner_id),array('system'));
+	}
+} 
