cf3436f01b25e379b6cfaadcd2da74075526b5e1
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2015-01-28 14:54:15 +0000

    #15334 - LP: percentage
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@57625 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/Tracking/classes/repository_statistics/class.ilLPProgressTableGUI.php
+++ Services/Tracking/classes/repository_statistics/class.ilLPProgressTableGUI.php
@@ -242,7 +242,7 @@ class ilLPProgressTableGUI extends ilLPTableBaseGUI
 			$this->tpl->setVariable("MARK_VALUE", $a_set["mark"]);
 			$this->tpl->setVariable("COMMENT_TEXT", $a_set["comment"]);
 						
-			if(!$this->isPercentageAvailable($a_set["obj_id"]) || (int)$a_set["percentage"] === 0)
+			if(!$this->isPercentageAvailable($a_set["obj_id"]) /* || (int)$a_set["percentage"] === 0 */) // #15334
 			{
 				$this->tpl->setVariable("PERCENTAGE_VALUE", "");
 			}
