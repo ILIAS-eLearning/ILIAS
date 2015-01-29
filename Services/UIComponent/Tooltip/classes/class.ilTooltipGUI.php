cc2977b475fbb296c76bc89b2cc749416e442da3
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2015-01-29 15:00:50 +0000

    tooltip: update to qtip 2.2.1
    git-svn-id: http://svn.ilias.de/svn/ilias/trunk@57650 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/UIComponent/Tooltip/classes/class.ilTooltipGUI.php
+++ Services/UIComponent/Tooltip/classes/class.ilTooltipGUI.php
@@ -82,8 +82,8 @@ class ilTooltipGUI
 
 		//			include_once("./Services/YUI/classes/class.ilYuiUtil.php");
 		//			ilYuiUtil::initTooltip();
-		$tpl->addCss("./Services/UIComponent/Tooltip/lib/qtip_2_2_0/jquery.qtip.min.css");
-		$tpl->addJavascript("./Services/UIComponent/Tooltip/lib/qtip_2_2_0/jquery.qtip.min.js");
+		$tpl->addCss("./Services/UIComponent/Tooltip/lib/qtip_2_2_1/jquery.qtip.min.css");
+		$tpl->addJavascript("./Services/UIComponent/Tooltip/lib/qtip_2_2_1/jquery.qtip.min.js");
 		
 		self::$library_initialized = true;
 	}
