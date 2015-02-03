33affe5130dc6516374b0f2b5daa72548a531534
Richard Klees <richard.klees@concepts-and-training.de>
2015-02-03 15:09:29 +0000

    gev2: fixed bug in training -> tep entry trafo.
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali2@57773 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/TEP/classes/class.ilTEPCourseEntries.php
+++ Services/TEP/classes/class.ilTEPCourseEntries.php
@@ -199,7 +199,8 @@ class ilTEPCourseEntries
 			}
 
 			#990
-			$org_id = $crs_utils->getTEPOrguId();
+			require_once("Services/GEV/Utils/classes/class.gevObjectUtils.php");
+			$org_id = gevObjectUtils::getObjId($crs_utils->getTEPOrguId());
 			if ($org_id != $a_entry->getOrgUnitId()) {
 				$a_entry->setOrgUnitId($org_id);
 				$changed = self::SYNC_UPDATED;
