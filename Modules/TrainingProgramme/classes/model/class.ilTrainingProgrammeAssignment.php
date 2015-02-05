d468f0f7c3ef87b8d4d3d1fe064165c009bfe9ed
Richard Klees <richard.klees@concepts-and-training.de>
2015-02-05 17:02:08 +0100

    tab indentation.

--- Modules/TrainingProgramme/classes/model/class.ilTrainingProgrammeAssignment.php
+++ Modules/TrainingProgramme/classes/model/class.ilTrainingProgrammeAssignment.php
@@ -17,74 +17,74 @@ require_once(dirname(__FILE__)."/../../../../Services/ActiveRecord/class.ActiveR
  */
 
 class ilTrainingProgrammeAssignment extends ActiveRecord {
-    /**
-     * @return string
-     */
-    static function returnDbTableName() {
-        return "prg_usr_assignments";
-    }
+	/**
+	 * @return string
+	 */
+	static function returnDbTableName() {
+		return "prg_usr_assignments";
+	}
 
-    /**
-     * Id of this assignment.
-     *
-     * @var int
-     * 
-     * @con_is_primary  true
-     * @con_is_unique   true
-     * @con_has_field   true
-     * @con_fieldtype   integer
-     * @con_length      4
-     */
-    protected $id;
+	/**
+	 * Id of this assignment.
+	 *
+	 * @var int
+	 * 
+	 * @con_is_primary  true
+	 * @con_is_unique   true
+	 * @con_has_field   true
+	 * @con_fieldtype   integer
+	 * @con_length      4
+	 */
+	protected $id;
  
-    /**
-     * The id of the user that is assigned. 
-     *
-     * @var int 
-     * 
-     * @con_has_field   true
-     * @con_fieldtype   integer 
-     * @con_length      4
-     * @con_is_notnull  true 
-     */
-    protected $usr_id;   
+	/**
+	 * The id of the user that is assigned. 
+	 *
+	 * @var int 
+	 * 
+	 * @con_has_field   true
+	 * @con_fieldtype   integer 
+	 * @con_length      4
+	 * @con_is_notnull  true 
+	 */
+	protected $usr_id;   
 
-    /**
-     * Root node of the program tree, the user was assigned to. Could be a subtree of
-     * a larger program. 
-     * 
-     * @var int 
-     * 
-     * @con_has_field   true
-     * @con_fieldtype   integer 
-     * @con_length      4
-     * @con_is_notnull  true
-     */
-    protected $root_prg_id;
+	/**
+	 * Root node of the program tree, the user was assigned to. Could be a subtree of
+	 * a larger program. 
+	 * 
+	 * @var int 
+	 * 
+	 * @con_has_field   true
+	 * @con_fieldtype   integer 
+	 * @con_length      4
+	 * @con_is_notnull  true
+	 */
+	protected $root_prg_id;
 
 
-    /**
-     * Timestamp of the moment of the assignment to or last update of the program.
-     *
-     * @var int 
-     * 
-     * @con_has_field   true
-     * @con_fieldtype   timestamp 
-     * @con_is_notnull  true 
-     */
-    protected $last_change; 
+	/**
+	 * Timestamp of the moment of the assignment to or last update of the program.
+	 *
+	 * @var int 
+	 * 
+	 * @con_has_field   true
+	 * @con_fieldtype   timestamp 
+	 * @con_is_notnull  true 
+	 */
+	protected $last_change; 
 
-    /**
-     * Id of user who did the assignment to or last update of the program.
-     *
-     * @var int 
-     * 
-     * @con_has_field   true
-     * @con_fieldtype   integer 
-     * @con_length      4
-     * @con_is_notnull  true 
-     */
-    protected $last_change_by;
+	/**
+	 * Id of user who did the assignment to or last update of the program.
+	 *
+	 * @var int 
+	 * 
+	 * @con_has_field   true
+	 * @con_fieldtype   integer 
+	 * @con_length      4
+	 * @con_is_notnull  true 
+	 */
+	protected $last_change_by;
  
 }
 
