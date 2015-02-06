990cd3432e8fe347c736c3650d09fe16a13b608e
Richard Klees <richard.klees@concepts-and-training.de>
2015-02-06 10:51:46 +0100

    added exception for violations of invariants on program tree.

---
+++ Modules/TrainingProgramme/classes/exceptions/class.ilTrainingProgrammTreeException.php
@@ -0,0 +1,11 @@
+<?php
+
+/* Copyright (c) 2015 Richard Klees <richard.klees@concepts-and-training.de> Extended GPL, see docs/LICENSE */
+
+/**
+ * Exception is thrown when invariants on the program tree would be violated
+ * by manipulation of tree.
+ */
+
+class ilTrainingProgrammTreeException extends ilException {
+}
