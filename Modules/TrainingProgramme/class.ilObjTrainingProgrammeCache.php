69648238e7392662ac03707c7046f51181dd13b9
Richard Klees <richard.klees@concepts-and-training.de>
2015-02-06 09:13:04 +0100

    cache for training programmes

---
+++ Modules/TrainingProgramme/class.ilObjTrainingProgrammeCache.php
@@ -0,0 +1,40 @@
+<?php
+
+/* Copyright (c) 2015 Richard Klees <richard.klees@concepts-and-training.de> Extended GPL, see docs/LICENSE */
+
+/**
+ * Cache for ilObjTrainingProgrammes.
+ *
+ * Implemented as singleton.
+ *
+ * @author : Richard Klees <richard.klees@concepts-and-training.de>
+ */
+
+class ilObjTrainingProgrammeCache {
+	static private  $instance; // ilObjTrainingProgrammeCache
+	
+	private function __construct() {
+		$this->instances = array();
+	}
+
+	static public function singleton() {
+		if (self::$instance === null) {
+			self::$instance = new ilObjTrainingProgrammeCache();
+		}
+		return self::$instance;
+	}
+	
+	protected $instances; // [ilObjTrainingProgramme]
+	
+	public function getInstance($a_ref_id) {
+		require_once("Modules/TrainingProgramme/classes/class.ilObjTrainingProgramme.php")
+		
+		if (!array_key_exists($this->instances)) {
+			$this->instances[$a_ref_id] = new ilObjTrainingProgramme($a_ref_id);
+		}
+		
+		return $this->instances[$a_ref_id];
+	}
+}
+
+?>
