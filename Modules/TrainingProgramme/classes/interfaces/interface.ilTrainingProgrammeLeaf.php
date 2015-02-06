19d7c6cf46d3764f85fd6d11db3d1659beac89e5
Richard Klees <richard.klees@concepts-and-training.de>
2015-02-06 10:51:10 +0100

    added stub for interface of program leaf nodes.

---
+++ Modules/TrainingProgramme/classes/interfaces/interface.ilTrainingProgrammeLeaf.php
@@ -0,0 +1,16 @@
+<?php
+
+/* Copyright (c) 2015 Richard Klees <richard.klees@concepts-and-training.de> Extended GPL, see docs/LICENSE */
+
+/**
+ * The interface a class has to fullfill if it should be used as leaf in a
+ * program.
+ *
+ * @author : Richard Klees <richard.klees@concepts-and-training.de>
+ */
+
+interface ilTrainingProgramLeaf {
+	
+}
+
+?>
