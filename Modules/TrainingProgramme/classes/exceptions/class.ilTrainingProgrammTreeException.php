57282986e8b0e2d8d0ce4d566b986b540057469e
Michael Herren <michherren@gmail.com>
2015-02-09 17:24:00 +0100

    fixed some includes

--- Modules/TrainingProgramme/classes/exceptions/class.ilTrainingProgrammTreeException.php
+++ Modules/TrainingProgramme/classes/exceptions/class.ilTrainingProgrammTreeException.php
@@ -1,11 +1,11 @@
 <?php
 
 /* Copyright (c) 2015 Richard Klees <richard.klees@concepts-and-training.de> Extended GPL, see docs/LICENSE */
+require_once("./Services/Exceptions/classes/class.ilException.php");
 
 /**
  * Exception is thrown when invariants on the program tree would be violated
  * by manipulation of tree.
  */
-
-class ilTrainingProgrammTreeException extends ilException {
+class ilTrainingProgrammeTreeException extends ilException {
 }
