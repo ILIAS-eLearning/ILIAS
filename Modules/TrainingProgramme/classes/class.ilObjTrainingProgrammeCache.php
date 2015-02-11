0f11003ed13814ffebcb384c75461b66a4a5b046
Richard Klees <richard.klees@concepts-and-training.de>
2015-02-11 16:43:58 +0100

    enhanced cache.

--- Modules/TrainingProgramme/classes/class.ilObjTrainingProgrammeCache.php
+++ Modules/TrainingProgramme/classes/class.ilObjTrainingProgrammeCache.php
@@ -11,7 +11,7 @@
  */
 
 class ilObjTrainingProgrammeCache {
-	static private  $instance; // ilObjTrainingProgrammeCache
+	static private  $instance = null; // ilObjTrainingProgrammeCache
 
 	private function __construct() {
 		$this->instances = array();
@@ -26,8 +26,13 @@ class ilObjTrainingProgrammeCache {
 	
 	protected $instances; // [ilObjTrainingProgramme]
 	
-	public function getInstance($a_ref_id) {
+	public function getInstanceByRefId($a_ref_id) {
 		require_once("Modules/TrainingProgramme/classes/class.ilObjTrainingProgramme.php");	
+		
+		// TODO: Maybe this should be done via obj_id instead of ref_id, since two
+		// ref_ids could point to the same object, hence leading to two instances of
+		// the same object. Since ilObjTrainingProgramme is a container, it should (??)
+		// only have one ref_id...
 		
 		if (!array_key_exists($a_ref_id, $this->instances)) {
 			$this->instances[$a_ref_id] = new ilObjTrainingProgramme($a_ref_id);
@@ -36,8 +41,18 @@ class ilObjTrainingProgrammeCache {
 		return $this->instances[$a_ref_id];
 	}
 	
+	public function addInstance(ilObjTrainingProgramme $a_prg) {
+		if (!$a_prg->getRefId()) {
+			throw new ilException("ilObjTrainingProgrammeCache::addInstance: "
+								 ."Can't add instance without ref_id.");
+		}
+		$this->instances[$a_prg->getRefId()] = $a_prg;
+	}
+	
 	/**
 	 * For testing purpose.
+	 *
+	 * TODO: Move to mock class in tests.
 	 */ 
 	public function test_clear() {
 		$this->instances = array();
