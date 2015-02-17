a3544a75865d1487101503f1bf3fb18250fa0e74
Richard Klees <richard.klees@concepts-and-training.de>
2015-02-17 13:47:22 +0100

    getters and setter for progress model class.

--- Modules/TrainingProgramme/classes/model/class.ilTrainingProgrammeProgress.php
+++ Modules/TrainingProgramme/classes/model/class.ilTrainingProgrammeProgress.php
@@ -161,7 +161,237 @@ class ilTrainingProgrammeProgress extends ActiveRecord {
 	 * @con_is_notnull  true 
 	 */
 	protected $last_change_by;
+	
+	
+	/**
+	 * Create a new progress object for a given program node and assignment.
+	 *
+	 * @return ilTrainingProgrammeProgress
+	 */
+	static public function createFor( ilTrainingProgramme $a_prg
+								    , ilTrainingProgrammeAssignment $a_ass) {
+	}
+	
+	/**
+	 * Get the assignment, this progress belongs to.
+	 *
+	 * @return ilTrainingProgrammeAssignment.
+	 */
+	public function getAssignmentId() {
+		return $this->assignment_id;
+	}
+	
+	protected function setAssignmentId($a_id) {
+		$this->assignment_id = $a_id;
+		return $this;
+	}
+	
+	/**
+	 * Get the id of the program node this progress belongs to.
+	 *
+	 * @return int
+	 */
+	public function getNodeId() {
+		return $this->prg_id;
+	}
+	
+	protected function setNodeId($a_id) {
+		$this->prg_id = $a_id;
+		return $this;
+	}
+	
+	/**
+	 * Get the id of the user this progress is for.
+	 *
+	 * @return int
+	 */
+	public function getUserId() {
+		return $this->usr_id;
+	}
+	
+	protected function setUserId($a_id) {
+		$this->usr_id = $a_id;
+		return $this;
+	}
+	
+	/**
+	 * Get the amount of points the user needs to achieve on the subnodes of this
+	 * node. Also the amount of points, this node yields for the progress on the
+	 * nodes above.
+	 *
+	 * @return int
+	 */
+	public function getAmountOfPoints() {
+		return $this->points;
+	}
+	
+	/**
+	 * Get the amount of points the user needs to achieve on the subnodes of this
+	 * node. Also the amount of points, this node yields for the progress on the
+	 * nodes above.
+	 *
+	 * Throws when amount of points is smaller then zero.
+	 *
+	 * @throws ilException
+	 * @return $this
+	 */
+	public function setAmountOfPoints($a_points) {
+		if (!is_numeric($a_points) || $a_points < 0) {
+			throw new ilException("ilTrainingProgrammeProgress::setAmountOfPoints: "
+								 ."Expected a number > 0 as argument.");
+		}
+		
+		$this->points = (int)$a_points;
+		$this->updateLastChange();
+		return $this;
+	}
+	
+	/**
+	 * Get the amount of points the user currently has achieved on the node.
+	 *
+	 * @return int
+	 */
+	public function getCurrentAmountOfPoints() {
+		return $this->points_cur;
+	}
+	
+	/**
+	 * Set the amount of points the user currently has achieved on this node.
+	 *
+	 * Throw when amount of points is smaller then zero.
+	 *
+	 * @throws ilException
+	 * @return $this
+	 */
+	public function setCurrentAmountOfPoints($a_points) {
+		if (!is_numeric($a_points) || $a_points < 0) {
+			throw new ilException("ilTrainingProgrammeProgress::setCurrentAmountOfPoints: "
+								 ."Expected a number > 0 as argument.");
+		}
+		
+		$this->points_cur = (int)$a_points;
+		$this->updateLastChange();
+		return $this;
+	}
+	
+	/**
+	 * Get the status the user has on this node.
+	 *
+	 * @return int - one of ilTrainingProgramme::STATUS_*
+	 */
+	public function getStatus() {
+		return $this->status;
+	}
+	
+	/**
+	 * Set the status of this node.
+	 *
+	 * Throws when status is none of ilTrainingProgramme::STATUS_*. Throws when
+	 * completion_by is set and status is STATUS_PROGRESS.
+	 * 
+	 * @throws ilException
+	 * @param  $a_status int - one of ilTrainingProgramme::STATUS_*
+	 * @return $this
+	 */
+	public function setStatus($a_status) {
+		$a_status = (int)$a_status;
+		if (!in_array($a_status, self::$STATUS)) {
+			throw new ilException("ilTrainingProgrammeProgress::setStatus: No status: "
+								 ."'$a_status'");
+		}
+		$this->status = $a_status;
+		$this->updateLastChange();
+		return $this;
+	}
+	
+	/**
+	 * Set the completion_by field.
+	 *
+	 * @param $a_id int | null
+	 * @return $this
+	 */
+	public function setCompletionBy($a_id) {
+		if ($a_id !== null) {
+			$a_id = (int)$a_id;
+		}
+		$this->completion_by = $a_id;
+		$this->updateLastChange();
+		return $this;
+	}
+	
+	/**
+	 * Get the id of object or user that lead to the successfull completion
+	 * of this node.
+	 *
+	 * @return int
+	 */
+	public function getCompletionBy() {
+		return $this->completion_by;
+	}
+	/**
+	 * Get the id of the user who did the last change on this assignment.
+	 * 
+	 * @return int
+	 */	
+	public function getLastChangeBy() {
+		return $this->last_change_by;
+	}
+	
+	/**
+	 * Set the id of the user who did the last change on this progress.
+	 * 
+	 * Throws when $a_usr_id is not the id of a user.
+	 * 
+	 * @throws ilException
+	 * @return $this
+	 */
+	public function setLastChangeBy($a_usr_id) {
+		if (ilObject::_lookupType($a_usr_id) != "usr") {
+			throw new ilException("ilTrainingProgrammeProgress::setLastChangeBy: '$a_usr_id' "
+								 ."is no id of a user.");
+		}
+		$this->last_change_by = $a_usr_id;
+		return $this;
+	}
+	
+	/**
+	 * Get the timestamp of the last change on this progress.
+	 *
+	 * @return ilDateTime
+	 */
+	public function getLastChange() {
+		return new ilDateTime($this->last_change, IL_CAL_DATETIME);
+	}
 
+	/**
+	 * Update the last change timestamp to the current time.
+	 *
+	 * @return $this
+	 */
+	public function updateLastChange() {
+		$this->setLastChange(new ilDateTime(ilUtil::now(), IL_CAL_DATETIME)); 
+		return $this;
+	}
+
+	/**
+	 * Set the last change timestamp to the given time.
+	 * 
+	 * Throws when given time is smaller then current timestamp since that is 
+	 * logically impossible.
+	 * 
+	 * @throws ilException
+	 * @return $this
+	 */
+	public function setLastChange(ilDateTime $a_timestamp) {
+		if (ilDateTime::_before($a_timestamp, $this->getLastChange())) {
+			throw new ilException("ilTrainingProgrammeProgress::setLastChange: Given "
+								 ."timestamp is before current timestamp. That "
+								 ."is logically impossible.");
+		}
+		
+		$this->last_change = $a_timestamp->get(IL_CAL_DATETIME);
+		return $this;
+	}
 }
 
 ?>
