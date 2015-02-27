d83aa4328203f89e9c1350bc0618c90bebd675b3
mjansen <mjansen@databay.de>
2015-02-27 14:42:35 +0100

    GIT information on Windows

--- Services/Administration/classes/class.ilGitInformation.php
+++ Services/Administration/classes/class.ilGitInformation.php
@@ -31,12 +31,27 @@ class ilGitInformation implements ilVersionControlInformation
 
 		$info = array();
 
+		// https://gist.github.com/reiaguilera/82d164c7211e299d63ac
 		if(!ilUtil::isWindows())
 		{
-			// https://gist.github.com/reiaguilera/82d164c7211e299d63ac
 			$version_mini_hash = ilUtil::execQuoted('git describe --always');
 			$version_number    = ilUtil::execQuoted('git rev-list HEAD | wc -l');
 			$line              = ilUtil::execQuoted('git log -1');
+
+			if($version_number[0])
+			{
+				$version_number = $version_number[0];
+			}
+
+			if($version_mini_hash[0])
+			{
+				$version_mini_hash = $version_mini_hash[0];
+			}
+
+			if($line && array_filter($line))
+			{
+				$line = implode(' | ', array_filter($line));
+			}
 		}
 		else
 		{
@@ -45,19 +60,19 @@ class ilGitInformation implements ilVersionControlInformation
 			$line              = trim(exec('git log -1'));
 		}
 
-		if($version_number[0])
+		if($version_number)
 		{
-			$info[] = sprintf($lng->txt('git_revision'), $version_number[0]);
+			$info[] = sprintf($lng->txt('git_revision'), $version_number);
 		}
 
-		if($version_mini_hash[0])
+		if($version_mini_hash)
 		{
-			$info[] = sprintf($lng->txt('git_hash_short'), $version_mini_hash[0]);
+			$info[] = sprintf($lng->txt('git_hash_short'), $version_mini_hash);
 		}
 
-		if($line && array_filter($line))
+		if($line)
 		{
-			$info[] = sprintf($lng->txt('git_last_commit'), implode(' | ', array_filter($line)));
+			$info[] = sprintf($lng->txt('git_last_commit'), $line);
 		}
 
 		self::$revision_information = $info;
