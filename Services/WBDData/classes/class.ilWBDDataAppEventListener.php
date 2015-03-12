3e762a87311a3f0b776c9a015ca89cc297c24469
Richard Klees <richard.klees@concepts-and-training.de>
2015-03-12 08:09:44 +0000

    gev2: temporarily disabled wbd error app event listener.
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali2@58378 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/WBDData/classes/class.ilWBDDataAppEventListener.php
+++ Services/WBDData/classes/class.ilWBDDataAppEventListener.php
@@ -12,7 +12,8 @@ class ilWBDDataAppEventListener {
 
 
 	public static function handleEvent($a_component, $a_event, $a_parameter) {
-		
+		// TODO: currently disabled because of incomplete db update.
+		return;
 		require_once("./Services/WBDData/classes/class.wbdErrorLog.php");
 		$wbderrlog = new wbdErrorLog();
 
