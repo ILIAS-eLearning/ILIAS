b4b71cb18af739d2a3aed8119aa721dd58dbc2f4
Bjoern Heyser <bheyser@databay.de>
2015-03-18 14:58:25 +0100

    complete pool title in random tests (**deleted** if not available anymore)

--- setup/sql/4_4_hotfixes.php
+++ setup/sql/4_4_hotfixes.php
@@ -1831,3 +1831,17 @@ $ilDB->manipulate(
 		$ilDB->addIndex('page_question',array('page_parent_type','page_id', 'page_lang'),'i1');
 	}
 ?>
+<#66>
+<?php
+
+$query = "
+	UPDATE tst_rnd_quest_set_qpls SET pool_title = (
+		COALESCE(
+			(SELECT title FROM object_data WHERE obj_id = pool_fi), %s 
+		)
+	) WHERE pool_title IS NULL OR pool_title = %s
+";
+
+$ilDB->manipulateF($query, array('text', 'text'), array('*** unknown/deleted ***', ''));
+
+?>
