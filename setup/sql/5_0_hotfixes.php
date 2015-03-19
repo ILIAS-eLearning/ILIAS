995a8fefa2e40309d8b7a130f071fe5d372f43e9
Bjoern Heyser <bheyser@databay.de>
2015-03-19 15:42:33 +0100

    db column for storing random test state 'final broken'

--- setup/sql/5_0_hotfixes.php
+++ setup/sql/5_0_hotfixes.php
@@ -29,3 +29,21 @@ $query = "
 $ilDB->manipulateF($query, array('text', 'text'), array('*** unknown/deleted ***', ''));
 
 ?>
+<#3>
+<?php
+
+if( !$ilDB->tableColumnExists('tst_tests', 'broken'))
+{
+	$ilDB->addTableColumn('tst_tests', 'broken',
+		array(
+			'type' => 'integer',
+			'length' => 1,
+			'notnull' => false,
+			'default' => null
+		)
+	);
+
+	$ilDB->queryF("UPDATE tst_tests SET broken = %s", array('integer'), array(0));
+}
+
+?>
