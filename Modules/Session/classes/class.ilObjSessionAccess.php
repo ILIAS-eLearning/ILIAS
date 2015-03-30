8b791b81377c3bef4810fbdaafaf32bbdf834c4f
Fabian Wolf <wolf@leifos.com>
2015-03-30 11:25:35 +0200

    Mantis: 15658, Session Wording

--- Modules/Session/classes/class.ilObjSessionAccess.php
+++ Modules/Session/classes/class.ilObjSessionAccess.php
@@ -50,7 +50,8 @@ class ilObjSessionAccess extends ilObjectAccess
 			array("permission" => "read", "cmd" => "infoScreen", "lang_var" => "info_short", "default" => true),
 			array("permission" => "read", "cmd" => "register", "lang_var" => "join_session"),
 			array("permission" => "read", "cmd" => "unregister", "lang_var" => "event_unregister"),
-			array("permission" => "write", "cmd" => "edit", "lang_var" => "edit"),
+			array("permission" => "write", "cmd" => "edit", "lang_var" => "settings"),
+			array("permission" => "write", "cmd" => "materials", "lang_var" => "crs_objective_add_mat"),
 			array('permission' => 'write', 'cmd' => 'members', 'lang_var' => 'event_edit_members')
 		);
 		
