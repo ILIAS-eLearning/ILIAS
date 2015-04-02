4629a2a3911ab1ff8ecd5cea60a950c583e7d4da
Michael Herren <michherren@gmail.com>
2015-04-02 17:41:02 +0200

    added deep-linking functionality (enable goto link)

--- Modules/TrainingProgramme/classes/class.ilObjTrainingProgrammeAccess.php
+++ Modules/TrainingProgramme/classes/class.ilObjTrainingProgrammeAccess.php
@@ -76,10 +76,9 @@ class ilObjTrainingProgrammeAccess extends ilObjectAccess {
 	 */
 	function _checkGoto($a_target)
 	{
-		die("here");
 		global $ilAccess;
 		$t_arr = explode('_', $a_target);
-		if ($t_arr[0] != 'orgu' || ((int)$t_arr[1]) <= 0) {
+		if ($t_arr[0] != 'prg' || ((int)$t_arr[1]) <= 0) {
 			return false;
 		}
 		if ($ilAccess->checkAccess('read', '', $t_arr[1])) {
