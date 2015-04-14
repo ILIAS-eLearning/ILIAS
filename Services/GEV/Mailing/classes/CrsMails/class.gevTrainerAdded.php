63a3753a27a86283550062ce84950f4d0488eb69
Richard Klees <richard.klees@concepts-and-training.de>
2015-04-14 07:32:18 +0000

    gev2: fixed footer of mails with ical
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali2@58821 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/GEV/Mailing/classes/CrsMails/class.gevTrainerAdded.php
+++ Services/GEV/Mailing/classes/CrsMails/class.gevTrainerAdded.php
@@ -27,36 +27,6 @@ class gevTrainerAdded extends gevCrsAutoMail {
 		return array();
 	}
 	
-	public function getMail($a_recipient) {
-		if ($this->getAdditionalMailSettings()->getSuppressMails()) {
-			return null;
-		}
-
-		
-		if ($this->checkUserID($a_recipient)) {
-			$a_recipient = array( "name" => ilObjUser::_lookupFullname($a_recipient)
-								, "email" => ilObjUser::_lookupEmail($a_recipient));
-		}
-		
-		$message = $this->getMessageFromTemplate($this->getTemplateId()
-												, null
-												, null
-												, null);
-
-		return array( "from" => $this->getFrom()
-					, "to" => $a_recipient["name"]." <".$a_recipient["email"].">"
-					, "cc" => $this->getCC($a_recipient)
-					, "bcc" => $this->getBCC($a_recipient)
-					, "subject" => $message["subject"]
-					, "message_plain" => $message["plain"]
-					, "message_html" => $message["html"]
-					, "attachments" => $this->getAttachmentsForMail($a_recipient)
-					);
-
-	}
-
-
-
 	public function getAttachmentsForMail() {
 		require_once ("Services/GEV/Mailing/classes/class.gevCrsMailAttachments.php");
 
