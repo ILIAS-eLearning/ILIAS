27ff9b88e3dc4631307a508bde5d4893f971dbf3
Richard Klees <richard.klees@rwth-aachen.de>
2015-04-19 17:48:18 +0200

    style

--- example.php
+++ example.php
@@ -15,26 +15,26 @@ $int_input = text_input("10")
 
 $all_inputs = formlet( 
     inject(collect()),
-    with_label("A text input: ", text_input()),$br,
-    with_label("A int input: ", with_errors($int_input)),$br,
-    with_label("A checkbox input...", checkbox()),$br,
-    with_label("A textarea input...", textarea()),$br,
-    with_label("A button input...", button("button")),$br,
-    with_label("An email input...", email()),$br,
-    with_label("A hidden input...", hidden("hidden")),$br,
-    with_label("A number input...", number(10, 0, 100, 5)),$br,
-    with_label("A password input...", password()),$br,
-    with_label("A reset input...", reset_button("RESET")),$br,
-    with_label("A search input...", search("RESET")),$br,
-    with_label("A url input...", url()),$br,
-    with_label("A select input...", select(array("one", "two", "three"))),$br,
+    with_label("A text input: ", text_input()), $br,
+    with_label("A int input: ", with_errors($int_input)), $br,
+    with_label("A checkbox input...", checkbox()), $br,
+    with_label("A textarea input...", textarea()), $br,
+    with_label("A button input...", button("button")), $br,
+    with_label("An email input...", email()), $br,
+    with_label("A hidden input...", hidden("hidden")), $br,
+    with_label("A number input...", number(10, 0, 100, 5)), $br,
+    with_label("A password input...", password()), $br,
+    with_label("A reset input...", reset_button("RESET")), $br,
+    with_label("A search input...", search("RESET")), $br,
+    with_label("A url input...", url()), $br,
+    with_label("A select input...", select(array("one", "two", "three"))), $br,
     with_label("A radio input...", radio(array("aa", "bb", "cc"))),
     inject(stop())
     );
 
 $formlet = formlet(
     inject(collect()),
-    $all_inputs,$br,$br,
+    $all_inputs, $br, $br,
     fieldset("In Fieldset: ", $all_inputs),
     submit("Absenden", array(), true),
     inject(stop())
