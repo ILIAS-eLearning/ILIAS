4867b69911039f7e8f4004d5b7746579896ee081
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2015-04-20 10:54:27 +0200

    #15800 - blog: rss

--- Modules/Blog/classes/class.ilObjBlog.php
+++ Modules/Blog/classes/class.ilObjBlog.php
@@ -84,7 +84,7 @@ class ilObjBlog extends ilObject2
 					",bg_color = ".$ilDB->quote($this->getBackgroundColor(), "text").
 					",font_color = ".$ilDB->quote($this->getFontcolor(), "text").
 					",img = ".$ilDB->quote($this->getImage(), "text").
-					",rss_active = ".$ilDB->quote($this->hasRSS(), "text").
+					",rss_active = ".$ilDB->quote($this->hasRSS(), "integer").
 					",approval = ".$ilDB->quote($this->hasApproval(), "integer").
 					" WHERE id = ".$ilDB->quote($this->id, "integer"));
 		}
