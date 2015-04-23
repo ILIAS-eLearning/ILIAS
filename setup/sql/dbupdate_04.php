533a847bf61948d4b678c73e3a77f7774fd437a6
Bjoern Heyser <bheyser@databay.de>
2015-04-23 11:54:54 +0200

    fixed missing last finished pass values so result presentation becomes consistent

--- setup/sql/dbupdate_04.php
+++ setup/sql/dbupdate_04.php
@@ -5883,3 +5883,7 @@ $ilDB->manipulate("UPDATE style_data SET ".
 <?php
 	$ilCtrlStructureReader->getStructure();
 ?>
+<#4481>
+<?php
+$ilDB->manipulate("UPDATE tst_active SET last_finished_pass = (tries - 1) WHERE last_finished_pass IS NULL");
+?>
