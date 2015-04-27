15e121db0c877c203a20474e6329ce3dc0920c49
Fabian Schmid <fs@studer-raimann.ch>
2015-04-27 16:12:05 +0200

    fix for 0015735

--- Modules/DataCollection/classes/class.ilDataCollectionTable.php
+++ Modules/DataCollection/classes/class.ilDataCollectionTable.php
@@ -1093,8 +1093,9 @@ class ilDataCollectionTable {
 		} else {
 			switch ($sortField->getDatatypeId()) {
 				case ilDataCollectionDatatype::INPUTFORMAT_RATING:
-					$joinStr .= "LEFT JOIN (SELECT AVG(sort_avg_rating.rating) AS avg_rating, sort_avg_rating.obj_id AS obj_id FROM il_rating as sort_avg_rating WHERE sort_avg_rating.sub_obj_id = {$sortField->getId()} GROUP BY sort_avg_rating.obj_id) AS sort_avg_rating on sort_avg_rating.obj_id = record.id ";
-					$selectStr .= " sort_avg_rating.avg_rating AS field_{$id},";
+					$joinStr .= "LEFT JOIN (SELECT (ROUND(AVG(rating), 1) * 10000 + COUNT(rating)) as rating, obj_id FROM il_rating GROUP BY obj_id) AS average ON average.obj_id = record.id";
+					$selectStr .= " average.rating AS field_{$id},";
+
 					break;
 				case ilDataCollectionDatatype::INPUTFORMAT_ILIAS_REF:
 					$joinStr .=
@@ -1155,9 +1156,9 @@ class ilDataCollectionTable {
 				$filterField = $this->getField($filter_field_id);
 				switch ($filterField->getDatatypeId()) {
 					case ilDataCollectionDatatype::INPUTFORMAT_RATING:
-						$joinStr .=
-							"INNER JOIN (SELECT AVG(avg_rating.rating) AS avg_rating, avg_rating.obj_id AS obj_id FROM il_rating as avg_rating WHERE avg_rating.sub_obj_id = {$filter_field_id} GROUP BY avg_rating.obj_id) AS avg_rating on avg_rating.avg_rating >= "
-							. $ilDB->quote($filter_value, 'integer') . " AND avg_rating.obj_id = record.id ";
+						// FSX Bugfix 0015735: The average is multiplied with 10000 and added to the amount of votes
+						$where_additions .= " AND average.rating >= " . $ilDB->quote($filter_value * 10000, 'integer');
+
 						break;
 					case ilDataCollectionDatatype::INPUTFORMAT_ILIAS_REF:
 						$joinStr .=
