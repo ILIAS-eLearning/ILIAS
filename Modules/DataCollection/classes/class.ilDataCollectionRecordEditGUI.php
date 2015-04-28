7bfc235500b6c5253a6d32fd870c359c06eff965
Fabian Schmid <fs@studer-raimann.ch>
2015-04-28 08:35:52 +0200

    fix for #0015840

--- Modules/DataCollection/classes/class.ilDataCollectionRecordEditGUI.php
+++ Modules/DataCollection/classes/class.ilDataCollectionRecordEditGUI.php
@@ -168,18 +168,23 @@ class ilDataCollectionRecordEditGUI {
 				}
 				$reftable = ilDataCollectionCache::getTableCache($reffield->getTableId());
 				foreach ($reftable->getRecords() as $record) {
-					// If the referenced field is MOB or FILE, we display the filename in the dropdown
-					if ($reffield->getDatatypeId() == ilDataCollectionDatatype::INPUTFORMAT_FILE) {
-						$file_obj = new ilObjFile($record->getRecordFieldValue($fieldref), false);
-						$options[$record->getId()] = $file_obj->getFileName();
-					} else {
-						if ($reffield->getDatatypeId() == ilDataCollectionDatatype::INPUTFORMAT_MOB) {
+					switch($reffield->getDatatypeId()) {
+						case ilDataCollectionDatatype::INPUTFORMAT_FILE:
+							$file_obj = new ilObjFile($record->getRecordFieldValue($fieldref), false);
+							$options[$record->getId()] = $file_obj->getFileName();
+							break;
+						case ilDataCollectionDatatype::INPUTFORMAT_MOB:
 							$media_obj = new ilObjMediaObject($record->getRecordFieldValue($fieldref), false);
 							$options[$record->getId()] = $media_obj->getTitle();
-						} else {
+							break;
+						case ilDataCollectionDatatype::INPUTFORMAT_DATETIME:
+							$options[$record->getId()] = $record->getRecordFieldSingleHTML($fieldref);
+							break;
+						default:
 							$options[$record->getId()] = $record->getRecordFieldValue($fieldref);
-						}
+							break;
 					}
+
 				}
 				asort($options);
 				$item->setOptions($options);
