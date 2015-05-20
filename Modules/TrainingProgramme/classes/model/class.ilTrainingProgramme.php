f575928976a526a95e0a8a8e2857e36c18384218
Michael Herren <michherren@gmail.com>
2015-05-20 14:19:20 +0200

    allow zero as a valid point value (needed for elective-programs)

--- Modules/TrainingProgramme/classes/model/class.ilTrainingProgramme.php
+++ Modules/TrainingProgramme/classes/model/class.ilTrainingProgramme.php
@@ -245,9 +245,10 @@ class ilTrainingProgramme extends ActiveRecord {
 	 */
 	public function setPoints($a_points) {
 		$a_points = (int)$a_points;
-		if ($a_points <= 0) {
-			throw new ilException("ilTrainingProgramme::setPoints: Points need to "
-								 ."be larger than zero.");
+
+		if ($a_points < 0) {
+			throw new ilException("ilTrainingProgramme::setPoints: Points cannot "
+								 ."be smaller than zero.");
 		}
 
 		$this->points = $a_points;
