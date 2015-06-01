0ed8e7475883c2086496d3c88e6d55a10f11276d
Richard Klees <richard.klees@concepts-and-training.de>
2015-06-01 09:35:25 +0200

    ilAdministrationGUI calls ilObjStudyProgrammeGUI

--- Services/Administration/classes/class.ilAdministrationGUI.php
+++ Services/Administration/classes/class.ilAdministrationGUI.php
@@ -39,6 +39,7 @@ include_once("./Services/Table/classes/class.ilTableGUI.php");
 * // BEGIN WebDAV
 * @ilCtrl_Calls ilAdministrationGUI: ilObjFileAccessSettingsGUI, ilPermissionGUI, ilObjRemoteTestGUI
 * // END WebDAV
+* @ilCtrl_Calls ilAdministrationGUI: ilObjStudyProgrammeGUI
 */
 class ilAdministrationGUI
 {
