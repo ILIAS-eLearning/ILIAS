fe1cf617c507726c84051d8ba5be8766e7298379
Richard Klees <richard.klees@concepts-and-training.de>
2015-06-19 16:59:34 +0200

    StudyProgramme: 2 event tests pass

--- Modules/StudyProgramme/classes/class.ilStudyProgrammeEvents.php
+++ Modules/StudyProgramme/classes/class.ilStudyProgrammeEvents.php
@@ -12,32 +12,41 @@ class ilStudyProgrammeEvents {
 	static public $app_event_handler = null;
 	
 	static protected function initAppEventHandler() {
-		if (self::$app_event_handler) {
+		if (self::$app_event_handler === null) {
 			global $ilAppEventHandler;
 			self::$app_event_handler = $ilAppEventHandler;
 		}
 	}
 	
 	static protected function raise($a_event, $a_parameter) {
+		self::initAppEventHandler();
 		self::$app_event_handler->raise(self::$component, $a_event, $a_parameter);
 	}
 	
 	static public function userAssigned(ilStudyProgrammeUserAssignment $a_assignment) {
-		self::raise("userAssigned", array(
-			
-		));
+		self::raise("userAssigned", array
+			( "root_prg_id"		=> $a_assignment->getStudyProgramme()->getId()
+			, "usr_id"			=> $a_assignment->getUserId()
+			, "ass_id"			=> $a_assignment->getId()
+			));
 	}
 	
 	static public function userDeassigned(ilStudyProgrammeUserAssignment $a_assignment) {
-		self::raise("userDeassigned", array(
-			
-		));
+		self::raise("userDeassigned", array
+			( "root_prg_id"		=> $a_assignment->getStudyProgramme()->getId()
+			, "usr_id"			=> $a_assignment->getUserId()
+			, "ass_id"			=> $a_assignment->getId()
+			));
 	}
 	
 	static public function userSuccessful(ilStudyProgrammeUserProgress $a_progress) {
-		self::raise("userSuccessful", array(
-			
-		));
+		$ass = $a_progress->getAssignment();
+		self::raise("userSuccessful", array
+			( "root_prg_id"		=> $ass->getStudyProgramme()->getId()
+			, "prg_id"			=> $a_progress->getStudyProgramme()->getId()
+			, "usr_id"			=> $ass->getUserId()
+			, "ass_id"			=> $ass->getId()
+			));
 	}
 }
 
