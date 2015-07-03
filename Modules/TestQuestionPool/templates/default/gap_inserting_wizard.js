eefc6e1e5b94aac4cf68a70a97b5d3005db6cc0d
Guido Vollbach <gvollbach@databay.de>
2015-07-03 08:35:40 +0200

    long menu: moved more functions to wizard
    Signed-off-by: Guido Vollbach <gvollbach@databay.de>

--- Modules/TestQuestionPool/templates/default/gap_inserting_wizard.js
+++ Modules/TestQuestionPool/templates/default/gap_inserting_wizard.js
@@ -1,6 +1,7 @@
 var Gap_inserting_wizard = (function () {
 	var pub = {};
-
+	pub.active_gap = -1;
+	
 	function insertGapCodeAtCaret(object)  {
 		return object.each(function(i) {
 			var code_start = '[' + pub.replacement_word + ']';
@@ -74,15 +75,111 @@ var Gap_inserting_wizard = (function () {
 
 	function setTextAreaValue(text)
 	{
+		var cursor, inGap;
 		if (typeof(tinymce) != 'undefined') {
+			//ToDo: Bug in tiny steals focus on setContent (tinymce Bug #6423)
+			var inst = tinyMCE.activeEditor;
+			cursor = getCursorPositionTiny(inst);
 			tinymce.get(pub.textarea).setContent(text);
+			inGap = cursorInGap(cursor);
+			if(inGap[1] != '-1' )
+			{
+				pub.active_gap = parseInt(inGap[1], 10);
+			}
+			setCursorPositionTiny(inst, pub.active_gap);
 		}
 		else {
 			var textarea =  $('textarea#' + pub.textarea);
+			cursor = textarea.prop('selectionStart');
 			textarea.val(text);
+			inGap = cursorInGap(cursor + 1);
+			if(inGap != '-1')
+			{
+				if(pub.active_gap == '-1')
+				{
+					setCaretPosition(textarea, cursor);
+				}
+				else
+				{
+					textarea.prop('selectionStart',ClozeGlobals.active_gap);
+					textarea.prop('selectionEnd',ClozeGlobals.active_gap);
+				}
+				pub.active_gap = parseInt(inGap[1], 10);
+			}
+		}
+	}
+	
+	function getCursorPositionTiny(editor)
+	{
+		var bm = editor.selection.getBookmark(0);
+		var selector = '[data-mce-type=bookmark]';
+		var bmElements = editor.dom.select(selector);
+		editor.selection.select(bmElements[0]);
+		editor.selection.collapse();
+		var elementID = '######cursor######';
+		var positionString = '<span id="' + elementID + '"></span>';
+		editor.selection.setContent(positionString);
+		var content = editor.getContent({format: 'html'});
+		var index = content.indexOf(positionString);
+		editor.dom.remove(elementID, false);
+		editor.selection.moveToBookmark(bm);
+		return index;
+	}
+
+	function setCursorPositionTiny(editor, index)
+	{
+		var content = editor.getContent({format: 'html'});
+		if( index == '-1')
+		{
+			index = 0;
+		}
+		var part1 = content.substr(0, index);
+		var part2 = content.substr(index);
+		var bookmark = editor.selection.getBookmark(0);
+		var positionString = '<span id="' + bookmark.id + '_start" data-mce-type="bookmark" data-mce-style="overflow:hidden;line-height:0px"></span>';
+		var contentWithString = part1 + positionString + part2;
+		editor.setContent(contentWithString, ({format: 'raw'}));
+		editor.selection.moveToBookmark(bookmark);
+		return bookmark;
+	}
+
+	function setCaretPosition(element, pos)
+	{
+		if (element.setSelectionRange) {
+			element.focus();
+			element.setSelectionRange(pos, pos);
+		}
+		else if (element.createTextRange) {
+			var range = element.createTextRange();
+			range.collapse(true);
+			range.moveEnd('character', pos);
+			range.moveStart('character', pos);
+			range.select();
 		}
 	}
 
+	function cursorInGap(position)
+	{
+		var text    = getTextAreaValue();
+		var end     = 0;
+		var inGap   = -1;
+		var gaps_length	= text.split(new RegExp('\\[' + pub.replacement_word + '[\\s\\S\\d]*?\\]')).length;
+		var gapNumber;
+		var gap_length = pub.replacement_word.length + 2;
+		for (var i = 0; i < gaps_length.length; i++) {
+			var start = text.indexOf('[' + pub.replacement_word + ' ', end);
+			end = text.indexOf('[/' + pub.replacement_word + ']', parseInt(end, 10)) + gap_length;
+			if (start < position && end >= position)
+			{
+				inGap = parseInt(end, 10) + 1;
+				var gapSize = parseInt(end, 10) - parseInt(start, 10);
+				var gapContent = text.substr(parseInt(start, 10) + gap_length, gapSize);
+				gapContent = gapContent.split(']');
+				gapNumber = gapContent[0];
+			}
+		}
+		return [gapNumber, inGap];
+	}
 	//Public property
 	pub.textarea  			= '';
 	pub.trigger_id			= '';
@@ -100,7 +197,9 @@ var Gap_inserting_wizard = (function () {
 			return false;
 		});
 	};
-	
+	pub.doSomething = function(){
+		
+	};
 
 	//Return just the public parts
 	return pub;
