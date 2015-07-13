d6c0b17bcd0eb7f722552b26b274d09367013683
Richard Klees <richard.klees@concepts-and-training.de>
2015-07-13 09:29:14 +0000

    gev2: implemented db update for decentral training requests.
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali2_dev2@59888 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/GEV/DecentralTrainings/classes/class.gevDecentralTrainingCreationRequestDB.php
+++ Services/GEV/DecentralTrainings/classes/class.gevDecentralTrainingCreationRequestDB.php
@@ -54,10 +54,50 @@ class gevDecentralTrainingCreationRequestDB {
 	}
 	
 	public function updateRequest(gevDecentralTrainingCreationRequest $request) {
-		
+		$ilDB = $this->getDB();
+		if ($request->requestId() === null) {
+			$this->throwException("Can't update request withou id.");
+		}
+		$settings = $a_request->settings();
+		$requested_ts = $a_request->requestedTS();
+		$finished_ts = $a_request->finishedTS();
+		$ilDB->manipulate(
+			"UPDATE ".self::TABLE_NAME."\n".
+			"       (request_id, user_id, template_obj_id, requested_ts,\n".
+			"       finished_ts, created_obj_id, trainer_ids, start_dt,\n".
+			"       end_dt, venue_obj_id, venue_text, orgu_ref_id, description,\n".
+			"       orga_info, webinar_link, webinar_password)\n".
+			" SET ( user_id = ".$ilDB->quote($a_request->userId(), "integer")."\n".
+			"     , template_obj_id = ".$ilDB->quote($a_request->templateObjId(), "integer")."\n".
+			"     , requested_ts = ".$ilDB->quote($requested_ts ? $requested_ts->get(IL_CAL_DATETIME) : null, "timestamp")."\n".
+			"     , finished_ts = ".$ilDB->quote($finished_ts ? $finished_ts->get(IL_CAL_DATETIME) : null, "timestamp")."\n".
+			"     , created_obj_id = ".$ilDB->quote($a_request->createdObjId(), "integer")."\n".
+			"     , trainer_ids = ".$ilDB->quote(implode(";", $a_request->trainerIds()), "text")."\n".
+			"     , start_dt = ".$ilDB->quote($settings->start()->get(IL_CAL_DATETIME), "timestamp")."\n".
+			"     , end_dt = ".$ilDB->quote($settings->end()->get(IL_CAL_DATETIME), "timestamp")."\n".
+			"     , venue_obj_id = ".$ilDB->quote($settings->venueObjId(), "integer")."\n".
+			"     , venue_text = ".$ilDB->quote($settings->venueText(), "text")."\n".
+			"     , orgu_ref_id= ".$ilDB->quote($settings->orguRefId(), "integer")."\n".
+			"     , description = ".$ilDB->quote($settings->description(), "text")."\n".
+			"     , orga_info = ".$ilDB->quote($settings->orgaInfo(), "text")."\n".
+			"     , webinar_link = ".$ilDB->quote($settings->webinarLink(), "text")."\n".
+			"     , webinar_password = ".$ilDB->quote($settings->webinarPassword(), "text")."\n".
+			"     )\n".
+			" WHERE request_id = ".$ilDB->quote($request->requestId(), "integer")."\n"
+		);
 	}
 	
+	
+	// HELPERS
+	
+	protected function throwException($msg) {
+		require_once("Services/GEV/DecentralTrainings/classes/class.gevDecentralTrainingException.php");
+		throw new gevDecentralTrainingException($msg);
+	}
+	
+	
 	// GETTERS FOR GLOBALS
+	
 	protected function getDB() {
 		global $ilDB;
 		return $ilDB;
