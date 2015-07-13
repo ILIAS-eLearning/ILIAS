9a24b3809456c7ff3831883d9379a541b2298c09
Richard Klees <richard.klees@concepts-and-training.de>
2015-07-13 08:46:52 +0000

    gev2: forgot to add database class for decentral training creation requests.
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali2_dev2@59884 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ Services/GEV/DecentralTrainings/classes/class.gevDecentralTrainingCreationRequestDB.php
@@ -0,0 +1,161 @@
+<?php
+
+/* Copyright (c) 2015 Richard Klees, Extended GPL, see docs/LICENSE */#
+
+/**
+* Database for decentral training creation request.
+*
+* @author	Richard Klees <richard.klees@concepts-and-training.de>
+* @version	$Id$
+*/
+
+class gevDecentralTrainingCreationRequestDB {
+	const TABLE_NAME = "dct_creation_requests";
+	
+	public function __construct() {
+		
+	}
+	
+	public function getRequest($a_request_id) {
+		assert(is_int($a_request_id));
+	}
+	
+	public function createRequest(gevDecentralTrainingCreationRequest $a_request) {
+		$ilDB = $this->getDB();
+		$request_id = $ilDB->nextId(self::TABLE_NAME);
+		$settings = $a_request->settings();
+		$requested_ts = $a_request->requestedTS();
+		$finished_ts = $a_request->finishedTS();
+		$ilDB->manipulate(
+			"INSERT INTO ".self::TABLE_NAME."\n".
+			"       (request_id, user_id, template_obj_id, requested_ts,\n".
+			"       finished_ts, created_obj_id, trainer_ids, start_dt,\n".
+			"       end_dt, venue_obj_id, venue_text, orgu_ref_id, description,\n".
+			"       orga_info, webinar_link, webinar_password)\n".
+			" VALUES ( ".$ilDB->quote($request_id, "integer")."\n".
+			"        , ".$ilDB->quote($a_request->userId(), "integer")."\n".
+			"        , ".$ilDB->quote($a_request->templateObjId(), "integer")."\n".
+			"        , ".$ilDB->quote($requested_ts ? $requested_ts->get(IL_CAL_DATETIME) : null, "timestamp")."\n".
+			"        , ".$ilDB->quote($finished_ts ? $finished_ts->get(IL_CAL_DATETIME) : null, "timestamp")."\n".
+			"        , ".$ilDB->quote($a_request->createdObjId(), "integer")."\n".
+			"        , ".$ilDB->quote(implode(";", $a_request->trainerIds()), "text")."\n".
+			"        , ".$ilDB->quote($settings->start()->get(IL_CAL_DATETIME), "timestamp")."\n".
+			"        , ".$ilDB->quote($settings->end()->get(IL_CAL_DATETIME), "timestamp")."\n".
+			"        , ".$ilDB->quote($settings->venueObjId(), "integer")."\n".
+			"        , ".$ilDB->quote($settings->venueText(), "text")."\n".
+			"        , ".$ilDB->quote($settings->orguRefId(), "integer")."\n".
+			"        , ".$ilDB->quote($settings->description(), "text")."\n".
+			"        , ".$ilDB->quote($settings->orgaInfo(), "text")."\n".
+			"        , ".$ilDB->quote($settings->webinarLink(), "text")."\n".
+			"        , ".$ilDB->quote($settings->webinarPassword(), "text")."\n".
+			"        )\n"
+		);
+		return $request_id;
+	}
+	
+	public function updateRequest(gevDecentralTrainingCreationRequest $request) {
+		
+	}
+	
+	// GETTERS FOR GLOBALS
+	protected function getDB() {
+		global $ilDB;
+		return $ilDB;
+	}
+	
+	// Installation
+	
+	static public function install_step1(ilDB $ilDB) {
+		if( $ilDB->tableExists(self::TABLE_NAME) ) {
+			throw new ilException("Database ".self::TABLE_NAME." already exists.");
+		}
+		
+		$ilDB->createTable(self::TABLE_NAME, array(
+			'request_id' => array(
+				'type' => 'integer',
+				'length' => 4,
+				'notnull' => true
+			),
+			'user_id' => array(
+				'type' => 'integer',
+				'length' => 4,
+				'notnull' => true
+			),
+			'template_obj_id' => array(
+				'type' => 'integer',
+				'length' => 4,
+				'notnull' => true
+			),
+			'requested_ts' => array(
+				'type' => 'timestamp',			
+				'notnull' => true
+			),
+			'finished_ts' => array(
+				'type' => 'timestamp',			
+				'notnull' => false
+			),
+			'created_obj_id' => array(
+				'type' => 'integer',
+				'length' => 4,
+				'notnull' => false
+			),
+			'trainer_ids' => array(
+				'type' => 'text',
+				'length' => 200,
+				'notnull' => true
+			),
+			// Settings
+			'start_dt' => array(
+				'type' => 'timestamp',			
+				'notnull' => true
+			),
+			'end_dt' => array(
+				'type' => 'timestamp',			
+				'notnull' => true
+			),
+			'venue_obj_id' => array(
+				'type' => 'integer',
+				'length' => 4,
+				'notnull' => false,
+				'default' => null
+			),
+			'venue_text' => array(
+				'type' => 'text',
+				'length' => 4000,
+				'notnull' => false,
+				'default' => null
+			),
+			'orgu_ref_id' => array(
+				'type' => 'integer',
+				'length' => 4,
+				'notnull' => false,
+				'default' => null
+			),
+			'description' => array(
+				'type' => 'text',
+				'length' => 128,
+				'notnull' => false
+			),
+			'orga_info' => array(
+				'type' => 'text',
+				'length' => 4000,
+				'notnull' => false
+			),
+			'webinar_link' => array(
+				'type' => 'text',
+				'length' => 200,
+				'notnull' => false,
+				'default' => null
+			),
+			'webinar_password' => array(
+				'type' => 'text',
+				'length' => 200,
+				'notnull' => false,
+				'default' => null
+			)
+		));
+			
+		$ilDB->addPrimaryKey(self::TABLE_NAME, array('request_id'));
+		$ilDB->createSequence(self::TABLE_NAME);
+	}
+}
