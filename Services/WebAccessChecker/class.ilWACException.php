420835586b9d2a65770f53b580276ef2d39c566d
Fabian Schmid <fs@studer-raimann.ch>
2015-07-28 17:32:30 +0200

    more exceptions

--- Services/WebAccessChecker/class.ilWACException.php
+++ Services/WebAccessChecker/class.ilWACException.php
@@ -9,17 +9,18 @@ require_once('./Services/Exceptions/classes/class.ilException.php');
  */
 class ilWACException extends ilException {
 
-	const BASE_CODE = 9000;
-	const CODE_NO_TYPE = 1;
-	const CODE_NO_PATH = 2;
-	const ACCESS_WITHOUT_CHECK = 3;
-	const ACCESS_DENIED = 10;
-	const NO_CHECKING_INSTANCE = 4;
+	const CODE_NO_TYPE = 9001;
+	const CODE_NO_PATH = 9002;
+	const ACCESS_WITHOUT_CHECK = 9003;
+	const NO_CHECKING_INSTANCE = 9004;
+	const WRONG_PATH_TYPE = 9005;
+	const ACCESS_DENIED = 9010;
 	/**
 	 * @var array
 	 */
 	protected static $messages = array(
 		self::CODE_NO_TYPE => 'No type for Path-Signing selected',
+		self::WRONG_PATH_TYPE => 'This path-type cannot be signed',
 		self::CODE_NO_PATH => 'No path for checking available',
 		self::ACCESS_WITHOUT_CHECK => 'the requested file cannot be delivered since it is not checked yet',
 		self::NO_CHECKING_INSTANCE => 'This path is not secured by a class',
@@ -31,7 +32,7 @@ class ilWACException extends ilException {
 	 * @param int $code
 	 */
 	public function __construct($code) {
-		parent::__construct(self::$messages[$code], self::BASE_CODE + $code);
+		parent::__construct(self::$messages[$code], $code);
 	}
 }
 
