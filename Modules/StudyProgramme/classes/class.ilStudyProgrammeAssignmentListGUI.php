5d8c5b88bc84aeca554fd329d0ff2c7c43bbe65a
Richard Klees <richard.klees@concepts-and-training.de>
2015-08-05 16:30:26 +0200

    i can haz progress bar.

--- Modules/StudyProgramme/classes/class.ilStudyProgrammeAssignmentListGUI.php
+++ Modules/StudyProgramme/classes/class.ilStudyProgrammeAssignmentListGUI.php
@@ -30,6 +30,7 @@ class ilStudyProgrammeAssignmentListGUI {
 	function __construct(ilStudyProgrammeUserAssignment $a_assignment) {
 		global $lng;
 		$this->il_lng = $lng;
+		$this->il_lng->loadLanguageModule("prg");
 		
 		$this->assignment = $a_assignment;
 		$this->tpl = null;
@@ -38,15 +39,17 @@ class ilStudyProgrammeAssignmentListGUI {
 	
 	public function getHTML() {
 		if ($this->html === null) {
-			$prg = $this->assignment->getStudyProgramme();
+			$programme = $this->assignment->getStudyProgramme();
+			$progress = $programme->getProgressForAssignment($this->assignment->getId());
 			
 			$tpl = $this->getTemplate("Modules/StudyProgramme", static::$tpl_file);
-			$tpl->setVariable("TXT_TITLE", $prg->getTitle());
-			$tpl->setVariable("TXT_DESC", $prg->getDescription());
-			$tpl->setVariable("SRC_ICON", $this->getIconPath($prg->getId()));
-			$tpl->setVariable("ALT_ICON", $this->getAltIcon($prg->getId()));
-			$tpl->setVariable("HREF_TITLE", $this->getTargetForProgramme($prg->getId()));
-			$tpl->setVariable("ICON_HREF", $this->getTargetForProgramme($prg->getId()));
+			$tpl->setVariable("TXT_TITLE", $programme->getTitle());
+			$tpl->setVariable("TXT_DESC", $programme->getDescription());
+			$tpl->setVariable("SRC_ICON", $this->getIconPath($programme->getId()));
+			$tpl->setVariable("ALT_ICON", $this->getAltIcon($programme->getId()));
+			$tpl->setVariable("HREF_TITLE", $this->getTargetForProgramme($programme->getId()));
+			$tpl->setVariable("ICON_HREF", $this->getTargetForProgramme($programme->getId()));
+			$tpl->setVariable("PROGRESS_BAR", $this->buildProgressBar($progress));
 			
 			$this->html = $tpl->get();
 		}
@@ -67,6 +70,52 @@ class ilStudyProgrammeAssignmentListGUI {
 	
 	protected function getTargetForProgramme($a_obj_id) {
 		return "http://www.google.de";
+	}
+	
+	protected function buildProgressBar(ilStudyProgrammeUserProgress $a_progress) {
+		return $this->buildProgressBarRaw(10, 40, 30);
+	}
+
+	protected function buildProgressBarRaw($a_tooltip_id, $a_result_in_percent, $a_limit_in_percent) {
+		assert(is_int($a_tooltip_id));
+		assert(is_int($a_result_in_percent));
+		assert($a_result_in_percent > 0);
+		assert($a_result_in_percent <= 100);
+		assert(is_int($a_limit_in_percent));
+		assert($a_limit_in_percent > 0);
+		assert($a_limit_in_percent <= 100);
+		
+		// Shameless copy of ilContainerObjectiveGUI::buildObjectiveProgressBar with modifications.
+		// I wish i could just use it, but there are some crs specific things that aren't parametrized...
+		$tpl = new ilTemplate("tpl.objective_progressbar.html", true, true, "Services/Container");
+					
+		$tt_txt = sprintf( $this->il_lng->txt("prg_progress_info")
+						 , $a_result_in_percent
+						 , $a_limit_in_percent
+						 );
+		
+		if($a_result_in_percent > $a_limit_in_percent) {
+			$bar_color = "#80f080";
+		}
+		else {
+			$bar_color = "#f08080";
+		}
+		
+		$limit_pos = (121-ceil(125/100*$a_limit_in_percent))*-1;
+		
+		$tpl->setCurrentBlock("statusbar_bl");
+		$tpl->setVariable("PERC_STATUS", $a_result_in_percent);
+		$tpl->setVariable("LIMIT_POS", $limit_pos);
+		$tpl->setVariable("PERC_WIDTH", $a_result_in_percent);
+		$tpl->setVariable("PERC_COLOR", $bar_color);
+		$tpl->setVariable("BG_COLOR", "#fff");
+		$tpl->setVariable("TT_ID", $a_tooltip_id);
+		$tpl->parseCurrentBlock();
+		
+		include_once("./Services/UIComponent/Tooltip/classes/class.ilTooltipGUI.php");
+		ilTooltipGUI::addTooltip($a_tooltip_id, $tt_txt);
+		
+		return $tpl->get();
 	}
 }
 
