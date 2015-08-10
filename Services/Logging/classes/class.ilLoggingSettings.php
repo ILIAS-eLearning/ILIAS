3a7110a35d907c2b714a5cd9764df290b0f1c589
smeyer <smeyer.ilias@gmx.de>
2015-08-10 13:45:38 +0200

    [logging] Added settings

--- Services/Logging/classes/class.ilLoggingSettings.php
+++ Services/Logging/classes/class.ilLoggingSettings.php
@@ -2,6 +2,7 @@
 /* Copyright (c) 1998-2015 ILIAS open source, Extended GPL, see docs/LICENSE */
 
 include_once './Services/Logging/classes/public/class.ilLogLevel.php';
+include_once './Services/Administration/classes/class.ilSetting.php';
 
 /** 
 * @defgroup ServicesLogging Services/Logging
@@ -16,6 +17,14 @@ class ilLoggingSettings
 {
 	protected static $instance = null;
 	
+	private $storage = null;
+	
+	private $level = null;
+	private $cache = FALSE;
+	private $cache_level = null;
+	private $memory_usage = FALSE;
+	
+	
 	/**
 	 * Singleton contructor
 	 *
@@ -23,6 +32,11 @@ class ilLoggingSettings
 	 */
 	private function __construct()
 	{
+		$this->level = ilLogLevel::INFO;
+		$this->cache_level = ilLogLevel::DEBUG;
+		
+		$this->storage = new ilSetting('logging');
+		
 		$this->read();
 	}
 
@@ -41,6 +55,14 @@ class ilLoggingSettings
 	}
 	
 	/**
+	 * @return ilSetting
+	 */
+	protected function getStorage()
+	{
+		return $this->storage;
+	}
+	
+	/**
 	 * Get log level
 	 * @return type
 	 */
@@ -48,6 +70,67 @@ class ilLoggingSettings
 	{
 		return ilLogLevel::INFO;
 	}
+	
+	/**
+	 * Set log level
+	 * @param type $a_level
+	 */
+	public function setLevel($a_level)
+	{
+		$this->level = $a_level;
+	}
+	
+	/**
+	 * Set cache level
+	 * @param type $a_level
+	 */
+	public function setCacheLevel($a_level)
+	{
+		$this->cache_level = $a_level;
+	}
+	
+	/**
+	 * Get cache level
+	 * @return type
+	 */
+	public function getCacheLevel()
+	{
+		return $this->cache_level;
+	}
+	
+	/**
+	 * Enable caching
+	 * @param type $a_status
+	 */
+	public function enableCaching($a_status)
+	{
+		$this->cache = $a_status;
+	}
+	
+	public function isCacheEnabled()
+	{
+		return $this->cache;
+	}
+	
+	/**
+	 * Enable logging of memory usage
+	 * @param type $a_stat
+	 */
+	public function enableMemoryUsage($a_stat)
+	{
+		$this->memory_usage = $a_stat;
+	}
+	
+	/**
+	 * Check if loggin of memory usage is enabled
+	 * @return type
+	 */
+	public function isMemoryUsageEnabled()
+	{
+		return $this->memory_usage;
+	}
+	
+	
 	
 	/**
 	 * 
@@ -73,7 +156,10 @@ class ilLoggingSettings
 	 */
 	public function update()
 	{
-		global $ilSetting;
+		$this->getStorage()->set('level', $this->getLevel());
+		$this->getStorage()->set('cache', (int) $this->isCacheEnabled());
+		$this->getStorage()->set('cache_level', $this->getCacheLevel());
+		$this->getStorage()->set('memory_usage', $this->isMemoryUsageEnabled());
 	}
 
 	
@@ -84,7 +170,10 @@ class ilLoggingSettings
 	 */
 	private function read()
 	{
-		global $ilSetting;
+		$this->setLevel($this->getStorage()->get('level',$this->level));
+		$this->enableCaching($this->getStorage()->get('cache',$this->cache));
+		$this->setCacheLevel($this->getStorage()->get('cache_level',$this->cache_level));
+		$this->enableMemoryUsage($this->getStorage()->get('memory_usage', $this->memory_usage));
 	}
 }
 ?>
