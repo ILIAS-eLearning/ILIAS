ff4ae416f552d4ee467d3d1d18d23c967ca17847
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2015-08-12 12:37:25 +0000

    gev2: Boyscouting globals
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali2_dev@60388 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/GEV/Mailing/classes/class.gevDeadlineMailingJob.php
+++ Services/GEV/Mailing/classes/class.gevDeadlineMailingJob.php
@@ -32,6 +32,11 @@ class gevDeadlineMailingJob extends ilCronJob {
 	}
 	
 	public function initCronMailData() {
+		global $ilDB, $ilLog;
+
+		$this->db = $ilDB;
+		$this->log = $ilLog;
+
 		require_once("Services/GEV/Mailing/classes/CrsMails/class.gevParticipationStatusNotSet.php");
 		
 		$this->deadline_jobs = array(
@@ -49,12 +54,12 @@ class gevDeadlineMailingJob extends ilCronJob {
 	}
 	
 	static public function isMailSend($a_crs_id, $a_mail_id) {
-		global $ilDB;
-		$res = $ilDB->query("SELECT COUNT(*) cnt FROM gev_crs_dl_mail_cron ".
-						    " WHERE crs_id = ".$ilDB->quote($a_crs_id, "integer").
-						    "   AND title = ".$ilDB->quote($a_mail_id, "text").
+		
+		$res = $this->db->query("SELECT COUNT(*) cnt FROM gev_crs_dl_mail_cron ".
+						    " WHERE crs_id = ".$this->db->quote($a_crs_id, "integer").
+						    "   AND title = ".$this->db->quote($a_mail_id, "text").
 						    "   AND NOT send_at IS NULL");
-		$rec = $ilDB->fetchAssoc($res);
+		$rec = $this->db->fetchAssoc($res);
 		return $rec["cnt"] > 0;
 	}
 	
@@ -63,9 +68,8 @@ class gevDeadlineMailingJob extends ilCronJob {
 		require_once("Services/GEV/Utils/classes/class.gevAMDUtils.php");
 		require_once("Services/GEV/Utils/classes/class.gevSettings.php");
 		
-		global $ilLog, $ilDB;
-		
 		$this->initCronMailData();
+		
 		$end_date_field_id = gevSettings::getInstance()->getAMDFieldId(gevSettings::CRS_AMD_END_DATE);
 		$start_date_field_id = gevSettings::getInstance()->getAMDFieldId(gevSettings::CRS_AMD_START_DATE);
 		$is_template_field_id = gevSettings::getInstance()->getAMDFieldId(gevSettings::CRS_AMD_IS_TEMPLATE);
@@ -84,41 +88,41 @@ class gevDeadlineMailingJob extends ilCronJob {
 				 "    ON cs.obj_id = oref.obj_id".
 				 "  JOIN adv_md_values_text is_template ".
 				 "    ON cs.obj_id = is_template.obj_id ".
-				 "   AND is_template.field_id = ".$ilDB->quote($is_template_field_id, "integer").
+				 "   AND is_template.field_id = ".$this->db->quote($is_template_field_id, "integer").
 				 "  LEFT JOIN adv_md_values_date end_date ".
 				 "    ON cs.obj_id = end_date.obj_id ".
-				 "   AND end_date.field_id = ".$ilDB->quote($end_date_field_id, "integer").
+				 "   AND end_date.field_id = ".$this->db->quote($end_date_field_id, "integer").
 				 "  JOIN adv_md_values_date start_date ".
 				 "    ON cs.obj_id = start_date.obj_id ".
-				 "   AND start_date.field_id = ".$ilDB->quote($start_date_field_id, "integer").
+				 "   AND start_date.field_id = ".$this->db->quote($start_date_field_id, "integer").
 				 " WHERE (  ( ADDDATE(end_date.value, ".$this->max_after_course_end." + ".$safety_margin.")".
-				 "            >= ".$ilDB->quote(date("Y-m-d"), "date").")".
+				 "            >= ".$this->db->quote(date("Y-m-d"), "date").")".
 				 "       OR ( end_date.value IS NULL AND ".
 				 "            ADDDATE(start_date.value, ".$this->max_after_course_end." + ".$safety_margin.")".
-				 "            >= ".$ilDB->quote(date("Y-m-d"), "date").")".
+				 "            >= ".$this->db->quote(date("Y-m-d"), "date").")".
 				 "       )".
 				 "   AND is_template.value <> '".gevSettings::YES."'".
 				 "   AND oref.deleted IS NULL".
 				 "";
 		
-		$res = $ilDB->query($query);
+		$res = $this->db->query($query);
 		$now = new ilDateTime(time(), IL_CAL_UNIX);
 		
-		while ($rec = $ilDB->fetchAssoc($res)) {
+		while ($rec = $this->db->fetchAssoc($res)) {
 			$crs_id = $rec["obj_id"];
-			$ilLog->write("ilDeadlineMailingJob::run: Checking course ".$crs_id.".");
+			$this->log->write("ilDeadlineMailingJob::run: Checking course ".$crs_id.".");
 			$auto_mails = new gevCrsAutoMails($crs_id);
 			
 			// determine which mails where already send for the course and which need to be
 			// send
-			$res2 = $ilDB->query("SELECT title FROM gev_crs_dl_mail_cron WHERE crs_id = ".$ilDB->quote($crs_id, "integer"));
+			$res2 = $this->db->query("SELECT title FROM gev_crs_dl_mail_cron WHERE crs_id = ".$this->db->quote($crs_id, "integer"));
 			$mails_send = array();
-			while($rec2 = $ilDB->fetchAssoc($res2)) {
+			while($rec2 = $this->db->fetchAssoc($res2)) {
 				$mails_send[] = $rec2["title"];
 			}
 			$mails_to_send = array_diff($this->deadline_jobs, $mails_send);
 			
-			$ilLog->write("ilDeadlineMailingJob::run: mails to send = ".implode(", ", $mails_to_send));
+			$this->log->write("ilDeadlineMailingJob::run: mails to send = ".implode(", ", $mails_to_send));
 			
 			// send the mails that need to be send and store the fact, the mails where send, in the
 			// deadline mailing table.
@@ -127,32 +131,32 @@ class gevDeadlineMailingJob extends ilCronJob {
 				$scheduled_time = $mail->getScheduledFor();
 				
 				if ($scheduled_time === null) {
-					$ilLog->write("ilDeadlineMailingJob::run: Expected mail ".$key." to have a scheduled time, but got none.");
+					$this->log->write("ilDeadlineMailingJob::run: Expected mail ".$key." to have a scheduled time, but got none.");
 					continue;
 				}
 				
-				$ilLog->write("lDeadlineMailingJob::run: Mail ".$key." scheduled for ".$scheduled_time->get(IL_CAL_DATETIME).".");
+				$this->log->write("lDeadlineMailingJob::run: Mail ".$key." scheduled for ".$scheduled_time->get(IL_CAL_DATETIME).".");
 				
 				if ( !ilDateTime::_before($scheduled_time, $now) ) {
 					continue;
 				}
 				
 				if ($mail->shouldBeSend()) {
-					$ilLog->write("ilDeadlineMailingJob::run: Send mail ".$key.".");
+					$this->log->write("ilDeadlineMailingJob::run: Send mail ".$key.".");
 					try {
 						$mail->send();
 					}
 					catch (Exception $e) {
-						$ilLog->write("ilDeadlineMailingJob::run: error when sending mail ".$key.".");
+						$this->log->write("ilDeadlineMailingJob::run: error when sending mail ".$key.".");
 					}
 				}
 				else {
-					$ilLog->write("ilDeadlineMailingJob:run: No need to send Mail.");
+					$this->log->write("ilDeadlineMailingJob:run: No need to send Mail.");
 				}
 				
-				$ilDB->manipulate("INSERT INTO gev_crs_dl_mail_cron (crs_id, title, send_at) VALUES ".
-								  "    ( ".$ilDB->quote($crs_id, "integer").
-								  "    , ".$ilDB->quote($key, "text").
+				$this->db->manipulate("INSERT INTO gev_crs_dl_mail_cron (crs_id, title, send_at) VALUES ".
+								  "    ( ".$this->db->quote($crs_id, "integer").
+								  "    , ".$this->db->quote($key, "text").
 								  "    , NOW()".
 								  "    )"
 								 );
