12aa3c4b5b8ff40d58118bc80d68f752fda26638
Richard Klees <richard.klees@concepts-and-training.de>
2015-08-13 16:09:11 +0200

    added db update step for study programme

--- setup/sql/dbupdate_04.php
+++ setup/sql/dbupdate_04.php
@@ -8701,6 +8701,54 @@ if(!$ilDB->tableColumnExists('exc_assignment','peer_crit_cat'))
 <#4622>
 <?php
 
+require_once("./Modules/StudyProgramme/classes/model/class.ilStudyProgramme.php");
+require_once("./Modules/StudyProgramme/classes/model/class.ilStudyProgrammeAssignment.php");
+require_once("./Modules/StudyProgramme/classes/model/class.ilStudyProgrammeProgress.php");
+
+ilStudyProgramme::installDB();
+ilStudyProgrammeAssignment::installDB();
+ilStudyProgrammeProgress::installDB();
+
+// Active Record does not support tuples as primary keys, so we have to
+// set those on our own.
+$ilDB->addUniqueConstraint( ilStudyProgrammeProgress::returnDbTableName()
+						  , array("assignment_id", "prg_id", "usr_id")
+						  );
+
+// ActiveRecord seems to not interpret con_is_null correctly, so we have to set
+// it manually.
+$ilDB->modifyTableColumn( ilStudyProgrammeProgress::returnDbTableName()
+						, "completion_by"
+						, array( "notnull" => false
+							   , "default" => null
+							   )
+						);
+$ilDB->modifyTableColumn( ilStudyProgrammeProgress::returnDbTableName()
+						, "last_change_by"
+						, array( "notnull" => false
+							   , "default" => null
+							   )
+						);
+
+require_once("./Services/Migration/DBUpdate_3560/classes/class.ilDBUpdateNewObjectType.php");
+$obj_type_id = ilDBUpdateNewObjectType::addNewType("prg", "StudyProgramme");
+$existing_ops = array("visible", "write", "copy", "delete", "edit_permission");
+foreach ($existing_ops as $op) {
+	$op_id = ilDBUpdateNewObjectType::getCustomRBACOperationId($op);
+	ilDBUpdateNewObjectType::addRBACOperation($obj_type_id, $op_id);		
+}
+
+require_once("./Modules/StudyProgramme/classes/model/class.ilStudyProgrammeAdvancedMetadataRecord.php");
+require_once("./Modules/StudyProgramme/classes/model/class.ilStudyProgrammeType.php");
+require_once("./Modules/StudyProgramme/classes/model/class.ilStudyProgrammeTypeTranslation.php");
+
+ilStudyProgrammeAdvancedMetadataRecord::installDB();
+ilStudyProgrammeType::installDB();
+ilStudyProgrammeTypeTranslation::installDB();
+
+?><#4622>
+<?php
+
 include_once('./Services/Migration/DBUpdate_3560/classes/class.ilDBUpdateNewObjectType.php');				
 $blog_type_id = ilDBUpdateNewObjectType::getObjectTypeId('blog');
 if($blog_type_id)
