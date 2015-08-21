a038a80c66ee99acc9f74d66f2f81ac06359f96c
smeyer-ilias <smeyer.ilias@gmx.de>
2015-08-21 10:43:33 +0200

    lok3 #30

--- Modules/Course/classes/Objectives/class.ilLOEditorGUI.php
+++ Modules/Course/classes/Objectives/class.ilLOEditorGUI.php
@@ -742,7 +742,9 @@ class ilLOEditorGUI
 		}
 		if(!$form instanceof ilPropertyFormGUI)
 		{
-			$form = $this->initTestForm(TRUE);
+			include_once './Modules/Course/classes/Objectives/class.ilLOTestAssignmentForm.php';
+			$form_helper = new ilLOTestAssignmentForm($this,$this->getParentObject(), $this->getTestType());
+			$form = $form_helper->initForm(TRUE);
 		}
 		$GLOBALS['tpl']->setContent($form->getHTML());
 		
@@ -773,7 +775,9 @@ class ilLOEditorGUI
 		
 		if(!$form instanceof ilPropertyFormGUI)
 		{
-			$form = $this->initTestForm();
+			include_once './Modules/Course/classes/Objectives/class.ilLOTestAssignmentForm.php';
+			$form_helper = new ilLOTestAssignmentForm($this, $this->getParentObject(),$this->getTestType());
+			$form = $form_helper->initForm(FALSE);
 		}
 		$GLOBALS['tpl']->setContent($form->getHTML());
 		
@@ -783,173 +787,7 @@ class ilLOEditorGUI
 				ilLOEditorStatus::SECTION_QTEST
 		);
 	}
-	
-	/**
-	 * Get assignable tests
-	 */
-	protected function getAssignableTests()
-	{
-		include_once './Modules/Course/classes/Objectives/class.ilLOTestAssignments.php';
-		$assignments = ilLOTestAssignments::getInstance($this->getParentObject()->getId());
-
-		$tests = array();
-		foreach($GLOBALS['tree']->getChildsByType($this->getParentObject()->getRefId(),'tst') as $tree_node)
-		{
-			if(!in_array($tree_node['child'], $assignments->getTests()))
-			{
-				$tests[] = $tree_node['child'];
-			}
-		}
-		return $tests;
-	}
-	
-	/**
-	 * Show test config form
-	 * @return \ilPropertyFormGUI
-	 */
-	protected function initTestForm($a_as_multi_assignment = FALSE)
-	{
-		include_once './Services/Form/classes/class.ilPropertyFormGUI.php';
-		$form = new ilPropertyFormGUI();
-		$form->setTitle($this->lng->txt('crs_loc_tst_assignment'));
-		$form->setFormAction($this->ctrl->getFormAction($this));
 		
-		if($a_as_multi_assignment)
-		{
-			$form->addCommandButton('saveMultiTestAssignment', $this->lng->txt('save'));
-		}
-		else
-		{
-			$form->addCommandButton('saveTest', $this->lng->txt('save'));
-		}
-		
-		switch($this->getTestType())
-		{
-			case self::TEST_TYPE_IT:
-				$form->setTitle($this->lng->txt('crs_loc_settings_itest_tbl'));
-				break;
-			
-			case self::TEST_TYPE_QT:
-				$form->setTitle($this->lng->txt('crs_loc_settings_qtest_tbl'));
-				break;
-				
-		}
-
-		$assignable = $this->getAssignableTests();
-
-		$cr_mode = new ilRadioGroupInputGUI($this->lng->txt('crs_loc_form_assign_it'),'mode');
-		$cr_mode->setRequired(true);
-		$cr_mode->setValue(self::TEST_NEW);
-		
-		$new = new ilRadioOption($this->lng->txt('crs_loc_form_tst_new'),self::TEST_NEW);
-
-		switch($this->getTestType())
-		{
-			case ilLOSettings::TYPE_TEST_INITIAL:
-				$new->setInfo($this->lng->txt("crs_loc_form_tst_new_initial_info"));
-				break;
-
-			case ilLOSettings::TYPE_TEST_QUALIFIED:
-				$new->setInfo($this->lng->txt("crs_loc_form_tst_new_qualified_info"));
-				break;
-		}
-
-		// title
-		$ti = new ilTextInputGUI($this->lng->txt("title"), "title");
-		$ti->setMaxLength(128);
-		$ti->setSize(40);
-		$ti->setRequired(true);
-		$new->addSubItem($ti);
-
-		// description
-		$ta = new ilTextAreaInputGUI($this->lng->txt("description"), "desc");
-		$ta->setCols(40);
-		$ta->setRows(2);
-		$new->addSubItem($ta);
-		
-		// Question assignment type
-		include_once './Modules/Test/classes/class.ilObjTest.php';
-		$this->lng->loadLanguageModule('assessment');
-		$qst = new ilRadioGroupInputGUI($this->lng->txt('tst_question_set_type'),'qtype');
-		$qst->setRequired(true);
-		
-		$random = new ilRadioOption(
-				$this->lng->txt('tst_question_set_type_random'),
-				ilObjTest::QUESTION_SET_TYPE_RANDOM
-		);
-		$qst->addOption($random);
-		
-		$fixed = new ilRadioOption(
-				$this->lng->txt('tst_question_set_type_fixed'),
-				ilObjTest::QUESTION_SET_TYPE_FIXED
-		);
-		$qst->addOption($fixed);
-		$new->addSubItem($qst);
-		$cr_mode->addOption($new);
-		
-		// assign existing
-		$existing = new ilRadioOption($this->lng->txt('crs_loc_form_assign'),self::TEST_ASSIGN);
-
-		switch($this->getTestType())
-		{
-			case ilLOSettings::TYPE_TEST_INITIAL:
-				$existing->setInfo($this->lng->txt("crs_loc_form_assign_initial_info"));
-				break;
-
-			case ilLOSettings::TYPE_TEST_QUALIFIED:
-				$existing->setInfo($this->lng->txt("crs_loc_form_assign_qualified_info"));
-				break;
-		}
-
-		if(!$assignable)
-		{
-			$existing->setDisabled(true);
-		}
-		$cr_mode->addOption($existing);
-		
-		$options = array();
-		$options[0] = $this->lng->txt('select_one');
-		foreach((array) $assignable as $tst_ref_id)
-		{
-			$tst_obj_id = ilObject::_lookupObjId($tst_ref_id);
-			$options[$tst_ref_id] = ilObject::_lookupTitle($tst_obj_id);
-		}
-		$selectable = new ilSelectInputGUI($this->lng->txt('crs_loc_form_available_tsts'),'tst');
-		$selectable->setRequired(true);
-		$selectable->setOptions($options);
-		$existing->addSubItem($selectable);
-		
-		$form->addItem($cr_mode);
-		
-		
-		if($a_as_multi_assignment)
-		{
-			include_once './Modules/Course/classes/Objectives/class.ilLOTestAssignments.php';
-			$assignments = ilLOTestAssignments::getInstance($this->getParentObject()->getId());
-			
-			include_once './Modules/Course/classes/class.ilCourseObjective.php';
-			$objective_ids = ilCourseObjective::_getObjectiveIds($this->getParentObject()->getId(), FALSE);
-
-			$options = array();
-			$options[0] = $this->lng->txt('select_one');
-			foreach($objective_ids as $oid)
-			{
-				$already_assigned_tst = $assignments->getTestByObjective($oid, $this->getTestType());
-				if(!$already_assigned_tst)
-				{
-					$options[$oid] = ilCourseObjective::lookupObjectiveTitle($oid);
-				}
-			}
-			
-			$objective = new ilSelectInputGUI($this->lng->txt('crs_objectives'),'objective');
-			$objective->setRequired(TRUE);
-			$objective->setOptions($options);
-			$form->addItem($objective);
-		}	
-		
-		return $form;
-	}
-
 	/**
 	 * Apply auto generated setttings template
 	 * @param ilObjTest $tst
@@ -1022,7 +860,10 @@ class ilLOEditorGUI
 		
 		$settings = ilLOSettings::getInstanceByObjId($this->getParentObject()->getId());
 		
-		$form = $this->initTestForm(TRUE);
+		include_once './Modules/Course/classes/Objectives/class.ilLOTestAssignmentForm.php';
+		$form_helper = new ilLOTestAssignmentForm($this,$this->getParentObject(), $this->getTestType());
+		$form = $form_helper->initForm(TRUE);
+		
 		if($form->checkInput())
 		{
 			$mode = $form->getInput('mode');
@@ -1090,7 +931,10 @@ class ilLOEditorGUI
 		
 		$settings = ilLOSettings::getInstanceByObjId($this->getParentObject()->getId());
 		
-		$form = $this->initTestForm();
+		include_once './Modules/Course/classes/Objectives/class.ilLOTestAssignmentForm.php';
+		$form_helper = new ilLOTestAssignmentForm($this, $this->getParentObject(),$this->getTestType());
+		$form = $form_helper->initForm(FALSE);
+		
 		if($form->checkInput())
 		{
 			$mode = $form->getInput('mode');
--- Modules/Course/classes/Objectives/class.ilLOEditorStatus.php
+++ Modules/Course/classes/Objectives/class.ilLOEditorStatus.php
@@ -176,13 +176,27 @@ class ilLOEditorStatus
 			if(!$this->getInitialTestStatus(false))
 			{
 				$_REQUEST['tt'] = ilLOSettings::TYPE_TEST_INITIAL;
-				return 'testOverview';
+				if($this->getSettings()->hasSeparateInitialTests())
+				{
+					return 'testsOverview';
+				}
+				else
+				{
+					return 'testOverview';
+				}
 			}
 		}
 		if(!$this->getQualifiedTestStatus(false))
 		{
 			$_REQUEST['tt'] = ilLOSettings::TYPE_TEST_QUALIFIED;
-			return 'testOverview';
+			if($this->getSettings()->hasSeparateQualifiedTests())
+			{
+				return 'testsOverview';
+			}
+			else
+			{
+				return 'testOverview';
+			}
 		}
 		if(!$this->getObjectivesStatus(false))
 		{
@@ -251,10 +265,13 @@ class ilLOEditorStatus
 		{
 			$done = $this->getInitialTestStatus();
 
-			$this->ctrl->setParameter($this->getCmdClass(),'tt', 1);
-
+			$command = $this->getSettings()->hasSeparateInitialTests() ? 
+					'testsOverview' :
+					'testOverview';
+			
+			
 			$list->addEntry($this->lng->txt('crs_objective_status_itest'),
-				$this->ctrl->getLinkTarget($this->getCmdClass(),'testOverview'),
+				$this->ctrl->getLinkTarget($this->getCmdClass(),$command),
 				$done
 					? ilChecklistGUI::STATUS_OK
 					: ilChecklistGUI::STATUS_NOT_OK,
@@ -266,11 +283,15 @@ class ilLOEditorStatus
 		// Step 4
 		// course qtest
 		$done = $this->getQualifiedTestStatus();
+		
+		$command = $this->getSettings()->hasSeparateQualifiedTests() ? 
+				'testsOverview' :
+				'testOverview';
 
 		$this->ctrl->setParameter($this->getCmdClass(),'tt', 2);
 
 		$list->addEntry($this->lng->txt('crs_objective_status_qtest'),
-			$this->ctrl->getLinkTarget($this->getCmdClass(),'testOverview'),
+			$this->ctrl->getLinkTarget($this->getCmdClass(),$command),
 			$done
 				? ilChecklistGUI::STATUS_OK
 				: ilChecklistGUI::STATUS_NOT_OK,
