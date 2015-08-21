fbd5300b98eb8030c9cb726d1705503195d5e075
smeyer <smeyer.ilias@gmx.de>
2015-08-21 15:13:27 +0200

    Added db update steps

--- setup/sql/dbupdate_04.php
+++ setup/sql/dbupdate_04.php
@@ -9122,3 +9122,154 @@ $ilDB->query('ALTER TABLE il_dcl_view ADD INDEX (type)');
 $ilDB->query('ALTER TABLE il_dcl_data ADD INDEX (main_table_id)');
 $ilDB->query('ALTER TABLE il_dcl_table ADD INDEX (obj_id)');
 ?>
+
+<#4660>
+<?php
+
+if(!$ilDB->tableColumnExists('loc_settings','it_type')) 
+{
+    $ilDB->addTableColumn(
+        'loc_settings',
+        'it_type',
+        array(
+            'type' => 'integer',
+			'length' => 1,
+            'notnull' => false,
+            'default' => 5
+        ));
+}
+?>
+<#4661>
+<?php
+
+if(!$ilDB->tableColumnExists('loc_settings','qt_type')) 
+{
+    $ilDB->addTableColumn(
+        'loc_settings',
+        'qt_type',
+        array(
+            'type' => 'integer',
+			'length' => 1,
+            'notnull' => false,
+            'default' => 1
+        ));
+}
+
+?>
+
+<#4662>
+<?php
+
+if(!$ilDB->tableColumnExists('loc_settings','it_start')) 
+{
+    $ilDB->addTableColumn(
+        'loc_settings',
+        'it_start',
+        array(
+            'type' => 'integer',
+			'length' => 1,
+            'notnull' => false,
+            'default' => 1
+        ));
+}
+
+?>
+
+<#4663>
+<?php
+
+if(!$ilDB->tableColumnExists('loc_settings','qt_start')) 
+{
+    $ilDB->addTableColumn(
+        'loc_settings',
+        'qt_start',
+        array(
+            'type' => 'integer',
+			'length' => 1,
+            'notnull' => false,
+            'default' => 1
+        ));
+}
+?>
+
+<#4664>
+<?php
+
+
+$query = 'UPDATE loc_settings SET it_type = '.$ilDB->quote(1,'integer').' WHERE type = '.$ilDB->quote(1,'integer');
+$res = $ilDB->manipulate($query);
+
+?>
+
+<#4665>
+<?php
+
+
+$query = 'UPDATE loc_settings SET qt_start = '.$ilDB->quote(0,'integer').' WHERE type = '.$ilDB->quote(4,'integer');
+$res = $ilDB->manipulate($query);
+
+?>
+
+<#4666>
+<?php
+
+if(!$ilDB->tableExists('loc_tst_assignments'))
+{
+	$ilDB->createTable('loc_tst_assignments', array(
+		'assignment_id' => array(
+			'type' => 'integer',
+			'length' => 4,
+			'notnull' => true,
+			'default' => 0
+		),
+		'container_id' => array(
+			'type' => 'integer',
+			'length' => 4,
+			'notnull' => true,
+			'default' => 0
+		),
+		'assignment_type' => array(
+			'type' => 'integer',
+			'length' => 1,
+			'notnull' => true,
+			'default' => 0
+		),
+		'objective_id' => array(
+			'type' => 'integer',
+			'length' => 4,
+			'notnull' => true,
+			'default' => 0
+		),
+		'tst_ref_id' => array(
+			'type' => 'integer',
+			'length' => 4,
+			'notnull' => true,
+			'default' => 0
+		)
+	));
+
+	$ilDB->addPrimaryKey('loc_tst_assignments', array('assignment_id'));
+	$ilDB->createSequence('loc_tst_assignments');
+
+}
+?>
+
+
+<#4667>
+<?php
+
+if(!$ilDB->tableColumnExists('loc_settings','passed_obj_mode')) 
+{
+    $ilDB->addTableColumn(
+        'loc_settings',
+        'passed_obj_mode',
+        array(
+            'type' => 'integer',
+			'length' => 1,
+            'notnull' => false,
+            'default' => 1
+        ));
+}
+?>
+
+
