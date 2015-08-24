ac41d684202cc21db75a13a5cc9d6b8867ef3253
Alex Killing <alex.killing@gmx.de>
2015-08-24 19:16:55 +0200

    awareness tool, system/course contacts, part xiii

--- setup/sql/dbupdate_04.php
+++ setup/sql/dbupdate_04.php
@@ -9799,3 +9799,77 @@ if(!$ilDB->numRows($res))
 	);
 }
 ?>
+<#4683>
+<?php
+if(!$ilDB->tableColumnExists('obj_members','contact'))
+{
+	$ilDB->addTableColumn(
+		'obj_members',
+		'contact',
+		array(
+			'type' => 'integer',
+			'length' => 1,
+			'notnull' => false,
+			'default' => 0
+		));
+}
+?>
+<#4684>
+<?php
+	// register new object type 'awra' for awareness tool administration
+	$id = $ilDB->nextId("object_data");
+	$ilDB->manipulateF("INSERT INTO object_data (obj_id, type, title, description, owner, create_date, last_update) ".
+		"VALUES (%s, %s, %s, %s, %s, %s, %s)",
+		array("integer", "text", "text", "text", "integer", "timestamp", "timestamp"),
+		array($id, "typ", "awra", "Awareness Tool Administration", -1, ilUtil::now(), ilUtil::now()));
+	$typ_id = $id;
+
+	// create object data entry
+	$id = $ilDB->nextId("object_data");
+	$ilDB->manipulateF("INSERT INTO object_data (obj_id, type, title, description, owner, create_date, last_update) ".
+		"VALUES (%s, %s, %s, %s, %s, %s, %s)",
+		array("integer", "text", "text", "text", "integer", "timestamp", "timestamp"),
+		array($id, "awra", "__AwarenessToolAdministration", "Awareness Tool Administration", -1, ilUtil::now(), ilUtil::now()));
+
+	// create object reference entry
+	$ref_id = $ilDB->nextId('object_reference');
+	$res = $ilDB->manipulateF("INSERT INTO object_reference (ref_id, obj_id) VALUES (%s, %s)",
+		array("integer", "integer"),
+		array($ref_id, $id));
+
+	// put in tree
+	$tree = new ilTree(ROOT_FOLDER_ID);
+	$tree->insertNode($ref_id, SYSTEM_FOLDER_ID);
+
+	// add rbac operations
+	// 1: edit_permissions, 2: visible, 3: read, 4:write
+	$ilDB->manipulateF("INSERT INTO rbac_ta (typ_id, ops_id) VALUES (%s, %s)",
+		array("integer", "integer"),
+		array($typ_id, 1));
+	$ilDB->manipulateF("INSERT INTO rbac_ta (typ_id, ops_id) VALUES (%s, %s)",
+		array("integer", "integer"),
+		array($typ_id, 2));
+	$ilDB->manipulateF("INSERT INTO rbac_ta (typ_id, ops_id) VALUES (%s, %s)",
+		array("integer", "integer"),
+		array($typ_id, 3));
+	$ilDB->manipulateF("INSERT INTO rbac_ta (typ_id, ops_id) VALUES (%s, %s)",
+		array("integer", "integer"),
+		array($typ_id, 4));
+?>
+<#4685>
+<?php
+	$ilCtrlStructureReader->getStructure();
+?>
+<#4686>
+<?php
+	$ilCtrlStructureReader->getStructure();
+?>
+<#4687>
+<?php
+	$ilCtrlStructureReader->getStructure();
+?>
+<#4688>
+<?php
+	$s = new ilSetting("awrn");
+	$s->set("max_nr_entries", 50);
+?>
