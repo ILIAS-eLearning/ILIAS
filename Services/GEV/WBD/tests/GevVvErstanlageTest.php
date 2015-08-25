67ac4e557168f5d3d3ebb110b6c1664ce453ed5d
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2015-08-25 13:53:56 +0000

    gev2: test VvErstanlage added
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali2_new_wbd@60501 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ Services/GEV/WBD/tests/GevVvErstanlageTest.php
@@ -0,0 +1,137 @@
+<?php
+class GevVvErstanlageTest extends RequestTestBase {
+	
+	public function setUp() {
+		$this->request = new gevWBDRequestVvErstanlage();
+	}
+
+	public function test_isImplmentedRequest() {
+		$this->assertInstanceOf("WBDRequest",$this->request);
+	}
+
+	public function xml_response_success() {
+		return array(simplexml_load_string('<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">'
+												.'<soap:Body>'
+												.'<ns1:putResponse xmlns:ns1="http://erstanlage.stammdaten.external.service.wbd.gdv.de/">'
+												.'<ErstanlageRueckgabewert>'
+													.'<TpInterneVermittlerId>7665</TpInterneVermittlerId>'
+													.'<VermittlerId>20150728-100390-74</VermittlerId>'
+													.'<AnlageDatum>2015-07-28T00:00:00+02:00</AnlageDatum>'
+													.'<BeginnZertifizierungsPeriode>2015-07-28T00:00:00+02:00</BeginnZertifizierungsPeriode>'
+												.'</ErstanlageRueckgabewert>'
+												.'</ns1:putResponse>'
+												.'</soap:Body>'
+											.'</soap:Envelope>'
+									)
+					,simplexml_load_string('<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">'
+												.'<soap:Body>'
+												.'<ns1:putResponse xmlns:ns1="http://erstanlage.stammdaten.external.service.wbd.gdv.de/">'
+												.'<ErstanlageRueckgabewert>'
+													.'<TpInterneVermittlerId>7665</TpInterneVermittlerId>'
+													.'<VermittlerId>20150728-100390-74</VermittlerId>'
+													.'<AnlageDatum>2015-07-28T00:00:00+02:00</AnlageDatum>'
+													.'<BeginnZertifizierungsPeriode>2015-07-28T00:00:00+02:00</BeginnZertifizierungsPeriode>'
+												.'</ErstanlageRueckgabewert>'
+												.'</ns1:putResponse>'
+												.'</soap:Body>'
+											.'</soap:Envelope>'
+									)
+					,simplexml_load_string('<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">'
+												.'<soap:Body>'
+												.'<ns1:putResponse xmlns:ns1="http://erstanlage.stammdaten.external.service.wbd.gdv.de/">'
+												.'<ErstanlageRueckgabewert>'
+													.'<TpInterneVermittlerId>7665</TpInterneVermittlerId>'
+													.'<VermittlerId>20150728-100390-74</VermittlerId>'
+													.'<AnlageDatum>2015-07-28T00:00:00+02:00</AnlageDatum>'
+													.'<BeginnZertifizierungsPeriode>2015-07-28T00:00:00+02:00</BeginnZertifizierungsPeriode>'
+												.'</ErstanlageRueckgabewert>'
+												.'</ns1:putResponse>'
+												.'</soap:Body>'
+											.'</soap:Envelope>'
+									)
+			);
+	}
+
+	public function xml_response_success_xml_fails() {
+		return array(simplexml_load_string('<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">'
+												.'<soap:Body>'
+												.'<ns1:putResponse xmlns:ns1="http://erstanlage.stammdaten.external.service.wbd.gdv.de/">'
+												.'<ErstanlageRueckgabewert>'
+													.'<TpIntersneVermittlerId>7665</TpIntersneVermittlerId>'
+													.'<VermittlerId>20150728-100390-74</VermittlerId>'
+													.'<AnlageDatum>2015-07-28T00:00:00+02:00</AnlageDatum>'
+													.'<BeginnZertifizierungsPeriode>2015-07-28T00:00:00+02:00</BeginnZertifizierungsPeriode>'
+												.'</ErstanlageRueckgabewert>'
+												.'</ns1:putResponse>'
+												.'</soap:Body>'
+											.'</soap:Envelope>'
+									)
+					,simplexml_load_string('<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">'
+												.'<soap:Body>'
+												.'<ns1:putResponse xmlns:ns1="http://erstanlage.stammdaten.external.service.wbd.gdv.de/">'
+												.'<ErstanlageRueckgabewert>'
+													.'<TpInterneVermittlerId>7665</TpInterneVermittlerId>'
+													.'<VermittlerId>20150728-100390-74</VermittlerId>'
+													.'<AnlagedDatum>2015-07-28T00:00:00+02:00</AnlagedDatum>'
+													.'<ZertifizierungsPeriode>2015-07-28T00:00:00+02:00</ZertifizierungsPeriode>'
+												.'</ErstanlageRueckgabewert>'
+												.'</ns1:putResponse>'
+												.'</soap:Body>'
+											.'</soap:Envelope>'
+									)
+					,simplexml_load_string('<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">'
+												.'<soap:Body>'
+												.'<ns1:putResponse xmlns:ns1="http://erstanlage.stammdaten.external.service.wbd.gdv.de/">'
+												.'<ErstanlageRueckgabewert>'
+													.'<TpInterneVermittlerId>7665</TpInterneVermittlerId>'
+													.'<VermittlerId>20150728-100390-74</VermittlerId>'
+													.'<AnlagedsaDatum>2015-07-28T00:00:00+02:00</AnlagedsaDatum>'
+													.'<BeginnZertifizierungsPeriode>2015-07-28T00:00:00+02:00</BeginnZertifizierungsPeriode>'
+												.'</ErstanlageRueckgabewert>'
+												.'</ns1:putResponse>'
+												.'</soap:Body>'
+											.'</soap:Envelope>'
+									)
+			);
+	}
+
+	public function xml_response_error() {
+		return array(simplexml_load_string('<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"">'
+												.'<soap:Body>'
+													.'<soap:Fault>'
+														.'<faultcode>soap:Server</faultcode>'
+														.'<faultstring>Der Vermittler ist deaktiviert.</faultstring>'
+														.'<detail>'
+															.'<ns1:VermittlerNichtAktivException xmlns:ns1="http://meldung.wp.external.service.wbd.gdv.de/"/>'
+														.'</detail>'
+													.'</soap:Fault>'
+												.'</soap:Body>'
+											.'</soap:Envelope>'
+									)
+					,simplexml_load_string('<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"">'
+												.'<soap:Body>'
+													.'<soap:Fault>'
+														.'<faultcode>soap:Server</faultcode>'
+														.'<faultstring>Der Vermittler ist deaktiviert.</faultstring>'
+														.'<detail>'
+															.'<ns1:VermittlerNichtAktivException xmlns:ns1="http://meldung.wp.external.service.wbd.gdv.de/"/>'
+														.'</detail>'
+													.'</soap:Fault>'
+												.'</soap:Body>'
+											.'</soap:Envelope>'
+									)
+					,simplexml_load_string('<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"">'
+												.'<soap:Body>'
+													.'<soap:Fault>'
+														.'<faultcode>soap:Server</faultcode>'
+														.'<faultstring>Der Vermittler ist deaktiviert.</faultstring>'
+														.'<detail>'
+															.'<ns1:VermittlerNichtAktivException xmlns:ns1="http://meldung.wp.external.service.wbd.gdv.de/"/>'
+														.'</detail>'
+													.'</soap:Fault>'
+												.'</soap:Body>'
+											.'</soap:Envelope>'
+									)
+			);
+	}
+}
