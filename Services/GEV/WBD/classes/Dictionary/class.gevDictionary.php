a16cf788b4b3fb59c81d558ac160f41c8243b654
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2015-08-25 13:08:20 +0000

    new gevDictionary
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali2_new_wbd@60498 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ Services/GEV/WBD/classes/Dictionary/class.gevDictionary.php
@@ -0,0 +1,126 @@
+<?php
+class gevDictionary extends Dictionary {
+	const WBD_NAME = "toWBDName";
+	const INTERNAL_NAME = "toInternalName";
+
+	const SERACH_IN_GENDER = "gender";
+	const SERACH_IN_COURSE_TYPE = "course_type";
+	const SEARCH_IN_STUDY_CONTENT = "study_content";
+	const SEARCH_IN_WBD_TYPE = "wbd_type";
+	const SERACH_IN_AGENT_STATUS = "agent_status";
+	const SERACH_IN_ADDRESS_TYPE = "assress_type";
+
+	static $mapping = array("toWBDName" 		=> array("gender" => array("m" => "001"
+																	,"f" => "002"
+																	,"w" => "002"
+															)
+														,"course_type" => array("Präsenzveranstaltung" => "001"
+																	,"Präsenztraining" => "001"
+																	,"Präsenz" => "001"
+																	,"Selbstlernkurs" => "001"
+																	,"gesteuertes E-Learning" => "004"
+																	,"Webinar" => "004"
+																	,"gesteuertes E-learning" => "004"
+																	,"gesteuertes e-learning" => "004"
+																	,"Virtuelles Training" => "004"
+																	,"Virtuelle Sitzung" => "004"
+																	,"Onlinetraining" => "005"
+																	,"Blended Learning" => "003"
+																	,"XX" => "002"
+															)
+														,"study_content" => array('Privat-Vorsorge-Lebens-/Rentenversicherung' => '001'
+																	,'Privat-Vorsorge-Kranken-/Pflegeversicherung' => '002'
+																	,'Firmenkunden-Sach-/Schadensversicherung' => '005'
+																	,'Firmenkunden-Sach-/Schadenversicherung' => '005'
+																	,'Spartenübergreifend' => '006'
+																	,'Sparten-übergreifend' => '006'
+																	,'Firmenkunden-Vorsorge (bAV/Personenversicherung)' => '004'
+																	,'Beratungskompetenz' => '007'
+																	,'Privat-Sach-/Schadenversicherung' => '003'
+															)
+														,"wbd_type" => array("3 - TP-Service" => "ja"
+																	,"2 - TP-Service" => "nein"
+															)
+														,"agent_status" => array("0 - aus Stellung" => "006"
+																	,"0 - aus Rolle" => "006"
+																	,"1 - Angestellter Außendienst" => "002"
+																	,"2 - Ausschließlichkeitsvermittler" => "001"
+																	,"3 - Makler" => "003"
+																	,"4 - Mehrfachagent" => "004"
+																	,"5 - Mitarbeiter eines Vermittlers" => "005"
+																	,"6 - Sonstiges" => "006"
+																	,"7 - keine Zuordnung"  => "006"
+																	  
+																	,"aus Stellung" => "006"
+																	,"aus Rolle" => "006"
+																	,"Angestellter Außendienst" => "002"
+																	,"Ausschließlichkeitsvermittler" => "001"
+																	,"Makler" => "003"
+																	,"Mehrfachagent" => "004"
+																	,"Mitarbeiter eines Vermittlers" => "005"
+																	,"Sonstiges" => "006"
+																	,"keine Zuordnung"  => "006"
+															)
+														,"address_type" => array("privat" => "001"
+																	,"geschäftlich" => "002"
+																	,"sonstiges" => "010"
+															)
+								),
+							  "toInternalName" 	=> array("gender" => array("001" => "m"
+																	,"002" => "f"
+															)
+														,"course_type" => array("001" => "Präsenz-Veranstaltung"
+																	,"002" => "Einzeltraining"
+																	,"003" => "Blended Learning"
+																	,"004" => "gesteuertes E-Learning"
+																	,"005" => "selbstgesteuertes E-Learning"
+															)
+														,"study_content" => array("001" => "Privat-Vorsorge-Lebens-/Rentenversicherung"
+																	,"002" => "Privat-Vorsorge-Kranken-/Pflegeversicherung"
+																	,"003" => "Privat-Sach-/Schadenversicherung"
+																	,"004" => "Firmenkunden-Vorsorge (bAV/Personenversicherung)"
+																	,"005" => "Firmenkunden-Sach-/Schadenversicherung"
+																	,"006" => "Sparten-übergreifend"
+																	,"007" => "Beratungskompetenz"
+															)
+														,"wbd_type" => array("ja" => "3 - TP-Service"
+																	,"nein" => "2 - TP-Basis"
+															)
+														,"agent_status" => array("001" => "Ausschließlichkeitsvermittler"
+																	,"002" => "Angestellter Außendienst"
+																	,"003" => "Makler"
+																	,"004" => "Mehrfachagent"
+																	,"005" => "Mitarbeiter eines Vermittlers"
+																	,"006" => "Sonstiges"
+															)
+														,"address_type" => array("001" => "privat"
+																	,"002" => "geschäftlich"
+																	,"010" => "sonstige"
+															)
+								)
+							);
+	
+	public function getWBDName($key, $section) {
+		return $this->getName($key, $section, self::WBD_NAME);
+	}
+
+	function getInternalName($key, $section) {
+		return $name = $this->getName($key, $section, self::INTERNAL_NAME);
+	}
+
+	private function getName($key, $section, $direction) {
+		if(!array_key_exists($direction,self::$mappings)) {
+			throw new LogicException("direction ".$direction." not found");
+		}
+
+		if(!array_key_exists($section,self::$mappings[$direction])) {
+			throw new LogicException("section ".$section." not found in direction ".$direction);
+		}
+
+		if(!array_key_exists($key,self::$mappings[$direction][$section])) {
+			throw new LogicException("value not found for key ".$key." in section ".$section." for direction ".$direction);
+		}
+
+		return self::$mapping[$dorection][$section][$key];
+	}
+}
