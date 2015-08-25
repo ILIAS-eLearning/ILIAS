76d1fd9ab54114cd56fc8ee2417cb32ac917b074
Uwe-Kohnle <kohnle@internetlehrer-gmbh.de>
2015-08-25 17:33:25 +0200

    SCORM 2004 - fix issues with jquery when having special chars in id

--- Modules/Scorm2004/scripts/buildrte/rte.js
+++ Modules/Scorm2004/scripts/buildrte/rte.js
@@ -1,4 +1,4 @@
-// Build: 2015420173949 
+// Build: 2015711221954 
 /*
 	+-----------------------------------------------------------------------------+
 	| ILIAS open source                                                           |
@@ -10887,12 +10887,10 @@ function toggleTree() {
 	
 	if (treeState==false) {
 		elm.innerHTML="Collapse All";
-		//treeYUI.expandAll();
 		il.NestedList.expandAll('rte_tree');
 		treeState=true;
 	} else {
 		elm.innerHTML="Expand All";
-		//treeYUI.collapseAll();
 		il.NestedList.collapseAll('rte_tree');
 		treeState=false;
 	}
@@ -12494,7 +12492,7 @@ function onDocumentClick (e)
 		{
 			//throw away API from previous sco and sync CMI and ADLTree
 			//onItemUndeliver();
-			mlaunch = msequencer.navigateStr( target.id.substr(3).replace(/_____/g,'.'));
+			mlaunch = msequencer.navigateStr( target.id.substr(3));
 
  			if (mlaunch.mSeqNonContent == null) {
 				//alert(activities[mlaunch.mActivityID]);
@@ -12721,8 +12719,8 @@ function buildNavTree(rootAct,name,tree){
 	{
 		var id=rootAct.id;
 		if (rootAct.isvisible==true && typeof(mlaunch.mNavState.mChoice[id])=="object") {
-			var it_id=(ITEM_PREFIX + rootAct.id).replace(/\./g,"_____");
-			il.NestedList.addNode('rte_tree', (""+par_id).replace(/\./g,"_____"), it_id,
+			var it_id=(ITEM_PREFIX + rootAct.id);
+			il.NestedList.addNode('rte_tree', (""+par_id), it_id,
 				"<a href='#this' id='" + it_id + "' target='_self'>" + rootAct.title + "</a>",
 				true);
 			par_id = ITEM_PREFIX + rootAct.id;
@@ -12736,8 +12734,8 @@ function buildNavTree(rootAct,name,tree){
 				var id=rootAct.item[i].id;
 				if (mlaunch.mNavState.mChoice!=null) {
 					if (rootAct.item[i].isvisible==true && typeof(mlaunch.mNavState.mChoice[id])=="object") {
-						var it_id=(ITEM_PREFIX + rootAct.item[i].id).replace(/\./g,"_____");
-						il.NestedList.addNode('rte_tree', (""+par_id).replace(/\./g,"_____"), it_id,
+						var it_id=(ITEM_PREFIX + rootAct.item[i].id);
+						il.NestedList.addNode('rte_tree', (""+par_id), it_id,
 							"<a href='#this' id='" + it_id + "' target='_self'>" + rootAct.item[i].title + "</a>",
 							true);
 						var next_par_id = ITEM_PREFIX + rootAct.item[i].id;
@@ -14035,7 +14033,7 @@ function onItemDeliverDo(item, wasSuspendAll) // onDeliver called from sequencin
 		item.parameters = "?"+ item.parameters;
 	} 
 	openedResource=[item.id, item.href+randNumber+item.parameters, this.config.package_url];
-	guiItemId = (ITEM_PREFIX + item.id).replace(/\./g,"_____");
+	guiItemId = (ITEM_PREFIX + item.id);
 	updateNav();
 	updateControls();
 	setResource();
@@ -14578,7 +14576,7 @@ function updateNav(ignore) {
 				// continue;
 			// }
 		// }
-		var elm = all(ITEM_PREFIX + tree[i].mActivityID.replace(/\./g,"_____"));
+		var elm = all(ITEM_PREFIX + tree[i].mActivityID);
 		// if (guiItem && ignore==true) {
 			// signActNode();
 			// continue;
@@ -14832,7 +14830,6 @@ var sharedData = new Array();
 var msequencer=new ADLSequencer();
 var mlaunch=null;
 var adlnavreq=null;
-var treeYUI=null;
 var logState=false;
 var treeState=true;
 
