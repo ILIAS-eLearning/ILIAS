0d7a4053281e79d57e0fb67c49c4b9ce50fcaf48
smeyer <smeyer.ilias@gmx.de>
2015-08-26 11:00:54 +0200

    LDAP multiple: # db, languages

--- setup/sql/dbupdate_04.php
+++ setup/sql/dbupdate_04.php
@@ -10067,3 +10067,32 @@ if ($ilDB->tableExists('search_tree'))
 		);
 	}
 ?>
+
+
+<#4699>
+<?php
+
+if(!$ilDB->tableColumnExists("ldap_server_settings", "username_filter"))
+{
+        $ilDB->addTableColumn("ldap_server_settings", "username_filter", array(
+                'type' => 'text',
+                'length' => 255,
+        ));
+}
+?>
+<#4700>
+<?php
+$query = "SELECT max(server_id) id FROM ldap_server_settings";
+$res = $ilDB->query($query);
+$set = $res->fetchRow(DB_FETCHMODE_OBJECT);
+
+if(!$set->id)
+{
+        $set->id = 1;
+}
+
+$query = "UPDATE ldap_role_assignments ".
+        "SET server_id = ".$set->id.
+        " WHERE server_id = 0";
+$this->db->manipulate($query);
+?>
