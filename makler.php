ddf9f9fa561990c61ad4cf8f488ae0ddbcd2d98d
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2015-08-27 08:41:38 +0000

    gev2: change ilUser makler offer
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali2@60537 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- makler.php
+++ makler.php
@@ -11,7 +11,12 @@
 */
 
 require_once("Services/Init/classes/class.ilInitialisation.php");
+require_once("Services/GEV/Utils/classes/class.gevSettings.php");
+require_once("Services/User/classes/class.ilObjUser.php");
+
 ilInitialisation::initILIAS();
+$GLOBALS["ilUser"] = new ilObjUser(gevSettings::getInstance()->getAgentOfferUserId());
+
 
 if (!isset($_GET["baseClass"])) {
 	$ilCtrl->initBaseClass("gevAgentOfferGUI");
