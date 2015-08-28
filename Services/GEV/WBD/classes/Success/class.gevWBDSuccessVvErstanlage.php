d2c126d326b6bbdb0869727f38352d20e31c47aa
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2015-08-28 07:13:09 +0000

    gev2: convert response string in required entity type
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali2_new_wbd@60545 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/GEV/WBD/classes/Success/class.gevWBDSuccessVvErstanlage.php
+++ Services/GEV/WBD/classes/Success/class.gevWBDSuccessVvErstanlage.php
@@ -1,4 +1,6 @@
 <?php
+chdir("/Library/WebServer/Documents/dev/4_4_generali2_new_wbd/");
+require_once("Services/Calendar/classes/class.ilDate.php");
 class gevWBDSuccessVvErstanlage extends WBDSuccess {
 	protected $internal_agent_id;
 	protected $agent_id;
@@ -9,12 +11,26 @@ class gevWBDSuccessVvErstanlage extends WBDSuccess {
 	const AGENT_ID = "VermittlerId";
 	const CREATE_DATE = "AnlageDatum";
 	const BEGIN_OF_CERTIFICATION_PERIOD = "BeginnZertifizierungsPeriode";
+	const DATE_SPLITTER = "T";
 
 	public function __construct($response) {
-		$this->internal_agent_id = $this->nodeValue($response,self::INTERNAL_AGENT_ID);
+		
+		$internal_agent_id = $this->nodeValue($response,self::INTERNAL_AGENT_ID);
+
+		if(!is_numeric($internal_agent_id)) {
+			throw new LogicException ("gevWBDSuccessVvErstanlage::__construct:internal agent ist is not a number");
+		}
+		$this->internal_agent_id = (int)$internal_agent_id;
+		
 		$this->agent_id = $this->nodeValue($response,self::AGENT_ID);
-		$this->create_date = $this->nodeValue($response,self::CREATE_DATE);
-		$this->begin_of_certification_period = $this->nodeValue($response,self::BEGIN_OF_CERTIFICATION_PERIOD);
+
+		$create_date = $this->nodeValue($response,self::CREATE_DATE);
+		$split = explode($create_date,self::DATE_SPLITTER);
+		$this->create_date = new ilDate($split[0],IL_CAL_DATE);
+
+		$begin_of_certification_period = $this->nodeValue($response,self::BEGIN_OF_CERTIFICATION_PERIOD);
+		$split = explode($begin_of_certification_period,self::DATE_SPLITTER);
+		$this->begin_of_certification_period = new ilDate($split[0],IL_CAL_DATE);
 	}
 
 	public function internalAgentId() {
@@ -25,7 +41,7 @@ class gevWBDSuccessVvErstanlage extends WBDSuccess {
 		return $this->internal_agent_id;
 	}
 
-	public function AgentId() {
+	public function agentId() {
 		if($this->agent_id === null) {
 			throw new LogicException("gevWBDSuccessVvErstanlage::AgentId:agent_id is NULL");
 		}
