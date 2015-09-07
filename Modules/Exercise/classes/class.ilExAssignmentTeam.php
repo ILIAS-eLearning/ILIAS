7b1481c8793151e65ed63cdfee593149bb34a76c
Fabian Wolf <wolf@leifos.com>
2015-09-07 15:28:56 +0200

    adding primarys for tables ecs_part_settings, il_exc_team_log, il_verification, il_wiki_imp_pages, il_wiki_missing_page and lo_access; removed tables: feedback_results, feedback_items and il_log
    Conflicts:
    	setup/sql/dbupdate_custom.php

--- Modules/Exercise/classes/class.ilExAssignmentTeam.php
+++ Modules/Exercise/classes/class.ilExAssignmentTeam.php
@@ -256,15 +256,19 @@ class ilExAssignmentTeam
 	public static function writeTeamLog($a_team_id, $a_action, $a_details = null)
 	{
 		global $ilDB, $ilUser;
-		
+		//BEGIN Patch primary keys
+		$id = $ilDB->nextId('il_exc_team_log');
+
 		$fields = array(
+			"log_id" => array("integer", $id),
 			"team_id" => array("integer", $a_team_id),
 			"user_id" => array("integer", $ilUser->getId()),
 			"action" => array("integer", $a_action),
 			"details" => array("text", $a_details),
 			"tstamp" => array("integer", time())
 		);
-		
+		//END Patch primary keys
+
 		$ilDB->insert("il_exc_team_log", $fields);
 	}
 	
