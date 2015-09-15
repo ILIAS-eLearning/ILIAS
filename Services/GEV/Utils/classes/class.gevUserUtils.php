9762b7040cdad2b9c19fabb74b32d2f974e7dfbc
Denis Klöpfer <denis.kloepfer@concepts-and-training.de>
2015-09-15 11:07:29 +0000

    gev2 require dbvUtils in userUtils, since it is used within, e.g. in getUVGBDOrCPoolNames, but not directly required yet. this might have caused errors later on
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali2_dev@60706 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/GEV/Utils/classes/class.gevUserUtils.php
+++ Services/GEV/Utils/classes/class.gevUserUtils.php
@@ -15,6 +15,7 @@ require_once("Services/CourseBooking/classes/class.ilCourseBooking.php");
 require_once("Services/CourseBooking/classes/class.ilUserCourseBookings.php");
 require_once("Services/GEV/Utils/classes/class.gevAMDUtils.php");
 require_once("Services/GEV/Utils/classes/class.gevUDFUtils.php");
+require_once("Services/GEV/Utils/classes/class.gevDBVUtils.php");
 require_once("Services/GEV/Utils/classes/class.gevCourseUtils.php");
 require_once("Services/GEV/Utils/classes/class.gevSettings.php");
 require_once("Services/GEV/Utils/classes/class.gevRoleUtils.php");
