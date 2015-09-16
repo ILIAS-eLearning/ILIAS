723ecf3b89a6e84a8890cc609f128df2dbfb0ccd
bheyser <bheyser@databay.de>
2015-09-16 14:32:20 +0200

    moved question navigation above question itself

--- Modules/TestQuestionPool/classes/class.assQuestionGUI.php
+++ Modules/TestQuestionPool/classes/class.assQuestionGUI.php
@@ -336,7 +336,7 @@ abstract class assQuestionGUI
 	{
 		if( $this->getNavigationGUI() )
 		{
-			$html .= $this->getNavigationGUI()->getHTML();
+			$html = $this->getNavigationGUI()->getHTML().$html;
 		}
 		
 		$postponed = "";
