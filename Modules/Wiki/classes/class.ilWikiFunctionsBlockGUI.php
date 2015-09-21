a7852e613f931c040fcfcf248bbfdf3b33e8c13e
Alex Killing <alex.killing@gmx.de>
2015-09-21 11:08:07 +0200

    fixed bug #15950: read-only page can be renamed by normal course member

--- Modules/Wiki/classes/class.ilWikiFunctionsBlockGUI.php
+++ Modules/Wiki/classes/class.ilWikiFunctionsBlockGUI.php
@@ -221,7 +221,8 @@ class ilWikiFunctionsBlockGUI extends ilBlockGUI
 			}
 		}
 
-		if ($ilAccess->checkAccess("edit_content", "", $_GET["ref_id"]))
+		if (($ilAccess->checkAccess("edit_content", "", $_GET["ref_id"]) && !$this->getPageObject()->getBlocked())
+			|| $ilAccess->checkAccess("write", "", $_GET["ref_id"]))
 		{
 			// rename
 			$list->addItem($lng->txt("wiki_rename_page"), "",
