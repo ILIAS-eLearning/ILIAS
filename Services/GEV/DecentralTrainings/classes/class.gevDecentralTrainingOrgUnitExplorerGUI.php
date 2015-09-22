a293508cee5a333bd03a683308389dba450027bc
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2015-09-22 07:25:56 +0000

    gev2: 1548 advice added at org unit selection
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali2_dev@60813 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/GEV/DecentralTrainings/classes/class.gevDecentralTrainingOrgUnitExplorerGUI.php
+++ Services/GEV/DecentralTrainings/classes/class.gevDecentralTrainingOrgUnitExplorerGUI.php
@@ -47,4 +47,116 @@ class gevDecentralTrainingOrgUnitExplorerGUI extends ilTEPOrgUnitExplorerGUI {
 
 		return array();
 	}
+
+	/**
+	 * Get HTML
+	 */
+	function getHTML()
+	{
+		global $tpl, $ilCtrl, $lng;
+
+		$this->beforeRendering();
+
+		$tpl->addJavascript(self::getLocalExplorerJsPath());
+		$tpl->addJavascript(self::getLocalJsTreeJsPath());
+		
+		$container_id = $this->getContainerId();
+		$container_outer_id = "il_expl2_jstree_cont_out_".$this->getId();
+		
+		// collect open nodes
+		$open_nodes = array($this->getDomNodeIdForNodeId($this->getNodeId($this->getRootNode())));
+		foreach ($this->open_nodes as $nid)
+		{
+			$open_nodes[] = $this->getDomNodeIdForNodeId($nid);
+		}
+		foreach ($this->custom_open_nodes as $nid)
+		{
+			$dnode = $this->getDomNodeIdForNodeId($nid);
+			if (!in_array($dnode, $open_nodes))
+			{
+				$open_nodes[] = $dnode;
+			}
+		}
+
+		// ilias config options
+		$url = "";
+		if (!$this->getOfflineMode())
+		{
+			if (is_object($this->parent_obj))
+			{
+				$url = $ilCtrl->getLinkTarget($this->parent_obj, $this->parent_cmd, "", true);
+			}
+			else
+			{
+				$url = $ilCtrl->getLinkTargetByClass($this->parent_obj, $this->parent_cmd, "", true);
+			}
+		}
+		$config = array(
+			"container_id" => $container_id,
+			"container_outer_id" => $container_outer_id,
+			"url" => $url,
+			"ajax" => $this->getAjax(),
+			);
+		
+		
+		// jstree config options
+		$js_tree_config = array(
+			"core" => array(
+				"animation" => 300,
+				"initially_open" => $open_nodes,
+				"open_parents" => false,
+				"strings" => array("loading" => "Loading ...", new_node => "New node")
+				),
+			"plugins" => array("html_data", "themes"),
+			"themes" => array("dots" => false, "icons" => false, "theme" => ""),
+			"html_data" => array()
+			);
+		
+		$tpl->addOnLoadCode('il.Explorer2.init('.json_encode($config).', '.json_encode($js_tree_config).');');
+		
+		//gev patch start
+		$etpl = new ilTemplate("tpl.explorer2_decentral.html", true, true, "Services/GEV/DecentralTrainings");
+		$etpl->setVariable("ADVICE",$lng->txt("gev_dec_training_org_unit_advice"));
+		//gev patch end
+
+
+
+		// render childs
+		$root_node = $this->getRootNode();
+		
+		if (!$this->getSkipRootNode() &&
+			$this->isNodeVisible($this->getRootNode()))
+		{
+			$this->listStart($etpl);
+			$this->renderNode($this->getRootNode(), $etpl);
+			$this->listEnd($etpl);
+		}
+		else
+		{		
+			$childs = $this->getChildsOfNode($this->getNodeId($root_node));
+			$childs = $this->sortChilds($childs, $this->getNodeId($root_node));
+			$any = false;
+			foreach ($childs as $child_node)
+			{
+				if ($this->isNodeVisible($child_node))
+				{
+					if (!$any)
+					{
+						$this->listStart($etpl);
+						$any = true;
+					}
+					$this->renderNode($child_node, $etpl);
+				}
+			}
+			if ($any)
+			{
+				$this->listEnd($etpl);
+			}
+		}
+		
+		$etpl->setVariable("CONTAINER_ID", $container_id);
+		$etpl->setVariable("CONTAINER_OUTER_ID", $container_outer_id);
+
+		return $etpl->get();
+	}
 }
