7afcde577488d1720d51be8ef2913304c5940a07
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2015-09-23 06:37:44 +0000

    gev2: schedule pdf ready
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali2@60827 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/GEV/DecentralTrainings/classes/class.gevDecentralTrainingCreateSchedulePDF.php
+++ Services/GEV/DecentralTrainings/classes/class.gevDecentralTrainingCreateSchedulePDF.php
@@ -7,12 +7,8 @@ require_once 'Services/Utilities/classes/class.ilUtil.php';
 require_once 'Services/Calendar/classes/class.ilDatePresentation.php';
 
 /**
- * @author  Maximilian Frings <mfrings@databay.de>
- * @author  Michael Jansen <mjansen@databay.de>
+ * @author  Stefan Hecken <stefan.hecken@concepts-and-training.de> 
  * @version $Id$
- *
- * ATTENTION: DO NOT EVER REUSE AN INSTANCE OF THIS CLASS AFTER A BILL PDF
- *            WAS CREATED.
  *
  */
 class gevDecentralTrainingCreateSchedulePDF
@@ -55,18 +51,24 @@ class gevDecentralTrainingCreateSchedulePDF
 		$this->blocks 	= gevCourseBuildingBlockUtils::getAllCourseBuildingBlocks(gevObjectUtils::getRefId($a_crs_id));
 		$this->pdf 		= new ilPDFHelper($this->mode, $this->unit, $this->format);
 		
+		$this->spaceLeft = 1.0;
+		$this->spaceRight = 1.0;
+		$this->lastYValue = 0;
+		$this->maxRight = 28;
+		$this->maxPageHeight = 19;
+
 		$this->headFontSize = 11;
 		$this->headerFontName = "Arial";
 		$this->headerText = "Ablaufplan";
 		$this->headerBold = "B";
-		$this->headerSpaceTop = 2.5;
+		$this->headerSpaceTop = 1.5;
 
 		$this->crsInfoFontSize = 10;
 		$this->crsInfoFontName = "Arial";
 		$this->crsInfoText = "Generelle Angaben zum Training";
 		$this->crsInfoBold = "B";
 		$this->crsInfoNoBold = "";
-		$this->crsInfoSpaceTop = 4.0;
+		$this->crsInfoSpaceTop = 3.0;
 		$this->crsInfoSpaceTopAdd = 0.5;
 		$this->crsInfoSecondColoumnSpaceLeft = 7;
 
@@ -78,12 +80,10 @@ class gevDecentralTrainingCreateSchedulePDF
 		$this->crsBlockSpaceTopAdd = 0.5;
 		$this->crsBlockSecondColoumnSpaceLeft = 7;
 		$this->crsBlockColoumnSpace = 0.3;
-		
-		$this->spaceLeft = 1.0;
-		$this->spaceRight = 1.0;
-		$this->lastYValue = 0;
-		$this->maxRight = 28;
 		$this->number_couloums_after_time = 3;
+		$this->crsBlockMultiplikatorColoumnWidth = $this->number_couloums_after_time + 1.2;
+		
+		
 
 	}
 
@@ -147,24 +147,28 @@ class gevDecentralTrainingCreateSchedulePDF
 
 		$this->pdf->SetFont($this->crsInfoFontName, $this->crsInfoBold, $this->crsInfoFontSize);
 		$this->pdf->WriteText($this->spaceLeft, $this->crsInfoSpaceTop, $this->crsInfoText);
-		$this->pdf->WriteText($this->spaceLeft, $this->crsInfoSpaceTop + (2 * $this->crsInfoSpaceTopAdd), "Titel");
-		$this->pdf->WriteText($this->spaceLeft, $this->crsInfoSpaceTop + (3 * $this->crsInfoSpaceTopAdd), "Untertitel");
-		$this->pdf->WriteText($this->spaceLeft, $this->crsInfoSpaceTop + (4 * $this->crsInfoSpaceTopAdd), "Nummer der Maßnahme");
-		$this->pdf->WriteText($this->spaceLeft, $this->crsInfoSpaceTop + (5 * $this->crsInfoSpaceTopAdd), "Datum");
-		$this->pdf->WriteText($this->spaceLeft, $this->crsInfoSpaceTop + (6 * $this->crsInfoSpaceTopAdd), "Veranstaltungsort");
-		$this->pdf->WriteText($this->spaceLeft, $this->crsInfoSpaceTop + (7 * $this->crsInfoSpaceTopAdd), "Bildungspunkte");
-		$this->pdf->WriteText($this->spaceLeft, $this->crsInfoSpaceTop + (8 * $this->crsInfoSpaceTopAdd), "Trainer");
-		$this->pdf->WriteText($this->spaceLeft, $this->crsInfoSpaceTop + ((9 + $jump_trainers) * $this->crsInfoSpaceTopAdd), "Trainingsbetreuer");
-		$this->pdf->WriteText($this->spaceLeft, $this->crsInfoSpaceTop + ((10 + $jump_trainers) * $this->crsInfoSpaceTopAdd), "Fachlich verantwortlich");
-		$this->pdf->WriteText($this->spaceLeft, $this->crsInfoSpaceTop + ((11 + $jump_trainers) * $this->crsInfoSpaceTopAdd), "Bei Rückfragen");
+
+		$this->pdf->Line($this->spaceLeft,$this->pdf->getY() + 0.2,$this->maxRight,$this->pdf->getY() + 0.2);
+		$this->pdf->Line($this->spaceLeft,$this->pdf->getY() + 0.3,$this->maxRight,$this->pdf->getY() + 0.3);
+
+		$this->pdf->WriteText($this->spaceLeft, $this->crsInfoSpaceTop + (2 * $this->crsInfoSpaceTopAdd), $this->encodeSpecialChars("Titel"));
+		$this->pdf->WriteText($this->spaceLeft, $this->crsInfoSpaceTop + (3 * $this->crsInfoSpaceTopAdd), $this->encodeSpecialChars("Untertitel"));
+		$this->pdf->WriteText($this->spaceLeft, $this->crsInfoSpaceTop + (4 * $this->crsInfoSpaceTopAdd), $this->encodeSpecialChars("Nummer der Maßnahme"));
+		$this->pdf->WriteText($this->spaceLeft, $this->crsInfoSpaceTop + (5 * $this->crsInfoSpaceTopAdd), $this->encodeSpecialChars("Datum"));
+		$this->pdf->WriteText($this->spaceLeft, $this->crsInfoSpaceTop + (6 * $this->crsInfoSpaceTopAdd), $this->encodeSpecialChars("Veranstaltungsort"));
+		$this->pdf->WriteText($this->spaceLeft, $this->crsInfoSpaceTop + (7 * $this->crsInfoSpaceTopAdd), $this->encodeSpecialChars("Bildungspunkte"));
+		$this->pdf->WriteText($this->spaceLeft, $this->crsInfoSpaceTop + (8 * $this->crsInfoSpaceTopAdd), $this->encodeSpecialChars("Trainer"));
+		$this->pdf->WriteText($this->spaceLeft, $this->crsInfoSpaceTop + ((9 + $jump_trainers) * $this->crsInfoSpaceTopAdd), $this->encodeSpecialChars("Trainingsbetreuer"));
+		$this->pdf->WriteText($this->spaceLeft, $this->crsInfoSpaceTop + ((10 + $jump_trainers) * $this->crsInfoSpaceTopAdd), $this->encodeSpecialChars("Fachlich verantwortlich"));
+		$this->pdf->WriteText($this->spaceLeft, $this->crsInfoSpaceTop + ((11 + $jump_trainers) * $this->crsInfoSpaceTopAdd), $this->encodeSpecialChars("Bei Rückfragen"));
 
 		$this->pdf->SetFont($this->crsInfoFontName, $this->crsInfoNoBold, $this->crsInfoFontSize);
-		$this->pdf->WriteText($this->crsInfoSecondColoumnSpaceLeft, $this->crsInfoSpaceTop + (2 * $this->crsInfoSpaceTopAdd), $meta_data["Titel"]);
-		$this->pdf->WriteText($this->crsInfoSecondColoumnSpaceLeft, $this->crsInfoSpaceTop + (3 * $this->crsInfoSpaceTopAdd), $meta_data["Untertitel"]);
-		$this->pdf->WriteText($this->crsInfoSecondColoumnSpaceLeft, $this->crsInfoSpaceTop + (4 * $this->crsInfoSpaceTopAdd), $meta_data["Nummer der Maßnahme"]);
-		$this->pdf->WriteText($this->crsInfoSecondColoumnSpaceLeft, $this->crsInfoSpaceTop + (5 * $this->crsInfoSpaceTopAdd), $meta_data["Datum"]);
-		$this->pdf->WriteText($this->crsInfoSecondColoumnSpaceLeft, $this->crsInfoSpaceTop + (6 * $this->crsInfoSpaceTopAdd), $meta_data["Veranstaltungsort"]);
-		$this->pdf->WriteText($this->crsInfoSecondColoumnSpaceLeft, $this->crsInfoSpaceTop + (7 * $this->crsInfoSpaceTopAdd), $meta_data["Bildungspunkte"]);
+		$this->pdf->WriteText($this->crsInfoSecondColoumnSpaceLeft, $this->crsInfoSpaceTop + (2 * $this->crsInfoSpaceTopAdd), $this->encodeSpecialChars($meta_data["Titel"]));
+		$this->pdf->WriteText($this->crsInfoSecondColoumnSpaceLeft, $this->crsInfoSpaceTop + (3 * $this->crsInfoSpaceTopAdd), $this->encodeSpecialChars($meta_data["Untertitel"]));
+		$this->pdf->WriteText($this->crsInfoSecondColoumnSpaceLeft, $this->crsInfoSpaceTop + (4 * $this->crsInfoSpaceTopAdd), $this->encodeSpecialChars($meta_data["Nummer der Maßnahme"]));
+		$this->pdf->WriteText($this->crsInfoSecondColoumnSpaceLeft, $this->crsInfoSpaceTop + (5 * $this->crsInfoSpaceTopAdd), $this->encodeSpecialChars($meta_data["Datum"]));
+		$this->pdf->WriteText($this->crsInfoSecondColoumnSpaceLeft, $this->crsInfoSpaceTop + (6 * $this->crsInfoSpaceTopAdd), $this->encodeSpecialChars($meta_data["Veranstaltungsort"]));
+		$this->pdf->WriteText($this->crsInfoSecondColoumnSpaceLeft, $this->crsInfoSpaceTop + (7 * $this->crsInfoSpaceTopAdd), $this->encodeSpecialChars($meta_data["Bildungspunkte"]));
 		
 		foreach ($trainers as $key => $value) {
 			$this->pdf->WriteText($this->crsInfoSecondColoumnSpaceLeft, $this->crsInfoSpaceTop + ((8+$key) * $this->crsInfoSpaceTopAdd), $value);
@@ -175,8 +179,9 @@ class gevDecentralTrainingCreateSchedulePDF
 								,$this->crsInfoSpaceTop + ((10 + $jump_trainers) * $this->crsInfoSpaceTopAdd)
 								, $meta_data["Fachlich verantwortlich"]);
 		$this->pdf->WriteText($this->crsInfoSecondColoumnSpaceLeft, $this->crsInfoSpaceTop + ((11 + $jump_trainers) * $this->crsInfoSpaceTopAdd), $meta_data["Bei Rückfragen"]);
-	
-		$this->lastYValue = $this->crsInfoSpaceTop + ((11 + $jump_trainers) * $this->crsInfoSpaceTopAdd);
+		
+		$this->pdf->Line($this->spaceLeft,$this->pdf->getY() + 1,$this->maxRight,$this->pdf->getY() + 1);
+		$this->pdf->Line($this->spaceLeft,$this->pdf->getY() + 1.1,$this->maxRight,$this->pdf->getY() + 1.1);
 	}
 
 	/**
@@ -184,7 +189,7 @@ class gevDecentralTrainingCreateSchedulePDF
 	*/
 	private function createBlockInfo() {
 		$date_string_width = $this->pdf->GetStringWidth("99:99:99") + $this->crsBlockColoumnSpace;
-		$coloumn_width = (($this->maxRight - $this->spaceLeft) / $this->number_couloums_after_time) - $this->crsBlockColoumnSpace;
+		$coloumn_width = (($this->maxRight - $this->spaceLeft) / $this->number_couloums_after_time) - ($this->crsBlockMultiplikatorColoumnWidth * $this->crsBlockColoumnSpace);
 
 		$x_firstColoumn = $this->spaceLeft;
 		$x_secondColoumn = $x_firstColoumn + $date_string_width;
@@ -192,45 +197,81 @@ class gevDecentralTrainingCreateSchedulePDF
 		$x_fourthColoumn = $x_thirdColoumn + $coloumn_width + $this->crsBlockColoumnSpace;
 		$x_fithColoumn = $x_fourthColoumn + $coloumn_width + $this->crsBlockColoumnSpace;
 
-		$y_value = $this->lastYValue + $this->crsBlockSpaceTop;
+		$y_value = $this->pdf->getY() + $this->crsBlockSpaceTop;
 
 		$this->pdf->SetFont($this->crsBlockFontName, $this->crsBlockBold, $this->crsBlockFontSize);
-		$this->pdf->WriteText($x_firstColoumn, $y_value, $this->lng->txt("gev_dec_crs_building_block_from"));
-		$this->pdf->WriteText($x_secondColoumn, $y_value, $this->lng->txt("gev_dec_crs_building_block_to"));
-		$this->pdf->WriteText($x_thirdColoumn, $y_value, $this->lng->txt("gev_dec_crs_building_block_block"));
-		$this->pdf->WriteText($x_fourthColoumn, $y_value, $this->lng->txt("gev_dec_crs_building_block_content"));
-		$this->pdf->WriteText($x_fithColoumn, $y_value, $this->lng->txt("gev_dec_crs_building_block_lern_dest"));
+		$this->pdf->WriteText($x_firstColoumn, $y_value, $this->encodeSpecialChars($this->lng->txt("gev_dec_crs_building_block_from")));
+		$this->pdf->WriteText($x_secondColoumn, $y_value, $this->encodeSpecialChars($this->lng->txt("gev_dec_crs_building_block_to")));
+		$this->pdf->WriteText($x_thirdColoumn, $y_value, $this->encodeSpecialChars($this->lng->txt("gev_dec_crs_building_block_block")));
+		$this->pdf->WriteText($x_fourthColoumn, $y_value, $this->encodeSpecialChars($this->lng->txt("gev_dec_crs_building_block_content")));
+		$this->pdf->WriteText($x_fithColoumn, $y_value, $this->encodeSpecialChars($this->lng->txt("gev_dec_crs_building_block_lern_dest")));
 
-		
+		$this->pdf->Line($this->spaceLeft,$y_value + 0.2,$this->maxRight,$y_value + 0.2);
+		$this->pdf->Line($this->spaceLeft,$y_value + 0.3,$this->maxRight,$y_value + 0.3);
+
+		$this->pdf->setXY($this->pdf->GetX(), $y_value + 0.3);
+
 		$this->pdf->SetFont($this->crsBlockFontName, $this->crsBlockNoBold, $this->crsBlockFontSize);
-		
-
-		$y_value = $this->pdf->GetY() + $this->crsBlockSpaceTop;
-
-		$this->pdf->SetFont($this->crsBlockFontName, $this->crsBlockNoBold, $this->crsBlockFontSize);
-		$this->pdf->WriteText($this->spaceLeft, $y_value, "99:99:99");
-		$this->pdf->WriteText($this->spaceLeft + $date_string_width, $y_value, "99:99:99");
-		/*$this->pdf->MultiCell($bla,$this->crsBlockSpaceTopAdd, "sdasdölskdjfkjsadkf ökasfjkl aslfjljsdk flksadhf  ashdfjlk dsfklhasdf aslkjfhklsdh flasdjhfka jdsfkljsdhaf aslkjfhkljsdhf asdfklj hsdaklf haslkjfh flkjasdhfk jsadfkhsalgfhglöiasdh l asdjhfkjlshaf gdhksgfkhgsjhds", 0, 'J', false);*/
-
+		$jumper = 0;
 		foreach ($this->blocks as $key => $value) {
-			$this->pdf->WriteText($this->spaceLeft, $this->lastYValue + $this->crsBlockSpaceTop, "99:99:99");
-			$this->pdf->WriteText($this->spaceLeft + $date_string_width, $this->lastYValue + $this->crsBlockSpaceTop, "99:99:99");
+			$y_value = $this->pdf->GetY() + $this->crsBlockSpaceTopAdd;
+
+			if($y_value + $max_height - (2*$this->crsBlockSpaceTopAdd) > $this->maxPageHeight) {
+				$this->pdf->AddPage();
+
+				$y_value = $this->pdf->getY() + $this->crsBlockSpaceTop;
+				$this->pdf->SetFont($this->crsBlockFontName, $this->crsBlockBold, $this->crsBlockFontSize);
+				$this->pdf->WriteText($x_firstColoumn, $y_value, $this->encodeSpecialChars($this->lng->txt("gev_dec_crs_building_block_from")));
+				$this->pdf->WriteText($x_secondColoumn, $y_value, $this->encodeSpecialChars($this->lng->txt("gev_dec_crs_building_block_to")));
+				$this->pdf->WriteText($x_thirdColoumn, $y_value, $this->encodeSpecialChars($this->lng->txt("gev_dec_crs_building_block_block")));
+				$this->pdf->WriteText($x_fourthColoumn, $y_value, $this->encodeSpecialChars($this->lng->txt("gev_dec_crs_building_block_content")));
+				$this->pdf->WriteText($x_fithColoumn, $y_value, $this->encodeSpecialChars($this->lng->txt("gev_dec_crs_building_block_lern_dest")));
+
+				$this->pdf->Line($this->spaceLeft,$y_value + 0.2,$this->maxRight,$y_value + 0.2);
+				$this->pdf->Line($this->spaceLeft,$y_value + 0.3,$this->maxRight,$y_value + 0.3);
+				$this->pdf->SetFont($this->crsBlockFontName, $this->crsBlockNoBold, $this->crsBlockFontSize);
+				$y_value += $this->crsBlockSpaceTopAdd + 0.3;
+			}
+			
+			$base = $value->getBuildingBlock();
+			$max_height = $this->calcMaxRowHeight($base->getTitle(),$base->getContent(), $base->getLearningDestination(), $coloumn_width);
+
+			$this->pdf->WriteText($x_firstColoumn, $y_value, $this->encodeSpecialChars($value->getStartTime()));
+			$this->pdf->WriteText($x_secondColoumn, $y_value, $this->encodeSpecialChars($value->getEndTime()));
+
+			$y_value -= $this->crsBlockSpaceTopAdd - (0.14 + $jumper);
+
+			$this->pdf->setXY($x_thirdColoumn - 0.1, $y_value);
+			$this->pdf->MultiCell($coloumn_width,$this->crsBlockSpaceTopAdd,$this->encodeSpecialChars($base->getTitle()));
+
+			$this->pdf->setXY($x_fourthColoumn - 0.1, $y_value);
+			$this->pdf->MultiCell($coloumn_width,$this->crsBlockSpaceTopAdd,$this->encodeSpecialChars($base->getContent()));
+
+			$this->pdf->setXY($x_fithColoumn - 0.1, $y_value);
+			$this->pdf->MultiCell($coloumn_width,$this->crsBlockSpaceTopAdd,$this->encodeSpecialChars($base->getLearningDestination()));
+
+			$this->pdf->setXY($x_firstColoumn, $y_value + $max_height - (2*$this->crsBlockSpaceTopAdd));
+			$jumper += 0.01;
 		}
 	}
 
 	private function calcMaxRowHeight($block_title,$content,$targets, $width) {
-		$h_block_title = $this->pdf->NbLines($width,$block_title);
-		$h_content = $this->pdf->NbLines($width,$content);
-		$h_targets = $this->pdf->NbLines($width,$targets);
+		$heights = array();
+		$heights[] = $this->pdf->NbLines($width,$block_title);
+		$heights[] = $this->pdf->NbLines($width,$content);
+		$heights[] = $this->pdf->NbLines($width,$targets);
 
-		if($h_block_title > $h_content && $h_block_title > $h_target) {
-			return $h_block_title;
-		}
+		return max($heights);
+	}
 
-		if($h_content > $h_block_title && $h_content > $h_target) {
-			return $h_content;
-		}
+	/**
+	 * @param  string $text
+	 * @return string
+	 */
+	private function encodeSpecialChars($text)
+	{
+		$text = iconv('UTF-8', 'windows-1252', $text);
 
-		return $h_target;
+		return $text;
 	}
 }
