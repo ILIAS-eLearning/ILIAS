6d970ea6526282731c01a12e3aa82aa5f8e8b0dd
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2015-10-09 06:59:47 +0000

    gev2: add success object
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali2_new_wbd@61044 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

---
+++ Services/GEV/WBD/classes/Success/class.gevWBDSuccessVermitVerwaltungAufnahme.php
@@ -0,0 +1,44 @@
+<?php
+/* Copyright (c) 1998-2015 ILIAS open source, Extended GPL, see docs/LICENSE */#
+
+/**
+* implementation of WBD Success for Service VermittlerVerwaltung Transferfähig machen
+*
+* @author	Stefan Hecken <shecken@concepts-and-training.de>
+* @version	$Id$
+*
+*/
+class gevWBDSuccessVermitVerwaltungAufnahme extends WBDSuccess {
+	protected $usr_id;
+	protected $row_id;
+
+	const AGENT_ID = "VermittlerId";
+	
+	public function __construct($usr_id, $row_id) {
+		if(!$usr_id) {
+			throw new LogicException("gevWBDSuccessVermitVerwaltungAufnahme: a usr_id must be provided");
+		}
+		
+		if(!$row_id) {
+			throw new LogicException("gevWBDSuccessVermitVerwaltungAufnahme: a row_id must be provided");
+		}
+
+		$this->row_id = $row_id;
+		$this->usr_id = $usr_id;
+	}
+	
+	/**
+	* gets the WBD Agent id
+	*
+	* @throws LogicException
+	* 
+	*@return string
+	*/
+	public function usrId() {
+		return $this->usr_id;
+	}
+
+	public function rowId() {
+		return $this->row_id;
+	}
+}
