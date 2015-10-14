3710bafd3282e984379faf352df837d8058e0db6
Stefan Wanzenried <stefan.wanzenried@gmail.com>
2015-10-14 14:24:23 +0200

    DataCollection: Add support for required checkboxes; #16852

---
+++ Modules/DataCollection/classes/class.ilDclCheckboxInputGUI.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once('./Services/Form/classes/class.ilCheckboxInputGUI.php');
+
+/**
+ * Class ilDclCheckboxInputGUI
+ *
+ * @author Stefan Wanzenried <sw@studer-raimann.ch>
+ */
+class ilDclCheckboxInputGUI extends ilCheckboxInputGUI
+{
+
+    public function checkInput()
+    {
+        if ($this->getRequired() && !isset($_POST[$this->getPostVar()])) {
+            global $lng;
+            $this->setAlert($lng->txt("msg_input_is_required"));
+            return false;
+        }
+
+        return parent::checkInput();
+    }
+
+}
