58b852cb59d2d65073c36a8c16ad7abcee4a7dbf
Fabian Wolf <wolf@leifos.com>
2015-10-19 15:32:39 +0200

    #16910 fix course xml-Referenz

--- Modules/Course/classes/class.ilCourseXMLWriter.php
+++ Modules/Course/classes/class.ilCourseXMLWriter.php
@@ -137,7 +137,7 @@ class ilCourseXMLWriter extends ilXmlWriter
 	// PRIVATE
 	function __buildHeader()
 	{
-		$this->xmlSetDtdDef("<!DOCTYPE Course PUBLIC \"-//ILIAS//DTD Course//EN\" \"".ILIAS_HTTP_PATH."/xml/ilias_crs_4_5.dtd\">");
+		$this->xmlSetDtdDef("<!DOCTYPE Course PUBLIC \"-//ILIAS//DTD Course//EN\" \"".ILIAS_HTTP_PATH."/xml/ilias_crs_5_0.dtd\">");
 		$this->xmlSetGenCmt("Export of ILIAS course ". $this->course_obj->getId()." of installation ".$this->ilias->getSetting('inst_id').".");
 		$this->xmlHeader();
 
