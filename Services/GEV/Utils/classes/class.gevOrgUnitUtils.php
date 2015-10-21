dc5b9490957b30b5cf741a53811694ee988887f5
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2015-10-21 15:41:34 +0200

    spx #1736 created new method to find all superiors of a user in the orgus where the user is nit superior
    Conflicts:
    	Services/GEV/Utils/classes/class.gevOrgUnitUtils.php

--- Services/GEV/Utils/classes/class.gevOrgUnitUtils.php
+++ Services/GEV/Utils/classes/class.gevOrgUnitUtils.php
@@ -966,7 +966,25 @@ class gevOrgUnitUtils {
 	}
 }
 
+public static function getSuperiorsOfUser($user_id) {
+		global $ilDB;
 
+		$sql = "SELECT DISTINCT sup_users.usr_id FROM object_data orgu
+				INNER JOIN object_reference refr ON refr.obj_id = orgu.obj_id
+				INNER JOIN object_data roles ON roles.title LIKE CONCAT('il_orgu_employee_',refr.ref_id)
+				INNER JOIN rbac_ua rbac ON rbac.usr_id = ".$ilDB->quote($user_id, "integer")." AND roles.obj_id = rbac.rol_id
+				INNER JOIN object_data sup_roles ON sup_roles.title LIKE CONCAT('il_orgu_superior_',refr.ref_id)
+				INNER JOIN rbac_ua sup_users ON sup_users.rol_id = sup_roles.obj_id
+				WHERE orgu.type = 'orgu'";
+
+		$res = $ilDB->query($sql);
+
+		$user_superiors = array();
+		while ($rec = $ilDB->fetchAssoc($res)) {
+			$user_superiors[] = $rec["usr_id"];
+		}
+		return $user_superiors;
+}
 
 class gevOrgUnitCache {
 	protected $root_id;
@@ -1007,5 +1025,3 @@ class gevOrgUnitCache {
 		return $this->cache[$import_id];
 	}
 }
-
-?>
