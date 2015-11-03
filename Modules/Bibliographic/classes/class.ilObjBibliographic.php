f1905d495df2d95a03c8f05c40ddfc9ab6b26a53
Fabian Wolf <wolf@leifos.com>
2015-11-03 13:44:35 +0100

    #17330 fix copy online status DataCollection and Survey Nr 2

--- Modules/Bibliographic/classes/class.ilObjBibliographic.php
+++ Modules/Bibliographic/classes/class.ilObjBibliographic.php
@@ -456,7 +456,6 @@ class ilObjBibliographic extends ilObject2 {
 	 * @return ilObjPoll
 	 */
 	public function doCloneObject(ilObjBibliographic $new_obj, $a_target_id, $a_copy_id = 0) {
-		$new_obj->cloneStructure($this->getId());
 
 		//copy online status if object is not the root copy object
 		$cp_options = ilCopyWizardOptions::_getInstance($a_copy_id);
@@ -465,6 +464,8 @@ class ilObjBibliographic extends ilObject2 {
 		{
 			$new_obj->setOnline($this->getOnline());
 		}
+
+		$new_obj->cloneStructure($this->getId());
 
 		return $new_obj;
 	}
@@ -480,7 +481,6 @@ class ilObjBibliographic extends ilObject2 {
 	public function cloneStructure($original_id) {
 		$original = new ilObjBibliographic($original_id);
 		$this->moveFile($original->getFileAbsolutePath());
-		$this->setOnline(false);
 		$this->setDescription($original->getDescription());
 		$this->setTitle($original->getTitle());
 		$this->setType($original->getType());
