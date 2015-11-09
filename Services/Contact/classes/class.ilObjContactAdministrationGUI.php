0a35312c6e0f8655226f46f64a465191f391d23f
mjansenDatabay <mjansen@databay.de>
2015-11-09 17:58:05 +0100

    Contacts: Administration form, change conditions for an enabled buddy system

--- Services/Contact/classes/class.ilObjContactAdministrationGUI.php
+++ Services/Contact/classes/class.ilObjContactAdministrationGUI.php
@@ -2,6 +2,7 @@
 /* Copyright (c) 1998-2015 ILIAS open source, Extended GPL, see docs/LICENSE */
 
 require_once 'Services/Object/classes/class.ilObject2GUI.php';
+require_once 'Services/Contact/BuddySystem/classes/class.ilBuddySystem.php';
 
 /**
  * Class ilObjContactAdministrationGUI
@@ -37,7 +38,7 @@ class ilObjContactAdministrationGUI extends ilObject2GUI
 	{
 		if($this->checkPermissionBool('read'))
 		{
-			$tabs_gui->addTarget('settings', $this->ctrl->getLinkTarget($this, 'configureMentorRoles'), array('showConfigurationForm', 'saveConfigurationForm'), __CLASS__);
+			$tabs_gui->addTarget('settings', $this->ctrl->getLinkTarget($this, 'showConfigurationForm'), array('', 'view', 'showConfigurationForm', 'saveConfigurationForm'), __CLASS__);
 		}
 
 		if($this->checkPermissionBool('edit_permission'))
@@ -74,11 +75,41 @@ class ilObjContactAdministrationGUI extends ilObject2GUI
 	}
 
 	/**
-	 * 
+	 * @return ilPropertyFormGUI 
 	 */
-	protected function showConfigurationForm()
+	protected function getConfigurationForm()
 	{
+		require_once 'Services/Form/classes/class.ilPropertyFormGUI.php';
+		$form = new ilPropertyFormGUI();
+		$form->setTitle($this->lng->txt('settings'));
+		$form->setFormAction($this->ctrl->getFormAction($this, 'saveConfigurationForm'));
 		
+		$enabled = new ilCheckboxInputGUI($this->lng->txt('buddy_enable'), 'enable');
+		$enabled->setInfo($this->lng->txt('buddy_enable_info'));
+		$enabled->setDisabled(!$this->checkPermissionBool('write'));
+		$form->addItem($enabled);
+
+		$form->addCommandButton('saveConfigurationForm', $this->lng->txt('save'));
+
+		return $form;
+	}
+
+	/**
+	 * @param ilPropertyFormGUI|null $form
+	 */
+	protected function showConfigurationForm(ilPropertyFormGUI $form = null)
+	{
+		$this->checkPermission('read');
+		
+		if(!($form instanceof ilPropertyFormGUI))
+		{
+			$form = $this->getConfigurationForm();
+			$form->setValuesByArray(array(
+				'enable' => (bool)ilBuddySystem::getInstance()->getSetting('enabled', 0)
+			));
+		}
+
+		$this->tpl->setContent($form->getHTML());
 	}
 
 	/**
@@ -86,6 +117,19 @@ class ilObjContactAdministrationGUI extends ilObject2GUI
 	 */
 	protected function saveConfigurationForm()
 	{
+		$this->checkPermission('write');
 
+		$form = $this->getConfigurationForm();
+		if(!$form->checkInput())
+		{
+			$form->setValuesByPost();
+			$this->showConfigurationForm($form);
+			return;
+		}
+
+		ilBuddySystem::getInstance()->setSetting('enabled', (bool)$form->getInput('enable') ? 1 : 0);
+
+		ilUtil::sendSuccess($this->lng->txt('saved_successfully'), true);
+		$this->ctrl->redirect($this);
 	}
 }
