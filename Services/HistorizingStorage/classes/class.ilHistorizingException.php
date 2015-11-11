6a18750d7dec5dd7cf56e47c60edd4efe359566f
Richard Klees <richard.klees@concepts-and-training.de>
2015-11-11 09:23:13 +0100

    new exclusiv exception class for historizing services.

---
+++ Services/HistorizingStorage/classes/class.ilHistorizingException.php
@@ -0,0 +1,12 @@
+<?php
+/* Copyright (c) 2015 Richard Klees, Extended GPL, see docs/LICENSE */
+
+require_once('./Services/Exceptions/classes/class.ilException.php');
+
+/**
+ * Exceptions from the Historizing Services.
+ * 
+ * @author Richard Klees <richard.klees@concepts-and-training.de>
+ */
+class ilHistorizingException extends ilException {
+}
