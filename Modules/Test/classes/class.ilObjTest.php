ddd72f903e0d5715e22ea4dcd69db4775199c39c
bheyser <bheyser@databay.de>
2015-11-17 15:15:57 +0100

    fixed mantis 0017452: Mehrfacher Testaufruf möglich trotz Begrenzung (AND PROBABLY SEVERAL ADDITIONAL BUGS ^^) - buxtehude :)

--- Modules/Test/classes/class.ilObjTest.php
+++ Modules/Test/classes/class.ilObjTest.php
@@ -8214,7 +8214,7 @@ function getAnswerFeedbackPoints()
 			$testPassesSelector->setActiveId($active_id);
 			$testPassesSelector->setLastFinishedPass($testSession->getLastFinishedPass());
 
-			$closedPasses = $testPassesSelector->getReportablePasses();
+			$closedPasses = $testPassesSelector->getClosedPasses();
 
 			if( count($closedPasses) >= $this->getNrOfTries() )
 			{
