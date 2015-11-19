78265dca4c37477de3dc4632f9aebc7dd0dbf701
Richard Klees <richard.klees@concepts-and-training.de>
2015-11-19 08:32:26 +0100

    check view_employee_bookings permission for superior mail (implemented #1911)

--- Modules/OrgUnit/classes/class.ilObjOrgUnitTree.php
+++ Modules/OrgUnit/classes/class.ilObjOrgUnitTree.php
@@ -128,11 +128,13 @@ class ilObjOrgUnitTree {
 
 	/**
 	 * If you want to have all orgunits where the current user has the write permission: use this with the parameter "write".
-	 * @param $operation string
+	 * @param string		$operation
+	 * @param int|null		$a_user_id
+	 * @param bool			$a_obj_and_ref_ids		Defaults to ref ids only.
 	 * @return int[] ids of the org units.
 	 */
 	// gev-patch start
-	public function getOrgusWhereUserHasPermissionForOperation($operation, $a_user_id = null) {
+	public function getOrgusWhereUserHasPermissionForOperation($operation, $a_user_id = null, $a_obj_and_ref_ids = false) {
 		if ($a_user_id === null) {
 			global $ilUser;
 			$a_user_id = $ilUser->getId();
@@ -163,7 +165,14 @@ class ilObjOrgUnitTree {
 			if(!in_array($res["op_id"], $perm_check))
 				continue;
 
-			$orgus[] = $res["ref_id"];
+			if (!$a_obj_and_ref_ids) {
+				$orgus[] = $res["ref_id"];
+			}
+			else {
+				$orgus[] = array( "obj_id" => $res["obj_id"]
+								, "ref_id" => $res["ref_id"]
+								);
+			}
 		}
 		return $orgus;
 	}
