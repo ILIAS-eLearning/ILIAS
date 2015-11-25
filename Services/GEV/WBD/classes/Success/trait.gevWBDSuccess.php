ce25c80706debb1b91434fd65e63835a1907db12
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2015-11-25 11:03:13 +0000

    working progress
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_generali2_new_wbd@61370 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/GEV/WBD/classes/Success/trait.gevWBDSuccess.php
+++ Services/GEV/WBD/classes/Success/trait.gevWBDSuccess.php
@@ -11,6 +11,15 @@
 require_once("Services/Calendar/classes/class.ilDate.php");
 trait gevWBDSuccess{
 
+	const DATE_SPLITTER = "T";
+
+	/**
+	* get the date out of iso date
+	*
+	* @param string 		$iso_date_string
+	*
+	* @return ilDate
+	*/
 	protected function createDate($iso_date_string) {
 		$split = explode(self::DATE_SPLITTER,$iso_date_string);
 		return new ilDate($split[0],IL_CALC_DATE);
