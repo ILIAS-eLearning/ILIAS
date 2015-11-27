02807ca43f2e577e309687689d436194b543ff39
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2015-11-27 12:30:09 +0100

    1400 modified crs-hours calc logic

--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportASTD/classes/class.ilObjReportASTD.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportASTD/classes/class.ilObjReportASTD.php
@@ -28,8 +28,11 @@ class ilObjReportASTD extends ilObjReportBase {
 	}
 
 	protected function prepareQueryComponents($a_query) {
-		$this->categories = array(	'astd_hours_not_self_learn' 	=>	" SUM( IF(type IN ('Virtuelles Training','Präsenztraining','Webinar') AND thours IS NOT NULL, thours, 0)) "
-									,'astd_hours_self_learn' 		=>	" SUM( IF(type = 'Selbstlernkurs' AND credit_points IS NOT NULL, GREATEST(credit_points,0)/1.33, 0)) " 
+		$this->categories = array(	'astd_hours_not_self_learn' 	=>	" SUM( IF(type IN ('Virtuelles Training','Präsenztraining','Webinar'), "
+																			."IF(chours IS NOT NULL, chours, "
+																				."IF(thours IS NOT NULL, thours, "
+																					."4*GREATEST(credit_points,0)/3)), 0)) "
+									,'astd_hours_self_learn' 		=>	" SUM( IF(type = 'Selbstlernkurs' AND credit_points IS NOT NULL, 4*GREATEST(credit_points,0)/3, 0)) " 
 									,'astd_hours_language_course'	=>	' 0 '
 									,'astd_participators'			=>	' COUNT(DISTINCT usr_id)'
 									,'astd_accomodation_cost'		=>	" SUM( IF( type = 'Präsenztraining' AND begin_date IS NOT NULL AND end_date IS NOT NULL, (DATEDIFF(end_date,begin_date)+1)*"
