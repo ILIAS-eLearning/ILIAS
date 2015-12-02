771bdff577612763b3232a80e998bc64c8d5f62e
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2015-12-02 15:30:45 +0100

    gev2: #1931 added new tep type

--- setup/sql/dbupdate_custom.php
+++ setup/sql/dbupdate_custom.php
@@ -4693,3 +4693,10 @@ ilCustomInstaller::activatePlugin(IL_COMP_SERVICE, "User", "udfc", "GEVUserData"
 		);
 	}
 ?>
+
+<#189>
+<?php
+// TEP categories
+$sql = "INSERT INTO tep_type (id, title, bg_color, font_color, tep_active) VALUES (25, 'Weiterbildungstage', 'bf6364', '000000', 1)";
+$ilDB->manipulate($sql);
+?>
