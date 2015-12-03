e20095677a4d230fd6a6acd06798bcf55f632ca0
Alex Killing <alex.killing@gmx.de>
2015-12-03 17:29:59 +0100

    fixed bug #17628: Failed test: Kurs importieren - Old glossary import file

--- Modules/Glossary/classes/class.ilGlossaryImportConfig.php
+++ Modules/Glossary/classes/class.ilGlossaryImportConfig.php
@@ -12,27 +12,7 @@ include_once("./Services/Export/classes/class.ilImportConfig.php");
  */
 class ilGlossaryImportConfig extends ilImportConfig
 {
-	protected $pre_51_import = false;
 
-	/**
-	 * Set pre 51 import
-	 *
-	 * @param boolean $a_val import file is lower than 5.1	
-	 */
-	function setPre51Import($a_val)
-	{
-		$this->pre_51_import = $a_val;
-	}
-	
-	/**
-	 * Get pre 51 import
-	 *
-	 * @return boolean import file is lower than 5.1
-	 */
-	function getPre51Import()
-	{
-		return $this->pre_51_import;
-	}
 }
 
 ?>
