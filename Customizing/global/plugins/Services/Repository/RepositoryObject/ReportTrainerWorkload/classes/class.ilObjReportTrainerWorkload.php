10fc2a6251dfb6f0609fc09cd5361d4a41cc9a44
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2015-12-07 09:59:57 +0100

    1926-1930 report trainer workload

--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportTrainerWorkload/classes/class.ilObjReportTrainerWorkload.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportTrainerWorkload/classes/class.ilObjReportTrainerWorkload.php
@@ -15,31 +15,63 @@ class ilObjReportTrainerWorkload extends ilObjReportBase {
 	protected $sum_row;
 	protected $relevant_parameters = array();
 
+	protected $norms;
+
+
+	public function __construct($ref_id) {
+		parent::__construct($ref_id);
+
+		require_once $this->plugin->getDirectory().'/config/cfg.trainer_workload.php';
+	}
+
 	public function initType() {
 		 $this->setType("xrtw");
 	}
 
+	protected function hoursPerConditionRatioNorm($condition, $name, $function) {
+		$sql = 	"SUM(IF(".$condition
+				.",	".$function
+				.",	0)"
+				.")"
+				." as ".$name;
+		return $sql;
+	}
+
 	protected function buildQuery($query) {
-		$query	
+		$query 	->select("hu.user_id")
+				->select_raw("CONCAT(hu.lastname, ', ', hu.firstname) as fullname")
+				->select_raw($this->gIldb->quote($this->filter->get('org_unit')[0],"text")." as orgu_title");
+		foreach($this->cats as $condition => $cat_settings) {
+			$query->select_raw($this->hoursPerConditionRatioNorm($cat_settings['condition'],$condition,$cat_settings['weight']));
+		}
+		$query->from("hist_tep ht")
+					->join("hist_user hu")
+						->on("ht.user_id = hu.user_id"
+								." AND ".$this->gIldb->in("hu.user_id",$this->getRelevantUsers(),false,"integer"))
+					->join("hist_tep_individ_days htid")
+						->on("individual_days = id")
+					->left_join("hist_course hc")
+						->on("context_id = crs_id AND ht.category  = 'Training' AND hc.hist_historic = 0")
+					->group_by("hu.user_id")
+					->compile();
 		return $query;
 	}
 
 	protected function buildFilter($filter) {
 		$this->getRelevantOrgus();
 		$filter	->dateperiod( 	"period"
-								 , $this->lng->txt("gev_period")
-								 , $this->lng->txt("gev_until")
+								 , $this->plugin->txt("period")
+								 , $this->plugin->txt("until")
 								 , "ht.begin_date"
 								 , "ht.begin_date"
 								 , date("Y")."-01-01"
 								 , date("Y")."-12-31"
 								 , false
-								 , " OR ht.hist_historic IS NULL"
 								 )
 				->multiselect( "org_unit"
-								 , $this->lng->txt("gev_org_unit_short")
+								 , $this->plugin->txt("org_unit_short")
 								 , "orgu_title"
-								 , $this->relevant_orgus
+								 , $this->getRelevantOrgus()
 								 , array()
 								 , "OR TRUE"
 								 , 200
@@ -61,24 +93,68 @@ class ilObjReportTrainerWorkload extends ilObjReportBase {
 	}
 
 	protected function buildTable($table) {
-		$table	->column("lastname", "lastname")
-				->column("firstname", "firstname")
-				->column("odbd", "gev_bd")
-				->column("credit_points", "gev_credit_points")
-				->column("max_credit_points", "gev_credit_points_forecast");
+
+		$table->column("fullname", $this->plugin->txt("fullname"),true);
+		foreach($this->meta_cats as $meta_category => $categories) {
+			foreach ($categories as $category) {
+				$table->column($category,$this->plugin->txt($category),true);
+			}
+			if(count($categories)>1) {
+				$table->column($meta_category."_sum", $this->plugin->txt($meta_category."_sum"),true);
+			}
+			if(isset($this->norms[$meta_category])) {
+				$table->column($meta_category."_workload", $this->plugin->txt($meta_category."_workload"),true);
+			}
+		}
+		$this->buildSumTable();
 		return parent::buildTable($table);
 	}
 
+	protected function buildSumTable() {
+		$this->table_sums = catReportTable::create();
+		foreach($this->meta_cats as $meta_category => $categories) {
+			foreach ($categories as $category) {
+				$this->table_sums->column($category, $this->plugin->txt($category),true);
+			}
+			if(count($categories)>1) {
+				$this->table_sums->column($meta_category."_sum", $this->plugin->txt($meta_category."_sum"),true);
+			}
+			if(isset($this->norms[$meta_category])) {
+				$this->table_sums->column($meta_category."_workload", $this->plugin->txt($meta_category."_workload"),true);
+			}
+		}
+		$this->table_sums->template("tpl.gev_trainer_workload_sum_row.html", $this->plugin->getDirectory());
+	}
+
+	public function deliverSumTable() {
+		return $this->table_sums;
+	}
+
 	protected function buildOrder($order) {
-		$order 	->defaultOrder("lastname", "ASC")
+		$order 	->defaultOrder("fullname", "ASC")
 				;
 		return $order;
 	}
 
 	protected function fetchData(callable $callback) {
-		$data = parent::fetchData(null);
+		$data = parent::fetchData('static::identity');
+		$count_rows = 0;
+		$this->sum_row = array();
+		foreach ($this->meta_cats as $meta_category => $categories) {
+			foreach($categories as $category) {
+				$this->sum_row[$category] = 0;
+			}
+			if(count($categories)>1) {
+				$this->sum_row[$meta_category.'_sum'] = 0;
+			}
+			if(isset($this->norms[$meta_category])) {
+				$this->sum_row[$meta_category.'_workload'] = 0;
+			}
+		}
+		$period_days_factor = $this->getPeriodDays()/365;
 		foreach($data as &$trainer_data) {
-			foreach ($this->workload_meta as $meta_category => $categories) {
+			$count_rows++;
+			foreach ($this->meta_cats as $meta_category => $categories) {
 				if(count($categories)>1) {
 					$trainer_data[$meta_category.'_sum'] = 0;
 					foreach ($categories as $category) {
@@ -87,21 +163,42 @@ class ilObjReportTrainerWorkload extends ilObjReportBase {
 					}
 					$this->sum_row[$meta_category.'_sum'] += $trainer_data[$meta_category.'_sum'];
 					if( isset($this->norms[$meta_category])) {
-						$trainer_data[$meta_category.'_workload'] = $trainer_data[$meta_category.'_sum']/$this->norms[$meta_category];
+						$trainer_data[$meta_category.'_workload'] = 100*$trainer_data[$meta_category.'_sum']/($this->norms[$meta_category]*$period_days_factor);
 						$this->sum_row[$meta_category.'_workload'] += $trainer_data[$meta_category.'_workload'];
 					}
 				} else {
 					$this->sum_row[$meta_category] += $trainer_data[$meta_category];
 					if( isset($this->norms[$meta_category])) {
-						$trainer_data[$meta_category.'_workload'] = $trainer_data[$meta_category]/$this->norms[$meta_category];
+						$trainer_data[$meta_category.'_workload'] = 100*$trainer_data[$meta_category.'_sum']/($this->norms[$meta_category]*$period_days_factor);
 						$this->sum_row[$meta_category.'_workload'] += $trainer_data[$meta_category.'_workload'];
 					}
 				}
 			}
 			$trainer_data = call_user_func($callback,$trainer_data);
 		}
+		//die();
+		foreach($this->norms as $meta_category => $norm) {
+			$this->sum_row[$meta_category.'_workload'] = $this->sum_row[$meta_category.'_workload']/$count_rows;
+		}
 		$this->sum_row = call_user_func($callback,$this->sum_row);
  		return $data;
+	}
+
+	protected function getPeriodDays() {
+		$dates = $this->filter->get("period");
+        foreach($dates as &$il_date_obj) {
+            $il_date_obj = $il_date_obj->getUnixTime();
+        }
+
+       	return $period_days = ($dates["end"] - $dates["start"])/86400+1;
+	}
+
+	static protected function identity ($rec) {
+		return $rec;
+	}
+
+	public function fetchSumData() {
+		return $this->sum_row;
 	}
 
 	protected function getRelevantOrgus() {
@@ -127,6 +224,12 @@ class ilObjReportTrainerWorkload extends ilObjReportBase {
 	}
 
 	protected function getRelevantUsers() {
+		require_once './Services/AccessControl/classes/class.ilObjRole.php';
+		$ignore_roles_ids = array();
+		foreach ($this->ignore_roles as $role_title) {
+			array_merge($ignore_roles_ids,	ilObjRole::_getIdsForTitle($role_title,'role'));
+		}
+
 		$sql = 	"SELECT huo.usr_id, rpa.rol_id, rpa.ops_id, rop.ops_id AS chk "
 				."	FROM rbac_pa rpa"
 				."	JOIN rbac_operations rop "
@@ -136,15 +239,22 @@ class ilObjReportTrainerWorkload extends ilObjReportBase {
 				."		ON ore.ref_id = rpa.ref_id "
 				."	JOIN rbac_ua rua "
 				."		ON rua.rol_id = rpa.rol_id "
+				."	LEFT JOIN hist_userrole hur "
+				."		ON hur.usr_id = rua.usr_id "
+				."			AND ".$this->gIldb->in('hur.rol_id',$ignore_roles_ids,false,'integer')
+				."			AND hur.hist_version = 0 "
+				."			AND hur.action = 1 "
 				."	JOIN hist_userorgu huo "
-				."		ON `action` = 1 AND hist_historic =0 "
+				."		ON huo.`action` >= 0 AND huo.hist_historic = 0 "
 				."			AND huo.usr_id = rua.usr_id "
 				."			AND ore.obj_id = huo.orgu_id ";
-		$org_unit_filter = $this->filter->get("org_unit")
-		if(count($org_unit_filter)>0) {
-			$sql .= " 	AND ".$this->gIldb->in('huo.orgu_title',$org_unit_filter,false,'text');
-		}
 
+		if(count($org_unit_filter)>0) {
+			$sql .= " 	AND ".$this->gIldb->in('huo.orgu_title',$this->filter->get("org_unit"),false,'text');
+		}
+		$sql .= "	WHERE hur.hist_historic IS NULL";
+
+		//die($sql);
 
 		$res = $this->gIldb->query($sql);
 		$relevan_users = array();
@@ -165,7 +275,7 @@ class ilObjReportTrainerWorkload extends ilObjReportBase {
 			.",".$this->gIldb->quote(1, "integer")
 			.",".$this->gIldb->quote(1, "integer")
 			.",".$this->gIldb->quote(1, "integer")
-			")");
+			.")");
 	}
 
 
@@ -206,27 +316,27 @@ class ilObjReportTrainerWorkload extends ilObjReportBase {
 	}
 
 	public function getAnnualNormTraining() {
-		return $this->annual_norm_training;
+		return $this->norms['training'];
 	}
 
 	public function getAnnualNormOperation() {
-		return $this->annual_norm_operation;
+		return $this->norms['operation'];
 	}
 
 	public function getAnnualNormOffice() {
-		return $this->annual_norm_office;
+		return $this->norms['office'];
 	}
 
 	public function setAnnualNormTraining($a_val) {
-		$this->annual_norm_training = $a_val;
+		$this->norms['training'] = $a_val;
 	}
 
-	public function getAnnualNormOperation($a_val) {
-		$this->annual_norm_operation = $a_val;
+	public function setAnnualNormOperation($a_val) {
+		$this->norms['operation'] = $a_val;
 	}
 
-	public function getAnnualNormOffice($a_val) {
-		$this->annual_norm_office = $a_val;
+	public function setAnnualNormOffice($a_val) {
+		$this->norms['office'] = $a_val;
 	}
 
 	public function setOnline($a_val) {
--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportTrainerWorkload/classes/class.ilObjReportTrainerWorkloadGUI.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportTrainerWorkload/classes/class.ilObjReportTrainerWorkloadGUI.php
@@ -14,6 +14,11 @@ class ilObjReportTrainerWorkloadGUI extends ilObjReportBaseGUI {
 		return 'xrtw';
 	}
 
+	protected function afterConstructor() {
+		parent::afterConstructor();
+		$this->tpl->addCSS($this->object->plugin->getStylesheetLocation('report.css'));
+	}
+
 	protected function prepareTitle($a_title) {
 		$a_title = parent::prepareTitle($a_title);
 		$a_title->image("GEV_img/ico-head-edubio.png");
@@ -23,37 +28,107 @@ class ilObjReportTrainerWorkloadGUI extends ilObjReportBaseGUI {
 	protected function settingsForm($data = null) {
 		$settings_form = parent::settingsForm($data);
 
-		$is_online = new ilCheckboxInputGUI($this->gLng->txt('online'),'online');
+		$is_online = new ilCheckboxInputGUI($this->object->plugin->txt('online'),'online');
 		$is_online->setValue(1);
 		if(isset($data["online"])) {
 			$is_online->setChecked($data["online"]);
 		}
 		$settings_form->addItem($is_online);
 
+		$annual_norm_training = new ilNumberInputGUI($this->object->plugin->txt('annual_norm_training'),'annual_norm_training');
+		if(isset($data['annual_norm_training'])) {
+			$annual_norm_training->setValue($data['annual_norm_training']);
+		}
+		$settings_form->addItem($annual_norm_training);
+
+		$annual_norm_operation = new ilNumberInputGUI($this->object->plugin->txt('annual_norm_operation'),'annual_norm_operation');
+		if(isset($data['annual_norm_operation'])) {
+			$annual_norm_operation->setValue($data['annual_norm_operation']);
+		}
+		$settings_form->addItem($annual_norm_operation);
+
+		$annual_norm_office = new ilNumberInputGUI($this->object->plugin->txt('annual_norm_office'),'annual_norm_office');
+		if(isset($data['annual_norm_office'])) {
+			$annual_norm_office->setValue($data['annual_norm_office']);
+		}
+		$settings_form->addItem($annual_norm_office);
+
 		return $settings_form;
+	}
+
+	protected function renderTable() {
+		$table = parent::renderTable();
+		$sum_table = $this->renderSumTable();
+		return $sum_table.$table;
+	}
+
+	private function renderSumTable(){
+		
+		$table = new catTableGUI($this, "view");
+		$table->setEnableTitle(false);
+		$table->setTopCommands(false);
+		$table->setEnableHeader(true);
+		$table_sums = $this->object->deliverSumtable();
+
+		$table->setRowTemplate(
+			$table_sums->row_template_filename, 
+			$table_sums->row_template_module
+		);
+
+		$table->addColumn("", "blank", "0px", false);
+		foreach ($table_sums->columns as $col) {
+			$table->addColumn( $col[2] ? $col[1] : $this->lng->txt($col[1])
+							 , $col[0]
+							 , $col[3]
+							 );
+		}
+
+		$sum_row = $this->object->fetchSumData();	
+		if(count($sum_row) == 0) {
+			foreach(array_keys($table_sums->columns) as $field) {
+				$sum_row[$field] = 0;
+			}
+		}
+
+		$table->setData(array($sum_row));
+		$this->enableRelevantParametersCtrl();
+		$return = $table->getHtml();
+		$this->disableRelevantParametersCtrl();
+		return $return;
 	}
 
 	protected function getSettingsData() {
 		$data = parent::getSettingsData();
 		$data["online"] = $this->object->getOnline();
+		$data["annual_norm_training"] = $this->object->getAnnualNormTraining();
+		$data["annual_norm_operation"] = $this->object->getAnnualNormOperation();
+		$data["annual_norm_office"] = $this->object->getAnnualNormOffice();
 		return $data;
 	}
 
 	protected function saveSettingsData($data) {
 		$this->object->setOnline($data["online"]);
+		$this->object->setAnnualNormTraining($data["annual_norm_training"]);
+		$this->object->setAnnualNormOperation($data["annual_norm_operation"]);
+		$this->object->setAnnualNormOffice($data["annual_norm_office"]);
 		parent::saveSettingsData($data);
 	}
 
 	public static function transformResultRow($rec) {
 		global $ilCtrl;
-		foreach ($rec as &$value) {
-			$value = number_format($value,2,',','.');
+		foreach ($rec as $key => &$value) {
+			if($key != 'fullname') {
+				if(strpos($key,'_workload') === false) {
+					$value = number_format($value,2,',','.');
+				} else {
+					$value = number_format($value,0,',','.');
+				}
+			}
 		}
 		return parent::transformResultRow($rec);
 	}
 
 	public static function transformResultRowXLS($rec) {
-		$rec['odbd'] = $rec['org_unit_above1'];
-		return parent::transformResultRow($rec);
+		return self::transformResultRow($rec);
 	}
 }
