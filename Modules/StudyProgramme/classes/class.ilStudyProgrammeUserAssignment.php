2a4c6b6e159a752af5815ca8cdc49eed992c75af
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2015-12-08 14:26:47 +0100

    Study Programme comment added

--- Modules/StudyProgramme/classes/class.ilStudyProgrammeUserAssignment.php
+++ Modules/StudyProgramme/classes/class.ilStudyProgrammeUserAssignment.php
@@ -56,8 +56,9 @@ class ilStudyProgrammeUserAssignment {
 		$assignments = ilStudyProgrammeAssignment::where(array( "usr_id" => $a_user_id ))
 													->get();
 
+		//if parent object is deleted or in trash
+		//the assignment for the user should not be returned
 		$ret = array();
-
 		foreach($assignments as $ass) {
 			$ass_obj =	new ilStudyProgrammeUserAssignment($ass);
 			foreach (ilObject::_getAllReferences($ass_obj->assignment->getRootId()) as $value) {
