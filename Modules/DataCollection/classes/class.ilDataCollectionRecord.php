ILIAS-eLearning/ILIAS

commit fec04acbcbeffe7f02d20e0a8de87c5786cf1848

[FIX] Mantis 0017238: You could not simulate an import into an empty datacollection.

Modules/DataCollection/classes/class.ilDataCollectionRecord.php
@@ -157,6 +157,12 @@ class ilDataCollectionRecord {
 			. $ilDB->quote($this->getOwner(), "integer") . "," . $ilDB->quote($this->getLastEditBy(), "integer") . "
 			)";
 		$ilDB->manipulate($query);
+
+		$this->loadRecordFields();
+		foreach ($this->getRecordFields() as $recordField) {
+			$recordField->doCreate();
+
+		}
 	}
 
 
@@ -745,7 +751,7 @@ class ilDataCollectionRecord {
 
 
 	/**
-	 * @return array
+	 * @return ilDataCollectionRecordField[]
 	 */
 	public function getRecordFields() {
 		$this->loadRecordFields();

Modules/DataCollection/classes/class.ilDataCollectionRecordField.php
@@ -71,23 +71,23 @@ class ilDataCollectionRecordField {
 	 * Read object data from database
 	 */
 	protected function doRead() {
+		if(!$this->record->getId())
+			return;
+
 		$query = "SELECT * FROM il_dcl_record_field WHERE field_id = " . $this->db->quote($this->field->getId(), "integer") . " AND record_id = "
 			. $this->db->quote($this->record->getId(), "integer");
 		$set = $this->db->query($query);
 		$rec = $this->db->fetchAssoc($set);
 		$this->id = $rec['id'];
-
-		if ($this->id == NULL) {
-			$this->doCreate();
-		}
+		
 		$this->loadValue();
 	}
 
 
 	/**
-	 * Create object in database
+	 * Creates an Id and a database entry.
 	 */
-	protected function doCreate() {
+	public function doCreate() {
 		$id = $this->db->nextId("il_dcl_record_field");
 		$query = "INSERT INTO il_dcl_record_field (id, record_id, field_id) VALUES (" . $this->db->quote($id, "integer") . ", "
 			. $this->db->quote($this->record->getId(), "integer") . ", " . $this->db->quote($this->field->getId(), "text") . ")";

commit bc08c6763888b5e954df5a3b29e06d2a48147d18

DataCollection: Fix bugs introduced with fec04acbcbeffe7f02d20e0a8de87c5786cf1848

Modules/DataCollection/classes/class.ilDataCollectionRecord.php
@@ -157,12 +157,6 @@ class ilDataCollectionRecord {
 			. $ilDB->quote($this->getOwner(), "integer") . "," . $ilDB->quote($this->getLastEditBy(), "integer") . "
 			)";
 		$ilDB->manipulate($query);
-
-		$this->loadRecordFields();
-		foreach ($this->getRecordFields() as $recordField) {
-			$recordField->doCreate();
-
-		}
 	}
 
 

Modules/DataCollection/classes/class.ilDataCollectionRecordField.php
@@ -79,7 +79,11 @@ class ilDataCollectionRecordField {
 		$set = $this->db->query($query);
 		$rec = $this->db->fetchAssoc($set);
 		$this->id = $rec['id'];
-		
+
+		if ($this->id == null) {
+			$this->doCreate();
+		}
+
 		$this->loadValue();
 	}
 
@@ -87,7 +91,7 @@ class ilDataCollectionRecordField {
 	/**
 	 * Creates an Id and a database entry.
 	 */
-	public function doCreate() {
+	protected function doCreate() {
 		$id = $this->db->nextId("il_dcl_record_field");
 		$query = "INSERT INTO il_dcl_record_field (id, record_id, field_id) VALUES (" . $this->db->quote($id, "integer") . ", "
 			. $this->db->quote($this->record->getId(), "integer") . ", " . $this->db->quote($this->field->getId(), "text") . ")";
@@ -101,6 +105,9 @@ class ilDataCollectionRecordField {
 	 */
 	public function doUpdate() {
 		//$this->loadValue(); //Removed Mantis #0011799
+		if (!$this->id) {
+			$this->doCreate();
+		}
 		$datatype = $this->field->getDatatype();
 		$query = "DELETE FROM il_dcl_stloc" . $datatype->getStorageLocation() . "_value WHERE record_field_id = "
 			. $this->db->quote($this->id, "integer");

Modules/DataCollection/classes/class.ilDataCollectionRecordListGUI.php
@@ -565,6 +565,7 @@ class ilDataCollectionRecordListGUI {
 
 		/** @var ilCtrl $ilCtrl */
 		/** @var ilTabsGUI $ilTabs */
+		$ilCtrl->setParameter($this, 'mode', self::MODE_VIEW);
 		$ilTabs->addSubTab('mode_1', $lng->txt('view'), $ilCtrl->getLinkTarget($this, 'listRecords'));
 		if ($this->table_obj->hasPermissionToDeleteRecords((int)$_GET['ref_id'])) {
 			$ilCtrl->setParameter($this, 'mode', self::MODE_MANAGE);