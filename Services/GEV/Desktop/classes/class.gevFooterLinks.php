14aca4fc5f6c49f0eeb632d7046c0b51cd820c0b
Richard Klees <richard.klees@concepts-and-training.de>
2015-12-18 09:32:23 +0100

    formatting

--- Services/GEV/Desktop/classes/class.gevFooterLinks.php
+++ Services/GEV/Desktop/classes/class.gevFooterLinks.php
@@ -18,88 +18,96 @@ class gevFooterLinks {
 			$uutils = gevUserUtils::getInstance($user_id);
 		}
 
-		return array(/*BiProgramm Agent*/		array("link" => "./Customizing/global/skin/genv/static/documents/"
-										  					   ."Weiterbildungsprogramme_2015_Vermittler.pdf"
-														 ,"desc" => "gev_footer_bipro_agent"
-									 					 ,"display" => function() use ($uutils) {
-									 					 						if($uutils) {
-									 					 							if($uutils->seeBiproAgent()) {
-									 					 								$today = date("Y-m-d");
-									 					 								if($today < "2016-01-01") {
-									 					 									return true;
-									 					 								} else {
-									 					 									return false;
-									 					 								}
-									 					 							}
-																					return false;
-									 					 						}
-														 					}
-													),
-					 /*BiProgramm Superior*/ 	array("link" => "./Customizing/global/skin/genv/static/documents/"
-										  					   ."Weiterbildungsprogramme_2015_Fuehrungskraefte.pdf"
-													 ,"desc" => "gev_footer_bipro_superior"
-								 					 ,"display" => function() use ($uutils) {
-																			if($uutils) {
-																				if($uutils->seeBiproSuperior()) {
-								 					 								$today = date("Y-m-d");
-								 					 								if($today < "2016-01-01") {
-								 					 									return true;
-								 					 								} else {
-								 					 									return false;
-								 					 								}
-								 					 							}
-																				return false;
-																			}
-													 					}
-												),
-					 /*Infos Decentral*/ 		array("link" => "./static_pages.php?tpl=infpraesenz"
-													 ,"desc" => "gev_footer_infos_decentral"
-													 ,"display" => true
-												),
-					 /*Infors Webinar*/ 		array("link" => "./static_pages.php?tpl=infwebinar"
-					 								 ,"desc" => "gev_footer_infos_webinar"
-					 								 ,"display" => true
-					 							),
-					 /*AGB Agent*/ 				array("link" => "./Customizing/global/skin/genv/static/documents/"
-										  					   ."GEV_Makler_Finale_Nutzungsbedingungen.pdf"
-					 								 ,"desc" => "gev_footer_terms_of_use"
-					 								 ,"display" => function() use ($uutils) {																			
-																			if($uutils) {
-																				if ($uutils->hasRoleIn(array("VP"))) {
-																					return true;
-																				}
-																				return false;
-																			}
-										 								}
-					 							),
-					 /*AGB*/					array("link" => "./static_pages.php?tpl=agb"
-													 ,"desc" => "gev_footer_terms_of_use"
-								 					 ,"display" => function() use ($uutils) {
-																			if($uutils) {
-																				if (!$uutils->hasRoleIn(array("VP"))) {
-																					return true;
-																				}
-																				return false;
-																			}
-																		}
-												),
-					 /*Infos WBD registration*/ array("link" => "./static_pages.php?tpl=wbd_docs"
-													 ,"desc" => "gev_footer_infos_wbd_registration"
-								 					 ,"display" => true
-												),
-					 /*Contact*/				array("link" => "./static_pages.php?tpl=kontakt"
-													 ,"desc" => "gev_footer_contact"
-								 					 ,"display" => true
-												),
-					 /*Imprint*/				array("link" => "./static_pages.php?tpl=impressum"
-													 ,"desc" => "gev_footer_imprint"
-								 					 ,"display" => true
-												),
-					 /*Academy Benried*/		array("link" => "./static_pages.php?tpl=gszbernried"
-													 ,"desc" => "gev_footer_academy_benried"
-								 					 ,"display" => true
-												)
-			);
+		return array(
+		/*BiProgramm Agent*/array
+			( "link" => "./Customizing/global/skin/genv/static/documents/Weiterbildungsprogramme_2015_Vermittler.pdf"
+			, "desc" => "gev_footer_bipro_agent"
+			, "display" => function() use ($uutils) {
+					if($uutils) {
+						if($uutils->seeBiproAgent()) {
+							$today = date("Y-m-d");
+							if($today < "2016-01-01") {
+								return true;
+							} else {
+								return false;
+							}
+						}
+					return false;
+					}
+				}
+			),
+		/*BiProgramm Superior*/array
+			( "link" => "./Customizing/global/skin/genv/static/documents/Weiterbildungsprogramme_2015_Fuehrungskraefte.pdf"
+			, "desc" => "gev_footer_bipro_superior"
+			, "display" => function() use ($uutils) {
+					if($uutils) {
+						if($uutils->seeBiproSuperior()) {
+ 								$today = date("Y-m-d");
+ 								if($today < "2016-01-01") {
+ 									return true;
+ 								} else {
+ 									return false;
+ 								}
+ 							}
+						return false;
+					}
+				}
+			),
+		/*Infos Decentral*/array
+			( "link" => "./static_pages.php?tpl=infpraesenz"
+			, "desc" => "gev_footer_infos_decentral"
+			, "display" => true
+			),
+		/*Infos Webinar*/array
+			( "link" => "./static_pages.php?tpl=infwebinar"
+			, "desc" => "gev_footer_infos_webinar"
+			, "display" => true
+			),
+		/*AGB Agent*/array
+			( "link" => "./Customizing/global/skin/genv/static/documents/GEV_Makler_Finale_Nutzungsbedingungen.pdf"
+			, "desc" => "gev_footer_terms_of_use"
+			, "display" => function() use ($uutils) {
+					if($uutils) {
+						if ($uutils->hasRoleIn(array("VP"))) {
+							return true;
+						}
+						return false;
+					}
+				}
+			),
+		 /*AGB*/array
+		 	( "link" => "./static_pages.php?tpl=agb"
+			 ,"desc" => "gev_footer_terms_of_use"
+			 ,"display" => function() use ($uutils) {
+					if($uutils) {
+						if (!$uutils->hasRoleIn(array("VP"))) {
+							return true;
+						}
+						return false;
+					}
+				}
+			),
+		/*Infos WBD registration*/array
+			( "link" => "./static_pages.php?tpl=wbd_docs"
+			, "desc" => "gev_footer_infos_wbd_registration"
+			, "display" => true
+			),
+		/*Contact*/array
+			( "link" => "./static_pages.php?tpl=kontakt"
+			, "desc" => "gev_footer_contact"
+			, "display" => true
+			),
+		/*Imprint*/array
+			( "link" => "./static_pages.php?tpl=impressum"
+			, "desc" => "gev_footer_imprint"
+			, "display" => true
+			),
+		/*Academy Benried*/array
+			( "link" => "./static_pages.php?tpl=gszbernried"
+			, "desc" => "gev_footer_academy_benried"
+			, "display" => true
+			)
+		);
 	}
 
 	public function getFooterLinksFor($user_id = null) {
