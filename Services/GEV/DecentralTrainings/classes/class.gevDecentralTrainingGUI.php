835725abad7eb3c253a302447a0b40504a58df16
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2015-12-18 15:30:56 +0100

    gev2: #2021 search parent of parent added

--- Services/GEV/DecentralTrainings/classes/class.gevDecentralTrainingGUI.php
+++ Services/GEV/DecentralTrainings/classes/class.gevDecentralTrainingGUI.php
@@ -1639,8 +1639,8 @@ class gevDecentralTrainingGUI {
 			$usr_utils = gevUserUtils::getInstance($trainer_id);
 			if($usr_utils->isUVGDBV()) {
 				if($pers_org_unit = $uvg_org_units->getOrgUnitIdOf($usr_utils->getId())) {
-					$tree = ilObjOrgUnitTree::_getInstance();
-					$above_ref_id = $tree->getParent(gevObjectUtils::getRefId($pers_org_unit));
+					$ou_tree = ilObjOrgUnitTree::_getInstance();
+					$above_ref_id = $ou_tree->getParent($ou_tree->getParent(gevObjectUtils::getRefId($pers_org_unit)));
 					$trainer_orgus[] = $above_ref_id;
 				}
 			} else {
