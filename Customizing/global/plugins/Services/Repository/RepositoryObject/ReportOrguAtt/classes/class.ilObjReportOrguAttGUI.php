fa827978ebc2b7deac57046f5daa4a8f414b11bd
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-01-12 11:23:58 +0100

    data formatting

--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportOrguAtt/classes/class.ilObjReportOrguAttGUI.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportOrguAtt/classes/class.ilObjReportOrguAttGUI.php
@@ -9,7 +9,8 @@ require_once 'Services/ReportsRepository/classes/class.ilObjReportBaseGUI.php';
 * @ilCtrl_Calls ilObjReportOrguAttGUI: ilCommonActionDispatcherGUI
 */
 class ilObjReportOrguAttGUI extends ilObjReportBaseGUI {
-
+	static $od_regexp = null;
+	static $bd_regexp = null;
 	public function getType() {
 		return 'xroa';
 	}
@@ -35,23 +36,24 @@ class ilObjReportOrguAttGUI extends ilObjReportBaseGUI {
 		$table->setEnableTitle(false);
 		$table->setTopCommands(false);
 		$table->setEnableHeader(true);
+		$sum_table = $this->object->deliverSumTable();
 		$table->setRowTemplate(
-			$this->object->deliverSumTable()->row_template_filename, 
-			$this->object->deliverSumTable()->row_template_module
+			$sum_table->row_template_filename, 
+			$sum_table->row_template_module
 		);
 
 		$table->addColumn("", "blank", "0px", false);
-		foreach ($this->object->deliverSumTable()->columns as $col) {
+		$cnt = 1;
+		$table->setLimit($cnt);
+		$table->setMaxCount($cnt);
+		foreach ($sum_table->columns as $col) {
 			$table->addColumn( $col[2] ? $col[1] : $this->object->plugin->txt($col[1])
 							 , $col[0]
 							 , $col[3]
 							 );
-		}		
-		$cnt = 1;
-		$table->setLimit($cnt);
-		$table->setMaxCount($cnt);
-
-		$table = $this->object->insertSumData($table);
+		}
+		$callback = get_class($this).'::transformResultRow';
+		$table = $this->object->insertSumData($table,$callback);
 
 		$this->enableRelevantParametersCtrl();
 		$return = $table->getHtml();
@@ -61,5 +63,38 @@ class ilObjReportOrguAttGUI extends ilObjReportBaseGUI {
 
 	protected function getRowTemplateTitle() {
 		return "tpl.gev_attendance_by_orgunit_row.html";
+	}
+
+	public static function transformResultRow($rec) {
+
+		foreach($rec as &$data) {
+			if((string)$data === "0") {
+				$data = '-';
+			}
+		}
+		if(isset($rec['org_unit_above1'])) {
+			if(!self::$od_regexp || !self::$bd_regexp ) {
+				require_once './Customizing/global/plugins/Services/Repository/RepositoryObject/ReportOrguAtt/config/od_bd_strings.php';
+			}
+			$orgu_above1 =  $rec['org_unit_above1'];
+			$orgu_above2 =  $rec['org_unit_above2'];
+			if (preg_match(self::$od_regexp, $orgu_above1)) {
+				$od = $orgu_above1;
+			} elseif(preg_match(self::$od_regexp, $orgu_above2)) {
+				$od = $orgu_above2;
+			} else {
+				$od = '-';
+			}
+
+			if (preg_match(self::$bd_regexp, $orgu_above1)) {
+				$bd = $orgu_above1;
+			} elseif(preg_match(self::$bd_regexp, $orgu_above2)) {
+				$bd = $orgu_above2;
+			} else {
+				$bd = '-';
+			}
+			$rec['odbd'] = $od .'/' .$bd;
+		}
+		return parent::transformResultRow($rec);
 	}
 }
