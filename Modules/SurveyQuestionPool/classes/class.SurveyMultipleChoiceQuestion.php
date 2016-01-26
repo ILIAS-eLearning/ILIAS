518cdaeb04ac5041238a17d9d3dab93926891482
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2016-01-26 10:53:02 +0100

    #17895 - survey: constraint vs scale

--- Modules/SurveyQuestionPool/classes/class.SurveyMultipleChoiceQuestion.php
+++ Modules/SurveyQuestionPool/classes/class.SurveyMultipleChoiceQuestion.php
@@ -954,7 +954,11 @@ class SurveyMultipleChoiceQuestion extends SurveyQuestion
 	function getPreconditionValueOutput($value)
 	{
 		$category = $this->categories->getCategory($value);
-		return ($value + 1) . " - " . ((strlen($category->title)) ? $category->title : $this->lng->txt('other_answer'));
+		
+		// #17895 - see getPreconditionOptions()
+		return $category->scale . 
+			" - " . 
+			((strlen($category->title)) ? $category->title : $this->lng->txt('other_answer'));
 	}
 
 	public function getCategories()
