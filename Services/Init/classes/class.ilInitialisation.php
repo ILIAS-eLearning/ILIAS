88f7e19a8988790130dffddf6669478fd3ca3dfb
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-01-29 12:09:06 +0100

    add USE_WHOOPS variable to list of global constants

--- Services/Init/classes/class.ilInitialisation.php
+++ Services/Init/classes/class.ilInitialisation.php
@@ -346,6 +346,9 @@ class ilInitialisation
 		define ("SYSTEM_FOLDER_ID",$ilClientIniFile->readVariable('system','SYSTEM_FOLDER_ID'));
 		define ("ROLE_FOLDER_ID",$ilClientIniFile->readVariable('system','ROLE_FOLDER_ID'));
 		define ("MAIL_SETTINGS_ID",$ilClientIniFile->readVariable('system','MAIL_SETTINGS_ID'));
+		// gev patch start : use whoops error handler for errors outside devmode domain
+		define ("USE_WHOOPS",$ilClientIniFile->readVariable("system","USE_WHOOPS"));
+		// gev patch end
 		
 		// this is for the online help installation, which sets OH_REF_ID to the
 		// ref id of the online module
