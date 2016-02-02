17390ebe30aae27290fd95f7971fa558575f8c5f
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-02-02 17:37:01 +0100

    removed deprecated report files

--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportAttendanceByEmployee/classes/class.ilObjReportAttendanceByEmployeeAccess.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once 'Services/ReportsRepository/classes/class.ilObjReportBaseAccess.php';
-
-
-class ilObjReporAttendaceByEmployeeAccess extends ilObjReportBaseAccess {
-	const TABLE_TITLE = 'rep_robj_rae';
-}
