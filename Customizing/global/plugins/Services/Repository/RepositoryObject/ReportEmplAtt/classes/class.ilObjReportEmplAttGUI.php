b16ec5ee294bbdf3e2ead28ea129eaaaafc2b7a8
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-02-02 18:12:52 +0100

    added transform result row

--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportEmplAtt/classes/class.ilObjReportEmplAttGUI.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportEmplAtt/classes/class.ilObjReportEmplAttGUI.php
@@ -19,4 +19,44 @@ class ilObjReportEmplAttGUI extends ilObjReportBaseGUI {
 		$a_title->image("GEV_img/ico-head-edubio.png");
 		return $a_title;
 	}
+
+	protected static function transformResultRow($rec) {
+		// credit_points
+		if ($rec["credit_points"] == -1) {
+			$rec["credit_points"] = $this->lng->txt("gev_table_no_entry");
+		}
+
+		//date
+		if( $rec["begin_date"] && $rec["end_date"] 
+			&& ($rec["begin_date"] != '0000-00-00' && $rec["end_date"] != '0000-00-00' )
+			){
+			$start = new ilDate($rec["begin_date"], IL_CAL_DATE);
+			$end = new ilDate($rec["end_date"], IL_CAL_DATE);
+			$date = '<nobr>' .ilDatePresentation::formatPeriod($start,$end) .'</nobr>';
+			//$date = ilDatePresentation::formatPeriod($start,$end);
+		} else {
+			$date = '-';
+		}
+		$rec['date'] = $date;
+		
+		// od_bd
+		if ( $rec["org_unit_above2"] == "-empty-") {
+			if ($rec["org_unit_above1"] == "-empty-") {
+				$rec["od_bd"] = $this->lng->txt("gev_table_no_entry");
+			}
+			else {
+				$rec["od_bd"] = $rec["org_unit_above1"];
+			}
+		}
+		else {
+			$rec["od_bd"] = $rec["org_unit_above2"]."/".$rec["org_unit_above1"];
+		}
+
+		if($rec["participation_status"] == "nicht gesetzt") {
+			$rec["participation_status"] = "gebucht, noch nicht abgeschlossen";
+		}
+
+		return parent::transformResultRow($rec);
+	}
 }
+
