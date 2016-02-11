f8f896b2f18e45cc31983d2ac15d7b84f7772e77
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-02-11 13:16:09 +0100

    2103 ret copy paste err

--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportTrDemandRet/classes/class.ilReportTrDemandRetPlugin.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportTrDemandRet/classes/class.ilReportTrDemandRetPlugin.php
@@ -5,6 +5,6 @@ require_once 'Services/ReportsRepository/classes/class.ilReportBasePlugin.php';
 class ilReportTrDemandRetPlugin extends ilReportBasePlugin {
 	// must correspond to the plugin subdirectory
 	protected function getReportName() {
-		return "ilReportTrDemandRet";
+		return "ReportTrDemandRet";
 	}
 }
