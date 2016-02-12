5753f3a4291223d8fe2e77e847a104ce52b6db6e
Richard Klees <richard.klees@concepts-and-training.de>
2016-02-12 16:35:38 +0100

    interface for counter, with methods now...

--- src/UI/Element/Counter.php
+++ src/UI/Element/Counter.php
@@ -4,8 +4,27 @@
 
 namespace ILIAS\UI\Element;
 
+
 /**
  * This tags a counter object.
  */
 interface Counter extends \ILIAS\UI\Element {
+    /**
+     * Get the type of the counter.
+     *
+     * @return  CounterType
+     */
+    public function type();
+
+    /**
+     * Get the number on the counter.
+     *
+     * @return  int
+     */
+    public function amount();
 }
+
+// Tags for the different types of counters.
+class CounterType {};
+final class NoveltyCounterType extends CounterType {};
+final class StatusCounterType extends CounterType {};
