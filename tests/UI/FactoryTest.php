3500b19eb98bf61ccc5b11569c3b835be91c1254
Richard Klees <richard.klees@concepts-and-training.de>
2016-02-12 16:36:49 +0100

    little code smithing.

--- tests/UI/FactoryTest.php
+++ tests/UI/FactoryTest.php
@@ -6,19 +6,17 @@ require_once("libs/composer/vendor/autoload.php");
  * Defines tests every UI-factory should pass.
  */
 abstract class FactoryTest extends PHPUnit_Framework_TestCase {
-    public function setUp() {
-        $this->f = $this->getFactoryInstance();
-    }
-
     abstract public function getFactoryInstance();
 
     public function test_implements_factory_interface() {
-        $this->assertInstanceOf("ILIAS\\UI\\Factory", $this->f);
-        
+        $f = $this->getFactoryInstance();
+
+        $this->assertInstanceOf("ILIAS\\UI\\Factory", $f);
+
         // TODO: One could automate this by using the docstring on Factory.
         // This would be nice as we would make sure that documentation an
         // behaviour match up.
-        $this->assertInstanceOf("ILIAS\\UI\\Factory\\Counter", $this->f->counter());
-        $this->assertInstanceOf("ILIAS\\UI\\Factory\\Glyph", $this->f->glyph());
+        $this->assertInstanceOf("ILIAS\\UI\\Factory\\Counter", $f->counter());
+        $this->assertInstanceOf("ILIAS\\UI\\Factory\\Glyph", $f->glyph());
     }
 }
