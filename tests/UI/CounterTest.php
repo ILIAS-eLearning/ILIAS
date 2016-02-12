f57633be0f2aee5363619fae29d5f4a2384dfff9
Richard Klees <richard.klees@concepts-and-training.de>
2016-02-12 16:50:54 +0100

    UI: started tests on counters

---
+++ tests/UI/CounterTest.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once("libs/composer/vendor/autoload.php");
+
+/**
+ * Defines tests every UI-factory should pass.
+ */
+abstract class CounterTest extends PHPUnit_Framework_TestCase {
+    abstract public function getFactoryInstance();
+
+    public function test_implements_factory_interface() {
+        $f = $this->getFactoryInstance();
+
+        $this->assertInstanceOf("ILIAS\\UI\\Factory\\Counter", $f);
+
+        $this->assertInstanceOf("ILIAS\\UI\\Element\\Counter", $f->status(0));
+        $this->assertInstanceOf("ILIAS\\UI\\Element\\Counter", $f->novelty(0));
+    }
+
+    /**
+     * @dataProvider amount_provider
+     */
+    public function test_status_counter($amount) {
+        $f = $this->getFactoryInstance();
+
+        $c = $f->status($amount);
+
+        $this->assertNotNull($c);
+        $this->assertInstanceOf("ILIAS\\UI\\Element\\StatusCounterType", $c->type());
+        $this->assertEquals($amount, $c->amount());
+    }
+
+    /**
+     * @dataProvider amount_provider
+     */
+    public function test_novelty_counter($amount) {
+        $f = $this->getFactoryInstance();
+
+        $c = $f->novelty($amount);
+
+        $this->assertNotNull($c);
+        $this->assertInstanceOf("ILIAS\\UI\\Element\\NoveltyCounterType", $c->type());
+        $this->assertEquals($amount, $c->amount());
+    }
+
+    public function amount_provider() {
+        return array
+            ( array(-13)
+            , array(0)
+            , array(23)
+            , array(42)
+            );
+    }
+}
