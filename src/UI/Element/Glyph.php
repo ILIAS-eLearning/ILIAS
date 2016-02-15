1e84ed8810d60f057c663f25f4a6fb8219143b1e
Richard Klees <richard.klees@concepts-and-training.de>
2016-02-15 15:38:55 +0100

    UI: adjusted interface to glyph.

--- src/UI/Element/Glyph.php
+++ src/UI/Element/Glyph.php
@@ -11,10 +11,42 @@ interface Glyph extends \ILIAS\UI\Element {
     /**
      * Add a counter to the glyph.
      *
-     * @param   Counter
-     * @throws  \InvalidArgumentException   If an according counter is already
-     *                                      attached to the glyph.
+     * If there already is a counter of the added counter type, replace that
+     * counter by the new one.
+     *
+     * @param   Counter $counter
      * @return  Glyph
      */
     public function addCounter(Counter $counter);
+
+    /**
+     * Get the type of the glyph.
+     *
+     * @return  GlyphType
+     */
+    public function type();
+
+    /**
+     * Get all counters attached to this glyph.
+     *
+     * @return  Counter[]
+     */
+    public function counters();
 }
+
+// Tags for the different types of counters.
+class GlyphType {};
+final class UpGlyphType extends GlyphType {};
+final class DownGlyphType extends GlyphType {};
+final class AddGlyphType extends GlyphType {};
+final class RemoveGlyphType extends GlyphType {};
+final class PreviousGlyphType extends GlyphType {};
+final class NextGlyphType extends GlyphType {};
+final class CalendarGlyphType extends GlyphType {};
+final class CloseGlyphType extends GlyphType {};
+final class AttachmentGlyphType extends GlyphType {};
+final class CaretGlyphType extends GlyphType {};
+final class DragGlyphType extends GlyphType {};
+final class SearchGlyphType extends GlyphType {};
+final class FilterGlyphType extends GlyphType {};
+final class InfoGlyphType extends GlyphType {};
