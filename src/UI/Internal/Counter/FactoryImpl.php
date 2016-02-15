77a946afff0f39a94a71532fee1bcd33a27952fb
Richard Klees <richard.klees@concepts-and-training.de>
2016-02-15 16:31:19 +0100

    UI: don't cache counter type.

--- src/UI/Internal/Counter/FactoryImpl.php
+++ src/UI/Internal/Counter/FactoryImpl.php
@@ -5,26 +5,18 @@
 namespace ILIAS\UI\Internal\Counter;
 
 class FactoryImpl implements \ILIAS\UI\Factory\Counter {
-    private $novelty_type = null;
-    private $status_type = null;
-
-    public function __construct() {
-        $this->novelty_type = new \ILIAS\UI\Element\NoveltyCounterType();
-        $this->status_type = new \ILIAS\UI\Element\StatusCounterType();
-    }
-
     /**
      * @inheritdoc
      */
     public function status($amount) {
-        return new CounterImpl($this->status_type, $amount);
+        return new CounterImpl(new \ILIAS\UI\Element\StatusCounterType(), $amount);
     }
 
     /**
      * @inheritdoc
      */
     public function novelty($amount) {
-        return new CounterImpl($this->novelty_type, $amount);
+        return new CounterImpl(new \ILIAS\UI\Element\NoveltyCounterType(), $amount);
     }
 }
 
