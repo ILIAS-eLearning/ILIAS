327fa901a6bf5340c81db928874b4b2dac34fac2
Richard Klees <richard.klees@concepts-and-training.de>
2016-02-15 16:37:31 +0100

    UI: import namespace in Counter/FactoryImpl

--- src/UI/Internal/Counter/FactoryImpl.php
+++ src/UI/Internal/Counter/FactoryImpl.php
@@ -3,20 +3,21 @@
 /* Copyright (c) 2015 Richard Klees <richard.klees@concepts-and-training.de> Extended GPL, see docs/LICENSE */
 
 namespace ILIAS\UI\Internal\Counter;
+use ILIAS\UI\Element as E;
 
 class FactoryImpl implements \ILIAS\UI\Factory\Counter {
     /**
      * @inheritdoc
      */
     public function status($amount) {
-        return new CounterImpl(new \ILIAS\UI\Element\StatusCounterType(), $amount);
+        return new CounterImpl(new E\StatusCounterType(), $amount);
     }
 
     /**
      * @inheritdoc
      */
     public function novelty($amount) {
-        return new CounterImpl(new \ILIAS\UI\Element\NoveltyCounterType(), $amount);
+        return new CounterImpl(new E\NoveltyCounterType(), $amount);
     }
 }
 
