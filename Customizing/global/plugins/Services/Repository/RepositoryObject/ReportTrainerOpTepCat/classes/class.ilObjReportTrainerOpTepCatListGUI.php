9a769b97ca92ce826efb8decb5d6c52686dc8531
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-02-16 10:32:35 +0100

    included right lang vars

---
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportTrainerOpTepCat/classes/class.ilObjReportTrainerOpTepCatListGUI.php
@@ -0,0 +1,35 @@
+<?php
+require_once 'Services/ReportsRepository/classes/class.ilObjReportBaseListGUI.php';
+  /**
+* ListGUI implementation for Report plugin. This one
+* handles the presentation in container items (categories, courses, ...)
+* together with the corresponfing ...Access class.
+*/
+class ilObjReportTrainerOpTepCatListGUI extends ilObjReportBaseListGUI {
+
+	/**
+	* Init type
+	*/
+	public function initType() {
+		$this->setType("xttc");
+		parent::initType();
+	}
+
+	/**
+	* Get name of gui class handling the commands
+	*/
+	public function getGuiClass() {
+		return "ilObjReportTrainerOpTepCatGUI";
+	}
+	
+	public function getProperties() {
+		$props = array();
+		$this->plugin->includeClass("class.ilObjReportTrainerOpTepCatAccess.php");
+
+		if (!ilObjReportTrainerOpTepCatAccess::checkOnline($this->obj_id)) {
+			$props[] = array("alert" => true, "property" => $this->lng->txt("status"),
+			"value" => $this->lng->txt("offline"));
+		}
+		return $props;
+	}
+}
