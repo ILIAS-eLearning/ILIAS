cc69cb86f6b650a6e0d7828ae854e57b809c4105
smeyer <smeyer.ilias@gmx.de>
2016-02-17 15:37:24 +0100

    composer install for monolog lib

--- libs/composer/vendor/composer/autoload_psr4.php
+++ libs/composer/vendor/composer/autoload_psr4.php
@@ -7,4 +7,5 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'Whoops\\' => array($vendorDir . '/filp/whoops/src/Whoops'),
+    'Monolog\\' => array($vendorDir . '/monolog/monolog/src/Monolog'),
 );
