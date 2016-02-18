b804ea6ad1a58b94704eaad6d8ba98193aeef2a0
Alex Killing <alex.killing@gmx.de>
2016-02-18 16:50:25 +0100

    fixed bug 17815: Short title does not appears on login page

--- Services/Init/classes/class.ilStartUpGUI.php
+++ Services/Init/classes/class.ilStartUpGUI.php
@@ -338,8 +338,7 @@ class ilStartUpGUI
 		
 
 		// not controlled by login page editor
-
-		$tpl->setVariable("PAGETITLE", $lng->txt("startpage"));
+		$tpl->setVariable("PAGETITLE",  "- ".$lng->txt("startpage"));
 		$tpl->setVariable("ILIAS_RELEASE", $ilSetting->get("ilias_version"));
 		
 		$this->ctrl->setTargetScript("ilias.php");
@@ -355,7 +354,8 @@ class ilStartUpGUI
 		{
 			$tpl->setVariable('LPE',$page_editor_html);
 		}
-		
+
+		$tpl->fillWindowTitle();
 		$tpl->fillCssFiles();
 		$tpl->fillJavaScriptFiles();
 
