10e19356624ff9f297837c647b771cc705ab88ce
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-02-22 10:31:37 +0100

    gev2: #2104 remove obsolet comment

--- Customizing/global/plugins/Services/Repository/RepositoryObject/BuildingBlockPool/classes/class.ilObjBuildingBlockPoolListGUI.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/BuildingBlockPool/classes/class.ilObjBuildingBlockPoolListGUI.php
@@ -9,9 +9,7 @@ require_once 'Services/Repository/classes/class.ilObjectPluginListGUI.php';
 */
 
 class ilObjBuildingBlockPoolListGUI extends ilObjectPluginListGUI {
-	/**
-	* This is probably more of a hack, since this functions responsibility nothing has to do with GUI properties, as it would seem.
-	*/
+
 	public function initType() {
 		$this->setType("xbbp");
 	}
