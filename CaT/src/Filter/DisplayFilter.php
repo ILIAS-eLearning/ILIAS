ba1cfa86b4f69b8f96d70a745448715f5cef44e5
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-02-24 14:48:48 +0100

    decide which gui must be renderd next

--- CaT/src/Filter/DisplayFilter.php
+++ CaT/src/Filter/DisplayFilter.php
@@ -8,87 +8,167 @@ namespace CaT\Filter;
 * Decides which kind of Filter should be displayed and initialize GUI
 */
 class DisplayFilter {
+	const START_PATH = "0";
+
 	protected $sequence;
 	protected $post_values;
 
-	public function __construct(Filters\Sequence $sequence, array $post_values = array(), $position = "") {
+	public function __construct(Filters\Sequence $sequence, array $post_values = array(), $position = "0") {
 		assert(is_string($position));
+
 		$this->sequence = $sequence;
 		$this->post_values = (!empty($post_values)) ? unserialize($post_values) : $post_values;
-		$this->position = ($position != "") ? unserialize($position) : array();
+		$this->position = $position;
+
+		$this->navi = new Navigator($this->sequence, $this->position);
 	}
 
 	/**
-	* starts working the sequence
+	* saves data from current FilterGUI into $post_array
 	*/
-	public function start() {
-		$this->workSequence($this->sequence);
+	public function saveFilter() {
+		$this->post_values[$this->navi->path()] = $_POST;
+		$this->renderNextFilterGUI(false);
 	}
 
 	/**
-	* works the sequence Step By Step
+	* render next filter gui
 	*
-	* @param $sequence
+	* @param $first_filter 		user filter at position 0 or not
 	*/
-	protected function workSequence($sequence) {
-		foreach ($sequence as $key => $value) {
+	public function renderNextFilterGUI($first_filter = true) {
+		if($first_filter) {
+			$this->navi->select("0");
+			$this->position = "0";
+			return $this->filterGUI($this->navi->current());
+		}
 
-			$filter_class = get_class($value);
-			switch($filter_class) {
-				case Filters\Sequence:
-					$this->workSequence($value);
-					break;
-				case Filters\DatePeriod:
-					return $this->workDatePeriod($value);
-					break;
-				case Filters\Multiselect:
-					return $this->workMultiselect($value);
-					break;
-				case Filters\Option:
-					return $this->workOption($value);
-					break;
-				case Filters\Text:
-					return $this->workText($value);
-					break;
-				default:
-					throw new Exception("No Known Filter");
+		if($next = $this->getNextRight()) {
+			$this->filterGUI($next);
+		} else {
+			if($next = $this->getNextUpRight()) {
+				$this->filterGUI($next);
+			}
+		}
+
+		return true;
+	}
+
+	protected function filterGUI($filter) {
+		$filter_class = get_class($filter);
+
+		switch($filter_class) {
+			case Filters\DatePeriod:
+				$this->renderDatePeriod($filter);
+				break;
+			case Filters\Multiselect:
+				$this->renderMultiselect($filter);
+				break;
+			case Filters\Option:
+				$this->renderOption($filter);
+				break;
+			case Filters\Text:
+				$this->renderText($filter);
+				break;
+			case Filters\Sequence:
+				$this->navi->enter();
+				$this->filterGUI($this->navi->current());
+				break;
+			case Filters\OneOf:
+				$this->renderOnOf($filter);
+				break;
+			default:
+				throw new Exception("Filter class not known");
+		}
+	}
+
+	/**
+	* get next right node on limb
+	*
+	* @return current_filter || false
+	*/
+	protected function getNextRight() {
+		try{
+			$this->navi->right();
+			return $this->navi->current();
+		} catch (\OutOfBoundsException $e) {
+			//end of limb
+			return false;
+		}
+	}
+
+	/**
+	* get the next right node at any upper node
+	*
+	* @return current_filter || false
+	*/
+	protected function getNextUpRight() {
+		while($this->getUp()) {
+			$tmp = $this->getNextRight(); 
+
+			if($tmp) {
+				return $tmp;
 			}
 		}
 	}
 
 	/**
-	* works the DatePeriod Filter
+	* get node in tree one step up
+	*
+	* @return upper_filter || false
+	*/
+	protected function getUp() {
+		try {
+			$this->navi->up();
+			return $this->navi->current();
+		} catch (\OutOfBoundsException $e) {
+			//top of tree reached
+			return false;
+		}
+	}
+
+	/**
+	* render the DatePeriod Filter
 	*
 	* @param $filter 
 	*/
-	protected function workDatePeriod($filter) {
+	protected function renderDatePeriod($filter) {
 
 	}
 
 	/**
-	* works the Multiselect Filter
+	* render the Multiselect Filter
 	*
 	* @param $filter 
 	*/
-	protected function workMultiselect($filter) {
+	protected function renderMultiselect($filter) {
 
 	}
 
 	/**
-	* works the Option Filter
+	* render the Option Filter
 	*
 	* @param $filter 
 	*/
-	protected function workOption($filter) {
+	protected function renderOption($filter) {
 
 	}
 
 	/**
-	* works the Text Filter
+	* render the Text Filter
 	*
 	* @param $filter 
 	*/
-	protected function workText($filter) {
+	protected function renderText($filter) {
+
+	}
+
+	/**
+	* render the OneOf Filter
+	*
+	* @param $filter
+	*/
+	protected function renderOnOf($filter) {
 
 	}
 }
