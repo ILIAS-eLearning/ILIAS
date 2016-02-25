bf8b6fbf3beb330714581282a4b6ced3e196a3a7
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-02-25 08:44:34 +0100

    add gui for option filter

---
+++ Services/ReportsRepository/classes/class.catFilterOptionGUI.php
@@ -0,0 +1,40 @@
+<?php
+require_once("Services/Form/classes/class.ilPropertyFormGUI.php");
+require_once("Services/Form/classes/class.ilSelectInputGUI.php");
+require_once("Services/Form/classes/class.ilHiddenInputGUI.php");
+
+class catFilterOptionGUI {
+	protected $parent;
+	protected $filter;
+	protected $path;
+	protected $options;
+
+	public function __construct($parent, $filter, $path, array $post_values) {
+		$this->parent = $parent;
+		$this->filter = $filter;
+		$this->path = $path;
+		$this->options = array("0"=>"Ja","1"=>"Nein");
+	}
+
+	public function executeCommand() {
+
+	}
+
+	public function getHTML() {
+		$form = new ilPropertyFormGUI();
+		$form->setTitle("Title");
+		$form->addCommandButton("saveFilter", $this->lng->txt("continue"));
+		$form->setFormAction($this->ctrl->getFormAction($this->parent));
+
+		$select = new ilSelectInputGUI($this->filter->label(), $this->path);
+		$select->setInfo($this->filter->description());
+		$select->setOptions($this->options);
+		$form->addItem($select);
+
+		$post_values = new ilHiddenInputGUI("post_values");
+		$post_values->setValue(serialize($post_values));
+		$form->addItem($post_values);
+
+		return $form->getHTML();
+	}
+}
