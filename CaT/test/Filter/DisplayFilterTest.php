7d77482a91494de330e56330321241245f238498
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-02-25 09:03:54 +0100

    add test with lower level

--- CaT/test/Filter/DisplayFilterTest.php
+++ CaT/test/Filter/DisplayFilterTest.php
@@ -26,23 +26,35 @@ class DisplayFilterTest extends PHPUnit_Framework_TestCase {
 			$this->assertInstanceOf($classes[$counter], $gui);
 			$counter++;
 		}
-		
-		/*$gui = $df->saveFilter();
-		$this->assertInstanceOf("catFilterOptionGUI", $gui);
-		$gui = $df->saveFilter();*/
 	}
 
 	public function test_display_filter_more_level() {
 		$f1 = $this->factory->text();
-		$f2 = $this->factory->text();
-		$f3 = $this->factory->text();
+		$f2 = $this->factory->multiselect();
+		$f3 = $this->factory->option();
+		$f4 = $this->factory->dateperiod();
+		$fs = $this->factory->sequence($f1, $f2, $f3, $f4);
 
 		$f21 = $this->factory->text();
-		$f22 = $this->factory->text();
-		$f23 = $this->factory->text();
-		$fs2 = $this->factory->sequence($f21, $f22, $f23);
+		$f22 = $this->factory->multiselect();
+		$f23 = $this->factory->option();
+		$f24 = $this->factory->dateperiod();
+		$fs2 = $this->factory->sequence($f21, $f22, $f23, $f24);
 
-		$fs = $this->factory->sequence($f1, $fs2, $f2, $f3);
+		$fs = $this->factory->sequence($f1, $fs2, $f2, $f3, $f4);
 
+		$classes = array("catFilterTextGUI", "catFilterTextGUI", "catFilterMultiselectGUI", "catFilterOptionGUI"
+						, "catFilterDatePeriodGUI", "catFilterMultiselectGUI", "catFilterOptionGUI", "catFilterDatePeriodGUI");
+		$counter = 1;
+
+		$df = new \CaT\Filter\DisplayFilter($fs);
+
+		$gui = $df->getNextFilterGUI(true);
+		$this->assertInstanceOf("catFilterTextGUI", $gui);
+		
+		while($gui = $df->saveFilter()) {
+			$this->assertInstanceOf($classes[$counter], $gui);
+			$counter++;
+		}
 	}
 }
