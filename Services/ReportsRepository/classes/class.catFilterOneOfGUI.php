92739706133fe899c98124c0b6e6afaad0952c9f
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-02-25 09:44:24 +0100

    add OneOf filter gui

---
+++ Services/ReportsRepository/classes/class.catFilterOneOfGUI.php
@@ -0,0 +1,72 @@
+<?php
+require_once("Services/Form/classes/class.ilPropertyFormGUI.php");
+require_once("Services/Form/classes/class.ilRadioGroupInputGUI.php");
+require_once("Services/Form/classes/class.ilRadioOption.php");
+require_once("Services/Form/classes/class.ilHiddenInputGUI.php");
+
+class catFilterOneOfGUI {
+	protected $parent;
+	protected $filter;
+	protected $path;
+	protected $options;
+
+	public function __construct($parent, $filter, $path, array $post_values) {
+		$this->parent = $parent;
+		$this->filter = $filter;
+		$this->path = $path;
+	}
+
+	public function executeCommand() {}
+
+	public function getHTML() {
+		$form = new ilPropertyFormGUI();
+		$form->setTitle("Title");
+		$form->addCommandButton("saveFilter", $this->lng->txt("continue"));
+		$form->setFormAction($this->ctrl->getFormAction($this->parent));
+
+		//create radiogroup
+		$group = new ilRadioGroupInputGUI($this->lng->txt("gev_course_type"), "g".$this->path);
+
+		//create options
+		foreach ($filter->subs() as $key => $sub_filter) {
+			$option = new ilRadioOption($sub_filter->description(), "o".$this->path);
+
+			$filter_class = get_class($sub_filter);
+
+			switch($filter_class) {
+				case "CaT\Filter\Filters\DatePeriod":
+					$duration = new ilDateDurationInputGUI($this->sub_filter->label(), $this->path);
+					$duration->setShowDate(true);
+					$duration->setShowTime(false);
+					$option->addSubItem($duration);
+					break;
+				case "CaT\Filter\Filters\Multiselect":
+					$multi_select = new ilMultiSelectInputGUI($this->sub_filter->label(), $this->path);
+					$multi_select->setOptions($this->sub_filter->options());
+					$option->addSubItem($multi_select);
+					break;
+				case "CaT\Filter\Filters\Option":
+					$select = new ilSelectInputGUI($this->sub_filter->label(), $this->path);
+					$select->setOptions(array("0"=>"Ja","1"=>"Nein"));
+					$option->addSubItem($select);
+					break;
+				case "CaT\Filter\Filters\Text":
+					$input = new ilTextInputGUI($this->sub_filter->label(), $this->path);
+					$option->addSubItem($input);
+					break;
+			}
+		}
+
+		//add option to group
+		$group->addOption($option);
+
+		//add group to form
+		$form->addItem($group);
+
+		$post_values = new ilHiddenInputGUI("post_values");
+		$post_values->setValue(serialize($post_values));
+		$form->addItem($post_values);
+
+		return $form->getHTML();
+	}
+}
