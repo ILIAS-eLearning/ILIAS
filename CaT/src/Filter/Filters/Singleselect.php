e233b6add0d3f245871ec6c302ee877d77e7c6ec
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-02-26 16:35:03 +0100

    change constructor

--- CaT/src/Filter/Filters/Singleselect.php
+++ CaT/src/Filter/Filters/Singleselect.php
@@ -5,6 +5,22 @@
 namespace CaT\Filter\Filters;
 
 class Singleselect extends Multiselect {
+	
+	public function __construct(\CaT\Filter\FilterFactory $factory, $label, $description, $options,
+								$default_choice = array(), array $mappings = array(),
+								array $mapping_result_types = array()) {
+		assert('is_string($label)');
+		assert('is_string($description)');
+
+		$this->setFactory($factory);
+		$this->setLabel($label);
+		$this->setDescription($description);
+		$this->setMappings($mappings, $mapping_result_types);
+
+		$this->options = $options;
+		$this->default_choice = $default_choice;
+	}
+
 	/**
 	 * @inheritdocs
 	 */
