0beae757f77ff6b4225ab5b77dfe0c166e9624f7
Evan Jackson <evantjackson@gmail.com>
2016-02-29 15:09:13 -0400

    fixed botched merge

--- Services/Tracking/classes/rubric/class.ilLPRubricCard.php
+++ Services/Tracking/classes/rubric/class.ilLPRubricCard.php
@@ -563,6 +563,7 @@ class ilLPRubricCard
             );
             
             $broken_weight=explode('-',$weights[$k]);
+            sort($broken_weight);
             
             if($this->ilDB->numRows($set)>0){
                 $row=$this->ilDB->fetchAssoc($set);
