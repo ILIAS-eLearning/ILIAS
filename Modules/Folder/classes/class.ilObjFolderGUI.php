58487815b8c14171b75ceb791d91b179e26ab2ed
smeyer-ilias <smeyer.ilias@gmx.de>
2016-03-08 15:52:06 +0100

    Deprecated constructor Modules/Folder

--- Modules/Folder/classes/class.ilObjFolderGUI.php
+++ Modules/Folder/classes/class.ilObjFolderGUI.php
@@ -27,7 +27,7 @@ class ilObjFolderGUI extends ilContainerGUI
 	* Constructor
 	* @access	public
 	*/
-	function ilObjFolderGUI($a_data, $a_id = 0, $a_call_by_reference = true, $a_prepare_output = false)
+	public function __construct($a_data, $a_id = 0, $a_call_by_reference = true, $a_prepare_output = false)
 	{
 		$this->type = "fold";
 		parent::__construct($a_data, $a_id, $a_call_by_reference, $a_prepare_output, false);
