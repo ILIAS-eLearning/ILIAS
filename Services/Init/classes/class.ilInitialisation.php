8ba8f18b36808e09eb254c1d51d8632346c153f0
Alex Killing <alex.killing@gmx.de>
2016-03-16 17:22:32 +0100

    workaround for bug #17990: Substyle not rendered when following internal link

--- Services/Init/classes/class.ilInitialisation.php
+++ Services/Init/classes/class.ilInitialisation.php
@@ -852,6 +852,24 @@ class ilInitialisation
 	{
 		if (self::$already_initialized) 
 		{
+			// workaround for bug #17990
+			// big mess. we prevent double initialisations with ILIAS 5.1, which is good, but...
+			// the style service uses $_GET["ref_id"] to determine
+			// the context styles. $_GET["ref_id"] is "corrected" by the "goto" procedure and which calls
+			// initILIAS again.
+			// we need a mechanism that detemines our repository context and stores that in an information object
+			// usable by the style component afterwars. This needs new concepts and a refactoring.
+			if(ilContext::initClient())
+			{
+				global $tpl;
+				if (is_object($tpl))
+				{
+					// load style sheet depending on user's settings
+					$location_stylesheet = ilUtil::getStyleSheetLocation();
+					$tpl->setVariable("LOCATION_STYLESHEET", $location_stylesheet);
+				}
+			}
+
 			return;
 		}
 
