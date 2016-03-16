629a7a99f7cc85926168150f7e59798e0bc2d664
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-03-16 10:48:57 +0100

    gev2: #2189 change check message

--- Services/GEV/WBD/classes/Checks/WBDPreliminaryBWVIdIsNotEmpty.php
+++ Services/GEV/WBD/classes/Checks/WBDPreliminaryBWVIdIsNotEmpty.php
@@ -2,7 +2,7 @@
 require_once ("Services/GEV/WBD/classes/Interfaces/WBDPreliminary.php");
 
 class WBDPreliminaryBWVIdIsNotEmpty extends WBDPreliminary {
-	static $message = "User has BWV ID.";
+	static $message = "User has no BWV ID.";
 
 	public function message() {
 		return self::$message;
