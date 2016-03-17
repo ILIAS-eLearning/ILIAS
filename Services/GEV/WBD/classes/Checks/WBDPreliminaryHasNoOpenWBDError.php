c43c051bda3bc9406914a5186a629914addba308
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-03-17 08:55:26 +0100

    gev2: #2189 add custom lang vars

--- Services/GEV/WBD/classes/Checks/WBDPreliminaryHasNoOpenWBDError.php
+++ Services/GEV/WBD/classes/Checks/WBDPreliminaryHasNoOpenWBDError.php
@@ -2,7 +2,7 @@
 require_once ("Services/GEV/WBD/classes/Interfaces/WBDPreliminary.php");
 
 class WBDPreliminaryHasNoOpenWBDError extends WBDPreliminary {
-	static $message = "ge_wbd_check_open_wbd_errors";
+	static $message = "gev_wbd_check_open_wbd_errors";
 	protected $wbd_errors;
 
 	public function __construct(array $wbd_errors) {
