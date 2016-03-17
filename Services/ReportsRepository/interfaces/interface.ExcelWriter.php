a7864dbcf7b22d8a8cb6f3d31b71e19a23f73463
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-03-17 11:26:36 +0100

    extended interface for excel writer

--- Services/ReportsRepository/interfaces/interface.ExcelWriter.php
+++ Services/ReportsRepository/interfaces/interface.ExcelWriter.php
@@ -1,8 +1,49 @@
 <?php
 
 interface ExcelWriter {
-	public function setRowFormat(array $row_format);
+
+	/**
+	 * sets row formats for upcoming rows: bold
+	 */
+	public function setRowFormatBold();
+
+	/**
+	 * sets row formats for upcoming rows: wrap
+	 */
+	public function setRowFormatWrap();
+
+	/**
+	 * adds sheets with name and sets them current 
+	 * @var (string) $name
+	 */
 	public function addSheet($name);
-	public function writeRow(array $row_data, $sheet);
-	public function deliverFile($filename);
+
+	/**
+	 * turn to shee having name
+	 * @var (string) $name
+	 */
+	public function setSheet($name);
+
+	/**
+	 * write to current sheet 
+	 * @var (array) $row_data
+	 */
+	public function writeRow(array $row_data);
+
+	/**
+	 * define output browser
+	 * @var (string) $filename
+	 */
+	public function setOutputBrowser($filename);
+
+	/**
+	 * define output file
+	 * @var (string) $filename
+	 */
+	public function setOutputFile($filename);
+
+	/**
+	 * close and possibly deliver file
+	 */
+	public function close();
 }
