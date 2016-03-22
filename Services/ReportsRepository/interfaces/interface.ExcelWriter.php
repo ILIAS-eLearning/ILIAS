ba0f67d4a0079eb1a98ebdf4763b290fe50b72ed
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-03-22 13:52:59 +0100

    docstrings return

--- Services/ReportsRepository/interfaces/interface.ExcelWriter.php
+++ Services/ReportsRepository/interfaces/interface.ExcelWriter.php
@@ -4,11 +4,13 @@ interface ExcelWriter {
 
 	/**
 	 * sets row formats for upcoming rows: bold
+	 * @return ExcelWriter $this to ensure fluent interface
 	 */
 	public function setRowFormatBold();
 
 	/**
 	 * sets row formats for upcoming rows: wrap
+	 * @return ExcelWriter $this to ensure fluent interface
 	 */
 	public function setRowFormatWrap();
 
@@ -16,6 +18,7 @@ interface ExcelWriter {
 	 * adds sheets with name and sets them current 
 	 * @var string $name
 	 * @throws \InvalidArgumentException
+	 * @return ExcelWriter $this to ensure fluent interface
 	 */
 	public function addSheet($name);
 
@@ -23,12 +26,14 @@ interface ExcelWriter {
 	 * turn to shee having name
 	 * @var string $name
 	 * @throws \InvalidArgumentException
+	 * @return ExcelWriter $this to ensure fluent interface
 	 */
 	public function setSheet($name);
 
 	/**
 	 * write to current sheet 
 	 * @var array $row_data
+	 * @return ExcelWriter $this to ensure fluent interface
 	 */
 	public function writeRow(array $row_data);
 
