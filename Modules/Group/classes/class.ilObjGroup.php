17072bdc1289456a096ab84179e7821da15fd7ff
smeyer-ilias <smeyer.ilias@gmx.de>
2016-03-23 10:45:59 +0100

    0018166: Global User Role looses Local Policy when a course is moved

--- Modules/Group/classes/class.ilObjGroup.php
+++ Modules/Group/classes/class.ilObjGroup.php
@@ -1184,6 +1184,31 @@ class ilObjGroup extends ilContainer implements ilMembershipRegistrationCodes
 
 		return $row["obj_id"];
 	}
+	
+	/**
+	 * 
+	 * @global $ilDB $ilDB
+	 * @param int $a_obj_id
+	 * @return int
+	 */
+	public static function lookupGroupStatusTemplateId($a_obj_id)
+	{
+		global $ilDB;
+		
+		$type = self::lookupGroupTye($a_obj_id);
+		if($type == GRP_TYPE_CLOSED)
+		{
+			$query = 'SELECT obj_id FROM object_data WHERE type = '.$ilDB->quote('rolt','text').' AND title = '.$ilDB->quote('il_grp_status_closed','text');
+		}
+		else
+		{
+			$query = 'SELECT obj_id FROM object_data WHERE type = '.$ilDB->quote('rolt','text').' AND title = '.$ilDB->quote('il_grp_status_open','text');
+		}
+		$res = $ilDB->query($query);
+		$row = $res->fetchRow(DB_FETCHMODE_ASSOC);
+		
+		return isset($row['obj_id']) ? $row['obj_id'] : 0;
+	}
 
 
 	
