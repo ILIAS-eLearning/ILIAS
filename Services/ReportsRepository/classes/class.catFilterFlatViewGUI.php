9b8392244c36f836e501a05f35fdaf1ac241981b
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-03-31 13:19:42 +0200

    gev2: change to one filter tpl. clean up render function

--- Services/ReportsRepository/classes/class.catFilterFlatViewGUI.php
+++ Services/ReportsRepository/classes/class.catFilterFlatViewGUI.php
@@ -16,14 +16,7 @@ class catFilterFlatViewGUI {
 	}
 
 	public function render($post = null) {
-		require_once("Services/CaTUIComponents/classes/class.catPropertyFormTplGUI.php");
-		$form = new catPropertyFormTplGUI();
-		$form->setTemplate("tpl.cat_filter_flat_view.html", "Services/ReportsRepository");
-		$form->setFormAction($this->gCtrl->getFormAction($this->parent_obj));
-		$form->setShowTopButtons(false);
-		$form->getTemplate()->setVariable("BTN_CMD", $this->cmd_save);
-		$form->getTemplate()->setVariable("BTN_VALUE", $this->gLng->txt("gev_filter"));
-
+		$form = $this->initForm();
 		$form_tpl = $form->getTemplate();
 
 		$result = array();
@@ -33,31 +26,7 @@ class catFilterFlatViewGUI {
 			$new_path = array($next_filter_gui->path() => "val");
 			$result = $new_path + $result;
 
-			//if sequence lvl > 2 is reached cry
-			if(substr_count($next_filter_gui->path(), "_") > 1) {
-				throw new Exception("lass das");
-			}
-
-			//sequences in main sequence render filter sidy by side
-			if(substr_count($next_filter_gui->path(), "_") == 1) {
-				$tpl = new ilTemplate("tpl.cat_filter_flat_view_sbs_element.html", true, true, "Services/ReportsRepository");
-			} else {
-				$tpl = new ilTemplate("tpl.cat_filter_flat_view_element.html", true, true, "Services/ReportsRepository");
-			}
-
-			if($post !== null) {
-				$val = $post[$next_filter_gui->path()];
-				$next_filter_gui->setValue($val);
-			}
-
-			if($next_filter_gui instanceof catFilterMultiselectGUI) {
-				$tpl->touchBlock("checkbox");
-			}
-
-			$tpl->setVariable("FILTER_TITLE", $next_filter_gui->formElement()->getTitle());
-			$tpl->setVariable("FILTER_GUI", $next_filter_gui->formElement()->render());
-			$tpl->setVariable("FILTER_INFO", $next_filter_gui->formElement()->getInfo());
-			$tpl->setVariable("FILTER_PATH", $next_filter_gui->path());
+			$tpl = $this->createFilterTemplate($next_filter_gui);
 
 			$form_tpl->setCurrentBlock("filter_element");
 			$form_tpl->setVariable("FILTER_GUI", $tpl->get());
@@ -65,5 +34,49 @@ class catFilterFlatViewGUI {
 		}
 
 		return $form->getHtml();
+	}
+
+	protected function initForm() {
+		require_once("Services/CaTUIComponents/classes/class.catPropertyFormTplGUI.php");
+		$form = new catPropertyFormTplGUI();
+		$form->setTemplate("tpl.cat_filter_flat_view.html", "Services/ReportsRepository");
+		$form->setFormAction($this->gCtrl->getFormAction($this->parent_obj));
+		$form->setShowTopButtons(false);
+		$form->getTemplate()->setVariable("BTN_CMD", $this->cmd_save);
+		$form->getTemplate()->setVariable("BTN_VALUE", $this->gLng->txt("gev_filter"));
+
+		return $form;
+	}
+
+	protected function createFilterTemplate($next_filter_gui) {
+		//if sequence lvl > 2 is reached cry
+		if(substr_count($next_filter_gui->path(), "_") > 1) {
+			throw new Exception("lass das");
+		}
+		
+		$tpl = new ilTemplate("tpl.cat_filter_flat_view_element.html", true, true, "Services/ReportsRepository");
+
+		//sequences in main sequence render filter sidy by side
+		if(substr_count($next_filter_gui->path(), "_") == 1) {
+			$tpl->setVariable("FLOAT", "ilFloatLeft");
+		} else {
+			$tpl->touchBlock("single");
+		}
+
+		if($post !== null) {
+			$val = $post[$next_filter_gui->path()];
+			$next_filter_gui->setValue($val);
+		}
+
+		if($next_filter_gui instanceof catFilterMultiselectGUI) {
+			$tpl->touchBlock("checkbox");
+		}
+
+		$tpl->setVariable("FILTER_TITLE", $next_filter_gui->formElement()->getTitle());
+		$tpl->setVariable("FILTER_GUI", $next_filter_gui->formElement()->render());
+		$tpl->setVariable("FILTER_INFO", $next_filter_gui->formElement()->getInfo());
+		$tpl->setVariable("FILTER_PATH", $next_filter_gui->path());
+
+		return $tpl;
 	}
 }
