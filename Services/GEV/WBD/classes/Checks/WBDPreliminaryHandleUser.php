2c27805d9f28802ad051d4da3d0182c3ff7c5d23
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-04-13 10:19:02 +0200

    gev2: change param name

--- Services/GEV/WBD/classes/Checks/WBDPreliminaryHandleUser.php
+++ Services/GEV/WBD/classes/Checks/WBDPreliminaryHandleUser.php
@@ -7,18 +7,17 @@ require_once ("Services/GEV/WBD/classes/Abstracts/WBDPreliminary.php");
 * if user is in $specified_user_ids do not handle
 */
 class WBDPreliminaryHandleUser extends WBDPreliminary {
-	protected $specified_user_ids;
+	protected $user_not_to_handle;
 	static $message = "gev_wbd_check_no_handle_user";
 
-	public function __construct(array $specified_user_ids) {
-
-		$this->specified_user_ids = $specified_user_ids;
+	public function __construct(array $user_not_to_handle) {
+		$this->user_not_to_handle = $user_not_to_handle;
 	}
 
 	/** 
 	 * @inheritdoc 
 	 */
 	public function performCheck(gevWBD $wbd) {
-		return !$wbd->userIdIn($this->specified_user_ids);
+		return !$wbd->userIdIn($this->user_not_to_handle);
 	}
 }
