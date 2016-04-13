c15a91d3ba37f80305c89e72b0bdb9a82d1808bc
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-04-13 10:13:29 +0200

    gev2: change check user is handled or not

--- Services/GEV/WBD/classes/Checks/WBDPreliminaryHandleUser.php
+++ Services/GEV/WBD/classes/Checks/WBDPreliminaryHandleUser.php
@@ -1,7 +1,12 @@
 <?php
 require_once ("Services/GEV/WBD/classes/Abstracts/WBDPreliminary.php");
 
-class WBDPreliminaryIsUserNotToHandle extends WBDPreliminary {
+/**
+* Check if user ist to handle
+*
+* if user is in $specified_user_ids do not handle
+*/
+class WBDPreliminaryHandleUser extends WBDPreliminary {
 	protected $specified_user_ids;
 	static $message = "gev_wbd_check_no_handle_user";
 
@@ -14,6 +19,6 @@ class WBDPreliminaryIsUserNotToHandle extends WBDPreliminary {
 	 * @inheritdoc 
 	 */
 	public function performCheck(gevWBD $wbd) {
-		return $wbd->hasSpecialUserId($this->specified_user_ids);
+		return !$wbd->userIdIn($this->specified_user_ids);
 	}
 }
