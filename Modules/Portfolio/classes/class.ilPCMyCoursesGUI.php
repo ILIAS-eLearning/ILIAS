03760742759daa58353245e43911e1f370550691
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2016-04-13 15:00:02 +0200

    php7: static, constructor

--- Modules/Portfolio/classes/class.ilPCMyCoursesGUI.php
+++ Modules/Portfolio/classes/class.ilPCMyCoursesGUI.php
@@ -21,9 +21,9 @@ class ilPCMyCoursesGUI extends ilPageContentGUI
 	* Constructor
 	* @access	public
 	*/
-	function ilPCMyCoursesGUI($a_pg_obj, $a_content_obj, $a_hier_id, $a_pc_id = "")
+	function __construct($a_pg_obj, $a_content_obj, $a_hier_id, $a_pc_id = "")
 	{
-		parent::ilPageContentGUI($a_pg_obj, $a_content_obj, $a_hier_id, $a_pc_id);
+		parent::__construct($a_pg_obj, $a_content_obj, $a_hier_id, $a_pc_id);
 	}
 
 	/**
