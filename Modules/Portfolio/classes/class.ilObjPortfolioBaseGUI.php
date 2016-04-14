ab110071d1dc42944c71ade8b89c868ca493dcb7
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2016-04-14 14:13:26 +0200

    #18291 - portfolio: no pages check

--- Modules/Portfolio/classes/class.ilObjPortfolioBaseGUI.php
+++ Modules/Portfolio/classes/class.ilObjPortfolioBaseGUI.php
@@ -661,11 +661,15 @@ abstract class ilObjPortfolioBaseGUI extends ilObject2GUI
 		
 		if(!$a_content)
 		{
-			// get current page content
-			$page_gui = $this->getPageGUIInstance($current_page);
-			$page_gui->setEmbedded(true);
+			// #18291
+			if($current_page)
+			{
+				// get current page content
+				$page_gui = $this->getPageGUIInstance($current_page);
+				$page_gui->setEmbedded(true);
 
-			$content = $this->ctrl->getHTML($page_gui);			
+				$content = $this->ctrl->getHTML($page_gui);		
+			}
 		}
 		else
 		{
@@ -679,7 +683,7 @@ abstract class ilObjPortfolioBaseGUI extends ilObject2GUI
 						
 		// blog posting comments are handled within the blog
 		$notes = "";
-		if($a_show_notes && $this->object->hasPublicComments() && !$current_blog)
+		if($a_show_notes && $this->object->hasPublicComments() && !$current_blog && $current_page)
 		{			
 			include_once("./Services/Notes/classes/class.ilNoteGUI.php");			
 			$note_gui = new ilNoteGUI($portfolio_id, $current_page, "pfpg");
