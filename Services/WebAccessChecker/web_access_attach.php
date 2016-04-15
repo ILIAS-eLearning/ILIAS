c33d658362881ab21915edaf8408f2ca6fab49b1
Fabian Schmid <fs@studer-raimann.ch>
2016-04-15 16:26:30 +0200

    [FIX] WAC replaced + with " "

--- Services/WebAccessChecker/web_access_attach.php
+++ Services/WebAccessChecker/web_access_attach.php
@@ -2,9 +2,11 @@
 /**
  * Runs the ILIAS WebAccessChecker 2.0
  *
- * @author Fabian Schmid <fs@studer-raimann.ch>
+ * @author     Fabian Schmid <fs@studer-raimann.ch>
+ *
+ * @deprecated Thias file will be removed with ILIAS 5.2. Use wac.php instead
  */
 
 chdir('../../');
 require_once('./Services/WebAccessChecker/classes/class.ilWebAccessChecker.php');
-ilWebAccessChecker::run(urldecode($_SERVER['REQUEST_URI']));
+ilWebAccessChecker::run(rawurldecode($_SERVER['REQUEST_URI']));
