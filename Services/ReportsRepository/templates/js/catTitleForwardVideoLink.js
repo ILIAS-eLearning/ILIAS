d7bbfd64119622aed64d5cd3e15a4695acd10062
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-04-18 11:14:18 +0200

    gev2: #2264 remove empty row

--- Services/ReportsRepository/templates/js/catTitleForwardVideoLink.js
+++ Services/ReportsRepository/templates/js/catTitleForwardVideoLink.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 	$(document).on("click","#catVideoLeftId, #catVideoCenterId, #catVideoRightId", function(e) {
-		
 		window.open($("#catVideoLinkUrlId").html(), '_blank');
 	});
 });
