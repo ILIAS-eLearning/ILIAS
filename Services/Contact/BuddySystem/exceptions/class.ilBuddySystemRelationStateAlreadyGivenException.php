b1a2db6528bea1ed6f861c5ec91ad170cebdba61
mjansenDatabay <mjansen@databay.de>
2016-04-25 16:20:59 +0200

    Changes according to Mantis issue #18339

---
+++ Services/Contact/BuddySystem/exceptions/class.ilBuddySystemRelationStateAlreadyGivenException.php
@@ -0,0 +1,8 @@
+<?php
+/* Copyright (c) 1998-2016 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+require_once 'Services/Contact/BuddySystem/exceptions/class.ilBuddySystemRelationStateException.php';
+
+class ilBuddySystemRelationStateAlreadyGivenException extends ilBuddySystemRelationStateException
+{
+}
