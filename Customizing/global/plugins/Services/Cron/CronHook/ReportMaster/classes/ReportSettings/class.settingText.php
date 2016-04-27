b49a32f8460efa2af81af482f84796df65183908
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-04-27 14:14:53 +0200

    setting text

---
+++ Customizing/global/plugins/Services/Cron/CronHook/ReportMaster/classes/ReportSettings/class.settingText.php
@@ -0,0 +1,26 @@
+<?php
+require_once 'Customizing/global/plugins/Services/Cron/CronHook/ReportMaster/classes/ReportSettings/class.setting.php';
+
+class settingText extends setting {
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function defaultDefaultValue() {
+		return "";
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function defaultToForm() {
+		return function($val) {return $val;};
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function defaultFromForm() {
+		return function($val) {return $val;};
+	}
+}
