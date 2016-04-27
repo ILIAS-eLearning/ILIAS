9d3f073735873801c539fec3e9b5aaad0cb72022
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-04-27 14:12:09 +0200

    include full field list needed currently

--- Customizing/global/plugins/Services/Cron/CronHook/ReportMaster/sql/dbupdate.php
+++ Customizing/global/plugins/Services/Cron/CronHook/ReportMaster/sql/dbupdate.php
@@ -15,10 +15,28 @@ $fields =
 		'video_link' => array(
 			'type' => 'text',
 			'length' => 400,
-			'notnull' => true
+			'notnull' => false
 		));
 if(!$ilDB->tableExists("rep_master_data")) {
 	$ilDB->createTable("rep_master_data", $fields);
 	$ilDB->addPrimaryKey("rep_master_data", array("id"));
 }
+?>
+
+<#2>
+<?php
+	if (!$ilDB->tableColumnExists('rep_master_data', 'pdf_link')) {		
+		$ilDB->addTableColumn('rep_master_data', 'pdf_link',array(
+			'type' => 'text',
+			'length' => 400,
+			'notnull' => false
+		));
+	}
+
+	if (!$ilDB->tableColumnExists('rep_master_data', 'tooltip_info')) {		
+		$ilDB->addTableColumn('rep_master_data', 'tooltip_info',array(
+			'type' => 'clob',
+			'notnull' => false
+		));
+	}
 ?>
