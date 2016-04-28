c686a25b3cea297d8500600792bfb015c6250f70
mjansenDatabay <mjansen@databay.de>
2016-04-28 14:15:17 +0200

    Removed HTTP GET param

--- Modules/Course/classes/class.ilObjCourseGUI.php
+++ Modules/Course/classes/class.ilObjCourseGUI.php
@@ -132,7 +132,6 @@ class ilObjCourseGUI extends ilContainerGUI
 			array(), 
 			array(
 				'type' => 'new', 
-				'rcp_to' => implode(',',$rcps),
 				'sig'	=> $this->createMailSignature()
 		)));
 	}
