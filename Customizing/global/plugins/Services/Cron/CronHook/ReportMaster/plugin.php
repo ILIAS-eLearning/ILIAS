c115aadbb67f963e165ab5f1f9eb50d57556ca1d
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-04-28 09:41:46 +0200

    adjusted lugin version

--- Customizing/global/plugins/Services/Cron/CronHook/ReportMaster/plugin.php
+++ Customizing/global/plugins/Services/Cron/CronHook/ReportMaster/plugin.php
@@ -4,7 +4,7 @@
 $id = "ReportMaster";
   
 // code version; must be changed for all code changes
-$version = "0.0.2";
+$version = "0.0.3";
   
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
