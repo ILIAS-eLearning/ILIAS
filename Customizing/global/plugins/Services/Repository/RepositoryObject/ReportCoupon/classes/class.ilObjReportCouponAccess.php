75083dd4221ddc823a7039985eaede9bd3c6dbc6
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-04-28 17:12:29 +0200

    adapted coupon to new logic

--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportCoupon/classes/class.ilObjReportCouponAccess.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportCoupon/classes/class.ilObjReportCouponAccess.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'Services/ReportsRepository/classes/class.ilObjReportBaseAccess.php';
+require_once 'Customizing/global/plugins/Services/Cron/CronHook/ReportMaster/classes/ReportBase/class.ilObjReportBaseAccess.php';
 
 
 class ilObjReportCouponAccess extends ilObjReportBaseAccess {
