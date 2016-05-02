50516bc4a8d71d95c1767c60d4e3f33ce35574e3
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-05-02 14:47:49 +0200

    splicing OD/BD and OD/FD/BD ID into several roles

--- Services/GEV/WBD/classes/class.gevWBD.php
+++ Services/GEV/WBD/classes/class.gevWBD.php
@@ -86,6 +86,8 @@ class gevWBD {
 			*/
 			"OD /BD"
 			,"OD/BD"
+			,"OD"
+			,"BD"
 			,"FD"
 			,"Org PV 59"
 			,"PV 59"
@@ -130,6 +132,8 @@ class gevWBD {
 			,'ID FK'
 			,'ID MA'
 			,'OD/FD/BD ID'
+			,'OD/FD ID'
+			,'BD ID'
 			,'FDA'
 			,'Ausbilder'
 			,'Azubi'
