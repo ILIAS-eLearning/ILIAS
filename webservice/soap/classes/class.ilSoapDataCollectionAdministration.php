2ff29230ebba264affeb4fd5765c726b24a6f812
Michael Herren <michherren@gmail.com>
2016-05-04 14:46:04 +0200

    added async-export to soap-service

---
+++ webservice/soap/classes/class.ilSoapDataCollectionAdministration.php
@@ -0,0 +1,89 @@
+<?php
+  /*
+   +-----------------------------------------------------------------------------+
+   | ILIAS open source                                                           |
+   +-----------------------------------------------------------------------------+
+   | Copyright (c) 1998-2006 ILIAS open source, University of Cologne            |
+   |                                                                             |
+   | This program is free software; you can redistribute it and/or               |
+   | modify it under the terms of the GNU General Public License                 |
+   | as published by the Free Software Foundation; either version 2              |
+   | of the License, or (at your option) any later version.                      |
+   |                                                                             |
+   | This program is distributed in the hope that it will be useful,             |
+   | but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+   | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+   | GNU General Public License for more details.                                |
+   |                                                                             |
+   | You should have received a copy of the GNU General Public License           |
+   | along with this program; if not, write to the Free Software                 |
+   | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
+   +-----------------------------------------------------------------------------+
+  */
+
+include_once('./webservice/soap/classes/class.ilSoapAdministration.php');
+require_once ('./Modules/DataCollection/classes/class.ilDclContentExporter.php');
+require_once ('./Modules/DataCollection/classes/class.ilObjDataCollectionAccess.php');
+
+/**
+ * Soap data-collection administration methods
+ *
+ * @author Michael Herren <mh@studer-raimann.ch>
+ *
+ * @package ilias
+ */
+class ilSoapDataCollectionAdministration extends ilSoapAdministration
+{
+	public function __construct()
+	{
+		parent::ilSoapAdministration();
+	}
+
+
+	/**
+	 * Export DataCollection async
+	 *
+	 * @param               $sid
+	 * @param               $target_ref_id
+	 * @param null|int      $table_id
+	 * @param string        $format
+	 * @param null|string   $filepath
+	 *
+	 * @return soap_fault|SoapFault
+	 */
+	public function exportDataCollectionContent ($sid, $target_ref_id, $table_id = null, $format = ilDclContentExporter::EXPORT_EXCEL, $filepath = null) {
+
+		$this->initAuth($sid);
+		$this->initIlias();
+
+   	    if(!$this->__checkSession($sid))
+		{
+			return $this->__raiseError($this->__getMessage(),$this->__getMessageCode());
+		}
+
+        if(!$target_obj = new ilObjDataCollection($target_ref_id))
+		{
+			return $this->__raiseError('No valid target given.', 'CLIENT');
+		}
+
+		if(ilObject::_isInTrash($target_ref_id))
+		{
+			return $this->__raiseError("Parent with ID $target_ref_id has been deleted.", 'CLIENT_TARGET_DELETED');
+		}
+
+		if(!ilObjDataCollectionAccess::hasReadAccess($target_ref_id))
+		{
+			return $this->__raiseError('Check access failed. No permission to read DataCollection', "CLIENT_PERMISSION_ISSUE");
+		}
+
+    	try
+        {
+	        $exporter = new ilDclContentExporter($target_ref_id, $table_id);
+	        return $exporter->export($format, $filepath);
+        }
+        catch(ilException $exception) {
+        	return $this->__raiseError($exception->getMessage(), $exception->getCode());
+     	}
+	 }
+}
+?>
