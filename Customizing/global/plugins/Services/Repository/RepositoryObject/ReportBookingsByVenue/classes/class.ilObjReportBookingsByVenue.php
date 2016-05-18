4134449fb94a03193b591050615cced62b929d46
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-05-18 10:53:15 +0200

    bookings by venue WIP

--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportBookingsByVenue/classes/class.ilObjReportBookingsByVenue.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportBookingsByVenue/classes/class.ilObjReportBookingsByVenue.php
@@ -1,1 +1,24 @@
 <?php
+
+require_once 'Customizing/global/plugins/Services/Cron/CronHook/ReportMaster/classes/ReportBase/class.ilObjReportBase.php';
+
+ini_set('max_execution_time', 0);
+set_time_limit(0);
+
+class ilObjReportBookingsByVenue extends ilObjReportBase {
+	
+	protected $relevant_parameters = array();
+
+	public function __construct($a_ref_id = 0) {
+		parent::__construct($a_ref_id);
+	}
+
+	public function initType() {
+		 $this->setType("xbbv");
+	}
+
+	protected function createLocalReportSettings() {
+		$this->local_report_settings =
+			$this->s_f->reportSettings('rep_robj_rbbv');
+	}
+}
--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportBookingsByVenue/classes/class.ilObjReportBookingsByVenueGUI.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportBookingsByVenue/classes/class.ilObjReportBookingsByVenueGUI.php
@@ -1,1 +1,31 @@
 <?php
+
+require_once 'Customizing/global/plugins/Services/Cron/CronHook/ReportMaster/classes/ReportBase/class.ilObjReportBaseGUI.php';
+/**
+* User Interface class for example repository object.
+* ...
+* @ilCtrl_isCalledBy ilObjReportBookingsByVenueGUI: ilRepositoryGUI, ilAdministrationGUI, ilObjPluginDispatchGUI
+* @ilCtrl_Calls ilObjReportBookingsByVenueGUI: ilPermissionGUI, ilInfoScreenGUI, ilObjectCopyGUI
+* @ilCtrl_Calls ilObjReportBookingsByVenueGUI: ilCommonActionDispatcherGUI
+*/
+class ilObjReportBookingsByVenueGUI extends ilObjReportBaseGUI {
+
+	public function getType() {
+		return 'xbbv';
+	}
+
+	protected function prepareTitle($a_title) {
+		$a_title = parent::prepareTitle($a_title);
+		$a_title->image("GEV_img/ico-head-edubio.png");
+		return $a_title;
+	}
+
+
+	public static function transformResultRow($rec) {
+		return parent::transformResultRow($rec);
+	}
+
+	public static function transformResultRowXLSX($rec) {
+		return parent::transformResultRow($rec);
+	}
+}
