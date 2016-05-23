6482a4e043edf94219212f6ae56e29bf917fda77
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2016-05-23 15:01:34 +0200

    #18408 - exercise: save selected

--- Modules/Exercise/classes/class.ilExerciseManagementGUI.php
+++ Modules/Exercise/classes/class.ilExerciseManagementGUI.php
@@ -818,8 +818,10 @@ class ilExerciseManagementGUI
 		{
 			$this->ctrl->redirect($this, "members");
 		}
-		$data = array();
-		foreach(array_keys($members) as $user_id)
+		
+		// #18408 - saveStatus() will rollout teams, we need raw (form) data here 
+		$data = array();				
+		foreach(array_keys($_POST["member"]) as $user_id)
 		{
 			$data[-1][$user_id] = array(
 				"status" => ilUtil::stripSlashes($_POST["status"][$user_id])
