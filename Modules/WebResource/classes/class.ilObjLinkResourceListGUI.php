c20d409b61008865069605e844e36680dd3f1b69
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2016-05-23 11:28:25 +0200

    #18419 - weblinks: internal/external

--- Modules/WebResource/classes/class.ilObjLinkResourceListGUI.php
+++ Modules/WebResource/classes/class.ilObjLinkResourceListGUI.php
@@ -83,30 +83,21 @@ class ilObjLinkResourceListGUI extends ilObjectListGUI
 	*/
 	function getCommandFrame($a_cmd)
 	{
-		// #16820
-		return;
-		
-		switch($a_cmd)
-		{
-			case "":
-				if(ilObjLinkResourceAccess::_checkDirectLink($this->obj_id))
-				{
-					$frame = '_blank';
-				}
-				else
-				{
-					$frame = ilFrameTargetInfo::_getFrame("RepositoryContent");
-				}
-				break;
-
-			default:
-		}
-
-		return $frame;
+		// #16820 / #18419		
+		if(ilObjLinkResourceAccess::_checkDirectLink($this->obj_id))
+		{			
+			$link = ilObjLinkResourceAccess::_getFirstLink($this->obj_id);
+			
+			// we could use the "internal" flag, but it would not work for "old" links
+			include_once "Services/Form/classes/class.ilFormPropertyGUI.php";
+			include_once "Services/Form/classes/class.ilLinkInputGUI.php";								
+			if(!ilLinkInputGUI::isInternalLink($link["target"]))			
+			{		
+				return '_blank';
+			}						
+		}			
 	}
 			
-
-
 	/**
 	* Get item properties
 	*
