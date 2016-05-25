ea78f1e6a3f7b3e2b70a1ad49d5538be8e8af0ca
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-05-25 16:36:12 +0200

    remove obsolete training categories

--- Customizing/global/plugins/Services/AdvancedMetaData/AdvancedMDClaiming/CourseAMD/sql/dbupdate.php
+++ Customizing/global/plugins/Services/AdvancedMetaData/AdvancedMDClaiming/CourseAMD/sql/dbupdate.php
@@ -892,3 +892,36 @@ gevAMDUtils::addAMDField( "Verwaltung"
 						, ilAdvancedMDFieldDefinition::TYPE_TEXT
 						);
 ?>
+
+<#27>
+<?php
+require_once("Services/AdvancedMetaData/classes/class.ilAdvancedMDFieldDefinition.php");
+require_once("Services/GEV/Utils/classes/class.gevAMDUtils.php");
+require_once("Services/GEV/Utils/classes/class.gevSettings.php");
+
+$amdutils = gevAMDUtils::getInstance();
+
+$options = array(
+	  "SUHK - Privatkunden"
+	, "SUHK - Firmenkunden"
+	, "Leben und Rente"
+	, "Betriebliche Altersvorsorge"
+	, "Akquise / Verkauf"
+	, "Beratungs- und Tarifierungstools"
+	, "Büromanagment"
+	, "Neue Medien"
+	, "Unternehmensführung"
+	, "Agenturmanagment"
+	, "Führung"
+	, "Grundausbildung"
+	, "Erstausbildung"
+	, "Qualifizierungsprogramme"
+	, "Assistanceleistungen"
+	, "Investment"
+	, "Kranken und Pflege"
+	, "Rechtsschutz"
+	, "Bausparen und Finanzieren"
+	);
+
+$amdutils->updateOptionsOfAMDField(gevSettings::CRS_AMD_TOPIC, $options);
+?>
