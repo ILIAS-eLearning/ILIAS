838e6e6b294a7946b76d4861aab90a6d9fc75701
Anton Arsenij <a.arsenij@gmx.de>
2016-05-27 18:21:56 +0200

    Release 4.4.15

--- include/inc.ilias_version.php
+++ include/inc.ilias_version.php
@@ -9,6 +9,6 @@
 *
 * @package ilias-core
 */
-define("ILIAS_VERSION", "4.4.14 2016-03-01");
-define("ILIAS_VERSION_NUMERIC", "4.4.14");			// must be always x.y.z: x, y and z are numbers
+define("ILIAS_VERSION", "4.4.15 2016-05-27");
+define("ILIAS_VERSION_NUMERIC", "4.4.15");			// must be always x.y.z: x, y and z are numbers
 ?>
