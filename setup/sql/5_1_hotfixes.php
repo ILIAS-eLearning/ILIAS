7bd35375a455603e5ad561b85a11ea53dfcd9977
Alex Killing <alex.killing@gmx.de>
2016-05-31 11:50:55 +0200

    fixed 18479: Cannot copy media pool

--- setup/sql/5_1_hotfixes.php
+++ setup/sql/5_1_hotfixes.php
@@ -131,4 +131,24 @@ if(!$ilDB->indexExistsByFields('usr_data_multi',array('usr_id')))
 <?php
 	$ilCtrlStructureReader->getStructure();
 ?>
+<#10>
+<?php
+include_once('./Services/Migration/DBUpdate_3560/classes/class.ilDBUpdateNewObjectType.php');
+$tgt_ops_id = ilDBUpdateNewObjectType::getCustomRBACOperationId('copy');
+if($tgt_ops_id)
+{
+	$mep_type_id = ilDBUpdateNewObjectType::getObjectTypeId('mep');
+	if($mep_type_id)
+	{
+		if (!ilDBUpdateNewObjectType::isRBACOperation($mep_type_id, $tgt_ops_id))
+		{
+			// add "copy" to (external) feed
+			ilDBUpdateNewObjectType::addRBACOperation($mep_type_id, $tgt_ops_id);
 
+			// clone settings from "write" to "copy"
+			$src_ops_id = ilDBUpdateNewObjectType::getCustomRBACOperationId('write');
+			ilDBUpdateNewObjectType::cloneOperation('mep', $src_ops_id, $tgt_ops_id);
+		}
+	}
+}
+?>
