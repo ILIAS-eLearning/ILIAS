f2c7a4b332878250b0ff3a97b2c5c57d8587c017
Alex Killing <alex.killing@gmx.de>
2016-06-01 18:58:15 +0200

    fixed template calls

--- Services/Skill/classes/class.ilSkillCategoryGUI.php
+++ Services/Skill/classes/class.ilSkillCategoryGUI.php
@@ -43,7 +43,7 @@ class ilSkillCategoryGUI extends ilSkillTreeNodeGUI
 	{
 		global $ilCtrl, $tpl, $ilTabs;
 		
-		$tpl->getStandardTemplate();
+		//$tpl->getStandardTemplate();
 		
 		$next_class = $ilCtrl->getNextClass($this);
 		$cmd = $ilCtrl->getCmd();
