10ad58c97e4452d767c420ac3da52e978b8fdc74
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-06-01 15:24:54 +0200

    adapted dbv sup report to point to point to refs corresponding to dbv reports, if proper object type

--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportDBVSuperior/classes/class.ilObjReportDBVSuperiorGUI.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportDBVSuperior/classes/class.ilObjReportDBVSuperiorGUI.php
@@ -11,12 +11,13 @@ require_once 'Services/Form/classes/class.ilNumberInputGUI.php';
 */
 class ilObjReportDBVSuperiorGUI extends ilObjReportBaseGUI {
 
-	protected static $year = 2015;
-
+	protected static $dbv_report_ref = null;
+	protected static $bd_regexp;
+	protected static $od_regexp;
 	protected function afterConstructor() {
 		parent::afterConstructor();
-		if( null !== $this->object) {
-			self::$year = $this->object->settings['year'];
+		if( null !== $this->object && 'xrdv' === ilObject::_lookupType($this->object->settings['dbv_report_ref'], true)) {
+			self::$dbv_report_ref = $this->object->settings['dbv_report_ref'];
 		}
 	}
 
@@ -32,14 +33,45 @@ class ilObjReportDBVSuperiorGUI extends ilObjReportBaseGUI {
 
 
 	public static function transformResultRow($rec) {
+		if(self::$dbv_report_ref) {
+			$rec = self::addDbvReportLink($rec);
+		}
+		return parent::transformResultRow(self::setBD($rec));
+	}
+
+	public static function addDbvReportLink($rec) {
 		global $ilCtrl;
-		$rec['odbd'] = $rec['org_unit_above1'];
-		$ilCtrl->setParameterByClass("gevDBVReportGUI", "target_user_id", $rec["user_id"]);
-		$ilCtrl->setParameterByClass("gevDBVReportGUI", "year", self::$year);
-		$rec["dbv_report_link"] = $ilCtrl->getLinkTargetByClass(array("gevDesktopGUI","gevDBVReportGUI"));
-		$ilCtrl->setParameterByClass("gevDBVReportGUI", "target_user_id", null);
-		$ilCtrl->setParameterByClass("gevDBVReportGUI", "year",  null);
-		return parent::transformResultRow($rec);
+		$ilCtrl->setParameterByClass("ilObjReportDBVGUI", "target_user_id", $rec["user_id"]);
+		$ilCtrl->setParameterByClass("ilObjReportDBVGUI", "ref_id", self::$dbv_report_ref);
+		$rec["dbv_report_link"] = $ilCtrl->getLinkTargetByClass(array("ilObjPluginDispatchGUI","ilObjReportDBVGUI"));
+		$ilCtrl->setParameterByClass("ilObjReportDBVGUI", "target_user_id", null);
+		$ilCtrl->setParameterByClass("ilObjReportDBVGUI", "ref_id",  null);
+		return $rec;
+	}
+
+	public static function setBD($rec) {
+		if(!self::$bd_regexp ) {
+			require_once './Customizing/global/plugins/Services/Cron/CronHook/ReportMaster/config/od_bd_strings.php';
+		}
+		$orgu_above1 =  $rec['org_unit_above1'];
+		$orgu_above2 =  $rec['org_unit_above2'];
+
+		if (preg_match(self::$bd_regexp, $orgu_above1)) {
+			$bd = $orgu_above1;
+		} elseif(preg_match(self::$bd_regexp, $orgu_above2)) {
+			$bd = $orgu_above2;
+		} else {
+			$bd = '-';
+		}
+		$rec['odbd'] = $bd;
+		return $rec;
+	}
+
+	protected function renderSettings() {
+		parent::renderSettings();
+		if('xrdv' !== ilObject::_lookupType($this->object->settings['dbv_report_ref'], true)) {
+			ilUtil::sendInfo($this->object->settings['dbv_report_ref']." corresponds not to a DBV Report");
+		}
 	}
 
 	public static function transformResultRowXLSX($rec) {
