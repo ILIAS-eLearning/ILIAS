4853e5899a18e908fff065c996b11101e78d8b76
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-06-02 17:07:55 +0200

    finishing gui for edubio

--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportEduBio/classes/class.ilObjReportEduBioGUI.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportEduBio/classes/class.ilObjReportEduBioGUI.php
@@ -23,14 +23,20 @@ class ilObjReportEduBioGUI extends ilObjReportBaseGUI {
 
 
 	public function performCustomCommand($cmd) {
+		//die($cmd);
 		switch ($cmd) {
 			case "getCertificate":
-				return $this->getCertificate();
+				$this->object->prepareRelevantParameters();
+				$this->getCertificate();
+				break;
 			case "getBill":
-				return $this->getBill();
+				$this->object->prepareRelevantParameters();
+				$this->getBill();
+				break;
 			default:
 				return false;
 		}
+		return true;
 	}
 
 	protected function prepareTitle($a_title) {
@@ -67,8 +73,8 @@ class ilObjReportEduBioGUI extends ilObjReportBaseGUI {
 		self::$success_img  = '<img src="'.ilUtil::getImagePath("GEV_img/ico-key-green.png").'" />';
 		self::$in_progress_img = '<img src="'.ilUtil::getImagePath("GEV_img/ico-key-orange.png").'" />';
 		self::$failed_img = '<img src="'.ilUtil::getImagePath("GEV_img/ico-key-red.png").'" />';
-		parent::renderReport();
 		self::$target_user_id = $this->object->target_user_id;
+		parent::renderReport();
 	}
 
 	protected function render() {
@@ -93,14 +99,14 @@ class ilObjReportEduBioGUI extends ilObjReportBaseGUI {
 			if(gevWBD::getInstance($this->object->target_user_id)->transferPointsToWBD()) {
 				$tpl->setVariable("WBDTRANSVISIBIBLE", "visible");
 				$tpl->setCurrentBlock("wbd_transfer");
-				$tpl->setVariable("TRANSFER_TITLE", $this->lng->txt("gev_wbd_transfer_on"));
+				$tpl->setVariable("TRANSFER_TITLE", $this->object->plugin->txt("gev_wbd_transfer_on"));
 				$tpl->parseCurrentBlock();
 			}
 			elseif (gevWBD::getInstance($this->object->target_user_id)->wbdRegistrationIsPending()){
 				$tpl->setVariable("WBDPOINTSVISIBIBLE", "invisible");
 				$tpl->setCurrentBlock("wbd_reg_pending");
 				$tpl->setVariable("WBDREGPENDINGVISIBIBLE", "visible");
-				$tpl->setVariable("WBD_REG_PENDING", $this->lng->txt("gev_wbd_reg_pending"));
+				$tpl->setVariable("WBD_REG_PENDING", $this->object->plugin->txt("gev_wbd_reg_pending"));
 				$tpl->parseCurrentBlock();
 			}
 			else {
@@ -111,8 +117,8 @@ class ilObjReportEduBioGUI extends ilObjReportBaseGUI {
 	}
 
 	protected function insertAcademyPoints($tpl) {
-		$tpl->setVariable("ACADEMY_SUM_TITLE", $this->lng->txt("gev_points_in_academy"));
-		$tpl->setVariable("ACADEMY_SUM_FIVE_YEAR_TITLE", $this->lng->txt("gev_points_in_five_years"));
+		$tpl->setVariable("ACADEMY_SUM_TITLE",$this->object->plugin->txt("gev_points_in_academy"));
+		$tpl->setVariable("ACADEMY_SUM_FIVE_YEAR_TITLE", $this->object->plugin->txt("gev_points_in_five_years"));
 
 		$tpl->setVariable("ACADEMY_FIVE_YEAR", $this->object->academy_data["five_year"]);
 
@@ -125,10 +131,10 @@ class ilObjReportEduBioGUI extends ilObjReportBaseGUI {
 	}
 
 	protected function insertWBDPoints($tpl) {
-		$tpl->setVariable("WBD_SUM_TITLE", $this->lng->txt("gev_points_in_wbd"));
-		$tpl->setVariable("WBD_SUM_CERT_PERIOD_TITLE", $this->lng->txt("gev_points_in_wbd_cert_period"));
-		$tpl->setVariable("WBD_SUM_CUR_YEAR_TITLE", $this->lng->txt("gev_points_in_wbd_cert_year"));
-		$tpl->setVariable("WBD_SUM_CUR_YEAR_PRED_TITLE", $this->lng->txt("gev_points_at_end_of_cert_year"));
+		$tpl->setVariable("WBD_SUM_TITLE", $this->object->plugin->txt("gev_points_in_wbd"));
+		$tpl->setVariable("WBD_SUM_CERT_PERIOD_TITLE", $this->object->plugin->txt("gev_points_in_wbd_cert_period"));
+		$tpl->setVariable("WBD_SUM_CUR_YEAR_TITLE", $this->object->plugin->txt("gev_points_in_wbd_cert_year"));
+		$tpl->setVariable("WBD_SUM_CUR_YEAR_PRED_TITLE", $this->object->plugin->txt("gev_points_at_end_of_cert_year"));
 
 		$tpl->setVariable("WBD_CERT_PERIOD", $this->object->wbd_data["cert_period"]);
 		$tpl->setVariable("WBD_CERT_YEAR", $this->object->wbd_data["cert_year"]);
@@ -150,23 +156,14 @@ class ilObjReportEduBioGUI extends ilObjReportBaseGUI {
 	protected function getBill() {
 		// check weather this bill really belongs to an edu bio record of the current user.
 		$bill_id = $_GET["bill_id"];
-		$res = $this->db->query( "SELECT crs_id"
-								."  FROM hist_usercoursestatus "
-								." WHERE usr_id = ".$this->db->quote($this->object->target_user_id, "integer")
-								."   AND bill_id = ".$this->db->quote($bill_id, "text")
-								."   AND hist_historic = 0"
-								);
 		
-		if ($this->db->numRows($res) != 1) {
-			return $this->render();
+		if (!$this->object->validateBill($bill_id)) {
+			$this->gCtrl->redirect($this, "showContent");
 		}
-		$rec = $this->db->fetchAssoc($res);
-
 		require_once("Services/GEV/Utils/classes/class.gevBillStorage.php");
-		require_once 'Services/Utilities/classes/class.ilUtil.php';
-
+		$year = ilBill::getInstanceByBillNumber($bill_id)->getBillYear();
 		$fname = "Rechnung_".$bill_id.".pdf";
-		$bill_storage = gevBillStorage::getInstance();
+		$bill_storage = gevBillStorage::getInstance($year);
 		$path = $bill_storage->getPathByBillNumber($bill_id);
 		ilUtil::deliverFile($path, $fname, 'application/pdf', false, false, true);
 	}
@@ -174,37 +171,25 @@ class ilObjReportEduBioGUI extends ilObjReportBaseGUI {
 	protected function getCertificate() {
 		// check weather this cert really belongs to an edu bio of the current user
 		$cert_id = $_GET["cert_id"];
-		$res = $this->db->query( "SELECT COUNT(*) cnt"
-								."  FROM hist_usercoursestatus "
-								." WHERE usr_id = ".$this->db->quote($this->object->target_user_id, "integer")
-								."   AND certificate = ".$this->db->quote($cert_id, "integer"));
-		if ($rec = $this->db->fetchAssoc($res)) {
-			if ($rec["cnt"] == 0) {
-				return $this->render();
-			}
+		if (!$this->object->validateCertificate($cert_id)) {
+			$this->gCtrl->redirect($this, "showContent");
 		}
-
-		// query certificate data
-		$res = $this->db->query( "SELECT hc.certfile, hs.crs_id "
-								."  FROM hist_certfile hc"
-								." JOIN hist_usercoursestatus hs ON hs.certificate = hc.row_id"
-								." WHERE hc.row_id = ".$this->db->quote($cert_id, "integer"));
-		if ($rec = $this->db->fetchAssoc($res)) {
+		$cert_data = $this->object->certificateData($cert_id);
+		if ($cert_data) {
 			require_once("Services/Utilities/classes/class.ilUtil.php");
 			require_once("Services/GEV/Utils/classes/class.gevCourseUtils.php");
-			$crs_utils = gevCourseUtils::getInstance($rec["crs_id"]);
-			ilUtil::deliverData(base64_decode($rec["certfile"]), "Zertifikat_".$crs_utils->getCustomId().".pdf", "application/pdf");
-		}
-		else {
-			return $this->render();
+			$crs_utils = gevCourseUtils::getInstance($cert_data["crs_id"]);
+			ilUtil::deliverData(base64_decode($cert_data["certfile"]), "Zertifikat_".$crs_utils->getCustomId().".pdf", "application/pdf");
+		} else {
+			$this->gCtrl->redirect($this, "showContent");
 		}
 	}
 
 	public static function transformResultRow($rec) {
-		global $lng, $ilCtrl;
+		global $lng;
 		$no_entry = $lng->txt("gev_table_no_entry");
 
-		$rec["fee"] = (($rec["bill_id"] != -1 || $this->target_user_utils->paysFees())&& $rec["fee"] != -1)
+		$rec["fee"] = (($rec["bill_id"] != -1 || gevUserUtils::getInstanceById(self::$target_user_id)->paysFees())&& $rec["fee"] != -1)
 					? $rec["fee"] = gevCourseUtils::formatFee($rec["fee"])." &euro;"
 					: $rec["fee"] == "-empty-";
 
@@ -237,26 +222,19 @@ class ilObjReportEduBioGUI extends ilObjReportBaseGUI {
 			$rec["date"] = ilDatePresentation::formatDate($start)." - <br/>".ilDatePresentation::formatDate($end);
 		}
 
+		$rec['credit_points'] = $rec['credit_points'] >= 0 ? $rec['credit_points'] : 0;
 		$rec["wbd"] = in_array($rec["okz"], array("OKZ1", "OKZ2", "OKZ3"))
 					? $lng->txt("yes")
 					: $lng->txt("no");
 
 		$rec["action"] = "";
 		if ($rec["bill_id"] != -1 && $rec["bill_id"] != "-empty-") {
-			$ilCtrl->setParameterByClass("ilObjReportEduBioGUI", "bill_id", $rec["bill_id"]);
-			$ilCtrl->setParameterByClass("ilObjReportEduBioGUI", "target_user_id", self::$target_user_id);
-			$rec["action"] = "<a href='".$ilCtrl->getLinkTargetByClass(array("ilObjPluginDispatchGUI","ilObjReportEduBioGUI"), "getBill")."'>"
-						   . self::$get_bill_img."</a>";
-			$ilCtrl->setParameterByClass("ilObjReportEduBioGUI",   "bill_id", null);
-			$ilCtrl->setParameterByClass("ilObjReportEduBioGUI",   "target_user_id", null);
+			$params = array("bill_id" => $rec["bill_id"],  "target_user_id" => self::$target_user_id);
+			$rec["action"] .= "<a href='".self::getLinkToThis("getBill",$params)."'>". self::$get_bill_img."</a>";
 		}
 		if ($rec["certificate"] != -1 && $rec["certificate"] != 0) {
-			$ilCtrl->setParameterByClass("ilObjReportEduBioGUI",  "cert_id", $rec["certificate"]);
-			$ilCtrl->setParameterByClass("ilObjReportEduBioGUI",  "target_user_id", self::$target_user_id);
-			$rec["action"] .= "<a href='".$this->ctrl->getLinkTarget($this, "getCertificate")."'>"
-						   . self::$get_cert_img."</a>";
-			$ilCtrl->setParameterByClass("ilObjReportEduBioGUI",  "cert_id", null);
-			$ilCtrl->setParameterByClass("ilObjReportEduBioGUI",  "target_user_id", null);
+			$params = array("cert_id" => $rec["certificate"],  "target_user_id" => self::$target_user_id);
+			$rec["action"] .= "<a href='".self::getLinkToThis("getCertificate",$params)."'>". self::$get_cert_img."</a>";
 		}
 		if ($rec["ref_id"] !== null) {
 			$rec["link_open"] = "<a href='goto.php?target=crs_".$rec["ref_id"]."'>";
@@ -266,14 +244,62 @@ class ilObjReportEduBioGUI extends ilObjReportBaseGUI {
 			$rec["link_open"] = "";
 			$rec["link_close"] = "";
 		}
+		return parent::transformResultRow($rec);
+	}
 
-		foreach ($rec as $key => $value) {
-			if ($value == '-empty-' || $value == -1) {
-				$rec[$key] = $no_entry;
-				continue;
-			}
+	public static function transformResultRowXLSX($rec) {
+		global $lng;
+		$no_entry = $lng->txt("gev_table_no_entry");
+
+		$rec["fee"] = (($rec["bill_id"] != -1 || gevUserUtils::getInstanceById(self::$target_user_id)->paysFees())&& $rec["fee"] != -1)
+					? $rec["fee"] = gevCourseUtils::formatFee($rec["fee"])
+					: $rec["fee"] == "-empty-";
+
+		if ($rec["participation_status"] == "teilgenommen") {
+			$rec["status"] = self::$success_img;
 		}
-		
-		return $rec;
+		else if (in_array($rec["participation_status"], array("fehlt entschuldigt", "fehlt ohne Absage"))
+			 ||  in_array($rec["booking_status"], array("kostenpflichtig storniert", "kostenfrei storniert"))
+			) {
+			$rec["status"] = self::$failed_img;
+		}
+		else {
+			$rec["status"] = self::$in_progress_img;
+		}
+
+		if ($rec["begin_date"] == "0000-00-00" && $rec["end_date"] == "0000-00-00") {
+			$rec["date"] = $no_entry;
+		}
+		else if ($rec["end_date"] == "0000-00-00") {
+			$dt = new ilDate($rec["begin_date"], IL_CAL_DATE);
+			$rec["date"] = $lng->txt("gev_from")." ".ilDatePresentation::formatDate($dt);
+		}
+		else if ($rec["begin_date"] == "0000-00-00") {
+			$dt = new ilDate($rec["end_date"], IL_CAL_DATE);
+			$rec["date"] = $lng->txt("gev_until")." ".ilDatePresentation::formatDate($dt);
+		}
+		else {
+			$start = new ilDate($rec["begin_date"], IL_CAL_DATE);
+			$end = new ilDate($rec["end_date"], IL_CAL_DATE);
+			$rec["date"] = ilDatePresentation::formatDate($start)." - <br/>".ilDatePresentation::formatDate($end);
+		}
+
+		$rec['credit_points'] = $rec['credit_points'] >= 0 ? $rec['credit_points'] : 0;
+		$rec["wbd"] = in_array($rec["okz"], array("OKZ1", "OKZ2", "OKZ3"))
+					? $lng->txt("yes")
+					: $lng->txt("no");
+		return parent::transformResultRowXLSX($rec);
+	}
+
+	protected static function getLinkToThis($cmd,$params) {
+		global $ilCtrl;
+		foreach ($params as $key => $value) {
+			$ilCtrl->setParameterByClass("ilObjReportEduBioGUI",  $key, $value);
+		}
+		$link = $ilCtrl->getLinkTargetByClass(array("ilObjPluginDispatchGUI","ilObjReportEduBioGUI"), $cmd);
+		foreach ($params as $key => $value) {
+			$ilCtrl->setParameterByClass("ilObjReportEduBioGUI",  $key, null);
+		}
+		return $link;
 	}
 }
