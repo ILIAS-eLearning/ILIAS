ef7643c74dee88c95c7a7c40abcd529b2df4d0a6
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-06-02 14:20:43 +0200

    deleted dublicate file with typo in name

--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportEduBio/classes/classs.ilObjReportEduBioAccess.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once 'Customizing/global/plugins/Services/Cron/CronHook/ReportMaster/classes/ReportBase/class.ilObjReportBaseAccess.php';
-
-
-class ilObjReportDBVSuperiorAccess extends ilObjReportBaseAccess {
-	const TABLE_TITLE = 'rep_robj_reb';
-}
