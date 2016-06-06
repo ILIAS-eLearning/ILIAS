f22dbc26b12f7805cd49dd3f5f57ac182793f36a
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2016-06-06 10:42:04 +0200

    #18508 - exercise: global feedback

--- Modules/Exercise/classes/class.ilObjExerciseGUI.php
+++ Modules/Exercise/classes/class.ilObjExerciseGUI.php
@@ -2379,8 +2379,9 @@ class ilObjExerciseGUI extends ilObjectGUI
 					$this->form->getItemByPostVar("peer")
 						->setAlert($lng->txt("exc_needs_deadline"));
 					$valid = false;
-				}					
-				if($_POST["fb"])
+				}		
+				// #18508
+				if($_POST["fb"] && $_POST["fb_date"] == ilExAssignment::FEEDBACK_DATE_DEADLINE)
 				{
 					$this->form->getItemByPostVar("fb")
 						->setAlert($lng->txt("exc_needs_deadline"));
