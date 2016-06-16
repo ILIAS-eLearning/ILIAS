80962681b95901fcae7335527d2050958843ae83
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-06-16 15:05:47 +0200

    moved the factory crashtest to test folder and stripped it from its namespace

--- tests/UI/FactoriesCrawlerCrashtestdummy.php
+++ tests/UI/FactoriesCrawlerCrashtestdummy.php
@@ -1,15 +1,16 @@
 <?php
-namespace ILIAS\UI\Implementation\Crawler;
 use  ILIAS\UI\Implementation\Crawler\Exception as Ex;
 use ILIAS\UI\Implementation\Crawler\Entry as En;
+use ILIAS\UI\Implementation\Crawler as Crawler;
+
 /**
  * This is a wrapper around the actual FactoriesCrawler created in order
- * to make it more suitable for abstract factory testing. Instead of simply 
+ * to make it more suitable for abstract factory testing. Instead of simply
  * dying with an exception at an invalid factory branch, it will be skipped,
  * after logging the exception. Note that this may also be tested using Crawler
  * 	tests by including an appropriate exception handler.
  */
-class FactoriesCrawlerCrashtestdummy extends FactoriesCrawler {
+class FactoriesCrawlerCrashtestdummy extends Crawler\FactoriesCrawler {
 	protected $exception_handler;
 	public function __construct(Ex\CrawlerExceptionHandler $exception_handler) {
 		$this->exception_handler = $exception_handler;
