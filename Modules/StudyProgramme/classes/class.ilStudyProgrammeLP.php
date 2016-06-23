c2e0f4b6917fcd91cf4b3b94fe6e291c735dd2f0
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-06-23 13:27:42 +0200

    signature mismatch PHP7 error

--- Modules/StudyProgramme/classes/class.ilStudyProgrammeLP.php
+++ Modules/StudyProgramme/classes/class.ilStudyProgrammeLP.php
@@ -26,7 +26,7 @@ class ilStudyProgrammeLP extends ilObjectLP {
 			);
 	}
 	
-	public function getMembers() {
+	public function getMembers($a_search = true) {
 		if ($this->prg === null) {
 			require_once("Modules/StudyProgramme/classes/class.ilObjStudyProgramme.php");
 			$this->prg = new ilObjStudyProgramme($this->obj_id, false);
