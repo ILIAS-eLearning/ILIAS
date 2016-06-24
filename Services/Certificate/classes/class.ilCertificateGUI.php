758ff5c195d704601b4a19fa27fa448fbc84c300
mjansenDatabay <mjansen@databay.de>
2016-06-24 10:15:33 +0200

    RTE: DIC / Changes

--- Services/Certificate/classes/class.ilCertificateGUI.php
+++ Services/Certificate/classes/class.ilCertificateGUI.php
@@ -33,10 +33,9 @@ include_once("./Services/Certificate/classes/class.ilCertificate.php");
 class ilCertificateGUI
 {
 	/**
-	* ilCertificate object reference
-	*
-	* @var integer
-	*/
+	 * ilCertificate object reference
+	 * @var ilCertificate
+	 */
 	protected $object;
 
 	/**
