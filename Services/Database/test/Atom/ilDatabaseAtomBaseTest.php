2fa55b1ac044b19a598b513e4b8f84252ea0f10d
Fabian Schmid <fs@studer-raimann.ch>
2016-07-11 12:47:35 +0200

    improved tests

--- Services/Database/test/Atom/ilDatabaseAtomBaseTest.php
+++ Services/Database/test/Atom/ilDatabaseAtomBaseTest.php
@@ -33,21 +33,37 @@ class ilDatabaseAtomBaseTest extends PHPUnit_Framework_TestCase {
 	 * @var bool
 	 */
 	protected $backupGlobals = false;
+	/**
+	 * @var ilDBInterface
+	 */
+	protected $ilDBInterfaceGalera;
+	/**
+	 * @var ilDBInterface
+	 */
+	protected $ilDBInterfaceInnoDB;
 
 
 	protected function setUp() {
 		require_once("./Services/PHPUnit/classes/class.ilUnitUtil.php");
 		ilUnitUtil::performInitialisation();
 		require_once('./Services/Database/classes/Atom/class.ilAtomQuery.php');
+
+		global $ilClientIniFile;
+		$this->ilDBInterfaceGalera = ilDBWrapperFactory::getWrapper(ilDBConstants::TYPE_PDO_MYSQL_GALERA);
+		$this->ilDBInterfaceGalera->initFromIniFile($ilClientIniFile);
+		$this->ilDBInterfaceGalera->connect();
+
+		$this->ilDBInterfaceInnoDB = ilDBWrapperFactory::getWrapper(ilDBConstants::TYPE_PDO_MYSQL_INNODB);
+		$this->ilDBInterfaceInnoDB->initFromIniFile($ilClientIniFile);
+		$this->ilDBInterfaceInnoDB->connect();
 	}
 
 
 	public function testGetInstance() {
-		global $ilDB;
-		$serializable = new ilAtomQuery($ilDB);
+		$serializable = new ilAtomQuery($this->ilDBInterfaceGalera);
 		$this->assertEquals($serializable->getIsolationLevel(), ilAtomQuery::ISOLATION_SERIALIZABLE);
 
-		$serializable = new ilAtomQuery($ilDB, ilAtomQuery::ISOLATION_SERIALIZABLE);
+		$serializable = new ilAtomQuery($this->ilDBInterfaceGalera, ilAtomQuery::ISOLATION_SERIALIZABLE);
 		$this->assertEquals($serializable->getIsolationLevel(), ilAtomQuery::ISOLATION_SERIALIZABLE);
 	}
 
@@ -56,9 +72,8 @@ class ilDatabaseAtomBaseTest extends PHPUnit_Framework_TestCase {
 	 * @throws \ilDatabaseException
 	 */
 	public function testReadUncommited() {
-		global $ilDB;
 		$this->setExpectedException('ilDatabaseException');
-		$other = new ilAtomQuery($ilDB, ilAtomQuery::ISOLATION_READ_UNCOMMITED);
+		$other = new ilAtomQuery($this->ilDBInterfaceGalera, ilAtomQuery::ISOLATION_READ_UNCOMMITED);
 		$other->run();
 	}
 
@@ -67,9 +82,8 @@ class ilDatabaseAtomBaseTest extends PHPUnit_Framework_TestCase {
 	 * @throws \ilDatabaseException
 	 */
 	public function testReadCommited() {
-		global $ilDB;
 		$this->setExpectedException('ilDatabaseException');
-		$other = new ilAtomQuery($ilDB, ilAtomQuery::ISOLATION_READ_COMMITED);
+		$other = new ilAtomQuery($this->ilDBInterfaceGalera, ilAtomQuery::ISOLATION_READ_COMMITED);
 		$other->run();
 	}
 
@@ -78,9 +92,121 @@ class ilDatabaseAtomBaseTest extends PHPUnit_Framework_TestCase {
 	 * @throws \ilDatabaseException
 	 */
 	public function testReadRepeatedRead() {
-		global $ilDB;
 		$this->setExpectedException('ilDatabaseException');
-		$other = new ilAtomQuery($ilDB, ilAtomQuery::ISOLATION_REPEATED_READ);
+		$other = new ilAtomQuery($this->ilDBInterfaceGalera, ilAtomQuery::ISOLATION_REPEATED_READ);
 		$other->run();
+	}
+
+
+	/**
+	 * @throws \ilDatabaseException
+	 */
+	public function testAnomalies() {
+		$this->setExpectedException('ilDatabaseException');
+		ilAtomQuery::checkAnomaly('lorem');
+	}
+
+
+	/**
+	 * @throws \ilDatabaseException
+	 */
+	public function testLevel() {
+		$this->setExpectedException('ilDatabaseException');
+		ilAtomQuery::checkIsolationLevel('lorem');
+	}
+
+	
+	public function testRisks() {
+		$ilAtomQuery = new ilAtomQuery($this->ilDBInterfaceGalera);
+		$ilAtomQuery->lockTableWrite('il_db_tests_atom');
+		$this->assertEquals(array(), $ilAtomQuery->getRisks());
+	}
+
+
+	/**
+	 * @throws \ilDatabaseException
+	 */
+	public function checkCallables() {
+		$ilAtomQuery = new ilAtomQuery($this->ilDBInterfaceGalera);
+		$ilAtomQuery->addQueryCallable(function (ilDBInterface $ilDB) {
+			$ilDB->getDBType();
+		});
+		$ilAtomQuery->run();
+	}
+
+
+	public function testCreateTable() {
+		if ($this->ilDBInterfaceGalera->sequenceExists('il_db_tests_atom')) {
+			$this->ilDBInterfaceGalera->dropSequence('il_db_tests_atom');
+		}
+		$this->ilDBInterfaceGalera->createTable('il_db_tests_atom', $fields = array(
+			'id'        => array(
+				'type'    => 'integer',
+				'length'  => 4,
+				'notnull' => true,
+			),
+			'is_online' => array(
+				'type'    => 'integer',
+				'length'  => 1,
+				'notnull' => false,
+			),
+		), true);
+		$this->ilDBInterfaceGalera->addPrimaryKey('il_db_tests_atom', array( 'id' ));
+		$this->ilDBInterfaceGalera->createSequence('il_db_tests_atom');
+		$this->assertTrue($this->ilDBInterfaceGalera->tableExists('il_db_tests_atom'));
+	}
+
+
+	/**
+	 * @depends testCreateTable
+	 */
+	public function testWriteAtomOne() {
+		$ilAtomQuery = new ilAtomQuery($this->ilDBInterfaceGalera);
+		$ilAtomQuery->lockTableWrite('il_db_tests_atom');
+		$query = function (ilDBInterface $ilDB) {
+			$ilDB->insert('il_db_tests_atom', array(
+				'id'        => array( 'integer', $ilDB->nextId('il_db_tests_atom') ),
+				'is_online' => array( 'integer', 1 ),
+			));
+		};
+		$ilAtomQuery->addQueryCallable($query);
+		$ilAtomQuery->addQueryCallable($query);
+		$ilAtomQuery->run();
+
+		$res = $this->ilDBInterfaceGalera->query('SELECT * FROM il_db_tests_atom');
+		$results = array();
+		while ($d = $this->ilDBInterfaceGalera->fetchAssoc($res)) {
+			$results[] = $d;
+		}
+		$this->assertEquals(array(
+			0 => array(
+				'id'        => '1',
+				'is_online' => '1',
+			),
+			1 => array(
+				'id'        => '2',
+				'is_online' => '1',
+			),
+		), $results);
+	}
+
+
+	/**
+	 * @depends testCreateTable
+	 */
+	public function testWriteAtomParallel() {
+		$ilAtomQueryOne = new ilAtomQuery($this->ilDBInterfaceGalera);
+		$ilAtomQueryOne->addTable('il_db_tests_atom', ilAtomQuery::LOCK_WRITE);
+		$query = function (ilDBInterface $ilDB) {
+			$ilDB->insert('il_db_tests_atom', array(
+				'id'        => array( 'integer', $ilDB->nextId('il_db_tests_atom') ),
+				'is_online' => array( 'integer', 1 ),
+			));
+		};
+		$ilAtomQueryOne->addQueryCallable($query);
+		$ilAtomQueryOne->addQueryCallable($query);
+
+		// Run both
+		$ilAtomQueryOne->run();
 	}
 }
