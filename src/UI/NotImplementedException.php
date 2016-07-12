502cf66d6318a1f5a8aa89dfb0d4beb4f6b32d9e
Richard Klees <richard.klees@concepts-and-training.de>
2016-07-12 07:04:11 +0200

    UI: fixed NotImplementedException

--- src/UI/NotImplementedException.php
+++ src/UI/NotImplementedException.php
@@ -2,9 +2,11 @@
 
 /* Copyright (c) 2015 Richard Klees <richard.klees@concepts-and-training.de> Extended GPL, see docs/LICENSE */
 
+namespace ILIAS\UI;
+
 /**
  * This exception indicates that an UI component was accepted by the JF but is
  * not backed by a real implementation.
  */
-class NotImplementedException implements \Exeption {
+class NotImplementedException extends \Exception {
 }
