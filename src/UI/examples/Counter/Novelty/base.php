c7d20750a749f6a7120fdd44ea991d87d24bd836
Timon Amstutz <timon.amstutz@bluewin.ch>
2016-07-12 18:29:32 +0200

    Glyph and counter examples modified

---
+++ src/UI/examples/Counter/Novelty/base.php
@@ -0,0 +1,9 @@
+<?php
+function base() {
+    global $DIC;
+    $f = $DIC->ui()->factory();
+    $renderer = $DIC->ui()->renderer();
+
+    return $renderer->render($f->glyph()->mail("#")
+        ->withCounter($f->counter()->novelty(3)));
+}
---
+++ src/UI/examples/Glyph/User/user.php
@@ -0,0 +1,8 @@
+<?php
+function user() {
+	global $DIC;
+	$f = $DIC->ui()->factory();
+	$renderer = $DIC->ui()->renderer();
+
+	return $renderer->render($f->glyph()->user("#"));
+}
