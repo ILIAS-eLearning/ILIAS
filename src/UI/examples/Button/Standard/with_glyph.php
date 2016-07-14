b1f88cdb4ef75f9e44912e9dc4c989e188a0eebc
Richard Klees <richard.klees@concepts-and-training.de>
2016-07-14 10:30:17 +0200

    UI: added some examples for button.

---
+++ src/UI/examples/Button/Standard/with_glyph.php
@@ -0,0 +1,11 @@
+<?php
+function with_glyph() {
+    global $DIC;
+    $f = $DIC->ui()->factory();
+    $renderer = $DIC->ui()->renderer();
+
+    return $renderer->render(
+		$f->button()->standard("Goto ILIAS", "http://www.ilias.de")
+			->withGlyph($f->glyph()->next())
+	);
+}
