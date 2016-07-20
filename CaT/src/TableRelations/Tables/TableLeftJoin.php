5ea463a39f6a265aa6b9cacd29b36e720e1e7817
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-07-20 08:57:17 +0200

    table join and table left join have allmost functionality, thus we may derive them from common class

--- CaT/src/TableRelations/Tables/TableLeftJoin.php
+++ CaT/src/TableRelations/Tables/TableLeftJoin.php
@@ -1,25 +1,5 @@
 <?php
 namespace \CaT\TableRelations\Tables;
-use \CaT\TableRelations\Graphs as Graphs;
 
-
-class TableLeftJoin implements Graphs\abstractTableDependency,Graphs\abstractEdge {
-
-	public function dependingTables(abstractTable $from, abstractTable $to, Predicates\Predicate $predicate) {
-		$this->from = $from;
-		$this->to = $to;
-		$this->predicate = $predicate;
-	}
-
-	public function from() {
-		return $this->from->id();
-	}
-
-	public function to() {
-		return $this->to->id();
-	}
-
-	public function dependanceCondition() {
-		return $this->predicate;
-	}
+class TableLeftJoin extends TableDependency {
 }
