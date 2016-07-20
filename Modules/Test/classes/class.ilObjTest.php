b255a31745324a4f82294e1157df7ba43b0c2dab
mjansen <mjansen@databay.de>
2016-07-20 11:40:33 +0200

    Exclude objects within buyable courses

--- Modules/Test/classes/class.ilObjTest.php
+++ Modules/Test/classes/class.ilObjTest.php
@@ -5899,6 +5899,9 @@ function getAnswerFeedbackPoints()
 				case "pass_scoring":
 					$this->setPassScoring($metadata["entry"]);
 					break;
+				case "pass_deletion_allowed":
+					$this->setPassDeletionAllowed((int)$metadata["entry"]);
+					break;
 				case "show_summary":
 					$this->setListOfQuestionsSettings($metadata["entry"]);
 					break;
@@ -6157,6 +6160,11 @@ function getAnswerFeedbackPoints()
 		$a_xml_writer->xmlStartTag("qtimetadatafield");
 		$a_xml_writer->xmlElement("fieldlabel", NULL, "pass_scoring");
 		$a_xml_writer->xmlElement("fieldentry", NULL, $this->getPassScoring());
+		$a_xml_writer->xmlEndTag("qtimetadatafield");
+
+		$a_xml_writer->xmlStartTag("qtimetadatafield");
+		$a_xml_writer->xmlElement("fieldlabel", NULL, "pass_deletion_allowed");
+		$a_xml_writer->xmlElement("fieldentry", NULL, (int)$this->isPassDeletionAllowed());
 		$a_xml_writer->xmlEndTag("qtimetadatafield");
 
 		// score reporting date
