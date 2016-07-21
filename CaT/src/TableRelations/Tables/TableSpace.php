007c799c161f4da9249b55046a1eb7ca012fbed1
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-07-21 13:23:59 +0200

    explained TableSpace object

--- CaT/src/TableRelations/Tables/TableSpace.php
+++ CaT/src/TableRelations/Tables/TableSpace.php
@@ -1,5 +1,11 @@
 <?php
 namespace \CaT\TableRelations\Tables;
 
+/**
+ * Table management takes place here.
+ * Translates tables to graphs and operates them
+ * to create and forward sorted graphs/query representatives
+ * to graph interpreters.
+ */
 class TableSpace {
 }
