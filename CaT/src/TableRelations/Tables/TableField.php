d2ab07fc800c20cb733b5f0a4532d0f5e276e032
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-07-21 13:32:33 +0200

    explained TableField 's

--- CaT/src/TableRelations/Tables/TableField.php
+++ CaT/src/TableRelations/Tables/TableField.php
@@ -3,7 +3,10 @@
 namespace \CaT\TableRelations\Tables;
 
 use \CaT\Filter\Predicates as Predicates;
-
+/**
+ * A field combining Predicate-Fields and table functinality.
+ * Note: it may still be used in Predicates.
+ */
 class TableField extends Predicates\Field implements abstractTableField {
 
 	public function __construct(Predicates\PredicateFactory $f, $name, $table_id = null) {
@@ -11,6 +14,10 @@ class TableField extends Predicates\Field implements abstractTableField {
 		parent::__construct($f, $name);
 	}
 
+	/**
+	 * Any TableField may be related to a Table.
+	 * Two different Tables may contain fields with equal name.
+	 */
 	public function tableId() {
 		return $this->table_id;
 	}
