d12565e17576d0bbbe7acc2a4d79262205294466
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-07-22 16:53:39 +0200

    using nonnummeric string in graph tests, as php autoconverts them to ints :-(

--- CaT/test/TableRelations/Graphs/GraphTest.php
+++ CaT/test/TableRelations/Graphs/GraphTest.php
@@ -1,99 +1,99 @@
 <?php
-require_once "test/TableRelations/Graphs/GraphTestFactory.php";
-use \CaT\TableRelations\Graphs as Graphs;
+require_once "CaT/test/TableRelations/Graphs/GraphTestFactory.php";
+use CaT\TableRelations\Graphs as Graphs;
 class GraphTest extends PHPUnit_Framework_TestCase {
 	public function setUp() {
 		$this->gtf = new GraphTestFactory();
 	}
 
 	public function circleGraph() {
-		$gtf = $this->gtf();
+		$gtf = $this->gtf;
 		$g = $gtf->Graph();
-		$g->addNode($gtf->Node("1"));
-		$g->addNode($gtf->Node("2"));
-		$g->addNode($gtf->Node("3"));
-		$g->connectNodesSymmetric($gtf->Edge("1","2"));
-		$g->connectNodesSymmetric($gtf->Edge("2","3"));
-		$g->connectNodesSymmetric($gtf->Edge("3","1"));
+		$g->addNode($gtf->Node("a1"));
+		$g->addNode($gtf->Node("a2"));
+		$g->addNode($gtf->Node("a3"));
+		$g->connectNodesSymmetric($gtf->Edge("a1","a2"));
+		$g->connectNodesSymmetric($gtf->Edge("a2","a3"));
+		$g->connectNodesSymmetric($gtf->Edge("a3","a1"));
 		return $g;
 	}
 
 	public function starGraph() {
-		$gtf = $this->gtf();
+		$gtf = $this->gtf;
 		$g = $gtf->Graph();
-		$g->addNode($gtf->Node("1"),1);
-		$g->addNode($gtf->Node("2"),1);
-		$g->addNode($gtf->Node("3"),2);
-		$g->addNode($gtf->Node("4"),1);
-		$g->addNode($gtf->Node("5"),1);
-		$g->addNode($gtf->Node("6"),2);
-		$g->connectNodesSymmetric($gtf->Edge("1","2"));
-		$g->connectNodesSymmetric($gtf->Edge("2","3"));
-		$g->connectNodesSymmetric($gtf->Edge("3","1"));
-		$g->connectNodesDirected($gtf->Edge("1","4"));
-		$g->connectNodesDirected($gtf->Edge("2","5"));
-		$g->connectNodesDirected($gtf->Edge("3","6"));
+		$g->addNode($gtf->Node("a1"),1);
+		$g->addNode($gtf->Node("a2"),1);
+		$g->addNode($gtf->Node("a3"),2);
+		$g->addNode($gtf->Node("a4"),1);
+		$g->addNode($gtf->Node("a5"),1);
+		$g->addNode($gtf->Node("a6"),2);
+		$g->connectNodesSymmetric($gtf->Edge("a1","a2"));
+		$g->connectNodesSymmetric($gtf->Edge("a2","a3"));
+		$g->connectNodesSymmetric($gtf->Edge("a3","a1"));
+		$g->connectNodesDirected($gtf->Edge("a1","a4"));
+		$g->connectNodesDirected($gtf->Edge("a2","a5"));
+		$g->connectNodesDirected($gtf->Edge("a3","a6"));
 		return $g;
 	}
 
 	public function test_add_nodes() {
 		$g = $this->circleGraph();
-
+		$g_edges = array("a1" => "a2"
+						,"a2" => "a3"
+						,"a3" => "a1");
 		$nodes = $g->nodes();
 		$edges = $g->edges();
 
 		foreach ($nodes as $node) {
 			$node_ids[] = $node->id();
 		}
-		$this->asserCount(3, $node_ids);
-		$this->assertCount(0,array_diff($node_ids, array("1","2","3")));
-		$this->assertCount(0,array_diff(array("1","2","3"), $node_ids));
+		$this->assertCount(3, $node_ids);
+		$this->assertCount(0,array_diff($node_ids, array("a1","a2","a3")));
+		$this->assertCount(0,array_diff(array("a1","a2","a3"), $node_ids));
 
-		$this->asserCount(3, $edges);
+		$this->assertCount(3, $edges);
 		foreach ($edges as $edge) {
-			$from_id = $edge->from();
-			$to_id = $edge->to();
-			$this->assertTrue(in_array($from, array("1","2","3")));
-			$this->assertTrue((string)((int)$from+1) === $to);
+			$from_id = $edge->fromId();
+			$to_id = $edge->toId();
+			$this->assertEquals($g_edges[$from_id], $to_id);
 		}
 	}
 
 	public function test_nodes_between() {
 		$g = $this->starGraph();
-		$nodes = $g->getNodesBetween("1","5");
+		$nodes = $g->getNodesBetween("a1","a5");
 		foreach ($nodes as $node) {
 			$node_ids[] = $node->id();
 		}
-		$this->asserCount(4,$node_ids);
-		$this->assertCount(0,array_diff($node_ids, array("1","2","3","5")));
-		$this->assertCount(0,array_diff(array("1","2","3","5"), $node_ids));
+		$this->assertCount(0,array_diff($node_ids, array("a1","a2","a3","a5")));
+		$this->assertCount(0,array_diff(array("a1","a2","a3","a5"), $node_ids));
 	}
 
 	public function test_nodes_within_subgraph_between() {
 		$g = $this->starGraph();
-		$nodes = $g->getNodesWithinSubgraphBetween("1","5",1);
+		$nodes = $g->getNodesWithinSubgraphBetween("a1","a5",1);
 		foreach ($nodes as $node) {
 			$node_ids[] = $node->id();
 		}
 		$this->asserCount(3,$node_ids);
-		$this->assertCount(0,array_diff($node_ids, array("1","2","5")));
-		$this->assertCount(0,array_diff(array("1","2","5"), $node_ids));
+		$this->assertCount(0,array_diff($node_ids, array("a1","a2","a5")));
+		$this->assertCount(0,array_diff(array("a1","a2","a5"), $node_ids));
 	}
 
 	/**
-	 * @expectedException Graphs\GraphException
+	 * @expectedException CaT\TableRelations\Graphs\GraphException
 	 */
 	public function test_no_double_nodes_in_graph() {
 		$g = $this->circleGraph();
-		$g->addNode($this->gtf->Node("3"));
+		$g->addNode($this->gtf->Node("a3"));
 	}
 
 	public function test_graph_connected() {
 		$g = $this->starGraph();
 		$this->assertTrue($g->isConnected());
-		$g->addNode($gtf->Node("7"));
-		$g->addNode($gtf->Node("8"));
-		$g->connectNodesDirected($gtf->Edge("7","8"));
+		$g->addNode($gtf->Node("a7"));
+		$g->addNode($gtf->Node("a8"));
+		$g->connectNodesDirected($gtf->Edge("a7","a8"));
 		$this->assertFalse($g->isConnected());
 	}
 }
