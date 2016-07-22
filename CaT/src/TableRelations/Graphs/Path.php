adb224bd9e27b590ae7482162ede45ba980b522a
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-07-22 15:19:18 +0200

    introducing paths in order to cache graph information while building it

--- CaT/src/TableRelations/Graphs/Path.php
+++ CaT/src/TableRelations/Graphs/Path.php
@@ -9,7 +9,92 @@ namespace CaT\TableRelations\Graphs;
  */
 class Path {
 	protected $sequence = array();
-	public function __construct($start_node_id, array $data) {
+	protected $start_node = null;
+	protected $end_node = null;
 
+	public static function getInstanceByStartId($start_node_id, array $data) {
+		$path = new Path;
+		return $path->addNode($start_node_id, $data);
+	}
+
+	public static function getInstanceBySequence(array $sequence) {
+		$path = new Path;
+		foreach ($sequence as $node_id => $data) {
+			$path->addNode($node_id, $data);
+		}
+		return $path;
+	}
+
+
+	public function addNode($node_id, array $data) {
+		if(isset($this->sequence[$node_id])) {
+			throw new GraphException("$node_id allready in path");
+		}
+		if($this->start_node === null) {
+			$this->start_node = $node_id;
+		}
+		$this->sequence[$node_id] = $data;
+		$this->end_node = $node_id;
+		return $this;
+	}
+
+	protected function addSequence(array $sequence) {
+		$end_node = $this->end_node;
+		foreach ($sequence as $node_id => $data) {
+			assert('is_array($data)');
+			$this->sequence[$node_id] = $data;
+			$end_node = $node_id;
+		}
+		$this->end_node = $end_node;
+	}
+
+	public function getNodeData($node_id) {
+		return $this->sequence[$node_id];
+	}
+
+	public function intersectsPathAt(Path $path) {
+		$this_nodes = array_keys($this->sequence);
+		$other_nodes = array_keys($path->sequence);
+		if(count(array_intersect($this_nodes, $other_nodes)) > 0) {
+			foreach ($this->sequence as $node_id => $stuff) {
+				if(isset($path->sequence[$node_id])) {
+					return $node_id;
+				}
+			}
+		}
+		return null;
+	}
+
+	public function append(Path $path) {
+		if($this->intersectsPathAt($path) === null) {
+			$this->addSequence($path->sequence);
+		}
+		return $this;
+	}
+
+	public function appendUpTo(Path $path, $node_id = null) {
+		if($node_id === null) {
+			$node_id = $this->intersectsPathAt($path);
+		}
+		$seq_to_add = array();
+		foreach ($path->sequence as $other_node_id => $data) {
+			if($other_node_id === $node_id) {
+				break;
+			}
+			$seq_to_add[$other_node_id] = $data;
+		}
+		$this->addSequence($seq_to_add);
+		return $this;
+	}
+
+	public function contains($node_id) {
+		if(isset($this->sequence[$node_id])) {
+			return true;
+		}
+		return false;
+	}
+
+	public function sequence() {
+		return $this->sequence;
 	}
 }
---
+++ CaT/test/TableRelations/Graphs/PathTest.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once "CaT/test/TableRelations/Graphs/GraphTestFactory.php";
+use CaT\TableRelations\Graphs as Graphs;
+class PathTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * @expectedException CaT\TableRelations\Graphs\GraphException
+	 */
+	public function test_path_double_node() {
+		$path = Graphs\Path::getInstanceBySequence(array("1"=>array(),"2"=>array(),"3"=>array()));
+		$path->addNode("2",array());
+	}
+
+	public function test_contains() {
+		$path = Graphs\Path::getInstanceBySequence(array("1"=>array(),"2"=>array(),"3"=>array()));
+		$this->assertTrue($path->contains("2"));
+		$this->assertFalse($path->contains("2a"));
+	}
+
+	public function test_path_crossing() {
+		$path1 = Graphs\Path::getInstanceBySequence(array("1"=>array(),"2"=>array(),"3"=>array()));
+		$path2 = Graphs\Path::getInstanceBySequence(array("1a"=>array(),"2"=>array(),"3a"=>array()));
+		$this->assertEquals($path1->intersectsPathAt($path2),"2");
+	}
+
+	public function test_append() {
+		$path1 = Graphs\Path::getInstanceBySequence(array("1"=>array(),"2"=>array(),"3"=>array()));
+		$path2 = Graphs\Path::getInstanceBySequence(array("1a"=>array(),"2a"=>array(),"3a"=>array()));
+		$seq = $path1->append($path2)->sequence();
+		$this->assertSame($seq,
+			array("1"=>array(),"2"=>array(),"3"=>array(),"1a"=>array(),"2a"=>array(),"3a"=>array()));
+	}
+
+	public function test_append_up_to_id() {
+		$path1 = Graphs\Path::getInstanceBySequence(array("1"=>array(),"2"=>array(),"3"=>array()));
+		$path2 = Graphs\Path::getInstanceBySequence(array("1a"=>array(),"2a"=>array(),"3a"=>array(),"2"=>array(),"4a"=>array()));
+		$seq = $path1->appendUpTo($path2,"3a")->sequence();
+		$this->assertSame($seq,
+			array("1"=>array(),"2"=>array(),"3"=>array(),"1a"=>array(),"2a"=>array()));
+	}
+
+	public function test_append_up_to_crossing() {
+		$path1 = Graphs\Path::getInstanceBySequence(array("1"=>array(),"2"=>array(),"3"=>array()));
+		$path2 = Graphs\Path::getInstanceBySequence(array("1a"=>array(),"2a"=>array(),"3a"=>array(),"2"=>array(),"4a"=>array()));
+		$seq = $path1->appendUpTo($path2)->sequence();
+		$this->assertSame($seq,
+			array("1"=>array(),"2"=>array(),"3"=>array(),"1a"=>array(),"2a"=>array(),"3a"=>array()));
+	}
+}
