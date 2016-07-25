734013c43113973ff8f466307fd2e795cb55b365
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-07-25 16:23:49 +0200

    overwrote Field::name to make equaly named fields in different tables not being percieved as identical by Predicates

--- CaT/src/TableRelations/Tables/TableField.php
+++ CaT/src/TableRelations/Tables/TableField.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace \CaT\TableRelations\Tables;
+namespace CaT\TableRelations\Tables;
 
-use \CaT\Filter\Predicates as Predicates;
+use CaT\Filter as Filters;
 /**
  * A field combining Predicate-Fields and table functinality.
  * Note: it may still be used in Predicates.
  */
-class TableField extends Predicates\Field implements abstractTableField {
+class TableField extends Filters\Predicates\Field implements abstractTableField {
 
-	public function __construct(Predicates\PredicateFactory $f, $name, $table_id = null) {
+	public function __construct(Filters\PredicateFactory $f, $name, $table_id = null) {
 		$this->table_id = $table_id;
 		parent::__construct($f, $name);
 	}
@@ -22,5 +22,14 @@ class TableField extends Predicates\Field implements abstractTableField {
 	 */
 	public function tableId() {
 		return $this->table_id;
+	}
+
+	/**
+	 * To avoid ambiguity we have to include related table-id into fieldname.
+	 *
+	 * @return	string
+	 */
+	public function name() {
+		return $this->table_id.'.'.parent::name();
 	}
 }
