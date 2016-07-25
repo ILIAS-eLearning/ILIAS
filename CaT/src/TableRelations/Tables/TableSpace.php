ceb363b11a1fb24299fe9389eea7d6d0377d8fed
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-07-25 16:24:15 +0200

    table space wip

--- CaT/src/TableRelations/Tables/TableSpace.php
+++ CaT/src/TableRelations/Tables/TableSpace.php
@@ -9,4 +9,28 @@ namespace \CaT\TableRelations\Tables;
  * Also it runs consistency-checks on Tables/fields provided.
  */
 class TableSpace {
+	protected $graph;
+	protected $fields = array();
+	protected $tables = array();
+	public function __construct(Graphs\abstractGraph $graph) {
+		$this->graph = $graph;
+	}
+
+	public function addTable(abstractTable\Table $table) {
+		$table_id = $table->id();
+		if(isset($this->tables[$table_id])) {
+			throw new TableException("$table_id allready in table");
+		}
+		$this->fields[$table_id] = array();
+		foreach ($table->fields() as $field) {
+			$this->fields[$table_id] = $field;
+		}
+		$this->graph->addNode($table);
+		return $this;
+	}
+
+	public function addDependency(abstractTableDependency $dep) {
+		
+		return $this;
+	}
 }
