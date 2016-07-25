eb52cdef34bea2ceb6a292d8a8917c2b0749ebb0
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-07-25 16:16:16 +0200

    tests for tables wip

---
+++ CaT/test/TableRelations/Tables/TableFieldTest.php
@@ -0,0 +1,15 @@
+<?php
+use CaT\TableRelations\Tables as Tables;
+
+class TableFieldTest extends PHPUnit_Framework_TestCase {
+
+	public function setUp() {
+		$this->tf = new Tables\TableFactory();
+	}
+
+	public function test_field() {
+		$field = $this->tf->TableField("name", "a_table_id");
+		$this->assertEquals($field->name(),"a_table_id.name");
+		$this->assertEquals($field->table_id,"a_table_id");
+	}
+}
