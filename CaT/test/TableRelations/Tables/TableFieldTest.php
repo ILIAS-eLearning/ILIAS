35063e09e7da18c9d4a9d2ca82d09e6e49bfc9d4
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-07-26 09:43:10 +0200

    function TableField->Field

--- CaT/test/TableRelations/Tables/TableFieldTest.php
+++ CaT/test/TableRelations/Tables/TableFieldTest.php
@@ -8,7 +8,7 @@ class TableFieldTest extends PHPUnit_Framework_TestCase {
 	}
 
 	public function test_field() {
-		$field = $this->tf->TableField("name", "a_table_id");
+		$field = $this->tf->Field("name", "a_table_id");
 		$this->assertEquals($field->name(),"a_table_id.name");
 		$this->assertEquals($field->table_id,"a_table_id");
 	}
