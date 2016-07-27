e05b2425558bbdb4953a746b53f4a69681f98a26
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-07-27 14:10:30 +0200

    intorducing derived tables built from table spaces (in analaogy to subselects)

---
+++ CaT/src/TableRelations/Tables/DerivedTable.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace CaT\TableRelations\Tables;
+
+
+class DerivedTable implements AbstractTable{
+	protected $space;
+	protected $fields;
+	protected $id;
+	protected $subgraph;
+	protected $constrain = null;
+	public function __construct(TableSpace $space, $id) {
+		if(count($space->requested()) === 0) {
+			throw new TableException("$id:can't contruct by space, no fields requested");
+		}
+		foreach ($space->requested as $field) {
+			$this->fields[$field->name_simple()] = new TableField($field->name_simple(), $id);
+		}
+		$this->id = $id;
+	}
+
+	public function addConstrain(Predicates\Predicate $constrain) {
+		foreach($constrain->fields() as $field) {
+			if(!$this->fieldInTable($field)) {
+				$name = $field->name_simple();
+				throw new TableException("field $name not in table.");
+			}
+		}
+		$this->constrain = $constrain;
+	}
+
+
+	public function fieldInTable(AbstractTableField $field) {
+		if(!isset($this->fields[$field->name_simple()])) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function fields() {
+		return $this->fields;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function title() {
+		return $this->id;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function id() {
+		return $this->id;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function constrain() {
+		return $this->constrain;
+	}
+
+	public function subgraph() {
+		return $this->subgraph;
+	}
+
+	public function setSubgraph($subgraph) {
+		$this->subgraph = $subgraph;
+	}
+}
