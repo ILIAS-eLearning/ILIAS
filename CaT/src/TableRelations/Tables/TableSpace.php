7c44a550f05526a4241d2a67f60e62736265052b
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-07-27 12:36:08 +0200

    tableSpace WIP

--- CaT/src/TableRelations/Tables/TableSpace.php
+++ CaT/src/TableRelations/Tables/TableSpace.php
@@ -11,26 +11,36 @@ namespace \CaT\TableRelations\Tables;
 class TableSpace {
 	protected $graph;
 	protected $fields = array();
-	protected $tables = array();
 	public function __construct(Graphs\AbstractGraph $graph) {
 		$this->graph = $graph;
 	}
 
 	public function addTable(AbstractTable\Table $table) {
 		$table_id = $table->id();
-		if(isset($this->tables[$table_id])) {
+		if($this->graph->getNodeById($from->id())) {
 			throw new TableException("$table_id allready in space");
 		}
 		$this->fields[$table_id] = array();
 		foreach ($table->fields() as $field) {
 			$this->fields[$table_id] = $field;
 		}
-		$this->graph->addNode($table);
+		$this->graph->addNode($table, $subgraph);
 		return $this;
 	}
 
 	public function addDependency(AbstractTableDependency $dep) {
-		
+		$from = $dep->from();
+		$to = $dep->to();
+		if(!$from == $this->graph->getNodeById($from->id()) || !$to == $this->graph->getNodeById($to->id())) {
+			throw new TableException("tables ".$from->id.", ".$to->id." seem not to be in space");
+		}
+		if($dep instanceof TableJoin) {
+			$this->graph->connectNodesSymmetric($dep);
+		} elseif($dep instanceof TableLeftJoin) {
+			$this->graph->connectNodesDirected($dep);
+		} else {
+			throw new TableException("unnkown dependency type");
+		}
 		return $this;
 	}
 }
