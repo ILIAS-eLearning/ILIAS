035427320f8048195ee1716b67f6bbd81af6d656
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-07-29 17:11:16 +0200

    query object wip

--- CaT/src/TableRelations/Tables/AbstractQuery.php
+++ CaT/src/TableRelations/Tables/AbstractQuery.php
@@ -1,9 +1,42 @@
 <?php
 
-interface AbstractQuery {
+namespace CaT\TableRelations\Tables;
+
+use CaT\Filter\Predicates as Predicates;
+
+/**
+ * An object representing quereis on a graph.
+ * Tables inside query may be iterated inside a 
+ * foreach loop.
+ */
+
+interface AbstractQuery extends \Iterator {
+
+	/**
+	 * List of requested fields
+	 *
+	 * @return Predicates\Field[]
+	 */
 	public function requested();
-	public function root();
-	public function dependent();
+
+	/**
+	 * Get the root table.
+	 *
+	 * @return Table.
+	 */
+	public function rootTable();
+
+	/**
+	 * Return having condition
+	 *
+	 * @return Predicates\Predicate
+	 */
 	public function having();
+
+	/**
+	 * Get a field to group by, if any.
+	 *
+	 * @return Predicates\Field|null
+	 */
 	public function groupedBy();
 }
--- CaT/src/TableRelations/Tables/Query.php
+++ CaT/src/TableRelations/Tables/Query.php
@@ -2,6 +2,63 @@
 
 namespace CaT\TableRelations\Tables;
 
+use CaT\Filter\Predicates as Predicates;
+/**
+ * @inheritdoc
+ */
 class Query {
-	
+
+	public function valid() {
+		return $this->path->valid();
+	}
+
+	public function key() {
+		return $this->path->key();
+	}
+
+	public function current() {
+		return $this->path->current();
+	}
+
+	public function next() {
+		$this->current++;
+		return $this->path->next();
+	}
+
+	/**
+	 * We ignore root table in $path.
+	 */
+	public function rewind() {
+		$this->path->rewind();
+		$this->path->next();
+	}
+
+	public function currentJoinCondition() {
+		
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function requested() {
+
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function rootTable() {
+
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function having() {}
+
+
+	/**
+	 * @inheritdoc
+	 */
+	public function groupedBy() {}	
 }
