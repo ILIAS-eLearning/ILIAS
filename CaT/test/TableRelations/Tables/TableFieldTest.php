f87431311bac72cee5bc20e42d99ac84c0bddd30
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-07-29 09:11:18 +0200

    Testing derived fields

--- CaT/test/TableRelations/Tables/TableFieldTest.php
+++ CaT/test/TableRelations/Tables/TableFieldTest.php
@@ -10,6 +10,15 @@ class TableFieldTest extends PHPUnit_Framework_TestCase {
 	public function test_field() {
 		$field = $this->tf->Field("name", "a_table_id");
 		$this->assertEquals($field->name(),"a_table_id.name");
+		$this->assertEquals($field->name_simple(),"name");
 		$this->assertEquals($field->table_id,"a_table_id");
+	}
+
+	public function test_derived_field() {
+		$tf = $this->tf;
+		$field = $tf->DerivedField("name",function($field_a,$field_b) {return $field_a->name().'+'.$field_b->name();}
+			,array($tf->field("name_a","table_a"),$tf->field("name_b","table_b")));
+		$this->assertEquals(call_user_func_array($field->postprocess, $field->derivedFrom()),
+							"table_a.name_a+table_b.name_b");
 	}
 }
