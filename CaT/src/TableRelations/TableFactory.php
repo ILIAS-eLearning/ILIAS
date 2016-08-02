a04a0b46c736d947179be237a31e118bb92c5ed0
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-08-02 15:23:30 +0200

    namespacing

--- CaT/src/TableRelations/TableFactory.php
+++ CaT/src/TableRelations/TableFactory.php
@@ -1,38 +1,47 @@
 <?php
-namespace CaT\TableRelations\Tables;
+namespace CaT\TableRelations;
 
 use CaT\Filter as Filters;
 
 class TableFactory {
-	public function __construct() {
-		$this->predicate_factory = new Filters\PredicateFactory;
+	public function __construct(Filters\PredicateFactory $predicate_factory, GraphFactory $gf) {
+		$this->predicate_factory = $predicate_factory;
+		$this->graph_factory = $gf;
 	}
 
 	public function Field($name, $table_id = null) {
-		return new TableField($this->predicate_factory, $name, $table_id);
+		return new Tables\TableField($this->predicate_factory, $name, $table_id);
 	}
 
 	public function Table($name, $table_id,array $fields = array()) {
-		$table = new Table($name, $table_id);
+		$table = new Tables\Table($name, $table_id);
 		foreach ($fields as $field) {
 			$table->addField($field);
 		}
 		return $table;
 	}
 
-	public function TableJoin(AbstractTable $from, AbstractTable $to, Filters\Predicates\Predicate $predicate) {
-		$table = new TableJoin;
+	public function TableJoin(Tables\AbstractTable $from, Tables\AbstractTable $to, Filters\Predicates\Predicate $predicate) {
+		$table = new Tables\TableJoin;
 		$table->dependingTables($from, $to, $predicate);
 		return $table;
 	}
 
-	public function TableLeftJoin(AbstractTable $from, AbstractTable $to, Filters\Predicates\Predicate $predicate) {
-		$table = new TableLeftJoin;
+	public function TableLeftJoin(Tables\AbstractTable $from, Tables\AbstractTable $to, Filters\Predicates\Predicate $predicate) {
+		$table = new Tables\TableLeftJoin;
 		$table->dependingTables($from, $to, $predicate);
 		return $table;
 	}
 
 	public function DerivedField($name, \Closure $postprocess,$fields) {
-		return new DerivedField($this->predicate_factory,$name,$postprocess,$fields);
+		return new Tables\DerivedField($this->predicate_factory,$name,$postprocess,$fields);
+	}
+
+	public function TableSpace() {
+		return new Tables\TableSpace($this, $this->graph_factory,$this->predicate_factory);
+	}
+
+	public function query() {
+		return new Tables\Query;
 	}
 }
