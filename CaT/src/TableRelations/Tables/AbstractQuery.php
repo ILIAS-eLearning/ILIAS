6d63747948e4252411b5fdb56a65baa63e073598
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-08-02 16:19:35 +0200

    abstract query now as immutable obj

--- CaT/src/TableRelations/Tables/AbstractQuery.php
+++ CaT/src/TableRelations/Tables/AbstractQuery.php
@@ -3,6 +3,7 @@
 namespace CaT\TableRelations\Tables;
 
 use CaT\Filter\Predicates as Predicates;
+use CaT\TableRelations\Graphs as Graphs;
 
 /**
  * An object representing quereis on a graph.
@@ -46,4 +47,60 @@ interface AbstractQuery extends \Iterator {
 	 * @return Predicates\Predicvate|null
 	 */
 	public function filter();
+
+	/**
+	 * Set the fields to be returned in query.
+	 *
+	 * @param	Predicates\Field[] $requested_fields
+	 * @return	AbstractQuery
+	 */
+	public function withRequested(array $requested_fields);
+
+	/**
+	 * Set the root table.
+	 *
+	 * @param	AbstractTable	$root_table
+	 * @return	AbstractQuery
+	 */
+	public function withRootTable(AbstractTable $root_table);
+
+	/**
+	 * Set a sequence of joins. Non obligatory.
+	 *
+	 * @param Graphs\Path $path
+	 * @return	AbstractQuery
+	 */
+	public function withJoins(Graphs\Path $path);
+
+	/**
+	 * Set the joins associated whith join-tables.
+	 *
+	 * @param Predicates\Predicate[][] $requested_fields
+	 * @return	AbstractQuery
+	 */
+	public function withJoinConditions(array $join_conditions);
+
+	/**
+	 * Set filter (i.e. where).
+	 *
+	 * @param Predicates\Predicate $filter
+	 * @return	AbstractQuery
+	 */
+	public function withFilter(Predicates\Predicate $filter);
+
+	/**
+	 * Set having (i.e. where).
+	 *
+	 * @param Predicates\Predicate $having
+	 * @return	AbstractQuery
+	 */
+	public function withHaving(Predicates\Predicate $having);
+
+	/**
+	 * Set having (i.e. where).
+	 *
+	 * @param Predicate\Field $group_by
+	 * @return	AbstractQuery
+	 */
+	public function withGroupByField($group_by);
 }
