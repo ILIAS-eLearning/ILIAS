5bf3ade746ac1a4251f814ec6d2bbb88cf0a96a3
Timon Amstutz <timon.amstutz@bluewin.ch>
2016-08-02 17:07:24 +0200

    Hopefully last intendation fix.

--- src/UI/Component/Deck/Deck.php
+++ src/UI/Component/Deck/Deck.php
@@ -5,9 +5,9 @@
 namespace ILIAS\UI\Component\Deck;
 
 interface Deck extends \ILIAS\UI\Component\Component {
-    /**
-     * Different sizes of the card.
-     */
+	/**
+	 * Different sizes of the card.
+	 */
 	const SIZE_XS = 1; //12 Cards per row
 	const SIZE_S = 2; //6 Cards per row
 	const SIZE_M = 3; //4 Cards per row
@@ -16,26 +16,26 @@ interface Deck extends \ILIAS\UI\Component\Component {
 	const SIZE_FULL = 12; //1 Card per row
 
 	/**
-     * Set the cards to be displayed in the deck
+	 * Set the cards to be displayed in the deck
 	 * @param \ILIAS\UI\Component\Card\Card[] $cards
 	 * @return Deck
 	 */
 	public function withCards($cards);
 
 	/***
-     * Get the cards to be displayed in the deck
+	 * Get the cards to be displayed in the deck
 	 * @return \ILIAS\UI\Component\Card\Card[]
 	 */
 	public function getCards();
 
 	/**
-     * Set the cards size
+	 * Set the cards size
 	 * @return Deck
 	 */
 	public function withCardsSize($size);
 
 	/**
-     * Get the cards size
+	 * Get the cards size
 	 * @return int
 	 */
 	public function getCardsSize();
