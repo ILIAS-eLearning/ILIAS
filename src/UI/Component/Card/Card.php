1da071d22bc62dd0e99eb16c9c545d2bf3deb955
Timon Amstutz <timon.amstutz@bluewin.ch>
2016-08-02 16:27:30 +0200

    More intendation fixes

--- src/UI/Component/Card/Card.php
+++ src/UI/Component/Card/Card.php
@@ -10,42 +10,42 @@ namespace ILIAS\UI\Component\Card;
  */
 interface Card extends \ILIAS\UI\Component\Component {
 
-    /**
-     * Sets the title in the heading section of the card
-     * @param $title
-     * @return Card
-     */
-    public function withTitle($title);
+	/**
+	 * Sets the title in the heading section of the card
+	 * @param $title
+	 * @return Card
+	 */
+	public function withTitle($title);
 
-    /**
-     * Gets the title in the heading section of the card
-     * @return string
-     */
-    public function getTitle();
+	/**
+	 * Gets the title in the heading section of the card
+	 * @return string
+	 */
+	public function getTitle();
 
-    /**
-     * Set multiple sections of the card as array
-     * @param \ILIAS\UI\Component\Component[] $sections
-     * @return Card
-     */
-    public function withSections(array $sections);
+	/**
+	 * Set multiple sections of the card as array
+	 * @param \ILIAS\UI\Component\Component[] $sections
+	 * @return Card
+	 */
+	public function withSections(array $sections);
 
-    /**
-     * Get the multiple sections of the card as array
-     * @return \ILIAS\UI\Component\Component[]
-     */
-    public function getSections();
+	/**
+	 * Get the multiple sections of the card as array
+	 * @return \ILIAS\UI\Component\Component[]
+	 */
+	public function getSections();
 
-    /**
-     * Set the image of the card
-     * @param \ILIAS\UI\Component\Image\Image $image
-     * @return Card
-     */
-    public function withImage(\ILIAS\UI\Component\Image\Image $image);
+	/**
+	 * Set the image of the card
+	 * @param \ILIAS\UI\Component\Image\Image $image
+	 * @return Card
+	 */
+	public function withImage(\ILIAS\UI\Component\Image\Image $image);
 
-    /**
-     * Get the image of the card
-     * @return mixed
-     */
-    public function getImage();
+	/**
+	 * Get the image of the card
+	 * @return mixed
+	 */
+	public function getImage();
 }
