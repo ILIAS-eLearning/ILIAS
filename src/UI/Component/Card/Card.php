8a39ab111d9ce1ff602cac575dcfea5c361e3732
Timon Amstutz <timon.amstutz@bluewin.ch>
2016-08-02 10:25:38 +0200

    Fixed intendation

--- src/UI/Component/Card/Card.php
+++ src/UI/Component/Card/Card.php
@@ -10,55 +10,55 @@ namespace ILIAS\UI\Component\Card;
  */
 interface Card extends \ILIAS\UI\Component\Component {
 
-	/**
+    /**
      * Sets the title in the heading section of the card
-	 * @param $title
-	 * @return Card
-	 */
-	public function withTitle($title);
+     * @param $title
+     * @return Card
+     */
+    public function withTitle($title);
 
-	/***
+    /**
      * Gets the title in the heading section of the card
-	 * @return string
-	 */
-	public function getTitle();
+     * @return string
+     */
+    public function getTitle();
 
-	/**
+    /**
      * Set additional content displayed directly underneath the title
-	 * @param \ILIAS\UI\Component\Component $section
-	 * @return Card
-	 */
-	public function withHeaderSection(\ILIAS\UI\Component\Component $section);
+     * @param \ILIAS\UI\Component\Component $section
+     * @return Card
+     */
+    public function withHeaderSection(\ILIAS\UI\Component\Component $section);
 
-	/**
+    /**
      * Get content displayed directly underneath the title
-	 * @return \ILIAS\UI\Component\Component
-	 */
-	public function getHeaderSection();
+     * @return \ILIAS\UI\Component\Component
+     */
+    public function getHeaderSection();
 
-	/**
+    /**
      * Set multiple sections of the card as array
-	 * @param \ILIAS\UI\Component\Component[] $sections
-	 * @return Card
-	 */
-	public function withContentSections($sections);
+     * @param \ILIAS\UI\Component\Component[] $sections
+     * @return Card
+     */
+    public function withContentSections($sections);
 
-	/**
+    /**
      * Get the multiple sections of the card as array
-	 * @return \ILIAS\UI\Component\Component[]
-	 */
-	public function getContentSections();
+     * @return \ILIAS\UI\Component\Component[]
+     */
+    public function getContentSections();
 
-	/**
+    /**
      * Set the image of the card
-	 * @param \ILIAS\UI\Component\Image\Image $image
-	 * @return Card
-	 */
-	public function withImage(\ILIAS\UI\Component\Image\Image $image);
+     * @param \ILIAS\UI\Component\Image\Image $image
+     * @return Card
+     */
+    public function withImage(\ILIAS\UI\Component\Image\Image $image);
 
-	/**
+    /**
      * Get the image of the card
-	 * @return mixed
-	 */
-	public function getImage();
+     * @return mixed
+     */
+    public function getImage();
 }
