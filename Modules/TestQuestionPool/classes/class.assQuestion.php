77782c33c1f71cb0060099a6478238e586e5b015
mjansen <mjansen@databay.de>
2016-08-04 08:52:59 +0200

    Fixed Mantis issue #17787

--- Modules/TestQuestionPool/classes/class.assQuestion.php
+++ Modules/TestQuestionPool/classes/class.assQuestion.php
@@ -3616,6 +3616,15 @@ abstract class assQuestion
 		{
 			$collected .= $solution_array["value"];
 		}
+
+		require_once 'Modules/TestQuestionPool/classes/class.ilAssQuestionHintList.php';
+		$questionHintList = ilAssQuestionHintList::getListByQuestionId($this->getId());
+		foreach($questionHintList as $questionHint)
+		{
+			/* @var $questionHint ilAssQuestionHint */
+			$collected .= $questionHint->getText();
+		}
+
 		return $collected;
 	}
 
