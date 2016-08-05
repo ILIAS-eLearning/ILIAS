a0529a2c8df50e0b042a09894d6949da86bf1100
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-08-05 09:45:39 +0200

    introduce property own file cache and remove unused callback function

--- Services/UICore/lib/html-it/IT.php
+++ Services/UICore/lib/html-it/IT.php
@@ -357,6 +357,13 @@ class HTML_Template_IT
     );
 
     /**
+     * Property own file content cache to speed up template engine
+     *
+     * @var array   string => string
+     */
+    protected $this->file_content_cache;
+
+    /**
      * Builds some complex regular expressions and optinally sets the
      * file root directory.
      *
@@ -383,6 +390,8 @@ class HTML_Template_IT
                              ')\s+-->(.*)<!--\s+END\s+\1\s+-->@sm';
 
         $this->setRoot($root);
+
+        $this->file_content_cache = array();
     } // end constructor
 
 
@@ -910,6 +919,10 @@ class HTML_Template_IT
 
         $filename = $this->fileRoot . $filename;
 
+        if(array_key_exists($filename, $this->file_content_cache)) {
+            return $this->file_content_cache[$filename];
+        }
+
         require_once('./Services/GlobalCache/classes/class.ilGlobalCache.php');
         $this->real_filename = $filename;
         $ilGlobalCache = ilGlobalCache::getInstance(ilGlobalCache::COMP_TEMPLATE);
@@ -927,17 +940,12 @@ class HTML_Template_IT
 
             $content = fread($fh, $fsize);
             $ilGlobalCache->set($filename, $content, 60);
+            $this->file_content_cache[$filename] = $content;
             fclose($fh);
         }
 
 
-        return preg_replace_callback(
-            "#<!-- INCLUDE (.*) -->#im",
-            function ($hit) {
-                return $this->getFile($hit[1]);
-            },
-            $content
-        );
+        return $content;
     } // end func getFile
 
     /**
