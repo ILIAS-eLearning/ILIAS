8263d4b0d7f09d7de33d3ce70e3d602dca7dcf06
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-08-05 15:31:36 +0200

    obj WIP

--- Modules/ManualAssessment/classes/class.ilObjManualAssessment.php
+++ Modules/ManualAssessment/classes/class.ilObjManualAssessment.php
@@ -9,11 +9,32 @@
 
 
 require_once 'Services/Object/classes/class.ilObject.php';
-
+require_once 'Modules/ManualAssessment/classes/Settings/class.ilManualAssessmentSettings.php';
+require_once 'Modules/ManualAssessment/classes/Settings/class.ilManualAssessmentSettingsStorageDB.php';
 class ilObjManualAssessment extends ilObject {
 	public function __construct($a_id = 0, $a_call_by_reference = true) {
 		global $DIC;
 		$this->type = "mass";
 		parent::__construct($a_id, $a_call_by_reference);
+		$this->settings_storage = new ilManualAssessmentSettingsStorageDB($DIC["ilDB"]);
+	}
+
+	public function create() {
+		parent::create();
+		$this->settings = new ilManualAssessmentSettings($this);
+		$this->settings_storage->createSettings($this->settings);
+	}
+
+	public function loadSettings() {
+		return $this->settings_storage->loadSettings($this);
+	}
+
+	public function updateSettings(ilManualAssessmentSettings $settings) {
+		$this->settings_storage->updateSettings($settings);
+	}
+
+	public function delete() {
+		$this->settings_storage->deleteSettings($this);
+		parent::delete();
 	}
 }
