9d81d2e6cd57418e50981b8d3d9afc94186b5f3f
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2016-08-09 15:20:52 +0200

    jquery: using migrate only in DEVMODE

--- Services/jQuery/classes/class.iljQueryUtil.php
+++ Services/jQuery/classes/class.iljQueryUtil.php
@@ -22,8 +22,13 @@ class iljQueryUtil
 	static function initjQuery($a_tpl = null)
 	{
 		global $tpl;
-			
-		self::handleDevMode();
+	
+		self::$ver = DEVMODE 
+			? "3_1_0"
+			: "2_2_4";
+		self::$min = DEVMODE
+			? ""
+			: "-min";
 		
 		if ($a_tpl == null)
 		{
@@ -32,13 +37,16 @@ class iljQueryUtil
 
 		$a_tpl->addJavaScript(self::getLocaljQueryPath(), true, 1);
 		
-		// adding jquery-migrate for >= 1.9.x
-		$major = explode("_", self::$ver);
-		$major = $major[0]*100+$major[1];
-		if($major >= 109)
+		if(DEVMODE)
 		{
-			$path = str_replace("jquery", "jquery-migrate", self::getLocaljQueryPath());			
-			$a_tpl->addJavaScript($path, true, 1);
+			// adding jquery-migrate for >= 1.9.x
+			$major = explode("_", self::$ver);
+			$major = $major[0]*100+$major[1];
+			if($major >= 109)
+			{
+				$path = str_replace("jquery", "jquery-migrate", self::getLocaljQueryPath());			
+				$a_tpl->addJavaScript($path, true, 1);
+			}
 		}
 	}
 	
@@ -48,27 +56,15 @@ class iljQueryUtil
 	static function initjQueryUI()
 	{
 		global $tpl;
-		
-		self::handleDevMode();
-		
+			
 		$tpl->addJavaScript(self::getLocaljQueryUIPath(), true, 1);
-	}
-	
-	static protected function handleDevMode()
-	{
-		self::$ver = DEVMODE 
-			? "3_1_0"
-			: "2_2_4";
-		self::$min = DEVMODE
-			? ""
-			: "-min";
 	}
 	
 	/**
 	 * Get local path of jQuery file
 	 */
 	static function getLocaljQueryPath()
-	{
+	{		
 		return "./Services/jQuery/js/".self::$ver."/jquery".self::$min.".js";
  	}
 
