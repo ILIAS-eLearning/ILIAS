4bd8ace49e3c01661b0e5fdd4a5ba7864be32759
root@www.ilias.de <info@ilias.de>
2016-08-10 21:10:48 +0200

    Release 5.0.14

--- include/inc.ilias_version.php
+++ include/inc.ilias_version.php
@@ -9,6 +9,6 @@
 *
 * @package ilias-core
 */
-define("ILIAS_VERSION", "5.0.13 2016-06-29");
-define("ILIAS_VERSION_NUMERIC", "5.0.13");			// must be always x.y.z: x, y and z are numbers
+define("ILIAS_VERSION", "5.0.14 2016-08-10");
+define("ILIAS_VERSION_NUMERIC", "5.0.14");			// must be always x.y.z: x, y and z are numbers
 ?>
