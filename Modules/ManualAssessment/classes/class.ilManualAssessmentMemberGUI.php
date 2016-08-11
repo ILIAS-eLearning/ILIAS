f46f4bad4f5cc7c4812560ceaae8d1ece70482f1
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-08-11 09:54:15 +0200

    manual assessment mebmer gui

---
+++ Modules/ManualAssessment/classes/class.ilManualAssessmentMemberGUI.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * For the purpose of streamlining the grading and learning-process status definition
+ * outside of tests, SCORM courses e.t.c. the ManualAssessment is used.
+ * It caries a LPStatus, which is set manually.
+ *
+ * @author Denis Klöpfer <denis.kloepfer@concepts-and-training.de>
+ *
+ * @ilCtrl_Calls ilManualAssessmentMembersGUI: ilRepositorySearchGUI
+ */
+class ilManualAssessmentMemberGUI {
+	
+}
