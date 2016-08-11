cbdcf731355ce5eb73c7e052731db5d1379794e4
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-08-11 17:03:43 +0200

    established some controll over graded-finalized relations consistency

--- Modules/ManualAssessment/classes/Members/class.ilManualAssessmentMember.php
+++ Modules/ManualAssessment/classes/Members/class.ilManualAssessmentMember.php
@@ -1,6 +1,9 @@
 <?php
 require_once 'Modules/ManualAssessment/classes/class.ilObjManualAssessment.php';
 require_once 'Services/User/classes/class.ilObjUser.php';
+require_once 'Modules/ManualAssessment/exceptions/class.ilManualAssessmentException.php';
+require_once 'Modules/ManualAssessment/classes/Members/class.ilManualAssessmentMembers.php';
+
 class ilManualAssessmentMember {
 	protected $mass;
 	protected $usr;
@@ -9,9 +12,18 @@ class ilManualAssessmentMember {
 	protected $internal_note;
 	protected $examiner_id;
 	protected $notify;
+	protected $finalized;
 	protected $grade;
 
-	public function __construct(ilObjManualAssessment $mass, ilObjUser $usr) {
+	public function __construct(ilObjManualAssessment $mass, ilObjUser $usr, array $data) {
+
+		$this->record = $data[ilManualAssessmentMembers::FIELD_RECORD];
+		$this->internal_note = $data[ilManualAssessmentMembers::FIELD_INTERNAL_NOTE];
+		$this->examiner_id = $data[ilManualAssessmentMembers::FIELD_EXAMINER_ID];
+		$this->notify = $data[ilManualAssessmentMembers::FIELD_NOTIFY];
+		$this->grade = $data[ilManualAssessmentMembers::FIELD_GRADE];
+		$this->finalized = $data[ilManualAssessmentMembers::FIELD_FINALIZED];
+
 		$this->mass = $mass;
 		$this->usr = $usr;
 	}
@@ -44,35 +56,58 @@ class ilManualAssessmentMember {
 		return $this->mass->getId();
 	}
 
+	public function finalized() {
+		return $this->finalized === 1 ? true : false;
+	}
+
+	public function mayBeFinalized() {
+		return !($this->grade === null);
+	}
+
 	public function withRecord($record) {
-		$clone = clone $this;
-		$clone->record = $record;
-		return $clone;
+		if(!$this->finalized()) {
+			$clone = clone $this;
+			$clone->record = $record;
+			return $clone;
+		}
+		throw new ilManualAssessmentException('user allready finalized');
 	}
 
 	public function withInternalNote($internal_note) {
-		$clone = clone $this;
-		$clone->internal_note = $internal_note;
-		return $clone;
+		if(!$this->finalized()) {
+			$clone = clone $this;
+			$clone->internal_note = $internal_note;
+			return $clone;
+		}
+		throw new ilManualAssessmentException('user allready finalized');
 	}
 
 	public function withExaminerId($examiner_id) {
-		assert('ilObjUser::_exists($examiner_id)');
-		$clone = clone $this;
-		$clone->examiner_id = $examiner_id;
-		return $clone;
+		if(!$this->finalized()) {
+			assert('ilObjUser::_exists($examiner_id)');
+			$clone = clone $this;
+			$clone->examiner_id = $examiner_id;
+			return $clone;
+		}
+		throw new ilManualAssessmentException('user allready finalized');
 	}
 
 	public function withNotify($notify) {
-		$clone = clone $this;
-		$clone->notify = (bool)$notify;
-		return $clone;
+		if(!$this->finalized()) {
+			$clone = clone $this;
+			$clone->notify = (bool)$notify;
+			return $clone;
+		}
+		throw new ilManualAssessmentException('user allready finalized');
 	}
 
 	public function withGrade($grade) {
-		$clone = clone $this;
-		$clone->grade = $grade;
-		return $clone;
+		if(!$this->finalized()) {
+			$clone = clone $this;
+			$clone->grade = $grade;
+			return $clone;
+		}
+		throw new ilManualAssessmentException('user allready finalized');
 	}
 
 	public function lastname() {
@@ -87,4 +122,16 @@ class ilManualAssessmentMember {
 		return $this->usr->getLogin();
 	}
 
+	public function name() {
+		return $this->usr->getLastname().', '.$this->usr->getFirstname();
+	}
+
+	public function withFinilized() {
+		if(!$this->grade) {
+			$clone = clone $this;
+			$clone->finalized = 1;
+			return $clone;
+		}
+		throw new ilManualAssessmentException('user not graded');
+	}
 }
