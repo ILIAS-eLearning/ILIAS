26e106ac3102f7a232ad172dbaef6f072df08fd9
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-08-11 09:32:10 +0200

    define members-fields in interface

--- Modules/ManualAssessment/interfaces/Members/interface.ilManualAssessmentMembersStorage.php
+++ Modules/ManualAssessment/interfaces/Members/interface.ilManualAssessmentMembersStorage.php
@@ -1,6 +1,18 @@
 <?php
-require_once "Modules/ManualAssessment/exceptions/class.ilManualAssessmentException.php";
+require_once 'Modules/ManualAssessment/classes/class.ilObjManualAssessment.php';
 interface ilManualAssessmentMembersStorage {
+
+	const FIELD_FIRSTNAME = 'firstname';
+	const FIELD_LASTNAME = 'lastname';
+	const FIELD_LOGIN = 'login';
+	const FIELD_USR_ID = 'usr_id';
+	const FIELD_GRADE = 'grade';
+	const FIELD_EXAMINER_ID = 'examiner_id';
+	const FIELD_EXAMINER_FIRSTNAME = 'examiner_firstname';
+	const FIELD_EXAMINER_LASTNAME = 'examiner_lastname';
+	const FIELD_RECORD = 'record';
+	const FIELD_INTERNAL_NOTE = 'internal_note';
+	const FIELD_NOTIFY = 'notify';
 
 	/**
 	 * Get ilManualAssessmentMembers-object containing meberinfo
@@ -26,12 +38,20 @@ interface ilManualAssessmentMembersStorage {
 	 *
 	 * @param	ilManualAssessmentMember	$member
 	 */
-	public function saveMember(ilManualAssessmentMember $member);
+	public function updateMember(ilManualAssessmentMember $member);
 
 	/**
 	 * Update storage entry associated with member-object.
 	 *
 	 * @param	ilManualAssessmentMember	$member
 	 */
-	public function updateMember(ilManualAssessmentMember $member);
+	public function updateMembers(ilManualAssessmentMembers $members);
+
+	/**
+	 * Delete entries associated with members-object.
+	 *
+	 * @param	ilManualAssessmentMember	$member
+	 */
+	public function deleteMembers(ilObjManualAssessment $obj);
+
 }
