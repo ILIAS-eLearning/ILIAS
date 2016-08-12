1222916859f9eccb265019c7c9b58402357a9ea3
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-08-12 16:51:48 +0200

    made lptablebasegui familiar with mass

--- Services/Tracking/classes/class.ilLPTableBaseGUI.php
+++ Services/Tracking/classes/class.ilLPTableBaseGUI.php
@@ -425,6 +425,7 @@ class ilLPTableBaseGUI extends ilTable2GUI
 		$options['svy'] = $lng->txt('objs_svy');		
 		$options['tst'] = $lng->txt('objs_tst');		
 		$options['prg'] = $lng->txt('objs_prg');
+		$options['mass'] = $lng->txt('objs_prg');
 		
 		if($a_allow_undefined_lp)
 		{			
