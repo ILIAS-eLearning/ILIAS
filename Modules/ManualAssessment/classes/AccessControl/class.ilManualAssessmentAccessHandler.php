f5583e919026c388d2f10bf3626170f4288236a8
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-08-16 13:34:15 +0200

    implemented access control wrapper for mass

--- Modules/ManualAssessment/classes/AccessControl/class.ilManualAssessmentAccessHandler.php
+++ Modules/ManualAssessment/classes/AccessControl/class.ilManualAssessmentAccessHandler.php
@@ -1,1 +1,46 @@
 <?php
+require_once 'Modules/ManualAssessment/interfaces/AccessControl/interface.ManualAssessmentAccessHandler.php';
+require_once 'Services/AccessControl/classes/class.ilObjRole.php';
+class ilManualAssessmentAccessHandler implements ManualAssessmentAccessHandler {
+
+	protected $handler;
+	protected $admin;
+	protected $review;
+
+	const DEAFULT_ROLE = 'il_mass_member';
+
+	public function __construct(ilAccessHandler $handler, ilRbacAdmin $admin, ilRbacReview $review) {
+		$this->handler = $handler;
+		$this->admin = $admin;
+		$this->review = $review;
+	}
+
+	public function checkAccessOfUserToObj(ilObjUser $usr, ilObjManualAssessment $mass, $operation) {
+		return $this->handler->checkAccessOfUser($usr->getId(), $operation, '',  $mass->getRefId(), 'mass');
+	}
+
+	public function initDefaultRolesForObject(ilObjManualAssessment $mass) {
+		$role = ilObjRole::createDefaultRole(
+				$this->getRoleTitleByObj($mass),
+				"Admin of crs obj_no.".$mass->getId(),
+				self::DEAFULT_ROLE,
+				$mass->getRefId()
+		);
+	}
+
+	public function assignUserToMemberRole(ilObjUser $usr, ilObjManualAssessment $mass) {
+		return $this->admin->assignUser($this->getMemberRoleIdForObj($mass),$usr->getId());
+	}
+
+	public function deassignUserFromMemberRole(ilObjUser $usr, ilObjManualAssessment $mass) {
+		return $this->admin->deassignUser($this->getMemberRoleIdForObj($mass),$usr->getId());
+	}
+
+	protected function getRoleTitleByObj(ilObjManualAssessment $mass) {
+		return self::DEAFULT_ROLE.'_'.$mass->getRefId();
+	}
+
+	protected function getMemberRoleIdForObj(ilObjManualAssessment $mass) {
+		return current($this->review->getLocalRoles($mass->getRefId()));
+	}
+}
