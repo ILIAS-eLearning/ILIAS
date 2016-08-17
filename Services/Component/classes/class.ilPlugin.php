21e677db73996eb26698cb4e688d5f75d132f9d4
Fabian Schmid <fs@studer-raimann.ch>
2016-08-17 14:08:40 +0200

    removed out-commented and unused things

--- Services/Component/classes/class.ilPlugin.php
+++ Services/Component/classes/class.ilPlugin.php
@@ -502,17 +502,8 @@ abstract class ilPlugin
 		// this enables default language fallback
 		$prefix = $a_mod_prefix."_".$a_pl_id;
 		return $lng->txt($prefix."_".$a_lang_var, $prefix);
-
-		/*
-		return ilLanguage::_lookupEntry($lng->lang_key, $a_mod_prefix."_".$a_pl_id,
-			$a_mod_prefix."_".$a_pl_id."_".$a_lang_var);		 
-		*/
 	}
 
-
-	static function lookupTxtById($plugin_id, $lang_var) {
-
-	}
 
 	/**
 	 * Get template from plugin
@@ -628,42 +619,6 @@ abstract class ilPlugin
 				. "'."));
 
 		}
-		//
-		//		global $ilDB;
-		//
-		//		// read/set basic data
-		//		$q = "SELECT * FROM il_plugin".
-		//			" WHERE component_type = ".$ilDB->quote($a_ctype, "text").
-		//			" AND component_name = ".$ilDB->quote($a_cname, "text").
-		//			" AND slot_id = ".$ilDB->quote($a_slot_id, "text").
-		//			" AND name = ".$ilDB->quote($a_pname, "text");
-		//		$set = $ilDB->query($q);
-		//		if ($rec = $ilDB->fetchAssoc($set))
-		//		{
-		//			return $rec;
-		//		}
-		//		else		// no record? create one
-		//		{
-		//			// silently create these records is not a good idea, since
-		//			// the function can be called with "wrong parameters"
-		//			// raise exceptions instead
-		//			include_once("./Services/Component/exceptions/class.ilPluginException.php");
-		//			throw (new ilPluginException("No plugin record found for '".$a_ctype."', '".$a_cname."', '".$a_slot_id."', '".$a_pname."'."));
-		//
-		//			$q = "INSERT INTO il_plugin (component_type, component_name, slot_id, name)".
-		//				" VALUES (".$ilDB->quote($a_ctype, "text").",".
-		//				$ilDB->quote($a_cname, "text").",".
-		//				$ilDB->quote($a_slot_id, "text").",".
-		//				$ilDB->quote($a_pname, "text").")";
-		//			$ilDB->manipulate($q);
-		//			$q = "SELECT * FROM il_plugin".
-		//				" WHERE component_type = ".$ilDB->quote($a_ctype, "text").
-		//				" AND component_name = ".$ilDB->quote($a_cname, "text").
-		//				" AND slot_id = ".$ilDB->quote($a_slot_id, "text").
-		//				" AND name = ".$ilDB->quote($a_pname, "text");
-		//			$set = $ilDB->query($q);
-		//			return $ilDB->fetchAssoc($set);
-		//		}
 	}
 
 	/**
@@ -974,8 +929,6 @@ abstract class ilPlugin
 	 */
 	static function getPluginObject($a_ctype, $a_cname, $a_slot_id, $a_pname)
 	{
-		global $ilDB;
-
 		include_once("./Services/Component/classes/class.ilPluginSlot.php");
 		$slot_name = ilPluginSlot::lookupSlotName($a_ctype, $a_cname, $a_slot_id);
 
@@ -984,15 +937,6 @@ abstract class ilPlugin
 		if (! $rec) {
 			return NULL;
 		}
-
-		// this check is done due to security reasons
-		//		$set = $ilDB->queryF("SELECT * FROM il_component WHERE type = %s ".
-		//			" AND name = %s", array("text", "text"),
-		//			array($a_ctype, $a_cname));
-		//		if (!$ilDB->fetchAssoc($set))
-		//		{
-		//			return null;
-		//		}
 
 		$file = "./Customizing/global/plugins/".$a_ctype."/".
 			$a_cname."/".$slot_name."/".
@@ -1040,7 +984,7 @@ abstract class ilPlugin
 		$plugins = array();
 
 		$cached_component = ilCachedComponentData::getInstance();
-		//		while($rec = $ilDB->fetchAssoc($set))
+
 		$lookupActivePluginsBySlotId = $cached_component->lookupActivePluginsBySlotId($a_slot_id);
 		foreach($lookupActivePluginsBySlotId as $rec)
 		{
@@ -1117,4 +1061,3 @@ abstract class ilPlugin
 		}
 	}
 }
-?>
