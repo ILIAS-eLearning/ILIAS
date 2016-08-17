c783e414a6c5ce879700b7c95d11b158f1be12fd
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-08-17 17:21:39 +0200

    variable columns and permission checks

--- Modules/ManualAssessment/classes/class.ilManualAssessmentMembersTableGUI.php
+++ Modules/ManualAssessment/classes/class.ilManualAssessmentMembersTableGUI.php
@@ -18,32 +18,45 @@ class ilManualAssessmentMembersTableGUI extends ilTable2GUI {
 		$this->setRowTemplate("tpl.members_table_row.html", "Modules/ManualAssessment");
 		$this->setFormAction($this->ctrl->getFormAction($a_parent_obj, "view"));
 		$this->parent_obj = $a_parent_obj;
-		$columns = array( "name" 				=> array("name")
-						, "login" 				=> array("login")
-						, "lp_status" 			=> array("lp_status")
-						, "graded_by"			=> array("graded_by")
-						, "actions"				=> array(null)
-						);
-		$may_edit = $this->userMayEditGrade();
-		$may_view = $this->userMayViewGrade();
-		foreach ($columns as $lng_var => $params) {
+		$this->may_edit = $this->userMayEditGrades();
+		$this->may_view = $this->userMayViewGrades();
+		$this->columns = $this->visibleColumns();
+		foreach ($this->columns as $lng_var => $params) {
 			$this->addColumn($this->lng->txt($lng_var), $params[0]);
 		}
 		$this->setData(iterator_to_array($a_parent_obj->object->loadMembers()));
 	}
 
+	protected function visibleColumns() {
+		$columns = array( 'name' 				=> array('name')
+						, 'login' 				=> array('login'));
+		if($this->may_view) {
+			$columns['lp_status'] = array('lp_status');
+			$columns['graded_by'] = array('graded_by');
+		}
+		$columns['actions'] = array(null);
+		return $columns;
+	}
+
 	protected function fillRow($a_set) {
+		$this->tpl->setCurrentBlock('user_data');
 		$this->tpl->setVariable("FIRSTNAME", $a_set["firstname"]);
 		$this->tpl->setVariable("LASTNAME", $a_set["lastname"]);
 		$this->tpl->setVariable("LOGIN", $a_set["login"]);
-		$this->tpl->setVariable("LP_STATUS",
-			$this->getImagetPathForStatus($a_set[ilManualAssessmentMembers::FIELD_LEARNING_PROGRESS]));
-
-		if($a_set[ilManualAssessmentMembers::FIELD_EXAMINER_ID]) {
-			$this->tpl->setVariable("GRADED_BY", $a_set[ilManualAssessmentMembers::FIELD_EXAMINER_LASTNAME].", "
-												.$a_set[ilManualAssessmentMembers::FIELD_EXAMINER_FIRSTNAME]);
+		$this->tpl->parseCurrentBlock();
+		if($this->may_view) {
+			$this->tpl->setCurrentBlock('lp_info');
+			$this->tpl->setVariable("LP_STATUS",
+				$this->getImagetPathForStatus($a_set[ilManualAssessmentMembers::FIELD_LEARNING_PROGRESS]));
+			$this->tpl->setVariable("GRADED_BY", $a_set[ilManualAssessmentMembers::FIELD_EXAMINER_ID] 
+				?	$a_set[ilManualAssessmentMembers::FIELD_EXAMINER_LASTNAME].", "
+						.$a_set[ilManualAssessmentMembers::FIELD_EXAMINER_FIRSTNAME]
+				: 	'');
+			$this->tpl->parseCurrentBlock();
 		}
+		$this->tpl->setCurrentBlock('actions');
 		$this->tpl->setVariable("ACTIONS",$this->buildActionDropDown($a_set));
+		$this->tpl->parseCurrentBlock();
 	}
 
 	protected function getImagetPathForStatus($a_status) {
@@ -66,25 +79,32 @@ class ilManualAssessmentMembersTableGUI extends ilTable2GUI {
 
 	protected function buildActionDropDown($a_set) {
 		$l = new ilAdvancedSelectionListGUI();
-
 		if(!$a_set['finalized']) {	
 			$this->ctrl->setParameter($this->parent_obj, 'usr_id', $a_set['usr_id']);
 			$target = $this->ctrl->getLinkTarget($this->parent_obj,'removeUser');
 			$this->ctrl->setParameter($this->parent_obj, 'usr_id', null);
-			$l->addItem($this->lng->txt("mass_usr_remove"), 'removeUser', $target);
+			$l->addItem($this->lng->txt('mass_usr_remove'), 'removeUser', $target);
 		}
+		$this->ctrl->setParameterByClass('ilManualAssessmentMemberGUI', 'usr_id', $a_set['usr_id']);
 		if(!$a_set['finalized'] && $this->may_edit) {
-			$this->ctrl->setParameterByClass('ilManualAssessmentMemberGUI', 'usr_id', $a_set['usr_id']);
 			$target = $this->ctrl->getLinkTargetByClass('ilManualAssessmentMemberGUI','edit');
-			$this->ctrl->setParameterByClass('ilManualAssessmentMemberGUI', 'usr_id', null);
-			$l->addItem($this->lng->txt($lng_var_edit), 'edit', $target);
+			$l->addItem($this->lng->txt('mass_usr_edit'), 'edit', $target);
+		} 
+		if(($a_set['finalized'] && $this->may_edit) || $this->may_view) {
+			$target = $this->ctrl->getLinkTargetByClass('ilManualAssessmentMemberGUI','view');
+			$l->addItem($this->lng->txt('mass_usr_view'), 'view', $target);
 		}
+		$this->ctrl->setParameterByClass('ilManualAssessmentMemberGUI', 'usr_id', null);
 		return $l->getHTML();
 	}
 
 	protected function userMayEditGrades() {
 		return $this->parent_obj->object->accessHandler()
-			->checkAccessToObj($this->parent_obj->object,'edit_grades');
+			->checkAccessToObj($this->parent_obj->object,'edit_learning_progress');
 	}
 
+	protected function userMayViewGrades() {
+		return $this->parent_obj->object->accessHandler()
+			->checkAccessToObj($this->parent_obj->object,'read_learning_progress');
+	}
 }
