59373a2040ae1641d087b152facd2a742fe47031
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-08-18 17:32:54 +0200

    conversion to string for strict compares

--- Services/Tracking/classes/status/class.ilLPStatusManualAssessment.php
+++ Services/Tracking/classes/status/class.ilLPStatusManualAssessment.php
@@ -6,8 +6,9 @@ require_once 'Modules/ManualAssessment/classes/LearningProgress/class.ilManualAs
 require_once 'Modules/ManualAssessment/classes/Members/class.ilManualAssessmentMembers.php';
 
 class ilLPStatusManualAssessment extends ilLPStatus {
+
 	static function _getCountInProgress($a_obj_id) {
-		return count($this->_getInProgress($a_obj_id));
+		return count(self::_getInProgress($a_obj_id));
 	}
 
 	static function _getInProgress($a_obj_id) {
@@ -19,27 +20,27 @@ class ilLPStatusManualAssessment extends ilLPStatus {
 		return count(self::_getCompleted($a_obj_id));
 	}
 
-	static function _getCompleted($a_obj_id) {		
+	static function _getCompleted($a_obj_id) {
 		return ilManualAssessmentLPInterface::getMembersHavingStatusIn($a_obj_id, 
 			ilManualAssessmentMembers::LP_COMPLETED);
 	}
 
-	/*static function _getCountFailed($a_obj_id) {
+	static function _getCountFailed() {
 		return count(self::_getFailed($a_obj_id));
-	}*/
+	}
 
 	static function _getFailed($a_obj_id) {
 		return ilManualAssessmentLPInterface::getMembersHavingStatusIn($a_obj_id, 
 			ilManualAssessmentMembers::LP_FAILED);
 	}
 
-	public function determineStatus($a_obj_id, $a_user_id, $a_obj = null) {
-		switch (ilManualAssessmentLPInterface::determineStatusOfMember($a_obj_id,$a_user_id)) {
-			case ilManualAssessmentMembers::LP_IN_PROGRESS:
+	function determineStatus($a_obj_id, $a_user_id, $a_obj = null) {
+		switch ((string)ilManualAssessmentLPInterface::determineStatusOfMember($a_obj_id,$a_user_id)) {
+			case (string)ilManualAssessmentMembers::LP_IN_PROGRESS:
 				return self::LP_STATUS_IN_PROGRESS_NUM;
-			case ilManualAssessmentMembers::LP_FAILED: 
+			case (string)ilManualAssessmentMembers::LP_FAILED: 
 				return self::LP_STATUS_FAILED_NUM;
-			case ilManualAssessmentMembers::LP_COMPLETED:
+			case (string)ilManualAssessmentMembers::LP_COMPLETED:			
 				return self::LP_STATUS_COMPLETED_NUM;
 			default:
 				return self::LP_STATUS_NOT_ATTEMPTED_NUM;
