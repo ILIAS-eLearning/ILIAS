d5001897962e0a541a8d50482970fb9db06aa1d5
mjansen <mjansen@databay.de>
2016-08-18 19:28:01 +0200

    On-Screen Chat: Translations

--- Services/OnScreenChat/js/onscreenchat.js
+++ Services/OnScreenChat/js/onscreenchat.js
@@ -231,9 +231,9 @@
 
 			template = template.replace('[[participants]]', participantsName.join(', '));
 			template = template.replace(/\[\[conversationId\]\]/g, conversation.id);
-			template = template.replace('/#:#close#:#/g', il.Language.txt('close'));
-			template = template.replace('/#:#chat_osc_add_user#:#/g', il.Language.txt('chat_osc_add_user'));
-			template = template.replace('/#:#chat_osc_send#:#/g', il.Language.txt('chat_osc_send'));
+			template = template.replace('#:#close#:#', il.Language.txt('close'));
+			template = template.replace('#:#chat_osc_add_user#:#', il.Language.txt('chat_osc_add_user'));
+			template = template.replace('#:#chat_osc_send#:#', il.Language.txt('chat_osc_send'));
 
 			return template;
 		},
@@ -355,7 +355,7 @@
 				show: true,
 				body: getModule().config.modalTemplate
 						.replace(/\[\[conversationId\]\]/g, $(this).attr('data-onscreenchat-add'))
-						.replace(/#:#username#:#/g, il.Language.txt('username'))
+						.replace('#:#username#:#', il.Language.txt('username'))
 				  
 			});
 		},
