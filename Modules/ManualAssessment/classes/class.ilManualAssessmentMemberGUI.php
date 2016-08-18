6f37d8b566ca4a1bf8d48008326e45a0f0179e6c
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-08-18 14:03:31 +0200

    send notification when finalizing

--- Modules/ManualAssessment/classes/class.ilManualAssessmentMemberGUI.php
+++ Modules/ManualAssessment/classes/class.ilManualAssessmentMemberGUI.php
@@ -7,6 +7,7 @@ require_once 'Services/Form/classes/class.ilCheckboxInputGUI.php';
 require_once 'Services/Form/classes/class.ilNonEditableValueGUI.php';
 require_once 'Services/Form/classes/class.ilSelectInputGUI.php';
 require_once 'Modules/ManualAssessment/classes/LearningProgress/class.ilManualAssessmentLPInterface.php';
+require_once 'Modules/ManualAssessment/classes/Notification/class.ilManualAssessmentPrimitiveMailNotificator.php';
 /**
  * For the purpose of streamlining the grading and learning-process status definition
  * outside of tests, SCORM courses e.t.c. the ManualAssessment is used.
@@ -15,21 +16,24 @@ require_once 'Modules/ManualAssessment/classes/LearningProgress/class.ilManualAs
  * @author Denis Klöpfer <denis.kloepfer@concepts-and-training.de>
  */
 class ilManualAssessmentMemberGUI {
+		protected $notificator;
+
 		public function __construct($members_gui ,$a_parent_gui, $a_ref_id) {
-		global $DIC;
-		$this->ctrl = $DIC['ilCtrl'];
-		$this->members_gui = $members_gui;
-		$this->parent_gui = $a_parent_gui;
-		$this->object = $a_parent_gui->object;
-		$this->ref_id = $a_ref_id;
-		$this->tpl =  $DIC['tpl'];
-		$this->lng = $DIC['lng'];
-		$this->ctrl->saveParameter($this,'usr_id');
-		$this->examinee = new ilObjUser($_GET['usr_id']);
-		$this->examiner = $DIC['ilUser'];
-		$this->setTabs($DIC['ilTabs']);
-		$this->member = $this->object->membersStorage()
-							->loadMember($this->object,$this->examinee);
+			$this->notificator = new ilManualAssessmentPrimitiveMailNotificator();
+			global $DIC;
+			$this->ctrl = $DIC['ilCtrl'];
+			$this->members_gui = $members_gui;
+			$this->parent_gui = $a_parent_gui;
+			$this->object = $a_parent_gui->object;
+			$this->ref_id = $a_ref_id;
+			$this->tpl =  $DIC['tpl'];
+			$this->lng = $DIC['lng'];
+			$this->ctrl->saveParameter($this,'usr_id');
+			$this->examinee = new ilObjUser($_GET['usr_id']);
+			$this->examiner = $DIC['ilUser'];
+			$this->setTabs($DIC['ilTabs']);
+			$this->member = $this->object->membersStorage()
+								->loadMember($this->object,$this->examinee);
 	}
 
 	public function executeCommand() {
@@ -80,9 +84,9 @@ class ilManualAssessmentMemberGUI {
 			if($form->checkInput()) {
 				$member = $this->updateDataInMemberByArray($this->member,$_POST);
 				if($member->mayBeFinalized()) {
-					$this->member = $member->withFinalized();
 					$this->object->membersStorage()->updateMember($this->member);
 					ilManualAssessmentLPInterface::updateLPStatusOfMember($member);
+					$this->member = $member->withFinalized()->maybeSendNotification($this->notificator);
 					$this->view();
 				} else {
 					ilUtil::sendFailure('member may not be finalized');
