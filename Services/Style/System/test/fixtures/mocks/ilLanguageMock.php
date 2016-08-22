a083b607394af13bfc38290411524f7a78d37679
Timon Amstutz <timon.amstutz@bluewin.ch>
2016-08-22 11:39:17 +0200

    System Style Tests

---
+++ Services/Style/System/test/fixtures/mocks/ilLanguageMock.php
@@ -0,0 +1,25 @@
+<?php
+
+/* Copyright (c) 2015 Richard Klees <richard.klees@concepts-and-training.de> Extended GPL, see docs/LICENSE */
+/* Copyright (c) 2016 Timon Amstutz <timon.amstutz@ilub.unibe.ch> Extended GPL, see docs/LICENSE */
+
+/**
+ * Class ilLanguageMock
+ */
+class ilLanguageMock {
+	public $requested = array();
+
+	/**
+	 * ilLanguageMock constructor.
+	 */
+	public function __construct() {}
+
+	/**
+	 * @param $a_topic
+	 * @return mixed
+	 */
+	public function txt($a_topic) {
+		$this->requested[] = $a_topic;
+		return $a_topic;
+	}
+}
