060c0b18dce0d844b3ff8375cd8dcb7af4badc76
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-08-25 10:18:34 +0200

    removed a lot of copypaste nonsense

--- Modules/ManualAssessment/classes/Settings/class.ilManualAssessmentSettingsStorageDB.php
+++ Modules/ManualAssessment/classes/Settings/class.ilManualAssessmentSettingsStorageDB.php
@@ -5,6 +5,7 @@
  */
 require_once 'Modules/ManualAssessment/interfaces/Settings/interface.ilManualAssessmentSettingsStorage.php';
 require_once 'Modules/ManualAssessment/classes/Settings/class.ilManualAssessmentSettings.php';
+require_once 'Modules/ManualAssessment/classes/Settings/class.ilManualAssessmentInfoSettings.php';
 require_once 'Modules/ManualAssessment/classes/class.ilObjManualAssessment.php';
 class ilManualAssessmentSettingsStorageDB implements ilManualAssessmentSettingsStorage {
 
@@ -62,7 +63,7 @@ class ilManualAssessmentSettingsStorageDB implements ilManualAssessmentSettingsS
 			$sql = 	'SELECT contact, responsibility, phone, mails, consultation_hours'
 					.'	FROM mass_info_settings WHERE obj_id = '.$this->db->quote($obj_id,'integer');
 			if($res = $this->db->fetchAssoc($this->db->query($sql))) {
-				return new ilManualAssessmentSettings($obj,
+				return new ilManualAssessmentInfoSettings($obj,
 					$res["contact"]
 					,$res["responsibility"]
 					,$res['phone']
@@ -71,7 +72,7 @@ class ilManualAssessmentSettingsStorageDB implements ilManualAssessmentSettingsS
 			}
 			throw new ilManualAssessmentException("$obj_id not in database");
 		} else {
-			return new ilManualAssessmentSettings($obj);
+			return new ilManualAssessmentInfoSettings($obj);
 		}
 	}
 
@@ -80,9 +81,21 @@ class ilManualAssessmentSettingsStorageDB implements ilManualAssessmentSettingsS
 	 *
 	 * @param	ilManualAssessmentSettings	$settings
 	 */
-	public function updateInfoSettings(ilManualAssessmentSettings $settings) {
-		$sql = 'UPDATE mass_settings SET content = %s,record_template = %s WHERE obj_id = %s';
-		$this->db->manipulateF($sql,array("text","text","integer"),array($settings->content(),$settings->recordTemplate(),$settings->getId()));
+	public function updateInfoSettings(ilManualAssessmentInfoSettings $settings) {
+		$sql = 	'UPDATE mass_info_settings SET '
+				.'	contact = %s'
+				.'	,responsibility = %s'
+				.'	,phone = %s'
+				.'	,mails = %s'
+				.'	,consultation_hours = %s'
+				.' WHERE obj_id = %s';
+		$this->db->manipulateF($sql,array('text','text','text','text','text','integer'),
+			array(	$settings->contact()
+					,$settings->responsibility()
+					,$settings->phone()
+					,$settings->mails()
+					,$settings->consultationHours()
+					,$settings->id()));
 	}
 
 	/**
