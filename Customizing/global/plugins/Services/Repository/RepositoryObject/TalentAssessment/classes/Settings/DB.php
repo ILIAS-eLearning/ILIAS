6d009c145a91a8103d9aceeedc6d520f41c937cb
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-08-26 11:31:35 +0200

    gev2: 2392 fill db handle

--- Customizing/global/plugins/Services/Repository/RepositoryObject/TalentAssessment/classes/Settings/DB.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/TalentAssessment/classes/Settings/DB.php
@@ -11,9 +11,21 @@ interface DB {
 	/**
 	 * create new settings entries
 	 *
-	 * @param 	int 			$obj_id
+	 * @param 	int 				$obj_id
+	 * @param 	int 				$state
+	 * @param 	int 				$career_goal_id
+	 * @param 	string 				$username
+	 * @param 	string 				$firstname
+	 * @param 	string 				$lastname
+	 * @param 	string 				$email
+	 * @param 	ilDateTime|null 	$start_date
+	 * @param 	ilDateTime|null 	$end_date
+	 * @param 	int|null 			$venue
+	 * @param 	int|null 			$org_unit
+	 *
+	 * @return TalentAssessment
 	 */
-	public function create($obj_id);
+	public function create($obj_id, $state, $career_goal_id, $username, $firstname, $lastname, $email, $start_date, $end_date, $venue, $org_unit);
 
 	/**
 	 * updates settings entries
