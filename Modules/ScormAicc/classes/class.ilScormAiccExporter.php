a63995d0bbc3514f962cb6647d964bfc5198b174
Uwe-Kohnle <kohnle@internetlehrer-gmbh.de>
2016-08-28 23:54:52 +0200

    improvements for SCORM-Export

--- Modules/ScormAicc/classes/class.ilScormAiccExporter.php
+++ Modules/ScormAicc/classes/class.ilScormAiccExporter.php
@@ -16,7 +16,7 @@ class ilScormAiccExporter extends ilXmlExporter
 	public function getXmlRepresentation($a_entity, $a_schema_version, $a_id)
 	{
 		$this->dataset->setExportDirectories($this->dir_relative, $this->dir_absolute);
-		//using own getXmlRepresentation function in ilSAHSDataSet
+		//using own getXmlRepresentation function in ilScormAiccDataSet
 		return $this->dataset->getExtendedXmlRepresentation($a_entity, $a_schema_version, $a_id, "", false, true);
 	}
 
@@ -24,28 +24,21 @@ class ilScormAiccExporter extends ilXmlExporter
 	function getValidSchemaVersions($a_entity)
 	{
 		return array (
-			"4.3.0" => array(
-				"namespace" => "http://www.ilias.de/Modules/ScormAicc/sahs/4_3",
-				"xsd_file" => "ilias_mod_4_3.xsd",
+			"5.1.0" => array(
+				"namespace" => "http://www.ilias.de/Modules/ScormAicc/sahs/5_1",
+				"xsd_file" => "xml/ilias_sahs_5_1.xsd",
 				"uses_dataset" => true,
-				"min" => "4.3.0",
-				"max" => "4.4.99"),
-			"4.5.0" => array(
-				"namespace" => "http://www.ilias.de/Modules/ScormAicc/sahs/4_5",
-				"xsd_file" => "ilias_mod_4_5.xsd",
-				"uses_dataset" => true,
-				"min" => "4.5.0",
+				"min" => "5.1.0",
 				"max" => "")
 		);
 	}
 
+/*
 	public function getXmlExportTailDependencies($a_entity, $a_target_release, $a_ids)
 	{
 		$md_ids = array();
-		foreach ($a_ids as $mob_id)
-		{
-			$md_ids[] = "0:".$mob_id.":mob";
-		}
+		$md_ids[0] = "0:".$mob_id.":mob";
+
 		return array (
 			array(
 				"component" => "Modules/ScormAicc",
@@ -53,6 +46,7 @@ class ilScormAiccExporter extends ilXmlExporter
 				"ids" => $md_ids)
 			);
 	}
+*/
 	
 }
 ?>
