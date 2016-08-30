71b196cde1231520996ded0783a5e7a837d4c27a
smeyer-ilias <smeyer.ilias@gmx.de>
2016-08-30 12:57:38 +0200

    Fixed role selection
    Conflicts:
    	lang/ilias_de.lang
    	lang/ilias_en.lang

--- Services/User/classes/class.ilObjUserGUI.php
+++ Services/User/classes/class.ilObjUserGUI.php
@@ -386,6 +386,14 @@ class ilObjUserGUI extends ilObjectGUI
 		if ($this->form_gui->checkInput())
 		{
 // @todo: external account; time limit check and savings
+			$role_select = $this->form_gui->getItemByPostVar('default_role');
+			if(!array_key_exists($role_select->getValue(), (array) $role_select->getOptions()))
+			{
+				$role_select->setAlert($this->lng->txt('err_invalid_form_input'));
+				ilUtil::sendFailure($this->lng->txt('err_check_input'));
+				$this->form_gui->setValuesByPost();
+				return $tpl->setContent($this->form_gui->getHtml());
+			}
 
 			// checks passed. save user
 			$userObj = $this->loadValuesFromForm();
