3160b52749b8d34a617b4d053db7f86a290a27fc
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-09-01 16:19:37 +0200

    gev2: 2392 add career goal lang vals

--- Customizing/global/plugins/Services/Repository/RepositoryObject/CareerGoal/classes/Settings/ilFormHelper.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/CareerGoal/classes/Settings/ilFormHelper.php
@@ -18,35 +18,35 @@ trait ilFormHelper {
 	 * @return null
 	 */
 	public function addSettingsFormItems(\ilPropertyFormGUI $form) {
-		$ni = new \ilNumberInputGUI($this->txt(""), ilActions::F_LOWMARK);
+		$ni = new \ilNumberInputGUI($this->txt("lowmark"), ilActions::F_LOWMARK);
 		$ni->setMinValue(0);
 		$ni->setMaxValue(5);
-		$ni->setInfo($this->txt(""));
+		$ni->setInfo($this->txt("lowmark_info"));
 		$ni->setRequired(true);
 		$ni->allowDecimals(true);
 		$form->addItem($ni);
 
-		$ni = new \ilNumberInputGUI($this->txt(""), ilActions::F_SHOULD_SPECIFICATION);
+		$ni = new \ilNumberInputGUI($this->txt("sheduled_specification"), ilActions::F_SHOULD_SPECIFICATION);
 		$ni->setMinValue(0);
 		$ni->setMaxValue(5);
-		$ni->setInfo($this->txt(""));
+		$ni->setInfo($this->txt("sheduled_specification_info"));
 		$ni->setRequired(true);
 		$ni->allowDecimals(true);
 		$form->addItem($ni);
 
-		$ti = new \ilTextAreaInputGUI($this->txt(""), ilActions::F_DEFAULT_TEXT_FAILED);
+		$ti = new \ilTextAreaInputGUI($this->txt("default_text_failed"), ilActions::F_DEFAULT_TEXT_FAILED);
 		$ti->setRequired(true);
-		$ti->setInfo($this->txt(""));
+		$ti->setInfo($this->txt("default_text_failed_info"));
 		$form->addItem($ti);
 
-		$ti = new \ilTextAreaInputGUI($this->txt(""), ilActions::F_DEFAULT_TEXT_PARTIAL);
+		$ti = new \ilTextAreaInputGUI($this->txt("default_text_partial"), ilActions::F_DEFAULT_TEXT_PARTIAL);
 		$ti->setRequired(true);
-		$ti->setInfo($this->txt(""));
+		$ti->setInfo($this->txt("default_text_partial_info"));
 		$form->addItem($ti);
 
-		$ti = new \ilTextAreaInputGUI($this->txt(""), ilActions::F_DEFAULT_TEXT_SUCCESS);
+		$ti = new \ilTextAreaInputGUI($this->txt("default_text_success"), ilActions::F_DEFAULT_TEXT_SUCCESS);
 		$ti->setRequired(true);
-		$ti->setInfo($this->txt(""));
+		$ti->setInfo($this->txt("default_text_success_info"));
 		$form->addItem($ti);
 	}
 }
