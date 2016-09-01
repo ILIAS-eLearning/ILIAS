31cca4fbca6b0d1ba7233d6fed7ed0007ee34bd4
Guido Vollbach <gvollbach@databay.de>
2016-09-01 09:52:58 +0200

    chat: validate that id is not null

--- Modules/Chatroom/chat/SocketTasks/ConversationMessage.js
+++ Modules/Chatroom/chat/SocketTasks/ConversationMessage.js
@@ -1,18 +1,21 @@
 var Container = require('../AppContainer');
 
 module.exports = function(conversationId, userId, message) {
-	Container.getLogger().info('SendMessage "%s" by "%s" in conversation %s', message, userId, conversationId);
-	var namespace = Container.getNamespace(this.nsp.name);
-	var conversation = namespace.getConversations().getById(conversationId);
-	var messageObj = {
-		conversationId: conversationId,
-		userId: userId,
-		message: message,
-		timestamp: (new Date).getTime()
-	};
+	if(conversationId !== null && userId !== null && message !== null)
+	{
+		Container.getLogger().info('SendMessage "%s" by "%s" in conversation %s', message, userId, conversationId);
+		var namespace = Container.getNamespace(this.nsp.name);
+		var conversation = namespace.getConversations().getById(conversationId);
+		var messageObj = {
+			conversationId: conversationId,
+			userId: userId,
+			message: message,
+			timestamp: (new Date).getTime()
+		};
 
-	namespace.getDatabase().persistConversationMessage(messageObj);
+		namespace.getDatabase().persistConversationMessage(messageObj);
 
-	conversation.emit('conversation', conversation.json());
-	conversation.send(messageObj);
+		conversation.emit('conversation', conversation.json());
+		conversation.send(messageObj);
+	}
 };
