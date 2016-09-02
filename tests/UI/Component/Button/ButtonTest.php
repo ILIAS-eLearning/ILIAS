cb56a25d8ad964787d86a074b57404a5b51c7d8a
mjansen <mjansen@databay.de>
2016-09-02 15:36:39 +0200

    UI: Fixed unit test

--- tests/UI/Component/Button/ButtonTest.php
+++ tests/UI/Component/Button/ButtonTest.php
@@ -210,7 +210,7 @@ class ButtonTest extends ILIAS_UI_TestBase {
 
 		$id = $ids[0];
 		$expected = "<button type=\"button\" class=\"close\" data-dismiss=\"modal\" id=\"$id\">".
-					"	<span aria-hidden=\"true\">x</span>".
+					"	<span aria-hidden=\"true\">&times;</span>".
 					"	<span class=\"sr-only\">Close</span>".
 					"</button>";
 		$this->assertEquals($expected, $html);
