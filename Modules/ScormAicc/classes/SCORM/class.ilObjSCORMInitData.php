84fb4208d3089c85918e8b051e157a578ebe8dfa
Uwe-Kohnle <kohnle@internetlehrer-gmbh.de>
2016-09-16 21:59:52 +0200

    fix Mantis #19024

--- Modules/ScormAicc/classes/SCORM/class.ilObjSCORMInitData.php
+++ Modules/ScormAicc/classes/SCORM/class.ilObjSCORMInitData.php
@@ -15,7 +15,7 @@
 class ilObjSCORMInitData
 {
 
-	function encodeURIComponent($str) {
+	static function encodeURIComponent($str) {
 		$revert = array('%21'=>'!', '%2A'=>'*', '%27'=>"'", '%28'=>'(', '%29'=>')', '%7E'=>'~');
 		return strtr(rawurlencode($str), $revert);
 	}
