a5971ba9058dcb00b3a14d34d8ac8e9976a02c49
mjansen <mjansen@databay.de>
2016-09-28 08:50:07 +0200

    Question Types: File upload

--- Modules/TestQuestionPool/classes/class.ilMatchingWizardInputGUI.php
+++ Modules/TestQuestionPool/classes/class.ilMatchingWizardInputGUI.php
@@ -315,6 +315,7 @@ class ilMatchingWizardInputGUI extends ilTextInputGUI
 					$tpl->parseCurrentBlock();
 				}
 				$tpl->setCurrentBlock('addimage');
+				$tpl->setVariable("IMAGE_BROWSE", $lng->txt('select_file'));
 				$tpl->setVariable("IMAGE_ID", $this->getPostVar() . "[image][$i]");
 				$tpl->setVariable("IMAGE_SUBMIT", $lng->txt("upload"));
 				$tpl->setVariable("IMAGE_ROW_NUMBER", $i);
