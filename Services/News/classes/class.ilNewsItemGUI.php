98b24faaf38924fc7c6548c8fc9cf61e9bb7c685
Richard Klees <richard.klees@concepts-and-training.de>
2016-09-30 15:36:32 +0200

    use ilCtrl->getParentReturn instead of ilCtrl->getParentReturnByClass

--- Services/News/classes/class.ilNewsItemGUI.php
+++ Services/News/classes/class.ilNewsItemGUI.php
@@ -683,7 +683,7 @@ class ilNewsItemGUI
 
 		$ilTabs->clearTargets();
 		$ilTabs->setBackTarget($lng->txt("back"),
-			$ilCtrl->getParentReturnByClass("ilnewsitemgui"));
+			$ilCtrl->getParentReturn($this));
 	}
 
 	/**
