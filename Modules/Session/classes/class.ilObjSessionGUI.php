85e48d85b74dc3f1b0a7da677f4042bea4d4622c
smeyer-ilias <smeyer.ilias@gmx.de>
2016-10-19 11:25:19 +0200

    0019426: Failed test: Eine neue Sitzung manuell anlegen (mit Wiederholungen)

--- Modules/Session/classes/class.ilObjSessionGUI.php
+++ Modules/Session/classes/class.ilObjSessionGUI.php
@@ -19,6 +19,10 @@ include_once './Services/PersonalDesktop/interfaces/interface.ilDesktopItemHandl
 */
 class ilObjSessionGUI extends ilObjectGUI implements ilDesktopItemHandling
 {
+	/**
+	 * @var ilLogger
+	 */
+	protected $logger = null;
 
 
 	public $lng;
@@ -53,6 +57,8 @@ class ilObjSessionGUI extends ilObjectGUI implements ilDesktopItemHandling
 
 		$this->tpl = $tpl;
 		$this->ctrl = $ilCtrl;
+		
+		$this->logger = $GLOBALS['DIC']->logger()->sess();
 	}
 	
 	
@@ -1844,12 +1850,10 @@ class ilObjSessionGUI extends ilObjectGUI implements ilDesktopItemHandling
 				break;
 				
 			case 3:
-				$end_dt['year'] = (int) $_POST['until_end']['date']['y'];
-				$end_dt['mon'] = (int) $_POST['until_end']['date']['m'];
-				$end_dt['mday'] = (int) $_POST['until_end']['date']['d'];
-				
+				$frequence = $this->form->getItemByPostVar('frequence');
+				$end = $frequence->getRecurrence()->getFrequenceUntilDate();
 				$this->rec->setFrequenceUntilCount(0);
-				$this->rec->setFrequenceUntilDate(new ilDate($end_dt,IL_CAL_FKT_GETDATE,$this->timezone));
+				$this->rec->setFrequenceUntilDate($end);
 				break;
 		}
 	}
