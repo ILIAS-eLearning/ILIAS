ed3545ea2171d8246b4d575039afe2078051e182
xus <xus206@hotmail.com>
2016-10-28 16:45:54 +0200

    Possible bugfix_19469

--- Modules/Survey/classes/class.ilSurveyPageGUI.php
+++ Modules/Survey/classes/class.ilSurveyPageGUI.php
@@ -22,6 +22,7 @@ class ilSurveyPageGUI
 	protected $has_next_page; // [bool]
 	protected $has_datasets; // [bool]
 	protected $use_pool; // [bool]
+	protected $log;
 	
 	/**
 	* Constructor
@@ -34,6 +35,7 @@ class ilSurveyPageGUI
 		$this->editor_gui = $a_survey_editor_gui;
 		$this->ref_id = $a_survey->getRefId();
 		$this->object = $a_survey;
+		$this->log = ilLoggerFactory::getLogger("svy");
 	}
 
 	/**
@@ -177,11 +179,37 @@ class ilSurveyPageGUI
 		
 		// append to survey 
 		$next_id = $ilDB->nextId('svy_svy_qst');
+
+		$this->log->debug("INSERT INTO svy_svy_qst (survey_question_id, survey_fi,question_fi, sequence, tstamp) VALUES ($next_id, ".$this->object->getSurveyId().", $survey_question_id, $sequence, time)");
+
 		$affectedRows = $ilDB->manipulateF("INSERT INTO svy_svy_qst (survey_question_id, survey_fi,".
 			"question_fi, sequence, tstamp) VALUES (%s, %s, %s, %s, %s)",
 			array('integer', 'integer', 'integer', 'integer', 'integer'),
 			array($next_id, $this->object->getSurveyId(), $survey_question_id, $sequence, time())
 		);
+
+		#19469
+		$result = $ilDB->queryF("SELECT obj_fi FROM svy_svy WHERE survey_id = %s LIMIT 1",
+			array('integer'),
+			array($this->object->getSurveyId())
+		);
+		$this->log->debug("SELECT obj_fi FROM svy_svy WHERE survey_id = ".$this->object->getSurveyId()." LIMIT 1");
+
+		if ($ilDB->numRows($result))
+		{
+			while ($row = $ilDB->fetchAssoc($result))
+			{
+				$survey_object_id = $row['obj_fi'];
+			}
+			if($survey_object_id > 0)
+			{
+				$affectedRows = $ilDB->manipulateF("UPDATE svy_question SET obj_fi= %s WHERE question_id = %s ",
+					array('integer', 'integer'),
+					array($survey_object_id, $survey_question_id)
+				);
+				$this->log->debug("UPDATE svy_question SET obj_fi= ".$survey_object_id." WHERE question_id = ".$survey_question_id);
+			}
+		}
 
 		return $survey_question_id;
 	}
