1c71d7960308ac42a37f255aa88d678cac091b79
Bjoern Heyser <bheyser@databay.de>
2016-11-11 14:43:27 +0100

    fixed fix for second context and removed tmp workaround - found another not affecting issue, that might become a problem by any change in the calling/requesting/redirecting strategy

--- Modules/TestQuestionPool/classes/class.assQuestion.php
+++ Modules/TestQuestionPool/classes/class.assQuestion.php
@@ -848,9 +848,10 @@ abstract class assQuestion
 					array_push($output, '<a href="' . $this->getSuggestedSolutionPathWeb() . $solution["value"]["name"] . '">' . $possible_texts[0] . '</a>');
 					break;
 				case "text":
-					$output = $this->fixSvgToPng($output);
-					$output = $this->fixUnavailableSkinImageSources($output);
-					array_push($output, $this->prepareTextareaOutput($solution["value"], true));
+					$solutionValue = $solution["value"];
+					$solutionValue = $this->fixSvgToPng($solutionValue);
+					$solutionValue = $this->fixUnavailableSkinImageSources($solutionValue);
+					$output[] = $this->prepareTextareaOutput($solutionValue, true);
 					break;
 			}
 		}
@@ -2230,7 +2231,7 @@ abstract class assQuestion
 	public function fixUnavailableSkinImageSources($html)
 	{
 		$matches = null;
-		if(! is_array($html) && preg_match_all('/src="(.*?)"/m', $html, $matches) )
+		if( preg_match_all('/src="(.*?)"/m', $html, $matches) )
 		{
 			$sources = $matches[1];
 			
@@ -2785,7 +2786,7 @@ abstract class assQuestion
 		);
 		if ($affectedRows == 1)
 		{
-			$this->suggested_solutions["subquestion_index"] = array(
+			$this->suggested_solutions[$subquestion_index] = array(
 				"type" => $type,
 				"value" => $value,
 				"internal_link" => $solution_id,
