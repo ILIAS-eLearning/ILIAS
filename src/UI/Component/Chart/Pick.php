476ffd13f8fd74168e41eca77ece19316ce99ced
Timon Amstutz <timon.amstutz@bluewin.ch>
2016-11-21 12:59:30 +0100

    Update Pick.php
    Changed wrong return doc comment

--- src/UI/Component/Chart/Pick.php
+++ src/UI/Component/Chart/Pick.php
@@ -12,7 +12,7 @@ interface Pick {
 	/**
 	 * Sets a key value pair as items for the list. Key is used as title and value is a boolean marking highlighted values.
 	 * @param array string => boolean Set of elements to be rendered, boolean should be true if highlighted
-	 * @return \ILIAS\UI\Component\Listing\Descriptive
+	 * @return \ILIAS\UI\Component\Chart\Pick\Pick
 	 */
 	public function withItems(array $items);
 
