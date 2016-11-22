4655bb1c064fe8a6382d907e2b5493163223a4a2
smeyer-ilias <smeyer.ilias@gmx.de>
2016-11-22 12:23:49 +0100

    0019673: Failed test: Kurs importieren $$

--- Modules/Course/classes/class.ilCourseXMLWriter.php
+++ Modules/Course/classes/class.ilCourseXMLWriter.php
@@ -359,7 +359,9 @@ class ilCourseXMLWriter extends ilXmlWriter
 		$this->xmlEndTag('Period');		
 		$this->xmlElement('WaitingListAutoFill',null,(int)$this->course_obj->hasWaitingListAutoFill());
 		$this->xmlElement('CancellationEnd',null,($this->course_obj->getCancellationEnd() && !$this->course_obj->getCancellationEnd()->isNull()) ? $this->course_obj->getCancellationEnd()->get(IL_CAL_UNIX) : null);
-		$this->xmlElement('MinMembers',null,(int)$this->course_obj->getSubscriptionMinMembers());		
+		$this->xmlElement('MinMembers',null,(int)$this->course_obj->getSubscriptionMinMembers());	
+		
+		$this->xmlElement('ViewMode', null, $this->course_obj->getViewMode());
 
 		$this->xmlEndTag('Settings');
 
