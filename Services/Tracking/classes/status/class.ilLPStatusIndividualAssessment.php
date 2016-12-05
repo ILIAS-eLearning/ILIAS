b919172c55c383269f3e0808931344e5cb575290
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-12-05 14:11:20 +0100

    also considered recent changes

--- Services/Tracking/classes/status/class.ilLPStatusIndividualAssessment.php
+++ Services/Tracking/classes/status/class.ilLPStatusIndividualAssessment.php
@@ -7,12 +7,21 @@ require_once 'Modules/IndividualAssessment/classes/Members/class.ilIndividualAss
 
 class ilLPStatusIndividualAssessment extends ilLPStatus {
 
+	static function _getNotAttempted($a_obj_id) {
+		return ilIndividualAssessmentLPInterface::getMembersHavingStatusIn($a_obj_id, 
+			ilIndividualAssessmentMembers::LP_NOT_ATTEMPTED);
+	}
+
+	static function _getCountNotAttempted($a_obj_id) {
+		return count(self::_getNotAttempted($a_obj_id));
+	}
+
 	static function _getCountInProgress($a_obj_id) {
 		return count(self::_getInProgress($a_obj_id));
 	}
 
 	static function _getInProgress($a_obj_id) {
-		return ilIndividualAssessmentLPInterface::getMembersHavingStatusIn($a_obj_id,
+		return ilIndividualAssessmentLPInterface::getMembersHavingStatusIn($a_obj_id, 
 			ilIndividualAssessmentMembers::LP_IN_PROGRESS);
 	}
 
@@ -21,7 +30,7 @@ class ilLPStatusIndividualAssessment extends ilLPStatus {
 	}
 
 	static function _getCompleted($a_obj_id) {
-		return ilIndividualAssessmentLPInterface::getMembersHavingStatusIn($a_obj_id,
+		return ilIndividualAssessmentLPInterface::getMembersHavingStatusIn($a_obj_id, 
 			ilIndividualAssessmentMembers::LP_COMPLETED);
 	}
 
@@ -30,12 +39,14 @@ class ilLPStatusIndividualAssessment extends ilLPStatus {
 	}
 
 	static function _getFailed($a_obj_id) {
-		return ilIndividualAssessmentLPInterface::getMembersHavingStatusIn($a_obj_id,
+		return ilIndividualAssessmentLPInterface::getMembersHavingStatusIn($a_obj_id, 
 			ilIndividualAssessmentMembers::LP_FAILED);
 	}
 
 	function determineStatus($a_obj_id, $a_user_id, $a_obj = null) {
 		switch ((string)ilIndividualAssessmentLPInterface::determineStatusOfMember($a_obj_id,$a_user_id)) {
+			case (string)ilIndividualAssessmentMembers::LP_NOT_ATTEMPTED:
+				return self::LP_STATUS_NOT_ATTEMPTED_NUM;
 			case (string)ilIndividualAssessmentMembers::LP_IN_PROGRESS:
 				return self::LP_STATUS_IN_PROGRESS_NUM;
 			case (string)ilIndividualAssessmentMembers::LP_FAILED:
