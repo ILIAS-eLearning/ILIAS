c332809e278aa5a7f58470b4686213a2b93c23dc
Timon Amstutz <timon.amstutz@bluewin.ch>
2016-12-12 16:28:17 +0100

    UI View Control Proposal

---
+++ src/UI/examples/ViewControl/Mode/mockup.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Only serving as Mockup
+ * Todo: Replace with actual example as soon as implemented
+ */
+function mockup() {
+	//Loading factories
+	global $DIC;
+	$f = $DIC->ui()->factory();
+	$renderer = $DIC->ui()->renderer();
+
+	//Genarating and rendering the mockup
+	$image = $f->image()->responsive("src/UI/examples/ViewControl/Mode/mockup1.png", "mockup1");
+	$html = $renderer->render($image);
+
+	return $html;
+}
