bd1604e0c6cf4c0ca9b6ff7c59c69393d7902db7
Timon Amstutz <timon.amstutz@bluewin.ch>
2016-12-12 16:28:17 +0100

    cherry pick c332809

--- tests/UI/Base.php
+++ tests/UI/Base.php
@@ -30,6 +30,7 @@ class NoUIFactory implements Factory {
 	public function legacy($content) {}
 	public function panel() {}
 	public function modal() {}
+	public function viewControl() {}
 }
 
 class LoggingRegistry implements ResourceRegistry {
