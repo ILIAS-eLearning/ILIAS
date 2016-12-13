922773985fa0da3cbd69cb6ca4daeb410e14b953
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-12-13 10:46:16 +0100

    show crs title if completed by is crs reference

--- Modules/StudyProgramme/classes/class.ilStudyProgrammeMembersTableGUI.php
+++ Modules/StudyProgramme/classes/class.ilStudyProgrammeMembersTableGUI.php
@@ -147,7 +147,7 @@ class ilStudyProgrammeMembersTableGUI extends ilTable2GUI {
 				   ."     , prgrs.status"
 				   ."     , blngs.title belongs_to"
 				   ."     , cmpl_usr.login accredited_by"
-				   ."     , cmpl_obj.title completion_by"
+				   ."     , IF(cmpl_obj.type = 'crsr', parent_crs.title, cmpl_obj.title) completion_by"
 				   ."     , prgrs.assignment_id assignment_id"
 				   ."     , ass.root_prg_id root_prg_id"
 				   ."     , ass.last_change assign_date"
@@ -220,7 +220,9 @@ class ilStudyProgrammeMembersTableGUI extends ilTable2GUI {
 				."  JOIN object_data blngs ON blngs.obj_id = ass.root_prg_id"
 				."  LEFT JOIN usr_data cmpl_usr ON cmpl_usr.usr_id = prgrs.completion_by"
 				."  LEFT JOIN usr_data ass_usr ON ass_usr.usr_id = ass.last_change_by"
-				."  LEFT JOIN object_data cmpl_obj ON cmpl_obj.obj_id = prgrs.completion_by";
+				."  LEFT JOIN object_data cmpl_obj ON cmpl_obj.obj_id = prgrs.completion_by"
+				."  LEFT JOIN container_reference con_ref ON con_ref.obj_id = prgrs.completion_by"
+				."  LEFT JOIN object_data parent_crs ON parent_crs.obj_id = con_ref.target_obj_id";
 	}
 
 	protected function getWhere($a_prg_id) {
