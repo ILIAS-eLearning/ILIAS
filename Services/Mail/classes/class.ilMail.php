9e7cb321da305ad822d9dcdfe221f3f966543a86
mjansen <mjansen@databay.de>
2017-01-03 09:14:38 +0100

    Mail: Fixed Mantis issue #20042

--- Services/Mail/classes/class.ilMail.php
+++ Services/Mail/classes/class.ilMail.php
@@ -2361,7 +2361,7 @@ class ilMail
 			include_once 'Services/WebServices/SOAP/classes/class.ilSoapClient.php';
 
 			$soap_client = new ilSoapClient();
-			$soap_client->setResponseTimeout(1);
+			$soap_client->setResponseTimeout(5);
 			$soap_client->enableWSDL(true);
 			$soap_client->init();
 
