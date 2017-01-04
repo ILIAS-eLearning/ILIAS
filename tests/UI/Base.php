65284a1aad1c260d0aa2030bba1ab8f9f11bc03e
Stefan Wanzenried <stefan.wanzenried@gmail.com>
2017-01-04 13:22:16 +0100

    Refactoring of tests

--- tests/UI/Base.php
+++ tests/UI/Base.php
@@ -30,6 +30,7 @@ class NoUIFactory implements Factory {
 	public function legacy($content) {}
 	public function panel() {}
     public function modal() {}
+	public function connector() {}
 }
 
 class LoggingRegistry implements ResourceRegistry {
