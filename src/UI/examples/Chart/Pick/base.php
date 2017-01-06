90d1b288b724c54a928d2e77c1f876323fad452c
Timon Amstutz <timon.amstutz@bluewin.ch>
2017-01-06 17:13:30 +0100

    UI Pick: Replaced HTML mockup with png

--- src/UI/examples/Chart/Pick/base.php
+++ src/UI/examples/Chart/Pick/base.php
@@ -1,13 +1,14 @@
 <?php
-
-/* Copyright (c) 2016 Timon Amstutz <timon.amstutz@ilub.unibe.ch> Extended GPL, see docs/LICENSE */
-
+/**
+ * Mockup
+ */
 function base() {
-	$bar1 = "<button type=\"button\" class=\"btn btn-info btn-sm\">Grade 1</button>";
-
-	$bar2 = "<button type=\"button\" class=\"btn btn-info btn-sm active\">Grade 2</button>";
-
-	$bar3 = "<button type=\"button\" class=\"btn btn-info btn-sm\">Grade 3</button>";
-
-	return $bar1." ".$bar2." ".$bar3;
+	global $DIC;
+	$f = $DIC->ui()->factory();
+	$renderer = $DIC->ui()->renderer();
+	$image = $f->image()->responsive(
+			"src/UI/examples/Chart/mockup.png",
+			"Mockup");
+	$html = $renderer->render($image);
+	return $html;
 }
