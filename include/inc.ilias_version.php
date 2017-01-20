ec86071ae67319f815dcc6a0c9e24ebde5bbdb90
Fabian Schmid <fs@studer-raimann.ch>
2017-01-20 14:55:57 +0100

    since ILIAS 5.2 has not yet been released i switch this back to beta 3

--- include/inc.ilias_version.php
+++ include/inc.ilias_version.php
@@ -9,5 +9,5 @@
 *
 * @package ilias-core
 */
-define("ILIAS_VERSION", "5.2.0 2016-01-18");
+define("ILIAS_VERSION", "5.2.0 Beta 3 2016-10-31");
 define("ILIAS_VERSION_NUMERIC", "5.2.0");			// must be always x.y.z: x, y and z are numbers
