81290f789d259e709bc4ec5e4f012f3e3e333f4b
Alex Killing <alex.killing@gmx.de>
2017-01-25 11:46:38 +0100

    fix 19795, remove debugging

--- Services/Migration/DBUpdate_5069/classes/class.ilDBUpdate5069.php
+++ Services/Migration/DBUpdate_5069/classes/class.ilDBUpdate5069.php
@@ -25,7 +25,7 @@ class ilDBUpdate5069
 		{
 			include_once("./Services/Migration/DBUpdate_5069/classes/class.ilFSStorageExercise5069.php");
 			$storage = new ilFSStorageExercise5069($row['exc_id'], $row['id']);
-echo "0 -".$row['peer_crit_cat']."<br>";
+//echo "0 -".$row['peer_crit_cat']."<br>";
 			//if the assignment has criteria category
 			if($row['peer_crit_cat'])
 			{
@@ -35,11 +35,11 @@ echo "0 -".$row['peer_crit_cat']."<br>";
 					" WHERE parent = ".$ilDB->quote($row['peer_crit_cat'],"integer").
 					" AND type = ".$ilDB->quote('file','text')
 				);
-echo "#".$q."#<br>";
+//echo "#".$q."#<br>";
 				while($row_crit = $ilDB->fetchAssoc($res_crit))
 				{
 					$original_path = $storage->getPeerReviewUploadPath($row['peer_id'], $row['giver_id'], $row_crit['id']);
-echo "a-$original_path-<br>";
+//echo "a-$original_path-<br>";
 					$path_peaces = explode('/', rtrim($original_path, '/'));
 					array_pop($path_peaces);
 					$previous_dir_path = "";
@@ -48,28 +48,25 @@ echo "a-$original_path-<br>";
 						$previous_dir_path .= $piece."/";
 					}
 					$dir_content = array_diff(scandir($previous_dir_path), array('.', '..'));
-echo $previous_dir_path."<br>";
+//echo $previous_dir_path."<br>";
 					//loop the directory content
 					foreach($dir_content as $content)
 					{
-		echo "b";
 						if(is_dir($previous_dir_path.$content))
 						{
-		echo "c-$content-".$row['giver_id'].$row_crit['id']."-";
 							//if the directory name is wrong(giver_id+criteria_id)
 							if($content == $row['giver_id'].$row_crit['id'])
 							{
-		echo "d<br>";
 								if(!is_dir($previous_dir_path.$row['giver_id']))
 								{
 									mkdir($previous_dir_path.$row['giver_id']);
-echo "1 make dir ".$previous_dir_path.$row['giver_id']."<br>";
+//echo "1 make dir ".$previous_dir_path.$row['giver_id']."<br>";
 								}
 
 								if(!is_dir($previous_dir_path.$row['giver_id']."/".$row_crit['id']))
 								{
 									mkdir($previous_dir_path.$row['giver_id']."/".$row_crit['id']);
-echo "2 make dir ".$previous_dir_path.$row['giver_id']."/".$row_crit['id']."<br>";
+//echo "2 make dir ".$previous_dir_path.$row['giver_id']."/".$row_crit['id']."<br>";
 								}
 
 
@@ -79,25 +76,7 @@ echo "2 make dir ".$previous_dir_path.$row['giver_id']."/".$row_crit['id']."<br>
 								{
 									rename($old, $new);
 								}
-echo "rename ".$old." to ".$new."<br>";
-
-								/*
-								//take the files of the wrong directory and copy them into the proper new directory.
-								$sub_dir_content = array_diff(scandir($original_path), array('.', '..'));
-echo "reading ".$original_path."<br>";
-								foreach($sub_dir_content as $sub_content)
-								{
-echo "checking ".$original_path.$sub_content."<br>";
-									if(!is_dir($original_path.$sub_content))
-									{
-//										$copy = copy($original_path.$sub_content, $previous_dir_path.$row['giver_id']."/".$row_crit['id']."/".$sub_content);
-echo "3 copy ".$original_path.$sub_content." to ".$previous_dir_path.$row['giver_id']."/".$row_crit['id']."/".$sub_content."<br>";
-									}
-								}
-//								array_map("unlink",glob($previous_dir_path.$content."/*.*"));
-echo " 4 unlink ".$previous_dir_path.$content."/*.*";
-//								rmdir($previous_dir_path.$content);
-echo "4 rmdir ".$previous_dir_path.$content."<br>";*/
+//echo "rename ".$old." to ".$new."<br>";
 							}
 						}
 					}
@@ -125,8 +104,8 @@ echo "4 rmdir ".$previous_dir_path.$content."<br>";*/
 						if (substr($content, 0, strlen($row['giver_id'])) === $row['giver_id'])
 						{
 							$new_filename = substr($content, strlen($row['giver_id']));
-							$copy = copy($previous_dir_path.$content, $original_path."/".$new_filename);
-echo "5 copy ".$previous_dir_path.$content." to ".$original_path."/".$new_filename."<br>";
+							copy($previous_dir_path.$content, $original_path."/".$new_filename);
+//echo "5 copy ".$previous_dir_path.$content." to ".$original_path."/".$new_filename."<br>";
 						}
 					}
 				}
@@ -135,7 +114,7 @@ echo "5 copy ".$previous_dir_path.$content." to ".$original_path."/".$new_filena
 
 		}
 
-		die("ende");
+//		die("ende");
 	}
 	
 }
