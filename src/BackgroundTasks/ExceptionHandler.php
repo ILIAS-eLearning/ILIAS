7b362bd365ed7e5d53269c127ad68aa22b8005a6
Fabian Schmid <fs@studer-raimann.ch>
2017-01-31 08:02:57 +0100

    fixed type-hinting Part II

--- src/BackgroundTasks/ExceptionHandler.php
+++ src/BackgroundTasks/ExceptionHandler.php
@@ -14,10 +14,10 @@ interface ExceptionHandler {
 	/**
 	 * When working on a bucket and an exception occurs the exception handler will try to end the bucket operation gracefully.
 	 *
-	 * @param Exception $exception
+	 * @param Exceptions\Exception $exception
 	 * @param Bucket $bucket
 	 * @param Task|null $task
 	 * @return void
 	 */
-	public function handleException(Exception $exception, Bucket $bucket, Task $task = null);
+	public function handleException(Exceptions\Exception $exception, Bucket $bucket, Task $task = null);
 }
