1899aafc14a169eab7715e165d5bb60a0a24060a
Alex Killing <alex.killing@gmx.de>
2017-02-25 10:34:28 +0100

    fixed bug #19842: Participant View: Text Submission leads back to first participants

--- Modules/Exercise/classes/class.ilExerciseManagementGUI.php
+++ Modules/Exercise/classes/class.ilExerciseManagementGUI.php
@@ -38,7 +38,7 @@ class ilExerciseManagementGUI
 		$this->assignment = $a_ass;
 		
 		$ilCtrl->saveParameter($this, array("vw", "member_id"));
-		
+
 		// :TODO:
 		$this->ctrl = $ilCtrl;
 		$this->tabs_gui = $ilTabs;
@@ -119,7 +119,8 @@ class ilExerciseManagementGUI
 				$ilCtrl->forwardCommand($gui);				
 				break;
 				
-			case "ilexsubmissiontextgui":															
+			case "ilexsubmissiontextgui":
+				$ilCtrl->saveParameter($this, array("part_id"));
 				include_once "Modules/Exercise/classes/class.ilExSubmissionTextGUI.php";
 				$gui = new ilExSubmissionTextGUI($this->exercise, $this->initSubmission());
 				$ilCtrl->forwardCommand($gui);				
