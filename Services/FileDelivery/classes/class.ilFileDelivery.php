a786832a082cea131bc1cc43f5ee62d4819b0a3f
Fabian Schmid <fs@studer-raimann.ch>
2017-03-08 14:29:09 +0100

    instanceof check in filedelivery since ilClientIni isn't present in fast delivery mode of WAC

--- Services/FileDelivery/classes/class.ilFileDelivery.php
+++ Services/FileDelivery/classes/class.ilFileDelivery.php
@@ -750,7 +750,7 @@ class ilFileDelivery {
 		 * @var $ilClientIniFile ilIniFile
 		 */
 
-		if ($ilClientIniFile->readVariable('file_access', 'disable_ascii')) {
+		if ($ilClientIniFile instanceof ilIniFile && $ilClientIniFile->readVariable('file_access', 'disable_ascii')) {
 			$this->setConvertFileNameToAsci(false);
 			$this->setUrlencodeFilename(false);
 		}
