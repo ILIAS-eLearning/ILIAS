f1af34b61f8347604549dd4df0fa6d438eb161a0
Nils Haagen <nhaagen@cat06.de>
2017-03-28 15:48:37 +0200

    updated example for rating input

---
+++ src/UI/examples/Input/Rating/with_byline.php
@@ -0,0 +1,24 @@
+<?php
+function with_byline() {
+	global $DIC;
+	$f = $DIC->ui()->factory();
+	$renderer = $DIC->ui()->renderer();
+
+	$captions = array(
+		'opt1',
+		'opt2',
+		'opt3',
+		'opt4',
+		'opt5'
+	);
+	$byline = 'This is a short explanation of the'
+		.' topic to be rated.'
+		.' <br>The byline <i>MAY</i> contain HTML,'
+		.' but usually <i>SHOULD</i> not.';
+
+
+	$ri = $f->input()->rating('topic', $captions)
+		->withByline($byline);
+
+	return $renderer->render($ri);
+}
