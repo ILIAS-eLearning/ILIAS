9761ec9bf0486efce0b3058da26cca310b2cf9f0
Richard Klees <richard.klees@concepts-and-training.de>
2017-04-10 09:03:32 +0200

    Validation: README and factory.

---
+++ src/Validation/Factory.php
@@ -0,0 +1,76 @@
+<?php
+/* Copyright (c) 2017 Richard Klees <richard.klees@concepts-and-training.de> Extended GPL, see docs/LICENSE */
+
+namespace ILIAS\Validation;
+
+/**
+ * Factory for creating restrictions.
+ */
+class Factory {
+	// COMBINATORS
+
+	/**
+	 * Get a restriction that sequentially checks the supplied restrictions.
+	 *
+	 * The new restriction tells the problem of the first violated restriction.
+	 *
+	 * @param   Restriction[]   $others
+	 * @return  Restriction
+	 */
+	public function seq(array $others);
+
+	/**
+	 * Get a restriction that checks the supplied restrictions in parallel.
+	 *
+	 * The new restriction tells the problems of all violated restrictions.
+	 *
+	 * @param   Restriction[]   $others
+	 * @return	Restriction
+	 */
+	public function par(array $others);
+
+	/**
+	 * Get a negated restriction.
+	 *
+	 * @param   Restriction   $other
+	 * @return  Restriction
+	 */
+	public function not(Restriction $other);
+
+	// SOME RESTRICTOINS
+
+	/**
+	 * Get a restriction for an integer.
+	 *
+	 * @return  Restriction
+	 */
+	public function isInt();
+
+	/**
+	 * Get the restriction that some value is larger than $min.
+	 *
+	 * @param   int   $min
+	 * @return  Restriction
+	 */
+	public function greaterThan($min);
+
+	/**
+	 * Get the restriction that some value is smaller then $max.
+	 *
+	 * @param	int   $max
+	 * @return  Restriction
+	 */
+	public function lessThan($max);
+
+	/**
+	 * Get a custom restriction.
+	 *
+	 * If the provided value !$is_ok will either use the $error (if it is a string)
+	 * or provide the value to the $error callback.
+	 *
+	 * @param   callable          $is_ok MUST return boolean
+	 * @param   string|callable   $error
+	 * @return  Restriction
+	 */
+	public function custom($is_ok, $error);
+}
--- src/Validation/README.md
+++ src/Validation/README.md
@@ -12,3 +12,39 @@ in the system with objects performing the validations makes it possible to build
 a set of common validations used throughout the system. Having a known set of
 validations makes it possible to perform at least some of the validations on
 client side someday.
+
+```php
+
+require_once(__DIR__."/Factory.php");
+
+$f = new Validation/Factory;
+
+// Build some basic restrictions
+$gt0 = $f->greaterThan(0);
+$lt10 = $f->lessThan(10);
+
+// Check them and react:
+if (!$gt0->appliesTo(1)) {
+	assert(false); // does not happen
+}
+
+// Let them throw an exception:
+$raised = false;
+try {
+	$lt10->check(20);
+	assert(false); // does not happen
+}
+catch (\InvalidArgumentException $e) {
+	$raised = true;
+}
+assert($raised);
+
+// Get to know what the problem with some value is:
+assert(is_string($gt0->problemWith(-10)));
+
+// Combine them in a way that the restrictions are checked one after another:
+$between_0_10 = $f->seq([$gt0, $lt10]);
+
+// Or in a way that they are checked independently:
+$also_between_0_10 = $f->par([$gt0, $lt10]);
+```
