493615cae64656430391ec6ef0a5011ad0a041c3
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2017-04-20 13:23:25 +0200

    additional assertions on isOk and isError

--- tests/Data/FactoryTests.php
+++ tests/Data/FactoryTests.php
@@ -1,7 +1,9 @@
 <?php
 /* Copyright (c) 2017 Stefan Hecken <stefan.hecken@concepts-and-training.de> Extended GPL, see docs/LICENSE */
 
-namespace ILIAS\Data;
+require_once("libs/composer/vendor/autoload.php");
+
+use ILIAS\Data;
 
 /**
  * Testing the faytory of result objects
@@ -20,10 +22,14 @@ class FactoryTests extends PHPUnit_Framework_TestCase {
 	public function testOk() {
 		$result = $this->f->ok(3.154);
 		$this->assertInstanceOf(Data\Result::class, $result);
+		$this->assertTrue($result->isOk());
+		$this->assertFalse($result->isError());
 	}
 
 	public function testError() {
 		$result = $this->f->error("Something went wrong");
 		$this->assertInstanceOf(Data\Result::class, $result);
+		$this->assertTrue($result->isError());
+		$this->assertFalse($result->isOk());
 	}
 }
