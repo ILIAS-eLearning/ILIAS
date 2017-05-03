d39f8a2c8ba26bc9a1adcf1a7998fd50476c38ed
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2017-05-03 11:42:18 +0200

    Check thrown exception message is correct

--- tests/Validation/Constraints/SequentialTest.php
+++ tests/Validation/Constraints/SequentialTest.php
@@ -42,6 +42,7 @@ class SequentialTest extends PHPUnit_Framework_TestCase {
 			$constraint->check(2);
 			$raised = false;
 		} catch (UnexpectedValueException $e) {
+			$this->assertEquals("The checked value is not greater.", $e->getMessage());
 			$raised = true;
 		}
 
