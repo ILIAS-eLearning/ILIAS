7541fd069569cc577066105ba436526d79729352
Fabian Schmid <fs@studer-raimann.ch>
2017-05-08 13:10:13 +0200

    added but ignored dev-requierments

--- libs/composer/vendor/composer/autoload_namespaces.php
+++ libs/composer/vendor/composer/autoload_namespaces.php
@@ -6,9 +6,12 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'org\\bovigo\\vfs\\' => array($vendorDir . '/mikey179/vfsStream/src/main/php'),
     'Psr\\Log\\' => array($vendorDir . '/psr/log'),
+    'Prophecy\\' => array($vendorDir . '/phpspec/prophecy/src'),
     'Pimple' => array($vendorDir . '/pimple/pimple/src'),
     'PHPExcel' => array($vendorDir . '/phpoffice/phpexcel/Classes'),
+    'Mockery' => array($vendorDir . '/mockery/mockery/library'),
     'LibRIS' => array($vendorDir . '/technosophos/LibRIS/src'),
     'HTMLPurifier' => array($vendorDir . '/ezyang/htmlpurifier/library'),
     'GetId3' => array($vendorDir . '/phansys/getid3'),
