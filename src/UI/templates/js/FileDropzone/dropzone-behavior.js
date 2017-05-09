dfaf8f3349750a34ddcce9fd8e7b97e97af12b5b
Nicolas Märchy <nm@studer-raimann.ch>
2017-05-09 14:42:15 +0200

    Add jsdoc to functions.

--- src/UI/templates/js/FileDropzone/dropzone-behavior.js
+++ src/UI/templates/js/FileDropzone/dropzone-behavior.js
@@ -1,6 +1,8 @@
 /**
- * @uthor nmaerchy <nm@studer-raimann.ch>
- * @version 0.0.1
+ * Provides functions for the dropzone highlighting.
+ *
+ * @author nmaerchy <nm@studer-raimann.ch>
+ * @version 0.0.3
  */
 
 var il = il || {};
@@ -8,14 +10,38 @@ il.UI = il.UI || {};
 (function($, UI) {
 	UI.dropzone = (function ($) {
 
-		var enableDropDesign = function (config) {
-			$("#" + config.id).addClass("modal-backdrop in");
-			$(".dropzone").addClass("dragenter");
+		var css = {
+			"default": "dz-drag-hover",
+			"darkendBackground": "dragenter"
 		};
 
+		/**
+		 * Enables highlighting for all file dropzones on the page by the passed in argument.
+		 *
+		 * @param config Object a javascript object like {"id": "", "darkendBackground": true}
+		 */
+		var enableDropDesign = function (config) {
+			if (config.darkendBackground) {
+				$("#" + config.id + "-darkend").addClass("modal-backdrop in"); // <- bootstrap classes, should not be changed
+				$(".il-file-dropzone").addClass(css.darkendBackground);
+			} else {
+				$(".il-file-dropzone").addClass(css.default);
+			}
+
+		};
+
+		/**
+		 * Disables highlighting for all file dropzone on the page by the passed in argument.
+		 *
+		 * @param config Object a javascript object like {"id": "", "darkendBackground": true}
+		 */
 		var disableDropDesign = function (config) {
-			$($("#" + config.id)).removeClass("modal-backdrop in");
-			$(".dropzone").removeClass("dragenter");
+			if (config.darkendBackground) {
+				$("#" + config.id + "-darkend").removeClass("modal-backdrop in"); // <- bootstrap classes, should not be changed
+				$(".il-file-dropzone").removeClass(css.darkendBackground);
+			} else {
+				$(".il-file-dropzone").removeClass(css.default);
+			}
 		};
 
 		return {
