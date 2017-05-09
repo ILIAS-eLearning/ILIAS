4b35cee1794c78327eb23784854726fb4a4fcd8b
Nicolas Märchy <nm@studer-raimann.ch>
2017-05-09 09:14:10 +0200

    Create unit test FileDropzoneRenderer render wrapper dropzone.

--- tests/UI/Component/FileDropzone/FileDropzoneRendererTest.php
+++ tests/UI/Component/FileDropzone/FileDropzoneRendererTest.php
@@ -13,16 +13,50 @@ require_once(__DIR__."/../../Base.php");
  */
 class FileDropzoneRendererTest extends ILIAS_UI_TestBase {
 
+	/**
+	 * A standard dropzone ----------------------------------------------------------------
+	 */
+
+	/**
+	 * should be rendered with the css class .standard and no content inside the dropzone div.
+	 */
 	public function testRenderStandardDropzone() {
 
 		$standardDropzone = new \ILIAS\UI\Implementation\Component\FileDropzone\Standard();
 
 		// setup expected objects
-		$expectedHtml = "<div id=\"id_1-darkend\"></div><div id=\"id_1\" class=\"il-file-dropzone\"></div>";
+		$expectedHtml = "<div id=\"id_1-darkend\"></div><div id=\"id_1\" class=\"il-file-dropzone standard\"></div>";
 
 		// start test
 		$html = $this->normalizeHTML(
 			$this->getDefaultRenderer()->render($standardDropzone)
+		);
+
+		$this->assertEquals($expectedHtml, $html);
+	}
+
+
+	/**
+	 * A wrapper dropzone -----------------------------------------------------------------
+	 */
+
+	/**
+	 * should be rendered with the css class .wrapper and all passed in ILIAS UI components inside the div.
+	 */
+	public function testRenderWrapperDropzone() {
+
+		// setup expected objects
+		$expectedHtml = "<div id=\"id_1-darkend\"></div><div id=\"id_1\" class=\"il-file-dropzone wrapper\"><p>Pretty smart, isn't it?</p><p>Yeah, this is really smart.</p></div>";
+
+		// start test
+		$wrapperDropzone = new \ILIAS\UI\Implementation\Component\FileDropzone\Wrapper();
+		$exampleTextQuestion = new \ILIAS\UI\Implementation\Component\Legacy\Legacy("<p>Pretty smart, isn't it?</p>");
+		$exampleTextAnswer = new \ILIAS\UI\Implementation\Component\Legacy\Legacy("<p>Yeah, this really smart.</p>");
+
+		$wrapperDropzone = $wrapperDropzone->withContent(array($exampleTextQuestion, $exampleTextAnswer));
+
+		$html = $this->normalizeHTML(
+			$this->getDefaultRenderer()->render($wrapperDropzone)
 		);
 
 		$this->assertEquals($expectedHtml, $html);
