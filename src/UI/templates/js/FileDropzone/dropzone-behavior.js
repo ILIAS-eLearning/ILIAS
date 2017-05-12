98705a61de8559f713d719a16d5785634561cc0e
Nicolas Märchy <nm@studer-raimann.ch>
2017-05-12 13:14:55 +0200

    Add javascript doc comments.

--- src/UI/templates/js/FileDropzone/dropzone-behavior.js
+++ src/UI/templates/js/FileDropzone/dropzone-behavior.js
@@ -2,7 +2,7 @@
  * Provides functions for the dropzone highlighting.
  *
  * @author nmaerchy <nm@studer-raimann.ch>
- * @version 0.0.3
+ * @version 0.0.4
  */
 
 var il = il || {};
@@ -10,14 +10,30 @@ il.UI = il.UI || {};
 (function($, UI) {
 	UI.dropzone = (function ($) {
 
-		var enableDarkendBackground = function (dropzoneId) {
-			$("#" + dropzoneId + "-darkend").addClass("modal-backdrop in"); // <- bootstrap classes, should not be changed
-			$(".il-file-dropzone").addClass("darkend-drag-enter");
+		var css = {
+			"darkendBackground": "modal-backdrop in", // <- bootstrap classes, should not be changed
+			"darkendDragEnter": "darkend-drag-enter"
 		};
 
+		/**
+		 * Enables the darkend background and highlights all file dropzones on the page.
+		 *
+		 * @param {string} dropzoneId the html id of the dropzone
+		 */
+		var enableDarkendBackground = function (dropzoneId) {
+			$("#" + dropzoneId + "-darkend").addClass(css.darkendBackground);
+			$(".il-file-dropzone").addClass(css.darkendDragEnter);
+		};
+
+		/**
+		 *
+		 * Disables the darkend background and removes the highlighting of all file dropzones on the page.
+		 *
+		 * @param {string} dropzoneId the html id of the dropzone
+		 */
 		var disableDarkendBackground = function (dropzoneId) {
-			$("#" + dropzoneId + "-darkend").removeClass("modal-backdrop in"); // <- bootstrap classes, should not be changed
-			$(".il-file-dropzone").removeClass("darkend-drag-enter");
+			$("#" + dropzoneId + "-darkend").removeClass(css.darkendBackground);
+			$(".il-file-dropzone").removeClass(css.darkendDragEnter);
 		};
 
 		return {
