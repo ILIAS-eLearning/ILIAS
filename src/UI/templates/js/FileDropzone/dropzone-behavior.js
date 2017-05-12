eac34e60bd0c83f879d888af889603b12377c3d3
Nicolas Märchy <nm@studer-raimann.ch>
2017-05-12 11:04:24 +0200

    Add javascript functions to enable darkened Design for a dropzone.

--- src/UI/templates/js/FileDropzone/dropzone-behavior.js
+++ src/UI/templates/js/FileDropzone/dropzone-behavior.js
@@ -10,43 +10,19 @@ il.UI = il.UI || {};
 (function($, UI) {
 	UI.dropzone = (function ($) {
 
-		var css = {
-			"default": "dz-drag-hover",
-			"darkendBackground": "dragenter"
+		var enableDarkendBackground = function (dropzoneId) {
+			$("#" + dropzoneId + "-darkend").addClass("modal-backdrop in"); // <- bootstrap classes, should not be changed
+			$(".il-file-dropzone").addClass("darkend-drag-enter");
 		};
 
-		/**
-		 * Enables highlighting for all file dropzones on the page by the passed in argument.
-		 *
-		 * @param config Object a javascript object like {"id": "", "darkendBackground": true}
-		 */
-		var enableDropDesign = function (config) {
-			if (config.darkendBackground) {
-				$("#" + config.id + "-darkend").addClass("modal-backdrop in"); // <- bootstrap classes, should not be changed
-				$(".il-file-dropzone").addClass(css.darkendBackground);
-			} else {
-				$(".il-file-dropzone").addClass(css.default);
-			}
-
-		};
-
-		/**
-		 * Disables highlighting for all file dropzone on the page by the passed in argument.
-		 *
-		 * @param config Object a javascript object like {"id": "", "darkendBackground": true}
-		 */
-		var disableDropDesign = function (config) {
-			if (config.darkendBackground) {
-				$("#" + config.id + "-darkend").removeClass("modal-backdrop in"); // <- bootstrap classes, should not be changed
-				$(".il-file-dropzone").removeClass(css.darkendBackground);
-			} else {
-				$(".il-file-dropzone").removeClass(css.default);
-			}
+		var disableDarkendBackground = function (dropzoneId) {
+			$("#" + dropzoneId + "-darkend").removeClass("modal-backdrop in"); // <- bootstrap classes, should not be changed
+			$(".il-file-dropzone").removeClass("darkend-drag-enter");
 		};
 
 		return {
-			enableDropDesign: enableDropDesign,
-			disableDropDesign: disableDropDesign
+			enableDarkendBackground: enableDarkendBackground,
+			disableDarkendBackground: disableDarkendBackground
 		};
 	})($);
 })($, il.UI);
