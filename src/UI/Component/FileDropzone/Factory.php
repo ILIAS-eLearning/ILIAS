40fb58e41db06e517aea32020fd72496c4829e20
Nicolas Märchy <nm@studer-raimann.ch>
2017-05-16 11:16:11 +0200

    Update php doc comments for wrapper and standard drop zone.

--- src/UI/Component/FileDropzone/Factory.php
+++ src/UI/Component/FileDropzone/Factory.php
@@ -27,13 +27,14 @@ interface Factory {
 	 *      The standard dropzone highlights with a bright yellow on drag enter.
 	 *   effect: >
 	 *      If the darkend background is set to true, every FileDropzone on the page will be highlighted.
-	 *      On file drop event, this dropzones triggers a signal with the event data.
+	 *      On file drop event, this dropzones triggers all registered signals with the event data.
 	 *   rivals:
 	 *     Rival 1: A wrapper dropzone can hold other ILIAS UI components instead of a message.
 	 *
 	 * rules:
 	 *   usage:
-	 *     1: Most pages should not have more than one standard dropzone.
+	 *     1: Most pages should not have a standard dropzone.
+	 *     2: A page with a standard dropzone should not contain more than one of them.
 	 *   interaction:
 	 *     1: A user drops a file into the dropzone area to trigger a signal.
 	 *     2: Any file dropped from a user will not be uploaded through this dropzone.
@@ -54,9 +55,12 @@ interface Factory {
 	 *   purpose: >
 	 *      The wrapper dropzone is used to display other ILIAS UI components
 	 *      inside the dropzone.
-	 *   composition: The wrapper dropzone uses the darkend background by default.
+	 *   composition: >
+	 *      The wrapper dropzone uses the darkend background by default and is not visible before the drag enter event.
 	 *   effect: >
-	 *      Every FileDropzone on the page will be highlighted on dragenter by the user.
+	 *      Every FileDropzone on the page will be highlighted on dragenter on the html document by the user.
+	 *      If a page contains two or more wrapper dropzones, the setting for the darkend background
+	 *      of the last rendered dropzone will be used.
 	 *   rivals:
 	 *     Rival 1: A standard dropzone can display a message instead of other ILIAS UI components.
 	 *
@@ -71,15 +75,16 @@ interface Factory {
 	 *     2: Any file dropped from a user will not be uploaded through this dropzone.
 	 *     3: The wrapper dropzone only listens on file drop events by a user.
 	 *   style:
-	 *     1: This dropzone does not have any padding or margin.
-	 *     2: The height and the width is determined by the components inside.
+	 *     1: This dropzone does not have any margin.
+	 *     2: This dropzone does have a 1px padding to display a possible margin on the inner elements.
+	 *     3: The height and the width is determined by the components inside.
 	 *
 	 * ---
 	 *
-	 * @param Component[]|Component $conent an array or a single instance of ILIAS UI components
+	 * @param Component[]|Component $content an array or a single instance of ILIAS UI components
 	 *
 	 * @return Wrapper
 	 */
-	public function wrapper($conent);
+	public function wrapper($content);
 
 }
