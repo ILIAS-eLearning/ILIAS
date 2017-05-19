a6ff6d1fed707c64e1ca1882b902f7ea81056ccf
Nils Haagen <nhaagen@cat06.de>
2017-05-19 16:00:32 +0200

    revision, added rivals

--- src/UI/Component/Layout/Page/Factory.php
+++ src/UI/Component/Layout/Page/Factory.php
@@ -9,16 +9,16 @@ interface Factory {
      * ---
      * description:
      *   purpose: >
-     *     The Notification Center concentrates the visualization of any
-     *     push-notification into one expandable glyph.
+     *     The Notification Center concentrates the visualization of _any_
+     *     event-notification into one expandable glyph.
      *     This unification removes the multitude of dedicated notification-icons
      *     in favor of visual cleanliness as well as providing a designated
      *     location for further extensions.
      *
      *   composition: >
      *      The Notification Center is visualized as a glyph with a counter.
-     *      Clicked, a list with the notifying services and their respective
-     *      counter-glyphs is expanded.
+     *      Clicked, a pop-over list with the notifying services and their
+     *      respective counter-glyphs is expanded.
      *
      *   effect: >
      *      All notifications, regardless of their origin, are summed up in
@@ -26,11 +26,15 @@ interface Factory {
      *      When clicked, a list is shown with all notifying services.
      *      The entries each consist of the services' respective glyph, counter
      *      and title.
-     *      Entries as well can be clicked; the user is then redirected to the
+     *      Entries as well can be clicked; the user is then directed to the
      *      view of the service.
      *
-     *   rivals: >
-     *
+     *   rivals:
+     *     Awareness Tool: >
+     *        As the Awareness Tool does not actually target potentially
+     *        permanent notifications to the user and offers further functionality
+     *        when an entry is expanded, it cannot be properly enclosed in the
+     *        Notification Center and should reside next to it.
      *
      * rules:
      *   usage:
@@ -42,7 +46,7 @@ interface Factory {
      *
      * @return  \ILIAS\UI\Component\Layout\Page\NotificationCenter
      */
-    public function notificationcenter();
+    public function notificationCenter();
 
 
 }
