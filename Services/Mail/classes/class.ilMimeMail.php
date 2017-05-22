3b60a60bbbb4cc51e7fa502b5f645e8b74117ac8
mjansen <mjansen@databay.de>
2017-05-22 14:33:40 +0200

    Mail: Feature SMTP-Settings-Page

--- Services/Mail/classes/class.ilMimeMail.php
+++ Services/Mail/classes/class.ilMimeMail.php
@@ -383,7 +383,7 @@ class ilMimeMail
 			$skin = $ilClientIniFile->readVariable('layout', 'skin');
 
 			$bracket_path = './Services/Mail/templates/default/tpl.html_mail_template.html';
-			if($skin != 'delos')
+			if($skin != 'default')
 			{
 				$tplpath = './Customizing/global/skin/' . $skin . '/Services/Mail/tpl.html_mail_template.html';
 
@@ -409,7 +409,7 @@ class ilMimeMail
 			$mail->Body    = str_replace( '{PLACEHOLDER}', ilUtil::makeClickable( $this->body ), $bracket );
 
 			$directory = './Services/Mail/templates/default/img/';
-			if($skin != 'delos')
+			if($skin != 'default')
 			{
 				$directory = './Customizing/global/skin/' . $skin . '/Services/Mail/img/';
 			}
