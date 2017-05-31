a576e109e695edd1f4493f73a104d9fe199345c0
Alex Killing <alex.killing@gmx.de>
2017-05-31 12:14:32 +0200

    split buttons, proposal amstutz, i

---
+++ src/UI/Component/Button/Split/Month.php
@@ -0,0 +1,27 @@
+<?php
+
+/* Copyright (c) 2016 Timon Amstutz <timon.amstutz@ilub.unibe.ch> Extended GPL, see docs/LICENSE */
+
+namespace ILIAS\UI\Component\Button\Split;
+
+use ILIAS\UI\Component\JavaScriptBindable;
+
+/**
+ * This describes the month split Button. Note that actions are bound to the month by using the JavaScriptBindable
+ * withOnLoadCodeFunction
+ *
+ * @example:
+ * 		$month_picker->withOnLoadCode(function($id) {
+ *			return "$( "#$id someMonthSelector" ).bind( "click", function() {
+				someActionAfterClickedOnSpecificMonth;
+			});";
+ *		});
+ */
+interface Month extends Split, JavaScriptBindable {
+	/**
+	 * Get the month shown as default
+	 *
+	 * @return	string
+	 */
+	public function getDefaultMonth();
+}
