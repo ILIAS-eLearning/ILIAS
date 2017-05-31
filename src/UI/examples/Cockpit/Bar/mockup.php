22ac2b44def3cf213e1b207804b457ae4e798e11
Nils Haagen <nhaagen@cat06.de>
2017-05-31 10:51:03 +0200

    examples for mobile and widescreen

--- src/UI/examples/Cockpit/Bar/mockup.php
+++ src/UI/examples/Cockpit/Bar/mockup.php
@@ -6,8 +6,24 @@ function mockup() {
 	global $DIC;
 	$f = $DIC->ui()->factory();
 	$renderer = $DIC->ui()->renderer();
+
+	$buf = array();
+
 	$image = $f->image()->standard(
 			"src/UI/examples/Cockpit/Bar/bar_closed.png",
 			"");
-	return $renderer->render($image);
+	array_push($buf, $renderer->render($image));
+
+	$image = $f->image()->standard(
+			"src/UI/examples/Cockpit/Bar/bar_mobile.png",
+			"");
+	array_push($buf, $renderer->render($image));
+
+	$image = $f->image()->standard(
+			"src/UI/examples/Cockpit/Bar/bar_widescreen.png",
+			"");
+	array_push($buf, $renderer->render($image));
+
+
+	return implode('<br><hr><br>', $buf);
 }
