677a5b15a1ed9c9600977250bdd78c7b238a9e13
Fabian Schmid <fs@studer-raimann.ch>
2017-05-31 16:48:28 +0200

    [FIX] Implemented activation of users via shibboleth

--- Services/AuthShibboleth/classes/User/class.shibUser.php
+++ Services/AuthShibboleth/classes/User/class.shibUser.php
@@ -125,6 +125,19 @@ class shibUser extends ilObjUser {
 
 
 	public function create() {
+		$c = shibConfig::getInstance();
+		if($c->isActivateNew()) {
+			$this->setActive(false);
+			require_once('./Services/Registration/classes/class.ilRegistrationMailNotification.php');
+			require_once('./Services/Registration/classes/class.ilRegistrationSettings.php');
+			$ilRegistrationSettings = new ilRegistrationSettings();
+			$mail = new ilRegistrationMailNotification();
+			$mail->setType(ilRegistrationMailNotification::TYPE_NOTIFICATION_CONFIRMATION);
+			$mail->setRecipients($ilRegistrationSettings->getApproveRecipients());
+			$mail->setAdditionalInformation(array('usr' => $this));
+			$mail->send();
+		}
+
 		if ($this->getLogin() != '' AND $this->getLogin() != '.') {
 			parent::create();
 		} else {
