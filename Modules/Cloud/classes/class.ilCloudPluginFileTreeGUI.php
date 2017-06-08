1505dfe1b607f1c252ca7d9ebd09ee3e67e842c8
theodortruffer <tt@studer-raimann.ch>
2017-06-08 09:49:36 +0200

    0020722: Bad representation of file information

--- Modules/Cloud/classes/class.ilCloudPluginFileTreeGUI.php
+++ Modules/Cloud/classes/class.ilCloudPluginFileTreeGUI.php
@@ -172,7 +172,7 @@ class ilCloudPluginFileTreeGUI extends ilCloudPluginGUI {
 			}
 
 			$item->setVariable("TXT_DESC",
-				pathinfo($node->getPath(), PATHINFO_EXTENSION) . "&nbsp;&nbsp;&nbsp;" . $node->getSize() . "&nbsp;&nbsp;&nbsp;" . $modified);
+				$this->formatBytes($node->getSize()) . "&nbsp;&nbsp;&nbsp;" . $modified);
 			if ($download) {
 				$item->setVariable("TXT_TITLE_LINKED", basename($node->getPath()));
 				$item->setVariable("HREF_TITLE_LINKED", $ilCtrl->getLinkTarget($gui_class, "getFile") . "&id=" . $node->getId());
@@ -184,6 +184,42 @@ class ilCloudPluginFileTreeGUI extends ilCloudPluginGUI {
 		$this->setItemVariablePlugin($item, $node);
 
 		return $item->get();
+	}
+
+
+	/**
+	 * @param     $bytes
+	 * @param int $precision
+	 *
+	 * @return string
+	 */
+	protected function formatBytes($bytes, $precision = 2) {
+		if ($bytes >= 1073741824)
+		{
+			$bytes = number_format($bytes / 1073741824, $precision) . ' GB';
+		}
+		elseif ($bytes >= 1048576)
+		{
+			$bytes = number_format($bytes / 1048576, $precision) . ' MB';
+		}
+		elseif ($bytes >= 1024)
+		{
+			$bytes = number_format($bytes / 1024, $precision) . ' KB';
+		}
+		elseif ($bytes > 1)
+		{
+			$bytes = $bytes . ' bytes';
+		}
+		elseif ($bytes == 1)
+		{
+			$bytes = $bytes . ' byte';
+		}
+		else
+		{
+			$bytes = '0 bytes';
+		}
+
+		return $bytes;
 	}
 
 
