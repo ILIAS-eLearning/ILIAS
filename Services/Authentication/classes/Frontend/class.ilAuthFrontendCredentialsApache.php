abae873202dd20c86c8e67b5d5b7223549a4ab98
mjansen <mjansen@databay.de>
2017-06-12 10:30:38 +0200

    Auth: Fixed #20685
    (cherry picked from commit 866c8e4)

--- Services/Authentication/classes/Frontend/class.ilAuthFrontendCredentialsApache.php
+++ Services/Authentication/classes/Frontend/class.ilAuthFrontendCredentialsApache.php
@@ -1,5 +1,4 @@
 <?php
-
 /* Copyright (c) 1998-2010 ILIAS open source, Extended GPL, see docs/LICENSE */
 
 include_once './Services/Authentication/classes/Frontend/class.ilAuthFrontendCredentials.php';
@@ -14,7 +13,6 @@ include_once './Services/Authentication/interfaces/interface.ilAuthCredentials.p
 class ilAuthFrontendCredentialsApache extends ilAuthFrontendCredentials implements ilAuthCredentials
 {
 	private $settings = null;
-	
 
 	/**
 	 * Constructor
@@ -26,8 +24,7 @@ class ilAuthFrontendCredentialsApache extends ilAuthFrontendCredentials implemen
 		include_once './Services/Administration/classes/class.ilSetting.php';
 		$this->settings = new ilSetting('apache_auth');
 	}
-	
-	
+
 	/**
 	 * @return \ilSetting
 	 */
@@ -35,31 +32,88 @@ class ilAuthFrontendCredentialsApache extends ilAuthFrontendCredentials implemen
 	{
 		return $this->settings;
 	}
-	
+
 	/**
 	 * Init credentials from request
 	 */
 	public function initFromRequest()
 	{
 		$this->getLogger()->dump($_SERVER, ilLogLevel::DEBUG);
-		$this->getLogger()->debug($this->getSettings()->get('apache_auth_username_direct_mapping_fieldname',''));
-		
+		$this->getLogger()->debug($this->getSettings()->get('apache_auth_username_direct_mapping_fieldname', ''));
+
 		include_once './Services/AuthApache/classes/class.ilAuthProviderApache.php';
-		
+
 		switch($this->getSettings()->get('apache_auth_username_config_type'))
 		{
 			case ilAuthProviderApache::APACHE_AUTH_TYPE_DIRECT_MAPPING:
 				if(array_key_exists($this->getSettings()->get('apache_auth_username_direct_mapping_fieldname'), $_SERVER))
 				{
-					$this->setUsername($_SERVER[$this->getSettings()->get('apache_auth_username_direct_mapping_fieldname','')]);
+					$this->setUsername($_SERVER[$this->getSettings()->get('apache_auth_username_direct_mapping_fieldname', '')]);
 				}
 				break;
-				
+
 			case ilAuthProviderApache::APACHE_AUTH_TYPE_BY_FUNCTION:
 				include_once 'Services/AuthApache/classes/custom_username_func.php';
 				$this->setUsername(ApacheCustom::getUsername());
 				break;
 		}
 	}
+
+	/**
+	 * @return bool
+	 */
+	public function hasValidTargetUrl()
+	{
+		if(!isset($_GET['r']) || 0 == strlen(trim($_GET['r'])))
+		{
+			return false;
+		}
+
+		$url = trim($_GET['r']);
+
+		$validDomains = array();
+		$path         = ILIAS_DATA_DIR . '/' . CLIENT_ID . '/apache_auth_allowed_domains.txt';
+		if(file_exists($path) && is_readable($path))
+		{
+			foreach(file($path) as $line)
+			{
+				if(trim($line))
+				{
+					$validDomains[] = trim($line);
+				}
+			}
+		}
+
+		$urlParts       = parse_url($url);
+		$redirectDomain = $urlParts['host'];
+
+		$validRedirect = false;
+		foreach($validDomains as $validDomain)
+		{
+			if($redirectDomain === $validDomain)
+			{
+				$validRedirect = true;
+				break;
+			}
+
+			if(strlen($redirectDomain) > (strlen($validDomain) + 1))
+			{
+				if(substr($redirectDomain, (0 - strlen($validDomain) - 1)) === '.' . $validDomain)
+				{
+					$validRedirect = true;
+					break;
+				}
+			}
+		}
+
+		return $validRedirect;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getTargetUrl()
+	{
+		return ilUtil::appendUrlParameterString(trim($_GET['r']), 'passed_sso=1');
+	}
 }
-?>
