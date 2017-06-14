47cf10ee0db3d8ac3d1261a36d9d2a841efef74f
smeyer-ilias <smeyer.ilias@gmx.de>
2017-06-14 17:08:04 +0200

    Added db update for session role

--- setup/sql/dbupdate_custom.php
+++ setup/sql/dbupdate_custom.php
@@ -12,3 +12,80 @@ ilDBUpdateNewObjectType::addRBACTemplate(
 	]
 );
 ?>
+<#2>
+<?php
+
+// add new role entry for each session
+$query = 'SELECT obd.obj_id,ref_id,owner  FROM object_data obd '.
+	'join object_reference obr on obd.obj_id = obr.obj_id'.' '.
+	'where type = '.$ilDB->quote('sess','text');
+$res = $ilDB->query($query);
+while($row = $res->fetchRow(ilDBConstants::FETCHMODE_OBJECT))
+{
+	// add role entry
+	$id = $ilDB->nextId("object_data");
+	$q = "INSERT INTO object_data ".
+		"(obj_id,type,title,description,owner,create_date,last_update) ".
+		"VALUES ".
+		"(".
+		 $ilDB->quote($id, "integer").",".
+		 $ilDB->quote('role', "text").",".
+		 $ilDB->quote('il_sess_participant_'.$row->ref_id, "text").",".
+		 $ilDB->quote('Participant of session obj_no.'.$row->obj_id, "text").",".
+		 $ilDB->quote($row->owner, "integer").",".
+		 $ilDB->now().",".
+		 $ilDB->now().")";
+
+	$ilDB->manipulate($q);
+	
+	// add role data
+	$rd = 'INSERT INTO role_data (role_id) VALUES ('.$id.')';
+	$ilDB->manipulate($rd);
+	
+	// assign to session
+	$fa = 'INSERT INTO rbac_fa (rol_id,parent,assign,protected,blocked ) VALUES('.
+		$ilDB->quote($id,'integer').', '.
+		$ilDB->quote($row->ref_id,'integer').', '.
+		$ilDB->quote('y','text').', '.
+		$ilDB->quote('n','text').', '.
+		$ilDB->quote(0,'integer').' '.
+		')';
+
+	$ilDB->manipulate($fa);
+	
+	// assign template permissions
+	$temp = 'INSERT INTO rbac_templates (rol_id,type,ops_id,parent) VALUES('.
+		$ilDB->quote($id,'integer').', '.
+		$ilDB->quote('sess','text').', '.
+		$ilDB->quote(2,'integer').', '.
+		$ilDB->quote($row->ref_id,'integer').') ';
+	$ilDB->manipulate($temp);
+	
+	// assign template permissions
+	$temp = 'INSERT INTO rbac_templates (rol_id,type,ops_id,parent) VALUES('.
+		$ilDB->quote($id,'integer').', '.
+		$ilDB->quote('sess','text').', '.
+		$ilDB->quote(3,'integer').', '.
+		$ilDB->quote($row->ref_id,'integer').') ';
+	$ilDB->manipulate($temp);
+	
+	// assign permission
+	$pa = 'INSERT INTO rbac_pa (rol_id,ops_id,ref_id) VALUES('.
+		$ilDB->quote($id,'integer').', '.
+		$ilDB->quote(serialize([2,3]),'text').', '.
+		$ilDB->quote($row->ref_id,'integer').')';
+	$ilDB->manipulate($pa);
+	
+	// assign users
+	$users = 'SELECT usr_id from event_participants WHERE event_id = '.$ilDB->quote($row->obj_id,'integer');
+	$user_res = $ilDB->query($users);
+	while($user_row = $user_res->fetchRow(ilDBConstants::FETCHMODE_OBJECT))
+	{
+		$ua = 'INSERT INTO rbac_ua (usr_id,rol_id) VALUES('.
+			$ilDB->quote($user_row->usr_id,'integer').', '.
+			$ilDB->quote($id,'integer').')';
+		$ilDB->manipulate($ua);
+	}
+		
+}
+?>
