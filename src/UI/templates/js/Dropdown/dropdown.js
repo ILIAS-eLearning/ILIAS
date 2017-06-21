360bedfa9b6749a0b669ae4d299f8ffe704c78e5
Alex Killing <alex.killing@gmx.de>
2017-06-21 23:18:46 +0200

    fixed focus

--- src/UI/templates/js/Dropdown/dropdown.js
+++ src/UI/templates/js/Dropdown/dropdown.js
@@ -3,14 +3,9 @@
 		var dropdown = $(event.target);
 
 		dropdown.find('.dropdown-toggle').attr('aria-expanded', true);
-
-		// focus first link
-		setTimeout(function() {
-			dropdown.find('.dropdown-menu li:first-child a').focus();
-		}, 10);
 	});
 
-	// On dropdown close
+	// on close
 	$(document).on('hidden.bs.dropdown', function(event) {
 		var dropdown = $(event.target);
 
