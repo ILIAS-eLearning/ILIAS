8a070aff15f2f2589d5337313007ae7e47ae6fac
Alex Killing <alex.killing@gmx.de>
2017-06-21 11:20:20 +0200

    added tests

--- tests/UI/Component/Button/ButtonFactoryTest.php
+++ tests/UI/Component/Button/ButtonFactoryTest.php
@@ -6,6 +6,7 @@ class ButtonFactoryTest extends AbstractFactoryTest {
 	public $kitchensink_info_settings = array
 		( "standard"	=> array("context" => false)
 		, "close"		=> array("context" => false)
+		, "shy"			=> array("context" => false)
 		);
 
 	public $factory_title = 'ILIAS\\UI\\Component\\Button\\Factory';
