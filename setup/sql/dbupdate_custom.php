f3a9a94cfcfe9decdddb8506f3ca11da46566832
smeyer-ilias <smeyer.ilias@gmx.de>
2017-06-21 12:25:59 +0200

    Added Dtpl closed session

--- setup/sql/dbupdate_custom.php
+++ setup/sql/dbupdate_custom.php
@@ -89,3 +89,82 @@ while($row = $res->fetchRow(ilDBConstants::FETCHMODE_OBJECT))
 		
 }
 ?>
+<#3>
+<?php
+$id = $ilDB->nextId("object_data");
+$q = "INSERT INTO object_data ".
+	"(obj_id,type,title,description,owner,create_date,last_update) ".
+	"VALUES ".
+	"(".
+	 $ilDB->quote($id, "integer").",".
+	 $ilDB->quote('rolt', "text").",".
+	 $ilDB->quote('il_sess_status_closed', "text").",".
+	 $ilDB->quote('Closed session template','text').', '.
+	 $ilDB->quote(0, "integer").",".
+	 $ilDB->now().",".
+	 $ilDB->now().")";
+
+$ilDB->manipulate($q);
+
+$query = "INSERT INTO rbac_fa VALUES (".$ilDB->quote($id).", 8, 'n', 'n', 0)";
+$ilDB->manipulate($query);
+	
+?>
+<#4>
+<?php
+$id = $ilDB->nextId('didactic_tpl_settings');
+$query = 'INSERT INTO didactic_tpl_settings (id,enabled,type,title, description,info,auto_generated,exclusive_tpl) values( '.
+	$ilDB->quote($id, 'integer').', '.
+	$ilDB->quote(1,'integer').', '.
+	$ilDB->quote(1,'integer').', '.
+	$ilDB->quote('sess_closed','text').', '.
+	$ilDB->quote('sess_closed_info','text').', '.
+	$ilDB->quote('','text').', '.
+	$ilDB->quote(1,'integer').', '.
+	$ilDB->quote(0,'integer').' '.
+	')';
+$ilDB->manipulate($query);
+
+$query = 'INSERT INTO didactic_tpl_sa (id, obj_type) values( '.
+	$ilDB->quote($id, 'integer').', '.
+	$ilDB->quote('sess','text').
+	')';
+$ilDB->manipulate($query);
+
+
+$aid = $ilDB->nextId('didactic_tpl_a');
+$query = 'INSERT INTO didactic_tpl_a (id, tpl_id, type_id) values( '.
+	$ilDB->quote($aid, 'integer').', '.
+	$ilDB->quote($id, 'integer').', '.
+	$ilDB->quote(1,'integer').
+	')';
+$ilDB->manipulate($query);
+
+$query = 'select obj_id from object_data where type = '.$ilDB->quote('rolt','text').' and title = '.$ilDB->quote('il_sess_status_closed','text');
+$res = $ilDB->query($query);
+while($row = $res->fetchRow(ilDBConstants::FETCHMODE_OBJECT))
+{
+	$closed_id = $row->obj_id;
+}
+
+$query = 'INSERT INTO didactic_tpl_alp (action_id, filter_type, template_type, template_id) values( '.
+	$ilDB->quote($aid, 'integer').', '.
+	$ilDB->quote(3, 'integer').', '.
+	$ilDB->quote(2,'integer').', '.
+	$ilDB->quote($closed_id,'integer').
+	')';
+$ilDB->manipulate($query);
+
+
+$fid = $ilDB->nextId('didactic_tpl_fp');
+$query = 'INSERT INTO didactic_tpl_fp (pattern_id, pattern_type, pattern_sub_type, pattern, parent_id, parent_type ) values( '.
+	$ilDB->quote($fid, 'integer').', '.
+	$ilDB->quote(1, 'integer').', '.
+	$ilDB->quote(1,'integer').', '.
+	$ilDB->quote('.*','text').', '.
+	$ilDB->quote($aid,'integer').', '.
+	$ilDB->quote('action','text').
+	')';
+$ilDB->manipulate($query);
+
+?>
