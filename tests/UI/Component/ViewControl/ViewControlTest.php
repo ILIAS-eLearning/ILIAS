eb2c9a6836e64f7cbec9a00083e73742548f20ba
xus <xus206@hotmail.com>
2017-06-22 16:56:22 +0200

    unit tests with buttons instead of data links

--- tests/UI/Component/ViewControl/ViewControlTest.php
+++ tests/UI/Component/ViewControl/ViewControlTest.php
@@ -16,14 +16,13 @@ class ViewControlTest extends ILIAS_UI_TestBase
 	{
 		$view_control_f = $this->getViewControlFactory();
 		$button_f = new ILIAS\UI\Implementation\Component\Button\Factory();
-		$data_f = new ILIAS\Data\Factory();
 
-		$back = $data_f->link("", "http://www.ilias.de");
-		$next = $data_f->link("", "http://www.github.com");
+		$back = $button_f->standard("", "http://www.ilias.de");
+		$next = $button_f->standard("", "http://www.github.com");
 		$button = $button_f->standard("Today", "");
 
-		$this->assertInstanceOf("ILIAS\\Data\\Link\\Link", $back);
-		$this->assertInstanceOf("ILIAS\\Data\\Link\\Link", $next);
+		$this->assertInstanceOf("ILIAS\\UI\\Component\\Button\\Button", $back);
+		$this->assertInstanceOf("ILIAS\\UI\\Component\\Button\\Button", $next);
 		$this->assertInstanceOf("ILIAS\\UI\\Component\\Button\\Button", $button);
 		$this->assertInstanceOf("ILIAS\\UI\\Component\\ViewControl\\Factory", $view_control_f);
 
@@ -34,43 +33,39 @@ class ViewControlTest extends ILIAS_UI_TestBase
 
 	public function test_viewcontrol_section_get_previous_actions()
 	{
-		$data_f = new ILIAS\Data\Factory();
 		$button_f = new ILIAS\UI\Implementation\Component\Button\Factory();
 
-
-		$back = $data_f->link("", "http://www.ilias.de");
-		$next = $data_f->link("", "http://www.github.com");
+		$back = $button_f->standard("", "http://www.ilias.de");
+		$next = $button_f->standard("", "http://www.github.com");
 		$button = $button_f->standard("Today", "");
 
 		$action = $this->getViewControlFactory()->section($back,$button,$next)->getPreviousActions();
 
-		$this->assertInstanceOf("\\ILIAS\\Data\\Link\\Link",$action);
+		$this->assertInstanceOf("ILIAS\\UI\\Component\\Button\\Button",$action);
 	}
 
 	public function test_viewcontrol_section_get_next_actions()
 	{
-		$data_f = new ILIAS\Data\Factory();
 		$button_f = new ILIAS\UI\Implementation\Component\Button\Factory();
 
-		$back = $data_f->link("", "http://www.ilias.de");
-		$next = $data_f->link("", "http://www.github.com");
+		$back = $button_f->standard("", "http://www.ilias.de");
+		$next = $button_f->standard("", "http://www.github.com");
 		$button = $button_f->standard("Today", "");
 
 		$action = $this->getViewControlFactory()->section($back,$button,$next)->getNextActions();
 
-		$this->assertInstanceOf("\\ILIAS\\Data\\Link\\Link",$action);
+		$this->assertInstanceOf("ILIAS\\UI\\Component\\Button\\Button",$action);
 	}
 
 	public function test_render_viewcontrol_section()
 	{
 		$view_control_f = $this->getViewControlFactory();
-		$data_f = new ILIAS\Data\Factory();
 		$button_f = new ILIAS\UI\Implementation\Component\Button\Factory();
 
 		$r = $this->getDefaultRenderer();
 
-		$back = $data_f->link("", "http://www.ilias.de");
-		$next = $data_f->link("", "http://www.github.com");
+		$back = $button_f->standard("", "http://www.ilias.de");
+		$next = $button_f->standard("", "http://www.github.com");
 		$button = $button_f->standard("Today", "");
 
 		$section = $view_control_f->section($back,$button,$next);
