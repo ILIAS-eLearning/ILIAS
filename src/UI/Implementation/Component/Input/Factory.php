062553fbc6801dcbd98125e7feaa8382b4668eea
Richard Klees <richard.klees@concepts-and-training.de>
2017-07-06 15:03:30 +0200

    UI: instantiate text field correctly.

--- src/UI/Implementation/Component/Input/Factory.php
+++ src/UI/Implementation/Component/Input/Factory.php
@@ -5,12 +5,23 @@
 namespace ILIAS\UI\Implementation\Component\Input;
 
 use ILIAS\UI\Component\Input as I;
+use ILIAS\Data;
 
 class Factory implements I\Factory {
+	/**
+ 	 * @var	Data\Factory
+	 */
+	protected $data_factory;
+
+	public function __construct() {
+		// TODO: This is not too good. Maybe we should give a DIC container.
+		$this->data_factory = new Data\Factory;
+	}
+
 	/**
 	 * @inheritdoc
 	 */
 	public function text($label, $byline = null) {
-		return new Text($label, $byline);
+		return new Text($this->data_factory, $label, $byline);
 	}
 }
