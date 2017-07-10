1ca788adf37c7af0ec2ac9f14057c617d71e43ec
Alex Killing <alex.killing@gmx.de>
2017-07-10 17:13:52 +0200

    ui panel: add optional action dropdowns for panels

--- tests/UI/Component/Panel/PanelTest.php
+++ tests/UI/Component/Panel/PanelTest.php
@@ -65,6 +65,37 @@ class PanelTest extends ILIAS_UI_TestBase {
 		$this->assertEquals($p->getContent(), array($c));
 	}
 
+	public function test_standard_with_actions() {
+		$fp = $this->getPanelFactory();
+		$f = $this->getFactory();
+
+		$p = $fp->standard("Title",array(new ComponentDummy()));
+
+		$actions = $f->dropdown()->standard(array(
+			$f->button()->shy("ILIAS", "https://www.ilias.de"),
+			$f->button()->shy("GitHub", "https://www.github.com")
+		));
+
+		$p = $p->withActions($actions);
+
+		$this->assertEquals($p->getActions(), $actions);
+	}
+
+	public function test_sub_with_actions() {
+		$fp = $this->getPanelFactory();
+		$f = $this->getFactory();
+
+		$p = $fp->sub("Title",array(new ComponentDummy()));
+
+		$actions = $f->dropdown()->standard(array(
+			$f->button()->shy("ILIAS", "https://www.ilias.de"),
+			$f->button()->shy("GitHub", "https://www.github.com")
+		));
+
+		$p = $p->withActions($actions);
+
+		$this->assertEquals($p->getActions(), $actions);
+	}
 
 	public function test_sub_with_card() {
 		$fp = $this->getPanelFactory();
@@ -97,28 +128,32 @@ class PanelTest extends ILIAS_UI_TestBase {
 
 
 	public function test_render_standard() {
-		$f = $this->getPanelFactory();
+		$f = $this->getFactory();
 		$r = $this->getDefaultRenderer();
-		$p = $f->standard("Title",array());
 
-		$html = new DOMDocument();
-		$html->formatOutput = true;
-		$html->preserveWhiteSpace = false;
+		$actions = $f->dropdown()->standard(array(
+			$f->button()->shy("ILIAS", "https://www.ilias.de"),
+			$f->button()->shy("GitHub", "https://www.github.com")
+		));
 
-		$expected = new DOMDocument();
-		$expected->formatOutput = true;
-		$expected->preserveWhiteSpace = false;
+		$p = $f->panel()->standard("Title",array())->withActions($actions);
 
 		$html = $r->render($p);
 
-		$expected_html =
-				"<div class=\"panel panel-primary\">".
-				"   <div class=\"panel-heading ilHeader\">".
-				"       <h3 class=\"ilHeader\">Title</h3>".
-				"   </div>".
-				"   <div class=\"panel-body\"></div>".
-				"</div>";
-
+		$expected_html = <<<EOT
+<div class="panel panel-primary">
+	<div class="panel-heading ilHeader clearfix">
+		<h3 class="ilHeader">Title</h3>
+		<div class="dropdown"><button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown"  aria-haspopup="true" aria-expanded="false"> <span class="caret"></span></button>
+			<ul class="dropdown-menu">
+				<li><a class="btn btn-link" href="https://www.ilias.de" data-action="https://www.ilias.de">ILIAS</a></li>
+				<li><a class="btn btn-link" href="https://www.github.com" data-action="https://www.github.com">GitHub</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="panel-body"></div>
+</div>
+EOT;
 		$this->assertHTMLEquals($expected_html, $html);
 	}
 
@@ -126,23 +161,43 @@ class PanelTest extends ILIAS_UI_TestBase {
 		$f = $this->getFactory();
 		$fp = $this->getPanelFactory();
 		$r = $this->getDefaultRenderer();
-		$p = $fp->sub("Title",array());
+
+		$actions = $f->dropdown()->standard(array(
+			$f->button()->shy("ILIAS", "https://www.ilias.de"),
+			$f->button()->shy("GitHub", "https://www.github.com")
+		));
+
+		$p = $fp->sub("Title",array())->withActions($actions);
 		$card = $f->card("Card Title");
 		$p = $p->withCard($card);
 		$html = $r->render($p);
 
-		$expected_html =
-				"<div class=\"panel panel-primary\">".
-				"   <div class=\"panel-heading ilBlockHeader\">".
-				"       <h4>Title</h4>".
-				"   </div>".
-				"   <div class=\"panel-body\"><div class=\"row\">".
-				"       <div class=\"col-sm-8\"></div>".
-				"       <div class=\"col-sm-4\">".
-				"           <div class=\"il-card thumbnail\"><div class=\"card-no-highlight\"></div><div class=\"caption\"><h5 class=\"card-title\">Card Title</h5></div></div>".
-				"       </div>".
-				"   </div></div>".
-				"</div>";
+		$expected_html = <<<EOT
+<div class="panel panel-primary">
+	<div class="panel-heading ilBlockHeader clearfix">
+		<h4>Title</h4>
+		<div class="dropdown"><button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown"  aria-haspopup="true" aria-expanded="false"> <span class="caret"></span></button>
+			<ul class="dropdown-menu">
+				<li><a class="btn btn-link" href="https://www.ilias.de" data-action="https://www.ilias.de">ILIAS</a></li>
+				<li><a class="btn btn-link" href="https://www.github.com" data-action="https://www.github.com">GitHub</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="panel-body">
+		<div class="row">
+			<div class="col-sm-8"></div>
+			<div class="col-sm-4">
+				<div class="il-card thumbnail">
+					<div class="card-no-highlight"></div>
+					<div class="caption">
+						<h5 class="card-title">Card Title</h5>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+EOT;
 
 		$this->assertHTMLEquals($expected_html, $html);
 	}
@@ -165,7 +220,7 @@ class PanelTest extends ILIAS_UI_TestBase {
 				"   <div class=\"panel-body\">".
 				"
              <div class=\"panel panel-primary\">".
-				"           <div class=\"panel-heading ilBlockHeader\">".
+				"           <div class=\"panel-heading ilBlockHeader clearfix\">".
 				"               <h4>Title</h4>".
 				"           </div>".
 				"           <div class=\"panel-body\"><div class=\"row\">".
