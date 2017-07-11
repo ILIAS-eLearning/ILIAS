742cf8f5953ab4e6467e3560e5de4f8ff25509a8
Fabian Schmid <fs@studer-raimann.ch>
2017-07-11 17:29:30 +0200

    removed some strict type declarations

--- Services/WebAccessChecker/interfaces/interface.ilComponentWebAccessChecker.php
+++ Services/WebAccessChecker/interfaces/interface.ilComponentWebAccessChecker.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types=1);
+// declare(strict_types=1);
 /* Copyright (c) 1998-2009 ILIAS open source, Extended GPL, see docs/LICENSE */
 
 /**
--- Services/WebAccessChecker/interfaces/interface.ilWACCheckingClass.php
+++ Services/WebAccessChecker/interfaces/interface.ilWACCheckingClass.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types=1);
+// declare(strict_types=1);
 
 require_once('./Services/Object/classes/class.ilObject2.php');
 
