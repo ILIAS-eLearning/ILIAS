b8a6d6046de4bb87179b28f3369a33c1f11e99c3
mjansen <mjansen@databay.de>
2017-07-20 08:24:10 +0200

    Prepared trunk integration

--- setup/sql/dbupdate_04.php
+++ setup/sql/dbupdate_04.php
@@ -18772,4 +18772,31 @@ if (! $ilDB->tableExists('il_bt_value_to_task')) {
 <?php
 	$ilCtrlStructureReader->getStructure();
 ?>
+<#5092>
+<?php
+if(!$ilDB->tableColumnExists('chatroom_settings','online_status'))
+{
+	$ilDB->addTableColumn('chatroom_settings', 'online_status', array(
+		'type'    => 'integer',
+		'length'  => 1,
+		'notnull' => true,
+		'default' => 0
+	));
+}
 
+$ilDB->manipulateF("UPDATE chatroom_settings SET online_status = %s", array('integer'), array(1));
+?>
+<#5093>
+<?php
+if(!$ilDB->tableColumnExists('chatroom_bans', 'actor_id'))
+{
+	$ilDB->addTableColumn('chatroom_bans', 'actor_id',
+		array(
+			'type'    => 'integer',
+			'length'  => 4,
+			'notnull' => false,
+			'default' => null
+		)
+	);
+}
+?>
