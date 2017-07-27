f53c6165835c0e07fafbd5659a637158b73fb801
mjansen <mjansen@databay.de>
2017-07-27 11:53:03 +0200

    Mail: DIC

--- Services/Mail/classes/class.ilMailExplorer.php
+++ Services/Mail/classes/class.ilMailExplorer.php
@@ -32,7 +32,8 @@ class ilMailExplorer extends ilTreeExplorerGUI
 	{
 		global $DIC;
 
-		$this->lng = $DIC->language();
+		$this->lng  = $DIC->language();
+		$this->ctrl = $DIC->ctrl();
 
 		$this->tree = new ilTree($a_user_id);
 		$this->tree->setTableNames('mail_tree','mail_obj_data');
