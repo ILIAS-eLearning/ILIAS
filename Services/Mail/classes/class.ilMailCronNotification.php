e423f73440fb764522b41c67e1a2e78329f902e0
mjansen <mjansen@databay.de>
2017-07-27 11:28:45 +0200

    Mail: DIC

--- Services/Mail/classes/class.ilMailCronNotification.php
+++ Services/Mail/classes/class.ilMailCronNotification.php
@@ -11,6 +11,27 @@ include_once "Services/Cron/classes/class.ilCronJob.php";
  */
 class ilMailCronNotification extends ilCronJob
 {
+	/**
+	 * @var \ilLanguage
+	 */
+	protected $lng;
+
+	/**
+	 * @var \ilSetting
+	 */
+	protected $settings;
+
+	/**
+	 * ilMailCronNotification constructor.
+	 */
+	public function __construct()
+	{
+		global $DIC;
+
+		$this->lng      = $DIC->language();
+		$this->settings = $DIC->settings();
+	}
+
 	public function getId()
 	{
 		return "mail_notification";
@@ -18,16 +39,12 @@ class ilMailCronNotification extends ilCronJob
 	
 	public function getTitle()
 	{
-		global $lng;
-				
-		return $lng->txt("cron_mail_notification");
+		return $this->lng->txt("cron_mail_notification");
 	}
 	
 	public function getDescription()
 	{
-		global $lng;
-				
-		return $lng->txt("cron_mail_notification_desc");
+		return  $this->lng->txt("cron_mail_notification_desc");
 	}
 	
 	public function getDefaultScheduleType()
@@ -59,39 +76,29 @@ class ilMailCronNotification extends ilCronJob
 	{	
 		require_once 'Services/Mail/classes/class.ilMailSummaryNotification.php';
 		$msn = new ilMailSummaryNotification();
-		$msn->send();		
-	
+		$msn->send();
+
 		$result = new ilCronJobResult();
-		$result->setStatus(ilCronJobResult::STATUS_OK);		
+		$result->setStatus(ilCronJobResult::STATUS_OK);
 		return $result;
 	}
-	
+
 	public function addCustomSettingsToForm(ilPropertyFormGUI $a_form)
 	{
-		global $lng, $ilSetting;
-		
-		$cb = new ilCheckboxInputGUI($lng->txt("cron_mail_notification_message"), "mail_notification_message");
-		$cb->setInfo($lng->txt("cron_mail_notification_message_info"));	
-		$cb->setChecked($ilSetting->get("mail_notification_message"));		
+		$cb = new ilCheckboxInputGUI($this->lng->txt("cron_mail_notification_message"), "mail_notification_message");
+		$cb->setInfo($this->lng->txt("cron_mail_notification_message_info"));
+		$cb->setChecked($this->settings->get("mail_notification_message"));
 		$a_form->addItem($cb);
 	}
-	
+
 	public function saveCustomSettings(ilPropertyFormGUI $a_form)
-	{			
-		global $ilSetting;
-		
-		$ilSetting->set('mail_notification_message', $_POST['mail_notification_message'] ? 1 : 0);	
-			
+	{
+		$this->settings->set('mail_notification_message', $_POST['mail_notification_message'] ? 1 : 0);
 		return true;
 	}
-	
+
 	public function activationWasToggled($a_currently_active)
-	{		
-		global $ilSetting;
-		
-		// propagate cron-job setting to object setting
-		$ilSetting->set('mail_notification', (bool)$a_currently_active);
+	{
+		$this->settings->set('mail_notification', (bool)$a_currently_active);
 	}
 }
-
-?>
