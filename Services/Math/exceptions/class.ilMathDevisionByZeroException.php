604e00d6939584ca4d298f516a950ee903645dc6
mjansen <mjansen@databay.de>
2017-08-19 11:55:03 +0200

    Math

--- Services/Math/exceptions/class.ilMathDevisionByZeroException.php
+++ Services/Math/exceptions/class.ilMathDevisionByZeroException.php
@@ -1,5 +1,5 @@
 <?php
-/* Copyright (c) 1998-2016 ILIAS open source, Extended GPL, see docs/LICENSE */
+/* Copyright (c) 1998-2017 ILIAS open source, Extended GPL, see docs/LICENSE */
 
 require_once 'Services/Math/exceptions/class.ilMathException.php';
 
