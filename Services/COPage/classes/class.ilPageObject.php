05a0c055c8e62884a3d41826096308b229da7b24
Alex Killing <alex.killing@gmx.de>
2017-08-22 15:01:28 +0200

    feature: Update ILIAS-Internal External Links on Copying Container

--- Services/COPage/classes/class.ilPageObject.php
+++ Services/COPage/classes/class.ilPageObject.php
@@ -2440,6 +2440,8 @@ abstract class ilPageObject
 	 */
 	function handleRepositoryLinksOnCopy($a_mapping, $a_source_ref_id)
 	{
+		global $tree, $objDefinition;
+
 		$this->buildDom();
 		$this->log->debug("Handle repository links...");
 
@@ -2488,6 +2490,100 @@ abstract class ilPageObject
 			}
 		}
 		unset($xpc);
+
+		// resolve normal external links
+		$ilias_url = parse_url(ILIAS_HTTP_PATH);
+		$xpc = xpath_new_context($this->dom);
+		$path = "//ExtLink";
+		$res = xpath_eval($xpc, $path);
+		for($i = 0; $i < count($res->nodeset); $i++)
+		{
+			$href = $res->nodeset[$i]->get_attribute("Href");
+			$this->log->debug("Href: ".$href);
+
+			$url = parse_url($href);
+
+			// only handle links on same host
+			$this->log->debug("Host: ".$url["host"]);
+			if ($url["host"] != "" && $url["host"] != $ilias_url["host"])
+			{
+				continue;
+			}
+
+			// get parameters
+			$par = array();
+			foreach (explode("&", $url["query"]) as $p)
+			{
+				$p = explode("=", $p);
+				$par[$p[0]] = $p[1];
+			}
+
+			// get ref id
+			$ref_id = 0;
+			if (is_int(strpos($href, "goto.php")))
+			{
+				$t = explode("_", $par["target"]);
+				if ($objDefinition->isRBACObject($t[0]))
+				{
+					$ref_id = (int) $t[1];
+					$type = $t[0];
+				}
+			}
+			else if (is_int(strpos($href, "ilias.php")))
+			{
+				$ref_id = (int) $par["ref_id"];
+			}
+
+
+			if ($ref_id > 0)
+			{
+				if (isset($a_mapping[$ref_id]))
+				{
+					$new_ref_id = $a_mapping[$ref_id];
+					$new_href = "";
+					// we have a mapping -> replace the ID
+					if (is_int(strpos($href, "goto.php")))
+					{
+						$nt = str_replace($type."_".$ref_id, $type."_".$new_ref_id, $par["target"]);
+						$new_href = str_replace("target=".$par["target"], "target=".$nt, $href);
+					}
+					else if (is_int(strpos($href, "ilias.php")))
+					{
+						$new_href = str_replace("ref_id=".$par["ref_id"], "ref_id=".$new_ref_id, $href);
+					}
+					if ($new_href != "")
+					{
+						$this->log->debug("... ext link replace " . $href . " with " . $new_href . ".");
+						$res->nodeset[$i]->set_attribute("Href", $new_href);
+					}
+				}
+				else if ($tree->isGrandChild($a_source_ref_id, $ref_id))
+				{
+					// we have no mapping, but the linked object is child of the original node -> remove link
+					$this->log->debug("... remove ext links.");
+					if ($res->nodeset[$i]->parent_node()->node_name() == "MapArea")	// simply remove map areas
+					{
+						$parent = $res->nodeset[$i]->parent_node();
+						$parent->unlink_node($parent);
+					}
+					else	// replace link by content of the link for other internal links
+					{
+						$source_node = $res->nodeset[$i];
+						$new_node = $source_node->clone_node(true);
+						$new_node->unlink_node($new_node);
+						$childs = $new_node->child_nodes();
+						for ($j = 0; $j < count($childs); $j++)
+						{
+							$this->log->debug("... move node $j " . $childs[$j]->node_name() . " before " . $source_node->node_name());
+							$source_node->insert_before($childs[$j], $source_node);
+						}
+						$source_node->unlink_node($source_node);
+					}
+				}
+			}
+		}
+		unset($xpc);
+
 	}
 
 
