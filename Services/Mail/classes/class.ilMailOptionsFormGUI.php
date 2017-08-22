e28b2c97334e876b979b23d9458ab134be0c2d60
mjansen <mjansen@databay.de>
2017-08-22 09:30:30 +0200

    Mail: Fixed #20924

--- Services/Mail/classes/class.ilMailOptionsFormGUI.php
+++ Services/Mail/classes/class.ilMailOptionsFormGUI.php
@@ -59,7 +59,7 @@ class ilMailOptionsFormGUI extends \ilPropertyFormGUI
 		{
 			throw new \InvalidArgumentException(sprintf(
 				'Parameter $parentGui must be ilCtrl enabled by implementing executeCommand(), %s given.',
-				is_object($parentGui)? get_class($parentGui) : var_export($parentGui, 1)
+				is_object($parentGui) ? get_class($parentGui) : var_export($parentGui, 1)
 			));
 		}
 
