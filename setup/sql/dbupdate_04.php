b7dd902cbf6f7fd9cf7293304a5c7dec8708b651
Alex Killing <alex.killing@gmx.de>
2017-08-25 14:00:53 +0200

    added missing db steps

--- setup/sql/dbupdate_04.php
+++ setup/sql/dbupdate_04.php
@@ -20085,3 +20085,30 @@ if(!$ilDB->tableColumnExists('saml_idp_settings', 'entity_id'))
 	);
 }
 ?>
+<#5173>
+<?php
+if($ilDB->tableExists('cal_categories_hidden') )
+{
+	$ilDB->renameTable('cal_categories_hidden', 'cal_cat_visibility');
+	$ilDB->addTableColumn('cal_cat_visibility', 'obj_id', array(
+		"type" => "integer",
+		"length" => 4,
+		"notnull" => true,
+		"default" => 0
+	));
+	$ilDB->addTableColumn('cal_cat_visibility', 'visible', array(
+		"type" => "integer",
+		"length" => 1,
+		"notnull" => true,
+		"default" => 0
+	));
+}
+?>
+<#5174>
+<?php
+if($ilDB->tableExists('cal_cat_visibility'))
+{
+	$ilDB->dropPrimaryKey('cal_cat_visibility');
+		$ilDB->addPrimaryKey('cal_cat_visibility', array('user_id','cat_id','obj_id'));
+}
+?>
