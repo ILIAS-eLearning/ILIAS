ba5e3311e7b5f60376ae2800ca80a31890274dd0
mjansen <mjansen@databay.de>
2017-08-25 17:31:47 +0200

    Letter Avatar: Forum implementation and tiny refactoring

--- Services/Init/classes/class.ilInitialisation.php
+++ Services/Init/classes/class.ilInitialisation.php
@@ -585,6 +585,17 @@ class ilInitialisation
 	}
 
 	/**
+	 * @param \ILIAS\DI\Container $c
+	 */
+	protected static function initAvatar(\ILIAS\DI\Container $c)
+	{
+		$c["user.avatar.factory"] = function ($c) {
+			require_once 'Services/User/Avatar/classes/class.ilUserAvatarFactory.php';
+			return new ilUserAvatarFactory($c);
+		};
+	}
+
+	/**
 	 * initialise $ilSettings object and define constants
 	 * 
 	 * Used in Soap
@@ -1111,6 +1122,7 @@ class ilInitialisation
 
 		self::initSettings();
 		self::initMail($GLOBALS['DIC']);
+		self::initAvatar($GLOBALS['DIC']);
 		
 		
 		// --- needs settings	
