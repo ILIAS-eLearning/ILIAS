3b9e7f96ad081aa2262c130a240b43861fc9c59d
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2017-08-25 09:03:03 +0200

    Implementation of getFailed function

--- Services/Tracking/classes/status/class.ilLPStatusStudyProgramme.php
+++ Services/Tracking/classes/status/class.ilLPStatusStudyProgramme.php
@@ -26,25 +26,37 @@ class ilLPStatusStudyProgramme extends ilLPStatus
 	}
 	
 	static function _getCountCompleted($a_obj_id) {
-		return count($this->_getCompleted($a_obj_id));
+		return count(self::_getCompleted($a_obj_id));
 	}
 	
 	static function _getCompleted($a_obj_id)
-	{		
+	{
 		require_once("Modules/StudyProgramme/classes/class.ilObjStudyProgramme.php");
 		$prg = new ilObjStudyProgramme($a_obj_id, false);
 		return $prg->getIdsOfUsersWithCompletedProgress();
 	}
-	
+
+	static function _getFailed($a_obj_id)
+	{
+		require_once("Modules/StudyProgramme/classes/class.ilObjStudyProgramme.php");
+		$prg = new ilObjStudyProgramme($a_obj_id, false);
+		return $prg->getIdsOfUsersWithFailedProgress();
+	}
+
+	static function _getCountFailed()
+	{
+		return 0;
+	}
+
 	function determineStatus($a_obj_id, $a_user_id, $a_obj = null)
 	{
 		require_once("Modules/StudyProgramme/classes/class.ilObjStudyProgramme.php");
 		$prg = new ilObjStudyProgramme($a_obj_id, false);
 		$progresses = $prg->getProgressesOf($a_user_id);
-		
+
 		$successful = false;
 		$relevant = false;
-		
+
 		foreach ($progresses as $progress) {
 			if ($progress->isSuccessful()) {
 				$successful = true;
@@ -53,14 +65,21 @@ class ilLPStatusStudyProgramme extends ilLPStatus
 			if ($progress->isRelevant()) {
 				$relevant = true;
 			}
+			if($progress->isFailed()) {
+				$failed = true;
+			}
 		}
-		
+
 		if ($successful) {
 			return ilLPStatus::LP_STATUS_COMPLETED_NUM;
+		}
+		if($failed) {
+			return ilLPStatus::LP_STATUS_FAILED_NUM;
 		}
 		if ($relevant) {
 			return ilLPStatus::LP_STATUS_IN_PROGRESS_NUM;
 		}
+
 		return ilLPStatus::LP_STATUS_NOT_ATTEMPTED_NUM;
 	}
 }
