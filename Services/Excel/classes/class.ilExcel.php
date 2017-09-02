ab1cdc3b9883dbb44c4d994a715431ddbc1a4b94
Alex Killing <alex.killing@gmx.de>
2017-09-02 13:26:00 +0200

    use dic for globals

--- Services/Excel/classes/class.ilExcel.php
+++ Services/Excel/classes/class.ilExcel.php
@@ -12,6 +12,11 @@ include_once './libs/composer/vendor/autoload.php';
 class ilExcel
 {
 	/**
+	 * @var ilLanguage
+	 */
+	protected $lng;
+
+	/**
 	 * @var PHPExcel
 	 */
 	protected $workbook; // [PHPExcel]
@@ -31,6 +36,9 @@ class ilExcel
 	 */
 	public function __construct()
 	{
+		global $DIC;
+
+		$this->lng = $DIC->language();
 		$this->setFormat(self::FORMAT_XML);		
 		$this->workbook = new PHPExcel();	
 		$this->workbook->removeSheetByIndex(0);
@@ -153,7 +161,7 @@ class ilExcel
 	 */
 	protected function prepareValue($a_value)
 	{
-		global $lng;
+		$lng = $this->lng;
 		
 		// :TODO: does this make sense?
 		if(is_bool($a_value))
@@ -198,7 +206,7 @@ class ilExcel
 	 */
 	protected function prepareBooleanValue($a_value)
 	{
-		global $lng;
+		$lng = $this->lng;
 
 		return $a_value ? $lng->txt('yes') : $lng->txt('no');
 	}
