b1b02e71b23747ffa81db818f41c365e04b1c546
Nicolas Schäfli <ns@studer-raimann.ch>
2017-09-04 15:56:12 +0200

    ilUtil directory creation umask fixed

--- Services/Utilities/classes/class.ilUtil.php
+++ Services/Utilities/classes/class.ilUtil.php
@@ -2404,9 +2404,10 @@ class ilUtil
 			$path = ".";
 		}
 
-		// create directory with file permissions of parent directory
-		umask(0000);
-		return @mkdir($a_dir,fileperms($path));
+		//create directory with fixed permission 0755 and reset umask for the current process
+		umask(0022); //reset mask
+		$access = 0755;
+		return mkdir($a_dir, $access);
 	}
 
 
@@ -2451,7 +2452,7 @@ class ilUtil
 			}
 		}
 
-		umask(0000);
+		umask(0022); //reset umask
 		foreach ($dirs as $dirindex => $dir)
 		{
 			// starting with the longest existing path
@@ -2465,7 +2466,7 @@ class ilUtil
 						// at the end of a directory in mkdir, see Mantis #2554
 						$dir = substr($dir,0,strlen($dir)-1);
 					}
-					if (! mkdir($dir, $umask))
+					if (! mkdir($dir, 0755))
 					{
 						error_log("Can't make directory: $dir");
 						return false;
@@ -2475,11 +2476,6 @@ class ilUtil
 				{
 					error_log("$dir is not a directory");
 					return false;
-				}
-				else
-				{
-					// get umask of the last existing parent directory
-					$umask = fileperms($dir);
 				}
 			}
 		}
