724416dc9ba2c9f3d88feec88d9470f3b42c22d5
smeyer-ilias <smeyer.ilias@gmx.de>
2017-09-22 16:09:43 +0200

    0020732: Apache Authentication: Missing feature in 5.2.x -> Account migration support and account creation (regardless of migration set.);
    Conflicts:
    	Services/Authentication/classes/Frontend/class.ilAuthFrontend.php

--- Services/Authentication/classes/Frontend/class.ilAuthFrontend.php
+++ Services/Authentication/classes/Frontend/class.ilAuthFrontend.php
@@ -116,12 +116,32 @@ class ilAuthFrontend
 		}
 		
 		$user->setAuthMode(ilSession::get(static::MIG_DESIRED_AUTHMODE));
+		
+		$this->getLogger()->debug('new auth mode is: ' . ilSession::get(self::MIG_DESIRED_AUTHMODE));
+		
 		$user->setExternalAccount(ilSession::get(static::MIG_EXTERNAL_ACCOUNT));
 		$user->update();
 		
-		// @todo call provider and update account data, role assignment, ...
-		
-		return true;
+		foreach($this->getProviders() as $provider)
+		{
+			if(!$provider instanceof ilAuthProviderAccountMigrationInterface)
+			{
+				$this->logger->warning('Provider: ' . get_class($provider) .' does not support account migration.');
+				throw new InvalidArgumentException('Invalid auth provider given.');
+			}
+			$this->getCredentials()->setUsername(ilSession::get(static::MIG_EXTERNAL_ACCOUNT));
+			$provider->migrateAccount(
+				$this->getStatus(),
+				$user->getLogin()
+			);
+			switch($this->getStatus()->getStatus())
+			{
+				case ilAuthStatus::STATUS_AUTHENTICATED:
+					return $this->handleAuthenticationSuccess($provider);
+					
+			}
+		}
+		return $this->handleAuthenticationFail();
 	}
 	
 	/**
@@ -131,6 +151,11 @@ class ilAuthFrontend
 	{
 		foreach($this->providers as $provider)
 		{
+			if(!$provider instanceof ilAuthProviderAccountMigrationInterface)
+			{
+				$this->logger->warning('Provider: ' . get_class($provider) .' does not support account migration.');
+				throw new InvalidArgumentException('Invalid auth provider given.');
+			}
 			$provider->createNewAccount($this->getStatus());
 
 			switch($this->getStatus()->getStatus())
