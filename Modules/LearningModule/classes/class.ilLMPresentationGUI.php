b46a6c11e186f69e7ae3615085b061274c83c567
Alex Killing <alex.killing@gmx.de>
2017-09-25 14:32:49 +0200

    fixed bug #21331: File List: File Download Broken

--- Modules/LearningModule/classes/class.ilLMPresentationGUI.php
+++ Modules/LearningModule/classes/class.ilLMPresentationGUI.php
@@ -3725,14 +3725,24 @@ class ilLMPresentationGUI
 			if ($il["Target"] == str_replace("_file_", "_dfile_", $_GET["file_id"]))
 			{
 				$file = explode("_", $_GET["file_id"]);
-				$file_id = (int) $file[count($file) - 1];
+				$file_id = (int)$file[count($file) - 1];
 				require_once("./Modules/File/classes/class.ilObjFile.php");
 				$fileObj = new ilObjFile($file_id, false);
 				$fileObj->sendFile();
 				exit;
 			}
 		}
+		if (in_array($_GET["file_id"], $pg_obj->getAllFileObjIds()))
+		{
+			require_once("./Modules/File/classes/class.ilObjFile.php");
+			$file = explode("_", $_GET["file_id"]);
+			$file_id = (int)$file[count($file) - 1];
+			$fileObj = new ilObjFile($file_id, false);
+			$fileObj->sendFile();
+			exit;
+		}
 	}
+
 
 	/**
 	* download source code paragraph
