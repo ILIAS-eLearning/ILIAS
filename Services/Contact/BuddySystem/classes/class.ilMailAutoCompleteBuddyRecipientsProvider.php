ff0000c99632bf0f72a79eaeba34eed9662c067a
mjansen <mjansen@databay.de>
2017-10-04 14:15:25 +0200

    Mail: Fixed autocomplete query
    (cherry picked from commit 4197554)

--- Services/Contact/BuddySystem/classes/class.ilMailAutoCompleteBuddyRecipientsProvider.php
+++ Services/Contact/BuddySystem/classes/class.ilMailAutoCompleteBuddyRecipientsProvider.php
@@ -20,9 +20,14 @@ class ilMailAutoCompleteBuddyRecipientsProvider extends ilMailAutoCompleteUserPr
 
 		$joins = array();
 
+		$this->user_id;
+
 		$joins[] = '
 			INNER JOIN buddylist
-			ON (buddylist.usr_id = usr_data.usr_id OR buddylist.buddy_usr_id = usr_data.usr_id)';
+			ON (
+				(buddylist.usr_id = usr_data.usr_id AND buddylist.buddy_usr_id = ' . $this->db->quote($this->user_id, 'integer') . ') OR
+				(buddylist.buddy_usr_id = usr_data.usr_id AND buddylist.usr_id = ' . $this->db->quote($this->user_id, 'integer') . ')
+			)';
 
 		$joins[] = '
 			LEFT JOIN usr_pref profpref
