7f52814c4574428f1d5adc4ae3f3342af6ef4134
Timon Amstutz <timon.amstutz@bluewin.ch>
2017-10-05 17:41:51 +0200

    Fixed tests after refactoring

--- tests/UI/Component/Input/Container/Form/FormFactoryTest.php
+++ tests/UI/Component/Input/Container/Form/FormFactoryTest.php
@@ -5,11 +5,11 @@ require_once 'tests/UI/AbstractFactoryTest.php';
 use \ILIAS\UI\Component\Input\Container\Form;
 
 class FormFactoryTest extends AbstractFactoryTest {
-	public $kitchensink_info_settings = array
-		(
-		);
+	public $kitchensink_info_settings = array(
 
-	public $factory_title = 'ILIAS\\UI\\Component\\Form\\Factory';
+	);
+
+	public $factory_title = 'ILIAS\\UI\\Component\\Input\\Container\\Form\\Factory';
 
 	final public function buildFactory() {
 		return new \ILIAS\UI\Implementation\Component\Input\Container\Form\Factory;
