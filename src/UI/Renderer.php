70eb13eb00ed6aadd4b95bb1e3051ea6fbca6a0e
Richard Klees <richard.klees@concepts-and-training.de>
2017-10-20 14:37:58 +0200

    UI: some more docu about context dependant rendering

--- src/UI/Renderer.php
+++ src/UI/Renderer.php
@@ -40,24 +40,11 @@ interface Renderer {
 	 * spawned this functionality is the observation, that e.g. items representing
 	 * repository objects are renderer in different lists, where the individual items
 	 * look different every time but are morally the same item. Another use case could
-	 * be a kiosk mode for replaying tests or using ILIAS a LTI tool provider.
+	 * be a special rendering of input fields in filters over tables.
 	 *
-	 * Consider you have two components: Container and Item, where Items can be
-	 * rendered inside containers or on their own, but should look differently in
-	 * both situations. You could implement a standard renderer for the Item, that
-	 * renders the item without taking the container into account. This would go
-	 * in the standard Renderer-class in the Item-namespace.
-	 * To take care of the rendering in the container, a second renderer Renderer_Container
-	 * can be implemented in the Item-namespace, that takes care of the rendering
-	 * in the container context.
-	 *
-	 * $item = $ui_factory->item();
-	 * $container = $ui_factory->container();
-	 *
-	 * $default_renderer->render($item); // will use standard renderer
-	 *
-	 * $default_renderer_with_context = $default_renderer->withAdditionalContext($container);
-	 * $default_renderer_with_context->render($item); // will use Renderer_Container if available
+	 * If a component wants to render itself differently in different contexts, it must
+	 * implement a RendererFactory. The class \ILIAS\UI\Implementation\Render\FSLoader
+ 	 * contains directions how to do that.
 	 *
 	 * @param  Component	$context
 	 * @return Renderer
