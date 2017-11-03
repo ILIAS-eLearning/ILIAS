18a76088d8de9927c6734a3d44b3372a3690e35c
Alexander Killing <killing@leifos.de>
2017-11-03 22:55:15 +0100

    do not check tos for sys admins

--- Services/Awareness/classes/class.ilAwarenessUserCollector.php
+++ Services/Awareness/classes/class.ilAwarenessUserCollector.php
@@ -98,6 +98,8 @@ class ilAwarenessUserCollector
 	 */
 	public function collectUsers($a_online_only = false)
 	{
+		global $rbacreview;
+
 		$this->collections = array();
 
 		$awrn_logger = ilLoggerFactory::getLogger('awrn');
@@ -166,7 +168,8 @@ class ilAwarenessUserCollector
 		{
 			foreach (ilObjUser::getUsersAgreed(false, $all_users) as $u)
 			{
-				if ($u != SYSTEM_USER_ID)
+				if ($u != SYSTEM_USER_ID && !$rbacreview->isAssigned($u, SYSTEM_ROLE_ID))
+				//if ($u != SYSTEM_USER_ID)
 				{
 					$remove_users[] = $u;
 				}
