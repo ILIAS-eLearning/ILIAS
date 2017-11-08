3fc5da69aa8b51a500a06d6f3bfa765bde2c68c9
Nils Haagen <nhaagen@cat06.de>
2017-11-08 17:18:55 +0100

    fix to use environment with the mapping

--- src/UI/Implementation/Component/Table/Presentation.php
+++ src/UI/Implementation/Component/Table/Presentation.php
@@ -111,7 +111,7 @@ class Presentation implements T\Presentation {
 	 */
 	public function withEnvironment(array $environment) {
 		$clone = clone $this;
-		$clone->environment = $records;;
+		$clone->environment = $environment;;
 		return $clone;
 	}
 
