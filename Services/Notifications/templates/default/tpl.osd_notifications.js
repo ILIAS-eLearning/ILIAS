eb4b2a0748bcf2dad4ec883ecbd1773899eb072a
mjansen <mjansen@databay.de>
2017-11-14 18:12:13 +0100

    Performance issues: Fixed #22035

---
+++ Services/Notifications/templates/default/tpl.osd_notifications.js
@@ -0,0 +1,6 @@
+var OSDNotifier = OSDNotifications({
+	closeHtml:            {NOTIFICATION_CLOSE_HTML},
+	initialNotifications: {INITIAL_NOTIFICATIONS},
+	pollingIntervall:     {OSD_POLLING_INTERVALL},
+	playSound:            {OSD_PLAY_SOUND}
+});
