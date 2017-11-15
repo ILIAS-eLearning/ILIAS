be4709f15b8d1bd0d7c0e1276117557542637a34
mjansen <mjansen@databay.de>
2017-11-15 09:30:27 +0100

    Chat: Some renaming of callbacks for memory leak profiling

--- Modules/Chatroom/chat/Bootstrap/SetupNamespaces.js
+++ Modules/Chatroom/chat/Bootstrap/SetupNamespaces.js
@@ -7,42 +7,42 @@ var async = require('async');
 module.exports = function SetupNamespaces(callback) {
 	var clientConfigs = Container.getClientConfigs();
 
-	var setup = function(config, nextLoop) {
-		var setupNamespace = function(callback) {
+	function setupNamsepace(config, nextLoop) {
+		function createNamespace(callback) {
 			var namespace = Handler.createNamespace(config.name);
 
 			Container.getLogger().info('SetupNamespace %s!', namespace.getName());
 
 			callback(null, namespace, config);
-		};
+		}
 
-		var onEnd = function(err, result){
-			if(err) {
+		function onNamespaceSetupFinished(err, result) {
+			if (err) {
 				throw err;
 			}
 
 			nextLoop();
-		};
+		}
 
 		async.waterfall(
 			[
-				setupNamespace,
+				createNamespace,
 				SetupDatabase,
 				PreloadData
 			],
-			onEnd
+			onNamespaceSetupFinished
 		);
-	};
+	}
 
-	var onEnd = function(err) {
-		if(err) {
+	function onNamespacesSetupFinished(err) {
+		if (err) {
 			throw err;
 		}
 
 		Container.getLogger().info('SetupNamespace finished!');
 
 		callback();
-	};
+	}
 
-	async.eachSeries(clientConfigs, setup, onEnd);
+	async.eachSeries(clientConfigs, setupNamsepace, onNamespacesSetupFinished);
 };
