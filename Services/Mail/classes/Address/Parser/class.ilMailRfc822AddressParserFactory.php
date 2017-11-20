704b9a26717598e0a721b199d16230791175a91e
Niels Theen <ntheen@databay.de>
2017-11-20 10:44:17 +0100

    Establish non-static behavior for MailAddressParser

--- Services/Mail/classes/Address/Parser/class.ilMailRfc822AddressParserFactory.php
+++ Services/Mail/classes/Address/Parser/class.ilMailRfc822AddressParserFactory.php
@@ -11,7 +11,7 @@ class ilMailRfc822AddressParserFactory
 	 * @param string $a_address
 	 * @return ilMailRecipientParser
 	 */
-	public static function getParser($a_address)
+	public function getParser($a_address)
 	{
 		require_once 'Services/Mail/classes/Address/Parser/class.ilMailPearRfc822WrapperAddressParser.php';
 		require_once 'Services/Mail/classes/Address/Parser/class.ilMailRfc822AddressParser.php';
