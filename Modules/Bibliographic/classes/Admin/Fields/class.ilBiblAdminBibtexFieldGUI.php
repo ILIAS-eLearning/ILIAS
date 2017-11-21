616308ddea19b4e4fb8762475a447bc5e0714b4d
Bense <bs@studer-raimann.ch>
2017-11-21 09:48:36 +0100

    type specific GUIs

--- Modules/Bibliographic/classes/Admin/Fields/class.ilBiblAdminBibtexFieldGUI.php
+++ Modules/Bibliographic/classes/Admin/Fields/class.ilBiblAdminBibtexFieldGUI.php
@@ -7,6 +7,11 @@
 
 class ilBiblAdminBibtexFieldGUI extends ilBiblAdminFieldGUI {
 
+	public function __construct() {
+		$this->initType();
+		parent::__construct();
+	}
+
 	protected function initType() {
 		$this->type = ilBiblField::DATA_TYPE_BIBTEX;
 	}
--- Modules/Bibliographic/classes/Admin/Fields/class.ilBiblAdminRisFieldGUI.php
+++ Modules/Bibliographic/classes/Admin/Fields/class.ilBiblAdminRisFieldGUI.php
@@ -7,6 +7,11 @@
 
 class ilBiblAdminRisFieldGUI extends ilBiblAdminFieldGUI {
 
+	public function __construct() {
+		$this->initType();
+		parent::__construct();
+	}
+
 	public function initType() {
 		$this->type = ilBiblField::DATA_TYPE_RIS;
 	}
