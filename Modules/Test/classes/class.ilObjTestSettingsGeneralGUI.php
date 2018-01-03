c3f2d7ebbc52fda03afa73da85fec78d950972ce
Björn Heyser <bh@bjoernheyser.de>
2018-01-03 20:33:04 +0100

    0022038: Failed test: Übersicht gegebener Antworten

--- Modules/Test/classes/class.ilObjTestSettingsGeneralGUI.php
+++ Modules/Test/classes/class.ilObjTestSettingsGeneralGUI.php
@@ -1372,11 +1372,6 @@ class ilObjTestSettingsGeneralGUI extends ilTestSettingsGUI
 		$enable_examview->setValue(1);
 		$enable_examview->setChecked($this->testOBJ->getEnableExamview());
 		$enable_examview->setInfo($this->lng->txt("enable_examview_desc"));
-		$show_examview_html = new ilCheckboxInputGUI('', 'show_examview_html');
-		$show_examview_html->setValue(1);
-		$show_examview_html->setChecked($this->testOBJ->getShowExamviewHtml());
-		$show_examview_html->setOptionTitle($this->lng->txt("show_examview_html"));
-		$enable_examview->addSubItem($show_examview_html);
 		$show_examview_pdf = new ilCheckboxInputGUI('', 'show_examview_pdf');
 		$show_examview_pdf->setValue(1);
 		$show_examview_pdf->setChecked($this->testOBJ->getShowExamviewPdf());
@@ -1455,7 +1450,6 @@ class ilObjTestSettingsGeneralGUI extends ilTestSettingsGUI
 		if( $this->formPropertyExists($form, 'enable_examview') )
 		{
 			$this->testOBJ->setEnableExamview($form->getItemByPostVar('enable_examview')->getChecked());
-			$this->testOBJ->setShowExamviewHtml($form->getItemByPostVar('show_examview_html')->getChecked());
 			$this->testOBJ->setShowExamviewPdf($form->getItemByPostVar('show_examview_pdf')->getChecked());
 		}
 
