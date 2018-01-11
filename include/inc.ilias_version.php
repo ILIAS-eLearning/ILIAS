df0eb4476a206b3e25ae14ca1b652778bd37e65e
aarsenij <anton@ilias.de>
2018-01-11 16:33:46 +0100

    Fix release year

--- include/inc.ilias_version.php
+++ include/inc.ilias_version.php
@@ -9,5 +9,5 @@
 *
 * @package ilias-core
 */
-define("ILIAS_VERSION", "5.2.11 2017-01-10");
+define("ILIAS_VERSION", "5.2.11 2018-01-10");
 define("ILIAS_VERSION_NUMERIC", "5.2.11");			// must be always x.y.z: x, y and z are numbers
