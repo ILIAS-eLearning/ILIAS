c519c32d796646b447cff568d7ddbaef4af253be
Fabian Schmid <fs@studer-raimann.ch>
2018-01-12 08:38:40 +0100

    boyscouting

--- Services/MyStaff/classes/ListCourses/class.ilMStListCoursesTableGUI.php
+++ Services/MyStaff/classes/ListCourses/class.ilMStListCoursesTableGUI.php
@@ -340,12 +340,12 @@ class ilMStListCoursesTableGUI extends ilTable2GUI {
 		$this->tpl->setVariable('ACTIONS', $selection->getHTML());
 	}
 
+
 	/**
-	 * Get profile back url
+	 * @return string
 	 */
-	function getProfileBackUrl()
-	{
-		return rawurlencode($this->ctrl()->getLinkTargetByClass(strtolower(ilMStListCoursesGUI::class), "index"));
+	private function getProfileBackUrl() {
+		return rawurlencode($this->ctrl()->getLinkTargetByClass(strtolower(ilMStListCoursesGUI::class), ilMStListCoursesGUI::CMD_INDEX));
 	}
 
 
