21117ab65edb98a4e4df2b2e7cb014f05dcc0c67
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2018-03-01 15:17:52 +0100

    including uri data object

---
+++ src/Data/URI.php
@@ -0,0 +1,265 @@
+<?php
+
+namespace ILIAS\Data;
+
+
+/**
+ * The scope of this class is split ilias-conform URI's into components.
+ * Please refer to RFC 3986 for details.
+ * Notice: ilias-confor URI's will form a SUBSET of RFC 3986.
+ * We limit the set of allowed sub-delimiters in authority.
+ * We require a schema and an authority to be present.
+ * If any part is located and it is invalid an exception will be thrown
+ * instead of just omiting it.
+ */
+class URI
+{
+	protected $schema;
+	protected $authority;
+	protected $path;
+	protected $query;
+	protected $fragment;
+
+
+	const ALPHA_SUB_REGEX = 'A-Za-z';
+	const DIGIT_SUB_REGEX = '0-9';
+	const ALPHA_DIGIT_SUB_REGEX = self::ALPHA_SUB_REGEX.self::DIGIT_SUB_REGEX;
+	const UNRESERVED_SUB_REGEX = self::ALPHA_DIGIT_SUB_REGEX.'\\-\\._~';
+	const PENCODED_SUB_REGEX = self::ALPHA_DIGIT_SUB_REGEX.'%';
+	const BASEURI_ALLOWED_SUBDELIMS_SUB_REGEX = '\\$,;';
+	const SUBDELIMS_SUB_REGEX = '\\$,;=!&\'"\\(\\)\\*\\+';
+
+	const BASEURI_PCHAR_SUB_REGEX =
+							self::PENCODED_SUB_REGEX
+							.self::BASEURI_ALLOWED_SUBDELIMS_SUB_REGEX
+							.self::UNRESERVED_SUB_REGEX
+							.':@';
+
+	const PCHAR_SUB_REGEX = self::PENCODED_SUB_REGEX
+							.self::SUBDELIMS_SUB_REGEX
+							.self::UNRESERVED_SUB_REGEX
+							.':@';
+
+	const SCHEMA_REGEX = '#^['.self::ALPHA_SUB_REGEX.']'
+							.'['.self::ALPHA_DIGIT_SUB_REGEX.'\\+\\-\\.]*(?=(://))#';
+
+	const AUTHORITY_REGEX = '#^['.self::UNRESERVED_SUB_REGEX
+								.self::PENCODED_SUB_REGEX
+								.self::BASEURI_ALLOWED_SUBDELIMS_SUB_REGEX
+								.']+(:['.self::DIGIT_SUB_REGEX.']+)?(?=(\\#|\\?|/|$))#';
+
+	const HOST_REGEX = '#^['.self::UNRESERVED_SUB_REGEX
+							.self::PENCODED_SUB_REGEX
+							.self::BASEURI_ALLOWED_SUBDELIMS_SUB_REGEX.']+(?=(:|\\#|\\?|/|$))#';
+
+	//starts with ':' and ends with $ within authority
+	const PORT_LOCATION_REGEX = '#(?<=:).+$#';
+	const PORT_REGEX = '#['.self::DIGIT_SUB_REGEX.']+$#';
+	
+	//starts with end authority and ends with any ?,# or $
+	const PATH_LOCATION_REGEX = '#^[^\\#^\\?]+(?=(\\#|\\?|$))#'; 
+	const PATH_REGEX = '#^['.self::BASEURI_PCHAR_SUB_REGEX.'/]+$#';
+	
+	//starts with first ? and ends with any # or $
+	const QUERY_LOCATION_REGEX = '#^\\?[^\\#]+(?=(\\#|$))#';
+	const QUERY_REGEX = '#^\\?['.self::PCHAR_SUB_REGEX.'/\\?]+$#';
+
+	//starts with first # and ends with $
+	const FRAGMENT_LOCATION_REGEX = '#\\#.+$#';
+	const FRAGMENT_REGEX = '#^\\#['.self::PCHAR_SUB_REGEX.'/\\?]+$#';
+
+
+	/**
+	 * Walk left-right and try to identify uri-building-parts.
+	 */
+	public function __construct($uri_string)
+	{
+		assert('is_string($uri_string)');
+		$uri_string = trim($uri_string);
+		list($uri_string, $this->schema) = $this->trimBySchema($uri_string);
+		list($uri_string, $this->authority, $this->host, $this->port) = $this->trimByAuthority($uri_string);
+		list($uri_string, $path) = $this->trimByPath($uri_string);
+		$this->path = $path;
+		list($uri_string, $query) = $this->trimByQuery($uri_string);
+		$this->query = $query;
+		list($uri_string, $fragment) = $this->trimByFragment($uri_string);
+		$this->fragment = $fragment;
+	}
+
+	/**
+	 * Find schema in uri_string. Extract it and cut uri_string by the
+	 * schema-part.
+	 *
+	 * @return	[string,string]	leftover_uri,located schema
+	 */
+	protected function trimBySchema($uri_string)
+	{
+		if(preg_match(self::SCHEMA_REGEX, $uri_string, $finding) === 1) {
+			$schema = $finding[0];
+			$uri_string = substr($uri_string, strlen($schema) + 3); // remove '://'
+		} else {
+			throw new \InvalidArgumentException('undefined schema');
+		}
+		return [$uri_string,$schema];
+	}
+
+	/**
+	 * Find authority and its subparts in uri_string.
+	 * Extract them and cut uri_string by the
+	 * authority-part.
+	 *
+	 * @return	[string,string,string,string]	leftover_uri,located schema,located host,located port
+	 */
+	protected function trimByAuthority($uri_string)
+	{
+		if(preg_match(self::AUTHORITY_REGEX, $uri_string, $finding) === 1) {
+			$authority = $finding[0]; 
+			$uri_string = substr($uri_string, strlen($authority));
+		} else {
+			throw new \InvalidArgumentException('undefined authority');
+		}
+		if(preg_match(self::HOST_REGEX, $authority, $finding) === 1) {
+			$host = $finding[0];
+		}
+		if(preg_match(self::PORT_LOCATION_REGEX, $authority, $finding) === 1) {
+			$port = $finding[0];
+			if(!preg_match(self::PORT_REGEX, $port)) {
+				throw new \InvalidArgumentException('invalid port '.$port);
+			}
+		}
+		$uri_string = ltrim($uri_string,'/'); //remove any number of left-dangling '/' since they are irrelevant
+		return [$uri_string,$authority,$host,$port];
+	}
+
+	/**
+	 * Find path in uri_string. Extract it and cut uri_string by the
+	 * path-part.
+	 *
+	 * @return	[string,string]	leftover_uri,located path
+	 */
+	protected function trimByPath($uri_string)
+	{
+		if(preg_match(self::PATH_LOCATION_REGEX, $uri_string, $finding) === 1) {
+			$path = $finding[0]; 
+			if(!preg_match(self::PATH_REGEX, $path)) {
+				throw new \InvalidArgumentException('invalid path '.$path);
+			}
+			$uri_string = substr($uri_string, strlen($path));
+			$path = trim($path,'/');
+		}
+		ltrim($uri_string,'/');
+		return [$uri_string,$path];
+	}
+
+	/**
+	 * Find query in uri_string. Extract it and cut uri_string by the
+	 * query-part.
+	 *
+	 * @return	[string,string]	leftover_uri,located query
+	 */
+	protected function trimByQuery($uri_string)
+	{
+		if(preg_match(self::QUERY_LOCATION_REGEX, $uri_string, $finding) === 1) {
+			$query = $finding[0];
+			if(!preg_match(self::QUERY_REGEX, $query)) {
+				throw new \InvalidArgumentException('invalid query '.$query);
+			}
+			$uri_string = substr($uri_string, strlen($query));
+			$query = ltrim($query,'?');
+		}
+		return [$uri_string,$query];
+	}
+
+
+	/**
+	 * Find fragment in uri_string. Extract it and cut uri_string by the
+	 * fragment-part.
+	 *
+	 * @return	[string,string]	leftover_uri,located fragment
+	 */
+	protected function trimByFragment($uri_string)
+	{
+		if(preg_match(self::FRAGMENT_LOCATION_REGEX, $uri_string, $finding) === 1) {
+			$fragment = $finding[0];
+			if(!preg_match(self::FRAGMENT_REGEX, $fragment)) {
+				throw new \InvalidArgumentException('invalid fragment '.$fragment);
+			}
+			$uri_string = substr($uri_string, strlen($fragment));
+			$fragment = ltrim($fragment,'#');
+		}
+		return [$uri_string,$fragment];
+	}
+
+
+	/**
+	 * @return	string
+	 */
+	public function schema()
+	{
+		return $this->schema;
+	}
+
+	/**
+	 * @return	string
+	 */
+	public function authority()
+	{
+		return $this->authority;
+	}
+
+	/**
+	 * @return	string
+	 */
+	public function port()
+	{
+		return $this->port;
+	}
+
+	/**
+	 * @return	string
+	 */
+	public function host()
+	{
+		return $this->host;
+	}
+
+	/**
+	 * @return	string
+	 */
+	public function path()
+	{
+		return $this->path;
+	}
+
+	/**
+	 * @return	string
+	 */
+	public function query()
+	{
+		return $this->query;
+	}
+
+	/**
+	 * @return	string
+	 */
+	public function fragment()
+	{
+		return $this->fragment;
+	}
+
+	/**
+	 * Get a well-formed URI consisting only out of
+	 * schema, authority and port.
+	 *
+	 * @return	string
+	 */
+	public function baseURI()
+	{
+		$return = $this->schema().'://'.$this->authority();
+		if($this->path()) {
+			$return .= '/'.$this->path();
+		}
+		return $return;
+	}
+
+}
---
+++ tests/Data/URITest.php
@@ -0,0 +1,280 @@
+<?php
+require_once("libs/composer/vendor/autoload.php");
+
+use ILIAS\Data;
+
+class URITest extends PHPUnit_Framework_TestCase {
+
+	const URI_COMPLETE = 'g+it://github.com:8080/someaccount/somerepo/somerepo.git/?query_par_1=val_1&query_par_2=val_2#fragment';
+
+	const URI_NO_PATH_1 = 'g-it://githu%2Db.com:8080?query_par_1=val_1&query_par_2=val_2#fragment';
+	const URI_NO_PATH_2 = 'g.it://githu%1Fb.com:8080/?query_par_1=val_1&query_par_2=val_2#fragment';
+
+	const URI_NO_QUERY_1 = 'git://github.com:8080/someaccount/somerepo/somerepo.git/#fragment';
+	const URI_NO_QUERY_2 = 'git://github.com:8080/someaccount/somerepo/somerepo.git#fragment';
+
+	const URI_AUTHORITY_AND_QUERY_1 = 'git://github.com?query_p$,;:A!"\'*+()ar_1=val_1&quer?y_par_2=val_2';
+	const URI_AUTHORITY_AND_QUERY_2 = 'git://github.com/?qu/ery_p$,;:A!"\'*+()ar_1=val_1&quer?y_par_2=val_2';
+
+	const URI_AUTHORITY_AND_FRAGMENT = 'git://github.com:8080/#fragment$,;:A!"\'*+()ar_1=val_1&';
+
+	const URI_AUTHORITY_PATH_FRAGMENT = 'git://git$,;hub.com:8080/someacc$,;ount/somerepo/somerepo.git#frag:A!"\'*+()arment';
+
+	const URI_PATH = 'git://git$,;hub.com:8080/someacc$,;ount/somerepo/somerepo.git/';
+
+	const URI_AUTHORITY_ONLY = 'git://git$,;hub.com';
+
+	const URI_NO_SCHEMA = 'git$,;hub.com:8080/someacc$,;ount/somerepo/somerepo.git/';
+
+	const URI_NO_AUTHORITY = 'git://:8080/someaccount/somerepo/somerepo.git/?query_par_1=val_1&query_par_2=val_2#fragment';
+
+	const URI_WRONG_SCHEMA = 'gi$t://git$,;hub.com';
+
+	const URI_WRONG_AUTHORITY_1 = 'git://git$,;hu<b.com:8080/someacc$,;ount/somerepo/somerepo.git/';
+	const URI_WRONG_AUTHORITY_2 = 'git://git$,;hu=b.com/someacc$,;ount/somerepo/somerepo.git/';
+
+	public function test_init()
+	{
+		return new ILIAS\Data\URI(self::URI_COMPLETE);
+	}
+
+	/**
+	 * @depends test_init
+	 */
+	public function test_components($uri)
+	{
+		$this->assertEquals($uri->schema(),'g+it');
+		$this->assertEquals($uri->authority(),'github.com:8080');
+		$this->assertEquals($uri->host(),'github.com');
+		$this->assertEquals($uri->port(),'8080');
+		$this->assertEquals($uri->path(),'someaccount/somerepo/somerepo.git');
+		$this->assertEquals($uri->query(),'query_par_1=val_1&query_par_2=val_2');
+		$this->assertEquals($uri->fragment(),'fragment');
+	}
+
+	/**
+	 * @depends test_init
+	 */
+	public function test_base_uri($uri)
+	{
+		$this->assertEquals($uri->baseURI(),'g+it://github.com:8080/someaccount/somerepo/somerepo.git');
+	}
+
+	/**
+	 * @depends test_init
+	 */
+	public function test_base_uri_idempotent($uri)
+	{
+		$base_uri = $uri->baseURI();
+		$this->assertEquals($base_uri,'g+it://github.com:8080/someaccount/somerepo/somerepo.git');
+		$this->assertEquals($uri->schema(),'g+it');
+		$this->assertEquals($uri->authority(),'github.com:8080');
+		$this->assertEquals($uri->host(),'github.com');
+		$this->assertEquals($uri->port(),'8080');
+		$this->assertEquals($uri->path(),'someaccount/somerepo/somerepo.git');
+		$this->assertEquals($uri->query(),'query_par_1=val_1&query_par_2=val_2');
+		$this->assertEquals($uri->fragment(),'fragment');
+
+		$uri = new ILIAS\Data\URI($base_uri);
+		$this->assertEquals($base_uri,$uri->baseURI());
+		$this->assertEquals($uri->schema(),'g+it');
+		$this->assertEquals($uri->authority(),'github.com:8080');
+		$this->assertEquals($uri->host(),'github.com');
+		$this->assertEquals($uri->port(),'8080');
+		$this->assertEquals($uri->path(),'someaccount/somerepo/somerepo.git');
+		$this->assertNull($uri->query());
+		$this->assertNull($uri->fragment());
+	}
+
+
+	/**
+	 * @depends test_init
+	 */
+	public function test_no_path()
+	{
+		$uri = new ILIAS\Data\URI(self::URI_NO_PATH_1);
+		$this->assertEquals($uri->schema(),'g-it');
+		$this->assertEquals($uri->authority(),'githu%2Db.com:8080');
+		$this->assertEquals($uri->host(),'githu%2Db.com');
+		$this->assertEquals($uri->port(),'8080');
+		$this->assertNull($uri->path());
+		$this->assertEquals($uri->query(),'query_par_1=val_1&query_par_2=val_2');
+		$this->assertEquals($uri->fragment(),'fragment');
+
+		$uri = new ILIAS\Data\URI(self::URI_NO_PATH_2);
+		$this->assertEquals($uri->schema(),'g.it');
+		$this->assertEquals($uri->authority(),'githu%1Fb.com:8080');
+		$this->assertEquals($uri->host(),'githu%1Fb.com');
+		$this->assertEquals($uri->port(),'8080');
+		$this->assertNull($uri->path());
+		$this->assertEquals($uri->query(),'query_par_1=val_1&query_par_2=val_2');
+		$this->assertEquals($uri->fragment(),'fragment');
+	}
+
+	/**
+	 * @depends test_init
+	 */
+	public function test_no_query()
+	{
+		$uri = new ILIAS\Data\URI(self::URI_NO_QUERY_1);
+		$this->assertEquals($uri->schema(),'git');
+		$this->assertEquals($uri->authority(),'github.com:8080');
+		$this->assertEquals($uri->host(),'github.com');
+		$this->assertEquals($uri->port(),'8080');
+		$this->assertEquals($uri->path(),'someaccount/somerepo/somerepo.git');
+		$this->assertNull($uri->query());
+		$this->assertEquals($uri->fragment(),'fragment');
+
+		$uri = new ILIAS\Data\URI(self::URI_NO_QUERY_2);
+		$this->assertEquals($uri->schema(),'git');
+		$this->assertEquals($uri->authority(),'github.com:8080');
+		$this->assertEquals($uri->host(),'github.com');
+		$this->assertEquals($uri->port(),'8080');
+		$this->assertEquals($uri->path(),'someaccount/somerepo/somerepo.git');
+		$this->assertNull($uri->query());
+		$this->assertEquals($uri->fragment(),'fragment');
+	}
+
+	/**
+	 * @depends test_init
+	 */
+	public function test_authority_and_query()
+	{
+		$uri = new ILIAS\Data\URI(self::URI_AUTHORITY_AND_QUERY_1);
+		$this->assertEquals($uri->schema(),'git');
+		$this->assertEquals($uri->authority(),'github.com');
+		$this->assertEquals($uri->host(),'github.com');
+		$this->assertNull($uri->port());
+		$this->assertNull($uri->path());
+		$this->assertEquals($uri->query(),'query_p$,;:A!"\'*+()ar_1=val_1&quer?y_par_2=val_2');
+		$this->assertNull($uri->fragment());
+
+		$uri = new ILIAS\Data\URI(self::URI_AUTHORITY_AND_QUERY_2);
+		$this->assertEquals($uri->schema(),'git');
+		$this->assertEquals($uri->authority(),'github.com');
+		$this->assertEquals($uri->host(),'github.com');
+		$this->assertNull($uri->port());
+		$this->assertNull($uri->path());
+		$this->assertEquals($uri->query(),'qu/ery_p$,;:A!"\'*+()ar_1=val_1&quer?y_par_2=val_2');
+		$this->assertNull($uri->fragment());
+	}
+
+	/**
+	 * @depends test_init
+	 */
+	public function test_authority_and_fragment()
+	{
+		$uri = new ILIAS\Data\URI(self::URI_AUTHORITY_AND_FRAGMENT);
+		$this->assertEquals($uri->schema(),'git');
+		$this->assertEquals($uri->authority(),'github.com:8080');
+		$this->assertEquals($uri->host(),'github.com');
+		$this->assertEquals($uri->port(),'8080');
+		$this->assertNull($uri->path());
+		$this->assertNull($uri->query());
+		$this->assertEquals($uri->fragment(),'fragment$,;:A!"\'*+()ar_1=val_1&');
+	}
+	/**
+	 * @depends test_init
+	 */
+	public function test_authority_path_fragment()
+	{
+		$uri = new ILIAS\Data\URI(self::URI_AUTHORITY_PATH_FRAGMENT);
+		$this->assertEquals($uri->schema(),'git');
+		$this->assertEquals($uri->authority(),'git$,;hub.com:8080');
+		$this->assertEquals($uri->host(),'git$,;hub.com');
+		$this->assertEquals($uri->port(),'8080');
+		$this->assertEquals($uri->path(),'someacc$,;ount/somerepo/somerepo.git');
+		$this->assertNull($uri->query());
+		$this->assertEquals($uri->fragment(),'frag:A!"\'*+()arment');
+	}
+
+	/**
+	 * @depends test_init
+	 */
+	public function test_path()
+	{
+		$uri = new ILIAS\Data\URI(self::URI_PATH);
+		$this->assertEquals($uri->schema(),'git');
+		$this->assertEquals($uri->authority(),'git$,;hub.com:8080');
+		$this->assertEquals($uri->host(),'git$,;hub.com');
+		$this->assertEquals($uri->port(),'8080');
+		$this->assertEquals($uri->path(),'someacc$,;ount/somerepo/somerepo.git');
+		$this->assertNull($uri->query());
+		$this->assertNull($uri->fragment());
+		$this->assertEquals($uri->baseURI(),'git://git$,;hub.com:8080/someacc$,;ount/somerepo/somerepo.git');
+	}
+
+	/**
+	 * @depends test_init
+	 */
+	public function test_authority_only()
+	{
+		$uri = new ILIAS\Data\URI(self::URI_AUTHORITY_ONLY);
+		$this->assertEquals($uri->schema(),'git');
+		$this->assertEquals($uri->authority(),'git$,;hub.com');
+		$this->assertEquals($uri->host(),'git$,;hub.com');
+		$this->assertNull($uri->port());
+		$this->assertNull($uri->path());
+		$this->assertNull($uri->query());
+		$this->assertNull($uri->fragment());
+		$this->assertEquals($uri->baseURI(),'git://git$,;hub.com');
+	}
+
+	/**
+	 * @depends test_init
+	 */
+	public function test_no_schema()
+	{
+		try {
+			new ILIAS\Data\URI(self::URI_NO_SCHEMA);
+			$this->assertFalse('did not throw');
+		} catch(\InvalidArgumentException $e) {
+
+		}
+	}
+
+	/**
+	 * @depends test_init
+	 */
+	public function test_no_authority()
+	{
+		try {
+			new ILIAS\Data\URI(self::URI_NO_AUTHORITY);
+			$this->assertFalse('did not throw');
+		} catch(\InvalidArgumentException $e) {
+
+		}
+	}
+
+	/**
+	 * @depends test_init
+	 */
+	public function test_wrong_char_in_schema()
+	{
+		try {
+			new ILIAS\Data\URI(self::URI_WRONG_SCHEMA);
+			$this->assertFalse('did not throw');
+		} catch(\InvalidArgumentException $e) {
+
+		}
+	}
+
+	/**
+	 * @depends test_init
+	 */
+	public function test_wrong_authority_in_schema()
+	{
+		try {
+			new ILIAS\Data\URI(self::URI_WRONG_AUTHORITY_1);
+			$this->assertFalse('did not throw');
+		} catch(\InvalidArgumentException $e) {
+
+		}
+		try {
+			new ILIAS\Data\URI(self::URI_WRONG_AUTHORITY_2);
+			$this->assertFalse('did not throw');
+		} catch(\InvalidArgumentException $e) {
+
+		}
+	}
+}
