ce034296da2c93d98aff58a8a48b252dc5382f19
Alexander Killing <killing@leifos.de>
2018-03-11 18:18:47 +0100

    abandonded old import version

--- Modules/Survey/classes/class.ilObjSurvey.php
+++ Modules/Survey/classes/class.ilObjSurvey.php
@@ -3801,11 +3801,13 @@ class ilObjSurvey extends ilObject
 	}
 
 	/**
-	* Imports a survey from XML into the ILIAS database
-	*
-	* @return boolean True, if the import succeeds, false otherwise
-	* @access public
-	*/
+ 	 * Imports a survey from XML into the ILIAS database
+	 * @param $file_info
+	 * @param $svy_qpl_id
+	 * @return string
+	 * @throws ilFileUtilsException
+	 * @throws ilInvalidSurveyImportFileException
+	 */
 	function importObject($file_info, $svy_qpl_id)
 	{
 		if ($svy_qpl_id < 1) $svy_qpl_id = -1;
@@ -3880,13 +3882,16 @@ class ilObjSurvey extends ilObject
 			}
 
 			unset($_SESSION["import_mob_xhtml"]);
-			if (strpos($xml, "questestinterop"))
+			if (!strpos($xml, "questestinterop"))
 			{
+				include_once("./Modules/Exercises/classes/class.ilInvalidSurveyImportFileException.php");
+				throw new ilInvalidSurveyImportFileException("Unsupported survey version (< 3.8) found.");
+				/*
 				include_once "./Services/Survey/classes/class.SurveyImportParserPre38.php";
 				$import = new SurveyImportParserPre38($svy_qpl_id, "", TRUE);
 				$import->setSurveyObject($this);
 				$import->setXMLContent($xml);
-				$import->startParsing();
+				$import->startParsing();*/
 			}
 			else
 			{
