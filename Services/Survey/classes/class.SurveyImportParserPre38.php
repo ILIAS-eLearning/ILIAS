8d35d5f0f13a2e1ad34285d3f1d42c7513bad7a0
Alexander Killing <killing@leifos.de>
2018-03-11 18:11:17 +0100

    abandonded old import version

--- Services/Survey/classes/class.SurveyImportParserPre38.php
+++ Services/Survey/classes/class.SurveyImportParserPre38.php
@@ -94,6 +94,7 @@ class SurveyImportParserPre38 extends ilSaxParser
 	*/
 	function SurveyImportParserPre38($a_spl_id, $a_xml_file = '', $spl_exists = FALSE)
 	{
+		die ("Class surveyImportParserPre38 is deprecated.");
 		parent::ilSaxParser($a_xml_file);
 		$this->spl_id = $a_spl_id;
 		$this->has_error = FALSE;
