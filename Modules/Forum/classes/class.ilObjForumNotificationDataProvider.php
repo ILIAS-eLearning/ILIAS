2610498035d361dd6f66511252e0ac503a67a0d5
mjansen <mjansen@databay.de>
2018-03-20 14:55:13 +0100

    Forum: Possible performance fix for Mantis issue #22722
    (cherry picked from commit afe3ab0)

--- Modules/Forum/classes/class.ilObjForumNotificationDataProvider.php
+++ Modules/Forum/classes/class.ilObjForumNotificationDataProvider.php
@@ -21,9 +21,15 @@ class ilObjForumNotificationDataProvider implements ilForumNotificationMailData
 	protected $obj_id = 0;
 
 	/**
-	 * @var string $post_user_name
+	 * @var string|null $post_user_name
 	 */
-	protected $post_user_name = '';
+	protected $post_user_name = null;
+
+	/**
+	 * @var string|null $update_user_name
+	 */
+	protected $update_user_name = null;
+
 	/**
 	 * @var int
 	 */
@@ -240,15 +246,17 @@ class ilObjForumNotificationDataProvider implements ilForumNotificationMailData
 	 */
 	public function getPostUserName($user_lang)
 	{
-		$authorinfo = new ilForumAuthorInformation(
-			$this->getPosAuthorId(),
-			$this->getPosDisplayUserId(),
-			$this->getPosUserAlias(),
-			$this->getImportName()
-		);
-		$this->post_user_name = $this->getPublicUserInformation($authorinfo);
-		
-		return $this->post_user_name;
+		if ($this->post_user_name === null) {
+			$authorinfo           = new ilForumAuthorInformation(
+				$this->getPosAuthorId(),
+				$this->getPosDisplayUserId(),
+				$this->getPosUserAlias(),
+				$this->getImportName()
+			);
+			$this->post_user_name = $this->getPublicUserInformation($authorinfo);
+		}
+
+		return (string)$this->post_user_name;
 	}
 
 	/**
@@ -257,15 +265,17 @@ class ilObjForumNotificationDataProvider implements ilForumNotificationMailData
 	 */
 	public function getPostUpdateUserName($user_lang)
 	{
-		$authorinfo = new ilForumAuthorInformation(
-			$this->getPosAuthorId(),
-			$this->getPostUpdateUserId(),
-			$this->getPosUserAlias(),
-			$this->getImportName()
-		);
-		$this->post_user_name = $this->getPublicUserInformation($authorinfo);
-	
-		return $this->post_user_name;
+		if ($this->update_user_name === null) {
+			$authorinfo             = new ilForumAuthorInformation(
+				$this->getPosAuthorId(),
+				$this->getPostUpdateUserId(),
+				$this->getPosUserAlias(),
+				$this->getImportName()
+			);
+			$this->update_user_name = $this->getPublicUserInformation($authorinfo);
+		}
+
+		return (string)$this->update_user_name;
 	}
 	
 	/**
