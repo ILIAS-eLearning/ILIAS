074f5b2673f27e3c961115f9efd991e48e60609d
mjansenDatabay <mjansen@databay.de>
2018-03-30 12:31:22 +0200

    Mime Type Detection: Added unit tests

---
+++ Services/Utilities/test/ilServicesUtilitiesSuite.php
@@ -0,0 +1,21 @@
+<?php
+/* Copyright (c) 1998-2018 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+/**
+ * Class ilServicesUtilitiesSuite
+ */
+class ilServicesUtilitiesSuite extends \PHPUnit_Framework_TestSuite
+{
+	/**
+	 * @return \PHPUnit_Framework_TestSuite
+	 */
+	public static function suite()
+	{
+		$suite = new self();
+
+		require 'Services/Utilities/test/ilMimeTypeTest.php';
+		$suite->addTestSuite('ilMimeTypeTest');
+
+		return $suite;
+	}
+}
