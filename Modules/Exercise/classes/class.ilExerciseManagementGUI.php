044f739ec9b85c0eadacbbd7a8e9f5bc0ff3ffb0
xus <xus206@hotmail.com>
2018-04-10 16:42:26 +0200

    [Fix 22903] exc Assign. evaluation set feedback async.

--- Modules/Exercise/classes/class.ilExerciseManagementGUI.php
+++ Modules/Exercise/classes/class.ilExerciseManagementGUI.php
@@ -907,6 +907,7 @@ class ilExerciseManagementGUI
 					{
 						$member_status = $this->assignment->getMemberStatus($user_id);
 						$member_status->setComment(ilUtil::stripSlashes($comment));
+						$member_status->setFeedback(true);
 						$member_status->update();
 						
 						if(trim($comment))
