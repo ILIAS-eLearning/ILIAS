7d84ea6b3195eb8f0eca3f7133beab283c071c28
Björn Heyser <bh@bjoernheyser.de>
2018-04-19 11:07:17 +0200

    rename with native func, no dir rename wrapper available

--- Modules/TestQuestionPool/classes/class.ilObjQuestionPool.php
+++ Modules/TestQuestionPool/classes/class.ilObjQuestionPool.php
@@ -1134,16 +1134,7 @@ class ilObjQuestionPool extends ilObject
 								include_once "./Services/Utilities/classes/class.ilUtil.php";
 								ilUtil::makeDirParents($target_path);
 							}
-							try
-							{
-								require_once 'Services/Utilities/classes/class.ilFileUtils.php';
-								ilFileUtils::rename($source_path, $target_path . $question_object["question_id"]);
-							}
-							catch (\ilFileUtilsException $e)
-							{
-								\ilLoggerFactory::getRootLogger()->error($e->getMessage());
-							}
-							
+							rename($source_path, $target_path . $question_object["question_id"]);
 						}
 						// update question count of source question pool
 						ilObjQuestionPool::_updateQuestionCount($source_questionpool);
