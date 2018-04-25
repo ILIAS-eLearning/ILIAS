c98fb5a727163f9416127a5e26c89c8eaa54dc0b
aarsenij <anton@ilias.de>
2018-04-25 19:04:03 +0200

    Release 5.2.15

--- include/inc.ilias_version.php
+++ include/inc.ilias_version.php
@@ -9,5 +9,5 @@
 *
 * @package ilias-core
 */
-define("ILIAS_VERSION", "5.2.14 2018-03-15");
-define("ILIAS_VERSION_NUMERIC", "5.2.14");			// must be always x.y.z: x, y and z are numbers
+define("ILIAS_VERSION", "5.2.15 2018-04-25");
+define("ILIAS_VERSION_NUMERIC", "5.2.15");			// must be always x.y.z: x, y and z are numbers
