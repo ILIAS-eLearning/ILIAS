e3f3ec6e468bd01a5570e8c79d52d28fd25597f6
xus <xus206@hotmail.com>
2018-04-25 16:16:13 +0200

    First steps with the counter

--- src/UI/templates/js/Input/Field/textarea.js
+++ src/UI/templates/js/Input/Field/textarea.js
@@ -1,1 +1,49 @@
-//TODO
+var il = il || {};
+il.UI = il.UI || {};
+
+(function($, UI) {
+
+    UI.textarea = (function ($) {
+        /**
+         * Old changeCounter
+         * TODO-> I need the element id in the DOM.
+         */
+        var changeCounter = function(textarea_id, feedback_id, min_limit, max_limit) {
+            var text_length = $('#'+textarea_id).val().length;
+            if(max_limit > 0)
+            {
+                var text_remaining = max_limit - text_length;
+                $('#'+feedback_id).html(il.Language.txt("form_chars_remaining") +" "+ text_remaining);
+                return true;
+            }
+        };
+
+        /**
+         * TODO-> remove the old one and implement this counter
+         * @param textarea_id
+         * @param feedback_id
+         * @param min_limit
+         * @param max_limit
+         */
+        var changeCounterNew = function(textarea_id, feedback_id, min_limit, max_limit) {
+            $('#'+textarea_id).addEventListener("input", function(){
+                if(max_limit > 0) {
+                    var currentLength = this.value.length;
+                    var text_remaining = max_limit - currentLength;
+                    $('#'+feedback_id).html(il.Language.txt("form_chars_remaining") +" "+ text_remaining);
+                    return true;
+                }
+            })
+        };
+
+        /**
+         * Public interface
+         */
+        return {
+            //remote this old changeCounter
+            changeCounter: changeCounter,
+            changeCounterNew: changeCounterNew
+        };
+
+    })($);
+})($, il.UI);
