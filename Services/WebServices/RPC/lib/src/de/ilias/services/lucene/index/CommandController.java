d3550f099712e21cbeaf8c255ab49444b1fe7b3d
smeyer-ilias <smeyer.ilias@gmx.de>
2018-04-26 17:20:49 +0200

    Fixed race conditions; added missing lib

--- Services/WebServices/RPC/lib/src/de/ilias/services/lucene/index/CommandController.java
+++ Services/WebServices/RPC/lib/src/de/ilias/services/lucene/index/CommandController.java
@@ -167,7 +167,7 @@ public class CommandController {
 	}
 	
 
-	public void initRefresh() throws SQLException {
+	public void initRefresh() throws SQLException, ConfigurationException {
 		
 		queue.deleteNonIncremental();
 		queue.addNonIncremental();
