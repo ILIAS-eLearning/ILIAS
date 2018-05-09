308d2b0ce8986ed6e4ddb64e869af231dbfe8fc0
Fabian Schmid <fs@studer-raimann.ch>
2018-05-09 16:04:11 +0200

    moved steps to dbupdate_04

--- setup/sql/dbupdate_04.php
+++ setup/sql/dbupdate_04.php
@@ -22051,3 +22051,197 @@ $ilCtrlStructureReader->getStructure();
 <?php
 $ilCtrlStructureReader->getStructure();
 ?>
+<#5268>
+<?php
+$fields = array(
+	'id' => array(
+		'notnull' => '1',
+		'type' => 'integer',
+		'length' => '4',
+
+	),
+	'identifier' => array(
+		'notnull' => '1',
+		'type' => 'text',
+		'length' => '50',
+
+	),
+	'data_type' => array(
+		'notnull' => '1',
+		'type' => 'integer',
+		'length' => '1',
+
+	),
+	'position' => array(
+		'type' => 'integer',
+		'length' => '3',
+
+	),
+	'is_standard_field' => array(
+		'notnull' => '1',
+		'type' => 'integer',
+		'length' => '1',
+
+	),
+	'object_id' => array(
+		'notnull' => '1',
+		'type' => 'integer',
+		'length' => '4',
+
+	),
+);
+global $ilDB;
+if (! $ilDB->tableExists('il_bibl_field')) {
+	$ilDB->createTable('il_bibl_field', $fields);
+	$ilDB->addPrimaryKey('il_bibl_field', array( 'id' ));
+
+	if (! $ilDB->sequenceExists('il_bibl_field')) {
+		$ilDB->createSequence('il_bibl_field');
+	}
+
+}
+?>
+<#5269>
+<?php
+$fields = array(
+	'id' => array(
+		'notnull' => '1',
+		'type' => 'integer',
+		'length' => '4',
+
+	),
+	'field_id' => array(
+		'notnull' => '1',
+		'type' => 'integer',
+		'length' => '4',
+
+	),
+	'object_id' => array(
+		'notnull' => '1',
+		'type' => 'integer',
+		'length' => '4',
+
+	),
+	'filter_type' => array(
+		'type' => 'integer',
+		'length' => '1',
+
+	),
+
+);
+if (! $ilDB->tableExists('il_bibl_filter')) {
+	$ilDB->createTable('il_bibl_filter', $fields);
+	$ilDB->addPrimaryKey('il_bibl_filter', array( 'id' ));
+
+	if (! $ilDB->sequenceExists('il_bibl_filter')) {
+		$ilDB->createSequence('il_bibl_filter');
+	}
+
+}
+?>
+<#5270>
+<?php
+if(!$ilDB->tableColumnExists("il_bibl_data", "file_type")) {
+	$ilDB->addTableColumn("il_bibl_data", "file_type", [
+		"type" => "integer",
+		"notnull" => true,
+		"length" => 1,
+		"default" => 1
+	]);
+}
+
+$type = function ($filename) {
+	if (strtolower(substr($filename, - 6)) == "bibtex"
+		|| strtolower(substr($filename, - 3)) == "bib") {
+		return 2;
+	}
+	return 1;
+};
+
+$res = $ilDB->query("SELECT * FROM il_bibl_data");
+while($d = $ilDB->fetchObject($res)) {
+	$type_id = (int)$type($d->filname);
+	$ilDB->update("il_bibl_data", [
+		"file_type" => [ "integer", $type_id ]
+	], [ "id" => $d->id ]);
+}
+?>
+<#5271>
+<?php
+$fields = array(
+	'id' => array(
+		'notnull' => '1',
+		'type' => 'integer',
+		'length' => '4',
+
+	),
+	'field_id' => array(
+		'notnull' => '1',
+		'type' => 'integer',
+		'length' => '8',
+
+	),
+	'language_key' => array(
+		'notnull' => '1',
+		'type' => 'text',
+		'length' => '2',
+
+	),
+	'translation' => array(
+		'type' => 'text',
+		'length' => '256',
+
+	),
+	'description' => array(
+		'type' => 'clob',
+
+	),
+
+);
+if (! $ilDB->tableExists('il_bibl_translation')) {
+	$ilDB->createTable('il_bibl_translation', $fields);
+	$ilDB->addPrimaryKey('il_bibl_translation', array( 'id' ));
+
+	if (! $ilDB->sequenceExists('il_bibl_translation')) {
+		$ilDB->createSequence('il_bibl_translation');
+	}
+
+}
+?>
+<#5272>
+<?php
+$ilCtrlStructureReader->getStructure();
+?>
+<#5273>
+<?php
+// TODO fill filetype_id with the correct values
+if ($ilDB->tableExists('il_bibl_overview_model')) {
+	if($ilDB->tableColumnExists('il_bibl_overview_model', 'filetype')) {
+		$type = function ($filetype_string) {
+			if (strtolower($filetype_string) == "bib"
+				|| strtolower($filetype_string) == "bibtex"
+			) {
+				return 2; // see ilBiblTypeFactoryInterface::DATA_TYPE_BIBTEX
+			}
+
+			return 1; // ilBiblTypeFactoryInterface::DATA_TYPE_RIS
+		};
+
+		if (!$ilDB->tableColumnExists('il_bibl_overview_model', 'file_type_id')) {
+			$ilDB->addTableColumn('il_bibl_overview_model', 'file_type_id', array("type" => "integer", 'length' => 4));
+		}
+
+		$res = $ilDB->query("SELECT * FROM il_bibl_overview_model");
+		while ($d = $ilDB->fetchObject($res)) {
+			$type_id = (int)$type($d->filetype);
+			$ilDB->update(
+				"il_bibl_overview_model", [
+				"file_type_id" => ["integer", $type_id],
+			], ["ovm_id" => ["integer", $d->ovm_id]]
+			);
+		}
+
+		$ilDB->dropTableColumn('il_bibl_overview_model', 'filetype');
+	}
+}
+?>
