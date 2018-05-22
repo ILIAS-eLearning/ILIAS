2a0939efed399f3819f78a89bed122f79329d326
mjansen <mjansen@databay.de>
2018-05-22 17:25:33 +0200

    Setup: Fixed #23080, #22723, #23079

--- setup/classes/class.ilSetupGUI.php
+++ setup/classes/class.ilSetupGUI.php
@@ -218,7 +218,7 @@ class ilSetupGUI
 				$this->active_tab = "password";
 				break;
 
-			case "changepassword":
+			case "changemasterpassword":
 				$this->setDisplayMode("view");
 				$this->changeMasterPassword();
 				$this->active_tab = "password";
@@ -546,7 +546,7 @@ class ilSetupGUI
 			{
 				if ($this->display_mode == "view" ||
 					$this->cmd == "clientlist" ||
-					$this->cmd == "changepassword" ||
+					$this->cmd == "changemasterpassword" ||
 					$this->cmd == 'savemasterpassword' ||
 					$this->cmd == "mastersettings") {
 					$this->tpl->setCurrentBlock("add_client");
