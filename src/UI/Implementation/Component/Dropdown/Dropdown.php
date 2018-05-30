fef62e708f4a44f4da92b70847bc200af1856eee
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2018-05-30 14:26:27 +0200

    use helper function instead of own

--- src/UI/Implementation/Component/Dropdown/Dropdown.php
+++ src/UI/Implementation/Component/Dropdown/Dropdown.php
@@ -28,12 +28,20 @@ abstract class Dropdown implements C\Dropdown\Dropdown {
 	 */
 	protected $items;
 
+
+	protected static $proper_item_classes =
+		[\ILIAS\UI\Component\Button\Shy::class
+		,\ILIAS\UI\Component\Divider\Horizontal::class];
+
 	/**
 	 * Dropdown constructor.
 	 * @param array<\ILIAS\UI\Component\Button\Shy|\ILIAS\UI\Component\Divider\Horizontal> $items
 	 */
 	public function __construct(array $items) {
-		$this->checkItems($items);
+		$this->checkArgListElements(
+			'items'
+			,$items
+			,self::$proper_item_classes);
 		$this->items = $items;
 	}
 
@@ -87,23 +95,5 @@ abstract class Dropdown implements C\Dropdown\Dropdown {
 	 */
 	public function appendOnHover(Signal $signal) {
 		return $this->appendTriggeredSignal($signal, 'hover');
-	}
-
-
-	/**
-	 * Make sure every item is of proper type.
-	 *
-	 * @param	array<\ILIAS\UI\Component\Button\Shy|\ILIAS\UI\Component\Divider\Horizontal>	$items
-	 * @return	void
-	 * @throws	\InvalidArgumentException
-	 */
-	protected function checkItems(array $items)
-	{
-		foreach ($items as $item) {
-			if(!(	$item instanceof \ILIAS\UI\Component\Button\Shy
-				||	$item instanceof \ILIAS\UI\Component\Divider\Horizontal)) {
-					throw new \InvalidArgumentException('invalid item');
-			}
-		}
 	}
 }
