de464f09f9117f38e31a2830422fb27283fe50ac
Benjamin Seglias <bs@studer-raimann.ch>
2018-05-31 13:20:32 +0200

    possible fix for mantis bug 0023105

--- Services/Database/classes/class.ilDBUpdate.php
+++ Services/Database/classes/class.ilDBUpdate.php
@@ -1,6 +1,7 @@
 <?php
 /* Copyright (c) 1998-2009 ILIAS open source, Extended GPL, see docs/LICENSE */
 
+require_once("./setup/classes/class.ilCtrlStructureReader.php");
 /**
  * Database Update class
  *
