346c6629d739cb504462d2e3efe748dcb920f6d8
daniwe4 <daniel.weise@concepts-and-training.de>
2018-06-19 07:20:33 +0000

    add file

---
+++ setup/classes/class.ilSetupErrorHandling.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'Services/Init/classes/class.ilErrorHandling.php';
+/**
+ * @inheritdoc
+ */
+class ilSetupErrorHandling extends ilErrorHandling
+{
+	/**
+	 * @inheritdoc
+	 */
+	public function getHandler()
+	{
+		global $ilLog;
+		if ($ilLog) {
+			$ilLog->write("err");
+		}
+		return $this->devmodeHandler();
+	}
+}
