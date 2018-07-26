7f9cae71b1c1ea3223cd94b96206d0b292cae3ec
Niels Theen <ntheen@databay.de>
2018-07-26 11:30:28 +0200

    Certificate Path constants

---
+++ Services/Certificate/classes/File/class.ilCertificatePathConstants.php
@@ -0,0 +1,11 @@
+<?php
+
+
+class ilCertificatePathConstants
+{
+	const COURSE_PATH    = CLIENT_WEB_DIR . '/course/certificates/';
+	const TEST_PATH      = CLIENT_WEB_DIR . '/assessment/certificates/';
+	const EXERCISE_PATH  = CLIENT_WEB_DIR . '/exercise/certificates/';
+	const SKILL_PATH     = CLIENT_WEB_DIR . '/certificates/skill/';
+	const SCORM_PATH     = CLIENT_WEB_DIR . '/certificates/scorm/';
+}
