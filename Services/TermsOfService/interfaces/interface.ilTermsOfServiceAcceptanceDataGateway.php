fb8195d44923992424927e1b8b688e2b85bb6cf5
mjansen <mjansen@databay.de>
2018-08-14 11:06:42 +0200

    Terms of Service: Fixed PhpDoc

--- Services/TermsOfService/interfaces/interface.ilTermsOfServiceAcceptanceDataGateway.php
+++ Services/TermsOfService/interfaces/interface.ilTermsOfServiceAcceptanceDataGateway.php
@@ -2,8 +2,8 @@
 /* Copyright (c) 1998-2012 ILIAS open source, Extended GPL, see docs/LICENSE */
 
 /**
- * @author  Michael Jansen <mjansen@databay.de>
- * @version $Id$
+ * Interface ilTermsOfServiceAcceptanceDataGateway
+ * @author Michael Jansen <mjansen@databay.de>
  */
 interface ilTermsOfServiceAcceptanceDataGateway
 {
