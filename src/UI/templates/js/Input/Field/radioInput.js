1638bdfe44189872933b6fbaf6659649a322854e
Nils Haagen <nhaagen@cat06.de>
2018-08-15 10:51:19 +0200

    style radio-input (and checkbox)

--- src/UI/templates/js/Input/Field/radioInput.js
+++ src/UI/templates/js/Input/Field/radioInput.js
@@ -12,26 +12,32 @@ il.UI.Input = il.UI.Input || {};
     il.UI.Input.radio = (function ($) {
         var init = function (id) {
             var id = '#' + id,
-                options = $(id).children('input:radio');
+                options = $(id).find('input:radio');
+
             options.change(function(){_select(id);});
             _hidegroups(id);
         };
 
         var _current = function (id) {
-            return $(id).children('input:radio:checked').val()
+            return $(id).children('.il-input-radiooption').children('input:radio:checked').val();
         }
+
         var _hidegroups = function (id) {
             var current = _current(id),
                 visible_id = id.substr(1) + '_' + current + '_group';
-            $(id).children('.il-dependant-group').each(function(i, group) {
-                if(group.id !== visible_id) {
-                    $(group).hide();
+
+            $(id).children('.il-input-radiooption').children('.il-dependant-group').each(
+                function(i, group) {
+                    if(group.id !== visible_id) {
+                        $(group).hide();
+                    }
                 }
-            });
+            );
         }
         var _select = function (id) {
             var value = _current(id),
                 group_id = id + '_' + value + '_group';
+
             _hidegroups(id);
              $(group_id).show();
         };
@@ -41,3 +47,4 @@ il.UI.Input = il.UI.Input || {};
         };
 
     })($);
+})($, il.UI.Input);
