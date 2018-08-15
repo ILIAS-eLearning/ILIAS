da9191799670ad0d43b4192a4b6837a833da2711
mjansen <mjansen@databay.de>
2018-08-15 12:42:20 +0200

    Terms of Service: Prepared unit tests

---
+++ Services/TermsOfService/test/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'libs/composer/vendor/autoload.php';
+require_once 'Services/TermsOfService/test/ilTermsOfServiceBaseTest.php';
+require_once 'Services/TermsOfService/test/factories/ilTermsOfServiceEntityFactoryTest.php';
+require_once 'Services/TermsOfService/test/factories/ilTermsOfServiceDataGatewayFactoryTest.php';
+require_once 'Services/TermsOfService/test/factories/ilTermsOfServiceTableDataProviderFactoryTest.php';
+require_once 'Services/TermsOfService/test/provider/ilTermsOfServiceAcceptanceHistoryProviderTest.php';
+require_once 'Services/TermsOfService/test/entities/ilTermsOfServiceAcceptanceEntityTest.php';
+require_once 'Services/TermsOfService/test/gateways/ilTermsOfServiceAcceptanceDatabaseGatewayTest.php';
--- Services/TermsOfService/test/ilServicesTermsOfServiceSuite.php
+++ Services/TermsOfService/test/ilServicesTermsOfServiceSuite.php
@@ -1,7 +1,7 @@
 <?php
 /* Copyright (c) 1998-2018 ILIAS open source, Extended GPL, see docs/LICENSE */
 
-require_once 'libs/composer/vendor/autoload.php';
+require_once __DIR__ . '/bootstrap.php';
 
 /**
  * Class ilServicesTermsOfServiceSuite
@@ -16,24 +16,16 @@ class ilServicesTermsOfServiceSuite extends \PHPUnit_Framework_TestSuite
 	{
 		$suite = new self();
 
-		require_once 'Services/TermsOfService/test/ilTermsOfServiceBaseTest.php';
-
-		require_once 'Services/TermsOfService/test/factories/ilTermsOfServiceEntityFactoryTest.php';
 		$suite->addTestSuite('ilTermsOfServiceEntityFactoryTest');
 
-		require_once 'Services/TermsOfService/test/factories/ilTermsOfServiceDataGatewayFactoryTest.php';
 		$suite->addTestSuite('ilTermsOfServiceDataGatewayFactoryTest');
 
-		require_once 'Services/TermsOfService/test/factories/ilTermsOfServiceTableDataProviderFactoryTest.php';
 		$suite->addTestSuite('ilTermsOfServiceTableDataProviderFactoryTest');
 
-		require_once 'Services/TermsOfService/test/provider/ilTermsOfServiceAcceptanceHistoryProviderTest.php';
 		$suite->addTestSuite('ilTermsOfServiceAcceptanceHistoryProviderTest');
 
-		require_once 'Services/TermsOfService/test/entities/ilTermsOfServiceAcceptanceEntityTest.php';
 		$suite->addTestSuite('ilTermsOfServiceAcceptanceEntityTest');
 
-		require_once 'Services/TermsOfService/test/gateways/ilTermsOfServiceAcceptanceDatabaseGatewayTest.php';
 		$suite->addTestSuite('ilTermsOfServiceAcceptanceDatabaseGatewayTest');
 
 		return $suite;
