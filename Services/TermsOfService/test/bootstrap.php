de581137ce73acded13cd3c48ba7d12bae2293cb
mjansen <mjansen@databay.de>
2018-08-16 14:57:24 +0200

    Terms of Service: Auto test discovery

--- Services/TermsOfService/test/bootstrap.php
+++ Services/TermsOfService/test/bootstrap.php
@@ -1,22 +1,3 @@
 <?php
 require_once 'libs/composer/vendor/autoload.php';
-
-require_once 'Services/TermsOfService/test/ilTermsOfServiceBaseTest.php';
-require_once 'Services/TermsOfService/test/criteria/ilTermsOfServiceCriterionBaseTest.php';
-require_once 'Services/TermsOfService/test/evaluation/ilTermsOfServiceEvaluationBaseTest.php';
-
-require_once 'Services/TermsOfService/test/factories/ilTermsOfServiceEntityFactoryTest.php';
-require_once 'Services/TermsOfService/test/factories/ilTermsOfServiceDataGatewayFactoryTest.php';
-require_once 'Services/TermsOfService/test/factories/ilTermsOfServiceTableDataProviderFactoryTest.php';
-require_once 'Services/TermsOfService/test/factories/ilTermsOfServiceCriterionTypeFactoryTest.php';
-require_once 'Services/TermsOfService/test/provider/ilTermsOfServiceAcceptanceHistoryProviderTest.php';
-require_once 'Services/TermsOfService/test/entities/ilTermsOfServiceAcceptanceEntityTest.php';
-require_once 'Services/TermsOfService/test/gateways/ilTermsOfServiceAcceptanceDatabaseGatewayTest.php';
-require_once 'Services/TermsOfService/test/evaluation/ilTermsOfServiceDocumentEvaluationTest.php';
-require_once 'Services/TermsOfService/test/evaluation/ilTermsOfServiceDocumentCriteriaEvaluationTest.php';
-require_once 'Services/TermsOfService/test/criteria/ilTermsOfServiceUserHasLanguageCriterionTest.php';
-require_once 'Services/TermsOfService/test/criteria/ilTermsOfServiceUserHasGlobalRoleCriterionTest.php';
-require_once 'Services/TermsOfService/test/criteria/ilTermsOfServiceNullCriterionTest.php';
-require_once 'Services/TermsOfService/test/criteria/ilTermsOfServiceCriterionConfigTest.php';
-require_once 'Services/TermsOfService/test/criteria/ilTermsOfServiceDocumentCriterionAssignmentConstraintTest.php';
-require_once 'Services/TermsOfService/test/history/ilTermsOfServiceAcceptanceHistoryCriteriaBagTest.php';
+require_once 'ilTermsOfServiceBaseTest.php';
--- Services/TermsOfService/test/ilServicesTermsOfServiceSuite.php
+++ Services/TermsOfService/test/ilServicesTermsOfServiceSuite.php
@@ -16,21 +16,34 @@ class ilServicesTermsOfServiceSuite extends \PHPUnit_Framework_TestSuite
 	{
 		$suite = new self();
 
-		$suite->addTestSuite('ilTermsOfServiceEntityFactoryTest');
-		$suite->addTestSuite('ilTermsOfServiceDataGatewayFactoryTest');
-		$suite->addTestSuite('ilTermsOfServiceTableDataProviderFactoryTest');
-		$suite->addTestSuite('ilTermsOfServiceCriterionTypeFactoryTest');
-		$suite->addTestSuite('ilTermsOfServiceAcceptanceHistoryProviderTest');
-		$suite->addTestSuite('ilTermsOfServiceAcceptanceEntityTest');
-		$suite->addTestSuite('ilTermsOfServiceAcceptanceDatabaseGatewayTest');
-		$suite->addTestSuite('ilTermsOfServiceDocumentEvaluationTest');
-		$suite->addTestSuite('ilTermsOfServiceDocumentCriteriaEvaluationTest');
-		$suite->addTestSuite('ilTermsOfServiceUserHasLanguageCriterionTest');
-		$suite->addTestSuite('ilTermsOfServiceUserHasGlobalRoleCriterionTest');
-		$suite->addTestSuite('ilTermsOfServiceNullCriterionTest');
-		$suite->addTestSuite('ilTermsOfServiceCriterionConfigTest');
-		$suite->addTestSuite('ilTermsOfServiceAcceptanceHistoryCriteriaBagTest');
-		$suite->addTestSuite('ilTermsOfServiceDocumentCriterionAssignmentConstraintTest');
+		foreach (new \RegExIterator(
+					 new \RecursiveIteratorIterator(
+						 new \RecursiveDirectoryIterator(__DIR__, \FilesystemIterator::SKIP_DOTS),
+						 \RecursiveIteratorIterator::LEAVES_ONLY
+					 ), '/BaseTest\.php$/') as $file) {
+			/** @var \SplFileInfo $file */
+			require_once $file->getPathname();
+		}
+
+		foreach (new \RegExIterator(
+			new \RecursiveIteratorIterator(
+				new \RecursiveDirectoryIterator(__DIR__, \FilesystemIterator::SKIP_DOTS),
+				\RecursiveIteratorIterator::LEAVES_ONLY
+			), '/(?<!Base)Test\.php$/') as $file) {
+			/** @var \SplFileInfo $file */
+			require_once $file->getPathname();
+
+			$className = preg_replace('/(.*?)(\.php)/', '$1', $file->getBasename());
+			if (class_exists($className)) {
+				$reflection = new \ReflectionClass($className);
+				if (
+					!$reflection->isAbstract() &&
+					!$reflection->isInterface() &&
+					$reflection->isSubclassOf(\PHPUnit_Framework_TestCase::class)) {
+					$suite->addTestSuite($className);
+				}
+			}
+		}
 
 		return $suite;
 	}
