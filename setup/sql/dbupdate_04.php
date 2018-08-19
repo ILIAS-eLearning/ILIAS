e00be31ac2110b805f36b12ad582ad3e1f729b92
Björn Heyser <bh@bjoernheyser.de>
2018-08-19 13:09:59 +0200

    org unit operation registrations for ou sensitive test object

--- setup/sql/dbupdate_04.php
+++ setup/sql/dbupdate_04.php
@@ -22525,3 +22525,38 @@ if (!$ilrqtix) {
 <?php
 $ilCtrlStructureReader->getStructure();
 ?>
+<#5292> 
+<?php
+try
+{
+	require_once 'Modules/OrgUnit/classes/Positions/Operation/class.ilOrgUnitOperationQueries.php';
+
+	ilOrgUnitOperationQueries::registerNewOperation(
+		ilOrgUnitOperation::OP_READ_LEARNING_PROGRESS,
+		'Read Test Participants Learning Progress',
+		ilOrgUnitOperationContext::CONTEXT_TST
+	);
+	
+	ilOrgUnitOperationQueries::registerNewOperation(
+		ilOrgUnitOperation::OP_ACCESS_RESULTS,
+		'Access Test Participants Results',
+		ilOrgUnitOperationContext::CONTEXT_TST
+	);
+	
+	ilOrgUnitOperationQueries::registerNewOperation(
+		ilOrgUnitOperation::OP_MANAGE_PARTICIPANTS,
+		'Manage Test Participants',
+		ilOrgUnitOperationContext::CONTEXT_TST
+	);
+	
+	ilOrgUnitOperationQueries::registerNewOperation(
+		ilOrgUnitOperation::OP_SCORE_PARTICIPANTS,
+		'Score Test Participants',
+		ilOrgUnitOperationContext::CONTEXT_TST
+	);
+	
+}
+catch(ilException $e)
+{
+}
+?>
