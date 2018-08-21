3365eb4979296a100eb9d8966681a49b988b106e
Richard Klees <richard.klees@concepts-and-training.de>
2018-08-21 13:57:47 +0200

    PHPUnit: forward args in run_tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cp Services/PHPUnit/config/cfg.phpunit.template.php Services/PHPUnit/config/cfg.phpunit.php
-libs/composer/vendor/phpunit/phpunit/phpunit --bootstrap ./libs/composer/vendor/autoload.php --configuration ./Services/PHPUnit/config/PhpUnitConfig.xml --exclude-group needsInstalledILIAS;
+libs/composer/vendor/phpunit/phpunit/phpunit --bootstrap ./libs/composer/vendor/autoload.php --configuration ./Services/PHPUnit/config/PhpUnitConfig.xml --exclude-group needsInstalledILIAS $@;
