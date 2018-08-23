f987c3866e5d8d615e8b10a09f6264ccadc3955e
Lukas Zehnder <lz@studer-raimann.ch>
2018-08-23 08:06:48 +0200

    Fixed wrong type/action after importing file

--- Modules/File/classes/class.ilFileXMLWriter.php
+++ Modules/File/classes/class.ilFileXMLWriter.php
@@ -120,7 +120,8 @@ class ilFileXMLWriter extends ilXmlWriter {
 			"obj_id" => "il_" . IL_INST_ID . "_file_" . $this->file->getId(),
 			"version" => $this->file->getVersion(),
 			"size" => $this->file->getFileSize(),
-			"type" => $this->file->getFileType()
+			"type" => $this->file->getFileType(),
+			"action" => $this->file->getAction()
 		);
 
 		$this->xmlStartTag("File", $attribs);
@@ -141,7 +142,10 @@ class ilFileXMLWriter extends ilXmlWriter {
 				$attribs = array(
 					"version" => $version["version"],
 					"date" => ilUtil::date_mysql2time($version["date"]),
-					"usr_id" => "il_" . IL_INST_ID . "_usr_" . $version["user_id"]
+					"usr_id" => "il_" . IL_INST_ID . "_usr_" . $version["user_id"],
+					"action" => $version["action"],
+					"rollback_version" => $version["rollback_version"],
+					"rollback_user_id" => $version["rollback_user_id"]
 				);
 
 				$content = "";
