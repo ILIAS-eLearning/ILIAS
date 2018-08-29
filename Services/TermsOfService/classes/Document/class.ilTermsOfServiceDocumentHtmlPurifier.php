36738b891e7845482bc99679bb92a617e292e876
mjansenDatabay <mjansen@databay.de>
2018-08-29 20:29:46 +0200

    Terms of Service: Made HTML purifier testable

--- Services/TermsOfService/classes/Document/class.ilTermsOfServiceDocumentHtmlPurifier.php
+++ Services/TermsOfService/classes/Document/class.ilTermsOfServiceDocumentHtmlPurifier.php
@@ -7,24 +7,50 @@
  */
 class ilTermsOfServiceDocumentHtmlPurifier extends \ilHtmlPurifierAbstractLibWrapper
 {
+	/** @var array */
+	private $allowedTags = [];
+
+	/** @var string */
+	protected $cacheDirectory = '';
+
+	/**
+	 * ilTermsOfServiceDocumentHtmlPurifier constructor.
+	 * @param array|null $allowedTags
+	 * @param string     $cacheDirectory
+	 */
+	public function __construct(array $allowedTags = null, string $cacheDirectory = null)
+	{
+		if (null === $cacheDirectory) {
+			$cacheDirectory = \ilHtmlPurifierAbstractLibWrapper::_getCacheDirectory();
+		}
+		$this->cacheDirectory = $cacheDirectory;
+
+		if (null === $allowedTags) {
+			$allowedTags =  \ilObjAdvancedEditing::_getUsedHTMLTags('textarea');
+		}
+		$this->allowedTags = $allowedTags;
+
+		parent::__construct();
+		$this->allowedTags = $allowedTags;
+	}
+
 	/**
 	 * @inheritdoc
 	 */
 	protected function getPurifierConfigInstance()
 	{
-		$config = HTMLPurifier_Config::createDefault();
+		$config = \HTMLPurifier_Config::createDefault();
 		$config->set('HTML.DefinitionID', 'ilias termsofservice document');
 		$config->set('HTML.DefinitionRev', 1);
 		$config->set('HTML.TargetBlank', true);
-		$config->set('Cache.SerializerPath', \ilHtmlPurifierAbstractLibWrapper::_getCacheDirectory());
+		$config->set('Cache.SerializerPath', $this->cacheDirectory);
 		$config->set('HTML.Doctype', 'XHTML 1.0 Strict');
 
-		$tags = \ilObjAdvancedEditing::_getUsedHTMLTags('textarea');
+		$tags = $this->allowedTags;
 		$tags = $this->makeElementListTinyMceCompliant($tags);
 
 		$tags[] = 'b';
 		$tags[] = 'i';
-		$tags[] = 'u';
 
 		$config->set('HTML.AllowedElements', $this->removeUnsupportedElements($tags));
 		$config->set('HTML.ForbiddenAttributes', 'div@style');
