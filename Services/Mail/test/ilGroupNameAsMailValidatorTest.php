31944c46c7eb15bf614124646a3ca7c3ef40ac0b
mjansen <mjansen@databay.de>
2018-09-06 12:03:33 +0200

    Mail: Unit Tests

--- Services/Mail/test/ilGroupNameAsMailValidatorTest.php
+++ Services/Mail/test/ilGroupNameAsMailValidatorTest.php
@@ -1,9 +1,34 @@
 <?php
+/* Copyright (c) 1998-2018 ILIAS open source, Extended GPL, see docs/LICENSE */
 
+/**
+ * Class ilGroupNameAsMailValidatorTest
+ * @author Niels Theen <ntheen@databay.de>
+ * @author Michael Jansen <mjansen@databay.de>
+ */
 class ilGroupNameAsMailValidatorTest extends \ilMailBaseTest
 {
-	public function testObjectCanBeCreated()
+	/**
+	 * 
+	 */
+	public function testGroupIsDetectedIfGroupNameExists()
 	{
-		$validator = new ilGroupNameAsMailValidator('someHost');
+		$validator = new \ilGroupNameAsMailValidator('someHost', function(string $groupName) {
+			return true;
+		});
+
+		$this->assertTrue($validator->validate(new ilMailAddress('phpunit', 'someHost')));
+	}
+
+	/**
+	 *
+	 */
+	public function testGroupIsNotDetectedIfGroupNameDoesNotExists()
+	{
+		$validator = new \ilGroupNameAsMailValidator('someHost', function(string $groupName) {
+			return false;
+		});
+
+		$this->assertFalse($validator->validate(new ilMailAddress('someHost', 'someHost')));
 	}
 }
