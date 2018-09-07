afc6fd897a920aa158bf6833d13b07dad2e24f7d
Niels Theen <ntheen@databay.de>
2018-09-07 12:56:34 +0200

    Skill is currently not supported for certificates

--- Services/Certificate/classes/File/class.ilCertificatePathConstants.php
+++ Services/Certificate/classes/File/class.ilCertificatePathConstants.php
@@ -6,6 +6,5 @@ class ilCertificatePathConstants
 	const COURSE_PATH    = '/course/certificates/';
 	const TEST_PATH      = '/assessment/certificates/';
 	const EXERCISE_PATH  = '/exercise/certificates/';
-	const SKILL_PATH     = '/certificates/skill/';
 	const SCORM_PATH     = '/certificates/scorm/';
 }
