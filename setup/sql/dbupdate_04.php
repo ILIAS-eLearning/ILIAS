5e2e1108bfde982c864ed64fc7c0258c1626fb5a
Niels Theen <ntheen@databay.de>
2018-09-07 17:03:52 +0200

    Fix double variable

--- setup/sql/dbupdate_04.php
+++ setup/sql/dbupdate_04.php
@@ -22785,7 +22785,7 @@ $directories = array(
 foreach ($directories as $type => $relativePath) {
 	$directory = $web_path . $relativePath;
 
-	if (false === file_exists($directory))
+	if (false === file_exists($directory)) {
 		continue;
 	}
 
