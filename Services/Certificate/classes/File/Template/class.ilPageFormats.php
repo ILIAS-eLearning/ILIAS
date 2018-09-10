fa81ca5386fc60f644e8576d851eaa7a8483388b
Niels Theen <ntheen@databay.de>
2018-09-10 14:13:15 +0200

    Unit test for page formats

--- Services/Certificate/classes/File/Template/class.ilPageFormats.php
+++ Services/Certificate/classes/File/Template/class.ilPageFormats.php
@@ -21,47 +21,47 @@ class ilPageFormats
 	public function fetchPageFormats()
 	{
 		return array(
-			"a4" => array(
-				"name" => $this->language->txt("certificate_a4"), // (297 mm x 210 mm)
-				"value" => "a4",
-				"width" => "210mm",
-				"height" => "297mm"
+			'a4' => array(
+				'name' => $this->language->txt('certificate_a4'), // (297 mm x 210 mm)
+				'value' => 'a4',
+				'width' => '210mm',
+				'height' => '297mm'
 			),
-			"a4landscape" => array(
-				"name" => $this->language->txt("certificate_a4_landscape"), // (210 mm x 297 mm)",
-				"value" => "a4landscape",
-				"width" => "297mm",
-				"height" => "210mm"
+			'a4landscape' => array(
+				'name' => $this->language->txt('certificate_a4_landscape'), // (210 mm x 297 mm)',
+				'value' => 'a4landscape',
+				'width' => '297mm',
+				'height' => '210mm'
 			),
-			"a5" => array(
-				"name" => $this->language->txt("certificate_a5"), // (210 mm x 148.5 mm)
-				"value" => "a5",
-				"width" => "148mm",
-				"height" => "210mm"
+			'a5' => array(
+				'name' => $this->language->txt('certificate_a5'), // (210 mm x 148.5 mm)
+				'value' => 'a5',
+				'width' => '148mm',
+				'height' => '210mm'
 			),
-			"a5landscape" => array(
-				"name" => $this->language->txt("certificate_a5_landscape"), // (148.5 mm x 210 mm)
-				"value" => "a5landscape",
-				"width" => "210mm",
-				"height" => "148mm"
+			'a5landscape' => array(
+				'name' => $this->language->txt('certificate_a5_landscape'), // (148.5 mm x 210 mm)
+				'value' => 'a5landscape',
+				'width' => '210mm',
+				'height' => '148mm'
 			),
-			"letter" => array(
-				"name" => $this->language->txt("certificate_letter"), // (11 inch x 8.5 inch)
-				"value" => "letter",
-				"width" => "8.5in",
-				"height" => "11in"
+			'letter' => array(
+				'name' => $this->language->txt('certificate_letter'), // (11 inch x 8.5 inch)
+				'value' => 'letter',
+				'width' => '8.5in',
+				'height' => '11in'
 			),
-			"letterlandscape" => array(
-				"name" => $this->language->txt("certificate_letter_landscape"), // (8.5 inch x 11 inch)
-				"value" => "letterlandscape",
-				"width" => "11in",
-				"height" => "8.5in"
+			'letterlandscape' => array(
+				'name' => $this->language->txt('certificate_letter_landscape'), // (8.5 inch x 11 inch)
+				'value' => 'letterlandscape',
+				'width' => '11in',
+				'height' => '8.5in'
 			),
-			"custom" => array(
-				"name" => $this->language->txt("certificate_custom"),
-				"value" => "custom",
-				"width" => "",
-				"height" => ""
+			'custom' => array(
+				'name' => $this->language->txt('certificate_custom'),
+				'value' => 'custom',
+				'width' => '',
+				'height' => ''
 			)
 		);
 	}
