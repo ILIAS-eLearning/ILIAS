7465a2fe1a486c903498ec59ae6d9ce2a6ba870e
smeyer-ilias <smeyer.ilias@gmx.de>
2018-09-12 14:19:15 +0200

    autoload

--- libs/composer/vendor/composer/autoload_real.php
+++ libs/composer/vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit2fffdf922cf8fdbf1f62eec345993c83
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit2fffdf922cf8fdbf1f62eec345993c83', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
