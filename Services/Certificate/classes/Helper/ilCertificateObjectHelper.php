1a99186845f0faf0dde57d7d0fdcd21e46e41939
Niels Theen <ntheen@databay.de>
2018-09-13 15:42:16 +0200

    Missing method

--- Services/Certificate/classes/Helper/ilCertificateObjectHelper.php
+++ Services/Certificate/classes/Helper/ilCertificateObjectHelper.php
@@ -14,4 +14,13 @@ class ilCertificateObjectHelper
 	{
 		return ilObjectFactory::getInstanceByObjId($objectId);
 	}
+
+	/**
+	 * @param int $refId
+	 * @return int
+	 */
+	public function lookupObjId(int $refId) : int
+	{
+		return ilObject::_lookupObjId($refId);
+	}
 }
