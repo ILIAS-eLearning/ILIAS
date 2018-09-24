56346c2e695f3e3cabfdc59287a922c7e8fc7304
Niels Theen <ntheen@databay.de>
2018-09-24 13:38:30 +0200

    Add Unittest to user preloader

---
+++ Services/Certificate/test/ilCertificateObjectsForUserPreloaderTest.php
@@ -0,0 +1,62 @@
+<?php
+/* Copyright (c) 1998-2018 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+/**
+ * @author  Niels Theen <ntheen@databay.de>
+ */
+class ilCertificateObjectsForUserPreloaderTest extends PHPUnit_Framework_TestCase
+{
+	public function testUsersWithCertifcatesWillBePreoloaded()
+	{
+		$userCertificateRepository = $this->getMockBuilder('ilUserCertificateRepository')
+			->disableOriginalConstructor()
+			->getMock();
+
+		$userCertificateRepository->method('fetchObjectWithCertificateForUser')
+			->willReturn(array(1, 2, 3));
+
+		$preloader = new ilCertificateObjectsForUserPreloader($userCertificateRepository);
+
+		$preloader->preLoad(100, array(500, 200));
+
+		$result = $preloader->isPreloaded(100, 1);
+
+		$this->assertTrue($result);
+	}
+
+	public function testUserWithCertificateIsNotPreloaded()
+	{
+		$userCertificateRepository = $this->getMockBuilder('ilUserCertificateRepository')
+			->disableOriginalConstructor()
+			->getMock();
+
+		$userCertificateRepository->method('fetchObjectWithCertificateForUser')
+			->willReturn(array(1, 2, 3));
+
+		$preloader = new ilCertificateObjectsForUserPreloader($userCertificateRepository);
+
+		$preloader->preLoad(100, array(500, 200));
+
+		$result = $preloader->isPreloaded(100, 5);
+
+		$this->assertFalse($result);
+	}
+
+	public function testUserIsNoProloaded()
+	{
+		$userCertificateRepository = $this->getMockBuilder('ilUserCertificateRepository')
+			->disableOriginalConstructor()
+			->getMock();
+
+		$userCertificateRepository->method('fetchObjectWithCertificateForUser')
+			->willReturn(array(1, 2, 3));
+
+		$preloader = new ilCertificateObjectsForUserPreloader($userCertificateRepository);
+
+		$preloader->preLoad(100, array(500, 200));
+
+		$result = $preloader->isPreloaded(200, 1);
+
+		$this->assertFalse($result);
+	}
+}
