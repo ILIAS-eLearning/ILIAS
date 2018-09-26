e95ae1b628db09295a168c2687edc00b6658d5ae
Alexander Killing <killing@leifos.de>
2018-09-26 12:18:59 +0200

    fixed bug #23640: Images using imagemaps are wrongly scaled; this bug is also on the agenda due to the use of the patched directory and the general process

--- Services/jQuery/classes/class.iljQueryUtil.php
+++ Services/jQuery/classes/class.iljQueryUtil.php
@@ -90,6 +90,7 @@ class iljQueryUtil {
 	 * Get local path of maphilight file
 	 */
 	public static function getLocalMaphilightPath() {
-		return "./libs/bower/bower_components/maphilight/jquery.maphilight.min.js";
+		return "./libs/patched/maphilight_14_03_20/maphilight.js";
+		//return "./libs/bower/bower_components/maphilight/jquery.maphilight.min.js";
 	}
 }
