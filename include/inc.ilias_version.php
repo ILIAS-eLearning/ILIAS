92fc9cc23fc8304be1e6106dfc0873107d070681
Fabian Schmid <fs@studer-raimann.ch>
2018-10-04 12:23:35 +0200

    [RELEASE] packed ILIAS 5.3.10

--- include/inc.ilias_version.php
+++ include/inc.ilias_version.php
@@ -9,5 +9,5 @@
 *
 * @package ilias-core
 */
-define("ILIAS_VERSION", "5.3.9 2018-09-29");
-define("ILIAS_VERSION_NUMERIC", "5.3.9");			// must be always x.y.z: x, y and z are numbers
+define("ILIAS_VERSION", "5.3.10 2018-10-04");
+define("ILIAS_VERSION_NUMERIC", "5.3.10");			// must be always x.y.z: x, y and z are numbers
