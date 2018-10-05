8893f8e9081a398846eeec37a27ebaa394d87647
Fabian Schmid <fs@studer-raimann.ch>
2018-10-05 11:29:23 +0200

    adjustments in PHPDoc

--- src/GlobalScreen/Provider/StaticProvider/StaticMainMenuProvider.php
+++ src/GlobalScreen/Provider/StaticProvider/StaticMainMenuProvider.php
@@ -1,7 +1,7 @@
 <?php namespace ILIAS\GlobalScreen\Provider\StaticProvider;
 
 use ILIAS\GlobalScreen\MainMenu\isEntry;
-use ILIAS\GlobalScreen\MainMenu\Slate\SlateInterfaceInterface;
+use ILIAS\GlobalScreen\MainMenu\Slate\Slate;
 use ILIAS\GlobalScreen\Provider\StaticProvider;
 
 /**
@@ -12,7 +12,7 @@ use ILIAS\GlobalScreen\Provider\StaticProvider;
 interface StaticMainMenuProvider extends StaticProvider {
 
 	/**
-	 * @return SlateInterfaceInterface[] These are Slates which will be
+	 * @return Slate[] These are Slates which will be
 	 * available for configuration and will be collected once during a
 	 * StructureReload.
 	 */
