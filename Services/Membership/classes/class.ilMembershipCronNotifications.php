b46047e87a41598767cf39a1bd5398cf6fd0ec70
Alexander Killing <killing@leifos.de>
2018-10-10 16:16:39 +0200

    cherry picked mail fix from trunk

--- Services/Membership/classes/class.ilMembershipCronNotifications.php
+++ Services/Membership/classes/class.ilMembershipCronNotifications.php
@@ -378,6 +378,7 @@ class ilMembershipCronNotifications extends ilCronJob
 		$ilUser = $DIC['ilUser'];
 		$ilClientIniFile = $DIC['ilClientIniFile'];
 		$tree = $DIC['tree'];
+		$log = $DIC->logger()->mmbr();
 		
 		include_once "./Services/Notification/classes/class.ilSystemNotification.php";
 		$ntf = new ilSystemNotification();		
@@ -479,6 +480,7 @@ class ilMembershipCronNotifications extends ilCronJob
 		// #10044
 		$mail = new ilMail(ANONYMOUS_USER_ID);
 		$mail->enableSOAP(false); // #10410
+		$log->debug("Send mail to ".ilObjUser::_lookupLogin($a_user_id));
 		$mail->sendMail(ilObjUser::_lookupLogin($a_user_id), 
 			null, 
 			null,
