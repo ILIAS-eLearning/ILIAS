cfa37f9e1d589f0d55d135523d54eebce8f53c0f
Björn Heyser <bh@bjoernheyser.de>
2018-10-10 14:08:14 +0200

    started completing the offline presentation interface

---
+++ Services/AssessmentQuestion/interfaces/interface.ilAsqQuestionOfflinePresentationExporter.php
@@ -0,0 +1,7 @@
+<?php
+/* Copyright (c) 1998-2013 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+interface ilAsqQuestionOfflinePresentationExporter
+{
+	public function exportQuestion();
+}
