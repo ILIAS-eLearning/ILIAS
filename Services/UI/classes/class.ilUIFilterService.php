40a577c78082f65ea7ee2a1e971fe76bfcba1283
Thomas Famula <famula@leifos.de>
2018-10-11 13:31:19 +0200

    Still basic Filter Service ideas

--- Services/UI/classes/class.ilUIFilterService.php
+++ Services/UI/classes/class.ilUIFilterService.php
@@ -11,9 +11,15 @@
 class ilUIFilterService
 {
 	/**
+	 * @var ilUIService
+	 */
+	protected $service;
+
+	/**
 	 * @var ilUIServiceDependencies
 	 */
 	protected $_deps;
+
 	/**
 	 * Constructor
 	 * @param ilUIService $service
@@ -21,8 +27,8 @@ class ilUIFilterService
 	 */
 	public function __construct(ilUIService $service, ilUIServiceDependencies $deps)
 	{
-		$this->_deps = $deps;
 		$this->service = $service;
+		$this->_deps = $deps;
 	}
 
 
@@ -44,26 +50,74 @@ class ilUIFilterService
 
 		// read cmdFilter from request and update session data
 
-		if ($_REQUEST["cmdFilter"] == "expand")
-		{
-			ilSession::set("ui_service_filter_expanded_".$filter_id, 1);
+		if ($_REQUEST["cmdFilter"] == "toggleOn") {
+			ilSession::set("ui_service_filter_activated_".$filter_id, true);
 		}
 
-		// daten von session holen
-		$is_expanded = ilSession::get("ui_service_filter_expanded");
+		if ($_REQUEST["cmdFilter"] == "toggleOff") {
+			ilSession::set("ui_service_filter_activated_".$filter_id, false);
+		}
 
-		//Step 3: Define the filter and attach the inputs. The filter is initially activated in this case.
+		if ($_REQUEST["cmdFilter"] == "expand") {
+			ilSession::set("ui_service_filter_expanded_".$filter_id, true);
+		}
+
+		if ($_REQUEST["cmdFilter"] == "collapse") {
+			ilSession::set("ui_service_filter_expanded_".$filter_id, false);
+		}
+
+
+		$input_id = 0;
+
+		if ($_REQUEST["cmdFilter"] == "apply") {
+			foreach ($is_input_rendered as $i) {
+				if ($i == true) {
+					ilSession::set("ui_service_filter_is_input_rendered_" . $input_id . "_" . $filter_id, true);
+				} else {
+					ilSession::set("ui_service_filter_is_input_rendered_" . $input_id . "_" . $filter_id, false);
+				}
+				$input_id++;
+			}
+		}
+
+		if ($_REQUEST["cmdFilter"] == "reset") {
+			foreach ($is_input_rendered as $i) {
+					ilSession::clear("ui_service_filter_is_input_rendered_" . $input_id . "_" . $filter_id);
+				$input_id++;
+			}
+		}
+
+
+		// get data from session
+
+		$is_activated = ilSession::get("ui_service_filter_activated_".$filter_id);
+		$is_expanded = ilSession::get("ui_service_filter_expanded_".$filter_id);
+
+		//compose a new array because rendering of inputs has eventually changed
+		if (ilSession::get("ui_service_filter_is_input_rendered_0" . "_" . $filter_id) != null) {
+			$is_input_rendered = array();
+			for ($i = 0; $i <= $input_id; $i++) {
+				$is_input_rendered[] = ilSession::get("ui_service_filter_is_input_rendered_" . $i . "_" . $filter_id);
+			}
+		}
+
+
+		// create the KS Filter
+
 		$filter = $ui->input()->container()->filter()->standard(
 			$base_action."&cmdFilter=toggleOn",
 			$base_action."&cmdFilter=toggleOff",
 			$base_action."&cmdFilter=expand",
 			$base_action."&cmdFilter=collapse",
-			$base_action."&cmdFilter=reset",
 			$base_action."&cmdFilter=apply",
+			$base_action."&cmdFilter=reset",
 			$inputs,
-			$is_input_rendered, true);
+			$is_input_rendered,
+			$is_activated,
+			$is_expanded);
+
+		return $filter;
 
 	}
-
 
 }
