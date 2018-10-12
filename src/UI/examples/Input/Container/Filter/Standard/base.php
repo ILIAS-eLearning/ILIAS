8f91622c69f50e516dd0d254bf8026bcfe8ea976
Thomas Famula <famula@leifos.de>
2018-10-12 10:20:52 +0200

    Still basic Filter Service ideas 2

--- src/UI/examples/Input/Container/Filter/Standard/base.php
+++ src/UI/examples/Input/Container/Filter/Standard/base.php
@@ -15,9 +15,8 @@ function base() {
 	$numeric_input2 = $ui->input()->field()->numeric("Number 2");
 
 	//Step 3: Define the filter and attach the inputs. The filter is initially activated in this case.
-	$filter = $ui->input()->container()->filter()->standard("#", "#", "#",
-		"#","#", "#", [$text_input1, $text_input2, $numeric_input1, $numeric_input2],
-		[true, true, true, true], true);
+	$filter = $DIC->uiService()->filter()->standard("filter_ID", $_SERVER['REQUEST_URI'], [$text_input1, $text_input2, $numeric_input1, $numeric_input2],
+		[true, true, true, true], true, true);
 
 	//Step 4: Render the filter
 	return $renderer->render($filter);
