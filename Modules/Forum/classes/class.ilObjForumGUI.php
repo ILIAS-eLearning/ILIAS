e068a0555fec936b0777fa30bf644cf898bdf4d7
mjansen <mjansen@databay.de>
2018-10-23 08:30:21 +0200

    Forum: Fixed #23760

--- Modules/Forum/classes/class.ilObjForumGUI.php
+++ Modules/Forum/classes/class.ilObjForumGUI.php
@@ -2245,19 +2245,27 @@ class ilObjForumGUI extends ilObjectGUI implements ilDesktopItemHandling
 	 */
 	public function savePostObject()
 	{
-		
-	
-		if(!isset($_POST['del_file']) || !is_array($_POST['del_file'])) $_POST['del_file'] = array();
-
-		if($this->objCurrentTopic->isClosed())
-		{
-			$_GET['action'] = '';
-			return $this->viewThreadObject();
+		if (!$this->objCurrentTopic->getId()) {
+			\ilUtil::sendFailure($this->lng->txt('frm_action_not_possible_thr_deleted'), true);
+			$this->ctrl->redirect($this);
 		}
 
+		if ($this->objCurrentTopic->isClosed()) {
+			\ilUtil::sendFailure($this->lng->txt('frm_action_not_possible_thr_closed'), true);
+			$this->ctrl->redirect($this);
+		}
+
+		if(!isset($_POST['del_file']) || !is_array($_POST['del_file'])) $_POST['del_file'] = array();
+
 		$oReplyEditForm = $this->getReplyEditForm();
-		if($oReplyEditForm->checkInput())
-		{
+		if ($oReplyEditForm->checkInput()) {
+			if (!$this->objCurrentPost->getId()) {
+				$_GET['action'] = '';
+				\ilUtil::sendFailure($this->lng->txt('frm_action_not_possible_parent_deleted'));
+				$this->viewThreadObject();
+				return;
+			}
+
 			$this->doCaptchaCheck();
 
 			// init objects
@@ -5005,7 +5013,18 @@ $this->doCaptchaCheck();
 	
 	public function saveAsDraftObject()
 	{
+		if (!$this->objCurrentTopic->getId()) {
+			\ilUtil::sendFailure($this->lng->txt('frm_action_not_possible_thr_deleted'), true);
+			$this->ctrl->redirect($this);
+		}
+
+		if ($this->objCurrentTopic->isClosed()) {
+			\ilUtil::sendFailure($this->lng->txt('frm_action_not_possible_thr_closed'), true);
+			$this->ctrl->redirect($this);
+		}
+
 		if(!isset($_POST['del_file']) || !is_array($_POST['del_file'])) $_POST['del_file'] = array();
+
 		$autosave_draft_id = 0;
 		if(ilForumPostDraft::isAutoSavePostDraftAllowed() && isset($_POST['draft_id']))
 		{
@@ -5014,8 +5033,15 @@ $this->doCaptchaCheck();
 		$oReplyEditForm = $this->getReplyEditForm();
 		if($oReplyEditForm->checkInput())
 		{
-$this->doCaptchaCheck();
-			
+			if (!$this->objCurrentPost->getId()) {
+				$_GET['action'] = '';
+				\ilUtil::sendFailure($this->lng->txt('frm_action_not_possible_parent_deleted'));
+				$this->viewThreadObject();
+				return;
+			}
+
+			$this->doCaptchaCheck();
+
 			// init objects
 			$oForumObjects = $this->getForumObjects();
 			/**
