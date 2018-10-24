fc2317e0df5b5cffeaf69c4307aba824d3456026
Alexander Killing <killing@leifos.de>
2018-10-24 18:45:10 +0200

    cp: merged filter changes from 5_4_cat_filters2 up to 9198d37

--- src/UI/templates/js/Input/Container/filter.js
+++ src/UI/templates/js/Input/Container/filter.js
@@ -16,16 +16,21 @@ il.UI = il.UI || {};
 		 * @param id
 		 * @param value_as_string
 		 */
-		var handleChange = function(event, id, value_as_string) {
+		var onFieldUpdate = function(event, id, value_as_string) {
 			var pop_id = $("#" + id).parents(".il-popover").attr("id");
-			$("span[data-target='" + pop_id + "']").html(value_as_string);
+			if (pop_id) {	// we have an already opened popover
+				$("span[data-target='" + pop_id + "']").html(value_as_string);
+			} else {
+				// no popover yet, we are still in the same input group and search for the il-filter-field span
+				$("#" + id).parents(".input-group").find("span.il-filter-field").html(value_as_string);
+			}
 		};
 
 		/**
 		 * Public interface
 		 */
 		return {
-			handleChange: handleChange
+			onFieldUpdate: onFieldUpdate
 		};
 
 	})($);
