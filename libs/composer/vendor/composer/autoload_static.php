4ac021baa6ecf2086686e62004fb1281db63ca48
smeyer-ilias <smeyer.ilias@gmx.de>
2018-10-24 19:34:37 +0200

    Added openid-connect lib

--- libs/composer/vendor/composer/autoload_static.php
+++ libs/composer/vendor/composer/autoload_static.php
@@ -22,6 +22,10 @@ class ComposerStaticInit2fffdf922cf8fdbf1f62eec345993c83
     );
 
     public static $prefixLengthsPsr4 = array (
+        'p' => 
+        array (
+            'phpseclib\\' => 10,
+        ),
         'c' => 
         array (
             'cweagans\\Composer\\' => 18,
@@ -89,6 +93,10 @@ class ComposerStaticInit2fffdf922cf8fdbf1f62eec345993c83
     );
 
     public static $prefixDirsPsr4 = array (
+        'phpseclib\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib',
+        ),
         'cweagans\\Composer\\' => 
         array (
             0 => __DIR__ . '/..' . '/cweagans/composer-patches/src',
@@ -1206,6 +1214,8 @@ class ComposerStaticInit2fffdf922cf8fdbf1f62eec345993c83
         'JaimePerez\\TwigConfigurableI18n\\Twig\\Extensions\\Extension\\I18n' => __DIR__ . '/..' . '/jaimeperez/twig-configurable-i18n/src/Twig/Extensions/Extension/I18n.php',
         'JaimePerez\\TwigConfigurableI18n\\Twig\\Extensions\\Node\\Trans' => __DIR__ . '/..' . '/jaimeperez/twig-configurable-i18n/src/Twig/Extensions/Node/Trans.php',
         'JaimePerez\\TwigConfigurableI18n\\Twig\\Extensions\\TokenParser\\Trans' => __DIR__ . '/..' . '/jaimeperez/twig-configurable-i18n/src/Twig/Extensions/TokenParser/Trans.php',
+        'Jumbojett\\OpenIDConnectClient' => __DIR__ . '/..' . '/jumbojett/openid-connect-php/src/OpenIDConnectClient.php',
+        'Jumbojett\\OpenIDConnectClientException' => __DIR__ . '/..' . '/jumbojett/openid-connect-php/src/OpenIDConnectClient.php',
         'LTI\\ilMainMenuGUI' => __DIR__ . '/../..' . '/../../Services/LTI/classes/class.ilMainMenuGUI.php',
         'LTI\\ilTemplate' => __DIR__ . '/../..' . '/../../Services/LTI/classes/class.ilTemplate.php',
         'League\\Flysystem\\AdapterInterface' => __DIR__ . '/..' . '/league/flysystem/src/AdapterInterface.php',
@@ -3273,6 +3283,7 @@ class ComposerStaticInit2fffdf922cf8fdbf1f62eec345993c83
         'ilAuthFrontendCredentialsCAS' => __DIR__ . '/../..' . '/../../Services/CAS/classes/class.ilAuthFrontendCredentialsCAS.php',
         'ilAuthFrontendCredentialsHTTP' => __DIR__ . '/../..' . '/../../Services/Authentication/classes/Frontend/class.ilAuthFrontendCredentialsHTTP.php',
         'ilAuthFrontendCredentialsLTI' => __DIR__ . '/../..' . '/../../Services/LTI/classes/InternalProvider/class.ilAuthFrontendCredentialsLTI.php',
+        'ilAuthFrontendCredentialsOpenIdConnect' => __DIR__ . '/../..' . '/../../Services/OpenIdConnect/classes/class.ilAuthFrontendCredentialsOpenIdConnect.php',
         'ilAuthFrontendCredentialsSaml' => __DIR__ . '/../..' . '/../../Services/Saml/classes/class.ilAuthFrontendCredentialsSaml.php',
         'ilAuthFrontendCredentialsShibboleth' => __DIR__ . '/../..' . '/../../Services/AuthShibboleth/classes/class.ilAuthFrontendCredentialsShibboleth.php',
         'ilAuthFrontendFactory' => __DIR__ . '/../..' . '/../../Services/Authentication/classes/Frontend/class.ilAuthFrontendFactory.php',
@@ -3298,6 +3309,7 @@ class ComposerStaticInit2fffdf922cf8fdbf1f62eec345993c83
         'ilAuthProviderInterface' => __DIR__ . '/../..' . '/../../Services/Authentication/interfaces/interface.ilAuthProviderInterface.php',
         'ilAuthProviderLDAP' => __DIR__ . '/../..' . '/../../Services/LDAP/classes/class.ilAuthProviderLDAP.php',
         'ilAuthProviderLTI' => __DIR__ . '/../..' . '/../../Services/LTI/classes/InternalProvider/class.ilAuthProviderLTI.php',
+        'ilAuthProviderOpenIdConnect' => __DIR__ . '/../..' . '/../../Services/OpenIdConnect/classes/class.ilAuthProviderOpenIdConnect.php',
         'ilAuthProviderRadius' => __DIR__ . '/../..' . '/../../Services/Radius/classes/class.ilAuthProviderRadius.php',
         'ilAuthProviderSaml' => __DIR__ . '/../..' . '/../../Services/Saml/classes/class.ilAuthProviderSaml.php',
         'ilAuthProviderShibboleth' => __DIR__ . '/../..' . '/../../Services/AuthShibboleth/classes/class.ilAuthProviderShibboleth.php',
@@ -7183,6 +7195,30 @@ class ComposerStaticInit2fffdf922cf8fdbf1f62eec345993c83
         'nusoap_base' => __DIR__ . '/../..' . '/../../Services/SOAPAuth/include/nusoap.php',
         'pdoDB' => __DIR__ . '/../..' . '/../../Services/ActiveRecord/Connector/DataBase/class.pdoDB.php',
         'phpmailerException' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
+        'phpseclib\\Crypt\\AES' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/Crypt/AES.php',
+        'phpseclib\\Crypt\\Base' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/Crypt/Base.php',
+        'phpseclib\\Crypt\\Blowfish' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/Crypt/Blowfish.php',
+        'phpseclib\\Crypt\\DES' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/Crypt/DES.php',
+        'phpseclib\\Crypt\\Hash' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/Crypt/Hash.php',
+        'phpseclib\\Crypt\\RC2' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/Crypt/RC2.php',
+        'phpseclib\\Crypt\\RC4' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/Crypt/RC4.php',
+        'phpseclib\\Crypt\\RSA' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/Crypt/RSA.php',
+        'phpseclib\\Crypt\\Random' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/Crypt/Random.php',
+        'phpseclib\\Crypt\\Rijndael' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/Crypt/Rijndael.php',
+        'phpseclib\\Crypt\\TripleDES' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/Crypt/TripleDES.php',
+        'phpseclib\\Crypt\\Twofish' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/Crypt/Twofish.php',
+        'phpseclib\\File\\ANSI' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/File/ANSI.php',
+        'phpseclib\\File\\ASN1' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/File/ASN1.php',
+        'phpseclib\\File\\ASN1\\Element' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/File/ASN1/Element.php',
+        'phpseclib\\File\\X509' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/File/X509.php',
+        'phpseclib\\Math\\BigInteger' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/Math/BigInteger.php',
+        'phpseclib\\Net\\SCP' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/Net/SCP.php',
+        'phpseclib\\Net\\SFTP' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/Net/SFTP.php',
+        'phpseclib\\Net\\SFTP\\Stream' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/Net/SFTP/Stream.php',
+        'phpseclib\\Net\\SSH1' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/Net/SSH1.php',
+        'phpseclib\\Net\\SSH2' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/Net/SSH2.php',
+        'phpseclib\\System\\SSH\\Agent' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/System/SSH/Agent.php',
+        'phpseclib\\System\\SSH\\Agent\\Identity' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/System/SSH/Agent/Identity.php',
         'shibConfig' => __DIR__ . '/../..' . '/../../Services/AuthShibboleth/classes/Config/class.shibConfig.php',
         'shibServerData' => __DIR__ . '/../..' . '/../../Services/AuthShibboleth/classes/ServerData/class.shibServerData.php',
         'shibUser' => __DIR__ . '/../..' . '/../../Services/AuthShibboleth/classes/User/class.shibUser.php',
---
+++ libs/composer/vendor/jumbojett/openid-connect-php/README.md
@@ -0,0 +1,133 @@
+PHP OpenID Connect Basic Client
+========================
+A simple library that allows an application to authenticate a user through the basic OpenID Connect flow.
+This library hopes to encourage OpenID Connect use by making it simple enough for a developer with little knowledge of
+the OpenID Connect protocol to setup authentication.
+
+A special thanks goes to Justin Richer and Amanda Anganes for their help and support of the protocol.
+
+# Requirements #
+ 1. PHP 5.4 or greater
+ 2. CURL extension
+ 3. JSON extension
+
+## Install ##
+ 1. Install library using composer
+```
+composer require jumbojett/openid-connect-php
+```
+ 2. Include composer autoloader
+```php
+require __DIR__ . '/vendor/autoload.php';
+```
+
+## Example 1: Basic Client ##
+
+```php
+use Jumbojett\OpenIDConnectClient;
+
+$oidc = new OpenIDConnectClient('https://id.provider.com',
+                                'ClientIDHere',
+                                'ClientSecretHere');
+$oidc->setCertPath('/path/to/my.cert');
+$oidc->authenticate();
+$name = $oidc->requestUserInfo('given_name');
+
+```
+
+[See openid spec for available user attributes][1]
+
+## Example 2: Dynamic Registration ##
+
+```php
+use Jumbojett\OpenIDConnectClient;
+
+$oidc = new OpenIDConnectClient("https://id.provider.com");
+
+$oidc->register();
+$client_id = $oidc->getClientID();
+$client_secret = $oidc->getClientSecret();
+
+// Be sure to add logic to store the client id and client secret
+```
+
+## Example 3: Network and Security ##
+```php
+// Configure a proxy
+$oidc->setHttpProxy("http://my.proxy.com:80/");
+
+// Configure a cert
+$oidc->setCertPath("/path/to/my.cert");
+```
+
+## Example 4: Request Client Credentials Token ##
+
+```php
+use Jumbojett\OpenIDConnectClient;
+
+$oidc = new OpenIDConnectClient('https://id.provider.com',
+                                'ClientIDHere',
+                                'ClientSecretHere');
+$oidc->providerConfigParam(array('token_endpoint'=>'https://id.provider.com/connect/token'));
+$oidc->addScope('my_scope');
+
+// this assumes success (to validate check if the access_token property is there and a valid JWT) :
+$clientCredentialsToken = $oidc->requestClientCredentialsToken()->access_token;
+
+```
+
+## Example 5: Request Resource Owners Token (with client auth) ##
+
+```php
+use Jumbojett\OpenIDConnectClient;
+
+$oidc = new OpenIDConnectClient('https://id.provider.com',
+                                'ClientIDHere',
+                                'ClientSecretHere');
+$oidc->providerConfigParam(array('token_endpoint'=>'https://id.provider.com/connect/token'));
+$oidc->addScope('my_scope');
+
+//Add username and password
+$oidc->addAuthParam(array('username'=>'<Username>'));
+$oidc->addAuthParam(array('password'=>'<Password>'));
+
+//Perform the auth and return the token (to validate check if the access_token property is there and a valid JWT) :
+$token = $oidc->requestResourceOwnerToken(TRUE)->access_token;
+
+```
+
+## Example 6: Basic client for implicit flow e.g. with Azure AD B2C (see http://openid.net/specs/openid-connect-core-1_0.html#ImplicitFlowAuth) ##
+
+```php
+use Jumbojett\OpenIDConnectClient;
+
+$oidc = new OpenIDConnectClient('https://id.provider.com',
+                                'ClientIDHere',
+                                'ClientSecretHere');
+$oidc->setResponseTypes(array('id_token'));
+$oidc->addScope(array('openid'));
+$oidc->setAllowImplicitFlow(true);
+$oidc->addAuthParam(array('response_mode' => 'form_post'));
+$oidc->setCertPath('/path/to/my.cert');
+$oidc->authenticate();
+$sub = $oidc->getVerifiedClaims('sub');
+
+```
+
+
+## Development Environments ##
+In some cases you may need to disable SSL security on on your development systems.
+Note: This is not recommended on production systems.
+
+```php
+$oidc->setVerifyHost(false);
+$oidc->setVerifyPeer(false);
+```
+
+### Todo ###
+- Dynamic registration does not support registration auth tokens and endpoints
+
+  [1]: http://openid.net/specs/openid-connect-basic-1_0-15.html#id_res
+  
+## Contributing ###
+ - All pull requests, once merged, should be added to the changelog.md file.
