7f160bd915ced8b22521e2a8120665f573f73f1e
xus <xus206@hotmail.com>
2018-10-24 11:20:05 +0200

    db il_md_cpr_selections new default column and new copyright value set as default

--- setup/sql/dbupdate_custom.php
+++ setup/sql/dbupdate_custom.php
@@ -23,3 +23,32 @@ if(!$ilDB->tableExists('il_meta_oer_stat'))
 	));
 }
 ?>
+<#2>
+<?php
+
+if($ilDB->tableExists('il_md_cpr_selections'))
+{
+	if(!$ilDB->tableColumnExists('il_md_cpr_selections','is_default'))
+	{
+		$ilDB->addTableColumn('il_md_cpr_selections', 'is_default', array(
+			'type' => 'integer',
+			'length' => 1,
+			'notnull' => true,
+			'default' => 0
+		));
+	}
+
+	$id = $ilDB->nextId('il_md_cpr_selections');
+	$ilDB->insert("il_md_cpr_selections", array(
+			'entry_id' => array('integer',$id),
+			'title' => array('text', 'All rights reserved'),
+			'description' => array('clob', ''),
+			'copyright' => array('clob', 'This work has all rights reserved by the owner.'),
+			'language' => array('text', 'en'),
+			'costs' => array('integer', '0'),
+			'cpr_restrictions' => array('integer', '1'),
+			'is_default' => array('integer', '1')
+		)
+	);
+}
+?>
