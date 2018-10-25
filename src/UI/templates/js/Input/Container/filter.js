6eba9a74ca9ce8120e0a063f1882c44acf631825
Alexander Killing <killing@leifos.de>
2018-10-25 22:09:03 +0200

    some less, js polishing, needs more

--- src/UI/templates/js/Input/Container/filter.js
+++ src/UI/templates/js/Input/Container/filter.js
@@ -51,7 +51,8 @@ il.UI = il.UI || {};
 		 * @param value_as_string
 		 */
 		var onFieldUpdate = function(event, id, value_as_string) {
-			var pop_id = $("#" + id).parents(".il-popover").attr("id");
+			var $el = $("#" + id);
+			var pop_id = $el.parents(".il-popover").attr("id");
 			if (pop_id) {	// we have an already opened popover
 				$("span[data-target='" + pop_id + "']").html(value_as_string);
 			} else {
@@ -60,16 +61,22 @@ il.UI = il.UI || {};
 			}
 
 			//Show labels and values in Filter Bar
-            var input_name = $("#" + id).attr("name");
+            var input_name = $el.attr("name");
             var input_num = input_name.substring(13);
-            var input_label = $("#" + id).parents(".input-group").find(".leftaddon").html();
+            var input_label = $el.parents(".input-group").find(".leftaddon").html();
             if (input_label == undefined) {
                 var old_input_label = $("#" + input_num).html();
                 var last_char = old_input_label.indexOf(":");
                 old_input_label = old_input_label.substring(0, last_char);
-                $("span[id='" + input_num + "']").html(old_input_label + ":" + value_as_string + ",");
+				if (value_as_string != "") {
+					value_as_string = old_input_label + ": " + value_as_string;
+				}
+                $("span[id='" + input_num + "']").html(value_as_string);
             } else {
-                $("span[id='" + input_num + "']").html(input_label + ":" + value_as_string + ",");
+            	if (value_as_string != "") {
+					value_as_string = input_label + ": " + value_as_string;
+				}
+                $("span[id='" + input_num + "']").html(value_as_string);
 			}
 		};
 
@@ -79,23 +86,24 @@ il.UI = il.UI || {};
          * @param id
          */
         var onRemoveClick = function(event, id) {
-			var index = $("#" + id).parents(".il-popover-container").index();
-			storeFilterStatus($("#" + id), index, "0");
+			var $el = $("#" + id);
+			var index = $el.parents(".il-popover-container").index();
+			storeFilterStatus($el, index, "0");
 
             //Remove Input Field from Filter
-            $("#" + id).parents(".il-popover-container").hide();
+			$el.parents(".il-popover-container").hide();
             //Clear Input Field when it is removed
-            $("#" + id).parents(".il-popover-container").find(".il-standard-popover-content").children().val("");
-            $("#" + id).parents(".il-popover-container").find(".il-filter-field").html("");
-            var label = $("#" + id).parents(".input-group").find(".input-group-addon").html();
+			$el.parents(".il-popover-container").find(".il-standard-popover-content").children().val("");
+			$el.parents(".il-popover-container").find(".il-filter-field").html("");
+            var label = $el.parents(".input-group").find(".input-group-addon").html();
 
             //Add Input Field to Add-Button
-            $("#" + id).parents(".il-standard-form").find(".btn-link").filter(function() {
+			$el.parents(".il-standard-form").find(".btn-link").filter(function() {
                 return $(this).text() === label;
             }).parents("li").show();
 
             //Show Add-Button when not all Input Fields are shown in the Filter
-            var addableInputs = $("#" + id).parents(".il-standard-form").find(".il-popover-container:hidden").length;
+            var addableInputs = $el.parents(".il-standard-form").find(".il-popover-container:hidden").length;
             if (addableInputs != 0) {
                 $("#" + id).parents(".il-standard-form").find(".btn-bulky").parents(".il-popover-container").show();
             }
@@ -107,33 +115,35 @@ il.UI = il.UI || {};
          * @param id
          */
         var onAddClick = function(event, id) {
+        	var $el = $("#" + id);
             //Remove Input Field from Add-Button
-            $("#" + id).parent().hide();
-            var label = $("#" + id).text();
+
+            var label = $el.text();
+			$el.parent().hide();
 
             // Store show/hide status in hidden status inputs
-            var index = $("#" + id).parent().index();
-			storeFilterStatus($("#" + id), index, "1");
+            var index = $el.parent().index();
+			storeFilterStatus($el, index, "1");
 
             // Add Input Field to Filter
-            $("#" + id).parents(".il-standard-form").find(".input-group-addon").filter(function() {
+			$el.parents(".il-standard-form").find(".input-group-addon").filter(function() {
                 return $(this).text() === label;
             }).parents(".il-popover-container").show();
 
             //Imitate a click on the Input Field in the Fiter and focus on the element (input, select,...) in the Popover
-            $("#" + id).parents(".il-standard-form").find(".input-group-addon").filter(function() {
+			$el.parents(".il-standard-form").find(".input-group-addon").filter(function() {
                 return $(this).text() === label;
             }).parent().find(".il-filter-field").click()
                 .parents(".il-popover-container").find(".il-standard-popover-content").children().focus();
 
             //Hide Add-Button when all Input Fields are shown in the Filter
-            var addableInputs = $("#" + id).parents(".il-standard-form").find("li:visible").length;
+            var addableInputs = $el.parents(".il-standard-form").find("li:visible").length;
             if (addableInputs == 0) {
                 $("#" + id).parents(".il-standard-form").find(".btn-bulky").parents(".il-popover-container").hide();
             }
 
             //Hide the Popover of the Add-Button when adding Input Field
-            $("#" + id).parents(".il-standard-form").find(".btn-bulky").parents(".il-popover-container").find(".il-popover").hide();
+			$el.parents(".il-standard-form").find(".btn-bulky").parents(".il-popover-container").find(".il-popover").hide();
         };
 
 		/**
