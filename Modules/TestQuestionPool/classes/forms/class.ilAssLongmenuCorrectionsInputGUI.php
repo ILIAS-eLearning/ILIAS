807071699cec92750b2f23a69d64ab353d4f669f
Björn Heyser <bh@bjoernheyser.de>
2018-10-27 17:40:59 +0200

    extracted render stuff from insert method

---
+++ Modules/TestQuestionPool/classes/forms/class.ilAssLongmenuCorrectionsInputGUI.php
@@ -0,0 +1,16 @@
+<?php
+
+/* Copyright (c) 1998-2013 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+/**
+ * Class ilAssLongmenuCorrectionsInputGUI
+ *
+ * @author    Björn Heyser <info@bjoernheyser.de>
+ * @version    $Id$
+ *
+ * @package    Modules/Test(QuestionPool)
+ */
+class ilAssLongmenuCorrectionsInputGUI
+{
+	
+}
