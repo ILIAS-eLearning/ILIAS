adcec7296afdc3e63ff08030d9d82d0fad695564
smeyer-ilias <smeyer.ilias@gmx.de>
2018-10-28 17:19:10 +0100

    Timings personal timings

--- setup/sql/dbupdate_custom.php
+++ setup/sql/dbupdate_custom.php
@@ -101,3 +101,36 @@ if($ilDB->tableColumnExists('crs_items','latest_end_rel'))
 	$ilDB->dropTableColumn('crs_items','latest_end_rel');
 }
 ?>
+<#7>
+<?php
+if(!$ilDB->tableExists('crs_timings_user'))
+{
+	$ilDB->createTable('crs_timings_user', array(
+		'ref_id' => array(
+			'type' => 'integer',
+			'length' => 4,
+			'notnull' => true,
+			'default' => 0
+		),
+		'usr_id' => array(
+			'type' => 'integer',
+			'length' => 4,
+			'notnull' => true,
+			'default' => 0
+		),
+		'sstart' => array(
+			'type' => 'integer',
+			'length' => 4,
+			'notnull' => true,
+			'default' => 0
+		),
+		'ssend' => array(
+			'type' => 'integer',
+			'length' => 4,
+			'notnull' => true,
+			'default' => 0
+		)
+	));
+	$ilDB->addPrimaryKey('crs_timings_user', array('ref_id', 'usr_id'));
+}
+?>
