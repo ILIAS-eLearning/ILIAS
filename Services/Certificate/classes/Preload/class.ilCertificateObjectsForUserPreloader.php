c06ca8744d15173b43ef946f704fa80bedef0c24
mjansen <mjansen@databay.de>
2018-10-29 08:56:22 +0100

    Certificate: Fixed issue with certificate pre-loading for course references (see: https://mantis.ilias.de/view.php?id=23833)

--- Services/Certificate/classes/Preload/class.ilCertificateObjectsForUserPreloader.php
+++ Services/Certificate/classes/Preload/class.ilCertificateObjectsForUserPreloader.php
@@ -6,16 +6,16 @@
  */
 class ilCertificateObjectsForUserPreloader
 {
-	/**
-	 * @var array
-	 */
-	private static $certificates = array();
+	/** @var array */
+	private static $certificates = [];
 
-	/**
-	 * @var ilUserCertificateRepository
-	 */
+	/** @var ilUserCertificateRepository */
 	private $userCertificateRepository;
 
+	/**
+	 * ilCertificateObjectsForUserPreloader constructor.
+	 * @param ilUserCertificateRepository $userCertificateRepository
+	 */
 	public function __construct(ilUserCertificateRepository $userCertificateRepository)
 	{
 		$this->userCertificateRepository = $userCertificateRepository;
@@ -23,11 +23,13 @@ class ilCertificateObjectsForUserPreloader
 
 	/**
 	 * @param int $userId
-	 * @param array $objIds
+	 * @param int[] $objIds
 	 */
 	public function preLoad(int $userId, array $objIds)
 	{
-		$objectIdsWithUserCertificate = $this->userCertificateRepository->fetchObjectWithCertificateForUser($userId, $objIds);
+		$objectIdsWithUserCertificate = $this->userCertificateRepository->fetchObjectWithCertificateForUser(
+			$userId, $objIds
+		);
 		self::$certificates[$userId] = $objectIdsWithUserCertificate;
 	}
 
@@ -36,7 +38,7 @@ class ilCertificateObjectsForUserPreloader
 	 * @param int $objId
 	 * @return bool
 	 */
-	public function isPreloaded(int $userId, int $objId)
+	public function isPreloaded(int $userId, int $objId): bool
 	{
 		if (false === array_key_exists($userId, self::$certificates)) {
 			return false;
@@ -45,6 +47,7 @@ class ilCertificateObjectsForUserPreloader
 		if (true === in_array($objId, self::$certificates[$userId])) {
 			return true;
 		}
+
 		return false;
 	}
 }
