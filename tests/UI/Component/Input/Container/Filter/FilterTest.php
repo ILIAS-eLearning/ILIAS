73ab79c6ecf80be689df27a3561d7a2c6df70cf2
Thomas Famula <famula@leifos.de>
2018-10-29 10:36:26 +0100

    Added some more tests

--- tests/UI/Component/Input/Container/Filter/FilterTest.php
+++ tests/UI/Component/Input/Container/Filter/FilterTest.php
@@ -44,20 +44,17 @@ class ConcreteFilter extends Filter {
 class FilterTest extends ILIAS_UI_TestBase
 {
 
-	protected function buildFactory()
-	{
-		return new ILIAS\UI\Implementation\Component\Input\Container\Filter\Factory(new SignalGenerator());
+	protected function buildFactory() {
+		return new ILIAS\UI\Implementation\Component\Input\Container\Filter\Factory();
 	}
 
 
-	protected function buildInputFactory()
-	{
+	protected function buildInputFactory() {
 		return new ILIAS\UI\Implementation\Component\Input\Field\Factory(new SignalGenerator());
 	}
 
 
-	public function test_getInputs()
-	{
+	public function test_getInputs() {
 		//$f = $this->buildFactory();
 		$if = $this->buildInputFactory();
 		$name_source = new FixedNameSourceFilter();
@@ -86,5 +83,61 @@ class FilterTest extends ILIAS_UI_TestBase
 			$this->assertNotContains($name, $seen_names);
 			$seen_names[] = $name;
 		}
+	}
+
+	public function test_with_activated() {
+		$f = $this->buildFactory();
+		$if = $this->buildInputFactory();
+		$inputs = [$if->text(""), $if->text("")];
+		$inputs_rendered = [true, true];
+
+		$filter = $f->standard("#", "#", "#", "#",
+			"#", "#", $inputs, $inputs_rendered, false, false);
+		$filter1 = $filter->withActivated();
+
+		$this->assertFalse($filter->isActivated());
+		$this->assertTrue($filter1->isActivated());
+	}
+
+	public function test_with_deactivated() {
+		$f = $this->buildFactory();
+		$if = $this->buildInputFactory();
+		$inputs = [$if->text(""), $if->text("")];
+		$inputs_rendered = [true, true];
+
+		$filter = $f->standard("#", "#", "#", "#",
+			"#", "#", $inputs, $inputs_rendered, true, false);
+		$filter1 = $filter->withDeactivated();
+
+		$this->assertTrue($filter->isActivated());
+		$this->assertFalse($filter1->isActivated());
+	}
+
+	public function test_with_expanded() {
+		$f = $this->buildFactory();
+		$if = $this->buildInputFactory();
+		$inputs = [$if->text(""), $if->text("")];
+		$inputs_rendered = [true, true];
+
+		$filter = $f->standard("#", "#", "#", "#",
+			"#", "#", $inputs, $inputs_rendered, false, false);
+		$filter1 = $filter->withExpanded();
+
+		$this->assertFalse($filter->isExpanded());
+		$this->assertTrue($filter1->isExpanded());
+	}
+
+	public function test_with_collapsed() {
+		$f = $this->buildFactory();
+		$if = $this->buildInputFactory();
+		$inputs = [$if->text(""), $if->text("")];
+		$inputs_rendered = [true, true];
+
+		$filter = $f->standard("#", "#", "#", "#",
+			"#", "#", $inputs, $inputs_rendered, false, true);
+		$filter1 = $filter->withCollapsed();
+
+		$this->assertTrue($filter->isExpanded());
+		$this->assertFalse($filter1->isExpanded());
 	}
 }
