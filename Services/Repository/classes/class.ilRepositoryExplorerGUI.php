5ae032576fd6d7dbb5e284854d9fd438ddaa3a4c
mjansen <mjansen@databay.de>
2018-10-30 16:36:57 +0100

    Object Service/Repository Explorer: Fixed Mantis issue 23852

--- Services/Repository/classes/class.ilRepositoryExplorerGUI.php
+++ Services/Repository/classes/class.ilRepositoryExplorerGUI.php
@@ -647,7 +647,7 @@ class ilRepositoryExplorerGUI extends ilTreeExplorerGUI
 					{
 						include_once("./Modules/LearningModule/classes/class.ilObjLearningModule.php");
 						$lm_obj = new ilObjLearningModule($a_node["child"]);
-						if((!$lm_obj->getOnline()) && (!$rbacsystem->checkAccess('write', $a_node["child"])))
+						if(($lm_obj->getOfflineStatus()) && (!$rbacsystem->checkAccess('write', $a_node["child"])))
 						{
 							return false;
 						}
@@ -657,7 +657,7 @@ class ilRepositoryExplorerGUI extends ilTreeExplorerGUI
 					{
 						include_once("./Modules/HTMLLearningModule/classes/class.ilObjFileBasedLM.php");
 						$lm_obj = new ilObjFileBasedLM($a_node["child"]);
-						if((!$lm_obj->getOnline()) && (!$rbacsystem->checkAccess('write', $a_node["child"])))
+						if(($lm_obj->getOfflineStatus()) && (!$rbacsystem->checkAccess('write', $a_node["child"])))
 						{
 							return false;
 						}
@@ -667,7 +667,7 @@ class ilRepositoryExplorerGUI extends ilTreeExplorerGUI
 					{
 						include_once("./Modules/ScormAicc/classes/class.ilObjSAHSLearningModule.php");
 						$lm_obj = new ilObjSAHSLearningModule($a_node["child"]);
-						if((!$lm_obj->getOnline()) && (!$rbacsystem->checkAccess('write', $a_node["child"])))
+						if(($lm_obj->getOfflineStatus()) && (!$rbacsystem->checkAccess('write', $a_node["child"])))
 						{
 							return false;
 						}
