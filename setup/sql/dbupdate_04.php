a552e2e44a317fd500c1d1ca6dccd26731a72e40
xus <xus206@hotmail.com>
2018-11-05 12:29:10 +0100

    Missing DB tables for booking participants

--- setup/sql/dbupdate_04.php
+++ setup/sql/dbupdate_04.php
@@ -25122,3 +25122,43 @@ if(!in_array("primary", $const)) {
 }	
 	
 ?>
+<#5424>
+<?php
+if (!$ilDB->tableExists('booking_member'))
+{
+	$ilDB->createTable('booking_member', array(
+		'participant_id' => array(
+			'type' => 'integer',
+			'length' => 4,
+			'notnull' => true,
+			'default' => 0
+		),
+		'user_id' => array(
+			'type' => 'integer',
+			'length' => 4,
+			'notnull' => true,
+			'default' => 0
+		),
+		'booking_pool_id' => array(
+			'type' => 'text',
+			'length' => 255,
+			'notnull' => true
+		),
+		'assigner_user_id' => array(
+			'type' => 'integer',
+			'length' => 4,
+			'notnull' => true,
+			'default' => 0
+		)
+	));
+	$ilDB->addPrimaryKey('booking_member', array('participant_id', 'user_id', 'booking_pool_id'));
+	$ilDB->createSequence('booking_member');
+}
+?>
+<#5425>
+<?php
+if(!$ilDB->tableColumnExists('booking_reservation','assigner_id'))
+{
+	$ilDB->addTableColumn("booking_reservation", "assigner_id", array("type" => "integer", "length" => 4, "notnull" => true, "default" => 0));
+}
+?>
