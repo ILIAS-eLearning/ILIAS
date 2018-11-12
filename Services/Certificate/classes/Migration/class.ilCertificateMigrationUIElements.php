9b18c66a54dc916e4ee79df360a35922548f5c84
Niels Theen <ntheen@databay.de>
2018-11-12 13:52:34 +0100

    Add test for certificate migration GUI

--- Services/Certificate/classes/Migration/class.ilCertificateMigrationUIElements.php
+++ Services/Certificate/classes/Migration/class.ilCertificateMigrationUIElements.php
@@ -16,29 +16,46 @@ class ilCertificateMigrationUIElements
 	/** @var \ilLanguage */
 	protected $lng;
 
+	/** @var ilCertificateMigration */
+	private $migrationHelper;
+
 	/**
 	 * ilCertificateMigrationUIElements constructor.
 	 * @param \ilObjUser $user
 	 * @param \ILIAS\DI\UIServices $ui
 	 * @param \ilLanguage $lng
+	 * @param ilCertificateMigration|null $certificateMigration
 	 */
-	public function __construct(\ilObjUser $user = null, \ILIAS\DI\UIServices $ui = null, \ilLanguage $lng = null)
-	{
+	public function __construct(
+		\ilObjUser $user = null,
+		\ILIAS\DI\UIServices $ui = null,
+		\ilLanguage $lng = null,
+		\ilCertificateMigration $migrationHelper = null
+	) {
 		global $DIC;
 
 		if (null === $user) {
 			$user = $DIC->user();
 		}
+
 		if (null === $ui) {
 			$ui = $DIC->ui();
 		}
+
 		if (null === $lng) {
 			$lng = $DIC->language();
 		}
+
 		$lng->loadLanguageModule('cert');
 		$this->user = $user;
 		$this->ui = $ui;
 		$this->lng = $lng;
+
+		if (null === $migrationHelper){
+			$migrationHelper = new \ilCertificateMigration($this->user->getId());
+		}
+		$this->migrationHelper = $migrationHelper;
+
 	}
 
 	/**
@@ -56,8 +73,7 @@ class ilCertificateMigrationUIElements
 			$ui_factory->button()->standard($this->lng->txt("certificate_migration_go"), $link),
 		];
 
-		$migrationHelper = new \ilCertificateMigration($this->user->getId());
-		if ($migrationHelper->isTaskFailed()) {
+		if ($this->migrationHelper->isTaskFailed()) {
 			$messageBox = $ui_factory->messageBox()
 				->failure($this->lng->txt('certificate_migration_lastrun_failed'))
 				->withButtons($message_buttons);
