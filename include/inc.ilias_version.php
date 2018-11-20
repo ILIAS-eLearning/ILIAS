5708a8f1921d799b0ebd533fab69709ac1bb3454
Fabian Schmid <fs@studer-raimann.ch>
2018-11-20 09:42:07 +0100

    build release 5.3.11

--- include/inc.ilias_version.php
+++ include/inc.ilias_version.php
@@ -9,5 +9,5 @@
 *
 * @package ilias-core
 */
-define("ILIAS_VERSION", "5.3.10 2018-10-04");
-define("ILIAS_VERSION_NUMERIC", "5.3.10");			// must be always x.y.z: x, y and z are numbers
+define("ILIAS_VERSION", "5.3.11 2018-11-20");
+define("ILIAS_VERSION_NUMERIC", "5.3.11");			// must be always x.y.z: x, y and z are numbers
