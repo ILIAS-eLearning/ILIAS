7739a11e1781dbdb73206222f5b07d8ed253cbbe
smeyer-ilias <smeyer.ilias@gmx.de>
2018-11-22 12:17:19 +0100

    Fixed 0023499: Sessions in Folder: "Back to Folder Content" generates error

--- Modules/Session/classes/class.ilObjSessionGUI.php
+++ Modules/Session/classes/class.ilObjSessionGUI.php
@@ -1924,6 +1924,8 @@ class ilObjSessionGUI extends ilObjectGUI implements ilDesktopItemHandling
 	{
 		global $DIC;
 
+		$objDefinition = $DIC['objDefinition'];
+
 		$tree = $DIC->repositoryTree();
 		$ctrl = $DIC->ctrl();
 
@@ -1931,9 +1933,9 @@ class ilObjSessionGUI extends ilObjectGUI implements ilDesktopItemHandling
 
 		// #11650
 		$parent_type = ilObject::_lookupType($parent_id, true);
-		$parent_class = ($parent_type == "grp")
-			? "ilObjGroupGUI"
-			: "ilObjCourseGUI";
+
+		$parent_class = $objDefinition->getClassName($parent_type);
+		$parent_class = 'ilObj'.$parent_class.'GUI';
 
 		$ctrl->setParameterByClass($parent_class, "ref_id", $parent_id);
 		$ctrl->redirectByClass($parent_class, "view");
