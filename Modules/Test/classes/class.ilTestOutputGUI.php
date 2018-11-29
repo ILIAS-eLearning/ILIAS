b70515751577fd18195f4609ea1ccbdf3470f87a
Björn Heyser <bh@bjoernheyser.de>
2018-11-29 15:28:31 +0100

    0023129: Submit Validation Alert Loop

--- Modules/Test/classes/class.ilTestOutputGUI.php
+++ Modules/Test/classes/class.ilTestOutputGUI.php
@@ -468,6 +468,10 @@ abstract class ilTestOutputGUI extends ilTestPlayerAbstractGUI
 			
 			$this->ctrl->setParameter($this, 'pmode', ilTestPlayerAbstractGUI::PRESENTATION_MODE_VIEW);
 		}
+		else
+		{
+			$this->ctrl->redirect($this, ilTestPlayerCommands::SHOW_QUESTION);
+		}
 
 // fau: testNav - remember to prevent the navigation confirmation
 		$this->saveNavigationPreventConfirmation();
@@ -677,20 +681,15 @@ abstract class ilTestOutputGUI extends ilTestPlayerAbstractGUI
 		
 		if( !$this->isParticipantsAnswerFixed($questionId) )
 		{
-// fau: testNav - handle answer fixation and intermediate submit
-			// always save the question when feedback is requested
-			// if( $this->object->isInstantFeedbackAnswerFixationEnabled() )
-			if (true)
+			if( $this->saveQuestionSolution(true) )
 			{
-				$this->saveQuestionSolution(true);
 				$this->removeIntermediateSolution();
 				$this->setAnswerChangedParameter(false);
 			}
 			else
 			{
-				$this->handleIntermediateSubmit();
+				$this->ctrl->redirect($this, ilTestPlayerCommands::SHOW_QUESTION);
 			}
-// fau.
 			$this->testSequence->setQuestionChecked($questionId);
 			$this->testSequence->saveToDb();
 		}
