49dd46c85e7b1a26b2c116647aed2b28330afee4
Alexander Killing <killing@leifos.de>
2018-11-29 21:14:25 +0100

    fixed second part of #23299: Style definitions lost for media objects

--- setup/sql/dbupdate_04.php
+++ setup/sql/dbupdate_04.php
@@ -25269,3 +25269,11 @@ include_once("./Services/Migration/DBUpdate_3136/classes/class.ilDBUpdate3136.ph
 ilDBUpdate3136::addStyleClass("Code", "code_block", "pre",
 	array());
 ?>
+<#5430>
+<?php
+$ilDB->update("style_data", array(
+	"uptodate" => array("integer", 0)
+), array(
+	"1" => array("integer", 1)
+));
+?>
