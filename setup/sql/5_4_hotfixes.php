38cfd8db44637a1b11527f0de0f2f937ad91ef30
Nils Haagen <nhaagen@cat06.de>
2018-11-30 15:51:01 +0100

    0023964; set permission on LSO role-templates for admin and member

--- setup/sql/5_4_hotfixes.php
+++ setup/sql/5_4_hotfixes.php
@@ -18,3 +18,59 @@ $ilDB->update("style_data", array(
 	"1" => array("integer", 1)
 ));
 ?>
+
+<#4>
+<?php
+$template = 'il_lso_admin';
+$perms = [
+	'create_htlm',
+	'create_iass',
+	'create_copa',
+	'create_svy',
+	'create_svy',
+	'create_lm',
+	'create_exc',
+	'create_tst',
+	'create_sahs',
+	'create_file',
+	'participate',
+	'unparticipate',
+	'edit_learning_progress',
+	'manage_members',
+	'copy'
+];
+
+$query = "SELECT obj_id FROM object_data"
+	." WHERE object_data.type = " .$ilDB->quote('rolt', 'text')
+	." AND title = " .$ilDB->quote($template,'text');
+$result = $ilDB->query($query);
+$rol_id = array_shift($ilDB->fetchAssoc($result));
+
+$op_ids = [];
+$query = "SELECT ops_id FROM rbac_operations"
+	." WHERE operation IN ('"
+	.implode("', '", $perms)
+	."')";
+$result = $ilDB->query($query);
+while($row = $ilDB->fetchAssoc($result)) {
+	$op_ids[] = $row['ops_id'];
+}
+
+include_once('./Services/Migration/DBUpdate_3560/classes/class.ilDBUpdateNewObjectType.php');
+ilDBUpdateNewObjectType::setRolePermission($rol_id, 'lso', $op_ids,	ROLE_FOLDER_ID);
+?>
+
+<#5>
+<?php
+include_once('./Services/Migration/DBUpdate_3560/classes/class.ilDBUpdateNewObjectType.php');
+$template = 'il_lso_member';
+$op_id = ilDBUpdateNewObjectType::getCustomRBACOperationId('unparticipate');
+
+$query = "SELECT obj_id FROM object_data"
+	." WHERE object_data.type = " .$ilDB->quote('rolt', 'text')
+	." AND title = " .$ilDB->quote($template,'text');
+$result = $ilDB->query($query);
+$rol_id = array_shift($ilDB->fetchAssoc($result));
+
+ilDBUpdateNewObjectType::setRolePermission($rol_id, 'lso', [$op_id], ROLE_FOLDER_ID);
+?>
