c1704fe7ec99af2138a7252c2b4bb7f2bf5851d0
Alexander Killing <killing@leifos.de>
2018-12-06 17:16:34 +0100

    fixed bug #21554: Address search in ilLocationInputGUI outputs undefined

--- Services/Maps/js/ServiceGoogleMaps.js
+++ Services/Maps/js/ServiceGoogleMaps.js
@@ -229,7 +229,7 @@ function ilShowUserMarker(id, j)
 function ilMapClicked(id, map, location)
 {
 	map.setCenter(location);
-    ilUpdateLocationInput(id, map, location);
+    ilUpdateLocationInput(id, map, location, "");
 }
 
 function ilLookupAddress(id, address)
@@ -237,10 +237,8 @@ function ilLookupAddress(id, address)
 	var map = ilMap[id];
 	
 	var geocoder = new google.maps.Geocoder();
-	geocoder.geocode({address: address}, function(result)
-	{
-		if (result[0]["geometry"])
-		{
+	geocoder.geocode({address: address}, function(result)  {
+		if (Array.isArray(result) && result.length > 0 && typeof result[0] === "object" && "geometry" in result[0]) {
 			map.setCenter(result[0]["geometry"]["location"]);
 			ilUpdateLocationInput(id, map, result[0]["geometry"]["location"],
 				result[0]["formatted_address"]);
