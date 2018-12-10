a0b7a331b5fdbe3e44d99a1fc5eb926766edaf71
Alexander Killing <killing@leifos.de>
2018-12-10 17:14:12 +0100

    fixed bug #23718: wrong learning recommendation message you need to improve the competence is wrongly shown for some comptences

--- Services/Skill/classes/class.ilPersonalSkillsGUI.php
+++ Services/Skill/classes/class.ilPersonalSkillsGUI.php
@@ -1196,6 +1196,39 @@ class ilPersonalSkillsGUI
 	}
 
 	/**
+	 * Get actual levels
+	 *
+	 * @param array $skills
+	 * @param int $user_id
+	 */
+	protected function getActualLevels($skills, $user_id)
+	{
+		// get actual levels for gap analysis
+		$this->actual_levels = array();
+		include_once("./Services/Skill/classes/class.ilBasicSkill.php");
+		foreach ($skills as $sk)
+		{
+			$bs = new ilBasicSkill($sk["base_skill_id"]);
+			if ($this->gap_mode == "max_per_type")
+			{
+				$max = $bs->getMaxLevelPerType($sk["tref_id"], $this->gap_mode_type, $user_id);
+				$this->actual_levels[$sk["base_skill_id"]][$sk["tref_id"]] = $max;
+			}
+			else if ($this->gap_mode == "max_per_object")
+			{
+				$max = $bs->getMaxLevelPerObject($sk["tref_id"], $this->gap_mode_obj_id, $user_id);
+				$this->actual_levels[$sk["base_skill_id"]][$sk["tref_id"]] = $max;
+			}
+			else
+			{
+				$max = $bs->getMaxLevel($sk["tref_id"], $user_id);
+				$this->actual_levels[$sk["base_skill_id"]][$sk["tref_id"]] = $max;
+			}
+		}
+	}
+
+
+	/**
 	 * Get gap analysis html
 	 *
 	 * @param
@@ -1255,22 +1288,7 @@ class ilPersonalSkillsGUI
 		}
 
 		// get actual levels for gap analysis
-		$this->actual_levels = array();
-		include_once("./Services/Skill/classes/class.ilBasicSkill.php");
-		foreach ($skills as $sk)
-		{
-			$bs = new ilBasicSkill($sk["base_skill_id"]);
-			if ($this->gap_mode == "max_per_type")
-			{
-				$max = $bs->getMaxLevelPerType($sk["tref_id"], $this->gap_mode_type, $user_id);
-				$this->actual_levels[$sk["base_skill_id"]][$sk["tref_id"]] = $max;
-			}
-			else if ($this->gap_mode == "max_per_object")
-			{
-				$max = $bs->getMaxLevelPerObject($sk["tref_id"], $this->gap_mode_obj_id, $user_id);
-				$this->actual_levels[$sk["base_skill_id"]][$sk["tref_id"]] = $max;
-			}
-		}
+		$this->getActualLevels($skills, $user_id);
 
 		$incl_self_eval = false;
 		if (count($this->getGapAnalysisSelfEvalLevels() > 0))
@@ -1957,8 +1975,7 @@ class ilPersonalSkillsGUI
 			}
 		}
 
-		include_once("./Services/Skill/classes/class.ilSkillTree.php");
-		$stree = new ilSkillTree();
+		$this->getActualLevels($skills, $this->user->getId());
 
 		// render
 		$html = "";
