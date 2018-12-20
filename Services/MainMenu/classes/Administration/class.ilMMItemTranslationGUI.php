6264a516268f14fbe80f39a76fe8e796247510a5
Bense <bs@studer-raimann.ch>
2018-12-20 11:03:33 +0100

    removed unecessary load language module

--- Services/MainMenu/classes/Administration/class.ilMMItemTranslationGUI.php
+++ Services/MainMenu/classes/Administration/class.ilMMItemTranslationGUI.php
@@ -36,7 +36,6 @@ class ilMMItemTranslationGUI {
 	public function __construct(ilMMItemFacadeInterface $item_facade, ilMMItemRepository $repository) {
 		$this->item_facade = $item_facade;
 		$this->repository = $repository;
-		$this->lng()->loadLanguageModule("mme");
 	}
 
 
