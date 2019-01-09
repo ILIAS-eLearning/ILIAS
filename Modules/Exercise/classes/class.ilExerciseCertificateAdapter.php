dfd8e89eab176d3d1c46e3bc9c88713516daa334
Niels Theen <ntheen@databay.de>
2019-01-09 09:19:21 +0100

    Relocate hasCertificate method to adapter class in order for migration tasks

--- Modules/Exercise/classes/class.ilExerciseCertificateAdapter.php
+++ Modules/Exercise/classes/class.ilExerciseCertificateAdapter.php
@@ -182,6 +182,37 @@ class ilExerciseCertificateAdapter extends ilCertificateAdapter
 	{
 		return $this->object->getId();
 	}
+
+	/**
+	 * This code was orignaly located in `ilObjExercise` and has been moved
+	 * here for `ilCertificateMigrationJob`
+	 *
+	 * @param int $objectId
+	 * @param int $userId
+	 * @return bool
+	 */
+	public function hasUserCertificate(int $userId)
+	{
+		// show certificate?
+		if(ilCertificate::isActive() && ilCertificate::isObjectActive($this->object->getId())) {
+			$certificate_visible = $this->object->getCertificateVisibility();
+			// if not never
+			if($certificate_visible != 2) {
+				// if passed only
+				$status = ilExerciseMembers::_lookupStatus($this->object->getId(), $userId);
+				if($certificate_visible == 1 && $status == "passed") {
+					return true;
+				}
+
+				// always (excluding notgraded)
+				else if($certificate_visible == 0 && $status != "notgraded") {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
 }
 
 ?>
