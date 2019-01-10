eaf5f008aef2a2352ebf7c8794f29cbbc80fa626
Björn Heyser <bh@bjoernheyser.de>
2019-01-10 17:24:30 +0100

    fixed method call after cherry pick to version < 5.4

--- Modules/TestQuestionPool/classes/import/qti12/class.assTextQuestionImport.php
+++ Modules/TestQuestionPool/classes/import/qti12/class.assTextQuestionImport.php
@@ -234,7 +234,7 @@ class assTextQuestionImport extends assQuestionImport
 			$index = $this->fetchIndexFromFeedbackIdent($ident);
 			
 			$this->object->feedbackOBJ->importSpecificAnswerFeedback(
-				$this->object->getId(),0, $index, ilRTE::_replaceMediaObjectImageSrc($material, 1)
+				$this->object->getId(), $index, ilRTE::_replaceMediaObjectImageSrc($material, 1)
 			);
 		}
 		foreach ($feedbacksgeneric as $correctness => $material)
