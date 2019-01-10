0956709a860e1b29018568eba94c5d37540d0f2f
Fabian Wolf <wolf@ilias.de>
2019-01-10 14:50:25 +0100

    Release 5.2.21

--- include/inc.ilias_version.php
+++ include/inc.ilias_version.php
@@ -9,5 +9,5 @@
 *
 * @package ilias-core
 */
-define("ILIAS_VERSION", "5.2.20 2018-11-20");
-define("ILIAS_VERSION_NUMERIC", "5.2.20");			// must be always x.y.z: x, y and z are numbers
+define("ILIAS_VERSION", "5.2.21 2019-01-10");
+define("ILIAS_VERSION_NUMERIC", "5.2.21");			// must be always x.y.z: x, y and z are numbers
