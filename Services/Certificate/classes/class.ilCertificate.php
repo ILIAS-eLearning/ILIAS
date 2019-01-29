3a22d7eabd50580f8fb708da5d6b0ab8fd18d077
Björn Heyser <bh@bjoernheyser.de>
2019-01-29 15:43:49 +0100

    remove file after delivery since there should be no need to keep it any longer

--- Services/Certificate/classes/class.ilCertificate.php
+++ Services/Certificate/classes/class.ilCertificate.php
@@ -376,7 +376,7 @@ class ilCertificate
 		ilUtil::zip($dir, $zipfilePath);
 		ilUtil::delDir($dir);
 		if ($deliver) {
-			ilUtil::deliverFile($zipfilePath, $zipfile, "application/zip");
+			ilUtil::deliverFile($zipfilePath, $zipfile, "application/zip", false, true);
 		}
 		return $zipfilePath;
 	}
