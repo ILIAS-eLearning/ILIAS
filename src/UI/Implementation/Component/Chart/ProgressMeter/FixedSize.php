e81b65920867b7e7be044e1ca6a5a36b359369f8
Ralph Dittrich <dittrich@qualitus.de>
2019-02-11 16:27:45 +0100

    Cleanup for FixedSize ProgressMeter

--- src/UI/Implementation/Component/Chart/ProgressMeter/FixedSize.php
+++ src/UI/Implementation/Component/Chart/ProgressMeter/FixedSize.php
@@ -9,23 +9,5 @@ use ILIAS\UI\Component as C;
  * Class ProgressMeter
  * @package ILIAS\UI\Implementation\Component\Chart\ProgressMeter
  */
-class FixedSize extends Standard implements C\Chart\ProgressMeter\FixedSize {
-
-	/**
-	 * @param float|int $width
-	 * @return $this
-	 */
-	public function setWidth($width)
-	{
-		$this->width = $width;
-		return $this;
-	}
-
-	/**
-	 * @return float|int
-	 */
-	public function getWidth()
-	{
-		return $this->width;
-	}
-}
+class FixedSize extends Standard implements C\Chart\ProgressMeter\FixedSize
+{}
