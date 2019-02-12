e3c65d5c4e39c3ea6d27dc3b2442800fbc751707
Raphael Heer <raphael.heer@hslu.ch>
2019-02-12 13:30:06 +0100

    WebDAV: Fixed access checking for creating objects

--- Services/WebDAV/classes/class.ilWebDAVRepositoryHelper.php
+++ Services/WebDAV/classes/class.ilWebDAVRepositoryHelper.php
@@ -50,9 +50,21 @@ class ilWebDAVRepositoryHelper
      * @param $a_ref_id
      * @return bool
      */
-    public function checkAccess($a_permission, $a_ref_id)
+    public function checkAccess($a_permission, $a_ref_id) : bool
     {
         return $this->access->checkAccess($a_permission, '', $a_ref_id);
+    }
+
+    /**
+     * Just a redirect to the checkAccess method of ilAccess to check for creation of certain obj types
+     *
+     * @param int $a_ref_id
+     * @param string $a_type
+     * @return bool
+     */
+    public function checkCreateAccessForType(int $a_ref_id, string $a_type) : bool
+    {
+        return $this->access->checkAccess('create', '', $a_ref_id, $a_type);
     }
 
     /**
