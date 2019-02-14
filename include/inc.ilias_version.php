762a2b51c64ccd0609c518d199b19b6cc5402472
Fabian Wolf <wolf@ilias.de>
2019-02-14 16:45:45 +0100

    Release 5.3.13

--- include/inc.ilias_version.php
+++ include/inc.ilias_version.php
@@ -9,5 +9,5 @@
 *
 * @package ilias-core
 */
-define("ILIAS_VERSION", "5.3.12 2019-01-04");
-define("ILIAS_VERSION_NUMERIC", "5.3.12");			// must be always x.y.z: x, y and z are numbers
+define("ILIAS_VERSION", "5.3.13 2019-02-14");
+define("ILIAS_VERSION_NUMERIC", "5.3.13");			// must be always x.y.z: x, y and z are numbers
