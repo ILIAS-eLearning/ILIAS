1887c1f39ec9be6b275d7f39b7d9d831d2a25b68
Richard Klees <richard.klees@concepts-and-training.de>
2019-02-14 16:32:27 +0100

    UI: added test for Input\Field\Group::withDisabled

---
+++ tests/UI/Component/Input/Field/GroupInputTest.php
@@ -0,0 +1,52 @@
+<?php
+
+/* Copyright (c) 2018 Nils Haagen <nils.haagen@concepts-and-training.de> Extended GPL, see docs/LICENSE */
+
+require_once(__DIR__ . "/../../../../../libs/composer/vendor/autoload.php");
+require_once(__DIR__ . "/../../../Base.php");
+
+use ILIAS\UI\Implementation\Component\Input\Field\Group;
+use ILIAS\UI\Component\Input\Field\Input;
+use \ILIAS\Data;
+use \ILIAS\Validation;
+use \ILIAS\Transformation;
+
+interface Input1 extends Input {};
+interface Input2 extends Input {};
+
+class GroupInputTest extends ILIAS_UI_TestBase {
+	public function setUp() {
+		$this->child1 = $this->createMock(Input1::class);
+		$this->child2 = $this->createMock(Input2::class);
+		$this->data_factory = $this->createMock(Data\Factory::class);
+		$this->validation_factory = $this->createMock(Validation\Factory::class);
+		$this->transformation_factory = $this->createMock(Transformation\Factory::class);
+		$this->group = new Group(
+			$this->data_factory,
+			$this->validation_factory,
+			$this->transformation_factory,
+			[$this->child1, $this->child2],
+			"LABEL",
+			"BYLINE"
+		);
+	}
+
+	public function testWithDisabledDisablesChildren() {
+		$this->assertNotSame($this->child1, $this->child2);
+
+		$this->child1
+			->expects($this->once())
+			->method("withDisabled")
+			->with(true)
+			->willReturn($this->child2);
+		$this->child2
+			->expects($this->once())
+			->method("withDisabled")
+			->with(true)
+			->willReturn($this->child1);
+
+		$new_group = $this->group->withDisabled(true);
+
+		$this->assertEquals([$this->child2, $this->child1], $new_group->getInputs());
+	}
+}
