f0f25ca6b1e907c58bb11188cd270c6c104d7617
Fabian Schmid <fs@studer-raimann.ch>
2019-02-15 10:28:42 +0100

    check for ilPlugin

--- Services/Component/classes/class.ilPluginAdmin.php
+++ Services/Component/classes/class.ilPluginAdmin.php
@@ -136,7 +136,7 @@ class ilPluginAdmin {
 	 * Should the plugin be updated
 	 *
 	 * @param string[] &$plugin_data
-	 * @param string   $last_update_version
+	 * @param string    $last_update_version
 	 *
 	 * @return void
 	 */
@@ -206,7 +206,7 @@ class ilPluginAdmin {
 	 * db value $active
 	 *
 	 * @param string[] &$plugin_data
-	 * @param bool     $active
+	 * @param bool      $active
 	 *
 	 * @return void
 	 */
@@ -570,7 +570,9 @@ class ilPluginAdmin {
 		// return array(); // current fix
 		foreach (self::getActivePlugins() as $plugin) {
 			$pl = self::getPluginObjectById($plugin['plugin_id']);
-			array_push($providers, $pl->promoteGlobalScreenProvider());
+			if ($pl instanceof ilPlugin) {
+				array_push($providers, $pl->promoteGlobalScreenProvider());
+			}
 		}
 
 		return $providers;
