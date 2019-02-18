46f1a9a37de26b757761c0e28480870749bf17cc
Alexander Killing <killing@leifos.de>
2019-02-18 11:23:21 +0100

    [Fix 24936 and 24045] Booking pool does not show available objects

--- Modules/BookingManager/classes/class.ilObjBookingPoolGUI.php
+++ Modules/BookingManager/classes/class.ilObjBookingPoolGUI.php
@@ -484,6 +484,8 @@ class ilObjBookingPoolGUI extends ilObjectGUI
 			else
 			{
 				$dates = array();
+
+				//loop for 1 week
 				$has_open_slot = $this->buildDatesBySchedule($week_start, $hours, $schedule, $object_ids, $seed, $dates);
 				
 				// find first open slot
@@ -623,16 +625,21 @@ class ilObjBookingPoolGUI extends ilObjectGUI
 		foreach(ilCalendarUtil::_buildWeekDayList($seed,$week_start)->get() as $date)
 		{
 			$date_info = $date->get(IL_CAL_FKT_GETDATE,'','UTC');
-			
-			if($av_from || 
-				$av_to)
+
+			#24045 and #24936
+			if($av_from || $av_to)
 			{
-				$today = $date->get(IL_CAL_DATE);						
-				if($av_from > $today ||
-					$av_to < $today)
+				$today = $date->get(IL_CAL_DATE);
+
+				if ($av_from && $av_from > $today)
 				{
 					continue;
-				}			
+				}
+
+				if($av_to && $av_to < $today)
+				{
+					continue;
+				}
 			}
 
 			$slots = array();
