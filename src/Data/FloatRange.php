21ddac7b17fe399aedb4065fc761521a9ec8bbd0
Niels Theen <ntheen@databay.de>
2019-02-26 13:18:37 +0100

    Add FloatRange data type

---
+++ src/Data/FloatRange.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+/* Copyright (c) 1998-2019 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+/**
+ * @author  Niels Theen <ntheen@databay.de>
+ */
+
+namespace ILIAS\Data;
+
+
+class FloatRange
+{
+	private $minimum;
+	private $maximum;
+
+	/**
+	 * @param float $minimum
+	 * @param float $maximum
+	 * @throws \ilException
+	 */
+	public function __construct(float $minimum, float $maximum)
+	{
+		if ($maximum === $minimum) {
+			throw new \ilException(sprintf('The maximum("%s") can NOT be same than the minimum("%s")', $maximum, $minimum));
+		}
+
+		if ($maximum < $minimum) {
+			throw new \ilException(sprintf('The maximum("%s") can NOT be lower than the minimum("%s")', $maximum, $minimum));
+		}
+
+		$this->minimum = $minimum;
+		$this->maximum = $maximum;
+	}
+
+	/**
+	 * @return float
+	 */
+	public function minimumAsFloat()
+	{
+		return $this->minimum;
+	}
+
+	/**
+	 * @return float
+	 */
+	public function maximumAsFloat()
+	{
+		return $this->maximum;
+	}
+}
---
+++ tests/Data/FloatRangeTest.php
@@ -0,0 +1,62 @@
+<?php
+/* Copyright (c) 1998-2019 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+/**
+ * @author  Niels Theen <ntheen@databay.de>
+ */
+
+namespace ILIAS\Data;
+
+require_once 'libs/composer/vendor/autoload.php';
+
+class FloatRangeTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * @throws \ilException
+	 */
+	public function testValidFloatRanges()
+	{
+		$floatRange = new FloatRange(3.0 , 100.4);
+
+		$this->assertSame($floatRange->minimumAsFloat(), 3.0);
+		$this->assertSame($floatRange->maximumAsFloat(), 100.4);
+	}
+
+	/**
+	 * @throws \ilException
+	 */
+	public function testHexIsAllowForRanges()
+	{
+		$floatRange = new FloatRange(0x3 , 0xA);
+
+		$this->assertSame($floatRange->minimumAsFloat(), 3.0);
+		$this->assertSame($floatRange->maximumAsFloat(), 10.0);
+	}
+
+	/**
+	 * @throws \ilException
+	 */
+	public function testBinaryIsAllowForRanges()
+	{
+		$floatRange = new FloatRange(0b11 , 0b1010);
+
+		$this->assertSame($floatRange->minimumAsFloat(), 3.0);
+		$this->assertSame($floatRange->maximumAsFloat(), 10.0);
+	}
+
+	/**
+	 * @expectedException  \ilException
+	 */
+	public function testRangeIsSameThrowsException()
+	{
+		$floatRange = new FloatRange(3.0, 3.0);
+	}
+
+	/**
+	 * @expectedException  \ilException
+	 */
+	public function testMaximumsIsLowerThanMinimumThrowsException()
+	{
+		$floatRange = new FloatRange(3.0, 1.0);
+	}
+}
