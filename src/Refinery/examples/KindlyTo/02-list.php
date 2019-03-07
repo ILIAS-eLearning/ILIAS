030f032420261f8edd81d33e3eddfa06a681da92
Niels Theen <ntheen@databay.de>
2019-03-07 09:38:12 +0100

    Add examples for KindlyTo/Group

---
+++ src/Refinery/examples/KindlyTo/02-list.php
@@ -0,0 +1,21 @@
+<?php
+/* Copyright (c) 1998-2019 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+/**
+ * @author  Niels Theen <ntheen@databay.de>
+ */
+function toList() {
+	global $DIC;
+
+	$language = $DIC->language();
+	$dataFactory = new ILIAS\Data\Factory();
+	$validationFactory = new \ILIAS\Refinery\Validation\Factory($dataFactory, $language);
+
+	$factory = new ILIAS\Refinery\BasicFactory($validationFactory);
+
+	$transformation = $factory->kindlyTo()->listOf(new \ILIAS\Refinery\KindlyTo\Transformation\IntegerTransformation());
+
+	$result = $transformation->transform(array(5, '1', 4.3));
+
+	return $result;
+}
---
+++ src/Refinery/examples/KindlyTo/04-tuple.php
@@ -0,0 +1,26 @@
+<?php
+/* Copyright (c) 1998-2019 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+/**
+ * @author  Niels Theen <ntheen@databay.de>
+ */
+function toList() {
+	global $DIC;
+
+	$language = $DIC->language();
+	$dataFactory = new ILIAS\Data\Factory();
+	$validationFactory = new \ILIAS\Refinery\Validation\Factory($dataFactory, $language);
+
+	$factory = new ILIAS\Refinery\BasicFactory($validationFactory);
+
+	$transformation = $factory->kindlyTo()->tupleOf(
+		array(
+			new \ILIAS\Refinery\KindlyTo\Transformation\IntegerTransformation(),
+			new \ILIAS\Refinery\KindlyTo\Transformation\StringTransformation()
+		)
+	);
+
+	$result = $transformation->transform(array(5.3, 2));
+
+	return $result;
+}
