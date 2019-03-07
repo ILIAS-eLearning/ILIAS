d06d4a442652eef70de43d55b2208de9c972ec6d
Björn Heyser <bh@bjoernheyser.de>
2019-03-07 11:46:18 +0100

    fixed mantis 0020852: [date_completed] not resovled in test certificates unless lp activated

--- Modules/Test/classes/class.ilObjTest.php
+++ Modules/Test/classes/class.ilObjTest.php
@@ -4302,6 +4302,7 @@ function getAnswerFeedbackPoints()
 		$found["test"]["total_requested_hints"] = $results['hint_count'];
 		$found["test"]["total_hint_points"] = $results['hint_points'];
 		$found["test"]["result_pass"] = $results['pass'];
+		$found['test']['result_tstamp'] = $results['tstamp'];
 		$found['test']['obligations_answered'] = $results['obligations_answered'];
 		
 		if( (!$total_reached_points) or (!$total_max_points) )
