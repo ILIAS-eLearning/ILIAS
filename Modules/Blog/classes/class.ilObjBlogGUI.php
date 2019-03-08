5966420c6404fcaa35c396bd262b9ea44633b6fc
Alexander Killing <killing@leifos.de>
2019-03-08 14:03:48 +0100

    some rephrasings according to the online help group

--- Modules/Blog/classes/class.ilObjBlogGUI.php
+++ Modules/Blog/classes/class.ilObjBlogGUI.php
@@ -275,19 +275,20 @@ class ilObjBlogGUI extends ilObject2GUI implements ilDesktopItemHandling
 		$opt->setInfo($lng->txt("blog_nav_mode_month_list_info"));
 		$nav_mode->addOption($opt);
 		
+
+		$mon_num = new ilNumberInputGUI($lng->txt("blog_nav_mode_month_list_num_month"), "nav_list_mon");
+		$mon_num->setInfo($lng->txt("blog_nav_mode_month_list_num_month_info"));
+		$mon_num->setSize(3);
+		$mon_num->setMinValue(1);
+		$opt->addSubItem($mon_num);
+
 		$detail_num = new ilNumberInputGUI($lng->txt("blog_nav_mode_month_list_num_month_with_post"), "nav_list_mon_with_post");
 		$detail_num->setInfo($lng->txt("blog_nav_mode_month_list_num_month_with_post_info"));
 		//$detail_num->setRequired(true);
 		$detail_num->setSize(3);
 		//$detail_num->setMinValue(0);
 		$opt->addSubItem($detail_num);
-		
-		$mon_num = new ilNumberInputGUI($lng->txt("blog_nav_mode_month_list_num_month"), "nav_list_mon");
-		$mon_num->setInfo($lng->txt("blog_nav_mode_month_list_num_month_info"));
-		$mon_num->setSize(3);
-		$mon_num->setMinValue(1);
-		$opt->addSubItem($mon_num);
-		
+
 		$opt = new ilRadioOption($lng->txt("blog_nav_mode_month_single"), ilObjBlog::NAV_MODE_MONTH);
 		$opt->setInfo($lng->txt("blog_nav_mode_month_single_info"));
 		$nav_mode->addOption($opt);
