04007a054d5ac21970b40c7e546728909b0f4a48
xus <jesuslopezreyes@gmail.com>
2019-03-12 12:06:13 +0100

    wrong link

--- src/UI/examples/Dropdown/Standard/with_buttons_and_links.php
+++ src/UI/examples/Dropdown/Standard/with_buttons_and_links.php
@@ -5,7 +5,7 @@ function with_buttons_and_links() {
 	$renderer = $DIC->ui()->renderer();
 
 	$items = array(
-		$f->button()->shy("Github", "https://www.ilias.de"),
+		$f->button()->shy("Github", "https://www.github.com"),
 		$f->link()->standard("ILIAS", "https://www.ilias.de")->withOpenInNewViewport(true)
 	);
 	return $renderer->render($f->dropdown()->standard($items)->withLabel("Actions"));
