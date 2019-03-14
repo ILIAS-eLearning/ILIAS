bf997154042b8df381eaf29a5c67fd86aeddccd5
daniwe4 <daniel.weise@concepts-and-training.de>
2019-03-14 14:48:28 +0100

    replace switch construct with a shorter construct

--- Modules/LearningSequence/classes/Player/class.ilLegacyKioskModeView.php
+++ Modules/LearningSequence/classes/Player/class.ilLegacyKioskModeView.php
@@ -226,40 +226,7 @@ class ilLegacyKioskModeView implements ILIAS\KioskMode\View
 
 	private function getTitleByType(string $type): string
 	{
-		switch ($type) {
-			case "file":
-				$title = $this->lng->txt("obj_file");
-				break;
-			case "copa":
-				$title = $this->lng->txt("obj_copa");
-				break;
-			case "lm":
-				$title = $this->lng->txt("obj_lm");
-				break;
-			case "html":
-				$title = $this->lng->txt("obj_htlm");
-				break;
-			case "sahs":
-				$title = $this->lng->txt("obj_sahs");
-				break;
-			case "iass":
-				$title = $this->lng->txt("obj_iass");
-				break;
-			case "exc":
-				$title = $this->lng->txt("obj_exc");
-				break;
-			case "tst":
-				$title = $this->lng->txt("obj_tst");
-				break;
-			case "svy":
-				$title = $this->lng->txt("obj_svy");
-				break;
-			default:
-				$title = "";
-				break;
-		}
-
-		return $title;
+		return $this->lng->txt("obj_".$type);
 	}
 
 }
