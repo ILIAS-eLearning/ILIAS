775746010264b69442c4e7634c41b22911b4a761
mjansen <mjansen@databay.de>
2019-03-21 20:31:54 +0100

    phpstan: Configuration

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-libs/composer/vendor/bin/phpstan analyse --configuration phpstan.neon --level 3 ./src $@;
+libs/composer/vendor/bin/phpstan analyse -c phpstan.neon -l 3 -n --no-progress . $@;
 
 libs/composer/vendor/phpunit/phpunit/phpunit --bootstrap ./libs/composer/vendor/autoload.php --configuration ./Services/PHPUnit/config/PhpUnitConfig.xml --exclude-group needsInstalledILIAS --verbose $@;
