8d21fecff4cb410d4f04378b2ca409c27ce0ae98
Niels Theen <ntheen@databay.de>
2019-04-03 15:07:16 +0200

    Add new datatypes to Data/Factory

--- src/Data/Factory.php
+++ src/Data/Factory.php
@@ -2,6 +2,10 @@
 /* Copyright (c) 2017 Richard Klees <richard.klees@concepts-and-training.de> Extended GPL, see docs/LICENSE */
 
 namespace ILIAS\Data;
+use ILIAS\Data\Range\FloatRange;
+use ILIAS\Data\Range\IntegerRange;
+use ILIAS\Data\Range\StrictFloatRange;
+use ILIAS\Data\Range\StrictIntegerRange;
 
 /**
  * Builds data types.
@@ -91,5 +95,63 @@ class Factory {
 	 */
 	public function refId(int $ref_id): ReferenceId {
 		return new ReferenceId($ref_id);
+	}
+
+	/**
+	 * @param $value
+	 * @return Alphanumeric
+	 */
+	public function alphanumeric($value): Alphanumeric
+	{
+		return new Alphanumeric($value);
+	}
+
+	/**
+	 * @param int $value
+	 * @return PositiveInteger
+	 */
+	public function positiveInteger(int $value): PositiveInteger
+	{
+		return new PositiveInteger($value);
+	}
+
+	/**
+	 * @param int $minimum
+	 * @param int $maximum
+	 * @return IntegerRange
+	 */
+	public function integerRange(int $minimum, int $maximum): IntegerRange
+	{
+		return new IntegerRange($minimum, $maximum);
+	}
+
+	/**
+	 * @param int $minimum
+	 * @param int $maximum
+	 * @return StrictIntegerRange
+	 */
+	public function strictIntegerRange(int $minimum, int $maximum): StrictIntegerRange
+	{
+		return new StrictIntegerRange($minimum, $maximum);
+	}
+
+	/**
+	 * @param float $minimum
+	 * @param float $maximum
+	 * @return FloatRange
+	 */
+	public function floatRange(float $minimum, float $maximum): FloatRange
+	{
+		return new FloatRange($minimum, $maximum);
+	}
+
+	/**
+	 * @param float $minimum
+	 * @param float $maximum
+	 * @return StrictFloatRange
+	 */
+	public function strictFloatRange(float $minimum, float $maximum): StrictFloatRange
+	{
+		return new StrictFloatRange($minimum, $maximum);
 	}
 }
--- tests/Data/DataFactoryTest.php
+++ tests/Data/DataFactoryTest.php
@@ -12,6 +12,12 @@ use PHPUnit\Framework\TestCase;
  * @author Stefan Hecken <stefan.hecken@concepts-and-training.de>
  */
 class DataFactoryTest extends TestCase {
+
+	/**
+	 * @var Data\Factory
+	 */
+	private $f;
+
 	protected function setUp(): void {
 		$this->f = new Data\Factory();
 	}
@@ -39,4 +45,33 @@ class DataFactoryTest extends TestCase {
 		$this->assertInstanceOf(Data\Password::class, $pwd);
 	}
 
+	public function testAlphanumeric() {
+		$dataType = $this->f->alphanumeric('someValue');
+		$this->assertInstanceOf(Data\Alphanumeric::class, $dataType);
+	}
+
+	public function testPositiveInteger() {
+		$dataType = $this->f->positiveInteger(100);
+		$this->assertInstanceOf(Data\PositiveInteger::class, $dataType);
+	}
+
+	public function testIntegerRange() {
+		$dataType = $this->f->integerRange(1, 100);
+		$this->assertInstanceOf(Data\Range\IntegerRange::class, $dataType);
+	}
+
+	public function testStrictIntegerRange() {
+		$dataType = $this->f->strictIntegerRange(1, 100);
+		$this->assertInstanceOf(Data\Range\StrictIntegerRange::class, $dataType);
+	}
+
+	public function testFloatRange() {
+		$dataType = $this->f->floatRange(1.4, 100.2);
+		$this->assertInstanceOf(Data\Range\FloatRange::class, $dataType);
+	}
+
+	public function testStrictFloatRange() {
+		$dataType = $this->f->strictFloatRange(1.4, 100.2);
+		$this->assertInstanceOf(Data\Range\StrictFloatRange::class, $dataType);
+	}
 }
