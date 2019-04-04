32cad08edc224efeb1ef51c81b8f9124a749613c
Niels Theen <ntheen@databay.de>
2019-04-04 09:42:11 +0200

    Fix applyTo for In/Parallel Transformation + Add more unit test with kindlyTo Transformations to assure this transfromation works

--- tests/Refinery/In/Transformation/ParallelTest.php
+++ tests/Refinery/In/Transformation/ParallelTest.php
@@ -31,8 +31,53 @@ class ParallelTest extends TestCase
 
 		$result = $parallel->transform('hello');
 
-		$this->assertEquals($result, array('hello', 'hello'));
+		$this->assertEquals(array('hello', 'hello'), $result);
 	}
+
+	public function testParallelTransformationWithKindlyToTransformations()
+	{
+		$parallel = new Parallel(
+			array(
+				new \ILIAS\Refinery\KindlyTo\Transformation\StringTransformation(),
+				new \ILIAS\Refinery\KindlyTo\Transformation\IntegerTransformation(),
+				new \ILIAS\Refinery\KindlyTo\Transformation\FloatTransformation()
+			)
+		);
+
+		$result = $parallel->transform(42.3);
+
+		$this->assertEquals(array('42.3', 42, 42.3), $result);
+	}
+
+	public function testParallelTransformationForApplyTo()
+	{
+		$parallel = new Parallel(
+			array(
+				new StringTransformation(),
+				new StringTransformation()
+			)
+		);
+
+		$result = $parallel->applyTo(new Ok('hello'));
+
+		$this->assertEquals(array('hello', 'hello'), $result->value());
+	}
+
+	public function testParallelTransformationForApplyToWithKindlyToTransformations()
+	{
+		$parallel = new Parallel(
+			array(
+				new \ILIAS\Refinery\KindlyTo\Transformation\StringTransformation(),
+				new \ILIAS\Refinery\KindlyTo\Transformation\IntegerTransformation(),
+				new \ILIAS\Refinery\KindlyTo\Transformation\FloatTransformation()
+			)
+		);
+
+		$result = $parallel->applyTo(new Ok(42.3));
+
+		$this->assertEquals(array('42.3', 42, 42.3), $result->value());
+	}
+
 
 	/**
 	 * @expectedException InvalidArgumentException
