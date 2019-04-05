adf1d132ec25e1215a89f5af0399fcbdb1ffb946
Alexander Killing <killing@leifos.de>
2019-04-05 14:16:02 +0200

    fixed bug #25125: Numeric Question: Fatal error in DOMDocument::loadXML()

--- Services/MediaObjects/classes/class.ilObjMediaObject.php
+++ Services/MediaObjects/classes/class.ilObjMediaObject.php
@@ -716,14 +716,14 @@ class ilObjMediaObject extends ilObject
 					if ($item->getCaption() != "")
 					{
 						$xml .= "<Caption Align=\"bottom\">".
-							str_replace("&", "&amp;", $item->getCaption())."</Caption>";
+							$this->escapeProperty($item->getCaption())."</Caption>";
 					}
 
 					// Text Representation
 					if ($item->getTextRepresentation() != "")
 					{
 						$xml .= "<TextRepresentation>".
-							str_replace("&", "&amp;", $item->getTextRepresentation())."</TextRepresentation>";
+							$this->escapeProperty($item->getTextRepresentation())."</TextRepresentation>";
 					}
 
 					// Parameter
@@ -777,14 +777,14 @@ class ilObjMediaObject extends ilObject
 					if ($item->getCaption() != "")
 					{
 						$xml .= "<Caption Align=\"bottom\">".
-							str_replace("&", "&amp;", $item->getCaption())."</Caption>";
+							$this->escapeProperty($item->getCaption())."</Caption>";
 					}
 					
 					// Text Representation
 					if ($item->getTextRepresentation() != "")
 					{
 						$xml .= "<TextRepresentation>".
-							str_replace("&", "&amp;", $item->getTextRepresentation())."</TextRepresentation>";
+							$this->escapeProperty($item->getTextRepresentation())."</TextRepresentation>";
 					}
 
 					// Parameter
@@ -884,6 +884,18 @@ class ilObjMediaObject extends ilObject
 		$xml .= "</MediaObject>";
 		return $xml;
 	}
+
+	/**
+	 * Escape property (e.g. title, caption) to XSLT -> HTML output
+	 *
+	 * @param string $a_value
+	 * @return string
+	 */
+	protected function escapeProperty($a_value)
+	{
+		return htmlspecialchars($a_value);
+	}
+
 
 	/**
 	* Replace "&" (if not an "&amp;") with "&amp;"
