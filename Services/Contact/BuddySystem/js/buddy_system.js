da3f8081d30c5e8d1513be36a4589350d090c902
mjansen <mjansen@databay.de>
2019-04-10 13:29:51 +0200

    BuddySystem: Minor JS changes

--- Services/Contact/BuddySystem/js/buddy_system.js
+++ Services/Contact/BuddySystem/js/buddy_system.js
@@ -3,7 +3,7 @@
 		config: {},
 
 		setConfig: function (config) {
-			var bs = $scope.il.BuddySystem;
+			let bs = $scope.il.BuddySystem;
 			bs.config = config;
 		}
 	};
@@ -12,16 +12,17 @@
 		config: {},
 
 		setConfig: function (config) {
-			var btn = $scope.il.BuddySystemButton;
+			let btn = $scope.il.BuddySystemButton;
 			btn.config = config;
 		},
 
 		init: function () {
-			var btn = $scope.il.BuddySystemButton, bs = $scope.il.BuddySystem;
+			let btn = $scope.il.BuddySystemButton,
+				bs = $scope.il.BuddySystem,
+				trigger_selector = "a[data-target-state], button[data-target-state]";
 
-			var trigger_selector = "a[data-target-state], button[data-target-state]";
-			$("." + btn.config.bnt_class).on("click", trigger_selector, function (e) {
-				var $trigger = $(this);
+			let onWidgetClick = function onWidgetClick(e) {
+				let $trigger = $(this);
 
 				if ($trigger.data("submitted") === true) {
 					// Prevent concurrent requests
@@ -31,14 +32,14 @@
 				e.preventDefault();
 				e.stopPropagation();
 
-				var $container = $trigger.closest('.' + btn.config.bnt_class);
+				let $container = $trigger.closest('.' + btn.config.bnt_class);
 
-				var values = {};
+				let values = {};
 				values["usr_id"] = $container.data("buddy-id");
 				values["action"] = $trigger.data("action");
 				values["cmd[" + bs.config.transition_state_cmd + "]"] = 1;
 
-				var promise = $.ajax({
+				let promise = $.ajax({
 					url:        bs.config.http_post_url,
 					type:       "POST",
 					data:       values,
@@ -47,7 +48,7 @@
 						$("." + btn.config.bnt_class).filter(function() {
 							return $(this).data("buddy-id") == $container.data("buddy-id");
 						}).each(function() {
-							var container = $(this);
+							let container = $(this);
 							container.find(trigger_selector)
 								.data("submitted", true)
 								.attr("disabled", true);
@@ -56,17 +57,17 @@
 				});
 
 				promise.done(function (response) {
-					var state = $container.data("current-state");
+					let state = $container.data("current-state");
 
-					if (response.success != undefined) {
-						if (response.state != undefined && response.state_html != undefined) {
+					if (response.success !== undefined) {
+						if (response.state !== undefined && response.state_html !== undefined) {
 							if (state != response.state) {
 								$($scope).trigger("il.bs.stateChange.beforeButtonWidgetReRendered", [$container.data("buddy-id"), response.state, state]);
 
 								$("." + btn.config.bnt_class).filter(function() {
 									return $(this).data("buddy-id") == $container.data("buddy-id");
 								}).each(function() {
-									var container = $(this);
+									let container = $(this);
 									container.find(".button-container").html(response.state_html);
 									container.data("current-state", response.state);
 								});
@@ -79,13 +80,13 @@
 					$("." + btn.config.bnt_class).filter(function() {
 						return $(this).data("buddy-id") == $container.data("buddy-id");
 					}).each(function() {
-						var container = $(this);
+						let container = $(this);
 						container.find(trigger_selector)
 							.data("submitted", false)
 							.attr("disabled", false);
 					});
 
-					if (response.message != undefined) {
+					if (response.message !== undefined) {
 						$container.find("button").popover({
 							container: "body",
 							content:   response.message,
@@ -102,43 +103,49 @@
 					$("." + btn.config.bnt_class).filter(function() {
 						return $(this).data("buddy-id") == $container.data("buddy-id");
 					}).each(function() {
-						var container = $(this);
+						let container = $(this);
 						container.find(trigger_selector)
 							.data("submitted", false)
 							.attr("disabled", false);
 					});
 				});
+			};
+
+			$($scope).on("il.bs.stateChange.afterStateChangePerformed", function(event, usr_id, is_state, was_state) {
+				if (
+					(was_state === "ilBuddySystemLinkedRelationState" || was_state === "ilBuddySystemRequestedRelationState") && is_state !== was_state
+				) {
+					if (typeof il.Awareness != "undefined") {
+						il.Awareness.reload();
+					}
+				}
+				return true;
 			});
+
+			$($scope).on("il.bs.domelement.added", function(ev, id) {
+				$("#" + id).find("." + btn.config.bnt_class).on("click", trigger_selector, onWidgetClick);
+			});
+
+			$("." + btn.config.bnt_class).on("click", trigger_selector, onWidgetClick);
 		}
 	};
-
-	$($scope).on("il.bs.stateChange.afterStateChangePerformed", function(event, usr_id, is_state, was_state) {
-		if (
-			(was_state == "ilBuddySystemLinkedRelationState" || was_state == "ilBuddySystemRequestedRelationState") && is_state != was_state
-		) {
-			if (typeof il.Awareness != "undefined") {
-				il.Awareness.reload();
-			}
-		}
-		return true;
-	});
 
 	$(document).ready(function() {
 		$("#awareness_trigger").on("awrn:shown", function(event) {
 			$("#awareness-content").find("a[data-target-state]").off("click").on("click", function(e) {
-				var bs = $scope.il.BuddySystem,
+				let bs = $scope.il.BuddySystem,
 					$elm = $(this),
 					usr_id = $elm.data("buddy-id");
 
 				e.preventDefault();
 				e.stopPropagation();
 
-				var values = {};
+				let values = {};
 				values["usr_id"] = usr_id;
 				values["action"] = $elm.data("action");
 				values["cmd[" + bs.config.transition_state_cmd + "]"] = 1;
 
-				var promise = $.ajax({
+				let promise = $.ajax({
 					url:        bs.config.http_post_url,
 					type:       "POST",
 					data:       values,
@@ -148,10 +155,10 @@
 				});
 
 				promise.done(function (response) {
-					var state = $elm.data("current-state");
-					if (response.success != undefined) {
-						if (response.state != undefined) {
-							if (state != response.state) {
+					let state = $elm.data("current-state");
+					if (response.success !== undefined) {
+						if (response.state !== undefined) {
+							if (state !== response.state) {
 								$($scope).trigger("il.bs.stateChange.afterStateChangePerformed", [usr_id, response.state, state]);
 							}
 						}
