43c9efd1e4a26ab0e6e6127e5baf1edd2bba1dfc
Niels Theen <ntheen@databay.de>
2019-04-12 10:56:42 +0200

    Additional descriptions for tooltips

--- src/UI/Component/Tooltip/Factory.php
+++ src/UI/Component/Tooltip/Factory.php
@@ -15,17 +15,21 @@ interface Factory
 	 * description:
 	 *   purpose: >
 	 *      Standard Tooltips are used to display other components.
-	 *      Whenever you want to use the standard-tooltip, please hand in a PullRequest and discuss
-	 *      it.
+	 *      Tooltips should give the users additional information about a
+	 *      displayed component.
+	 *      Tooltips can be used e.g. to give information for the correct
+	 *      input value in a form or an displayed value in a view.
 	 *   composition: >
 	 *      The content of a Standard Tooltip displays the components.
+	 *      A tooltip consists of a placement, ui components to be displayed
+	 *      and the signal the tooltip reacts to.
 	 * rules:
 	 *   usage:
 	 *      1: >
-	 *          Standard Tooltips ...
+	 *          Standard Tooltips MUST have an array of \ILIAS\UI\Component\Component
 	 * ---
 	 *
-	 * @param Component[]
+	 * @param Component[] $contents An array components that will be displayed in the tooltip.
 	 * @return \ILIAS\UI\Component\Tooltip\Standard
 	 */
 	public function standard(array $contents): \ILIAS\UI\Component\Tooltip\Standard;
