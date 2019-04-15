f693e242c07c377ab7f8620ceb8704a08fe1b800
Manfred Kellermann <m.kellermann@bfe.de>
2019-04-15 13:07:09 +0200

    PHP 7.2 compatibility ILIAS 5.3

--- Services/SystemCheck/classes/class.ilSCTask.php
+++ Services/SystemCheck/classes/class.ilSCTask.php
@@ -146,7 +146,7 @@ class ilSCTask
 		global $ilDB;
 		
 		$query = 'UPDATE sysc_tasks SET '.
-				'last_update = '.$ilDB->quote($this->getLastUpdate()->get(IL_CAL_DATETIME,'',UTC),'timestamp').', '.
+				'last_update = '.$ilDB->quote($this->getLastUpdate()->get(IL_CAL_DATETIME,'',ilTimeZone::UTC),'timestamp').', '.
 				'status = '.$ilDB->quote($this->getStatus(),'integer').', '.
 				'identifier = '.$ilDB->quote($this->getIdentifier(),'text').' '.
 				'WHERE id = '.$ilDB->quote($this->getId(),'integer');
