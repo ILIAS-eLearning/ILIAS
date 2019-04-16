1f3dc26310f1605f200db75a6302ddedd5de06e7
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2019-04-16 14:50:15 +0200

    Study Programme: include basic certificate functionality
    * added according adapter and placeholders in Certificate service
    * using own template file to edit certificate

--- Services/Certificate/classes/File/Template/Path/class.ilCertificatePathConstants.php
+++ Services/Certificate/classes/File/Template/Path/class.ilCertificatePathConstants.php
@@ -14,4 +14,5 @@ class ilCertificatePathConstants
     const SCORM_PATH     = '/scorm/certificates/';
     const LTICON_PATH     = '/lticon/certificates/';
     const CMIX_PATH     = '/cmix/certificates/';
+    const STUDY_PROGRAMME_PATH = '/prg/certificates/';
 }
