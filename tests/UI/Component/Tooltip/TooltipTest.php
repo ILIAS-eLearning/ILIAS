c2a13830adbd60896af61a0cb571388c2fb1b214
mjansenDatabay <mjansen@databay.de>
2019-04-19 15:18:07 +0200

    UI Tooltip: More tests

--- tests/UI/Component/Tooltip/TooltipTest.php
+++ tests/UI/Component/Tooltip/TooltipTest.php
@@ -1,5 +1,6 @@
 <?php declare(strict_types=1);
 
+use ILIAS\UI\Component\Tooltip\Standard;
 use ILIAS\UI\Component\Tooltip\Tooltip;
 use \ILIAS\UI\Implementation as I;
 
@@ -14,24 +15,53 @@ require_once __DIR__ . '/../../Base.php';
  */
 class TooltipTest extends ILIAS_UI_TestBase
 {
+
 	/**
-	 *
+	 * @return Standard
 	 */
-	public function testPlacingATooltipWorksAsExpected()
+	public function testInitialPlacementIsAlwaysTop(): Standard
 	{
 		$factory  = new I\Component\Tooltip\Factory(new I\Component\SignalGenerator);
 
-		$tooltip1 = $factory->standard([new I\Component\Legacy\Legacy('phpunit')]);
-		$tooltip2 = $tooltip1->withPlacementLeft();
+		$tooltip = $factory->standard([new I\Component\Legacy\Legacy('phpunit')]);
+
+		$this->assertEquals(Tooltip::PLACEMENT_TOP, $tooltip->getPlacement());
+
+		return $tooltip;
+	}
+
+	/**
+	 * @param Standard $tooltip
+	 * @depends testInitialPlacementIsAlwaysTop
+	 */
+	public function testRenderedHtmlMatchesExptectedDom(Standard $tooltip)
+	{
+		$expected = <<<EOT
+<div class="il-standard-tooltip" style="display:none;" id="id_1">
+	<div class="il-standard-tooltip-content">phpunit</div>
+</div>
+EOT;
+		$this->assertEquals(
+			$this->normalizeHTML($expected),
+			$this->normalizeHTML($this->getDefaultRenderer()->render($tooltip))
+		);
+	}
+
+	/**
+	 * @depends testInitialPlacementIsAlwaysTop
+	 * @param Standard $tooltip
+	 */
+	public function testPlacingATooltipWorksAsExpected(Standard $tooltip)
+	{
+		$tooltip2 = $tooltip->withPlacementLeft();
 		$tooltip3 = $tooltip2->withPlacementRight();
 		$tooltip4 = $tooltip3->withPlacemenBottom();
 
-		$this->assertEquals(Tooltip::PLACEMENT_TOP, $tooltip1->getPlacement());
 		$this->assertEquals(Tooltip::PLACEMENT_LEFT, $tooltip2->getPlacement());
 		$this->assertEquals(Tooltip::PLACEMENT_RIGHT, $tooltip3->getPlacement());
 		$this->assertEquals(Tooltip::PLACEMENT_BOTTOM, $tooltip4->getPlacement());
-		$this->assertEquals($tooltip1->contents(), $tooltip2->contents());
-		$this->assertEquals($tooltip1->contents(), $tooltip3->contents());
-		$this->assertEquals($tooltip1->contents(), $tooltip4->contents());
+		$this->assertEquals($tooltip->contents(), $tooltip2->contents());
+		$this->assertEquals($tooltip->contents(), $tooltip3->contents());
+		$this->assertEquals($tooltip->contents(), $tooltip4->contents());
 	}
 }
