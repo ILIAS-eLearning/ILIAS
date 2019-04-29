d97b7c633b97341363fe763f9996b5aea572fb21
Richard Klees <richard.klees@concepts-and-training.de>
2019-04-29 11:11:13 +0200

    Setup: introduced App

--- setup/cli.php
+++ setup/cli.php
@@ -2,4 +2,19 @@
 
 /* Copyright (c) 2019 Richard Klees <richard.klees@concepts-and-training.de> Extended GPL, see docs/LICENSE */
 
-echo "Hello CLI!\n";
+require_once(__DIR__."/../libs/composer/vendor/autoload.php");
+
+$c = build_container_for_setup();
+$app = $c["app"];
+$app->run();
+
+function build_container_for_setup() {
+	$c = new \Pimple\Container;
+
+	$c["app"] =  function($c) {
+		return new \ILIAS\Setup\CLI\App();
+	};
+
+	return $c;
+}
+
