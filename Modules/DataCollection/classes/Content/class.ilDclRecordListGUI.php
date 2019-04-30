250fccc9626757b9399c1e2bcb2082502f97e21f
Theodor Truffer <tt@studer-raimann.ch>
2019-04-30 16:45:02 +0200

    boyscouting

--- Modules/DataCollection/classes/Content/class.ilDclRecordListGUI.php
+++ Modules/DataCollection/classes/Content/class.ilDclRecordListGUI.php
@@ -320,7 +320,7 @@ class ilDclRecordListGUI {
 		$table->initFilter();
 		$table->resetOffset();
 		$table->writeFilterToSession();
-		$this->ctrl->redirect($this, 'listRecords');
+		$this->ctrl->redirect($this, self::CMD_LIST_RECORDS);
 	}
 
 
@@ -440,7 +440,7 @@ class ilDclRecordListGUI {
 		if ($n_skipped) {
 			ilUtil::sendInfo(sprintf($this->lng->txt('dcl_skipped_delete_records'), $n_skipped), true);
 		}
-		$this->ctrl->redirect($this, 'listRecords');
+		$this->ctrl->redirect($this, self::CMD_LIST_RECORDS);
 	}
 
 
@@ -468,12 +468,12 @@ class ilDclRecordListGUI {
 
 		/** @var ilTabsGUI $ilTabs */
 		$this->ctrl->setParameter($this, self::GET_MODE, self::MODE_VIEW);
-		$ilTabs->addSubTab('mode_1', $this->lng->txt('view'), $this->ctrl->getLinkTarget($this, 'listRecords'));
+		$ilTabs->addSubTab('mode_1', $this->lng->txt('view'), $this->ctrl->getLinkTarget($this, self::CMD_LIST_RECORDS));
 		$this->ctrl->clearParameters($this);
 
 		if ($this->table_obj->hasPermissionToDeleteRecords((int)$_GET['ref_id'])) {
 			$this->ctrl->setParameter($this, self::GET_MODE, self::MODE_MANAGE);
-			$ilTabs->addSubTab('mode_2', $this->lng->txt('dcl_manage'), $this->ctrl->getLinkTarget($this, 'listRecords'));
+			$ilTabs->addSubTab('mode_2', $this->lng->txt('dcl_manage'), $this->ctrl->getLinkTarget($this, self::CMD_LIST_RECORDS));
 			$this->ctrl->clearParameters($this);
 		}
 
