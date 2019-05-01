1c10882588ab9a2ad300ed5a205bfac64d017eed
mjansenDatabay <mjansen@databay.de>
2019-05-01 09:35:45 +0200

    UI Tooltip: Tiny example change

--- src/UI/examples/Tooltip/Standard/show_tooltip_for_button.php
+++ src/UI/examples/Tooltip/Standard/show_tooltip_for_button.php
@@ -12,20 +12,19 @@ function show_tooltip_for_button() {
 	$renderer = $DIC->ui()->renderer();
 
 	$tooltip = $factory->tooltip()
-		->standard([$factory->legacy('Hello World')]);
+		->standard([$factory->legacy('Hello World!')]);
 	$button = $factory->button()
 		->standard('Hover Me!', '#')
 		->withOnHover($tooltip->getShowSignal());
 
 	$tooltip2 = $factory->tooltip()
 		->standard([$factory->legacy(
-			'Foo bar with some <span style="color:red; font-weight: bold">HTML!</span>' .
-			'<ul><li>a</li><li>b</li><li>c</li></ul>'
+			'Foo <i>bar</i> with some <b>HTML</b>!'
 		)])
 		->withPlacementRight();
 	$button2 = $factory->button()
 		->standard('Click Me!', '#')
 		->withOnClick($tooltip2->getShowSignal());
 
-	return $renderer->render([$tooltip, $button, $tooltip2, $button2]);
+	return $renderer->render([$tooltip, $button, new \ILIAS\UI\Implementation\Component\Legacy\Legacy('&nbsp;'), $tooltip2, $button2]);
 }
