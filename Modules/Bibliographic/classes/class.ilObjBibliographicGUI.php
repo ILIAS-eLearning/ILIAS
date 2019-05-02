2bc555262534526cd31b85c9602c78923d212c90
Fabian Schmid <fs@studer-raimann.ch>
2019-05-02 18:52:58 +0200

    [FIX] 0025357: Bibliography: Updating settings not possible

--- Modules/Bibliographic/classes/class.ilObjBibliographicGUI.php
+++ Modules/Bibliographic/classes/class.ilObjBibliographicGUI.php
@@ -321,7 +321,7 @@ class ilObjBibliographicGUI extends ilObject2GUI implements ilDesktopItemHandlin
 		$lng = $DIC['lng'];
 		$form = parent::initEditForm();
 		// Add File-Upload
-		$in_file = new ilFileStandardDropzoneInputGUI($lng->txt("bibliography file"), "bibliographic_file");
+		$in_file = new ilFileInputGUI($lng->txt("bibliography file"), "bibliographic_file");
 		$in_file->setSuffixes(array("ris", "bib", "bibtex"));
 		$in_file->setRequired(false);
 		$cb_override = new ilCheckboxInputGUI($this->lng->txt("override_entries"), "override_entries");
