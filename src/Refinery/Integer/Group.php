fccf7c7ce2402874b1b641ed3dfd8fb9020db079
Niels Theen <ntheen@databay.de>
2019-05-08 09:47:21 +0200

    Add docblock description for new factory group methods

--- src/Refinery/Integer/Group.php
+++ src/Refinery/Integer/Group.php
@@ -28,11 +28,25 @@ class Group
 		$this->language = $language;
 	}
 
+	/**
+	 * Creates a constraint that can be used to check if an integer value is
+	 * greater than the defined lower limit.
+	 *
+	 * @param int $minimum - lower limit for the new constraint
+	 * @return GreaterThan
+	 */
 	public function isGreaterThan(int $minimum) : GreaterThan
 	{
 		return new GreaterThan($minimum, $this->dataFactory, $this->language);
 	}
 
+	/**
+	 * Creates a constraint that can be used to check if an integer value is
+	 * less than the defined upper limit.
+	 *
+	 * @param int $maximum - upper limit for the new constraint
+	 * @return LessThan
+	 */
 	public function isLessThan(int $maximum) : LessThan
 	{
 		return new LessThan($maximum, $this->dataFactory, $this->language);
