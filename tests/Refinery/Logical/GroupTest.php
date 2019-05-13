a903485ec864eb830556c0650586e2956cd10322
Niels Theen <ntheen@databay.de>
2019-05-13 10:00:27 +0200

    Add logical group test

---
+++ tests/Refinery/Logical/GroupTest.php
@@ -0,0 +1,81 @@
+<?php
+/* Copyright (c) 1998-2019 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+namespace ILIAS\Tests\Refinery\Logical;
+
+use ILIAS\Data\Factory;
+use ILIAS\Refinery\Integer\Constraints\GreaterThan;
+use ILIAS\Refinery\Integer\Constraints\LessThan;
+use ILIAS\Refinery\Logical\Constraint\LogicalOr;
+use ILIAS\Refinery\Logical\Constraint\Not;
+use ILIAS\Refinery\Logical\Constraint\Parallel;
+use ILIAS\Refinery\Logical\Constraint\Sequential;
+use ILIAS\Refinery\Logical\Group;
+use ILIAS\Tests\Refinery\TestCase;
+
+require_once('./libs/composer/vendor/autoload.php');
+
+class GroupTest extends TestCase
+{
+	/**
+	 * @var Group
+	 */
+	private $group;
+
+	/**
+	 * @var Factory
+	 */
+	private $dataFactory;
+
+	/**
+	 * @var \ilLanguage
+	 */
+	private $language;
+
+	/**
+	 * @var GreaterThan
+	 */
+	private $greaterThanConstraint;
+
+	/**
+	 * @var LessThan
+	 */
+	private $lessThanConstaint;
+
+	public function setUp() : void
+	{
+		$this->dataFactory = new Factory();
+		$this->language    = $this->getMockBuilder('\ilLanguage')
+			->disableOriginalConstructor()
+			->getMock();
+
+		$this->group = new Group($this->dataFactory, $this->language);
+
+		$this->greaterThanConstraint = new GreaterThan(2, $this->dataFactory, $this->language);
+		$this->lessThanConstaint = new LessThan(5, $this->dataFactory, $this->language);
+	}
+
+	public function testLogicalOrGroup()
+	{
+		$instance = $this->group->logicalOr(array($this->greaterThanConstraint, $this->lessThanConstaint));
+		$this->assertInstanceOf(LogicalOr::class, $instance);
+	}
+
+	public function testNotGroup()
+	{
+		$instance = $this->group->not($this->greaterThanConstraint);
+		$this->assertInstanceOf(Not::class, $instance);
+	}
+
+	public function testParallelGroup()
+	{
+		$instance = $this->group->parallel(array($this->greaterThanConstraint, $this->lessThanConstaint));
+		$this->assertInstanceOf(Parallel::class, $instance);
+	}
+
+	public function testSequentialGroup()
+	{
+		$instance = $this->group->sequential(array($this->greaterThanConstraint, $this->lessThanConstaint));
+		$this->assertInstanceOf(Sequential::class, $instance);
+	}
+}
