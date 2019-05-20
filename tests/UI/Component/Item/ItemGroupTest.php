04499af4c1c71d80a02e9db07c069e474ed8dd6d
Richard Klees <richard.klees@concepts-and-training.de>
2019-05-20 08:50:13 +0200

    UI: fixed rendering test of item group

--- tests/UI/Component/Item/ItemGroupTest.php
+++ tests/UI/Component/Item/ItemGroupTest.php
@@ -87,8 +87,7 @@ class ItemGroupTest extends ILIAS_UI_TestBase {
 
 		$expected = <<<EOT
 <div class="il-item-group">
-	<h4>group</h4>
-	<div class="il-item-group-items">
+	<h4>group</h4><div class="il-item-group-items">
 		<div class="il-panel-listing-std-item-container"><div class="il-item il-std-item ">
 			<h5>title1</h5>
 		</div></div><div class="il-panel-listing-std-item-container"><div class="il-item il-std-item ">
