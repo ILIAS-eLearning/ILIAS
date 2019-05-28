af965847ac7fa1958cffe099006941da45c42f07
Richard Klees <richard.klees@concepts-and-training.de>
2019-05-28 10:51:30 +0200

    Setup: some cleanup

--- src/Setup/ObjectiveIterator.php
+++ src/Setup/ObjectiveIterator.php
@@ -60,28 +60,28 @@ class ObjectiveIterator implements \Iterator {
 		$this->environment = $environment;
 	}
 
-    public function rewind() {
+	public function rewind() {
 		$this->stack = [$this->objective];
 		$this->current = null; 
 		$this->returned = [];
 		$this->reverse_dependencies = [];
 		$this->next();
-    }
+	}
 
-    public function current() {
+	public function current() {
 		if ($this->current === null) {
 			throw new \LogicException(
 				"Iterator is finished or wasn't initialized correctly internally."
 			);
 		}
 		return $this->current;
-    }
+	}
 
-    public function key() {
+	public function key() {
 		return $this->current()->getHash();
-    }
+	}
 
-    public function next() {
+	public function next() {
 		if (count($this->stack) === 0) {
 			$this->current = null;
 			return;
@@ -109,11 +109,11 @@ class ObjectiveIterator implements \Iterator {
 			$this->setReverseDependency($p->getHash(), $hash);
 		}
 		$this->next();
-    }
+	}
 
-    public function valid() {
+	public function valid() {
 		return $this->current !== null;
-    }
+	}
 
 	protected function detectDependencyCycles(string $cur, string $next) {
 		if (!isset($this->reverse_dependencies[$next])) {
@@ -134,62 +134,5 @@ class ObjectiveIterator implements \Iterator {
 			$this->reverse_dependencies[$other] = [];
 		}
 		$this->reverse_dependencies[$other][] = $cur;
-	}
-
-	/**
-	 * @return \Traversable<Objective>
-	 */
-	public function allObjectives() : \Traversable {
-		// TODO: Factor this out in a single class.
-		$stack = [$this->objective];
-		$returned = [];
-		$reverse_deps = [];
-
-		while(count($stack) > 0) {
-			$cur = array_pop($stack);
-			$preconditions = array_filter(
-				$cur->getPreconditions($this->environment),
-				function($p) use ($returned) {
-					return !isset($returned[$p->getHash()]);
-				}
-			);
-
-			$hash = $cur->getHash();
-			if (count($preconditions) === 0) {
-				if (!isset($returned[$hash])) {
-					yield $cur;
-					$returned[$hash] = true;
-				}
-			}
-			else {
-				$stack[] = $cur;
-
-				if (isset($reverse_deps[$hash])) {
-					$f = null;
-					$f = function($cur, $next) use (&$f, $reverse_deps) {
-						if (!isset ($reverse_deps[$next])) {
-							return;
-						}
-						if (in_array($cur, $reverse_deps[$next])) {
-							throw new UnachievableException(
-								"The objectives contain a dependency cycle and won't all be achievable."
-							);
-						}
-						foreach ($reverse_deps[$next] as $d) {
-							$f($cur, $d);
-						}
-					};
-					$f($hash, $hash);
-				}
-
-				foreach (array_reverse($preconditions) as $p) {
-					$stack[] = $p;
-					if (!isset($reverse_deps[$p->getHash()])) {
-						$reverse_deps[$p->getHash()] = [];
-					}
-					$reverse_deps[$p->getHash()][] = $hash;
-				}
-			}
-		}
 	}
 }
