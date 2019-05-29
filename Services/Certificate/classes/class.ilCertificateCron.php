d1b9e99291478a62cf6f5bd6ce35c9ca7a96e48d
Niels Theen <ntheen@databay.de>
2019-05-29 15:27:09 +0200

    Return valid cron job result code on run

--- Services/Certificate/classes/class.ilCertificateCron.php
+++ Services/Certificate/classes/class.ilCertificateCron.php
@@ -143,10 +143,13 @@ class ilCertificateCron extends \ilCronJob
 	{
 		$this->init();
 
+		$result = new ilCronJobResult();
+		$result->setStatus(ilCronJobResult::STATUS_NO_ACTION);
+
 		$currentMode = $this->settings->get('persistent_certificate_mode', 'persistent_certificate_mode_instant');
 		if ($currentMode !== 'persistent_certificate_mode_cron') {
 			$this->logger->warning(sprintf('Will not start cron job, because the mode is not set as cron job. Current Mode in settings: "%s"', $currentMode));
-			return;
+			return $result;
 		}
 
 		$this->logger->info('START - Begin with cron job to create user certificates from templates');
@@ -183,7 +186,6 @@ class ilCertificateCron extends \ilCronJob
 			}
 		}
 
-		$result = new ilCronJobResult();
 		$result->setStatus($status);
 		if (count($succeededGenerations) > 0) {
 			$result->setMessage(sprintf(
