d616ad85df155652e9291e3bdc9888f8eccde046
Alexander Killing <killing@leifos.de>
2019-05-29 16:58:23 +0200

    fixed count check

--- Services/Skill/classes/class.ilPersonalSkillsGUI.php
+++ Services/Skill/classes/class.ilPersonalSkillsGUI.php
@@ -179,7 +179,7 @@ class ilPersonalSkillsGUI
 	 *
 	 * @param array $a_val self evaluation values key1: base_skill_id, key2: tref_id: value: level id	
 	 */
-	function setGapAnalysisSelfEvalLevels($a_val)
+	function setGapAnalysisSelfEvalLevels(array $a_val)
 	{
 		$this->gap_self_eval_levels = $a_val;
 	}
@@ -1291,7 +1291,7 @@ class ilPersonalSkillsGUI
 		$this->getActualLevels($skills, $user_id);
 
 		$incl_self_eval = false;
-		if (count($this->getGapAnalysisSelfEvalLevels() > 0))
+		if (count($this->getGapAnalysisSelfEvalLevels()) > 0)
 		{
 			$incl_self_eval = true;
 			$self_vals = $this->getGapAnalysisSelfEvalLevels();
