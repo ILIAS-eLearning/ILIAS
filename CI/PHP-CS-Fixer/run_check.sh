306d7f696b838f07ea1e3aab47cd6ec47352eefd
Guido Vollbach <gvollbach@databay.de>
2019-06-04 14:05:40 +0200

    added more script

--- CI/PHP-CS-Fixer/run_check.sh
+++ CI/PHP-CS-Fixer/run_check.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
+PHPFIX_RESULTS_PATH="/tmp/phpfix_results"
 
-libs/composer/vendor/friendsofphp/php-cs-fixer/php-cs-fixer fix --config=./CI/PHP-CS-Fixer/code-format.php_cs --dry-run
+PRE="\t*** "
+
+function printLn() {
+	echo -e "$PRE $1"
+}
+
+libs/composer/vendor/friendsofphp/php-cs-fixer/php-cs-fixer fix --config=./CI/PHP-CS-Fixer/code-format.php_cs --dry-run --format=json > "$PHPFIX_RESULTS_PATH"
+
+PIPE_EXIT_CODE=`echo ${PIPESTATUS[0]}`
+
+printLn "Command exited with code: $PIPE_EXIT_CODE"
+
+if [[ $PIPE_EXIT_CODE -gt 0 ]]
+	then
+		printLn "Errors were found, exiting with error code."
+		exit 99
+else
+		printLn "No errors were found."
+		exit 0
+fi
