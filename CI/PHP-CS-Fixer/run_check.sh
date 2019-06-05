7d2ddd21270a1104e080d6737346944802ca9965
Guido Vollbach <gvollbach@databay.de>
2019-06-05 14:48:32 +0200

    build scelleton for script

--- CI/PHP-CS-Fixer/run_check.sh
+++ CI/PHP-CS-Fixer/run_check.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 PHPFIX_RESULTS_PATH="/tmp/phpfix_results"
+PHP_CS_FIXER="libs/composer/vendor/friendsofphp/php-cs-fixer/php-cs-fixer"
 
 PRE="\t*** "
 
@@ -7,17 +8,14 @@ function printLn() {
 	echo -e "$PRE $1"
 }
 
-libs/composer/vendor/friendsofphp/php-cs-fixer/php-cs-fixer fix --config=./CI/PHP-CS-Fixer/code-format.php_cs --dry-run --format=json > "$PHPFIX_RESULTS_PATH"
-
-PIPE_EXIT_CODE=`echo ${PIPESTATUS[0]}`
-
-printLn "Command exited with code: $PIPE_EXIT_CODE"
-
-if [[ $PIPE_EXIT_CODE -gt 0 ]]
-	then
-		printLn "Errors were found, exiting with error code."
-		exit 99
+if [[ -e $PHP-CS-FIXER ]]
+  then
+  	$PHP-CS-FIXER fix --config=./CI/PHP-CS-Fixer/code-format.php_cs --dry-run --format=json > "$PHPFIX_RESULTS_PATH"
+	
+	PIPE_EXIT_CODE=`echo ${PIPESTATUS[0]}`
+	
+	printLn "Command exited with code: $PIPE_EXIT_CODE"
 else
-		printLn "No errors were found."
-		exit 0
+	printLn "No php-cs-fixer found, please install it with the following command:"
+	printLn "\tcomposer require friendsofphp/php-cs-fixer"
 fi
