076719d7ca71373e138a91119af0d8975cbaf03d
Niels Theen <ntheen@databay.de>
2019-06-06 11:26:33 +0200

    Creating mysql IN chain to delte "on cascade". Foreign keys with "ON DELETE CASCADE" would be much simpler here. But we can't use it.

--- setup/sql/dbupdate_05.php
+++ setup/sql/dbupdate_05.php
@@ -1102,18 +1102,27 @@ if(!$ilDB->tableExists('il_cert_bgtask_migr')) {
 ?>
 <#5505>
 <?php
-if ($ilDB->tableExists('il_bt_value')) {
-    $deleteBucketsSql = 'DELETE FROM il_bt_value WHERE bucket_id IN (SELECT id FROM il_bt_bucket WHERE title = ' . $ilDB->quote('Certificate Migration', 'text') . ') ';
-    $ilDB->manipulate($deleteBucketsSql);
-}
-
-if ($ilDB->tableExists('il_bt_value_to_task')) {
-    $deleteValueToTask = 'DELETE FROM il_bt_value_to_task WHERE bucket_id IN (SELECT id FROM il_bt_bucket WHERE title =  ' . $ilDB->quote('Certificate Migration', 'text') . ')';
-    $ilDB->manipulate($deleteValueToTask);
-}
-
 if ($ilDB->tableExists('il_bt_task')) {
-    $deleteBackgroundTasksSql = 'DELETE FROM il_bt_task WHERE type = ' . $ilDB->quote('ilCertificateMigrationReducedInteraction', 'text');
+    if ($ilDB->tableExists('il_bt_value_to_task')) {
+        if ($ilDB->tableExists('il_bt_value')) {
+            $deleteBucketValuesSql = '
+DELETE FROM il_bt_value WHERE id IN (
+    SELECT value_id FROM il_bt_value_to_task WHERE task_id IN (
+        SELECT id FROM il_bt_task WHERE ' . $ilDB->like('type', 'text', 'ilCertificateMigration%') . '
+    )
+)';
+            $ilDB->manipulate($deleteBucketValuesSql);
+        }
+
+        $deleteValueToTask = '
+DELETE FROM il_bt_value_to_task
+WHERE task_id IN (
+    SELECT id FROM il_bt_task WHERE ' . $ilDB->like('type', 'text', 'ilCertificateMigration%') . '
+)';
+
+        $ilDB->manipulate($deleteValueToTask);
+    }
+    $deleteBackgroundTasksSql = 'DELETE FROM il_bt_task WHERE ' . $ilDB->like('type', 'text', 'ilCertificateMigration%');
     $ilDB->manipulate($deleteBackgroundTasksSql);
 }
 
@@ -1121,4 +1130,5 @@ if ($ilDB->tableExists('il_bt_bucket')) {
     $deleteBucketsSql = 'DELETE FROM il_bt_bucket WHERE title = ' . $ilDB->quote('Certificate Migration', 'text') ;
     $ilDB->manipulate($deleteBucketsSql);
 }
+
 ?>
