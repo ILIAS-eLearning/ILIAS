45303e309c27cdc9892672c9fdace8ef7a811980
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2019-06-07 15:58:07 +0200

    tweaks: bugfixes and referenced object cache

--- Modules/StudyProgrammeReference/classes/class.ilObjStudyProgrammeReference.php
+++ Modules/StudyProgrammeReference/classes/class.ilObjStudyProgrammeReference.php
@@ -2,6 +2,7 @@
 
 class ilObjStudyProgrammeReference extends ilContainerReference
 {
+	protected $referenced_object;
 	/**
 	 * Constructor 
 	 * @param int $a_id reference id
@@ -11,41 +12,38 @@ class ilObjStudyProgrammeReference extends ilContainerReference
 	{
 		global $DIC;
 		$this->type = 'prgr';
-		$this->tree = $DIC['ilTree'];
+		$this->tree = $DIC['tree'];
 		parent::__construct($a_id,$a_call_by_reference);
 	}
 
 	/**
 	 * Overwritten from ilObject.
 	 *
-	 * Calls nodeInserted on parent object if parent object is another program.
+	 * Calls nodeInserted on parent object if parent object is a program.
 	 */
 	public function putInTree($a_parent_ref) {
 		$res = parent::putInTree($a_parent_ref);
 
 		if (ilObject::_lookupType($a_parent_ref, true) == "prg") {
 			$par = ilObjStudyProgramme::getInstanceByRefId($a_parent_ref);
-			$par->nodeInserted($this);
+			$par->nodeInserted($this->getReferencedObject());
+		} else {
+			throw new Exception('invalid parent type');
 		}
 
 		return $res;
 	}
 
 	public function getParent() {
-		if ($this->parent === false) {
-			$parent_data = $this->tree->getParentNodeData($this->getRefId());
-			if ($parent_data["type"] != "prg") {
-				$this->parent = null;
-			}
-			else {
-				$this->parent = ilObjStudyProgramme::getInstanceByRefId($parent_data["ref_id"]);
-			}
-		}
-		return $this->parent;
+		$parent_data = $this->tree->getParentNodeData($this->getRefId());
+		return ilObjStudyProgramme::getInstanceByRefId($parent_data["ref_id"]);
 	}
 
 	public function getReferencedObject()
 	{
-		return new ilObjStudyProgramme::getInstanceByRefId($this->target_ref_id);
+		if(!$this->referenced_object) {
+			$this->referenced_object = ilObjStudyProgramme::getInstanceByRefId($this->target_ref_id);
+		}
+		return $this->referenced_object;
 	}
 }
