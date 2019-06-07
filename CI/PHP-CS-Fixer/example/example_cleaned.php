8b31ffa8f6c4b6606eda721e8ec65dc6a4046cad
Guido Vollbach <gvollbach@databay.de>
2019-06-07 13:47:52 +0200

    cleaned example file

--- CI/PHP-CS-Fixer/example/example_cleaned.php
+++ CI/PHP-CS-Fixer/example/example_cleaned.php
@@ -27,29 +27,29 @@ class Foo extends Bar implements FooInterface
 
 /** thx to https://mlocati.github.io/php-cs-fixer-configurator for the examples **/
 // cast_spaces
-$a = 0; $b = 0; $c = 0; $d=0; $e= 0 ; $f =0;
-$bar = ( string )  $a;
-$foo = (int)$b;
+$a = 0; $b = 0; $c = 0; $d = 0; $e = 0 ; $f = 0;
+$bar = (string) $a;
+$foo = (int) $b;
 // concat_space
-$foo = 'bar' . 3 . 'baz'.'qux';
+$foo = 'bar' . 3 . 'baz' . 'qux';
 // binary_operator_spaces
-$a= 1  + $b^ $d !==  $e or   $f;
+$a = 1 + $b ^ $d !== $e or $f;
 // unary_operator_spaces
 $sample = 0;
-$sample ++;
--- $sample;
-$sample = ! ! $a;
-$sample = ~  $c;
-function & foo()
+$sample++;
+--$sample;
+$sample = !!$a;
+$sample = ~$c;
+function &foo()
 {
 }
 // function_typehint_space
-function sample(array$a)
+function sample(array $a)
 {
 }
 // return_type_declaration
-function bar(int $a):string
+function bar(int $a) : string
 {
 };
 // whitespace_after_comma_in_array
-$sample = array(1,'a',$b,);
+$sample = array(1, 'a', $b, );
