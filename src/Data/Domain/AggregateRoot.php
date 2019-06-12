5d3b979842f000bdb33e33ac0827c0550a42bbca
Theodor Truffer <tt@studer-raimann.ch>
2019-06-12 10:41:32 +0200

    changes in Data/Domain

--- src/Data/Domain/AggregateRoot.php
+++ src/Data/Domain/AggregateRoot.php
@@ -3,6 +3,74 @@
 
 namespace ILIAS\Data\Domain;
 
-interface AggregateRoot extends RecordsEvents, IsEventSourced, TracksChanges {
+/**
+ * Class AggregateRoot
+ * @package ILIAS\Data\Domain
+ *
+ * @author Theodor Truffer <tt@studer-raimann.ch>
+ */
+abstract class AggregateRoot implements RecordsEvents, TracksChanges {
+
+	/** @var DomainEvent[] */
+	private $recorded_events = [];
+
+	/**
+	 * @return DomainEvents
+	 */
+	public function getRecordedEvents(): DomainEvents {
+		return new DomainEvents($this->recorded_events);
+	}
+
+	/**
+	 *
+	 */
+	public function clearRecordedEvents(): void {
+		$this->recorded_events = [];
+	}
+
+	/**
+	 * @param DomainEvent $domainEvent
+	 */
+	protected function recordApplyAndPublishThat(DomainEvent $domainEvent)
+	{
+		$this->recordThat($domainEvent);
+		$this->applyThat($domainEvent);
+		$this->publishThat($domainEvent);
+	}
+
+	/**
+	 * @param DomainEvent $domainEvent
+	 */
+	protected function recordThat(DomainEvent $domainEvent)
+	{
+		$this->recorded_events[] = $domainEvent;
+	}
+
+	/**
+	 * @param DomainEvent $domainEvent
+	 */
+	protected function applyThat(DomainEvent $domainEvent)
+	{
+		$modifier = 'apply' . get_class($domainEvent);
+		$this->$modifier($domainEvent);
+	}
+
+	/**
+	 * @param DomainEvent $domainEvent
+	 */
+	protected function publishThat(DomainEvent $domainEvent)
+	{
+		DomainEventPublisher::getInstance()->publish($domainEvent);
+	}
+
+	/**
+	 * @return IdentifiesAggregate
+	 */
+	abstract public function getAggregateId(): IdentifiesAggregate;
+
+	/**
+	 * @return bool
+	 */
+	abstract public function hasChanges(): bool;
 
 } 
