bdc850b5fd9aaf4dd206606fda41cd735ca195f9
Uwe-Kohnle <kohnle@internetlehrer-gmbh.de>
2019-06-30 21:56:14 +0200

    Fix Mantis #22964

--- Modules/ScormAicc/classes/class.ilObjSAHSLearningModule.php
+++ Modules/ScormAicc/classes/class.ilObjSAHSLearningModule.php
@@ -1411,6 +1411,19 @@ class ilObjSAHSLearningModule extends ilObject
 			$new_obj->readObject();
 		}
 		
+		// Copy learning progress settings (Mantis #0022964)
+		include_once('Services/Tracking/classes/class.ilLPObjSettings.php');
+		$obj_settings = new ilLPObjSettings($this->getId());
+		$obj_settings->cloneSettings($new_obj->getId());
+
+		include_once('Services/Object/classes/class.ilObjectLP.php');
+		/** @var ilScormLP $olp */
+		$olp = ilObjectLP::getInstance($this->getId());
+		$collection = $olp->getCollectionInstance();
+		if($collection)
+		{
+			$collection->cloneCollection($new_obj->getRefId(), $cp_options->getCopyId());
+		}
 		return $new_obj;
 	}
 	
