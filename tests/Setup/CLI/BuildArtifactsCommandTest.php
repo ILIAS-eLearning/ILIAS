4b5a174486c38eca661ade2aa64b5e0cdfa01a26
Richard Klees <richard.klees@concepts-and-training.de>
2019-07-08 13:48:32 +0200

    Setup: added stubs for build-artifacts command

---
+++ tests/Setup/CLI/BuildArtifactsCommandTest.php
@@ -0,0 +1,69 @@
+<?php
+
+/* Copyright (c) 2019 Richard Klees <richard.klees@concepts-and-training.de> Extended GPL, see docs/LICENSE */
+
+namespace ILIAS\Tests\Setup\CLI;
+
+use ILIAS\Setup;
+use Symfony\Component\Console\Tester\CommandTester;
+use ILIAS\Refinery\Factory as Refinery;
+use ILIAS\Data\Factory as DataFactory;
+
+class BuildArtifactsCommandTest extends \PHPUnit\Framework\TestCase {
+	public function testBasicFunctionality() {
+		$refinery = new Refinery($this->createMock(DataFactory::class), $this->createMock(\ilLanguage::class));
+
+		$agent = $this->createMock(Setup\Agent::class);
+		$config_reader = $this->createMock(Setup\CLI\ConfigReader::class);
+		$command = new Setup\CLI\BuildArtifactsCommand($agent, $config_reader);
+
+		$tester = new CommandTester($command);
+
+		$config = $this->createMock(Setup\Config::class);
+		$config_file = "config_file";
+		$config_file_content = ["config_file"];
+
+		$objective = $this->createMock(Setup\Objective::class);
+		$env = $this->createMock(Setup\Environment::class);
+
+		$config_reader
+			->expects($this->once())
+			->method("readConfigFile")
+			->with($config_file)
+			->willReturn($config_file_content);
+
+		$agent
+			->expects($this->once())
+			->method("hasConfig")
+			->willReturn(true);
+
+		$agent
+			->expects($this->once())
+			->method("getArrayToConfigTransformation")
+			->with()
+			->willReturn($refinery->custom()->transformation(function($v) use ($config_file_content, $config) {
+				$this->assertEquals($v, $config_file_content);
+				return $config;
+			}));
+
+		$agent
+			->expects($this->once())
+			->method("getBuildArtifactObjective")
+			->with($config)
+			->willReturn($objective);
+
+		$objective
+			->expects($this->once())
+			->method("getPreconditions")
+			->willReturn([]);
+
+		$objective
+			->expects($this->once())
+			->method("achieve")
+			->willReturn($env);
+
+		$tester->execute([
+			"config" => $config_file
+		]);
+	}
+}
