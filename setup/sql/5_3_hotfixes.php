7baee1a3eee4cfa1c62165c6090567aafe15514f
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2019-07-16 11:28:12 +0200

    fixed prg lp adjustement procedure

--- setup/sql/5_3_hotfixes.php
+++ setup/sql/5_3_hotfixes.php
@@ -650,6 +650,7 @@ if( $ilDB->tableExists($tempTableName) )
 
 ?>
 
+
 <#47>
 <?php
 	$ilCtrlStructureReader->getStructure();
@@ -657,11 +658,28 @@ if( $ilDB->tableExists($tempTableName) )
 
 <#48>
 <?php
-$q = "SELECT ref_id FROM prg_settings"
-	."	JOIN object_reference USING(obj_id)"
-	."	WHERE lp_mode = 1 AND deleted IS NULL";
+$q = "SELECT prg_settings.obj_id FROM prg_settings"
+	."	JOIN object_reference prg_ref ON prg_settings.obj_id = prg_ref.obj_id"
+	."	JOIN tree ON parent = prg_ref.ref_id"
+	."	LEFT JOIN object_reference child_ref ON tree.child = child_ref.ref_id"
+	."	LEFT JOIN object_data child ON child_ref.obj_id = child.obj_id"
+	."	WHERE lp_mode = 2 AND prg_ref.deleted IS NULL AND child.obj_id IS NULL";
 $res = $ilDB->query($q);
+$to_adjsut = [];
 while($rec = $ilDB->fetchAssoc($res)) {
-	ilObjStudyProgramme::getInstanceByRefId((int)$rec["ref_id"])->adjustLPMode();
+		$to_adjust[] = (int)$rec['obj_id'];
 }
+$ilDB->manipulate('UPDATE prg_settings SET lp_mode = 0 WHERE '.$ilDB->in('obj_id',$to_adjust,false,'integer'));
+$q = "SELECT prg_settings.obj_id FROM prg_settings"
+	."	JOIN object_reference prg_ref ON prg_settings.obj_id = prg_ref.obj_id"
+	."	JOIN tree ON parent = prg_ref.ref_id"
+	."	JOIN object_reference child_ref ON tree.child = child_ref.ref_id"
+	."	JOIN object_data child ON child_ref.obj_id = child.obj_id"
+	."	WHERE lp_mode = 2 AND prg_ref.deleted IS NULL AND child.type = 'prg'";
+$res = $ilDB->query($q);
+$to_adjsut = [];
+while($rec = $ilDB->fetchAssoc($res)) {
+		$to_adjust[] = (int)$rec['obj_id'];
+}
+$ilDB->manipulate('UPDATE prg_settings SET lp_mode = 1 WHERE '.$ilDB->in('obj_id',$to_adjust,false,'integer'));
 ?>
