39668ceaac94d5e185d5c9e3712c2a3397d06b08
Fabian Schmid <fs@studer-raimann.ch>
2019-07-19 14:06:21 +0200

    removed metabar items (which will come back later in a correct provider)

--- Services/MainMenu/classes/Provider/class.ilMMCustomTopBarProvider.php
+++ Services/MainMenu/classes/Provider/class.ilMMCustomTopBarProvider.php
@@ -1,6 +1,5 @@
 <?php
 
-use ILIAS\GlobalScreen\Identification\IdentificationInterface;
 use ILIAS\GlobalScreen\Scope\MetaBar\Provider\AbstractStaticMetaBarProvider;
 use ILIAS\GlobalScreen\Scope\MetaBar\Provider\StaticMetaBarProvider;
 
@@ -17,33 +16,6 @@ class ilMMCustomTopBarProvider extends AbstractStaticMetaBarProvider implements 
      */
     public function getMetaBarItems() : array
     {
-        $f = $this->dic->ui()->factory();
-        $txt = function ($id) {
-            return $this->dic->language()->txt($id);
-        };
-        $mb = $this->globalScreen()->metaBar();
-        $id = function ($id) : IdentificationInterface {
-            return $this->if->identifier($id);
-        };
-
-        $item[] = $mb->topLegacyItem($id('help'))
-            ->withLegacyContent($f->legacy("NOT PROVIDED"))
-            ->withSymbol($f->symbol()->glyph()->help())
-            ->withTitle("Help")
-            ->withPosition(2);
-
-        $item[] = $mb->topLegacyItem($id('notifications'))
-            ->withLegacyContent($f->legacy("NOT PROVIDED"))
-            ->withSymbol($f->symbol()->glyph()->notification()->withCounter($f->counter()->novelty(3)))
-            ->withTitle("Notifications")
-            ->withVisibilityCallable(
-                function () {
-                    return !$this->dic->user()->isAnonymous();
-                }
-            )
-            ->withPosition(3);
-
-
-        return $item;
+        return [];
     }
 }
