9fe67f9d25f65c5dd21b682ccfae5a7515c4c1c9
mstuder <ms@studer-raimann.ch>
2019-07-23 17:18:49 +0200

    WIP

--- Services/AssessmentQuestion/PublicApi/AsqAuthoringService.php
+++ Services/AssessmentQuestion/PublicApi/AsqAuthoringService.php
@@ -7,17 +7,6 @@
  */
 class AsqAuthoringService
 {
-	/**
-	 * @param string $question_uuid
-	 *
-	 * @return QuestionDto
-	 *
-	 * Get a single question from the DB by ID
-	 */
-	public function GetQuestion(string $question_uuid) : QuestionDto {
-		return $question_repository->getQuestion($question_uuid);
-	}
-
 
 	/**
 	 * @param string $parent_id
@@ -32,25 +21,25 @@ class AsqAuthoringService
 
 	/**
 	 * @param string          $parent_id
-	 * @param AsqColumns|null $columns
 	 *
 	 * @return mixed
 	 *
 	 * Gets all questions of a Container from db as an Array containing
-	 * the columns defined in the $columns parameter
+	 * the generic question data fields
 	 */
-	public function GetQuestionsOfContainerAsAssocArray(string $parent_id, AsqColumns $columns) {
-		return $question_repository->getQuestionsOfContainerAsArray($parent_id, $columns);
+	public function GetQuestionsOfContainerAsAssocArray(string $parent_id) {
+		return $question_repository->getQuestionsOfContainerAsArray($parent_id);
 	}
 
 	/**
 	 * @param string|null $question_uuid
 	 *
-	 * @return QuestionAuthoringGUI
+	 * @return ILIAS\UI\Component\Link
 	 *
 	 * Gets the link to the question authoring component
 	 */
 	public function GetEditLink(string $question_uuid) : \ILIAS\UI\Component\Link\Link {
-		//create link to edit component for given question
+
 	}
+
 }
