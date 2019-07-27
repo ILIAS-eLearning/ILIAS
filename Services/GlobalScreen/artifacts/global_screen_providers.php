43e3945f2d3a6785bdb88651d807d1fa9e942635
Alexander Killing <killing@leifos.de>
2019-07-27 13:38:59 +0200

    fixed bug #25729: Exercise/Mail Templates: Missing translations

--- Services/GlobalScreen/artifacts/global_screen_providers.php
+++ Services/GlobalScreen/artifacts/global_screen_providers.php
@@ -32,7 +32,6 @@
   'ILIAS\\GlobalScreen\\Scope\\Layout\\Provider\\ModificationProvider' => 
   array (
     0 => 'ILIAS\\Container\\Screen\\MemberViewLayoutProvider',
-    1 => 'ILIAS\\UICore\\DummyLogoProvider',
-    2 => 'ILIAS\\UICore\\PageContentProvider',
+    1 => 'ILIAS\\UICore\\PageContentProvider',
   ),
 );
