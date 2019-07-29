34b64ae278a4ac1e995244abe48baed46080f5aa
Alexander Killing <killing@leifos.de>
2019-07-29 13:47:09 +0200

    moved process docu to readme

--- Modules/Exercise/AssMemberState/classes/class.ilExcAssMemberState.php
+++ Modules/Exercise/AssMemberState/classes/class.ilExcAssMemberState.php
@@ -3,33 +3,7 @@
 /* Copyright (c) 1998-2017 ILIAS open source, Extended GPL, see docs/LICENSE */
 
 /**
- * Handles everything about the state (current phase) of a user in an assignment using
- * assignment, individual deadline, user and team information.
- *
- *
- *   Absolute Deadlines: GS <-> DL - GPD - ID
- *   Relative Deadlines: GS - IS <-> CaD - ID
- *
- *
- * GS)  General Start: As entered in settings. For absolute deadlines, this also starts the submission, for relative
- *      deadline this allows the user to start the submission period. (0 = immediately)
- * IS)  Individual Start: TS when user hits "Start" button for an assignment using a relative deadline
- * SS)  Submission Start: For absolute deadlines this is General Start, for relative deadlines Individual Start
- * DL)  Deadline: absolute Deadline (e.g. 5.12.2017) as set in settings
- * RD)  Relative Deadline: relative Deadline (e.g. 10 Days) as set in settings
- * CaD) Calculated Deadline: Starting Timestamp + Relative Deadline
- * ID)  Individual Deadline: Set by tutor in "Submissions and Grade" screen
- * CoD) Common Deadline: Deadline or Calculated Deadline
- *      Used for "Ended on" or "Edit Until" presentation
- * OD)  Official Deadline: Max of (Deadline and Individual Deadline) or (Calculated Deadline and Individual Deadline)
- * ED)  Effective Deadline: Max of official deadline and grace period end date
- * GPD) Grace Period End Date: As being set in the settings of assignmet by tutor
- * GP)  Grace Period: Period between Official Deadline and Grace Period End Date.
- * SP)  Submission Period: From Submission Start (if not given immediately) to Max of (Official Deadline and Grace Period End Date)
- * LS)  Late Submission Period: Submissions being handed in during Grace Period
- * PS)  Peer Review Start: Max of (Official Deadline OF ALL USERS and Grace Period End Date)
- * PD)  Peer Review Deadline: As being set in the settings of assignmet by tutor
- * PP)  Peer Review Period: From Peer Feedback Start to Peer Feedback Deadline (may be infinite, if no deadline given)
+ * State of a participant in the progress of an exercise assignment
  *
  * @author Alex Killing <alex.killing@gmx.de>
  * @ingroup ModulesExercise
