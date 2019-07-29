69d3a84e667fcb609b8169449b0e55dd2692493f
Fabian Schmid <fs@studer-raimann.ch>
2019-07-29 07:20:37 +0200

    CS fix

--- Modules/File/classes/PreProcessors/class.ilCountPDFPagesPreProcessors.php
+++ Modules/File/classes/PreProcessors/class.ilCountPDFPagesPreProcessors.php
@@ -10,25 +10,28 @@ use ILIAS\FileUpload\Processor\PreProcessor;
  *
  * @author Fabian Schmid <fs@studer-raimann.ch>
  */
-class ilCountPDFPagesPreProcessors implements PreProcessor {
+class ilCountPDFPagesPreProcessors implements PreProcessor
+{
 
-	const PAGE_COUNT = 'page_count';
+    const PAGE_COUNT = 'page_count';
 
 
-	/**
-	 * @inheritdoc
-	 */
-	public function process(FileStream $stream, Metadata $metadata) {
-		if ($metadata->getMimeType() == ilMimeTypeUtil::APPLICATION__PDF
-		    && PATH_TO_GHOSTSCRIPT != "") {
+    /**
+     * @inheritdoc
+     */
+    public function process(FileStream $stream, Metadata $metadata)
+    {
+        if ($metadata->getMimeType() == ilMimeTypeUtil::APPLICATION__PDF
+            && PATH_TO_GHOSTSCRIPT != ""
+        ) {
 
-			$PATH_TO_PDF = $stream->getMetadata('uri');
-			$arg = "-q -dNODISPLAY -c \"($PATH_TO_PDF) (r) file runpdfbegin pdfpagecount = quit\";";
-			$return = ilUtil::execQuoted(PATH_TO_GHOSTSCRIPT, $arg);
+            $PATH_TO_PDF = $stream->getMetadata('uri');
+            $arg = "-q -dNODISPLAY -c \"($PATH_TO_PDF) (r) file runpdfbegin pdfpagecount = quit\";";
+            $return = ilUtil::execQuoted(PATH_TO_GHOSTSCRIPT, $arg);
 
-			$metadata->additionalMetaData()->put(self::PAGE_COUNT, (string)$return[0]);
-		}
+            $metadata->additionalMetaData()->put(self::PAGE_COUNT, (string) $return[0]);
+        }
 
-		return new ProcessingStatus(ProcessingStatus::OK, 'ilCountPDFPagesPreProcessors');
-	}
+        return new ProcessingStatus(ProcessingStatus::OK, 'ilCountPDFPagesPreProcessors');
+    }
 }
