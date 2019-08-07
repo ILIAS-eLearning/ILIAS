a5d27fb04c616d25b237c837552f7b682e123636
ukohnle <kohnle@internetlehrer-gmbh.de>
2019-08-07 17:05:26 +0000

    php7.2 issue count

--- Modules/ScormAicc/classes/SCORM/class.ilSCORMExplorer.php
+++ Modules/ScormAicc/classes/SCORM/class.ilSCORMExplorer.php
@@ -218,7 +218,7 @@ class ilSCORMExplorer extends ilExplorer
 			$tpl = new ilTemplate("tpl.sahs_tree_free.html", true, true, "Modules/ScormAicc");
 		}
 
-		if (count($option["childs"])) {
+		if (is_array($option["childs"]) && count($option["childs"])) {
 			foreach ($option["childs"] as $key => $ch_option) {
 				$tpl->setCurrentBlock("childs");
 				$tpl->setVariable("CHILDS", $this->createOutput($ch_option, $jsApi)->get());
