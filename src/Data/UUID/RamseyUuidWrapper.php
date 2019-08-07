2d7c4f43a5c1300fd5e7d57d93a5a663e20abc73
Theodor Truffer <tt@studer-raimann.ch>
2019-08-07 16:32:46 +0200

    leaner wrapper for library ramsey UUID (#2044)
    * library ramsey\uuid: removed unnecessary methods in wrapper
    
    * library ramsey\uuid: renamed UuidInterface -> Uuid

--- src/Data/UUID/RamseyUuidWrapper.php
+++ src/Data/UUID/RamseyUuidWrapper.php
@@ -11,7 +11,7 @@ use Ramsey\Uuid\UuidInterface as RamseyUuidInterface;
  *
  * @author Theodor Truffer <tt@studer-raimann.ch>
  */
-class RamseyUuidWrapper implements UuidInterface {
+class RamseyUuidWrapper implements Uuid {
 
 	/**
 	 * @var RamseyUuidInterface
@@ -38,7 +38,7 @@ class RamseyUuidWrapper implements UuidInterface {
 	 * @param RamseyUuidWrapper $other
 	 * @return int
 	 */
-	public function compareTo(UuidInterface $other): int {
+	public function compareTo(Uuid $other): int {
 		return $this->wrapped_uuid->compareTo($other->getWrappedUuid());
 	}
 
@@ -46,120 +46,8 @@ class RamseyUuidWrapper implements UuidInterface {
 	 * @param RamseyUuidWrapper $other
 	 * @return bool
 	 */
-	public function equals(UuidInterface $other): bool {
+	public function equals(Uuid $other): bool {
 		return $this->wrapped_uuid->equals($other->getWrappedUuid());
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getBytes(): string {
-		return $this->wrapped_uuid->getBytes();
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getHex(): string {
-		return $this->wrapped_uuid->getHex();
-	}
-
-	/**
-	 * @return array
-	 */
-	public function getFieldsHex(): array {
-		return $this->wrapped_uuid->getFieldsHex();
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getClockSeqHiAndReservedHex(): string {
-		return $this->wrapped_uuid->getClockSeqHiAndReservedHex();
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getClockSeqLowHex(): string {
-		return $this->wrapped_uuid->getClockSeqLowHex();
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getClockSequenceHex(): string {
-		return $this->wrapped_uuid->getClockSequenceHex();
-	}
-
-	/**
-	 * @return int
-	 */
-	public function getInteger(): int {
-		return $this->wrapped_uuid->getInteger();
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getLeastSignificantBitsHex(): string {
-		return $this->wrapped_uuid->getLeastSignificantBitsHex();
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getMostSignificantBitsHex(): string {
-		return $this->wrapped_uuid->getMostSignificantBitsHex();
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getNodeHex(): string {
-		return $this->wrapped_uuid->getNodeHex();
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getTimeHiAndVersionHex(): string {
-		return $this->wrapped_uuid->getTimeHiAndVersionHex();
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getTimeLowHex(): string {
-		return $this->wrapped_uuid->getTimeLowHex();
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getTimeMidHex(): string {
-		return $this->wrapped_uuid->getTimeMidHex();
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getUrn(): string {
-		return $this->wrapped_uuid->getUrn();
-	}
-
-	/**
-	 * @return int
-	 */
-	public function getVariant(): int {
-		return $this->wrapped_uuid->getVariant();
-	}
-
-	/**
-	 * @return int
-	 */
-	public function getVersion(): int {
-		return $this->wrapped_uuid->getVersion();
 	}
 
 	/**
