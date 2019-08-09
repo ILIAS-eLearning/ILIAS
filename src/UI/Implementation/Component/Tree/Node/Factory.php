bea5cf1dc5a9b36174962952296e0a7173692e66
Niels Theen <ntheen@databay.de>
2019-08-09 16:34:30 +0200

    Add uri link parameter to node factory

--- src/UI/Implementation/Component/Tree/Node/Factory.php
+++ src/UI/Implementation/Component/Tree/Node/Factory.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace ILIAS\UI\Implementation\Component\Tree\Node;
 
+use ILIAS\Data\URI;
 use ILIAS\UI\Component\Tree\Node as INode;
 use ILIAS\UI\Component\Symbol\Icon\Icon as IIcon;
 use ILIAS\UI\Component\Tree\Node\Bylined as IByline;
@@ -10,13 +11,13 @@ use \ILIAS\UI\Implementation\Component\Tree\Node\Bylined;
 
 class Factory implements INode\Factory
 {
-	/**
-	 * @inheritdoc
-	 */
-	public function simple(string $label, IIcon $icon=null): INode\Simple
-	{
-		return new Simple($label, $icon);
-	}
+    /**
+     * @inheritdoc
+     */
+    public function simple(string $label, IIcon $icon = null, URI $link = null) : INode\Simple
+    {
+        return new Simple($label, $icon, $link);
+    }
 
 	public function bylined(string $label, string $byline, IIcon $icon = null) : IByline
 	{
