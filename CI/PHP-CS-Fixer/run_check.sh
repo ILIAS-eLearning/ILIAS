cbaf4905d07f72dd505fbff3af2d641bae68472c
Guido Vollbach <gvollbach@databay.de>
2019-08-12 08:36:09 +0200

    php-cs-fixer: result handling

--- CI/PHP-CS-Fixer/run_check.sh
+++ CI/PHP-CS-Fixer/run_check.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 PHPFIX_RESULTS_PATH="/tmp/phpfix_results"
 PHP_CS_FIXER="libs/composer/vendor/friendsofphp/php-cs-fixer/php-cs-fixer"
+TRAVIS_RESULTS_DIRECTORY="/tmp/CI-Results/"
 
 PRE="\t*** "
-echo `pwd`
-echo `ls libs/composer/vendor/friendsofphp/php-cs-fixer/`
+
 function printLn() {
 	echo -e "$PRE $1"
 }
@@ -16,6 +16,19 @@ if [[ -x "$PHP_CS_FIXER" ]]
 	PIPE_EXIT_CODE=`echo ${PIPESTATUS[0]}`
 	
 	printLn "Command exited with code: $PIPE_EXIT_CODE"
+
+	printLn "Cloning results repository, copy results file."
+	if [ -d "$TRAVIS_RESULTS_DIRECTORY" ]; then
+		printLn "Starting to remove old temp directory"
+		rm -rf "$TRAVIS_RESULTS_DIRECTORY"
+	fi
+
+	cd /tmp && git clone https://github.com/ILIAS-eLearning/CI-Results
+
+	printLn "Switching directory and run results handling."
+	cp "$PHPFIX_RESULTS_PATH" "$TRAVIS_RESULTS_DIRECTORY/data/"
+	cd "$TRAVIS_RESULTS_DIRECTORY" && ./run.sh
+	
 else
 	printLn "No php-cs-fixer found, please install it with the following command:"
 	printLn "\tcomposer require friendsofphp/php-cs-fixer"
