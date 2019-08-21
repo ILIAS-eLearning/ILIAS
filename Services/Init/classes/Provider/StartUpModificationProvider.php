29f36681ccb5490e75bd43a922833753fce0b695
Fabian Schmid <fs@studer-raimann.ch>
2019-08-21 17:05:41 +0200

    implemented null mainbar in login

--- Services/Init/classes/Provider/StartUpModificationProvider.php
+++ Services/Init/classes/Provider/StartUpModificationProvider.php
@@ -2,8 +2,11 @@
 
 namespace ILIAS\Init\Provider;
 
+use ILIAS\GlobalScreen\Scope\Layout\Factory\MainBarModification;
 use ILIAS\GlobalScreen\Scope\Layout\Provider\AbstractModificationProvider;
+use ILIAS\GlobalScreen\ScreenContext\Stack\CalledContexts;
 use ILIAS\GlobalScreen\ScreenContext\Stack\ContextCollection;
+use ILIAS\UI\Component\MainControls\MainBar;
 
 /**
  * Class StartUpModificationProvider
@@ -19,5 +22,14 @@ class StartUpModificationProvider extends AbstractModificationProvider
     public function isInterestedInContexts() : ContextCollection
     {
         return $this->context_collection->external();
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    public function getMainBarModification(CalledContexts $screen_context_stack) : ?MainBarModification
+    {
+        return $this->factory->mainbar()->withModification(function (?MainBar $current) : ?MainBar { return null; })->withLowPriority();
     }
 }
