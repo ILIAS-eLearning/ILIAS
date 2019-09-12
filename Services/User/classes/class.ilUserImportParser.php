9a03138fd2313366fc2e831f91f7b4e1bcf7c581
mjansen <mjansen@databay.de>
2019-09-12 12:52:15 +0200

    User Welcome Mail: Fixed Mantis issue 26060

--- Services/User/classes/class.ilUserImportParser.php
+++ Services/User/classes/class.ilUserImportParser.php
@@ -266,11 +266,6 @@ class ilUserImportParser extends ilSaxParser
 	private $current_messenger_type;
 
 	/**
-	 * @var array
-	 */
-	private static $account_mail_cache = array();
-
-	/**
 	* Constructor
 	*
 	* @param	string		$a_xml_file		xml file
@@ -339,6 +334,7 @@ class ilUserImportParser extends ilSaxParser
 
 		include_once("Services/Mail/classes/class.ilAccountMail.php");
 		$this->acc_mail = new ilAccountMail();
+		$this->acc_mail->setAttachConfiguredFiles(true);
 		$this->acc_mail->useLangVariablesAsFallback(true);
 
 		parent::__construct($a_xml_file);
@@ -2295,52 +2291,8 @@ class ilUserImportParser extends ilSaxParser
 			($this->isSendMail() && $this->userObj->getEmail() != ""))
 		{
 			$this->acc_mail->setUser($this->userObj);
-
-			$amail = $this->readAccountMailFromCache($this->userObj->getLanguage());
-			if($amail["att_file"])
-			{
-				include_once "Services/User/classes/class.ilFSStorageUserFolder.php";
-				$fs = new ilFSStorageUserFolder(USER_FOLDER_ID);
-				$fs->create();
-				$path = $fs->getAbsolutePath() . "/";
-
-				$this->acc_mail->addAttachment($path . "/" . $amail["lang"], $amail["att_file"]);
-			}
 			$this->acc_mail->send();
 		}
-	}
-
-	/**
-	 * @param $lang_key
-	 * @return mixed
-	 */
-	private function readAccountMailFromCache($lang_key)
-	{
-		if(!isset(self::$account_mail_cache[$lang_key]))
-		{
-			$default_lang_key = $GLOBALS['DIC']["lng"]->getDefaultLanguage();
-
-			// try individual account mail in user administration
-			include_once './Services/User/classes/class.ilObjUserFolder.php';
-
-			$amail = ilObjUserFolder::_lookupNewAccountMail($lang_key);
-
-			if (trim($amail["body"]) != "" && trim($amail["subject"]) != "")
-			{
-				self::$account_mail_cache[$lang_key] = $amail;
-			}
-			else
-			{
-				$lang_key = $default_lang_key;
-			}
-
-			if(!isset(self::$account_mail_cache[$default_lang_key]))
-			{
-				$amail = ilObjUserFolder::_lookupNewAccountMail($default_lang_key);
-				self::$account_mail_cache[$default_lang_key] = $amail;
-			}
-		}
-		return self::$account_mail_cache[$lang_key];
 	}
 
 	/**
