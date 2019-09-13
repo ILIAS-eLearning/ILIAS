96a1f178653f34db9e98769e89297a917635a095
Fabian Schmid <fs@studer-raimann.ch>
2019-09-13 17:21:42 +0200

    CS-fix

--- src/HTTP/Response/Sender/DefaultResponseSenderStrategy.php
+++ src/HTTP/Response/Sender/DefaultResponseSenderStrategy.php
@@ -12,49 +12,51 @@ use Psr\Http\Message\ResponseInterface;
  *
  * @author  Nicolas Schäfli <ns@studer-raimann.ch>
  */
-class DefaultResponseSenderStrategy implements ResponseSenderStrategy {
+class DefaultResponseSenderStrategy implements ResponseSenderStrategy
+{
 
-	/**
-	 * Sends the rendered response to the client.
-	 *
-	 * @param ResponseInterface $response The response which should be send to the client.
-	 *
-	 * @return void
-	 * @throws ResponseSendingException Thrown if the response was already sent to the client.
-	 */
-	public function sendResponse(ResponseInterface $response) {
-		//check if the request is already send
-		if (headers_sent()) {
-			throw new ResponseSendingException("Response was already sent.");
-		}
+    /**
+     * Sends the rendered response to the client.
+     *
+     * @param ResponseInterface $response The response which should be send to the client.
+     *
+     * @return void
+     * @throws ResponseSendingException Thrown if the response was already sent to the client.
+     */
+    public function sendResponse(ResponseInterface $response)
+    {
+        //check if the request is already send
+        if (headers_sent()) {
+            throw new ResponseSendingException("Response was already sent.");
+        }
 
-		//set status code
-		http_response_code($response->getStatusCode());
+        //set status code
+        http_response_code($response->getStatusCode());
 
-		//render all headers
-		foreach ($response->getHeaders() as $key => $header) {
-			header("$key: " . $response->getHeaderLine($key));
-		}
+        //render all headers
+        foreach ($response->getHeaders() as $key => $header) {
+            header("$key: " . $response->getHeaderLine($key));
+        }
 
-		//rewind body stream
-		$response->getBody()->rewind();
+        //rewind body stream
+        $response->getBody()->rewind();
 
-		//detach psr-7 stream from resource
-		$resource = $response->getBody()->detach();
+        //detach psr-7 stream from resource
+        $resource = $response->getBody()->detach();
 
-		$sendStatus = false;
+        $sendStatus = false;
 
-		if (is_resource($resource)) {
-			set_time_limit(0);
-			$sendStatus = fpassthru($resource);
+        if (is_resource($resource)) {
+            set_time_limit(0);
+            $sendStatus = fpassthru($resource);
 
-			//free up resources
-			fclose($resource);
-		}
+            //free up resources
+            fclose($resource);
+        }
 
-		//check if the body was successfully send to the client
-		if ($sendStatus === false) {
-			throw new ResponseSendingException("Could not send body content to client.");
-		}
-	}
+        //check if the body was successfully send to the client
+        if ($sendStatus === false) {
+            throw new ResponseSendingException("Could not send body content to client.");
+        }
+    }
 }
