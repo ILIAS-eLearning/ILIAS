f2762e965945766fabb7a4a978b93280723f5ddc
mjansenDatabay <mjansen@databay.de>
2019-09-19 20:11:57 +0200

    Authentication: Removed dead class ilAuthCalendar

--- src/Setup/ImplementationOfInterfaceFinder.php
+++ src/Setup/ImplementationOfInterfaceFinder.php
@@ -24,7 +24,6 @@ class ImplementationOfInterfaceFinder
             '/tests/',
             '/setup/',
             // Classes using removed Auth-class from PEAR
-            '.*ilAuthCalendar.*',
             '.*ilAuthCAS.*',
             '.*ilAuthContainerCAS.*',
             '.*ilAuthContainerECS.*',
