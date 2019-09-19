95155f4221afd68da9293487c34cad55dc1a2275
mjansenDatabay <mjansen@databay.de>
2019-09-19 19:59:13 +0200

    Authentication: Removed unused methods of ilAuthFactory

--- Services/Authentication/classes/class.ilAuthFactory.php
+++ Services/Authentication/classes/class.ilAuthFactory.php
@@ -72,12 +72,7 @@ class ilAuthFactory
 	 * @var int
 	 */
 	private static $context = self::CONTEXT_WEB;
-	
-	/**
-	 * @var array context specific options
-	 */
-	private static $context_options = array();
-	
+
 	/**
 	 * 
 	 * @return int current context
@@ -96,74 +91,4 @@ class ilAuthFactory
 	{
 		self::$context = $a_context;
 	}
-	
-	/**
-	 * set context specific options for 
-	 * later use in factory.
-	 * @return 
-	 * @param object $a_context
-	 * @param object $a_options
-	 */
-	public static function setContextOptions($a_context,$a_options)
-	{
-		self::$context_options[$a_context] = $a_options;
-	}
-	
-	/**
-	 * Get options for a specific context
-	 * @return 
-	 */
-	public static function getContextOptions($a_context)
-	{
-		return self::$context_options[$a_context] ? 
-			self::$context_options[$a_context] : 
-			array();
-	}
-	
-	
-	/**
-	 * The factory
-	 * @param object	$container ilAuthContainerBase
-	 * @param array		$options		
-	 * @return object ilAuthContainerBase
-	 */
-	public static function factory(ilAuthContainerBase $deco)
-	{
-		$options = self::getContextOptions(self::getContext());
-		
-		switch(self::$context)
-		{
-			case self::CONTEXT_HTTP:
-				include_once './Services/Authentication/classes/class.ilAuthHTTP.php';
-				return new ilAuthHTTP($deco,$options);
-
-			case self::CONTEXT_SOAP:
-				include_once './Services/WebServices/SOAP/classes/class.ilAuthSOAP.php';
-				return new ilAuthSOAP($deco,$options);
-				
-			case self::CONTEXT_CAS:
-				include_once './Services/CAS/classes/class.ilAuthCAS.php';
-				return new ilAuthCAS($deco,$options);
-				
-			case self::CONTEXT_CALENDAR:
-				include_once './Services/Calendar/classes/class.ilAuthCalendar.php';
-				return new ilAuthCalendar($deco,$options);
-				
-			case self::CONTEXT_CALENDAR_TOKEN:
-				include_once './Services/Calendar/classes/class.ilAuthCalendarToken.php';
-				include_once './Services/Calendar/classes/class.ilAuthContainerCalendarToken.php';
-				// Force token authentication
-				$GLOBALS['DIC']['ilLog']->write('Calling calendar token');
-				return new ilAuthCalendarToken(new ilAuthContainerCalendarToken(),$options);
-				
-			case self::CONTEXT_ECS:
-				include_once './Services/WebServices/ECS/classes/class.ilAuthECS.php';
-				return new ilAuthECS($deco,$options);
-			
-			case self::CONTEXT_APACHE:
-				include_once './Services/AuthApache/classes/class.ilAuthApache.php';
-				return new ilAuthApache($deco,$options);
-		}
-	}
 }
-?>
