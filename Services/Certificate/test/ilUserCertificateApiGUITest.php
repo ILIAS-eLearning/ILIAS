ceda9421bdc3d83f669c8495a0d7a111efaeac34
mjansen <mjansen@databay.de>
2019-09-24 09:07:26 +0200

    Certificate: Fix unit test

--- Services/Certificate/test/ilUserCertificateApiGUITest.php
+++ Services/Certificate/test/ilUserCertificateApiGUITest.php
@@ -1,23 +1,29 @@
 <?php
 /* Copyright (c) 1998-2019 ILIAS open source, Extended GPL, see docs/LICENSE */
 
+use Psr\Http\Message\ServerRequestInterface;
+
 /**
  * @author  Niels Theen <ntheen@databay.de>
  */
 class ilUserCertificateApiGUITest extends ilCertificateBaseTestCase
 {
-    public function testCreationOfGuiClass()
+    /**
+     * 
+     */
+    public function testCreationOfGuiClass() : void
     {
         $language = $this->getMockBuilder('ilLanguage')
             ->disableOriginalConstructor()
             ->getMock();
 
-        $request = new \GuzzleHttp\Psr7\Request('GET', 'ilias.de');
+        $request = $this->getMockBuilder(ServerRequestInterface::class)->getMock();
 
         $logger = $this->getMockBuilder('ilLogger')
             ->disableOriginalConstructor()
             ->getMock();
 
         $gui = new ilUserCertificateApiGUI($language, $request, $logger);
+        $this->assertInstanceOf(ilUserCertificateApiGUI::class, $gui);
     }
 }
