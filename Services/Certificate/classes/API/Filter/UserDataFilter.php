efffa96ac91d5174b4a144bbed5dcab510221282
mjansen <mjansen@databay.de>
2019-09-26 09:37:11 +0200

    Certificate: Change filter according to review remarks

--- Services/Certificate/classes/API/Filter/UserDataFilter.php
+++ Services/Certificate/classes/API/Filter/UserDataFilter.php
@@ -8,76 +8,200 @@ namespace Certificate\API\Filter;
  */
 class UserDataFilter
 {
-    /** @var string */
+    /** @var string|null */
     private $objectTitle;
 
-    /** @var int */
+    /** @var int|null */
     private $objectId;
 
-    /** @var int */
+    /** @var int|null */
     private $issuedBeforeTimestamp;
 
     /** @var int|null */
     private $issuedAfterTimestamp;
 
     /** @var bool */
-    private $onlyActive;
+    private $onlyActive = true;
 
-    /** @var string */
+    /** @var string|null */
     private $userFirstName;
 
-    /** @var string */
+    /** @var string|null */
     private $userLastName;
 
-    /** @var string */
+    /** @var string|null */
     private $userLogin;
 
-    /** @var string */
+    /** @var string|null */
     private $userEmail;
 
-    /** @var array */
+    /** @var int[] */
     private $userIds = [];
 
     /**
-     * @param array $userIds
-     * @param string $userFirstName
-     * @param string $userLastName
-     * @param string $userLogin
-     * @param string $userEmail - matches on the first and the second email address
-     * @param string|null $objectTitle
-     * @param int|null $objectId
-     * @param int|null $issuedBeforeTimestamp
-     * @param int|null $issuedAfterTimestamp
-     * @param bool $onlyActive - Show only the currently active certificates of the user
+     * @param int[] $usrIds
+     * @param bool $onlyActive Show only the currently active certificates of the user
      * @throws \ilException
      */
     public function __construct(
-        array $userIds,
-        string $userFirstName = null,
-        string $userLastName = null,
-        string $userLogin = null,
-        string $userEmail = null,
-        string $objectTitle = null,
-        int $objectId = null,
-        int $issuedBeforeTimestamp = null,
-        int $issuedAfterTimestamp = null,
+        array $usrIds,
         bool $onlyActive = true
     ) {
-        if (array() === $userIds) {
-            throw new \ilException('The array of user ids is empty');
+        if ([] === $usrIds) {
+            throw new \ilException('The passed array of user ids must not be empty!');
         }
 
-        $this->userIds = $userIds;
-        $this->userFirstName = $userFirstName;
-        $this->userLastName = $userLastName;
-        $this->userLogin = $userLogin;
-        $this->userEmail = $userEmail;
+        array_walk($usrIds, function(int $usrId) {
+           // Do nothing, use this for type safety of array values 
+        });
 
-        $this->objectTitle = $objectTitle;
-        $this->objectId = $objectId;
-        $this->issuedBeforeTimestamp = $issuedBeforeTimestamp;
-        $this->issuedAfterTimestamp = $issuedAfterTimestamp;
+        $this->userIds = $usrIds;
         $this->onlyActive = $onlyActive;
+    }
+
+    /**
+     * @param string|null $title
+     * @return $this
+     */
+    public function withObjectTitle(?string $title) : self
+    {
+        $clone = clone $this;
+        $clone->objectTitle = $title;
+        
+        return $clone;
+    }
+
+    /**
+     * @param string|null $firstName
+     * @return $this
+     */
+    public function withUserFirstName(?string $firstName) : self
+    {
+        $clone = clone $this;
+        $clone->userFirstName = $firstName;
+
+        return $clone;
+    }
+
+    /**
+     * @param string|null $lastName
+     * @return $this
+     */
+    public function withUserLastName(?string $lastName) : self
+    {
+        $clone = clone $this;
+        $clone->userLastName = $lastName;
+
+        return $clone;
+    }
+
+    /**
+     * @param string|null $login
+     * @return $this
+     */
+    public function withUserLogin(?string $login) : self
+    {
+        $clone = clone $this;
+        $clone->userLogin = $login;
+
+        return $clone;
+    }
+
+    /**
+     * @param string|null $emailAddress
+     * @return $this
+     */
+    public function withUserEmailAddress(?string $emailAddress) : self
+    {
+        $clone = clone $this;
+        $clone->userEmail = $emailAddress;
+
+        return $clone;
+    }
+
+    /**
+     * @param int|null $objId
+     * @return $this
+     */
+    public function withObjectId(?int $objId) : self
+    {
+        $clone = clone $this;
+        $clone->objectId = $objId;
+
+        return $clone;
+    }
+
+    /**
+     * @param int|null $timestamp
+     * @return $this
+     */
+    public function withIssuedBeforeTimestamp(?int $timestamp) : self
+    {
+        $clone = clone $this;
+        $clone->issuedBeforeTimestamp = $timestamp;
+
+        return $clone;
+    }
+
+    /**
+     * @param int|null $timestamp
+     * @return $this 
+     */
+    public function withIssuedAfterTimestamp(?int $timestamp) : self
+    {
+        $clone = clone $this;
+        $clone->issuedAfterTimestamp = $timestamp;
+
+        return $clone;
+    }
+
+    /**
+     * @param bool $status
+     * @return $this
+     */
+    public function withOnlyActive(bool $status) : self
+    {
+        $clone = clone $this;
+        $clone->onlyActive = $status;
+
+        return $clone;
+    }
+
+    /**
+     * @param int[] $usrIds
+     * @return $this
+     * @throws \ilException
+     */
+    public function withUserIds(array $usrIds) :self
+    {
+        if ([] === $usrIds) {
+            throw new \ilException('The passed array of user ids must not be empty!');
+        }
+
+        array_walk($usrIds, function(int $usrId) {
+            // Do nothing, use this for type safety of array values 
+        });
+        
+        $clone = clone $this;
+        $clone->userIds = $usrIds;
+
+        return $clone;
+    }
+
+    /**
+     * @param int[] $usrIds
+     * @return $this
+     */
+    public function withAdditionalUserIds(array $usrIds) :self
+    {
+        array_walk($usrIds, function(int $usrId) {
+            // Do nothing, use this for type safety of array values 
+        });
+
+        $clone = clone $this;
+        $clone->userIds = array_unique(array_merge($clone->userIds, $usrIds));
+
+        return $clone;
     }
 
     /**
@@ -115,7 +239,7 @@ class UserDataFilter
     /**
      * @return bool
      */
-    public function isOnlyActive() : ?bool
+    public function isOnlyActive() : bool
     {
         return $this->onlyActive;
     }
