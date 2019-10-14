10db37eb200fee747c8b7b73bbaa8120d1d5d46d
Raphael Heer <raphael.heer@hslu.ch>
2019-10-14 09:39:38 +0200

    Draft for the legacy component to return custom js code

--- src/UI/Component/Legacy/Legacy.php
+++ src/UI/Component/Legacy/Legacy.php
@@ -16,4 +16,21 @@ interface Legacy extends \ILIAS\UI\Component\Component
      * @return	string
      */
     public function getContent();
+
+    /**
+     * Get a legacy component like this, but with an additional signal with custom JavaScript code
+     *
+     * @param $signal_name
+     * @param $js_code
+     * @return Legacy
+     */
+    public function withCustomSignal(string $signal_name, string $js_code) : Legacy;
+
+    /**
+     * Get signal with custom JavaScript code
+     *
+     * @param $signal_name
+     * @return \ILIAS\UI\Component\Signal
+     */
+    public function getCustomSignal(string $signal_name) : \ILIAS\UI\Component\Signal;
 }
