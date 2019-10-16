45f9f4aaf9409d26960958b921d65f0ca9a5236e
Fabian Schmid <fs@studer-raimann.ch>
2019-10-16 14:43:48 +0200

    some method descriptions

--- src/UI/Component/MainControls/ModeInfo.php
+++ src/UI/Component/MainControls/ModeInfo.php
@@ -15,12 +15,16 @@ interface ModeInfo extends Component
 {
 
     /**
+     * Returns the title of the currently available mode.
+     *
      * @return string
      */
     public function getTitle() : string;
 
 
     /**
+     * The mode can be specified with further information displayed to the user.
+     *
      * @param string $info_message
      *
      * @return ModeInfo
@@ -29,12 +33,16 @@ interface ModeInfo extends Component
 
 
     /**
+     * Returns a specification of the current mode, if any.
+     *
      * @return string
      */
     public function getDescription() : string;
 
 
     /**
+     * Returns the Close button, which must close the current mode.
+     *
      * @return \ILIAS\UI\Component\Button\Close
      */
     public function getCloseButton() : \ILIAS\UI\Component\Button\Close;
