163d8ff7307197a4809c4b228dcdea4691ca22d6
Raphael Heer <raphael.heer@hslu.ch>
2019-10-18 12:59:31 +0200

    WebDAV Added new functionalities to the instructions repo interface

--- Services/WebDAV/classes/mount_instructions/ilWebDAVMountInstructionsRepository.php
+++ Services/WebDAV/classes/mount_instructions/ilWebDAVMountInstructionsRepository.php
@@ -8,7 +8,9 @@ interface ilWebDAVMountInstructionsRepository
     public function getMountInstructionsDocumentById(int $id) : ilWebDAVMountInstructionsDocument;
     public function getMountInstructionsByLanguage(string $language) : ilWebDAVMountInstructionsDocument;
     public function getAllMountInstructions() : array;
+    public function doMountInstructionsExistByLanguage(string $language) : bool;
     public function updateMountInstructionsById(ilWebDAVMountInstructionsDocument $document);
+    public function updateSortingValueById($id, $a_new_sorting_value);
     public function deleteMountInstructionsById(int $id);
 
 }
