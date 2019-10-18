e2b3048fbbf786d596a998bc87591dcb83262299
Alexander Killing <killing@leifos.de>
2019-10-18 12:05:36 +0200

    booking with preferences, https://docu.ilias.de/goto_docu_wiki_wpage_5688_1357.html

--- setup/sql/dbupdate_05.php
+++ setup/sql/dbupdate_05.php
@@ -1528,3 +1528,74 @@ if(!$ilDB->tableExists('skl_profile_role'))
 <?php
 $ilCtrlStructureReader->getStructure();
 ?>
+<#5534>
+<?php
+if (!$ilDB->tableColumnExists('booking_settings', 'preference_nr'))
+{
+    $ilDB->addTableColumn('booking_settings', 'preference_nr', array(
+        "type" => "integer",
+        "notnull" => true,
+        "length" => 4,
+        "default" => 0
+    ));
+}
+?>
+<#5535>
+<?php
+if (!$ilDB->tableColumnExists('booking_settings', 'pref_deadline'))
+{
+    $ilDB->addTableColumn('booking_settings', 'pref_deadline', array(
+        "type" => "integer",
+        "notnull" => true,
+        "length" => 4,
+        "default" => 0
+    ));
+}
+?>
+<#5536>
+<?php
+if( !$ilDB->tableExists('booking_preferences') )
+{
+    $ilDB->createTable('booking_preferences', array(
+        'book_pool_id' => array(
+            'type' => 'integer',
+            'length' => 4,
+            'notnull' => true,
+            'default' => 0
+        ),
+        'user_id' => array(
+            'type' => 'integer',
+            'length' => 4,
+            'notnull' => true,
+            'default' => 0
+        ),
+        'book_obj_id' => array(
+            'type' => 'integer',
+            'length' => 4,
+            'notnull' => true,
+            'default' => 0
+        )
+    ));
+    $ilDB->addPrimaryKey('booking_preferences', ['book_pool_id', 'user_id', 'book_obj_id']);
+}
+?>
+<#5537>
+<?php
+$ilCtrlStructureReader->getStructure();
+?>
+<#5538>
+<?php
+if (!$ilDB->tableColumnExists('booking_settings', 'pref_booking_hash'))
+{
+    $ilDB->addTableColumn('booking_settings', 'pref_booking_hash', array(
+        "type" => "text",
+        "notnull" => true,
+        "length" => 23,
+        "default" => "0"
+    ));
+}
+?>
+<#5539>
+<?php
+$ilCtrlStructureReader->getStructure();
+?>
