966dd11c583bc0418dce93da2aac3b5ff29a7211
Ingmar Szmais <iszmais@databay.de>
2019-10-21 13:01:49 +0200

    spaces

--- Services/Certificate/classes/Template/Action/Clone/class.ilCertificateCloneAction.php
+++ Services/Certificate/classes/Template/Action/Clone/class.ilCertificateCloneAction.php
@@ -112,18 +112,18 @@ class ilCertificateCloneAction
 			$backgroundImageFile = basename($backgroundImagePath);
 			$backgroundImageThumbnail = dirname($backgroundImagePath) . '/background.jpg.thumb.jpg';
 
-            $newBackgroundImage = '';
-            $newBackgroundImageThumbnail = '';
+			$newBackgroundImage = '';
+			$newBackgroundImageThumbnail = '';
 
 			if ($this->fileSystem->has($backgroundImagePath) &&
-                !$this->fileSystem->hasDir($backgroundImagePath)
-            ) {
-                $newBackgroundImage = $newCertificate->getBackgroundImageDirectory() . $backgroundImageFile;
-                $newBackgroundImageThumbnail = str_replace($webDir, '', $newCertificate->getBackgroundImageThumbPath());
+				!$this->fileSystem->hasDir($backgroundImagePath)
+			) {
+				$newBackgroundImage = $newCertificate->getBackgroundImageDirectory() . $backgroundImageFile;
+				$newBackgroundImageThumbnail = str_replace($webDir, '', $newCertificate->getBackgroundImageThumbPath());
 
 				if ($this->fileSystem->has($newBackgroundImage) &&
-                    !$this->fileSystem->hasDir($newBackgroundImage)
-                ) {
+					!$this->fileSystem->hasDir($newBackgroundImage)
+				) {
 					$this->fileSystem->delete($newBackgroundImage);
 				}
 
@@ -134,11 +134,11 @@ class ilCertificateCloneAction
 			}
 
 			if ($this->fileSystem->has($backgroundImageThumbnail) &&
-                !$this->fileSystem->hasDir($backgroundImageThumbnail)
-            ) {
+				!$this->fileSystem->hasDir($backgroundImageThumbnail)
+			) {
 				if ($this->fileSystem->has($newBackgroundImageThumbnail) &&
-                    !$this->fileSystem->hasDir($newBackgroundImageThumbnail)
-                ) {
+					!$this->fileSystem->hasDir($newBackgroundImageThumbnail)
+				) {
 					$this->fileSystem->delete($newBackgroundImageThumbnail);
 				}
 
