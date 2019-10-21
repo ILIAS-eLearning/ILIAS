4001c06e494cd453791d1e8c43f862b1e1270b0b
Alexander Killing <killing@leifos.de>
2019-10-21 09:35:35 +0200

    favourites

--- Services/PersonalDesktop/classes/Provider/PDMainBarProvider.php
+++ Services/PersonalDesktop/classes/Provider/PDMainBarProvider.php
@@ -28,54 +28,34 @@ class PDMainBarProvider extends AbstractStaticMainMenuProvider
     {
         $dic = $this->dic;
 
-        $icon = $this->dic->ui()->factory()->symbol()->icon()->standard("pdts", "")->withIsOutlined(true);
+        $f = $this->dic->ui()->factory();
+
         $icon = $this->dic->ui()->factory()->symbol()->icon()->custom(\ilUtil::getImagePath("simpleline/heart.svg"), "");
 
-        // Favorites
-        $entries[] = $this->mainmenu->link($this->if->identifier('mm_pd_sel_items'))
-            ->withTitle($this->dic->language()->txt("mm_favorites"))
-            ->withAction("ilias.php?baseClass=ilPersonalDesktopGUI&cmd=jumpToSelectedItems")
-            ->withParent(StandardTopItemsProvider::getInstance()->getPersonalWorkspaceIdentification())
-            ->withPosition(10)
-	        ->withSymbol($icon)
-            ->withNonAvailableReason($this->dic->ui()->factory()->legacy("{$this->dic->language()->txt('component_not_active')}"))
-            ->withAvailableCallable(
-                function () use ($dic) {
-                    return $dic->settings()->get('disable_my_offers', 0) == 0;
-                }
-            )
-            ->withVisibilityCallable(
-                function () use ($dic) {
-                    $pdItemsViewSettings = new ilPDSelectedItemsBlockViewSettings($dic->user());
+        $fav_list = new \ilFavouritesListGUI();
+        $contents = $f->legacy($fav_list->render());
 
-                    return (bool) $pdItemsViewSettings->allViewsEnabled() || $pdItemsViewSettings->enabledSelectedItems();
-                }
-            );
+        return [
+            $this->mainmenu->complex($this->if->identifier('mm_pd_sel_items'))
+                ->withTitle($this->dic->language()->txt("mm_favorites"))
+                ->withSymbol($icon)
+                ->withContent($contents)
+                ->withParent(StandardTopItemsProvider::getInstance()->getPersonalWorkspaceIdentification())
+                ->withAlwaysAvailable(true)
+                ->withPosition(10)
+                ->withAvailableCallable(
+                    function () use ($dic) {
+                        return $dic->settings()->get('disable_my_offers', 0) == 0;
+                    }
+                )
+                ->withVisibilityCallable(
+                    function () use ($dic) {
+                        $pdItemsViewSettings = new ilPDSelectedItemsBlockViewSettings($dic->user());
 
-        /*
-        $icon = $this->dic->ui()->factory()->symbol()->icon()->custom(\ilUtil::getImagePath("simpleline/home.svg"), "");
+                        return (bool) $pdItemsViewSettings->allViewsEnabled() || $pdItemsViewSettings->enabledSelectedItems();
+                    }
+                )
 
-        // Dashboard
-        $entries[] = $this->mainmenu->link($this->if->identifier('mm_pd_crs_grp'))
-            ->withTitle($this->dic->language()->txt("mm_dashboard"))
-            ->withAction("ilias.php?baseClass=ilPersonalDesktopGUI&cmd=jumpToMemberships")
-            ->withParent(StandardTopItemsProvider::getInstance()->getRepositoryIdentification())
-            ->withPosition(10)
-            ->withSymbol($icon)
-            ->withNonAvailableReason($this->dic->ui()->factory()->legacy("{$this->dic->language()->txt('component_not_active')}"))
-            ->withAvailableCallable(
-                function () use ($dic) {
-                    return $dic->settings()->get('disable_my_memberships', 0) == 0;
-                }
-            )
-            ->withVisibilityCallable(
-                function () use ($dic) {
-                    $pdItemsViewSettings = new ilPDSelectedItemsBlockViewSettings($dic->user());
-
-                    return (bool) $pdItemsViewSettings->allViewsEnabled() || $pdItemsViewSettings->enabledMemberships();
-                }
-            );*/
-
-        return $entries;
+        ];
     }
 }
