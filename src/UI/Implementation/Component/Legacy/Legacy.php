30b0e1f2d8cb7c5674d845baedbf05a15e668b44
Raphael Heer <raphael.heer@hslu.ch>
2019-10-24 11:16:37 +0200

    KS Legacy custom signal: Implemented new interface functionality

--- src/UI/Implementation/Component/Legacy/Legacy.php
+++ src/UI/Implementation/Component/Legacy/Legacy.php
@@ -7,6 +7,7 @@ namespace ILIAS\UI\Implementation\Component\Legacy;
 use ILIAS\UI\Component as C;
 use ILIAS\UI\Component\Signal;
 use ILIAS\UI\Implementation\Component\ComponentHelper;
+use ILIAS\UI\Implementation\Component\SignalGeneratorInterface;
 use ILIAS\UI\NotImplementedException;
 
 /**
@@ -22,16 +23,28 @@ class Legacy implements C\Legacy\Legacy
      */
     private $content;
 
+    /**
+     * @var SignalGeneratorInterface
+     */
+    private $signal_generator;
+
+    /**
+     * @var array
+     */
+    private $signal_list;
 
     /**
      * Legacy constructor.
      * @param string $content
+     * @param SignalGeneratorInterface $signal_generator
      */
-    public function __construct($content)
+    public function __construct($content, SignalGeneratorInterface $signal_generator)
     {
         $this->checkStringArg("content", $content);
 
         $this->content = $content;
+        $this->signal_generator = $signal_generator;
+        $this->signal_list = array();
     }
 
     /**
@@ -47,7 +60,9 @@ class Legacy implements C\Legacy\Legacy
      */
     public function withCustomSignal(string $signal_name, string $js_code) : \ILIAS\UI\Component\Legacy\Legacy
     {
-        throw new NotImplementedException("withCustomSignal is not implemented yet");
+        $clone = clone $this;
+        $clone->registerSignalAndCustomCode($signal_name, $js_code);
+        return $clone;
     }
 
     /**
@@ -55,6 +70,27 @@ class Legacy implements C\Legacy\Legacy
      */
     public function getCustomSignal(string $signal_name) : Signal
     {
-        throw new NotImplementedException("getCustomSignal is not implemented yet");
+        if(!key_exists($signal_name, $this->signal_list))
+        {
+            throw new \InvalidArgumentException("Signal with name $signal_name is not registered");
+        }
+
+        return $this->signal_list[$signal_name]['signal'];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAllSignals() : array
+    {
+        return $this->signal_list;
+    }
+
+    private function registerSignalAndCustomCode(string $signal_name, string $js_code)
+    {
+        $this->signal_list[$signal_name] = array(
+            'signal'   => $this->signal_generator->create(),
+            'js_code' => $js_code
+        );
     }
 }
