6b4c1b59700031d345c1804ccc5ac1b7fb928428
Richard Klees <richard.klees@concepts-and-training.de>
2019-10-28 06:51:16 +0100

    VirusScanner/Setup: fill virus scanner settings to ilias.ini

--- Services/VirusScanner/classes/Setup/class.ilVirusScannerSetupConfig.php
+++ Services/VirusScanner/classes/Setup/class.ilVirusScannerSetupConfig.php
@@ -32,24 +32,31 @@ class ilVirusScannerSetupConfig implements Setup\Config {
 		?string $path_to_clean
 	) {
 		$scanners = [
-			VIRUS_SCANNER_NONE,
-			VIRUS_SCANNER_SOPHOS,
-			VIRUS_SCANNER_ANTIVIR,
-			VIRUS_SCANNER_CLAMAV
+			self::VIRUS_SCANNER_NONE,
+			self::VIRUS_SCANNER_SOPHOS,
+			self::VIRUS_SCANNER_ANTIVIR,
+			self::VIRUS_SCANNER_CLAMAV
 		];
 		if (!in_array($virus_scanner, $scanners)) {
 			throw new \InvalidArgumentException(
 				"Unknown virus scanner: '$virus_scanner'"
 			);
 		}
-		if ($virus_scanner !== VIRUS_SCANNER_NONE && (!$path_to_scan || !$path_to_clean)) {
+		if ($virus_scanner !== self::VIRUS_SCANNER_NONE && (!$path_to_scan || !$path_to_clean)) {
 			throw new \InvalidArgumentException(
 				"Missing path to scan and/or clean commands for virus scanner."
 			);
 		}
 		$this->virus_scanner = $virus_scanner;
-		$this->path_to_scan = $path_to_scan;
-		$this->path_to_clean = $path_to_clean;
+		$this->path_to_scan = $this->toLinuxConvention($path_to_scan);
+		$this->path_to_clean = $this->toLinuxConvention($path_to_clean);
+	}
+
+	protected function toLinuxConvention(?string $p) : ?string {
+		if (!$p) {
+			return null;
+		}
+		return preg_replace("/\\\\/","/",$p);
 	}
 
 	public function getVirusScanner() : string {
