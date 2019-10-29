ef8a50edfb4d617f23f7eb154cebae3ebfd1e0d7
Raphael Heer <raphael.heer@hslu.ch>
2019-10-29 16:01:07 +0100

    WebDAV: Refactor Mount Instructions API to return content without modal

--- Services/WebDAV/classes/mount_instructions/class.ilWebDAVBaseMountInstructions.php
+++ Services/WebDAV/classes/mount_instructions/class.ilWebDAVBaseMountInstructions.php
@@ -16,18 +16,48 @@ abstract class ilWebDAVBaseMountInstructions
 
     public function __construct(ilWebDAVMountInstructionsRepository $a_repo,
         ilWebDAVUriBuilder $a_uri_builder,
-        ilSetting $a_settings,
-        string $a_language)
+        ilSetting $a_settings)
     {
         $this->repo = $a_repo;
         $this->uri_builder = $a_uri_builder;
         $this->settings = $a_settings;
-        $this->language = $a_language;
     }
 
-    public function getMountInstructionsAsArray() : array
+    public static function buildMountInstructionsObjectFromURI(ilWebDAVMountInstructionsRepositoryImpl $a_repo, \Psr\Http\Message\RequestInterface $a_request)
     {
-        $document = $this->repo->getMountInstructionsByLanguage($this->language);
+        $uri_builder = new ilWebDAVUriBuilder($a_request);
+        $uri = $a_request->getUri()->getPath();
+
+        $splitted_uri = explode('/', $uri);
+
+        // Remove path elements before and until webdav script
+        while($value = array_shift($splitted_uri) != 'webdav.php' && count($splitted_uri) > 0);
+
+        $client_id = array_shift($splitted_uri);
+        $path_value = array_shift($splitted_uri);
+
+        if(strlen($path_value) == 2)
+        {
+            return new ilWebDAVObjectlessMountInstructions($a_repo,
+                $uri_builder,
+                new ilSetting('file_access'));
+        }
+        else if (substr($path_value, 0, 4) == 'ref_')
+        {
+            return new ilWebDAVObjectMountInstructions($a_repo,
+                $uri_builder,
+                new ilSetting('file_access'),
+                (int)substr($path_value, 4));
+        }
+        else
+        {
+            throw new InvalidArgumentException("Invalid path given");
+        }
+    }
+
+    public function getMountInstructionsAsArray(string $a_language) : array
+    {
+        $document = $this->repo->getMountInstructionsByLanguage($a_language);
         $processed = $document->getProcessedInstructions();
         $mount_instructions = json_decode($processed, true);
 
--- Services/WebDAV/classes/mount_instructions/class.ilWebDAVMountInstructionsGUI.php
+++ Services/WebDAV/classes/mount_instructions/class.ilWebDAVMountInstructionsGUI.php
@@ -19,54 +19,67 @@ class ilWebDAVMountInstructionsGUI {
     protected $ref_id;
     protected $mount_instruction;
     
-    public function __construct() 
+    public function __construct(ilWebDAVBaseMountInstructions $a_mount_instruction)
     {
         global $DIC;
 
         $this->uri_builder = new ilWebDAVUriBuilder($DIC->http()->request());
         $this->user_language = $DIC->user()->getLanguage();
-        $this->mount_instruction = $this->buildMountInstructionsFromUri(
-            $DIC->http()->request()->getUri()->getPath(),
-            new ilWebDAVMountInstructionsRepositoryImpl($DIC->database())
-        );
-
+        $this->mount_instruction = $a_mount_instruction;
     }
 
-    public function buildMountInstructionsFromUri($a_uri, ilWebDAVMountInstructionsRepository $repo)
-    {
-        $splitted_uri = explode('/', $a_uri);
-
-        // Remove path elements before and until webdav script
-        while($value = array_shift($splitted_uri) != 'webdav.php');
-
-        $client_id = array_shift($splitted_uri);
-        $path_value = array_shift($splitted_uri);
-
-        if(strlen($path_value) == 2)
-        {
-            return new ilWebDAVObjectlessMountInstructions($repo,
-                $this->uri_builder,
-                new ilSetting('file_access'),
-                $path_value);
-        }
-        else if (substr($path_value, 0, 4) == 'ref_')
-        {
-            return new ilWebDAVObjectMountInstructions($repo,
-                $this->uri_builder,
-                new ilSetting('file_access'),
-                $this->user_language,
-                (int)substr($path_value, 4));
-        }
-        else{
-            throw new InvalidArgumentException("Invalid path given");
-        }
-    }
-
-    public function renderMountInstructionModal()
+    public function buildGUIFromGivenMountInstructions($a_mount_instructions)
     {
         global $DIC;
+        $f = $DIC->ui()->factory();
+        $r = $DIC->ui()->renderer();
 
-        $modal = new ilWebDAVMountInstructionsModalGUI($this->mount_instruction, $DIC->ui()->factory(), $DIC->ui()->renderer(), $DIC->language());
-        $modal->printMountInstructionModalAndExit();
+        // List of all components to render
+        $comps = array();
+
+        // This is an additional legacy component. It contains the java script function to substitute the shown instructions
+        $js_function_legacy = $f->legacy('<script>function showInstructions(id){'."\n"
+            // Hide all instruction divs at first
+            .'$(".instructions").hide();'."\n"
+            // Show the div which is given as an argument
+            .'$("#"+id).show();}</script>');
+
+        /*
+         * This is an associative array. The key is the title of the button, the value the used signal. E.g.:
+         * array(
+         *      "WINDOWS" => signal_for_windows_legacy_component,
+         *      "MAC" => signal_for_mac_legacy_component,
+         *      "LINUX" => signal_for_linux_legacy_component);
+         */
+        $view_control_actions = array();
+
+        // Used to hide all <div>'s except for the first one
+        $hidden = "";
+        foreach ($a_mount_instructions as $title => $text)
+        {
+            // Create legacy component for mount instructions. Mount instructions text is wrapped in a <div>-tag
+            $legacy = $f->legacy("<div id='$title' class='instructions' $hidden>$text</div>");
+
+            // Add to the list of components to render
+            $comps[] = $legacy;
+
+            // Add signal to the list for the view control
+            $view_control_actions[$title] = 'https://ilias.de';
+        }
+        $view_control = $f->viewControl()->mode($view_control_actions, "Aria Label");
+
+        // Add view control and legacy add the beginning of the array (so they will be rendered first)
+        array_unshift($comps, $view_control);
+        array_unshift($comps, $js_function_legacy);
+
+        return $r->render($comps);
+    }
+
+    public function renderMountInstructionsContent()
+    {
+        $instructions = $this->mount_instruction->getMountInstructionsAsArray($this->user_language);
+
+        echo $this->buildGUIFromGivenMountInstructions($instructions);
+        exit;
     }
 }
