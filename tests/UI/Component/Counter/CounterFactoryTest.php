637b68e9cd30cb4237616e5e656f60649d007ac2
Timon Amstutz <timon.amstutz@bluewin.ch>
2019-11-01 18:04:36 +0100

    UI Components: Counter Interface, Implementation and Tests

--- tests/UI/Component/Counter/CounterFactoryTest.php
+++ tests/UI/Component/Counter/CounterFactoryTest.php
@@ -4,8 +4,7 @@ require_once 'tests/UI/AbstractFactoryTest.php';
 
 class CounterFactoryTest extends AbstractFactoryTest
 {
-    public $kitchensink_info_settings = array( "status" => array("context" => false)
-        );
+    public $kitchensink_info_settings = array( "status" => array("context" => false));
 
 
     public $factory_title = 'ILIAS\\UI\\Component\\Counter\\Factory';
