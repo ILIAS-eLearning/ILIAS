993eb790e9480153c00d608c4c639051effc7163
Timon Amstutz <timon.amstutz@bluewin.ch>
2019-11-03 18:24:11 +0100

    UI Counter: Reverting accidents

--- tests/UI/Component/Counter/CounterFactoryTest.php
+++ tests/UI/Component/Counter/CounterFactoryTest.php
@@ -4,7 +4,8 @@ require_once 'tests/UI/AbstractFactoryTest.php';
 
 class CounterFactoryTest extends AbstractFactoryTest
 {
-    public $kitchensink_info_settings = array( "status" => array("context" => false));
+    public $kitchensink_info_settings = array( "status" => array("context" => false)
+        );
 
 
     public $factory_title = 'ILIAS\\UI\\Component\\Counter\\Factory';
