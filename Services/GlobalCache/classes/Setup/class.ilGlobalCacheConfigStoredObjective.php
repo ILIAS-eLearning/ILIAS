215de37752225df2e3ff0100cabdb3bbe7baf5a3
Richard Klees <richard.klees@concepts-and-training.de>
2019-11-04 15:55:00 +0100

    GlobalCache/Setup: configure global cache in cli-setup

---
+++ Services/GlobalCache/classes/Setup/class.ilGlobalCacheConfigStoredObjective.php
@@ -0,0 +1,49 @@
+<?php
+
+/* Copyright (c) 2019 Richard Klees <richard.klees@concepts-and-training.de> Extended GPL, see docs/LICENSE */
+
+use ILIAS\Setup;
+
+class ilGlobalCacheConfigStoredObjective implements Setup\Objective {
+	/**
+	 * @var	\ilGlobalCacheSettings
+	 */
+	protected $settings;
+
+	public function __construct(
+		\ilGlobalCacheSettings $settings
+	) {
+		$this->settings = $settings;
+	}
+
+	public function getHash() : string {
+		return hash("sha256", self::class);
+	}
+
+	public function getLabel() : string {
+		return "Store configuration of Services/GlobalCache";
+	}
+
+	public function isNotable() : bool {
+		return false;
+	}
+
+	public function getPreconditions(Setup\Environment $environment) : array {
+		$common_config = $environment->getConfigFor("common");
+		return [
+			new ilIniFilePopulatedObjective($common_config),
+		];
+	}
+
+	public function achieve(Setup\Environment $environment) : Setup\Environment {
+		$client_ini = $environment->getResource(Setup\Environment::RESOURCE_CLIENT_INI);
+
+		$this->settings->writeToIniFile($client_ini);
+
+		if (!$client_ini->write()) {
+			throw new Setup\UnachievableException("Could not write client.ini.php");
+		}
+
+		return $environment;
+	}
+}
