52a09a66e17958c25c80782b1df07ecc206eaa3c
Richard Klees <richard.klees@concepts-and-training.de>
2019-11-07 07:45:50 +0100

    BackgroundTasks/Setup: added agent, config and objectives

---
+++ Services/BackgroundTasks/classes/Setup/class.ilBackgroundTasksSetupAgent.php
@@ -0,0 +1,72 @@
+<?php
+
+/* Copyright (c) 2019 Richard Klees <richard.klees@concepts-and-training.de> Extended GPL, see docs/LICENSE */
+
+use ILIAS\Setup;
+use ILIAS\Refinery;
+use ILIAS\Data;
+use ILIAS\UI;
+
+class ilBackgroundTasksSetupAgent implements Setup\Agent {
+	/**
+	 * @var Refinery\Factory
+	 */
+	protected $refinery;
+
+	public function __construct(
+		Refinery\Factory $refinery
+	) {
+		$this->refinery = $refinery;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function hasConfig() : bool {
+		return true;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function getConfigInput(Setup\Config $config = null) : UI\Component\Input\Field\Input {
+		throw new \LogicException("Not yet implemented.");
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function getArrayToConfigTransformation() : Refinery\Transformation {
+		return $this->refinery->custom()->transformation(function($data) {
+			return new \ilBackgroundTasksSetupConfig(
+				$data["type"] ?? \ilBackgroundTasksSetupConfig::TYPE_SYNCHRONOUS,
+				$data["max_number_of_concurrent_tasks"] ?? 1
+			);
+		});	
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function getInstallObjective(Setup\Config $config = null) : Setup\Objective {
+		return new Setup\ObjectiveCollection(
+			"Complete objectives from Services/BackgroundTasks",
+			false,
+			new ilBackgroundTasksConfigStoredObjective($config)
+		);
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function getUpdateObjective(Setup\Config $config = null) : Setup\Objective {
+		return new Setup\NullObjective();
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function getBuildArtifactObjective() : Setup\Objective {
+		return new Setup\NullObjective();
+	}
+}
