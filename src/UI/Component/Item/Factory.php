cf59eecded80d455adb1b5a9926858eb7023aaf7
Timon Amstutz <timon.amstutz@gmail.com>
2019-11-14 09:29:57 +0100

    UI Components: Notifications Remove redundant PHPDocs
    Co-Authored-By: Richard Klees <richard.klees@concepts-and-training.de> (+14 squashed commits)
    Squashed commits:
    [1a40d2d] UI Components: Notifications Remove redundant PHPDocs
    
    Co-Authored-By: Richard Klees <richard.klees@concepts-and-training.de>
    [66167b1] UI Components: Notifications Remove redundant PHPDocs
    
    Co-Authored-By: Richard Klees <richard.klees@concepts-and-training.de>
    [fcef495] UI Components: Notifications Remove redundant PHPDocs
    
    Co-Authored-By: Richard Klees <richard.klees@concepts-and-training.de>
    [37aebe8] UI Components: Notifications Remove redundant PHPDocs
    
    Co-Authored-By: Richard Klees <richard.klees@concepts-and-training.de>
    [59ababc] UI Components: Notifications Remove redundant PHPDocs
    
    Co-Authored-By: Richard Klees <richard.klees@concepts-and-training.de>
    [a531d14] UI Components: Notifications Remove redundant PHPDocs
    
    Co-Authored-By: Richard Klees <richard.klees@concepts-and-training.de>
    [fa51882] UI Components: Notifications Remove redundant PHPDocs
    
    Co-Authored-By: Richard Klees <richard.klees@concepts-and-training.de>
    [fab3c0f] UI Components: Notifications Remove redundant PHPDocs
    
    Co-Authored-By: Richard Klees <richard.klees@concepts-and-training.de>
    [54b7a29] UI Components: Notifications Remove redundant PHPDocs
    
    Co-Authored-By: Richard Klees <richard.klees@concepts-and-training.de>
    [5a86e17] UI Components: Notifications Remove redundant PHPDocs
    
    Co-Authored-By: Richard Klees <richard.klees@concepts-and-training.de>
    [e8e1482] UI Components: Notifications Remove redundant PHPDocs
    
    Co-Authored-By: Richard Klees <richard.klees@concepts-and-training.de>
    [8c41ace] UI Components: Notifications Remove redundant PHPDocs
    
    Co-Authored-By: Richard Klees <richard.klees@concepts-and-training.de>
    [c9906e0] UI Components: Notifications Remove redundant PHPDocs
    
    Co-Authored-By: Richard Klees <richard.klees@concepts-and-training.de>
    [2161c076] UI Components: Notifications Remove redundant PHPDocs
    
    Co-Authored-By: Richard Klees <richard.klees@concepts-and-training.de>

--- src/UI/Component/Item/Factory.php
+++ src/UI/Component/Item/Factory.php
@@ -90,7 +90,6 @@ interface Factory
      *       The purpose of each interaction MUST be clearly labeled by text.
      * ---
      * @param string|\ILIAS\UI\Component\Button\Shy|\ILIAS\UI\Component\Link\Standard $title Title of the item holding the main interaction
-     * @return \ILIAS\UI\Component\Item\Notification
      */
-    public function notification($title, \ILIAS\UI\Component\Symbol\Icon\Icon $icon);
+    public function notification($title, \ILIAS\UI\Component\Symbol\Icon\Icon $lead) : Notification;
 }
--- src/UI/Component/Item/Notification.php
+++ src/UI/Component/Item/Notification.php
@@ -14,37 +14,30 @@ interface Notification extends Item
      * the description. Note this should only be used, if the content section
      * needs to hold legacy content that currently does not have a place in the
      * UI components.
-     * @param \ILIAS\UI\Component\Legacy\Legacy $component
-     * @return self
      */
     public function withAdditionalContent(\ILIAS\UI\Component\Legacy\Legacy $component) : Notification;
 
     /**
      * Get the additional content of the item or null.
-     * @return \ILIAS\UI\Component\Legacy\Legacy|null $component
      */
-    public function getAdditionalContent();
+    public function getAdditionalContent() : ?\ILIAS\UI\Component\Legacy\Legacy;
 
     /**
      * Get an Item like this with an url to consulted async, when to close button is pressed.
      * With this url, information may be stored persistently in the DB without interrupting the workflow
      * of the user (e.g. setting a flag, that the message was consulted).
-     * @param string $url
-     * @return self
      */
     public function withCloseAction(string $url) : Notification;
 
     /**
      * Get the url attached to this Notification Item
-     * @return self
      */
-    public function getCloseAction();
+    public function getCloseAction() : Notification;
 
     /**
      * Get an Notification Item like this, but with a set of
      * Notifications, this Notification Item will aggregate.
      * @param Notification[] $aggregate_notifications
-     * @return self
      */
     public function withAggregateNotifications(array $aggregate_notifications) : Notification;
 
@@ -57,15 +50,12 @@ interface Notification extends Item
 
     /**
      * Set icon as lead
-     * @param \ILIAS\UI\Component\Symbol\Icon\Icon $icon lead icon
-     * @return Icon
      */
-    public function withLeadIcon(\ILIAS\UI\Component\Symbol\Icon\Icon $icon);
+    public function withLeadIcon(\ILIAS\UI\Component\Symbol\Icon\Icon $lead) : Notification;
 
     /**
      * Get icon as lead. Note that Notifications only accept Icons as lead,
      * this is different from the standard Item.
-     * @return \ILIAS\UI\Component\Symbol\Icon\Icon
      */
     public function getLeadIcon() : \ILIAS\UI\Component\Symbol\Icon\Icon;
 }
