695be40cbbce39f1079bbd45ed1dfd3de8d7273b
mjansen <mjansen@databay.de>
2019-11-15 14:23:29 +0100

    Certificate: Fixed leading / in background image path

--- Services/Certificate/classes/Template/Action/Import/class.ilCertificateTemplateImportAction.php
+++ Services/Certificate/classes/Template/Action/Import/class.ilCertificateTemplateImportAction.php
@@ -184,7 +184,11 @@ class ilCertificateTemplateImportAction
 						$basePath = rtrim(dirname($this->getBackgroundImageDirectory($rootDir)), '/');
 						$fileName = basename($matches[1]);
 
-						return 'url(' . $basePath . '/' . $fileName . ')';
+						if (strlen($basePath) > 0) {
+							$basePath .= '/';
+						}
+
+						return 'url(' . $basePath . $fileName . ')';
 					}, $xsl);
 				} elseif (strpos($file['entry'], '.jpg') !== false) {
 					$newBackgroundImageName = 'background_' . $newVersion . '.jpg';
