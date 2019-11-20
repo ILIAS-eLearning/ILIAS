8f068a675a28c7f78c29fbafeb5c223e5c2a1901
mjansen <mjansen@databay.de>
2019-11-20 09:24:25 +0100

    OnScreenChat/NotificationCenter: Add first business logic to notification center provider

--- Services/OnScreenChat/classes/Provider/OnScreenChatNotificationProvider.php
+++ Services/OnScreenChat/classes/Provider/OnScreenChatNotificationProvider.php
@@ -17,6 +17,78 @@ class OnScreenChatNotificationProvider extends AbstractNotificationProvider impl
      */
     public function getNotifications(): array
     {
-        return [];
+        $id = function (string $id): IdentificationInterface {
+            return $this->if->identifier($id);
+        };
+
+        if (0 === (int)$this->dic->user()->getId() || $this->dic->user()->isAnonymous()) {
+            return [];
+        }
+
+        $chatSettings = new \ilSetting('chatroom');
+        $isEnabled = $chatSettings->get('chat_enabled') && $chatSettings->get('enable_osc');
+        if (!$isEnabled) {
+            return [];
+        }
+
+        $factory = $this->globalScreen()->notifications()->factory();
+
+        $this->dic->language()->loadLanguageModule('chatroom');
+
+        $showAcceptMessageChange = (
+            !\ilUtil::yn2tf($this->dic->user()->getPref('chat_osc_accept_msg')) &&
+            !(bool)$this->dic->settings()->get('usr_settings_hide_chat_osc_accept_msg', false) &&
+            !(bool)$this->dic->settings()->get('usr_settings_disable_chat_osc_accept_msg', false)
+        );
+
+        $description = $this->dic->language()->txt('chat_osc_nc_no_conv');
+        if ($showAcceptMessageChange) {
+            $description = sprintf(
+                $this->dic->language()->txt('chat_osc_dont_accept_msg'),
+                $this->dic->ctrl()->getLinkTargetByClass(
+                    ['ilDashboardGUI', 'ilPersonalSettingsGUI', 'ilPersonalChatSettingsFormGUI'],
+                    'showChatOptions'
+                )
+            );
+        }
+
+        $icon = $this->dic->ui()->factory()
+            ->symbol()
+            ->icon()
+            ->standard('chtr', 'conversations');
+        $title = $this->dic->language()->txt('chat_osc_conversations');
+        if (!$showAcceptMessageChange) {
+            $title = $this->dic->ui()->factory()
+                ->link()
+                ->standard($this->dic->language()->txt('chat_osc_conversations'), '#');
+        }
+
+        $notificationItem = $this->dic->ui()->factory()
+            ->item()
+            ->notification($title, $icon)
+            ->withDescription($description);
+        if (!$showAcceptMessageChange) {
+            $notificationItem = $notificationItem
+                ->withAdditionalOnLoadCode(
+                    function($id) {
+                        //Note this just some random content to show case the basic pattern you might use
+                        //we add one after 5 seconds
+                        return "";
+                    }
+                );
+        }
+
+        $group = $factory
+            ->standardGroup($id('chat_bucket_group'))
+            ->withTitle('Chat') // TODO
+            ->addNotification(
+                $factory->standard($id('chat_bucket'))
+                    ->withNotificationItem($notificationItem)
+                    ->withNewAmount(0)
+            );
+
+        return [
+            $group,
+        ];
     }
 }
