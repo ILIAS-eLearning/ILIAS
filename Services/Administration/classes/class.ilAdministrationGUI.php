0a94ed85a4bec2e1b25bd811ec03235de5745cfa
smeyer <smeyer.ilias@gmx.de>
2019-11-22 12:06:05 +0100

    trash: removed deprecated creation mode

--- Services/Administration/classes/class.ilAdministrationGUI.php
+++ Services/Administration/classes/class.ilAdministrationGUI.php
@@ -108,8 +108,6 @@ class ilAdministrationGUI
 
 		$ilMainMenu->setActive("administration");
 		
-		$this->creation_mode = false;
-
 		$this->ctrl->saveParameter($this, array("ref_id", "admin_mode"));
 		
 		if ($_GET["admin_mode"] != "repository")
@@ -155,27 +153,10 @@ class ilAdministrationGUI
 			$ilErr->raiseError($this->lng->txt('permission_denied'),$ilErr->WARNING);
 		}
 		
-		// check creation mode
-		// determined by "new_type" parameter
-		$new_type = $_POST["new_type"]
-			? $_POST["new_type"]
-			: $_GET["new_type"];
-		if ($new_type != "")
-		{
-			$this->creation_mode = true;
-		}
 	
-		// determine next class
-		if ($this->creation_mode)
-		{
-			$obj_type = $new_type;
-			$class_name = $this->objDefinition->getClassName($obj_type);
-			$next_class = strtolower("ilObj".$class_name."GUI");
-			$this->ctrl->setCmdClass($next_class);
-		}
 		// set next_class directly for page translations
 		// (no cmdNode is given in translation link)
-		elseif ($this->ctrl->getCmdClass() == "ilobjlanguageextgui")
+		if ($this->ctrl->getCmdClass() == "ilobjlanguageextgui")
 		{
 			$next_class = "ilobjlanguageextgui";
 		}
@@ -242,8 +223,6 @@ class ilAdministrationGUI
 						}
 						else
 						{
-							if (!$this->creation_mode)
-							{
 								if(is_subclass_of($class_name, "ilObject2GUI"))
 								{
 									$this->gui_obj = new $class_name($this->cur_ref_id, ilObject2GUI::REPOSITORY_NODE_ID);
@@ -253,30 +232,10 @@ class ilAdministrationGUI
 									$this->gui_obj = new $class_name("", $this->cur_ref_id, true, false);
 								}
 							}
-							else
-							{
-								if(is_subclass_of($class_name, "ilObject2GUI"))
-								{
-									$this->gui_obj = new $class_name(null, ilObject2GUI::REPOSITORY_NODE_ID, $this->cur_ref_id);
-								}
-								else
-								{
-									$this->gui_obj = new $class_name("", 0, true, false);
-								}
-							}
-						}
-						$this->gui_obj->setCreationMode($this->creation_mode);
+						$this->gui_obj->setCreationMode(false);
 					}
-					$tabs_out = ($new_type == "")
-						? true
-						: false;
-
-					// set standard screen id
-//					if (strtolower($next_class) == strtolower($this->ctrl->getCmdClass()) ||
-//						"ilpermissiongui" == strtolower($this->ctrl->getCmdClass()))
-//					{
+					$tabs_out = true;
 						$ilHelp->setScreenIdComponent(ilObject::_lookupType($this->cur_ref_id,true));
-//					}
 					$this->showTree();
 						
 					$this->ctrl->setReturn($this, "return");					
