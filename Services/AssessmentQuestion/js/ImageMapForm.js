96f0faa43a19e4ec96518e729aafe7109e04c9c2
Adrian Lüthi <adi.l@bluewin.ch>
2019-11-28 12:58:20 +0100

    show selected regions in imagequestion preview

--- Services/AssessmentQuestion/js/ImageMapForm.js
+++ Services/AssessmentQuestion/js/ImageMapForm.js
@@ -12,6 +12,7 @@ class Point {
 
 let popup = null;
 let canvas;
+let preview_canvas;
 let typ;
 let coordinates;
 let label;
@@ -47,6 +48,7 @@ let display_coordinate_selector = function() {
             break;
         case TYPE_POLYGON:
             poly_points = [];
+            current_coordinates = null;
             canvas.mouseup(generate_polygon);
             break;
     }
@@ -85,6 +87,7 @@ let submit_popup = function() {
         coordinates.val(current_coordinates);
         label.html(current_coordinates);
         close_popup();
+        update_preview();
     }
 };
 
@@ -257,7 +260,113 @@ let map_poly = function(g) {
     g.closePath();
 };
 
+let update_preview = function() {
+    if (preview_canvas == null) {
+        initialize_preview();
+    }
+    
+    let g = preview_canvas[0].getContext('2d');
+    g.clearRect(0, 0, preview_canvas.width(), preview_canvas.height());
+    g.fillStyle = 'rgba(255, 255, 255, 0.8)';
+    
+    $('.aot_row').each(function(index, item) {
+        let type = $(item).find('select[id$=imedd_type]').val();
+        let coordinates = $(item).find('input[id$=imedd_coordinates]').val();
+        
+        switch (type) {
+            case TYPE_RECTANGLE:
+                draw_preview_rectangle(coordinates, g);
+                break;
+            case TYPE_CIRCLE:
+                draw_preview_circle(coordinates, g);
+                break;
+            case TYPE_POLYGON:
+                draw_preview_polygon(coordinates, g);
+                break;
+        }
+    });
+};
+
+let initialize_preview = function() {
+    let preview_image = $('.image_preview');
+    
+    if (preview_image.length === 1) {
+        preview_canvas = $('<canvas></canvas');
+        preview_canvas.css('position', 'absolute');
+        preview_canvas[0].width = preview_image.width();
+        preview_canvas[0].height = preview_image.height();
+        preview_canvas.css('bottom', preview_image.css('marginBottom'));
+        preview_canvas.css('left', preview_image.parents('.col-sm-9').css('paddingLeft'));
+        preview_image.after(preview_canvas);
+    }
+};
+
+let rect_regex = /x:([^;]*);y:([^;]*);width:([^;]*);height:([^;]*)/;
+let draw_preview_rectangle = function(coordinates, g) {
+    
+    let matches = coordinates.match(rect_regex);
+
+    g.beginPath();
+    g.rect(map_to_preview_width(matches[1]), 
+           map_to_preview_height(matches[2]), 
+           map_to_preview_width(matches[3]), 
+           map_to_preview_height(matches[4]));
+    g.fill();
+};
+
+let circle_regex = /cx:([^;]*);cy:([^;]*);rx:([^;]*);ry:([^;]*)/;
+let draw_preview_circle = function(coordinates, g) {
+    let matches = coordinates.match(circle_regex);
+    
+    g.beginPath();
+    g.ellipse(map_to_preview_width(matches[1]), 
+              map_to_preview_height(matches[2]), 
+              map_to_preview_width(matches[3]), 
+              map_to_preview_height(matches[4]),
+              0, 0, 2 * Math.PI);
+    g.fill();
+};
+
+let draw_preview_polygon = function(coordinates, g) {
+    let points = coordinates.substring(7).split(' ');
+    
+    g.beginPath();
+
+    let start = points[0].split(',');
+    g.moveTo(map_to_preview_width(start[0]), 
+             map_to_preview_height(start[1]));
+
+    let i;
+    for (i = 1; i < points.length; i += 1) {
+        let point = points[i].split(',');
+        g.lineTo(map_to_preview_width(point[0]), 
+                 map_to_preview_height(point[1]));
+    }
+
+    g.closePath();
+    
+    g.fill();
+};
+
+let map_to_preview_height = function(value) {
+    return map_to_preview(value, preview_canvas[0].height);
+};
+
+let map_to_preview_width = function(value) {
+    return map_to_preview(value, preview_canvas[0].width);
+};
+
+let map_to_preview = function(value, preview_value) {
+    let floatval = parseFloat(value);
+    return (floatval / 100.0) * preview_value;
+};
+
+$(window).load(function() {
+    update_preview();
+});
+
 $(document).on('keyup', process_imgkey);
 $(document).on('click', '.js_select_coordinates', display_coordinate_selector);
 $(document).on('click', '.js_image_select', submit_popup);
 $(document).on('click', '.js_image_cancel, .close', close_popup);
+$(document).on('click', '.js_remove', update_preview);
