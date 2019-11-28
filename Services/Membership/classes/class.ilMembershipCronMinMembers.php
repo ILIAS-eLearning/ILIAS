ba2ce20bdc0d68489463ad202774868f3fd6458f
mjansen <mjansen@databay.de>
2019-11-28 16:25:50 +0100

    Mail: Removed soap related method call

--- Services/Membership/classes/class.ilMembershipCronMinMembers.php
+++ Services/Membership/classes/class.ilMembershipCronMinMembers.php
@@ -179,7 +179,6 @@ class ilMembershipCronMinMembers extends ilCronJob
 		$ntf->addAdditionalInfo("mem_cron_min_members_task", "");
 
 		$mail = new ilMail(ANONYMOUS_USER_ID);
-		$mail->enableSOAP(false); // #10410
 		$mail->enqueue(
 			ilObjUser::_lookupLogin($a_reci_id),
 			null,
