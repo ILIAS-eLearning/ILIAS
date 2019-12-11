b486ac5767b298664c3774188fd0a468d89c275c
Alexander Killing <killing@leifos.de>
2019-12-11 09:18:21 +0100

    coding style fixes

--- Modules/HTMLLearningModule/classes/class.ilHTMLLearningModuleExporter.php
+++ Modules/HTMLLearningModule/classes/class.ilHTMLLearningModuleExporter.php
@@ -12,78 +12,73 @@ include_once("./Services/Export/classes/class.ilXmlExporter.php");
  */
 class ilHTMLLearningModuleExporter extends ilXmlExporter
 {
-	private $ds;
+    private $ds;
 
-	/**
-	 * Initialisation
-	 */
-	function init()
-	{
-		include_once("./Modules/HTMLLearningModule/classes/class.ilHTMLLearningModuleDataSet.php");
-		$this->ds = new ilHTMLLearningModuleDataSet();
-		$this->ds->setExportDirectories($this->dir_relative, $this->dir_absolute);
-		$this->ds->setDSPrefix("ds");
-	}
+    /**
+     * Initialisation
+     */
+    public function init()
+    {
+        include_once("./Modules/HTMLLearningModule/classes/class.ilHTMLLearningModuleDataSet.php");
+        $this->ds = new ilHTMLLearningModuleDataSet();
+        $this->ds->setExportDirectories($this->dir_relative, $this->dir_absolute);
+        $this->ds->setDSPrefix("ds");
+    }
 
 
-	/**
-	 * Get tail dependencies
-	 *
-	 * @param		string		entity
-	 * @param		string		target release
-	 * @param		array		ids
-	 * @return		array		array of array with keys "component", entity", "ids"
-	 */
-	function getXmlExportTailDependencies($a_entity, $a_target_release, $a_ids)
-	{
+    /**
+     * Get tail dependencies
+     *
+     * @param		string		entity
+     * @param		string		target release
+     * @param		array		ids
+     * @return		array		array of array with keys "component", entity", "ids"
+     */
+    public function getXmlExportTailDependencies($a_entity, $a_target_release, $a_ids)
+    {
+        $md_ids = array();
+        foreach ($a_ids as $id) {
+            $md_ids[] = $id . ":0:htlm";
+        }
 
-		$md_ids = array();
-		foreach ($a_ids as $id)
-		{
-			$md_ids[] = $id.":0:htlm";
-		}
+        return array(
+            array(
+                "component" => "Services/MetaData",
+                "entity" => "md",
+                "ids" => $md_ids)
+            );
+    }
 
-		return array (
-			array(
-				"component" => "Services/MetaData",
-				"entity" => "md",
-				"ids" => $md_ids)
-			);
-	}
+    /**
+     * Get xml representation
+     *
+     * @param	string		entity
+     * @param	string		target release
+     * @param	string		id
+     * @return	string		xml string
+     */
+    public function getXmlRepresentation($a_entity, $a_schema_version, $a_id)
+    {
+        $this->ds->setExportDirectories($this->dir_relative, $this->dir_absolute);
+        return $this->ds->getXmlRepresentation($a_entity, $a_schema_version, $a_id, "", true, true);
+    }
 
-	/**
-	 * Get xml representation
-	 *
-	 * @param	string		entity
-	 * @param	string		target release
-	 * @param	string		id
-	 * @return	string		xml string
-	 */
-	public function getXmlRepresentation($a_entity, $a_schema_version, $a_id)
-	{
-		$this->ds->setExportDirectories($this->dir_relative, $this->dir_absolute);
-		return $this->ds->getXmlRepresentation($a_entity, $a_schema_version, $a_id, "", true, true);
-	}
-
-	/**
-	 * Returns schema versions that the component can export to.
-	 * ILIAS chooses the first one, that has min/max constraints which
-	 * fit to the target release. Please put the newest on top.
-	 *
-	 * @return
-	 */
-	function getValidSchemaVersions($a_entity)
-	{
-		return array (
-			"4.1.0" => array(
-				"namespace" => "http://www.ilias.de/Modules/HTMLLearningModule/htlm/4_1",
-				"xsd_file" => "ilias_htlm_4_1.xsd",
-				"uses_dataset" => true,
-				"min" => "4.1.0",
-				"max" => "")
-		);
-	}
-
+    /**
+     * Returns schema versions that the component can export to.
+     * ILIAS chooses the first one, that has min/max constraints which
+     * fit to the target release. Please put the newest on top.
+     *
+     * @return
+     */
+    public function getValidSchemaVersions($a_entity)
+    {
+        return array(
+            "4.1.0" => array(
+                "namespace" => "http://www.ilias.de/Modules/HTMLLearningModule/htlm/4_1",
+                "xsd_file" => "ilias_htlm_4_1.xsd",
+                "uses_dataset" => true,
+                "min" => "4.1.0",
+                "max" => "")
+        );
+    }
 }
-
-?>
