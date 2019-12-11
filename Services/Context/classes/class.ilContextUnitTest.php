b3cf4b19ff12e3c1506ad2557b2b43113c579c8b
Alexander Killing <killing@leifos.de>
2019-12-11 10:30:00 +0100

    coding style fixes

--- Services/Context/classes/class.ilContextUnitTest.php
+++ Services/Context/classes/class.ilContextUnitTest.php
@@ -4,113 +4,110 @@
 
 include_once "Services/Context/interfaces/interface.ilContextTemplate.php";
 
-/** 
+/**
  * Service context for unit test
- * 
+ *
  * @author Jörg Lützenkirchen <luetzenkirchen@leifos.com>
  * @version $Id$
- * 
+ *
  * @ingroup ServicesContext
  */
 class ilContextUnitTest implements ilContextTemplate
-{			
-	/**
-	 * Are redirects supported?
-	 * 
-	 * @return bool 
-	 */
-	public static function supportsRedirects()
-	{
-		return false;
-	}
-	
-	/**
-	 * Based on user authentication?
-	 *  
-	 * @return bool
-	 */
-	public static function hasUser()
-	{
-		return true;
-	}
-	
-	/**
-	 * Uses HTTP aka browser 
-	 * 
-	 * @return bool 
-	 */
-	public static function usesHTTP()
-	{
-		return false;
-	}
-	
-	/**
-	 * Has HTML output
-	 *  
-	 * @return bool
-	 */
-	public static function hasHTML()
-	{
-		return false;
-	}
-	
-	/**
-	 * Uses template engine
-	 *  
-	 * @return bool
-	 */
-	public static function usesTemplate()
-	{
-		return false;
-	}
-	
-	/**
-	 * Init client
-	 *  
-	 * @return bool
-	 */
-	public static function initClient()
-	{
-		return true;
-	}
-	
-	/**
-	 * Try authentication
-	 *  
-	 * @return bool
-	 */
-	public static function doAuthentication()
-	{
-		return false;
-	}
+{
+    /**
+     * Are redirects supported?
+     *
+     * @return bool
+     */
+    public static function supportsRedirects()
+    {
+        return false;
+    }
+    
+    /**
+     * Based on user authentication?
+     *
+     * @return bool
+     */
+    public static function hasUser()
+    {
+        return true;
+    }
+    
+    /**
+     * Uses HTTP aka browser
+     *
+     * @return bool
+     */
+    public static function usesHTTP()
+    {
+        return false;
+    }
+    
+    /**
+     * Has HTML output
+     *
+     * @return bool
+     */
+    public static function hasHTML()
+    {
+        return false;
+    }
+    
+    /**
+     * Uses template engine
+     *
+     * @return bool
+     */
+    public static function usesTemplate()
+    {
+        return false;
+    }
+    
+    /**
+     * Init client
+     *
+     * @return bool
+     */
+    public static function initClient()
+    {
+        return true;
+    }
+    
+    /**
+     * Try authentication
+     *
+     * @return bool
+     */
+    public static function doAuthentication()
+    {
+        return false;
+    }
 
-	/**
-	 * Check if persistent session handling is supported
-	 * @return boolean
-	 */
-	public static function supportsPersistentSessions()
-	{
-		return false;
-	}
-	
-	/**
-	 * Supports push messages
-	 *
-	 * @return bool
-	 */
-	public static function supportsPushMessages()
-	{
-		return false;
-	}
+    /**
+     * Check if persistent session handling is supported
+     * @return boolean
+     */
+    public static function supportsPersistentSessions()
+    {
+        return false;
+    }
+    
+    /**
+     * Supports push messages
+     *
+     * @return bool
+     */
+    public static function supportsPushMessages()
+    {
+        return false;
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public static function isSessionMainContext()
-	{
-		return true;
-	}
-
+    /**
+     * @inheritdoc
+     */
+    public static function isSessionMainContext()
+    {
+        return true;
+    }
 }
-
-?>
