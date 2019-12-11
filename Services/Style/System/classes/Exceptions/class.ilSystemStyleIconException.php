240845a1ef865566ead6cb9f9df39425473098e5
Alexander Killing <killing@leifos.de>
2019-12-11 12:03:00 +0100

    coding style fixes

--- Services/Style/System/classes/Exceptions/class.ilSystemStyleIconException.php
+++ Services/Style/System/classes/Exceptions/class.ilSystemStyleIconException.php
@@ -12,23 +12,21 @@ require_once 'class.ilSystemStyleExceptionBase.php';
  */
 class ilSystemStyleIconException extends ilSystemStyleExceptionBase
 {
-	const IMAGES_FOLDER_DOES_NOT_EXIST = 1001;
-	const ICON_DOES_NOT_EXIST = 1002;
+    const IMAGES_FOLDER_DOES_NOT_EXIST = 1001;
+    const ICON_DOES_NOT_EXIST = 1002;
 
-	protected function assignMessageToCode()
-	{
-		switch ($this->code)
-		{
-			case self::IMAGES_FOLDER_DOES_NOT_EXIST:
-				$this->message = "Images folder set for this style does not exist or can not be read: " . $this->add_info;
-				break;
-			case self::ICON_DOES_NOT_EXIST:
-				$this->message = "The selected Icon does not exit: " . $this->add_info;
-				break;
-			default:
-				$this->message = "Unknown Exception " . $this->add_info;
-				break;
-		}
-	}
-
+    protected function assignMessageToCode()
+    {
+        switch ($this->code) {
+            case self::IMAGES_FOLDER_DOES_NOT_EXIST:
+                $this->message = "Images folder set for this style does not exist or can not be read: " . $this->add_info;
+                break;
+            case self::ICON_DOES_NOT_EXIST:
+                $this->message = "The selected Icon does not exit: " . $this->add_info;
+                break;
+            default:
+                $this->message = "Unknown Exception " . $this->add_info;
+                break;
+        }
+    }
 }
