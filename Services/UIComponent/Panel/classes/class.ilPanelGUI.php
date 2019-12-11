d7909181c1d679f5a8834c602523d71aed526355
Alexander Killing <killing@leifos.de>
2019-12-11 12:10:33 +0100

    coding style fixes

--- Services/UIComponent/Panel/classes/class.ilPanelGUI.php
+++ Services/UIComponent/Panel/classes/class.ilPanelGUI.php
@@ -11,223 +11,213 @@
  */
 class ilPanelGUI
 {
-	const PANEL_STYLE_PRIMARY = 0;
-	const PANEL_STYLE_SECONDARY = 1;
+    const PANEL_STYLE_PRIMARY = 0;
+    const PANEL_STYLE_SECONDARY = 1;
 
-	const HEADING_STYLE_SUBHEADING = 0;
-	const HEADING_STYLE_BLOCK = 1;
-	
-	const FOOTER_STYLE_BLOCK = 0;
+    const HEADING_STYLE_SUBHEADING = 0;
+    const HEADING_STYLE_BLOCK = 1;
+    
+    const FOOTER_STYLE_BLOCK = 0;
 
-	protected $heading = "";
-	protected $body = "";
-	protected $footer = "";
-	protected $panel_style = 0;
-	protected $heading_style = 0;	
-	protected $footer_style = 0;	
+    protected $heading = "";
+    protected $body = "";
+    protected $footer = "";
+    protected $panel_style = 0;
+    protected $heading_style = 0;
+    protected $footer_style = 0;
 
-	/**
-	 * Constructor
-	 */
-	protected function  __construct()
-	{
+    /**
+     * Constructor
+     */
+    protected function __construct()
+    {
+    }
 
-	}
+    /**
+     * Get instance
+     *
+     * @return ilPanelGUI panel instance
+     */
+    public static function getInstance()
+    {
+        return new ilPanelGUI();
+    }
 
-	/**
-	 * Get instance
-	 *
-	 * @return ilPanelGUI panel instance
-	 */
-	static function getInstance()
-	{
-		return new ilPanelGUI();
-	}
+    /**
+     * Set heading
+     *
+     * @param string $a_val heading
+     */
+    public function setHeading($a_val)
+    {
+        $this->heading = $a_val;
+    }
 
-	/**
-	 * Set heading
-	 *
-	 * @param string $a_val heading
-	 */
-	function setHeading($a_val)
-	{
-		$this->heading = $a_val;
-	}
+    /**
+     * Get heading
+     *
+     * @return string heading
+     */
+    public function getHeading()
+    {
+        return $this->heading;
+    }
 
-	/**
-	 * Get heading
-	 *
-	 * @return string heading
-	 */
-	function getHeading()
-	{
-		return $this->heading;
-	}
+    /**
+     * Set body
+     *
+     * @param string $a_val body
+     */
+    public function setBody($a_val)
+    {
+        $this->body = $a_val;
+    }
 
-	/**
-	 * Set body
-	 *
-	 * @param string $a_val body
-	 */
-	function setBody($a_val)
-	{
-		$this->body = $a_val;
-	}
+    /**
+     * Get body
+     *
+     * @return string body
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+    
+    /**
+     * Set footer
+     *
+     * @param string $a_val footer
+     */
+    public function setFooter($a_val)
+    {
+        $this->footer = $a_val;
+    }
 
-	/**
-	 * Get body
-	 *
-	 * @return string body
-	 */
-	function getBody()
-	{
-		return $this->body;
-	}
-	
-	/**
-	 * Set footer
-	 *
-	 * @param string $a_val footer
-	 */
-	function setFooter($a_val)
-	{
-		$this->footer = $a_val;
-	}
+    /**
+     * Get body
+     *
+     * @return string body
+     */
+    public function getFooter()
+    {
+        return $this->footer;
+    }
 
-	/**
-	 * Get body
-	 *
-	 * @return string body
-	 */
-	function getFooter()
-	{
-		return $this->footer;
-	}
+    /**
+     * Set panel style
+     *
+     * @param int $a_val panel_style
+     */
+    public function setPanelStyle($a_val)
+    {
+        $this->panel_style = $a_val;
+    }
 
-	/**
-	 * Set panel style
-	 *
-	 * @param int $a_val panel_style
-	 */
-	function setPanelStyle($a_val)
-	{
-		$this->panel_style = $a_val;
-	}
+    /**
+     * Get panel style
+     *
+     * @return int panel_style
+     */
+    public function getPanelStyle()
+    {
+        return $this->panel_style;
+    }
 
-	/**
-	 * Get panel style
-	 *
-	 * @return int panel_style
-	 */
-	function getPanelStyle()
-	{
-		return $this->panel_style;
-	}
+    /**
+     * Set heading style
+     *
+     * @param int $a_val heading style
+     */
+    public function setHeadingStyle($a_val)
+    {
+        $this->heading_style = $a_val;
+    }
 
-	/**
-	 * Set heading style
-	 *
-	 * @param int $a_val heading style
-	 */
-	function setHeadingStyle($a_val)
-	{
-		$this->heading_style = $a_val;
-	}
+    /**
+     * Get heading style
+     *
+     * @return int heading style
+     */
+    public function getHeadingStyle()
+    {
+        return $this->heading_style;
+    }
+    
+    /**
+     * Set footer style
+     *
+     * @param int $a_val footer style
+     */
+    public function setFooterStyle($a_val)
+    {
+        $this->footer_style = $a_val;
+    }
 
-	/**
-	 * Get heading style
-	 *
-	 * @return int heading style
-	 */
-	function getHeadingStyle()
-	{
-		return $this->heading_style;
-	}
-	
-	/**
-	 * Set footer style
-	 *
-	 * @param int $a_val footer style
-	 */
-	function setFooterStyle($a_val)
-	{
-		$this->footer_style = $a_val;
-	}
+    /**
+     * Get footer style
+     *
+     * @return int footer style
+     */
+    public function getFooterStyle()
+    {
+        return $this->footer_style;
+    }
 
-	/**
-	 * Get footer style
-	 *
-	 * @return int footer style
-	 */
-	function getFooterStyle()
-	{
-		return $this->footer_style;
-	}
+    /**
+     * Get HTML
+     *
+     * @return string html
+     */
+    public function getHTML()
+    {
+        $tpl = new ilTemplate("tpl.panel.html", true, true, "Services/UIComponent/Panel");
 
-	/**
-	 * Get HTML
-	 *
-	 * @return string html
-	 */
-	function getHTML()
-	{
-		$tpl = new ilTemplate("tpl.panel.html", true, true, "Services/UIComponent/Panel");
+        $head_outer_div_style = "";
+        if ($this->getHeading() != "") {
+            $tpl->setCurrentBlock("heading");
+            $tpl->setVariable("HEADING", $this->getHeading());
 
-		$head_outer_div_style = "";
-		if ($this->getHeading() != "")
-		{
-			$tpl->setCurrentBlock("heading");
-			$tpl->setVariable("HEADING", $this->getHeading());
+            switch ($this->getHeadingStyle()) {
+                case self::HEADING_STYLE_BLOCK:
+                    $tpl->setVariable("HEAD_DIV_STYLE", "panel-heading ilBlockHeader");
+                    $tpl->setVariable("HEAD_H3_STYLE", "ilBlockHeader");
+                    $head_outer_div_style = "il_Block";
+                    break;
 
-			switch ($this->getHeadingStyle())
-			{
-				case self::HEADING_STYLE_BLOCK:
-					$tpl->setVariable("HEAD_DIV_STYLE", "panel-heading ilBlockHeader");
-					$tpl->setVariable("HEAD_H3_STYLE", "ilBlockHeader");
-					$head_outer_div_style = "il_Block";
-					break;
+                case self::HEADING_STYLE_SUBHEADING:
+                    $tpl->setVariable("HEAD_DIV_STYLE", "panel-heading ilHeader");
+                    $tpl->setVariable("HEAD_H3_STYLE", "ilHeader");
+                    break;
+            }
 
-				case self::HEADING_STYLE_SUBHEADING:
-					$tpl->setVariable("HEAD_DIV_STYLE", "panel-heading ilHeader");
-					$tpl->setVariable("HEAD_H3_STYLE", "ilHeader");
-					break;
-			}
+            $tpl->parseCurrentBlock();
+        }
 
-			$tpl->parseCurrentBlock();
-		}
+        $tpl->setVariable("BODY", $this->getBody());
+        
+        if ($this->getFooter() != "") {
+            $tpl->setCurrentBlock("footer");
+            $tpl->setVariable("FOOTER", $this->getFooter());
+            
+            switch ($this->getFooterStyle()) {
+                case self::FOOTER_STYLE_BLOCK:
+                    $tpl->setVariable("FOOT_DIV_STYLE", "panel-footer ilBlockInfo");
+                    break;
+            }
+                        
+            $tpl->parseCurrentBlock();
+        }
 
-		$tpl->setVariable("BODY", $this->getBody());
-		
-		if ($this->getFooter() != "")
-		{
-			$tpl->setCurrentBlock("footer");
-			$tpl->setVariable("FOOTER", $this->getFooter());
-			
-			switch ($this->getFooterStyle())
-			{
-				case self::FOOTER_STYLE_BLOCK:
-					$tpl->setVariable("FOOT_DIV_STYLE", "panel-footer ilBlockInfo");					
-					break;
-			}
-						
-			$tpl->parseCurrentBlock();
-		}
+        switch ($this->getPanelStyle()) {
+            case self::PANEL_STYLE_SECONDARY:
+                $tpl->setVariable("PANEL_STYLE", "panel panel-default " . $head_outer_div_style);
+                break;
 
-		switch ($this->getPanelStyle())
-		{
-			case self::PANEL_STYLE_SECONDARY:
-				$tpl->setVariable("PANEL_STYLE", "panel panel-default ".$head_outer_div_style);
-				break;
+            default:
+                $tpl->setVariable("PANEL_STYLE", "panel panel-primary " . $head_outer_div_style);
+                break;
+        }
 
-			default:
-				$tpl->setVariable("PANEL_STYLE", "panel panel-primary ".$head_outer_div_style);
-				break;
-		}				
-
-		return $tpl->get();
-	}
-
-
+        return $tpl->get();
+    }
 }
-
-?>
--- Services/UIComponent/classes/class.ilUIHookProcessor.php
+++ Services/UIComponent/classes/class.ilUIHookProcessor.php
@@ -7,100 +7,90 @@
  *
  * @author Alex Killing <alex.killing@gmx.de>
  * @version $Id$
- * @ingroup 
+ * @ingroup
  */
 class ilUIHookProcessor
 {
-	/**
-	 * @var ilPluginAdmin
-	 */
-	protected $plugin_admin;
+    /**
+     * @var ilPluginAdmin
+     */
+    protected $plugin_admin;
 
-	var $append = array();
-	var $prepend = array();
-	var $replace = "";
-	
-	/**
-	 * Constructor
-	 *
-	 * @param
-	 * @return
-	 */
-	function __construct($a_comp, $a_part, $a_pars)
-	{
-		global $DIC;
+    public $append = array();
+    public $prepend = array();
+    public $replace = "";
+    
+    /**
+     * Constructor
+     *
+     * @param
+     * @return
+     */
+    public function __construct($a_comp, $a_part, $a_pars)
+    {
+        global $DIC;
 
-		$this->plugin_admin = $DIC["ilPluginAdmin"];
-		$ilPluginAdmin = $DIC["ilPluginAdmin"];
-		
-		include_once("./Services/UIComponent/classes/class.ilUIHookPluginGUI.php");
-		
-		// user interface hook [uihk]
-		$pl_names = $ilPluginAdmin->getActivePluginsForSlot(IL_COMP_SERVICE, "UIComponent", "uihk");
-		$this->replaced = false;
-		foreach ($pl_names as $pl)
-		{
-			$ui_plugin = ilPluginAdmin::getPluginObject(IL_COMP_SERVICE, "UIComponent", "uihk", $pl);
-			$gui_class = $ui_plugin->getUIClassInstance();
-			$resp = $gui_class->getHTML($a_comp, $a_part, $a_pars);
+        $this->plugin_admin = $DIC["ilPluginAdmin"];
+        $ilPluginAdmin = $DIC["ilPluginAdmin"];
+        
+        include_once("./Services/UIComponent/classes/class.ilUIHookPluginGUI.php");
+        
+        // user interface hook [uihk]
+        $pl_names = $ilPluginAdmin->getActivePluginsForSlot(IL_COMP_SERVICE, "UIComponent", "uihk");
+        $this->replaced = false;
+        foreach ($pl_names as $pl) {
+            $ui_plugin = ilPluginAdmin::getPluginObject(IL_COMP_SERVICE, "UIComponent", "uihk", $pl);
+            $gui_class = $ui_plugin->getUIClassInstance();
+            $resp = $gui_class->getHTML($a_comp, $a_part, $a_pars);
 
-			if ($resp["mode"] != ilUIHookPluginGUI::KEEP)
-			{
-				switch($resp["mode"])
-				{
-					case ilUIHookPluginGUI::PREPEND:
-						$this->prepend[] = $resp["html"];
-						break;
-						
-					case ilUIHookPluginGUI::APPEND:
-						$this->append[] = $resp["html"];
-						break;
-						
-					case ilUIHookPluginGUI::REPLACE:
-						if (!$this->replaced)
-						{
-							$this->replace = $resp["html"];
-							$this->replaced = true;
-						}
-						break;
-				}
-			}
-		}
-	}
+            if ($resp["mode"] != ilUIHookPluginGUI::KEEP) {
+                switch ($resp["mode"]) {
+                    case ilUIHookPluginGUI::PREPEND:
+                        $this->prepend[] = $resp["html"];
+                        break;
+                        
+                    case ilUIHookPluginGUI::APPEND:
+                        $this->append[] = $resp["html"];
+                        break;
+                        
+                    case ilUIHookPluginGUI::REPLACE:
+                        if (!$this->replaced) {
+                            $this->replace = $resp["html"];
+                            $this->replaced = true;
+                        }
+                        break;
+                }
+            }
+        }
+    }
 
-	/**
-	 * Should HTML be replaced completely?
-	 *
-	 * @return
-	 */
-	function replaced()
-	{
-		return $this->replaced;
-	}
-	
-	/**
-	 * Get HTML
-	 *
-	 * @param string $html html
-	 * @return string html
-	 */
-	function getHTML($html)
-	{
-		if ($this->replaced)
-		{
-			$html = $this->replace;
-		}
-		foreach ($this->append as $a)
-		{
-			$html.= $a;
-		}
-		foreach ($this->prepend as $p)
-		{
-			$html = $p.$html;
-		}
-		return $html;
-	}
-	
+    /**
+     * Should HTML be replaced completely?
+     *
+     * @return
+     */
+    public function replaced()
+    {
+        return $this->replaced;
+    }
+    
+    /**
+     * Get HTML
+     *
+     * @param string $html html
+     * @return string html
+     */
+    public function getHTML($html)
+    {
+        if ($this->replaced) {
+            $html = $this->replace;
+        }
+        foreach ($this->append as $a) {
+            $html.= $a;
+        }
+        foreach ($this->prepend as $p) {
+            $html = $p . $html;
+        }
+        return $html;
+    }
 }
-
-?>
