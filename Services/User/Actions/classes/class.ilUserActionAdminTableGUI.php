2f9bc6b10eb5c283e5f84aea613deee7462f27cc
Alexander Killing <killing@leifos.de>
2019-12-11 12:18:47 +0100

    coding style fixes

--- Services/User/Actions/classes/class.ilUserActionAdminTableGUI.php
+++ Services/User/Actions/classes/class.ilUserActionAdminTableGUI.php
@@ -14,63 +14,59 @@ include_once("./Services/Table/classes/class.ilTable2GUI.php");
  */
 class ilUserActionAdminTableGUI extends ilTable2GUI
 {
-	/**
-	 * @var ilCtrl
-	 */
-	protected $ctrl;
+    /**
+     * @var ilCtrl
+     */
+    protected $ctrl;
 
-	/**
-	 * @var ilTemplate
-	 */
-	protected $tpl;
+    /**
+     * @var ilTemplate
+     */
+    protected $tpl;
 
-	/**
-	 * @var ilLanguage
-	 */
-	protected $lng;
+    /**
+     * @var ilLanguage
+     */
+    protected $lng;
 
 
-	/**
-	 * Constructor
-	 */
-	function __construct($a_parent_obj, $a_parent_cmd, $a_data, $a_write_permission = false)
-	{
-		global $DIC;
+    /**
+     * Constructor
+     */
+    public function __construct($a_parent_obj, $a_parent_cmd, $a_data, $a_write_permission = false)
+    {
+        global $DIC;
 
-		$this->ctrl = $DIC->ctrl();
-		$this->lng = $DIC->language();
-		$this->tpl = $DIC["tpl"];
+        $this->ctrl = $DIC->ctrl();
+        $this->lng = $DIC->language();
+        $this->tpl = $DIC["tpl"];
 
-		parent::__construct($a_parent_obj, $a_parent_cmd);
+        parent::__construct($a_parent_obj, $a_parent_cmd);
 
-		$this->setData($a_data);
-		$this->setTitle($this->lng->txt(""));
+        $this->setData($a_data);
+        $this->setTitle($this->lng->txt(""));
 
-		$this->addColumn($this->lng->txt("user_action"));
-		$this->addColumn($this->lng->txt("active"), "", "1");
-		
-		$this->setFormAction($this->ctrl->getFormAction($a_parent_obj));
-		$this->setRowTemplate("tpl.user_action_admin_row.html", "Services/User/Actions");
+        $this->addColumn($this->lng->txt("user_action"));
+        $this->addColumn($this->lng->txt("active"), "", "1");
+        
+        $this->setFormAction($this->ctrl->getFormAction($a_parent_obj));
+        $this->setRowTemplate("tpl.user_action_admin_row.html", "Services/User/Actions");
 
-		//$this->addMultiCommand("", $this->lng->txt(""));
-		if ($a_write_permission)
-		{
-			$this->addCommandButton("save", $this->lng->txt("save"));
-		}
-	}
-	
-	/**
-	 * Fill table row
-	 */
-	protected function fillRow($a_set)
-	{
-		if ($a_set["active"])
-		{
-			$this->tpl->touchBlock("checked");
-		}
-		$this->tpl->setVariable("VAL", $a_set["action_type_name"]);
-		$this->tpl->setVariable("ACTION_ID", $a_set["action_comp_id"].":".$a_set["action_type_id"]);
-	}
-
+        //$this->addMultiCommand("", $this->lng->txt(""));
+        if ($a_write_permission) {
+            $this->addCommandButton("save", $this->lng->txt("save"));
+        }
+    }
+    
+    /**
+     * Fill table row
+     */
+    protected function fillRow($a_set)
+    {
+        if ($a_set["active"]) {
+            $this->tpl->touchBlock("checked");
+        }
+        $this->tpl->setVariable("VAL", $a_set["action_type_name"]);
+        $this->tpl->setVariable("ACTION_ID", $a_set["action_comp_id"] . ":" . $a_set["action_type_id"]);
+    }
 }
-?>
--- Services/User/Actions/classes/class.ilWorkspaceUserActionProvider.php
+++ Services/User/Actions/classes/class.ilWorkspaceUserActionProvider.php
@@ -13,71 +13,71 @@ include_once("./Services/User/Actions/classes/class.ilUserActionProvider.php");
  */
 class ilWorkspaceUserActionProvider extends ilUserActionProvider
 {
-	protected $wsp_activated;
+    protected $wsp_activated;
 
-	/**
-	 * Construct
-	 *
-	 * @param
-	 * @return
-	 */
-	function __construct()
-	{
-		global $lng, $ilSetting;
+    /**
+     * Construct
+     *
+     * @param
+     * @return
+     */
+    public function __construct()
+    {
+        global $lng, $ilSetting;
 
-		$this->wsp_activated = (!$ilSetting->get("disable_personal_workspace"));
-		$lng->loadLanguageModule("wsp");
-		parent::__construct();
-	}
+        $this->wsp_activated = (!$ilSetting->get("disable_personal_workspace"));
+        $lng->loadLanguageModule("wsp");
+        parent::__construct();
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	function getComponentId()
-	{
-		return "pwsp";
-	}
+    /**
+     * @inheritdoc
+     */
+    public function getComponentId()
+    {
+        return "pwsp";
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	function getActionTypes()
-	{
-		return array(
-			"shared_res" => $this->lng->txt("wsp_shared_resources")
-		);
-	}
+    /**
+     * @inheritdoc
+     */
+    public function getActionTypes()
+    {
+        return array(
+            "shared_res" => $this->lng->txt("wsp_shared_resources")
+        );
+    }
 
-	/**
-	 * Collect all actions
-	 *
-	 * @param int $a_target_user target user
-	 * @return ilUserActionCollection collection
-	 */
-	function collectActionsForTargetUser($a_target_user)
-	{
-		global $ilCtrl, $lng;
+    /**
+     * Collect all actions
+     *
+     * @param int $a_target_user target user
+     * @return ilUserActionCollection collection
+     */
+    public function collectActionsForTargetUser($a_target_user)
+    {
+        global $ilCtrl, $lng;
 
-		$coll = ilUserActionCollection::getInstance();
-		include_once("./Services/User/Actions/classes/class.ilUserAction.php");
+        $coll = ilUserActionCollection::getInstance();
+        include_once("./Services/User/Actions/classes/class.ilUserAction.php");
 
-		if (!$this->wsp_activated)
-		{
-			return $coll;
-		}
+        if (!$this->wsp_activated) {
+            return $coll;
+        }
 
-		$f = new ilUserAction();
-		$f->setType("shared_res");
-		$f->setText($lng->txt("wsp_shared_resources"));
-		$ilCtrl->setParameterByClass("ilobjworkspacerootfoldergui", "user", ilObjUser::_lookupLogin($a_target_user));
-		$f->setHref($ilCtrl->getLinkTargetByClass(array("ilpersonaldesktopgui", "ilpersonalworkspacegui", "ilobjworkspacerootfoldergui"),
-			"listSharedResourcesOfOtherUser"));
+        $f = new ilUserAction();
+        $f->setType("shared_res");
+        $f->setText($lng->txt("wsp_shared_resources"));
+        $ilCtrl->setParameterByClass("ilobjworkspacerootfoldergui", "user", ilObjUser::_lookupLogin($a_target_user));
+        $f->setHref($ilCtrl->getLinkTargetByClass(
+            array("ilpersonaldesktopgui", "ilpersonalworkspacegui", "ilobjworkspacerootfoldergui"),
+            "listSharedResourcesOfOtherUser"
+        ));
 
-		//$f->setData(array("test" => "you", "user" => $a_target_user));
+        //$f->setData(array("test" => "you", "user" => $a_target_user));
 
-		$coll->addAction($f);
+        $coll->addAction($f);
 
-		return $coll;
-	}
+        return $coll;
+    }
 }
-?>
