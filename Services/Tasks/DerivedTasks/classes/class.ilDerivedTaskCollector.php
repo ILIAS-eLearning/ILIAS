9fc474af7d24857d74a442d440cb589acc0a721b
Alexander Killing <killing@leifos.de>
2019-12-11 12:07:55 +0100

    coding style fixes

--- Services/Tasks/DerivedTasks/classes/class.ilDerivedTaskCollector.php
+++ Services/Tasks/DerivedTasks/classes/class.ilDerivedTaskCollector.php
@@ -10,50 +10,45 @@
  */
 class ilDerivedTaskCollector
 {
-	/**
-	 * @var ilTaskService
-	 */
-	protected $service;
+    /**
+     * @var ilTaskService
+     */
+    protected $service;
 
-	/**
-	 * Constructor
-	 * @param ilTaskService $service
-	 */
-	public function __construct(ilTaskService $service)
-	{
-		$this->service = $service;
-	}
+    /**
+     * Constructor
+     * @param ilTaskService $service
+     */
+    public function __construct(ilTaskService $service)
+    {
+        $this->service = $service;
+    }
 
-	/**
-	 * Get entries
-	 *
-	 * @param int $user_id user id
-	 * @return ilDerivedTask[]
-	 */
-	public function getEntries(int $user_id)
-	{
-		$sort_array = [];
-		/** @var ilDerivedTaskProvider $provider */
-		foreach ($this->service->derived()->factory()->getAllProviders(true, $user_id) as $provider)
-		{
-			foreach ($provider->getTasks($user_id) as $t)
-			{
-				$sort_array[] = array("entry" => $t,"ts" => $t->getDeadline());
-			}
-		}
+    /**
+     * Get entries
+     *
+     * @param int $user_id user id
+     * @return ilDerivedTask[]
+     */
+    public function getEntries(int $user_id)
+    {
+        $sort_array = [];
+        /** @var ilDerivedTaskProvider $provider */
+        foreach ($this->service->derived()->factory()->getAllProviders(true, $user_id) as $provider) {
+            foreach ($provider->getTasks($user_id) as $t) {
+                $sort_array[] = array("entry" => $t,"ts" => $t->getDeadline());
+            }
+        }
 
-		$sort_array = ilUtil::sortArray($sort_array, "ts", "desc");
+        $sort_array = ilUtil::sortArray($sort_array, "ts", "desc");
 
-		// add today entry
-		$entries = [];
+        // add today entry
+        $entries = [];
 
-		foreach ($sort_array as $s)
-		{
-			$entries[] = $s["entry"];
-		}
+        foreach ($sort_array as $s) {
+            $entries[] = $s["entry"];
+        }
 
-		return $entries;
-	}
-
-
+        return $entries;
+    }
 }
--- Services/Tasks/DerivedTasks/classes/class.ilDerivedTasksGUI.php
+++ Services/Tasks/DerivedTasks/classes/class.ilDerivedTasksGUI.php
@@ -10,182 +10,164 @@
  */
 class ilDerivedTasksGUI
 {
-	/**
-	 * @var \ILIAS\DI\Container
-	 */
-	protected $dic;
+    /**
+     * @var \ILIAS\DI\Container
+     */
+    protected $dic;
 
-	/**
-	 * @var ilCtrl
-	 */
-	protected $ctrl;
+    /**
+     * @var ilCtrl
+     */
+    protected $ctrl;
 
-	/**
-	 * @var ilTemplate
-	 */
-	protected $main_tpl;
+    /**
+     * @var ilTemplate
+     */
+    protected $main_tpl;
 
-	/**
-	 * @var ilTaskService
-	 */
-	protected $task;
+    /**
+     * @var ilTaskService
+     */
+    protected $task;
 
-	/**
-	 * @var ilObjUser
-	 */
-	protected $user;
+    /**
+     * @var ilObjUser
+     */
+    protected $user;
 
-	/**
-	 * @var \ILIAS\DI\UIServices
-	 */
-	protected $ui;
+    /**
+     * @var \ILIAS\DI\UIServices
+     */
+    protected $ui;
 
-	/**
-	 * @var ilLanguage
-	 */
-	protected $lng;
+    /**
+     * @var ilLanguage
+     */
+    protected $lng;
 
-	/**
-	 * Constructor
-	 * @param \ILIAS\DI\Container|null $dic
-	 */
-	public function __construct(\ILIAS\DI\Container $di_container = null)
-	{
-		global $DIC;
+    /**
+     * Constructor
+     * @param \ILIAS\DI\Container|null $dic
+     */
+    public function __construct(\ILIAS\DI\Container $di_container = null)
+    {
+        global $DIC;
 
-		if (is_null($di_container))
-		{
-			$di_container = $DIC;
-		}
-		$this->dic = $di_container;
-		$this->ctrl = $DIC->ctrl();
-		$this->main_tpl = $DIC->ui()->mainTemplate();
-		$this->task = $DIC->task();
-		$this->user = $DIC->user();
-		$this->ui = $DIC->ui();
-		$this->lng = $DIC->language();
+        if (is_null($di_container)) {
+            $di_container = $DIC;
+        }
+        $this->dic = $di_container;
+        $this->ctrl = $DIC->ctrl();
+        $this->main_tpl = $DIC->ui()->mainTemplate();
+        $this->task = $DIC->task();
+        $this->user = $DIC->user();
+        $this->ui = $DIC->ui();
+        $this->lng = $DIC->language();
 
-		$this->lng->loadLanguageModule("task");
-	}
+        $this->lng->loadLanguageModule("task");
+    }
 
-	/**
-	 * Execute command
-	 */
-	function executeCommand()
-	{
-		$ctrl = $this->ctrl;
-		$main_tpl = $this->main_tpl;
-		$main_tpl->loadStandardTemplate();
+    /**
+     * Execute command
+     */
+    public function executeCommand()
+    {
+        $ctrl = $this->ctrl;
+        $main_tpl = $this->main_tpl;
+        $main_tpl->loadStandardTemplate();
 
-		$next_class = $ctrl->getNextClass($this);
-		$cmd = $ctrl->getCmd("show");
+        $next_class = $ctrl->getNextClass($this);
+        $cmd = $ctrl->getCmd("show");
 
-		switch ($next_class)
-		{
-			default:
-				if (in_array($cmd, array("show")))
-				{
-					$this->$cmd();
-				}
-		}
-		$main_tpl->printToStdout();
-	}
+        switch ($next_class) {
+            default:
+                if (in_array($cmd, array("show"))) {
+                    $this->$cmd();
+                }
+        }
+        $main_tpl->printToStdout();
+    }
 
-	/**
-	 * Show list of tasks
-	 */
-	protected function show()
-	{
-		$ui = $this->ui;
-		$lng = $this->lng;
-		$main_tpl = $this->main_tpl;
+    /**
+     * Show list of tasks
+     */
+    protected function show()
+    {
+        $ui = $this->ui;
+        $lng = $this->lng;
+        $main_tpl = $this->main_tpl;
 
-		$main_tpl->setTitle($lng->txt("task_derived_tasks"));
+        $main_tpl->setTitle($lng->txt("task_derived_tasks"));
 
-		$f = $ui->factory();
-		$renderer = $ui->renderer();
+        $f = $ui->factory();
+        $renderer = $ui->renderer();
 
-		$collector = $this->task->derived()->factory()->collector();
+        $collector = $this->task->derived()->factory()->collector();
 
-		$entries = $collector->getEntries($this->user->getId());
+        $entries = $collector->getEntries($this->user->getId());
 
-		$list_items_with_deadline = [];
-		$list_items_without_deadline = [];
-		$item_groups = [];
+        $list_items_with_deadline = [];
+        $list_items_without_deadline = [];
+        $item_groups = [];
 
-		// item groups from tasks
-		foreach ($entries as $i)
-		{
-			$props = [];
+        // item groups from tasks
+        foreach ($entries as $i) {
+            $props = [];
 
-			$title = $i->getTitle();
-			$link = '';
+            $title = $i->getTitle();
+            $link = '';
 
-			if ($i->getRefId() > 0)
-			{
-				$obj_id = ilObject::_lookupObjId($i->getRefId());
-				$obj_type = ilObject::_lookupType($obj_id);
-				$props[$lng->txt("obj_".$obj_type )] = ilObject::_lookupTitle($obj_id);
+            if ($i->getRefId() > 0) {
+                $obj_id = ilObject::_lookupObjId($i->getRefId());
+                $obj_type = ilObject::_lookupType($obj_id);
+                $props[$lng->txt("obj_" . $obj_type)] = ilObject::_lookupTitle($obj_id);
 
-				$link = ilLink::_getStaticLink($i->getRefId());
-			}
+                $link = ilLink::_getStaticLink($i->getRefId());
+            }
 
-			if ($i->getWspId() > 0)
-			{
-				$wst = new ilWorkspaceTree($this->user->getId());
-				$obj_id = $wst->lookupObjectId($i->getWspId());
-				$obj_type = ilObject::_lookupType($obj_id);
-				$props[$lng->txt("obj_".$obj_type )] = ilObject::_lookupTitle($obj_id);
-			}
+            if ($i->getWspId() > 0) {
+                $wst = new ilWorkspaceTree($this->user->getId());
+                $obj_id = $wst->lookupObjectId($i->getWspId());
+                $obj_type = ilObject::_lookupType($obj_id);
+                $props[$lng->txt("obj_" . $obj_type)] = ilObject::_lookupTitle($obj_id);
+            }
 
-			if (strlen($i->getUrl()) > 0) {
-				$link = $i->getUrl();
-			}
+            if (strlen($i->getUrl()) > 0) {
+                $link = $i->getUrl();
+            }
 
-			if (strlen($link) > 0) {
-				$title = $f->button()->shy($title, $link);
-			}
+            if (strlen($link) > 0) {
+                $title = $f->button()->shy($title, $link);
+            }
 
-			if ($i->getStartingTime() > 0)
-			{
-				$start = new ilDateTime($i->getStartingTime(), IL_CAL_UNIX);
-				$props[$lng->txt("task_start")] = ilDatePresentation::formatDate($start);
-			}
-			if ($i->getDeadline() > 0)
-			{
-				$end = new ilDateTime($i->getDeadline(), IL_CAL_UNIX);
-				$props[$lng->txt("task_deadline")] = ilDatePresentation::formatDate($end);
-			}
-			$item = $f->item()->standard($title)->withProperties($props);
-			if ($i->getDeadline() > 0)
-			{
-				$list_items_with_deadline[] = $item;
-			}
-			else
-			{
-				$list_items_without_deadline[] = $item;
-			}
-		}
-		if (count($list_items_with_deadline) > 0)
-		{
-			$item_groups[] = $f->item()->group($lng->txt("task_tasks_with_deadline"), $list_items_with_deadline);
-		}
-		if (count($list_items_without_deadline) > 0)
-		{
-			$item_groups[] = $f->item()->group($lng->txt("task_tasks_without_deadline"), $list_items_without_deadline);
-		}
+            if ($i->getStartingTime() > 0) {
+                $start = new ilDateTime($i->getStartingTime(), IL_CAL_UNIX);
+                $props[$lng->txt("task_start")] = ilDatePresentation::formatDate($start);
+            }
+            if ($i->getDeadline() > 0) {
+                $end = new ilDateTime($i->getDeadline(), IL_CAL_UNIX);
+                $props[$lng->txt("task_deadline")] = ilDatePresentation::formatDate($end);
+            }
+            $item = $f->item()->standard($title)->withProperties($props);
+            if ($i->getDeadline() > 0) {
+                $list_items_with_deadline[] = $item;
+            } else {
+                $list_items_without_deadline[] = $item;
+            }
+        }
+        if (count($list_items_with_deadline) > 0) {
+            $item_groups[] = $f->item()->group($lng->txt("task_tasks_with_deadline"), $list_items_with_deadline);
+        }
+        if (count($list_items_without_deadline) > 0) {
+            $item_groups[] = $f->item()->group($lng->txt("task_tasks_without_deadline"), $list_items_without_deadline);
+        }
 
-		// output list panel or info message
-		if (count($item_groups) > 0)
-		{
-			$std_list = $f->panel()->listing()->standard("", $item_groups);
-			$main_tpl->setContent($renderer->render($std_list));
-		}
-		else
-		{
-			ilUtil::sendInfo($lng->txt("task_no_tasks"));
-		}
-	}
-
-	
+        // output list panel or info message
+        if (count($item_groups) > 0) {
+            $std_list = $f->panel()->listing()->standard("", $item_groups);
+            $main_tpl->setContent($renderer->render($std_list));
+        } else {
+            ilUtil::sendInfo($lng->txt("task_no_tasks"));
+        }
+    }
 }
