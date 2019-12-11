1013c357c9091cfc526bbcd2fa1e35d443180bf9
Alexander Killing <killing@leifos.de>
2019-12-11 12:23:24 +0100

    coding style fixes

--- Services/Utilities/classes/Setup/class.ilUtilitiesSetupAgent.php
+++ Services/Utilities/classes/Setup/class.ilUtilitiesSetupAgent.php
@@ -7,67 +7,74 @@ use ILIAS\Refinery;
 use ILIAS\Data;
 use ILIAS\UI;
 
-class ilUtilitiesSetupAgent implements Setup\Agent {
-	/**
-	 * @var Refinery\Factory
-	 */
-	protected $refinery;
+class ilUtilitiesSetupAgent implements Setup\Agent
+{
+    /**
+     * @var Refinery\Factory
+     */
+    protected $refinery;
 
-	public function __construct(
-		Refinery\Factory $refinery
-	) {
-		$this->refinery = $refinery;
-	}
+    public function __construct(
+        Refinery\Factory $refinery
+    ) {
+        $this->refinery = $refinery;
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function hasConfig() : bool {
-		return true;
-	}
+    /**
+     * @inheritdoc
+     */
+    public function hasConfig() : bool
+    {
+        return true;
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function getConfigInput(Setup\Config $config = null) : UI\Component\Input\Field\Input {
-		throw new \LogicException("Not yet implemented.");
-	}
+    /**
+     * @inheritdoc
+     */
+    public function getConfigInput(Setup\Config $config = null) : UI\Component\Input\Field\Input
+    {
+        throw new \LogicException("Not yet implemented.");
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function getArrayToConfigTransformation() : Refinery\Transformation {
-		return $this->refinery->custom()->transformation(function($data) {
-			return new \ilUtilitiesSetupConfig(
-				$data["path_to_convert"] ?? "/usr/bin/convert",
-				$data["path_to_zip"] ?? "/usr/bin/zip",
-				$data["path_to_unzip"] ?? "/usr/bin/unzip"
-			);
-		});	
-	}
+    /**
+     * @inheritdoc
+     */
+    public function getArrayToConfigTransformation() : Refinery\Transformation
+    {
+        return $this->refinery->custom()->transformation(function ($data) {
+            return new \ilUtilitiesSetupConfig(
+                $data["path_to_convert"] ?? "/usr/bin/convert",
+                $data["path_to_zip"] ?? "/usr/bin/zip",
+                $data["path_to_unzip"] ?? "/usr/bin/unzip"
+            );
+        });
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function getInstallObjective(Setup\Config $config = null) : Setup\Objective {
-		return new Setup\ObjectiveCollection(
-			"Complete objectives from Services/Utilities",
-			false,
-			new ilUtilitiesConfigStoredObjective($config)
-		);
-	}
+    /**
+     * @inheritdoc
+     */
+    public function getInstallObjective(Setup\Config $config = null) : Setup\Objective
+    {
+        return new Setup\ObjectiveCollection(
+            "Complete objectives from Services/Utilities",
+            false,
+            new ilUtilitiesConfigStoredObjective($config)
+        );
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function getUpdateObjective(Setup\Config $config = null) : Setup\Objective {
-		return new Setup\NullObjective();
-	}
+    /**
+     * @inheritdoc
+     */
+    public function getUpdateObjective(Setup\Config $config = null) : Setup\Objective
+    {
+        return new Setup\NullObjective();
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function getBuildArtifactObjective() : Setup\Objective {
-		return new Setup\NullObjective();
-	}
+    /**
+     * @inheritdoc
+     */
+    public function getBuildArtifactObjective() : Setup\Objective
+    {
+        return new Setup\NullObjective();
+    }
 }
--- Services/Utilities/classes/class.ilConfirmationTableGUI.php
+++ Services/Utilities/classes/class.ilConfirmationTableGUI.php
@@ -4,7 +4,7 @@
 include_once("./Services/Table/classes/class.ilTable2GUI.php");
 
 /**
-* TableGUI class for 
+* TableGUI class for
 *
 * @author Alex Killing <alex.killing@gmx.de>
 * @version $Id$
@@ -13,73 +13,65 @@ include_once("./Services/Table/classes/class.ilTable2GUI.php");
 */
 class ilConfirmationTableGUI extends ilTable2GUI
 {
-	/**
-	 * @var ilAccessHandler
-	 */
-	protected $access;
+    /**
+     * @var ilAccessHandler
+     */
+    protected $access;
 
-	
-	/**
-	* Constructor
-	*/
-	function __construct($a_use_icons)
-	{
-		global $DIC;
+    
+    /**
+    * Constructor
+    */
+    public function __construct($a_use_icons)
+    {
+        global $DIC;
 
-		$this->ctrl = $DIC->ctrl();
-		$this->lng = $DIC->language();
-		$this->access = $DIC->access();
-		$ilCtrl = $DIC->ctrl();
-		$lng = $DIC->language();
-		$ilAccess = $DIC->access();
-		$lng = $DIC->language();
-		
-		$this->use_icons = $a_use_icons;
-		
-		parent::__construct(null, "");
-		$this->setTitle($lng->txt(""));
-		$this->setLimit(9999);
-		
-		if ($this->use_icons)
-		{
-			$this->addColumn($this->lng->txt("type"), "", "1");
-		}
-		$this->addColumn($this->lng->txt("title"));
-		
-		$this->setEnableHeader(true);
-		$this->setRowTemplate("tpl.confirmation_row.html", "Services/Utilities");
-		$this->disable("footer");
-		$this->setEnableTitle(true);
-	}
-	
-	/**
-	* Fill table row
-	*/
-	protected function fillRow($item)
-	{
-		$lng = $this->lng;
+        $this->ctrl = $DIC->ctrl();
+        $this->lng = $DIC->language();
+        $this->access = $DIC->access();
+        $ilCtrl = $DIC->ctrl();
+        $lng = $DIC->language();
+        $ilAccess = $DIC->access();
+        $lng = $DIC->language();
+        
+        $this->use_icons = $a_use_icons;
+        
+        parent::__construct(null, "");
+        $this->setTitle($lng->txt(""));
+        $this->setLimit(9999);
+        
+        if ($this->use_icons) {
+            $this->addColumn($this->lng->txt("type"), "", "1");
+        }
+        $this->addColumn($this->lng->txt("title"));
+        
+        $this->setEnableHeader(true);
+        $this->setRowTemplate("tpl.confirmation_row.html", "Services/Utilities");
+        $this->disable("footer");
+        $this->setEnableTitle(true);
+    }
+    
+    /**
+    * Fill table row
+    */
+    protected function fillRow($item)
+    {
+        $lng = $this->lng;
 
-		if ($this->use_icons)
-		{
-			if ($item["img"] != "")
-			{
-				$this->tpl->setCurrentBlock("img_cell");
-				$this->tpl->setVariable("IMG_ITEM", $item["img"]);
-				$this->tpl->setVariable("ALT_ITEM", $item["alt"]);
-				$this->tpl->parseCurrentBlock();
-			}
-			else
-			{
-				$this->tpl->touchBlock("blank_cell");
-			}
-		}
-		$this->tpl->setVariable("TXT_ITEM", $item["text"]);
-		if(isset($item['var'])  && $item['var'])
-		{
-			$this->tpl->setVariable('VAR_ITEM', $item['var']);
-			$this->tpl->setVariable('ID', $item['id']);
-		}
-	}
-
+        if ($this->use_icons) {
+            if ($item["img"] != "") {
+                $this->tpl->setCurrentBlock("img_cell");
+                $this->tpl->setVariable("IMG_ITEM", $item["img"]);
+                $this->tpl->setVariable("ALT_ITEM", $item["alt"]);
+                $this->tpl->parseCurrentBlock();
+            } else {
+                $this->tpl->touchBlock("blank_cell");
+            }
+        }
+        $this->tpl->setVariable("TXT_ITEM", $item["text"]);
+        if (isset($item['var'])  && $item['var']) {
+            $this->tpl->setVariable('VAR_ITEM', $item['var']);
+            $this->tpl->setVariable('ID', $item['id']);
+        }
+    }
 }
-?>
