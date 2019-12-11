9c0092991cfaca937008c2983964278d90bb2094
Alexander Killing <killing@leifos.de>
2019-12-11 09:35:43 +0100

    coding style fixes

--- Modules/Portfolio/classes/class.ilFSStoragePortfolio.php
+++ Modules/Portfolio/classes/class.ilFSStoragePortfolio.php
@@ -8,18 +8,18 @@
  */
 class ilFSStoragePortfolio extends ilFileSystemStorage
 {
-	public function __construct($a_container_id = 0)
-	{
-		parent::__construct(self::STORAGE_SECURED, true, $a_container_id);
-	}
-	
-	protected function getPathPostfix()
-	{
-	 	return 'prtf';
-	}
-	
-	protected function getPathPrefix()
-	{
-	 	return 'ilPortfolio';
-	}
+    public function __construct($a_container_id = 0)
+    {
+        parent::__construct(self::STORAGE_SECURED, true, $a_container_id);
+    }
+    
+    protected function getPathPostfix()
+    {
+        return 'prtf';
+    }
+    
+    protected function getPathPrefix()
+    {
+        return 'ilPortfolio';
+    }
 }
