c9d22d099f8726d8e40e29c99ec45c4b223c6b09
Alexander Killing <killing@leifos.de>
2019-12-11 11:38:45 +0100

    coding style fixes

--- Services/News/classes/class.ilNewsCache.php
+++ Services/News/classes/class.ilNewsCache.php
@@ -12,71 +12,72 @@ include_once("./Services/Cache/classes/class.ilCache.php");
  */
 class ilNewsCache extends ilCache
 {
-	/**
-	 * @var ilSetting
-	 */
-	protected $settings;
+    /**
+     * @var ilSetting
+     */
+    protected $settings;
 
-	static $disabled = false;
-	
-	/**
-	 * Constructor
-	 *
-	 * @param
-	 * @return
-	 */
-	function __construct()
-	{
-		global $DIC;
+    public static $disabled = false;
+    
+    /**
+     * Constructor
+     *
+     * @param
+     * @return
+     */
+    public function __construct()
+    {
+        global $DIC;
 
-		$this->settings = $DIC->settings();
-		$ilSetting = $DIC->settings();
-		
-		$news_set = new ilSetting("news");
-		$news_set->get("acc_cache_mins");
-		
-		parent::__construct("ServicesNews", "News", true);
-		$this->setExpiresAfter($news_set->get("acc_cache_mins") * 60);
-		if ((int) $news_set->get("acc_cache_mins") == 0)
-		{
-			self::$disabled = true;
-		}
-	}
-	
-	/**
-	 * Check if cache is disabled
-	 * @return 
-	 */
-	public function isDisabled()
-	{
-		return self::$disabled or parent::isDisabled();
-	}
-	
-	
-	/**
-	 * Read an entry
-	 */
-	function readEntry($a_id)
-	{
-		if (!$this->isDisabled())
-		{
-			return parent::readEntry($a_id);
-		}
-		return false;
-	}
-	
-	
-	/**
-	 * Id is user_id:ref_id, we store ref_if additionally
-	 */
-	function storeEntry($a_id, $a_value, $a_int_key1 = null, $a_int_key2 = null,
-						$a_text_key1 = null, $a_text_key2 = null)
-	{
-		$ilSetting = $this->settings;
-		if(!$this->isDisabled())
-		{
-			parent::storeEntry($a_id, $a_value);
-		}
-	}
+        $this->settings = $DIC->settings();
+        $ilSetting = $DIC->settings();
+        
+        $news_set = new ilSetting("news");
+        $news_set->get("acc_cache_mins");
+        
+        parent::__construct("ServicesNews", "News", true);
+        $this->setExpiresAfter($news_set->get("acc_cache_mins") * 60);
+        if ((int) $news_set->get("acc_cache_mins") == 0) {
+            self::$disabled = true;
+        }
+    }
+    
+    /**
+     * Check if cache is disabled
+     * @return
+     */
+    public function isDisabled()
+    {
+        return self::$disabled or parent::isDisabled();
+    }
+    
+    
+    /**
+     * Read an entry
+     */
+    public function readEntry($a_id)
+    {
+        if (!$this->isDisabled()) {
+            return parent::readEntry($a_id);
+        }
+        return false;
+    }
+    
+    
+    /**
+     * Id is user_id:ref_id, we store ref_if additionally
+     */
+    public function storeEntry(
+        $a_id,
+        $a_value,
+        $a_int_key1 = null,
+        $a_int_key2 = null,
+        $a_text_key1 = null,
+        $a_text_key2 = null
+    ) {
+        $ilSetting = $this->settings;
+        if (!$this->isDisabled()) {
+            parent::storeEntry($a_id, $a_value);
+        }
+    }
 }
-?>
