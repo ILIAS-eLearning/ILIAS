20b10eb1b37a4600d21b03b2a37affa3694ed975
Alexander Killing <killing@leifos.de>
2019-12-11 10:29:55 +0100

    coding style fixes

--- Services/Context/classes/class.ilContextSoapNoAuth.php
+++ Services/Context/classes/class.ilContextSoapNoAuth.php
@@ -7,78 +7,86 @@ include_once "Services/Context/interfaces/interface.ilContextTemplate.php";
 /**
  * Service context for soap without handling authentication
  */
-class ilContextSoapNoAuth implements ilContextTemplate {
+class ilContextSoapNoAuth implements ilContextTemplate
+{
 
-	/**
-	 * @inheritdoc
-	 */
-	public static function supportsRedirects() {
-		return false;
-	}
+    /**
+     * @inheritdoc
+     */
+    public static function supportsRedirects()
+    {
+        return false;
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public static function hasUser() {
-		return false;
-	}
+    /**
+     * @inheritdoc
+     */
+    public static function hasUser()
+    {
+        return false;
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public static function usesHTTP() {
-		return true;
-	}
+    /**
+     * @inheritdoc
+     */
+    public static function usesHTTP()
+    {
+        return true;
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public static function hasHTML() {
-		return false;
-	}
+    /**
+     * @inheritdoc
+     */
+    public static function hasHTML()
+    {
+        return false;
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public static function usesTemplate() {
-		return false;
-	}
+    /**
+     * @inheritdoc
+     */
+    public static function usesTemplate()
+    {
+        return false;
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public static function initClient() {
-		return true;
-	}
+    /**
+     * @inheritdoc
+     */
+    public static function initClient()
+    {
+        return true;
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public static function doAuthentication() {
-		return false;
-	}
+    /**
+     * @inheritdoc
+     */
+    public static function doAuthentication()
+    {
+        return false;
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public static function supportsPersistentSessions() {
-		return false;
-	}
+    /**
+     * @inheritdoc
+     */
+    public static function supportsPersistentSessions()
+    {
+        return false;
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public static function supportsPushMessages()
-	{
-		return false;
-	}
+    /**
+     * @inheritdoc
+     */
+    public static function supportsPushMessages()
+    {
+        return false;
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public static function isSessionMainContext()
-	{
-		return true;
-	}
-
+    /**
+     * @inheritdoc
+     */
+    public static function isSessionMainContext()
+    {
+        return true;
+    }
 }
