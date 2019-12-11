587e4d6550706098fc82f74aa68b4407db41e23f
Alexander Killing <killing@leifos.de>
2019-12-11 09:17:11 +0100

    coding style fixes

--- Modules/Glossary/classes/class.ilGlossaryTermPermission.php
+++ Modules/Glossary/classes/class.ilGlossaryTermPermission.php
@@ -11,102 +11,93 @@
  */
 class ilGlossaryTermPermission
 {
-	/**
-	 * @var ilObjUser
-	 */
-	protected $user;
+    /**
+     * @var ilObjUser
+     */
+    protected $user;
 
-	/**
-	 * @var ilAccessHandler
-	 */
-	protected $access;
+    /**
+     * @var ilAccessHandler
+     */
+    protected $access;
 
-	/**
-	 * @var int[]
-	 */
-	protected $glossary_id = array();
+    /**
+     * @var int[]
+     */
+    protected $glossary_id = array();
 
-	/**
-	 * @var array
-	 */
-	protected $permission = array();
+    /**
+     * @var array
+     */
+    protected $permission = array();
 
-	/**
-	 * @var ilLogger
-	 */
-	protected $log;
+    /**
+     * @var ilLogger
+     */
+    protected $log;
 
-	/**
-	 * ilGlossaryTermPermission constructor.
-	 */
-	private function __construct()
-	{
-		global $DIC;
+    /**
+     * ilGlossaryTermPermission constructor.
+     */
+    private function __construct()
+    {
+        global $DIC;
 
-		$this->user = $DIC->user();
-		$this->access = $DIC->access();
+        $this->user = $DIC->user();
+        $this->access = $DIC->access();
 
-		$this->log = ilLoggerFactory::getLogger('glo');
-	}
+        $this->log = ilLoggerFactory::getLogger('glo');
+    }
 
-	/**
-	 * Get instance
-	 * @return ilGlossaryTermPermission
-	 */
-	static public function getInstance()
-	{
-		return new self();
-	}
+    /**
+     * Get instance
+     * @return ilGlossaryTermPermission
+     */
+    public static function getInstance()
+    {
+        return new self();
+    }
 
-	/**
-	 * Check permission
-	 *
-	 * @param string $a_perm
-	 * @param int $a_term_id
-	 * @return bool
-	 */
-	public function checkPermission($a_perm, $a_term_id)
-	{
-		$this->log->debug("check permission ".$a_perm." for ".$a_term_id.".");
-		$glo_id = $this->getGlossaryIdForTerm($a_term_id);
-		if (!isset($this->permission[$a_perm][$glo_id]))
-		{
-			$this->permission[$a_perm][$glo_id] = false;
-			$this->log->debug("...checking references");
-			foreach (ilObject::_getAllReferences($glo_id) as $ref_id)
-			{
-				$this->log->debug("...".$ref_id);
-				if ($this->permission[$a_perm][$glo_id] == true)
-				{
-					continue;
-				}
-				if ($this->access->checkAccess($a_perm, "", $ref_id))
-				{
-					$this->permission[$a_perm][$glo_id] = true;
-				}
-			}
-		}
-		$this->log->debug("...return ".((int) $this->permission[$a_perm][$glo_id]));
-		return $this->permission[$a_perm][$glo_id];
-	}
+    /**
+     * Check permission
+     *
+     * @param string $a_perm
+     * @param int $a_term_id
+     * @return bool
+     */
+    public function checkPermission($a_perm, $a_term_id)
+    {
+        $this->log->debug("check permission " . $a_perm . " for " . $a_term_id . ".");
+        $glo_id = $this->getGlossaryIdForTerm($a_term_id);
+        if (!isset($this->permission[$a_perm][$glo_id])) {
+            $this->permission[$a_perm][$glo_id] = false;
+            $this->log->debug("...checking references");
+            foreach (ilObject::_getAllReferences($glo_id) as $ref_id) {
+                $this->log->debug("..." . $ref_id);
+                if ($this->permission[$a_perm][$glo_id] == true) {
+                    continue;
+                }
+                if ($this->access->checkAccess($a_perm, "", $ref_id)) {
+                    $this->permission[$a_perm][$glo_id] = true;
+                }
+            }
+        }
+        $this->log->debug("...return " . ((int) $this->permission[$a_perm][$glo_id]));
+        return $this->permission[$a_perm][$glo_id];
+    }
 
-	/**
-	 * Get glossary for term
-	 *
-	 * @param int $a_term_id
-	 * @return int
-	 */
-	protected function getGlossaryIdForTerm($a_term_id)
-	{
-		if (!isset($this->glossary_id[$a_term_id]))
-		{
-			include_once("./Modules/Glossary/classes/class.ilGlossaryTerm.php");
-			$this->glossary_id[$a_term_id] = ilGlossaryTerm::_lookGlossaryID($a_term_id);
-		}
-		return $this->glossary_id[$a_term_id];
-	}
-
-
+    /**
+     * Get glossary for term
+     *
+     * @param int $a_term_id
+     * @return int
+     */
+    protected function getGlossaryIdForTerm($a_term_id)
+    {
+        if (!isset($this->glossary_id[$a_term_id])) {
+            include_once("./Modules/Glossary/classes/class.ilGlossaryTerm.php");
+            $this->glossary_id[$a_term_id] = ilGlossaryTerm::_lookGlossaryID($a_term_id);
+        }
+        return $this->glossary_id[$a_term_id];
+    }
 }
-
-?>
