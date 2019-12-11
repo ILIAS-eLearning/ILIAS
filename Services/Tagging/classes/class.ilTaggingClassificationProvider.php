78312b7e30920e98cce377d5c720f78a642f19c5
Alexander Killing <killing@leifos.de>
2019-12-11 16:52:27 +0100

    fixed bug #26482: \ilObjSurvey::checkConstraint: count(): Parameter must be an array or an object that implements Countable

--- Services/Tagging/classes/class.ilTaggingClassificationProvider.php
+++ Services/Tagging/classes/class.ilTaggingClassificationProvider.php
@@ -80,7 +80,7 @@ class ilTaggingClassificationProvider extends ilClassificationProvider
     public function render(array &$a_html, $a_parent_gui)
     {
         $lng = $this->lng;
-        
+
         $lng->loadLanguageModule("tagging");
         
         $all_tags = $this->getSubTreeTags();
