0c0ac672c0ee1d2e3791c4bea46759957c2b690c
Alexander Killing <killing@leifos.de>
2019-12-11 11:38:55 +0100

    coding style fixes

--- Services/News/classes/class.ilNewsDefaultRendererGUI.php
+++ Services/News/classes/class.ilNewsDefaultRendererGUI.php
@@ -12,149 +12,145 @@ include_once("./Services/News/interfaces/interface.ilNewsRendererGUI.php");
  */
 class ilNewsDefaultRendererGUI implements ilNewsRendererGUI
 {
-	protected $lng_key;
+    protected $lng_key;
 
-	/**
-	 * @var ilCtrl
-	 */
-	protected $ctrl;
+    /**
+     * @var ilCtrl
+     */
+    protected $ctrl;
 
-	/**
-	 * @var ilLanguage
-	 */
-	protected $lng;
+    /**
+     * @var ilLanguage
+     */
+    protected $lng;
 
-	/**
-	 * @var ilNewsItem
-	 */
-	protected $news_item;
+    /**
+     * @var ilNewsItem
+     */
+    protected $news_item;
 
-	/**
-	 * @var int
-	 */
-	protected $news_ref_id;
+    /**
+     * @var int
+     */
+    protected $news_ref_id;
 
-	/**
-	 * Constructor
-	 *
-	 * @param
-	 * @return
-	 */
-	function __construct()
-	{
-		global $DIC;
+    /**
+     * Constructor
+     *
+     * @param
+     * @return
+     */
+    public function __construct()
+    {
+        global $DIC;
 
-		$this->ctrl = $DIC->ctrl();
-		$this->lng = $DIC->language();
-	}
+        $this->ctrl = $DIC->ctrl();
+        $this->lng = $DIC->language();
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	function setNewsItem(ilNewsItem $a_news_item, $a_news_ref_id)
-	{
-		$this->news_item = $a_news_item;
-		$this->news_ref_id = $a_news_ref_id;
-	}
+    /**
+     * @inheritdoc
+     */
+    public function setNewsItem(ilNewsItem $a_news_item, $a_news_ref_id)
+    {
+        $this->news_item = $a_news_item;
+        $this->news_ref_id = $a_news_ref_id;
+    }
 
-	/**
-	 * Get news item
-	 *
-	 * @return ilNewsItem
-	 */
-	function getNewsItem()
-	{
-		return $this->news_item;
-	}
+    /**
+     * Get news item
+     *
+     * @return ilNewsItem
+     */
+    public function getNewsItem()
+    {
+        return $this->news_item;
+    }
 
-	/**
-	 * Get news ref id
-	 *
-	 * @return int ref id
-	 */
-	function getNewsRefId()
-	{
-		return $this->news_ref_id;
-	}
-
-
-	/**
-	 * @inheritdoc
-	 */
-	function setLanguage($a_lang_key)
-	{
-		$this->lng_key = $a_lang_key;
-	}
+    /**
+     * Get news ref id
+     *
+     * @return int ref id
+     */
+    public function getNewsRefId()
+    {
+        return $this->news_ref_id;
+    }
 
 
-	/**
-	 * @inheritdoc
-	 */
-	public function getTimelineContent()
-	{
-		return $this->getDetailContent();
-	}
-
-	/**
-	 * @inheritdoc
-	 */
-	function getDetailContent()
-	{
-		if ($this->news_item->getContentTextIsLangVar())
-		{
-			$this->lng->loadLanguageModule($this->news_item->getContextObjType());
-			return ilNewsItem::determineNewsContent($this->news_item->getContextObjType(), $this->news_item->getContent(),
-				$this->news_item->getContentTextIsLangVar());
-		}
-
-		$content = $this->makeClickable($this->news_item->getContent());
-		if (!$this->news_item->getContentHtml())
-		{
-			$content = "<p>".nl2br($content)."</p>";
-		}
-		$content.= $this->news_item->getContentLong();
-
-		return $content;
-	}
-
-	/**
-	 * Make clickable
-	 *
-	 * @param
-	 * @return
-	 */
-	function makeClickable($a_str)
-	{
-		// this fixes bug 8744.
-		// If the string already contains a tags our makeClickable does not work
-		if (is_int(strpos($a_str, "</a>")) && is_int(strpos($a_str, "<a")))
-		{
-			return $a_str;
-		}
-
-		return ilUtil::makeClickable($a_str);
-	}
+    /**
+     * @inheritdoc
+     */
+    public function setLanguage($a_lang_key)
+    {
+        $this->lng_key = $a_lang_key;
+    }
 
 
-	/**
-	 * @param ilAdvancedSelectionListGUI $list
-	 */
-	public function addTimelineActions(ilAdvancedSelectionListGUI $list)
-	{
+    /**
+     * @inheritdoc
+     */
+    public function getTimelineContent()
+    {
+        return $this->getDetailContent();
+    }
 
-	}
+    /**
+     * @inheritdoc
+     */
+    public function getDetailContent()
+    {
+        if ($this->news_item->getContentTextIsLangVar()) {
+            $this->lng->loadLanguageModule($this->news_item->getContextObjType());
+            return ilNewsItem::determineNewsContent(
+                $this->news_item->getContextObjType(),
+                $this->news_item->getContent(),
+                $this->news_item->getContentTextIsLangVar()
+            );
+        }
 
-	/**
-	 * Get object link
-	 *
-	 * @return string link href url
-	 */
-	function getObjectLink()
-	{
-		include_once("./Services/Link/classes/class.ilLink.php");
-		return ilLink::_getLink($this->getNewsRefId());
-	}
-	
+        $content = $this->makeClickable($this->news_item->getContent());
+        if (!$this->news_item->getContentHtml()) {
+            $content = "<p>" . nl2br($content) . "</p>";
+        }
+        $content.= $this->news_item->getContentLong();
+
+        return $content;
+    }
+
+    /**
+     * Make clickable
+     *
+     * @param
+     * @return
+     */
+    public function makeClickable($a_str)
+    {
+        // this fixes bug 8744.
+        // If the string already contains a tags our makeClickable does not work
+        if (is_int(strpos($a_str, "</a>")) && is_int(strpos($a_str, "<a"))) {
+            return $a_str;
+        }
+
+        return ilUtil::makeClickable($a_str);
+    }
+
+
+    /**
+     * @param ilAdvancedSelectionListGUI $list
+     */
+    public function addTimelineActions(ilAdvancedSelectionListGUI $list)
+    {
+    }
+
+    /**
+     * Get object link
+     *
+     * @return string link href url
+     */
+    public function getObjectLink()
+    {
+        include_once("./Services/Link/classes/class.ilLink.php");
+        return ilLink::_getLink($this->getNewsRefId());
+    }
 }
-
-?>
