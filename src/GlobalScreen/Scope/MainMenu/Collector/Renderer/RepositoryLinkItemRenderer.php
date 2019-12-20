4976a0fa66fd975866970f187d59679d19e6c1c1
Timon Amstutz <timon.amstutz@gmail.com>
2019-12-20 11:39:17 +0100

    Globa Screen/Main Menu: Show Repository Link and Seperator (#2415)
    * Globa Screen/Main Menu: Show Repository Link and Seperator
    
    * Globa Screen/Main Menu: Show Repository Link and Seperator, fix docs
    
    * Globa Screen/Main Menu: Show Repository Link and Seperator, removed seperator

---
+++ src/GlobalScreen/Scope/MainMenu/Collector/Renderer/RepositoryLinkItemRenderer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ILIAS\GlobalScreen\Scope\MainMenu\Collector\Renderer;
+
+use ILIAS\GlobalScreen\Scope\MainMenu\Factory\isItem;
+use ILIAS\UI\Component\Component;
+
+/**
+ * Class RepositoryLinkItemRenderer
+ */
+class RepositoryLinkItemRenderer extends BaseTypeRenderer
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function getComponentWithContent(isItem $item) : Component
+    {
+        return $this->ui_factory->link()->bulky($this->getStandardSymbol($item), $item->getTitle(), $this->getURI($item->getAction()));
+    }
+}
