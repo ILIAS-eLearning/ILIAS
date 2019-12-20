0bdfeca9c1840e90f6dffb19142631b876b674c5
Timon Amstutz <timon.amstutz@bluewin.ch>
2019-12-20 11:37:23 +0100

    Globa Screen/Main Menu: Show Repository Link and Seperator, removed seperator

--- Services/MainMenu/classes/Provider/CustomMainBarProvider.php
+++ Services/MainMenu/classes/Provider/CustomMainBarProvider.php
@@ -11,7 +11,6 @@ use ILIAS\GlobalScreen\Scope\MainMenu\Collector\Renderer\LostItemRenderer;
 use ILIAS\GlobalScreen\Scope\MainMenu\Collector\Renderer\TopLinkItemRenderer;
 use ILIAS\GlobalScreen\Scope\MainMenu\Collector\Renderer\TopParentItemRenderer;
 use ILIAS\GlobalScreen\Scope\MainMenu\Collector\Renderer\RepositoryLinkItemRenderer;
-use ILIAS\GlobalScreen\Scope\MainMenu\Collector\Renderer\SeperatorItemRenderer;
 use ILIAS\GlobalScreen\Scope\MainMenu\Factory\hasAction;
 use ILIAS\GlobalScreen\Scope\MainMenu\Factory\hasTitle;
 use ILIAS\GlobalScreen\Scope\MainMenu\Factory\isChild;
@@ -155,7 +154,7 @@ class CustomMainBarProvider extends AbstractStaticMainMenuProvider implements St
         $link_list->setCreationPrevented(true);
         $c->add($link_list);
         // Separator
-        $c->add(new TypeInformation(Separator::class, $this->translateType(Separator::class), new SeperatorItemRenderer(), new ilMMTypeHandlerSeparator(), $this->translateByline(Separator::class)));
+        $c->add(new TypeInformation(Separator::class, $this->translateType(Separator::class), null, new ilMMTypeHandlerSeparator(), $this->translateByline(Separator::class)));
         // RepositoryLink
         $c->add(new TypeInformation(RepositoryLink::class, $this->translateType(RepositoryLink::class), new RepositoryLinkItemRenderer(), new ilMMTypeHandlerRepositoryLink()));
         // Lost
