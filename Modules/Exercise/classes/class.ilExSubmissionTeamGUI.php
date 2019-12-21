3b63907e8dadf2ab29a8a2239b26524d33da5c32
Alexander Killing <killing@leifos.de>
2019-12-21 11:38:18 +0100

    fixed bug #21598: Fehlende Sprachvariable: -exc_members_already_assigned_team-

--- Modules/Exercise/classes/class.ilExSubmissionTeamGUI.php
+++ Modules/Exercise/classes/class.ilExSubmissionTeamGUI.php
@@ -287,9 +287,9 @@ class ilExSubmissionTeamGUI
                 $this->submission->validatePeerReviews()
             );
             // :TODO: notification?
+            ilUtil::sendSuccess($this->lng->txt("settings_saved"), true);
         }
-        
-        ilUtil::sendSuccess($this->lng->txt("settings_saved"), true);
+
         $this->ctrl->redirect($this, "submissionScreenTeam");
     }
     
