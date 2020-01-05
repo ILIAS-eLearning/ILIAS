11fdd944c71d304c2638dd47b09cdb63d579d6f3
Alexander Killing <killing@leifos.de>
2020-01-05 16:15:57 +0100

    fixed bug 26849: Test6: Skills vs Competences tile

--- Services/Skill/GlobalScreen/classes/class.ilSkillGSToolProvider.php
+++ Services/Skill/GlobalScreen/classes/class.ilSkillGSToolProvider.php
@@ -29,7 +29,17 @@ class ilSkillGSToolProvider extends AbstractDynamicToolProvider
      */
     public function getToolsForContextStack(CalledContexts $called_contexts) : array
     {
+        $lang = $this->dic->language();
+
+        $lang->loadLanguageModule("skill");
+
+        $title = $lang->txt("skmg_skills");
+
         $tools = [];
+
+        $icon = $this->dic->ui()->factory()->symbol()->icon()->custom(\ilUtil::getImagePath("outlined/icon_skmg.svg"), $title);
+
+
         $additional_data = $called_contexts->current()->getAdditionalData();
         if ($additional_data->is(self::SHOW_SKILL_TREE, true)) {
             $iff = function ($id) {
@@ -39,25 +49,30 @@ class ilSkillGSToolProvider extends AbstractDynamicToolProvider
                 return $this->dic->ui()->factory()->legacy($content);
             };
             $tools[] = $this->factory->tool($iff("tree"))
-                ->withTitle("Skills")
+                ->withTitle($title)
+                ->withSymbol($icon)
                 ->withContentWrapper(function () use ($l) {
                     return $l($this->getSkillTree());
                 });
         }
+
+        $title = $lang->txt("skmg_skill_templates");
+        $icon = $this->dic->ui()->factory()->symbol()->icon()->custom(\ilUtil::getImagePath("outlined/icon_skmg.svg"), $title);
+
         if ($additional_data->is(self::SHOW_TEMPLATE_TREE, true)) {
             $iff = function ($id) {
-                return $this->identification_provider->identifier($id);
+                return $this->identification_provider->contextAwareIdentifier($id);
             };
             $l = function (string $content) {
                 return $this->dic->ui()->factory()->legacy($content);
             };
             $tools[] = $this->factory->tool($iff("tree"))
                 ->withTitle("Templates")
+                ->withSymbol($icon)
                 ->withContentWrapper(function () use ($l) {
                     return $l($this->getTemplateTree());
                 });
         }
-
         return $tools;
     }
 
