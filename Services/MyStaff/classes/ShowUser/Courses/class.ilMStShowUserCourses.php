29858d8cbbd187da2d89d2d43494ac35bfd91372
Michael Jansen <mjansen@databay.de>
2020-01-06 08:45:06 +0100

    MyStaff: Fix Mantis issue 26855 (#2421)

--- Services/MyStaff/classes/ShowUser/Courses/class.ilMStShowUserCourses.php
+++ Services/MyStaff/classes/ShowUser/Courses/class.ilMStShowUserCourses.php
@@ -10,30 +10,4 @@ use ILIAS\MyStaff\ListCourses\ilMStListCourses;
  */
 class ilMStShowUserCourses extends ilMStListCourses
 {
-
-
-    /**
-     * @param array  $arr_usr_ids
-     * @param array  $arr_filter
-     * @param string $tmp_table_user_matrix
-     *
-     * @return string
-     */
-    protected function createWhereStatement(array $arr_usr_ids, array $arr_filter, $tmp_table_user_matrix)
-    {
-        global $DIC;
-
-        if (!$arr_filter['usr_id']) {
-            return '';
-        }
-
-        $where = parent::createWhereStatement($arr_usr_ids, $arr_filter, $tmp_table_user_matrix);
-        $usr_filter = "usr_data.usr_id = " . $DIC->database()->quote($arr_filter['usr_id'], 'integer');
-
-        if (empty($where)) {
-            return ' WHERE ' . $usr_filter;
-        } else {
-            return $where . ' AND ' . $usr_filter;
-        }
-    }
 }
