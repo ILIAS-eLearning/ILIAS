cf7f9026614d2cce9eb523075edb2b4de18de7a2
Fabian Schmid <fs@studer-raimann.ch>
2020-01-10 16:16:22 +0100

    [FIX] 0026720: Tooltips in Online Help not working

--- src/GlobalScreen/Scope/MainMenu/Factory/TopItem/TopLinkItem.php
+++ src/GlobalScreen/Scope/MainMenu/Factory/TopItem/TopLinkItem.php
@@ -3,24 +3,20 @@
 use ILIAS\GlobalScreen\Scope\MainMenu\Factory\AbstractBaseItem;
 use ILIAS\GlobalScreen\Scope\MainMenu\Factory\hasAction;
 use ILIAS\GlobalScreen\Scope\MainMenu\Factory\hasSymbol;
+use ILIAS\GlobalScreen\Scope\MainMenu\Factory\hasSymbolTrait;
 use ILIAS\GlobalScreen\Scope\MainMenu\Factory\hasTitle;
 use ILIAS\GlobalScreen\Scope\MainMenu\Factory\isTopItem;
-use ILIAS\UI\Component\Symbol\Symbol;
-use ILIAS\UI\Component\Symbol\Glyph;
-use ILIAS\UI\Component\Symbol\Icon;
+use ILIAS\GlobalScreen\Scope\MainMenu\Factory\SymbolDecoratorTrait;
 
 /**
  * Class TopLinkItem
- *
  * @author Fabian Schmid <fs@studer-raimann.ch>
  */
 class TopLinkItem extends AbstractBaseItem implements hasTitle, hasAction, isTopItem, hasSymbol
 {
+    use SymbolDecoratorTrait;
+    use hasSymbolTrait;
 
-    /**
-     * @var Symbol
-     */
-    protected $symbol;
     /**
      * @var bool
      */
@@ -34,20 +30,17 @@ class TopLinkItem extends AbstractBaseItem implements hasTitle, hasAction, isTop
      */
     protected $action = '';
 
-
     /**
      * @param string $title
-     *
      * @return hasTitle|TopLinkItem
      */
     public function withTitle(string $title) : hasTitle
     {
-        $clone = clone($this);
+        $clone        = clone($this);
         $clone->title = $title;
 
         return $clone;
     }
-
 
     /**
      * @return string
@@ -57,20 +50,17 @@ class TopLinkItem extends AbstractBaseItem implements hasTitle, hasAction, isTop
         return $this->title;
     }
 
-
     /**
      * @param string $action
-     *
      * @return hasAction|TopLinkItem
      */
     public function withAction(string $action) : hasAction
     {
-        $clone = clone($this);
+        $clone         = clone($this);
         $clone->action = $action;
 
         return $clone;
     }
-
 
     /**
      * @return string
@@ -80,20 +70,17 @@ class TopLinkItem extends AbstractBaseItem implements hasTitle, hasAction, isTop
         return $this->action;
     }
 
-
     /**
      * @param bool $is_external
-     *
      * @return TopLinkItem
      */
     public function withIsLinkToExternalAction(bool $is_external) : hasAction
     {
-        $clone = clone $this;
+        $clone                     = clone $this;
         $clone->is_external_action = $is_external;
 
         return $clone;
     }
-
 
     /**
      * @return bool
@@ -103,39 +90,4 @@ class TopLinkItem extends AbstractBaseItem implements hasTitle, hasAction, isTop
         return $this->is_external_action;
     }
 
-
-    /**
-     * @inheritDoc
-     */
-    public function withSymbol(Symbol $symbol) : hasSymbol
-    {
-        // bugfix mantis 25526: make aria labels mandatory
-        if (($symbol instanceof Icon\Icon || $symbol instanceof Glyph\Glyph)
-            && ($symbol->getAriaLabel() === "")) {
-            throw new \LogicException("the symbol's aria label MUST be set to ensure accessibility");
-        }
-
-        $clone = clone $this;
-        $clone->symbol = $symbol;
-
-        return $clone;
-    }
-
-
-    /**
-     * @inheritDoc
-     */
-    public function getSymbol() : Symbol
-    {
-        return $this->symbol;
-    }
-
-
-    /**
-     * @inheritDoc
-     */
-    public function hasSymbol() : bool
-    {
-        return $this->symbol instanceof Symbol;
-    }
 }
