32582a739114e2ca3eb3b4e6f997ff4b23479474
Nils Haagen <nhaagen@cat06.de>
2020-01-13 13:11:23 +0100

    fix 27046 - default create_db to true, fix typo

--- Services/Database/classes/Setup/class.ilDatabaseSetupAgent.php
+++ Services/Database/classes/Setup/class.ilDatabaseSetupAgent.php
@@ -49,7 +49,7 @@ class ilDatabaseSetupAgent implements Setup\Agent
                 $data["database"] ?? "ilias",
                 $data["user"] ?? null,
                 $data["password"] ? $password->transform($data["password"]) : null,
-                $data["create_database"] ?? null,
+                $data["create_database"] ?? true,
                 $data["collation"] ?? null,
                 $data["port"] ?? null,
                 $data["path_to_db_dump"] ?? null
