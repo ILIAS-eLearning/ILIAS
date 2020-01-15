a9293fd4db98163196fa0222b6e1147a52c1b657
Nils Haagen <nhaagen@cat06.de>
2020-01-15 15:01:42 +0100

    Create data-directory for mail (fixes #27173)

---
+++ Services/Mail/classes/Setup/class.ilMailSetupAgent.php
@@ -0,0 +1,63 @@
+<?php
+
+/* Copyright (c) 2020 Nils Haagen <nils.haagen@concepts-and-training.de> Extended GPL, see docs/LICENSE */
+
+use ILIAS\Setup;
+use ILIAS\Refinery;
+use ILIAS\Data;
+use ILIAS\UI;
+
+class ilMailSetupAgent implements Setup\Agent
+{
+    /**
+     * @inheritdoc
+     */
+    public function hasConfig() : bool
+    {
+        return false;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getConfigInput(Setup\Config $config = null) : UI\Component\Input\Field\Input
+    {
+        throw new \LogicException("Agent has no config.");
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getArrayToConfigTransformation() : Refinery\Transformation
+    {
+        throw new \LogicException("Agent has no config.");
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getInstallObjective(Setup\Config $config = null) : Setup\Objective
+    {
+        return new Setup\ObjectiveCollection(
+            "Complete objetives from Services/Mail",
+            false,
+            new ilMailDirectoriesCreatedObjective()
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getUpdateObjective(Setup\Config $config = null) : Setup\Objective
+    {
+        return new Setup\NullObjective();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getBuildArtifactObjective() : Setup\Objective
+    {
+        return new Setup\NullObjective();
+    }
+}
