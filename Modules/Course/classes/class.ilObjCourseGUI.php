ba79024bf31ffa0253c4499af7a5213a7fc6a7c6
smeyer <smeyer.ilias@gmx.de>
2020-01-23 12:17:58 +0100

    0027217: Privaciy Settings for Show Members and Participant List in Courses

--- Modules/Course/classes/class.ilObjCourseGUI.php
+++ Modules/Course/classes/class.ilObjCourseGUI.php
@@ -964,7 +964,9 @@ class ilObjCourseGUI extends ilContainerGUI
         $this->object->setAboStatus((int) $form->getInput('abo'));
         $this->object->setShowMembers((int) $form->getInput('show_members'));
         
+        if (\ilPrivacySettings::_getInstance()->participantsListInCoursesEnabled()) {
         $this->object->setShowMembersExport((int) $form->getInput('show_members_export'));
+        }
         $this->object->setMailToMembersType((int) $form->getInput('mail_type'));
         
         $this->object->enableSessionLimit((int) $form->getInput('sl'));
@@ -1490,11 +1492,13 @@ class ilObjCourseGUI extends ilContainerGUI
         $mem->setInfo($this->lng->txt('crs_show_members_info'));
         $form->addItem($mem);
         
+        // check privacy
+        if(\ilPrivacySettings::_getInstance()->participantsListInCoursesEnabled()) {
         $part_list = new ilCheckboxInputGUI($this->lng->txt('crs_show_member_export'), 'show_members_export');
         $part_list->setChecked($this->object->getShowMembersExport());
         $part_list->setInfo($this->lng->txt('crs_show_member_export_info'));
         $mem->addSubItem($part_list);
-        
+        }
 
         // Show members type
         $mail_type = new ilRadioGroupInputGUI($this->lng->txt('crs_mail_type'), 'mail_type');
