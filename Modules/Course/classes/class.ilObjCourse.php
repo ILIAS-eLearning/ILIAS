40232993bebf6b93ae3f8cb20190f1c61ddf68dc
smeyer <smeyer.ilias@gmx.de>
2020-01-23 12:17:18 +0100

    0027217: Privaciy Settings for Show Members and Participant List in Courses

--- Modules/Course/classes/class.ilObjCourse.php
+++ Modules/Course/classes/class.ilObjCourse.php
@@ -1584,7 +1584,13 @@ class ilObjCourse extends ilContainer implements ilMembershipRegistrationCodes
             $this->enableWaitingList($row->waiting_list);
             $this->setImportantInformation($row->important);
             $this->setShowMembers($row->show_members);
+
+            if (\ilPrivacySettings::_getInstance()->participantsListInCoursesEnabled()) {
             $this->setShowMembersExport($row->show_members_export);
+            }
+            else {
+                $this->setShowMembersExport(false);
+            }
             $this->setLatitude($row->latitude);
             $this->setLongitude($row->longitude);
             $this->setLocationZoom($row->location_zoom);
