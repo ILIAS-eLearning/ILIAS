dfb63531dd0b89c6944dc02e8a57c6270e843c0a
Alexander Killing <killing@leifos.de>
2020-01-25 17:28:28 +0100

    fixed bug #27096: Failed test: Fehler/Worte-markieren-Frage erstellen/bearbeiten

--- Services/COPage/classes/class.ilPCQuestion.php
+++ Services/COPage/classes/class.ilPCQuestion.php
@@ -391,7 +391,7 @@ class ilPCQuestion extends ilPageContent
         if ($this->getPage()->getPageConfig()->getEnableSelfAssessment()) {
             return array("./Modules/Scorm2004/templates/default/question_handling.css",
                 "Modules/TestQuestionPool/templates/default/test_javascript.css",
-                'Modules/TestQuestionPool/templates/default/ta.css');
+                'Modules/Test/templates/default/ta.css');
         }
         return array();
     }
