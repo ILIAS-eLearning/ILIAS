b6185a013fb2cb41d20b72aca67456bd7151f6fb
Nils Haagen <nhaagen@cat06.de>
2020-02-13 12:26:02 +0100

    fix 27301 - LearningSequence needs a folder (#2495)

---
+++ Modules/LearningSequence/classes/Setup/class.ilLearningSequenceSetupAgent.php
@@ -0,0 +1,66 @@
+<?php
+
+/* Copyright (c) 2020 Nils Haagen <nils.haagen@concepts-and-training.de> Extended GPL, see docs/LICENSE */
+
+use ILIAS\Setup;
+use ILIAS\Refinery;
+use ILIAS\UI;
+
+class ilLearningSequenceSetupAgent implements Setup\Agent
+{
+    /**
+     * @inheritdoc
+     */
+    public function hasConfig() : bool
+    {
+        return false;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getConfigInput(Setup\Config $config = null) : UI\Component\Input\Field\Input
+    {
+        throw new \LogicException("Agent has no config.");
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getArrayToConfigTransformation() : Refinery\Transformation
+    {
+        throw new \LogicException("Agent has no config.");
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getInstallObjective(Setup\Config $config = null) : Setup\Objective
+    {
+        $dir_objective = new ilFileSystemComponentDataDirectoryCreatedObjective(
+            ilLearningSequenceFilesystem::PATH_PRE,
+            ilFileSystemComponentDataDirectoryCreatedObjective::WEBDIR
+        );
+        return new Setup\ObjectiveCollection(
+            "Complete objectives from Modules/LearningSequence",
+            false,
+            $dir_objective
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getUpdateObjective(Setup\Config $config = null) : Setup\Objective
+    {
+        return new Setup\NullObjective();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getBuildArtifactObjective() : Setup\Objective
+    {
+        return new Setup\NullObjective();
+    }
+}
