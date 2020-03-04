7332e7758cc6cf1a93f13d33563320d475108ad7
Fabian Wolf <wolf@ilias.de>
2020-03-04 16:00:41 +0100

    Release 5.3.20

--- include/inc.ilias_version.php
+++ include/inc.ilias_version.php
@@ -9,5 +9,5 @@
 *
 * @package ilias-core
 */
-define("ILIAS_VERSION", "5.3.19 2020-02-07");
-define("ILIAS_VERSION_NUMERIC", "5.3.19");			// must be always x.y.z: x, y and z are numbers
+define("ILIAS_VERSION", "5.3.20 2020-03-04");
+define("ILIAS_VERSION_NUMERIC", "5.3.20");			// must be always x.y.z: x, y and z are numbers
