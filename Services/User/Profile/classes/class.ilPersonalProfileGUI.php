1f387ef6917ccf053b40be926fc3f9c7c8d3b0fc
Alexander Killing <killing@leifos.de>
2020-03-05 22:49:40 +0100

    fixed bug #26630: Profil veröffentlichen in Info-Message [nur ILIAS 6]

--- Services/User/Profile/classes/class.ilPersonalProfileGUI.php
+++ Services/User/Profile/classes/class.ilPersonalProfileGUI.php
@@ -752,14 +752,15 @@ class ilPersonalProfileGUI
                 ? $ilUser->prefs["public_profile"]
                 : "n";
             if ($pub_prof == "n") {
-                $button = $DIC->ui()->factory()->button()->shy(
-                    "» " . $lng->txt("user_make_profile_public"),
-                    $ctrl->getLinkTarget($this, "showPublicProfile")
+
+                $box = $DIC->ui()->factory()->messageBox()->info($it)->withLinks(
+                    [$DIC->ui()->factory()->link()->standard($lng->txt("user_make_profile_public"),
+                        $ctrl->getLinkTarget($this, "showPublicProfile")
+                        )]
                 );
-                $it.= "<br><br>" . $DIC->ui()->renderer()->render($button);
+
+                $it = $DIC->ui()->renderer()->render($box);
             }
-            
-            ilUtil::sendInfo(nl2br($it));
         }
 
         $this->setHeader();
@@ -774,7 +775,7 @@ class ilPersonalProfileGUI
             }
         }
 
-        $this->tpl->setContent($this->form->getHTML());
+        $this->tpl->setContent($it.$this->form->getHTML());
 
         $this->tpl->printToStdout();
     }
