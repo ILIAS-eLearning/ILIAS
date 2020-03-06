61b83cc8e764d96e30ac55183a8ef1c4f588e0c0
Alexander Killing <killing@leifos.de>
2020-03-06 14:14:38 +0100

    fixed bug #27389: fehlerhafte Auslieferung von verlinkten Blöcken, die h1- und h2-Absatzformat verwenden

--- Services/COPage/classes/class.ilPageObjectGUI.php
+++ Services/COPage/classes/class.ilPageObjectGUI.php
@@ -2767,7 +2767,7 @@ class ilPageObjectGUI
                 $anchor = str_replace(
                     "TocH",
                     "TocA",
-                    substr($a_output, $os, strpos($a_output, "<", $os) - $os - 3)
+                    substr($a_output, $os, strpos($a_output, "<", $os) - $os - 4)
                 );
 
                 // get heading
