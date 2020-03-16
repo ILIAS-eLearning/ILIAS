ff8be818a2ff05bf37e9dc2b3c99fc39c9ff62cd
smeyer <smeyer.ilias@gmx.de>
2020-03-16 11:30:06 +0100

    Merge pull request #2552 from M-Barz/BT-0027813-TypeError-Fix
    0027813: Added validation for string type when setting external account

--- Services/OpenIdConnect/classes/class.ilAuthProviderOpenIdConnect.php
+++ Services/OpenIdConnect/classes/class.ilAuthProviderOpenIdConnect.php
@@ -130,19 +130,24 @@ class ilAuthProviderOpenIdConnect extends ilAuthProvider implements ilAuthProvid
         }
 
         $uid_field = $this->settings->getUidField();
-        $ext_acocunt = $user_info->$uid_field;
+        $ext_account = $user_info->$uid_field;
 
-        $this->getLogger()->debug('Authenticated external account: ' . $ext_acocunt);
+        $this->getLogger()->debug('Authenticated external account: ' . $ext_account);
 
 
         $int_account = ilObjUser::_checkExternalAuthAccount(
             ilOpenIdConnectUserSync::AUTH_MODE,
-            $ext_acocunt
+            $ext_account
         );
 
         try {
             $sync = new ilOpenIdConnectUserSync($this->settings, $user_info);
-            $sync->setExternalAccount($ext_acocunt);
+            if(!is_string($ext_account)) {
+                $status->setStatus(ilAuthStatus::STATUS_AUTHENTICATION_FAILED);
+                $status->setReason('err_wrong_login');
+                return $status;
+            }
+            $sync->setExternalAccount($ext_account);
             $sync->setInternalAccount($int_account);
             $sync->updateUser();
 
