059b138f51e01660e3586457fe194079c1c634b7
lauraquellmalz <quellmalz.laura@gmail.com>
2020-03-17 17:47:27 +0100

    Implemented global reporting tool

--- CI/Import/Variables.sh
+++ CI/Import/Variables.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# CS fixer (Todo for later)
-TMP_DIR="/tmp"
-PHP_CS_FIXER_RESULTS_PATH="$TMP_DIR/phpfix_results"
-PHP_CS_FIXER="libs/composer/vendor/friendsofphp/php-cs-fixer/php-cs-fixer"
-
 # Setup the results data directories
 RESULTS_DATA_DIRECTORY="CI/results/data"
 
@@ -15,6 +10,10 @@ RESULTS_DATA_DIRECTORY_PHPUNIT_RESULTS_PATH="/tmp/phpunit_results"
 RESULTS_DATA_DIRECTORY_PHPUNIT_TMP="$RESULTS_DATA_DIRECTORY/phpunit_changed.csv"
 
 RESULTS_DATA_DIRECTORY_DICTO="$RESULTS_DATA_DIRECTORY/dicto_latest.csv"
+
+# CS fixer (Todo for later)
+PHP_CS_FIXER_RESULTS_PATH="$RESULTS_DATA_DIRECTORY/phpfix_results"
+PHP_CS_FIXER="libs/composer/vendor/friendsofphp/php-cs-fixer/php-cs-fixer"
 
 TRAVIS_RESULTS_DIRECTORY="/tmp/CI-Results"
 
