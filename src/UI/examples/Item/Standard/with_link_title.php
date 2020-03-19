f1bd0d09d66e3957aa50aa26fb79b92ceab6aabd
Nils Haagen <nhaagen@cat06.de>
2020-03-19 08:49:38 +0100

    Update src/UI/examples/Item/Standard/with_link_title.php
    I guess, this is fair enough ;)
    
    Co-Authored-By: Richard Klees <richard.klees@concepts-and-training.de>

--- src/UI/examples/Item/Standard/with_link_title.php
+++ src/UI/examples/Item/Standard/with_link_title.php
@@ -9,7 +9,7 @@ function with_link_title()
     $renderer = $DIC->ui()->renderer();
     $app_item = $f->item()->standard($f->link()->standard("ILIAS", "https://www.ilias.de"))
         ->withProperties(array(
-            "Code Repo" => $f->button()->shy("GitHub", "https://www.github.com"),
+            "Code Repo" => $f->button()->shy("ILIAS on GitHub", "https://www.github.com/ILIAS-eLearning/ILIAS"),
             "Location" => "Room 123, Main Street 44, 3012 Bern"))
         ->withDescription("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.");
     return $renderer->render($app_item);
