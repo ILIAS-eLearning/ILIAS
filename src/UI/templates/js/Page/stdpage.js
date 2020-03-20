6b585579bcac95e88d13adfe536e05d433e18d6c
Nils Haagen <nhaagen@cat06.de>
2020-03-20 10:21:13 +0100

    27281, #27282 - resize dropdown/datepicker after opening when there is no space

--- src/UI/templates/js/Page/stdpage.js
+++ src/UI/templates/js/Page/stdpage.js
@@ -4,6 +4,7 @@ il.UI = il.UI || {};
 	ui.page = (function($) {
 
 		var breakpoint_max_width = 768, //this corresponds to @grid-float-breakpoint-max, see mainbar.less/metabar.less
+			resized_poppers_margin = 25, //dropdown, date-picker
 			mq_orientation = window.matchMedia("(orientation: portrait)");
 
 		var isSmallScreen = function() {
@@ -22,11 +23,31 @@ il.UI = il.UI || {};
 			return isPortrait() ? 'portrait' : 'landscape';
 		};
 
+		var fitContainerToContent = function(target_container) {
+			var content_container = $('.il-layout-page-content'),
+				margin = resized_poppers_margin,
+				max_width = content_container.width() - 2 * margin,
+				target_left = content_container.offset().left - target_container.parent().offset().left + margin;
+
+			if( target_container.width() < max_width &&
+				target_container.offset().left > content_container.offset().left
+			) {
+				return;
+			}
+			window.setTimeout(function(){
+				target_container.css({
+					'left': target_left,
+					'max-width': max_width
+				});
+			}, 100)
+		}
+
 		return {
 			isSmallScreen: isSmallScreen,
 			getOrientation: getOrientation,
 			isPortrait: isPortrait,
 			isLandscape: isLandscape,
+			fit: fitContainerToContent
 		};
 
 	})($);
