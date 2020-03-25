39fd553a45c4545eff9e5a57f0843fa1afa3860a
smeyer <smeyer.ilias@gmx.de>
2020-03-25 13:54:56 +0100

    Fixed count() warning

--- Services/Calendar/classes/ConsultationHours/class.ilConsultationHoursGUI.php
+++ Services/Calendar/classes/ConsultationHours/class.ilConsultationHoursGUI.php
@@ -170,7 +170,7 @@ class ilConsultationHoursGUI
         
         $_SESSION['ch_apps'] = $_REQUEST['apps'];
         
-        if (!count($_SESSION['ch_apps'])) {
+        if (empty($_SESSION['ch_apps'])) {
             ilUtil::sendFailure($this->lng->txt('select_one'), true);
             $GLOBALS['DIC']['ilCtrl']->redirect($this, 'appointmentList');
         }
@@ -606,8 +606,8 @@ class ilConsultationHoursGUI
                     '',
                     true,
                     true
-                        ) . ', ' . ilDatePresentation::formatDate($entry->getStart())
-                );
+                ) . ', ' . ilDatePresentation::formatDate($entry->getStart())
+            );
         }
         $tpl->setContent($confirm->getHTML());
     }
