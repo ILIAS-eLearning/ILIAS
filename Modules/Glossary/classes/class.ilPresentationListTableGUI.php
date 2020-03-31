efa1642097be38a54e148ceb8db436dcd460e653
Alexander Killing <killing@leifos.de>
2020-03-31 11:52:15 +0200

    fixed bug #27960: html tags are visible in Glossary overview

--- Modules/Glossary/classes/class.ilPresentationListTableGUI.php
+++ Modules/Glossary/classes/class.ilPresentationListTableGUI.php
@@ -35,6 +35,10 @@ class ilPresentationListTableGUI extends ilTable2GUI
      */
     protected $ctrl;
 
+    /**
+     * @var ilPageConfig
+     */
+    protected $page_config;
 
     /**
      * Constructor
@@ -57,7 +61,10 @@ class ilPresentationListTableGUI extends ilTable2GUI
         $this->tax_node = $a_tax_node;
         $this->tax_id = $a_tax_id;
         $this->setId("glopr" . $this->glossary->getId());
-        
+
+        $gdf = new ilGlossaryDefPage();
+        $this->page_config = $gdf->getPageConfig();
+
         include_once("./Modules/Glossary/classes/class.ilGlossaryAdvMetaDataAdapter.php");
         $adv_ad = new ilGlossaryAdvMetaDataAdapter($this->glossary->getRefId());
         $this->adv_fields = $adv_ad->getAllFields();
@@ -227,6 +234,11 @@ class ilPresentationListTableGUI extends ilTable2GUI
                         $short_str = $def_obj->getShortText();
                     } else {
                         $short_str = $def["short_text"];
+                    }
+                    
+                    if (!$this->page_config->getPreventHTMLUnmasking()) {
+                        $short_str = str_replace("&lt;", "<", $short_str);
+                        $short_str = str_replace("&gt;", ">", $short_str);
                     }
 
                     // replace tex
