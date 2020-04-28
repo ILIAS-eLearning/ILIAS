eff471a21b6f5f799e8e717923098e6fba0df565
Timon Amstutz <timon.amstutz@bluewin.ch>
2020-04-28 09:33:25 +0200

    Session Access: Do not load all participants to check access of only one

--- Modules/Session/classes/class.ilObjSessionAccess.php
+++ Modules/Session/classes/class.ilObjSessionAccess.php
@@ -86,9 +86,7 @@ class ilObjSessionAccess extends ilObjectAccess
         if (!$a_user_id) {
             $a_user_id = $ilUser->getId();
         }
-        include_once './Modules/Session/classes/class.ilSessionParticipants.php';
-        $part = ilSessionParticipants::getInstance($a_ref_id);
-        
+
         switch ($a_cmd) {
             case 'register':
                 
@@ -98,10 +96,12 @@ class ilObjSessionAccess extends ilObjectAccess
                 if ($ilUser->isAnonymous()) {
                     return false;
                 }
-                if ($part->isAssigned($a_user_id)) {
+                
+                include_once './Modules/Session/classes/class.ilSessionParticipants.php';
+                if(ilSessionParticipants::_isParticipant($a_ref_id,$a_user_id)) {
                     return false;
                 }
-                if ($part->isSubscriber($a_user_id)) {
+                if (ilSessionParticipants::_isSubscriber($a_obj_id,$a_user_id)) {
                     return false;
                 }
                 include_once './Modules/Session/classes/class.ilSessionWaitingList.php';
