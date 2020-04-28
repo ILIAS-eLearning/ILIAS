0d9d9895da90402aed9ec720f15ad3d9855ab201
Maximilian Becker <mbecker@databay.de>
2020-04-28 09:56:24 +0200

    0026361: PROBLEMS IN THE EXECUTION OF A QUESTION TYPE TEST

--- Services/Math/test/ilMathBaseAdapterTest.php
+++ Services/Math/test/ilMathBaseAdapterTest.php
@@ -150,7 +150,8 @@ abstract class ilMathBaseAdapterTest extends PHPUnit_Framework_TestCase
     public function mulData()
     {
         return [
-            ['1', '2', '2', self::DEFAULT_SCALE]
+            ['1', '2', '2', self::DEFAULT_SCALE],
+            ['1', '', '0', self::DEFAULT_SCALE]
         ];
     }
 
