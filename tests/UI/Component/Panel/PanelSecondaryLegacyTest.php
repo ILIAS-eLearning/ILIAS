d89f92d2e4f64c022efdb32ca0cb2d5fdf98f78f
nmatuschek <nmatuschek@databay.de>
2020-05-05 09:40:37 +0200

    Apply headline schema

--- tests/UI/Component/Panel/PanelSecondaryLegacyTest.php
+++ tests/UI/Component/Panel/PanelSecondaryLegacyTest.php
@@ -172,7 +172,7 @@ class PanelSecondaryLegacyTest extends ILIAS_UI_TestBase
         $expected_html = <<<EOT
 <div class="panel panel-secondary panel-flex">
 	<div class="panel-heading ilHeader clearfix">
-		<h3 class="ilHeader">Title</h3>
+		<h4 class="ilHeader">Title</h4>
 		<div class="dropdown"><button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown"  aria-label="actions" aria-haspopup="true" aria-expanded="false"> <span class="caret"></span></button>
 			<ul class="dropdown-menu">
 				<li><button class="btn btn-link" data-action="https://www.ilias.de" id="id_1">ILIAS</button></li>
@@ -207,7 +207,7 @@ EOT;
         $expected_html = <<<EOT
 <div class="panel panel-secondary panel-flex">
 	<div class="panel-heading ilHeader clearfix">
-		<h3 class="ilHeader">Title</h3>
+		<h4 class="ilHeader">Title</h4>
 		<div class="il-viewcontrol-sortation" id="">
 			<div class="dropdown">
 				<button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" aria-label="actions" aria-haspopup="true" aria-expanded="false">
@@ -249,7 +249,7 @@ EOT;
         $expected_html = <<<EOT
 <div class="panel panel-secondary panel-flex">
 	<div class="panel-heading ilHeader clearfix">
-		<h3 class="ilHeader">Title</h3>
+		<h4 class="ilHeader">Title</h4>
 		<div class="il-viewcontrol-pagination">
 			<span class="browse previous">
 				<a class="glyph" href="http://ilias.de?page=0" aria-label="back">
@@ -295,7 +295,7 @@ EOT;
         $expected_html = <<<EOT
 <div class="panel panel-secondary panel-flex">
 	<div class="panel-heading ilHeader clearfix">
-		<h3 class="ilHeader">Title</h3>
+		<h4 class="ilHeader">Title</h4>
 		<div class="il-viewcontrol-section">
 			<a class="btn btn-default " href="http://www.ilias.de" aria-label="previous" data-action="http://www.ilias.de">
 				<span class="glyphicon glyphicon-chevron-left"></span>
@@ -332,7 +332,7 @@ EOT;
         $expected_html = <<<EOT
 <div class="panel panel-secondary panel-flex">
 	<div class="panel-heading ilHeader clearfix">
-		<h3 class="ilHeader">Title</h3>
+		<h4 class="ilHeader">Title</h4>
 	</div>
 	<div class="panel-body">
 		Legacy content
