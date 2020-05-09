68abe108a15d786cd886c8c0bb647791452bb519
Uwe Kohnle <kohnle@internetlehrer-gmbh.de>
2020-05-09 22:00:35 +0000

    LTIconsumer show info for lp only if lp is activated

--- Modules/LTIConsumer/classes/class.ilObjLTIConsumerGUI.php
+++ Modules/LTIConsumer/classes/class.ilObjLTIConsumerGUI.php
@@ -695,7 +695,8 @@ class ilObjLTIConsumerGUI extends ilObject2GUI
             $info->addProperty('Current User ID', $DIC->user()->getId());
         }
 
-        if ($this->object->getProvider()->getHasOutcome()) {
+		require_once('Services/Tracking/classes/class.ilLPObjSettings.php');
+        if ($this->object->getProvider()->getHasOutcome() && ilLPObjSettings::_lookupDBMode($this->object->getId()) != 0) {
             $info->addSection($DIC->language()->txt("lti_info_learning_progress_section"));
             $info->addProperty(
                 $DIC->language()->txt("mastery_score"),
@@ -742,7 +743,9 @@ class ilObjLTIConsumerGUI extends ilObject2GUI
     {
         global $DIC; /* @var \ILIAS\DI\Container $DIC */
         
-        if ($this->object->getOfflineStatus() || $this->object->isLaunchMethodEmbedded()) {
+        if ($this->object->getOfflineStatus() ||
+            $this->object->isLaunchMethodEmbedded() ||
+            $this->object->getProvider()->getAvailability() == ilLTIConsumeProvider::AVAILABILITY_NONE) {
             return;
         }
         
@@ -803,8 +806,9 @@ class ilObjLTIConsumerGUI extends ilObject2GUI
     protected function handleAvailablityMessage()
     {
         global $DIC; /* @var \ILIAS\DI\Container $DIC */
-        
-        if ($this->object->getProvider()->getAvailability() == ilLTIConsumeProvider::AVAILABILITY_NONE) {
+        if ($this->object->getProvider()->getProviderUrl() == '') {
+            ilUtil::sendFailure($DIC->language()->txt('lti_provider_not_set_msg'));
+        } elseif ($this->object->getProvider()->getAvailability() == ilLTIConsumeProvider::AVAILABILITY_NONE) {
             ilUtil::sendFailure($DIC->language()->txt('lti_provider_not_avail_msg'));
         }
     }
