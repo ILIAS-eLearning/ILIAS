6b558446c87202bbe812873bc0132251b45bd93a
daniwe4 <daniel.weise@concepts-and-training.de>
2020-05-13 15:08:57 +0200

    Filesystem: implemented isApplicable over base class

--- Services/FileSystem/classes/Setup/class.ilFileSystemComponentDataDirectoryCreatedObjective.php
+++ Services/FileSystem/classes/Setup/class.ilFileSystemComponentDataDirectoryCreatedObjective.php
@@ -70,4 +70,13 @@ class ilFileSystemComponentDataDirectoryCreatedObjective extends Setup\Objective
         $this->path = $this->buildPath($environment);
         return parent::achieve($environment);
     }
+
+    /**
+     * @inheritDoc
+     */
+    public function isApplicable(Setup\Environment $environment) : bool
+    {
+        $this->path = $this->buildPath($environment);
+        return parent::isApplicable($environment);
+    }
 }
