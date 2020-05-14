6f7d54725c7040b1d503eea92b0c75b47abd649c
Theodor Truffer <tt@studer-raimann.ch>
2020-05-14 13:54:16 +0200

    0025451: text_selection Multiselect: Sortation isn't working well

--- Modules/DataCollection/classes/Fields/Selection/class.ilDclSelectionFieldModel.php
+++ Modules/DataCollection/classes/Fields/Selection/class.ilDclSelectionFieldModel.php
@@ -244,6 +244,10 @@ abstract class ilDclSelectionFieldModel extends ilDclBaseFieldModel
         global $DIC;
         $ilDB = $DIC['ilDB'];
 
+        if ($this->isMulti()) {
+            return null;
+        }
+
         $sql_obj = new ilDclRecordQueryObject();
 
         $select_str = "sel_opts_{$this->getId()}.value AS field_{$this->getId()}";
