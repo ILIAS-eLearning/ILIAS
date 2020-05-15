067b4e3167e4ccbb5eb5cac4c20e48a112b8e08d
jssmo <jsmmo@jsmmo.jsmmo>
2020-05-15 12:48:26 +0200

    Checks for existence of $tree in container in the initialisation

--- Services/Init/classes/class.ilInitialisation.php
+++ Services/Init/classes/class.ilInitialisation.php
@@ -878,7 +878,7 @@ class ilInitialisation
         } else {
             self::initGlobal('lng', ilLanguage::getFallbackInstance());
         }
-        if (is_object($rbacsystem)) {
+        if (is_object($rbacsystem) && $DIC->offsetExists('tree')) {
             $rbacsystem->initMemberView();
         }
     }
