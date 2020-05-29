ca6d9550c86afbab57ba9e6f85027e91c7dc1fa2
Alexander Killing <killing@leifos.de>
2020-05-29 19:11:36 +0200

    bundling basics

---
+++ docs/development/js/bundling-example/component-module.js
@@ -0,0 +1,8 @@
+/* global $, il */
+import sub1 from "./component-submodule1"
+import sub2 from "./component-submodule2"
+
+export default {
+  sub1: sub1,
+  sub2: sub2
+};
---
+++ docs/development/js/bundling-example/component-submodule1.js
@@ -0,0 +1,7 @@
+export default {
+
+  public1: function() {
+    console.log("Submodule 1, public1()");
+    $("body").append("<div>Submodule 1, public1()</div>");
+  }
+}
