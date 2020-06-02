f5ceaf3590390aae2e1bb0ac3c10a5a8696563af
Theodor Truffer <tt@studer-raimann.ch>
2020-06-02 09:43:50 +0200

    0025820: falsified values of textfields in export when content is syntactical numeric (2)

--- Modules/DataCollection/classes/Fields/TextSelection/class.ilDclTextSelectionRecordFieldModel.php
+++ Modules/DataCollection/classes/Fields/TextSelection/class.ilDclTextSelectionRecordFieldModel.php
@@ -1,6 +1,8 @@
 <?php
 /* Copyright (c) 1998-2009 ILIAS open source, Extended GPL, see docs/LICENSE */
 
+use PhpOffice\PhpSpreadsheet\Cell\DataType;
+
 /**
  * Class ilDclTextSelectionRecordFieldModel
  *
@@ -10,4 +12,17 @@ class ilDclTextSelectionRecordFieldModel extends ilDclSelectionRecordFieldModel
 {
     const PROP_SELECTION_TYPE = 'text_selection_type';
     const PROP_SELECTION_OPTIONS = 'text_selection_options';
+
+    /**
+     * @param ilExcel $worksheet
+     * @param         $row
+     * @param         $col
+     * @throws \PhpOffice\PhpSpreadsheet\Exception
+     */
+    public function fillExcelExport(ilExcel $worksheet, &$row, &$col)
+    {
+        $worksheet->setCell($row, $col, $this->getExportValue(), DataType::TYPE_STRING);
+        $col++;
+    }
+
 }
