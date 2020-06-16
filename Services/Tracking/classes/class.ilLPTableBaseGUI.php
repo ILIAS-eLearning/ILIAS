a9c020aeb1f00da57a14b820f5d525b3a860787e
Marvin Barz <barz@leifos.de>
2020-06-16 15:03:43 +0200

    Added usage intensity filters and usability for repository and category

--- Services/Tracking/classes/class.ilLPTableBaseGUI.php
+++ Services/Tracking/classes/class.ilLPTableBaseGUI.php
@@ -403,6 +403,8 @@ class ilLPTableBaseGUI extends ilTable2GUI
         $options['lso'] = $lng->txt('objs_lso');
 
         if ($a_allow_undefined_lp) {
+            $options['root'] = $lng->txt('obj_reps');
+            $options['cat'] = $lng->txt('objs_cat');
             $options["webr"] = $lng->txt("objs_webr");
             $options["wiki"] = $lng->txt("objs_wiki");
             $options["blog"] = $lng->txt("objs_blog");
