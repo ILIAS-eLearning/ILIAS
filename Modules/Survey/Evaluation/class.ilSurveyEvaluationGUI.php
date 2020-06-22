aa4e0e6572e7229a0ef1b17848c043a282e0f6ec
Alexander Killing <killing@leifos.de>
2020-06-22 12:06:31 +0200

    fixed #28507: Results (Details, Per Participant, Export) are not accessible, Error-Code

--- Modules/Survey/Evaluation/class.ilSurveyEvaluationGUI.php
+++ Modules/Survey/Evaluation/class.ilSurveyEvaluationGUI.php
@@ -972,6 +972,12 @@ class ilSurveyEvaluationGUI
             $question_res = $question_res[0][1];
             $matrix = true;
         }
+
+        // see #28507 (matrix question without a row)
+        if (!is_object($question_res)) {
+            return;
+        }
+
         $question = $question_res->getQuestion();
 
         // question "overview"
@@ -1468,7 +1474,12 @@ class ilSurveyEvaluationGUI
         foreach ($this->object->getSurveyQuestions() as $qdata) {
             $q_eval = SurveyQuestion::_instanciateQuestionEvaluation($qdata["question_id"], $a_finished_ids);
             $q_res = $q_eval->getResults();
-                        
+
+            // see #28507 (matrix question without a row)
+            if (is_array($q_res) && !is_object($q_res[0][1])) {
+                continue;
+            }
+
             $question = is_array($q_res)
                 ? $q_res[0][1]->getQuestion()
                 : $q_res->getQuestion();
