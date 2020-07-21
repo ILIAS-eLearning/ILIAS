4183335d28bd2466d397efcd77828a19177e40fb
mjansen <mjansen@databay.de>
2020-07-21 11:24:51 +0200

    UI/Footer: Add interface changes to support shy buttons with modals

--- src/UI/examples/Modal/RoundTrip/show_a_modal_which_cannot_be_closed_with_the_keyboard.php
+++ src/UI/examples/Modal/RoundTrip/show_a_modal_which_cannot_be_closed_with_the_keyboard.php
@@ -7,7 +7,7 @@ function show_a_modal_which_cannot_be_closed_with_the_keyboard()
 
     $modal = $factory->modal()->roundtrip('My Modal 1', $factory->legacy('You cannot close this modal with the ESC key'));
     $modal = $modal->withCloseWithKeyboard(false);
-    $button1 = $factory->button()->standard('Open Modal 1', '#')
+    $button1 = $factory->button()->shy('Open Modal 1', '#')
         ->withOnClick($modal->getShowSignal());
 
     return $renderer->render([$button1, $modal]);
