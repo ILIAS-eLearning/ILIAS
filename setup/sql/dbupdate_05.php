810a9c63adf0afaf195059af4a5e1c1d06085ff3
Thomas Famula <famula@leifos.de>
2020-07-22 15:18:47 +0200

    Manual Sorting for Competences in Competence Profiles - dbupdate

--- setup/sql/dbupdate_05.php
+++ setup/sql/dbupdate_05.php
@@ -4396,3 +4396,40 @@ $ilCtrlStructureReader->getStructure();
 $mcdir = CLIENT_WEB_DIR."/magpie_cache";
 ilUtil::delDir($mcdir);
 ?>
+<#5675>
+<?php
+if (!$ilDB->tableColumnExists('skl_profile_level', 'order_nr'))
+{
+    $ilDB->addTableColumn('skl_profile_level', 'order_nr', array(
+        "type" => "integer",
+        "notnull" => true,
+        "default" => 0,
+        "length" => 4
+    ));
+}
+?>
+<#5676>
+<?php
+if ($ilDB->tableExists('skl_profile_level')) {
+    $profiles = ilSkillProfile::getProfiles();
+    if (!empty($profiles)) {
+        foreach ($profiles as $id => $profile) {
+            $set = $ilDB->query(
+                "SELECT profile_id, base_skill_id, tref_id, order_nr FROM skl_profile_level WHERE " .
+                " profile_id = " . $ilDB->quote($id, "integer")
+            );
+            $cnt = 1;
+            while ($rec = $ilDB->fetchAssoc($set)) {
+                $ilDB->manipulate(
+                    "UPDATE skl_profile_level SET " .
+                    " order_nr = " . $ilDB->quote(($cnt * 10), "integer") .
+                    " WHERE profile_id = " . $ilDB->quote($rec["profile_id"], "integer") .
+                    " AND base_skill_id = " . $ilDB->quote($rec["base_skill_id"], "integer") .
+                    " AND tref_id = " . $ilDB->quote($rec["tref_id"], "integer")
+                );
+                $cnt++;
+            }
+        }
+    }
+}
+?>
