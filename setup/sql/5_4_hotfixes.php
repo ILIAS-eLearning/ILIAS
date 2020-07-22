a9b1157a3946e402de08c44736edf6e797ae98e5
Alexander Killing <killing@leifos.de>
2020-07-22 10:33:30 +0200

    fixed bug #28622

--- setup/sql/5_4_hotfixes.php
+++ setup/sql/5_4_hotfixes.php
@@ -1354,3 +1354,9 @@ $ilDB->modifyTableColumn(
 <?php
 $ilCtrlStructureReader->getStructure();
 ?>
+<#93>
+<?php
+    // remove magpie cache dir
+    $mcdir = CLIENT_WEB_DIR."/magpie_cache";
+    ilUtil::delDir($mcdir);
+?>
