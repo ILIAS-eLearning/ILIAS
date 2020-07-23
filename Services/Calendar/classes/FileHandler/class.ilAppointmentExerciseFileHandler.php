a947b1a82417957816eb9b60683c7ecb8ec8ac28
Alexander Killing <killing@leifos.de>
2020-07-23 11:28:49 +0200

    fixed #28513: Übung - Angehängte Datei auch außerhalb des Terminplans aufrufbar

--- Services/Calendar/classes/FileHandler/class.ilAppointmentExerciseFileHandler.php
+++ Services/Calendar/classes/FileHandler/class.ilAppointmentExerciseFileHandler.php
@@ -21,11 +21,16 @@ class ilAppointmentExerciseFileHandler extends ilAppointmentBaseFileHandler impl
      */
     public function getFiles()
     {
+        global $DIC;
+
+        $user_id = $DIC->user()->getId();
+
         $ass_id = $this->appointment['event']->getContextId() / 10;			// see ilExAssignment->handleCalendarEntries $dl parameter
         $assignment = new ilExAssignment($ass_id);
         $ass_files = $assignment->getFiles();
         $files = array();
-        if (count($ass_files)) {
+        $state = ilExcAssMemberState::getInstanceByIds($assignment->getId(), $user_id);
+        if (count($ass_files) && $state->areInstructionsVisible()) {
             foreach ($ass_files as $ass_file) {
                 $files[] = $ass_file['fullpath'];
             }
