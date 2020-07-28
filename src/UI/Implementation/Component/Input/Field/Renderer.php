26c306b0b042f59d195c29ae9322f6c8373630f7
Fabian Schmid <fs@studer-raimann.ch>
2020-07-28 11:00:49 +0200

    [FIX] 0026683: File Input accepts wrong data types

--- src/UI/Implementation/Component/Input/Field/Renderer.php
+++ src/UI/Implementation/Component/Input/Field/Renderer.php
@@ -884,6 +884,7 @@ JS;
         $settings->accepted_files = implode(',', $component->getAcceptedMimeTypes());
         $settings->existing_file_ids = $input->getValue();
         $settings->existing_files = $component->getUploadHandler()->getInfoForExistingFiles($input->getValue() ?? []);
+        $settings->dictInvalidFileType = $this->txt('form_msg_file_wrong_file_type');
 
         $input = $component->withAdditionalOnLoadCode(
             function ($id) use ($settings) {
