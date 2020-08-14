3ed835d8dc0620a833159e14e4bdeaf6d50a3e5e
Fabian Wolf <wolf@ilias.de>
2020-08-14 15:25:05 +0200

    Release 6.2

--- include/inc.ilias_version.php
+++ include/inc.ilias_version.php
@@ -9,5 +9,5 @@
 *
 * @package ilias-core
 */
-define("ILIAS_VERSION", "6.1 2020-07-10");
-define("ILIAS_VERSION_NUMERIC", "6.1");			// since version ILIAS 6 this must be always x.y: x and y are numbers
+define("ILIAS_VERSION", "6.2 2020-08-14");
+define("ILIAS_VERSION_NUMERIC", "6.2");			// since version ILIAS 6 this must be always x.y: x and y are numbers
