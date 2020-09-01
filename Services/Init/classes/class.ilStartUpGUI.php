d024835392940951787511888b3bd7f8ad7aadaa
smeyer <smeyer.ilias@gmx.de>
2020-09-01 14:08:54 +0200

    0028306: Login via OpenID Connect: Missing translation to German (coded)

--- Services/Init/classes/class.ilStartUpGUI.php
+++ Services/Init/classes/class.ilStartUpGUI.php
@@ -2201,9 +2201,14 @@ class ilStartUpGUI
     {
         global $DIC;
 
+        $lang = $DIC->language();
+
         $oidc_settings = ilOpenIdConnectSettings::getInstance();
         if ($oidc_settings->getActive()) {
             $tpl = new ilTemplate('tpl.login_element.html', true, true, 'Services/OpenIdConnect');
+
+            $lang->loadLanguageModule('auth');
+            $tpl->setVariable('TXT_OIDCONNECT_HEADER', $lang->txt('auth_oidc_login_element_info'));
 
             $target = empty($_GET['target']) ? '' : ('?target=' . (string) $_GET['target']);
             switch ($oidc_settings->getLoginElementType()) {
