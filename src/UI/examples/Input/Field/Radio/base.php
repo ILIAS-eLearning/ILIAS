716fb752fc44189ea839b4f3bfa955c24a7760bd
Nils Haagen <nhaagen@cat06.de>
2020-09-14 08:22:14 +0200

    UI/Input/Radio - numeric values, fix #28862

--- src/UI/examples/Input/Field/Radio/base.php
+++ src/UI/examples/Input/Field/Radio/base.php
@@ -4,36 +4,22 @@
  */
 function base()
 {
-    //Step 0: Declare dependencies
+    //Step 1: Declare dependencies
     global $DIC;
     $ui = $DIC->ui()->factory();
     $renderer = $DIC->ui()->renderer();
     $request = $DIC->http()->request();
 
-
-    //Step 1: define dependant fields
-    $dependant_field1 = $ui->input()->field()->text("Item 1", "Just some dependent group field");
-    $dependant_field2 = $ui->input()->field()->text("Item 2", "Just some dependent group field");
-    $dependant_fields = [
-        "dependant_field_a" => $dependant_field1,
-        "dependant_field_b" => $dependant_field2
-    ];
-
-    $radio_d = $ui->input()->field()->radio("Dep. Radio", "check an option")
-        ->withOption('value1', 'label1', 'byline1')
-        ->withOption('value2', 'label2', 'byline2', $dependant_fields);
-
-    //Step 2: define the radio
+    //Step 2: define the radio with options
     $radio = $ui->input()->field()->radio("Radio", "check an option")
         ->withOption('value1', 'label1', 'byline1')
-        ->withOption('value2', 'label2', 'byline2', $dependant_fields)
-        ->withOption('value3', 'label3', 'byline3', [$radio_d]);
+        ->withOption('10', 'numeric value (ten)', 'byline2')
+        ->withOption('030', 'not-numeric value', 'byline3');
 
     //Step 3: define form and form actions
     $form = $ui->input()->container()->form()->standard('#', ['radio' => $radio]);
 
-    //Step 4: implement some form data processing. Note, the value of the checkbox will
-    // be 'checked' if checked an null if unchecked.
+    //Step 4: implement some form data processing.
     if ($request->getMethod() == "POST") {
         $form = $form->withRequest($request);
         $result = $form->getData();
