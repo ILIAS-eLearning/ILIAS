effb0b12b2cfe3dda00afabd1730a18885a658e2
Nils Haagen <nhaagen@cat06.de>
2020-09-16 13:57:10 +0200

    LSO, AdminGUI: added autoload-files

--- libs/composer/vendor/composer/autoload_real.php
+++ libs/composer/vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInit2fffdf922cf8fdbf1f62eec345993c83
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
