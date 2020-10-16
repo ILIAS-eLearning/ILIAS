3b6f20a9db8359a598c74c4eabcb96ec6a88acdf
iszmais <iszmais@databay.de>
2020-10-16 15:47:00 +0200

    Added own menu view for tag lists

---
+++ Services/Tagging/classes/class.ilTaggingSlateContentMenuGUI.php
@@ -0,0 +1,58 @@
+<?php
+
+/* Copyright (c) 1998-2019 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+use ILIAS\Data\URI;
+
+/**
+ * Tagging slate UI
+ *
+ * @author killing@leifos.de
+ */
+class ilTaggingSlateContentMenuGUI extends ilTaggingSlateContentGUI
+{
+    /**
+     * show resources
+     *
+     * @param
+     * @return string
+     */
+    protected function showResourcesForTag()
+    {
+        // back button
+        $tag_cmd = $this->ctrl->getLinkTarget($this, "showTagCloud", "", true, false);
+        $items[] = $this->ui->factory()->button()->bulky(
+            $this->ui->factory()->symbol()->glyph()->back(),
+            $this->lng->txt("back"), "#")->withOnLoadCode(function ($id) use ($tag_cmd) {
+            return "$(\"#$id\").click(function() { 
+                il.Util.ajaxReplaceInner('$tag_cmd', 'il-tag-slate-container'); return false;
+            });";
+        });
+
+        $items[] = $this->ui->factory()->item()->standard(sprintf(
+            $this->lng->txt("tagging_resources_for_tag"),
+            "<i>" . $_GET["tag"] . "</i>"
+        ));
+
+        // resource list
+        include_once("./Services/Tagging/classes/class.ilTagging.php");
+
+        foreach (ilTagging::getObjectsForTagAndUser($this->user->getId(), $_GET["tag"]) as $key => $obj) {
+            $ref_ids = ilObject::_getAllReferences($obj["obj_id"]);
+            foreach ($ref_ids as $ref_id) {
+                if ($obj["obj_type"] != "" && ($this->access->checkAccess("visible", "", $ref_id) ||
+                        $this->access->checkAccess("read", "", $ref_id))) {
+                    $title = ilObject::_lookupTitle($obj["obj_id"]);
+                    $items[] = $this->ui->factory()->link()->bulky(
+                        $this->ui->factory()->symbol()->icon()->custom(ilObject::_getIcon($obj["obj_id"]), $title),
+                        $title,
+                        new URI(ilLink::_getLink($ref_id))
+                    );
+                }
+            }
+        }
+
+        echo $this->ui->renderer()->renderAsync($items);
+        exit;
+    }
+}
