5a56f73e59a5fa0fa62a85e8f0c7f5813100a185
Uwe Kohnle <kohnle@internetlehrer-gmbh.de>
2020-10-25 11:37:47 +0000

    modified xapi proxy for debug options

--- Modules/CmiXapi/xapiproxy.php
+++ Modules/CmiXapi/xapiproxy.php
@@ -9,6 +9,26 @@ use \GuzzleHttp\RequestOptions;
 use \GuzzleHttp\Psr7\Uri;
 use \Zend\HttpHandlerRunner\Emitter\SapiEmitter;
 
+// ToDo: json_decode(obj,true) as assoc array might be faster?
+$specificAllowedStatements = NULL;
+/*
+$specificAllowedStatements = array(
+    "http://adlnet.gov/expapi/verbs/completed",
+    "http://adlnet.gov/expapi/verbs/passed",
+    "http://adlnet.gov/expapi/verbs/initialized",
+    "http://adlnet.gov/expapi/verbs/terminated",
+    "http://adlnet.gov/expapi/verbs/launched"
+);
+*/
+$replacedValues = NULL;
+/*
+$replacedValues = array(
+  'timestamp' => '1970-01-01T00:00:00.000Z',
+  'result.duration' => 'PT00.000S'
+);
+*/
+$blockSubStatements = false;
+
 // check options requests
 if (strtoupper($_SERVER["REQUEST_METHOD"]) == "OPTIONS") {
     header('HTTP/1.1 204 No Content');
@@ -27,7 +47,7 @@ if (!empty($_SERVER['PHP_AUTH_USER']) && !empty($_SERVER['PHP_AUTH_PW'])) {
     $client = $basicAuth[0];
     $token = $basicAuth[1];
 } else {
-    //$log->info("no credentials:\nREQUEST_METHOD=".$_SERVER["REQUEST_METHOD"]."\nPHP_AUTH_USER=".$_SERVER['PHP_AUTH_USER']."\nPHP_AUTH_PW=".$_SERVER['PHP_AUTH_PW']);
+    //$log->debug("no credentials:\nREQUEST_METHOD=".$_SERVER["REQUEST_METHOD"]."\nPHP_AUTH_USER=".$_SERVER['PHP_AUTH_USER']."\nPHP_AUTH_PW=".$_SERVER['PHP_AUTH_PW']);
     header('HTTP/1.1 401 Authorization Required');
     exit;
 }
@@ -42,7 +62,7 @@ $log = ilLoggerFactory::getLogger('cmix');
 try {
     $authToken = ilCmiXapiAuthToken::getInstanceByToken($token);
     $lrsType = new ilCmiXapiLrsType($authToken->getLrsTypeId());
-    
+
     if (!$lrsType->isAvailable()) {
         throw new ilCmiXapiException(
             'lrs endpoint (id=' . $authToken->getLrsTypeId() . ') unavailable (responded 401-unauthorized)'
@@ -72,10 +92,17 @@ function handleRequest(ServerRequestInterface $request)
     global $log;
     $method = strtolower($request->getMethod());
     $log->debug("Request Method: " . $method);
+    $path = $request->getUri()->getPath();
     switch ($method) {
         case "post":
         case "put":
-            handlePostPutRequest($request);
+            if (preg_match('/\/statements$/',$path)) {
+                $log->debug("handle Post-Put statements " . $path);
+                handlePostPutRequest($request);
+            }
+            else {
+                handleProxy($request);
+            }
             break;
         default:
             handleProxy($request);
@@ -92,29 +119,138 @@ function handlePostPutRequest(ServerRequestInterface $request)
     if (empty($body)) {
         $log->warning("empty body in handlePostPutRequest");
         handleProxy($request);
-    } else {
+    }
+    else {
+        try {
+            $log->debug("handle statements");
+            $retarr = handleStatements($request, $body);
+            if (is_array($retarr)) {
+                //$log->debug("X: " . var_export($ret, TRUE));
+                $body = json_encode($retarr[0]); // new body with allowed statements
+                $fakePostBody = $retarr[1]; // fake post php array of ALL statments as if all statements were processed
+            }
+        }
+        catch(Exception $e) {
+            $log->error($e->getMessage());
+            exitProxyError();
+        }
         try {
             $body = modifyBody($body);
-            $log->debug($body);
-            $changes = array(
+            // $log->debug($body);
+            $changes = array (
                 "body" => $body
             );
             $req = \GuzzleHttp\Psr7\modify_request($request, $changes);
-            handleProxy($req);
-        } catch (Exception $e) { // ToDo: Errorhandling
-            $log->error($e->getMessage());
-            handleProxy($request);
+            handleProxy($req, $fakePostBody);
         }
+        catch(Exception $e) {
+            $log->error($e->getMessage());
+            handleProxy($request, $fakePostBody);
+        }
+    }
+}
+
+/**
+ * handle blocked request
+ * @param ServerRequestInterface $request
+ */
+
+function handleStatements(ServerRequestInterface $request, $body) {
+    global $log, $specificAllowedStatements, $blockSubStatements;
+    // everything is allowed
+    if (!is_array($specificAllowedStatements) && !$blockSubStatements) {
+        $log->debug("all statement are allowed");
+        return NULL;
+    }
+    $obj = json_decode($body, false);
+    // single statement object
+    if (is_object($obj) && isset($obj->verb)) {
+        $log->debug("json is object and statement");
+        $isSubStatement = isSubStatementCheck($obj);
+        $verb = $obj->verb->id;
+        if ($blockSubStatements && $isSubStatement) {
+            $log->debug("sub-statement is NOT allowed, fake response - " . $verb);
+            fakeResponseBlocked(NULL);
+        }
+		// $specificAllowedStatements
+		if (!is_array($specificAllowedStatements)) {
+			return NULL;
+		}
+        if (in_array($verb,$specificAllowedStatements)) {
+            $log->debug("statement is allowed, do nothing - " . $verb);
+            return NULL;
+        }
+        else {
+            $log->debug("statement is NOT allowed, fake response - " . $verb);
+            fakeResponseBlocked(NULL);
+        }
+    }
+    // array of statement objects
+    if (is_array($obj) && count($obj) > 0 && isset($obj[0]->verb)) {
+        $log->debug("json is array of statements");
+        $ret = array();
+        $up = array();
+        for ($i=0; $i<count($obj); $i++) {
+            array_push($ret,$obj[$i]->id); // push every statementid for fakePostResponse
+            $isSubStatement = isSubStatementCheck($obj[$i]);
+            $verb = $obj[$i]->verb->id;
+            if ($blockSubStatements && $isSubStatement) {
+                $log->debug("sub-statement is NOT allowed - " .$verb);
+            }
+            else {
+                if (!is_array($specificAllowedStatements) || (is_array($specificAllowedStatements) && in_array($verb,$specificAllowedStatements))) {
+                    $log->debug("statement is allowed - " . $verb);
+                    array_push($up,$obj[$i]);
+                }
+            }
+        }
+        if (count($up) === 0) { // nothing allowed
+            $log->debug("no allowed statements in array - fake response...");
+            fakeResponseBlocked($ret);
+        }
+        elseif (count($up) !== count($ret)) { // mixed request with allowed and not allowed statements
+            $log->debug("mixed with allowed and unallowed statements");
+            return array($up,$ret);
+        }
+        else {
+            // just return nothing
+            return NULL;
+        }
+    }
+}
+
+function fakeResponseBlocked($post=NULL) {
+    global $log;
+    $log->debug("fakeResponseFromBlockedRequest");
+    if ($post===NULL) {
+        $log->debug("post === NULL");
+        header('HTTP/1.1 204 No Content');
+        header('Access-Control-Allow-Origin: '.$_SERVER["HTTP_ORIGIN"]);
+        header('Access-Control-Allow-Credentials: true');
+        header('X-Experience-API-Version: 1.0.3');
+        exit;
+    }
+    else {
+        $ids = json_encode($post);
+        $log->debug("post: " . $ids);
+        header('HTTP/1.1 200 Ok');
+        header('Access-Control-Allow-Origin: '.$_SERVER["HTTP_ORIGIN"]);
+        header('Access-Control-Allow-Credentials: true');
+        header('X-Experience-API-Version: 1.0.3');
+        header('Content-Length: ' . strlen($ids));
+        header('Content-Type: application/json; charset=utf-8');
+        echo $ids;
+        exit;
     }
 }
 
 /**
  * handle proxy request
  */
-function handleProxy(ServerRequestInterface $request)
+function handleProxy(ServerRequestInterface $request, $fakePostBody = NULL)
 {
     global $log, $authToken, $lrsType;
-    
+
     $endpoint = $lrsType->getLrsEndpoint();
     $log->debug("Endpoint: " . $endpoint);
     //$endpoint = "https://lrs.example.com/lrs.php";
@@ -128,7 +264,7 @@ function handleProxy(ServerRequestInterface $request)
     $queryParams = $request->getQueryParams();
     $parts_reg = '/^(.*?xapiproxy\.php)(.+)/'; // ToDo: replace hard coded regex?
     preg_match($parts_reg, $full_uri, $cmd_parts);
-    
+
     if (count($cmd_parts) === 3) { // should always
         try {
             $cmd = $cmd_parts[2];
@@ -142,7 +278,7 @@ function handleProxy(ServerRequestInterface $request)
             $httpclient = new Client();
             $promise = $httpclient->sendAsync($req, $req_opts);
             $response = $promise->wait();
-            handleResponse($request, $response);
+            handleResponse($request, $response, $fakePostBody);
         } catch (Exception $e) { // ToDo: Errorhandling
             header("HTTP/1.1 500 XapiProxy Error");
             echo "HTTP/1.1 500 XapiProxy Error";
@@ -156,13 +292,21 @@ function handleProxy(ServerRequestInterface $request)
     }
 }
 
-function handleResponse(ServerRequestInterface $request, ResponseInterface $response)
+function handleResponse(ServerRequestInterface $request, ResponseInterface $response, $fakePostBody = NULL)
 {
     global $log;
+    if ($fakePostBody !== NULL) {
+        $origBody = $response->getBody();
+        $log->debug("orig body: " . $origBody);
+        $log->debug("fake body: " . json_encode($fakePostBody));
+        // because there is a real response object, it should also be possible to override the response stream...
+        // but this does the job as well:
+        fakeResponseBlocked($fakePostBody);
+    }
     // check transfer encoding bug
     $headers = $response->getHeaders();
     if (array_key_exists('Transfer-Encoding', $headers) && $headers['Transfer-Encoding'][0] == "chunked") {
-        $log->info("sniff response transfer-encoding for unallowed Content-length");
+        $log->debug("sniff response transfer-encoding for unallowed Content-length");
         $body = $response->getBody();
         $status = $response->getStatusCode();
         unset($headers['Transfer-Encoding']);
@@ -176,41 +320,52 @@ function handleResponse(ServerRequestInterface $request, ResponseInterface $resp
 
 function modifyBody($body)
 {
-    global $log;
+    global $log, $replacedValues;
 
     $obj = json_decode($body, false);
- 
+
     if (json_last_error() != JSON_ERROR_NONE) {
         // JSON is not valid
         $log->error(json_last_error_msg());
         return $body;
     }
-    
-    $log->debug(json_encode($obj, JSON_PRETTY_PRINT)); // only in DEBUG mode for better performance
-    
+
+    // $log->debug(json_encode($obj, JSON_PRETTY_PRINT)); // only in DEBUG mode for better performance
+
     if (is_object($obj)) {
-        $log->debug("");
-        
+        if (is_array($replacedValues)) {
+            foreach ($replacedValues as $key => $value) {
+                setValue($obj,$key,$value);
+            }
+        }
         handleStatementEvaluation($obj);
     }
-    
+
     if (is_array($obj)) {
         for ($i = 0; $i < count($obj); $i++) {
+            if (is_array($replacedValues)) {
+                foreach ($replacedValues as $key => $value) {
+                    setValue($obj[$i],$key,$value);
+                }
+            }
             handleStatementEvaluation($obj[$i]);
         }
     }
-    
+
     return json_encode($obj);
 }
+
+
 
 function handleStatementEvaluation($xapiStatement)
 {
     global $authToken, $log;
-    
+
     /* @var ilObjCmiXapi $object */
     $object = ilObjectFactory::getInstanceByObjId($authToken->getObjId());
 
     if( (string)$object->getLaunchMode() === (string)ilObjCmiXapi::LAUNCH_MODE_NORMAL ) {
+        // ToDo: check function hasContextActivitiesParentNotEqualToObject!
         $statementEvaluation = new ilXapiStatementEvaluation($log, $object);
         $statementEvaluation->evaluateStatement($xapiStatement, $authToken->getUsrId());
 
@@ -219,6 +374,49 @@ function handleStatementEvaluation($xapiStatement)
             $authToken->getUsrId()
         );
     }
+}
+
+function setValue(&$obj, $path, $value) {
+    global $log;
+    $path_components = explode('.', $path);
+    if (count($path_components) == 1) {
+        if (property_exists($obj,$path_components[0])) {
+            $obj->{$path_components[0]} = $value;
+        }
+    }
+    else {
+        if (property_exists($obj, $path_components[0])) {
+            setValue($obj->{array_shift($path_components)}, implode('.', $path_components), $value);
+        }
+    }
+}
+
+function isSubStatementCheck($obj) {
+    global $log;
+    if (
+        isset($obj->context) &&
+        isset($obj->context->contextActivities) &&
+        is_array($obj->context->contextActivities->parent)
+    ) {
+        $log->debug("is Substatement");
+        return true;
+    }
+    else {
+        $log->debug("is not Substatement");
+        return false;
+    }
+}
+
+function exitResponseError() {
+    header("HTTP/1.1 412 Wrong Response");
+    echo "HTTP/1.1 412 Wrong Response";
+    exit;
+}
+
+function exitProxyError() {
+    header("HTTP/1.1 500 XapiProxy Error (Ask For Logs)");
+    echo "HTTP/1.1 500 XapiProxy Error (Ask For Logs)";
+    exit;
 }
 
 // use only for debugging states before ILIAS Init
