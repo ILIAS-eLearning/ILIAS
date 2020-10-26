8a8bf2204b4643fa6d37ab57a1469c6ef75a79b3
mjansen <mjansen@databay.de>
2020-10-26 15:04:48 +0100

    Forum: Fix Mantis issue 29086

--- Modules/Forum/classes/class.ilObjForumGUI.php
+++ Modules/Forum/classes/class.ilObjForumGUI.php
@@ -4099,16 +4099,17 @@ class ilObjForumGUI extends \ilObjectGUI implements \ilDesktopItemHandling
 
             \ilForumUtil::moveMediaObjects($form->getInput('message'), 'frm~d:html', $draftId, 'frm~d:html', $draftId);
 
+            $draftFileData = new \ilFileDataForumDrafts($this->object->getId(), $draftId);
+
+            $files2delete = $form->getInput('del_file');
+            if (is_array($files2delete) && count($files2delete) > 0) {
+                $draftFileData->unlinkFilesByMD5Filenames($files2delete);
+            }
+
             if ($this->objProperties->isFileUploadAllowed()) {
-                $draftFileData = new \ilFileDataForumDrafts($this->object->getId(), $draftId);
                 $file = $_FILES['userfile'];
                 if (is_array($file) && !empty($file)) {
                     $draftFileData->storeUploadedFile($file);
-                }
-
-                $files2delete = $form->getInput('del_file');
-                if (is_array($files2delete) && count($files2delete) > 0) {
-                    $draftFileData->unlinkFilesByMD5Filenames($files2delete);
                 }
             }
 
@@ -4171,16 +4172,17 @@ class ilObjForumGUI extends \ilObjectGUI implements \ilDesktopItemHandling
                 $draft->getDraftId()
             );
 
+            $draftFileData = new \ilFileDataForumDrafts($this->object->getId(), $draft->getDraftId());
+
+            $files2delete = $form->getInput('del_file');
+            if (is_array($files2delete) && count($files2delete) > 0) {
+                $draftFileData->unlinkFilesByMD5Filenames($files2delete);
+            }
+
             if ($this->objProperties->isFileUploadAllowed()) {
-                $draftFileData = new \ilFileDataForumDrafts($this->object->getId(), $draft->getDraftId());
                 $file = $_FILES['userfile'];
                 if (is_array($file) && !empty($file)) {
                     $draftFileData->storeUploadedFile($file);
-                }
-
-                $files2delete = $form->getInput('del_file');
-                if (is_array($files2delete) && count($files2delete) > 0) {
-                    $draftFileData->unlinkFilesByMD5Filenames($files2delete);
                 }
             }
 
@@ -4394,18 +4396,19 @@ class ilObjForumGUI extends \ilObjectGUI implements \ilDesktopItemHandling
                     ilObjMediaObject::_saveUsage($mob, 'frm~d:html', $update_draft->getDraftId());
                 }
                 ilForumUtil::saveMediaObjects($oReplyEditForm->getInput('message'), 'frm~d:html', $update_draft->getDraftId());
-                
-                if ($this->objProperties->isFileUploadAllowed()) {
-                    $oFDForumDrafts = new ilFileDataForumDrafts($forumObj->getId(), $update_draft->getDraftId());
-                    $file = $_FILES['userfile'];
-                    if (is_array($file) && !empty($file)) {
-                        $oFDForumDrafts->storeUploadedFile($file);
-                    }
-                }
+
+                $oFDForumDrafts = new ilFileDataForumDrafts($forumObj->getId(), $update_draft->getDraftId());
 
                 $file2delete = $oReplyEditForm->getInput('del_file');
                 if (is_array($file2delete) && count($file2delete)) {
                     $oFDForumDrafts->unlinkFilesByMD5Filenames($file2delete);
+                }
+
+                if ($this->objProperties->isFileUploadAllowed()) {
+                    $file = $_FILES['userfile'];
+                    if (is_array($file) && !empty($file)) {
+                        $oFDForumDrafts->storeUploadedFile($file);
+                    }
                 }
 
                 $_SESSION['frm'][(int) $_GET['thr_pk']]['openTreeNodes'][] = (int) $this->objCurrentPost->getId();
