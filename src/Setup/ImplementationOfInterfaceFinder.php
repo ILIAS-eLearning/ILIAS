0f66ec1bbef76271f47bffc9bc3233c7daa4101a
Richard Klees <richard.klees@concepts-and-training.de>
2020-10-30 10:09:41 +0100

    Setup: improve interface to ImplementationOfInterfaceFinder

--- src/Setup/ImplementationOfInterfaceFinder.php
+++ src/Setup/ImplementationOfInterfaceFinder.php
@@ -9,42 +9,67 @@ namespace ILIAS\Setup;
  */
 class ImplementationOfInterfaceFinder
 {
-
     /**
      * @var string
      */
-    private $interface = "";
+    protected $root;
+
     /**
-     * @var array
+     * @var string[]
      */
-    private $ignore
-        = [
-            '.*/Customizing/',
-            '.*/libs/',
-            '.*/test/',
-            '.*/tests/',
-            '.*/setup/',
-            // Classes using removed Auth-class from PEAR
-            '.*ilSOAPAuth.*',
-            // Classes using unknown
-            '.*ilPDExternalFeedBlockGUI.*',
-        ];
+    protected $ignore = [
+        '.*/libs/',
+        '.*/test/',
+        '.*/tests/',
+        '.*/setup/',
+        // Classes using removed Auth-class from PEAR
+        '.*ilSOAPAuth.*',
+        // Classes using unknown
+        '.*ilPDExternalFeedBlockGUI.*',
+    ];
 
+    /**
+     * @var string[]|null
+     */
+    protected $classmap = null;
 
-    public function __construct(string $interface)
+    public function __construct()
     {
-        $this->interface = $interface;
-        $this->getAllClassNames();
+        $this->root = substr(__FILE__, 0, strpos(__FILE__, "/src"));
+        $this->classmap = include "./libs/composer/vendor/composer/autoload_classmap.php";
     }
 
-
-    protected function getAllClassNames() : \Iterator
+    /**
+     * The matcher finds the class names implementing the given interface, while
+     * ignoring paths in self::$ignore and and the additional patterns provided.
+     *
+     * Patterns are regexps (without delimiters) to define complete paths on the
+     * filesystem to be ignored.
+     *
+     * @param   string[] $additional_ignore
+     */
+    public function getMatchingClassNames(string $interface, array $additional_ignore = []) : \Iterator
     {
-        // We use the composer classmap ATM
-        $composer_classmap = include "./libs/composer/vendor/composer/autoload_classmap.php";
-        $root = substr(__FILE__, 0, strpos(__FILE__, "/src"));
+        foreach ($this->getAllClassNames($additional_ignore) as $class_name) {
+            try {
+                $r = new \ReflectionClass($class_name);
+                if ($r->isInstantiable() && $r->implementsInterface($interface)) {
+                    yield $class_name;
+                }
+            } catch (\Throwable $e) {
+                // noting to do here
+            }
+        }
+    }
 
-        if (!is_array($composer_classmap)) {
+    /**
+     * @param   string[] $additional_ignore
+     */
+    protected function getAllClassNames(array $additional_ignore) : \Iterator
+    {
+        $ignore = array_merge($this->ignore, $additional_ignore);
+
+        if (!is_array($this->classmap)) {
             throw new \LogicException("Composer ClassMap not loaded");
         }
 
@@ -55,29 +80,14 @@ class ImplementationOfInterfaceFinder
                 function ($v) {
                     return "(" . str_replace('/', '(/|\\\\)', $v) . ")";
                 },
-                $this->ignore
+                $ignore
             )
         );
 
-        foreach ($composer_classmap as $class_name => $file_path) {
-            $path = str_replace($root, "", realpath($file_path));
+        foreach ($this->classmap as $class_name => $file_path) {
+            $path = str_replace($this->root, "", realpath($file_path));
             if (!preg_match("#^" . $regexp . "$#", $path)) {
                 yield $class_name;
-            }
-        }
-    }
-
-
-    public function getMatchingClassNames() : \Iterator
-    {
-        foreach ($this->getAllClassNames() as $class_name) {
-            try {
-                $r = new \ReflectionClass($class_name);
-                if ($r->isInstantiable() && $r->implementsInterface($this->interface)) {
-                    yield $class_name;
-                }
-            } catch (\Throwable $e) {
-                // noting to do here
             }
         }
     }
