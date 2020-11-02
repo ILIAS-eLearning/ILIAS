886f37c65ba85634ea73beb65c3a5feef13b52e4
Maximilian Becker <mbecker@databay.de>
2020-11-02 12:19:32 +0100

    Update Services/User/Profile/classes/class.ilPersonalProfileGUI.php
    Co-authored-by: Michael Jansen <mjansen@databay.de>

--- Services/User/Profile/classes/class.ilPersonalProfileGUI.php
+++ Services/User/Profile/classes/class.ilPersonalProfileGUI.php
@@ -606,7 +606,7 @@ class ilPersonalProfileGUI
         ilSession::setClosingContext(ilSession::SESSION_CLOSE_USER);
         $GLOBALS['DIC']['ilAuthSession']->logout();
 
-        ilUtil::redirect("login.php?wdtdel=".$accdel."&cmd=force_login");
+        $this->ctrl::redirectToUrl('login.php?wdtdel=".$accdel."&cmd=force_login');
     }
 
     protected function withdrawAcceptanceLDAP() : void
