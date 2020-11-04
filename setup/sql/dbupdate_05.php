b35e0526a8d782b49b72ceb68331cd78de50e11d
Thomas Famula <famula@leifos.de>
2020-11-04 15:12:17 +0100

    dbupdate for skill assigned objects

--- setup/sql/dbupdate_05.php
+++ setup/sql/dbupdate_05.php
@@ -4866,3 +4866,25 @@ if (!$ilDB->tableColumnExists("skl_profile", "ref_id")) {
 <?php
 $ilCtrlStructureReader->getStructure();
 ?>
+<#5708>
+<?php
+if ($ilDB->tableExists('cont_skills')) {
+    $set = $ilDB->query(
+        "SELECT id, skill_id, tref_id FROM cont_skills"
+    );
+}
+
+if ($ilDB->tableExists('skl_usage')) {
+    while ($rec = $ilDB->fetchAssoc($set)) {
+        $ilDB->replace(
+            'skl_usage',
+            array(
+                'obj_id' => array('integer', $rec['id']),
+                'skill_id' => array('integer', $rec['skill_id']),
+                'tref_id' => array('integer', $rec['tref_id'])
+            ),
+            array()
+        );
+    }
+}
+?>
