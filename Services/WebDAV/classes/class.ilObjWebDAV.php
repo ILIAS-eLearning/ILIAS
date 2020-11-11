51b943df391cb66f069eb6c70ca369b13ced5be2
Raphael Heer <raphael.heer@hslu.ch>
2020-11-11 17:36:52 +0100

    Removed unused settings and methods in ilObjWebDAV

--- Services/WebDAV/classes/class.ilObjWebDAV.php
+++ Services/WebDAV/classes/class.ilObjWebDAV.php
@@ -92,28 +92,6 @@ class ilObjWebDAV extends ilObject
     }
 
     /**
-     * Sets the webdavActionsVisible property.
-     *
-     * @param boolean    new value
-     *
-     * @return    void
-     */
-    public function setWebdavActionsVisible($newValue)
-    {
-        $this->webdavActionsVisible = $newValue;
-    }
-
-    /**
-     * Gets the webdavActionsVisible property.
-     *
-     * @return    boolean    value
-     */
-    public function isWebdavActionsVisible()
-    {
-        return $this->webdavActionsVisible;
-    }
-
-    /**
      * Gets the defaultWebfolderInstructions property.
      * This is a read only property. The text is retrieved from $lng.
      *
@@ -180,25 +158,10 @@ class ilObjWebDAV extends ilObject
     private function write()
     {
         global $DIC;
-        $ilClientIniFile = $DIC['ilClientIniFile'];
         $settings = new ilSetting('webdav');
 
-        // Clear any old error messages
-        $ilClientIniFile->error(null);
-
-        if (!$ilClientIniFile->groupExists('file_access')) {
-            $ilClientIniFile->addGroup('file_access');
-        }
         $settings->set('webdav_enabled', $this->webdavEnabled ? '1' : '0');
         $settings->set('webdav_versioning_enabled', $this->webdavVersioningEnabled ? '1' : '0');
-        $ilClientIniFile->setVariable('file_access', 'webdav_actions_visible', $this->webdavActionsVisible ? '1' : '0');
-        $ilClientIniFile->write();
-
-        if ($ilClientIniFile->getError()) {
-            global $DIC;
-            $ilErr = $DIC['ilErr'];
-            $ilErr->raiseError($ilClientIniFile->getError(), $ilErr->WARNING);
-        }
     }
 
     /**
@@ -210,30 +173,8 @@ class ilObjWebDAV extends ilObject
 
         global $DIC;
         $settings = new ilSetting('webdav');
-        $ilClientIniFile = $DIC['ilClientIniFile'];
         $this->webdavEnabled = $settings->get('webdav_enabled', '0') == '1';
         // default_value = 1 for versionigEnabled because it was already standard before ilias5.4
         $this->webdavVersioningEnabled = $settings->get('webdav_versioning_enabled', '1') == '1';
-        $this->webdavActionsVisible = $ilClientIniFile->readVariable('file_access', 'webdav_actions_visible') == '1';
-        $ilClientIniFile->ERROR = false;
     }
-
-    /**
-     * Gets instructions for the usage of webfolders.
-     *
-     * The instructions consist of HTML text with placeholders.
-     * See _getWebfolderInstructionsFor for a description of the supported
-     * placeholders.
-     *
-     * @return String HTML text with placeholders.
-     */
-    public static function _getDefaultWebfolderInstructions()
-    {
-        global $lng;
-
-        return $lng->txt('webfolder_instructions_text');
-    }
-
-
-
 }
