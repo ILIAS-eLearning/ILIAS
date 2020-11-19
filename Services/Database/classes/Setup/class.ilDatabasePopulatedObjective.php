400ae4046aa6fc65ad16ba310889efa8d9cba4a3
Fabian Schmid <fs@studer-raimann.ch>
2020-11-19 16:37:41 +0100

    prepared reading of type specific dumps and setting default engine

--- Services/Database/classes/Setup/class.ilDatabasePopulatedObjective.php
+++ Services/Database/classes/Setup/class.ilDatabasePopulatedObjective.php
@@ -40,19 +40,30 @@ class ilDatabasePopulatedObjective extends \ilDatabaseObjective
 
     public function achieve(Setup\Environment $environment) : Setup\Environment
     {
+        /**
+         * @var $db ilDBInterface
+         * @var $io Setup\CLI\IOWrapper
+         */
         $db = $environment->getResource(Setup\Environment::RESOURCE_DATABASE);
+        $io = $environment->getResource(Setup\Environment::RESOURCE_ADMIN_INTERACTION);
 
-        $path_to_db_dump = $this->config->getPathToDBDump();
-        if (!is_file(realpath($path_to_db_dump)) ||
-            !is_readable(realpath($path_to_db_dump))) {
-            throw new Setup\UnachievableException(
-                "Cannot read database dump file: $path_to_db_dump"
-            );
+        // $this->setDefaultEngine($db); // maybe we could set the default?
+        $default = $this->getDefaultEngine($db);
+
+        $io->text("Default DB engine is {$default}");
+
+        switch ($default) {
+            case 'innodb':
+            case 'myisam':
+                $io->text("reading dump file, this may take a while...");
+                $this->readDumpFile($db);
+                break;
+
+            default:
+                throw new Setup\UnachievableException(
+                    "Cannot determine database default engine, must be InnoDB or MyISAM"
+                );
         }
-
-        $sql = file_get_contents(realpath($path_to_db_dump));
-        $statement = $db->prepareManip($sql);
-        $db->execute($statement);
 
         return $environment;
     }
@@ -67,7 +78,7 @@ class ilDatabasePopulatedObjective extends \ilDatabaseObjective
         return !$this->isDatabasePopulated($db);
     }
 
-    protected function isDatabasePopulated(\ilDBInterface $db)
+    protected function isDatabasePopulated(ilDBInterface $db) : bool
     {
         $probe_tables = ['usr_data', 'object_data', 'object_reference'];
         $number_of_probe_tables = count($probe_tables);
@@ -77,5 +88,64 @@ class ilDatabasePopulatedObjective extends \ilDatabaseObjective
         return
             $number_of_tables > self::MIN_NUMBER_OF_ILIAS_TABLES
             && count(array_intersect($tables, $probe_tables)) == $number_of_probe_tables;
+    }
+
+    /**
+     * @param ilDBInterface $db
+     * @throws ilDatabaseException
+     */
+    private function readDumpFile(ilDBInterface $db) : void
+    {
+        $path_to_db_dump = $this->config->getPathToDBDump();
+        if (!is_file(realpath($path_to_db_dump)) ||
+            !is_readable(realpath($path_to_db_dump))) {
+            throw new Setup\UnachievableException(
+                "Cannot read database dump file: $path_to_db_dump"
+            );
+        }
+
+        $sql = file_get_contents(realpath($path_to_db_dump));
+        $statement = $db->prepareManip($sql);
+        $db->execute($statement);
+    }
+
+    /**
+     * @param ilDBInterface|null $db
+     */
+    private function setDefaultEngine(ilDBInterface $db) : void
+    {
+        switch ($db->getDBType()) {
+            case ilDBConstants::TYPE_PDO_MYSQL_INNODB:
+            case ilDBConstants::TYPE_INNODB:
+            case ilDBConstants::TYPE_GALERA:
+                $db->manipulate('SET default_storage_engine=InnoDB;');
+                break;
+            case ilDBConstants::TYPE_PDO_MYSQL_MYISAM:
+            case ilDBConstants::TYPE_MYSQL:
+                $db->manipulate('SET default_storage_engine=MyISAM;');
+                break;
+
+        }
+    }
+
+    /**
+     * @param ilDBInterface $db
+     * @return string
+     */
+    private function getDefaultEngine(ilDBInterface $db) : string
+    {
+        try {
+            $r = $db->query('SHOW ENGINES ');
+
+            $default = '';
+            while ($d = $db->fetchObject($r)) {
+                if ($d->Support === 'DEFAULT') {
+                    $default = $d->Engine;
+                }
+            }
+            return strtolower($default);
+        } catch (Throwable $e) {
+            return 'unknown';
+        }
     }
 }
