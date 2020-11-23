c43cab4d29926f367d5dc648c2c89c1a61ef0c55
smeyer <smeyer.ilias@gmx.de>
2020-11-23 10:24:18 +0100

    0029225: Trying to open personal workspace leads to an error

--- Services/DidacticTemplate/classes/class.ilDidacticTemplateIconFactory.php
+++ Services/DidacticTemplate/classes/class.ilDidacticTemplateIconFactory.php
@@ -75,7 +75,7 @@ class ilDidacticTemplateIconFactory
         $obj_id = ilObject::_lookupObjId($ref_id);
         $type = ilObject::_lookupType($obj_id);
 
-        if (!$this->supportsCustomIcon($type)) {
+        if (strlen($type) || !$this->supportsCustomIcon($type)) {
             return '';
         }
         $assigned_template = $this->findAssignedTemplate($ref_id);
