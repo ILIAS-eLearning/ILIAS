798d2b03c175a6f4f7ff59bec8a2f54686afc5bb
Richard Klees <richard.klees@concepts-and-training.de>
2020-11-24 13:54:28 +0100

    Setup/Chatroom: use null-object for config instead

---
+++ src/Setup/NullConfig.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Copyright (c) 2019 Richard Klees <richard.klees@concepts-and-training.de> Extended GPL, see docs/LICENSE */
+
+namespace ILIAS\Setup;
+
+/**
+ * A configuration with no content.
+ */
+class NullConfig implements Config
+{
+}
