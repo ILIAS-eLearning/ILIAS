53bc530640130c511f1e6eddb3a864370f1ad3b5
Alexander Killing <killing@leifos.de>
2020-12-18 14:41:05 +0100

    29469: Deck cards responsive sizes not very well balanced

--- src/UI/Implementation/Component/Deck/Deck.php
+++ src/UI/Implementation/Component/Deck/Deck.php
@@ -132,20 +132,70 @@ class Deck implements D\Deck
      */
     public function getCardsSizeSmallDisplays()
     {
-        switch ($this->getCardsSize()) {
-            case self::SIZE_XS:
-                return 2;
-            case self::SIZE_S:
-                return 4;
-            case self::SIZE_M:
-                return 6;
-            case self::SIZE_L:
-                // no-break
-            case self::SIZE_XL:
-                // no-break
-            case self::SIZE_FULL:
-                return 12;
-        }
+        return $this->getCardsSizeForDisplaySize(self::SIZE_S);
+    }
+
+    /**
+     * @internal This function is only internal and returns the size of the cards for small displays.
+     * Note that this size tells how much space the card is using. The number of cards displayed by normal screen size is 12/size.
+     *
+     * @return int
+     */
+    public function getCardsSizeForDisplaySize($display_size)
+    {
+        /**
+         * Rationale
+         *
+         *  (1) Breakpoints:        768 - 992 - 1200
+         *  (2) Center Points:      384 (xs) - 880 (sm) - 1096 (md) - 1400 (lg)
+         *  (3) Normalized Ratio:	27% (xs) - 63% (sm) - 78% (md) - 100% (lg)
+         *  (4) Card sizes respecting ratio:
+         *      Extra Small:   3 (xs-4)  6 (sm-2)  6 (md-2) 12 (lg-1)
+         *      Small: 	       2 (xs-6)  4 (sm-3)  4 (md-3) 6 (lg-2)
+         *      Normal:        1 (xs-12) 2 (sm-6)  3 (md-4) 4 (lg-3)
+         *      Large:         1 (xs-12) 2 (sm-6)  2 (md-6) 3 (lg-4)
+         *      Extra Large:   1 (xs-12) 1 (sm-12) 2 (md-6) 2 (lg-6)
+         */
+        $sizes = [
+            self::SIZE_XS => [
+                self::SIZE_XS => 4,
+                self::SIZE_S => 2,
+                self::SIZE_M => 2,
+                self::SIZE_L => 1
+            ],
+            self::SIZE_S => [
+                self::SIZE_XS => 6,
+                self::SIZE_S => 3,
+                self::SIZE_M => 3,
+                self::SIZE_L => 2
+            ],
+            self::SIZE_M => [
+                self::SIZE_XS => 12,
+                self::SIZE_S => 6,
+                self::SIZE_M => 4,
+                self::SIZE_L => 3
+            ],
+            self::SIZE_L => [
+                self::SIZE_XS => 12,
+                self::SIZE_S => 6,
+                self::SIZE_M => 6,
+                self::SIZE_L => 4
+            ],
+            self::SIZE_XL => [
+                self::SIZE_XS => 12,
+                self::SIZE_S => 12,
+                self::SIZE_M => 6,
+                self::SIZE_L => 6
+            ],
+            self::SIZE_FULL => [
+                self::SIZE_XS => 12,
+                self::SIZE_S => 12,
+                self::SIZE_M => 12,
+                self::SIZE_L => 12
+            ],
+        ];
+
+        return $sizes[$this->getCardsSize()][$display_size];
     }
 
     private static $sizes = array(self::SIZE_FULL
