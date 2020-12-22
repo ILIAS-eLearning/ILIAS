2c5b5b469fb46dc39d847d768aa24a478a7381f5
mjansen <mjansen@databay.de>
2020-12-22 11:21:31 +0100

    Mail: Fix obsolete 'type' attribute

--- Services/Contact/classes/class.ilMailSearchGUI.php
+++ Services/Contact/classes/class.ilMailSearchGUI.php
@@ -134,7 +134,6 @@ class ilMailSearchGUI
             $mail_data["rcp_to"],
             $mail_data["rcp_cc"],
             $mail_data["rcp_bcc"],
-            $mail_data["m_type"],
             $mail_data["m_email"],
             $mail_data["m_subject"],
             $mail_data["m_message"],
