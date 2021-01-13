f2538b1ec4e899a31530818c1d33579bdd6fcc37
Marvin Barz <barz@leifos.de>
2021-01-13 15:38:17 +0100

    BT-28951: Added aria label for main menu search form input

--- Services/Search/classes/class.ilMainMenuSearchGUI.php
+++ Services/Search/classes/class.ilMainMenuSearchGUI.php
@@ -103,6 +103,7 @@ class ilMainMenuSearchGUI
         $this->tpl->setVariable('FORMACTION', 'ilias.php?baseClass=ilSearchController&cmd=post' .
             '&rtoken=' . $ilCtrl->getRequestToken() . '&fallbackCmd=remoteSearch');
         $this->tpl->setVariable('BTN_SEARCH', $this->lng->txt('search'));
+        $this->tpl->setVariable('SEARCH_INPUT_LABEL', $this->lng->txt('search_field'));
         
         $this->tpl->setVariable('AC_DATASOURCE', "ilias.php?baseClass=ilSearchController&cmd=autoComplete");
         
