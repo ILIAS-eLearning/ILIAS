ILIAS-eLearning/ILIAS

commit c24bc5363809630a8c63fe231ba58baf891d8757

Setup: ignore messages of db-updates during install

setup/cli.php
@@ -45,8 +45,12 @@ function get_agent_name_by_class(string $class_name) : string {
 	return $class_name;
 }
 
+// ATTENTION: This is a hack to get around the usage of the echo/exit pattern in
+// the setup for the command line version of the setup. Do not use this.
 function setup_exit($message) {
-	throw new \ILIAS\Setup\UnachievableException($message);
+	if (!defined("ILIAS_SETUP_IGNORE_DB_UPDATE_STEP_MESSAGES") && ILIAS_SETUP_IGNORE_DB_UPDATE_STEP_MESSAGES) {
+		throw new \ILIAS\Setup\UnachievableException($message);
+	}
 }
 
 function build_container_for_setup(string $executed_in_directory) {

setup/setup.php
@@ -43,6 +43,8 @@ if (php_sapi_name() === "cli") {
 }
 else {
 
+// ATTENTION: This is a hack to get around the usage of the echo/exit pattern in
+// the setup for the command line version of the setup.
 function setup_exit($message) {
 	echo "<pre>$message</pre>";
 	die();

src/Setup/CLI/InstallCommand.php
@@ -12,6 +12,7 @@ use ILIAS\Setup\Objective;
 use ILIAS\Setup\ObjectiveCollection;
 use ILIAS\Setup\AchievementTracker;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 
 /**
@@ -31,6 +32,13 @@ class InstallCommand extends BaseCommand {
 
 	protected function printOutroMessage(IOWrapper $io) {
 		$io->success("Installation complete. Thanks and have fun!");
+	}
+
+	public function execute(InputInterface $input, OutputInterface $output) {
+		// ATTENTION: This is a hack to get around the usage of the echo/exit pattern in
+		// the setup for the command line version of the setup. Do not use this.
+		define("ILIAS_SETUP_IGNORE_DB_UPDATE_STEP_MESSAGES", true);
+		return parent::execute($input, $output);
 	}
 
 	protected function buildEnvironment(Agent $agent, ?Config $config, IOWrapper $io) : Environment {

commit 91398231ea8b2c472cac56735cca3170ebf788f2

Fix logic error in setup_exit hack in CLI setup
Commit c24bc5363809 has introduced a mistake where a new constant is
evaluated. We should only consider its value after we have checked that
the constant is defined. This is what had probably been originally
meant, the code was just missing the required brackets.

setup/cli.php
@@ -42,7 +42,7 @@ $app->run();
 // the setup for the command line version of the setup. Do not use this.
 function setup_exit($message)
 {
-    if (!defined("ILIAS_SETUP_IGNORE_DB_UPDATE_STEP_MESSAGES") && ILIAS_SETUP_IGNORE_DB_UPDATE_STEP_MESSAGES) {
+    if (!(defined("ILIAS_SETUP_IGNORE_DB_UPDATE_STEP_MESSAGES") && ILIAS_SETUP_IGNORE_DB_UPDATE_STEP_MESSAGES)) {
         throw new \ILIAS\Setup\UnachievableException($message);
     }
 }