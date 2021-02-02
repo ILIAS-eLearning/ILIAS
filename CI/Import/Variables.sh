993bfdc82d021956e15bb987cb5017c70b30fe81
Laura <quellmalz.laura@gmail.com>
2021-02-02 11:42:45 +0100

    CI: Implement github actions as replacement for travis (ILIAS 7) (#3133)
    * add GitHub Action for code checkout
    * add GitHub Action for PHP, with appropriate extensions and composer v2
    * install dependencies with composer
    * run PHPUnit-tests
    * run CS-Fixer
    * run Special Char Checker
    * remove Travis config
    * disable reporting
    * move file in GlobalScreen to make it comply to PSR-4

--- CI/Import/Variables.sh
+++ CI/Import/Variables.sh
@@ -10,6 +10,16 @@ RESULTS_DATA_DIRECTORY_PHPUNIT="$RESULTS_DATA_DIRECTORY/phpunit_latest_$PHP_VERS
 RESULTS_DATA_DIRECTORY_PHPUNIT_RESULTS_PATH="/tmp/phpunit_results_$PHP_VERSION"
 RESULTS_DATA_DIRECTORY_PHPUNIT_TMP="$RESULTS_DATA_DIRECTORY/phpunit_changed_$PHP_VERSION.csv"
 
+CI_RESULTS_REPO="https://github.com/ILIAS-eLearning/CI-Results"
+
+PHP_CS_FIXER_RESULTS_PATH="$TMP_DIR/phpfix_results"
+PHP_CS_FIXER="libs/composer/vendor/friendsofphp/php-cs-fixer/php-cs-fixer"
+
+PHP_VERSION=$(php -v | tac | tail -n 1 | cut -d " " -f 2 | cut -c 1-3)
+PHPUNIT_PATH="$TMP_DIR/phpunit_latest_$PHP_VERSION.csv"
+PHPUNIT_PATH_TMP="$TMP_DIR/phpunit_changed_$PHP_VERSION.csv"
+PHPUNIT_RESULTS_PATH="$TMP_DIR/phpunit_results_$PHP_VERSION"
+
 RESULTS_DATA_DIRECTORY_DICTO="$RESULTS_DATA_DIRECTORY/dicto_latest.csv"
 
 # CS fixer (Todo for later)
