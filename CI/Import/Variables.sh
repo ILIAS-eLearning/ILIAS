97a57df191bc2dbf2040bf9be8a6bcb63337e507
Laura <quellmalz.laura@gmail.com>
2021-02-02 10:39:12 +0100

    CI: Implement github actions as replacement for travis (#3081)
    * add GitHub Action for code checkout
    * add GitHub Action for PHP, with appropriate extensions and composer v1
    * install dependencies with composer
    * run PHPUnit-tests
    * run CS-Fixer
    * run Special Char Checker
    * remove Travis config
    * disable reporting

--- CI/Import/Variables.sh
+++ CI/Import/Variables.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 TMP_DIR="/tmp"
 
+CI_RESULTS_REPO="https://github.com/ILIAS-eLearning/CI-Results"
+
 PHP_CS_FIXER_RESULTS_PATH="$TMP_DIR/phpfix_results"
 PHP_CS_FIXER="libs/composer/vendor/friendsofphp/php-cs-fixer/php-cs-fixer"
 
-PHP_VERSION=$(phpenv version-name)
+PHP_VERSION=$(php -v | tac | tail -n 1 | cut -d " " -f 2 | cut -c 1-3)
 PHPUNIT_PATH="$TMP_DIR/phpunit_latest_$PHP_VERSION.csv"
 PHPUNIT_PATH_TMP="$TMP_DIR/phpunit_changed_$PHP_VERSION.csv"
 PHPUNIT_RESULTS_PATH="$TMP_DIR/phpunit_results_$PHP_VERSION"
