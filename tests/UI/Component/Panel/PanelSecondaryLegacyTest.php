fa85762e8d94b4ef70ce9381e48bcd4a9349a2fc
Timon Amstutz <timon.amstutz@bluewin.ch>
2021-02-12 13:29:24 +0100

    UI Components View Controls: Avoid empty IDs see: #29842

--- tests/UI/Component/Panel/PanelSecondaryLegacyTest.php
+++ tests/UI/Component/Panel/PanelSecondaryLegacyTest.php
@@ -208,14 +208,14 @@ EOT;
 <div class="panel panel-secondary panel-flex">
 	<div class="panel-heading ilHeader clearfix">
 		<h4 class="ilHeader">Title</h4>
-		<div class="il-viewcontrol-sortation">
+		<div class="il-viewcontrol-sortation" id="id_1">
 			<div class="dropdown">
 				<button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" aria-label="actions" aria-haspopup="true" aria-expanded="false">
 					<span class="caret"></span>
 				</button>
 				<ul class="dropdown-menu">
-					<li><button class="btn btn-link" data-action="?sortation=a" id="id_1">A</button></li>
-					<li><button class="btn btn-link" data-action="?sortation=b" id="id_2">B</button></li>
+					<li><button class="btn btn-link" data-action="?sortation=a" id="id_2">A</button></li>
+					<li><button class="btn btn-link" data-action="?sortation=b" id="id_3">B</button></li>
 				</ul>
 			</div>
 		</div>
@@ -297,13 +297,13 @@ EOT;
 	<div class="panel-heading ilHeader clearfix">
 		<h4 class="ilHeader">Title</h4>
 		<div class="il-viewcontrol-section">
-			<a class="btn btn-default " href="http://www.ilias.de" aria-label="previous" data-action="http://www.ilias.de">
+			<a class="btn btn-default " href="http://www.ilias.de" aria-label="previous" data-action="http://www.ilias.de" id="id_1">
 				<span class="glyphicon glyphicon-chevron-left"></span>
 			</a>
 			<button class="btn btn-default" data-action="">
 				current
 			</button>
-			<a class="btn btn-default " href="http://www.github.com" aria-label="next" data-action="http://www.github.com">
+			<a class="btn btn-default " href="http://www.github.com" aria-label="next" data-action="http://www.github.com"  id="id_2">
 				<span class="glyphicon glyphicon-chevron-right"></span>
 			</a>
 		</div>
