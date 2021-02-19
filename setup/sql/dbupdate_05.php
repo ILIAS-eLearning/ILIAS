ac01d953f9c090046a542d01052fd38c316a90c9
Fabian Schmid <fs@studer-raimann.ch>
2021-02-19 07:33:49 +0100

    change columns

--- setup/sql/dbupdate_05.php
+++ setup/sql/dbupdate_05.php
@@ -6323,4 +6323,30 @@ if ($ilDB->tableColumnExists("reg_access_limit", "limit_relative_y")) {
     $ilDB->dropTableColumn("reg_access_limit", "limit_relative_y");
 }
 ?>
+<#5775>
+<?php
+$table_name = 'il_adn_notifications';
+$columns = [
+    'event_start',
+    'event_end',
+    'display_start',
+    'display_end',
+    'create_date',
+    'last_update',
+];
+
+foreach ($columns as $column) {
+    if ($ilDB->tableExists($table_name)) {
+        if ($ilDB->tableColumnExists($table_name, $column)) {
+            $ilDB->dropTableColumn($table_name, $column);
+        }
+        $ilDB->addTableColumn($table_name, $column, array(
+            "type" => "integer",
+            "notnull" => false,
+            "length" => 8,
+            "default" => 0
+        ));
+    }
+}
+?>
 
