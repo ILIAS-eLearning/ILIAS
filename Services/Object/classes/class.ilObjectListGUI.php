e5c066db71ea6f917248ae7c10df3951aa8e50ad
Alexander Killing <killing@leifos.de>
2021-02-24 16:07:43 +0100

    merged https://github.com/ILIAS-eLearning/ILIAS/pull/3093/files

--- Services/Object/classes/class.ilObjectListGUI.php
+++ Services/Object/classes/class.ilObjectListGUI.php
@@ -2621,7 +2621,7 @@ class ilObjectListGUI
         // public area, category, no info tab
         // todo: make this faster and remove type specific implementation if possible
         if ($a_use_asynch && !$a_get_asynch_commands && !$a_header_actions) {
-            if ($ilUser->getId() == ANONYMOUS_USER_ID && $this->type == "cat") {
+            if ($ilUser->getId() == ANONYMOUS_USER_ID && $this->checkInfoPageOnAsynchronousRendering()) {
                 include_once("./Services/Container/classes/class.ilContainer.php");
                 include_once("./Services/Object/classes/class.ilObjectServiceSettingsGUI.php");
                 if (!ilContainer::_lookupContainerSetting(
@@ -3836,7 +3836,8 @@ class ilObjectListGUI
         string $type,
         string $title,
         string $description
-    ) : ?\ILIAS\UI\Component\Card\Card {
+    ) : ?\ILIAS\UI\Component\Card\Card
+    {
         $ui = $this->ui;
 
         $this->initItem(
@@ -3864,8 +3865,8 @@ class ilObjectListGUI
 
         foreach ($this->current_selection_list->getItems() as $action_item) {
             $actions[] = $ui->factory()
-                ->button()
-                ->shy($action_item['title'], $action_item['link']);
+                            ->button()
+                            ->shy($action_item['title'], $action_item['link']);
         }
 
         $def_command = $this->getDefaultCommand();
@@ -3874,7 +3875,8 @@ class ilObjectListGUI
             $button =
                 $ui->factory()->button()->shy("Open", "")->withAdditionalOnLoadCode(function ($id) use ($def_command) {
                     return
-                        "$('#$id').click(function(e) { window.open('" . str_replace("&amp;", "&", $def_command["link"]) . "', '" . $def_command["frame"] . "');});";
+                        "$('#$id').click(function(e) { window.open('" . str_replace("&amp;", "&",
+                            $def_command["link"]) . "', '" . $def_command["frame"] . "');});";
                 });
             $actions[] = $button;
         }
@@ -3902,19 +3904,22 @@ class ilObjectListGUI
             $this->modifySAHSlaunch($def_command["link"], $def_command["frame"]);
 
         $image = $this->ui->factory()
-            ->image()
-            ->responsive($path, '');
+                          ->image()
+                          ->responsive($path, '');
         if ($def_command['link'] != '') {    // #24256
             if ($def_command["frame"] != "" && ($modified_link == $def_command["link"])) {
                 $image = $image->withAdditionalOnLoadCode(function ($id) use ($def_command) {
                     return
-                        "$('#$id').click(function(e) { window.open('" . str_replace("&amp;", "&", $def_command["link"]) . "', '" . $def_command["frame"] . "');});";
+                        "$('#$id').click(function(e) { window.open('" . str_replace("&amp;", "&",
+                            $def_command["link"]) . "', '" . $def_command["frame"] . "');});";
                 });
 
                 $button =
-                    $ui->factory()->button()->shy($title, "")->withAdditionalOnLoadCode(function ($id) use ($def_command) {
+                    $ui->factory()->button()->shy($title, "")->withAdditionalOnLoadCode(function ($id) use ($def_command
+                    ) {
                         return
-                            "$('#$id').click(function(e) { window.open('" . str_replace("&amp;", "&", $def_command["link"]) . "', '" . $def_command["frame"] . "');});";
+                            "$('#$id').click(function(e) { window.open('" . str_replace("&amp;", "&",
+                                $def_command["link"]) . "', '" . $def_command["frame"] . "');});";
                     });
                 $title = $ui->renderer()->render($button);
             } else {
@@ -3928,33 +3933,35 @@ class ilObjectListGUI
             }
             $app_info = ilSessionAppointment::_lookupAppointment($obj_id);
             $title = ilSessionAppointment::_appointmentToString(
-                $app_info['start'],
-                $app_info['end'],
-                $app_info['fullday']
-            ) . $title;
+                    $app_info['start'],
+                    $app_info['end'],
+                    $app_info['fullday']
+                ) . $title;
         }
 
         $icon = $this->ui->factory()
-            ->symbol()
-            ->icon()
-            ->standard($type, $this->lng->txt('obj_' . $type))
-            ->withIsOutlined(true);
+                         ->symbol()
+                         ->icon()
+                         ->standard($type, $this->lng->txt('obj_' . $type))
+                         ->withIsOutlined(true);
 
         // card title action
         $card_title_action = "";
-        if ($def_command["link"] != "" && ($def_command["frame"] == "" || $modified_link != $def_command["link"])) {	// #24256
+        if ($def_command["link"] != "" && ($def_command["frame"] == "" || $modified_link != $def_command["link"])) {    // #24256
             $card_title_action = $modified_link;
-        } else if ($def_command['link'] == "" &&
-            $this->getInfoScreenStatus() &&
-            $access->checkAccessOfUser(
-                $user->getId(),
-                "visible",
-                "",
-                $ref_id
-            )) {
-            $card_title_action = ilLink::_getLink($ref_id);
-            if ($image->getAction() == "") {
-                $image = $image->withAction($card_title_action);
+        } else {
+            if ($def_command['link'] == "" &&
+                $this->getInfoScreenStatus() &&
+                $access->checkAccessOfUser(
+                    $user->getId(),
+                    "visible",
+                    "",
+                    $ref_id
+                )) {
+                $card_title_action = ilLink::_getLink($ref_id);
+                if ($image->getAction() == "") {
+                    $image = $image->withAction($card_title_action);
+                }
             }
         }
 
@@ -3994,12 +4001,20 @@ class ilObjectListGUI
 
             $card = $card->withProgress(
                 $ui->factory()
-                    ->chart()
-                    ->progressMeter()
-                    ->mini(100, $percentage)
+                   ->chart()
+                   ->progressMeter()
+                   ->mini(100, $percentage)
             );
         }
 
         return $card;
+    }
+
+    /**
+     * @return bool
+     */
+    public function checkInfoPageOnAsynchronousRendering() : bool
+    {
+        return false;
     }
 }
