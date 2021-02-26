ce11aedf397868f2702e7ee69604ec960cddb558
daniwe4 <daniel.weise@concepts-and-training.de>
2021-02-26 10:27:42 +0100

    Setup UICore: 30234 use temp container for read structure

--- Services/UICore/classes/Setup/class.ilCtrlStructureStoredObjective.php
+++ Services/UICore/classes/Setup/class.ilCtrlStructureStoredObjective.php
@@ -1,6 +1,7 @@
 <?php declare(strict_types=1);
 
 use ILIAS\Setup;
+use ILIAS\DI;
 
 class ilCtrlStructureStoredObjective implements Setup\Objective
 {
@@ -71,9 +72,20 @@ class ilCtrlStructureStoredObjective implements Setup\Objective
             define("ILIAS_ABSOLUTE_PATH", dirname(__FILE__, 5));
         }
 
+        // ATTENTION: This is a total abomination. It only exists to allow various
+        // sub components of the various readers to run. This is a memento to the
+        // fact, that dependency injection is something we want. Currently, every
+        // component could just service locate the whole world via the global $DIC.
+        $DIC = $GLOBALS["DIC"];
+        $GLOBALS["DIC"] = new DI\Container();
+        $GLOBALS["DIC"]["ilDB"] = $db;
+
         $reader = $this->ctrl_reader->withDB($db);
         $reader->executed = false;
         $reader->readStructure(true);
+
+        $GLOBALS["DIC"] = $DIC;
+
         return $environment;
     }
 
