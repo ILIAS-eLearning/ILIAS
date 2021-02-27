27330971db0bf3af968c1ab6efb876b2982e3845
Timon Amstutz <timon.amstutz@bluewin.ch>
2021-02-27 09:47:09 +0100

    UI Components, Panels and Dropdowns, follow of PR #3210

--- src/UI/templates/js/Dropdown/dropdown.js
+++ src/UI/templates/js/Dropdown/dropdown.js
@@ -2,7 +2,6 @@
 	$(document).on('shown.bs.dropdown', function(event) {
 		var dropdown = $(event.target);
 		dropdown.find('.dropdown-toggle').attr('aria-expanded', true);
-		il.UI.page.fit(dropdown.find('.dropdown-menu'));
 	});
 
 	// on close
