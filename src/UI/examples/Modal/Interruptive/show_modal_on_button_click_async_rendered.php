ba15baa9dabdc0e992acffacb06211799588e554
Timon Amstutz <timon.amstutz@bluewin.ch>
2021-03-12 17:45:19 +0100

    UI Components : Code Format fixes, improvements from Feedback

--- src/UI/examples/Modal/Interruptive/show_modal_on_button_click_async_rendered.php
+++ src/UI/examples/Modal/Interruptive/show_modal_on_button_click_async_rendered.php
@@ -33,7 +33,8 @@ function show_modal_on_button_click_async_rendered()
     }
 
     // Display POST data of affected items in a panel
-    if (isset($_POST['interruptive_items']) && $_GET['modal_nr'] === '2') {
+    if (array_key_exists('interruptive_items', $_POST)
+        && array_key_exists('modal_nr', $_GET) && $_GET['modal_nr'] === '2') {
         $panel = $factory->panel()->standard(
             'Affected Items',
             $factory->legacy(print_r($_POST['interruptive_items'], true))
