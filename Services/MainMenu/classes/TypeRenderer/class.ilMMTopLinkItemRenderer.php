bca29d28b4deee8f707a4701d657a2d2e5ce3d7a
chfsx <fs@studer-raimann.ch>
2021-03-22 14:51:23 +0100

    [FIX] 0029319: Confusing behaviour of slate when jumping between repository positions and non-repository ones

---
+++ Services/MainMenu/classes/TypeRenderer/class.ilMMTopLinkItemRenderer.php
@@ -0,0 +1,20 @@
+<?php
+
+use ILIAS\UI\Component\Component;
+use ILIAS\GlobalScreen\Scope\MainMenu\Factory\isItem;
+use ILIAS\GlobalScreen\Scope\MainMenu\Collector\Renderer\TopLinkItemRenderer;
+
+/**
+ * Class ilMMTopLinkItemRenderer
+ * @author Fabian Schmid <fs@studer-raimann.ch>
+ */
+class ilMMTopLinkItemRenderer extends TopLinkItemRenderer
+{
+    use ilMMCloseOnClick;
+
+    public function getComponentWithContent(isItem $item) : Component
+    {
+        return parent::getComponentWithContent($this->addDisengageDecorator($item));
+    }
+
+}
