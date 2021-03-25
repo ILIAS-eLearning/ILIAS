a73b18d93c403ce9338b4f82bde4e81b091863f9
Stephan Winiker <stephan.winiker@hslu.ch>
2021-03-25 11:17:54 +0100

    Fix WebDAV Notification

--- Services/WebDAV/classes/dav/class.ilObjFileDAV.php
+++ Services/WebDAV/classes/dav/class.ilObjFileDAV.php
@@ -298,7 +298,7 @@ class ilObjFileDAV extends ilObjectDAV implements Sabre\DAV\IFile
                 break;
         }
 
-        $this->obj->notifyUpdate();
+        $this->obj->notifyUpdate($this->obj->getId());
     }
 
     /**
