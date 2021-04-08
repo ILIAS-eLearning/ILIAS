574a9493c7cf158a40d2746eef9d1b76739bde4a
smeyer <smeyer.ilias@gmx.de>
2021-04-08 13:12:40 +0200

    trac: replaced exits with exceptions

---
+++ Services/Tracking/exceptions/class.ilLPException.php
@@ -0,0 +1,10 @@
+<?php
+/* Copyright (c) 1998-2009 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+/**
+ * Base class for learning progress exceptions
+ */
+class ilLPException extends ilException
+{
+
+}
