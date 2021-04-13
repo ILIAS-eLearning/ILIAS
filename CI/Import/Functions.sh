52bb5aa8c7d893ec80b66d01355cbe7a024065c0
Laura Herzog <quellmalz.laura@gmail.com>
2021-04-13 07:15:12 +0200

    Outsourced changed file function

--- CI/Import/Functions.sh
+++ CI/Import/Functions.sh
@@ -3,3 +3,9 @@
 function printLn() {
 	echo -e "$PRE $1"
 }
+
+function get_changed_files() {
+  URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${PR_NUMBER}/files"
+  CHANGED_FILES=$(curl -s -X GET -G $URL | jq -r '.[] | .filename' | grep '.php')
+  echo ${CHANGED_FILES}
+}
