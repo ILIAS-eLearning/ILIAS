6a6c7cb0bc5eaf25c5e3bd28f1bb57b7ab235ec5
Alexander Killing <killing@leifos.de>
2021-04-16 16:10:49 +0200

    link file to Init maintenance

--- ilias.php
+++ ilias.php
@@ -1,16 +1,12 @@
 <?php
-/* Copyright (c) 1998-2009 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+/* Copyright (c) 1998-2021 ILIAS open source, GPLv3, see LICENSE */
 
 /**
-* ilias.php. main script.
-*
-* If you want to use this script your base class must be declared
-* within modules.xml.
-*
-* @author Alex Killing <alex.killing@gmx.de>
-* @version $Id$
-*
-*/
+ * ilias.php. main script.
+ * For changes please contact the maintainer in
+ * Services/Init/maintenance.json
+ */
 
 require_once("Services/Init/classes/class.ilInitialisation.php");
 ilInitialisation::initILIAS();
