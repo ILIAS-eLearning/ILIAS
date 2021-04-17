dc814c47115e30c975f620be1156d01dfdc51130
Uwe Kohnle <kohnle@internetlehrer-gmbh.de>
2021-04-17 22:49:50 +0000

    next fix for Articulate and Chrome

--- Modules/Scorm2004/scripts/rtemain/main.js
+++ Modules/Scorm2004/scripts/rtemain/main.js
@@ -1361,13 +1361,22 @@ function sendJSONRequest (url, data, callback, user, password, headers)
                     typeof(document.getElementById("res")) != "undefined" 
                     && typeof(document.getElementById("res").contentWindow) != "undefined" 
                     && typeof(document.getElementById("res").contentWindow.event) != "undefined" 
-                    && (document.getElementById("res").contentWindow.event.type=="unload" || document.getElementById("res").contentWindow.event.type=="beforeunload")
+                    && (document.getElementById("res").contentWindow.event.type=="unload" || document.getElementById("res").contentWindow.event.type=="beforeunload" || document.getElementById("res").contentWindow.event.type=="pagehide")
                    ) 
                 || (
                     typeof(window.event) != "undefined" 
                     && (window.event.type=="unload" || window.event.type=="beforeunload" || window.event.type=="click")
                    )
-                || ( //LM in frame
+                || (//LM in frame 1
+                    typeof(document.getElementById("res").contentWindow.document.getElementsByTagName("frame")[1]) != "undefined"
+                    && typeof(document.getElementById("res").contentWindow.document.getElementsByTagName("frame")[1].contentWindow) != "undefined"
+                    && typeof(document.getElementById("res").contentWindow.document.getElementsByTagName("frame")[1].contentWindow.event) != "undefined" 
+                    && (
+                        document.getElementById("res").contentWindow.document.getElementsByTagName("frame")[1].contentWindow.event.type=="unload" 
+                        || document.getElementById("res").contentWindow.document.getElementsByTagName("frame")[1].contentWindow.event.type=="beforeunload"
+                       )
+                )
+                || (//LM in frame 0
                     typeof(document.getElementById("res").contentWindow.document.getElementsByTagName("frame")[0]) != "undefined"
                     && typeof(document.getElementById("res").contentWindow.document.getElementsByTagName("frame")[0].contentWindow) != "undefined"
                     && typeof(document.getElementById("res").contentWindow.document.getElementsByTagName("frame")[0].contentWindow.event) != "undefined" 
@@ -1375,7 +1384,7 @@ function sendJSONRequest (url, data, callback, user, password, headers)
                         document.getElementById("res").contentWindow.document.getElementsByTagName("frame")[0].contentWindow.event.type=="unload" 
                         || document.getElementById("res").contentWindow.document.getElementsByTagName("frame")[0].contentWindow.event.type=="beforeunload"
                        )
-                   )
+                )
                 || ( //Articulate Rise
                     typeof(document.getElementById("res").contentWindow.document.getElementsByTagName("iframe")[1]) != "undefined" 
                     && typeof(document.getElementById("res").contentWindow.document.getElementsByTagName("iframe")[1].contentWindow) != "undefined" 
@@ -1383,6 +1392,16 @@ function sendJSONRequest (url, data, callback, user, password, headers)
                     && (
                         document.getElementById("res").contentWindow.document.getElementsByTagName("iframe")[1].contentWindow.event.type=="unload" 
                         || document.getElementById("res").contentWindow.document.getElementsByTagName("iframe")[1].contentWindow.event.type=="beforeunload"
+                       )
+                   )
+                || ( //Articulate Rise as SCORM 1.2 in 2004 Player
+                    typeof(document.getElementById("res").contentWindow.document.getElementsByTagName("frame")[0]) != "undefined" 
+                    && typeof(document.getElementById("res").contentWindow.document.getElementsByTagName("frame")[0].contentWindow.document.getElementsByTagName("iframe")[1]) != "undefined" 
+                    && typeof(document.getElementById("res").contentWindow.document.getElementsByTagName("frame")[0].contentWindow.document.getElementsByTagName("iframe")[1].contentWindow) != "undefined" 
+                    && typeof(document.getElementById("res").contentWindow.document.getElementsByTagName("frame")[0].contentWindow.document.getElementsByTagName("iframe")[1].contentWindow.event) != "undefined" 
+                    && (
+                        document.getElementById("res").contentWindow.document.getElementsByTagName("frame")[0].contentWindow.document.getElementsByTagName("iframe")[1].contentWindow.event.type=="unload" 
+                        || document.getElementById("res").contentWindow.document.getElementsByTagName("frame")[0].contentWindow.document.getElementsByTagName("iframe")[1].contentWindow.event.type=="beforeunload"
                        )
                    )
                 ) {
