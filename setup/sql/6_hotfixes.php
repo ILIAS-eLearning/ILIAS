9b07ba0a1b9d7e7b2391b8503711c3f43847b338
Uwe Kohnle <kohnle@internetlehrer-gmbh.de>
2021-04-18 21:43:31 +0000

    fix Mantis 28212

--- setup/sql/6_hotfixes.php
+++ setup/sql/6_hotfixes.php
@@ -1214,3 +1214,17 @@ if (!$ilDB->indexExistsByFields('exc_returned', array('filetitle'))) {
     $ilDB->addIndex('exc_returned', array('filetitle'), 'i3');
 }
 ?>
+<#48>
+<?php
+if ($ilDB->uniqueConstraintExists('cmi_gobjective', array('user_id','objective_id','scope_id'))) {
+    $ilDB->dropUniqueConstraintByFields('cmi_gobjective', array('user_id','objective_id','scope_id'));
+    $ilDB->addPrimaryKey('cmi_gobjective', array('user_id','objective_id','scope_id'));
+}
+?>
+<#49>
+<?php
+if ($ilDB->uniqueConstraintExists('cp_suspend', array('user_id','obj_id'))) {
+    $ilDB->dropUniqueConstraintByFields('cp_suspend', array('user_id','obj_id'));
+    $ilDB->addPrimaryKey('cp_suspend', array('user_id','obj_id'));
+}
+?>
