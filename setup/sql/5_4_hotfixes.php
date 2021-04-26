705a8e5ddebd35b265140b899e6e803bb5bcdeca
Uwe Kohnle <kohnle@internetlehrer-gmbh.de>
2021-04-26 09:37:51 +0000

    fix Mantis 30646

--- setup/sql/5_4_hotfixes.php
+++ setup/sql/5_4_hotfixes.php
@@ -1442,13 +1442,21 @@ if (!$ilDB->tableColumnExists('ldap_server_settings', 'escape_dn')) {
 <?php
 if ($ilDB->uniqueConstraintExists('cmi_gobjective', array('user_id','objective_id','scope_id'))) {
     $ilDB->dropUniqueConstraintByFields('cmi_gobjective', array('user_id','objective_id','scope_id'));
-    $ilDB->addPrimaryKey('cmi_gobjective', array('user_id','objective_id','scope_id'));
+}
+$query = "show index from cmi_gobjective where Key_name = 'PRIMARY'";
+$res = $ilDB->query($query);
+if (!$ilDB->numRows($res)) {
+    $ilDB->addPrimaryKey('cmi_gobjective', array('user_id', 'scope_id', 'objective_id'));
 }
 ?>
 <#105>
 <?php
 if ($ilDB->uniqueConstraintExists('cp_suspend', array('user_id','obj_id'))) {
     $ilDB->dropUniqueConstraintByFields('cp_suspend', array('user_id','obj_id'));
-    $ilDB->addPrimaryKey('cp_suspend', array('user_id','obj_id'));
+}
+$query = "show index from cp_suspend where Key_name = 'PRIMARY'";
+$res = $ilDB->query($query);
+if (!$ilDB->numRows($res)) {
+    $ilDB->addPrimaryKey('cp_suspend', array('user_id', 'obj_id'));
 }
 ?>
