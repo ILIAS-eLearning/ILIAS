2217972bc60f2acec408c7fc69b21edc574d9ac4
Alexander Killing <killing@leifos.de>
2021-05-03 12:11:47 +0200

    added index for booking_reservation table to improve issue #26388

--- setup/sql/dbupdate_05.php
+++ setup/sql/dbupdate_05.php
@@ -6542,3 +6542,15 @@ $ilDB->update("rbac_operations", [
     ]
 );
 ?>
+<#5786>
+<?php
+if (!$ilDB->indexExistsByFields('booking_reservation', array('date_from'))) {
+    $ilDB->addIndex('booking_reservation', array('date_from'), 'i3');
+}
+?>
+<#5787>
+<?php
+if (!$ilDB->indexExistsByFields('booking_reservation', array('date_to'))) {
+    $ilDB->addIndex('booking_reservation', array('date_to'), 'i4');
+}
+?>
