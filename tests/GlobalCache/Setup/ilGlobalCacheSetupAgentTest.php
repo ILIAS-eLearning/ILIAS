1f4da885e1b876809195e3a77e617833a7ea8482
chfsx <fs@studer-raimann.ch>
2021-05-12 18:28:20 +0200

    and finally fixed test

--- tests/GlobalCache/Setup/ilGlobalCacheSetupAgentTest.php
+++ tests/GlobalCache/Setup/ilGlobalCacheSetupAgentTest.php
@@ -104,11 +104,11 @@ class ilGlobalCacheSetupAgentTest extends TestCase
     public function testGetArrayToConfigTransformationWithDataServices() : void
     {
         $fnc = $this->obj->getArrayToConfigTransformation();
-
+    
         $services = [
-            "static",
-            "memcached",
-            "apc"
+            \ilGlobalCache::TYPE_STATIC => "static",
+            \ilGlobalCache::TYPE_MEMCACHED => "memcached",
+            \ilGlobalCache::TYPE_APC => "apc"
         ];
 
         $node = [
