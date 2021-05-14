4d228438627bc7ef4da7eb61b7759c6c8d1f8437
mjansen <mjansen@databay.de>
2021-05-14 17:00:13 +0200

    Environment: Add types and improvements

--- Services/Environment/classes/class.ilRuntime.php
+++ Services/Environment/classes/class.ilRuntime.php
@@ -8,10 +8,7 @@
  */
 final class ilRuntime
 {
-    /**
-     * @var self
-     */
-    private static $instance = null;
+    private static ?self $instance = null;
 
     /**
      * The runtime is a constant state during one request, so please use the public static getInstance() to instantiate the runtime
@@ -20,10 +17,7 @@ final class ilRuntime
     {
     }
 
-    /**
-     * @return self
-     */
-    public static function getInstance()
+    public static function getInstance() : self
     {
         if (self::$instance === null) {
             self::$instance = new self();
@@ -32,61 +26,40 @@ final class ilRuntime
         return self::$instance;
     }
 
-    /**
-     * Returns true when the runtime used is HHVM.
-     * @return bool
-     */
-    public function isHHVM()
+    public function isHHVM() : bool
     {
         return defined('HHVM_VERSION');
     }
 
-    /**
-     * Returns true when the runtime used is PHP.
-     * @return bool
-     */
-    public function isPHP()
+    public function isPHP() : bool
     {
         return !$this->isHHVM();
     }
 
-
-    /**
-     * @return bool
-     */
-    public function isFPM()
+    public function isFPM() : bool
     {
-        return (php_sapi_name() == 'fpm-fcgi');
+        return PHP_SAPI === 'fpm-fcgi';
     }
 
-    /**
-     * @return string
-     */
-    public function getVersion()
+    public function getVersion() : string
     {
         if ($this->isHHVM()) {
             return HHVM_VERSION;
-        } else {
-            return PHP_VERSION;
         }
+
+        return PHP_VERSION;
     }
 
-    /**
-     * @return string
-     */
-    public function getName()
+    public function getName() : string
     {
         if ($this->isHHVM()) {
             return 'HHVM';
-        } else {
-            return 'PHP';
         }
+
+        return 'PHP';
     }
 
-    /**
-     * A string representation of the runtime
-     */
-    public function __toString()
+    public function __toString() : string
     {
         return $this->getName() . ' ' . $this->getVersion();
     }
@@ -98,32 +71,26 @@ final class ilRuntime
     {
         if ($this->isHHVM()) {
             return ini_get('hhvm.log.runtime_error_reporting_level');
-        } else {
-            return ini_get('error_reporting');
         }
+
+        return ini_get('error_reporting');
     }
 
-    /**
-     * @return boolean
-     */
-    public function shouldLogErrors()
+    public function shouldLogErrors() : bool
     {
         if ($this->isHHVM()) {
             return (bool) ini_get('hhvm.log.use_log_file');
-        } else {
-            return (bool) ini_get('log_errors');
         }
+
+        return (bool) ini_get('log_errors');
     }
 
-    /**
-     * @return boolean
-     */
-    public function shouldDisplayErrors()
+    public function shouldDisplayErrors() : bool
     {
         if ($this->isHHVM()) {
             return (bool) ini_get('hhvm.debug.server_error_message');
-        } else {
-            return (bool) ini_get('display_errors');
         }
+
+        return (bool) ini_get('display_errors');
     }
 }
