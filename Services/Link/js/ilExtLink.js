18c8c0c13b5974f939b3af0da27ddb2d86ef9c76
Alexander Killing <killing@leifos.de>
2021-05-18 09:28:33 +0200

    fixed bug #30476: URL auto linking: “Overeagerness” preventable?

--- Services/Link/js/ilExtLink.js
+++ Services/Link/js/ilExtLink.js
@@ -8,7 +8,12 @@ il.ExtLink = {
 	 * Linkify wrapper
 	 */
 	autolink: function (selector, link_class) {
-		$(selector).linkify();
+		$(selector).linkify({
+			validate: {
+				url: (val) => /^https?:\/\//.test(val), // only allow URLs that begin with a protocol
+				email: false // don't linkify emails
+			}
+		});
 		if (typeof link_class !== "undefined") {
 			$(selector + " a.linkified").addClass(link_class);
 		}
