4566ea9e5d9d7abf356307fcb08bda0a339fc1d5
Nils Haagen <nhaagen@cat06.de>
2021-05-20 12:26:40 +0200

    LSO: #30445 - show start-button regardless of available contents

--- Modules/LearningSequence/classes/Player/class.ilObjLearningSequenceLearnerGUI.php
+++ Modules/LearningSequence/classes/Player/class.ilObjLearningSequenceLearnerGUI.php
@@ -16,7 +16,6 @@ class ilObjLearningSequenceLearnerGUI
 
     public function __construct(
         int $ls_ref_id,
-        bool $has_items,
         $first_access,
         int $usr_id,
         ilAccess $access,
@@ -33,7 +32,6 @@ class ilObjLearningSequenceLearnerGUI
     ) {
         $this->ls_object = $ls_object;
         $this->ls_ref_id = $ls_ref_id;
-        $this->has_items = $has_items;
         $this->first_access = $first_access;
         $this->usr_id = $usr_id;
         $this->access = $access;
@@ -123,38 +121,32 @@ class ilObjLearningSequenceLearnerGUI
     {
         $is_member = $this->roles->isMember($this->usr_id);
         $completed = $this->roles->isCompletedByUser($this->usr_id);
-        $has_items = $this->has_items;
 
         if (!$is_member) {
-            if ($has_items) {
-                $may_subscribe = $this->userMayJoin();
-                if ($may_subscribe) {
-                    $this->toolbar->addButton(
-                        $this->lng->txt("lso_player_start"),
-                        $this->ctrl->getLinkTarget($this, self::CMD_START)
-                    );
-                }
+            $may_subscribe = $this->userMayJoin();
+            if ($may_subscribe) {
+                $this->toolbar->addButton(
+                    $this->lng->txt("lso_player_start"),
+                    $this->ctrl->getLinkTarget($this, self::CMD_START)
+                );
             }
         } else {
             if (!$completed) {
-                if ($has_items) {
-                    $label = "lso_player_resume";
-                    if ($this->first_access === -1) {
-                        $label = "lso_player_start";
-                    }
+                $label = "lso_player_resume";
+                if ($this->first_access === -1) {
+                    $label = "lso_player_start";
+                }
 
-                    $this->toolbar->addButton(
-                        $this->lng->txt($label),
-                        $this->ctrl->getLinkTarget($this, self::CMD_VIEW)
-                    );
-                }
+                $this->toolbar->addButton(
+                    $this->lng->txt($label),
+                    $this->ctrl->getLinkTarget($this, self::CMD_VIEW)
+                );
             } else {
-                if ($has_items) {
-                    $this->toolbar->addButton(
-                        $this->lng->txt("lso_player_review"),
-                        $this->ctrl->getLinkTarget($this, self::CMD_VIEW)
-                    );
-                }
+                $this->toolbar->addButton(
+                    $this->lng->txt("lso_player_review"),
+                    $this->ctrl->getLinkTarget($this, self::CMD_VIEW)
+                );
+
                 if ($cmd === self::CMD_STANDARD) {
                     $this->toolbar->addButton(
                         $this->lng->txt("lso_player_extro"),
@@ -216,16 +208,20 @@ class ilObjLearningSequenceLearnerGUI
 
         switch ($response) {
             case null:
-                //render the page
                 $this->tpl->enableDragDropFileUpload(null);
                 $this->tpl->setContent('THIS SHOULD NOT SHOW');
                 return;
+            
+            case ilLSPlayer::RET_NOITEMS:
+                \ilUtil::sendInfo($this->lng->txt('container_no_items'));
+                $this->tpl->setContent('');
+                return;
 
-            case 'EXIT::' . $this->player::LSO_CMD_FINISH:
+            case ilLSPlayer::RET_EXIT . ilLSPlayer::LSO_CMD_FINISH:
                 $cmd = self::CMD_EXTRO;
                 break;
 
-            case 'EXIT::' . $this->player::LSO_CMD_SUSPEND:
+            case ilLSPlayer::RET_EXIT . ilLSPlayer::LSO_CMD_SUSPEND:
             default:
                 $cmd = self::CMD_STANDARD;
                 break;
