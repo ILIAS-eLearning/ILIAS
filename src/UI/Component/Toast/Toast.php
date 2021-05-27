26a8f4b9de910a3a7882a6079616b58a10644f41
iszmais <iszmais@databay.de>
2021-05-27 12:48:41 +0200

    reduce basic actions functionality

--- src/UI/Component/Toast/Toast.php
+++ src/UI/Component/Toast/Toast.php
@@ -51,23 +51,23 @@ interface Toast extends Component
     /**
      * Create a copy of this toast with an url, which is called when the item title is clicked.
      */
-    public function withTitleAction(string|Signal|Closure $action) : Toast;
+    public function withTitleAction(string|Closure $action) : Toast;
 
     /**
      * Get the url, which is called when the user clicks the item title.
      */
-    public function getTitleAction() : string|Signal|Closure;
+    public function getTitleAction() : string|Closure;
 
     /**
      * Create a copy of this toast with an url, which is called asynchronous when the item vanishes.
      * This action will not trigger if the vanishing is provoked by the user by interacting with the toast.
      */
-    public function withVanishAction(string|Signal|Closure $action) : Toast;
+    public function withVanishAction(string $action) : Toast;
 
     /**
      * Get the url, which is called when the item vanishes without user interaction.
      */
-    public function getVanishAction() : string|Signal|Closure;
+    public function getVanishAction() : string;
 
     /**
      * Get icon.
