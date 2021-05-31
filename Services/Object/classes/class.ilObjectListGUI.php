8c4eef059b81f7885d39004e0fa7b9b7b3fb09db
Alexander Killing <killing@leifos.de>
2021-05-31 11:35:56 +0200

    fixed bug #30732: Kachelansicht-> Pulldowmenü -> Eintrag Open

--- Services/Object/classes/class.ilObjectListGUI.php
+++ Services/Object/classes/class.ilObjectListGUI.php
@@ -3883,6 +3883,8 @@ class ilObjectListGUI
         $def_command = $this->getDefaultCommand();
 
         if ($def_command["frame"] != "") {
+            /* this seems to be introduced due to #25624, but does not fix it
+                removed with ##30732
             $button =
                 $ui->factory()->button()->shy("Open", "")->withAdditionalOnLoadCode(function ($id) use ($def_command) {
                     return
@@ -3892,7 +3894,7 @@ class ilObjectListGUI
                             $def_command["link"]
                         ) . "', '" . $def_command["frame"] . "');});";
                 });
-            $actions[] = $button;
+            $actions[] = $button;*/
         }
         $dropdown = $ui->factory()->dropdown()->standard($actions);
 
