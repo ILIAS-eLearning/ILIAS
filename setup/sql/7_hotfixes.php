1588e55db8f0d7c79b442ad296b9438af630ff7a
smeyer <smeyer.ilias@gmx.de>
2021-06-09 10:21:41 +0200

    Fixed 0028212: Galera cluster: no primary key in some tables

--- setup/sql/7_hotfixes.php
+++ setup/sql/7_hotfixes.php
@@ -822,3 +822,9 @@ if (!$ilDB->indexExistsByFields('booking_reservation', array('date_to'))) {
     $ilDB->addIndex('booking_reservation', array('date_to'), 'i4');
 }
 ?>
+<#45>
+<?php
+if (!$ilDB->indexExistsByFields('il_meta_oer_stat', ['obj_id'])) {
+    $ilDB->addPrimaryKey('il_meta_oer_stat', ['obj_id']);
+}
+?>
