83a1fe6a86461955c3016d7b5670bf53fe8ff6f6
Richard Klees <richard.klees@concepts-and-training.de>
2021-06-10 14:20:05 +0200

    IASS: remove tests depending on init

--- Modules/IndividualAssessment/test/ilModulesIndividualAssessmentSuite.php
+++ Modules/IndividualAssessment/test/ilModulesIndividualAssessmentSuite.php
@@ -8,10 +8,8 @@ class ilModulesIndividualAssessmentSuite extends TestSuite
     {
         $suite = new ilModulesIndividualAssessmentSuite();
         require_once("./Modules/IndividualAssessment/test/ilIndividualAssessmentSettingsTest.php");
-        require_once("./Modules/IndividualAssessment/test/ilIndividualAssessmentMembersTest.php");
         require_once("./Modules/IndividualAssessment/test/ilIndividualAssessmentUserGradingTest.php");
         $suite->addTestSuite('ilindividualAssessmentSettingsTest');
-        $suite->addTestSuite('ilIndividualAssessmentMembersTest');
         $suite->addTestSuite('ilIndividualAssessmentUserGradingTest');
         return $suite;
     }
