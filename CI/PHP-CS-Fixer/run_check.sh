7feb45739c683e57540ab41110d2553364337000
Laura Herzog <quellmalz.laura@gmail.com>
2021-06-14 09:42:11 +0200

    CI: Check if file exists before testing it

--- CI/PHP-CS-Fixer/run_check.sh
+++ CI/PHP-CS-Fixer/run_check.sh
@@ -16,13 +16,16 @@ else
   CHANGED_FILES=$(get_changed_files)
   for FILE in ${CHANGED_FILES}
   do
-  	echo "Check file: ${FILE}"
-  	RUNCSFIXER=$(libs/composer/vendor/bin/php-cs-fixer fix --using-cache=no --diff --config=./CI/PHP-CS-Fixer/code-format.php_cs ${FILE})
-  	RESULT=$?
-  	if [[ ${RESULT} -ne 0 ]]
+  	if [ -f ${FILE} ]
   	then
-  		exit ${RESULT}
-    fi
+	  	echo "Check file: ${FILE}"
+	  	RUNCSFIXER=$(libs/composer/vendor/bin/php-cs-fixer fix --using-cache=no --diff --config=./CI/PHP-CS-Fixer/code-format.php_cs ${FILE})
+	  	RESULT=$?
+	  	if [[ ${RESULT} -ne 0 ]]
+	  	then
+	  		exit ${RESULT}
+	    fi
+	  fi
   done
   exit 0
 fi
