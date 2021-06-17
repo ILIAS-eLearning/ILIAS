fcd508ddf88882108d97795cb1d08a69111c5c41
Richard Klees <richard.klees@concepts-and-training.de>
2021-06-17 14:55:41 +0200

    Services/Component: fix coding style

--- Services/Component/classes/class.ilPluginsOverviewTableFilterGUI.php
+++ Services/Component/classes/class.ilPluginsOverviewTableFilterGUI.php
@@ -29,24 +29,24 @@ class ilPluginsOverviewTableFilterGUI
     public function __construct(ilObjComponentSettingsGUI $parent)
     {
         global $DIC;
-        $this->renderer       = $DIC->ui()->renderer();
+        $this->renderer = $DIC->ui()->renderer();
         $this->filter_service = $DIC->uiService()->filter();
-        $field_factory        = $DIC->ui()->factory()->input()->field();
-        $txt                  = static function (string $id) use ($DIC): string {
+        $field_factory = $DIC->ui()->factory()->input()->field();
+        $txt = static function (string $id) use ($DIC) : string {
             return $DIC->language()->txt($id);
         };
 
         $all_slots = ilPluginSlot::getAllSlots();
         array_walk($all_slots, static function (array $d) use (&$slots, &$components, &$slot_ids) {
-            $slots[$d['slot_name']]                                        = $d['slot_name'];
-            $slot_ids[$d['slot_id']]                                       = $d['slot_id'];
+            $slots[$d['slot_name']] = $d['slot_name'];
+            $slot_ids[$d['slot_id']] = $d['slot_id'];
             $components[$d['component_type'] . '/' . $d['component_name']] = $d['component_type'] . '/' . $d['component_name'];
         });
 
-        $inputs       = [
-            ilPluginsOverviewTableGUI::F_PLUGIN_NAME    => $field_factory->text($txt('cmps_plugin')),
-            ilPluginsOverviewTableGUI::F_PLUGIN_ID      => $field_factory->text($txt('id')),
-            ilPluginsOverviewTableGUI::F_SLOT_NAME      => $field_factory->multiSelect($txt('cmps_plugin_slot'), $slots)->withValue($slots),
+        $inputs = [
+            ilPluginsOverviewTableGUI::F_PLUGIN_NAME => $field_factory->text($txt('cmps_plugin')),
+            ilPluginsOverviewTableGUI::F_PLUGIN_ID => $field_factory->text($txt('id')),
+            ilPluginsOverviewTableGUI::F_SLOT_NAME => $field_factory->multiSelect($txt('cmps_plugin_slot'), $slots)->withValue($slots),
             ilPluginsOverviewTableGUI::F_COMPONENT_NAME => $field_factory->multiSelect($txt('cmps_component'), $components)->withValue($components),
             ilPluginsOverviewTableGUI::F_PLUGIN_ACTIVE => $field_factory->select($txt('active'), [-1 => $txt('inactive'), 1 => $txt('active')]),
         ];
@@ -62,7 +62,8 @@ class ilPluginsOverviewTableFilterGUI
                 true,
             ],
             true,
-            true);
+            true
+        );
     }
 
     public function getHTML() : string
