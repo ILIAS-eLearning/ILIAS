eccce382cdaea3f9a903b6e819fdb505bbf655ef
Nils Haagen <nhaagen@cat06.de>
2021-06-18 12:03:16 +0200

    UI: introduce Url- and Link Input - LinkInput returns a datatype 'Link' - label might be emty string

---
+++ src/UI/Component/Input/Field/Link.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+/* Copyright (c) 2021 Nils Haagen <nils.haagen@concepts-and-training.de> Extended GPL, see docs/LICENSE */
+
+namespace ILIAS\UI\Component\Input\Field;
+
+/**
+ * This describes the Link input, a combination of Text and Url.
+ */
+interface Link extends Group
+{
+}
