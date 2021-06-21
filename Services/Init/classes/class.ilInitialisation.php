cabdbfd32525ce8f77e44c82090d27d329ff8bd7
chfsx <fs@studer-raimann.ch>
2021-06-21 13:44:25 +0200

    fixed ilInitialisation

--- Services/Init/classes/class.ilInitialisation.php
+++ Services/Init/classes/class.ilInitialisation.php
@@ -1806,7 +1806,7 @@ class ilInitialisation
         };
 
         $c["bt.persistence"] = function ($c) {
-            return new \ILIAS\BackgroundTasks\Implementation\Persistence\BasicPersistence();
+            return \ILIAS\BackgroundTasks\Implementation\Persistence\BasicPersistence::instance();
         };
 
         $c["bt.injector"] = function ($c) {
