fa443dbfe2f82419bd7fe936d97f41acc5be4161
Richard Klees <richard.klees@concepts-and-training.de>
2021-07-08 14:50:56 +0200

    Services/Component: add queries for plugins to pluginslot

--- Services/Component/classes/class.ilPluginSlotInfo.php
+++ Services/Component/classes/class.ilPluginSlotInfo.php
@@ -8,15 +8,21 @@ class ilPluginSlotInfo
     protected ilComponentInfo $component;
     protected string $id;
     protected string $name;
+    /**
+     * @var ilPluginInfo[]
+     */
+    protected array $plugins;
 
     public function __construct(
         ilComponentInfo $component,
         string $id,
-        string $name
+        string $name,
+        array &$plugins
     ) {
         $this->component = $component;
         $this->id = $id;
         $this->name = $name;
+        $this->plugins = &$plugins;
     }
 
     public function getComponent() : ilComponentInfo
@@ -37,5 +43,33 @@ class ilPluginSlotInfo
     public function getQualifiedName() : string
     {
         return $this->component->getQualifiedName() . "/" . $this->getName();
+    }
+
+    /**
+     * @return Iterator <ilPluginInfo>
+     */
+    public function getPlugins() : Iterator
+    {
+        foreach ($this->plugins as $id => $plugin) {
+            yield $id => $plugin;
+        }
+    }
+
+    public function hasPlugin(string $id) : bool
+    {
+        return isset($this->plugins[$id]);
+    }
+
+    /**
+     * @throws \InvalidArgumentException if plugin does not exist
+     */
+    public function getPlugin(string $id) : \ilPluginInfo
+    {
+        if (!$this->hasPlugin($id)) {
+            throw new \InvalidArgumentException(
+                "No plugin $id in slot {$this->getQualifiedName()}."
+            );
+        }
+        return $this->plugins[$id];
     }
 }
--- Services/Component/test/ilPluginSlotInfoTest.php
+++ Services/Component/test/ilPluginSlotInfoTest.php
@@ -16,11 +16,52 @@ class ilPluginSlotInfoTest extends TestCase
             $slots
         );
 
+        $plugins = [];
         $this->pluginslot = new ilPluginSlotInfo(
             $this->component,
             "slt1",
-            "Slot1"
+            "Slot1",
+            $plugins
         );
+
+        $v = $this->createMock(\ILIAS\Data\Version::class);
+        $this->plugin1 = new ilPluginInfo(
+            $this->pluginslot,
+            "plg1",
+            "",
+            true,
+            $v,
+            0,
+            $v,
+            0,
+            $v,
+            $v,
+            "",
+            "",
+            false,
+            false,
+            false
+        );
+        $plugins["plg1"] = $this->plugin1;
+
+        $this->plugin2 = new ilPluginInfo(
+            $this->pluginslot,
+            "plg2",
+            "",
+            true,
+            $v,
+            0,
+            $v,
+            0,
+            $v,
+            $v,
+            "",
+            "",
+            false,
+            false,
+            false
+        );
+        $plugins["plg2"] = $this->plugin2;
     }
 
     public function testGetter()
@@ -30,5 +71,32 @@ class ilPluginSlotInfoTest extends TestCase
         $this->assertEquals("slt1", $this->pluginslot->getId());
         $this->assertEquals("Slot1", $this->pluginslot->getName());
         $this->assertEquals("Modules/Module1/Slot1", $this->pluginslot->getQualifiedName());
+    }
+
+    public function testGetPlugins()
+    {
+        $plugins = iterator_to_array($this->pluginslot->getPlugins());
+        $this->assertEquals(2, count($plugins));
+        $this->assertEquals($this->plugin1, $plugins["plg1"]);
+        $this->assertEquals($this->plugin2, $plugins["plg2"]);
+    }
+
+    public function testHasPlugin()
+    {
+        $this->assertTrue($this->pluginslot->hasPlugin("plg1"));
+        $this->assertTrue($this->pluginslot->hasPlugin("plg2"));
+        $this->assertFalse($this->pluginslot->hasPlugin("plg3"));
+    }
+
+    public function testGetPlugin()
+    {
+        $this->assertEquals($this->plugin1, $this->pluginslot->getPlugin("plg1"));
+        $this->assertEquals($this->plugin2, $this->pluginslot->getPlugin("plg2"));
+    }
+
+    public function testGetUnknownPlugin()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->pluginslot->getPlugin("plg3");
     }
 }
