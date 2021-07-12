177ed5457fbee79e71e632aa695f7f3b7fc2eb9b
Richard Klees <richard.klees@concepts-and-training.de>
2021-07-12 16:08:01 +0200

    Services/Component: ilPluginInfo has methods to query state

--- Services/Component/test/ilPluginInfoTest.php
+++ Services/Component/test/ilPluginInfoTest.php
@@ -55,7 +55,6 @@ class ilPluginInfoTest extends TestCase
         $this->assertEquals("plg1", $this->plugin->getId());
         $this->assertEquals("Plugin1", $this->plugin->getName());
         $this->assertTrue($this->plugin->isActivated());
-        $this->assertTrue($this->plugin->isActive());
         $this->assertEquals($this->data_factory->version("1.0.0"), $this->plugin->getCurrentVersion());
         $this->assertEquals(12, $this->plugin->getCurrentDBVersion());
         $this->assertEquals($this->data_factory->version("1.0.0"), $this->plugin->getAvailableVersion());
@@ -67,5 +66,91 @@ class ilPluginInfoTest extends TestCase
         $this->assertTrue($this->plugin->supportsLearningProgress());
         $this->assertFalse($this->plugin->supportsExport());
         $this->assertTrue($this->plugin->supportsCLISetup());
+    }
+
+    public function testIsInstalled()
+    {
+        $this->assertTrue($this->plugin->isInstalled());
+    }
+
+    public function testIsNotInstalled()
+    {
+        $this->plugin = new ilPluginInfo(
+            $this->pluginslot,
+            "plg1",
+            "Plugin1",
+            true,
+            null,
+            null,
+            $this->data_factory->version("1.0.0"),
+            12,
+            $this->data_factory->version("6.0"),
+            $this->data_factory->version("6.99"),
+            "Richard Klees",
+            "richard.klees@concepts-and-training.de",
+            true,
+            false,
+            true
+        );
+
+        $this->assertFalse($this->plugin->isInstalled());
+    }
+
+    public function testUpdateIsNotRequired()
+    {
+        $this->assertFalse($this->plugin->isUpdateRequired());
+    }
+
+    public function testUpdateIsNotRequiredNotInstalled()
+    {
+        $this->plugin = new ilPluginInfo(
+            $this->pluginslot,
+            "plg1",
+            "Plugin1",
+            true,
+            null,
+            null,
+            $this->data_factory->version("1.0.0"),
+            12,
+            $this->data_factory->version("6.0"),
+            $this->data_factory->version("6.99"),
+            "Richard Klees",
+            "richard.klees@concepts-and-training.de",
+            true,
+            false,
+            true
+        );
+
+        $this->assertFalse($this->plugin->isUpdateRequired());
+    }
+
+    public function testUpdateIsRequired()
+    {
+        $this->plugin = new ilPluginInfo(
+            $this->pluginslot,
+            "plg1",
+            "Plugin1",
+            true,
+            $this->data_factory->version("2.0.0"),
+            11,
+            $this->data_factory->version("1.0.0"),
+            12,
+            $this->data_factory->version("6.0"),
+            $this->data_factory->version("6.99"),
+            "Richard Klees",
+            "richard.klees@concepts-and-training.de",
+            true,
+            false,
+            true
+        );
+
+        $this->assertTrue($this->plugin->isUpdateRequired());
+    }
+
+    public function testIsCompliantToILIAS()
+    {
+        $this->assertFalse($this->plugin->isCompliantToILIAS($this->data_factory->version("5.4")));
+        $this->assertTrue($this->plugin->isCompliantToILIAS($this->data_factory->version("6.5")));
+        $this->assertFalse($this->plugin->isCompliantToILIAS($this->data_factory->version("7.1")));
     }
 }
