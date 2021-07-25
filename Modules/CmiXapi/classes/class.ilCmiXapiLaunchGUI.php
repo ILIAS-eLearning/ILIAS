6a80a9c1dec074963367908b5158be96e87b3a9f
Uwe Kohnle <kohnle@internetlehrer-gmbh.de>
2021-07-25 16:51:20 +0000

    fix for launchlink

--- Modules/CmiXapi/classes/class.ilCmiXapiLaunchGUI.php
+++ Modules/CmiXapi/classes/class.ilCmiXapiLaunchGUI.php
@@ -60,12 +60,16 @@ class ilCmiXapiLaunchGUI
         if ($this->object->getSourceType() == ilObjCmiXapi::SRC_TYPE_REMOTE) {
             $launchLink = $this->object->getLaunchUrl();
         } elseif ($this->object->getSourceType() == ilObjCmiXapi::SRC_TYPE_LOCAL) {
-            $launchLink = implode('/', [
-                ILIAS_HTTP_PATH, ilUtil::getWebspaceDir(),
-                ilCmiXapiContentUploadImporter::RELATIVE_CONTENT_DIRECTORY_NAMEBASE . $this->object->getId()
-            ]);
+            if (preg_match("/^(https?:\/\/)/",$this->object->getLaunchUrl()) == 1) {
+                $launchLink = $this->object->getLaunchUrl();
+            } else {
+                $launchLink = implode('/', [
+                    ILIAS_HTTP_PATH, ilUtil::getWebspaceDir(),
+                    ilCmiXapiContentUploadImporter::RELATIVE_CONTENT_DIRECTORY_NAMEBASE . $this->object->getId()
+                ]);
 
-            $launchLink .= DIRECTORY_SEPARATOR . $this->object->getLaunchUrl();
+                $launchLink .= DIRECTORY_SEPARATOR . $this->object->getLaunchUrl();
+            }
         }
         
         foreach ($this->getLaunchParameters() as $paramName => $paramValue) {
