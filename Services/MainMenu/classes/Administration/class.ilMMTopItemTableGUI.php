38610fded7127f4109087e8ff7f2ee223c583f0e
chfsx <fs@studer-raimann.ch>
2021-08-09 12:48:16 +0200

    [FIX] 0030983: wrong view after deleting a main menu entry

--- Services/MainMenu/classes/Administration/class.ilMMTopItemTableGUI.php
+++ Services/MainMenu/classes/Administration/class.ilMMTopItemTableGUI.php
@@ -98,7 +98,7 @@ class ilMMTopItemTableGUI extends ilTable2GUI
             $rendered_modal = "";
             if ($item_facade->isDeletable()) {
                 $ditem = $factory->modal()->interruptiveItem($this->hash($a_set['identification']), $item_facade->getDefaultTitle());
-                $action = $this->ctrl->getFormActionByClass(ilMMSubItemGUI::class, ilMMSubItemGUI::CMD_DELETE);
+                $action = $this->ctrl->getFormActionByClass(ilMMTopItemGUI::class, ilMMTopItemGUI::CMD_DELETE);
                 $m = $factory->modal()
                                   ->interruptive($this->lng->txt(ilMMTopItemGUI::CMD_DELETE), $this->lng->txt(ilMMTopItemGUI::CMD_CONFIRM_DELETE), $action)
                                   ->withAffectedItems([$ditem]);
