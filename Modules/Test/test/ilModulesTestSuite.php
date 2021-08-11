4f04a61fbdc880e554ee97c879ffc402732ff5bf
Marvin Beym <mBeym@databay.de>
2021-08-11 08:10:21 +0200

    Adjusted ModulesTestSuite to load all tests in folder automatically

--- Modules/Test/test/ilModulesTestSuite.php
+++ Modules/Test/test/ilModulesTestSuite.php
@@ -2,6 +2,9 @@
 /* Copyright (c) 1998-2013 ILIAS open source, Extended GPL, see docs/LICENSE */
 
 use PHPUnit\Framework\TestSuite;
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . "/ilTestBaseClass.php";
 
 class ilModulesTestSuite extends TestSuite
 {
@@ -16,16 +19,39 @@ class ilModulesTestSuite extends TestSuite
         }
 
         $suite = new ilModulesTestSuite();
-    
-        include_once("./Modules/Test/test/ilassMarkTest.php");
-        $suite->addTestSuite("ilassMarkTest");
 
-        include_once("./Modules/Test/test/ilassMarkSchemaTest.php");
-        $suite->addTestSuite("ilassMarkSchemaTest");
+        foreach (new RegExIterator(
+                     new RecursiveIteratorIterator(
+                         new RecursiveDirectoryIterator(__DIR__, FilesystemIterator::SKIP_DOTS),
+                         RecursiveIteratorIterator::LEAVES_ONLY
+                     ),
+                     '/BaseTest\.php$/'
+                 ) as $file) {
+            /** @var SplFileInfo $file */
+            require_once $file->getPathname();
+        }
 
-        include_once("./Modules/Test/test/ilTestFinalMarkLangVarBuilderTest.php");
-        $suite->addTestSuite("ilTestFinalMarkLangVarBuilderTest");
-                
+        foreach (new RegExIterator(
+                     new RecursiveIteratorIterator(
+                         new RecursiveDirectoryIterator(__DIR__, FilesystemIterator::SKIP_DOTS),
+                         RecursiveIteratorIterator::LEAVES_ONLY
+                     ),
+                     '/(?<!Base)Test\.php$/'
+                 ) as $file) {
+            /** @var SplFileInfo $file */
+            require_once $file->getPathname();
+
+            $className = preg_replace('/(.*?)(\.php)/', '$1', $file->getBasename());
+            if (class_exists($className)) {
+                $reflection = new ReflectionClass($className);
+                if (
+                    !$reflection->isAbstract() &&
+                    !$reflection->isInterface() &&
+                    $reflection->isSubclassOf(TestCase::class)) {
+                    $suite->addTestSuite($className);
+                }
+            }
+        }
         return $suite;
     }
 }
