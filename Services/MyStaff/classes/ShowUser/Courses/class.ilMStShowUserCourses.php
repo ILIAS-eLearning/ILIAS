43c7755c202aff49b49e85cccd08fc5929cdb837
jeph864 <jephte.abijuru@minervis.com>
2021-09-02 16:32:20 +0200

    show enrolments for a single user

--- Services/MyStaff/classes/ShowUser/Courses/class.ilMStShowUserCourses.php
+++ Services/MyStaff/classes/ShowUser/Courses/class.ilMStShowUserCourses.php
@@ -10,4 +10,25 @@ use ILIAS\MyStaff\ListCourses\ilMStListCourses;
  */
 class ilMStShowUserCourses extends ilMStListCourses
 {
+    /**
+     * @param array  $arr_filter
+     * @return string
+     */
+    protected  function createWhereStatement(array $arr_filter)
+    {
+        global $DIC;
+
+        if (!$arr_filter['usr_id']) {
+            return '';
+        }
+
+        $where = parent::createWhereStatement($arr_filter);
+        $usr_filter = "a_table.usr_id = " . $DIC->database()->quote($arr_filter['usr_id'], 'integer');
+
+        if (empty($where)) {
+            return ' WHERE ' . $usr_filter;
+        } else {
+            return $where . ' AND ' . $usr_filter;
+        }
+    }
 }
