a016b9205ffe136b5fd120624f1e5a7fc90e2b64
Timon Amstutz <timon.amstutz@bluewin.ch>
2021-09-09 09:26:53 +0200

    Improved footer example, see #31324

--- src/UI/examples/MainControls/Footer/footer.php
+++ src/UI/examples/MainControls/Footer/footer.php
@@ -1,5 +1,21 @@
 <?php
+
 function footer()
+{
+    global $DIC;
+    $f = $DIC->ui()->factory();
+    $renderer = $DIC->ui()->renderer();
+
+    $url = 'src/UI/examples/MainControls/Footer/footer.php?new_footer_ui=1';
+
+    $page_demo = $f->button()->primary('See UI in fullscreen-mode', $url);
+
+    return $renderer->render([
+        $page_demo
+    ]);
+}
+
+function pageFooterDemoFooter($f)
 {
     global $DIC;
     $f = $DIC->ui()->factory();
@@ -9,23 +25,80 @@ function footer()
     $text = 'Additional info:';
     $links = [];
     $links[] = $f->link()->standard("Goto ILIAS", "http://www.ilias.de");
-    $links[] = $f->link()->standard("Goto ILIAS", "http://www.ilias.de");
+    $links[] = $f->link()->standard("Goto Mantis", "http://mantis.ilias.de");
 
-    $footer = $f->mainControls()->footer($links, $text)
-        ->withPermanentURL(
-            $df->uri(
-                isset($_SERVER['REQUEST_SCHEME']) ?  $_SERVER['REQUEST_SCHEME']:"http" .
-                '://' .
-                    (isset($_SERVER['SERVER_NAME']) ?  $_SERVER['SERVER_NAME']:"localhost") .
-                ':' .
-                    (isset($_SERVER['SERVER_PORT']) ?  $_SERVER['SERVER_PORT']:"80") .
-                '/' . ltrim(str_replace(
-                    'ilias.php',
-                    'goto.php?target=xxx12345',
-                    isset($_SERVER['SCRIPT_NAME']) ?  $_SERVER['SCRIPT_NAME']:""
-                ), '/')
-            )
-        );
-
-    return $renderer->render($footer);
+    return $f->mainControls()->footer($links, $text)->withPermanentURL(
+        $df->uri(
+            (isset($_SERVER['REQUEST_SCHEME']) ?  $_SERVER['REQUEST_SCHEME']:"http") . '://'
+            . (isset($_SERVER['SERVER_NAME']) ?  $_SERVER['SERVER_NAME']:"localhost") . ':'
+            . (isset($_SERVER['SERVER_PORT']) ?  $_SERVER['SERVER_PORT']:"80")
+            . (isset($_SERVER['SCRIPT_NAME']) ?  $_SERVER['SCRIPT_NAME']:"")
+            . "?new_footer_ui=1"
+        )
+    );
 }
+
+
+if ($_GET['new_footer_ui'] == '1') {
+    chdir('../../../../../');
+    require_once('src/UI/examples/Layout/Page/Standard/ui.php');
+    _initIliasForPreview();
+
+    global $DIC;
+
+    $f = $DIC->ui()->factory();
+    $renderer = $DIC->ui()->renderer();
+    $logo = $f->image()->responsive("templates/default/images/HeaderIcon.svg", "ILIAS");
+    $breadcrumbs = pageFooterDemoCrumbs($f);
+    $metabar = pageFooterDemoMetabar($f);
+    $mainbar = pageFooterDemoMainbar($f, $renderer);
+    $footer = pageFooterDemoFooter($f);
+
+    $entries = $mainbar->getEntries();
+    $tools = $mainbar->getToolEntries();
+    $content = pageFooterDemoContent($f, $renderer, $mainbar);
+
+    $page = $f->layout()->page()->standard(
+        $content,
+        $metabar,
+        $mainbar,
+        $breadcrumbs,
+        $logo,
+        $footer,
+        'UI PAGE FOOTER DEMO', //page title
+        'ILIAS', //short title
+        'Std. Page Footer Demo' //view title
+    )->withUIDemo(true);
+
+    echo $renderer->render($page);
+}
+
+
+function pageFooterDemoContent($f, $r, $mainbar)
+{
+    return array(
+        $f->panel()->standard(
+            'All about the Footer',
+            $f->legacy(
+                "See bellow"
+            )
+        ),
+    );
+}
+
+function pageFooterDemoMetabar($f)
+{
+    $help = $f->button()->bulky($f->symbol()->glyph()->help(), 'Help', '#');
+    return $f->mainControls()->metabar()->withAdditionalEntry('help', $help);
+}
+
+function pageFooterDemoCrumbs($f)
+{
+    return $f->breadcrumbs([]);
+}
+
+function pageFooterDemoMainbar($f, $r)
+{
+    return $f->mainControls()->mainbar();;
+}
+
--- src/UI/examples/MainControls/Footer/footer_with_modals.php
+++ src/UI/examples/MainControls/Footer/footer_with_modals.php
@@ -1,5 +1,21 @@
 <?php
+
 function footer_with_modals()
+{
+    global $DIC;
+    $f = $DIC->ui()->factory();
+    $renderer = $DIC->ui()->renderer();
+
+    $url = 'src/UI/examples/MainControls/Footer/footer_with_modals.php?new_footer_2_ui=1';
+
+    $page_demo = $f->button()->primary('See UI in fullscreen-mode', $url);
+
+    return $renderer->render([
+        $page_demo
+    ]);
+}
+
+function pageFooterDemo2Footer()
 {
     global $DIC;
     $f = $DIC->ui()->factory();
@@ -15,5 +31,41 @@ function footer_with_modals()
     $shyButton = $f->button()->shy('Terms Of Service', '#');
     $footer = $footer->withAdditionalModalAndTrigger($roundTripModal, $shyButton);
 
-    return $renderer->render($footer);
+    return $footer;
+}
+
+if ($_GET['new_footer_2_ui'] == '1') {
+    chdir('../../../../../');
+    require_once('src/UI/examples/Layout/Page/Standard/ui.php');
+    require_once('src/UI/examples/MainControls/Footer/footer.php');
+
+    _initIliasForPreview();
+
+    global $DIC;
+
+    $f = $DIC->ui()->factory();
+    $renderer = $DIC->ui()->renderer();
+    $logo = $f->image()->responsive("templates/default/images/HeaderIcon.svg", "ILIAS");
+    $breadcrumbs = pageFooterDemoCrumbs($f);
+    $metabar = pageFooterDemoMetabar($f);
+    $mainbar = pageFooterDemoMainbar($f, $renderer);
+    $footer = pageFooterDemo2Footer($f);
+
+    $entries = $mainbar->getEntries();
+    $tools = $mainbar->getToolEntries();
+    $content = pageFooterDemoContent($f, $renderer, $mainbar);
+
+    $page = $f->layout()->page()->standard(
+        $content,
+        $metabar,
+        $mainbar,
+        $breadcrumbs,
+        $logo,
+        $footer,
+        'UI PAGE FOOTER DEMO', //page title
+        'ILIAS', //short title
+        'Std. Page Footer Demo' //view title
+    )->withUIDemo(true);
+
+    echo $renderer->render($page);
 }
