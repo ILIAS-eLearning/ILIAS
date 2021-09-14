53bb65171fa6d0de07c56e0b707e1e4d42e10470
mjansen <mjansen@databay.de>
2021-09-14 12:28:16 +0200

    ContentPage: Only recalculate learning progress if not already passed on read access

--- Modules/ContentPage/classes/class.ilObjContentPage.php
+++ Modules/ContentPage/classes/class.ilObjContentPage.php
@@ -197,10 +197,13 @@ class ilObjContentPage extends \ilObject2 implements \ilContentPageObjectConstan
 
         $lp = ilObjectLP::getInstance($this->getId());
         if ($lp->isActive() && ((int) $lp->getCurrentMode() === ilLPObjSettings::LP_MODE_CONTENT_VISITED)) {
-            ilLPStatusWrapper::_updateStatus(
-                $this->getId(),
-                $usrId
-            );
+            $current_status = (int) ilLPStatus::_lookupStatus($this->getId(), $usrId, false);
+            if ($current_status !== ilLPStatus::LP_STATUS_COMPLETED_NUM) {
+                ilLPStatusWrapper::_updateStatus(
+                    $this->getId(),
+                    $usrId
+                );
+            }
         }
     }
 }
