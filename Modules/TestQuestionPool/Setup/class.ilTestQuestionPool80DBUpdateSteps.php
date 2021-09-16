df4ef467d3cdf2c2338383c66f67f64e53b3fac8
Richard Klees <richard.klees@concepts-and-training.de>
2021-09-16 10:33:50 +0200

    DB: adjusted new db steps classes to fit intended interface (#3621)
    Thanks a lot @klees !

--- Modules/TestQuestionPool/Setup/class.ilTestQuestionPool80DBUpdateSteps.php
+++ Modules/TestQuestionPool/Setup/class.ilTestQuestionPool80DBUpdateSteps.php
@@ -9,9 +9,9 @@ class ilTestQuestionPool80DBUpdateSteps implements ilDatabaseUpdateSteps
         $this->db = $db;
     }
 
-    public function step_1(ilDBInterface $db) : void
+    public function step_1() : void
     {
-        $db->manipulateF("DELETE FROM qpl_qst_type WHERE type_tag = %s", ['text'], ['assJavaApplet']);
-        $db->manipulateF("DELETE FROM qpl_qst_type WHERE type_tag = %s", ['text'], ['assFlashQuestion']);
+        $this->db->manipulateF("DELETE FROM qpl_qst_type WHERE type_tag = %s", ['text'], ['assJavaApplet']);
+        $this->db->manipulateF("DELETE FROM qpl_qst_type WHERE type_tag = %s", ['text'], ['assFlashQuestion']);
     }
 }
