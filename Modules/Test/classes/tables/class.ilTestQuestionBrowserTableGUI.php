fa09d48356a19ef8f81d261d0fbe6a5a06dc3f9a
Maximilian Becker <mbecker@databay.de>
2021-09-23 09:40:58 +0200

    0026549: Test-Tool - Importing Questions from QuestionPool (QP): QPs are not visible when searching for QP in Repository

--- Modules/Test/classes/tables/class.ilTestQuestionBrowserTableGUI.php
+++ Modules/Test/classes/tables/class.ilTestQuestionBrowserTableGUI.php
@@ -415,6 +415,7 @@ class ilTestQuestionBrowserTableGUI extends ilTable2GUI
         require_once 'Services/Form/classes/class.ilRepositorySelectorInputGUI.php';
         $ri = new ilRepositorySelectorInputGUI($this->lng->txt('repository'), 'repository_root_node');
         $ri->setHeaderMessage($this->lng->txt('question_browse_area_info'));
+        $ri->setClickableTypes(array('qpl'));
         $this->addFilterItem($ri);
         $ri->readFromSession();
         $this->filter['repository_root_node'] = $ri->getValue();
