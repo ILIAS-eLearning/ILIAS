22243873a09ff9ae65b94a9d55420d60104a9e60
mjansen <mjansen@databay.de>
2021-10-23 10:31:54 +0200

    ContentPage: Use explicit default value for settings retrieval

--- Modules/ContentPage/classes/class.ilContentPagePageConfig.php
+++ Modules/ContentPage/classes/class.ilContentPagePageConfig.php
@@ -19,7 +19,7 @@ class ilContentPagePageConfig extends ilPageConfig
         $this->setUsePageContainer(false);
 
         $mediaPoolSettings = new ilSetting('mobs');
-        if ($mediaPoolSettings->get('mep_activate_pages')) {
+        if ($mediaPoolSettings->get('mep_activate_pages', '0')) {
             $this->setEnablePCType('ContentInclude', true);
         }
     }
