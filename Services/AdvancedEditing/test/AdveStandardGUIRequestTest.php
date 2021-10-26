00dc948118d0d2d463fcc109121d2db43c4ca9b8
Alexander Killing <killing@leifos.de>
2021-10-26 14:39:02 +0200

    php8 fixes

---
+++ Services/AdvancedEditing/test/AdveStandardGUIRequestTest.php
@@ -0,0 +1,54 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test clipboard repository
+ *
+ * @author Alexander Killing <killing@leifos.de>
+ */
+class AdveStandardGUIRequestTest extends TestCase
+{
+    //protected $backupGlobals = false;
+
+    protected function setUp() : void
+    {
+        parent::setUp();
+    }
+
+    protected function tearDown() : void
+    {
+    }
+
+    protected function getRequest(array $get, array $post) : \ILIAS\AdvancedEditing\StandardGUIRequest
+    {
+        $http_mock = $this->createMock(ILIAS\HTTP\Services::class);
+        $lng_mock = $this->createMock(ilLanguage::class);
+        $data = new \ILIAS\Data\Factory();
+        $refinery = new \ILIAS\Refinery\Factory($data, $lng_mock);
+        return new \ILIAS\AdvancedEditing\StandardGUIRequest(
+            $http_mock,
+            $refinery,
+            $get,
+            $post
+        );
+    }
+
+    /**
+     * Test group
+     */
+    public function testGroup()
+    {
+        $request = $this->getRequest(
+            [
+                "grp" => "5"
+            ],
+            []
+        );
+
+        $this->assertEquals(
+            "5",
+            $request->getGroup()
+        );
+    }
+}
