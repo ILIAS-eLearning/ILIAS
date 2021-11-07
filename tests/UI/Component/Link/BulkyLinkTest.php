cffa4f5c7d0e5a41248e24f49457917c48af834d
swiniker <swiniker@users.noreply.github.com>
2021-11-07 11:47:30 +0100

    This changes the role-attribute in BulkyLinks for Menu-Items (#3737)
    Fix for: https://mantis.ilias.de/view.php?id=31552

--- tests/UI/Component/Link/BulkyLinkTest.php
+++ tests/UI/Component/Link/BulkyLinkTest.php
@@ -39,6 +39,28 @@ class BulkyLinkTest extends ILIAS_UI_TestBase
         $this->expectException(\TypeError::class);
         $this->factory->bulky('wrong param', "label", $this->target);
     }
+    
+    public function testWithAriaRole()
+    {
+        try {
+            $b = $this->factory->bulky($this->glyph, "label", $this->target)
+            ->withAriaRole(I\Button\Bulky::MENUITEM);
+            $this->assertEquals("menuitem", $b->getAriaRole());
+        } catch (\InvalidArgumentException $e) {
+            $this->assertFalse("This should not happen");
+        }
+    }
+    
+    public function testWithAriaRoleIncorrect()
+    {
+        try {
+            $this->factory->bulky($this->glyph, "label", $this->target)
+            ->withAriaRole("loremipsum");
+            $this->assertFalse("This should not happen");
+        } catch (\InvalidArgumentException $e) {
+            $this->assertTrue(true);
+        }
+    }
 
     public function testGetLabell() : void
     {
@@ -125,6 +147,24 @@ class BulkyLinkTest extends ILIAS_UI_TestBase
             . ' <span class="bulky-label">label</span>'
             . '</a>';
 
+        $this->assertHTMLEquals(
+            $expected,
+            $r->render($b)
+        );
+    }
+        
+    public function testRenderWithAriaRoleMenuitem()
+    {
+        $r = $this->getDefaultRenderer();
+        $b = $this->factory->bulky($this->icon, "label", $this->target)
+        ->withAriaRole(I\Button\Bulky::MENUITEM);
+        
+        $expected = ''
+        . '<a class="il-link link-bulky" href="http://www.ilias.de" role="menuitem">'
+        . '<img class="icon someExample small" src="./templates/default/images/icon_default.svg" alt="Example"/>'
+        . ' <span class="bulky-label">label</span>'
+        . '</a>';
+                        
         $this->assertHTMLEquals(
             $expected,
             $r->render($b)
