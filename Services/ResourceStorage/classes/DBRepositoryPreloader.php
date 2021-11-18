0761a786b53b3adcf2d3418fc9503db795200594
chfsx <fs@studer-raimann.ch>
2021-11-18 15:51:50 +0100

    soft preloader

--- Services/ResourceStorage/classes/DBRepositoryPreloader.php
+++ Services/ResourceStorage/classes/DBRepositoryPreloader.php
@@ -43,22 +43,26 @@ class DBRepositoryPreloader extends StandardRepositoryPreloader implements Repos
         if (count($requested) === 0) {
             return;
         }
-        $r = $this->db->query(
-            "SELECT * 
+        try {
+            $r = $this->db->query(
+                "SELECT * 
 FROM il_resource_revision
 JOIN il_resource_info ON il_resource_revision.rid = il_resource_info.rid AND il_resource_info.version_number = il_resource_revision.version_number
 JOIN il_resource ON il_resource_revision.rid = il_resource.rid
 JOIN il_resource_stkh_u ON il_resource_stkh_u.rid = il_resource.rid
 JOIN il_resource_stkh ON il_resource_stkh_u.stakeholder_id = il_resource_stkh.id
 WHERE " . $this->db->in('il_resource_revision.rid', $requested, false, 'text')
-        );
-        while ($d = $this->db->fetchAssoc($r)) {
-            $this->resource_repository->populateFromArray($d);
-            $this->revision_repository->populateFromArray($d);
-            $this->information_repository->populateFromArray($d);
-            $this->stakeholder_repository->populateFromArray($d);
+            );
+            while ($d = $this->db->fetchAssoc($r)) {
+                $this->resource_repository->populateFromArray($d);
+                $this->revision_repository->populateFromArray($d);
+                $this->information_repository->populateFromArray($d);
+                $this->stakeholder_repository->populateFromArray($d);
+            }
+            $this->preloaded = array_merge($this->preloaded, $identification_strings);
+            $this->preloaded = array_unique($this->preloaded);
+        } catch (\Throwable $t) {
+            // preloading failed, fallback will be loading one by one
         }
-        $this->preloaded = array_merge($this->preloaded, $identification_strings);
-        $this->preloaded = array_unique($this->preloaded);
     }
 }
