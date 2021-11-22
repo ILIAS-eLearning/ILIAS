1ace2febd6f746520abb023dae83cebe9119cb73
Marvin Beym <mBeym@databay.de>
2021-11-22 11:00:46 +0100

    Add test for achieving objective

--- tests/Setup/CLI/AchieveCommandTest.php
+++ tests/Setup/CLI/AchieveCommandTest.php
@@ -17,6 +17,11 @@ use ilUICoreSetupAgent;
 use ilLanguage;
 use ilSetupAgent;
 use ILIAS\Setup\AgentCollection;
+use ILIAS\Refinery\Transformation;
+use ILIAS\Setup\Objective;
+use ILIAS\Setup\Metrics;
+use ILIAS\Setup\ObjectiveCollection;
+use LogicException;
 
 class TestConfig implements Config
 {
@@ -143,6 +148,114 @@ class AchieveCommandTest extends TestCase
             "config" => $config_file,
             "objective" => $objective_name
         ]);
+    }
+
+    public function testAchieveObjective() : void
+    {
+        $refinery = new Refinery(
+            $this->createMock(DataFactory::class),
+            $this->createMock(ilLanguage::class)
+        );
+        $config_reader = $this->createMock(Setup\CLI\ConfigReader::class);
+
+        $agentCollection = new AgentCollection(
+            $this->refinery,
+            [
+                "testAgent" => new class implements Setup\Agent {
+
+                    public function hasConfig() : bool
+                    {
+                        return false;
+                    }
+
+                    public function getArrayToConfigTransformation() : Transformation
+                    {
+                        throw new LogicException(self::class . " has no Config.");
+                    }
+
+                    public function getInstallObjective(Config $config = null) : Objective
+                    {
+                        return new Setup\Objective\NullObjective();
+                    }
+
+                    public function getUpdateObjective(Config $config = null) : Objective
+                    {
+                        return new Setup\Objective\NullObjective();
+                    }
+
+                    public function getBuildArtifactObjective() : Objective
+                    {
+                        return new Setup\Objective\NullObjective();
+                    }
+
+                    public function getStatusObjective(Metrics\Storage $storage) : Objective
+                    {
+                        return new Setup\Objective\NullObjective();
+                    }
+
+                    public function getMigrations() : array
+                    {
+                        return [];
+                    }
+
+                    public function getNamedObjectives(?Config $config = null) : array
+                    {
+                        return [
+                            "testNamedObjective" => new Setup\ObjectiveConstructor("Test Named Objective",
+                                static function () use ($config) : ObjectiveCollection {
+                                    if (is_null($config)) {
+                                        throw new \RuntimeException(
+                                            "Missing Config for objective 'testNamedObjective'."
+                                        );
+                                    }
+
+                                    return new ObjectiveCollection(
+                                        "",
+                                        false,
+                                        new Objective\NullObjective()
+                                    );
+                                })
+                        ];
+                    }
+                },
+            ]
+        );
+
+        $agent_finder = $this->createMock(Setup\AgentFinder::class);
+        $agent_finder
+            ->expects($this->any())
+            ->method("getAgents")
+            ->willReturn($agentCollection);
+
+        $command = new Setup\CLI\AchieveCommand($agent_finder, $config_reader, [], $refinery);
+
+        $input_mock = $this->createMock(InputInterface::class);
+        $input_mock
+            ->expects($this->any())
+            ->method("getOption")
+            ->willReturn(false);
+
+        $input_mock
+            ->expects($this->any())
+            ->method("getArguments")
+            ->willReturn(["objective" => "testAgent.testNamedObjective"]);
+
+        $namedObjectiveCmd = "testAgent.testNamedObjective";
+        $input_mock
+            ->expects($this->any())
+            ->method("getArgument")
+            ->willReturn($namedObjectiveCmd);
+
+        $output = new StreamOutput(fopen('php://memory', 'wb', false));
+
+        $command->execute($input_mock, $output);
+        rewind($output->getStream());
+
+        $outputData = stream_get_contents($output->getStream());
+        $this->assertStringContainsString(
+            "[OK] Achieved objective '$namedObjectiveCmd'. Thanks and have fun!",
+            $outputData
+        );
     }
 
     public function testListNamedObjectives() : void
