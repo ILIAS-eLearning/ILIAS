ca691c488a914df68df6f286b6010149b93580eb
mjansen <mjansen@databay.de>
2021-11-29 11:10:44 +0100

    ContentPage: Online/Offline

--- Modules/ContentPage/classes/class.ilObjContentPageGUI.php
+++ Modules/ContentPage/classes/class.ilObjContentPageGUI.php
@@ -582,6 +582,14 @@ class ilObjContentPageGUI extends ilObject2GUI implements ilContentPageObjectCon
 
     protected function initEditCustomForm(ilPropertyFormGUI $a_form) : void
     {
+        $this->addAvailabilitySection($a_form);
+
+        $presentationHeader = new ilFormSectionHeaderGUI();
+        $presentationHeader->setTitle($this->lng->txt('settings_presentation_header'));
+        $a_form->addItem($presentationHeader);
+
+        $this->obj_service->commonSettings()->legacyForm($a_form, $this->object)->addTileImage();
+
         $sh = new ilFormSectionHeaderGUI();
         $sh->setTitle($this->lng->txt('obj_features'));
         $a_form->addItem($sh);
@@ -593,14 +601,6 @@ class ilObjContentPageGUI extends ilObject2GUI implements ilContentPageObjectCon
                 ilObjectServiceSettingsGUI::INFO_TAB_VISIBILITY
             ]
         );
-
-        $this->addAvailabilitySection($a_form);
-
-        $presentationHeader = new ilFormSectionHeaderGUI();
-        $presentationHeader->setTitle($this->lng->txt('settings_presentation_header'));
-        $a_form->addItem($presentationHeader);
-
-        $this->obj_service->commonSettings()->legacyForm($a_form, $this->object)->addTileImage();
     }
 
     private function addAvailabilitySection(ilPropertyFormGUI $form) : void
