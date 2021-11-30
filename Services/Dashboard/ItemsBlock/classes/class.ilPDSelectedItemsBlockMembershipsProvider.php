5c3a184ec61c4eaf135ec352f727be3b96959a53
mjansen <mjansen@databay.de>
2021-11-30 13:17:01 +0100

    Dashboard Performance: Switch approach to SQL

--- Services/Dashboard/ItemsBlock/classes/class.ilPDSelectedItemsBlockMembershipsProvider.php
+++ Services/Dashboard/ItemsBlock/classes/class.ilPDSelectedItemsBlockMembershipsProvider.php
@@ -43,6 +43,7 @@ class ilPDSelectedItemsBlockMembershipsProvider implements ilPDSelectedItemsBloc
             $objTypes = $this->repository->getValidObjectTypes();
         }
 
+        $references = [];
         foreach ($this->repository->getForUser($this->actor, $objTypes, $this->actor->getLanguage()) as $item) {
             $refId = $item->getRefId();
             $objId = $item->getObjId();
