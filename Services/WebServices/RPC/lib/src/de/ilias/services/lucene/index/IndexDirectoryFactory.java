5460db69666d5bb13293e0f6f07aa2b0a05c05e1
smeyer <smeyer.ilias@gmx.de>
2021-12-13 23:48:13 +0100

    src: new ilserver build

--- Services/WebServices/RPC/lib/src/de/ilias/services/lucene/index/IndexDirectoryFactory.java
+++ Services/WebServices/RPC/lib/src/de/ilias/services/lucene/index/IndexDirectoryFactory.java
@@ -7,7 +7,8 @@ package de.ilias.services.lucene.index;
 
 import java.io.File;
 import java.io.IOException;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.NIOFSDirectory;
 
@@ -17,7 +18,7 @@ import org.apache.lucene.store.NIOFSDirectory;
  */
 public class IndexDirectoryFactory {
 	
-	protected static Logger logger = Logger.getLogger(IndexDirectoryFactory.class);
+	protected static Logger logger = LogManager.getLogger(IndexDirectoryFactory.class);
 	
 	
 	/**
