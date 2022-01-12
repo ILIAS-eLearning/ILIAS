030b5957f645ccb10dd3c79a24b0b146843b565c
Thomas Famula <famula@leifos.de>
2022-01-12 17:12:26 +0100

    Extend skill category test

--- Services/Skill/test/SkillCategoryTest.php
+++ Services/Skill/test/SkillCategoryTest.php
@@ -1,4 +1,21 @@
-<?php
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ ********************************************************************
+ */
 
 use PHPUnit\Framework\TestCase;
 
@@ -27,7 +44,6 @@ class SkillCategoryTest extends TestCase
     {
         parent::setUp();
 
-
         $dic = new ILIAS\DI\Container();
         $GLOBALS['DIC'] = $dic;
 
@@ -44,14 +60,48 @@ class SkillCategoryTest extends TestCase
     {
     }
 
-    public function testSetGetTitle()
+    public function testProperties() : void
     {
         $sk = $this->skill_category;
-        $sk->setTitle("title");
+        $sk->setTitle("A skill category");
+        $sk->setDescription("This is a description for a skill category.");
+        $sk->setSelfEvaluation(true);
+        $sk->setOrderNr(10);
+        $sk->setStatus(0);
+        $sk->setId(4);
+        $sk->setImportId("an_import_id");
 
         $this->assertEquals(
-            "title",
+            "scat",
+            $sk->getType()
+        );
+        $this->assertEquals(
+            "A skill category",
             $sk->getTitle()
+        );
+        $this->assertEquals(
+            "This is a description for a skill category.",
+            $sk->getDescription()
+        );
+        $this->assertEquals(
+            true,
+            $sk->getSelfEvaluation()
+        );
+        $this->assertEquals(
+            10,
+            $sk->getOrderNr()
+        );
+        $this->assertEquals(
+            0,
+            $sk->getStatus()
+        );
+        $this->assertEquals(
+            4,
+            $sk->getId()
+        );
+        $this->assertEquals(
+            "an_import_id",
+            $sk->getImportId()
         );
     }
 }
