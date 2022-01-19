f61a5318ef57f13f7d446ac86af23aad6b70432c
mjansen <mjansen@databay.de>
2022-01-19 11:06:27 +0100

    Chatroom: Fix passing 2nd argument to ilCtrl::getCmd()

--- Modules/Chatroom/classes/class.ilObjChatroomGUI.php
+++ Modules/Chatroom/classes/class.ilObjChatroomGUI.php
@@ -204,9 +204,7 @@ class ilObjChatroomGUI extends ilChatroomObjectGUI implements ilCtrlBaseClassInt
 
             default:
                 try {
-                    $res = explode('-', $this->ctrl->getCmd('', [
-                        'view-toggleAutoMessageDisplayState'
-                    ]), 2);
+                    $res = explode('-', $this->ctrl->getCmd(''), 2);
                     $result = $this->dispatchCall($res[0], $res[1] ?? '');
                     if (!$result && method_exists($this, $this->ctrl->getCmd() . 'Object')) {
                         $this->prepareOutput();
