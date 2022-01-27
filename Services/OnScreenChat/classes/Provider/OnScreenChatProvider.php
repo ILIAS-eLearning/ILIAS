6bc3a04ef5cafca8fb5c920e874bf9ee86ad4a8d
iszmais <iszmais@databay.de>
2022-01-27 16:46:30 +0100

    renamed tranlsation vars

--- Services/OnScreenChat/classes/Provider/OnScreenChatProvider.php
+++ Services/OnScreenChat/classes/Provider/OnScreenChatProvider.php
@@ -62,7 +62,7 @@ class OnScreenChatProvider extends AbstractStaticMainMenuProvider
                     $isEnabled = $chatSettings->get('chat_enabled') && $chatSettings->get('enable_osc');
                     return $isUser && $isEnabled;
                 })
-                ->withTitle($this->dic->language()->txt('private_chats'))
+                ->withTitle($this->dic->language()->txt('mm_private_chats'))
                 ->withSymbol($icon)
                 ->withContent($this->dic->ui()->factory()->item()->shy('')->withAdditionalOnLoadCode(
                     function ($id) {
