adceff24c57500e4c5f25c1228456c66a3a9b606
Fabian Schmid <fabian@sr.solutions>
2022-02-01 14:38:30 +0100

    fixed version parameters in metacontent

--- src/GlobalScreen/Scope/Layout/MetaContent/Media/InlineCss.php
+++ src/GlobalScreen/Scope/Layout/MetaContent/Media/InlineCss.php
@@ -1,5 +1,15 @@
 <?php namespace ILIAS\GlobalScreen\Scope\Layout\MetaContent\Media;
 
+/******************************************************************************
+ * This file is part of ILIAS, a powerful learning management system.
+ * ILIAS is licensed with the GPL-3.0, you should have received a copy
+ * of said license along with the source code.
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ *      https://www.ilias.de
+ *      https://github.com/ILIAS-eLearning
+ *****************************************************************************/
+
 /**
  * Class InlineCss
  *
