cfc030ba0359a770e70e99c961b29773e148c57e
Fabian Schmid <fabian@sr.solutions>
2022-02-03 17:05:46 +0100

    fixed two new bugs after inifile has been typed

--- Services/Authentication/classes/class.ilSession.php
+++ Services/Authentication/classes/class.ilSession.php
@@ -252,7 +252,7 @@ class ilSession
                 $a_session_id = array_keys($a_session_id);
             }
             $q = "DELETE FROM usr_session WHERE " .
-                $ilDB->in("session_id", $a_session_id, "", "text");
+                $ilDB->in("session_id", $a_session_id, false, "text");
         }
 
         ilSessionIStorage::destroySession($a_session_id);
