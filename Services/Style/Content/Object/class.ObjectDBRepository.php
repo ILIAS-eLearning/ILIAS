3aa0f4fd15e2d6f2be62f7b0f541a0e859fa1e9e
Alex Killing <alex.killing@gmx.de>
2022-02-05 10:05:48 +0100

    Update Services/Style/Content/Object/class.ObjectDBRepository.php
    Co-authored-by: Michael Jansen <mjansen@databay.de>

--- Services/Style/Content/Object/class.ObjectDBRepository.php
+++ Services/Style/Content/Object/class.ObjectDBRepository.php
@@ -29,7 +29,7 @@ class ObjectDBRepository
     /**
      * @var \ilDBInterface
      */
-    protected $db;
+    protected \ilDBInterface $db;
 
     public function __construct(\ilDBInterface $db)
     {
