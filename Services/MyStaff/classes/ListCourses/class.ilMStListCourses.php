4f4a0ebb975f82820fa44dfa575026184c3d33ee
mjansen <mjansen@databay.de>
2022-02-15 18:30:07 +0100

    MyStaff: Fix query when determining courses of user(s)

--- Services/MyStaff/classes/ListCourses/class.ilMStListCourses.php
+++ Services/MyStaff/classes/ListCourses/class.ilMStListCourses.php
@@ -60,7 +60,7 @@ class ilMStListCourses
 	                    SELECT reg.obj_id, reg.usr_id, ' . ilMStListCourse::MEMBERSHIP_STATUS_REGISTERED . ' AS reg_status, lp.status AS lp_status FROM obj_members 
 		          AS reg
                         LEFT JOIN ut_lp_marks AS lp on lp.obj_id = reg.obj_id AND lp.usr_id = reg.usr_id
-                         WHERE ' . $this->dic->database()->in('reg.usr_id', $arr_usr_ids, false, 'integer') . '
+                         WHERE ' . $this->dic->database()->in('reg.usr_id', $arr_usr_ids, false, 'integer') . ' AND (reg.admin = 1 OR reg.tutor = 1 OR reg.member = 1)
 		            UNION
 	                    SELECT obj_id, usr_id, ' . ilMStListCourse::MEMBERSHIP_STATUS_WAITINGLIST . ' AS reg_status, 0 AS lp_status FROM crs_waiting_list AS waiting
 	                    WHERE ' . $this->dic->database()->in('waiting.usr_id', $arr_usr_ids, false, 'integer') . '
