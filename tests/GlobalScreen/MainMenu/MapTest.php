c3278855c062549f9b18d83145fb4ad79402f26f
Fabian Schmid <fabian@sr.solutions>
2022-02-18 09:42:42 +0100

    updated test of mm map
    # Conflicts:
    #	tests/GlobalScreen/MainMenu/MapTest.php

--- tests/GlobalScreen/MainMenu/MapTest.php
+++ tests/GlobalScreen/MainMenu/MapTest.php
@@ -23,6 +23,7 @@ require_once('./libs/composer/vendor/autoload.php');
  */
 class MapTest extends TestCase
 {
+
     protected IdentificationFactory $identification;
     protected MainMenuItemFactory $factory;
     protected StaticMainMenuProvider $provider;
@@ -198,12 +199,12 @@ class MapTest extends TestCase
                                 ->withParent($tp_1->getProviderIdentification())
                                 ->withPosition(2);
         $tp_1 = $tp_1->withChildren([
-            $tp_1_1,
             $tp_1_2,
+            $tp_1_1,
         ]);
         $map->add($tp_1);
-        $map->add($tp_1_1);
         $map->add($tp_1_2);
+        $map->add($tp_1_1);
         
         $tp_2 = $this->factory->topParentItem($this->getId('tp_2'))
                               ->withPosition(2);
@@ -214,19 +215,34 @@ class MapTest extends TestCase
                                 ->withParent($tp_2->getProviderIdentification())
                                 ->withPosition(2);
         $tp_2 = $tp_2->withChildren([
-            $tp_2_1,
             $tp_2_2,
+            $tp_2_1,
         ]);
         $map->add($tp_2);
         $map->add($tp_2_1);
         $map->add($tp_2_2);
+        
         $map->sort();
+        
         $this->assertEquals(1, $map->getSingleItemFromRaw($this->getId('tp_1'))->getPosition());
         $this->assertEquals(1, $map->getSingleItemFromRaw($this->getId('tp_1_1'))->getPosition());
         $this->assertEquals(2, $map->getSingleItemFromRaw($this->getId('tp_1_2'))->getPosition());
         $this->assertEquals(2, $map->getSingleItemFromRaw($this->getId('tp_2'))->getPosition());
         $this->assertEquals(1, $map->getSingleItemFromRaw($this->getId('tp_2_1'))->getPosition());
         $this->assertEquals(2, $map->getSingleItemFromRaw($this->getId('tp_2_2'))->getPosition());
+        
+        // check position in parent
+        $get_tp_1 = $map->getSingleItemFromFilter($this->getId('tp_1'));
+        $children_of_tp_1 = $get_tp_1->getChildren();
+        $first = reset($children_of_tp_1);
+        $this->assertEquals($tp_1_1, $first);
+        $this->assertEquals(1, $first->getPosition());
+        
+        $get_tp_2 = $map->getSingleItemFromFilter($this->getId('tp_2'));
+        $children_of_tp_2 = $get_tp_2->getChildren();
+        $first = reset($children_of_tp_2);
+        $this->assertEquals($tp_2_1, $first);
+        $this->assertEquals(1, $first->getPosition());
     }
     
     private function getDummyProvider()
