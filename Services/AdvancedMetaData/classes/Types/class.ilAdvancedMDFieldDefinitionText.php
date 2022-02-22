049d498271f746c17a98e16f095552589e22b01e
Marvin Barz <60610009+M-Barz@users.noreply.github.com>
2022-02-22 12:47:03 +0100

    BT-23546: Added character suffix to max length in metadata fields (#4060)

--- Services/AdvancedMetaData/classes/Types/class.ilAdvancedMDFieldDefinitionText.php
+++ Services/AdvancedMetaData/classes/Types/class.ilAdvancedMDFieldDefinitionText.php
@@ -145,6 +145,7 @@ class ilAdvancedMDFieldDefinitionText extends ilAdvancedMDFieldDefinition
         $max = new ilNumberInputGUI($lng->txt("md_adv_text_max_length"), "max");
         $max->setValue($this->getMaxLength());
         $max->setSize(10);
+        $max->setSuffix($lng->txt("characters"));
         $max->setMinValue(1);
         $max->setMaxValue(4000); // DB limit
         $a_form->addItem($max);
