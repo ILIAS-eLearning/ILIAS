07c9a17802f41d200458bf654501204ff42685ef
mjansen <mjansen@databay.de>
2022-03-10 13:35:31 +0100

    BuddySystem: Add usability fixed regarding filter and states (Mantis 31120)

--- Services/Dashboard/classes/class.ilDashboardGUI.php
+++ Services/Dashboard/classes/class.ilDashboardGUI.php
@@ -705,14 +705,6 @@ class ilDashboardGUI
     }
 
     /**
-     * Jump to contacts
-     */
-    public function jumpToContacts()
-    {
-        $this->ctrl->redirectByClass(array('ildashboardgui', 'ilcontactgui'));
-    }
-
-    /**
      * Jump to personal workspace
      */
     public function jumpToWorkspace()
