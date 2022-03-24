c1df155b6b9932112943a2b97c3b2ccaa84d6ad6
Fabian Schmid <fabian@sr.solutions>
2022-03-24 09:23:07 +0100

    added license header

--- Modules/Exercise/classes/class.ilExerciseXMLParser.php
+++ Modules/Exercise/classes/class.ilExerciseXMLParser.php
@@ -3,16 +3,19 @@
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
+ *
  * ILIAS is licensed with the GPL-3.0,
  * see https://www.gnu.org/licenses/gpl-3.0.en.html
  * You should have received a copy of said license along with the
  * source code, too.
+ *
  * If this is not the case or you just want to try ILIAS, you'll find
  * us at:
  * https://www.ilias.de
  * https://github.com/ILIAS-eLearning
- */
-
+ *
+ *********************************************************************/
+ 
 /**
  * Exercise XML Parser which completes/updates a given exercise by an xml string.
  * @author Roland Küstermann <roland@kuestermann.com>
