c5fdebbc2d9039940dd7fb164e9bbc64e71cffb8
Fabian Schmid <fabian@sr.solutions>
2022-03-29 12:53:55 +0200

    renamed shibboleth tests to right location

--- Services/AuthShibboleth/test/ilServicesAuthShibbolethSuite.php
+++ Services/AuthShibboleth/test/ilServicesAuthShibbolethSuite.php
@@ -18,14 +18,14 @@ use PHPUnit\Framework\TestSuite;
 
 require_once 'libs/composer/vendor/autoload.php';
 
-class ilAuthShibbolethSuite extends TestSuite
+class ilServicesAuthShibbolethSuite extends TestSuite
 {
     public static function suite() : self
     {
         $suite = new self();
         
-        require_once("./Services/AuthShibboleth/test/ilAuthShibbolethTest.php");
-        $suite->addTestSuite("ilAuthShibbolethTest");
+        require_once("./Services/AuthShibboleth/test/ilServicesAuthShibbolethTest.php");
+        $suite->addTestSuite("ilServicesAuthShibbolethTest");
         
         return $suite;
     }
