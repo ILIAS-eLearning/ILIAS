38442aeec62a9f903fa2bedd35f37659019f99d7
Nils Haagen <nhaagen@cat06.de>
2022-03-29 12:24:59 +0200

    Language/CI 32318, determine header length

--- CI/sort_langfile_entries.sh
+++ CI/sort_langfile_entries.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 echo "sorting langfile entries"
-HEADER_LENGTH=16
 LANGFILES=$(git diff --cached --name-only --diff-filter=ACM -- '*.lang')
 for FILE in $LANGFILES
 do
+	HEADER_LENGTH=$(awk '/<!-- language file start -->/ {print FNR}' ${FILE})
 	(head -n $HEADER_LENGTH ${FILE} && tail ${FILE} -n +$((HEADER_LENGTH + 1)) | sort ) > ${FILE}.tmp
 	mv ${FILE}.tmp ${FILE}
 done
