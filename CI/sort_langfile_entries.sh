64d9ea7a972057872c7ab2b50734d023aa77b8a3
Nils Haagen <nhaagen@cat06.de>
2022-03-30 09:06:26 +0200

    Language/CI 32318, switch linebreaks in langfiles

--- CI/sort_langfile_entries.sh
+++ CI/sort_langfile_entries.sh
@@ -5,25 +5,25 @@ langfiles=$(git diff --cached --name-only --diff-filter=ACM -- '*.lang')
 
 for file in $langfiles
 do
-	header_length=1
-	while [ true ]
-	do
-		header_length=$(($header_length + 1))
-		line=$(head $file -n ${header_length} | tail -n1)
+    header_length=1
+    while [ true ]
+    do
+        header_length=$(($header_length + 1))
+        line=$(head $file -n ${header_length} | tail -n1)
 
-		if [ "$line" = "$header_end" ]; then
-		    break
-		fi
+        if [ "$line" = "$header_end" ]; then
+            break
+        fi
 
-		if [ $header_length -gt 64 ]; then
-			echo "no proper header-marker found in ${file}; maybe check on line endings?" 
-		    break
-		fi
-	done
+        if [ $header_length -gt 64 ]; then
+            echo "no proper header-marker found in ${file}; maybe check on line endings?"
+            break
+        fi
+    done
 
-	echo  "sorting entries in ${file}";
-	(head ${file} -n $header_length && tail ${file} -n +$((header_length + 1)) | sort ) > ${file}.tmp
-	mv ${file}.tmp ${file}
+    echo  "sorting entries in ${file}";
+    (head ${file} -n $header_length && tail ${file} -n +$((header_length + 1)) | sort ) > ${file}.tmp
+    mv ${file}.tmp ${file}
 
 done
 exit 0
