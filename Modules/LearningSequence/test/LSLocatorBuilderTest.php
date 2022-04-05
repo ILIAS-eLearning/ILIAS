96d4a1a5f4f062eccbf5b6ce60279d8a5e5dceca
Fabian Schmid <fabian@sr.solutions>
2022-04-05 16:19:49 +0200

    license header

--- Modules/LearningSequence/test/LSLocatorBuilderTest.php
+++ Modules/LearningSequence/test/LSLocatorBuilderTest.php
@@ -9,6 +9,22 @@ use ILIAS\UI\Implementation\Component\BreadCrumbs\Breadcrumbs;
 require_once('IliasMocks.php');
 require_once(__DIR__ . "/../../../tests/UI/Base.php");
 
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+ 
 class LSLocatorBuilderTest extends ILIAS_UI_TestBase
 {
     use IliasMocks;
