02ac412bf43f2021653f6bef96e20a185954f0ff
Katrin Großkopf <katrin.grosskopf@kroepelin-projekte.de>
2022-04-05 11:19:38 +0200

    PHP 8 Review: removed unnecessary type declarations in PhpDoc

--- Modules/IndividualAssessment/classes/class.ilIndividualAssessmentLP.php
+++ Modules/IndividualAssessment/classes/class.ilIndividualAssessmentLP.php
@@ -24,8 +24,6 @@ class ilIndividualAssessmentLP extends ilObjectLP
 
     /**
      * Get an array of member ids participating in the object corresponding to this.
-     * @param bool $a_search
-     * @return array
      */
     public function getMembers(bool $a_search = true) : array
     {
