a97bdf775d3c0d021be6b18fc61662c1b70a918e
Fabian Schmid <fabian@sr.solutions>
2022-04-11 07:57:05 +0200

    LicenseHeader

--- Modules/WebResource/classes/class.ilObjLinkResourceGUI.php
+++ Modules/WebResource/classes/class.ilObjLinkResourceGUI.php
@@ -3,15 +3,18 @@
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
+ *
  * ILIAS is licensed with the GPL-3.0,
  * see https://www.gnu.org/licenses/gpl-3.0.en.html
  * You should have received a copy of said license along with the
  * source code, too.
+ *
  * If this is not the case or you just want to try ILIAS, you'll find
  * us at:
  * https://www.ilias.de
  * https://github.com/ILIAS-eLearning
- */
+ *
+ *********************************************************************/
 
 use ILIAS\HTTP\Services as HTTPService;
 use ILIAS\UI\Component\Component;
@@ -1676,7 +1679,6 @@ class ilObjLinkResourceGUI extends ilObject2GUI
         string $a_url
     ) : void {
         if ($a_url) {
-            require_once('Services/Tracking/classes/class.ilChangeEvent.php');
             ilChangeEvent::_recordReadEvent(
                 "webr",
                 $a_ref_id,
@@ -1772,10 +1774,10 @@ class ilObjLinkResourceGUI extends ilObject2GUI
                         sprintf(
                             $lng->txt("msg_no_perm_read_item"),
                             ilObject::_lookupTitle(
-                            ilObject::_lookupObjId(
+                                ilObject::_lookupObjId(
                                 (int) $a_target
                             )
-                        )
+                            )
                         ),
                         true
                     );
--- Modules/WebResource/classes/class.ilObjLinkResourceSubItemListGUI.php
+++ Modules/WebResource/classes/class.ilObjLinkResourceSubItemListGUI.php
@@ -3,16 +3,19 @@
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
+ *
  * ILIAS is licensed with the GPL-3.0,
  * see https://www.gnu.org/licenses/gpl-3.0.en.html
  * You should have received a copy of said license along with the
  * source code, too.
+ *
  * If this is not the case or you just want to try ILIAS, you'll find
  * us at:
  * https://www.ilias.de
  * https://github.com/ILIAS-eLearning
- */
-
+ *
+ *********************************************************************/
+ 
 /**
  * Show glossary terms
  * @author Stefan Meyer <meyer@leifos.com>
@@ -25,9 +28,9 @@ class ilObjLinkResourceSubItemListGUI extends ilSubItemListGUI
         foreach ($this->getSubItemIds(true) as $sub_item) {
             if (is_object($this->getHighlighter()) and strlen(
                 $this->getHighlighter()->getContent(
-                        $this->getObjId(),
-                        $sub_item
-                    )
+                    $this->getObjId(),
+                    $sub_item
+                )
             )) {
                 $this->tpl->setCurrentBlock('sea_fragment');
                 $this->tpl->setVariable(
