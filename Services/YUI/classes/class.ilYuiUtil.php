60aee1d62a3aa6865df3e7156d73be6f7678c2a4
Fabian Schmid <fabian@sr.solutions>
2022-04-11 18:42:18 +0200

    YUI: updated license header

--- Services/YUI/classes/class.ilYuiUtil.php
+++ Services/YUI/classes/class.ilYuiUtil.php
@@ -3,16 +3,19 @@
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
+ *
  * ILIAS is licensed with the GPL-3.0,
  * see https://www.gnu.org/licenses/gpl-3.0.en.html
  * You should have received a copy of said license along with the
  * source code, too.
+ *
  * If this is not the case or you just want to try ILIAS, you'll find
  * us at:
  * https://www.ilias.de
  * https://github.com/ILIAS-eLearning
- */
-
+ *
+ *********************************************************************/
+ 
 /**
  * Yahoo YUI Library Utility functions
  * @author Alexander Killing <killing@leifos.de>
@@ -25,7 +28,7 @@ class ilYuiUtil
         ?ilGlobalTemplateInterface $main_tpl = null
     ) : ilGlobalTemplateInterface {
         global $DIC;
-        return $main_tpl == null ? $DIC->ui()->mainTemplate() : $main_tpl;
+        return $main_tpl === null ? $DIC->ui()->mainTemplate() : $main_tpl;
     }
     
     /**
