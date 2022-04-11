b5aa5322b2fe04437c342726afc60ab7e3f6b947
Fabian Schmid <fabian@sr.solutions>
2022-04-11 15:16:58 +0200

    Bibl: added license header

--- Modules/Bibliographic/test/ilModulesBibliographicSuite.php
+++ Modules/Bibliographic/test/ilModulesBibliographicSuite.php
@@ -16,8 +16,22 @@
 
 use PHPUnit\Framework\TestSuite;
 
-require_once 'libs/composer/vendor/autoload.php';
-
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+ 
 class ilModulesBibliographicSuite extends TestSuite
 {
     public static function suite() : self
