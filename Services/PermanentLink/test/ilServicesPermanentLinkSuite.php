4c495bd1561184689d53ad4b1cf8d2ee8947ed15
Fabian Schmid <fabian@sr.solutions>
2022-04-11 18:18:40 +0200

    PermanentLink: Updated License Header

--- Services/PermanentLink/test/ilServicesPermanentLinkSuite.php
+++ Services/PermanentLink/test/ilServicesPermanentLinkSuite.php
@@ -3,18 +3,22 @@
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
+ *
  * ILIAS is licensed with the GPL-3.0,
  * see https://www.gnu.org/licenses/gpl-3.0.en.html
  * You should have received a copy of said license along with the
  * source code, too.
+ *
  * If this is not the case or you just want to try ILIAS, you'll find
  * us at:
  * https://www.ilias.de
  * https://github.com/ILIAS-eLearning
- */
-
+ *
+ *********************************************************************/
+ 
 use PHPUnit\Framework\TestSuite;
 
+/** @noRector */
 require_once 'libs/composer/vendor/autoload.php';
 
 /**
@@ -26,7 +30,7 @@ class ilServicesPermanentLinkSuite extends TestSuite
     public static function suite() : self
     {
         $suite = new self();
-
+        /** @noRector */
         require_once("./Services/PermanentLink/test/PermanentLinkGUITest.php");
         $suite->addTestSuite("PermanentLinkGUITest");
 
