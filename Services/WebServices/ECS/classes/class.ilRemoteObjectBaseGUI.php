cc44ce4873e890ab458ad8dafa440c0885a9a339
smeyer <smeyer.ilias@gmx.de>
2022-04-21 14:13:59 +0200

    ecs: consent modal

--- Services/WebServices/ECS/classes/class.ilRemoteObjectBaseGUI.php
+++ Services/WebServices/ECS/classes/class.ilRemoteObjectBaseGUI.php
@@ -56,9 +56,20 @@ abstract class ilRemoteObjectBaseGUI extends ilObject2GUI
                 $this->ctrl->forwardCommand($gui);
                 break;
 
+            case strtolower(ilECSUserConsentModalGUI::class):
+                $consent_gui = new ilECSUserConsentModalGUI(
+                    $this->user->getId(),
+                    $this->ref_id,
+                    $this->getObject()->getMID(),
+                    $this->getObject()
+                );
+                $this->ctrl->setReturn($this, 'call');
+                $this->ctrl->forwardCommand($consent_gui);
+                break;
+
             default:
                 if (!$cmd || $cmd === 'view') {
-                    $cmd = "editSettings";
+                    $cmd = "infoScreen";
                 }
                 $cmd .= "Object";
                 $this->logger->info("cmd before call:" . print_r($cmd, true));
@@ -152,28 +163,25 @@ abstract class ilRemoteObjectBaseGUI extends ilObject2GUI
     public function infoScreen() : void
     {
         if (!$this->access->checkAccess("visible", "", $this->object->getRefId())) {
-            $this->error->raiseError($this->lng->txt('msg_no_perm_read'), $this->error->MESSAGE);
+            $this->error->raiseError(
+                $this->lng->txt('msg_no_perm_read'), $this->error->MESSAGE
+            );
         }
-        
+
+        $this->ctrl->setReturn($this,'call');
+        $consent_gui = new ilECSUserConsentModalGUI(
+            $this->user->getId(),
+            $this->getRefId(),
+            $this->getObject()->getMID(),
+            $this->getObject(),
+            $this
+        );
+        $consent_gui->addLinkToToolbar($this->toolbar);
+
         $this->tabs_gui->activateTab('info');
 
         $info = new ilInfoScreenGUI($this);
-    
-        if ($this->user->getId() === ANONYMOUS_USER_ID ||
-            $this->object->isLocalObject()) {
-            $info->addButton(
-                $this->lng->txt($this->getType() . '_call'),
-                $this->object->getRemoteLink(),
-                'target="_blank"'
-            );
-        } else {
-            $info->addButton(
-                $this->lng->txt($this->getType() . '_call'),
-                $this->ctrl->getLinkTarget($this, 'call'),
-                'target="_blank"'
-            );
-        }
-        
+
         $info->addSection($this->lng->txt('ecs_general_info'));
         $info->addProperty($this->lng->txt('title'), $this->object->getTitle());
         if ($this->object->getOrganization()) {
