04fb6c4eb26b6913e06a6db33b20b10fc472dab8
smeyer <smeyer.ilias@gmx.de>
2022-04-21 14:44:07 +0200

    ecs: use ui message box for failure

--- Services/WebServices/ECS/classes/Consent/class.ilECSUserConsentModalGUI.php
+++ Services/WebServices/ECS/classes/Consent/class.ilECSUserConsentModalGUI.php
@@ -103,19 +103,6 @@ class ilECSUserConsentModalGUI
         return $this->consents->hasConsented($this->mid);
     }
 
-    public function executeCommand()
-    {
-        $cmd = $this->ctrl->getCmd(self::CMD_RENDER_MODAL);
-        //$cmd(); // no idea why this fails
-        switch ($cmd) {
-            case self::CMD_RENDER_MODAL:
-                $this->renderConsentModal();
-                break;
-            case self::CMD_SAVE_CONSENT:
-                $this->saveConsent();
-                break;
-        }
-    }
 
     protected function initMid() : void
     {
@@ -145,7 +132,7 @@ class ilECSUserConsentModalGUI
         if (
             $this->usr_id === ANONYMOUS_USER_ID ||
             $this->isLocalObject() ||
-            $this->consents->hasConsented($this->mid)
+            $this->hasConsented()
         ) {
             return '';
         }
@@ -161,7 +148,7 @@ class ilECSUserConsentModalGUI
         ) {
             return;
         }
-        if ($this->consents->hasConsented(1)) {
+        if ($this->hasConsented()) {
             $this->addRemoteLinkToToolbar($toolbar);
 
         } else {
@@ -224,17 +211,22 @@ class ilECSUserConsentModalGUI
 
         $submitted = (string) ($this->request->getParsedBody()['cmd'] ?? '');
         $valid = true;
+        $error_html = '';
         if (strcmp($submitted, 'submit') === 0) {
             if (!$this->saveConsent($form)) {
                 $form->setValuesByPost();
-                $form->getItemByPostVar('consent')->setAlert($this->lng->txt('ecs_consent_required'));
+                $error = $this->ui_factory->messageBox()->failure($this->lng->txt('ecs_consent_required'));
+                $error_html = $this->ui_renderer->render([$error]);
                 $valid = false;
             }
         }
 
         $modal = $this->ui_factory->modal()->roundtrip(
             $this->lng->txt('ecs_consent_modal_title'),
-            $this->ui_factory->legacy($form->getHTML())
+            $this->ui_factory->legacy(
+                $error_html .
+                $form->getHTML()
+            )
         )->withActionButtons([$agree]);
         if (!$valid) {
             $modal = $modal->withOnLoad($modal->getShowSignal());
@@ -276,9 +268,7 @@ class ilECSUserConsentModalGUI
     protected function initConsentForm()
     {
         $form = new ilPropertyFormGUI();
-        $form->setTableWidth('100%');
         $form->setId(uniqid('form'));
-        $form->setTarget('_top');
         $form->setFormAction('#');
 
         $title = new ilNonEditableValueGUI(
