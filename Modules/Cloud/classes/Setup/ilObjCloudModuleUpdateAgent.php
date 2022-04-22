f8c8ce0a07e614a4109c72d17832681fc365db7a
Timon Amstutz <timon.amstutz@bluewin.ch>
2022-04-22 17:46:30 +0200

    Cloud Module: Abandon the Module Part II

--- Modules/Cloud/classes/Setup/ilObjCloudModuleUpdateAgent.php
+++ Modules/Cloud/classes/Setup/ilObjCloudModuleUpdateAgent.php
@@ -1,20 +1,36 @@
 <?php declare(strict_types=1);
 
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
 use ILIAS\Setup;
 use ILIAS\Setup\Objective;
 use ILIAS\Setup\Metrics;
 
-class ilStudyProgrammeUpdateAgent extends Setup\Agent\NullAgent
+class ilObjCloudModuleUpdateAgent extends Setup\Agent\NullAgent
 {
     public function getUpdateObjective(Setup\Config $config = null) : Setup\Objective
     {
         return new ilDatabaseUpdateStepsExecutedObjective(
-            new ilStudyProgrammeProgressTableUpdateSteps()
+            new ilObjCloudModuleProgressTableUpdateSteps()
         );
     }
 
     public function getStatusObjective(Metrics\Storage $storage) : Objective
     {
-        return new \ilDatabaseUpdateStepsMetricsCollectedObjective($storage, new ilStudyProgrammeProgressTableUpdateSteps());
+        return new \ilDatabaseUpdateStepsMetricsCollectedObjective($storage, new ilObjCloudModuleProgressTableUpdateSteps());
     }
 }
--- Modules/Cloud/classes/class.ilObjCloudListGUI.php
+++ Modules/Cloud/classes/class.ilObjCloudListGUI.php
@@ -1,145 +1,53 @@
-<?php
-/* Copyright (c) 1998-2010 ILIAS open source, Extended GPL, see docs/LICENSE */
-
+<?php declare(strict_types=0);
 /**
- * Class ilObjCloudListGUI
- * @author  Timon Amstutz <timon.amstutz@ilub.unibe.ch>
- * @author  Martin Studer martin@fluxlabs.ch
- * $Id:
- * @extends ilObjectListGUI
- */
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
 class ilObjCloudListGUI extends ilObjectListGUI
 {
     public function init() : void
     {
-        global $DIC;
-        $lng = $DIC['lng'];
-
-        $this->copy_enabled = false;
-        $this->delete_enabled = true;
+        // Create static links for default command (linked title) or not
+        $this->static_link_enabled = true;
+        $this->delete_enabled = false;
         $this->cut_enabled = false;
-        $this->subscribe_enabled = true;
+        $this->subscribe_enabled = false;
         $this->link_enabled = false;
-        $this->info_screen_enabled = true;
-        $this->timings_enabled = true;
-        $this->type = "cld";
-        $this->gui_class_name = "ilobjcloudgui";
-
-        // general commands array
-        require_once('./Modules/Cloud/classes/class.ilObjCloudAccess.php');
-        $this->commands = ilObjCloudAccess::_getCommands();
-        $lng->loadLanguageModule("cld");
+        $this->copy_enabled = false;
+        $this->progress_enabled = false;
+        $this->notice_properties_enabled = false;
+        $this->info_screen_enabled = false;
+        $this->type = 'cld';
+        $this->gui_class_name = 'ilObjCloudGUI';
     }
 
-    public function getCommands() : array
-    {
-        $object = ilObjectFactory::getInstanceByRefId($this->ref_id);
-        $header_action_gui = ilCloudConnector::getHeaderActionGUIClass(ilCloudConnector::getServiceClass($object->getServiceName(),
-            $object->getId(), false));
-        $custom_urls = [];
-
-        if (method_exists($header_action_gui, "getCustomListActions")) {
-            // Fetch custom actions
-            $custom_list_actions = $header_action_gui->getCustomListActions();
-
-            if (is_array($custom_list_actions)) {
-                // Fetch custom URLs from the custom actions, if available
-                $custom_urls = $this->fetchCustomUrlsFromCustomActions($custom_list_actions, $custom_urls);
-                // Adjust commands of this object by adding the new custom ones
-                $this->commands = array_merge($this->commands, $custom_list_actions);
-            }
-        }
-
-        // Generate ilias link, check permissions, etc...
-        $ref_commands = parent::getCommands();
-
-        // Remove recently added custom actions from dynamic field "commands" as
-        // it may pass onto other ListGUIs and mess them up
-        if (method_exists($header_action_gui, "getCustomListActions")) {
-            $this->commands = $this->neutralizeCommands($this->commands, $custom_list_actions);
-        }
-
-        // Inject custom urls, if avilable
-        if (!empty($custom_urls)) {
-            $ref_commands = $this->injectCustomUrlsInCommands($custom_urls, $ref_commands);
-        }
-
-        return $ref_commands;
+    public function insertCommands(
+        bool $use_async = false,
+        bool $get_async_commands = false,
+        string $async_url = "",
+        bool $header_actions = false
+    ) : string {
+        return '';
     }
 
     public function getProperties() : array
     {
-        global $DIC;
-        $lng = $DIC['lng'];
-
-        $props = array();
-        require_once('./Modules/Cloud/classes/class.ilObjCloudAccess.php');
-        if (!ilObjCloudAccess::checkAuthStatus($this->obj_id)) {
-            $props[] = array(
-                "alert" => true,
-                "property" => $lng->txt("status"),
-                "value" => $lng->txt("cld_not_authenticated_offline"),
-            );
-        } else {
-            if (!ilObjCloudAccess::checkOnline($this->obj_id)) {
-                $props[] = array(
-                    "alert" => true,
-                    "property" => $lng->txt("status"),
-                    "value" => $lng->txt("offline"),
-                );
-            }
-        }
-
-        return $props;
-    }
-
-    /**
-     * Remove recently added custom actions from dynamic field "commands" as
-     * it may pass onto other ListGUIs and mess them up
-     * @param array $commands
-     * @param array $custom_list_actions
-     */
-    private function neutralizeCommands(array $commands, array $custom_list_actions) : array
-    {
-        foreach ($custom_list_actions as $custom_list_action) {
-            for ($i = 0, $iMax = count($commands); $i < $iMax; $i++) {
-                if ($commands[$i]["lang_var"] === $custom_list_action["lang_var"]) {
-                    unset($commands[$i]);
-                }
-            }
-        }
-        return $commands;
-    }
-
-    /**
-     * Inject predefined custom URLs into ref_commands and change its destination
-     */
-    private function injectCustomUrlsInCommands(array $custom_urls, array $ref_commands) : array
-    {
-        foreach ($custom_urls as $custom_url) {
-            foreach ($ref_commands as &$ref_command) {
-                if ($custom_url["id"] === $ref_command["lang_var"]) {
-                    $ref_command["link"] = $custom_url["link"];
-                }
-            }
-        }
-        return $ref_commands;
-    }
-
-    /**
-     * Fetches custom URLs from predefined actions and structures them appropriately
-     */
-    private function fetchCustomUrlsFromCustomActions(array $custom_list_actions, array $custom_urls) : array
-    {
-        foreach ($custom_list_actions as $custom_list_action) {
-            if (array_key_exists("custom_url", $custom_list_action)) {
-                $custom_urls[] = [
-                    "id" => $custom_list_action["lang_var"],
-                    "link" => $custom_list_action["custom_url"],
-                ];
-            }
-        }
-
-        return $custom_urls;
+        return [[
+            'alert' => true,
+            'property' => $this->lng->txt('status'),
+            'value' => $this->lng->txt('abandoned'),
+        ]];
     }
 }
