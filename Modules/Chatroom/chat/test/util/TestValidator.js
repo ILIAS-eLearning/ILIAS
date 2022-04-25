588b1d6ee4bb59e2d2d70589b2f510bb1e525e26
mjansen <mjansen@databay.de>
2022-04-25 16:32:36 +0200

    Chatroom: Remove obsolete test directory

--- Modules/Chatroom/chat/test/util/TestValidator.js
+++
@@ -1,8 +0,0 @@
-var AbstractValidator = require('../../Validation/Validator/AbstractValidator');
-
-var TestValidator = function(){
-	this.name = "TestValidator";
-};
-TestValidator.prototype = new AbstractValidator();
-
-module.exports = TestValidator;
