f07d3b3e88361d332f4974e73271503b208b75db
mjansen <mjansen@databay.de>
2022-05-11 19:26:01 +0200

    Registration: Fix missing default value when using `stripslashes` (PHP 8.1 / ILIAS 9)

--- Services/Registration/classes/class.ilRegistrationSettings.php
+++ Services/Registration/classes/class.ilRegistrationSettings.php
@@ -54,11 +54,15 @@ class ilRegistrationSettings
 
     protected ilSetting $settings;
 
-    public function __construct()
+    public function __construct(ilSetting $settings = null)
     {
         global $DIC;
 
-        $this->settings = $DIC->settings();
+        if (null == $settings) {
+            $settings = $DIC->settings();
+        }
+
+        $this->settings = $settings;
         $this->read();
     }
 
@@ -265,7 +269,7 @@ class ilRegistrationSettings
         $this->reg_allow_codes = (bool) $this->settings->get('reg_allow_codes');
 
         $this->approve_recipient_ids = unserialize(
-            stripslashes($this->settings->get('approve_recipient', "")),
+            stripslashes($this->settings->get('approve_recipient', serialize([]))),
             ['allowed_classes' => false]
         ) ?: [];
 
