8ccbfd9e47a6aeaf5bc6a218a90201494b6ba21e
Vincenzo Padula <padvincenzo@gmail.com>
2022-05-15 19:47:52 +0200

    Fix a mysql error
    This error happens when reloading the Ctrl structure, because `system` is a reserved word.

--- setup/classes/class.ilObjDefReader.php
+++ setup/classes/class.ilObjDefReader.php
@@ -143,7 +143,7 @@ class ilObjDefReader extends ilSaxParser
                     $ilDB->manipulateF(
                         "INSERT INTO il_object_def (id, class_name, component,location," .
                         "checkbox,inherit,translate,devmode,allow_link,allow_copy,rbac,default_pos," .
-                        "default_pres_pos,sideblock,grp,system,export,repository,workspace,administration," .
+                        "default_pres_pos,sideblock,grp,il_object_def.system,export,repository,workspace,administration," .
                         "amet,orgunit_permissions,lti_provider,offline_handling) VALUES " .
                         "(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
                         array("text", "text", "text", "text", "integer", "integer", "text", "integer","integer","integer",
