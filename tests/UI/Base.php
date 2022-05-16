5d867c9c294e7673f0f78c5bccf49e2a646ff0cb
iszmais <iszmais@databay.de>
2022-05-16 11:55:03 +0200

    Add DecoratedRenderer to unify UI Handling and manipulations of external sources

--- tests/UI/Base.php
+++ tests/UI/Base.php
@@ -10,6 +10,7 @@ require_once(__DIR__ . "/../../Services/Language/classes/class.ilLanguage.php");
 use ILIAS\UI\Component as C;
 use ILIAS\UI\Component\Component as IComponent;
 use ILIAS\UI\Implementaiton\Component as I;
+use ILIAS\UI\Implementation\Render\DecoratedRenderer;
 use ILIAS\UI\Implementation\Render\TemplateFactory;
 use ILIAS\UI\Implementation\Render\ResourceRegistry;
 use ILIAS\UI\Implementation\Render\JavaScriptBinding;
@@ -20,6 +21,7 @@ use ILIAS\UI\Implementation\Render;
 use ILIAS\UI\Implementation\Component\Symbol\Glyph\GlyphRendererFactory;
 use ILIAS\UI\Implementation\Component\Input\Field\FieldRendererFactory;
 use ILIAS\UI\Factory;
+use ILIAS\UI\Renderer;
 use PHPUnit\Framework\TestCase;
 
 class ilIndependentTemplateFactory implements TemplateFactory
@@ -175,6 +177,25 @@ class TestDefaultRenderer extends DefaultRenderer
     }
 }
 
+class TestDecoratedRenderer extends DecoratedRenderer
+{
+    private $manipulate = false;
+
+    public function manipulate() : void
+    {
+        $this->manipulate = true;
+    }
+
+    protected function manipulateRendering($component, Renderer $root) : ?string
+    {
+        if ($this->manipulate) {
+            return "This content was manipulated";
+        } else {
+            return null;
+        }
+    }
+}
+
 class IncrementalSignalGenerator extends \ILIAS\UI\Implementation\Component\SignalGenerator
 {
     protected $id = 0;
@@ -286,6 +307,11 @@ abstract class ILIAS_UI_TestBase extends TestCase
                 )
             );
         return new TestDefaultRenderer($component_renderer_loader);
+    }
+    
+    public function getDecoratedRenderer(Renderer $default)
+    {
+        return new TestDecoratedRenderer($default);
     }
 
     public function normalizeHTML($html)
