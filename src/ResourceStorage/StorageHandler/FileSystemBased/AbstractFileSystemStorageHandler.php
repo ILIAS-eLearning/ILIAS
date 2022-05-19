7c91d32fc7b7f8b0d33bd5a61a06ac3d158a3a6b
Fabian Schmid <fabian@sr.solutions>
2022-05-19 07:42:32 +0200

    LicenseHeader

--- src/ResourceStorage/StorageHandler/FileSystemBased/AbstractFileSystemStorageHandler.php
+++ src/ResourceStorage/StorageHandler/FileSystemBased/AbstractFileSystemStorageHandler.php
@@ -1,5 +1,20 @@
 <?php declare(strict_types=1);
 
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ */
+ 
 namespace ILIAS\ResourceStorage\StorageHandler\FileSystemBased;
 
 use ILIAS\Filesystem\Filesystem;
@@ -17,19 +32,6 @@ use ILIAS\ResourceStorage\Revision\UploadedFileRevision;
 use ILIAS\ResourceStorage\StorageHandler\StorageHandler;
 use ILIAS\ResourceStorage\StorageHandler\PathGenerator\PathGenerator;
 
-/******************************************************************************
- *
- * This file is part of ILIAS, a powerful learning management system.
- *
- * ILIAS is licensed with the GPL-3.0, you should have received a copy
- * of said license along with the source code.
- *
- * If this is not the case or you just want to try ILIAS, you'll find
- * us at:
- *      https://www.ilias.de
- *      https://github.com/ILIAS-eLearning
- *
- *****************************************************************************/
 /**
  * Class AbstractFileSystemStorageHandler
  * @author  Fabian Schmid <fs@studer-raimann.ch>
