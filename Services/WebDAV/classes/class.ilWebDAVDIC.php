e9f60bc14d26c1a75b47addccfbbae9e5a8d18be
Stephan Winiker <stephan.winiker@hslu.ch>
2022-05-20 11:02:13 +0200

    Fix License Headers

--- Services/WebDAV/classes/class.ilWebDAVDIC.php
+++ Services/WebDAV/classes/class.ilWebDAVDIC.php
@@ -6,19 +6,22 @@ use Sabre\DAV\Auth\Plugin as AuthPlugin;
 use Sabre\DAV\Locks\Plugin as LocksPlugin;
 use Sabre\DAV\Auth\Backend\BasicCallBack;
 
-/******************************************************************************
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
  *
- * This file is part of ILIAS, a powerful learning management system.
- *
- * ILIAS is licensed with the GPL-3.0, you should have received a copy
- * of said license along with the source code.
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
  *
  * If this is not the case or you just want to try ILIAS, you'll find
  * us at:
  * https://www.ilias.de
  * https://github.com/ILIAS-eLearning
  *
- *****************************************************************************/
+ *********************************************************************/
+ 
 class ilWebDAVDIC extends Container
 {
     public function initWithoutDIC() : void
