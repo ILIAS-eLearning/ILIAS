71641feda88a46188776aee503022d33a7932d90
Alexander Killing <killing@leifos.de>
2022-05-23 15:06:07 +0200

    merged https://github.com/ILIAS-eLearning/ILIAS/pull/4538; thanks to swiniker

--- Services/Taxonomy/classes/class.ilObjTaxonomyGUI.php
+++ Services/Taxonomy/classes/class.ilObjTaxonomyGUI.php
@@ -1,7 +1,21 @@
 <?php
 
-/* Copyright (c) 1998-2021 ILIAS open source, GPLv3, see LICENSE */
-
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+ 
 /**
  * Taxonomy GUI class
  * @author       Alexander Killing <killing@leifos.de>
@@ -18,7 +32,7 @@ class ilObjTaxonomyGUI extends ilObject2GUI
     protected string $assigned_item_comp_id;
     protected int $assigned_item_obj_id;
     protected string $assigned_item_type;
-    protected string $list_info;
+    protected string $list_info = '';
     protected int $current_tax_node;
     protected int $requested_tax_id;
     protected string $requested_move_ids;
--- Services/Taxonomy/classes/class.ilTaxonomyClassificationProvider.php
+++ Services/Taxonomy/classes/class.ilTaxonomyClassificationProvider.php
@@ -1,7 +1,21 @@
 <?php
 
-/* Copyright (c) 1998-2021 ILIAS open source, GPLv3, see LICENSE */
-
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+ 
 /**
  * Taxonomy classification provider
  * @author Jörg Lützenkirchen <luetzenkirchen@leifos.com>
@@ -31,7 +45,7 @@ class ilTaxonomyClassificationProvider extends ilClassificationProvider
             $tax_exp->setSkipRootNode(true);
             $tax_exp->setOnClick("il.Classification.toggle({tax_node: '{NODE_CHILD}'});");
 
-            if (is_array($this->selection)) {
+            if (isset($this->selection) && is_array($this->selection)) {
                 foreach ($this->selection as $node_id) {
                     $tax_exp->setPathOpen($node_id);
                     $tax_exp->setNodeSelected($node_id);
