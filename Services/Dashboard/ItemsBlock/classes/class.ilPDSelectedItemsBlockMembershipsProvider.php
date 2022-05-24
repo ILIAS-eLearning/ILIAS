d9e1da01eaef7f8298d31a65b5d64f80b1ab2ee6
iszmais <iszmais@databay.de>
2022-05-24 14:20:45 +0200

    unified dashboard visibilities with common repository visibilities

--- Services/Dashboard/ItemsBlock/classes/class.ilPDSelectedItemsBlockMembershipsProvider.php
+++ Services/Dashboard/ItemsBlock/classes/class.ilPDSelectedItemsBlockMembershipsProvider.php
@@ -63,8 +63,7 @@ class ilPDSelectedItemsBlockMembershipsProvider implements ilPDSelectedItemsBloc
             $parentTreeLftValue = $item->getParentLftTree();
             $parentTreeLftValue = sprintf("%010d", $parentTreeLftValue);
 
-            if (!$this->access->checkAccess('read', '', $refId) &&
-                !$this->access->checkAccess('visible', '', $refId)) {
+            if (!$this->access->checkAccess('visible', '', $refId)) {
                 continue;
             }
 
