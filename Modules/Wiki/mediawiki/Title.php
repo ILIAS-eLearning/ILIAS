56512c99f1b30320e9f8c24ad0eb87914b16c65d
Stephan Winiker <stephan.winiker@hslu.ch>
2022-05-25 13:47:39 +0200

    Revert "Fix License header for Title.php after merge of PR"
    This reverts commit 8b249d0b0a849e2a6e8da8680f35aa6a60fd33ae.

--- Modules/Wiki/mediawiki/Title.php
+++ Modules/Wiki/mediawiki/Title.php
@@ -17,22 +17,6 @@ define('MW_TITLECACHE_MAX', 1000);
 define('CASCADE', 1);
 
 /**
- * This file is part of ILIAS, a powerful learning management system
- * published by ILIAS open source e-Learning e.V.
- *
- * ILIAS is licensed with the GPL-3.0,
- * see https://www.gnu.org/licenses/gpl-3.0.en.html
- * You should have received a copy of said license along with the
- * source code, too.
- *
- * If this is not the case or you just want to try ILIAS, you'll find
- * us at:
- * https://www.ilias.de
- * https://github.com/ILIAS-eLearning
- *
- *********************************************************************/
- 
-/**
  * Title class
  * - Represents a title, which may contain an interwiki designation or namespace
  * - Can fetch various kinds of data from the database, albeit inefficiently.
