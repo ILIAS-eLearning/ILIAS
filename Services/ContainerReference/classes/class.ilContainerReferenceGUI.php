8a02f8497aeb7caf985901305ccc47e096b3a225
Alexander Killing <killing@leifos.de>
2022-06-07 13:12:17 +0200

    fixed bug #31863: Category Link: New language variable needed for button Erstellen in Creation Dialog

--- Services/ContainerReference/classes/class.ilContainerReferenceGUI.php
+++ Services/ContainerReference/classes/class.ilContainerReferenceGUI.php
@@ -222,7 +222,8 @@ class ilContainerReferenceGUI extends ilObjectGUI
 
         $form->setFormAction($this->ctrl->getFormAction($this));
         if ($a_mode === self::MODE_CREATE) {
-            $form->addCommandButton('save', $this->lng->txt('create'));
+            $lv = $this->getTargetType() . "r_add";   // see also https://mantis.ilias.de/view.php?id=31863
+            $form->addCommandButton('save', $this->lng->txt($lv));
             $form->addCommandButton('cancel', $this->lng->txt('cancel'));
         } else {
             $form->addCommandButton('update', $this->lng->txt('save'));
