8f2acc4b7e7aa2f733445ae574e4440e01909376
Thibeau Fuhrer <thibeau@sr.solutions>
2022-06-14 14:11:32 +0200

    [FIX] introduced constant authentication-time to prevent leaking information via side-channel.

--- Services/PrivacySecurity/classes/Setup/class.ilPrivacySecuritySetupConfig.php
+++ Services/PrivacySecurity/classes/Setup/class.ilPrivacySecuritySetupConfig.php
@@ -2,24 +2,41 @@
 
 declare(strict_types=1);
 
-/* Copyright (c) 2020 Daniel Weise <daniel.weise@concepts-and-training.de> Extended GPL, see docs/LICENSE */
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ */
 
 use ILIAS\Setup;
 
 class ilPrivacySecuritySetupConfig implements Setup\Config
 {
-    /**
-     * @var bool
-     */
     protected bool $force_https_on_login;
+    protected ?int $duration_in_ms;
 
-    public function __construct(bool $force_https_on_login = false)
+    public function __construct(bool $force_https_on_login = false, ?int $duration_in_ms = null)
     {
         $this->force_https_on_login = $force_https_on_login;
+        $this->duration_in_ms = $duration_in_ms;
     }
 
     public function getForceHttpsOnLogin(): bool
     {
         return $this->force_https_on_login;
+    }
+
+    public function getAuthDurationInMs() : ?int
+    {
+        return $this->duration_in_ms;
     }
 }
---
+++ src/HTTP/Duration/Duration.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ */
+
+namespace ILIAS\HTTP\Duration;
+
+use ILIAS\HTTP\Duration\Increment\IncrementStrategy;
+
+/**
+ * @author Thibeau Fuhrer <thibeau@sr.solutions>
+ */
+abstract class Duration
+{
+    /**
+     * number used for converting seconds (s) into microseconds (us/µ) or vise-versa.
+     */
+    protected const S_TO_US = 1_000_000;
+
+    /**
+     * number used for converting miliseconds (ms) into microseconds (us/µ) or vise-versa.
+     */
+    protected const MS_TO_US = 1_000;
+
+    protected ?IncrementStrategy $increment = null;
+    protected int $duration_in_ms;
+
+    public function __construct(int $duration_in_ms)
+    {
+        $this->duration_in_ms = $duration_in_ms;
+    }
+
+    public function withIncrement(IncrementStrategy $increment) : self
+    {
+        $clone = clone $this;
+        $clone->increment = $increment;
+
+        return $clone;
+    }
+
+    public function increment() : self
+    {
+        if (null === $this->increment) {
+            return $this;
+        }
+
+        $clone = clone $this;
+        $clone->duration_in_ms = $clone->increment->increment($clone->duration_in_ms);
+
+        return $clone;
+    }
+
+    public function withDuration(int $duration_in_ms) : self
+    {
+        $clone = clone $this;
+        $clone->duration_in_ms = $duration_in_ms;
+
+        return $clone;
+    }
+
+    public function getDuration() : int
+    {
+        return $this->duration_in_ms;
+    }
+}
