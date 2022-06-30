695a13f443bbed5156513709d2b6be518202212d
Tim Schmitz <schmitz@leifos.de>
2022-06-30 19:02:54 +0200

    a few php8 bugfixes

--- Services/AccessControl/classes/class.ilObjectRolePermissionTableGUI.php
+++ Services/AccessControl/classes/class.ilObjectRolePermissionTableGUI.php
@@ -482,7 +482,7 @@ class ilObjectRolePermissionTableGUI extends ilTable2GUI
         $roles = $this->review->getParentRoleIds($this->getRefId());
         $roles = $this->getParentObject()->applyRoleFilter(
             $roles,
-            $this->getFilterItemByPostVar('role')->getValue()
+            (int) $this->getFilterItemByPostVar('role')->getValue()
         );
 
         $possible_roles = array();
