d903ceed9c1bab5e154babf58ae00087dfa74d3d
Alexander Killing <killing@leifos.de>
2022-07-04 18:29:13 +0200

    33278: Pre8 Insert Resource List

--- Services/COPage/classes/class.ilPCResourcesGUI.php
+++ Services/COPage/classes/class.ilPCResourcesGUI.php
@@ -265,7 +265,7 @@ class ilPCResourcesGUI extends ilPageContentGUI
                 $tpl = new ilTemplate("tpl.resource_block.html", true, true, "Services/COPage");
                 $cnt = 0;
                 
-                if (is_array($childs_by_type[$type]) && count($childs_by_type[$type]) > 0) {
+                if (isset($childs_by_type[$type]) && count($childs_by_type[$type]) > 0) {
                     foreach ($childs_by_type[$type] as $child) {
                         $tpl->setCurrentBlock("row");
                         $tpl->setVariable("IMG", ilUtil::img(ilObject::_getIcon((int) $child["obj_id"], "small")));
