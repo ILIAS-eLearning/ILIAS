a15c86458e4bca00bdfe93c4b10e717074794620
Nils Haagen <nhaagen@cat06.de>
2022-07-04 13:44:26 +0200

    Object/Setup: use ilDB from environment instead of global DIC

--- Services/Tree/classes/Setup/class.ilTreeAdminNodeAddedObjective.php
+++ Services/Tree/classes/Setup/class.ilTreeAdminNodeAddedObjective.php
@@ -1,5 +1,21 @@
 <?php declare(strict_types=1);
 
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
 /* Copyright (c) 2021 - Daniel Weise <daniel.weise@concepts-and-training.de> - Extended GPL, see LICENSE */
 
 use ILIAS\Setup;
@@ -113,6 +129,7 @@ class ilTreeAdminNodeAddedObjective implements Setup\Objective
 
     public function isApplicable(Environment $environment) : bool
     {
-        return !((bool) ilObject::_getObjectTypeIdByTitle($this->type));
+        $db = $environment->getResource(Environment::RESOURCE_DATABASE);
+        return !((bool) ilObject::_getObjectTypeIdByTitle($this->type, $db));
     }
 }
