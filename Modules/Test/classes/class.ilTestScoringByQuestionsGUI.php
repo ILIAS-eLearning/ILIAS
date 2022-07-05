eeea1eb4c56e16963373a6bd4ea22d8800d69771
Maximilian Becker <mbecker@databay.de>
2022-07-05 10:37:45 +0200

    T&A: fix 31400 remove Char-Selector from manual feedback modal #4705

--- Modules/Test/classes/class.ilTestScoringByQuestionsGUI.php
+++ Modules/Test/classes/class.ilTestScoringByQuestionsGUI.php
@@ -1,5 +1,20 @@
 <?php
-/* Copyright (c) 1998-2013 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
 
 include_once 'Modules/Test/classes/inc.AssessmentConstants.php';
 include_once 'Modules/Test/classes/class.ilTestScoringGUI.php';
@@ -548,6 +563,9 @@ class ilTestScoringByQuestionsGUI extends ilTestScoringGUI
         $form->addItem($evaluated);
 
         $form->addCommandButton('checkConstraintsBeforeSaving', $this->lng->txt('save'));
+
+        $CharSelector = ilCharSelectorGUI::_getCurrentGUI();
+        $CharSelector->getConfig()->setAvailability(ilCharSelectorConfig::DISABLED);
 
         $tmp_tpl->setVariable(
             'MANUAL_FEEDBACK',
