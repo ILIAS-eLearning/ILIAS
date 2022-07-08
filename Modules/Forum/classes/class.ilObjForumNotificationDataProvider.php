0ed2c7305157972c7adb67a9e72e286662feefeb
Fabian Helfer <fhelfer@nb-fhelfer.office.databay.de>
2022-07-08 10:46:53 +0200

    added top_item to notification cache for performance

--- Modules/Forum/classes/class.ilObjForumNotificationDataProvider.php
+++ Modules/Forum/classes/class.ilObjForumNotificationDataProvider.php
@@ -272,23 +272,25 @@ class ilObjForumNotificationDataProvider implements ilForumNotificationMailData
 
             $row = $this->db->fetchAssoc($result);
 
+            $top_item_crs_ref = $this->tree->checkForParentType($this->getRefId(), 'crs');
+            $top_item_grp_ref = $this->tree->checkForParentType($this->getRefId(), 'grp');
+            $top_item_ref_id = $top_item_crs_ref > 0 ? $top_item_crs_ref : $top_item_grp_ref;
+            if ($top_item_ref_id) {
+                $top_item = ilObjectFactory::getInstanceByRefId($top_item_ref_id);
+                if ($top_item instanceof ilObjCourse || $top_item instanceof ilObjGroup) {
+                    $row['top_item_title'] = $top_item->getTitle();
+                    $row['top_item_type'] = $top_item->getType();
+                }
+            }
+
             $this->notificationCache->store($cacheKey, $row);
         }
 
-        $row = $this->notificationCache->fetch($cacheKey);
+        $row = $row ?? $this->notificationCache->fetch($cacheKey);
         $this->forum_id = (int) $row['top_pk'];
         $this->forum_title = (string) $row['top_name'];
-
-        $top_item_crs_ref = $this->tree->checkForParentType($this->getRefId(), 'crs');
-        $top_item_grp_ref = $this->tree->checkForParentType($this->getRefId(), 'grp');
-        $top_item_ref_id = $top_item_crs_ref > 0 ? $top_item_crs_ref : $top_item_grp_ref;
-        if ($top_item_ref_id) {
-            $top_item = ilObjectFactory::getInstanceByRefId($top_item_ref_id);
-            if ($top_item instanceof ilObjCourse || $top_item instanceof ilObjGroup) {
-                $this->top_item_title = $top_item->getTitle();
-                $this->top_item_type = $top_item->getType();
-            }
-        }
+        $this->top_item_title = $row['top_item_title'] ?? '';
+        $this->top_item_type = $row['top_item_type'] ?? '';
 
         $this->is_anonymized = (bool) $row['anonymized'];
     }
