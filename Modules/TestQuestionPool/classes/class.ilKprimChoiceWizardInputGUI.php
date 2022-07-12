bd97b798aa20caec2c453c62dfb13187df7f72cc
Maximilian Becker <mbecker@databay.de>
2022-07-12 09:27:51 +0200

    0033311: Pre8 Kprim Question

--- Modules/TestQuestionPool/classes/class.ilKprimChoiceWizardInputGUI.php
+++ Modules/TestQuestionPool/classes/class.ilKprimChoiceWizardInputGUI.php
@@ -79,7 +79,7 @@ class ilKprimChoiceWizardInputGUI extends ilSingleChoiceWizardInputGUI
                     $answer->setImageFile($a_value['imagename'][$index]);
                 }
                 
-                if (strlen($a_value['correctness'][$index])) {
+                if (isset($a_value['correctness']) && strlen($a_value['correctness'][$index])) {
                     $answer->setCorrectness((bool) $a_value['correctness'][$index]);
                 }
 
