0c6cade39edacfd44527245cfd5947ce7b324a4d
smeyer <smeyer.ilias@gmx.de>
2022-07-19 18:33:49 +0200

    trash: fixed restoring

--- Services/SystemCheck/classes/class.ilSCTaskTableGUI.php
+++ Services/SystemCheck/classes/class.ilSCTaskTableGUI.php
@@ -1,6 +1,19 @@
 <?php declare(strict_types=1);
-
-/* Copyright (c) 1998-2009 ILIAS open source, Extended GPL, see docs/LICENSE */
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
 
 /**
  * Table GUI for system check task overview
