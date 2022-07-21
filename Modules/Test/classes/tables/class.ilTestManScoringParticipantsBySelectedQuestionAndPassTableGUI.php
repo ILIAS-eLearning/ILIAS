92d3e14ab0a574d76c9548321f2140e5e426e5dc
mjansen <mjansen@databay.de>
2022-07-21 08:20:24 +0200

    Test: Use constant when creating ilDateTime

--- Modules/Test/classes/tables/class.ilTestManScoringParticipantsBySelectedQuestionAndPassTableGUI.php
+++ Modules/Test/classes/tables/class.ilTestManScoringParticipantsBySelectedQuestionAndPassTableGUI.php
@@ -186,7 +186,7 @@ class ilTestManScoringParticipantsBySelectedQuestionAndPassTableGUI extends ilTa
         }
         $fin_timestamp = $a_set['finalized_tstamp'] ?? 0;
         if ($fin_timestamp > 0) {
-            $time = new ilDateTime($fin_timestamp, 3);
+            $time = new ilDateTime($fin_timestamp, IL_CAL_UNIX);
             $this->tpl->setVariable('VAL_FINALIZED_ON', \ilDatePresentation::formatDate($time));
         }
 
