36594ef9a3b2ce6e30b76d19dfcb8ee8b5f2c975
Uwe Kohnle <kohnle@internetlehrer-gmbh.de>
2022-07-31 20:14:24 +0000

    changed copyright for classes and test (Services/LTI)

--- Services/LTI/test/ilServicesLTISuite.php
+++ Services/LTI/test/ilServicesLTISuite.php
@@ -1,21 +1,24 @@
 <?php declare(strict_types=1);
 
-use PHPUnit\Framework\TestSuite;
-use PHPUnit\Framework\TestCase;
-
-/******************************************************************************
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
  *
- * This file is part of ILIAS, a powerful learning management system.
- *
- * ILIAS is licensed with the GPL-3.0, you should have received a copy
- * of said license along with the source code.
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
  *
  * If this is not the case or you just want to try ILIAS, you'll find
  * us at:
- *      https://www.ilias.de
- *      https://github.com/ILIAS-eLearning
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
  *
- *****************************************************************************/
+ *********************************************************************/
+
+use PHPUnit\Framework\TestSuite;
+use PHPUnit\Framework\TestCase;
+
 /**
  * Class ilServicesLTISuite
  * @author Uwe Kohnle <support@internetlehrer-gmbh.de>
