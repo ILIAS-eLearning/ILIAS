88d23648748fab8beae3918b019a480af7016399
Alexander Killing <killing@leifos.de>
2022-07-31 17:18:48 +0200

    fixed $ not defined issue

---
+++ Services/Form/js/ServiceFormImageFileCapture.js
@@ -0,0 +1,73 @@
+$(function () {
+  'use strict';
+
+  /**
+   *  generates a still frame image from the stream in the <video>
+   *  appends the image to the <body>
+   */
+  function useCamera(event) {
+    event.preventDefault();
+    video.style.display = "";
+    button1.style.display = "none";
+
+
+    // access the web cam
+    navigator.mediaDevices.getUserMedia({ video: true })
+    // permission granted:
+    .then(function (stream) {
+      video.srcObject = stream;
+      button2.style.display = "";
+    })
+    // permission denied:
+    .catch(function (error) {
+      //
+    });
+  }
+
+  function takeSnapshot(event, base) {
+    event.preventDefault();
+    var img = document.getElementById(base + "_capture_img");
+    var exImg = document.getElementById(base + "_existing_img_section");
+    var hidden = document.getElementById(base + "_capture");
+    var context;
+    var width = video.offsetWidth
+      , height = video.offsetHeight;
+    canvas = document.getElementById(base + "_canvas");
+    canvas.width = width * 8;
+    canvas.height = height * 8;
+
+    context = canvas.getContext('2d');
+    context.drawImage(video, 0, 0, width * 8, height * 8);
+
+    img.style.display = "";
+    if (exImg) {
+      exImg.style.display = "none";
+    }
+    img.src = canvas.toDataURL('image/png');
+    img.width = width;
+    img.height = height;
+    hidden.value = img.src;
+  }
+
+  document.querySelectorAll("[data-form-image-file-capture-base]").forEach(button1 => {
+
+    const base = button1.dataset.formImageFileCaptureBase;
+    const video = document.getElementById(base + "_video");
+    let canvas;
+    const button2 = document.getElementById(base + "_button2");
+    if (navigator.mediaDevices) {
+      button1.style.display = "";
+      button2.style.display = "none";
+      button1.addEventListener('click', function (event) {
+        useCamera(event);
+      });
+      button2.addEventListener('click', function (event) {
+        takeSnapshot(event, base);
+      });
+    } else {
+      button1.style.display = "none";
+      button2.style.display = "none";
+    }
+  });
+
+});
