e46804d1252cf93ddb851caf6f29de95ed83f8c2
Fabian Helfer <fhelfer@nb-fhelfer.office.databay.de>
2022-08-01 11:37:32 +0200

    fix copyright dic

--- src/DI/Container.php
+++ src/DI/Container.php
@@ -23,22 +23,6 @@ use ILIAS\Repository;
 use ILIAS\Skill\Service\SkillService;
 
 /**
- * This file is part of ILIAS, a powerful learning management system
- * published by ILIAS open source e-Learning e.V.
- *
- * ILIAS is licensed with the GPL-3.0,
- * see https://www.gnu.org/licenses/gpl-3.0.en.html
- * You should have received a copy of said license along with the
- * source code, too.
- *
- * If this is not the case or you just want to try ILIAS, you'll find
- * us at:
- * https://www.ilias.de
- * https://github.com/ILIAS-eLearning
- *
- *********************************************************************/
-
-/**
  * Customizing of pimple-DIC for ILIAS.
  *
  * This just exposes some services in the container as plain methods
