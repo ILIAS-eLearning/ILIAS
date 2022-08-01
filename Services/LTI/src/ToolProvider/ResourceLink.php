afce52a5283c4e55c0d10c9cd5e66f89b7a4f205
Uwe Kohnle <kohnle@internetlehrer-gmbh.de>
2022-08-01 22:21:44 +0000

    changes for copyright

--- Services/LTI/src/ToolProvider/ResourceLink.php
+++ Services/LTI/src/ToolProvider/ResourceLink.php
@@ -1,5 +1,22 @@
 <?php
 
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
+
 namespace ILIAS\LTI\ToolProvider;
 
 use ILIAS\LTI\ToolProvider\DataConnector\DataConnector;
@@ -9,19 +26,6 @@ use ILIAS\LTI\ToolProvider\ApiHook\ApiHook;
 use DOMDocument;
 use DOMElement;
 
-/******************************************************************************
- *
- * This file is part of ILIAS, a powerful learning management system.
- *
- * ILIAS is licensed with the GPL-3.0, you should have received a copy
- * of said license along with the source code.
- *
- * If this is not the case or you just want to try ILIAS, you'll find
- * us at:
- *      https://www.ilias.de
- *      https://github.com/ILIAS-eLearning
- *
- *****************************************************************************/
 /**
  * Class to represent a platform resource link
  *
