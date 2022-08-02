8a1b0e03d596148be103ba074c906791c6522b7a
Richard Klees <richard.klees@concepts-and-training.de>
2022-08-02 08:03:41 +0200

    IASS: sort learning progress options
    * https://mantis.ilias.de/view.php?id=33146

--- Modules/IndividualAssessment/classes/class.ilIndividualAssessmentLP.php
+++ Modules/IndividualAssessment/classes/class.ilIndividualAssessmentLP.php
@@ -1,6 +1,20 @@
 <?php declare(strict_types=1);
 
-/* Copyright (c) 2016 Denis Klöpfer <denis.kloepfer@concepts-and-training.de> Extended GPL, see docs/LICENSE */
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
 
 class ilIndividualAssessmentLP extends ilObjectLP
 {
@@ -17,8 +31,8 @@ class ilIndividualAssessmentLP extends ilObjectLP
     public function getValidModes() : array
     {
         return [
-            ilLPObjSettings::LP_MODE_INDIVIDUAL_ASSESSMENT,
-            ilLPObjSettings::LP_MODE_DEACTIVATED
+            ilLPObjSettings::LP_MODE_DEACTIVATED,
+            ilLPObjSettings::LP_MODE_INDIVIDUAL_ASSESSMENT
         ];
     }
     
