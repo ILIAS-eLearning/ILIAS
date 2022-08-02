4dd735f083010482aae185c8b72458c39d1e2d89
mjansen <mjansen@databay.de>
2022-08-02 19:38:36 +0200

    App: Bump version number

--- include/inc.ilias_version.php
+++ include/inc.ilias_version.php
@@ -1,6 +1,20 @@
-<?php
+<?php declare(strict_types=1);
 
-/* Copyright (c) 1998-2020 ILIAS open source e-Learning e.V., Extended GPL, see docs/LICENSE */
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
 
 /**
 * sets ILIAS version (this file shouldn't be merged between cvs branches)
@@ -10,5 +24,5 @@
 *
 * @package ilias-core
 */
-define("ILIAS_VERSION", "8.0_alpha");
-define("ILIAS_VERSION_NUMERIC", "8.0");			// since version ILIAS 6 this must be always x.y: x and y are numbers
+const ILIAS_VERSION = "9.0_alpha";
+const ILIAS_VERSION_NUMERIC = "9.0";            // since version ILIAS 6 this must be always x.y: x and y are numbers
