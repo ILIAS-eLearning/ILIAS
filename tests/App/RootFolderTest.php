6fa27f7ff2cf8fc3a3aa2d4024744cd8726b38af
mjansen <mjansen@databay.de>
2022-08-02 15:51:49 +0200

    App: Add .php-cs-fixer.cache to root folder unit test exclusion list

--- tests/App/RootFolderTest.php
+++ tests/App/RootFolderTest.php
@@ -2,6 +2,22 @@
 
 declare(strict_types=1);
 
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -19,6 +35,7 @@ final class RootFolderTest extends TestCase
         'phpstan.local.neon',
         'phpstan-baseline.neon',
         '.php_cs.cache',
+        '.php-cs-fixer.cache',
         'calendar.php',
         'captainhook.json',
         'composer.json',
