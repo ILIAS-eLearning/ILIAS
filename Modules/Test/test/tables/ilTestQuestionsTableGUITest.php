87a3f1fd65d04486ab260d4ef3f31e052861d4ba
Timon Amstutz <timon.amstutz@bluewin.ch>
2022-08-06 21:01:09 +0200

    TA, Fix icon too large issue: #33180, Fix Test

--- Modules/Test/test/tables/ilTestQuestionsTableGUITest.php
+++ Modules/Test/test/tables/ilTestQuestionsTableGUITest.php
@@ -45,6 +45,8 @@ class ilTestQuestionsTableGUITest extends ilTestBaseTestCase
         $component_factory->method("getActivePluginsInSlot")->willReturn(new ArrayIterator());
         $this->setGlobalVariable("component.factory", $component_factory);
         $this->setGlobalVariable("ilDB", $this->createMock(ilDBInterface::class));
+        $this->setGlobalVariable("ui.renderer", $this->createMock(\ILIAS\UI\Renderer::class));
+        $this->setGlobalVariable("ui.factory", $this->createMock(\ILIAS\UI\Factory::class));
 
         $this->parentObj_mock = $this->getMockBuilder(ilObjTestGUI::class)->disableOriginalConstructor()->onlyMethods(array('getObject'))->getMock();
         $this->parentObj_mock->expects($this->any())->method('getObject')->willReturn($this->createMock(ilObjTest::class));
