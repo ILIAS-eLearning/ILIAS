4a2d8bfedbaaf14bab780dda0502d44c4974a56f
Maximilian Becker <mbecker@databay.de>
2022-08-08 14:04:04 +0200

    33718: Test "Multiple Choice ( Multiple Answer)" check

--- Modules/TestQuestionPool/classes/class.ilMultipleChoiceWizardInputGUI.php
+++ Modules/TestQuestionPool/classes/class.ilMultipleChoiceWizardInputGUI.php
@@ -69,7 +69,7 @@ class ilMultipleChoiceWizardInputGUI extends ilSingleChoiceWizardInputGUI
             // check answers
             if (is_array($foundvalues['answer'])) {
                 foreach ($foundvalues['answer'] as $aidx => $answervalue) {
-                    if (((strlen($answervalue)) == 0) && (strlen($foundvalues['imagename'][$aidx]) == 0)) {
+                    if (((strlen($answervalue)) == 0)  && (!isset($foundvalues['imagename']) || strlen($foundvalues['imagename'][$aidx]) == 0)) {
                         $this->setAlert($lng->txt("msg_input_is_required"));
                         return false;
                     }
