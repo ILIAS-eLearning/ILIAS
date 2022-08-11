4374ca3a4b6c0dd092f9cff4ff9064f6dcad1062
Maximilian Becker <mbecker@databay.de>
2022-08-11 10:53:03 +0200

    Merge pull request #4793 from kunzeus/Bugfix-0017861-Fix-maximum-length-255-characters-for-levenshtein-distance - Fix maximum levenshtein distance

--- src/Refinery/String/Group.php
+++ src/Refinery/String/Group.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 /**
  * This file is part of ILIAS, a powerful learning management system
@@ -19,9 +21,9 @@
 namespace ILIAS\Refinery\String;
 
 use ILIAS\Data\Factory;
+use ilLanguage;
 use ILIAS\Refinery\Constraint;
 use ILIAS\Refinery\Transformation;
-use ilLanguage;
 
 class Group
 {
@@ -42,7 +44,7 @@ class Group
      *                       with the new constraint
      * @return Constraint
      */
-    public function hasMinLength(int $minimum) : Constraint
+    public function hasMinLength(int $minimum): Constraint
     {
         return new HasMinLength($minimum, $this->dataFactory, $this->language);
     }
@@ -55,7 +57,7 @@ class Group
      *                       with the new constraint
      * @return Constraint
      */
-    public function hasMaxLength(int $maximum) : Constraint
+    public function hasMaxLength(int $maximum): Constraint
     {
         return new HasMaxLength($maximum, $this->dataFactory, $this->language);
     }
@@ -64,17 +66,18 @@ class Group
      * Creates a transformation that can be used to split a given
      * string by given delimiter.
      */
-    public function splitString(string $delimiter) : Transformation
+    public function splitString(string $delimiter): Transformation
     {
         return new SplitString($delimiter, $this->dataFactory);
     }
+
 
     /**
      * Creates a transformation that strips tags from a string.
      *
      * Uses php's strip_tags under the hood.
      */
-    public function stripTags() : Transformation
+    public function stripTags(): Transformation
     {
         return new StripTags();
     }
@@ -84,21 +87,21 @@ class Group
      *
      * Throws a LogicException in the transform method, if a not supported language is passed
      */
-    public function caseOfLabel(string $language_key) : Transformation
+    public function caseOfLabel(string $language_key): Transformation
     {
         return new CaseOfLabel($language_key);
     }
 
     /**
      * Creates a transformation to determine the estimated reading
-     * time of an human adult (roughly 275 WPM)
+     * time of a human adult (roughly 275 WPM)
      * If images should be taken into consideration, 12 seconds
      * are added to the first image, 11 for the second,
      * and minus an additional second for each subsequent image.
      * Any images after the tenth image are counted at three seconds.
      * The reading time returned in minutes as a integer value.
      */
-    public function estimatedReadingTime(bool $withImages = false) : Transformation
+    public function estimatedReadingTime(bool $withImages = false): Transformation
     {
         return new EstimatedReadingTime($withImages);
     }
@@ -110,5 +113,17 @@ class Group
     public function makeClickable() : Transformation
     {
         return new MakeClickable();
+    }
+
+    /**
+     * This method returns an instance of the Levenshtein class, to call the constructor of the
+     * LevenshteinTransformation class with either default values already set, or custom values for the cost
+     * calculation of the Levenshtein distance function.
+     *
+     * @return Levenshtein
+     */
+    public function levenshtein(): Levenshtein
+    {
+        return new Levenshtein();
     }
 }
