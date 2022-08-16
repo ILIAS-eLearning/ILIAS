4b3e8de09474cd220471a1c61926e4ce51749483
Tim Schmitz <schmitz@leifos.de>
2022-08-16 12:24:36 +0200

    fixes 33842

--- Services/Object/classes/class.ilObjectListGUI.php
+++ Services/Object/classes/class.ilObjectListGUI.php
@@ -684,7 +684,7 @@ class ilObjectListGUI
             return $this->access_cache[$permission]["-" . $cmd][$cache_prefix . $ref_id];
         }
 
-        if ($this->context == self::CONTEXT_REPOSITORY) {
+        if ($this->context == self::CONTEXT_REPOSITORY || $this->context == self::CONTEXT_SEARCH) {
             $access = $this->access->checkAccess($permission, $cmd, $ref_id, $type, (int) $obj_id);
             if ($this->access->getPreventCachingLastResult()) {
                 $this->prevent_access_caching = true;
@@ -789,7 +789,7 @@ class ilObjectListGUI
     */
     public function getCommandLink(string $cmd) : string
     {
-        if ($this->context == self::CONTEXT_REPOSITORY) {
+        if ($this->context == self::CONTEXT_REPOSITORY || $this->context == self::CONTEXT_SEARCH) {
             // BEGIN WebDAV Get mount webfolder link.
             if ($cmd == 'mount_webfolder' && ilDAVActivationChecker::_isActive()) {
                 global $DIC;
