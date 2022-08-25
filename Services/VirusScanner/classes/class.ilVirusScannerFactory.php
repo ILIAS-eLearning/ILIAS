b127ef147297a652740b9da5048e8819f7a98eaa
Maximilian Becker <mbecker@databay.de>
2022-08-25 09:13:38 +0200

    Removing ext change

--- Services/VirusScanner/classes/class.ilVirusScannerFactory.php
+++ Services/VirusScanner/classes/class.ilVirusScannerFactory.php
@@ -20,7 +20,7 @@ declare(strict_types=1);
 
 class ilVirusScannerFactory
 {
-    public static function _getInstance(): ?ilVirusScanner
+    public static function _getInstance(): ilVirusScanner
     {
         $vs = null;
 
