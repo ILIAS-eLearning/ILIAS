51bb4213b90dcd3d49f972b18d75845779b1684f
mjansen <mjansen@databay.de>
2022-08-25 14:45:43 +0200

    HTTP: Fix license header

--- src/HTTP/Wrapper/SuperGlobalDropInReplacement.php
+++ src/HTTP/Wrapper/SuperGlobalDropInReplacement.php
@@ -2,6 +2,22 @@
 
 declare(strict_types=1);
 
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
 namespace ILIAS\HTTP\Wrapper;
 
 use ILIAS\Refinery\Factory;
@@ -9,19 +25,6 @@ use ILIAS\Refinery\KeyValueAccess;
 use LogicException;
 use OutOfBoundsException;
 
-/******************************************************************************
- *
- * This file is part of ILIAS, a powerful learning management system.
- *
- * ILIAS is licensed with the GPL-3.0, you should have received a copy
- * of said license along with the source code.
- *
- * If this is not the case or you just want to try ILIAS, you'll find
- * us at:
- *      https://www.ilias.de
- *      https://github.com/ILIAS-eLearning
- *
- *****************************************************************************/
 /**
  * Class SuperGlobalDropInReplacement
  * This Class wraps SuperGlobals such as $_GET and $_POST to prevent modifying them in a future version.
