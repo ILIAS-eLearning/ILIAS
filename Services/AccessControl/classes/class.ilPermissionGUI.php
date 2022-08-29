e6d15b1364da2776424f82f3ef76aeecd74e0b5f
smeyer <smeyer.ilias@gmx.de>
2022-08-29 13:36:32 +0200

    0033976: Add New Local Role

--- Services/AccessControl/classes/class.ilPermissionGUI.php
+++ Services/AccessControl/classes/class.ilPermissionGUI.php
@@ -606,7 +606,7 @@ class ilPermissionGUI extends ilPermission2GUI
 
         $pro = new ilCheckboxInputGUI($this->lng->txt('role_protect_permissions'), 'pro');
         $pro->setInfo($this->lng->txt('role_protect_permissions_desc'));
-        $pro->setValue((string) 1);
+        $pro->setValue("1");
         $form->addItem($pro);
 
         $pd = new ilCheckboxInputGUI($this->lng->txt('rbac_add_recommended_content'), 'desktop');
@@ -644,7 +644,7 @@ class ilPermissionGUI extends ilPermission2GUI
                         )) . ": " . ilObjRole::_getTranslation($par["title"]),
                         (string) $par["obj_id"]
                     );
-                    $option->setInfo($par["desc"]);
+                    $option->setInfo($par["desc"] ?? '');
                     $rights->addOption($option);
                 }
                 $key++;
