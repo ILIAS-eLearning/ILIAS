ef10680096c694ace2c6677804fad833a606fc6a
Alexander Killing <killing@leifos.de>
2022-08-30 16:58:14 +0200

    fixed #34105: Failed test: Spalten und Zeilen im Bearbeitungsmodus hinzufügen

--- Services/COPage/Editor/js/src/components/table/ui/table-ui.js
+++ Services/COPage/Editor/js/src/components/table/ui/table-ui.js
@@ -189,16 +189,30 @@ export default class TableUI {
           ul.innerHTML = "";
 
             if (headType === "data-column-head") {
+              const th = b.closest("th");
+              const first = !(th.previousElementSibling.previousElementSibling);
+              const last = !(th.nextElementSibling);
               this.addDropdownAction(li_templ, ul, "cont_ed_new_col_before", af.colBefore(nr, cellPcid, tablePcid));
               this.addDropdownAction(li_templ, ul, "cont_ed_new_col_after", af.colAfter(nr, cellPcid, tablePcid));
-              this.addDropdownAction(li_templ, ul, "cont_ed_col_left", af.colLeft(nr, cellPcid, tablePcid));
-              this.addDropdownAction(li_templ, ul, "cont_ed_col_right", af.colRight(nr, cellPcid, tablePcid));
+              if (!first) {
+                this.addDropdownAction(li_templ, ul, "cont_ed_col_left", af.colLeft(nr, cellPcid, tablePcid));
+              }
+              if (!last) {
+                this.addDropdownAction(li_templ, ul, "cont_ed_col_right", af.colRight(nr, cellPcid, tablePcid));
+              }
               this.addDropdownAction(li_templ, ul, "cont_ed_delete_col", af.colDelete(nr, cellPcid, tablePcid));
             } else {
+              const tr = b.closest("tr");
+              const first = !(tr.previousElementSibling.previousElementSibling);
+              const last = !(tr.nextElementSibling);
               this.addDropdownAction(li_templ, ul, "cont_ed_new_row_before", af.rowBefore(nr, cellPcid, tablePcid));
               this.addDropdownAction(li_templ, ul, "cont_ed_new_row_after", af.rowAfter(nr, cellPcid, tablePcid));
-              this.addDropdownAction(li_templ, ul, "cont_ed_row_up", af.rowUp(nr, cellPcid, tablePcid));
-              this.addDropdownAction(li_templ, ul, "cont_ed_row_down", af.rowDown(nr, cellPcid, tablePcid));
+              if (!first) {
+                this.addDropdownAction(li_templ, ul, "cont_ed_row_up", af.rowUp(nr, cellPcid, tablePcid));
+              }
+              if (!last) {
+                this.addDropdownAction(li_templ, ul, "cont_ed_row_down", af.rowDown(nr, cellPcid, tablePcid));
+              }
               this.addDropdownAction(li_templ, ul, "cont_ed_delete_row", af.rowDelete(nr, cellPcid, tablePcid));
             }
         });
