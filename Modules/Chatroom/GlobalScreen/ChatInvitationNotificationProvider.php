371a8230f6719e2931e165b1872766d7dc385b60
mjansen <mjansen@databay.de>
2022-09-01 15:35:01 +0200

    Chatroom/Notification: Fix display of invitations
    (cherry picked from commit f39dc2e425ada4440358666c7972772f41cbd8f2)

--- Modules/Chatroom/GlobalScreen/ChatInvitationNotificationProvider.php
+++ Modules/Chatroom/GlobalScreen/ChatInvitationNotificationProvider.php
@@ -54,59 +54,58 @@ class ChatInvitationNotificationProvider extends AbstractNotificationProvider
         }
 
         $this->dic->language()->loadLanguageModule('chatroom');
-        $notificationItem = $this->dic->ui()->factory()->item()->notification(
-            $this->dic->language()->txt('chat_invitations'),
-            $this->dic->ui()->factory()->symbol()->icon()->standard(Standard::CHTA, 'chat_invitations')
-        )
-        ->withDescription($this->dic->language()->txt('chat_invitation_nc_no_inv'));
 
-        if (count($invitations) !== 0) {
-            $aggregatedItems = [];
-            foreach ($invitations as $invitation) {
-                $link = '';
-                if (count($invitation->getObject()->links) === 1) {
-                    $link = $this->dic->ui()->renderer()->render(
-                        $this->dic->ui()->factory()->link()->standard(
-                            $invitation->getObject()->shortDescription,
-                            $invitation->getObject()->links[0]->getUrl()
-                        )
-                    );
-                }
-                $aggregatedItems[] = $this->dic->ui()->factory()->item()->notification(
-                    $invitation->getObject()->title,
-                    $this->dic->ui()->factory()->symbol()->icon()->standard(Standard::CHTA, 'chat_invitations')
-                )
-                ->withDescription($link)
-                ->withProperties([
-                    $this->dic->language()->txt('time') => ilDatePresentation::formatDate(
-                        new ilDateTime($invitation->getTimeAdded(), IL_CAL_UNIX)
-                    )
-                ]);
-            }
-
-            $notificationItem = $this->dic->ui()->factory()->item()->notification(
-                $this->dic->ui()->factory()->link()->standard($this->dic->language()->txt('chat_invitations'), '#'),
-                $this->dic->ui()->factory()->symbol()->icon()->standard(Standard::CHTA, 'chat_invitations')
-            )
-                ->withAggregateNotifications($aggregatedItems)
-                ->withDescription(sprintf($this->dic->language()->txt('chat_invitation_nc_inv_x'), count($aggregatedItems)))
-                ->withProperties([
-                    $this->dic->language()->txt('time') => ilDatePresentation::formatDate(
-                        new ilDateTime($latest_time, IL_CAL_UNIX)
-                    )
-                ]);
-        } else {
+        if ($invitations === []) {
             return [];
         }
 
+        $aggregatedItems = [];
+        foreach ($invitations as $invitation) {
+            $link = '';
+            if (count($invitation->getObject()->links) === 1) {
+                $link = $this->dic->ui()->renderer()->render(
+                    $this->dic->ui()->factory()->link()->standard(
+                        $invitation->getObject()->shortDescription,
+                        $invitation->getObject()->links[0]->getUrl()
+                    )
+                );
+            }
+            $aggregatedItems[] = $this->dic->ui()->factory()->item()->notification(
+                $invitation->getObject()->title,
+                $this->dic->ui()->factory()->symbol()->icon()->standard(Standard::CHTA, 'chat_invitations')
+            )->withDescription($link)
+             ->withProperties([
+               $this->dic->language()->txt('time') => ilDatePresentation::formatDate(
+                   new ilDateTime($invitation->getTimeAdded(), IL_CAL_UNIX)
+               )
+           ]);
+        }
+
+        $notificationItem = $this->dic->ui()->factory()->item()->notification(
+            $this->dic->ui()->factory()->link()->standard($this->dic->language()->txt('chat_invitations'), '#'),
+            $this->dic->ui()->factory()->symbol()->icon()->standard(Standard::CHTA, 'chat_invitations')
+        )->withAggregateNotifications($aggregatedItems)
+         ->withDescription(
+             sprintf(
+                 $this->dic->language()->txt('chat_invitation_nc_inv_x'),
+                 count($aggregatedItems)
+             )
+         )->withProperties([
+             $this->dic->language()->txt('time') => ilDatePresentation::formatDate(
+                 new ilDateTime($latest_time, IL_CAL_UNIX)
+             )
+         ]);
+
         return [
-            $this->globalScreen()->notifications()->factory()->standardGroup($this->if->identifier('chat_invitation_bucket_group'))
-                 ->withTitle('Chat')
-                 ->addNotification(
-                     $this->globalScreen()->notifications()->factory()->standard($this->if->identifier('chat_invitation_bucket'))
-                          ->withNotificationItem($notificationItem)
-                          ->withNewAmount(count($invitations))
-                 )
+            $this->globalScreen()->notifications()->factory()->standardGroup(
+                $this->if->identifier('chat_invitation_bucket_group')
+            )->withTitle('Chat')
+             ->addNotification(
+                 $this->globalScreen()->notifications()->factory()->standard(
+                     $this->if->identifier('chat_invitation_bucket')
+                 )->withNotificationItem($notificationItem)
+                  ->withNewAmount(count($invitations))
+             )
         ];
     }
 }
