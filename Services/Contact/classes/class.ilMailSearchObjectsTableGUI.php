4c3d507417355b9e9ecf72505a40e0232f84f2f8
mjansen <mjansen@databay.de>
2022-09-16 14:05:59 +0200

    Contact: Fix sorting of courses and groups (Mantis 34480)
    (cherry picked from commit e74df99545096e4f071ba5c0307bae26ae1ee7eb)

--- Services/Contact/classes/class.ilMailSearchObjectsTableGUI.php
+++ Services/Contact/classes/class.ilMailSearchObjectsTableGUI.php
@@ -120,9 +120,9 @@ class ilMailSearchObjectsTableGUI extends ilTable2GUI
         $this->setShowRowsSelector(true);
 
         $this->addColumn('', '', '1px', true);
-        $this->addColumn($mode["lng_mail"], 'CRS_NAME', '30%');
-        $this->addColumn($this->lng->txt('path'), 'CRS_PATH', '30%');
-        $this->addColumn($this->lng->txt('crs_count_members'), 'OBJECT_NO_MEMBERS', '20%');
+        $this->addColumn($mode["lng_mail"], 'OBJECT_NAME', '30%');
+        $this->addColumn($this->lng->txt('path'), 'OBJECT_PATH', '30%');
+        $this->addColumn($this->lng->txt('obj_count_members'), 'OBJECT_NO_MEMBERS', '20%');
         $this->addColumn($this->lng->txt('actions'), '', '19%');
 
         if ($context === "mail") {
