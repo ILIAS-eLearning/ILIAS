1d13b1c68637158d5137d36f111e6978238330c3
Alexander Killing <killing@leifos.de>
2022-09-18 18:44:07 +0200

    fixed bug #32336: Str+Z löscht Textfelder

--- Services/COPage/Editor/js/src/components/paragraph/ui/tiny-wrapper.js
+++ Services/COPage/Editor/js/src/components/paragraph/ui/tiny-wrapper.js
@@ -611,6 +611,14 @@ export default class TinyWrapper {
         cb();
       });
       wrapper.autoScroll();
+      this.clearUndo();
+    }
+  }
+
+  // see e.g. #32336
+  clearUndo() {
+    if (this.tiny) {
+      this.tiny.undoManager.clear();
     }
   }
 
@@ -903,6 +911,7 @@ export default class TinyWrapper {
     }
     this.autoResize();
     this.setParagraphClass(characteristic);
+    this.clearUndo();
   }
 
   getText() {
