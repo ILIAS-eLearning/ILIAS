72c8430414f1e5bd788b2ea3191c321122e832f7
smeyer <smeyer.ilias@gmx.de>
2022-09-19 13:06:20 +0200

    0033892: Klick auf "Sprechstundenverwaltung" löst Fehler aus

--- Services/Calendar/classes/class.ilObjCalendarSettingsGUI.php
+++ Services/Calendar/classes/class.ilObjCalendarSettingsGUI.php
@@ -1,27 +1,22 @@
 <?php
 
 declare(strict_types=1);
-/*
-    +-----------------------------------------------------------------------------+
-    | ILIAS open source                                                           |
-    +-----------------------------------------------------------------------------+
-    | Copyright (c) 1998-2006 ILIAS open source, University of Cologne            |
-    |                                                                             |
-    | This program is free software; you can redistribute it and/or               |
-    | modify it under the terms of the GNU General Public License                 |
-    | as published by the Free Software Foundation; either version 2              |
-    | of the License, or (at your option) any later version.                      |
-    |                                                                             |
-    | This program is distributed in the hope that it will be useful,             |
-    | but WITHOUT ANY WARRANTY; without even the implied warranty of              |
-    | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
-    | GNU General Public License for more details.                                |
-    |                                                                             |
-    | You should have received a copy of the GNU General Public License           |
-    | along with this program; if not, write to the Free Software                 |
-    | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
-    +-----------------------------------------------------------------------------+
-*/
+
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
 
 /**
  * @author       Stefan Meyer <meyer@leifos.com>
@@ -123,26 +118,26 @@ class ilObjCalendarSettingsGUI extends ilObjectGUI
             $this->calendar_settings->setEnabled((bool) $form->getInput('enable'));
             $this->calendar_settings->setDefaultWeekStart((int) $form->getInput('default_week_start'));
             $this->calendar_settings->setDefaultTimeZone($form->getInput('default_timezone'));
-            $this->calendar_settings->setDefaultDateFormat($form->getInput('default_date_format'));
-            $this->calendar_settings->setDefaultTimeFormat($form->getInput('default_time_format'));
-            $this->calendar_settings->setEnableGroupMilestones($form->getInput('enable_grp_milestones'));
-            $this->calendar_settings->enableCourseCalendar($form->getInput('enabled_crs'));
-            $this->calendar_settings->setCourseCalendarVisible($form->getInput('visible_crs'));
-            $this->calendar_settings->enableGroupCalendar($form->getInput('enabled_grp'));
-            $this->calendar_settings->setGroupCalendarVisible($form->getInput('visible_grp'));
-            $this->calendar_settings->setDefaultDayStart($form->getInput('dst'));
-            $this->calendar_settings->setDefaultDayEnd($form->getInput('den'));
-            $this->calendar_settings->enableSynchronisationCache($form->getInput('sync_cache'));
-            $this->calendar_settings->setSynchronisationCacheMinutes($form->getInput('sync_cache_time'));
-            $this->calendar_settings->setCacheMinutes($form->getInput('cache_time'));
-            $this->calendar_settings->useCache($form->getInput('cache'));
-            $this->calendar_settings->enableNotification($form->getInput('cn'));
-            $this->calendar_settings->enableUserNotification($form->getInput('cnu'));
-            $this->calendar_settings->enableConsultationHours($form->getInput('ch'));
-            $this->calendar_settings->enableCGRegistration($form->getInput('cgr'));
-            $this->calendar_settings->enableWebCalSync($form->getInput('webcal'));
-            $this->calendar_settings->setWebCalSyncHours($form->getInput('webcal_hours'));
-            $this->calendar_settings->setShowWeeks($form->getInput('show_weeks'));
+            $this->calendar_settings->setDefaultDateFormat((int) $form->getInput('default_date_format'));
+            $this->calendar_settings->setDefaultTimeFormat((int) $form->getInput('default_time_format'));
+            $this->calendar_settings->setEnableGroupMilestones((bool) $form->getInput('enable_grp_milestones'));
+            $this->calendar_settings->enableCourseCalendar((bool) $form->getInput('enabled_crs'));
+            $this->calendar_settings->setCourseCalendarVisible((bool) $form->getInput('visible_crs'));
+            $this->calendar_settings->enableGroupCalendar((bool) $form->getInput('enabled_grp'));
+            $this->calendar_settings->setGroupCalendarVisible((bool) $form->getInput('visible_grp'));
+            $this->calendar_settings->setDefaultDayStart((int) $form->getInput('dst'));
+            $this->calendar_settings->setDefaultDayEnd((int) $form->getInput('den'));
+            $this->calendar_settings->enableSynchronisationCache((bool) $form->getInput('sync_cache'));
+            $this->calendar_settings->setSynchronisationCacheMinutes((int) $form->getInput('sync_cache_time'));
+            $this->calendar_settings->setCacheMinutes((int) $form->getInput('cache_time'));
+            $this->calendar_settings->useCache((bool) $form->getInput('cache'));
+            $this->calendar_settings->enableNotification((bool) $form->getInput('cn'));
+            $this->calendar_settings->enableUserNotification((bool) $form->getInput('cnu'));
+            $this->calendar_settings->enableConsultationHours((bool) $form->getInput('ch'));
+            $this->calendar_settings->enableCGRegistration((bool) $form->getInput('cgr'));
+            $this->calendar_settings->enableWebCalSync((bool) $form->getInput('webcal'));
+            $this->calendar_settings->setWebCalSyncHours((int) $form->getInput('webcal_hours'));
+            $this->calendar_settings->setShowWeeks((bool) $form->getInput('show_weeks'));
             $this->calendar_settings->enableBatchFileDownloads((bool) $form->getInput('batch_files'));
             $this->calendar_settings->setDefaultCal((int) $form->getInput('default_calendar_view'));
             $this->calendar_settings->setDefaultPeriod((int) $form->getInput('default_period'));
