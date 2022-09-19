b8dbac67f47652873793c50e88b1d3eb00cef7a5
mjansen <mjansen@databay.de>
2022-09-19 18:53:08 +0200

    TermsOfService: Fix all delete actions (Mantis 34530)

--- Services/TermsOfService/classes/Document/class.ilTermsOfServiceDocumentTableGUI.php
+++ Services/TermsOfService/classes/Document/class.ilTermsOfServiceDocumentTableGUI.php
@@ -195,7 +195,8 @@ class ilTermsOfServiceDocumentTableGUI extends ilTermsOfServiceTableGUI
                 $this->lng->txt('tos_doc_delete'),
                 $this->lng->txt('tos_sure_delete_documents_s'),
                 $this->ctrl->getFormAction($this->getParentObject(), 'deleteDocument')
-            );
+            )
+            ->withActionButtonLabel('deleteDocument');
 
         $deleteBtn = $this->uiFactory
             ->button()
@@ -246,7 +247,8 @@ class ilTermsOfServiceDocumentTableGUI extends ilTermsOfServiceTableGUI
                     $this->lng->txt('tos_doc_detach_crit_confirm_title'),
                     $this->lng->txt('tos_doc_sure_detach_crit'),
                     $this->ctrl->getFormAction($this->getParentObject(), 'detachCriterionAssignment')
-                );
+                )
+                ->withActionButtonLabel('detachCriterionAssignment');
 
             $deleteBtn = $this->uiFactory
                 ->button()
