aad03a092bed8adf305a0d637a09bcd97041d5a8
Fabian Schmid <fabian@sr.solutions>
2022-09-23 13:41:35 +0200

    [FIX] array access for corrupt fileobjects

--- Modules/File/classes/class.ilObjFileListGUI.php
+++ Modules/File/classes/class.ilObjFileListGUI.php
@@ -193,7 +193,7 @@ class ilObjFileListGUI extends ilObjectListGUI
         if (ilFileVersionsGUI::CMD_UNZIP_CURRENT_REVISION === $cmd) {
             $file_data = ilObjFileAccess::getListGUIData($this->obj_id);
 
-            return ilObjFileAccess::isZIP($file_data['mime']);
+            return ilObjFileAccess::isZIP($file_data['mime'] ?? null);
         }
 
         return parent::checkCommandAccess(
@@ -215,7 +215,7 @@ class ilObjFileListGUI extends ilObjectListGUI
 
         if (ilFileVersionsGUI::CMD_UNZIP_CURRENT_REVISION === $cmd) {
             $file_data = ilObjFileAccess::getListGUIData($this->obj_id);
-            if (ilObjFileAccess::isZIP($file_data['mime'])) {
+            if (ilObjFileAccess::isZIP($file_data['mime'] ?? null)) {
                 $this->ctrl->setParameterByClass(ilRepositoryGUI::class, 'ref_id', $this->ref_id);
                 $cmd_link = $this->ctrl->getLinkTargetByClass(
                     ilRepositoryGUI::class,
