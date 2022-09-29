d8e1048ee62fd31d4a1f2dad84ce96a6d7257b29
Fabian Schmid <fabian@sr.solutions>
2022-09-29 15:48:38 +0200

    [FIX] 0034637: Failed test: Magazin-Link hinzufügen

--- src/GlobalScreen/Scope/MainMenu/Factory/Item/RepositoryLink.php
+++ src/GlobalScreen/Scope/MainMenu/Factory/Item/RepositoryLink.php
@@ -1,6 +1,5 @@
 <?php
 
-declare(strict_types=1);
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -16,6 +15,8 @@ declare(strict_types=1);
  * https://github.com/ILIAS-eLearning
  *
  *********************************************************************/
+
+declare(strict_types=1);
 
 namespace ILIAS\GlobalScreen\Scope\MainMenu\Factory\Item;
 
@@ -44,7 +45,6 @@ class RepositoryLink extends AbstractChildItem implements
     hasAction,
     hasSymbol,
     isInterchangeableItem,
-    isTopItem,
     isChild
 {
     use hasSymbolTrait;
