e4ec8345eda2010c94f25183d4af5807063a0bfd
Alexander Killing <killing@leifos.de>
2022-10-05 10:31:39 +0200

    fixed 34707: Failed test: Einer Frage eine Überschrift hinzufügen

--- Modules/Survey/Page/class.ilSurveyPageEditGUI.php
+++ Modules/Survey/Page/class.ilSurveyPageEditGUI.php
@@ -824,7 +824,7 @@ class ilSurveyPageEditGUI
      */
     protected function addHeading(int $a_id): void
     {
-        $this->callEditor("addHeadingObject", "q_id", $a_id);
+        $this->callEditor("addHeading", "q_id", $a_id);
     }
 
     /**
