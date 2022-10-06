23e1727a10f8b6f404834d713a59b22274e09afc
Alexander Killing <killing@leifos.de>
2022-10-06 10:59:43 +0200

    fixed 34822: Failed test: Portfoliovorlage löschen

--- Services/Repository/Trash/class.ilRepositoryTrashGUI.php
+++ Services/Repository/Trash/class.ilRepositoryTrashGUI.php
@@ -420,7 +420,10 @@ class ilRepositoryTrashGUI
                     $this->tpl->setOnScreenMessage('success', $lng->txt("msg_removed"), true);
                 }
             } catch (Exception $e) {
-                $this->tpl->setOnScreenMessage('failure', $e->getMessage(), true);
+                //$this->tpl->setOnScreenMessage('failure', $e->getMessage(), true);
+                // alex: I outcommented this, since it makes tracking down errors impossible
+                // we need a call stack at least in the logs
+                throw $e;
             }
         }
     }
@@ -440,7 +443,10 @@ class ilRepositoryTrashGUI
             ilRepUtil::removeObjectsFromSystem($a_ref_ids, $a_from_recovery_folder);
             $this->tpl->setOnScreenMessage('success', $lng->txt("msg_removed"), true);
         } catch (Exception $e) {
-            $this->tpl->setOnScreenMessage('failure', $e->getMessage(), true);
+            // alex: I outcommented this, since it makes tracking down errors impossible
+            // we need a call stack at least in the logs
+            //$this->tpl->setOnScreenMessage('failure', $e->getMessage(), true);
+            throw $e;
             return false;
         }
 
