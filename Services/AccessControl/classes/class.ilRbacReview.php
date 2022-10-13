ad66f29d9a4ca7fdc439d708a3149df39e4001b0
Thomas Famula <famula@leifos.de>
2022-10-13 18:53:46 +0200

    Fix copyright notices

--- Services/AccessControl/classes/class.ilRbacReview.php
+++ Services/AccessControl/classes/class.ilRbacReview.php
@@ -1,6 +1,7 @@
 <?php
 
 declare(strict_types=1);
+
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -15,7 +16,8 @@ declare(strict_types=1);
  * https://www.ilias.de
  * https://github.com/ILIAS-eLearning
  *
- *********************************************************************/
+ ********************************************************************
+ */
 
 /**
  * class ilRbacReview
