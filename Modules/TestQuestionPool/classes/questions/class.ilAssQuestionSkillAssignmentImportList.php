9e3561d0561261d2b67086ab6f8cdaa001030f29
mjansen <mjansen@databay.de>
2022-10-13 16:14:24 +0200

    TestQuestionPool: Fix PHP 8.1 issues with `ilAssQuestionSkillAssignmentImportList`

--- Modules/TestQuestionPool/classes/questions/class.ilAssQuestionSkillAssignmentImportList.php
+++ Modules/TestQuestionPool/classes/questions/class.ilAssQuestionSkillAssignmentImportList.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -21,25 +23,18 @@
  * @version        $Id$
  *
  * @package     Modules/TestQuestionPool
+ * @implements Iterator<ilAssQuestionSkillAssignmentImport>
  */
 class ilAssQuestionSkillAssignmentImportList implements Iterator
 {
-    /**
-     * @var array[ilAssQuestionSkillAssignmentImport]
-     */
-    protected $assignments;
+    /** @var list<ilAssQuestionSkillAssignmentImport>  */
+    protected array $assignments;
 
-    /**
-     * ilAssQuestionSkillAssignmentImportList constructor.
-     */
     public function __construct()
     {
-        $this->assignments = array();
+        $this->assignments = [];
     }
 
-    /**
-     * @param ilAssQuestionSkillAssignmentImport $assignment
-     */
     public function addAssignment(ilAssQuestionSkillAssignmentImport $assignment): void
     {
         $this->assignments[] = $assignment;
@@ -50,55 +45,29 @@ class ilAssQuestionSkillAssignmentImportList implements Iterator
         return count($this->assignments) > 0;
     }
 
-    /**
-     * @return ilAssQuestionSkillAssignmentImport
-     */
     public function current(): ilAssQuestionSkillAssignmentImport
     {
         return current($this->assignments);
     }
 
-    /**
-     * @return ilAssQuestionSkillAssignmentImport
-     */
-    public function next(): ilAssQuestionSkillAssignmentImport
+    public function next(): void
     {
-        return next($this->assignments);
+        next($this->assignments);
     }
 
-    /**
-     * @return integer|bool
-     */
-    public function key()
+    public function key(): int
     {
-        $res = key($this->assignments);
-        return $res;
+        return key($this->assignments);
     }
 
-    /**
-     * @return bool
-     */
     public function valid(): bool
     {
         $res = key($this->assignments);
         return $res !== null;
     }
 
-    /**
-     * @return ilAssQuestionSkillAssignmentImport|bool
-     */
-    public function rewind()
+    public function rewind(): void
     {
-        return reset($this->assignments);
-    }
-
-    public function sleep(): void
-    {
-        // TODO: Implement __sleep() method.
-    }
-
-    public function wakeup(): void
-    {
-        // TODO: Implement __wakeup() method.
+        reset($this->assignments);
     }
 }
