38e9b229b10d02b85706b04f84fdc1f6b94a780b
Thibeau Fuhrer <thibeau@sr.solutions>
2022-10-17 12:26:53 +0200

    [FIX] add ILIAS copyright license-headers.

--- Services/UICore/classes/class.ilCtrlContext.php
+++ Services/UICore/classes/class.ilCtrlContext.php
@@ -1,8 +1,21 @@
 <?php
 
-declare(strict_types=1);
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ */
 
-/* Copyright (c) 2021 Thibeau Fuhrer <thf@studer-raimann.ch> Extended GPL, see docs/LICENSE */
+declare(strict_types=1);
 
 use ILIAS\Refinery\Factory as Refinery;
 use ILIAS\HTTP\Wrapper\RequestWrapper;
@@ -12,6 +25,7 @@ use ILIAS\HTTP\Wrapper\RequestWrapper;
  * current context information.
  *
  * @author Thibeau Fuhrer <thf@studer-raimann.ch>
+ * @noinspection AutoloadingIssuesInspection
  */
 class ilCtrlContext implements ilCtrlContextInterface
 {
