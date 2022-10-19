a1ca675e4fb4287de8beac37e58c1af1ee9f8a27
Nils Haagen <nhaagen@cat06.de>
2022-10-19 13:05:32 +0200

    TA: Repo for ScoreSettings, optional print_best_solution effective

--- Modules/Test/classes/class.ilObjTest.php
+++ Modules/Test/classes/class.ilObjTest.php
@@ -10093,14 +10093,9 @@ class ilObjTest extends ilObject implements ilMarkSchemaAware, ilEctsGradesEnabl
         $this->oldOnlineStatus = $oldOnlineStatus;
     }
 
-    public function setPrintBestSolutionWithResult($status)
-    {
-        $this->print_best_solution_with_result = (bool) $status;
-    }
-
     public function isBestSolutionPrintedWithResult(): bool
     {
-        return $this->print_best_solution_with_result;
+        return $this->getScoreSettings()->getResultDetailsSettings()->getPrintBestSolutionWithResult();
     }
 
     /**
