9a1f560dba0bbd9d7faa5fed37a35bf067ac97c1
smeyer <smeyer.ilias@gmx.de>
2022-10-24 18:41:21 +0200

    search: fixed search area selection

--- Services/Search/classes/Lucene/class.ilLuceneSearchGUI.php
+++ Services/Search/classes/Lucene/class.ilLuceneSearchGUI.php
@@ -1,28 +1,22 @@
 <?php
 
 declare(strict_types=1);
-/*
-    +-----------------------------------------------------------------------------+
-    | ILIAS open source                                                           |
-    +-----------------------------------------------------------------------------+
-    | Copyright (c) 1998-2006 ILIAS open source, University of Cologne            |
-    |                                                                             |
-    | This program is free software; you can redistribute it and/or               |
-    | modify it under the terms of the GNU General Public License                 |
-    | as published by the Free Software Foundation; either version 2              |
-    | of the License, or (at your option) any later version.                      |
-    |                                                                             |
-    | This program is distributed in the hope that it will be useful,             |
-    | but WITHOUT ANY WARRANTY; without even the implied warranty of              |
-    | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
-    | GNU General Public License for more details.                                |
-    |                                                                             |
-    | You should have received a copy of the GNU General Public License           |
-    | along with this program; if not, write to the Free Software                 |
-    | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
-    +-----------------------------------------------------------------------------+
-*/
 
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
 
 /**
 * @classDescription GUI for simple Lucene search
@@ -74,24 +68,26 @@ class ilLuceneSearchGUI extends ilSearchBaseGUI
         $next_class = $this->ctrl->getNextClass($this);
         $cmd = $this->ctrl->getCmd();
 
-        $this->prepareOutput();
         switch ($next_class) {
             case "ilpropertyformgui":
                 /*$this->initStandardSearchForm(ilSearchBaseGUI::SEARCH_FORM_LUCENE);
                 $ilCtrl->setReturn($this, 'storeRoot');
                 $ilCtrl->forwardCommand($this->form);*/
                 $form = $this->getSearchAreaForm();
+                $this->prepareOutput();
                 $this->ctrl->setReturn($this, 'storeRoot');
                 $this->ctrl->forwardCommand($form);
                 break;
 
             case 'ilobjectcopygui':
+                $this->prepareOutput();
                 $this->ctrl->setReturn($this, '');
                 $cp = new ilObjectCopyGUI($this);
                 $this->ctrl->forwardCommand($cp);
                 break;
 
             default:
+                $this->prepareOutput();
                 $this->initStandardSearchForm(ilSearchBaseGUI::SEARCH_FORM_LUCENE);
                 if (!$cmd) {
                     $cmd = "showSavedResults";
@@ -538,7 +534,7 @@ class ilLuceneSearchGUI extends ilSearchBaseGUI
         }
 
         // search area form
-        #$this->tpl->setVariable('SEARCH_AREA_FORM', $this->getSearchAreaForm()->getHTML());
+        $this->tpl->setVariable('SEARCH_AREA_FORM', $this->getSearchAreaForm()->getHTML());
         $this->tpl->setVariable("TXT_CHANGE", $this->lng->txt("change"));
 
         if (ilSearchSettings::getInstance()->isDateFilterEnabled()) {
