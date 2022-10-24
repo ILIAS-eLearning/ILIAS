2d8841f222daff976ba728519e5daf9ca0db68dc
Richard Klees <richard.klees@concepts-and-training.de>
2022-10-24 17:57:34 +0200

    LSO: redirect to content when settings are aborted (#34457)

--- Modules/LearningSequence/classes/Settings/class.ilObjLearningSequenceSettingsGUI.php
+++ Modules/LearningSequence/classes/Settings/class.ilObjLearningSequenceSettingsGUI.php
@@ -97,7 +97,7 @@ class ilObjLearningSequenceSettingsGUI
 
     protected function cancel(): void
     {
-        $this->ctrl->returnToParent($this);
+        $this->ctrl->redirectByClass(ilObjLearningSequenceGUI::class);
     }
 
 
