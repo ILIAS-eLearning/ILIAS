27a05d1dba821e6fd9897eb538fa873483f8fc17
smeyer <smeyer.ilias@gmx.de>
2022-10-25 15:34:37 +0200

    fixed precondition cloning (num_obligatory)

--- Services/Conditions/classes/class.ilConditionHandler.php
+++ Services/Conditions/classes/class.ilConditionHandler.php
@@ -129,6 +129,12 @@ class ilConditionHandler
         $this->validation = true;
     }
 
+    public static function resetCache(): void
+    {
+        self::$cond_for_target_cache = [];
+        self::$cond_target_rows = [];
+    }
+
     /**
      * @param string target type ILIAS obj type
      */
@@ -954,6 +960,8 @@ class ilConditionHandler
         $ilDB = $DIC['ilDB'];
 
         // Get all conditions
+
+        self::resetCache();
         $all = self::_getPersistedConditionsOfTarget($a_target_ref_id, $a_target_obj_id, $a_target_obj_type);
         $opt = self::getPersistedOptionalConditionsOfTarget($a_target_ref_id, $a_target_obj_id, $a_target_obj_type);
 
