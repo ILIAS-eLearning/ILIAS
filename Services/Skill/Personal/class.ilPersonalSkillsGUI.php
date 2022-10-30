c7c6e5539ac2339425f8adc3d62cecae5c74589f
Alexander Killing <killing@leifos.de>
2022-10-30 22:13:15 +0100

    fixed 35359: Failed test: Ergebnisse der Gap Analysis einsehen: Feedback aus 360°­Umfrage einsehen

--- Services/Skill/Personal/class.ilPersonalSkillsGUI.php
+++ Services/Skill/Personal/class.ilPersonalSkillsGUI.php
@@ -1422,7 +1422,7 @@ class ilPersonalSkillsGUI
                     $tooltips[$eval_dim] = $tooltips[$eval_dim] ?? null;
                 }
                 if ($incl_self_eval) {
-                    if ($self_vals[$l["base_skill_id"]][$l["tref_id"]] == $lv["id"]) {
+                    if (($self_vals[$l["base_skill_id"]][$l["tref_id"]] ?? 0) == $lv["id"]) {
                         $points[$self_eval_dim] = $cnt;
                         $tooltips[$self_eval_dim] = null;
                     } else {
