ee88361f642a918531d8d495ee7feaebfd78d1ec
Fabian Schmid <fabian@sr.solutions>
2022-10-31 15:00:47 +0100

    nosniff header for .sec downloads

--- src/ResourceStorage/Consumer/InlineConsumer.php
+++ src/ResourceStorage/Consumer/InlineConsumer.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -15,7 +13,10 @@ declare(strict_types=1);
  * us at:
  * https://www.ilias.de
  * https://github.com/ILIAS-eLearning
+ *
  *********************************************************************/
+
+declare(strict_types=1);
 
 namespace ILIAS\ResourceStorage\Consumer;
 
@@ -25,36 +26,10 @@ use ILIAS\HTTP\Response\ResponseHeader;
  * Class InlineConsumer
  * @package ILIAS\ResourceStorage\Consumer
  */
-class InlineConsumer extends BaseConsumer implements DeliveryConsumer
+class InlineConsumer extends BaseHTTPResponseBasedConsumer implements DeliveryConsumer
 {
-    public function run(): void
+    protected function getDisposition(): string
     {
-        global $DIC;
-
-        $revision = $this->getRevision();
-
-        $file_name = $this->file_name_policy->prepareFileNameForConsumer($this->file_name ?? $revision->getInformation()->getTitle());
-        $mime_type = $revision->getInformation()->getMimeType();
-        /** @noRector */
-        $response = $DIC->http()->response();
-        if ($this->file_name_policy->isValidExtension($revision->getInformation()->getSuffix())) {
-            $response = $response->withHeader(ResponseHeader::CONTENT_TYPE, $mime_type);
-        } else {
-            $response = $response->withHeader(ResponseHeader::CONTENT_TYPE, 'application/octet-stream');
-        }
-        $response = $response->withHeader(ResponseHeader::CONNECTION, 'close');
-        $response = $response->withHeader(ResponseHeader::ACCEPT_RANGES, 'bytes');
-        $response = $response->withHeader(
-            ResponseHeader::CONTENT_DISPOSITION,
-            'inline'
-            . '; filename="'
-            . $file_name
-            . '"'
-        );
-        $response = $response->withBody($this->storage_handler->getStream($revision));
-
-        $DIC->http()->saveResponse($response);
-        $DIC->http()->sendResponse();
-        $DIC->http()->close();
+        return 'inline';
     }
 }
