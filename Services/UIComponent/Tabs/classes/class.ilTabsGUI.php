6196db1b0d52377df7d42c19c0e5e087a84182d6
Timon Amstutz <timon.amstutz@bluewin.ch>
2022-11-04 15:27:13 +0100

    A11y, improve label of subtabs, see #33672

--- Services/UIComponent/Tabs/classes/class.ilTabsGUI.php
+++ Services/UIComponent/Tabs/classes/class.ilTabsGUI.php
@@ -480,7 +480,7 @@ class ilTabsGUI
             }
 
             if ($a_get_sub_tabs) {
-                $tpl->setVariable("TXT_SUBTABS", $lng->txt("subtabs"));
+                $tpl->setVariable("TXT_SUBTABS", $this->getTabTextOfId($this->getActiveTab()).": ".$lng->txt("subtabs"));
             } else {
                 $tpl->setVariable("TXT_TABS", $lng->txt("tabs"));
 
@@ -516,6 +516,20 @@ class ilTabsGUI
         } else {
             return "";
         }
+    }
+
+    protected function getTabTextOfId(string $id): string
+    {
+        foreach ($this->target as $i => $target) {
+            if ($this->target[$i]['id'] == $id) {
+                if ($target["dir_text"]) {
+                    return $target["text"];
+                } else {
+                    return $this->lng->txt($target["text"]);
+                }
+            }
+        }
+        return "";
     }
 
     public function getActiveTab(): string
