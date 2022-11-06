11c995a441c9f4d248cbe3f0b9558799b17940bd
Uwe Kohnle <kohnle@internetlehrer-gmbh.de>
2022-11-06 23:25:33 +0000

    fix mantis #34911

--- Modules/LTIConsumer/classes/class.ilLTIConsumerProviderUsageTableGUI.php
+++ Modules/LTIConsumer/classes/class.ilLTIConsumerProviderUsageTableGUI.php
@@ -107,9 +107,9 @@ class ilLTIConsumerProviderUsageTableGUI extends ilTable2GUI
         $treeNodes = [];
         foreach ($tree as $node) {
             $node['title'] = (int) $node['parent'] === 0 ? $DIC->language()->txt('repository') : $node['title'];
-            $treeNodes[] = $trashed === true ? $node['title'] : '<a href="' . ilLink::_getLink($node['ref_id']) . '">' . $node['title'] . '</a>';
+            $treeNodes[] = $trashed === true ? $node['title'] : '<a href="' . ilLink::_getLink((int) $node['ref_id']) . '">' . $node['title'] . '</a>';
         }
-        $endnode = '<a href="' . ilLink::_getLink($refId) . '">' . $title . '</a>';
+        $endnode = '<a href="' . ilLink::_getLink((int) $refId) . '">' . $title . '</a>';
         if ($trashed === true) {
             $treeNodes[] = $title;
         }
