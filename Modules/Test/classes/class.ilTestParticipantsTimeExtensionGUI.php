2a75cfa5f31c86b97035e529f0c3795893e53f31
Nils Haagen <nhaagen@cat06.de>
2022-11-10 15:37:10 +0100

    TA: 35347, properly set DIC-vars in construct

--- Modules/Test/classes/class.ilTestParticipantsTimeExtensionGUI.php
+++ Modules/Test/classes/class.ilTestParticipantsTimeExtensionGUI.php
@@ -35,39 +35,30 @@ class ilTestParticipantsTimeExtensionGUI
     public const CMD_SHOW_FORM = 'showForm';
     public const CMD_SET_TIMING = 'setTiming';
 
-    /**
-     * @var ilObjTest
-     */
-    protected $testObj;
+    protected ilObjTest $testObj;
+    protected ilCtrl $ctrl;
+    protected illanguage $lng;
     private \ilGlobalTemplateInterface $main_tpl;
 
     public function __construct(ilObjTest $testObj)
     {
         global $DIC;
-        $this->dic = $DIC;
-        $this->main_tpl = $this->dic->ui()->mainTemplate();
+        $this->ctrl = $DIC['ilCtrl'];
+        $this->lng = $DIC['lng'];
+        $this->main_tpl = $DIC->ui()->mainTemplate();
         $this->testObj = $testObj;
     }
 
-    /**
-     * @return ilObjTest
-     */
     public function getTestObj(): ilObjTest
     {
         return $this->testObj;
     }
 
-    /**
-     * @param ilObjTest $testObj
-     */
-    public function setTestObj($testObj)
+    public function setTestObj(ilObjTest $testObj)
     {
         $this->testObj = $testObj;
     }
 
-    /**
-     * @return bool
-     */
     protected function isExtraTimeFeatureAvailable(): bool
     {
         return (
@@ -82,10 +73,10 @@ class ilTestParticipantsTimeExtensionGUI
             ilObjTestGUI::accessViolationRedirect();
         }
 
-        switch ($this->dic->ctrl()->getNextClass($this)) {
+        switch ($this->ctrl->getNextClass($this)) {
             default:
 
-                $command = $this->dic->ctrl()->getCmd(self::CMD_SHOW_LIST) . 'Cmd';
+                $command = $this->ctrl->getCmd(self::CMD_SHOW_LIST) . 'Cmd';
 
                 $this->{$command}();
         }
@@ -94,7 +85,7 @@ class ilTestParticipantsTimeExtensionGUI
     public function showListCmd()
     {
         $tableGUI = new ilTimingOverviewTableGUI($this, self::CMD_SHOW_LIST);
-        $tableGUI->addCommandButton(self::CMD_SHOW_FORM, $this->dic->language()->txt('timing'));
+        $tableGUI->addCommandButton(self::CMD_SHOW_FORM, $this->lng->txt('timing'));
 
         $participantList = new ilTestParticipantList($this->getTestObj());
         $participantList->initializeFromDbRows($this->getTestObj()->getTestParticipants());
@@ -111,12 +102,12 @@ class ilTestParticipantsTimeExtensionGUI
                 'started' => '',
                 'extratime' => 0,
                 'login' => $participant->getLogin(),
-                'name' => $this->dic->language()->txt("anonymous")
+                'name' => $this->lng->txt("anonymous")
             ];
 
             $time = $this->getTestObj()->getStartingTimeOfUser($participant->getActiveId());
             if ($time) {
-                $started = $this->dic->language()->txt('tst_started') . ': ' . ilDatePresentation::formatDate(
+                $started = $this->lng->txt('tst_started') . ': ' . ilDatePresentation::formatDate(
                     new ilDateTime($time, IL_CAL_UNIX)
                 );
 
@@ -137,22 +128,22 @@ class ilTestParticipantsTimeExtensionGUI
 
         $tableGUI->setData($tableData);
 
-        $this->dic->ui()->mainTemplate()->setContent($this->dic->ctrl()->getHTML($tableGUI));
+        $this->main_tpl->setContent($this->ctrl->getHTML($tableGUI));
     }
 
     protected function showFormCmd()
     {
-        $this->dic->ui()->mainTemplate()->setContent($this->buildTimingForm()->getHTML());
+        $this->main_tpl->setContent($this->buildTimingForm()->getHTML());
     }
 
 
     protected function buildTimingForm(): ilPropertyFormGUI
     {
         $form = new ilPropertyFormGUI();
-        $form->setFormAction($this->dic->ctrl()->getFormAction($this));
+        $form->setFormAction($this->ctrl->getFormAction($this));
         $form->setTableWidth("100%");
         $form->setId("tst_change_workingtime");
-        $form->setTitle($this->dic->language()->txt("tst_change_workingtime"));
+        $form->setTitle($this->lng->txt("tst_change_workingtime"));
 
         // test users
         require_once 'Modules/Test/classes/class.ilTestParticipantList.php';
@@ -165,30 +156,30 @@ class ilTestParticipantsTimeExtensionGUI
 
         $addons = $this->getTestObj()->getTimeExtensionsOfParticipants();
 
-        $participantslist = new ilSelectInputGUI($this->dic->language()->txt('participants'), "participant");
+        $participantslist = new ilSelectInputGUI($this->lng->txt('participants'), "participant");
 
         $options = array(
-            '' => $this->dic->language()->txt('please_select'),
-            '0' => $this->dic->language()->txt('all_participants')
+            '' => $this->lng->txt('please_select'),
+            '0' => $this->lng->txt('all_participants')
         );
 
         foreach ($participantList as $participant) {
             $started = "";
 
             if ($this->getTestObj()->getAnonymity()) {
-                $name = $this->dic->language()->txt("anonymous");
+                $name = $this->lng->txt("anonymous");
             } else {
                 $name = $participant->getLastname() . ', ' . $participant->getFirstname();
             }
 
             $time = $this->getTestObj()->getStartingTimeOfUser($participant->getActiveId());
             if ($time) {
-                $started = ", " . $this->dic->language()->txt('tst_started') . ': ' . ilDatePresentation::formatDate(new ilDateTime($time, IL_CAL_UNIX));
+                $started = ", " . $this->lng->txt('tst_started') . ': ' . ilDatePresentation::formatDate(new ilDateTime($time, IL_CAL_UNIX));
             }
 
             $participant_id = $participant->getActiveId();
             if (array_key_exists($participant_id, $addons) && $addons[$participant_id] > 0) {
-                $started .= ", " . $this->dic->language()->txt('extratime') . ': ' . $addons[$participant_id] . ' ' . $this->dic->language()->txt('minutes');
+                $started .= ", " . $this->lng->txt('extratime') . ': ' . $addons[$participant_id] . ' ' . $this->lng->txt('minutes');
             }
 
             $options[$participant->getActiveId()] = $participant->getLogin() . ' (' . $name . ')' . $started;
@@ -199,12 +190,12 @@ class ilTestParticipantsTimeExtensionGUI
         $form->addItem($participantslist);
 
         // extra time
-        $extratime = new ilNumberInputGUI($this->dic->language()->txt("extratime"), "extratime");
-        $extratime->setInfo($this->dic->language()->txt('tst_extratime_info'));
+        $extratime = new ilNumberInputGUI($this->lng->txt("extratime"), "extratime");
+        $extratime->setInfo($this->lng->txt('tst_extratime_info'));
         $extratime->setRequired(true);
         $extratime->setMinValue(0);
         $extratime->setMinvalueShouldBeGreater(false);
-        $extratime->setSuffix($this->dic->language()->txt('minutes'));
+        $extratime->setSuffix($this->lng->txt('minutes'));
         $extratime->setSize(5);
         $form->addItem($extratime);
 
@@ -212,8 +203,8 @@ class ilTestParticipantsTimeExtensionGUI
             $form->setValuesByArray($_POST);
         }
 
-        $form->addCommandButton(self::CMD_SET_TIMING, $this->dic->language()->txt("save"));
-        $form->addCommandButton(self::CMD_SHOW_LIST, $this->dic->language()->txt("cancel"));
+        $form->addCommandButton(self::CMD_SET_TIMING, $this->lng->txt("save"));
+        $form->addCommandButton(self::CMD_SHOW_LIST, $this->lng->txt("cancel"));
 
         return $form;
     }
@@ -228,10 +219,10 @@ class ilTestParticipantsTimeExtensionGUI
                 $form->getInput('extratime')
             );
 
-            $this->main_tpl->setOnScreenMessage('success', sprintf($this->dic->language()->txt('tst_extratime_added'), $form->getInput('extratime')), true);
-            $this->dic->ctrl()->redirect($this, self::CMD_SHOW_LIST);
+            $this->main_tpl->setOnScreenMessage('success', sprintf($this->lng->txt('tst_extratime_added'), $form->getInput('extratime')), true);
+            $this->ctrl->redirect($this, self::CMD_SHOW_LIST);
         }
 
-        $this->dic->ui()->mainTemplate()->setVariable("ADM_CONTENT", $form->getHTML());
+        $this->main_tpl->setVariable("ADM_CONTENT", $form->getHTML());
     }
 }
