375baf21e9cb307b94833be092c9076e076cf3b9
Alexander Killing <killing@leifos.de>
2022-11-12 08:07:43 +0100

    fixed 26105: Breadcrumb des Magazin-Blogs wird nicht angezeigt.

--- Modules/Blog/classes/class.ilObjBlogGUI.php
+++ Modules/Blog/classes/class.ilObjBlogGUI.php
@@ -1152,7 +1152,7 @@ class ilObjBlogGUI extends ilObject2GUI implements ilDesktopItemHandling
         
         $lng = $DIC->language();
         $toolbar = $DIC->toolbar();
-        
+
         if (!$this->checkPermissionBool("read")) {
             ilUtil::sendInfo($lng->txt("no_permission"));
             return;
@@ -1217,7 +1217,6 @@ class ilObjBlogGUI extends ilObject2GUI implements ilDesktopItemHandling
         $owner = $this->object->getOwner();
         
         $ilTabs->clearTargets();
-        $ilLocator->clearItems();
         $tpl->setLocator();
         
         $back_caption = "";
@@ -1349,9 +1348,6 @@ class ilObjBlogGUI extends ilObject2GUI implements ilDesktopItemHandling
         $a_tpl->setTitleIcon($ppic);
         $a_tpl->setTitle($this->object->getTitle());
         $a_tpl->setDescription($name);
-        
-        // to get rid of locator in repository preview
-        $a_tpl->setVariable("LOCATOR", "");
     }
     
     /**
@@ -2653,7 +2649,7 @@ class ilObjBlogGUI extends ilObject2GUI implements ilDesktopItemHandling
         $ilLocator = $this->locator;
         
         if (is_object($this->object)) {
-            $ilLocator->addItem($this->object->getTitle(), $this->ctrl->getLinkTarget($this, ""), "", $this->node_id);
+            $ilLocator->addItem($this->object->getTitle(), $this->ctrl->getLinkTarget($this, "preview"), "", $this->node_id);
         }
     }
     
