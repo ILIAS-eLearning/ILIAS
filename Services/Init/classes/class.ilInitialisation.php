a2f8a3c2da888d48a64ce20bbdc55737179477b9
Fabian Helfer <fhelfer@nb-fhelfer.office.databay.de>
2022-11-16 11:44:54 +0100

    license ilinitialisation

--- Services/Init/classes/class.ilInitialisation.php
+++ Services/Init/classes/class.ilInitialisation.php
@@ -1,6 +1,20 @@
 <?php
 
-/* Copyright (c) 1998-2009 ILIAS open source, Extended GPL, see docs/LICENSE */
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
 
 // TODO:
 use ILIAS\BackgroundTasks\Dependencies\DependencyMap\BaseDependencyMap;
@@ -38,22 +52,6 @@ if (null === $DIC) {
     // Don't remove this, intellisense autocompletion does not work in PhpStorm without a top level assignment
     $DIC = new Container();
 }
-
-/**
- * This file is part of ILIAS, a powerful learning management system
- * published by ILIAS open source e-Learning e.V.
- *
- * ILIAS is licensed with the GPL-3.0,
- * see https://www.gnu.org/licenses/gpl-3.0.en.html
- * You should have received a copy of said license along with the
- * source code, too.
- *
- * If this is not the case or you just want to try ILIAS, you'll find
- * us at:
- * https://www.ilias.de
- * https://github.com/ILIAS-eLearning
- *
- *********************************************************************/
 
 /** @defgroup ServicesInit Services/Init
  */
