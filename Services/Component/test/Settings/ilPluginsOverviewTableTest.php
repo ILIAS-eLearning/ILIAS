dcafffd6d7c416afc1d0fc6d325f1ce0ed0f5197
tbongers-cat <tim.bongers@concepts-and-training.de>
2022-11-17 09:49:59 +0100

    Service/Component: added/corrected coypright notice in file headers

--- Services/Component/test/Settings/ilPluginsOverviewTableTest.php
+++ Services/Component/test/Settings/ilPluginsOverviewTableTest.php
@@ -1,8 +1,22 @@
 <?php
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ ********************************************************************
+ */
 
 declare(strict_types=1);
-
-/* Copyright (c) 2022 - Daniel Weise <daniel.weise@concepts-and-training.de> - Extended GPL, see LICENSE */
 
 use PHPUnit\Framework\TestCase;
 use ILIAS\UI\Factory;
