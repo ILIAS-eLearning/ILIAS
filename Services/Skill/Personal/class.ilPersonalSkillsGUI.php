14132a081ab7f75bba6df6c846c991a78107b541
Thomas Famula <famula@leifos.de>
2022-11-21 15:30:57 +0100

    Replace default image for competence profiles

--- Services/Skill/Personal/class.ilPersonalSkillsGUI.php
+++ Services/Skill/Personal/class.ilPersonalSkillsGUI.php
@@ -2152,7 +2152,7 @@ class ilPersonalSkillsGUI
                 $image = $this->ui_fac->image()->responsive($src->getSrc(), $this->lng->txt("skmg_custom_image_alt"));
             } else {
                 $image = $this->ui_fac->image()->responsive(
-                    "src/UI/examples/Image/HeaderIconLarge.svg",
+                    "./templates/default/images/logo/ilias_logo_72x72.png",
                     "ILIAS"
                 );
             }
