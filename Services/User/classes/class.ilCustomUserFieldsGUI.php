b8e78e7c81977755e4521540f5d4512b210ae6d7
Tim Schmitz <schmitz@leifos.de>
2022-12-08 11:59:05 +0100

    Bugfix 28976 for R7

--- Services/User/classes/class.ilCustomUserFieldsGUI.php
+++ Services/User/classes/class.ilCustomUserFieldsGUI.php
@@ -383,6 +383,12 @@ class ilCustomUserFieldsGUI
                 $valid = false;
             }
 
+            if ($access['required'] && $access['visible'] && !$access['changeable']) {
+                $this->confirm_change = true;
+                $form->getItemByPostVar("access")->setAlert($lng->txt('udf_required_and_visible_requires_changeable'));
+                $valid = false;
+            }
+
             if (!$this->field_id && $user_field_definitions->nameExists($form->getInput("name"))) {
                 $form->getItemByPostVar("name")->setAlert($lng->txt('udf_name_already_exists'));
                 $valid = false;
@@ -704,16 +710,31 @@ class ilCustomUserFieldsGUI
                 }
             }
         }
-        
+
+        $valid = true;
+
         foreach ($a_fields as $field_id => $definition) {
             if (isset($_POST['chb']['required_' . $field_id]) && (int) $_POST['chb']['required_' . $field_id] &&
                 (!isset($_POST['chb']['visib_reg_' . $field_id]) || !(int) $_POST['chb']['visib_reg_' . $field_id])) {
                 $this->confirm_change = true;
     
                 ilUtil::sendFailure($lng->txt('invalid_visible_required_options_selected'));
-                $this->listUserDefinedFields();
-                return false;
+                $valid = false;
             }
+
+            if (isset($_POST['chb']['required_' . $field_id]) && (int) $_POST['chb']['required_' . $field_id] &&
+                isset($_POST['chb']['visible_' . $field_id]) && (int) $_POST['chb']['visible_' . $field_id] &&
+                (!isset($_POST['chb']['changeable_' . $field_id]) || !(int) $_POST['chb']['changeable_' . $field_id])) {
+                $this->confirm_change = true;
+
+                ilUtil::sendFailure($lng->txt('udf_required_and_visible_requires_changeable'));
+                $valid = false;
+            }
+        }
+
+        if (!$valid) {
+            $this->listUserDefinedFields();
+            return false;
         }
         
         foreach ($a_fields as $field_id => $definition) {
