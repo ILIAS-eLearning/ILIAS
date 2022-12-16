ce21b54601b6fd079c96b4f0f655db0f0cba07f5
Richard Klees <richard.klees@concepts-and-training.de>
2022-12-16 10:53:50 +0100

    Setup: improve message when last Objective only has failed preconditions.

--- src/Setup/ObjectiveIterator.php
+++ src/Setup/ObjectiveIterator.php
@@ -133,7 +133,8 @@ class ObjectiveIterator implements \Iterator
             $this->markAsFailed($cur);
             if (count($this->stack) === 0) {
                 throw new UnachievableException(
-                    "Objective had failed preconditions."
+                    "Objective '" . $cur->getLabel() . "' had failed preconditions:\n  - " .
+                    implode("\n  - ", array_map(fn ($o) => $o->getLabel(), $failed_preconditions))
                 );
             }
             $this->next();
