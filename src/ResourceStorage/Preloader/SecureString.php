da483e8e11c51aaff6b8fbdaeb17ce5d70cee431
Stephan Kergomard <webmaster@kergomard.ch>
2022-12-19 15:29:59 -0300

    File: Fix Doubleencoding in List-View
    See: https://mantis.ilias.de/view.php?id=35975

--- src/ResourceStorage/Preloader/SecureString.php
+++ src/ResourceStorage/Preloader/SecureString.php
@@ -30,6 +30,6 @@ trait SecureString
 {
     protected function secure(string $string): string
     {
-        return htmlspecialchars(strip_tags($string), ENT_QUOTES, 'UTF-8');
+        return htmlspecialchars(strip_tags($string), ENT_QUOTES, 'UTF-8', false);
     }
 }
