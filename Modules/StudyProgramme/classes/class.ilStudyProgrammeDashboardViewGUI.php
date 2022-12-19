212c53317227ef4f427b1e126c6a42c595e6601c
Nils Haagen <nhaagen@cat06.de>
2022-12-19 16:03:12 +0100

    PRG: use usertable in dashboardview

--- Modules/StudyProgramme/classes/class.ilStudyProgrammeDashboardViewGUI.php
+++ Modules/StudyProgramme/classes/class.ilStudyProgrammeDashboardViewGUI.php
@@ -53,9 +53,6 @@ class ilStudyProgrammeDashboardViewGUI
         $this->usr_id = $usr_id;
     }
 
-    /**
-     * @throws ilException
-     */
     public function getHTML(): string
     {
         //array ilStudyProgrammeUserTableRow[]
@@ -66,31 +63,15 @@ class ilStudyProgrammeDashboardViewGUI
             if (! $this->isReadable($prg)) {
                 continue;
             }
-            $properties = [];
-
-            /*
-                        $progress = $assignment->getProgressTree();
-                        [$minimum_percents, $current_percents] = $this->calculatePercent(
-                            $current_prg,
-                            $progress->getCurrentAmountOfPoints()
-                        );
-            */
-            $properties[] = [$this->txt('prg_dash_label_status') => $row->getStatus()];
-
-
-
-            //$properties[] = $this->fillMinimumCompletion($minimum_percents);
-            //$properties[] = $this->fillCurrentCompletion($current_percents);
+            $properties = [
+                [$this->txt('prg_dash_label_minimum') => $row->getMinimumRequiredPercent()],
+                [$this->txt('prg_dash_label_gain') => $row->getCurrentPercent()],
+                [$this->txt('prg_dash_label_status') => $row->getStatus()],
+            ];
 
             if (in_array($row->getStatusRaw(), [ilPRGProgress::STATUS_COMPLETED, ilPRGProgress::STATUS_ACCREDITED])) {
-                //$restart_date = $assignment->getRestartDate();
-                //$properties[] = $this->fillRestartFrom($restart_date);
-
-
-                $validity = $row->getValidity();
-                if (true || $row->getExpiryDate()) {
-                    $validity .= ' (' . $row->getExpiryDate() . ')';
-                }
+                //$properties[] = [$this->txt('prg_dash_label_restart_from') => $row->getRestartDate()];
+                $validity = $row->getExpiryDate() ? $row->getExpiryDate() : $row->getValidity();
                 $properties[] = [$this->txt('prg_dash_label_valid') => $validity];
             } else {
                 $properties[] = [$this->txt('prg_dash_label_finish_until') =>$row->getDeadline()];
@@ -106,64 +87,6 @@ class ilStudyProgrammeDashboardViewGUI
         $panel = $this->factory->panel()->listing()->standard($this->lng->txt("dash_studyprogramme"), $group);
 
         return $this->renderer->render($panel);
-    }
-
-    protected function fillValidation(?bool $valid, ?DateTimeImmutable $validation_expiry_date): array
-    {
-        $validation = '';
-        if (!$valid) {
-            $validation = $this->txt('no');
-        }
-        if ($valid && is_null($validation_expiry_date)) {
-            $validation = $this->txt('yes');
-        }
-        if ($valid && !is_null($validation_expiry_date)) {
-            $date = new ilDate($validation_expiry_date->format('Y-m-d'), IL_CAL_DATE);
-            $validation = ilDatePresentation::formatDate($date);
-        }
-
-        return [$this->txt('prg_dash_label_valid') => $validation];
-    }
-
-    protected function fillMinimumCompletion(float $value): array
-    {
-        $title = $value . " " . $this->txt('percentage');
-        return [$this->txt('prg_dash_label_minimum') => $title];
-    }
-
-    protected function fillCurrentCompletion(float $value): array
-    {
-        $title = $value . " " . $this->txt('percentage');
-        return [$this->txt('prg_dash_label_gain') => $title];
-    }
-
-
-    protected function fillFinishUntil(DateTimeImmutable $value = null): array
-    {
-        $ret = [];
-        if (!is_null($value)) {
-            $date = new ilDate(
-                $value->format('Y-m-d'),
-                IL_CAL_DATE
-            );
-            $date_string = ilDatePresentation::formatDate($date);
-            $ret[$this->txt('prg_dash_label_finish_until')] = $date_string;
-        }
-        return $ret;
-    }
-
-    protected function fillRestartFrom(DateTimeImmutable $value = null): array
-    {
-        $ret = [];
-        if (!is_null($value)) {
-            $date = new ilDate(
-                $value->format('Y-m-d'),
-                IL_CAL_DATE
-            );
-            $date_string = ilDatePresentation::formatDate($date);
-            $ret[$this->txt('prg_dash_label_restart_from')] = $date_string;
-        }
-        return $ret;
     }
 
     protected function getVisibleOnPDMode(): string
@@ -189,40 +112,6 @@ class ilStudyProgrammeDashboardViewGUI
     protected function txt(string $code): string
     {
         return $this->lng->txt($code);
-    }
-
-    protected function calculatePercent(ilObjStudyProgramme $prg, int $current_points): array
-    {
-        $minimum_percents = 0;
-        $current_percents = 0;
-
-        if ($prg->hasLPChildren()) {
-            $minimum_percents = 100;
-            if ($current_points > 0) {
-                $current_percents = 100;
-            }
-        }
-
-        $children = $prg->getAllPrgChildren();
-        if (count($children) > 0) {
-            $max_points = 0;
-            foreach ($children as $child) {
-                $max_points += $child->getPoints();
-            }
-
-            if ($max_points > 0) {
-                $prg_points = $prg->getPoints();
-                $minimum_percents = round((100 * $prg_points / $max_points), 2);
-            }
-            if ($current_points > 0) {
-                $current_percents = round((100 * $current_points / $max_points), 2);
-            }
-        }
-
-        return [
-            $minimum_percents,
-            $current_percents
-        ];
     }
 
     protected function buildItem(
