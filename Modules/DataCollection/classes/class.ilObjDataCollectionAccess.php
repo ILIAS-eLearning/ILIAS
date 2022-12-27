f32c90a4a94a5aac3f54429560b544cb9af732cd
Timon Amstutz <timon.amstutz@bluewin.ch>
2022-12-27 16:27:23 +0100

    DC: Permission fix, see #36070
    (cherry picked from commit f4b2a77a85575f3f4e28f95271728b64017bfc49)

--- Modules/DataCollection/classes/class.ilObjDataCollectionAccess.php
+++ Modules/DataCollection/classes/class.ilObjDataCollectionAccess.php
@@ -284,11 +284,10 @@ class ilObjDataCollectionAccess extends ilObjectAccess
      * @param $table_id
      * @return bool
      */
-    protected static function hasAccessToTable(int $table_id): bool
+    protected static function hasAccessToTable(int $table_id, int $ref_id): bool
     {
         $table = ilDclCache::getTableCache($table_id);
-
-        return $table->getIsVisible() || ($table_id == $table->getCollectionObject()->getFirstVisibleTableId());
+        return self::hasWriteAccess($ref_id) || $table->getIsVisible();
     }
 
     public static function hasAccessTo(int $ref_id, int $table_id, int $tableview_id): bool
@@ -307,7 +306,7 @@ class ilObjDataCollectionAccess extends ilObjectAccess
         // check access
         return self::hasWriteAccess($ref_id)
             || (
-                self::hasReadAccess($ref_id) && self::hasAccessToTable($table_id) && self::hasAccessToTableView($tableview)
+                self::hasReadAccess($ref_id) && self::hasAccessToTable($table_id, $ref_id) && self::hasAccessToTableView($tableview)
             );
     }
 
