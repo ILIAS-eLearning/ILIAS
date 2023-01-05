ac082d6a45e82f0f5c6e97b3eef6fea80f591ad5
Alexander Killing <killing@leifos.de>
2023-01-05 19:17:27 +0100

    consistently use mediaelement js controls in mediacast and copage content for youtube/vimeo

--- Modules/MediaCast/classes/class.ilMediaCastTableGUI.php
+++ Modules/MediaCast/classes/class.ilMediaCastTableGUI.php
@@ -188,6 +188,13 @@ class ilMediaCastTableGUI extends ilTable2GUI
                 if (!is_null($med)) {
                     if ($med->getLocationType() === "Reference") {
                         $file = $med->getLocation();
+                        if (in_array($med->getFormat(), ["video/vimeo", "video/youtube"])) {
+                            if (!is_int(strpos($file, "?"))) {
+                                $file .= "?controls=0";
+                            } else {
+                                $file .= "&controls=0";
+                            }
+                        }
                     } else {
                         $file = ilWACSignedPath::signFile(
                             ilObjMediaObject::_getURL($mob->getId()) . "/" . $med->getLocation()
