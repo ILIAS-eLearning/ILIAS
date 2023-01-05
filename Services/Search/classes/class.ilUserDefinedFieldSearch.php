52ffe30452cdd2ffce99712d8b0afe5a13abd325
smeyer <smeyer.ilias@gmx.de>
2023-01-05 15:24:42 +0100

    Merge pull request #5373 from mjansenDatabay/hotfix/trunk/35116
    Search: Fix passing wrong PHP type as ID to search result entry

--- Services/Search/classes/class.ilUserDefinedFieldSearch.php
+++ Services/Search/classes/class.ilUserDefinedFieldSearch.php
@@ -1,7 +1,22 @@
 <?php
 
 declare(strict_types=1);
-/* Copyright (c) 1998-2009 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
 
 
 /**
@@ -24,7 +39,7 @@ class ilUserDefinedFieldSearch extends ilAbstractSearch
             $where;
         $res = $this->db->query($query);
         while ($row = $res->fetchRow(ilDBConstants::FETCHMODE_OBJECT)) {
-            $this->search_result->addEntry($row->usr_id, 'usr', $this->__prepareFound($row));
+            $this->search_result->addEntry((int) $row->usr_id, 'usr', $this->__prepareFound($row));
         }
         return $this->search_result;
     }
