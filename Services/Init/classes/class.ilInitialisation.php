40582a551069a2fe11254067c096ab812922e398
mjansen <mjansen@databay.de>
2023-01-06 11:15:57 +0100

    Init: Don't report deprecations

--- Services/Init/classes/class.ilInitialisation.php
+++ Services/Init/classes/class.ilInitialisation.php
@@ -1198,7 +1198,7 @@ class ilInitialisation
     public static function handleErrorReporting()
     {
         // push the error level as high as possible / sane
-        error_reporting(E_ALL & ~E_NOTICE);
+        error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
 
         // see handleDevMode() - error reporting might be overwritten again
         // but we need the client ini first
