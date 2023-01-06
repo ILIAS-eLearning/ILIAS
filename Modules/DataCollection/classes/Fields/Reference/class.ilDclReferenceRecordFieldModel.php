c8b5c4fdd5681ce14f2e7d2bebeac8f24e4b7917
Timon Amstutz <timon.amstutz@bluewin.ch>
2023-01-06 11:00:06 +0100

    Fix return type, see comment in PR #4882
    (cherry picked from commit 09bb7c1a3c4b1014d21c1bd2fb98d88621620db9)

--- Modules/DataCollection/classes/Fields/Reference/class.ilDclReferenceRecordFieldModel.php
+++ Modules/DataCollection/classes/Fields/Reference/class.ilDclReferenceRecordFieldModel.php
@@ -180,5 +180,6 @@ class ilDclReferenceRecordFieldModel extends ilDclBaseRecordFieldModel
         if ($reference_record) {
             return (string) $reference_record->getId();
         }
+        return null;
     }
 }
