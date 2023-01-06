23ccbcdf135c27f7515f5e0f6c6e10935473b893
mjansen <mjansen@databay.de>
2023-01-06 13:30:27 +0100

    User: Fix wrong confirmation dialogue
    See: https://mantis.ilias.de/view.php?id=35378

--- Services/User/classes/class.ilObjUserFolderGUI.php
+++ Services/User/classes/class.ilObjUserFolderGUI.php
@@ -2847,8 +2847,8 @@ class ilObjUserFolderGUI extends ilObjectGUI
             [self::class],
             "settings"
         ));
-        $confirmDialog->addButton($this->lng->txt("confirm"), "confirmUsrFieldChangeListeners");
-        $confirmDialog->addButton($this->lng->txt("cancel"), "settings");
+        $confirmDialog->setConfirm($this->lng->txt("confirm"), "confirmUsrFieldChangeListeners");
+        $confirmDialog->setCancel($this->lng->txt("cancel"), "settings");
 
         $tpl = new ilTemplate(
             "tpl.usr_field_change_listener_confirm.html",
