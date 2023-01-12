f258edbc42206d61fc7941ef8236b66b8c3fb2ab
Alexander Killing <killing@leifos.de>
2023-01-12 17:51:45 +0100

    removed debug code

--- Modules/Group/UserActions/js/GroupUserActions.js
+++ Modules/Group/UserActions/js/GroupUserActions.js
@@ -10,7 +10,6 @@ il.Group = il.Group || {};
 		public_interface = {
 
 			initCreationForm: function (event, url) {
-				console.log("initCreationForm");
 				event.preventDefault();
 				event.stopPropagation();
 				il.Util.sendAjaxGetRequestToUrl (url, {}, {}, function (o) {
@@ -27,10 +26,7 @@ il.Group = il.Group || {};
 
 					e.preventDefault();
 					values = $('#il_grp_action_modal_content form').serializeArray();
-					console.log("Form Submitted...");
-					console.log(values);
 					il.Util.sendAjaxPostRequestToUrl($(this).attr('action'), values, function (o) {
-						console.log("post sucess on form submitted");
 						$('#il_grp_action_modal_content').html(o);
 						il.Group.UserActions.setCreationSubmit();
 					});
@@ -39,11 +35,9 @@ il.Group = il.Group || {};
 			},
 
 			createGroup: function (e) {
-				console.log("createGroup");
 				e.preventDefault();
 				values = $('#il_grp_action_modal_content form').serializeArray();
 				il.Util.sendAjaxPostRequestToUrl($('#il_grp_action_modal_content form').attr('action'), values, function (o) {
-					console.log("post sucess in createGroup");
 					$('#il_grp_action_modal_content').html(o);
 				});
 			},
@@ -61,8 +55,6 @@ il.Group = il.Group || {};
 	$(function () {
 
 		function initEvents(id) {
-			console.log(id);
-
 			$(id).find("[data-grp-action-add-to='1']").each(function () {
 				$(this).on("click", function(e) {
 					var url;
@@ -81,7 +73,6 @@ il.Group = il.Group || {};
 
 					il.Util.sendAjaxGetRequestToUrl(url, [], [], function (r) {
 						var modal_content = $('#il_grp_action_modal_content');
-						//console.log(r.responseText);
 						if (modal_content.length) {
 							modal_content.html(r.responseText);
 							modal_content.closest('.il-modal-roundtrip').modal().show();
@@ -94,7 +85,6 @@ il.Group = il.Group || {};
 		}
 
 		$(document).on('il.user.actions.updated', function(ev, id) {
-			console.log("il.user.actions.updated catched, id: " + id);
 			initEvents("#" + id);
 		});
 		initEvents("body");
--- Services/Notes/js/ilNotes.js
+++ Services/Notes/js/ilNotes.js
@@ -25,7 +25,6 @@ const ilNotes = {
   old: false,
 
   listNotes: function (e, hash, update_code) {
-    console.log("listNotes");
     // prevent the default action
     e.preventDefault();
     e.stopPropagation(); // #11546 - list properties not working
@@ -41,7 +40,6 @@ const ilNotes = {
   },
 
   listComments: function (e, hash, update_code) {
-    console.log("listComments");
     // prevent the default action
     e.preventDefault();
     e.stopPropagation(); // #11546 - list properties not working
@@ -58,7 +56,6 @@ const ilNotes = {
 
   // init the notes editing panel
   initPanel: function (comments, e) {
-    console.log("initPanel");
     let head_str;
     const t = ilNotes;
 
@@ -76,7 +73,6 @@ const ilNotes = {
     });
     $("#il_notes_modal .modal-body").html("");
     $("#il_notes_modal").data("status", "loading");
-    console.log($("#il_notes_modal").data("status"));
 
     if (comments) {
       this.sendAjaxGetRequest({ cmd: "getCommentsHTML", cadh: this.hash },
@@ -88,37 +84,30 @@ const ilNotes = {
   },
 
   cmdAjaxLink: function (e, url) {
-    console.log("cmdAjaxLink");
-    console.log(url);
     e.preventDefault();
     this.sendAjaxGetRequestToUrl(url, {}, { mode: 'cmd' });
   },
 
   cmdAjaxForm: function (e, url) {
-    console.log("cmdAjaxForm");
     e.preventDefault();
 
     this.sendAjaxPostRequest(e.target, url, { mode: 'cmd' });
   },
 
   setAjaxUrl: function (url) {
-    console.log("setAjaxUrl");
     this.ajax_url = url;
   },
 
   getAjaxUrl: function () {
-    console.log("getAjaxUrl");
     return this.ajax_url;
   },
 
   sendAjaxGetRequest: function (par, args) {
-    console.log("sendAjaxGetRequest");
     var url = this.getAjaxUrl();
     this.sendAjaxGetRequestToUrl(url, par, args)
   },
 
   sendAjaxGetRequestToUrl: function (url, par, args) {
-    console.log("sendAjaxGetRequestToUrl");
     var k;
     args.reg_type = "get";
     args.url = url;
@@ -136,9 +125,6 @@ const ilNotes = {
 
   // send request per ajax
   sendAjaxPostRequest: function (form, url, args) {
-    console.log("sendAjaxPostRequest");
-    console.log(form);
-    console.log(url);
     args.reg_type = "post";
     var cb =
       {
@@ -153,17 +139,12 @@ const ilNotes = {
   },
 
   inModal: function () {
-    console.log("inModal");
     const status = $("#il_notes_modal").data("status");
     const cs = $("#il_notes_modal").css("display");
-    console.log($("#il_notes_modal").length);
-    console.log($("#il_notes_modal").data("status"));
-    console.log(status);
     return ($("#il_notes_modal").length && (status === "loading" || cs !== "none"));
   },
 
   handleAjaxSuccess: function (o) {
-    console.log("handleAjaxSuccess");
     var t;
     // perform page modification
     if (o.responseText !== undefined) {
@@ -176,12 +157,10 @@ const ilNotes = {
           il.UICore.setRightPanelContent(o.responseText);
         } else {
           if (t.inModal()) {
-            console.log("setting " + "#il_notes_modal .modal-body");
             $("#il_notes_modal").data("status", "");
             $("#il_notes_modal .modal-body").html(o.responseText);
             ilNotes.init(document.getElementById("il_notes_modal"));
           } else {
-            console.log("setting " + "#notes_embedded_outer");
             $("#notes_embedded_outer").html(o.responseText);
             ilNotes.init(document.getElementById("notes_embedded_outer"));
           }
@@ -211,9 +190,6 @@ const ilNotes = {
 
   // FailureHandler
   updateWidget: function (id, url) {
-    console.log("updateWidget");
-    console.log(id);
-    console.log(url);
     il.Util.ajaxReplace(url, id);
   },
 
