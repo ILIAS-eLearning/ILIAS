44eb61093c1e12f6f6379f74d68c0c556aed65e1
Fred Neumann <fred.neumann@fau.de>
2023-01-26 04:40:34 +0100

    Fix for #35542: Using Manual Scoring on unanswered questions changes their status to 'answered' Points are now only saved in the manual scoring by question or by participant, if they differ from the existing points. If no points are given to an unanswered question, then the question keeps its status as not answered, even if feedback is given. This allows to score unanswered question (e.g. if the answer is given on paper) but prevents setting questions unintentionally as answered.

--- Modules/Test/classes/class.ilTestScoringByQuestionsGUI.php
+++ Modules/Test/classes/class.ilTestScoringByQuestionsGUI.php
@@ -244,26 +244,32 @@ class ilTestScoringByQuestionsGUI extends ilTestScoringGUI
             if (false == $skipParticipant[$pass][$active_id]) {
                 foreach ((array) $questions as $qst_id => $reached_points) {
                     $this->saveFeedback($active_id, $qst_id, $pass, $ajax);
-                    $update_participant = assQuestion::_setReachedPoints(
-                        $active_id,
-                        $qst_id,
-                        $reached_points,
-                        $maxPointsByQuestionId[$qst_id],
-                        $pass,
-                        1,
-                        $this->object->areObligationsEnabled()
-                    );
+
+                    // fix #35543: save manual points only if they differ from the existing points
+                    // this prevents a question being set to "answered" if only feedback is entered
+                    $old_points = assQuestion::_getReachedPoints($active_id, $qst_id, $pass);
+                    if ($reached_points != $old_points) {
+                        $update_participant = assQuestion::_setReachedPoints(
+                            $active_id,
+                            $qst_id,
+                            $reached_points,
+                            $maxPointsByQuestionId[$qst_id],
+                            $pass,
+                            1,
+                            $this->object->areObligationsEnabled()
+                        );
+                    }
                 }
 
                 if ($update_participant) {
-                    $changed_one = true;
-                    $lastAndHopefullyCurrentQuestionId = $qst_id;
-
                     ilLPStatusWrapper::_updateStatus(
                         $this->object->getId(),
                         ilObjTestAccess::_getParticipantId($active_id)
                     );
                 }
+
+                $changed_one = true;
+                $lastAndHopefullyCurrentQuestionId = $qst_id;
             }
         }
 
