093b74dfd53d6db7c773c08b6c64fbdab4749c1e
Richard Klees <richard.klees@concepts-and-training.de>
2023-01-27 10:29:51 +0100

    LSO: fix #36364

--- Modules/LearningSequence/classes/Player/class.ilLSPlayer.php
+++ Modules/LearningSequence/classes/Player/class.ilLSPlayer.php
@@ -98,7 +98,7 @@ class ilLSPlayer
         $this->ls_items->storeState($state, $current_item_ref_id, $next_item->getRefId());
 
         //get proper view
-        if ($next_item !== $current_item) {
+        if ($next_item != $current_item) {
             $view = $this->view_factory->getViewFor($next_item);
             $state = $this->ls_items->getStateFor($next_item, $view);
         }
