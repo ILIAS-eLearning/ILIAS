c348a77411e3bc824abd85380a35a9486c3ec5f3
Fabian Helfer <fhelfer@nb-fhelfer.office.databay.de>
2023-02-01 14:09:53 +0100

    license changes dashboard

--- Services/Repository/GlobalScreen/classes/RepositoryMainBarProvider.php
+++ Services/Repository/GlobalScreen/classes/RepositoryMainBarProvider.php
@@ -16,6 +16,8 @@
  *
  *********************************************************************/
 
+declare(strict_types=1);
+
 namespace ILIAS\Repository\Provider;
 
 use ILIAS\GlobalScreen\Helper\BasicAccessCheckClosuresSingleton;
