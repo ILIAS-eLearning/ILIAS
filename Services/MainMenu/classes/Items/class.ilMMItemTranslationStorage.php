2347765d87016da381f957b3f37ba7465054d42a
Fabian Schmid <fabian@sr.solutions>
2023-02-01 07:47:16 +0100

    [FIX] 0036113: Main Menu / ActiveRecord »» ilMMItemTranslationStorage

--- Services/MainMenu/classes/Items/class.ilMMItemTranslationStorage.php
+++ Services/MainMenu/classes/Items/class.ilMMItemTranslationStorage.php
@@ -1,5 +1,21 @@
 <?php
 
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
 declare(strict_types=1);
 
 use ILIAS\GlobalScreen\Identification\IdentificationInterface;
@@ -107,7 +123,7 @@ class ilMMItemTranslationStorage extends CachedActiveRecord
      * @con_fieldtype  text
      * @con_length     256
      */
-    protected string $identification;
+    protected string $identification = '';
     /**
      * @con_has_field  true
      * @con_fieldtype  text
