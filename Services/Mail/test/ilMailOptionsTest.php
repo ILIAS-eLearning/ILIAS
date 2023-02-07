48ee160ffd7589fa54bac5d1a50508c9af8521b3
iszmais <iszmais@databay.de>
2023-02-07 19:17:17 +0100

    fix mail adress read with globale settings set

--- Services/Mail/test/ilMailOptionsTest.php
+++ Services/Mail/test/ilMailOptionsTest.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -18,59 +16,76 @@ declare(strict_types=1);
  *
  *********************************************************************/
 
+declare(strict_types=1);
+
+use PHPUnit\Framework\MockObject\MockObject;
+
 /**
- * Class ilMailOptionsTest
- * @author Niels Theen <ntheen@databay.de>
- * @author Michael Jansen <mjansen@databay.de>
+ * @author Ingmar Szmais <iszmais@databay.de>
  */
 class ilMailOptionsTest extends ilMailBaseTest
 {
-    /**
-     * @throws ReflectionException
-     */
+    protected MockObject $setting;
+    protected stdClass $object;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->database = $this->getMockBuilder(ilDBInterface::class)
+                               ->getMock();
+        $queryMock = $this->getMockBuilder(ilDBStatement::class)
+                          ->getMock();
+
+        $this->object = new stdClass();
+        $this->object->cronjob_notification = false;
+        $this->object->signature = 'smth';
+        $this->object->incoming_type = 1;
+        $this->object->mail_address_option = 0;
+        $this->object->email = 'test@test.com';
+        $this->object->second_email = 'ilias@ilias.com';
+
+        $this->database->expects($this->once())->method('fetchObject')->willReturn($this->object);
+        $this->database->expects($this->once())->method('queryF')->willReturn($queryMock);
+        $this->database->method('replace')->willReturn(0);
+        $this->setGlobalVariable('ilDB', $this->database);
+
+        $this->settings = $this->getMockBuilder(ilSetting::class)->disableOriginalConstructor()->getMock();
+    }
+
     public function testConstructor(): void
     {
-        $userId = 1;
+        $this->settings->expects($this->exactly(3))->method('get')->willReturnMap(
+            [
+                ['mail_incoming_mail', '', ''],
+                ['mail_address_option', '', ''],
+                ['show_mail_settings', null, '0']
+            ]
+        );
+        $this->setGlobalVariable('ilSetting', $this->settings);
 
-        $database = $this->getMockBuilder(ilDBInterface::class)
-            ->getMock();
-        $queryMock = $this->getMockBuilder(ilDBStatement::class)
-            ->getMock();
-
-        $object = new stdClass();
-        $object->cronjob_notification = false;
-        $object->signature = 'smth';
-        $object->incoming_type = 1;
-        $object->mail_address_option = 0;
-        $object->email = 'test@test.com';
-        $object->second_email = 'ilias@ilias.com';
-
-        $database->expects($this->once())->method('fetchObject')->willReturn($object);
-        $database->expects($this->once())->method('queryF')->willReturn($queryMock);
-
-        $this->setGlobalVariable('ilDB', $database);
-
-        $settings = $this->getMockBuilder(ilSetting::class)->disableOriginalConstructor()->onlyMethods([
-            'set',
-            'get',
-        ])->getMock();
-        $this->setGlobalVariable('ilSetting', $settings);
-
-        $mailOptions = new class ($userId) extends ilMailOptions {
-            public function read(): void
-            {
-                parent::read();
-            }
-        };
+        $mailOptions = new ilMailOptions(1);
 
         $this->assertSame('', $mailOptions->getSignature());
         $this->assertSame(ilMailOptions::INCOMING_LOCAL, $mailOptions->getIncomingType());
-        $this->assertSame(false, $mailOptions->isCronJobNotificationEnabled());
+        $this->assertFalse($mailOptions->isCronJobNotificationEnabled());
+    }
 
-        $mailOptions->read();
+    public function testConstructorWithUserSettings(): void
+    {
+        $this->settings->expects($this->exactly(3))->method('get')->willReturnMap(
+            [
+                ['mail_incoming_mail', '', ''],
+                ['mail_address_option', '', ''],
+                ['show_mail_settings', null, '1']
+            ]
+        );
+        $this->setGlobalVariable('ilSetting', $this->settings);
 
-        $this->assertSame($object->signature, $mailOptions->getSignature());
-        $this->assertSame($object->incoming_type, $mailOptions->getIncomingType());
-        $this->assertSame($object->cronjob_notification, $mailOptions->isCronJobNotificationEnabled());
+        $mailOptions = new ilMailOptions(1);
+
+        $this->assertSame($this->object->signature, $mailOptions->getSignature());
+        $this->assertSame($this->object->incoming_type, $mailOptions->getIncomingType());
+        $this->assertSame($this->object->cronjob_notification, $mailOptions->isCronJobNotificationEnabled());
     }
 }
