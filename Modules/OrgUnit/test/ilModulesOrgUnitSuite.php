c6b79339e9da04032283923fcbd8d6eb665e2aa3
Tim Schmitz <schmitz@leifos.de>
2023-02-23 11:53:42 +0100

    fixed cp error and notice

--- Modules/OrgUnit/test/ilModulesOrgUnitSuite.php
+++ Modules/OrgUnit/test/ilModulesOrgUnitSuite.php
@@ -2,19 +2,21 @@
 
 declare(strict_types=1);
 
-/******************************************************************************
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
  *
- * This file is part of ILIAS, a powerful learning management system.
- *
- * ILIAS is licensed with the GPL-3.0, you should have received a copy
- * of said license along with the source code.
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
  *
  * If this is not the case or you just want to try ILIAS, you'll find
  * us at:
- *      https://www.ilias.de
- *      https://github.com/ILIAS-eLearning
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
  *
- *****************************************************************************/
+ *********************************************************************/
 
 use PHPUnit\Framework\TestSuite;
 
@@ -33,8 +35,8 @@ class ilModulesOrgUnitSuite extends TestSuite
         require_once("./Modules/OrgUnit/test/ilOrgUnitOperationContextRegisteredObjectiveTest.php");
         $suite->addTestSuite("ilOrgUnitOperationContextRegisteredObjectiveTest");
         /** @noRector */
-        require_once("./Modules/OrgUnit/test/ilOrgUnitOperationContextRegisteredObjectiveTest.php");
-        $suite->addTestSuite("ilOrgUnitOperationContextRegisteredObjectiveTest");
+        require_once("./Modules/OrgUnit/test/ilOrgUnitOperationRegisteredObjectiveTest.php");
+        $suite->addTestSuite("ilOrgUnitOperationRegisteredObjectiveTest");
 
         return $suite;
     }
