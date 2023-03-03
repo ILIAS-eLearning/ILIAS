3ea1b8baff7fd859fee777f1422e7abf0fec1194
Stephan Kergomard <webmaster@kergomard.ch>
2023-03-03 10:21:39 +0100

    Test: Fix Linked Titles in List of Answers
    See: https://mantis.ilias.de/view.php?id=36213

--- Modules/Test/classes/class.ilTestEvaluationGUI.php
+++ Modules/Test/classes/class.ilTestEvaluationGUI.php
@@ -1282,8 +1282,7 @@ class ilTestEvaluationGUI extends ilTestServiceGUI
 
         //$questionAnchorNav = $this->object->canShowSolutionPrintview();
         $questionAnchorNav =
-            $this->object->getShowSolutionListOwnAnswers() &&
-            $this->object->getShowSolutionDetails() ;
+            $this->object->getShowSolutionListOwnAnswers();
 
         $tpl = new ilTemplate('tpl.il_as_tst_pass_details_overview_participants.html', true, true, "Modules/Test");
 
@@ -2005,7 +2004,7 @@ class ilTestEvaluationGUI extends ilTestServiceGUI
 				FROM qpl_hint_tracking
 				WHERE qhtr_active_fi = ' . $ilDB->quote($active_fi, 'integer') . '
 				AND qhtr_pass = ' . $ilDB->quote($pass, 'integer')
-            );
+        );
 
         if ($must_renumber) {
             $ilDB->manipulate(
@@ -2013,9 +2012,9 @@ class ilTestEvaluationGUI extends ilTestServiceGUI
 				SET qhtr_pass = qhtr_pass - 1
 				WHERE qhtr_active_fi = ' . $ilDB->quote($active_fi, 'integer') . '
 				AND qhtr_pass > ' . $ilDB->quote($pass, 'integer')
-                );
+            );
         }
-            
+
         // tst_test_rnd_qst -> nothing to do
 
         // tst_times
