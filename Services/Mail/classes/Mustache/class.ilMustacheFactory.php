8b0c9340eaeb886ad1559bb627b098a6987dd69a
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2023-03-07 12:05:23 +0100

    Mail: Rearrange copyright and declare statement

--- Services/Mail/classes/Mustache/class.ilMustacheFactory.php
+++ Services/Mail/classes/Mustache/class.ilMustacheFactory.php
@@ -1,4 +1,4 @@
-<?php declare(strict_types=1);
+<?php
 
 /**
  * This file is part of ILIAS, a powerful learning management system
@@ -16,9 +16,11 @@
  *
  *********************************************************************/
 
+declare(strict_types=1);
+
 class ilMustacheFactory
 {
-    public function getBasicEngine() : \Mustache_Engine
+    public function getBasicEngine(): \Mustache_Engine
     {
         return new \Mustache_Engine();
     }
