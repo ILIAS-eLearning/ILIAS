48788cfd9c84ed91b72655d5b86341087caa64ca
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2023-03-07 12:05:44 +0100

    Course: Rearrange copyright and declare statement

--- Modules/Course/classes/class.ilCourseMailTemplateTutorContext.php
+++ Modules/Course/classes/class.ilCourseMailTemplateTutorContext.php
@@ -1,6 +1,5 @@
 <?php
 
-declare(strict_types=0);
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -16,6 +15,8 @@ declare(strict_types=0);
  * https://github.com/ILIAS-eLearning
  *
  *********************************************************************/
+
+declare(strict_types=0);
 
 /**
  * Handles course mail placeholders
