41bcbb821e4a189128d26d984cef22fe6ad74563
Fabian Schmid <fabian@sr.solutions>
2023-03-13 10:52:18 +0100

    Render Cookies to Response

--- src/DI/HTTPServices.php
+++ src/DI/HTTPServices.php
@@ -1,5 +1,19 @@
 <?php
-/* Copyright (c) 2016 Richard Klees <richard.klees@concepts-and-training.de> Extended GPL, see docs/LICENSE */
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
 
 namespace ILIAS\DI;
 
@@ -122,7 +136,11 @@ class HTTPServices implements GlobalHttpState
      */
     public function sendResponse() : void
     {
-        $this->sender->sendResponse($this->response());
+        // Render Cookies to the response.
+        $response = $this->response();
+        $response = $this->cookieJar()->renderIntoResponseHeader($response);
+        
+        $this->sender->sendResponse($response);
     }
 
 
