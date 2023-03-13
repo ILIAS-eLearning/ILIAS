23a7b91835570503ab5e68da1b91fb73186ac85f
Fabian Schmid <fabian@sr.solutions>
2023-03-13 12:28:57 +0100

    DB: Check for RowFormat and Engine during Setup

--- Services/Database/classes/Setup/class.ilDatabaseSetupAgent.php
+++ Services/Database/classes/Setup/class.ilDatabaseSetupAgent.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -17,6 +15,8 @@ declare(strict_types=1);
  * https://github.com/ILIAS-eLearning
  *
  *********************************************************************/
+
+declare(strict_types=1);
 
 use ILIAS\Setup;
 use ILIAS\Refinery\Factory as Refinery;
@@ -76,6 +76,7 @@ class ilDatabaseSetupAgent implements Setup\Agent
             "Complete objectives from Services\Database",
             false,
             new ilDatabaseConfigStoredObjective($config),
+            new ilDatabaseEnvironmentValidObjective(),
             new \ilDatabasePopulatedObjective($config),
             new \ilDatabaseUpdatedObjective()
         );
@@ -90,6 +91,7 @@ class ilDatabaseSetupAgent implements Setup\Agent
         if ($config instanceof ilDatabaseSetupConfig) {
             $p[] = new \ilDatabaseConfigStoredObjective($config);
         }
+        $p[] = new ilDatabaseEnvironmentValidObjective();
         $p[] = new \ilDatabaseUpdatedObjective();
         return new Setup\ObjectiveCollection(
             "Complete objectives from Services\Database",
