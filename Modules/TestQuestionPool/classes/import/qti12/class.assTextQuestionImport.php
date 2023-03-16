0de8fb239bd0e4947a31dc32585e9a8717ee4a8b
Stephan Kergomard <webmaster@kergomard.ch>
2023-03-16 18:10:36 +0100

    Test: Fix Import of Essay Questino Without Scoring
    See: https://mantis.ilias.de/view.php?id=36976

--- Modules/TestQuestionPool/classes/import/qti12/class.assTextQuestionImport.php
+++ Modules/TestQuestionPool/classes/import/qti12/class.assTextQuestionImport.php
@@ -142,12 +142,16 @@ class assTextQuestionImport extends assQuestionImport
 
         $no_keywords_found = true;
 
-        $termscoring = $this->fetchTermScoring($item);
-        for ($i = 0, $iMax = count($termscoring); $i < $iMax; $i++) {
-            $this->object->addAnswer($termscoring[$i]->getAnswertext(), $termscoring[$i]->getPoints());
-            $no_keywords_found = false;
+        if ($item->getMetadataEntry('termrelation') !== 'non'
+            && $item->getMetadataEntry('termrelation') !== null) {
+            $termscoring = $this->fetchTermScoring($item);
+            for ($i = 0, $iMax = count($termscoring); $i < $iMax; $i++) {
+                $this->object->addAnswer($termscoring[$i]->getAnswertext(), $termscoring[$i]->getPoints());
+                $no_keywords_found = false;
+            }
         }
-        if (count($termscoring)) {
+
+        if ($item->getMetadataEntry('termrelation') !== null) {
             $this->object->setKeywordRelation($item->getMetadataEntry('termrelation'));
         }
 
