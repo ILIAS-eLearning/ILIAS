030244c6849963102d439b4329f304002f5b8a73
Guido Vollbach <gvollbach@databay.de>
2023-03-17 09:30:22 +0100

    Fixed strict type for tests

--- Services/PDFGeneration/test/bootstrap.php
+++ Services/PDFGeneration/test/bootstrap.php
@@ -1,7 +1,5 @@
 <?php
 
-
-declare(strict_types=1);
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -18,5 +16,6 @@ declare(strict_types=1);
  *
  *********************************************************************/
 
+declare(strict_types=1);
 
 require_once 'libs/composer/vendor/autoload.php';
