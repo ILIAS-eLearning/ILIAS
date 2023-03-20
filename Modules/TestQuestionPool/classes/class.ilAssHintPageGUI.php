c4761d383342293ecffc4d8430fa016a37a46e48
Stephan Kergomard <webmaster@kergomard.ch>
2023-03-20 13:08:32 +0100

    Test: Mark Selected Tab in Hint- and Feedback-Page
    See: https://mantis.ilias.de/view.php?id=33644

--- Modules/TestQuestionPool/classes/class.ilAssHintPageGUI.php
+++ Modules/TestQuestionPool/classes/class.ilAssHintPageGUI.php
@@ -35,11 +35,15 @@ class ilAssHintPageGUI extends ilPageObjectGUI
      */
     public function __construct($a_id = 0, $a_old_nr = 0)
     {
-        global $DIC;
-        $tpl = $DIC['tpl'];
-
         parent::__construct("qht", $a_id, $a_old_nr);
         $this->setTemplateTargetVar('ADM_CONTENT');
         $this->setTemplateOutput(true);
+    }
+
+    public function preview(): string
+    {
+        $page = parent::preview();
+        $this->tabs_gui->activateTab("pg");
+        return $page;
     }
 }
--- Modules/TestQuestionPool/classes/feedback/class.ilAssSpecFeedbackPageGUI.php
+++ Modules/TestQuestionPool/classes/feedback/class.ilAssSpecFeedbackPageGUI.php
@@ -34,11 +34,15 @@ class ilAssSpecFeedbackPageGUI extends ilPageObjectGUI
      */
     public function __construct($a_id = 0, $a_old_nr = 0)
     {
-        global $DIC;
-        $tpl = $DIC['tpl'];
-
         parent::__construct("qfbs", $a_id, $a_old_nr);
         $this->setTemplateTargetVar('ADM_CONTENT');
         $this->setTemplateOutput(true);
+    }
+
+    public function preview(): string
+    {
+        $page = parent::preview();
+        $this->tabs_gui->activateTab("pg");
+        return $page;
     }
 }
