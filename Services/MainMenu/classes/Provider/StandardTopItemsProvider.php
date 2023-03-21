f6abd19c11d6e70359bcc42f8ec7818ebeea2a56
Fabian Schmid <fabian@sr.solutions>
2023-03-21 13:36:31 +0100

    [FIX] 0037012: Hauptmenü Konfiguration Aktiv Feld
    # Conflicts:
    #	Services/MainMenu/classes/Provider/StandardTopItemsProvider.php

--- Services/MainMenu/classes/Provider/StandardTopItemsProvider.php
+++ Services/MainMenu/classes/Provider/StandardTopItemsProvider.php
@@ -1,5 +1,21 @@
 <?php
 
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
 declare(strict_types=1);
 
 namespace ILIAS\MainMenu\Provider;
@@ -136,23 +152,10 @@ class StandardTopItemsProvider extends AbstractStaticMainMenuProvider
         $icon = $this->dic->ui()->factory()->symbol()->icon()->custom(\ilUtil::getImagePath("icon_orga.svg"), $title);
 
         $organisation = $this->mainmenu->topParentItem($this->getOrganisationIdentification())
-            ->withVisibilityCallable($this->basic_access_helper->isUserLoggedIn(function (): bool {
-                return (new ilMyStaffCachedAccessDecorator(
-                    $this->dic,
-                    ilMyStaffAccess::getInstance()
-                ))->hasCurrentUserAccessToMyStaff();
-            }))
+            ->withVisibilityCallable($this->basic_access_helper->isUserLoggedIn())
             ->withSymbol($icon)
             ->withTitle($title)
-            ->withPosition(60)
-            ->withAvailableCallable(
-                function (): bool {
-                    return (new ilMyStaffCachedAccessDecorator(
-                        $this->dic,
-                        ilMyStaffAccess::getInstance()
-                    ))->hasCurrentUserAccessToMyStaff();
-                }
-            );
+            ->withPosition(60);
 
         $title = $f("mm_administration");
         $icon = $this->dic->ui()->factory()->symbol()->icon()->standard("adm", $title);
