164e8f6eb3a3da040e06312aee2152737cfa4270
Nicolas Roeser <Nicolas.Roeser@rz.uni-regensburg.de>
2023-03-27 11:43:51 +0200

    examples: Rectify capitalization of “GitHub”

--- src/UI/examples/Dropdown/Standard/with_buttons_and_links.php
+++ src/UI/examples/Dropdown/Standard/with_buttons_and_links.php
@@ -11,7 +11,7 @@ function with_buttons_and_links()
     $renderer = $DIC->ui()->renderer();
 
     $items = array(
-        $f->button()->shy("Github", "https://www.github.com"),
+        $f->button()->shy("GitHub", "https://www.github.com"),
         $f->link()->standard("ILIAS", "https://www.ilias.de")->withOpenInNewViewport(true)
     );
     return $renderer->render($f->dropdown()->standard($items)->withLabel("Actions"));
