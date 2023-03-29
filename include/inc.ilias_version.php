1c603a9f6bed143dc18825b1f8e07b80a5b848be
Fabian Wolf <wolf@ilias.de>
2023-03-29 16:21:40 +0200

    Release 6.23

--- include/inc.ilias_version.php
+++ include/inc.ilias_version.php
@@ -9,5 +9,5 @@
 *
 * @package ilias-core
 */
-define("ILIAS_VERSION", "6.22 2023-02-01");
-define("ILIAS_VERSION_NUMERIC", "6.22");			// since version ILIAS 6 this must be always x.y: x and y are numbers
+define("ILIAS_VERSION", "6.23 2023-03-29");
+define("ILIAS_VERSION_NUMERIC", "6.23");			// since version ILIAS 6 this must be always x.y: x and y are numbers
