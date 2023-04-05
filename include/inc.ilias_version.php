ea045cccce3d8f7aae662bacb172864c0da1bc84
Fabian Wolf <fabian@eros.rudelnet.de>
2023-04-05 17:48:21 +0200

    Release 7.20

--- include/inc.ilias_version.php
+++ include/inc.ilias_version.php
@@ -9,5 +9,5 @@
 *
 * @package ilias-core
 */
-define("ILIAS_VERSION", "7.19 2023-03-16");
-define("ILIAS_VERSION_NUMERIC", "7.19");			// since version ILIAS 6 this must be always x.y: x and y are numbers
+define("ILIAS_VERSION", "7.20 2023-04-05");
+define("ILIAS_VERSION_NUMERIC", "7.20");			// since version ILIAS 6 this must be always x.y: x and y are numbers
