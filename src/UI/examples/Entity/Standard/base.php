46e917ef22226a873366f707d53c1b4d1b4cb456
Ferdinand Engländer <ferdinand.englaender@concepts-and-training.de>
2023-04-12 18:16:51 +0200

    UI: entity styling & example

--- src/UI/examples/Entity/Standard/base.php
+++ src/UI/examples/Entity/Standard/base.php
@@ -10,11 +10,99 @@ function base()
     $f = $DIC->ui()->factory();
     $renderer = $DIC->ui()->renderer();
 
-    $secondary_id = $f->symbol()->icon()->standard('crs', 'some course', 'medium');
+    /*
+    * Basic Construction
+    */
+
+    $primary_id = "Building Better UX by Considering Information Architecture and User Intent";
+    $secondary_id = $f->symbol()->icon()->standard('crs', 'course icon', 'large');
+
+    // creating the entity object now so it can be filled in the logic section
+    $entity = $f->entity()->standard(
+        $primary_id,
+        $secondary_id
+    );
+
+    /*
+    * Dropdown Actions
+    */
+
     $actions = [
         $f->button()->shy("ILIAS", "https://www.ilias.de"),
         $f->button()->shy("GitHub", "https://www.github.com")
     ];
+    $entity = $entity->withActions($actions);
+
+    /*
+    * Logic for Pulling Availabilty Properties to Blocking Conditions
+    */
+
+    $availability_input = ['Available Seats' => 0, 'Available' => 'until 24.12.2023', 'Expected Preconditions' => 'UI Design 101', 'Passed Courses' => 'Painting'];
+
+    $blocking = $f->listing()->property();
+    $availability = $f->listing()->property();
+
+    foreach ($availability_input as $key => $value) {
+        switch($key) {
+            case 'Expected Preconditions':
+                if ($value !== $availability_input['Passed Courses']) {
+                    $blocking = $blocking->withProperty($key, $f->button()->shy("Preconditions", "http://www.ilias.de"), false);
+                }
+                break;
+
+            case 'Available Seats':
+                if ($value === 0) {
+                    $blocking = $blocking->withProperty($key, "No seats available", false);
+                } else {
+                    $availability = $availability->withProperty($key, (string)$value);
+                }
+                break;
+
+            case 'Passed Courses':
+                // are not displayed anywhere
+                break;
+
+            default:
+                $availability = $availability->withProperty($key, $value);
+                break;
+        }
+    };
+    // all remaining availability properties
+    $entity = $entity
+        ->withBlockingAvailabilityConditions($blocking)
+        ->withAvailability($availability);
+
+    /*
+    * All Other Semantic Groups
+    */
+
+    $reactions = [
+        $f->button()->tag('UX/UI', '#'), $f->button()->tag('First Semester', '#')
+    ];
+
+    $details = $f->listing()->property()
+        ->withProperty('Duration', '90 minutes')
+        ->withProperty('Recording', 'recording available', false)
+    ;
+
+    $status = [
+        $f->symbol()->icon()->custom('./templates/default/images/learning_progress/in_progress.svg', 'incomplete'),
+        $f->legacy('in progress')
+    ];
+
+    $entity = $entity
+    ->withPersonalStatus($status)
+    ->withDetails($details)
+    ->withReactions($reactions)
+    ;
+
+    /*
+    * Priority Areas
+    */
+
+    $featured_properties = $f->listing()->property()
+        ->withProperty('Event Date', '14.02.2023');
+
     $prio_reactions = [
         $f->symbol()->glyph()->love()
             ->withCounter($f->counter()->status(2)),
@@ -22,36 +110,18 @@ function base()
             ->withCounter($f->counter()->novelty(3))
             ->withCounter($f->counter()->status(7))
     ];
-    $reactions = [
-        $f->button()->tag('tag', '#')
-    ];
 
-    $details = $f->listing()->property()
-        ->withProperty('detail: ', '7')
-        ->withProperty('detail2', 'unlabled detail', false)
-        ->withProperty('another detail: ', 'anothervalue');
+    $main_detail_1 = $f->listing()->property()
+        ->withProperty('Room', '7')
+    ;
+    $main_detail_2 = $f->listing()->property()
+        ->withProperty('Description', 'This lecture is an introduction to basic concepts fundamental for an intuitive user experience. These basic principles are not directly connected to the visual design, yet they help us to discover a hierarchy in relevance that needs to be respected for the visual appearance.', false)
+    ;
 
-    $status = [
-        $f->symbol()->icon()->custom('./templates/default/images/learning_progress/in_progress.svg', 'incomplete'),
-        $f->legacy('personal status')
-    ];
-
-    $availability = $f->listing()->property()
-        ->withProperty('available', 'until 2024/12/24');
-
-    $entity = $f->entity()->standard(
-        'primary id',
-        $secondary_id
-    )
-    ->withFeaturedProperties('Status: offline')
-    ->withMainDetails('This is a descriptive text. This is a descriptive text. This is a descriptive text.')
-    ->withBlockingAvailabilityConditions('there are blocking conditions!')
-    ->withPersonalStatus($status)
-    ->withAvailability($availability)
-    ->withDetails($details)
+    $entity = $entity
+    ->withFeaturedProperties($featured_properties)
+    ->withMainDetails([$main_detail_1, $main_detail_2])
     ->withPrioritizedReactions($prio_reactions)
-    ->withReactions($reactions)
-    ->withActions($actions)
     ;
 
     return $renderer->render($entity);
