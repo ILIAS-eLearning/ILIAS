d2851fa7135aab1ee681e184d763fbf79fe31da0
Nils Haagen <nhaagen@cat06.de>
2023-04-13 14:13:09 +0200

    UI/ViewControl: example in form-wrapper

--- src/UI/examples/Input/ViewControl/Sortation/base.php
+++ src/UI/examples/Input/ViewControl/Sortation/base.php
@@ -10,14 +10,18 @@ function base()
     $f = $DIC['ui.factory'];
     $r = $DIC['ui.renderer'];
 
+    //construct with options and labels
     $sortation = $f->input()->viewControl()->sortation(
         [
             'field1:ASC' => 'Field 1, ascending',
             'field1:DESC' => 'Field 1, descending',
             'field2:ASC' => 'Field 2, ascending'
-
         ]
     );
 
-    return $r->render($sortation);
+    //wrap the control in a ViewControlContainer
+    $vc_container = $f->input()->container()->viewControl()->standard([$sortation])
+        ->withRequest($DIC->http()->request());
+
+    return $r->render([$vc_container, $f->divider()->horizontal()]) . print_r($vc_container->getData(), true);
 }
