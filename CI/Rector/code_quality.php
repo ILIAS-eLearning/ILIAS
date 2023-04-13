c9318f23e0541a41fa3bf0b9161545c5bfe8f5f3
Fabian Schmid <fabian@sr.solutions>
2023-04-13 06:29:53 +0200

    CodeStyle for Rector
    # Conflicts:
    #	CI/Rector/basic_rector.php
    #	CI/Rector/language_level.php
    #	CI/Rector/php8_rector.php

--- CI/Rector/code_quality.php
+++ CI/Rector/code_quality.php
@@ -2,20 +2,14 @@
 
 declare(strict_types=1);
 
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use ILIAS\CI\Rector\RemoveRequiresAndIncludesRector;
-use ILIAS\CI\Rector\ChangeLicenseHeader;
 use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\Set\ValueObject\SetList;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\DowngradeLevelSetList;
-use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
 
-return static function (RectorConfig $c): void {
-    $c->disableParallel();
-    $c->phpVersion(PhpVersion::PHP_80);
-    $c->sets([
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->disableParallel();
+    $rectorConfig->phpVersion(PhpVersion::PHP_80);
+    $rectorConfig->sets([
         SetList::DEAD_CODE,
         SetList::TYPE_DECLARATION,
         SetList::CODE_QUALITY,
--- CI/Rector/ilUtils/ilutil_rector.php
+++ CI/Rector/ilUtils/ilutil_rector.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use ILIAS\CI\Rector\ilUtils\ReplaceUtilSendMessageRector;
@@ -6,11 +8,11 @@ use Rector\Core\Configuration\Option;
 use ILIAS\CI\Rector\DIC\DICMemberResolver;
 use ILIAS\CI\Rector\DIC\DICDependencyManipulator;
 
-return function (ContainerConfigurator $containerConfigurator) : void {
+return function (ContainerConfigurator $containerConfigurator): void {
     // language level
     $language_level = require __DIR__ . "/../language_level.php";
     $language_level($containerConfigurator);
-    
+
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::DEBUG, false);
     $parameters->set(Option::SKIP, [
@@ -20,7 +22,7 @@ return function (ContainerConfigurator $containerConfigurator) : void {
         "Services/LTI",
         "Services/SOAPAuth/include"
     ]);
-    
+
     $services = $containerConfigurator->services();
     $services->set(DICMemberResolver::class)->autowire();
     $services->set(DICDependencyManipulator::class)->autowire();
