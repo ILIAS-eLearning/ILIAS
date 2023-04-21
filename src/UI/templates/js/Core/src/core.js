cb2458170ab4597bf776e6254b242d94a6c3a562
Nils Haagen <nhaagen@cat06.de>
2023-04-21 16:59:38 +0200

    UI, JS: import and inject global 'il' namespace (#5803)

--- src/UI/templates/js/Core/src/core.js
+++ src/UI/templates/js/Core/src/core.js
@@ -1,9 +1,7 @@
+import il from 'il';
 import replaceContent from './core.replaceContent.js'
 import Tooltip from './core.Tooltip.js'
 
-if (typeof il === 'undefined') {
-    var il = {}
-}
 il.UI = il.UI || {};
 il.UI.core = il.UI.core || {};
 
