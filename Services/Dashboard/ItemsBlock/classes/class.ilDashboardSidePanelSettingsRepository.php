7f1d4167ba0721b0b6f590f2ccf7d9b32651f706
Fabian Helfer <fhelfer@nb-fhelfer.office.databay.de>
2023-04-24 08:43:51 +0200

    fix copyright

--- Services/Dashboard/ItemsBlock/classes/class.ilDashboardSidePanelSettingsRepository.php
+++ Services/Dashboard/ItemsBlock/classes/class.ilDashboardSidePanelSettingsRepository.php
@@ -3,15 +3,20 @@
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
+ *
  * ILIAS is licensed with the GPL-3.0,
  * see https://www.gnu.org/licenses/gpl-3.0.en.html
  * You should have received a copy of said license along with the
  * source code, too.
+ *
  * If this is not the case or you just want to try ILIAS, you'll find
  * us at:
  * https://www.ilias.de
  * https://github.com/ILIAS-eLearning
- */
+ *
+ *********************************************************************/
+
+declare(strict_types=1);
 
 /**
  * Dashboard side panel settings Repo
@@ -54,7 +59,7 @@ class ilDashboardSidePanelSettingsRepository
     public function enable(string $mod, bool $active): void
     {
         if ($this->isValidModule($mod)) {
-            $this->setting->set("enable_" . $mod, (int) $active);
+            $this->setting->set("enable_" . $mod, $active ? '1' : '0');
         }
     }
 
@@ -62,7 +67,7 @@ class ilDashboardSidePanelSettingsRepository
     public function isEnabled(string $mod): bool
     {
         if ($this->isValidModule($mod)) {
-            return (bool) $this->setting->get("enable_" . $mod, true);
+            return (bool) $this->setting->get("enable_" . $mod, '1');
         }
         return false;
     }
