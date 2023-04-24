63e6c3fd549aaedc60d49e0cc8ff1e82814cec31
Pascal Seeland <pascal.seeland@tik.uni-stuttgart.de>
2023-04-24 10:41:53 +0200

    Fix licenseheader for PSR-12

--- Services/WebServices/ECS/classes/Setup/class.ilECSAgent.php
+++ Services/WebServices/ECS/classes/Setup/class.ilECSAgent.php
@@ -13,19 +13,18 @@
  * us at:
  * https://www.ilias.de
  * https://github.com/ILIAS-eLearning
- *
- *********************************************************************/
+ */
 
 use ILIAS\Setup;
 
 class ilECSAgent extends Setup\Agent\NullAgent
 {
-    public function getUpdateObjective(\ILIAS\Setup\Config $config = null) : \ILIAS\Setup\Objective
+    public function getUpdateObjective(\ILIAS\Setup\Config $config = null): \ILIAS\Setup\Objective
     {
         return new \ilDatabaseUpdateStepsExecutedObjective(new ilECSDBUpdateSteps());
     }
 
-    public function getStatusObjective(\ILIAS\Setup\Metrics\Storage $storage) : \ILIAS\Setup\Objective
+    public function getStatusObjective(\ILIAS\Setup\Metrics\Storage $storage): \ILIAS\Setup\Objective
     {
         return new \ilDatabaseUpdateStepsMetricsCollectedObjective($storage, new ilECSDBUpdateSteps());
     }
