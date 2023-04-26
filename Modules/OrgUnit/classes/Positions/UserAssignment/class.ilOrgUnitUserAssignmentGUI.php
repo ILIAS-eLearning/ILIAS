3d9d59207abe5a914f993933dec60f3e5ab29428
tbongers-cat <tim.bongers@concepts-and-training.de>
2023-04-26 14:38:24 +0200

    OrgU: Bugfix in User Assignment
    Wrong parameter for deleteRecursive()

--- Modules/OrgUnit/classes/Positions/UserAssignment/class.ilOrgUnitUserAssignmentGUI.php
+++ Modules/OrgUnit/classes/Positions/UserAssignment/class.ilOrgUnitUserAssignmentGUI.php
@@ -197,14 +197,14 @@ class ilOrgUnitUserAssignmentGUI extends BaseCommands
         $this->cancel();
     }
 
-    protected function deleteRecursive()
+    protected function deleteRecursive(): void
     {
         $r = $this->http->request();
         $assignments = $this->assignmentRepo
             ->getByUserAndPosition((int) $_POST['usr_id'], (int) $r->getQueryParams()['position_id']);
 
         foreach ($assignments as $assignment) {
-            $this->assignmentRepo->delete($assignment->getId());
+            $this->assignmentRepo->delete($assignment);
         }
         $this->main_tpl->setOnScreenMessage('success', $this->language->txt('remove_successful'), true);
         $this->cancel();
