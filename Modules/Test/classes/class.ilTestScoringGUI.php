ad12c7291ea76bd8de1adb51d5668bf98549962f
Stephan Kergomard <webmaster@kergomard.ch>
2023-04-26 16:22:53 +0200

    Test: Fix Correctiions For Org-Unit Permissions
    See: https://mantis.ilias.de/view.php?id=37040

--- Modules/Test/classes/class.ilTestScoringGUI.php
+++ Modules/Test/classes/class.ilTestScoringGUI.php
@@ -511,17 +511,17 @@ class ilTestScoringGUI extends ilTestServiceGUI
         if ($withData) {
             $participantStatusFilterValue = $table->getFilterItemByPostVar('participant_status')->getValue();
 
-            $participantList = new ilTestParticipantList($this->object);
+            $participant_list = new ilTestParticipantList($this->object);
 
-            $participantList->initializeFromDbRows(
+            $participant_list->initializeFromDbRows(
                 $this->object->getTestParticipantsForManualScoring($participantStatusFilterValue)
             );
 
-            $participantList = $participantList->getAccessFilteredList(
+            $filtered_participant_list = $participant_list->getAccessFilteredList(
                 ilTestParticipantAccessFilter::getScoreParticipantsUserFilter($this->ref_id)
             );
 
-            $table->setData($participantList->getParticipantsTableRows());
+            $table->setData($filtered_participant_list->getParticipantsTableRows());
         }
 
         return $table;
