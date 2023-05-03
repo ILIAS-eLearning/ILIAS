ff917307ee8484e251599e0fad52befd46c2ceeb
mjansen <mjansen@databay.de>
2023-05-03 13:20:41 +0200

    Init: Don't rely on `$_REQUEST`
    See: https://mantis.ilias.de/view.php?id=37244

--- Services/Init/classes/class.ilInitialisation.php
+++ Services/Init/classes/class.ilInitialisation.php
@@ -1591,16 +1591,7 @@ class ilInitialisation
 
     protected static function getCurrentCmd(): string
     {
-        $cmd = '';
-
-        $get = $_GET['cmd'] ?? '';
-        if (is_string($get) && $get !== '') {
-            $cmd = $get;
-        }
-
-        if ($cmd === '' || $cmd === 'post') {
-            $cmd = $_POST['cmd'] ?? [];
-        }
+        $cmd = $_POST['cmd'] ?? ($_GET['cmd'] ?? '');
 
         if (is_array($cmd)) {
             $cmd_keys = array_keys($cmd);
