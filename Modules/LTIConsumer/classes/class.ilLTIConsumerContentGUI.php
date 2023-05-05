8e6c9df9ff63e5fdb17280d53ea1d13fe9703f37
Uwe Kohnle <kohnle@internetlehrer-gmbh.de>
2023-05-05 12:23:59 +0000

    change lang_var launch lti

--- Modules/LTIConsumer/classes/class.ilLTIConsumerContentGUI.php
+++ Modules/LTIConsumer/classes/class.ilLTIConsumerContentGUI.php
@@ -190,9 +190,7 @@ class ilLTIConsumerContentGUI
         );
 
         $target = $this->object->getLaunchMethod() == "newWin" ? "_blank" : "_self";
-        $button = '<input class="btn btn-default ilPre" type="button" onClick="ltilaunch()" value = "' . $this->lng->txt("launch") . '" />';
-        $output = '';
-
+        $button = '<input class="btn btn-default ilPre" type="button" onClick="ltilaunch()" value = "' . $this->lng->txt("show_content") . '" />';
         $output = '<form id="lti_launch_form" name="lti_launch_form" action="' . $this->object->getProvider()->getProviderUrl() . '" method="post" target="' . $target . '" encType="application/x-www-form-urlencoded">';
         foreach ($launchParameters as $field => $value) {
             $output .= sprintf('<input type="hidden" name="%s" value="%s" />', $field, $value) . "\n";
@@ -237,8 +235,7 @@ class ilLTIConsumerContentGUI
         );
 
         $target = $this->object->getLaunchMethod() == "newWin" ? "_blank" : "_self";
-        $button = '<input class="btn btn-default ilPre" type="button" onClick="ltilaunch()" value = "' . $this->lng->txt("launch") . '" />';
-        $output = '';
+        $button = '<input class="btn btn-default ilPre" type="button" onClick="ltilaunch()" value = "' . $this->lng->txt("show_content") . '" />';
         $ltiMessageHint = (string) $this->object->getRefId() . ":" . CLIENT_ID;
         ilSession::set('lti_message_hint', $ltiMessageHint);
         $output = '<form id="lti_launch_form" name="lti_launch_form" action="' . $this->object->getProvider()->getInitiateLogin() . '" method="post" target="' . $target . '" encType="application/x-www-form-urlencoded">';
