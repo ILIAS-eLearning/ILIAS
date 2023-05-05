953bf34d9b52a104dec7ff0f1045830236271beb
Tim Schmitz <schmitz@leifos.de>
2023-05-05 16:30:29 +0200

    Calendar: add indices for R8 and above (31608)

---
+++ Services/Calendar/classes/Setup/class.ilCalendarDBUpdateSteps8.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
+/**
+ * @author  Tim Schmitz <schmitz@leifos.de>
+ */
+class ilCalendarDBUpdateSteps8 implements ilDatabaseUpdateSteps
+{
+    protected \ilDBInterface $db;
+
+    public function prepare(\ilDBInterface $db): void
+    {
+        $this->db = $db;
+    }
+
+    public function step_1(): void
+    {
+        // Add indices
+        if (!$this->db->indexExistsByFields('cal_entries', ['starta'])) {
+            $this->db->addIndex('cal_entries', ['starta'], 'i3');
+        }
+        if (!$this->db->indexExistsByFields('cal_entries', ['enda'])) {
+            $this->db->addIndex('cal_entries', ['enda'], 'i4');
+        }
+    }
+}
