8b984569f6262c727c8375e38b3fc512ceb265e3
Stephan Kergomard <webmaster@kergomard.ch>
2023-05-10 17:06:42 +0200

    Test: Remove Unused Functions

--- Modules/TestQuestionPool/classes/import/qti12/class.assQuestionImport.php
+++ Modules/TestQuestionPool/classes/import/qti12/class.assQuestionImport.php
@@ -302,26 +302,4 @@ class assQuestionImport
             ->withImportId($value);
         $repo->update($solution);
     }
-
-    protected function findSolutionTypeByValue(string $value): ?string
-    {
-        foreach (array_keys(assQuestionSuggestedSolution::TYPES) as $type) {
-            $search_type = '_' . $type . '_';
-            if (strpos($value, $search_type) !== false) {
-                return $type;
-            }
-        }
-        return null;
-    }
-
-
-    protected ?assQuestionSuggestedSolutionsDatabaseRepository $suggestedsolution_repo = null;
-    protected function getSuggestedSolutionsRepo(): assQuestionSuggestedSolutionsDatabaseRepository
-    {
-        if (is_null($this->suggestedsolution_repo)) {
-            $dic = ilQuestionPoolDIC::dic();
-            $this->suggestedsolution_repo = $dic['question.repo.suggestedsolutions'];
-        }
-        return $this->suggestedsolution_repo;
-    }
 }
