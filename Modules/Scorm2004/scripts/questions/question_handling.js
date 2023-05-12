f69dd0ceddef01986e0b0e4eda6d24dbaa1266f9
Fred Neumann <fred.neumann@fau.de>
2023-05-12 09:50:55 +0200

    Fix #36081: Antworten mit Umlauten für "Begriffe benennen"  (#5910)
    * Fix #36081: Antworten mit Umlauten für "Begriffe benennen" werden nicht akzeptiert
    * Fix #36081: Antworten mit Umlauten für "Begriffe benennen" werden nicht akzeptiert (Part 2)

--- Modules/Scorm2004/scripts/questions/question_handling.js
+++ Modules/Scorm2004/scripts/questions/question_handling.js
@@ -495,6 +495,11 @@ ilias.questions.assTextSubset = function(a_id) {
 		for (var c=0;c<questions[a_id].correct_answers.length;c++)
 		{
 			var correct_answer = questions[a_id].correct_answers[c]["answertext"];
+
+			// decode the HTML entity encoding of the correct answer
+			var parsed = new DOMParser().parseFromString(correct_answer, "text/html");
+			correct_answer =  parsed.documentElement.textContent;
+
 			if(questions[a_id].matching_method == "ci")
 			{
 				correct_answer = correct_answer.toLowerCase();
@@ -1219,6 +1224,11 @@ ilias.questions.showCorrectAnswers =function(a_id) {
 					{
 						correct_answer = correct_answer.toLowerCase();
 					}
+
+					// decode the HTML entity encoding of the correct answer
+					var parsed = new DOMParser().parseFromString(correct_answer, "text/html");
+					correct_answer =  parsed.documentElement.textContent;
+
 					if(correct_answer == answer)
 					{
 						found = true;
