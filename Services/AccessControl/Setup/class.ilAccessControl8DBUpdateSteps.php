04e258e505be530a8bca118336d2d21e5c11a61b
Stephan Kergomard <webmaster@kergomard.ch>
2023-05-14 18:02:18 +0200

    RBAC: Add Indeces
    See: https://mantis.ilias.de/view.php?id=31610

---
+++ Services/AccessControl/Setup/class.ilAccessControl8DBUpdateSteps.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+class ilAccessControl8DBUpdateSteps implements ilDatabaseUpdateSteps
+{
+    protected ilDBInterface $db;
+
+    public function prepare(ilDBInterface $db): void
+    {
+        $this->db = $db;
+    }
+
+    public function step_1(): void
+    {
+        if (!$this->db->indexExistsByFields('rbac_fa', ['assign', 'rol_id'])) {
+            $this->db->addIndex('rbac_fa', ['assign', 'rol_id'], 'i2');
+        }
+        if (!$this->db->indexExistsByFields('rbac_fa', ['assign', 'parent'])) {
+            $this->db->addIndex('rbac_fa', ['assign', 'parent'], 'i3');
+        }
+    }
+}
