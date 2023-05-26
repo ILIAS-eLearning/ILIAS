de872ef03bbdd02d5fc35bdb6e6df6a054f6af72
Fabian Helfer <fhelfer@nb-fhelfer.office.databay.de>
2023-05-26 11:18:19 +0200

    move updateTestPass earlier

--- Modules/Test/classes/class.ilTestEvaluationGUI.php
+++ Modules/Test/classes/class.ilTestEvaluationGUI.php
@@ -2160,8 +2160,20 @@ class ilTestEvaluationGUI extends ilTestServiceGUI
         $participantData->load($this->object->getTestId());
 
         if (in_array($activeId, $participantData->getActiveIds())) {
+            $testSession = new ilTestSession();
+            $testSession->loadFromDb($activeId);
+
+            assQuestion::_updateTestPassResults(
+                $activeId,
+                $testSession->getPass(),
+                $this->object->areObligationsEnabled(),
+                null,
+                $this->object->getId()
+            );
+
             $this->finishTestPass($activeId, $this->object->getId());
         }
+
 
         $this->redirectBackToParticipantsScreen();
     }
@@ -2191,8 +2203,20 @@ class ilTestEvaluationGUI extends ilTestServiceGUI
                 continue;
             }
 
+            $testSession = new ilTestSession();
+            $testSession->loadFromDb($participant->getActiveId());
+
+            assQuestion::_updateTestPassResults(
+                $participant->getActiveId(),
+                $testSession->getPass(),
+                $this->object->areObligationsEnabled(),
+                null,
+                $this->object->getId()
+            );
+
             $this->finishTestPass($participant->getActiveId(), $this->object->getId());
         }
+
 
         $this->redirectBackToParticipantsScreen();
     }
