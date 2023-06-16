353dd1c41b592248af8d1c69faf7b1790b2dd39a
Alexander Killing <killing@leifos.de>
2023-06-16 18:39:12 +0200

    37438: Import Content Page from ILIAS 7 with image link areas to internal link leads to crash

--- Services/COPage/classes/class.ilPageLinker.php
+++ Services/COPage/classes/class.ilPageLinker.php
@@ -99,7 +99,7 @@ class ilPageLinker implements \ILIAS\COPage\PageLinker
 
                 // anchor
                 $anc = $anc_add = "";
-                if ($int_link["Anchor"] != "") {
+                if (($int_link["Anchor"] ?? "") != "") {
                     $anc = $int_link["Anchor"];
                     $anc_add = "_" . rawurlencode($int_link["Anchor"]);
                 }
@@ -145,7 +145,7 @@ class ilPageLinker implements \ILIAS\COPage\PageLinker
 
                     case "WikiPage":
                         $wiki_anc = "";
-                        if ($int_link["Anchor"] != "") {
+                        if (($int_link["Anchor"] ?? "") != "") {
                             $wiki_anc = "#" . rawurlencode($int_link["Anchor"]);
                         }
                         $href = ilWikiPage::getGotoForWikiPageTarget($target_id) . $wiki_anc;
