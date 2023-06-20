678eaeab565f8d94a537cf78baa3ff894b4bf4ec
Alexander Killing <killing@leifos.de>
2023-06-20 11:18:56 +0200

    37458: Import a wiki

--- Modules/Wiki/libs/Sanitizer.php
+++ Modules/Wiki/libs/Sanitizer.php
@@ -328,6 +328,29 @@ $wgHtmlEntityAliases = array(
     'رلم' => 'rlm',
 );
 
+function codepointToUtf8($codepoint)
+{
+    if ($codepoint < 0x80) {
+        return chr($codepoint);
+    }
+    if ($codepoint < 0x800) {
+        return chr($codepoint >> 6 & 0x3f | 0xc0) .
+            chr($codepoint & 0x3f | 0x80);
+    }
+    if ($codepoint < 0x10000) {
+        return chr($codepoint >> 12 & 0x0f | 0xe0) .
+            chr($codepoint >> 6 & 0x3f | 0x80) .
+            chr($codepoint & 0x3f | 0x80);
+    }
+    if ($codepoint < 0x110000) {
+        return chr($codepoint >> 18 & 0x07 | 0xf0) .
+            chr($codepoint >> 12 & 0x3f | 0x80) .
+            chr($codepoint >> 6 & 0x3f | 0x80) .
+            chr($codepoint & 0x3f | 0x80);
+    }
+    return "";
+}
+
 
 /**
  * XHTML sanitizer for MediaWiki
