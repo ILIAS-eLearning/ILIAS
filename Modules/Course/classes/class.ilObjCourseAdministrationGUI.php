470460f6e65bf109b5246191a9e365a1dfb28175
Christoph Ludolf <ludolf@leifos.de>
2023-06-20 16:35:53 +0200

    mantis 32014

--- Modules/Course/classes/class.ilObjCourseAdministrationGUI.php
+++ Modules/Course/classes/class.ilObjCourseAdministrationGUI.php
@@ -15,6 +15,8 @@ include_once "./Services/Membership/classes/class.ilMembershipAdministrationGUI.
  */
 class ilObjCourseAdministrationGUI extends ilMembershipAdministrationGUI
 {
+    const SETTING_FAVOURITES_ENABLED = 'rep_favourites';
+
     protected function getType()
     {
         return "crss";
@@ -28,5 +30,36 @@ class ilObjCourseAdministrationGUI extends ilMembershipAdministrationGUI
     protected function getAdministrationFormId()
     {
         return ilAdministrationSettingsFormHandler::FORM_COURSE;
+    }
+
+    protected function addChildContentsTo($form)
+    {
+        $section = new ilFormSectionHeaderGUI();
+        $section->setTitle($this->lng->txt('crs_course_section'));
+        $form->addItem($section);
+
+        $checkBox = new ilCheckboxInputGUI($this->lng->txt('crs_favourites_enabled'), 'rep_favourites');
+        $checkBox->setInfo($this->lng->txt('crs_favourites_enabled_info'));
+        $checkBox->setChecked((bool) $this->ilSettings->get(self::SETTING_FAVOURITES_ENABLED, 0));
+        $form->addItem($checkBox);
+        return $form;
+    }
+
+    protected function saveChildSettings($form)
+    {
+        $this->ilSettings->set(self::SETTING_FAVOURITES_ENABLED, (int) $form->getInput(self::SETTING_FAVOURITES_ENABLED));
+    }
+
+    protected function getChildSettingsInfo($a_form_id)
+    {
+        switch ($a_form_id) {
+            case ilAdministrationSettingsFormHandler::FORM_GROUP:
+                $this->lng->loadLanguageModule("crs");
+                $fields = [
+                    'crs_favourites_enabled' => [ (bool) $this->ilSettings->get(self::SETTING_FAVOURITES_ENABLED, 0), ilAdministrationSettingsFormHandler::VALUE_BOOL ]
+                ];
+                return [ [ "editSettings", $fields ] ];
+        }
+        return [];
     }
 }
