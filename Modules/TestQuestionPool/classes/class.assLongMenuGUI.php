da4d4e0c08d15b9a495618a9d1527eda57475e3c
Stephan Kergomard <webmaster@kergomard.ch>
2023-06-21 17:58:54 +0200

    Test: Fix Form Validation in LongMenu-Question
    See: https://mantis.ilias.de/view.php?id=37514

--- Modules/TestQuestionPool/classes/class.assLongMenuGUI.php
+++ Modules/TestQuestionPool/classes/class.assLongMenuGUI.php
@@ -75,13 +75,15 @@ class assLongMenuGUI extends assQuestionGUI implements ilGuiQuestionScoringAdjus
         $form = $this->buildEditForm();
         $form->setValuesByPost();
         $check = $form->checkInput();
+        if (!$check) {
+            $this->editQuestion($form);
+            return 1;
+        }
         $this->writeQuestionGenericPostData();
         $this->writeQuestionSpecificPostData($form);
         $custom_check = $this->object->checkQuestionCustomPart($form);
-        if (!$check || !$custom_check) {
-            if (!$custom_check) {
-                $this->tpl->setOnScreenMessage('failure', $this->lng->txt("form_input_not_valid"));
-            }
+        if (!$custom_check) {
+            $this->tpl->setOnScreenMessage('failure', $this->lng->txt("form_input_not_valid"));
             $this->editQuestion($form);
             return 1;
         }
@@ -365,10 +367,10 @@ class assLongMenuGUI extends assQuestionGUI implements ilGuiQuestionScoringAdjus
 
     public function getTestOutput(
         $active_id,
-                        // hey: prevPassSolutions - will be always available from now on
-                           $pass,
-                        // hey.
-                           $is_postponed = false,
+        // hey: prevPassSolutions - will be always available from now on
+        $pass,
+        // hey.
+        $is_postponed = false,
         $use_post_solutions = false,
         $show_feedback = false
     ): string {
