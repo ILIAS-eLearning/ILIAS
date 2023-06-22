64ba55557c2fec4bf77aabcf8d00fde040eb9106
Alexander Killing <killing@leifos.de>
2023-06-22 15:44:41 +0200

    30691: Skipping between Learning Modules ILIAS inside Learning Sequence throws LogicException

--- Modules/LearningModule/Presentation/class.ilLMNavigationStatus.php
+++ Modules/LearningModule/Presentation/class.ilLMNavigationStatus.php
@@ -84,8 +84,14 @@ class ilLMNavigationStatus
 
         $this->chapter_has_no_active_page = false;
         $this->deactivated_page = false;
+
+        $requested_obj_id = $this->requested_obj_id;
+        if ($requested_obj_id > 0 && ilLMObject::_lookupContObjID($requested_obj_id) !== $this->lm->getId()) {
+            $requested_obj_id = 0;
+        }
+
         // determine object id
-        if ($this->requested_obj_id == 0) {
+        if ($requested_obj_id == 0) {
             $obj_id = $this->lm_tree->getRootId();
 
             if ($this->cmd == "resume") {
@@ -99,7 +105,7 @@ class ilLMNavigationStatus
                 }
             }
         } else {
-            $obj_id = $this->requested_obj_id;
+            $obj_id = $requested_obj_id;
             $active = ilLMPage::_lookupActive(
                 $obj_id,
                 $this->lm->getType(),
@@ -165,7 +171,7 @@ class ilLMNavigationStatus
             // check whether page found is within "clicked" chapter
             if ($this->lm_tree->isInTree($page_id)) {
                 $path = $this->lm_tree->getPathId($page_id);
-                if (!in_array($this->requested_obj_id, $path)) {
+                if (!in_array($requested_obj_id, $path)) {
                     $this->chapter_has_no_active_page = true;
                 }
             }
