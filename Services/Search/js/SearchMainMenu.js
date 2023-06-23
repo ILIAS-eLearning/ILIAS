80dffc4e6b0ebe9e5f7f2aba3af36a55c17bde03
Alexander Killing <killing@leifos.de>
2023-06-23 11:19:43 +0200

    11578: Context Sensitive Search - Suggestions from ILIAS

--- Services/Search/js/SearchMainMenu.js
+++ Services/Search/js/SearchMainMenu.js
@@ -1,51 +1,52 @@
 il.Util.addOnLoad(
-    function () {
+  () => {
+    const AC_DATASOURCE = 'ilias.php?baseClass=ilSearchControllerGUI&cmd=autoComplete';
 
-        var AC_DATASOURCE = 'ilias.php?baseClass=ilSearchControllerGUI&cmd=autoComplete';
+    // we must bind the blur event before the autocomplete item is added
+    document.getElementById('main_menu_search').addEventListener(
+      'blur',
+      (e) => { e.stopImmediatePropagation(); },
+    );
 
-        // we must bind the blur event before the autocomplete item is added
-        document.getElementById("main_menu_search").addEventListener(
-            "blur",
-            (e) => {e.stopImmediatePropagation();}
-        );
-
-        $("#main_menu_search").autocomplete({
-            source: AC_DATASOURCE + "&search_type=4",
-            appendTo: "#mm_search_menu_ac",
-            open: function (event, ui) {
-                $(".ui-autocomplete").position({
-                    my: "left top",
-                    at: "left top",
-                    of: $("#mm_search_menu_ac")
-                })
-            },
-            minLength: 3
+    $('#main_menu_search').autocomplete({
+      source: `${AC_DATASOURCE}&search_type=4`,
+      appendTo: '#mm_search_menu_ac',
+      open(event, ui) {
+        $('.ui-autocomplete').position({
+          my: 'left top',
+          at: 'left top',
+          of: $('#mm_search_menu_ac'),
         });
+      },
+      minLength: 3,
+    });
 
-        $("#ilMMSearchMenu input[type='radio']").change(function () {
-            /* close current search */
-            $("#main_menu_search").autocomplete("close");
-            $("#main_menu_search").autocomplete("enable");
+    $("#ilMMSearchMenu input[type='radio']").change(() => {
+      /* close current search */
+      $('#main_menu_search').autocomplete('close');
+      $('#main_menu_search').autocomplete('enable');
 
-            /* append search type */
+      /* append search type */
 
-            var orig_datasource = AC_DATASOURCE;
-            var checked_input = $('input[name=root_id]:checked', '#mm_search_form');
-            var type_val = checked_input.val();
+      const orig_datasource = AC_DATASOURCE;
+      const checked_input = $('input[name=root_id]:checked', '#mm_search_form');
+      const type_val = checked_input.val();
 
-            /* disable autocomplete for search at current position */
-            if (checked_input[0].id === 'ilmmsc') {
-                $("#main_menu_search").autocomplete("disable");
-                return;
-            }
+      /* disable autocomplete for search at current position */
+      if (checked_input[0].id === 'ilmmsc') {
+        $('#main_menu_search').autocomplete('disable');
+        return;
+      }
 
-            $("#main_menu_search").autocomplete("option",
-                {
-                    source: orig_datasource + "&search_type=" + type_val
-                });
+      $('#main_menu_search').autocomplete(
+        'option',
+        {
+          source: `${orig_datasource}&search_type=${type_val}`,
+        },
+      );
 
-            /* start new search */
-            $("#main_menu_search").autocomplete("search");
-        });
-    }
+      /* start new search */
+      $('#main_menu_search').autocomplete('search');
+    });
+  },
 );
