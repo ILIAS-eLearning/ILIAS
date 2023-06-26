fa629ec61e852eeba402251654b2a32ac5c9e37e
Christoph Ludolf <ludolf@leifos.de>
2023-06-26 09:46:33 +0200

    moved copyright to the beginning of the files

--- Modules/Course/classes/class.ilObjCourseAdministrationGUI.php
+++ Modules/Course/classes/class.ilObjCourseAdministrationGUI.php
@@ -1,6 +1,4 @@
 <?php
-
-declare(strict_types=0);
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -16,6 +14,8 @@ declare(strict_types=0);
  * https://github.com/ILIAS-eLearning
  *
  *********************************************************************/
+
+declare(strict_types=0);
 
 /**
  * Course Administration Settings
--- Services/Membership/classes/class.ilMembershipAdministrationGUI.php
+++ Services/Membership/classes/class.ilMembershipAdministrationGUI.php
@@ -1,8 +1,4 @@
 <?php
-
-declare(strict_types=1);
-
-
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -18,6 +14,8 @@ declare(strict_types=1);
  * https://github.com/ILIAS-eLearning
  *
  *********************************************************************/
+
+declare(strict_types=1);
 
 /**
  * Membership Administration Settings
