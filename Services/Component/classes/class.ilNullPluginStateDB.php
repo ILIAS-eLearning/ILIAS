a86bbbf36d098d235b081f9c7cec96ac34f71d59
Richard Klees <richard.klees@concepts-and-training.de>
2023-06-29 11:25:09 +0200

    Component: add component repository for build phase

--- Services/Component/classes/class.ilNullPluginStateDB.php
+++ Services/Component/classes/class.ilNullPluginStateDB.php
@@ -23,12 +23,32 @@ use ILIAS\Data\Version;
 /**
  * Repository interface for plugin state data.
  */
-interface ilPluginStateDB
+class ilNullPluginStateDB implements ilPluginStateDB
 {
-    public function isPluginActivated(string $id): bool;
-    public function setActivation(string $id, bool $activated): void;
-    public function getCurrentPluginVersion(string $id): ?Version;
-    public function getCurrentPluginDBVersion(string $id): ?int;
-    public function setCurrentPluginVersion(string $id, Version $version, int $db_version): void;
-    public function remove(string $id): void;
+    public function isPluginActivated(string $id): bool
+    {
+        return false;
+    }
+
+    public function setActivation(string $id, bool $activated): void
+    {
+    }
+
+    public function getCurrentPluginVersion(string $id): ?Version
+    {
+        return null;
+    }
+
+    public function getCurrentPluginDBVersion(string $id): ?int
+    {
+        return null;
+    }
+
+    public function setCurrentPluginVersion(string $id, Version $version, int $db_version): void
+    {
+    }
+
+    public function remove(string $id): void
+    {
+    }
 }
