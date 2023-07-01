1a602557540e05b11ae9fed5edfa1bc076367bcd
Alexander Killing <killing@leifos.de>
2023-07-01 21:55:30 +0200

    23732: Default Text-Templates and Exercise-Reminders

--- Modules/Exercise/Assignment/class.ilExAssignmentEditorGUI.php
+++ Modules/Exercise/Assignment/class.ilExAssignmentEditorGUI.php
@@ -552,6 +552,7 @@ class ilExAssignmentEditorGUI
         $r_group = new ilRadioGroupInputGUI($this->lng->txt("exc_reminder_mail_template"), $post_var);
         $r_group->setRequired(true);
         $r_group->addOption(new ilRadioOption($this->lng->txt("exc_reminder_mail_no_tpl"), 0));
+        $r_group->setValue(0);
 
         switch ($a_reminder_type) {
             case ilExAssignmentReminder::SUBMIT_REMINDER:
@@ -571,6 +572,9 @@ class ilExAssignmentEditorGUI
         $templateService = $DIC['mail.texttemplates.service'];
         foreach ($templateService->loadTemplatesForContextId($context->getId()) as $template) {
             $r_group->addOption(new ilRadioOption($template->getTitle(), $template->getTplId()));
+            if ($template->isDefault()) {
+                $r_group->setValue($template->getTplId());
+            }
         }
 
         return $r_group;
