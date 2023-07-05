f1e2b578e8a7894372b7342731e9ec01354d395c
Alexander Killing <killing@leifos.de>
2023-07-05 13:14:42 +0200

    35822: Surveys do not show up in Main Bar > Last Visited

--- Modules/Survey/classes/class.ilObjSurveyGUI.php
+++ Modules/Survey/classes/class.ilObjSurveyGUI.php
@@ -125,7 +125,7 @@ class ilObjSurveyGUI extends ilObjectGUI implements ilCtrlBaseClassInterface
         $ilTabs = $this->tabs;
         $access_manager = $this->access_manager;
         $survey = $this->survey;
-        if ($survey && !$access_manager->canAccessInfoScreen()) {
+        if ($survey) {
             if (!$access_manager->canAccessInfoScreen()) {
                 $this->noPermission();
             }
@@ -309,7 +309,7 @@ class ilObjSurveyGUI extends ilObjectGUI implements ilCtrlBaseClassInterface
         if (!$external_rater && !$this->getCreationMode() &&
             $this->checkPermissionBool("read")) {
             $this->ctrl->setParameterByClass("ilobjsurveygui", "ref_id", $this->ref_id);
-            $link = $this->ctrl->getLinkTargetByClass("ilobjsurveygui", "");
+            $link = $this->ctrl->getLinkTargetByClass("ilobjsurveygui", "infoScreen");
             $this->nav_history->addItem($this->ref_id, $link, "svy");
         }
     }
