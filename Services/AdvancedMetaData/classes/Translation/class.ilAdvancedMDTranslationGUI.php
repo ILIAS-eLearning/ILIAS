0f3428eaec2de3455b1ad0dde687b83370baa879
Christoph Ludolf <ludolf@leifos.de>
2023-07-07 14:51:36 +0200

    LUI, replaces ilLinkButtons of Services Calendar, Search, AdvancedMetadata

--- Services/AdvancedMetaData/classes/Translation/class.ilAdvancedMDTranslationGUI.php
+++ Services/AdvancedMetaData/classes/Translation/class.ilAdvancedMDTranslationGUI.php
@@ -1,5 +1,21 @@
 <?php
 
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
 declare(strict_types=1);
 
 use Psr\Http\Message\RequestInterface;
@@ -101,19 +117,6 @@ abstract class ilAdvancedMDTranslationGUI
             $this->language->txt('cancel')
         );
         return $form;
-    }
-
-    protected function addToolbarLanguageCreation(): void
-    {
-        $button = ilLinkButton::getInstance();
-        $button->setCaption('obj_add_languages');
-        $button->setUrl(
-            $this->ctrl->getLinkTargetByClass(
-                strtolower(get_class($this)),
-                self::CMD_ADD_TRANSLATION
-            )
-        );
-        $this->toolbar->addButtonInstance($button);
     }
 
     /**
