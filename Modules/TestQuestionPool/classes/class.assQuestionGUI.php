bc2d62f11763a6ce96b04aa044c4d297e557620c
Stephan Kergomard <webmaster@kergomard.ch>
2023-07-12 15:54:55 +0200

    Test: Fix Accordions in Feedback
    See: https://mantis.ilias.de/view.php?id=35817

--- Modules/TestQuestionPool/classes/class.assQuestionGUI.php
+++ Modules/TestQuestionPool/classes/class.assQuestionGUI.php
@@ -1122,6 +1122,7 @@ abstract class assQuestionGUI
         if (strlen($manual_feedback)) {
             return $manual_feedback;
         }
+
         $correct_feedback = $this->object->feedbackOBJ->getGenericFeedbackTestPresentation($this->object->getId(), true);
         $incorrect_feedback = $this->object->feedbackOBJ->getGenericFeedbackTestPresentation($this->object->getId(), false);
         if (strlen($correct_feedback . $incorrect_feedback)) {
@@ -1132,6 +1133,10 @@ abstract class assQuestionGUI
             } else {
                 $output = $incorrect_feedback;
             }
+        }
+
+        if ($this->object->isAdditionalContentEditingModePageObject()) {
+            return $output;
         }
         return $this->object->prepareTextareaOutput($output, true);
     }
