d815a72799685483ff77f8b2cc287a4c5149232a
Denis Strassner <denis.strassner@uni-hohenheim.de>
2023-07-27 14:34:33 +0200

    TA Bug 37663
    Change in PR 4610 seems to be the cause for the error in https://mantis.ilias.de/view.php?id=37663

--- Modules/Test/classes/class.ilTestExportGUI.php
+++ Modules/Test/classes/class.ilTestExportGUI.php
@@ -219,7 +219,7 @@ class ilTestExportGUI extends ilExportGUI
                 $data[] = [
                     'file' => $exp_file,
                     'size' => filesize($archive_dir . "/" . $exp_file),
-                    'timestamp' => $file_arr[0],
+                    'timestamp' => $file_arr[4],
                     'type' => $this->getExportTypeFromFileName($exp_file)
                 ];
             }
