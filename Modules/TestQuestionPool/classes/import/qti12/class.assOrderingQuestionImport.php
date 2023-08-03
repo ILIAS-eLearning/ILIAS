93713b738f3ea94c8358716df80833fe38706951
Stephan Kergomard <webmaster@kergomard.ch>
2023-08-03 12:19:36 +0100

    Test: Fix Import of Ordering Question
    See: https://mantis.ilias.de/view.php?id=37716

--- Modules/TestQuestionPool/classes/import/qti12/class.assOrderingQuestionImport.php
+++ Modules/TestQuestionPool/classes/import/qti12/class.assOrderingQuestionImport.php
@@ -324,7 +324,7 @@ class assOrderingQuestionImport extends assQuestionImport
             return $import_mapping;
         }
 
-        if (isset($tst_id)) {
+        if ($tst_id > 0) {
             $tst_object->questions[$question_counter++] = $this->object->getId();
             $import_mapping[$item->getIdent()] = ["pool" => 0, "test" => $this->object->getId()];
             return $import_mapping;
