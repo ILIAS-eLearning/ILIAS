5806d182317b681cde14755cef3c5e7be94593ec
Stephan Kergomard <webmaster@kergomard.ch>
2023-08-16 07:53:34 +0100

    Test: Tweeking a Language Variable

--- Modules/Test/classes/class.ilTestExportGUI.php
+++ Modules/Test/classes/class.ilTestExportGUI.php
@@ -238,7 +238,7 @@ class ilTestExportGUI extends ilExportGUI
         }
 
         $table->setData($data);
-        $this->tpl->setOnScreenMessage('info', $lng->txt('exp_limitations_info'), true);
+        $this->tpl->setOnScreenMessage('info', $lng->txt('no_manual_feedback_export_info'), true);
         $tpl->setContent($table->getHTML());
     }
 
