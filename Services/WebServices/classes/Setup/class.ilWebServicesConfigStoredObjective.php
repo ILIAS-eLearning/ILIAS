fc816995e41190907fe53d5e4686b1c86abd44ae
jeph864 <jephte.abijuru@minervis.com>
2023-08-22 09:27:04 +0200

    enable soap response timeout in  setup config

--- Services/WebServices/classes/Setup/class.ilWebServicesConfigStoredObjective.php
+++ Services/WebServices/classes/Setup/class.ilWebServicesConfigStoredObjective.php
@@ -53,6 +53,7 @@ class ilWebServicesConfigStoredObjective implements Setup\Objective
         );
         $settings->set("soap_wsdl_path", $this->config->getSOAPWsdlPath());
         $settings->set("soap_connect_timeout", (int) $this->config->getSOAPConnectTimeout());
+        $settings->set("soap_response_timeout", (int) $this->config->getSoapResponseTimeout());
         $settings->set("rpc_server_host", $this->config->getRPCServerHost());
         $settings->set("rpc_server_port", $this->config->getRPCServerPort());
 
