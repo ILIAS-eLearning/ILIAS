66b0244c012d1c8b91e1c659bda6fc3f0388fcbb
Jephte Abijuru <57417057+jeph864@users.noreply.github.com>
2023-08-22 10:47:37 +0200

    fix type
    Co-authored-by: Michael Jansen <mjansen@databay.de>

--- Services/WebServices/classes/Setup/class.ilWebServicesConfigStoredObjective.php
+++ Services/WebServices/classes/Setup/class.ilWebServicesConfigStoredObjective.php
@@ -53,7 +53,7 @@ class ilWebServicesConfigStoredObjective implements Setup\Objective
         );
         $settings->set("soap_wsdl_path", $this->config->getSOAPWsdlPath());
         $settings->set("soap_connect_timeout", (string) $this->config->getSOAPConnectTimeout());
-        $settings->set("soap_response_timeout", (int) $this->config->getSoapResponseTimeout());
+        $settings->set("soap_response_timeout", (string) $this->config->getSoapResponseTimeout());
         $settings->set("rpc_server_host", $this->config->getRPCServerHost());
         $settings->set("rpc_server_port", $this->config->getRPCServerPort());
 
