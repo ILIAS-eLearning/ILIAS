2af422bc6c244554d5e40623f84d6859e85e8760
Stefan Meyer <meyer@leifos.de>
2023-08-24 17:52:40 +0200

    Merge pull request #6266 from srsolutionsag/fix/0037785/8/wrong-quote-handling-in-search
    [FIX] 0037785: Search does not show the correct suggestions

--- Services/Search/classes/class.ilQueryParser.php
+++ Services/Search/classes/class.ilQueryParser.php
@@ -1,25 +1,24 @@
 <?php
-/*
-    +-----------------------------------------------------------------------------+
-    | ILIAS open source                                                           |
-    +-----------------------------------------------------------------------------+
-    | Copyright (c) 1998-2001 ILIAS open source, University of Cologne            |
-    |                                                                             |
-    | This program is free software; you can redistribute it and/or               |
-    | modify it under the terms of the GNU General Public License                 |
-    | as published by the Free Software Foundation; either version 2              |
-    | of the License, or (at your option) any later version.                      |
-    |                                                                             |
-    | This program is distributed in the hope that it will be useful,             |
-    | but WITHOUT ANY WARRANTY; without even the implied warranty of              |
-    | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
-    | GNU General Public License for more details.                                |
-    |                                                                             |
-    | You should have received a copy of the GNU General Public License           |
-    | along with this program; if not, write to the Free Software                 |
-    | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
-    +-----------------------------------------------------------------------------+
-*/
+
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
+
+define('QP_COMBINATION_AND', 'and');
+define('QP_COMBINATION_OR', 'or');
 
 /**
 * Class ilQueryParser
@@ -32,9 +31,6 @@
 * @package ilias-search
 *
 */
-define('QP_COMBINATION_AND', 'and');
-define('QP_COMBINATION_OR', 'or');
-
 class ilQueryParser
 {
     /**
@@ -164,10 +160,11 @@ class ilQueryParser
             return $this->quoted_words ? $this->quoted_words : array();
         } else {
             foreach ($this->quoted_words as $word) {
-                $tmp_word[] = str_replace('\"', '', $word);
+                $tmp_word[] = str_replace("\"", '', $word);
             }
             return $tmp_word ? $tmp_word : array();
         }
+        return $tmp_word ?? [];
     }
 
     public function getLuceneQueryString()
