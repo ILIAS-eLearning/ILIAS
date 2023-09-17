f9511e60054e7bca3da90de094b0130bf1daced4
Alexander Killing <killing@leifos.de>
2023-09-17 08:50:58 +0200

    fixed file preservation

--- Services/Form/classes/class.ilPropertyFormGUI.php
+++ Services/Form/classes/class.ilPropertyFormGUI.php
@@ -80,13 +80,13 @@ class ilPropertyFormGUI extends ilFormGUI
         $this->setPreventDoubleSubmission(true);
 
         // do it as early as possible
-        $this->rebuildUploadedFiles();
         if (isset($DIC["http"])) {
             $this->http = $DIC->http();
         }
         if (isset($DIC["refinery"])) {
             $this->refinery = $DIC->refinery();
         }
+        $this->rebuildUploadedFiles();
         if (isset($DIC["tpl"])) {      // some unit tests will fail otherwise
             $this->global_tpl = $DIC['tpl'];
         }
@@ -913,7 +913,7 @@ class ilPropertyFormGUI extends ilFormGUI
                     "type" => $_FILES[$a_field]["type"][$a_index][$a_sub_index],
                     "error" => $_FILES[$a_field]["error"][$a_index][$a_sub_index],
                     "size" => $_FILES[$a_field]["size"][$a_index][$a_sub_index],
-                    "is_upload" => true
+                    "is_upload" => $_FILES[$a_field]["is_upload"][$a_index][$a_sub_index] ?? true
                 );
             }
         } elseif ($a_sub_index) {
@@ -924,7 +924,7 @@ class ilPropertyFormGUI extends ilFormGUI
                     "type" => $_FILES[$a_field]["type"][$a_index],
                     "error" => $_FILES[$a_field]["error"][$a_index],
                     "size" => $_FILES[$a_field]["size"][$a_index],
-                    "is_upload" => true
+                    "is_upload" => $_FILES[$a_field]["is_upload"][$a_index] ?? true
                 );
             }
         } else {
@@ -935,7 +935,7 @@ class ilPropertyFormGUI extends ilFormGUI
                     "type" => $_FILES[$a_field]["type"],
                     "error" => $_FILES[$a_field]["error"],
                     "size" => $_FILES[$a_field]["size"],
-                    "is_upload" => true
+                    "is_upload" => $_FILES[$a_field]["is_upload"] ?? true
                 );
             }
         }
@@ -1020,6 +1020,7 @@ class ilPropertyFormGUI extends ilFormGUI
                                 $_FILES[$field]["type"][$idx][$idx2] = $type;
                                 $_FILES[$field]["error"][$idx][$idx2] = 0;
                                 $_FILES[$field]["size"][$idx][$idx2] = filesize($full_file);
+                                $_FILES[$field]["is_upload"][$idx][$idx2] = false;
                             }
                         } elseif ($idx != "") {
                             if (!$_FILES[$field]["tmp_name"][$idx]) {
@@ -1028,6 +1029,7 @@ class ilPropertyFormGUI extends ilFormGUI
                                 $_FILES[$field]["type"][$idx] = $type;
                                 $_FILES[$field]["error"][$idx] = 0;
                                 $_FILES[$field]["size"][$idx] = filesize($full_file);
+                                $_FILES[$field]["is_upload"][$idx] = false;
                             }
                         } else {
                             if (!$_FILES[$field]["tmp_name"]) {
@@ -1036,6 +1038,7 @@ class ilPropertyFormGUI extends ilFormGUI
                                 $_FILES[$field]["type"] = $type;
                                 $_FILES[$field]["error"] = 0;
                                 $_FILES[$field]["size"] = filesize($full_file);
+                                $_FILES[$field]["is_upload"] = false;
                             }
                         }
                     }
