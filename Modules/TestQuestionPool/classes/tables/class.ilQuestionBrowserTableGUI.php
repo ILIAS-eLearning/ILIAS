d3b8f8c81d047250972f00e2d3cc1e91a09f3dcb
Timon Amstutz <timon.amstutz@bluewin.ch>
2023-09-23 18:09:38 +0200

    QP Fix wrong icon for comments and nr not updating, see: #37045

--- Modules/TestQuestionPool/classes/tables/class.ilQuestionBrowserTableGUI.php
+++ Modules/TestQuestionPool/classes/tables/class.ilQuestionBrowserTableGUI.php
@@ -550,16 +550,20 @@ class ilQuestionBrowserTableGUI extends ilTable2GUI
             return '';
         }
 
-        $ajaxLink = $this->getCommentsAjaxLink($qData['question_id']);
+        $ajax_link = $this->getCommentsAjaxLink($qData['question_id']);
 
-        return "<a class='comment' href='#' onclick=\"return " . $ajaxLink . "\">
-                        <img src='" . ilUtil::getImagePath("comment_unlabeled.svg")
-            . "' alt='{$qData['comments']}'><span class='ilHActProp'>{$qData['comments']}</span></a>";
+        $comment_glyph = $this->ui_factory->symbol()->glyph()->comment()->withCounter(
+            $this->ui_factory->counter()->status($qData['comments'])
+        )->withAdditionalOnLoadCode(function ($id) use ($ajax_link): string {
+            return "document.getElementById('$id').onclick = function (event) { $ajax_link; };";
+        });
+        return $this->renderer->render($comment_glyph);
     }
 
     protected function getCommentsAjaxLink($questionId): string
     {
         $ajax_hash = ilCommonActionDispatcherGUI::buildAjaxHash(1, $this->request->getRefId(), 'quest', $this->parent_obj->object->getId(), 'quest', $questionId);
-        return ilNoteGUI::getListCommentsJSCall($ajax_hash, '');
+        $update_code = "il.UI.counter.getCounterObject($(\".ilTableOuter\")).incrementStatusCount(1);";
+        return ilNoteGUI::getListCommentsJSCall($ajax_hash, $update_code);
     }
 }
