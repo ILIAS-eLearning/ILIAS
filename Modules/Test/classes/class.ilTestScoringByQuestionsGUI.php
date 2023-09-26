109974f8b1c422ebde2dd2dc653ac4538d923f0f
Stephan Kergomard <webmaster@kergomard.ch>
2023-09-26 14:45:23 +0200

    Test: Fix Feedback Changable When Finalized

--- Modules/Test/classes/class.ilTestScoringByQuestionsGUI.php
+++ Modules/Test/classes/class.ilTestScoringByQuestionsGUI.php
@@ -215,7 +215,6 @@ class ilTestScoringByQuestionsGUI extends ilTestScoringGUI
                 continue;
             }
 
-
             $update_participant = false;
             $qst_id = null;
 
@@ -226,6 +225,21 @@ class ilTestScoringByQuestionsGUI extends ilTestScoringGUI
                 if (!isset($manPointsPost[$pass][$active_id])) {
                     $manPointsPost[$pass][$active_id] = [];
                 }
+
+                $feedback_text = $this->retrieveFeedback($active_id, $qst_id, $pass);
+
+                /**
+                 * 26.09.23 sk: Ok, this is a hack, but to do this right we need
+                 * to go through the whole flow here. It is very convoluted and
+                 * I'm unsure what would happen if I would really change something.
+                 * This feature is in urgent need of refactoring and a repo.
+                 */
+                $current_feedback_info = ilObjTest::getSingleManualFeedback($active_id, $qst_id, $pass);
+                if ($current_feedback_info['finalized_evaluation'] === 1) {
+                    $reached_points = assQuestion::_getReachedPoints($active_id, $qst_id, $pass);
+                    $feedback_text = $current_feedback_info['feedback'];
+                }
+
                 $maxPointsByQuestionId[$qst_id] = $this->questioninfo->getMaximumPoints($qst_id);
                 $manPointsPost[$pass][$active_id][$qst_id] = (float) $reached_points;
                 if ($reached_points > $maxPointsByQuestionId[$qst_id]) {
@@ -234,7 +248,7 @@ class ilTestScoringByQuestionsGUI extends ilTestScoringGUI
                     return;
                 }
 
-                $this->saveFeedback($active_id, $qst_id, $pass, $ajax);
+                $this->saveFinalization($active_id, $qst_id, $pass, $feedback_text, $ajax);
                 // fix #35543: save manual points only if they differ from the existing points
                 // this prevents a question being set to "answered" if only feedback is entered
                 $old_points = assQuestion::_getReachedPoints($active_id, $qst_id, $pass);
@@ -571,12 +585,6 @@ class ilTestScoringByQuestionsGUI extends ilTestScoringGUI
         );
     }
 
-    protected function saveFeedback(int $active_id, int $qst_id, int $pass, bool $is_single_feedback): void
-    {
-        $feedback = $this->retrieveFeedback($active_id, $qst_id, $pass);
-        $this->saveFinalization($active_id, $qst_id, $pass, $feedback, $is_single_feedback);
-    }
-
     protected function retrieveFeedback(int $active_id, int $qst_id, int $pass): ?string
     {
         $feedback = $this->testrequest->raw('feedback');
@@ -602,9 +610,9 @@ class ilTestScoringByQuestionsGUI extends ilTestScoringGUI
         ?string $feedback,
         bool $is_single_feedback
     ): void {
-        $evaluated = false;
+        $finalized = false;
         if ($this->doesValueExistsInPostArray('evaluated', $active_id, $qst_id, $pass)) {
-            $evaluated = (bool) ($_POST['evaluated'][$pass][$active_id][$qst_id] ?? false);
+            $finalized = (bool) ($_POST['evaluated'][$pass][$active_id][$qst_id] ?? false);
         }
 
         $this->object->saveManualFeedback(
@@ -612,7 +620,7 @@ class ilTestScoringByQuestionsGUI extends ilTestScoringGUI
             $qst_id,
             $pass,
             $feedback,
-            $evaluated,
+            $finalized,
             $is_single_feedback
         );
     }
