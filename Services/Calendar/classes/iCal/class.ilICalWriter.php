aa2da4613651c8a9ccafd80cefcfa775555bd7cd
Stefan Meyer <meyer@leifos.de>
2023-10-05 18:22:03 +0200

    Merge pull request #6353 from chlulei/7_mantis_31342
    Introduces calendar appointment export limit (1mb)

--- Services/Calendar/classes/iCal/class.ilICalWriter.php
+++ Services/Calendar/classes/iCal/class.ilICalWriter.php
@@ -1,28 +1,22 @@
 <?php
 
-declare(strict_types=1);
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
 
-/*
-    +-----------------------------------------------------------------------------+
-    | ILIAS open source                                                           |
-    +-----------------------------------------------------------------------------+
-    | Copyright (c) 1998-2001 ILIAS open source, University of Cologne            |
-    |                                                                             |
-    | This program is free software; you can redistribute it and/or               |
-    | modify it under the terms of the GNU General Public License                 |
-    | as published by the Free Software Foundation; either version 2              |
-    | of the License, or (at your option) any later version.                      |
-    |                                                                             |
-    | This program is distributed in the hope that it will be useful,             |
-    | but WITHOUT ANY WARRANTY; without even the implied warranty of              |
-    | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
-    | GNU General Public License for more details.                                |
-    |                                                                             |
-    | You should have received a copy of the GNU General Public License           |
-    | along with this program; if not, write to the Free Software                 |
-    | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
-    +-----------------------------------------------------------------------------+
-*/
+declare(strict_types=1);
 
 class ilICalWriter
 {
@@ -31,12 +25,17 @@ class ilICalWriter
     // minus one to fix multi line breaks.
     protected const LINE_SIZE = 74;
     protected const BEGIN_LINE_WHITESPACE = ' ';
+    protected const EMPTY = '';
 
-    private string $ical = '';
+
+    /**
+     * @var string[]
+     */
+    protected array $lines;
 
     public function __construct()
     {
-        $this->ical = '';
+        $this->lines = [];
     }
 
     public static function escapeText(string $a_text): string
@@ -61,7 +60,7 @@ class ilICalWriter
     public function addLine(string $a_line): void
     {
         // use multibyte split
-        $chunks = array();
+        $chunks = [];
         $len = ilStr::strLen($a_line);
         while ($len) {
             $chunks[] = ilStr::subStr($a_line, 0, self::LINE_SIZE);
@@ -70,20 +69,30 @@ class ilICalWriter
         }
 
         for ($i = 0; $i < count($chunks); $i++) {
-            $this->ical .= $chunks[$i];
-            if (isset($chunks[$i + 1])) {
-                $this->ical .= self::LINEBREAK;
-                $this->ical .= self::BEGIN_LINE_WHITESPACE;
-            }
+            $line = ($i > 0) ? self::BEGIN_LINE_WHITESPACE : self::EMPTY;
+            $line .= $chunks[$i];
+            $line .= (isset($chunks[$i + 1]) || ($i + 1) === count($chunks)) ? self::LINEBREAK : self::EMPTY;
+            $this->lines[] = $line;
         }
-        $this->ical .= self::LINEBREAK;
     }
 
-    /**
-     * Return ical string
-     */
-    public function __toString()
+    public function byteCount(): int
     {
-        return $this->ical;
+        return strlen($this->__toString());
+    }
+
+    public function clear(): void
+    {
+        $this->lines = [];
+    }
+
+    public function append(ilICalWriter $other): void
+    {
+        $this->lines = array_merge($this->lines, $other->lines);
+    }
+
+    public function __toString(): string
+    {
+        return implode('', $this->lines);
     }
 }
