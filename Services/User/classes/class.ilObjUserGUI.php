69eefab7c2eccce76d4f6d53f0b5068e7853e42b
Stephan Kergomard <webmaster@kergomard.ch>
2023-10-06 09:56:16 +0200

    User: Fix Rounding of Access Limits in Form
    See: https://mantis.ilias.de/view.php?id=29637

--- Services/User/classes/class.ilObjUserGUI.php
+++ Services/User/classes/class.ilObjUserGUI.php
@@ -1095,11 +1095,13 @@ class ilObjUserGUI extends ilObjectGUI
         $acfrom = new ilDateTimeInputGUI($this->lng->txt('crs_from'), 'time_limit_from');
         $acfrom->setRequired(true);
         $acfrom->setShowTime(true);
+        $acfrom->setMinuteStepSize(1);
         $op2->addSubItem($acfrom);
 
         $acto = new ilDateTimeInputGUI($this->lng->txt('crs_to'), 'time_limit_until');
         $acto->setRequired(true);
         $acto->setShowTime(true);
+        $acto->setMinuteStepSize(1);
         $op2->addSubItem($acto);
 
         $this->form_gui->addItem($radg);
