2cb0c374f8a8f09ebac22bd4c791b6fc026c257d
Thomas Famula <famula@leifos.de>
2023-10-06 12:26:58 +0200

    37966: Competence References should be deleted when a Competence Template is deleted, pt.2

--- Services/Skill/Node/class.ilSkillTemplateCategory.php
+++ Services/Skill/Node/class.ilSkillTemplateCategory.php
@@ -66,6 +66,18 @@ class ilSkillTemplateCategory extends ilSkillTreeNode
             }
         }
 
+        foreach (\ilSkillTemplateReference::_lookupTrefIdsForTemplateId($sctp_id) as $tref_id) {
+            $obj = ilSkillTreeNodeFactory::getInstance($tref_id);
+            $skill_tree = $this->skill_service->internal()->repo()->getTreeRepo()->getTreeForNodeId($tref_id);
+            $node_data = $skill_tree->getNodeData($tref_id);
+            if (is_object($obj)) {
+                $obj->delete();
+            }
+            if ($skill_tree->isInTree($tref_id)) {
+                $skill_tree->deleteTree($node_data);
+            }
+        }
+
         $ilDB->manipulate(
             "DELETE FROM skl_templ_ref WHERE "
             . " templ_id = " . $ilDB->quote($this->getId(), "integer")
