cbfef9b5849f7e4c6ddeec11e0a5e577347e1607
Alexander Killing <killing@leifos.de>
2023-10-12 15:39:07 +0200

    38159: Role Assignment in Local User Administration can not be started

--- Modules/Category/classes/class.ilObjCategoryGUI.php
+++ Modules/Category/classes/class.ilObjCategoryGUI.php
@@ -1132,7 +1132,7 @@ class ilObjCategoryGUI extends ilContainerGUI implements \ILIAS\Taxonomy\Setting
             $f_result[$counter]['checkbox'] = ilLegacyFormElementsUtil::formCheckbox(
                 in_array((int) $role['obj_id'], $ass_roles, true),
                 'role_ids[]',
-                $role['obj_id'],
+                (string) $role['obj_id'],
                 $disabled
             );
             $f_result[$counter]['title'] = $role_obj->getTitle() ?: "";
