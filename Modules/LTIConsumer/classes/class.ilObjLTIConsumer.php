aa9e4d0744d92f773d1db18beb2dc34b980d5508
Uwe Kohnle <kohnle@internetlehrer-gmbh.de>
2023-10-15 20:41:20 +0000

    note for Mantis 35435

--- Modules/LTIConsumer/classes/class.ilObjLTIConsumer.php
+++ Modules/LTIConsumer/classes/class.ilObjLTIConsumer.php
@@ -658,6 +658,9 @@ class ilObjLTIConsumer extends ilObject2
         global $DIC; /* @var \ILIAS\DI\Container $DIC */
 
         $roles = $DIC->access()->checkAccess('write', '', $this->getRefId()) ? "Instructor" : "Learner";
+        //todo if object is in course or group, roles would have to be taken from there s. Mantis 35435 - if necessary Jour Fixe topic
+        //$roles = "Administrator";
+
         if ($this->getProvider()->getAlwaysLearner() == true) {
             $roles = "Learner";
         }
