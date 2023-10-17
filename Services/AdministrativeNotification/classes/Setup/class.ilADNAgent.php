712e6b09613d1daa7dd4cb72bd6760bb922cb488
Lukas Zehnder <lukas@sr.solutions>
2023-10-17 14:34:09 +0200

    [FIX] Reintroduced method getNamedObjectives to prevent error

--- Services/AdministrativeNotification/classes/Setup/class.ilADNAgent.php
+++ Services/AdministrativeNotification/classes/Setup/class.ilADNAgent.php
@@ -13,7 +13,8 @@
  * us at:
  * https://www.ilias.de
  * https://github.com/ILIAS-eLearning
- */
+ *
+ *********************************************************************/
 
 declare(strict_types=1);
 
@@ -36,33 +37,25 @@ class ilADNAgent implements Setup\Agent
         $this->refinery = $refinery;
     }
 
-    /**
-     * @inheritdoc
-     */
+
     public function hasConfig(): bool
     {
         return false;
     }
 
-    /**
-     * @inheritdoc
-     */
+
     public function getArrayToConfigTransformation(): Refinery\Transformation
     {
         throw new \LogicException("Agent has no config.");
     }
 
-    /**
-     * @inheritdoc
-     */
+
     public function getInstallObjective(Config $config = null): Setup\Objective
     {
         return new Setup\Objective\NullObjective();
     }
 
-    /**
-     * @inheritdoc
-     */
+
     public function getUpdateObjective(Config $config = null): Setup\Objective
     {
         return new Setup\ObjectiveCollection(
@@ -74,26 +67,25 @@ class ilADNAgent implements Setup\Agent
         );
     }
 
-    /**
-     * @inheritdoc
-     */
+
     public function getBuildArtifactObjective(): Setup\Objective
     {
         return new Setup\Objective\NullObjective();
     }
 
-    /**
-     * @inheritdoc
-     */
+
     public function getStatusObjective(Setup\Metrics\Storage $storage): Setup\Objective
     {
         return new Setup\Objective\NullObjective();
     }
 
-    /**
-     * @inheritDoc
-     */
+
     public function getMigrations(): array
+    {
+        return [];
+    }
+
+    public function getNamedObjectives(?Config $config = null): array
     {
         return [];
     }
