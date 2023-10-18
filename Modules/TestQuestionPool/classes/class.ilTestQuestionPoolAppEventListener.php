d0b77f74cb9825e5dbc381b2603a152fba2fa0ae
Thomas Famula <famula@leifos.de>
2023-10-18 11:53:55 +0200

    Fix copyright

--- Modules/TestQuestionPool/classes/class.ilTestQuestionPoolAppEventListener.php
+++ Modules/TestQuestionPool/classes/class.ilTestQuestionPoolAppEventListener.php
@@ -1,9 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
-use ILIAS\TestQuestionPool\Skills;
-
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -18,8 +14,11 @@ use ILIAS\TestQuestionPool\Skills;
  * https://www.ilias.de
  * https://github.com/ILIAS-eLearning
  *
- ********************************************************************
- */
+ *********************************************************************/
+
+declare(strict_types=1);
+
+use ILIAS\TestQuestionPool\Skills\TestQuestionPoolSkillDBRepository;
 
 /**
  * @author Thomas Famula <famula@leifos.de>
@@ -31,7 +30,7 @@ class ilTestQuestionPoolAppEventListener implements ilAppEventListener
      */
     public static function handleEvent(string $a_component, string $a_event, array $a_parameter): void
     {
-        $qpl_skill_repo = new Skills\TestQuestionPoolSkillDBRepository();
+        $qpl_skill_repo = new TestQuestionPoolSkillDBRepository();
 
         if ($a_component === "Services/Skill" && $a_event === "deleteSkill") {
             $qpl_skill_repo->removeForSkill($a_parameter["node_id"], $a_parameter["is_reference"]);
