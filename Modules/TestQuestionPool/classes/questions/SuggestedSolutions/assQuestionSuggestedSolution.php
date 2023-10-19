b992858696cdf9f2a8ef724b69850142048cee4f
Fabian Helfer <fhelfer@nb-fhelfer.office.databay.de>
2023-10-19 13:38:17 +0200

    copyright

--- Modules/TestQuestionPool/classes/questions/SuggestedSolutions/assQuestionSuggestedSolution.php
+++ Modules/TestQuestionPool/classes/questions/SuggestedSolutions/assQuestionSuggestedSolution.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -17,6 +15,8 @@ declare(strict_types=1);
  * https://github.com/ILIAS-eLearning
  */
 
+declare(strict_types=1);
+
 namespace ILIAS\TA\Questions;
 
 /**
@@ -31,7 +31,6 @@ abstract class assQuestionSuggestedSolution
     public const TYPE_LM_PAGE = 'pg';
     public const TYPE_GLOSARY_TERM = 'git';
     public const TYPE_FILE = 'file';
-    public const TYPE_TEXT = 'text';
 
     public const TYPES = [
         self::TYPE_LM => 'obj_lm',
