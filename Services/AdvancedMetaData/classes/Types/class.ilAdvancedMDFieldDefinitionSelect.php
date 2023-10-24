81d4ec20aea08cf1df52306b45548895e29fe679
Tim Schmitz <schmitz@leifos.de>
2023-10-24 13:04:24 +0200

    AdvMD: type fix (38304)

--- Services/AdvancedMetaData/classes/Types/class.ilAdvancedMDFieldDefinitionSelect.php
+++ Services/AdvancedMetaData/classes/Types/class.ilAdvancedMDFieldDefinitionSelect.php
@@ -603,7 +603,13 @@ class ilAdvancedMDFieldDefinitionSelect extends ilAdvancedMDFieldDefinition
 
                     if ($sub_type == "wpg") {
                         // #15763 - adapt advmd page lists
-                        ilPCAMDPageList::migrateField((int) $obj_id, $this->getFieldId(), $old_option, $new_option, true);
+                        ilPCAMDPageList::migrateField(
+                            (int) $obj_id,
+                            $this->getFieldId(),
+                            (string) $old_option,
+                            (string) $new_option,
+                            true
+                        );
                     }
                 }
             }
