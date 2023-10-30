76dd5205caca3daa479b1ea3670dd5a2c9143908
Fabian Wolf <wolf@ilias.de>
2023-10-30 16:46:52 +0100

    Bump version number ILIAS 10

--- include/inc.ilias_version.php
+++ include/inc.ilias_version.php
@@ -26,5 +26,5 @@ declare(strict_types=1);
 *
 * @package ilias-core
 */
-const ILIAS_VERSION = "9.0_alpha";
-const ILIAS_VERSION_NUMERIC = "9.0";            // since version ILIAS 6 this must be always x.y: x and y are numbers
+const ILIAS_VERSION = "10.0_alpha";
+const ILIAS_VERSION_NUMERIC = "10.0";            // since version ILIAS 6 this must be always x.y: x and y are numbers
