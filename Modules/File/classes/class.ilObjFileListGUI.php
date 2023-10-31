b23dd0da277eaabc986fa1b5e0feb6a51f3e7702
Fabian Schmid <fabian@sr.solutions>
2023-10-31 16:27:16 +0100

    [FIX] method seems to be needed by ItemRenderer

--- Modules/File/classes/class.ilObjFileListGUI.php
+++ Modules/File/classes/class.ilObjFileListGUI.php
@@ -51,6 +51,14 @@ class ilObjFileListGUI extends ilObjectListGUI
         $this->action_repo = new ActionDBRepository($DIC->database());
     }
 
+    /**
+     * @description This methods seems to be called by ItemRenderer
+     * @deprecated
+     */
+    public function insertCommands(): void
+    {
+    }
+
     protected function getTileImagePath(): string
     {
         if (!$this->use_flavor_for_cards) {
