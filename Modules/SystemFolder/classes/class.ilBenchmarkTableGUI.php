a0a4a1c762be4c81bc015bb9093e80bb3a6ea92a
Fabian Schmid <fabian@sr.solutions>
2023-11-02 07:18:15 +0100

    [FIX] 0038295: Aggregated by First Table in SQL: ErrorException Undefined array key "cnt"

--- Modules/SystemFolder/classes/class.ilBenchmarkTableGUI.php
+++ Modules/SystemFolder/classes/class.ilBenchmarkTableGUI.php
@@ -23,15 +23,17 @@
  */
 class ilBenchmarkTableGUI extends ilTable2GUI
 {
-    protected string $mode;
     protected ilAccessHandler $access;
-
 
     /**
      * Constructor
      */
-    public function __construct($a_parent_obj, $a_parent_cmd, $a_records, $a_mode = "chronological")
-    {
+    public function __construct(
+        ?object $a_parent_obj,
+        string $a_parent_cmd,
+        array $a_records,
+        protected string $mode = "chronological"
+    ) {
         global $DIC;
 
         $this->ctrl = $DIC->ctrl();
@@ -44,7 +46,6 @@ class ilBenchmarkTableGUI extends ilTable2GUI
 
         parent::__construct($a_parent_obj, $a_parent_cmd);
         $this->setLimit(9999);
-        $this->mode = $a_mode;
 
         switch ($this->mode) {
             case "slowest_first":
@@ -82,24 +83,18 @@ class ilBenchmarkTableGUI extends ilTable2GUI
         $this->setRowTemplate("tpl.db_bench.html", "Modules/SystemFolder");
         $this->disable("footer");
         $this->setEnableTitle(true);
-
-        //		$this->addMultiCommand("", $lng->txt(""));
-        //		$this->addCommandButton("", $lng->txt(""));
     }
 
     /**
      * Get first occurence of string
-     *
-     * @param
-     * @return
      */
-    public function getFirst($a_str, $a_needles)
+    public function getFirst(string $a_str, array $a_needles): int
     {
         $pos = 0;
         foreach ($a_needles as $needle) {
-            $pos2 = strpos($a_str, $needle);
+            $pos2 = strpos($a_str, (string) $needle);
 
-            if ($pos2 > 0 && ($pos2 < $pos || $pos == 0)) {
+            if ($pos2 > 0 && ($pos2 < $pos || $pos === 0)) {
                 $pos = $pos2;
             }
         }
@@ -109,49 +104,49 @@ class ilBenchmarkTableGUI extends ilTable2GUI
 
     /**
      * Extract first table from sql
-     *
-     * @param
-     * @return
      */
-    public function extractFirstTableFromSQL($a_sql)
+    public function extractFirstTableFromSQL(string $a_sql): string
     {
-        $pos1 = $this->getFirst(strtolower($a_sql), array("from ", "from\n", "from\t", "from\r"));
+        $pos1 = $this->getFirst(strtolower($a_sql), ["from ", "from\n", "from\t", "from\r"]);
 
         $table = "";
         if ($pos1 > 0) {
             $tablef = substr(strtolower($a_sql), $pos1 + 5);
-            $pos2 = $this->getFirst($tablef, array(" ", "\n", "\t", "\r"));
-            if ($pos2 > 0) {
-                $table = substr($tablef, 0, $pos2);
-            } else {
-                $table = $tablef;
-            }
+            $pos2 = $this->getFirst($tablef, [" ", "\n", "\t", "\r"]);
+            $table = $pos2 > 0 ? substr($tablef, 0, $pos2) : $tablef;
         }
-        if (trim($table) != "") {
+        if (trim($table) !== "") {
             return $table;
         }
 
         return "";
     }
 
-
     /**
      * Get data by first table
-     *
-     * @param
-     * @return
      */
-    public function getDataByFirstTable($a_records)
+    public function getDataByFirstTable(array $a_records): array
     {
-        $data = array();
+        $data = [];
         foreach ($a_records as $r) {
             $table = $this->extractFirstTableFromSQL($r["sql"]);
+            if (trim($table) === '') {
+                continue;
+            }
             $data[$table]["table"] = $table;
-            $data[$table]["cnt"]++;
-            $data[$table]["time"] += $r["time"];
+            if (!isset($data[$table]["cnt"])) {
+                $data[$table]["cnt"] = 1;
+            } else {
+                $data[$table]["cnt"]++;
+            }
+            if (!isset($data[$table]["time"])) {
+                $data[$table]["time"] = $r["time"];
+            } else {
+                $data[$table]["time"] += $r["time"];
+            }
         }
-        if (count($data) > 0) {
-            $data = ilArrayUtil::sortArray($data, "time", "desc", true);
+        if ($data !== []) {
+            return ilArrayUtil::sortArray($data, "time", "desc", true);
         }
 
         return $data;
