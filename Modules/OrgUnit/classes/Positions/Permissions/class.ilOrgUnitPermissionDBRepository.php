8534458e1e4f69ea7e2ad0fd04e2f8b1bdd92483
Nils Haagen <nhaagen@cat06.de>
2023-11-02 15:06:17 +0100

    OrgUs: fix 37895, remove use of JetBrains\PhpStorm\NoReturn

--- Modules/OrgUnit/classes/Positions/Permissions/class.ilOrgUnitPermissionDBRepository.php
+++ Modules/OrgUnit/classes/Positions/Permissions/class.ilOrgUnitPermissionDBRepository.php
@@ -19,8 +19,6 @@
 
 declare(strict_types=1);
 
-use JetBrains\PhpStorm\NoReturn;
-
 class ilOrgUnitPermissionDBRepository implements OrgUnitPermissionRepository
 {
     public const TABLE_NAME = 'il_orgu_permissions';
