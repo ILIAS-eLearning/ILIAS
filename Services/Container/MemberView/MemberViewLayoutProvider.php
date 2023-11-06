1aee5ac32dfca03a42d5c098251d3c31e9308510
Fabian Schmid <fabian@sr.solutions>
2023-11-06 12:50:19 +0100

    [FIX] 0038567: Failed test: Voransicht für Mitglieder aktivieren

--- Services/Container/MemberView/MemberViewLayoutProvider.php
+++ Services/Container/MemberView/MemberViewLayoutProvider.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace ILIAS\Container\Screen;
-
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -17,6 +15,8 @@ namespace ILIAS\Container\Screen;
  * https://github.com/ILIAS-eLearning
  *
  *********************************************************************/
+
+namespace ILIAS\Container\Screen;
 
 use ILIAS\Data\URI;
 use ILIAS\GlobalScreen\Scope\Layout\Builder\StandardPageBuilder;
@@ -53,11 +53,13 @@ class MemberViewLayoutProvider extends AbstractModificationProvider
             return null;
         }
         $ref_id = $mv->getCurrentRefId();
-        $url = new URI(ilLink::_getLink(
-            $ref_id,
-            ilObject::_lookupType(ilObject::_lookupObjId($ref_id)),
-            ['mv' => 0]
-        ));
+        $url = new URI(
+            ilLink::_getLink(
+                $ref_id,
+                ilObject::_lookupType(ilObject::_lookupObjId($ref_id)),
+            )
+        );
+        $url = $url->withParameter('mv', 0);
 
         $modeinfo = $dic->ui()->factory()->mainControls()->modeInfo(
             $dic->language()->txt('mem_view_long'),
