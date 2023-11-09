8f1d252ba7d3d07f1248d512eb24216ffd60ed3c
Richard Klees <richard.klees@concepts-and-training.de>
2023-11-09 17:36:17 +0100

    FileDelivery: remove artifact from repo again

--- components/ILIAS/FileDelivery/src/artifacts/key_rotation.php
+++
@@ -1,7 +0,0 @@
-<?php return array (
-  0 => 'ebIXOSOn<>6TlpHZ;@wn"fbZD-`pXt\\0',
-  1 => 'ptp7r|\\.i%"?*CuqE=_Ce;\'"8p@5i8.n',
-  2 => 'pFre(trfb]p?\'zQ2dCf):SsYkuk\'9.%x',
-  3 => 'F#s^5_y>RC#&fVnFse?>zAe4@lH(wjh>',
-  4 => 'h%Ppf8Uee)l:jUYZ5GMxnTD+7e.*|dMq',
-);
