5aee34e97389869350b82df7b02de1efbe720a37
Tim Schmitz <schmitz@leifos.de>
2023-11-10 18:40:37 +0100

    Course: highlight current object in adopt content tree (36702)

--- Services/Object/classes/class.ilObjectCopyGUI.php
+++ Services/Object/classes/class.ilObjectCopyGUI.php
@@ -382,6 +382,7 @@ class ilObjectCopyGUI
         $exp->setTargetGet('ref_id');
         $exp->setPostVar('source');
         $exp->setCheckedItems($this->getSources());
+        $exp->highlightNode((string) $this->getFirstTarget());
 
         // Filter to container
         foreach (['cat', 'root', 'fold'] as $container) {
