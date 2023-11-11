1409eec32dfde34669c94cb90b0dece332ae175a
Alexander Killing <killing@leifos.de>
2023-11-11 10:34:57 +0100

    37373: ilGlossaryObjDeletionHandler Needs too many potentially unmet dependences on __construct()

--- Modules/Glossary/Service/class.InternalService.php
+++ Modules/Glossary/Service/class.InternalService.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -18,57 +16,55 @@ declare(strict_types=1);
  *
  *********************************************************************/
 
+declare(strict_types=1);
+
 namespace ILIAS\Glossary;
 
 use ILIAS\DI\Container;
 
-/**
- * @author Alexander Killing <killing@leifos.de>
- */
 class InternalService
 {
-    protected InternalDataService $data;
-    protected InternalRepoService $repo;
-    protected InternalDomainService $domain;
-    protected InternalGUIService $gui;
+    protected Container $DIC;
+    protected array $instance = [];
 
     public function __construct(Container $DIC)
     {
-        $this->data = new InternalDataService();
-
-        $this->repo = new InternalRepoService(
-            $this->data(),
-            $DIC->database()
-        );
-        $this->domain = new InternalDomainService(
-            $DIC,
-            $this->repo,
-            $this->data
-        );
-        $this->gui = new InternalGUIService(
-            $DIC,
-            $this->data,
-            $this->domain
-        );
+        $this->DIC = $DIC;
     }
 
     public function data(): InternalDataService
     {
-        return $this->data;
+        return $this->instance["data"] ??
+            $this->instance["data"] = new InternalDataService();
     }
 
     public function repo(): InternalRepoService
     {
-        return $this->repo;
+        return $this->instance["repo"] ??
+            $this->instance["repo"] = new InternalRepoService(
+                $this->data(),
+                $this->DIC->database()
+            );
     }
 
     public function domain(): InternalDomainService
     {
-        return $this->domain;
+        return $this->instance["domain"] ??
+            $this->instance["domain"] = new InternalDomainService(
+                $this->DIC,
+                $this->repo(),
+                $this->data()
+            );
     }
 
     public function gui(): InternalGUIService
     {
-        return $this->gui;
+        return $this->instance["gui"] ??
+            $this->instance["gui"] = new InternalGUIService(
+                $this->DIC,
+                $this->data(),
+                $this->domain()
+            );
+
     }
 }
