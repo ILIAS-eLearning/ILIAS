1014fffed54464947ddeb90b5301baca345f6921
Stephan Kergomard <webmaster@kergomard.ch>
2023-11-13 15:20:50 +0100

    User: Fix Missing Line Brake After Until
    See: https://mantis.ilias.de/view.php?id=38681

--- Services/User/classes/class.ilObjUser.php
+++ Services/User/classes/class.ilObjUser.php
@@ -3133,7 +3133,7 @@ class ilObjUser extends ilObject
             $body .= $language->txt('time_limit') . ': ' .
                 $language->txt('from') . " " .
                 $start->get(IL_CAL_DATETIME) . " ";
-            $body .= $language->txt('to') . ' ' . $end->get(IL_CAL_DATETIME);
+            $body .= $language->txt('to') . ' ' . $end->get(IL_CAL_DATETIME) . "\n";
         }
 
         /**
