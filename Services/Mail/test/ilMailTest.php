08d613428108f7206a664533e150fec613b19731
mjansen <mjansen@databay.de>
2023-11-15 09:04:22 +0100

    Mail: Rename tests
    (cherry picked from commit fa92d9dc5cefd5389944570e241516cf8b78a615)

--- Services/Mail/test/ilMailTest.php
+++ Services/Mail/test/ilMailTest.php
@@ -413,7 +413,7 @@ class ilMailTest extends ilMailBaseTest
         $this->assertSame($draftId, $instance->updateDraft($folderId, [], $to, $cc, $bcc, $subject, $message, $draftId, $usePlaceholders, $contextId, $params));
     }
 
-    public function testSavePostData(): void
+    public function testPersistingToStage(): void
     {
         $userId = 897;
         $attachments = [];
@@ -462,7 +462,7 @@ class ilMailTest extends ilMailBaseTest
         );
     }
 
-    public function testGetSavedData(): void
+    public function testRetrievalFromStage(): void
     {
         $userId = 789;
         $instance = $this->create(67, $userId);
