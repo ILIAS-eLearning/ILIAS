472b6fe27492e362456239820a7da00ab6f940d7
mjansen <mjansen@databay.de>
2023-11-15 08:53:47 +0100

    Mail: PSR-12

--- Services/Mail/classes/class.ilMail.php
+++ Services/Mail/classes/class.ilMail.php
@@ -396,7 +396,7 @@ class ilMail
 
         if (isset($row['attachments'])) {
             $unserialized = unserialize(stripslashes($row['attachments']), ['allowed_classes' => false]);
-            $row['attachments'] = is_array($unserialized) ? $unserialized : []; 
+            $row['attachments'] = is_array($unserialized) ? $unserialized : [];
         } else {
             $row['attachments'] = [];
         }
