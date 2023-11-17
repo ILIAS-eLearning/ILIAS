c9830888a35136ff8d650889294486b091ef6c25
Fabian Schmid <fabian@sr.solutions>
2023-11-17 07:31:02 +0100

    [FIX] 0038787: File/Archives/Tests: "testLargetZIPs" test creates huge directories when running within PhpStorm

--- tests/Filesystem/Util/ZipTest.php
+++ tests/Filesystem/Util/ZipTest.php
@@ -132,13 +132,16 @@ class ZipTest extends TestCase
         $this->assertEquals([], iterator_to_array($unzip->getFiles()));
     }
 
-    /*
-     * Stephan Kergomard - 2023-11-16: See https://mantis.ilias.de/view.php?id=38787
+
     public function testLargeZIPs(): void
     {
         // get ulimit
         $ulimit = (int) shell_exec('ulimit -n');
-        $this->assertGreaterThan(1000, $ulimit);
+        $limit = 2500;
+        if ($ulimit >= $limit) {
+            $this->markTestSkipped('ulimit is too high and would take too much resources');
+        }
+        $this->assertLessThan($limit, $ulimit);
 
         $zip = new Zip(new ZipOptions());
 
@@ -146,7 +149,7 @@ class ZipTest extends TestCase
 
         for ($i = 0; $i < $ulimit * 2; $i++) {
             $path_inside_zip = $file_names[] = 'test' . $i;
-            $zip->addStream(Streams::ofResource(fopen(__DIR__ . '/Convert/img/robot.jpg', 'rb')), $path_inside_zip);
+            $zip->addStream(Streams::ofString('-'), $path_inside_zip);
         }
         $this->assertTrue(true); // no warning or error
 
@@ -157,8 +160,6 @@ class ZipTest extends TestCase
         sort($file_names_in_zip);
         $this->assertEquals($file_names, $file_names_in_zip);
     }
-     *
-     */
 
     /**
      * @dataProvider getZips
