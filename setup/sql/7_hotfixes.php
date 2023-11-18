b143b93e07b07606423cd166c1e393d0aacc8e76
mjansen <mjansen@databay.de>
2023-11-18 20:16:30 +0100

    Certificate: Fix indexes

--- setup/sql/7_hotfixes.php
+++ setup/sql/7_hotfixes.php
@@ -1817,14 +1817,14 @@ if ($ilDB->tableExists('adv_md_values_text') &&
 <?php
 if (
     $this->db->tableExists('il_cert_template')
-    && !$this->db->indexExistsByFields('il_cert_template', ['obj_id', 'background_image_path'])
+    && !$this->db->indexExistsByFields('il_cert_template', ['background_image_path', 'currently_active'])
 ) {
     $this->db->addIndex('il_cert_template', ['background_image_path', 'currently_active'], 'i5');
 }
 
 if (
     $this->db->tableExists('il_cert_user_cert')
-    && !$this->db->indexExistsByFields('il_cert_user_cert', ['obj_id', 'background_image_path'])
+    && !$this->db->indexExistsByFields('il_cert_user_cert', ['background_image_path', 'currently_active'])
 ) {
     $this->db->addIndex('il_cert_user_cert', ['background_image_path', 'currently_active'], 'i7');
 }
