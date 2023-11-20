30ec7612bfe085ae573a4f5c1973d03cc8b47249
Alexander Killing <killing@leifos.de>
2023-11-20 12:40:46 +0100

    38582: Eigenschaften der Instanz nur angezeigt, wenn Medienobjekt mehrfach genutzt

--- Services/COPage/classes/class.ilPCMediaObject.php
+++ Services/COPage/classes/class.ilPCMediaObject.php
@@ -569,7 +569,9 @@ class ilPCMediaObject extends ilPageContent
                 return true;
             }
         }
-        return false;
+        // see https://mantis.ilias.de/view.php?id=38582
+        // we allow instance editing regardless of number of usages
+        return true;
     }
 
     public static function deleteHistoryLowerEqualThan(
