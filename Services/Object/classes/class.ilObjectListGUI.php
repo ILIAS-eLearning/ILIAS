ce48bee5e170a2b6e43e63424c5f4a755f45b58f
Stephan Kergomard <webmaster@kergomard.ch>
2023-11-21 16:19:49 +0100

    Object: Make Property Key Value More Robust
    See: https://mantis.ilias.de/view.php?id=38966

--- Services/Object/classes/class.ilObjectListGUI.php
+++ Services/Object/classes/class.ilObjectListGUI.php
@@ -3367,7 +3367,8 @@ class ilObjectListGUI
 
         $l = [];
         foreach ($this->determineProperties() as $p) {
-            if (($p["alert"] ?? false) && $p['property'] !== $this->lng->txt('learning_progress')) {
+            if (isset($p['alert']) && $p['alert'] == true
+                && isset($p['property']) && $p['property'] !== $this->lng->txt('learning_progress')) {
                 $l[(string) $p['property']] = (string) $p['value'];
             }
         }
