2bc0f5bf94b3ac7e63cd00f4070b1d38e1719172
Alexander Killing <killing@leifos.de>
2023-11-23 10:29:50 +0100

    exercise: language+form fixes

--- Modules/Exercise/Assignment/class.ilExAssignmentEditorGUI.php
+++ Modules/Exercise/Assignment/class.ilExAssignmentEditorGUI.php
@@ -505,9 +505,11 @@ class ilExAssignmentEditorGUI
         $form->addItem($rmd_grade);
 
         // relative deadline
-        $ti = new ilNumberInputGUI($lng->txt("days"), "relative_deadline");
+        $ti = new ilNumberInputGUI($lng->txt("exc_rem_time_after_start"), "relative_deadline");
+        $ti->setSuffix($lng->txt("days"));
         $ti->setMaxLength(3);
         $ti->setSize(3);
+        $ti->setRequired(true);
         $op2->addSubItem($ti);
 
         // last submission for relative deadline
