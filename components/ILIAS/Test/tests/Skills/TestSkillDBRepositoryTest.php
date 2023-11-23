b02024f1710c3b3f1a139459c428d43baf00695d
Matheus Zych <mzych@databay.de>
2023-11-23 14:48:04 +0100

    Added Skills Unit tests

---
+++ components/ILIAS/Test/tests/Skills/TestSkillDBRepositoryTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Skills;
+
+use ILIAS\Test\Skills\TestSkillDBRepository;
+use ilTestBaseTestCase;
+
+class TestSkillDBRepositoryTest extends ilTestBaseTestCase
+{
+    public function testConstruct(): void
+    {
+        $testSkillDBRepository = new TestSkillDBRepository();
+        $this->assertInstanceOf(TestSkillDBRepository::class, $testSkillDBRepository);
+
+        $testSkillDBRepository = new TestSkillDBRepository($this->createMock(\ilDBInterface::class));
+        $this->assertInstanceOf(TestSkillDBRepository::class, $testSkillDBRepository);
+    }
+
+    public function testRemoveForSkill(): void
+    {
+        $this->markTestSkipped();
+    }
+}
