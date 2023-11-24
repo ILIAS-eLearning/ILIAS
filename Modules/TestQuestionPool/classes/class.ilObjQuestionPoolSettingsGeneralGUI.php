9693a6d2b281013ca42975209c647c2ec49731f7
Stephan Kergomard <webmaster@kergomard.ch>
2023-11-24 09:52:46 +0100

    Revert "bugfix derived qpl have non changable titles"
    This reverts commit bc3c806e4d4676125bf305c340d5615e542744ac.

--- Modules/TestQuestionPool/classes/class.ilObjQuestionPoolSettingsGeneralGUI.php
+++ Modules/TestQuestionPool/classes/class.ilObjQuestionPoolSettingsGeneralGUI.php
@@ -155,7 +155,7 @@ class ilObjQuestionPoolSettingsGeneralGUI
         $md_section = $md_obj->getGeneral();
 
         // title
-        $md_section->setTitle($this->poolOBJ->getTitle());
+        $md_section->setTitle($form->getItemByPostVar('title')->getValue());
         $md_section->update();
 
         // Description
@@ -212,7 +212,7 @@ class ilObjQuestionPoolSettingsGeneralGUI
 
         $title = new ilTextInputGUI($this->lng->txt("title"), "title");
         $title->setRequired(true);
-        $title->setValue($this->poolOBJ->getTitle());
+        $title->setValue($md_section->getTitle());
         $form->addItem($title);
 
         $ids = $md_section->getDescriptionIds();
