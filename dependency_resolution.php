52c9dad1a43ea8f0fdff319f9557a6243b225d74
Richard Klees <richard.klees@concepts-and-training.de>
2023-11-24 16:48:13 +0100

    Component: write bootstrap artifact

---
+++ dependency_resolution.php
@@ -0,0 +1,16 @@
+<?php
+
+// This is the default resolution for ambiguous dependencies of components in
+// Standard ILIAS.
+//
+// The structure is as such: keys are components that use services ("dependant")
+// that need disambiguation, the value for each dependant is an array where the key
+// is the definition ("dependency") and the value is the implementation
+// ("implementation") to be used.
+//
+// The entry "*" for the dependant will define fallbacks to be used for all components
+// that have no explicit disambiguation.
+
+return [
+    "*" => []
+];
