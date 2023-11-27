6004c612046dd2adee49674cc10243abeaeb38f1
Fabian Helfer <fhelfer@nb-fhelfer.office.databay.de>
2023-11-27 14:05:34 +0100

    remove recommendencontent changes§

--- Services/Repository/RecommendedContent/classes/class.ilDashboardRecommendedContentGUI.php
+++ Services/Repository/RecommendedContent/classes/class.ilDashboardRecommendedContentGUI.php
@@ -118,12 +118,14 @@ class ilDashboardRecommendedContentGUI extends ilDashboardBlockGUI
     {
         $rec_manager = new ilRecommendedContentManager();
         $refIds = (array) ($this->http->request()->getParsedBody()['ref_id'] ?? []);
-        if ($refIds !== []) {
-            foreach ($refIds as $ref_id) {
-                $rec_manager->declineObjectRecommendation($this->user->getId(), (int) $ref_id);
-            }
-            $this->main_tpl->setOnScreenMessage('success', $this->lng->txt('pd_remove_multi_confirm'), true);
+        if ($refIds === []) {
+            $this->ctrl->redirect($this, 'manage');
         }
+
+        foreach ($refIds as $ref_id) {
+            $rec_manager->declineObjectRecommendation($this->user->getId(), (int) $ref_id);
+        }
+        $this->main_tpl->setOnScreenMessage('success', $this->lng->txt('pd_remove_multi_confirm'), true);
         $this->ctrl->returnToParent($this);
     }
 
