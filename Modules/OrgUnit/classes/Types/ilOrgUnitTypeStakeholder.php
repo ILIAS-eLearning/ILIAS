b9820baefd38518dce76919ece1d287a65651e78
Nils Haagen <nhaagen@cat06.de>
2023-11-28 16:43:42 +0100

    OrgU: use IRSS for type icons

---
+++ Modules/OrgUnit/classes/Types/ilOrgUnitTypeStakeholder.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
+declare(strict_types=1);
+
+use ILIAS\ResourceStorage\Stakeholder\AbstractResourceStakeholder;
+use ILIAS\FileUpload\Processor\WhitelistExtensionPreProcessor;
+
+class ilOrgUnitTypeStakeholder extends AbstractResourceStakeholder
+{
+    private const ID = 'PRGType';
+
+    public function __construct(
+        protected int $owner = 6
+    ) {
+    }
+
+    public function getId(): string
+    {
+        return self::ID;
+    }
+
+    public function getOwnerOfNewResources(): int
+    {
+        return $this->owner;
+    }
+
+    public function getPreprocessors(): array
+    {
+        $allowed = ['svg'];
+        $processor = new WhitelistExtensionPreProcessor($allowed);
+
+        return [
+            $processor
+        ];
+    }
+}
