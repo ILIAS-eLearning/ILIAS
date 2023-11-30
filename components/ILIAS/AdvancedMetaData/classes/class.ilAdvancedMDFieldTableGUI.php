92369ed047172d467e137963710f6f6f226c2e6c
Tim Schmitz <schmitz@leifos.de>
2023-11-30 15:28:50 +0100

    AdvMD: fix sorting in fields table (39191)

--- components/ILIAS/AdvancedMetaData/classes/class.ilAdvancedMDFieldTableGUI.php
+++ components/ILIAS/AdvancedMetaData/classes/class.ilAdvancedMDFieldTableGUI.php
@@ -54,8 +54,8 @@ class ilAdvancedMDFieldTableGUI extends ilTable2GUI
         $this->addColumn('', 'f', '1');
         $this->addColumn($this->lng->txt('position'), 'position', "5%");
         $this->addColumn($this->lng->txt('title'), 'title', "30%");
-        $this->addColumn($this->lng->txt('md_adv_field_fields'), 'type', "35%");
-        $this->addColumn($this->lng->txt('options'), 'obj_types', "30%");
+        $this->addColumn($this->lng->txt('md_adv_field_fields'), '', "35%");
+        $this->addColumn($this->lng->txt('options'), 'searchable', "30%");
 
         $this->setFormAction($this->ctrl->getFormAction($a_parent_obj));
         $this->setRowTemplate("tpl.edit_fields_row.html", "components/ILIAS/AdvancedMetaData");
