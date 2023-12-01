72ed111ec5dde96537d73c0ddf030723e928380d
Nils Haagen <nhaagen@cat06.de>
2023-12-01 11:23:03 +0100

    Maps: 38720, rollup as iife

--- Services/Maps/js/rollup.config.js
+++ Services/Maps/js/rollup.config.js
@@ -1,21 +1,53 @@
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ ******************************************************************** */
+
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+import copyright from '../../../CI/Copyright-Checker/copyright';
+import preserveCopyright from '../../../CI/Copyright-Checker/preserveCopyright';
 
 export default {
-    input: './src/ilOl.js',
-    output: {
-        file: './dist/ServiceOpenLayers.js',
-        format: 'es'
+  input: './src/ilOl.js',
+  output: {
+    file: './dist/ServiceOpenLayers.js',
+    format: 'iife',
+    globals: {
+      il: 'il',
     },
-    plugins: [resolve(), commonjs()],
-    onwarn: function(warning, superOnWarn) {
-        /*
+    banner: copyright,
+  },
+  external: ['il'],
+  plugins: [
+    resolve(),
+    commonjs(),
+    terser({
+      format: {
+        comments: preserveCopyright,
+      },
+    }),
+  ],
+  onwarn(warning, superOnWarn) {
+    /*
          * skip certain warnings
          * https://github.com/openlayers/openlayers/issues/10245
          */
-        if (warning.code === 'THIS_IS_UNDEFINED') {
-            return;
-        }
-        superOnWarn(warning);
+    if (warning.code === 'THIS_IS_UNDEFINED') {
+      return;
     }
+    superOnWarn(warning);
+  },
 };
--- Services/Maps/templates/default/tpl.openlayers_map.js
+++ Services/Maps/templates/default/tpl.openlayers_map.js
@@ -1,34 +1,53 @@
-let ilOLInvalidAddress = "{INVALID_ADDRESS_STRING}";
-let ilOLUserMarkers = {"{MAP_ID}" : []};
-let ilOLMapData = {
-    "{MAP_ID}" : [
-        {LAT},
-        {LONG},
-        {ZOOM},
-        {CENTRAL_MARKER},
-        {NAV_CONTROL},
-        {REPLACE_MARKER},
-        {TILES},
-        "{GEOLOCATION}"
-    ]
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ ******************************************************************** */
+
+/* eslint-disable */
+const ilOLInvalidAddress = '{INVALID_ADDRESS_STRING}';
+const ilOLUserMarkers = { '{MAP_ID}': [] };
+const ilOLMapData = {
+  '{MAP_ID}': [
+    {LAT},
+    {LONG},
+    {ZOOM},
+    {CENTRAL_MARKER},
+    {NAV_CONTROL},
+    {REPLACE_MARKER},
+    {TILES},
+    '{GEOLOCATION}',
+  ],
 };
 
+// <!-- BEGIN user_marker -->
+ilOLUserMarkers['{UMAP_ID}'][{CNT}] = new Array(
+  {ULONG},
+  {ULAT},
+  "<img style='float:right; margin-right:10px; margin-left:10px;' className='ilUserXXSmall' src='{IMG_USER}'\/><span className='small'>{USER_INFO}<\/span>",
+);
+// <!-- END user_marker -->
 
-<!-- BEGIN user_marker -->
-ilOLUserMarkers["{UMAP_ID}"][{CNT}] = new Array({ULONG}, {ULAT},
-    "<img style='float:right; margin-right:10px; margin-left:10px;' className='ilUserXXSmall' src='{IMG_USER}'\/><span className='small'>{USER_INFO}<\/span>");
-<!-- END user_marker -->
-
-let openLayer = initIlOpenLayerMaps(jQuery, ilOLInvalidAddress, ilOLMapData, ilOLUserMarkers);
+const openLayer = il.OLMaps.init(jQuery, ilOLInvalidAddress, ilOLMapData, ilOLUserMarkers);
 openLayer.forceResize(jQuery);
 openLayer.init(ilOLMapData);
 
-ilLookupAddress = function(id, address) {
-    return openLayer.jumpToAddress(id, address);
+ilLookupAddress = function (id, address) {
+  return openLayer.jumpToAddress(id, address);
 };
 ilUpdateMap = function (id) {
-    return openLayer.updateMap(id);
+  return openLayer.updateMap(id);
 };
-ilShowUserMarker = function(id, counter) {
-    return openLayer.moveToUserMarkerAndOpen(id, counter);
+ilShowUserMarker = function (id, counter) {
+  return openLayer.moveToUserMarkerAndOpen(id, counter);
 };
