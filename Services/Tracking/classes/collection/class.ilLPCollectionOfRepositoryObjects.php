806c864f71c8ad5ba63f0218d9257c26c9c95fa2
sroenchen <145426925+sroenchen@users.noreply.github.com>
2023-12-08 13:26:43 +0100

    Mantis #39342 - fix potential error in lpSettings
    https://mantis.ilias.de/view.php?id=39342
    fixes an error, that can occur, when using multiple lp collections within an object

--- Services/Tracking/classes/collection/class.ilLPCollectionOfRepositoryObjects.php
+++ Services/Tracking/classes/collection/class.ilLPCollectionOfRepositoryObjects.php
@@ -1,8 +1,22 @@
 <?php
 
-declare(strict_types=0);
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
 
-/* Copyright (c) 1998-2010 ILIAS open source, Extended GPL, see docs/LICENSE */
+declare(strict_types=1);
 
 /**
  * LP collection of repository objects
@@ -431,6 +445,7 @@ class ilLPCollectionOfRepositoryObjects extends ilLPCollection
             if (count((array) ($grouped_items['items'] ?? [])) > 1) {
                 foreach ($grouped_items['items'] as $grouped_item_id) {
                     if ($grouped_item_id == $item_id ||
+                        !isset($items[$grouped_item_id]) ||
                         !is_array($items[$grouped_item_id] ?? false)) { // #15498
                         continue;
                     }
