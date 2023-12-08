ae0eeaf8d8f331238823ee3770b222db5505e434
Alexander Killing <killing@leifos.de>
2023-12-08 14:58:17 +0100

    BOOK: adjust deletion of booking objects. (#21705) (#6401);

--- components/ILIAS/BookingManager/Reservations/class.ReservationDBRepository.php
+++ components/ILIAS/BookingManager/Reservations/class.ReservationDBRepository.php
@@ -331,4 +331,20 @@ class ReservationDBRepository
             return ($row["context_obj_id"] == $context_obj_id);
         });
     }
+
+    public function getReservationIdsByBookingObjectId(int $booking_object_id): array
+    {
+        $set = $this->db->queryF(
+            "SELECT booking_reservation_id FROM booking_reservation " .
+            " WHERE object_id = %s ",
+            ["integer"],
+            [$booking_object_id]
+        );
+        $ret = [];
+        while ($row = $this->db->fetchAssoc($set)) {
+            $ret[] = (int) $row['booking_reservation_id'];
+        }
+
+        return $ret;
+    }
 }
