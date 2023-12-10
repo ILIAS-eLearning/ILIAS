d979af8d44421b0ea52dd0eb98979277cd705a39
Alexander Killing <killing@leifos.de>
2023-12-10 22:17:55 +0100

    39048: Failed test: Feedback geben > 5-Sterne-Bewertung

--- components/ILIAS/Exercise/js/ilExcPeerReview.js
+++ components/ILIAS/Exercise/js/ilExcPeerReview.js
@@ -15,24 +15,25 @@
  *********************************************************************/
 
 il.ExcPeerReview = {
-	ajax_url: '',
-	
-	setAjax: function (url) {
-		this.ajax_url = url;
-	},
+  ajax_url: '',
 
-	saveCrit: function (node, rating_peer_id, rating_crit_id, value) {			
-		$.ajax({
-			url: this.ajax_url,
-			dataType: 'text',
-			type: 'POST',
-			data: {
-				peer_id: rating_peer_id,
-				crit_id: rating_crit_id,
-				value: value
-			}			
-		}).done(function(data) {			
-			 $(node).closest("div.crit_widget").html(data);
-		});
-	}		
+  setAjax: function (url) {
+    this.ajax_url = url;
+  },
+
+  saveCrit: function (node, rating_peer_id, rating_crit_id, value) {
+    $.ajax({
+      url: this.ajax_url,
+      dataType: 'text',
+      type: 'POST',
+      data: {
+        peer_id: rating_peer_id,
+        crit_id: rating_crit_id,
+        value: value
+      }
+    }).done(function (data) {
+      const cl = document.querySelector("div.crit_widget[data-crit-id='" + rating_crit_id + "']");
+      $(cl).html(data);
+    });
+  }
 };
