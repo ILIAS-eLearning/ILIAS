fd6fb725aabf4c04c4ce5289dcec1500a026bb7a
Nils Haagen <nhaagen@cat06.de>
2023-12-20 09:44:44 +0100

    TA: 39410, remove 'finalized_by' if finalization is not set

--- components/ILIAS/Test/classes/class.ilTestScoringByQuestionsGUI.php
+++ components/ILIAS/Test/classes/class.ilTestScoringByQuestionsGUI.php
@@ -324,6 +324,7 @@ class ilTestScoringByQuestionsGUI extends ilTestScoringGUI
                 $correction_feedback['finalized_evaluation'] = $this->lng->txt('yes');
             } else {
                 $correction_feedback['finalized_evaluation'] = $this->lng->txt('no');
+                $correction_feedback['finalized_by'] = '';
             }
 
             echo json_encode([ 'feedback' => $correction_feedback, 'points' => $correction_points, "translation" => ['yes' => $this->lng->txt('yes'), 'no' => $this->lng->txt('no')]]);
