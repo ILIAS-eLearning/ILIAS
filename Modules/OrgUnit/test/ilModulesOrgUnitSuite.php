a728b5c76b0aa9768929681b0178c776a9d55730
Nils Haagen <nhaagen@cat06.de>
2024-01-04 13:35:30 +0100

    OrgU: 39676, add test for AMD Form

--- Modules/OrgUnit/test/ilModulesOrgUnitSuite.php
+++ Modules/OrgUnit/test/ilModulesOrgUnitSuite.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -16,8 +14,9 @@ declare(strict_types=1);
  * https://www.ilias.de
  * https://github.com/ILIAS-eLearning
  *
- ********************************************************************
- */
+ *********************************************************************/
+
+declare(strict_types=1);
 
 use PHPUnit\Framework\TestSuite;
 
@@ -56,6 +55,9 @@ class ilModulesOrgUnitSuite extends TestSuite
 
         require_once("./Modules/OrgUnit/test/ilModulesOrgUnitPermissionTest.php");
         $suite->addTestSuite("ilModulesOrgUnitPermissionTest");
+
+        require_once("./Modules/OrgUnit/test/ilModulesOrgUnitTypeTest.php");
+        $suite->addTestSuite("ilModulesOrgUnitTypeTest");
 
         return $suite;
     }
