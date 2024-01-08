2fd9c8ded66c0e0e26d90e2e4960d05231fe2a93
Nils Haagen <nhaagen@cat06.de>
2024-01-08 14:29:39 +0100

    UI/Metabar, more button with aria-expanded (#37151, #38249) (#6846)
    * Revert "[FIX] UI #37151: add aria-expanded attribute to metabar more-button (#6318)"
    
    This reverts commit fc218e3f18f371d7b680f8bf3c035bdd21477477.
    
    * UI/Metabar, more button with aria-expanded (#37151, #38249)

--- src/UI/Implementation/Component/MainControls/Slate/Renderer.php
+++ src/UI/Implementation/Component/MainControls/Slate/Renderer.php
@@ -65,7 +65,7 @@ class Renderer extends AbstractComponentRenderer
                     $triggerer = $triggerer
                         ->withOnClick($trigger_signal)
                         ->withAdditionalOnLoadCode(
-                            fn($id) => "
+                            fn ($id) => "
                                     il.UI.maincontrols.mainbar.addTriggerSignal('{$trigger_signal}');
                                     il.UI.maincontrols.mainbar.addPartIdAndEntry('{$mb_id}', 'triggerer', '{$id}');
                                 "
@@ -132,15 +132,9 @@ class Renderer extends AbstractComponentRenderer
 
         $component = $component->withAdditionalOnLoadCode(
             function ($id) use ($slate_signals, $mb_id): string {
-                $js = "";
+                $js = "fn = il.UI.maincontrols.slate.onSignal;";
                 foreach ($slate_signals as $key => $signal) {
-                    $js .= "$(document).on('{$signal}', function(event, signalData) { 
-                        il.UI.maincontrols.slate.onSignal('{$key}', 
-                        event, 
-                        signalData, 
-                        '{$id}'
-                    ); 
-                    return false;});";
+                    $js .= "$(document).on('{$signal}', function(event, signalData) { fn('{$key}', event, signalData, '{$id}'); return false;});";
                 }
 
                 if ($mb_id) {
@@ -174,7 +168,7 @@ class Renderer extends AbstractComponentRenderer
     public function registerResources(\ILIAS\UI\Implementation\Render\ResourceRegistry $registry): void
     {
         parent::registerResources($registry);
-        $registry->register('./src/UI/templates/js/MainControls/dist/maincontrols.min.js');
+        $registry->register('./src/UI/templates/js/MainControls/slate.js');
     }
 
     /**
