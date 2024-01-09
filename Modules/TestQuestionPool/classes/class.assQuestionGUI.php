df3cf09d283c54a89e8ef6c3c8d3dc3446fb2670
Nils Haagen <nhaagen@cat06.de>
2024-01-09 12:30:20 +0100

    TA/LM: 39774, do not offer nested mode for vertical OrderingQuestion in LearningModule-context

--- Modules/TestQuestionPool/classes/class.assQuestionGUI.php
+++ Modules/TestQuestionPool/classes/class.assQuestionGUI.php
@@ -123,6 +123,7 @@ abstract class assQuestionGUI
 
     protected ilPropertyFormGUI $editForm;
     protected \ILIAS\TestQuestionPool\InternalRequestService $request;
+    protected bool $parent_type_is_lm = false;
 
     public function __construct()
     {
@@ -2095,4 +2096,14 @@ abstract class assQuestionGUI
             self::ALLOWED_PLAIN_TEXT_TAGS
         );
     }
+
+    public function isInLearningModuleContext(): bool
+    {
+        return $this->parent_type_is_lm;
+    }
+    public function setInLearningModuleContext(bool $flag): void
+    {
+        $this->parent_type_is_lm = $flag;
+    }
+
 }
