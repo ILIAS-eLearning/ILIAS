cb4e0ba4a4cfcea732f52f480a1fbcbb047f2697
Uwe Kohnle <kohnle@internetlehrer-gmbh.de>
2024-01-20 14:26:09 +0000

    probably parsing issue cmix

--- Modules/CmiXapi/classes/class.ilObjCmiXapiGUI.php
+++ Modules/CmiXapi/classes/class.ilObjCmiXapiGUI.php
@@ -109,7 +109,7 @@ class ilObjCmiXapiGUI extends ilObject2GUI
         $item->setRequired(true);
         $types = ilCmiXapiLrsTypeList::getTypesData(false, ilCmiXapiLrsType::AVAILABILITY_CREATE);
         foreach ($types as $type) {
-            $option = new ilRadioOption($type['title'], $type['type_id'], $type['description']);
+            $option = new ilRadioOption((string) $type['title'], (string) $type['type_id'], (string) $type['description']);
             $item->addOption($option);
         }
         #$item->setValue($this->object->typedef->getTypeId());
