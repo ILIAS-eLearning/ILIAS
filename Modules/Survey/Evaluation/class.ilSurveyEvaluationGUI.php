eac302a3eed77d87dbdac5e556ac97543e83aafe
Alexander Killing <killing@leifos.de>
2024-01-23 19:25:37 +0100

    39535: Failed test: Ergebnisse der Umfrage exportieren

--- Modules/Survey/Evaluation/class.ilSurveyEvaluationGUI.php
+++ Modules/Survey/Evaluation/class.ilSurveyEvaluationGUI.php
@@ -494,7 +494,7 @@ class ilSurveyEvaluationGUI
                     $row_results[1],
                     $excel_row,
                     $a_eval->getExportGrid($row_results[1]),
-                    is_array($texts[$row_title])
+                    is_array($texts[$row_title] ?? false)
                         ? array("" => $texts[$row_title])
                         : null
                 );
