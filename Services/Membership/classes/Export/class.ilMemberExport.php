6195ce1928011b69faa8c6ea44f2d6f570a83710
Stefan Meyer <meyer@leifos.de>
2024-01-26 17:39:45 +0100

    0040178: Failed test: Mitgliederdaten exportieren

--- Services/Membership/classes/Export/class.ilMemberExport.php
+++ Services/Membership/classes/Export/class.ilMemberExport.php
@@ -1,7 +1,4 @@
 <?php
-
-declare(strict_types=1);
-
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -17,6 +14,8 @@ declare(strict_types=1);
  * https://github.com/ILIAS-eLearning
  *
  *********************************************************************/
+
+declare(strict_types=1);
 
 /**
  * Class for generation of member export files
@@ -466,7 +465,10 @@ class ilMemberExport
         if (strpos($a_field, 'cdf_') !== 0) {
             return false;
         }
-        if (!$this->privacy->courseConfirmationRequired() or $this->agreement[$a_usr_id]['accepted']) {
+        if (
+            !$this->privacy->courseConfirmationRequired() ||
+            ($this->agreement[$a_usr_id]['accepted'] ?? false)
+        ) {
             $field_info = explode('_', $a_field);
             $field_id = $field_info[1] ?? 0;
             $value = '';
