ebc179b39bdd138879bdaf4aa906e0d130f20f9e
Fabian Helfer <fhelfer@nb-fhelfer.office.databay.de>
2024-01-29 13:54:36 +0100

    Bugfix qpl title null error

--- Modules/Test/classes/tables/class.ilTestQuestionsTableGUI.php
+++ Modules/Test/classes/tables/class.ilTestQuestionsTableGUI.php
@@ -227,7 +227,7 @@ class ilTestQuestionsTableGUI extends ilTable2GUI
 
         $question_pool_title = $this->lng->txt('tst_question_not_from_pool_info');
 
-        if (isset($a_set['orig_obj_fi'])) {
+        if (isset($a_set['orig_obj_fi']) && ilObject::_lookupTitle($a_set['orig_obj_fi']) !== null) {
             $question_pool_title = $this->buildPossiblyLinkedQuestonPoolTitle(
                 $this->ctrl,
                 $this->access,
