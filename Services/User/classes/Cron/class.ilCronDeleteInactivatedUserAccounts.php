d52ae66edbfa22f73ebc2b7dc65cefb7b88573a6
Stephan Kergomard <webmaster@kergomard.ch>
2024-01-30 15:57:47 +0100

    User: Fix Copyright

--- Services/User/classes/Cron/class.ilCronDeleteInactivatedUserAccounts.php
+++ Services/User/classes/Cron/class.ilCronDeleteInactivatedUserAccounts.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -18,7 +16,8 @@ declare(strict_types=1);
  *
  *********************************************************************/
 
-use ILIAS\Refinery\ConstraintViolationException;
+declare(strict_types=1);
+
 use ILIAS\Cron\Schedule\CronJobScheduleType;
 
 /**
