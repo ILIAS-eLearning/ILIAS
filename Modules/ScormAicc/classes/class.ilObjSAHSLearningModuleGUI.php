7dffda197f98a1cf655eafda14674eaaaa7fdce0
Uwe Kohnle <kohnle@internetlehrer-gmbh.de>
2024-01-30 15:16:33 +0000

    improvements for checking scorm import files

--- Modules/ScormAicc/classes/class.ilObjSAHSLearningModuleGUI.php
+++ Modules/ScormAicc/classes/class.ilObjSAHSLearningModuleGUI.php
@@ -425,23 +425,19 @@ class ilObjSAHSLearningModuleGUI extends ilObjectGUI
                     $zar->open($tempFile);
                     $zar->extractTo($lmTempDir);
                     $zar->close();
+                    ilFileUtils::renameExecutables($lmTempDir);
                     $importer = new ilScormAiccImporter();
                     $import_dirname = $lmTempDir . '/' . substr($_FILES["scormfile"]["name"], 0, -4);
-                    $importer->importXmlRepresentation("sahs", "", $import_dirname, null);
-                    $importFromXml = true;
-                    //                if ($importer->importXmlRepresentation("sahs", "", $import_dirname, null) == true) {
-                    //                    $importFromXml = true;
-                    //                }
-                    $mprops = $importer->moduleProperties;
                     try {
+                        $importer->importXmlRepresentation("sahs", "", $import_dirname, null);
+                        $importFromXml = true;
+                        $mprops = $importer->moduleProperties;
+
                         $subType = (string) $mprops["SubType"];
                         if ($subType === "scorm") {
                             $newObj = new ilObjSCORMLearningModule();
                         } else {
                             $newObj = new ilObjSCORM2004LearningModule();
-                            // $newObj->setEditable($_POST["editable"]=='y');
-                            // $newObj->setImportSequencing($_POST["import_sequencing"]);
-                            // $newObj->setSequencingExpertMode($_POST["import_sequencing"]);
                         }
                     } catch (\Exception $e) {
                         ilFileUtils::delDir($lmTempDir, false);
