14475e11c2c5291c52c5d252de14170f67c01633
Alexander Killing <killing@leifos.de>
2024-01-31 18:59:49 +0100

    40238: Course cannot be opened / content cannot be displayed

--- Services/Container/classes/class.ilContainer.php
+++ Services/Container/classes/class.ilContainer.php
@@ -693,7 +693,7 @@ class ilContainer extends ilObject
                 $object['type'],
                 ['file', 'fold', 'cat'],
                 true
-            ) && ilObjFileAccess::_isFileHidden($object['title'])) {
+            ) && ilObjFileAccess::_isFileHidden((string) $object['title'])) {
                 $this->setHiddenFilesFound(true);
                 if (!$a_admin_panel_enabled) {
                     continue;
@@ -703,7 +703,7 @@ class ilContainer extends ilObject
 
             // including event items!
             if (!self::$data_preloaded) {
-                $preloader->addItem($object["obj_id"], $object["type"], $object["child"]);
+                $preloader->addItem((int) $object["obj_id"], $object["type"], $object["child"]);
             }
 
             // filter side block items
@@ -711,7 +711,7 @@ class ilContainer extends ilObject
                 continue;
             }
 
-            $all_ref_ids[] = $object["child"];
+            $all_ref_ids[] = (int) $object["child"];
         }
 
         // data preloader
