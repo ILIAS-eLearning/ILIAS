861f83bbb8f322aa28fc16a725d0a2b832d0aef8
Fabian Schmid <fabian@sr.solutions>
2024-02-02 19:57:18 +0100

    [FIX] 0040093: When adding a new File Object, the label for 'Option 2' is missing.
    # Conflicts:
    #	components/ILIAS/File/classes/class.ilObjFileGUI.php

--- components/ILIAS/File/classes/class.ilObjFileGUI.php
+++ components/ILIAS/File/classes/class.ilObjFileGUI.php
@@ -314,8 +314,11 @@ class ilObjFileGUI extends ilObject2GUI
         $this->infoScreen();
     }
 
-    protected function addUIFormToAccordion(ilAccordionGUI $accordion, Standard $form, int $form_type): void
-    {
+    protected function addUIFormToAccordion(
+        ilAccordionGUI $accordion,
+        Standard $form,
+        int $form_type
+    ): void {
         // abort if form-type is unknown
         if (!in_array($form_type, [self::CFORM_NEW, self::CFORM_CLONE, self::CFORM_IMPORT], true)) {
             return;
@@ -341,10 +344,11 @@ class ilObjFileGUI extends ilObject2GUI
         if (!in_array($form_type, [self::CFORM_NEW, self::CFORM_CLONE, self::CFORM_IMPORT], true)) {
             return;
         }
+        $title = $form->getTitle();
         $form->setTitle(''); // see https://mantis.ilias.de/view.php?id=37786
 
-        $tpl = new ilTemplate("tpl.creation_acc_head.html", true, true, "components/ILIAS/Object");
-        $tpl->setVariable("TITLE", $this->lng->txt("option") . " " . $form_type . ": " . $form->getTitle());
+        $tpl = new ilTemplate("tpl.creation_acc_head.html", true, true, "components/Services/Object");
+        $tpl->setVariable("TITLE", $this->lng->txt("option") . " " . $form_type . ": " . $title);
 
         $accordion->addItem($tpl->get(), $form->getHTML());
     }
