0cd25fc636baad39d23e6f7b1d0822814d0bf3f2
Alexander Killing <killing@leifos.de>
2024-02-06 17:01:17 +0100

    40422: Link breaks (Customize) Page - Page becomes unrecoverable

--- components/ILIAS/MediaObjects/classes/class.ilObjMediaObject.php
+++ components/ILIAS/MediaObjects/classes/class.ilObjMediaObject.php
@@ -541,7 +541,7 @@ class ilObjMediaObject extends ilObject
                     // Parameter
                     $parameters = $item->getParameters();
                     foreach ($parameters as $name => $value) {
-                        $xml .= "<Parameter Name=\"$name\" Value=\"$value\"/>";
+                        $xml .= "<Parameter Name=\"$name\" Value=\"" . $this->escapeProperty($value) . "\"/>";
                     }
                     $xml .= $item->getMapAreasXML();
                     $xml .= "</MediaAliasItem>";
@@ -610,7 +610,7 @@ class ilObjMediaObject extends ilObject
                     // Parameter
                     $parameters = $item->getParameters();
                     foreach ($parameters as $name => $value) {
-                        $xml .= "<Parameter Name=\"$name\" Value=\"$value\"/>";
+                        $xml .= "<Parameter Name=\"$name\" Value=\"" . $this->escapeProperty($value) . "\"/>";
                     }
                     $xml .= $item->getMapAreasXML();
 
