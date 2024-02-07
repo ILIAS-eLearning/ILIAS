ce86d6c8b35f20e34f74d510f6c03132a5e12ca2
Fabian Schmid <fabian@sr.solutions>
2024-02-07 19:15:31 +0100

    [FIX] removed unused path methods

--- components/ILIAS/Setup/src/Artifact/BuildArtifactObjective.php
+++ components/ILIAS/Setup/src/Artifact/BuildArtifactObjective.php
@@ -22,7 +22,6 @@ namespace ILIAS\Setup\Artifact;
 
 use ILIAS\Setup;
 
-
 /**
  * This is an objective to build some artifact.
  */
@@ -93,7 +92,7 @@ abstract class BuildArtifactObjective implements Setup\Objective
      */
     public function getLabel(): string
     {
-        return 'Build ' . $this->getArtifactName().' Artifact';
+        return 'Build ' . $this->getArtifactName() . ' Artifact';
     }
 
     /**
@@ -115,43 +114,13 @@ abstract class BuildArtifactObjective implements Setup\Objective
     {
         $artifact = $this->buildIn($environment);
 
-        $path = $this->getAbsoluteArtifactPath();
+        $path = static::PATH();
 
         $this->makeDirectoryFor($path);
 
         file_put_contents($path, $artifact->serialize());
 
         return $environment;
-    }
-
-    private function getRelativeArtifactPath(): string
-    {
-        $here = realpath(__DIR__ . "/../../../../../");
-        return "./" . ltrim(str_replace($here, "", $this->getAbsoluteArtifactPath()), "/");
-    }
-
-    private function getAbsoluteArtifactPath(): string
-    {
-        $here = realpath(__DIR__ . "/../../../../../");
-
-        $artifact_path = static::PATH();
-
-        switch (true) {
-            case strpos($artifact_path, "/") === 0:
-            case strpos($artifact_path, "./") === 0:
-                return $this->realpath($artifact_path);
-                break;
-            case strpos($artifact_path, "../" . self::COMPONENTS_DIRECTORY . "") === 0:
-                return $this->realpath($here . "/" . self::COMPONENTS_DIRECTORY . "/" . $artifact_path);
-                break;
-
-            case strpos($artifact_path, "../") === 0:
-                $dirname = dirname((new \ReflectionClass($this))->getFileName());
-                return $this->realpath($dirname . "/" . $artifact_path);
-                break;
-            default:
-                return $this->realpath($artifact_path);
-        }
     }
 
     public function isApplicable(Setup\Environment $environment): bool
@@ -163,35 +132,9 @@ abstract class BuildArtifactObjective implements Setup\Objective
     {
         $dir = pathinfo($path)["dirname"];
         if (!file_exists($dir)) {
-            mkdir($dir, 0755, true);
-        }
-    }
-
-    /**
-     * @description we cannot use php's realpath because it does not work with with non existing paths.
-     *              Thanks to Beat Christen, https://stackoverflow.com/questions/20522605/what-is-the-best-way-to-resolve-a-relative-path-like-realpath-for-non-existing
-     */
-    protected function realpath(string $filename): string
-    {
-        $path = [];
-        foreach (explode('/', $filename) as $part) {
-            // ignore parts that have no value
-            if (empty($part) || $part === '.') {
-                continue;
-            }
-
-            if ($part !== '..') {
-                // cool, we found a new part
-                $path[] = $part;
-            } elseif (count($path) > 0) {
-                // going back up? sure
-                array_pop($path);
-            } else {
-                // now, here we don't like
-                throw new \RuntimeException('Climbing above the root is not permitted.');
+            if (!mkdir($dir, 0755, true) && !is_dir($dir)) {
+                throw new \RuntimeException(sprintf('Directory "%s" was not created', $dir));
             }
         }
-
-        return "/" . implode('/', $path);
     }
 }
