5e417335c2d45ffbd08598b7caf0f21508d98798
Stephan Kergomard <webmaster@kergomard.ch>
2024-02-07 09:32:51 +0100

    UI: Add Drilldown Example in Modal

---
+++ components/ILIAS/UI/src/examples/Menu/Drilldown/multi_column_drilldown_in_modal.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ILIAS\UI\examples\Menu\Drilldown;
+
+function multi_column_drilldown_in_modal()
+{
+    /**
+        0 Animal of the year
+        1    Switzerland
+        1.1        Riverine Amphipod (gammarus fossarum)
+        1.2        Wildcat
+        1.2.1           European Wildcat
+        1.2.2           African Wildcat
+        2    Germany
+        2.1        Otter
+        2.2        Mole
+                   --divider--
+        2.3        Deer
+    */
+
+
+    global $DIC;
+    /** @var ILIAS\UI\Factory $f */
+    $f = $DIC->ui()->factory();
+    $renderer = $DIC->ui()->renderer();
+
+    $ico = $f->symbol()->icon()->standard('', '')->withSize('small')->withAbbreviation('+');
+    $image = $f->image()->responsive("components/ILIAS/UI/src/examples/Image/mountains.jpg", "Image source: https://stocksnap.io, Creative Commons CC0 license");
+    $page = $f->modal()->lightboxImagePage($image, 'Mountains');
+    $modal = $f->modal()->lightbox($page);
+    $button = $f->button()->bulky($ico->withAbbreviation('>'), 'Modal', '')
+        ->withOnClick($modal->getShowSignal());
+
+    $uri = new \ILIAS\Data\URI('https://ilias.de');
+    $link = $f->link()->bulky($ico->withAbbreviation('>'), 'Link', $uri);
+    $divider = $f->divider()->horizontal();
+
+    $items = [
+        $f->menu()->sub('Switzerland', [
+            $f->menu()->sub('Riverine Amphipod', [$button, $link]),
+            $f->menu()->sub('Wildcat', [
+                $f->menu()->sub('European Wildcat', [$button, $link]),
+                $f->menu()->sub('African Wildcat', [$button, $link, $divider, $link])
+            ]),
+            $button,
+            $link
+        ]),
+
+        $f->menu()->sub('Germany', [
+            $f->menu()->sub('Otter', [$button, $link]),
+            $f->menu()->sub('Mole', [$button, $link]),
+            $divider,
+            $f->menu()->sub('Deer', [$button, $link])
+        ])
+    ];
+
+    $drilldown_modal = $f->modal()->roundtrip(
+        'My Modal',
+        [$f->menu()->drilldown('Animal of the year', $items)]
+    );
+
+    $button = $f->button()->standard('Open Animals', $drilldown_modal->getShowSignal());
+
+    return $renderer->render([
+        $button,
+        $drilldown_modal,
+        $modal
+    ]);
+}
