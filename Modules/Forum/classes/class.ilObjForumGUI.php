beb3be54ee0f19ee7328f454d9fc141a90827f07
Nadia Matuschek <nmatuschek@databay.de>
2024-02-09 10:24:59 +0100

    Bugfix #0040454: Backtarget 'All threads' redirects to repository (#7042)

--- Modules/Forum/classes/class.ilObjForumGUI.php
+++ Modules/Forum/classes/class.ilObjForumGUI.php
@@ -1724,10 +1724,12 @@ class ilObjForumGUI extends ilObjectGUI implements ilDesktopItemHandling, ilForu
         $this->tpl->setTitleIcon(ilObject::_getIcon(0, "big", "frm"));
 
         $ref_id = $this->retrieveRefId();
-
         $this->tabs_gui->setBackTarget(
             $this->lng->txt('frm_all_threads'),
-            'ilias.php?baseClass=ilRepositoryGUI&amp;ref_id=' . $ref_id
+            $this->ctrl->getLinkTarget(
+                $this,
+                'showThreads'
+            )
         );
 
         /** @var ilForum $frm */
