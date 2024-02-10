740b07cceffe53a116a4968342b57e16102f5f34
Alexander Killing <killing@leifos.de>
2024-02-10 14:20:33 +0100

    37985: Tabellen-Templates in Content-Styles lassen sich nicht bearbeiten

--- Services/Style/Content/classes/class.ilObjStyleSheetGUI.php
+++ Services/Style/Content/classes/class.ilObjStyleSheetGUI.php
@@ -1134,10 +1134,10 @@ class ilObjStyleSheetGUI extends ilObjectGUI
         $ctype = $this->style_request->getTempType();
         if ($ctype == "") {
             $ctype = "table";
-            $ilCtrl->setParameter($this, "temp_type", $ctype);
         }
 
         $this->setTemplatesSubTabs();
+        $ilCtrl->setParameter($this, "temp_type", $ctype);
         $ilTabs->setSubTabActive("sty_" . $ctype . "_templates");
 
         // action commands
@@ -1344,6 +1344,7 @@ class ilObjStyleSheetGUI extends ilObjectGUI
         $scs = ilObjStyleSheet::_getTemplateClassTypes(
             $this->style_request->getTempType()
         );
+
         foreach ($scs as $sc => $st) {
             $sc_input = new ilSelectInputGUI($lng->txt("sty_" . $sc . "_class"), $sc . "_class");
             $chars = $this->object->getCharacteristics($st);
