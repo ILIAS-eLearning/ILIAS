6f2d74ae11a2bf4fccd41dca749938ef6d44a155
Nils Haagen <nhaagen@cat06.de>
2024-02-15 09:47:20 +0100

    UI/Input/DateTime: 39859, add 'not null' to required-constraint of DateTime

--- src/UI/Implementation/Component/Input/Field/DateTime.php
+++ src/UI/Implementation/Component/Input/Field/DateTime.php
@@ -207,8 +207,12 @@ class DateTime extends FormInput implements C\Input\Field\DateTime
             return $this->requirement_constraint;
         }
 
-        return $this->refinery->string()->hasMinLength(1)
-            ->withProblemBuilder(fn($txt, $value) => $txt("datetime_required"));
+        return $this->refinery->logical()->sequential([
+            $this->refinery->logical()->not($this->refinery->null()),
+            $this->refinery->string()->hasMinLength(1)
+        ])
+        ->withProblemBuilder(fn($txt, $value) => $txt("datetime_required"));
+
     }
 
     /**
