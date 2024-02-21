97f1eab1b07aa3b947d5ae345d712ff7ec938662
Stephan Kergomard <webmaster@kergomard.ch>
2024-02-21 17:48:35 +0100

    Test: Fix Undefined Variable in Imports

--- components/ILIAS/TestQuestionPool/classes/class.ilObjQuestionPoolGUI.php
+++ components/ILIAS/TestQuestionPool/classes/class.ilObjQuestionPoolGUI.php
@@ -700,7 +700,7 @@ class ilObjQuestionPoolGUI extends ilObjectGUI implements ilCtrlBaseClassInterfa
         $new_obj->setPermissions($this->qplrequest->getRefId());
 
         $selected_questions = $this->retrieveSelectedQuestionsFromImportQuestionsSelectionForm(
-            $import_cmd,
+            'importVerifiedFile',
             $importdir,
             $qtifile
         );
