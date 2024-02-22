edda06869a6428a57492edf3292b0472f186651b
Stephan Kergomard <webmaster@kergomard.ch>
2024-02-22 17:37:44 +0100

    RBAC: Fix Anonymous Can Be Assigned To Role
    See: https://mantis.ilias.de/view.php?id=40608

--- Services/AccessControl/classes/class.ilObjRoleGUI.php
+++ Services/AccessControl/classes/class.ilObjRoleGUI.php
@@ -818,8 +818,12 @@ class ilObjRoleGUI extends ilObjectGUI
         }
 
         // assign new users
-        foreach ($assigned_users_new as $user) {
-            $this->rbacadmin->assignUser($this->object->getId(), $user);
+        foreach ($assigned_users_new as $user_id) {
+            if ($user_id === ANONYMOUS_USER_ID) {
+                $this->tpl->setOnScreenMessage('failure', $this->lng->txt('msg_anonymous_cannot_be_assigned'), true);
+                return;
+            }
+            $this->rbacadmin->assignUser($this->object->getId(), $user_id, false);
         }
 
         // update object data entry (to update last modification date)
