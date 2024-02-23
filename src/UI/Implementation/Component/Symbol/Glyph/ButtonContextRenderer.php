ee5d80cfd35479fcd3e77a52313a0d1b25fb5e05
Timon Amstutz <timon.amstutz@bluewin.ch>
2024-02-23 16:39:13 +0100

    Fix of #39992 und #37178

--- src/UI/Implementation/Component/Symbol/Glyph/ButtonContextRenderer.php
+++ src/UI/Implementation/Component/Symbol/Glyph/ButtonContextRenderer.php
@@ -37,6 +37,17 @@ class ButtonContextRenderer extends Renderer
 
     protected function renderLabel(Component\Component $component, Template $tpl): Template
     {
+        $aria_label = "";
+        foreach ($component->getCounters() as $counter) {
+            if($counter->getNumber() > 0) {
+                $aria_label .= $this->txt("counter_".$counter->getType()). " ".$counter->getNumber(). "; ";
+            }
+        }
+
+        if($aria_label != "") {
+            $tpl->setVariable("LABEL", $aria_label);
+        }
+
         return $tpl;
     }
 }
