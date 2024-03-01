26ea19faaf9af6500817d0376e95f2c5832335c8
Fabian Schmid <fabian@sr.solutions>
2024-03-01 16:14:18 +0100

    [FIX] use UTF8 for deprecated function calls

--- components/ILIAS/FileServices/classes/class.ilFileUtils.php
+++ components/ILIAS/FileServices/classes/class.ilFileUtils.php
@@ -68,30 +68,12 @@ class ilFileUtils
     }
 
     /**
-     * utf8-encodes string if it is not a valid utf8-string.
-     *
-     * @param string $string String to encode
-     *
-     * @return string utf-8-encoded string
-     * @author  Jan Hippchen
-     * @version 1.12.3.08
+     * @deprecated in ILIAS 9 for ILIAS 10: Use Refinery\String\UTF8 instead
      */
     public static function utf8_encode(string $string): string
     {
-        // From http://w3.org/International/questions/qa-forms-utf-8.html
-        return (preg_match(
-            '%^(?:
-			[\x09\x0A\x0D\x20-\x7E]            # ASCII
-			| [\xC2-\xDF][\x80-\xBF]             # non-overlong 2-byte
-			|  \xE0[\xA0-\xBF][\x80-\xBF]        # excluding overlongs
-			| [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}  # straight 3-byte
-			|  \xED[\x80-\x9F][\x80-\xBF]        # excluding surrogates
-			|  \xF0[\x90-\xBF][\x80-\xBF]{2}     # planes 1-3
-			| [\xF1-\xF3][\x80-\xBF]{3}          # planes 4-15
-			|  \xF4[\x80-\x8F][\x80-\xBF]{2}     # plane 16
-			)*$%xs',
-            $string
-        )) ? $string : utf8_encode($string);
+        global $DIC;
+        return $DIC->refinery()->string()->utf8()->transform($string);
     }
 
     /**
