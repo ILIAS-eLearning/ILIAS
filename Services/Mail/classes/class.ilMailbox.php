775393f97afd6011baec7b2f87e41669226117d3
mjansen <mjansen@databay.de>
2024-03-06 09:09:07 +0100

    Mail: Add workaround for corrupted root node
    See: https://mantis.ilias.de/view.php?id=40716

--- Services/Mail/classes/class.ilMailbox.php
+++ Services/Mail/classes/class.ilMailbox.php
@@ -50,10 +50,14 @@ class ilMailbox
     protected string $table_mail_obj_data;
     protected string $table_tree;
 
-    public function __construct(int $a_user_id = 0)
+    public function __construct(int $a_user_id)
     {
         global $DIC;
 
+        if ($a_user_id < 1) {
+            throw new InvalidArgumentException("Cannot create mailbox without user id");
+        }
+        
         $this->lng = $DIC->language();
         $this->db = $DIC->database();
 
@@ -61,10 +65,8 @@ class ilMailbox
         $this->table_mail_obj_data = 'mail_obj_data';
         $this->table_tree = 'mail_tree';
 
-        if ($this->usrId) {
-            $this->mtree = new ilTree($this->usrId);
-            $this->mtree->setTableNames($this->table_tree, $this->table_mail_obj_data);
-        }
+        $this->mtree = new ilTree($this->usrId);
+        $this->mtree->setTableNames($this->table_tree, $this->table_mail_obj_data);
 
         // i added this, becaus if i create a new user automatically during
         // CAS authentication, we have no $lng variable (alex, 16.6.2006)
@@ -79,6 +81,19 @@ class ilMailbox
                 'deleteMails' => $this->lng->txt('delete'),
             ];
         }
+    }
+
+    public function getRooFolder(): int
+    {
+        $res = $this->db->queryF(
+            'SELECT obj_id FROM ' . $this->table_mail_obj_data . ' WHERE user_id = %s AND m_type = %s',
+            ['integer', 'text'],
+            [$this->usrId, 'root']
+        );
+
+        $row = $this->db->fetchAssoc($res);
+
+        return (int) $row['obj_id'];
     }
 
     public function getInboxFolder(): int
@@ -131,11 +146,6 @@ class ilMailbox
         $row = $this->db->fetchAssoc($res);
 
         return (int) $row['obj_id'];
-    }
-
-    private function getRootFolderId(): int
-    {
-        return $this->mtree->getRootId();
     }
 
     /**
