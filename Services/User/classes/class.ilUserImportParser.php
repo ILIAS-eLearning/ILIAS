3f11f411b72a29ac2d9a6d0046bac11f64588764
Stephan Kergomard <webmaster@kergomard.ch>
2024-03-06 15:51:16 +0100

    User: Current Role Id May Be Int
    In the spirit of Polstel's Law a string might also be an int
    
    See: https://mantis.ilias.de/view.php?id=40730

--- Services/User/classes/class.ilUserImportParser.php
+++ Services/User/classes/class.ilUserImportParser.php
@@ -411,7 +411,7 @@ class ilUserImportParser extends ilSaxParser
                 if (($internal_id = ilUtil::__extractId($current_role_id, (int) IL_INST_ID)) > 0) {
                     $current_role_id = $internal_id;
                 }
-                $this->current_role_id = $current_role_id;
+                $this->current_role_id = (string) $current_role_id;
                 $this->current_role_type = $a_attribs["Type"];
                 $this->current_role_action = (!isset($a_attribs["Action"])) ? "Assign" : $a_attribs["Action"];
                 break;
