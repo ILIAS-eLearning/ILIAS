fac084e925fc24759cff9b8ac65bae75d5d72a04
Alexander Killing <killing@leifos.de>
2024-03-10 09:28:05 +0100

    40158: Failed test: Anzahl angezeigter Objekte beschränken

--- Services/Container/Content/ItemBlock/class.ItemBlockSequenceGenerator.php
+++ Services/Container/Content/ItemBlock/class.ItemBlockSequenceGenerator.php
@@ -67,9 +67,12 @@ class ItemBlockSequenceGenerator
         $this->block_sequence = $block_sequence;
         $this->item_set_manager = $item_set_manager;
         $this->container = $container;
-        $this->block_limit = (int) \ilContainer::_lookupContainerSetting($container->getId(), "block_limit");
         $this->mode_manager = $this->domain_service->content()->mode($container);
         $this->include_empty_blocks = $include_empty_blocks;
+        $this->block_limit = 0;
+        if (!$this->mode_manager->isActiveItemOrdering()) {
+            $this->block_limit = (int) \ilContainer::_lookupContainerSetting($container->getId(), "block_limit");
+        }
     }
 
     public function getSequence(): ItemBlockSequence
