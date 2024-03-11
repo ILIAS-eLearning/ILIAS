0176ed83f5536ba4173d8f6135f4e51a09826ecc
Stephan Kergomard <webmaster@kergomard.ch>
2024-03-11 08:05:32 +0100

    Test: Question Title Is Nullable
    See: https://mantis.ilias.de/view.php?id=40064#c102476

--- Modules/TestQuestionPool/classes/class.ilAssQuestionList.php
+++ Modules/TestQuestionPool/classes/class.ilAssQuestionList.php
@@ -664,8 +664,8 @@ class ilAssQuestionList implements ilTaxAssignedItemInfo
                 continue;
             }
 
-            $row['title'] = $tags_trafo->transform($row['title']);
-            $row['description'] = $tags_trafo->transform($row['description'] !== '' && $row['description'] !== null  ? $row['description'] : '&nbsp;');
+            $row['title'] = $tags_trafo->transform($row['title'] ?? '&nbsp;');
+            $row['description'] = $tags_trafo->transform($row['description'] !== '' && $row['description'] !== null ? $row['description'] : '&nbsp;');
             $row['author'] = $tags_trafo->transform($row['author']);
             $row['taxonomies'] = $this->loadTaxonomyAssignmentData($row['obj_fi'], $row['question_id']);
             $row['ttype'] = $this->lng->txt($row['type_tag']);
