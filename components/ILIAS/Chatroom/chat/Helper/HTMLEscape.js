7a2eab84659b0e3bfd514d5c6f3578eab0547fb0
Lukas Scharmer <lscharmer@databay.de>
2024-03-13 13:34:53 +0100

    Chatroom: Server send message linting

--- components/ILIAS/Chatroom/chat/Helper/HTMLEscape.js
+++ components/ILIAS/Chatroom/chat/Helper/HTMLEscape.js
@@ -1,4 +1,19 @@
 /**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ */
+
+/**
  * @constructor
  */
 function HTMLEscape() {}
@@ -7,12 +22,12 @@ function HTMLEscape() {}
  * @returns {number}
  */
 HTMLEscape.prototype.escape = function (html) {
-	return String(html)
-		.replace(/&(?!\w+;)/g, '&amp;')
-		.replace(/</g, '&lt;')
-		.replace(/>/g, '&gt;')
-		.replace(/"/g, '&quot;')
-		.replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, '?');
+  return String(html)
+    .replace(/&(?!\w+;)/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, '?');
 };
 
 /**
--- components/ILIAS/Chatroom/chat/SocketTasks/SendMessage.js
+++ components/ILIAS/Chatroom/chat/SocketTasks/SendMessage.js
@@ -1,40 +1,55 @@
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ */
+
 const Container = require('../AppContainer');
 const TextMessage = require('../Model/Messages/TextMessage');
 const TargetMessage = require('../Model/Messages/TargetMessage');
 
 module.exports = function (data, roomId) {
-	const serverRoomId = Container.createServerRoomId(roomId);
-	const namespace = Container.getNamespace(this.nsp.name);
+  const serverRoomId = Container.createServerRoomId(roomId);
+  const namespace = Container.getNamespace(this.nsp.name);
 
-	Container.getLogger().info('Message send to room %s of namespace %s', serverRoomId, namespace.getName());
-	if (typeof this.subscriber === "undefined") {
-		Container.getLogger().error("Missing subscriber, don't process message");
-		return;
-	}
+  Container.getLogger().info('Message send to room %s of namespace %s', serverRoomId, namespace.getName());
+  if (typeof this.subscriber === 'undefined') {
+    Container.getLogger().error("Missing subscriber, don't process message");
+    return;
+  }
 
-	const subscriber = {id: this.subscriber.getId(), username: this.subscriber.getName()};
-	let message = {};
+  const subscriber = { id: this.subscriber.getId(), username: this.subscriber.getName() };
+  let message = {};
 
-	if (data.target !== undefined) {
-		message = TargetMessage.create(data.content, roomId, subscriber, data.format, data.target);
+  if (data.target !== undefined) {
+    message = TargetMessage.create(data.content, roomId, subscriber, data.format, data.target);
 
-		if (message.target.public) {
-			namespace.getIO().in(serverRoomId).emit('message', message);
-		} else {
-			const target = namespace.getSubscriber(message.target.id);
-			const from = namespace.getSubscriber(message.from.id);
+    if (message.target.public) {
+      namespace.getIO().in(serverRoomId).emit('message', message);
+    } else {
+      const target = namespace.getSubscriber(message.target.id);
+      const from = namespace.getSubscriber(message.from.id);
 
-			const emitMessage = function (socketId) {
-				namespace.getIO().to(socketId).emit('message', message);
-			};
+      const emitMessage = function (socketId) {
+        namespace.getIO().to(socketId).emit('message', message);
+      };
 
-			from.getSocketIds().forEach(emitMessage);
-			target.getSocketIds().forEach(emitMessage);
-		}
-	} else {
-		message = TextMessage.create(data.content, roomId, subscriber, data.format);
-		this.nsp.in(serverRoomId).emit('message', message);
-	}
+      from.getSocketIds().forEach(emitMessage);
+      target.getSocketIds().forEach(emitMessage);
+    }
+  } else {
+    message = TextMessage.create(data.content, roomId, subscriber, data.format);
+    this.nsp.in(serverRoomId).emit('message', message);
+  }
 
-	namespace.getDatabase().persistMessage(message);
+  namespace.getDatabase().persistMessage(message);
 };
