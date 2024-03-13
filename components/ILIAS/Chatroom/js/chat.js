aa6dc52f37cb1930120bb37263499bbfcbcbb6a4
Lukas Scharmer <lscharmer@databay.de>
2024-03-13 12:53:10 +0100

    Chatroom: Remove server side HTML escaping for chat messages

--- components/ILIAS/Chatroom/js/chat.js
+++ components/ILIAS/Chatroom/js/chat.js
@@ -620,7 +620,7 @@
               }
 
               var messageSpan = $('<span class="chat content message"></span>');
-              messageSpan.text(messageSpan.text(content).text());
+              messageSpan.text(content);
               line.append($('<span class="chat content messageseparator">:</span>'))
                 .append(messageSpan);
 
