2a3c58000c9ef297489e556e4ad63d6b76b14dca
Thibeau Fuhrer <thibeau@sr.solutions>
2024-03-15 16:18:59 +0100

    [FIX] UI: Improve documentation about plugin manipulations (#7132)
    * [FIX] Docs: improve examples and docs about UI plugin manipulations.
    
    * [FIX] Docs: typos in docs about UI plugin manipulations.
    
    * [FIX] Docs: change ui manipulation example paths.
    
    Co-authored-by: Richard Klees <richard.klees@concepts-and-training.de>
    
    ---------
    
    Co-authored-by: Richard Klees <richard.klees@concepts-and-training.de>

---
+++ docs/development/code-examples/ui-exchange/custom-components/HelloWorld.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ */
+
+declare(strict_types=1);
+
+use ILIAS\UI\Implementation\Component\ComponentHelper;
+use ILIAS\UI\Component\Component;
+
+/**
+ * Some very basic component implementation.
+ */
+class HelloWorld implements Component
+{
+    use ComponentHelper;
+
+    protected $greeting = 'Hello World!';
+
+    public function withGreeting(string $greeting): self
+    {
+        $clone = clone $this;
+        $clone->greeting = $greeting;
+        return $clone;
+    }
+
+    public function getGreeting(): string
+    {
+        return $this->greeting;
+    }
+}
