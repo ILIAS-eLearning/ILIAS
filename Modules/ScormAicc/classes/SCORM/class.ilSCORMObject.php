936045ffb703e103a8ec5406c1b2859c691319c4
nmatuschek <nmatuschek@databay.de>
2024-03-15 09:40:33 +0100

    Fixed copyright check

--- Modules/ScormAicc/classes/SCORM/class.ilSCORMObject.php
+++ Modules/ScormAicc/classes/SCORM/class.ilSCORMObject.php
@@ -1,6 +1,5 @@
 <?php
 
-declare(strict_types=1);
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -25,6 +24,8 @@ declare(strict_types=1);
 *
 * @ingroup ModulesScormAicc
 */
+
+declare(strict_types=1);
 class ilSCORMObject
 {
     public int $id;
