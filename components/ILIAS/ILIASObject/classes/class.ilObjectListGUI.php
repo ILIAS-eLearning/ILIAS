f9d06e6e2e26b712c6cd3879057b1a7318efef78
Stephan Kergomard <webmaster@kergomard.ch>
2024-03-28 08:07:45 +0100

    Object: Fix Title and Description Output
    See: https://mantis.ilias.de/view.php?id=40990

--- components/ILIAS/ILIASObject/classes/class.ilObjectListGUI.php
+++ components/ILIAS/ILIASObject/classes/class.ilObjectListGUI.php
@@ -569,7 +569,10 @@ class ilObjectListGUI
     }
     public function setTitle(string $title): void
     {
-        $this->title = $title;
+        $this->title = strip_tags(
+            $title,
+            ilObjectGUI::ALLOWED_TAGS_IN_TITLE_AND_DESCRIPTION
+        );
     }
 
     /**
@@ -582,7 +585,10 @@ class ilObjectListGUI
 
     public function setDescription(string $description): void
     {
-        $this->description = $description;
+        $this->description = strip_tags(
+            $description,
+            ilObjectGUI::ALLOWED_TAGS_IN_TITLE_AND_DESCRIPTION
+        );
     }
 
     /**
@@ -2474,7 +2480,7 @@ class ilObjectListGUI
             }
         }
 
-        $this->title = ilObject::_lookupTitle($this->obj_id);
+        $this->setTitle(ilObject::_lookupTitle($this->obj_id));
         $dropdown_label = '<span class="hidden-xs">' .
                 $this->lng->txt('actions') .
                 '</span>';
