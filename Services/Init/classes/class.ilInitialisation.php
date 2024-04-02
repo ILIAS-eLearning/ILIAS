52d4d2ca92154d1d335410838dc7a01359fa3cc0
Jephte Abijuru <57417057+jeph864@users.noreply.github.com>
2024-04-02 11:15:56 +0200

    Merge pull request #7116 from mjansenDatabay/hotfix/8/37780
    SOAP/Init: Fix support for SOAP plugins (at least partly)

--- Services/Init/classes/class.ilInitialisation.php
+++ Services/Init/classes/class.ilInitialisation.php
@@ -1098,6 +1098,18 @@ class ilInitialisation
         self::initILIAS();
     }
 
+    public static function reInitUser(): void
+    {
+        if (ilContext::initClient() && ilContext::hasUser()) {
+            self::initSession();
+            self::initUser();
+
+            if (ilContext::supportsPersistentSessions()) {
+                self::resumeUserSession();
+            }
+        }
+    }
+
     /**
      * ilias initialisation
      */
