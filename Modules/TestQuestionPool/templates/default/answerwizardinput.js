b8f156b2b9f5ca225df00ccfe029765445b14bc8
Marvin Beym <mBeym@databay.de>
2024-04-03 14:05:00 +0200

    Fix answerwizardinput not finding + / - / up / down buttons

--- Modules/TestQuestionPool/templates/default/answerwizardinput.js
+++ Modules/TestQuestionPool/templates/default/answerwizardinput.js
@@ -1,109 +1,123 @@
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
 var AnswerWizardInput = {
 	init: function () {
-		this.initEvents($(this.tag_container));
-	},
+    this.initEvents($(this.tag_container));
+  },
 
-	initEvents: function (rootel) {
-		var that = this;
-		$(rootel).find('div.' + this.tag_button + '_add .glyph').click((e) => {
-			that.addRow(e);
-		});
-		$(rootel).find('div.' + this.tag_button + '_remove .glyph').click((e) => {
-			that.removeRow(e);
-		});
-		$(rootel).find('div.' + this.tag_button + '_up .glyph').click((e) => {
-			that.moveRowUp(e);
-		});
-		$(rootel).find('div.' + this.tag_button + '_down .glyph').click((e) => {
-			that.moveRowDown(e);
-		});
-	},
+  initEvents(rootel) {
+    const that = this;
+    $(rootel).find(`button.${this.tag_button}_add .glyph`).click((e) => {
+      that.addRow(e);
+    });
+    $(rootel).find(`button.${this.tag_button}_remove .glyph`).click((e) => {
+      that.removeRow(e);
+    });
+    $(rootel).find(`button.${this.tag_button}_up .glyph`).click((e) => {
+      that.moveRowUp(e);
+    });
+    $(rootel).find(`button.${this.tag_button}_down .glyph`).click((e) => {
+      that.moveRowDown(e);
+    });
+  },
 
-    onClickHandler: function (action, e) {
-        e.preventDefault();
-        const id_tag = e.currentTarget.parentNode.id.split('[');
-        const id = id_tag.pop().slice(0, -1);
-        const target = id_tag[0].split('_').pop();
-        let button = document.createElement('BUTTON');
-        button.type = 'submit';
-        button.name = 'cmd[' + action + target  + '][' + id + ']';
-        button.style.display = 'none';
-        e.target.insertAdjacentElement('afterend', button);
-        button.form.requestSubmit(button);
-    },
+  onClickHandler(action, e) {
+    e.preventDefault();
+    const id_tag = e.currentTarget.parentNode.id.split('[');
+    const id = id_tag.pop().slice(0, -1);
+    const target = id_tag[0].split('_').pop();
+    const button = document.createElement('BUTTON');
+    button.type = 'submit';
+    button.name = `cmd[${action}${target}][${id}]`;
+    button.style.display = 'none';
+    e.target.insertAdjacentElement('afterend', button);
+    button.form.requestSubmit(button);
+  },
 
-	addRow: function (e) {
-		// clone row
-		var source = this.getRowFromEvent(e);
-		var target = $(source).clone();
+  addRow(e) {
+    // clone row
+    const source = this.getRowFromEvent(e);
+    const target = $(source).clone();
 
-		// add events
-		this.initEvents(target);
+    // add events
+    this.initEvents(target);
 
-		// empty inputs
-		this.cleanRow(target);
+    // empty inputs
+    this.cleanRow(target);
 
-		$(source).after(target);
+    $(source).after(target);
 
-		this.reindexRows(this.getContainerFromEvent(e));
+    this.reindexRows(this.getContainerFromEvent(e));
 
-		il.Form.registerFileUploadInputEventTrigger();
+    il.Form.registerFileUploadInputEventTrigger();
 
-		let current_upload_fields = $("#files").children().length;
-		let max_upload_fields = parseInt(source.children(".imagewizard_add").attr("data-val"));
-		if(current_upload_fields === max_upload_fields) {
-			$(".imagewizard_add").hide();
-		}
-	},
+    const current_upload_fields = $('#files').children().length;
+    const max_upload_fields = parseInt(source.children('.imagewizard_add').attr('data-val'));
+    if (current_upload_fields === max_upload_fields) {
+      $('.imagewizard_add').hide();
+    }
+  },
 
-	removeRow: function (e) {
-		var source = this.getRowFromEvent(e);
-		var tbody = this.getContainerFromEvent(e);
+  removeRow(e) {
+    const source = this.getRowFromEvent(e);
+    const tbody = this.getContainerFromEvent(e);
 
-		// do not remove last row
-		if($(tbody).find(this.tag_row).size() > 1) {
-			$(source).remove();
-		}
-		// reset last remaining row
-		else {
-			this.cleanRow(source);
-		}
+    // do not remove last row
+    if ($(tbody).find(this.tag_row).size() > 1) {
+      $(source).remove();
+    }
+    // reset last remaining row
+    else {
+      this.cleanRow(source);
+    }
 
-		this.reindexRows(tbody);
+    this.reindexRows(tbody);
 
-		let current_upload_fields = $("#files").children().length;
-		let max_upload_fields = parseInt(source.children(".imagewizard_remove").attr("data-val"));
-		if(current_upload_fields <= max_upload_fields) {
-			$(".imagewizard_add").show();
-		}
-	},
+    const current_upload_fields = $('#files').children().length;
+    const max_upload_fields = parseInt(source.children('.imagewizard_remove').attr('data-val'));
+    if (current_upload_fields <= max_upload_fields) {
+      $('.imagewizard_add').show();
+    }
+  },
 
-	moveRowUp: function (e) {
-		var source = this.getRowFromEvent(e);
-		var prev = $(source).prev();
-		if(prev[0])
-		{
-			$(prev).before(source);
+  moveRowUp(e) {
+    const source = this.getRowFromEvent(e);
+    const prev = $(source).prev();
+    if (prev[0]) {
+      $(prev).before(source);
 
-			this.reindexRows(this.getContainerFromEvent(e));
-		}
-	},
+      this.reindexRows(this.getContainerFromEvent(e));
+    }
+  },
 
-	moveRowDown: function (e) {
-		var source = this.getRowFromEvent(e);
-		var next = $(source).next();
-		if(next[0])
-		{
-			$(next).after(source);
+  moveRowDown(e) {
+    const source = this.getRowFromEvent(e);
+    const next = $(source).next();
+    if (next[0]) {
+      $(next).after(source);
 
-			this.reindexRows(this.getContainerFromEvent(e));
-		}
-	},
+      this.reindexRows(this.getContainerFromEvent(e));
+    }
+  },
 
-	handleId: function (el, attr, new_idx) {
-		var parts = $(el).attr(attr).split('[');
-		parts.pop();
-		parts.push(new_idx + ']');
-		$(el).attr(attr, parts.join('['));
-	}
+  handleId(el, attr, new_idx) {
+    const parts = $(el).attr(attr).split('[');
+    parts.pop();
+    parts.push(`${new_idx}]`);
+    $(el).attr(attr, parts.join('['));
+  },
 };
