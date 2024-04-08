66f4a379facde1ea0401beca5a3a56871c50fb15
Nils Haagen <nhaagen@cat06.de>
2024-04-08 13:30:31 +0200

    OrgU: LUI, update general settings form

--- components/ILIAS/OrgUnit/classes/Settings/class.ilOrgUnitGlobalSettingsGUI.php
+++ components/ILIAS/OrgUnit/classes/Settings/class.ilOrgUnitGlobalSettingsGUI.php
@@ -16,7 +16,11 @@
  ********************************************************************
  */
 
-/* Copyright (c) 1998-2010 ILIAS open source, Extended GPL, see docs/LICENSE */
+use ILIAS\UI\Factory as UIFactory;
+use ILIAS\UI\Renderer as UIRenderer;
+use ILIAS\Refinery\Factory as Refinery;
+use Psr\Http\Message\ServerRequestInterface;
+use ILIAS\UI\Component\Input\Container\Form\Standard as StandardForm;
 
 /**
  * Global orgunit settings GUI
@@ -25,9 +29,19 @@
  */
 class ilOrgUnitGlobalSettingsGUI
 {
+    protected const CMD_EDIT = 'edit';
+    protected const CMD_SAVE = 'save';
+
     protected ilCtrl $ctrl;
     protected ilLanguage $lng;
     protected ilGlobalTemplateInterface $tpl;
+
+    protected ilSetting $settings;
+    protected ilObjectDefinition $object_definition;
+    protected UIFactory $ui_factory;
+    protected UIRenderer $ui_renderer;
+    protected Refinery $refinery;
+    protected ServerRequestInterface $request;
 
     public function __construct()
     {
@@ -38,139 +52,179 @@ class ilOrgUnitGlobalSettingsGUI
         $this->lng = $DIC->language();
         $this->lng->loadLanguageModule('orgu');
         $this->tpl = $DIC->ui()->mainTemplate();
+        $this->request = $DIC->http()->request();
+        $to_int = $DIC['refinery']->kindlyTo()->int();
+        $ref_id = $DIC['http']->wrapper()->query()->retrieve('ref_id', $to_int);
 
-        if (!ilObjOrgUnitAccess::_checkAccessSettings((int) $_GET['ref_id'])) {
+        if (!ilObjOrgUnitAccess::_checkAccessSettings($ref_id)) {
             $main_tpl->setOnScreenMessage('failure', $this->lng->txt("permission_denied"), true);
             $this->ctrl->redirectByClass(ilObjOrgUnitGUI::class);
         }
+
+        $this->settings = $DIC->settings();
+        $this->object_definition = $DIC["objDefinition"];
+        $this->ui_factory = $DIC['ui.factory'];
+        $this->ui_renderer = $DIC['ui.renderer'];
+        $this->refinery = $DIC['refinery'];
     }
 
     public function executeCommand(): void
     {
-        $cmd = $this->ctrl->getCmd('settings');
+        $cmd = $this->ctrl->getCmd(self::CMD_EDIT);
         $next_class = $this->ctrl->getNextClass($this);
 
         switch ($next_class) {
             default:
-                $this->$cmd();
-                break;
+                switch($cmd) {
+                    case self::CMD_SAVE:
+                        $this->save();
+                        break;
+                    case self::CMD_EDIT:
+                    default:
+                        $this->edit();
+                }
         }
     }
 
-    private function settings(ilPropertyFormGUI $form = null): void
+    private function edit(): void
     {
-        if (!$form instanceof ilPropertyFormGUI) {
-            $form = $this->initSettingsForm();
-        }
-        $this->tpl->setContent($form->getHTML());
+        $form = $this->getSettingsForm();
+        $this->tpl->setContent(
+            $this->ui_renderer->render($form)
+        );
     }
 
-    private function initSettingsForm(): ilPropertyFormGUI
+    private function save(): void
     {
-        global $DIC;
+        $form = $this->getSettingsForm()->withRequest($this->request);
+        $data = $form->getData();
 
-        $form = new ilPropertyFormGUI();
-        $form->setFormAction($this->ctrl->getFormAction($this, 'saveSettings'));
+        if(!$data) {
+            $this->tpl->setOnScreenMessage('failure', $this->lng->txt('err_check_input'), false);
+            $this->tpl->setContent($this->ui_renderer->render($form));
+        }
+        $enable_my_staff = current(array_shift($data));
+        $obj_settings = array_shift($data);
 
-        // My Staff
-        $section = new ilFormSectionHeaderGUI();
-        $section->setTitle($this->lng->txt('orgu_enable_my_staff'));
-        $form->addItem($section);
-
-        $item = new ilCheckboxInputGUI($this->lng->txt("orgu_enable_my_staff"), "enable_my_staff");
-        $item->setInfo($this->lng->txt("orgu_enable_my_staff_info"));
-        $item->setValue("1");
-        $item->setChecked(($DIC->settings()->get("enable_my_staff") ? true : false));
-        $form->addItem($item);
-
-        // Positions in Modules
-        $section = new ilFormSectionHeaderGUI();
-        $section->setTitle($this->lng->txt('orgu_global_set_positions'));
-        $form->addItem($section);
-
-        $objDefinition = $DIC['objDefinition'];
-        $available_types = $objDefinition->getOrgUnitPermissionTypes();
+        $available_types = $this->object_definition->getOrgUnitPermissionTypes();
         foreach ($available_types as $object_type) {
+
+            $active = false;
+            $changeable = false;
+            $default = false;
+            if(!is_null($obj_settings[$object_type])) {
+                list($active, $changeable, $default) = array_shift($obj_settings[$object_type]);
+            }
+            $obj_setting = new ilOrgUnitObjectTypePositionSetting($object_type);
+            $obj_setting->setActive($active);
+            $obj_setting->setChangeableForObject($changeable);
+            $obj_setting->setActivationDefault((int)$default);
+            $obj_setting->update();
+        }
+        $this->tpl->setOnScreenMessage('success', $this->lng->txt('settings_saved'), true);
+        $this->ctrl->redirect($this, self::CMD_EDIT);
+    }
+
+    protected function getSettingsForm(): StandardForm
+    {
+        $sections = [];
+        $sections[] = $this->ui_factory->input()->field()->section(
+            [
+                $this->ui_factory->input()->field()->checkbox(
+                    $this->lng->txt("orgu_enable_my_staff"),
+                    $this->lng->txt("orgu_enable_my_staff_info")
+                )
+                ->withValue($this->settings->get("enable_my_staff") ? true : false)
+            ],
+            $this->lng->txt("orgu_enable_my_staff")
+        );
+
+        $groups = [];
+        $values = [];
+        $available_types = $this->object_definition->getOrgUnitPermissionTypes();
+        foreach ($available_types as $object_type) {
+
             $setting = new ilOrgUnitObjectTypePositionSetting($object_type);
             $is_multi = false;
 
-            if ($objDefinition->isPlugin($object_type)) {
+            if ($this->object_definition->isPlugin($object_type)) {
                 $label = ilObjectPlugin::lookupTxtById($object_type, 'objs_' . $object_type);
             } else {
-                $is_multi = !$objDefinition->isSystemObject($object_type) && $object_type != ilOrgUnitOperationContext::CONTEXT_ETAL;
+                $is_multi = !$this->object_definition->isSystemObject($object_type)
+                    && $object_type != ilOrgUnitOperationContext::CONTEXT_ETAL;
                 $lang_prefix = $is_multi ? 'objs_' : 'obj_';
                 $label = $this->lng->txt($lang_prefix . $object_type);
             }
 
-            $type = new ilCheckboxInputGUI(
-                $this->lng->txt('orgu_global_set_positions_type_active') . ' ' . $label,
-                $object_type . '_active'
+            $changeable = [];
+            if($is_multi) {
+                $changeable[] = $this->ui_factory->input()->field()->switchableGroup(
+                    [
+                        $this->ui_factory->input()->field()->group(
+                            [
+                                $this->ui_factory->input()->field()->checkbox(
+                                    $this->lng->txt('orgu_global_set_type_default'),
+                                    $this->lng->txt('orgu_global_set_type_default_info'),
+                                )
+                                ->withValue((bool)$setting->getActivationDefault())
+                            ],
+                            $this->lng->txt('orgu_global_set_type_changeable_object'),
+                        )
+                        ,
+                        $this->ui_factory->input()->field()->group(
+                            [
+
+                            ],
+                            $this->lng->txt('orgu_global_set_type_changeable_no'),
+                        )
+                    ],
+                    $this->lng->txt('orgu_global_set_type_changeable')
+                )
+                ->withValue(
+                    $setting->isChangeableForObject() ? 0 : 1
+                )
+                ->withAdditionalTransformation(
+                    $this->refinery->custom()->transformation(
+                        function ($v) {
+                            $active = true;
+                            $changeable = !(bool)array_shift($v);
+                            $default = false;
+                            if($changeable) {
+                                $default = (bool)current(array_shift($v));
+                            }
+                            return [$active, $changeable, $default];
+                        }
+                    )
+                );
+            } else {
+                $changeable[] = $this->ui_factory->input()->field()->hidden()->withValue('true')
+                    ->withAdditionalTransformation(
+                        $this->refinery->custom()->transformation(
+                            fn($v) => [true, false, false]
+                        )
+                    );
+            }
+
+            $groups[$object_type] = $this->ui_factory->input()->field()->optionalGroup(
+                $changeable,
+                $this->lng->txt('orgu_global_set_positions_type_active') . ' ' . $label
             );
-            $type->setValue(1);
-            $type->setChecked($setting->isActive());
-            if ($is_multi) {
-                $scope = new ilRadioGroupInputGUI(
-                    $this->lng->txt('orgu_global_set_type_changeable'),
-                    $object_type . '_changeable'
-                );
-                $scope->setValue((int) $setting->isChangeableForObject());
 
-                $scope_object = new ilRadioOption(
-                    $this->lng->txt('orgu_global_set_type_changeable_object'),
-                    1
-                );
-                $default = new ilCheckboxInputGUI(
-                    $this->lng->txt('orgu_global_set_type_default'),
-                    $object_type . '_default'
-                );
-                $default->setInfo($this->lng->txt('orgu_global_set_type_default_info'));
-                $default->setValue(ilOrgUnitObjectTypePositionSetting::DEFAULT_ON);
-                $default->setChecked($setting->getActivationDefault());
-
-                $scope_object->addSubItem($default);
-                $scope->addOption($scope_object);
-
-                $scope_global = new ilRadioOption(
-                    $this->lng->txt('orgu_global_set_type_changeable_no'),
-                    0
-                );
-                $scope->addOption($scope_global);
-
-                $type->addSubItem($scope);
+            if(!$setting->isActive()) {
+                $groups[$object_type] = $groups[$object_type]->withValue(null);
             }
-            $form->addItem($type);
         }
-        $form->addCommandButton('saveSettings', $this->lng->txt('save'));
 
-        return $form;
+        $sections[] = $this->ui_factory->input()->field()->section(
+            $groups,
+            $this->lng->txt("orgu_global_set_positions")
+        );
+
+        $form_action = $this->ctrl->getFormAction($this, self::CMD_SAVE);
+        return $this->ui_factory->input()->container()->form()->standard(
+            $form_action,
+            $sections
+        );
     }
 
-    private function saveSettings(): void
-    {
-        global $DIC;
-        $objDefinition = $DIC['objDefinition'];
-        $form = $this->initSettingsForm();
-        if ($form->checkInput()) {
-            // Orgu Permissions / Positions in Modules
-            $available_types = $objDefinition->getOrgUnitPermissionTypes();
-            foreach ($available_types as $object_type) {
-                $obj_setting = new ilOrgUnitObjectTypePositionSetting($object_type);
-                $obj_setting->setActive((bool) $form->getInput($object_type . '_active'));
-                $obj_setting->setActivationDefault((int) $form->getInput($object_type . '_default'));
-                $obj_setting->setChangeableForObject((bool) $form->getInput($object_type
-                    . '_changeable'));
-                $obj_setting->update();
-            }
-
-            // MyStaff
-            $DIC->settings()->set("enable_my_staff", (int) $form->getInput('enable_my_staff'));
-
-            $this->tpl->setOnScreenMessage('success', $this->lng->txt('settings_saved'), true);
-            $this->ctrl->redirect($this, 'settings');
-        } else {
-            $form->setValuesByPost();
-            $this->tpl->setOnScreenMessage('failure', $this->lng->txt('err_check_input'), false);
-            $this->settings($form);
-        }
-    }
 }
