7963147f4ce1c3045ab816793b37edbdf2b47466
Pascal Seeland <pascal.seeland@tik.uni-stuttgart.de>
2024-04-08 05:26:07 +0200

    ECS: type fixes

--- components/ILIAS/WebServices/ECS/classes/class.ilECSRemoteUserRepository.php
+++ components/ILIAS/WebServices/ECS/classes/class.ilECSRemoteUserRepository.php
@@ -118,9 +118,9 @@ class ilECSRemoteUserRepository
         if ($row = $res->fetchRow(ilDBConstants::FETCHMODE_OBJECT)) {
             return new ilECSRemoteUser(
                 $remoteUserId,
-                $row->sid,
-                $row->mid,
-                $row->usr_id,
+                (int) $row->sid,
+                (int) $row->mid,
+                (int) $row->usr_id,
                 $row->remote_usr_id
             );
         }
