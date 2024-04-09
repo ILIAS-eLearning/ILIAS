c95d4fc0de0de9fbe8594b3d43a0ff70a9f2f1af
Fabian Helfer <82493694+fhelfer@users.noreply.github.com>
2024-04-09 10:00:00 +0200

    T&A Bugfix #0040787: Ordering Question (vertical): Saving Error Picture Mode without uploading (#7194)
    * Bugfix saving vertical ordering question error
    
    * Bugfix saving vertical ordering question error 2

--- components/ILIAS/Form/classes/class.ilPropertyFormGUI.php
+++ components/ILIAS/Form/classes/class.ilPropertyFormGUI.php
@@ -1029,7 +1029,7 @@ class ilPropertyFormGUI extends ilFormGUI
                         $name = $file[7];
 
                         if ($idx2 != "") {
-                            if (!$_FILES[$field]["tmp_name"][$idx][$idx2]) {
+                            if (!isset($_FILES[$field]["tmp_name"][$idx]) || !$_FILES[$field]["tmp_name"][$idx][$idx2]) {
                                 $_FILES[$field]["tmp_name"][$idx][$idx2] = $full_file;
                                 $_FILES[$field]["name"][$idx][$idx2] = $name;
                                 $_FILES[$field]["type"][$idx][$idx2] = $type;
@@ -1038,7 +1038,7 @@ class ilPropertyFormGUI extends ilFormGUI
                                 $_FILES[$field]["is_upload"][$idx][$idx2] = false;
                             }
                         } elseif ($idx != "") {
-                            if (!$_FILES[$field]["tmp_name"][$idx]) {
+                            if (!isset($_FILES[$field]["tmp_name"][$idx]) || $_FILES[$field]["tmp_name"][$idx]) {
                                 $_FILES[$field]["tmp_name"][$idx] = $full_file;
                                 $_FILES[$field]["name"][$idx] = $name;
                                 $_FILES[$field]["type"][$idx] = $type;
