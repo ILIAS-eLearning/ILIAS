9a565b0ec19bea7c93a628e94b4a39a0f6b47e80
iszmais <iszmais@databay.de>
2024-04-09 11:21:20 +0200

    Raise deletion event for all authors in forum thread
    (cherry picked from commit 36cba69142d0c273c5dc1dc323a9190962614a38)

--- components/ILIAS/Forum/classes/class.ilForum.php
+++ components/ILIAS/Forum/classes/class.ilForum.php
@@ -563,6 +563,7 @@ class ilForum
             );
         }
 
+        $affected_user_ids[] = $post->getPosAuthorId();
         $deleted_post_ids = $this->deletePostTree($p_node);
 
         $obj_history = new ilForumDraftsHistory();
@@ -614,6 +615,7 @@ class ilForum
                     }
                 } catch (Exception) {
                 }
+                $affected_user_ids[] = (int) $posrec['pos_author_id'];
             }
 
             $this->db->manipulateF('DELETE FROM frm_posts WHERE pos_thr_fk = %s', ['integer'], [$post->getThreadId()]);
@@ -717,7 +719,7 @@ class ilForum
                 [
                     'obj_id' => $this->getForumId(),
                     'ref_id' => $this->getForumRefId(),
-                    'post' => $post
+                    'user_ids' => $affected_user_ids
                 ]
             );
         }
