cdadfde2f73278246964495fdc3a80b603b4a97d
Thibeau Fuhrer <thibeau@sr.solutions>
2024-04-11 13:16:19 +0200

    [FIX] #39532,#39260 UI: distinguish dropzone example post URLs.

--- components/ILIAS/UI/src/examples/Dropzone/File/Wrapper/base.php
+++ components/ILIAS/UI/src/examples/Dropzone/File/Wrapper/base.php
@@ -11,11 +11,15 @@ function base()
     $factory = $DIC->ui()->factory();
     $renderer = $DIC->ui()->renderer();
     $request = $DIC->http()->request();
+    $wrapper = $DIC->http()->wrapper()->query();
+
+    $submit_flag = 'dropzone_wrapper_base';
+    $post_url = "{$request->getUri()}&$submit_flag";
 
     $dropzone = $factory
         ->dropzone()->file()->wrapper(
             'Upload your files here',
-            '#',
+            $post_url,
             $factory->messageBox()->info('Drag and drop files onto me!'),
             $factory->input()->field()->file(
                 new \ilUIAsyncDemoFileUploadHandlerGUI(),
@@ -25,7 +29,7 @@ function base()
 
     // please use ilCtrl to generate an appropriate link target
     // and check it's command instead of this.
-    if ('POST' === $request->getMethod()) {
+    if ($wrapper->has($submit_flag)) {
         $dropzone = $dropzone->withRequest($request);
         $data = $dropzone->getData();
     } else {
