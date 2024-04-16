dee657ac24fbccf6ccf5e48583e7fe66d6d1cbc5
Luka Stocker <luka.stocker@concepts-and-training.de>
2024-04-16 08:51:03 +0200

    UI: adjust rendering of header in property forms. (#41142)
    https://mantis.ilias.de/view.php?id=41142
    
    As changes before this commit resulted to missing header content like settings (e.g. learning progress) the rendering of the header in property forms had to be adjusted.
    
    If no required fields are set the header part for required text output won't get rendered (before it rendered an empty
    ).
    
    If at least one required field is set in a formular a required text will get rendered in the header of the formular.

--- components/ILIAS/Form/classes/class.ilPropertyFormGUI.php
+++ components/ILIAS/Form/classes/class.ilPropertyFormGUI.php
@@ -536,8 +536,8 @@ class ilPropertyFormGUI extends ilFormGUI
             }
 
             // required top
-            if ($this->required_text) {
-                $this->tpl->setCurrentBlock("header");
+            $this->tpl->setCurrentBlock("header");
+            if ($this->checkForRequiredField()) {
                 $this->tpl->setCurrentBlock("required_text_top");
                 $this->tpl->setVariable("TXT_REQUIRED_TOP", $lng->txt("required_field"));
                 $this->tpl->parseCurrentBlock();
@@ -1062,5 +1062,26 @@ class ilPropertyFormGUI extends ilFormGUI
                 }
             }
         }
+    }
+
+    protected function checkForRequiredField(): bool
+    {
+        foreach ($this->items as $item) {
+            if ($item->getType() != "hidden") {
+                if ($this->getMode() == "subform") {
+                    if (!$this->hideRequired($item->getType())) {
+                        if ($item->getRequired()) {
+                            return true;
+                        }
+                    }
+                } elseif (!$this->hideRequired($item->getType())) {
+                    if ($item->getRequired()) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 }
