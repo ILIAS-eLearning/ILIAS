a00b916db4ed081c233f2d06576472b3397d162b
Alexander Killing <killing@leifos.de>
2024-04-19 17:57:07 +0200

    40825: Error undefined array key caption

--- Services/Style/Content/classes/class.ilObjStyleSheetGUI.php
+++ Services/Style/Content/classes/class.ilObjStyleSheetGUI.php
@@ -1484,7 +1484,7 @@ class ilObjStyleSheetGUI extends ilObjectGUI
                 $this->style_request->getTempType()
             );
             foreach ($scs as $k => $type) {
-                $values[$k . "_class"] = $t["classes"][$k];
+                $values[$k . "_class"] = $t["classes"][$k] ?? "";
             }
             $this->form_gui->setValuesByArray($values);
         }
