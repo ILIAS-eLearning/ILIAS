fe746a34e2a9ce19a841d4b51e0d96810e9be5e0
Alexander Killing <killing@leifos.de>
2024-04-23 21:50:10 +0200

    38507: Lines/Zeilen button not usable or required in Admin

--- Services/Repository/Administration/class.ilModulesTableGUI.php
+++ Services/Repository/Administration/class.ilModulesTableGUI.php
@@ -67,7 +67,7 @@ class ilModulesTableGUI extends ilTable2GUI
             "tpl.table_row_module.html",
             "Services/Repository/Administration"
         );
-        $this->setLimit(10000);
+        $this->setLimit(9999);
         $this->setExternalSorting(true);
 
         $this->getComponents();
