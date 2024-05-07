b9de3bf42c72762d0de7bb20e1257226d70419a3
Stefan Meyer <meyer@leifos.de>
2024-05-07 09:55:10 +0200

    Course objectives: coding style

--- Modules/Course/classes/Objectives/class.ilCourseObjectiveResult.php
+++ Modules/Course/classes/Objectives/class.ilCourseObjectiveResult.php
@@ -1,7 +1,4 @@
 <?php
-
-declare(strict_types=0);
-
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -17,6 +14,8 @@ declare(strict_types=0);
  * https://github.com/ILIAS-eLearning
  *
  *********************************************************************/
+
+declare(strict_types=0);
 
 /**
  * class ilcourseobjective
@@ -393,18 +392,18 @@ class ilCourseObjectiveResult
     public static function __isFullfilled(array $question_points, array $objective_data): bool
     {
         global $DIC;
-        
+
         if (!is_array($objective_data['questions'])) {
             return false;
         }
         $max_points = 0;
         $reached_points = 0;
         foreach ($objective_data['questions'] as $question_id) {
-            if (array_key_exists($question_id,$question_points)) {
+            if (array_key_exists($question_id, $question_points)) {
                 $max_points += $question_points[$question_id]['max_points'];
                 $reached_points += $question_points[$question_id]['reached_points'] ?? 0;
             } else {
-                $DIC->logger()->crs()->warning('stale question in course objective assignment table id '.$question_id);
+                $DIC->logger()->crs()->warning('stale question in course objective assignment table id ' . $question_id);
             }
         }
         if (!$max_points) {
