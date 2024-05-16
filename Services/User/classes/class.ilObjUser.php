5332dde903ef5a6e502997b7900d1867e02a01e6
fneumann <fneumann@databay.de>
2024-05-16 10:38:09 +0200

    [Forum] Remove ilObjForum::_deleteUser call from ilObjUser
    The forum component already has an event listener for deleted user accounts.
    The call of this function is moved there.
    This allows a further internal refactoring in the forum component
    and reduces the dependencies in ilObjUser.

--- Services/User/classes/class.ilObjUser.php
+++ Services/User/classes/class.ilObjUser.php
@@ -1108,9 +1108,6 @@ class ilObjUser extends ilObject
             $ilDB->quote($this->getId(), "integer");
         $ilDB->manipulate($q);
 
-        // DELETE FORUM ENTRIES (not complete in the moment)
-        ilObjForum::_deleteUser($this->getId());
-
         // Delete crs entries
         ilObjCourse::_deleteUser($this->getId());
 
