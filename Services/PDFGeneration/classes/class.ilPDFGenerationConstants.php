db0e8fd9c3f82e542cdecf3a9684672be09809d7
Guido Vollbach <gvollbach@databay.de>
2024-05-24 11:38:10 +0200

    Restructure pdf config forms gui

--- Services/PDFGeneration/classes/class.ilPDFGenerationConstants.php
+++ Services/PDFGeneration/classes/class.ilPDFGenerationConstants.php
@@ -3,17 +3,14 @@
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
- *
  * ILIAS is licensed with the GPL-3.0,
  * see https://www.gnu.org/licenses/gpl-3.0.en.html
  * You should have received a copy of said license along with the
  * source code, too.
- *
  * If this is not the case or you just want to try ILIAS, you'll find
  * us at:
  * https://www.ilias.de
  * https://github.com/ILIAS-eLearning
- *
  *********************************************************************/
 
 declare(strict_types=1);
@@ -34,7 +31,7 @@ class ilPDFGenerationConstants
     /**
      * @return array<string, string>
      */
-    public static function getPageSizesNames(): array
+    public static function getPageSizesNames() : array
     {
         return [
             'A4' => 'A4',
@@ -55,15 +52,23 @@ class ilPDFGenerationConstants
         ];
     }
 
-
     /**
      * @return array<string, string>
      */
-    public static function getOrientations(): array
+    public static function getOrientations() : array
     {
         return [
-            'Portrait' => 'Portrait' ,
+            'Portrait' => 'Portrait',
             'Landscape' => 'Landscape'
         ];
+    }
+
+    public static function getFontStyles()
+    {
+        return array(
+            'serif' => 'serif',
+            'sans-serif' => 'sans-serif',
+            'monospace' => 'monospace'
+        );
     }
 }
