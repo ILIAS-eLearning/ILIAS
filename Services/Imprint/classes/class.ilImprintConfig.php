07ce3e3c810169a76f3ec0582297beef651a1cee
Luka Stocker <luka.stocker@concepts-and-training.de>
2024-06-03 08:31:29 +0200

    IMPR: allow setting internal links via page editor. (#37724)
    https://mantis.ilias.de/view.php?id=37724

--- Services/Imprint/classes/class.ilImprintConfig.php
+++ Services/Imprint/classes/class.ilImprintConfig.php
@@ -3,15 +3,18 @@
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
+ *
  * ILIAS is licensed with the GPL-3.0,
  * see https://www.gnu.org/licenses/gpl-3.0.en.html
  * You should have received a copy of said license along with the
  * source code, too.
+ *
  * If this is not the case or you just want to try ILIAS, you'll find
  * us at:
  * https://www.ilias.de
  * https://github.com/ILIAS-eLearning
- */
+ *
+ *********************************************************************/
 
 /**
  * Imprint page configuration
@@ -23,7 +26,7 @@ class ilImprintConfig extends ilPageConfig
     public function init(): void
     {
         $this->setPreventHTMLUnmasking(true);
-        $this->setEnableInternalLinks(false);
+        $this->setEnableInternalLinks(true);
         $this->setEnableWikiLinks(false);
         $this->setEnableActivation(true);
     }
