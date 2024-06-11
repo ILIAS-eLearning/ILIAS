67c735befe3309bb5e2c9ecc1d015ef5a5b9d583
iszmais <iszmais@databay.de>
2024-06-11 14:51:45 +0200

    Allow internal links inside the imprint CO page

--- Services/Imprint/classes/class.ilImprintConfig.php
+++ Services/Imprint/classes/class.ilImprintConfig.php
@@ -3,15 +3,20 @@
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
+ *
  * ILIAS is licensed with the GPL-3.0,
  * see https://www.gnu.org/licenses/gpl-3.0.en.html
  * You should have received a copy of said license along with the
  * source code, too.
+ *
  * If this is not the case or you just want to try ILIAS, you'll find
  * us at:
  * https://www.ilias.de
  * https://github.com/ILIAS-eLearning
- */
+ *
+ *********************************************************************/
+
+declare(strict_types=1);
 
 /**
  * Imprint page configuration
@@ -23,7 +28,7 @@ class ilImprintConfig extends ilPageConfig
     public function init(): void
     {
         $this->setPreventHTMLUnmasking(true);
-        $this->setEnableInternalLinks(false);
+        $this->setEnableInternalLinks(true);
         $this->setEnableWikiLinks(false);
         $this->setEnableActivation(true);
     }
