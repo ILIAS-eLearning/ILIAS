f6189aa102c93165a013e5b5a921a0a2479c9c1e
Alex Killing <killing@leifos.de>
2024-06-12 17:53:59 +0200

    31437: if not owner: copy via manage fails when plugin involved

--- Services/Container/classes/class.ilContainerGUI.php
+++ Services/Container/classes/class.ilContainerGUI.php
@@ -1056,7 +1056,7 @@ class ilContainerGUI extends ilObjectGUI implements ilDesktopItemHandling
                     continue;
                 }
 
-                if (!$rbacsystem->checkAccess('visible,read,copy', $node["ref_id"])) {
+                if (!$rbacsystem->checkAccess('visible,read', $node["ref_id"])) {
                     $no_copy[] = $node["ref_id"];
                 }
             }
