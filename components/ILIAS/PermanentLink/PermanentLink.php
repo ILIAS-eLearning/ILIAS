eeab7e5ef184773ccbb86fada92b19ded2f27ba8
Stephan Kergomard <webmaster@kergomard.ch>
2024-06-19 15:12:39 +0200

    PermanentLink: Deprecate With ILIAS 11

--- components/ILIAS/PermanentLink/PermanentLink.php
+++ components/ILIAS/PermanentLink/PermanentLink.php
@@ -20,6 +20,10 @@ declare(strict_types=1);
 
 namespace ILIAS;
 
+/**
+ * @deprecated 11 This component will be removed with ILIAS 11
+ *
+ */
 class PermanentLink implements Component\Component
 {
     public function init(
