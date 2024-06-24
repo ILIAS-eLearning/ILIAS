ebe44046443e5c4ff46c482d268294706eab41cd
fneumann <fneumann@databay.de>
2024-06-24 12:17:08 +0200

    MathJax: fix mantis #41564
    Use new constants and placeholders for default URLs of MathJax 2 and 3.
    
    Add js file for config and loading of MathJax 3.

---
+++ Services/MathJax/js/cdn-mathjax3-es5-tex-mml-chtml-safe.js
@@ -0,0 +1,17 @@
+/**
+ * Configure the safe mode and load Mathjax from CDN
+ * @see https://docs.mathjax.org/en/latest/web/configuration.html#configuring-and-loading-in-one-script
+ */
+
+window.MathJax = {
+  loader: {
+    load: ['ui/safe'],
+  },
+};
+
+(function () {
+  const script = document.createElement('script');
+  script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
+  script.async = true;
+  document.head.appendChild(script);
+}());
