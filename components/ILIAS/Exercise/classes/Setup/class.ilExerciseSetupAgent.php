e44dac4479c94f68694ec9260b4ce373545922d8
Fabian Helfer <fhelfer@databay.de>
2024-06-27 08:59:08 +0200

    Fix Exercise Setup titled Course

--- components/ILIAS/Exercise/classes/Setup/class.ilExerciseSetupAgent.php
+++ components/ILIAS/Exercise/classes/Setup/class.ilExerciseSetupAgent.php
@@ -32,7 +32,7 @@ class ilExerciseSetupAgent extends Setup\Agent\NullAgent
     public function getUpdateObjective(Setup\Config $config = null): Setup\Objective
     {
         return new ObjectiveCollection(
-            'Database is updated for component/ILIAS/Course',
+            'Database is updated for component/ILIAS/Exercise',
             false,
             new ilDatabaseUpdateStepsExecutedObjective(new ilExerciseDBUpdateSteps()),
         );
