ce0fdc956cc8f809afdaeafbb57815cb3828ee16
iszmais <iszmais@databay.de>
2024-07-01 18:38:55 +0200

    Fix function call for comments rendering on records

--- components/ILIAS/DataCollection/classes/DetailedView/class.ilDclDetailedViewGUI.php
+++ components/ILIAS/DataCollection/classes/DetailedView/class.ilDclDetailedViewGUI.php
@@ -313,7 +313,7 @@ class ilDclDetailedViewGUI
     protected function renderComments(bool $edit = false): string
     {
         if (!$edit) {
-            return $this->notesGUI->getCommentsHTML();
+            return $this->notesGUI->getListHTML();
         } else {
             return $this->notesGUI->editNoteForm();
         }
@@ -407,7 +407,7 @@ class ilDclDetailedViewGUI
         );
         //we then partially load the records. note that this also fills up session data.
         $this->table->getPartialRecords(
-            (string)$this->table->getId(),
+            (string) $this->table->getId(),
             $list->getOrderField(),
             $list->getOrderDirection(),
             $list->getLimit(),
