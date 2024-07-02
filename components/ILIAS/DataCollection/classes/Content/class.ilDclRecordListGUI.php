8ba8785368cdcf1f37440353cbe52001512d3045
iszmais <iszmais@databay.de>
2024-07-02 17:39:25 +0200

    Remove br from save and add to presentation

--- components/ILIAS/DataCollection/classes/Content/class.ilDclRecordListGUI.php
+++ components/ILIAS/DataCollection/classes/Content/class.ilDclRecordListGUI.php
@@ -203,7 +203,7 @@ class ilDclRecordListGUI
             if ($ilSetting->get('advanced_editing_javascript_editor')) {
                 $desc = "<div class='ilDclTableDescription'>" . $desc . "</div>";
             } else {
-                $desc = "<div class='ilDclTableDescription'>" . nl2br(ilUtil::stripSlashes($desc)) . "</div>";
+                $desc = "<div class='ilDclTableDescription'>" . nl2br($this->refinery->encode()->htmlSpecialCharsAsEntities()->transform($desc)) . "</div>";
             }
         }
         $this->tpl->setContent($desc . $list->getHTML());
