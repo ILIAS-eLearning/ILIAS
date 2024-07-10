7139bde274c642445c6f69def792cc408a74542a
Nils Haagen <nhaagen@cat06.de>
2024-07-10 12:50:48 +0200

    T&A, 41532: fix filtering for 'no taxonomy'

--- Modules/TestQuestionPool/classes/class.ilObjQuestionPoolGUI.php
+++ Modules/TestQuestionPool/classes/class.ilObjQuestionPoolGUI.php
@@ -1912,10 +1912,10 @@ class ilObjQuestionPoolGUI extends ilObjectGUI implements ilCtrlBaseClassInterfa
 
                 switch ($item) {
                     case 'taxonomies':
-                        if($value === 'null') {
-                            $table->addTaxonomyFilterNoTaxonomySet(true);
-                        } else {
-                            foreach($value as $tax_value) {
+                        foreach($value as $tax_value) {
+                            if($tax_value === 'null') {
+                                $table->addTaxonomyFilterNoTaxonomySet(true);
+                            } else {
                                 $tax_nodes = explode('-', $tax_value);
                                 $tax_id = array_shift($tax_nodes);
                                 $table->addTaxonomyFilter(
