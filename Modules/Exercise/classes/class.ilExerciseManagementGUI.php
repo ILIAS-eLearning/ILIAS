b3c37905da65cedbfe080aa8095dced340a30432
Alexander Killing <killing@leifos.de>
2024-07-12 08:18:20 +0200

    40132: Portfolio in exercise is not always the newest version

--- Modules/Exercise/classes/class.ilExerciseManagementGUI.php
+++ Modules/Exercise/classes/class.ilExerciseManagementGUI.php
@@ -2146,7 +2146,6 @@ class ilExerciseManagementGUI
         $submission = new ilExSubmission($this->assignment, $member_id);
 
         $last_opening = $submission->getLastOpeningHTMLView();
-
         $submission_time = $submission->getLastSubmission();
 
         // e.g. /<datadir>/<clientid>/ilExercise/3/exc_367/subm_1/<ass_id>/20210628175716_368
@@ -2193,7 +2192,6 @@ class ilExerciseManagementGUI
         if ($zip_original_full_path) {
             $file_copied = $this->copyFileToWebDir($zip_internal_path);
             $this->log->debug("file copied: " . $file_copied);
-
             if ($file_copied) {
                 ilFileUtils::unzip($file_copied, true);
                 $web_filesystem->delete($zip_internal_path);
@@ -2256,10 +2254,14 @@ class ilExerciseManagementGUI
 
         if ($data_filesystem->has($internal_file_path)) {
             $this->log->debug("internal file path: " . $internal_file_path);
-            if (!$web_filesystem->hasDir($internal_dirs)) {
-                $web_filesystem->createDir($internal_dirs);
+            if ($web_filesystem->hasDir($internal_dirs)) {
+                $web_filesystem->deleteDir($internal_dirs);
             }
+            $web_filesystem->createDir($internal_dirs);
 
+            if ($web_filesystem->has($internal_file_path)) {
+                $web_filesystem->delete($internal_file_path);
+            }
             if (!$web_filesystem->has($internal_file_path)) {
                 $this->log->debug("writing: " . $internal_file_path);
                 $stream = $data_filesystem->readStream($internal_file_path);
