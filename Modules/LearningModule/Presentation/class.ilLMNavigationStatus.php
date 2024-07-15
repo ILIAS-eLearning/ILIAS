8047dee97144355def0b6cf1d53c2e6d19e2f592
Alexander Killing <killing@leifos.de>
2024-07-15 12:43:40 +0200

    31888: No page found if page has Scheduled Activation (and Show Activation Info is enabled)

--- Modules/LearningModule/Presentation/class.ilLMNavigationStatus.php
+++ Modules/LearningModule/Presentation/class.ilLMNavigationStatus.php
@@ -131,8 +131,16 @@ class ilLMNavigationStatus
             (bool) $this->lm_set->get("time_scheduled_page_activation")
         );
 
+        $show = $active;
+
+        // look, whether activation data should be shown
+        $act_data = ilLMPage::_lookupActivationData((int) $curr_node["obj_id"], $this->lm->getType());
+        if ($act_data["show_activation_info"] ?? false) {
+            $show = true;
+        }
+
         if ($curr_node["type"] == "pg" &&
-            $active) {		// page in tree -> return page id
+            $show) {		// page in tree -> return page id
             $page_id = $curr_node["obj_id"];
         } else { 		// no page -> search for next page and return its id
             $succ_node = true;
@@ -228,7 +236,6 @@ class ilLMNavigationStatus
                     (bool) $this->lm_set->get("time_scheduled_page_activation")
                 );
             }
-
             if (is_array($succ_node) && $succ_node["obj_id"] > 0 &&
                 $user_id == ANONYMOUS_USER_ID &&
                 ($this->lm->getPublicAccessMode() == "selected" &&
