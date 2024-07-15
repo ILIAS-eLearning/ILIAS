f0cf977c63c2b8e3389276ecee49b15f909607c6
Thomas Joußen <tjoussen@databay.de>
2024-07-15 14:23:18 +0200

    Test: Fix CS in ilObjTest

--- components/ILIAS/Test/classes/class.ilObjTest.php
+++ components/ILIAS/Test/classes/class.ilObjTest.php
@@ -5150,10 +5150,10 @@ class ilObjTest extends ilObject
                 $reached_points = 0;
                 $max_points = 0;
                 $pass = ilObjTest::_getResultPass($active_id);
-		// abort if no valid pass can be found
-		if (!is_int($pass)) {
-			continue;
-		}
+                // abort if no valid pass can be found
+                if (!is_int($pass)) {
+                    continue;
+                }
                 foreach ($this->questions as $value) {
                     $question = ilObjTest::_instanciateQuestion($value);
                     if (is_object($question)) {
