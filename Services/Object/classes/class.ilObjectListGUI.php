62e6b88ff0de8517a9e877dca2ade2cf43d2d1e7
Stephan Kergomard <webmaster@kergomard.ch>
2024-07-22 16:20:50 +0200

    Object: Fix DropZones in ObjectLists
    See: https://mantis.ilias.de/view.php?id=41294

--- Services/Object/classes/class.ilObjectListGUI.php
+++ Services/Object/classes/class.ilObjectListGUI.php
@@ -27,6 +27,7 @@ use ILIAS\UI\Component\Item\Item;
 use ILIAS\UI\Component\Image\Image;
 use ILIAS\UI\Implementation\Component\SignalGenerator;
 use ILIAS\Notes\Note;
+use ILIAS\Container\Content\ModeSessionRepository;
 use ILIAS\HTTP\Services as HTTPServices;
 use ILIAS\Object\ilObjectDIC;
 
@@ -2920,6 +2921,7 @@ class ilObjectListGUI
         if ($this->context === self::CONTEXT_REPOSITORY
             && ($this->requested_cmd === "view" || $this->requested_cmd === "" || $this->requested_cmd === "render")
             && $file_upload_dropzone->isUploadAllowed($this->type)
+             && !(new ModeSessionRepository())->isAdminMode()
         ) {
             return $file_upload_dropzone->getDropzoneHtml();
         }
