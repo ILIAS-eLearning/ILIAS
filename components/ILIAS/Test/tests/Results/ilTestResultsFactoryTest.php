a72736d516fd4c3c42a748bb8badc68f26023b3c
Matheus Zych <mzych@databay.de>
2024-07-23 14:33:08 +0200

    Adds unit tests for ilTestResultsFactory

--- components/ILIAS/Test/tests/Results/ilTestResultsFactoryTest.php
+++ components/ILIAS/Test/tests/Results/ilTestResultsFactoryTest.php
@@ -18,21 +18,92 @@
 
 namespace Results;
 
+use ilObjTest;
 use ilTestBaseTestCase;
+use ilTestPassResult;
 use ilTestResultsFactory;
 use ilTestShuffler;
 use ILIAS\UI\Factory as UIFactory;
 use ILIAS\UI\Renderer as UIRenderer;
+use PHPUnit\Framework\MockObject\Exception;
 
 class ilTestResultsFactoryTest extends ilTestBaseTestCase
 {
+    private ilTestShuffler $test_shuffler;
+    private UIFactory $ui_factory;
+    private UIRenderer $ui_renderer;
+    private ilObjTest $test_obj;
+
+    /**
+     * @throws Exception
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->test_shuffler = $this->createMock(ilTestShuffler::class);
+        $this->ui_factory = $this->createMock(UIFactory::class);
+        $this->ui_renderer = $this->createMock(UIRenderer::class);
+        $this->test_obj = $this->createMock(ilObjTest::class);
+    }
+
     public function testConstruct(): void
     {
-        $ilTestResultsFactoryTest = new ilTestResultsFactory(
-            $this->createMock(ilTestShuffler::class),
-            $this->createMock(UIFactory::class),
-            $this->createMock(UIRenderer::class)
+        $ilTestResultsFactory = new ilTestResultsFactory(
+            $this->test_shuffler,
+            $this->ui_factory,
+            $this->ui_renderer
         );
-        $this->assertInstanceOf(ilTestResultsFactory::class, $ilTestResultsFactoryTest);
+
+        $this->assertInstanceOf(ilTestResultsFactory::class, $ilTestResultsFactory);
+    }
+
+    /**
+     * @dataProvider getPassResultsForDataProvider
+     */
+    public function testGetPassResultsFor(array $IO): void
+    {
+        $ilTestResultsFactory = new ilTestResultsFactory(
+            $this->test_shuffler,
+            $this->ui_factory,
+            $this->ui_renderer
+        );
+
+        if (is_null($IO['is_user_output'])) {
+            $ilTestPassResult = $ilTestResultsFactory->getPassResultsFor(
+                $this->test_obj,
+                $IO['active_id'],
+                $IO['pass_id'],
+            );
+        } else {
+            $ilTestPassResult = $ilTestResultsFactory->getPassResultsFor(
+                $this->test_obj,
+                $IO['active_id'],
+                $IO['pass_id'],
+                $IO['is_user_output']
+            );
+        }
+
+        $this->assertInstanceOf(ilTestPassResult::class, $ilTestPassResult);
+        $this->assertEquals($IO['active_id'], $ilTestPassResult->getActiveId());
+        $this->assertEquals($IO['pass_id'], $ilTestPassResult->getPass());
+    }
+
+    public static function getPassResultsForDataProvider(): array
+    {
+        return [
+            'zero_zero_default' => [['active_id' => 0, 'pass_id' => 0, 'is_user_output' => null]],
+            'zero_zero_true' => [['active_id' => 0, 'pass_id' => 0, 'is_user_output' => true]],
+            'zero_zero_false' => [['active_id' => 0, 'pass_id' => 0, 'is_user_output' => false]],
+            'one_zero_default' => [['active_id' => 1, 'pass_id' => 0, 'is_user_output' => null]],
+            'one_zero_true' => [['active_id' => 1, 'pass_id' => 0, 'is_user_output' => true]],
+            'one_zero_false' => [['active_id' => 1, 'pass_id' => 0, 'is_user_output' => false]],
+            'zero_one_default' => [['active_id' => 0, 'pass_id' => 1, 'is_user_output' => null]],
+            'zero_one_true' => [['active_id' => 0, 'pass_id' => 1, 'is_user_output' => true]],
+            'zero_one_false' => [['active_id' => 0, 'pass_id' => 1, 'is_user_output' => false]],
+            'one_one_default' => [['active_id' => 1, 'pass_id' => 1, 'is_user_output' => null]],
+            'one_one_true' => [['active_id' => 1, 'pass_id' => 1, 'is_user_output' => true]],
+            'one_one_false' => [['active_id' => 1, 'pass_id' => 1, 'is_user_output' => false]]
+        ];
     }
 }
