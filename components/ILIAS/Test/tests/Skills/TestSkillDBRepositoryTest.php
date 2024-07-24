8501ae3a925a3d25b2283942beea1676ceb5b3fd
Matheus Zych <mzych@databay.de>
2024-07-24 13:04:44 +0200

    Adds unit tests for TestSkillDBRepositoryTest

--- components/ILIAS/Test/tests/Skills/TestSkillDBRepositoryTest.php
+++ components/ILIAS/Test/tests/Skills/TestSkillDBRepositoryTest.php
@@ -21,6 +21,7 @@ namespace Skills;
 use ilDBInterface;
 use ILIAS\Test\Skills\TestSkillDBRepository;
 use ilTestBaseTestCase;
+use PHPUnit\Framework\MockObject\Exception;
 
 class TestSkillDBRepositoryTest extends ilTestBaseTestCase
 {
@@ -31,5 +32,73 @@ class TestSkillDBRepositoryTest extends ilTestBaseTestCase
 
         $testSkillDBRepository = new TestSkillDBRepository($this->createMock(ilDBInterface::class));
         $this->assertInstanceOf(TestSkillDBRepository::class, $testSkillDBRepository);
+    }
+
+    /**
+     * @dataProvider removeForSkillDataProvider
+     * @throws Exception
+     */
+    public function testRemoveForSkill(array $input, string $output): void
+    {
+        $skill_node_id_quoted = '`' . $input['skill_node_id'] .'`';
+
+        $db = $this->createMock(ilDBInterface::class);
+        $db->expects($this->once())->method('manipulate')->with(
+            $this->equalTo('DELETE FROM tst_skl_thresholds  WHERE ' . $output . ' = ' . $skill_node_id_quoted)
+        );
+        $db->expects($this->once())->method('quote')->with(
+            $this->equalTo($input['skill_node_id']),
+            $this->equalTo('integer')
+        )->willReturn($skill_node_id_quoted);
+
+        $this->assertNull((new TestSkillDBRepository($db))->removeForSkill($input['skill_node_id'], $input['is_reference']));
+    }
+
+    public static function removeForSkillDataProvider(): array
+    {
+        return [
+            'not_reference_0' => [
+                'input' => [
+                    'skill_node_id' => 0,
+                    'is_reference' => false
+                ],
+                'output' => 'skill_base_fi'
+            ],
+            'reference_0' => [
+                'input' => [
+                    'skill_node_id' => 0,
+                    'is_reference' => true
+                ],
+                'output' => 'skill_tref_fi'
+            ],
+            'not_reference_1' => [
+                'input' => [
+                    'skill_node_id' => 1,
+                    'is_reference' => false
+                ],
+                'output' => 'skill_base_fi'
+            ],
+            'reference_1' => [
+                'input' => [
+                    'skill_node_id' => 1,
+                    'is_reference' => true
+                ],
+                'output' => 'skill_tref_fi'
+            ],
+            'not_reference_2' => [
+                'input' => [
+                    'skill_node_id' => 2,
+                    'is_reference' => false
+                ],
+                'output' => 'skill_base_fi'
+            ],
+            'reference_2' => [
+                'input' => [
+                    'skill_node_id' => 2,
+                    'is_reference' => true
+                ],
+                'output' => 'skill_tref_fi'
+            ]
+        ];
     }
 }
