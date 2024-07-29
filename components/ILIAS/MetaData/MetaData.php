abc81b6efa8dcf18c886261f8a421d041dd4f6ba
Tim Schmitz <schmitz@leifos.de>
2024-07-29 16:26:23 +0200

    Metadata: re-add js

--- components/ILIAS/MetaData/MetaData.php
+++ components/ILIAS/MetaData/MetaData.php
@@ -36,5 +36,8 @@ class MetaData implements Component\Component
             new \ilMDSetupAgent(
                 $pull[\ILIAS\Refinery\Factory::class]
             );
+
+        $contribute[Component\Resource\PublicAsset::class] = fn() =>
+            new Component\Resource\ComponentJS($this, 'ilMetaCopyrightListener.js');
     }
 }
