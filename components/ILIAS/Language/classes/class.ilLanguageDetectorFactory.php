7110792efa58a366482950e2356dc610d99bfc25
Christian Knof <christian.knof@kroepelin-projekte.de>
2024-08-01 13:46:32 +0200

    Update language component paths
    Corrected the file path references from 'Language_' to 'Language' in multiple scripts, ensuring consistency and accuracy across the project. Adjusted documentation and configuration files to reflect these path changes accordingly.

--- components/ILIAS/Language/classes/class.ilLanguageDetectorFactory.php
+++ components/ILIAS/Language/classes/class.ilLanguageDetectorFactory.php
@@ -22,7 +22,7 @@ declare(strict_types=1);
 /**
  * Class ilLanguageDetectorFactory
  * @author Michael Jansen <mjansen@databay.de>
- * @ingroup components/ILIAS/Language_
+ * @ingroup components/ILIAS/Language
  */
 class ilLanguageDetectorFactory
 {
