83a72c6757c5b5fabd95aa3dbf9308c687ffc533
Patrick Bechtold <patrick.bechtold@kroepelin-projekte.de>
2024-08-13 17:29:32 +0200

    Fix duplicate identifier detection script
    Simplify syntax and improve message clarity in the duplicate identifier detection script. Adjust exit statuses for better script behavior.

--- scripts/check_langfile_duplicate_identifier.sh
+++ scripts/check_langfile_duplicate_identifier.sh
@@ -16,20 +16,19 @@ do
   # Find the duplicate identifiers
   duplicate_identifiers=$(sed -n '/<!-- language file start -->/,$p' $file | awk -F '#:#' '{print tolower($2)}' | sort -f | uniq -d)
 
-  if [[ -n $duplicate_identifiers ]]; then
-     echo "In Datei ${file} gibt es Duplikate für folgende Identifier:"
+  if [ -n "$duplicate_identifiers" ]; then
+     echo "In file ${file} are duplicates for the following identifier:"
 
      # Loop through each duplicate identifier
      for duplicate_identifier in $duplicate_identifiers; do
         # Find all lines containing this exact duplicate and print the modules
-        IFS=$'\n' lines_with_duplicate=($(sed -n '/<!-- language file start -->/,$p' $file | awk -F '#:#' -v id="$duplicate_identifier" 'tolower($2) == tolower(id){print $1}'))
+        lines_with_duplicate=$(sed -n '/<!-- language file start -->/,$p' "$file" | awk -F '#:#' -v id="$duplicate_identifier" 'tolower($2) == tolower(id){print $1}')
         # Remove duplicates and create a comma separated list of unique modules
-        modules_with_duplicate=$(echo "${lines_with_duplicate[@]}" | tr ' ' '\n' | sort -fu | tr '\n' ',' | sed 's/,$//')
-        echo "Doppelter Identifier \"$duplicate_identifier\" in Modulen: $modules_with_duplicate"
+        modules_with_duplicate=$(echo "${lines_with_duplicate}" | tr ' ' '\n' | sort -fu | tr '\n' ',' | sed 's/,$//')
+        echo "Duplicate identifier \"$duplicate_identifier\" found in modules: $modules_with_duplicate"
      done
 
-     # Mark that duplicates are found
-     exit_status=127
+     exit_status=1
   fi
 done
 
