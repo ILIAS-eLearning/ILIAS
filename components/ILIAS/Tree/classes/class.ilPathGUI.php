dda900f3f0d70a251ff9d79d2cb0c88d81431447
mjansen <mjansen@databay.de>
2024-08-13 08:12:25 +0200

    Tree/PathGUI: Properly handle HTML attributes
    See: https://mantis.ilias.de/view.php?id=22578

--- components/ILIAS/Tree/classes/class.ilPathGUI.php
+++ components/ILIAS/Tree/classes/class.ilPathGUI.php
@@ -1,34 +1,23 @@
 <?php
 
-declare(strict_types=1);
-/*
-    +-----------------------------------------------------------------------------+
-    | ILIAS open source                                                           |
-    +-----------------------------------------------------------------------------+
-    | Copyright (c) 1998-2006 ILIAS open source, University of Cologne            |
-    |                                                                             |
-    | This program is free software; you can redistribute it and/or               |
-    | modify it under the terms of the GNU General Public License                 |
-    | as published by the Free Software Foundation; either version 2              |
-    | of the License, or (at your option) any later version.                      |
-    |                                                                             |
-    | This program is distributed in the hope that it will be useful,             |
-    | but WITHOUT ANY WARRANTY; without even the implied warranty of              |
-    | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
-    | GNU General Public License for more details.                                |
-    |                                                                             |
-    | You should have received a copy of the GNU General Public License           |
-    | along with this program; if not, write to the Free Software                 |
-    | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
-    +-----------------------------------------------------------------------------+
-*/
-
 /**
- * Creates a path for a start and endnode
- * @author  Stefan Meyer <meyer@leifos.com>
- * @version $Id$
- * @ingroup ServicesTree
- */
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
+declare(strict_types=1);
+
 class ilPathGUI
 {
     private int $startnode;
@@ -42,6 +31,7 @@ class ilPathGUI
     protected ilLanguage $lng;
     protected ilTree $tree;
     protected ilObjectDefinition $objectDefinition;
+    protected \ILIAS\Refinery\Factory $refinery;
 
     /**
      * Constructor
@@ -55,6 +45,7 @@ class ilPathGUI
         $this->tree = $DIC->repositoryTree();
         $this->lng = $DIC->language();
         $this->objectDefinition = $DIC['objDefinition'];
+        $this->refinery = $DIC->refinery();
     }
 
     /**
@@ -203,7 +194,12 @@ class ilPathGUI
 
                 if (!$this->tree->isDeleted($ref_id)) {
                     $tpl->setCurrentBlock('locator_item');
-                    $tpl->setVariable('LINK_ITEM', ilLink::_getLink($ref_id, $type));
+                    $tpl->setVariable(
+                        'LINK_ITEM',
+                        $this->refinery->encode()->htmlAttributeValue()->transform(
+                            ilLink::_getLink($ref_id, $type)
+                        )
+                    );
                     $tpl->setVariable('ITEM', $title);
                     $tpl->parseCurrentBlock();
                 } else {
