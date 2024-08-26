e4cc93270171de96d7b38e3c96a88983be764ec6
mjansen <mjansen@databay.de>
2024-08-26 13:19:58 +0200

    Auth: Init global user for "blocked authentication" state
    See: https://mantis.ilias.de/view.php?id=41601
    Auth: Init global user for "blocked authentication" state
    
    See: https://mantis.ilias.de/view.php?id=41601

--- Services/Init/classes/class.ilInitialisation.php
+++ Services/Init/classes/class.ilInitialisation.php
@@ -1407,7 +1407,7 @@ class ilInitialisation
         }
 
         if (
-            !$DIC['ilAuthSession']->isAuthenticated() or
+            !$DIC['ilAuthSession']->isAuthenticated() ||
             $DIC['ilAuthSession']->isExpired()
         ) {
             if ($GLOBALS['DIC']['ilAuthSession']->isExpired()) {
@@ -1417,6 +1417,7 @@ class ilInitialisation
             $current_script = substr(strrchr($_SERVER["PHP_SELF"], "/"), 1);
             if (self::blockedAuthentication($current_script)) {
                 ilLoggerFactory::getLogger('init')->debug('Authentication is started in current script.');
+                self::initUserAccount();
                 // nothing todo: authentication is done in current script
                 return;
             }
