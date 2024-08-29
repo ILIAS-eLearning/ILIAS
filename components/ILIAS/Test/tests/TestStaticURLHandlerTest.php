1142d18a9a40dfd5992e0e269103cfb0fa9551c7
Matheus Zych <mzych@databay.de>
2024-08-29 11:20:53 +0200

    Adds unit tests for TestStaticURLHandler

---
+++ components/ILIAS/Test/tests/TestStaticURLHandlerTest.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\MockObject\Exception;
+
+class TestStaticURLHandlerTest extends ilTestBaseTestCase
+{
+    /**
+     * @throws ReflectionException|Exception
+     */
+    public function testConstruct(): void
+    {
+        $this->assertInstanceOf(TestStaticURLHandler::class, $this->createInstanceOf(TestStaticURLHandler::class));
+    }
+
+    /**
+     * @throws ReflectionException|Exception
+     */
+    public function testGetNamespace(): void
+    {
+        $test_static_url_handler = $this->createInstanceOf(TestStaticURLHandler::class);
+
+        $this->assertEquals('tst', $test_static_url_handler->getNamespace());
+    }
+
+    /**
+     * @dataProvider buildQuestionURLDataProvider
+     * @throws ReflectionException|Exception
+     */
+    public function testBuildQuestionURL(string $input, string $output): void
+    {
+        $test_static_url_handler = $this->createInstanceOf(TestStaticURLHandler::class);
+
+        $this->assertEquals(
+            $output,
+            self::callMethod(
+                $test_static_url_handler,
+                'buildQuestionURL',
+                [$input, $this->createMock(ilCtrl::class)]
+            )
+        );
+    }
+
+    public static function buildQuestionURLDataProvider(): array
+    {
+        return [
+            'empty' => ['', ''],
+            'string' => ['string', ''],
+            'strING' => ['strING', ''],
+            'STRING' => ['STRING', '']
+        ];
+    }
+}
