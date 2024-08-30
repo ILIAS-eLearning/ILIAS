e51d1e1d9aba337acf73dc0e08a281f7d0294467
Matheus Zych <mzych@databay.de>
2024-08-30 08:43:26 +0200

    Adds unit tests for QuestionPoolLinkedTitleBuilder

--- components/ILIAS/Test/tests/Questions/QuestionPoolLinkedTitleBuilderTest.php
+++ components/ILIAS/Test/tests/Questions/QuestionPoolLinkedTitleBuilderTest.php
@@ -2,10 +2,15 @@
 
 namespace Questions;
 
+use ilAccessHandler;
+use ilCtrl;
 use ILIAS\Test\Questions\QuestionPoolLinkedTitleBuilder;
+use ilLanguage;
 use ilTestBaseTestCase;
 use PHPUnit\Framework\MockObject\Exception;
 use ReflectionException;
+use ILIAS\UI\Factory;
+use ILIAS\UI\Renderer;
 
 class QuestionPoolLinkedTitleBuilderTest extends ilTestBaseTestCase
 {
@@ -15,6 +20,84 @@ class QuestionPoolLinkedTitleBuilderTest extends ilTestBaseTestCase
     public function testConstruct(): void
     {
         $question_pool_linked_title_builder = $this->createTraitInstanceOf(QuestionPoolLinkedTitleBuilder::class);
-        $this->assertInstanceOf(self::DYNAMIC_CLASS, $question_pool_linked_title_builder);
+        $this->assertInstanceOf(self::DYNAMIC_CLASS . self::$DYNAMIC_CLASS_COUNT, $question_pool_linked_title_builder);
+    }
+
+    /**
+     * @dataProvider buildPossiblyLinkedTestTitleDataProvider
+     * @throws ReflectionException|Exception
+     */
+    public function testBuildPossiblyLinkedTestTitle(string $input, string $output): void
+    {
+        $question_pool_linked_title_builder = $this->createTraitInstanceOf(QuestionPoolLinkedTitleBuilder::class);
+
+        $this->assertEquals(
+            $output,
+            $question_pool_linked_title_builder->buildPossiblyLinkedTestTitle(
+                $this->createMock(ilCtrl::class),
+                $this->createMock(ilAccessHandler::class),
+                $this->createMock(ilLanguage::class),
+                $this->createMock(Factory::class),
+                $this->createMock(Renderer::class),
+                1,
+                $input,
+                false
+            )
+        );
+    }
+
+    public static function buildPossiblyLinkedTestTitleDataProvider(): array
+    {
+        return [
+            'empty' => ['', ' ()'],
+            'string' => ['string', 'string ()'],
+            'strING' => ['strING', 'strING ()'],
+            'STRING' => ['STRING', 'STRING ()']
+        ];
+    }
+
+    /**
+     * @dataProvider buildPossiblyLinkedQuestionPoolTitleDataProvider
+     * @throws ReflectionException|Exception
+     */
+    public function testBuildPossiblyLinkedQuestionPoolTitle(array $input, string $output): void
+    {
+        $question_pool_linked_title_builder = $this->createTraitInstanceOf(QuestionPoolLinkedTitleBuilder::class);
+
+        $this->assertEquals(
+            $output,
+            $question_pool_linked_title_builder->buildPossiblyLinkedQuestionPoolTitle(
+                $this->createMock(ilCtrl::class),
+                $this->createMock(ilAccessHandler::class),
+                $this->createMock(ilLanguage::class),
+                $this->createMock(Factory::class),
+                $this->createMock(Renderer::class),
+                $input['qpl_id'],
+                $input['title'],
+                false
+            )
+        );
+    }
+
+    public static function buildPossiblyLinkedQuestionPoolTitleDataProvider(): array
+    {
+        return [
+            'null_empty' => [['qpl_id' => null, 'title' => ''], ''],
+            'negative_one_empty' => [['qpl_id' => -1, 'title' => ''], ''],
+            'zero_empty' => [['qpl_id' => 0, 'title' => ''], ''],
+            'one_empty' => [['qpl_id' => 1, 'title' => ''], ''],
+            'null_string' => [['qpl_id' => null, 'title' => 'string'], 'string'],
+            'negative_one_string' => [['qpl_id' => -1, 'title' => 'string'], ''],
+            'zero_string' => [['qpl_id' => 0, 'title' => 'string'], ''],
+            'one_string' => [['qpl_id' => 1, 'title' => 'string'], ''],
+            'null_strING' => [['qpl_id' => null, 'title' => 'strING'], 'strING'],
+            'negative_one_strING' => [['qpl_id' => -1, 'title' => 'strING'], ''],
+            'zero_strING' => [['qpl_id' => 0, 'title' => 'strING'], ''],
+            'one_strING' => [['qpl_id' => 1, 'title' => 'strING'], ''],
+            'null_STRING' => [['qpl_id' => null, 'title' => 'STRING'], 'STRING'],
+            'negative_one_STRING' => [['qpl_id' => -1, 'title' => 'STRING'], ''],
+            'zero_STRING' => [['qpl_id' => 0, 'title' => 'STRING'], ''],
+            'one_STRING' => [['qpl_id' => 1, 'title' => 'STRING'], '']
+        ];
     }
 }
