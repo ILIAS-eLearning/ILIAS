c931acb077c291f6f4c708afee9c79855b3fbe09
Matheus Zych <mzych@databay.de>
2024-09-05 10:18:27 +0200

    Fixes unit tests for TestGlobalSettingsRepository

--- components/ILIAS/Test/tests/Administration/TestGlobalSettingsRepositoryTest.php
+++ components/ILIAS/Test/tests/Administration/TestGlobalSettingsRepositoryTest.php
@@ -19,45 +19,18 @@
 declare(strict_types=1);
 
 use ILIAS\Test\Administration\TestGlobalSettingsRepository;
-use ILIAS\Test\Administration\TestLoggingSettings;
+use PHPUnit\Framework\MockObject\Exception;
 
 class TestGlobalSettingsRepositoryTest extends ilTestBaseTestCase
 {
-    private TestGlobalSettingsRepository $testGlobalSettingsRepository;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->testGlobalSettingsRepository = new TestGlobalSettingsRepository(new ilSetting("assessment"));
-    }
-
     /**
-     * @dataProvider provideLoggingSettings
+     * @throws ReflectionException|Exception
      */
-    public function test_get_and_storeLoggingSettings($testLoggingSettings): void
+    public function testConstruct(): void
     {
-        $this->testGlobalSettingsRepository->storeLoggingSettings($testLoggingSettings);
-        $settings = $this->testGlobalSettingsRepository->getLoggingSettings();
-        $this->assertEquals($testLoggingSettings->isLoggingEnabled(), $settings->isLoggingEnabled());
-        $this->assertEquals($testLoggingSettings->isIPLoggingEnabled(), $settings->isIPLoggingEnabled());
-    }
-
-    public static function provideLoggingSettings(): array
-    {
-        return [
-            "dataset 1: both enabled" => [
-                "testLoggingSettings" => new TestLoggingSettings(true, true)
-            ],
-            "dataset 2: only logging enabled " => [
-                "testLoggingSettings" => new TestLoggingSettings(true, false)
-            ],
-            "dataset 3: only ip logging enabled" => [
-                "testLoggingSettings" => new TestLoggingSettings(false, true)
-            ],
-            "dataset 4: both disabled" => [
-                "testLoggingSettings" => new TestLoggingSettings(false, false)
-            ]
-        ];
+        $this->assertInstanceOf(
+            TestGlobalSettingsRepository::class,
+            $this->createInstanceOf(TestGlobalSettingsRepository::class)
+        );
     }
 }
