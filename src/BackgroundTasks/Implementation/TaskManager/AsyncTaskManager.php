bc3575e2b619b27ab3947f42047c3f1fb1b6f1c9
mjansen <mjansen@databay.de>
2024-09-09 15:30:31 +0200

    BackgroundTask: Use `CLIENT_ID` constant as fallback for SOAP request

--- src/BackgroundTasks/Implementation/TaskManager/AsyncTaskManager.php
+++ src/BackgroundTasks/Implementation/TaskManager/AsyncTaskManager.php
@@ -48,12 +48,16 @@ class AsyncTaskManager extends BasicTaskManager
         $soap_client->enableWSDL(true);
         $soap_client->init();
         $session_id = session_id();
-        $client_id = $DIC->http()->wrapper()->cookie()->has('ilClientId')
-            ? $DIC->http()->wrapper()->cookie()->retrieve(
-                'ilClientId',
-                $DIC->refinery()->kindlyTo()->string()
-            )
-            : '';
+        $client_id = $DIC->http()->wrapper()->cookie()->retrieve(
+            'ilClientId',
+            $DIC->refinery()->byTrying([
+                $DIC->refinery()->kindlyTo()->string(),
+                $DIC->refinery()->always(
+                    defined('CLIENT_ID') ? CLIENT_ID : null
+                )
+            ])
+        );
+
         try {
             $soap_client->call(self::CMD_START_WORKER, array(
                 $session_id . '::' . $client_id,
