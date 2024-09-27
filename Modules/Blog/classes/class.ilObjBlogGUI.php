a4456112218776e5a100912e37111f86e6faa132
Alex Killing <killing@leifos.de>
2024-09-27 14:49:01 +0200

    42169: Blog doesn't appear

--- Modules/Blog/classes/class.ilObjBlogGUI.php
+++ Modules/Blog/classes/class.ilObjBlogGUI.php
@@ -1921,7 +1921,7 @@ class ilObjBlogGUI extends ilObject2GUI implements ilDesktopItemHandling
             $wtpl->setCurrentBlock("keyword");
             foreach ($keywords as $keyword => $counter) {
                 if (!$a_link_template) {
-                    $ilCtrl->setParameter($this, "kwd", urlencode($keyword)); // #15885
+                    $ilCtrl->setParameter($this, "kwd", urlencode((string) $keyword)); // #15885
                     $url = $ilCtrl->getLinkTarget($this, $a_list_cmd);
                     $ilCtrl->setParameter($this, "kwd", "");
                 } else {
@@ -2194,7 +2194,7 @@ class ilObjBlogGUI extends ilObject2GUI implements ilDesktopItemHandling
 
         $keywords = array();
         foreach ($tmp as $item) {
-            $keywords[$item["keyword"]] = $item["counter"];
+            $keywords[(string) $item["keyword"]] = $item["counter"];
         }
         return $keywords;
     }
