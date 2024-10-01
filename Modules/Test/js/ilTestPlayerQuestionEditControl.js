9a9f796f52bc1ed81af25fb9a5de26f89d33da43
Stephan Kergomard <webmaster@kergomard.ch>
2024-10-01 18:16:35 +0200

    Test: Fix Autosave Message
    See: https://mantis.ilias.de/view.php?id=40959

--- Modules/Test/js/ilTestPlayerQuestionEditControl.js
+++ Modules/Test/js/ilTestPlayerQuestionEditControl.js
@@ -65,7 +65,11 @@ il.TestPlayerQuestionEditControl = new function() {
         backgroundDetectorUrl: '',              // url called by the background detector
         forcedInstantFeedback: false,            // forced feedback will change the submit command
         nextQuestionLocks: false,
+<<<<<<< HEAD
         questionLocked: false
+=======
+        autosaveFailureMessage: ''
+>>>>>>> bba7f427e9f (Test: Fix Autosave Message)
     };
 
     /**
@@ -729,7 +733,7 @@ il.TestPlayerQuestionEditControl = new function() {
      * @param jqXHR
      */
     function autoSaveFailure(jqXHR) {
-      let responseText = 'Autosafe Timeout'
+      let responseText = config.autosaveFailureMessage;
       if (typeof jqXHR.responseText !== 'undefined') {
         responseText = jqXHR.responseText ;
       }
