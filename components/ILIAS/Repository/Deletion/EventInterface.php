454e212eb1a80b6bd3dd57964747df2db50d6e5d
Alex Killing <killing@leifos.de>
2024-10-11 19:52:00 +0200

    deletion process refactoring, part i

---
+++ components/ILIAS/Repository/Deletion/EventInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
+declare(strict_types=1);
+
+namespace ILIAS\Repository\Deletion;
+
+use ILIAS\Repository\InternalDomainService;
+
+interface EventInterface
+{
+    public function beforeMoveToTrash(int $ref_id, array $subnodes): void;
+    public function afterMoveToTrash(int $ref_id, int $old_parent_ref_id): void;
+}
