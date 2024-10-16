53092015cc544d67702c49a14ba31d3da5381103
Lukas Eichenauer <leichenauer@databay.de>
2024-10-16 17:22:13 +0200

    add empty cell component to display a placeholder when provided cell content is empty

---
+++ components/ILIAS/UI/src/Component/Table/EmptyCell.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
+declare(strict_types=1);
+
+namespace ILIAS\UI\Component\Table;
+
+/**
+ * A EmptyCell represents the content in a table cell when the cell is empty. It is used to display a placeholder
+ * in the cell.
+ */
+interface EmptyCell extends \ILIAS\UI\Component\Component
+{
+    public function getEmptyPlaceholder(): string;
+}
---
+++ components/ILIAS/UI/src/Implementation/Component/Table/EmptyCell.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
+declare(strict_types=1);
+
+namespace ILIAS\UI\Implementation\Component\Table;
+
+use ILIAS\UI\Component\Table as T;
+use ILIAS\UI\Implementation\Component\ComponentHelper;
+
+class EmptyCell implements T\EmptyCell
+{
+    use ComponentHelper;
+
+    public function __construct(
+        protected string $empty_placeholder
+    ) {
+    }
+
+    public function getEmptyPlaceholder(): string
+    {
+        return $this->empty_placeholder;
+    }
+}
