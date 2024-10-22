d5d0f4807a60ed5111c6807c39abbbc5224a80ec
Stephan Kergomard <webmaster@kergomard.ch>
2024-10-22 08:18:53 +0200

    Test: Readd Lookup For TestId

--- components/ILIAS/Test/src/Participants/ParticipantRepository.php
+++ components/ILIAS/Test/src/Participants/ParticipantRepository.php
@@ -144,6 +144,22 @@ class ParticipantRepository
         }
     }
 
+    public function lookupTestIdByActiveId(int $active_id): int
+    {
+        $result = $this->database->queryF(
+            'SELECT test_fi FROM tst_active WHERE active_id = %s',
+            ['integer'],
+            [$active_id]
+        );
+        $test_id = -1;
+        if ($this->database->numRows($result) > 0) {
+            $row = $this->database->fetchAssoc($result);
+            $test_id = (int) $row['test_fi'];
+        }
+
+        return $test_id;
+    }
+
     /**
      *
      * @param array<Participant> $selected_participants
