1e456d8a8cf128530db97524d1613a32ff07642a
Nils Haagen <nhaagen@cat06.de>
2025-03-24 09:02:10 +0100

    UI/Input/Pagination: 38501/38505, use buttons with symbols for rocker navigation

--- components/ILIAS/UI/tests/Component/Input/ViewControl/ViewControlPaginationTest.php
+++ components/ILIAS/UI/tests/Component/Input/ViewControl/ViewControlPaginationTest.php
@@ -171,11 +171,9 @@ class ViewControlPaginationTest extends ViewControlTestBase
         $expected = $this->brutallyTrimHTML('
 <div class="il-viewcontrol il-viewcontrol-pagination l-bar__element" id="id_13">
     <div class="dropdown il-viewcontrol-pagination__sectioncontrol">
-            <div class="btn btn-ctrl browse previous">
-                <a tabindex="0" class="glyph" aria-label="back" id="id_8">
-                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                </a>
-            </div>
+        <button class="btn btn-link" id="id_8">
+            <span class="glyph" aria-label="back" role="img"><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span></span>
+        </button>
 
         <button class="btn btn-link" id="id_1">1</button>
         <span class="il-viewcontrol-pagination__spacer">...</span>
@@ -187,11 +185,9 @@ class ViewControlPaginationTest extends ViewControlTestBase
         <span class="il-viewcontrol-pagination__spacer">...</span>
         <button class="btn btn-link" id="id_7">21</button>
 
-        <div class="btn btn-ctrl browse next">
-            <a tabindex="0" class="glyph" aria-label="next" id="id_9">
-                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-            </a>
-        </div>
+        <button class="btn btn-link" id="id_9">
+            <span class="glyph" aria-label="next" role="img"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span></span>
+        </button>
     </div>
 
     <div class="dropdown il-viewcontrol-pagination__num-of-items">
