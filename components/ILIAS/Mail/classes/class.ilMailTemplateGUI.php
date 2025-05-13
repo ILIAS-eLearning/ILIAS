baf69ec5097a075af45f9b0de0ec0a58bba563c1
Fabian Helfer <fhelfer@databay.de>
2025-05-13 11:18:22 +0200

    Bugfix DataTable ilMailTemplateGUI ALL_OBJECTS action

--- components/ILIAS/Mail/classes/class.ilMailTemplateGUI.php
+++ components/ILIAS/Mail/classes/class.ilMailTemplateGUI.php
@@ -307,8 +307,14 @@ class ilMailTemplateGUI
         if ($this->http->wrapper()->query()->has('mail_template_tpl_ids')) {
             $templateIds = $this->http->wrapper()->query()->retrieve(
                 'mail_template_tpl_ids',
-                $this->refinery->kindlyTo()->listOf($this->refinery->kindlyTo()->int())
+                $this->refinery->kindlyTo()->listOf($this->refinery->kindlyTo()->string())
             );
+            if ($templateIds === ['ALL_OBJECTS']) {
+                $templateIds = array_map(
+                    static fn(array $template): int => (int) ($template['tpl_id'] ?? 0),
+                    $this->service->listAllTemplatesAsArray()
+                );
+            }
         }
 
         if (0 === count($templateIds)) {
