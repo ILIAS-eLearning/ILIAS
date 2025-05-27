543f0c7fc2d4e3a9d136cef895086ae12e926d90
Ferdinand Engländer <ferdinand.englaender@concepts-and-training.de>
2025-05-27 16:51:12 +0200

    10/UI/Button/month Firefox fallback 43178

--- components/ILIAS/UI/resources/js/Button/button.js
+++ components/ILIAS/UI/resources/js/Button/button.js
@@ -19,13 +19,97 @@ il.UI.button = il.UI.button || {};
 (function ($, il) {
   il.UI.button = (function ($) {
     /* month button */
+    const isMonthSupported = () => {
+      const input = document.createElement('input');
+      input.setAttribute('type', 'month');
+
+      // Valid browsers will reject this value in a month input, Firefox does currently accept it.
+      const notADateValue = 'not-a-month';
+      input.value = notADateValue;
+      const rejectsInvalid = input.value !== notADateValue;
+
+      // Safari does reject the input but also has a bad month picker on desktop
+      const userAgent = navigator.userAgent.toLowerCase();
+      const isIphone = userAgent.includes('iphone');
+      const isIpad = userAgent.includes('ipad');
+      const isSafari = userAgent.includes('safari') && !userAgent.includes('chrome'); // chrome's user agent also includes the word 'safari'
+
+      const isLikelyBadWebKit = isSafari && !isIphone && !isIpad;
+
+      return rejectsInvalid && !isLikelyBadWebKit;
+    };
+
     const initMonth = function (id) {
+      const container = document.querySelector(`#${id}`);
       const btn = document.querySelector(`#${id} > input`);
-      btn.addEventListener('change', (e) => {
-        const value = e.srcElement.value.split('-').reverse().join('-');
-        const id = e.srcElement.closest('.il-btn-month').getAttribute('id');
+      const triggerEvent = (id, value) => {
         $(`#${id}`).trigger('il.ui.button.month.changed', [id, value]);
+      };
+      btn.addEventListener('change', (e) => {
+        const value = e.target.value.split('-').reverse().join('-');
+        const id = e.target.closest('.il-btn-month').getAttribute('id');
+        triggerEvent(id, value);
       });
+
+      //
+      // Fallback for browsers that do not support a month picker e.g. Firefox, Safari on desktop
+      // Be aware that this disregards the initially rendered input element, but fires the same event.
+      //
+      if (!isMonthSupported()) {
+        btn.style.display = 'none';
+        const valueOnLoad = btn.value.split('-');
+
+        // month selector dropdown
+        const monthDropdown = document.createElement('select');
+        function populateMonthDropdown(selectElement, locale = navigator.language) {
+          // DateTimeFormat generates month names in the client's language
+          const formatter = new Intl.DateTimeFormat(locale, { month: 'long' });
+
+          for (let i = 0; i < 12; i++) {
+            const monthValue = String(i + 1).padStart(2, '0');
+            const date = new Date(2020, i, 1);
+            const monthLabel = formatter.format(date);
+
+            const option = document.createElement('option');
+            option.value = monthValue;
+            option.textContent = monthLabel;
+
+            selectElement.appendChild(option);
+          }
+        }
+        populateMonthDropdown(monthDropdown);
+        monthDropdown.value = valueOnLoad[1];
+        container.appendChild(monthDropdown);
+
+        // year input
+        const yearNumber = document.createElement('input');
+        // relying on inputmode rather than type=number because html5 validation only works on type=text
+        yearNumber.setAttribute('type', 'text');
+        yearNumber.setAttribute('inputmode', 'numeric');
+        yearNumber.setAttribute('minlength', '2');
+        yearNumber.setAttribute('maxlength', '4');
+        yearNumber.setAttribute(
+          'pattern',
+          '([0-9]{2})|([0-9]{4})',
+        );
+        yearNumber.style.width = '6ch';
+        yearNumber.value = valueOnLoad[0];
+        container.appendChild(yearNumber);
+
+        // handle input
+        const passOnValues = () => {
+          if (yearNumber.value.length === 2) { // converting two digit years like 19 to 2019
+            yearNumber.value = `20${yearNumber.value}`;
+          }
+          if (yearNumber.checkValidity()) { // prevents the event to trigger on 1 or 3 digit inputs
+            const monthYear = `${monthDropdown.value}-${yearNumber.value}`;
+            triggerEvent(id, monthYear);
+          }
+        };
+
+        yearNumber.addEventListener('change', passOnValues);
+        monthDropdown.addEventListener('change', passOnValues);
+      }
     };
 
     /* toggle button */
