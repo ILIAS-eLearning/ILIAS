f33da9c414364e3fb53b2f0b553a9efdbfaeaf99
Tim Schmitz <schmitz@leifos.de>
2025-06-13 09:58:20 +0200

    fix cp notices

--- components/ILIAS/BookingManager/Objects/class.ObjectsManager.php
+++ components/ILIAS/BookingManager/Objects/class.ObjectsManager.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -14,6 +12,8 @@ declare(strict_types=1);
  * https://www.ilias.de
  * https://github.com/ILIAS-eLearning
  *********************************************************************/
+
+declare(strict_types=1);
 
 namespace ILIAS\BookingManager\Objects;
 
