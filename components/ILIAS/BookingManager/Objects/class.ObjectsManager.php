7e12d48189cd7dfa2ae17a233c1e803ed7dd885c
Tim Schmitz <schmitz@leifos.de>
2025-06-13 10:01:43 +0200

    fix cp notices even harder

--- components/ILIAS/BookingManager/Objects/class.ObjectsManager.php
+++ components/ILIAS/BookingManager/Objects/class.ObjectsManager.php
@@ -3,14 +3,17 @@
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
+ *
  * ILIAS is licensed with the GPL-3.0,
  * see https://www.gnu.org/licenses/gpl-3.0.en.html
  * You should have received a copy of said license along with the
  * source code, too.
+ *
  * If this is not the case or you just want to try ILIAS, you'll find
  * us at:
  * https://www.ilias.de
  * https://github.com/ILIAS-eLearning
+ *
  *********************************************************************/
 
 declare(strict_types=1);
