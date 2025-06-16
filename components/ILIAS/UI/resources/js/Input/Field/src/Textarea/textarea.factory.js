c386c07668ccbc9aaf7bc45e51f0e092ceb35c5e
Luka Stocker <luka.stocker@concepts-and-training.de>
2025-06-16 10:15:03 +0200

    UI: adjust markdown javascript files.

--- components/ILIAS/UI/resources/js/Input/Field/src/Textarea/textarea.factory.js
+++ components/ILIAS/UI/resources/js/Input/Field/src/Textarea/textarea.factory.js
@@ -13,35 +13,35 @@
  * https://github.com/ILIAS-eLearning
  */
 
-import Textarea from "./textarea.class.js";
+import Textarea from './textarea.class.js';
 
 /**
  * @author Thibeau Fuhrer <thibeau@sr.solutions>
  */
 export default class TextareaFactory {
-    /**
+  /**
      * @type {Array<string, Textarea>}
      */
-    instances = [];
+  instances = [];
 
-    /**
+  /**
      * @param {string} input_id
      * @return {void}
      * @throws {Error} if the input was already initialized.
      */
-    init(input_id) {
-        if (undefined !== this.instances[input_id]) {
-            throw new Error(`Textarea with input-id '${input_id}' has already been initialized.`);
-        }
-
-        this.instances[input_id] = new Textarea(input_id);
+  init(input_id) {
+    if (undefined !== this.instances[input_id]) {
+      throw new Error(`Textarea with input-id '${input_id}' has already been initialized.`);
     }
 
-    /**
+    this.instances[input_id] = new Textarea(input_id);
+  }
+
+  /**
      * @param {string} input_id
      * @return {Textarea|null}
      */
-    get(input_id) {
-        return this.instances[input_id] ?? null;
-    }
+  get(input_id) {
+    return this.instances[input_id] ?? null;
+  }
 }
