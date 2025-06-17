26c8aabc1c9460727570316cf8f7a4889df42cd2
Matheus Zych <mzych@databay.de>
2025-06-17 08:15:10 +0200

    Fixes syncing a matching question back to the original question pool question which removes the associated images of both questions

--- Modules/TestQuestionPool/classes/class.assMatchingQuestion.php
+++ Modules/TestQuestionPool/classes/class.assMatchingQuestion.php
@@ -1595,6 +1595,7 @@ class assMatchingQuestion extends assQuestion implements ilObjQuestionScoringAdj
             ilFileUtils::makeDirParents($origImagePath);
             ilFileUtils::rCopy($dupImagePath, $origImagePath);
         }
+        assQuestion::instantiateQuestion($origQuestionId)->copyImages($dupQuestionId, origParentObjId);
     }
 
     protected function createMatchingTerm(string $term = '', string $picture = '', int $identifier = 0): assAnswerMatchingTerm
