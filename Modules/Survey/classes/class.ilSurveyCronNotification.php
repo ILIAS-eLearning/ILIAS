02b9baaf9e866412ec9de70b7200e1cdeab2324a
Felix Jonas Wiegleb <jwiegleb@databay.de>
2025-06-27 13:25:07 +0200

    Truncate notifications to fit DB limit

--- Modules/Survey/classes/class.ilSurveyCronNotification.php
+++ Modules/Survey/classes/class.ilSurveyCronNotification.php
@@ -28,6 +28,7 @@ class ilSurveyCronNotification extends ilCronJob
 {
     protected ilLanguage $lng;
     protected ilTree $tree;
+    public const MAX_MESSAGE_LENGTH = 397;
 
     public function __construct()
     {
@@ -104,8 +105,18 @@ class ilSurveyCronNotification extends ilCronJob
         $result->setStatus($status);
 
         if (count($message)) {
-            $result->setMessage("Ref-Ids: " . implode(", ", $message) . ' / ' . "#" . count($message));
+            $full_msg = "Ref-Ids: " . implode(", ", $message) . ' / ' . "#" . count($message);
+
+            if (mb_strlen($full_msg) > self::MAX_MESSAGE_LENGTH) {
+                $short_msg = mb_substr($full_msg, 0, self::MAX_MESSAGE_LENGTH) . '...';
+                $log->info("Notification message was truncated to fit DB limit. Full message: \"$full_msg\"");
+            } else {
+                $short_msg = $full_msg;
+            }
+
+            $result->setMessage($short_msg);
         }
+
         $log->debug("end");
         return $result;
     }
