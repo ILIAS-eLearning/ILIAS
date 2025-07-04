1107f45ee90efada82e23cf4e1d5c958f7ce9563
fneumann <fneumann@databay.de>
2025-07-04 13:55:10 +0200

    Improve example explanation

--- components/ILIAS/UI/src/examples/Legacy/LatexContent/base.php
+++ components/ILIAS/UI/src/examples/Legacy/LatexContent/base.php
@@ -8,9 +8,17 @@ namespace ILIAS\UI\examples\Legacy\LatexContent;
  * ---
  * description: >
  *   Example for rendering a legacy content with laTeX code.
+ *   The content can be text or HTML.
+ *   LaTeX code within is embedded in the delimiters [tex] and [/tex]
  *
  * expected output: >
- *   ILIAS shows a box including a formula which is rendered by MathJax.
+ *   ILIAS shows the string 'This should be rendered as a formula: '
+ *   followed by a mathematical function definition with an integral.
+ *   The function definition is rendered graphically.
+ *   The rendering may take a tenth of a second when the page is shown.
+ *   Before that the LaTeX source code is shown.
+ *   A right click with the mouse on the rendered expression will show a popup menu from MathJax.
+ *   Here you can set different display options.
  * ---
  */
 function base()
@@ -21,7 +29,7 @@ function base()
     $renderer = $DIC->ui()->renderer();
 
     //Init Component
-    $legacy = $f->legacy()->latexContent('[tex]f(x)=\int_{-\infty}^x e^{-t^2}dt[/tex]');
+    $legacy = $f->legacy()->latexContent('This should be rendered as a formula: [tex]f(x)=\int_{-\infty}^x e^{-t^2}dt[/tex]');
 
     //Render
     return $renderer->render($legacy);
