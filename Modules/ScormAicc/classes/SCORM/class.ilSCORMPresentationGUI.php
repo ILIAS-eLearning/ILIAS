dfcfe1903d5e5665462699e01ed48ada9fcc675b
sroenchen <145426925+sroenchen@users.noreply.github.com>
2025-07-16 10:50:48 +0200

    Mantis #45149 - fix Scorm1.2 favicons in Ilias9
    https://mantis.ilias.de/view.php?id=45149

--- Modules/ScormAicc/classes/SCORM/class.ilSCORMPresentationGUI.php
+++ Modules/ScormAicc/classes/SCORM/class.ilSCORMPresentationGUI.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -17,6 +15,8 @@ declare(strict_types=1);
  * https://github.com/ILIAS-eLearning
  *
  *********************************************************************/
+
+declare(strict_types=1);
 
 /**
 * Class ilSCORMPresentationGUI
@@ -146,6 +146,7 @@ class ilSCORMPresentationGUI
         }
         $api_link = $this->ctrl->getLinkTarget($this, "apiInitData");
         $this->tpl->setVariable("TITLE", $this->slm->getTitle());
+        $this->tpl->setVariable("ICON", ilUtil::getImagePath("favicon.ico", ""));
         $this->tpl->setVariable("API_LINK", $api_link);
         $this->tpl->printToStdout("DEFAULT", false, true);
 
