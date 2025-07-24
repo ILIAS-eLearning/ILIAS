f5878442679948061260e48637ede4dab36a184e
Matheus Zych <mzych@databay.de>
2025-07-24 08:06:05 +0200

    Adds small adjustments after review

--- components/ILIAS/BookingManager/Participants/class.ilBookingParticipant.php
+++ components/ILIAS/BookingManager/Participants/class.ilBookingParticipant.php
@@ -183,28 +183,27 @@ class ilBookingParticipant
             $name = $user_name['lastname'] . ", " . $user_name['firstname'];
             $index = $a_booking_pool . "_" . $row['user_id'];
 
+            $title = (string) ($row['title'] ?? '');
+            $object_id = (int) ($row['object_id'] ?? -1);
             if (!isset($res[$index])) {
                 $res[$index] = ['object_title' => [], 'obj_count' => 0, 'object_ids' => [], 'name' => $name];
 
-                $title = $row['title'];
-                $object_id = $row['object_id'];
                 if (
                     $status !== ilBookingReservation::STATUS_CANCELLED
-                    && $title !== ""
-                    && $title !== null
-                    && $object_id !== null
+                    && $title !== ''
+                    && $object_id !== -1
                 ) {
                     $res[$index]['object_title'] = [$title];
                     $res[$index]['obj_count'] = 1;
                     $res[$index]['object_ids'][] = $object_id;
                 }
-            } elseif ($row['title'] !== "" && (!in_array($row['title'], $res[$index]['object_title'], true) && $status != ilBookingReservation::STATUS_CANCELLED)) {
-                $res[$index]['object_title'][] = $row['title'];
+            } elseif ($title !== '' && (!in_array($title, $res[$index]['object_title'], true) && $status !== ilBookingReservation::STATUS_CANCELLED)) {
+                $res[$index]['object_title'][] = $title;
                 if (!isset($res[$index]['obj_count'])) {
                     $res[$index]['obj_count'] = 0;
                 }
                 $res[$index]['obj_count'] += 1;
-                $res[$index]['object_ids'][] = $row['object_id'];
+                $res[$index]['object_ids'][] = $object_id;
             }
             $res[$index]['user_id'] = $row['user_id'];
         }
