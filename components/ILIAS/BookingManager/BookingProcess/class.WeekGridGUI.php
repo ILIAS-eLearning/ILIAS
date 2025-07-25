d2d11169a02e05415b21ad72b61f3dc8858bc32c
Matheus Zych <mzych@databay.de>
2025-07-25 17:10:18 +0200

    Fixes copyright in week grid gui

--- components/ILIAS/BookingManager/BookingProcess/class.WeekGridGUI.php
+++ components/ILIAS/BookingManager/BookingProcess/class.WeekGridGUI.php
@@ -1,19 +1,22 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
+ *
  * ILIAS is licensed with the GPL-3.0,
  * see https://www.gnu.org/licenses/gpl-3.0.en.html
  * You should have received a copy of said license along with the
  * source code, too.
+ *
  * If this is not the case or you just want to try ILIAS, you'll find
  * us at:
  * https://www.ilias.de
  * https://github.com/ILIAS-eLearning
+ *
  *********************************************************************/
+
+declare(strict_types=1);
 
 namespace ILIAS\BookingManager\BookingProcess;
 
@@ -126,8 +129,6 @@ class WeekGridGUI
                 $data["end_ts"] = $end->get(IL_CAL_UNIX);
                 $data["entries"] = $this->getEntriesForCell($data["start_ts"], $data["end_ts"]);
                 $cells[$week][$hour] = $data;
-                // store how much slots are max. to be displayed in parallel per day
-                $cells[$week]["col_span"] = 1;
             }
             $week++;
         }
