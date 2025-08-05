232455dfbdf011e7292607b612f2d9a14c461770
mjansen <mjansen@databay.de>
2025-08-05 08:27:43 +0200

    Test: Fix type check against redirection URL

--- Modules/Test/classes/class.ilTestPlayerAbstractGUI.php
+++ Modules/Test/classes/class.ilTestPlayerAbstractGUI.php
@@ -757,10 +757,10 @@ abstract class ilTestPlayerAbstractGUI extends ilTestServiceGUI
         }
 
         // redirect after test
-        $redirection_url = $this->object->getMainSettings()->getFinishingSettings()->getRedirectionUrl();
-        if (!$this->object->canShowTestResults($this->test_session)
-            && $this->object->getMainSettings()->getFinishingSettings()->getRedirectionMode() !== '0'
-            && $redirection_url !== '') {
+        $redirection_url = $this->object->getMainSettings()->getFinishingSettings()->getRedirectionUrl() ?? '';
+        if ($redirection_url !== '' &&
+            !$this->object->canShowTestResults($this->test_session)
+            && $this->object->getMainSettings()->getFinishingSettings()->getRedirectionMode() !== '0') {
             if ($this->object->isRedirectModeKiosk()) {
                 if ($this->object->getKioskMode()) {
                     ilUtil::redirect($redirection_url);
