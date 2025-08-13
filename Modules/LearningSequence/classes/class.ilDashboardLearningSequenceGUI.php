18d433561a1fa42e9b9910b2ad992e9309f97747
iszmais <iszmais@databay.de>
2025-08-13 15:30:22 +0200

    refactor dashboard manage modal translations

--- Modules/LearningSequence/classes/class.ilDashboardLearningSequenceGUI.php
+++ Modules/LearningSequence/classes/class.ilDashboardLearningSequenceGUI.php
@@ -104,7 +104,7 @@ class ilDashboardLearningSequenceGUI extends ilDashboardBlockGUI
 
     public function getBlockType(): string
     {
-        return 'pdlern';
+        return 'dash_ls';
     }
 
     public function confirmedRemove(array $ids): void
@@ -129,10 +129,5 @@ class ilDashboardLearningSequenceGUI extends ilDashboardBlockGUI
     public function removeMultipleEnabled(): bool
     {
         return true;
-    }
-
-    public function getRemoveMultipleActionText(): string
-    {
-        return $this->lng->txt('pd_unsubscribe_multiple_memberships');
     }
 }
