ILIAS-eLearning/ILIAS

commit 9d54dbd408ad2d3e8d445807bb7f972929898cf3

updated shib_login und shib_logout

shib_logout.php
@@ -1,38 +1,33 @@
 <?php
-/* Copyright (c) 1998-2009 ILIAS open source, Extended GPL, see docs/LICENSE */
-
-// Just for debugging the WSDL part
-ini_set("soap.wsdl_cache_enabled", "0"); // disabling WSDL cache
-
-/**
- * Shibboleth login script for ilias
+/******************************************************************************
  *
- * $Id: shib_login.php 15434 2007-11-27 09:02:01Z smeyer $
+ * This file is part of ILIAS, a powerful learning management system.
  *
- * @author  Lukas Haemmerle <haemmerle@switch.ch>
- * @package ilias-layout
- */
-
-// Requirements:
-// PHP 5 with SOAP support (should be available in default deployment)
-
-// Front channel logout
-
-// Note: Generally the back-channel logout should be used once the Shibboleth
-//       Identity Provider supports Single Log Out!
-//       Front-channel logout is not of much use.
-
-if (isset($_GET['return']) && isset($_GET['action']) && $_GET['action'] == 'logout') {
-
-    // Load all the IILIAS stuff
-    require_once "include/inc.header.php";
-
+ * ILIAS is licensed with the GPL-3.0, you should have received a copy
+ * of said license along with the source code.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *****************************************************************************/
+/** @noRector */
+require_once("libs/composer/vendor/autoload.php");
+global $DIC;
+$q = $DIC->http()->wrapper()->query();
+if(
+    $q->has('return')
+    && $q->has('action')
+    && $q->retrieve('action', $DIC->refinery()->to()->string()) === 'logout'
+) {
+    ilInitialisation::initILIAS();
     // Logout out user from application
     // Destroy application session/cookie etc
     $GLOBALS['DIC']['ilAuthSession']->logout();
 
     // Finally, send user to the return URL
-    ilUtil::redirect($_GET['return']);
+    ilUtil::redirect($q->retrieve('action', $DIC->refinery()->kindlyTo()->string()));
 }
 
 // Back channel logout //
@@ -44,11 +39,9 @@ if (isset($_GET['return']) && isset($_GET['action']) && $_GET['action'] == 'logo
 //       See function LogoutNotification below
 
 elseif (!empty($HTTP_RAW_POST_DATA)) {
-    include_once "Services/Context/classes/class.ilContext.php";
     ilContext::init(ilContext::CONTEXT_SOAP);
 
     // Load ILIAS libraries and initialise ILIAS in non-web context
-    require_once("Services/Init/classes/class.ilInitialisation.php");
     ilInitialisation::initILIAS();
 
     // Set SOAP header

shib_login.php
@@ -1,25 +1,40 @@
 <?php
-/* Copyright (c) 1998-2009 ILIAS open source, Extended GPL, see docs/LICENSE */
-
-/**
- * Shibboleth login script for ilias
+/******************************************************************************
  *
- * $Id$
- * @author  Lukas Haemmerle <haemmerle@switch.ch>
- * @package ilias-layout
- */
-
-include_once './Services/Context/classes/class.ilContext.php';
+ * This file is part of ILIAS, a powerful learning management system.
+ *
+ * ILIAS is licensed with the GPL-3.0, you should have received a copy
+ * of said license along with the source code.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *****************************************************************************/
+/** @noRector */
+require_once("libs/composer/vendor/autoload.php");
 ilContext::init(ilContext::CONTEXT_SHIBBOLETH);
-
-require_once("Services/Init/classes/class.ilInitialisation.php");
 ilInitialisation::initILIAS();
+global $DIC;
 
-if (!$_SERVER['HTTP_SHIB_APPLICATION_ID'] && !$_SERVER['Shib-Application-ID'] && !$_SERVER['REDIRECT_Shib_Application_ID']) {
-    $message = "This file must be protected by Shibboleth, otherwise you cannot use Shibboleth authentication! Consult the <a href=\"Services/AuthShibboleth/README.SHIBBOLETH.txt\">documentation</a> on how to configure Shibboleth authentication properly.";
-    $ilias->raiseError($message, $ilias->error_obj->WARNING);
+$server = $DIC->http()->request()->getQueryParams();
+
+if (
+    !isset($server['HTTP_SHIB_APPLICATION_ID'])
+    && !isset($server['Shib-Application-ID'])
+    && !isset($server['REDIRECT_Shib_Application_ID'])
+) {
+    $factory = $DIC->ui()->factory();
+    $message_box = $factory->messageBox()->failure("The file shib_login.php must be protected by Shibboleth, otherwise you cannot use Shibboleth authentication.")->withButtons([
+        $factory->button()->standard('Open Documentation', './Services/AuthShibboleth/README.md')
+    ]);
+
+    $DIC->ui()->mainTemplate()->setContent($DIC->ui()->renderer()->render($message_box));
+    $DIC->ui()->mainTemplate()->printToStdout();
+} else {
+    // authentication is done here ->
+    $DIC->ctrl()->callBaseClass(ilStartUpGUI::class);
+    $DIC->ctrl()->setCmd('doShibbolethAuthentication');
 }
 
-// authentication is done here ->
-$ilCtrl->setCmd('doShibbolethAuthentication');
-$ilCtrl->callBaseClass('ilStartUpGUI');

commit 7f36b162e4aa5e8150babeed0220d445c18cc309

Add missing initialisation for shib_logout.php as with shib_login.php
Seems like this broke with 9d54dbd408ad2d3e8d445807bb7f972929898cf3

shib_logout.php
@@ -14,7 +14,10 @@
  *****************************************************************************/
 /** @noRector */
 require_once("libs/composer/vendor/autoload.php");
+ilContext::init(ilContext::CONTEXT_SHIBBOLETH);
+ilInitialisation::initILIAS();
 global $DIC;
+
 $q = $DIC->http()->wrapper()->query();
 if (
     $q->has('return')