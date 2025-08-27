2d5507212f62e4dbcadd918353144942ada6275f
Lukas Eichenauer <leichenauer@databay.de>
2025-08-27 09:21:31 +0200

    feat: introduce openidconnect strategy

---
+++ components/ILIAS/WebServices/ECS/classes/Auth/class.ilOIDCAuthStrategy.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ */
+
+declare(strict_types=1);
+
+class ilOIDCAuthStrategy extends ilECSAuthStrategy
+{
+    public function __construct(
+        private readonly ilCtrlInterface $ctrl,
+        private readonly ilLogger $logger
+    ) {
+    }
+    public function isActive(): bool
+    {
+        return ilOpenIdConnectSettings::getInstance()->getActive();
+    }
+
+    public function getName(): string
+    {
+        return 'oidc';
+    }
+    public function handleLogin(string $redirection_target): void
+    {
+        $this->logger->info('Redirect to oidc authentication');
+        $this->ctrl->redirectToURL('openidconnect.php?target=' . $redirection_target);
+    }
+}
