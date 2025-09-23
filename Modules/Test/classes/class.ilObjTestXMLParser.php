ed91d8de4852a65f4c1de90e2f457338d233321b
Fabian Helfer <fhelfer@databay.de>
2025-09-23 13:00:13 +0000

    T&A: Fix taxFilter open to RCE via Import
    Signed-off-by: Releasemanager <webmaster@ilias.de>

--- Modules/Test/classes/class.ilObjTestXMLParser.php
+++ Modules/Test/classes/class.ilObjTestXMLParser.php
@@ -289,7 +289,7 @@ class ilObjTestXMLParser extends ilSaxParser
             );
             $sourcePoolDefinition->setMappedTaxonomyFilter($mappedTaxFilter);
         } elseif (isset($attr['taxFilter']) && strlen($attr['taxFilter']) > 0) {
-            $mappedTaxFilter = unserialize($attr['taxFilter']);
+            $mappedTaxFilter = unserialize($attr['taxFilter'], ['allowed_classes' => false]);
             $sourcePoolDefinition->setMappedTaxonomyFilter($mappedTaxFilter);
         }
     }
