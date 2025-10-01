56e753481e0d56614176c2dff146484f38fe988d
Nils Haagen <nhaagen@cat06.de>
2025-10-01 15:48:01 +0200

    10/Setup: 45564, CalledFromRootObjective

--- components/ILIAS/Setup/src/CLI/App.php
+++ components/ILIAS/Setup/src/CLI/App.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -15,8 +17,6 @@
  * https://github.com/ILIAS-eLearning
  *
  *********************************************************************/
-
-declare(strict_types=1);
 
 namespace ILIAS\Setup\CLI;
 
@@ -50,18 +50,6 @@ class App extends Application implements EntryPoint
 
     public function enter(): int
     {
-        $this->verifyIliasRoot();
         return $this->run();
-    }
-
-    private function verifyIliasRoot(): void
-    {
-        $ilroot = realpath(__DIR__ . '/../../../../../');
-        if (getcwd() !== $ilroot) {
-            $msg = "Please run the setup from ILIAS root - "
-                . "there are components using relative pathes.\n";
-            print $msg;
-            exit(1);
-        }
     }
 }
