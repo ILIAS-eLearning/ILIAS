fbd44925e7b9022686813683161737dca17fc3c1
Alexander Killing <killing@leifos.de>
2025-10-09 22:56:53 +0200

    jstree dependency

--- components/ILIAS/UIComponent/UIComponent.php
+++ components/ILIAS/UIComponent/UIComponent.php
@@ -45,7 +45,7 @@ class UIComponent implements Component\Component
         $contribute[Component\Resource\PublicAsset::class] = fn() =>
             new Component\Resource\ComponentJS($this, "ilTextHighlighter.js");
 
-        /* $contribute[Component\Resource\PublicAsset::class] = static fn() => new class () implements Component\Resource\PublicAsset {
+        $contribute[Component\Resource\PublicAsset::class] = static fn() => new class () implements Component\Resource\PublicAsset {
             public function getSource(): string
             {
                 return "node_modules/jstree";
@@ -54,8 +54,7 @@ class UIComponent implements Component\Component
             {
                 return "node_modules/jstree";
             }
-        }; */
-
+        };
 
         /* This library was missing after discussing dependencies for ILIAS 10
         $contribute[Component\Resource\PublicAsset::class] = static fn() =>
