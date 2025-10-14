0f796f64761d802b1863bb561d32054b5ea28283
Marvin Beym <mBeym@databay.de>
2025-10-14 14:21:46 +0200

    Use new translation for new mail form

--- components/ILIAS/Mail/classes/class.ilMailFormGUI.php
+++ components/ILIAS/Mail/classes/class.ilMailFormGUI.php
@@ -299,7 +299,7 @@ class ilMailFormGUI
 
     public function searchUsers(bool $save = true): void
     {
-        $this->tpl->setTitle($this->lng->txt('mail'));
+        $this->tpl->setTitle($this->lng->txt('mail_new'));
 
         if ($save) {
             $files = $this->getBodyParam(
@@ -504,7 +504,7 @@ class ilMailFormGUI
             'tpl.mail_new.html',
             'components/ILIAS/Mail'
         );
-        $this->tpl->setTitle($this->lng->txt('mail'));
+        $this->tpl->setTitle($this->lng->txt('mail_new'));
 
         $this->lng->loadLanguageModule('crs');
 
