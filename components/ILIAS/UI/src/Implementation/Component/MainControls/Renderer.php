cf81c81723172c06e1526a17d52b2317a1dc8e50
Daniel Cazalla (ZallaxDev) <86362063+ZallaxDev@users.noreply.github.com>
2025-10-15 13:10:57 +0200

    Fix: replace 'Show More' glyph label with 'Menu' in mobile Metabar
    The "Show More" glyph button in the mobile view of the Metabar has been replaced
    with a descriptive text label "Menu".
    This change improves accessibility and clarity for users, following the recommendations in ticket #41471.

--- components/ILIAS/UI/src/Implementation/Component/MainControls/Renderer.php
+++ components/ILIAS/UI/src/Implementation/Component/MainControls/Renderer.php
@@ -247,7 +247,7 @@ class Renderer extends AbstractComponentRenderer
         ];
         $entries = $component->getEntries();
 
-        $more_label = $this->txt('show_more');
+        $more_label = $this->txt('menu_label');
         $more_symbol = $f->symbol()->glyph()->disclosure()
             ->withCounter($f->counter()->novelty(0))
             ->withCounter($f->counter()->status(0));
