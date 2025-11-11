c5a91c373b126ad186baf5d1f8e297c95d13ec5e
Fabian Helfer <fhelfer@databay.de>
2025-11-11 16:03:42 +0100

    Legal Docs: Review remove main_template

--- components/ILIAS/LegalDocuments/classes/Administration.php
+++ components/ILIAS/LegalDocuments/classes/Administration.php
@@ -50,7 +50,6 @@ use ILIAS\LegalDocuments\Value\DocumentContent;
 use ILIAS\UI\Component\Input\Container\Form\Form;
 use ILIAS\User\Settings\Administration\SettingsGUI;
 use ilAdministrationGUI;
-use ilGlobalTemplateInterface;
 
 class Administration
 {
@@ -58,7 +57,6 @@ class Administration
     private readonly Closure $confirmation;
     private WrapperFactory $http_wrapper;
     private Factory $refinery;
-    private ilGlobalTemplateInterface $main_template;
 
     /**
      * @param null|Closure(): Confirmation $confirmation
@@ -74,7 +72,6 @@ class Administration
         $this->confirmation = $confirmation ?? fn() => new Confirmation($this->container->language());
         $this->http_wrapper = $http_wrapper ?? $this->container->http()->wrapper();
         $this->refinery = $refinery ?? $this->container->refinery();
-        $this->main_template = $this->ui->mainTemplate();
     }
 
     /**
@@ -328,8 +325,10 @@ class Administration
             $group = $group->withValue($value);
         }
 
-        $this->main_template->setOnScreenMessage($this->main_template::MESSAGE_TYPE_INFO, $this->ui->txt('form_criterion_standard_fields_info_text'));
-        ;
+        $this->ui->mainTemplate()->setOnScreenMessage(
+            $this->ui->mainTemplate()::MESSAGE_TYPE_INFO,
+            $this->ui->txt('form_criterion_standard_fields_info_text')
+        );
 
         $title = $this->ui->create()->input()->field()->text($this->ui->txt('form_document'))->withValue($document->content()->title())->withDisabled(true);
 
