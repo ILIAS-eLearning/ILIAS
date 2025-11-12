d370ff9cebb63435f5c11a438fcf4b36e130c3f2
qualitus-dahme <dahme@qualitus.de>
2025-11-12 15:40:25 +0100

    0046165: SCORM: Trying to access certificate template configuration crashes for SCORM repository objects (#10439)
    * SCORM Debug Tool without YUI2
    
    * check if array key exists
    
    * Scorm: Change sequence of parameters when transforming datetime (cherry-picked from release_9: 1111a6eb9e3)
    
    * Possible type error fixed (https://mantis.ilias.de/view.php?id=45306)
    
    * Type error fixed (https://mantis.ilias.de/view.php?id=45429)
    
    * Revert "Scorm: Change sequence of parameters when transforming datetime (#9825)"
    
    This reverts commit 2be2ba5809ee1b49715ec61a8ebb6ba2ffdb9de4.
    
    * Mantis #45149 - Fix Scorm1.2 favicons again for Ilias9
    
    * Scorm: Change sequence of parameters when transforming datetime
    
    * Type fixes (#40842)
    
    * Fix 0046165: SCORM: Trying to access certificate template configuration crashes for SCORM repository objects

--- components/ILIAS/Scorm2004/classes/class.ilObjSCORM2004LearningModuleGUI.php
+++ components/ILIAS/Scorm2004/classes/class.ilObjSCORM2004LearningModuleGUI.php
@@ -27,11 +27,11 @@ use ILIAS\GlobalScreen\ScreenContext\ContextServices;
  * @author Uwe Kohnle <kohnle@internetlehrer-gmbh.de>
  *
  * @ilCtrl_Calls ilObjSCORM2004LearningModuleGUI: ilFileSystemGUI, ilObjectMetaDataGUI, ilPermissionGUI, ilLearningProgressGUI
- * @ilCtrl_Calls ilObjSCORM2004LearningModuleGUI: ilInfoScreenGUI, ilSCORM2004ChapterGUI, ilSCORM2004SeqChapterGUI, ilSCORM2004PageNodeGUI, ilSCORM2004ScoGUI
- * @ilCtrl_Calls ilObjSCORM2004LearningModuleGUI: ilCertificateGUI, ilObjStyleSheetGUI, ilNoteGUI, ilSCORM2004AssetGUI
+ * @ilCtrl_Calls ilObjSCORM2004LearningModuleGUI: ilInfoScreenGUI
+ * @ilCtrl_Calls ilObjSCORM2004LearningModuleGUI: ilObjStyleSheetGUI, ilNoteGUI
  * @ilCtrl_Calls ilObjSCORM2004LearningModuleGUI: ilCommonActionDispatcherGUI
  * @ilCtrl_Calls ilObjSCORM2004LearningModuleGUI: ilSCORM2004TrackingItemsPerScoFilterGUI, ilSCORM2004TrackingItemsPerUserFilterGUI, ilSCORM2004TrackingItemsTableGUI
- * @ilCtrl_Calls ilObjSCORM2004LearningModuleGUI: ilLTIProviderObjectSettingGUI
+ * @ilCtrl_Calls ilObjSCORM2004LearningModuleGUI: ilLTIProviderObjectSettingGUI, ilCertificateGUI
  */
 class ilObjSCORM2004LearningModuleGUI extends ilObjSCORMLearningModuleGUI
 {
