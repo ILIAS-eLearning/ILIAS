26ae2a962ce6b5ce8cecb01fafcdf1636987e72e
ingoj <120396930+ingoj@users.noreply.github.com>
2025-11-17 17:15:40 +0100

    avoid permission errors
    this fix checks if the current user has proper permissions for roles and context. Links are only rendered if permissions for the container for a local role are granted
    The path display has the same problem about permissions, but a fix for this is needed in Services/Tree (will be suggested soon)

--- Services/User/classes/class.ilRoleAssignmentTableGUI.php
+++ Services/User/classes/class.ilRoleAssignmentTableGUI.php
@@ -107,6 +107,8 @@ class ilRoleAssignmentTableGUI extends ilTable2GUI
 
     protected function fillRow(array $a_set): void // Missing array type.
     {
+        global $DIC;
+        
         if ($this->edit_access) {
             if (isset($a_set['checkbox']['id'])) {
                 $this->tpl->setVariable('VAL_ID', $a_set['checkbox']['id']);
@@ -122,28 +124,39 @@ class ilRoleAssignmentTableGUI extends ilTable2GUI
         $this->ctrl->setParameterByClass("ilobjrolegui", "ref_id", $a_set['ref_id']);
         $this->ctrl->setParameterByClass("ilobjrolegui", "obj_id", $a_set["obj_id"]);
 
-        $this->tpl->setVariable(
-            'ROLE',
-            $this->renderer->render(
-                $this->factory->link()->standard(
-                    ilObjRole::_getTranslation($a_set['title']),
-                    $this->ctrl->getLinkTargetByClass(ilObjRoleGUI::class, 'perm')
+        if ($DIC['ilAccess']->checkAccess('edit_permission','',(int) $a_set['ref_id'])) { 
+            $this->tpl->setVariable(
+                'ROLE',
+                $this->renderer->render(
+                    $this->factory->link()->standard(
+                        ilObjRole::_getTranslation($a_set['title']),
+                        $this->ctrl->getLinkTargetByClass(ilObjRoleGUI::class, 'perm')
+                    )
                 )
-            )
-        );
+            );
+        } else {
+            $this->tpl->setVariable(
+                'ROLE',
+                ilObjRole::_getTranslation($a_set['title'])
+            );
+        }
+            
         $this->tpl->setVariable('DESCRIPTION', $a_set['description']);
         // Add link to objector local Rores
         $context = $a_set['context'];
         if ($a_set['role_type'] === 'local') {
-            $context = $this->renderer->render(
-                $this->factory->link()->standard(
-                    $context,
-                    ilLink::_getLink(
-                        $a_set['ref_id'],
-                        ilObject::_lookupType(ilObject::_lookupObjId($a_set['ref_id']))
+            if ($DIC['ilAccess']->checkAccess('visible','',(int) $a_set['ref_id']) || 
+                $DIC['ilAccess']->checkAccess('read','',(int) $a_set['ref_id'])) {
+                $context = $this->renderer->render(
+                    $this->factory->link()->standard(
+                        $context,
+                        ilLink::_getLink(
+                            $a_set['ref_id'],
+                            ilObject::_lookupType(ilObject::_lookupObjId($a_set['ref_id']))
+                        )
                     )
-                )
-            );
+                );
+            }
         }
         $this->tpl->setVariable('CONTEXT', $context);
         $this->tpl->setVariable('PATH', $a_set['path']);
