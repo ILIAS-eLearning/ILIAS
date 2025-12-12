8de390ad368f636a68918148646c422cfd1100fb
sagun <sagun.karki@minervis.com>
2025-12-12 10:33:21 +0100

    Apply offline flag when creating forum via addObject

--- components/ILIAS/soap/classes/class.ilSoapObjectAdministration.php
+++ components/ILIAS/soap/classes/class.ilSoapObjectAdministration.php
@@ -606,12 +606,13 @@ class ilSoapObjectAdministration extends ilSoapAdministration
                 $newObj->setImportId($object_data['import_id']);
             }
 
-            if ($objDefinition->supportsOfflineHandling($newObj->getType())) {
-                $newObj->setOfflineStatus((bool) $object_data['offline']);
-            }
             $newObj->setTitle($object_data['title']);
             $newObj->setDescription($object_data['description']);
             $newObj->create(); // true for upload
+            if ($objDefinition->supportsOfflineHandling($newObj->getType()) && isset($object_data['offline'])) {
+                $newObj->setOfflineStatus((bool) $object_data['offline']);
+                $newObj->update();
+            }
             $newObj->createReference();
             $newObj->putInTree($a_target_id);
             $newObj->setPermissions($a_target_id);
