b41329bc9aceab0193b7f932a1181c09d98d565d
Matheus Zych <mzych@databay.de>
2025-12-15 08:02:42 +0100

    Adds logging for when sourcepath of imagemap question files does not exist or is not readable

--- components/ILIAS/TestQuestionPool/classes/class.assImagemapQuestion.php
+++ components/ILIAS/TestQuestionPool/classes/class.assImagemapQuestion.php
@@ -206,6 +206,17 @@ class assImagemapQuestion extends assQuestion implements ilObjQuestionScoringAdj
         }
 
         if (!file_exists($image_source_path) || !is_readable($image_source_path)) {
+            $this->log->root()->alert(
+                "Could not copy imagemap question files: Source path '{$image_source_path}' does not exist or is not readable.",
+                [
+                    'source_question_id' => $source_question_id,
+                    'source_parent_id' => $source_parent_id,
+                    'target_question_id' => $target_question_id,
+                    'target_parent_id' => $target_parent_id,
+                    'image_source_path' => $image_source_path,
+                    'image_target_path' => $image_target_path
+                ]
+            );
             return;
         }
 
