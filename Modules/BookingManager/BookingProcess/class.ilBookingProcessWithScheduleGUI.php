680b8e8f24d324968ee922b9bb9bb77f960dd7a1
Matheus Zych <mzych@databay.de>
2025-12-16 08:39:40 +0100

    Improves redirect after booking of an item and adds missing language variables in booking pool

--- Modules/BookingManager/BookingProcess/class.ilBookingProcessWithScheduleGUI.php
+++ Modules/BookingManager/BookingProcess/class.ilBookingProcessWithScheduleGUI.php
@@ -50,6 +50,7 @@ class ilBookingProcessWithScheduleGUI implements \ILIAS\BookingManager\BookingPr
     protected ilObjUser $user;
     protected int $book_obj_id;
     protected array $rsv_ids = [];
+    protected ?string $origin_cmd = null;
 
     public function __construct(
         ilObjBookingPool $pool,
@@ -288,7 +289,8 @@ class ilBookingProcessWithScheduleGUI implements \ILIAS\BookingManager\BookingPr
             ->number("nr", $period, "", 1, 1, $counter)
             ->radio("recurrence", $this->lng->txt("book_recurrence"), "", "0")
             ->radioOption("0", $this->lng->txt("book_no_recurrence"))
-            ->radioOption("1", $this->lng->txt("book_book_recurrence"));
+            ->radioOption("1", $this->lng->txt("book_book_recurrence"))
+            ->hidden("origin_cmd", $this->book_request->getOriginCmd());
         if ($this->pool->usesMessages()) {
             $form = $form->textarea(
                 "message",
@@ -322,6 +324,11 @@ class ilBookingProcessWithScheduleGUI implements \ILIAS\BookingManager\BookingPr
             $this->gui->modal($this->getBookgingObjectTitle())
                       ->form($form)
                       ->send();
+        }
+
+        $origin_cmd = $form->getData("origin_cmd");
+        if (is_string($origin_cmd) && $origin_cmd !== "") {
+            $this->origin_cmd = $origin_cmd;
         }
 
         $message = $this->pool->usesMessages()
@@ -515,6 +522,9 @@ class ilBookingProcessWithScheduleGUI implements \ILIAS\BookingManager\BookingPr
         $this->ctrl->setParameter($this, "recurrence", $recurrence);
         $this->ctrl->setParameter($this, "nr", $nr);
         $this->ctrl->setParameter($this, "until", $until);
+        if (is_string($this->origin_cmd) && $this->origin_cmd !== "") {
+            $this->ctrl->setParameter($this, "origin_cmd", $this->origin_cmd);
+        }
         return $this->ctrl->getLinkTarget($this, "bookAvailableItems");
     }
 
