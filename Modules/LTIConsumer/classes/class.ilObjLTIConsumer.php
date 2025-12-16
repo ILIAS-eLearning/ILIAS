61032e08a6ef1e17dd25d9658132e45e7366e575
Sergio Santiago <sergiosantiago0403@gmail.com>
2025-12-16 14:43:03 +0100

    fix: cs-fixer

--- Modules/LTIConsumer/classes/class.ilObjLTIConsumer.php
+++ Modules/LTIConsumer/classes/class.ilObjLTIConsumer.php
@@ -661,7 +661,7 @@ class ilObjLTIConsumer extends ilObject2
     {
         global $DIC; /* @var \ILIAS\DI\Container $DIC */
         $DIC->user()->setExternalAccount($cmixUser->getUsrIdent());
-        
+
         $roles = $DIC->access()->checkAccess('write', '', $this->getRefId()) ? "Instructor" : "Learner";
         //todo if object is in course or group, roles would have to be taken from there s. Mantis 35435 - if necessary Jour Fixe topic
         if ($DIC->rbac()->review()->isAssigned($DIC->user()->getId(), SYSTEM_ROLE_ID)) {
