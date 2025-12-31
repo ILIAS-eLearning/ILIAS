edc202ff433e492331d37c7275fb55a3c42045a3
Fabian Helfer <fhelfer@databay.de>
2025-12-31 16:44:29 +0100

    [Fix] 46579 UI: Sortation ViewControl does not send values

--- components/ILIAS/UI/src/Implementation/Component/Input/ViewControl/Renderer.php
+++ components/ILIAS/UI/src/Implementation/Component/Input/ViewControl/Renderer.php
@@ -161,7 +161,7 @@ class Renderer extends AbstractComponentRenderer
                              container = event.target.closest('.il-viewcontrol-sortation');
                         }
                         let inputs = container.querySelectorAll('.il-viewcontrol-value > input');
-                        let val = signal_data.options.value.split(':');
+                        let val = signal_data.options.value
                         inputs[0].value = val[0];
                         inputs[1].value = val[1];
                         $(event.target).trigger('{$container_submit_signal}');
