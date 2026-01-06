e100c6692f3a44b7967ab137d0271147e3e9157c
Fabian Helfer <fhelfer@databay.de>
2026-01-06 14:17:59 +0100

    [Fix] OrderingTable: UnitTest testOrderingTableRenderTableHeaderWithoutActionsWithoutData

--- components/ILIAS/UI/tests/Component/Table/OrderingRendererTest.php
+++ components/ILIAS/UI/tests/Component/Table/OrderingRendererTest.php
@@ -89,7 +89,7 @@ class OrderingRendererTest extends TableRendererTestBase
             </tr>
             </thead>
             <tbody class="c-table-data__body">
-                <tr class="c-table-data__row even">
+                <tr class="c-table-data__row even" draggable="true">
                     <td class="c-table-data__cell c-table-data__cell--full-width " colspan="3" tabindex="-1"><span class="c-table-data__cell__col-title">:</span>ui_table_no_records</td>
                 </tr>
             </tbody>
