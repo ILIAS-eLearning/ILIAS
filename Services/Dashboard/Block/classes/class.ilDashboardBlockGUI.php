69913e2ceab59ba42c2c75f7dc0fd4e75308ed3c
iszmais <iszmais@databay.de>
2026-01-07 15:16:34 +0100

    fix string comparison on dash

--- Services/Dashboard/Block/classes/class.ilDashboardBlockGUI.php
+++ Services/Dashboard/Block/classes/class.ilDashboardBlockGUI.php
@@ -334,7 +334,6 @@ abstract class ilDashboardBlockGUI extends ilBlockGUI implements ilDesktopItemHa
             }
             $grouped_items[$title][] = $item;
         }
-        ksort($grouped_items);
         $grouped_items = array_map($this->sortByTitle(...), $grouped_items);
         return $grouped_items;
     }
@@ -601,7 +600,7 @@ abstract class ilDashboardBlockGUI extends ilBlockGUI implements ilDesktopItemHa
     {
         usort(
             $data,
-            static fn(BlockDTO $left, BlockDTO $right): int => strcmp($left->getTitle(), $right->getTitle())
+            static fn(BlockDTO $left, BlockDTO $right): int => strcasecmp($left->getTitle(), $right->getTitle())
         );
         return $data;
     }
