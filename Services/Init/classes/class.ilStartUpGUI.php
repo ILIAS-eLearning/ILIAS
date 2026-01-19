d57d730abc1b68a033018f135fba70a5b3839dcd
mjansen <mjansen@databay.de>
2026-01-19 11:10:07 +0100

    Init: Revert fix for Mantis 32755
    See:
    - https://mantis.ilias.de/view.php?id=46782
    - https://mantis.ilias.de/view.php?id=32755
    - https://github.com/ILIAS-eLearning/ILIAS/pull/9323/files

--- Services/Init/classes/class.ilStartUpGUI.php
+++ Services/Init/classes/class.ilStartUpGUI.php
@@ -823,7 +823,10 @@ class ilStartUpGUI implements ilCtrlBaseClassInterface, ilCtrlSecurityInterface
     ): string {
         global $tpl;
 
-        $shib_is_default_without_local_login = (int) $this->setting->get('auth_mode') === ilAuthUtils::AUTH_SHIBBOLETH;
+        $shib_is_default_without_local_login = (
+            (int) $this->setting->get('auth_mode') === ilAuthUtils::AUTH_SHIBBOLETH &&
+            !$this->setting->get('shib_auth_allow_local', '0')
+        );
         $cas_is_default = (int) $this->setting->get('auth_mode') === ilAuthUtils::AUTH_CAS;
 
         if ($cas_is_default || $shib_is_default_without_local_login) {
