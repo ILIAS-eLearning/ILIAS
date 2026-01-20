53361613f1d19654555047175533615f3ef3983b
Matheus Zych <mzych@databay.de>
2026-01-20 13:33:46 +0100

    BookingPool: Remove reservations on pool deletion
    See: https://mantis.ilias.de/view.php?id=21705
    
    Deleting or trashing a booking pool left reservations and calendar entries behind. The Booking Manager module now listens for ILIAS object `delete` and `toTrash` events; `ObjectEvent::handleDeletion` resolves each affected pool and calls `ilBookingObject::deleteReservationsAndCalEntries` for every booking object.

--- components/ILIAS/BookingManager/Service/class.InternalDomainService.php
+++ components/ILIAS/BookingManager/Service/class.InternalDomainService.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -18,8 +16,11 @@ declare(strict_types=1);
  *
  *********************************************************************/
 
+declare(strict_types=1);
+
 namespace ILIAS\BookingManager;
 
+use ILIAS\BookingManager\Objects\ObjectEvent;
 use ILIAS\DI\Container;
 use ILIAS\Repository\GlobalDICDomainServices;
 use ILIAS\BookingManager\BookingProcess\BookingProcessManager;
@@ -126,6 +127,11 @@ class InternalDomainService
     public function userEvent(): UserEvent
     {
         return self::$instances["user_event"] ??= new UserEvent($this);
+    }
+
+    public function objectEvent(): ObjectEvent
+    {
+        return new ObjectEvent();
     }
 
     public function bookingSettings(): SettingsManager
