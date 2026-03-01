5e65b5230f63e399ed3a567e9d10b9c07f6d10c7
Sagun Karki <sagun.karki@minervis.com>
2026-03-01 13:24:50 +0100

    Fix SOAP authentication: reinitialize user in SOAP context

--- components/ILIAS/soap/classes/class.ilSoapAdministration.php
+++ components/ILIAS/soap/classes/class.ilSoapAdministration.php
@@ -147,7 +147,7 @@ class ilSoapAdministration
 
         session_id($sid);
 
-        if (ilContext::getType() !== ilContext::CONTEXT_SOAP) {
+        if (ilContext::getType() === ilContext::CONTEXT_SOAP) {
             ilInitialisation::reInitUser();
             ilUtil::setCookie(session_name(), $sid);
         }
