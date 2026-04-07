48cfc8289e705e9cd15c4204202b34b9ce595f91
Fabian Helfer <fhelfer@databay.de>
2026-04-07 11:12:01 +0200

    [Fix] UI: Deprecated Glyph Action fix ilLikeGUI highlighting on buttons

--- components/ILIAS/Like/resources/Like.js
+++ components/ILIAS/Like/resources/Like.js
@@ -3,12 +3,14 @@ il.Like = il.Like || {};
 (function ($, il) {
   il.Like = (function ($) {
     const toggle = function (url, glyph_id, widget_id, exp_id) {
+      const glyphEl = $(`#${glyph_id} .glyph`).first();
+      const highlightTarget = glyphEl.length ? glyphEl : $(`#${glyph_id}`);
       let val;
-      if ($(`#${glyph_id}`).hasClass('highlighted')) {
-        $(`#${glyph_id}`).removeClass('highlighted');
+      if (highlightTarget.hasClass('highlighted')) {
+        highlightTarget.removeClass('highlighted');
         val = 0;
       } else {
-        $(`#${glyph_id}`).addClass('highlighted');
+        highlightTarget.addClass('highlighted');
         val = 1;
       }
       // il.Util.ajaxReplace(url + "&cmd=saveExpression&exp=" + exp_id + "&val=" + val + "&dom_id=" + widget_id, widget_id + "_ec");
