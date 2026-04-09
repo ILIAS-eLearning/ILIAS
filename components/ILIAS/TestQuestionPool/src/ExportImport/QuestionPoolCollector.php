4e30463283c412b4c323f696d3fad76e840b5acb
Lukas Eichenauer <leichenauer@databay.de>
2026-04-09 12:45:45 +0200

    feat(qpl): implement question pool export and integrate core export components

---
+++ components/ILIAS/TestQuestionPool/src/ExportImport/QuestionPoolCollector.php
@@ -0,0 +1,226 @@
+<?php
+
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
+declare(strict_types=1);
+
+namespace ILIAS\TestQuestionPool\ExportImport;
+
+use assQuestion;
+use Generator;
+use ilAssQuestionSkillAssignmentList;
+use ilDBInterface;
+use ilObjQuestionPool;
+use ilAssClozeTestFeedback;
+use ilAssMultiOptionQuestionFeedback;
+use ilAssSpecificFeedbackIdentifierList;
+use ILIAS\Data\ObjectId;
+use ILIAS\TestQuestionPool\ExportImport\Foundation\Contracts\DataCollector;
+use ILIAS\TestQuestionPool\ExportImport\Foundation\Normalizing\Envelopes\Id;
+use ILIAS\TestQuestionPool\ExportImport\Envelopes\Feedback;
+use ILIAS\TestQuestionPool\Questions\GeneralQuestionProperties;
+use ILIAS\TestQuestionPool\Questions\GeneralQuestionPropertiesRepository;
+use ilUnitConfigurationRepository;
+
+/**
+ * Collector to aggregate data from the question pool for export.
+ */
+class QuestionPoolCollector implements DataCollector
+{
+    /** @var array<int, GeneralQuestionProperties> $questions */
+    private ?array $questions = null;
+    private ?ilObjQuestionPool $pool_object = null;
+    private ?ilAssQuestionSkillAssignmentList $skill_assignments = null;
+
+    public function __construct(
+        private readonly GeneralQuestionPropertiesRepository $question_repository,
+        private readonly ilDBInterface $db,
+        private readonly ObjectId $pool_id
+    ) {
+    }
+
+    /**
+     * Get the ID of the question pool.
+     *
+     * @return ObjectId
+     */
+    public function getPoolId(): ObjectId
+    {
+        return $this->pool_id;
+    }
+
+    /**
+     * Get the object of the question pool. It will be loaded from the database if not already loaded.
+     */
+    public function getObject(): ilObjQuestionPool
+    {
+        if ($this->pool_object === null) {
+            $this->pool_object = new ilObjQuestionPool($this->pool_id->toInt(), false);
+            $this->pool_object->read();
+        }
+
+        return $this->pool_object;
+    }
+
+    /**
+     * Collect the question properties for all questions in the question pool.
+     *
+     * @return array<int, GeneralQuestionProperties>
+     */
+    public function getQuestionProperties(): array
+    {
+        if ($this->questions === null) {
+            $this->questions = $this->question_repository->getForParentObjectId($this->pool_id->toInt());
+        }
+        return $this->questions;
+    }
+
+    /**
+     * Collect the question objects for all questions in the question pool.
+     *
+     * @return Generator<assQuestion>
+     */
+    public function getQuestionObjects(): Generator
+    {
+        foreach ($this->getQuestionProperties() as $question) {
+            yield assQuestion::instantiateQuestion($question->getQuestionId());
+        }
+    }
+
+    /*
+        Units
+    */
+
+    /**
+     * Collect the unit categories and their units for all formula questions in the question pool.
+     *
+     * @return Generator<\assFormulaQuestionUnit|\assFormulaQuestionUnitCategory>
+     */
+    public function getUnits(): Generator
+    {
+        foreach ($this->getQuestionProperties() as $question) {
+            if ($question->getClassName() === 'assFormulaQuestion') {
+                $repository = new ilUnitConfigurationRepository($question->getQuestionId());
+                yield from $repository->getCategorizedUnits();
+            }
+        }
+    }
+
+    /*
+        Feedback
+    */
+
+    /**
+     * Collect the feedback content for a question and return it as a Feedback transfer object.
+     */
+    public function getFeedback(assQuestion $question): Feedback
+    {
+        $feedback = new Feedback(
+            new Id($question->getId(), 'question'),
+            $question->feedbackOBJ->getGenericFeedbackExportPresentation($question->getId(), false),
+            $question->feedbackOBJ->getGenericFeedbackExportPresentation($question->getId(), true),
+            $this->loadSpecificFeedback($question),
+        );
+
+        return $feedback;
+    }
+
+    private function loadSpecificFeedback(assQuestion $question): array
+    {
+        // Skip if specific feedback is not available or supported by the question type.
+        if (
+            !$question->feedbackOBJ instanceof ilAssMultiOptionQuestionFeedback ||
+            !$question->feedbackOBJ->isSpecificAnswerFeedbackAvailable($question->getId())
+        ) {
+            return [];
+        }
+
+        // Cloze question type specific feedback uses the identifier list to load the answer-specific feedback.
+        if ($question->feedbackOBJ instanceof ilAssClozeTestFeedback) {
+            $feedback_list = new ilAssSpecificFeedbackIdentifierList();
+            $feedback_list->load($question->getId());
+
+            $feedback = [];
+            foreach ($feedback_list as $identifier) {
+                $feedback[$identifier->getAnswerIndex()] = [
+                    'answer_index' => $identifier->getAnswerIndex(),
+                    'question_index' => $identifier->getQuestionIndex(),
+                    'feedback' => $question->feedbackOBJ->getSpecificAnswerFeedbackExportPresentation(
+                        $question->getId(),
+                        $identifier->getQuestionIndex(),
+                        $identifier->getAnswerIndex()
+                    ),
+                ];
+            }
+            return $feedback;
+        }
+
+        // Other question types with multiple answer options share the same approach
+        foreach (array_keys($question->feedbackOBJ->getAnswerOptionsByAnswerIndex()) as $answer_index) {
+            $feedback[$answer_index] = [
+                'answer_index' => $answer_index,
+                'question_index' => 0,
+                'feedback' => $question->feedbackOBJ->getSpecificAnswerFeedbackExportPresentation(
+                    $question->getId(),
+                    0,
+                    $answer_index
+                ),
+            ];
+        }
+        return $feedback;
+    }
+
+    /*
+        Skill Assignments
+    */
+
+    /**
+     * @return array<\ilAssQuestionSkillAssignment>
+     */
+    public function getSkillAssignments(): array
+    {
+        if ($this->skill_assignments === null) {
+            $this->skill_assignments = new ilAssQuestionSkillAssignmentList($this->db);
+            $this->skill_assignments->setParentObjId($this->pool_id->toInt());
+            $this->skill_assignments->loadFromDb();
+            $this->skill_assignments->loadAdditionalSkillData();
+        }
+
+        $assignments = [];
+        foreach ($this->getQuestionProperties() as $question) {
+            $assignments = array_merge(
+                $assignments,
+                $this->skill_assignments->getAssignmentsByQuestionId($question->getQuestionId())
+            );
+        }
+        
+        return $assignments;
+    }
+
+    /*
+        CO Page & Media Objects
+    */
+
+    public function getQuestionPageIds(): array
+    {
+        $question_page_ids = [];
+        foreach ($this->getQuestionObjects() as $question) {
+            $question_page_ids[] = "qpl:{$question->getId()}";
+        }
+        return $question_page_ids;
+    }
+}
---
+++ components/ILIAS/TestQuestionPool/src/ExportImport/QuestionPoolExporter.php
@@ -0,0 +1,206 @@
+<?php
+
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
+declare(strict_types=1);
+
+namespace ILIAS\TestQuestionPool\ExportImport;
+
+use assFormulaQuestionUnit;
+use assFormulaQuestionUnitCategory;
+use ilDBInterface;
+use ILIAS\Data\ObjectId;
+use ILIAS\Data\UUID\Factory as UUIDFactory;
+use ILIAS\Export\ExportHandler\I\Consumer\ExportWriter\HandlerInterface as ExportWriter;
+use ILIAS\Export\ExportHandler\I\Consumer\ExportConfig\CollectionInterface as ExportConfig;
+use ILIAS\TestQuestionPool\ExportImport\Foundation\Builder;
+use ILIAS\TestQuestionPool\ExportImport\Foundation\Contracts\Serializer;
+use ILIAS\TestQuestionPool\ExportImport\Foundation\Contracts\Transformations;
+use ILIAS\TestQuestionPool\ExportImport\Foundation\ExportContext;
+use ILIAS\Questions\Units\Repository as UnitsRepository;
+use ILIAS\Taxonomy\DomainService as Taxonomy;
+use ILIAS\TestQuestionPool\ExportImport\Pipes\CollectQuestionImages;
+use ILIAS\TestQuestionPool\Questions\GeneralQuestionPropertiesRepository;
+use SebastianBergmann\CodeCoverage\Report\Xml\Unit;
+
+/**
+ * Orchestrates the export of a question pool. It uses the Builder to create a pipeline of transformations that are used
+ * to normalize the data and then writes the normalized data to the serializer. It also copies the needed files to the
+ * export directory.
+ */
+class QuestionPoolExporter
+{
+    public function __construct(
+        private readonly Builder $builder,
+        private readonly GeneralQuestionPropertiesRepository $question_repository,
+        private readonly ilDBInterface $db,
+        private readonly Taxonomy $taxonomy
+    ) {
+    }
+
+    /**
+     * Performs the export for a given question pool. It returns the export context which contains the serialized data
+     * and the dependencies of the export.
+     */
+    public function export(
+        ObjectId $pool_id,
+        ExportConfig $config,
+        Serializer $serializer,
+        ExportWriter $writer,
+        string $export_dir
+    ): ExportContext {
+        $context = $this->prepare($pool_id, $config);
+        $context = $this->process($context, $serializer);
+        return $this->write($context, $writer, $export_dir);
+    }
+
+    /**
+     * Prepares the export context by creating the transformations and the question image pipe. It returns the export
+     * context which is used to share the context between the prepare, process and write steps.
+     */
+    public function prepare(ObjectId $pool_id, ExportConfig $config): ExportContext
+    {
+        $question_image_pipe = new CollectQuestionImages(
+            new UUIDFactory(),
+            $pool_id
+        );
+
+        $transformations = $this->builder->withAdditionalPipes([$question_image_pipe])
+            ->create();
+
+        return new ExportContext($pool_id, $config, $transformations);
+    }
+
+    /**
+     * Normalizes the question pool object and its questions and writes them to the serializer. It also collects the
+     * dependencies of the export.
+     */
+    public function process(ExportContext $context, Serializer $serializer): ExportContext
+    {
+        $context->setSerializer($serializer);
+        $tt = $context->getTransformations();
+
+        $collector = new QuestionPoolCollector(
+            $this->question_repository,
+            $this->db,
+            $context->getPoolId()
+        );
+
+        $serializer->group(
+            'general',
+            fn() => $this->exportObject($collector, $tt, $serializer, $context)
+        );
+        $serializer->group(
+            'units',
+            fn() => $this->exportUnits($collector, $tt, $serializer)
+        );
+        $serializer->group(
+            'questions',
+            fn() => $this->exportQuestions($collector, $tt, $serializer, $context)
+        );
+        $serializer->group(
+            'skill_assignments',
+            fn() => $this->exportSkillAssignments($collector, $tt, $serializer)
+        );
+
+        return $context;
+    }
+
+    /**
+     * Finalizes the export by copying the question images to the export directory and returning the export context.
+     */
+    public function write(ExportContext $export, ExportWriter $writer, string $export_dir): ExportContext
+    {
+        // Copy the question images to the export directory
+        $question_image_pipe = $export->getTransformations()->context(CollectQuestionImages::class);
+        foreach ($question_image_pipe->getFiles() as $file) {
+            $writer->writeFileByFilePath($file['from'], "{$export_dir}/" . $file['to']);
+        }
+
+        return $export;
+    }
+
+
+    protected function exportObject(
+        QuestionPoolCollector $collector,
+        Transformations $transformations,
+        Serializer $serializer,
+        ExportContext $export
+    ): void {
+        $serializer->append('object', $transformations->normalize($collector->getObject()));
+
+        $obj_id = $collector->getPoolId()->toInt();
+
+        $export->addDependency('components/ILIAS/ILIASObject', 'common', [$obj_id]);
+        $export->addDependency('components/ILIAS/MetaData', 'qpl', ["{$obj_id}:0:qpl"]);
+        $export->addDependency(
+            'components/ILIAS/Taxonomy',
+            'tax',
+            $this->taxonomy->getUsageOfObject($obj_id)
+        );
+    }
+
+    protected function exportUnits(
+        QuestionPoolCollector $collector,
+        Transformations $transformations,
+        Serializer $serializer
+    ): void {
+        $categories = [];
+
+        foreach ($collector->getUnits() as $item) {
+            if($item instanceof assFormulaQuestionUnitCategory) {
+                $categories[$item->getId()] = $transformations->normalize($item);
+            }
+
+            if($item instanceof assFormulaQuestionUnit) {
+                $categories[$item->getCategory()]['units'][] = $transformations->normalize($item);
+            }
+        }
+
+        foreach($categories as $category) {
+            $serializer->append('category', $category);
+        }
+    }
+
+    protected function exportQuestions(
+        QuestionPoolCollector $collector,
+        Transformations $transformations,
+        Serializer $serializer,
+        ExportContext $export
+    ): void {
+        foreach ($collector->getQuestionObjects() as $question) {
+            $serializer->append('question', [
+                ... $transformations->normalize($question),
+                'feedback' => $transformations->normalize(
+                    $collector->getFeedback($question)
+                )
+            ]);
+
+            $export->addDependency('components/ILIAS/COPage', 'pg', ["qpl:{$question->getId()}"]);
+        }
+    }
+
+    protected function exportSkillAssignments(
+        QuestionPoolCollector $collector,
+        Transformations $transformations,
+        Serializer $serializer,
+    ): void {
+        foreach ($collector->getSkillAssignments() as $assignment) {
+            $serializer->append('skill_assignment', $transformations->normalize($assignment));
+        }
+    }
+}
