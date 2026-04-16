b7103091a19dfa32508d1b0c2d3c3346b5b0ee64
Fabian Schmid <fabian@sr.solutions>
2026-04-16 13:46:41 +0200

    Component Revision: Environment — bootstrap wiring + configuration classes
    Introduce bootstrapped configuration services: IliasIni, ClientIdProvider,
    Directories, ServerConfiguration, ClientIni and related repositories. Wire
    all into the component bootstrap so consumers can inject them via $use[].

---
+++ components/ILIAS/Environment/src/Configuration/Instance/WorkingDirectories.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
+declare(strict_types=1);
+
+namespace ILIAS\Environment\Configuration\Instance;
+
+/**
+ * @author Fabian Schmid <fabian@sr.solutions>
+ */
+class WorkingDirectories implements Directories
+{
+    public function __construct(
+        private IliasIni $ilias_ini,
+        private ClientIdProvider $client_id_provider,
+    ) {
+    }
+
+    public function getRoot(): string
+    {
+        return realpath(__DIR__ . '/../../../../../../');
+    }
+
+    public function getPublic(): string
+    {
+        return $this->getRoot() . '/public';
+    }
+
+    public function getDataDir(): string
+    {
+        return $this->ilias_ini->getDataDirectory();
+    }
+
+    public function getWebDir(): string
+    {
+        return $this->getPublic() . '/data/' . $this->client_id_provider->getClientId()->toString();
+    }
+}
