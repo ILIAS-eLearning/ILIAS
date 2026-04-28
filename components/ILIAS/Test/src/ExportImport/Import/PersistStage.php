d427b0a517c6da0cf25836a5f38db81baf42ebbc
Lukas Eichenauer <leichenauer@databay.de>
2026-04-28 11:53:02 +0200

    feat(test): introduce user import resolving

---
+++ components/ILIAS/Test/src/ExportImport/Import/PersistStage.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
+declare(strict_types=1);
+
+namespace ILIAS\Test\ExportImport\Import;
+
+use ILIAS\Language\Language;
+use ILIAS\TestQuestionPool\ExportImport\Foundation\Contracts\ImportStage;
+use ILIAS\TestQuestionPool\ExportImport\Foundation\Importing\ImportContext;
+use ILIAS\TestQuestionPool\ExportImport\Foundation\Importing\ImportSessionRepository;
+use ILIAS\TestQuestionPool\ExportImport\Foundation\Importing\StageResult;
+use ILIAS\TestQuestionPool\ExportImport\Foundation\Serializing\SimpleXMLDeserializer;
+use ILIAS\TestQuestionPool\ExportImport\Import\UploadValidationStage;
+use ilImport;
+
+/**
+ * Final stage of the test import process. Imports the test object and all its dependencies using `ilImport`. It will
+ * delegate the import to the `ilTestImporter` class.
+ */
+class PersistStage implements ImportStage
+{
+    public function __construct(
+        private readonly Language $lng,
+        private readonly int $requested_ref_id,
+        private readonly ImportSessionRepository $session
+    ) {
+    }
+
+    public function getIdentifier(): string
+    {
+        return 'persist';
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->lng->txt('qpl_import_step_persist');
+    }
+
+    public function getDescription(): ?string
+    {
+        return '';
+    }
+
+    public function process(ImportContext $context): StageResult
+    {
+        $component_import_dir = dirname($context->get(UploadValidationStage::COMPONENT_IMPORT_FILE));
+        $mappings_file = "{$component_import_dir}/mappings.xml";
+        if (!file_exists($mappings_file) || !is_file($mappings_file)) {
+            return StageResult::error($context, $this->lng->txt('obj_import_file_error'));
+        }
+
+        $deserializer = new SimpleXMLDeserializer()->open(file_get_contents($mappings_file));
+        $deserializer->addHandler('mappings', function (array $mappings) use (&$context) {
+            $context = $context->with('mappings', $mappings);
+        });
+        $deserializer->process();
+
+        $importer = new ilImport($this->requested_ref_id);
+        $importer->importObject(
+            null,
+            $context->get(UploadValidationStage::FILE_TO_IMPORT),
+            basename($context->get(UploadValidationStage::FILE_TO_IMPORT)),
+            'tst',
+            'components/ILIAS/Test',
+            true,
+        );
+
+        // Context is updated by the TestImporter so we need to reload it
+        return StageResult::complete($this->session->getContext());
+    }
+}
---
+++ components/ILIAS/Test/src/ExportImport/Import/UserImportResolver.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
+declare(strict_types=1);
+
+namespace ILIAS\Test\ExportImport\Import;
+
+use ilDBInterface;
+use ILIAS\Test\Settings\GlobalSettings\UserIdentifiers;
+use ilImportMapping;
+use Psr\Log\LoggerInterface;
+
+class UserImportResolver
+{
+    public function __construct(
+        private readonly ilDBInterface $db,
+        private readonly LoggerInterface $log,
+    ) {
+    }
+
+    /**
+     * @param array<int, mixed> $users
+     * @return array<int, int>
+     */
+    public function resolve(UserIdentifiers $criteria, array $users): array
+    {
+        $this->log->debug("Resolving user import for criteria {$criteria->value}");
+
+        if (!$this->db->tableColumnExists('usr_data', $criteria->value)) {
+            $this->log->error("User criteria field {$criteria->value} does not exist in usr_data table, using anonymous user ID for all users");
+
+            return array_fill_keys(array_keys($users), ANONYMOUS_USER_ID);
+        }
+
+        $in_clause = $this->db->in(
+            $criteria->value,
+            array_values($users),
+            false,
+            $criteria->getColumnType()
+        );
+        $query = $this->db->query("SELECT usr_id, {$criteria->value} AS identifier FROM usr_data WHERE {$in_clause}");
+
+        $db_mapping = [];
+        foreach ($this->db->fetchAll($query) as $row) {
+            $db_mapping[$row['identifier']] = $row['usr_id'];
+        }
+
+        $mapping = [];
+        foreach ($users as $original_id => $identifier) {
+            if (isset($db_mapping[$identifier])) {
+                $this->log->debug("User identifier {$identifier} found, mapping user {$original_id} to {$db_mapping[$identifier]}");
+                $mapping[$original_id] = $db_mapping[$identifier];
+            } else {
+                $this->log->warning("User identifier {$identifier} not found for user {$original_id}, using anonymous user ID");
+                $mapping[$original_id] = ANONYMOUS_USER_ID;
+            }
+        }
+
+        return $mapping;
+    }
+
+    /**
+     * @param array<int, int> $user_mapping
+     */
+    public function store(array $user_mapping, ilImportMapping $import_mapping): void
+    {
+        foreach ($user_mapping as $original_id => $user_id) {
+            $import_mapping->addMapping('tst', 'user', (string) $original_id, (string) $user_id);
+        }
+    }
+}
