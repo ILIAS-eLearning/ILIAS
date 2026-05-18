a4ea0d66f4047c1f76affe73ebae2914c1456aca
Fabian Schmid <fabian@sr.solutions>
2026-05-18 08:47:44 +0200

    [FIX] 0047417: Permissions tab of file not highlighted when selected

--- components/ILIAS/File/classes/class.ilObjFileGUI.php
+++ components/ILIAS/File/classes/class.ilObjFileGUI.php
@@ -225,7 +225,7 @@ class ilObjFileGUI extends ilObject2GUI
 
                 // repository permissions
             case 'ilpermissiongui':
-                $ilTabs->activateTab("id_permissions");
+                $this->tabs_gui->activateTab('perm_settings');
                 $perm_gui = new ilPermissionGUI($this);
                 $this->ctrl->forwardCommand($perm_gui);
                 break;
@@ -1066,8 +1066,15 @@ class ilObjFileGUI extends ilObject2GUI
             );
         }
 
-        // will add permission tab if needed
-        parent::setTabs();
+        // Permissions-tabs, had to add it here, see https://mantis.ilias.de/view.php?id=47417
+        if ($this->access->checkAccess('edit_permission', '', $this->ref_id)) {
+            $this->tabs_gui->addTarget(
+                "perm_settings",
+                $this->ctrl->getLinkTargetByClass([self::class, ilPermissionGUI::class], "perm"),
+                ["perm", "info", "owner"],
+                ilPermissionGUI::class
+            );
+        }
     }
 
     protected function initSettingsTab(): void
