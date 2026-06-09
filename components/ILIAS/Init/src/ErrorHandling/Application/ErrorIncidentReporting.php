4da08707e93a6ed3251c103f35cf596af7756f12
mjansen <mjansen@databay.de>
2026-06-09 09:40:26 +0200

    [FEATURE] Init/ErrorHandling: Add `ReportErrorIncident` use case and application ports

---
+++ components/ILIAS/Init/src/ErrorHandling/Application/ErrorIncidentReporting.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
+declare(strict_types=1);
+
+namespace ILIAS\Init\ErrorHandling\Application;
+
+use ILIAS\Init\ErrorHandling\Incident\ErrorIncident;
+use Whoops\Exception\Inspector;
+
+/**
+ * Application port for reporting an exception as a logged error incident.
+ */
+interface ErrorIncidentReporting
+{
+    public function report(Inspector $inspector): ?ErrorIncident;
+}
