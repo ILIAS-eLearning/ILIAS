50c4c7d40ba451b4ef9fcde1c49baad36dd6d4ba
mjansen <mjansen@databay.de>
2026-06-09 09:50:00 +0200

    [FEATURE] Init/ErrorHandling: Add outbound logging adapters for error incident reporting

---
+++ components/ILIAS/Init/src/ErrorHandling/Infrastructure/Logging/LoggingErrorFileStorageAdapter.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
+declare(strict_types=1);
+
+namespace ILIAS\Init\ErrorHandling\Infrastructure\Logging;
+
+use ILIAS\Init\ErrorHandling\Application\ErrorLogFileStorage;
+use Whoops\Exception\Inspector;
+
+final class LoggingErrorFileStorageAdapter implements ErrorLogFileStorage
+{
+    public function write(
+        Inspector $inspector,
+        string $directory,
+        string $file_name,
+        array $sensitive_parameter_names
+    ): void {
+        $writer = new \ilLoggingErrorFileStorage($inspector, $directory, $file_name);
+        $writer = $writer->withExclusionList($sensitive_parameter_names);
+        $writer->write();
+    }
+}
