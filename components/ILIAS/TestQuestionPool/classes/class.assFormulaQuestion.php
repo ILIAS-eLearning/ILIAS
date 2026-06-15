52d3e9e6be495017f5e198e61aac4418e51e2298
Lukas Eichenauer <47783030+lukas-heinrich@users.noreply.github.com>
2026-06-15 08:06:32 +0200

    Test: Fix unit handling in advanced rating evaluation

--- components/ILIAS/TestQuestionPool/classes/class.assFormulaQuestion.php
+++ components/ILIAS/TestQuestionPool/classes/class.assFormulaQuestion.php
@@ -834,8 +834,7 @@ class assFormulaQuestion extends assQuestion implements iQuestionCondition, Ques
                 $this->getVariables(),
                 $this->getResults(),
                 $user_solution[$result->getResult()]['value'] ?? '',
-                $user_solution[$result->getResult()]['unit'] ?? null,
-                $this->unitrepository->getUnits()
+                $user_solution[$result->getResult()]['unit'] ?? null
             );
         }
 
@@ -853,8 +852,7 @@ class assFormulaQuestion extends assQuestion implements iQuestionCondition, Ques
                 $this->getVariables(),
                 $this->getResults(),
                 $user_solution[$result->getResult()] ?? '',
-                $unit_id !== null ? $this->unitrepository->getUnit((int) $unit_id) : null,
-                $this->unitrepository->getUnits()
+                $unit_id !== null ? $this->unitrepository->getUnit((int) $unit_id) : null
             );
         }
 
