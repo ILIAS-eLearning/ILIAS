cfcd986fec94abe1695663bb6fede95c92fdcd05
ZallaxDev <dustzallax@gmail.com>
2026-06-22 13:18:15 +0200

    [Fix] Added declare strict types

--- components/ILIAS/Survey/classes/class.ilObjSurveyGUI.php
+++ components/ILIAS/Survey/classes/class.ilObjSurveyGUI.php
@@ -16,6 +16,8 @@
  *
  *********************************************************************/
 
+declare(strict_types=1);
+
 use ILIAS\Survey\Participants;
 use ILIAS\Survey\InternalGUIService;
 use ILIAS\User\Profile\PublicProfileGUI;
