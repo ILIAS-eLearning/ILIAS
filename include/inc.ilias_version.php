963508a8f4da1ea7702784141d8196484a79b0b4
Releasemanager <webmaster@ilias.de>
2026-07-07 13:56:13 +0000

    Release 9.21

--- include/inc.ilias_version.php
+++ include/inc.ilias_version.php
@@ -26,5 +26,5 @@ declare(strict_types=1);
 *
 * @package ilias-core
 */
-const ILIAS_VERSION = "9.20 2026-05-26";
-const ILIAS_VERSION_NUMERIC = "9.20";            // since version ILIAS 6 this must be always x.y: x and y are numbers
+const ILIAS_VERSION = "9.21 2026-07-07";
+const ILIAS_VERSION_NUMERIC = "9.21";            // since version ILIAS 6 this must be always x.y: x and y are numbers
