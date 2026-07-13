b52b113bbeb4be2827d60666d49a61d8cdc4f376
Fabian Helfer <82493694+fhelfer@users.noreply.github.com>
2026-07-13 11:04:19 +0200

    [Bugfix] Mail 047999: Fix CC/BCC Mails Generic Placeholder not replaced with corresponding value (#11713)

--- components/ILIAS/Mail/tests/ilMailTest.php
+++ components/ILIAS/Mail/tests/ilMailTest.php
@@ -687,7 +687,6 @@ class ilMailTest extends ilMailBaseTestCase
             $this->getMockBuilder(ilObjUser::class)->disableOriginalConstructor()->getMock(),
             $this->getMockBuilder(ilMailTemplatePlaceholderResolver::class)->disableOriginalConstructor()->getMock(),
             null,
-            null,
             $this->getMockBuilder(MailSignatureService::class)->disableOriginalConstructor()->getMock(),
         );
 
