419c356352b2f111cdd86f1adb856346335a4c14
Sagun Karki <51822939+sKarki999@users.noreply.github.com>
2026-07-22 10:14:35 +0200

    fix: preserve blocked status of course members in updateCourse SOAP call (0040575) (#11602)

--- components/ILIAS/soap/classes/class.ilSoapCourseAdministration.php
+++ components/ILIAS/soap/classes/class.ilSoapCourseAdministration.php
@@ -1,25 +1,20 @@
 <?php
-/*
- +-----------------------------------------------------------------------------+
- | ILIAS open source                                                           |
- +-----------------------------------------------------------------------------+
- | Copyright (c) 1998-2001 ILIAS open source, University of Cologne            |
- |                                                                             |
- | This program is free software; you can redistribute it and/or               |
- | modify it under the terms of the GNU General Public License                 |
- | as published by the Free Software Foundation; either version 2              |
- | of the License, or (at your option) any later version.                      |
- |                                                                             |
- | This program is distributed in the hope that it will be useful,             |
- | but WITHOUT ANY WARRANTY; without even the implied warranty of              |
- | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
- | GNU General Public License for more details.                                |
- |                                                                             |
- | You should have received a copy of the GNU General Public License           |
- | along with this program; if not, write to the Free Software                 |
- | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
- +-----------------------------------------------------------------------------+
-*/
+
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
 
 /**
  * Soap course administration methods
@@ -375,10 +370,7 @@ class ilSoapCourseAdministration extends ilSoapAdministration
         $md = new ilMD($tmp_course->getId(), 0, 'crs');
         $md->deleteAll();
 
-        ilCourseParticipants::_deleteAllEntries($tmp_course->getId());
-
         ilCourseWaitingList::_deleteAll($tmp_course->getId());
-
 
         $xml_parser = new ilCourseXMLParser($tmp_course);
         $xml_parser->setMode(ilCourseXMLParser::MODE_SOAP);
