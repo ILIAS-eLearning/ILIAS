abc91438cd087e2b3f8fc7cb0ae061320a986715
Tim Schmitz <104776863+schmitz-ilias@users.noreply.github.com>
2026-07-28 09:35:00 +0200

    User: skip broken users in gallery
    See: https://mantis.ilias.de/view.php?id=47791

--- components/ILIAS/User/classes/Gallery/class.ilUsersGalleryParticipants.php
+++ components/ILIAS/User/classes/Gallery/class.ilUsersGalleryParticipants.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -17,6 +15,8 @@ declare(strict_types=1);
  * https://github.com/ILIAS-eLearning
  *
  *********************************************************************/
+
+declare(strict_types=1);
 
 /**
  * Class ilUsersGalleryParticipants
@@ -40,6 +40,10 @@ class ilUsersGalleryParticipants extends ilAbstractUsersGalleryCollectionProvide
 
         foreach ($usr_ids as $usr_id) {
             if (isset($this->users[$usr_id])) {
+                continue;
+            }
+
+            if (!ilObjUser::userExists([$usr_id])) {
                 continue;
             }
 
