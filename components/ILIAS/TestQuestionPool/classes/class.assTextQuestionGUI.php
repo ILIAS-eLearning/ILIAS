1a29080bda21ce4439738c81f429c065829b73fd
Stephan Kergomard <webmaster@kergomard.ch>
2026-07-29 15:08:26 +0200

    Test: Fix Display of SpecialChars in Essay
    See: https://mantis.ilias.de/view.php?id=48138

--- components/ILIAS/TestQuestionPool/classes/class.assTextQuestionGUI.php
+++ components/ILIAS/TestQuestionPool/classes/class.assTextQuestionGUI.php
@@ -445,7 +445,7 @@ class assTextQuestionGUI extends assQuestionGUI implements ilGuiQuestionScoringA
         if (is_object($this->getPreviewSession())) {
             $template->setVariable(
                 "ESSAY",
-                ilLegacyFormElementsUtil::prepareFormOutput(
+                $this->prepareUserSolutionForAnswerFormOutput(
                     (string) $this->getPreviewSession()->getParticipantsSolution()
                 )
             );
@@ -477,14 +477,11 @@ class assTextQuestionGUI extends assQuestionGUI implements ilGuiQuestionScoringA
         if ($active_id) {
             $solutions = $this->object->getUserSolutionPreferingIntermediate($active_id, $pass);
             foreach ($solutions as $solution_value) {
-                $user_solution = $solution_value["value1"];
+                $raw_user_solution = $solution_value["value1"];
             }
-
-            if ($this->tiny_mce_enabled) {
-                $user_solution = htmlentities($user_solution);
-            }
-
-            $user_solution = str_replace(['{', '}', '\\'], ['&#123;', '&#125;', '&#92;'], $user_solution);
+            $user_solution = $this->prepareUserSolutionForAnswerFormOutput(
+                $raw_user_solution
+            );
         }
 
         $template = new ilTemplate("tpl.il_as_qpl_text_question_output.html", true, true, "components/ILIAS/TestQuestionPool");
@@ -794,5 +791,15 @@ class assTextQuestionGUI extends assQuestionGUI implements ilGuiQuestionScoringA
     {
         $this->writeQuestionSpecificPostData($form);
         $this->writeAnswerSpecificPostData($form);
+    }
+
+    private function prepareUserSolutionForAnswerFormOutput(
+        string $solution
+    ): string {
+        if ($this->tiny_mce_enabled) {
+            $solution = htmlentities($solution);
+        }
+
+        return str_replace(['{', '}', '\\'], ['&#123;', '&#125;', '&#92;'], $solution);
     }
 }
