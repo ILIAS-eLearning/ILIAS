50a9e3106a96ade70f6559d7d8c2ad14ec4addd2
Saúl Díaz <sauldiazdiaz210@gmail.com>
2026-08-04 12:16:43 +0200

    [FIX] LTI: Return 404 for unknown AGS resource links

--- components/ILIAS/LTIConsumer/classes/class.ilLTIConsumerGradeServiceResults.php
+++ components/ILIAS/LTIConsumer/classes/class.ilLTIConsumerGradeServiceResults.php
@@ -253,7 +253,7 @@ class ilLTIConsumerGradeServiceResults extends ilLTIConsumerResourceBase
         global $DIC;
         $tree = $DIC->repositoryTree();
         $node_data = $tree->getNodeData($resource_link_ref_id);
-        if (!$node_data || $node_data['type'] !== 'lti' ||
+        if (!isset($node_data['type']) || $node_data['type'] !== 'lti' ||
             !in_array($context_id, $tree->getPathId($resource_link_ref_id), true)) {
             throw ilLTIConsumerHttpException::notFound('Resource link not found');
         }
