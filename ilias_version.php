9a7c60f915ed166b823dbd43dee74da8494567de
Releasemanager <webmaster@ilias.de>
2026-08-11 14:09:55 +0000

    Release 11.3

--- ilias_version.php
+++ ilias_version.php
@@ -26,5 +26,5 @@ declare(strict_types=1);
 *
 * @package ilias-core
 */
-const ILIAS_VERSION = "11.2 2026-07-07";
-const ILIAS_VERSION_NUMERIC = "11.2";            // since version ILIAS 6 this must be always x.y: x and y are numbers
+const ILIAS_VERSION = "11.3 2026-08-11";
+const ILIAS_VERSION_NUMERIC = "11.3";            // since version ILIAS 6 this must be always x.y: x and y are numbers
