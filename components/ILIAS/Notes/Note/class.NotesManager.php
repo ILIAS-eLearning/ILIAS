db460861ed3dddd7e700d80d20508f182ac9ea8a
Matheus Zych <mzych@databay.de>
2026-08-14 09:20:51 +0200

    Notes: Fix Comments In Daily Mail
    See: https://mantis.ilias.de/view.php?id=48196
    
    New comments were missing from daily mail notifications because note
    queries enforced empty `obj_type` and `news_id = 0`. The repository now
    applies those filters only when set, and context loading no longer forces
    invalid sub-object constraints.

--- components/ILIAS/Notes/Note/class.NotesManager.php
+++ components/ILIAS/Notes/Note/class.NotesManager.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -17,6 +15,8 @@ declare(strict_types=1);
  * https://github.com/ILIAS-eLearning
  *
  *********************************************************************/
+
+declare(strict_types=1);
 
 namespace ILIAS\Notes;
 
@@ -129,13 +129,8 @@ class NotesManager
         bool $ascending = false,
         string $since = ""
     ): array {
-        $context = $this->data->context(
-            $obj_id,
-            0,
-            ""
-        );
         return $this->db_repo->getNotesForContext(
-            $context,
+            $this->data->context($obj_id),
             $type,
             $incl_sub,
             $author,
