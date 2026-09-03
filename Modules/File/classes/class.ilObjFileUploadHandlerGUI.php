2745aa0c736907bc757ad7eb520d56c34fa9338d
Fabian Schmid <fabian@sr.solutions>
2026-09-03 11:05:10 +0000

    Unauthorized Upload of Files
    Signed-off-by: Releasemanager <webmaster@ilias.de>

--- Modules/File/classes/class.ilObjFileUploadHandlerGUI.php
+++ Modules/File/classes/class.ilObjFileUploadHandlerGUI.php
@@ -36,10 +36,57 @@ use ILIAS\ResourceStorage\Stakeholder\ResourceStakeholder;
  */
 class ilObjFileUploadHandlerGUI extends AbstractCtrlAwareIRSSUploadHandler
 {
+    private const PARAM_REF_ID = 'ref_id';
+
+    private ilAccessHandler $access;
+    private \ILIAS\Refinery\Factory $refinery;
+    private ilObjUser $user;
+
+    public function __construct()
+    {
+        global $DIC;
+
+        $this->access = $DIC->access();
+        $this->refinery = $DIC->refinery();
+        $this->user = $DIC->user();
+
+        parent::__construct();
+    }
+
     protected function getStakeholder(): ResourceStakeholder
     {
         global $DIC;
         return new ilObjFileStakeholder($DIC->user()->getId());
+    }
+
+    protected function getUploadResult(): HandlerResult
+    {
+        if (!$this->mayStageUploadForRepository()) {
+            return new BasicHandlerResult(
+                $this->getFileIdentifierParameterName(),
+                HandlerResult::STATUS_FAILED,
+                '',
+                $this->language->txt('permission_denied')
+            );
+        }
+
+        return parent::getUploadResult();
+    }
+
+    private function mayStageUploadForRepository(): bool
+    {
+        $query = $this->http->wrapper()->query();
+
+        if ($query->has(self::PARAM_REF_ID)) {
+            $ref_id = $query->retrieve(self::PARAM_REF_ID, $this->refinery->kindlyTo()->int());
+            return $ref_id > 0
+                && $this->access->checkAccess('create_' . ilObjFile::OBJECT_TYPE, '', $ref_id);
+        }
+
+        // No repository target (e.g. personal workspace via wsp_id); the create permission
+        // is enforced in ilObjFileGUI::uploadFiles() for that case. An anonymous user has
+        // no such target and is refused here as well.
+        return !($this->user->isAnonymous() || $this->user->getId() < 1);
     }
 
     protected function getClassPath(): array
