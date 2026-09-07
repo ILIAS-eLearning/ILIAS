27feaafefa3ba3059f542c2c99d686fc13f8b435
Stefan Meyer <meyer@leifos.de>
2026-09-07 12:14:59 +0200

    0048294: Personal Learning Progress cannot be opened

--- components/ILIAS/Tracking/classes/class.ilLearningProgressBaseGUI.php
+++ components/ILIAS/Tracking/classes/class.ilLearningProgressBaseGUI.php
@@ -123,11 +123,21 @@ abstract class ilLearningProgressBaseGUI
 
     final public function executeCommand(): void
     {
-        $has_access = ilLearningProgressAccess::checkPermission('read_learning_progress', $this->getRefId());
+        $has_access = false;
+        if ($this->getMode() === self::LP_CONTEXT_REPOSITORY) {
+            $has_access = ilLearningProgressAccess::checkAccess($this->getRefId());
+        } elseif ($this->getMode() === self::LP_CONTEXT_ADMINISTRATION) {
+            $has_access = ilObjUserTracking::_hasLearningProgressOtherUsers();
+        } elseif ($this->getMode() === self::LP_CONTEXT_USER_FOLDER) {
+            $has_access = $this->access->checkAccess('read', '', $this->getRefId());
+        } elseif ($this->getMode() === self::LP_CONTEXT_PERSONAL_DESKTOP) {
+            $has_access = ilObjUserTracking::_hasLearningProgressLearner() || ilObjUserTracking::_hasLearningProgressOtherUsers();
+        } elseif ($this->getMode() === self::LP_CONTEXT_ORG_UNIT) {
+            $has_access = ilObjOrgUnitAccess::_checkAccessToUserLearningProgress($this->getRefId(), $this->getUserId());
+        }
         if ($has_access) {
             $this->handleCommand();
-        }
-        if (!$has_access) {
+        } else {
             $this->error->raiseError($this->lng->txt('permission_denied'), $this->error->WARNING);
         }
     }
