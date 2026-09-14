33aa92cc682755ab6e0b5f6eed08281143a0e269
Matheus Zych <mzych@databay.de>
2026-09-14 15:31:57 +0200

    ItemGroup: Fix Dataset Import/Export
    See: https://mantis.ilias.de/view.php?id=31861
    
    See: https://mantis.ilias.de/view.php?id=48043
    
    See: https://mantis.ilias.de/view.php?id=48101
    
    After replacing `hide_title` and `behaviour` with `display` and
    `toggleable_initially`, export still used the legacy fields.
    Add dataset version `10.12` and exporter schema `12.0` for the new
    columns, and map both legacy and current records onto the item
    group display settings on import.

--- components/ILIAS/ItemGroup/classes/class.ilItemGroupDataSet.php
+++ components/ILIAS/ItemGroup/classes/class.ilItemGroupDataSet.php
@@ -16,96 +16,100 @@
  *
  *********************************************************************/
 
-/**
- * Item group data set class
- * @author Alexander Killing <killing@leifos.de>
- */
+declare(strict_types=1);
+
 class ilItemGroupDataSet extends ilDataSet
 {
     protected ilObjItemGroup $current_obj;
 
     public function getSupportedVersions(): array
     {
-        return array("4.3.0", "5.3.0");
+        return ['4.3.0', '5.3.0', '10.12'];
     }
 
     public function getXmlNamespace(string $a_entity, string $a_schema_version): string
     {
-        return "https://www.ilias.de/xml/Modules/ItemGroup/" . $a_entity;
+        return "https://www.ilias.de/xml/Modules/ItemGroup/{$a_entity}";
     }
 
     protected function getTypes(string $a_entity, string $a_version): array
     {
-        if ($a_entity == "itgr") {
-            switch ($a_version) {
-                case "4.3.0":
-                    return array(
-                        "Id" => "integer",
-                        "Title" => "text",
-                        "Description" => "text");
-                case "5.3.0":
-                    return array(
-                        "Id" => "integer",
-                        "HideTitle" => "integer",
-                        "Behaviour" => "integer",
-                        "Title" => "text",
-                        "Description" => "text");
-            }
-        }
-
-        if ($a_entity == "itgr_item") {
-            switch ($a_version) {
-                case "4.3.0":
-                case "5.3.0":
-                    return array(
-                        "ItemGroupId" => "integer",
-                        "ItemId" => "text"
-                        );
-            }
-        }
-        return [];
+        return match ($a_entity) {
+            'itgr' => match ($a_version) {
+                '4.3.0' => [
+                    'Id' => ilDBConstants::T_INTEGER,
+                    'Title' => ilDBConstants::T_TEXT,
+                    'Description' => ilDBConstants::T_TEXT,
+                ],
+                '5.3.0' => [
+                    'Id' => ilDBConstants::T_INTEGER,
+                    'HideTitle' => ilDBConstants::T_INTEGER,
+                    'Behaviour' => ilDBConstants::T_INTEGER,
+                    'ListPresentation' => ilDBConstants::T_TEXT,
+                    'TileSize' => ilDBConstants::T_INTEGER,
+                    'Title' => ilDBConstants::T_TEXT,
+                    'Description' => ilDBConstants::T_TEXT,
+                ],
+                '10.12' => [
+                    'Id' => ilDBConstants::T_INTEGER,
+                    'Display' => ilDBConstants::T_TEXT,
+                    'ToggleableInitially' => ilDBConstants::T_TEXT,
+                    'ListPresentation' => ilDBConstants::T_TEXT,
+                    'TileSize' => ilDBConstants::T_INTEGER,
+                    'Title' => ilDBConstants::T_TEXT,
+                    'Description' => ilDBConstants::T_TEXT,
+                ],
+                default => [],
+            },
+            'itgr_item' => match ($a_version) {
+                '4.3.0', '5.3.0', '10.12' => [
+                    'ItemGroupId' => ilDBConstants::T_INTEGER,
+                    'ItemId' => ilDBConstants::T_TEXT,
+                ],
+                default => [],
+            },
+        };
     }
 
     public function readData(string $a_entity, string $a_version, array $a_ids): void
     {
-        $ilDB = $this->db;
+        $in_obj_id = $this->db->in('obj_id', $a_ids, false, ilDBConstants::T_INTEGER);
 
-        if ($a_entity == "itgr") {
-            switch ($a_version) {
-                case "4.3.0":
-                    $this->getDirectDataFromQuery("SELECT obj_id id, title, description " .
-                        " FROM object_data " .
-                        "WHERE " .
-                        $ilDB->in("obj_id", $a_ids, false, "integer"));
-                    break;
-                case "5.3.0":
-                    $this->getDirectDataFromQuery("SELECT obj_id id, title, description, hide_title, behaviour " .
-                        " FROM object_data JOIN itgr_data ON (object_data.obj_id = itgr_data.id)" .
-                        "WHERE " .
-                        $ilDB->in("obj_id", $a_ids, false, "integer"));
-                    break;
-            }
-        }
-
-        if ($a_entity == "itgr_item") {
-            switch ($a_version) {
-                case "4.3.0":
-                case "5.3.0":
-                    $this->getDirectDataFromQuery($q = "SELECT item_group_id itgr_id, item_ref_id item_id" .
-                        " FROM item_group_item " .
-                        "WHERE " .
-                        $ilDB->in("item_group_id", $a_ids, false, "integer"));
-                    break;
-            }
-        }
+        match ($a_entity) {
+            'itgr' => match ($a_version) {
+                '4.3.0' => $this->getDirectDataFromQuery(
+                    "SELECT obj_id id, title, description, list_presentation, tile_size FROM object_data 
+                    WHERE {$in_obj_id}"
+                ),
+                '5.3.0' => $this->getDirectDataFromQuery(
+                    "SELECT obj_id id, title, description, hide_title, behaviour, list_presentation, tile_size FROM object_data 
+                    INNER JOIN itgr_data ON object_data.obj_id = itgr_data.id 
+                    WHERE {$in_obj_id}"
+                ),
+                '10.12' => $this->getDirectDataFromQuery(
+                    "SELECT obj_id id, title, description, list_presentation, tile_size, display, toggleable_initially FROM object_data 
+                    INNER JOIN itgr_data ON object_data.obj_id = itgr_data.id 
+                    WHERE {$in_obj_id}"
+                ),
+                default => '',
+            },
+            'itgr_item' => match ($a_version) {
+                '4.3.0', '5.3.0', '10.12' => $this->getDirectDataFromQuery(
+                    "SELECT item_group_id itgr_id, item_ref_id item_id FROM item_group_item 
+                    WHERE {$this->db->in('item_group_id', $a_ids, false, ilDBConstants::T_INTEGER)}"
+                ),
+                default => null,
+            },
+            default => null,
+        };
     }
 
     public function getXmlRecord(string $a_entity, string $a_version, array $a_set): array
     {
-        if ($a_entity == "itgr_item") {
-            // make ref id an object id
-            $a_set["ItemId"] = ilObject::_lookupObjId($a_set["ItemId"]);
+        if ($a_entity === 'itgr_item') {
+            $a_set['ItemId'] = ilObject::_lookupObjId($a_set['ItemId']);
         }
+
         return $a_set;
     }
 
@@ -115,14 +119,10 @@ class ilItemGroupDataSet extends ilDataSet
         ?array $a_rec = null,
         ?array $a_ids = null
     ): array {
-        switch ($a_entity) {
-            case "itgr":
-                return array(
-                    "itgr_item" => array("ids" => $a_rec["Id"] ?? [])
-                );
-        }
-
-        return [];
+        return match ($a_entity) {
+            'itgr' => ['itgr_item' => ['ids' => $a_rec['Id'] ?? []]],
+            default => [],
+        };
     }
 
     public function importRecord(
@@ -133,45 +133,77 @@ class ilItemGroupDataSet extends ilDataSet
         string $a_schema_version
     ): void {
         $a_rec = $this->stripTags($a_rec);
+
         switch ($a_entity) {
-            case "itgr":
-                if ($new_id = $a_mapping->getMapping('components/ILIAS/Container', 'objs', $a_rec['Id'])) {
+            case 'itgr':
+                $new_id = (int) ($a_mapping->getMapping('components/ILIAS/Container', 'objs', $a_rec['Id']) ?? 0);
+
+                if ($new_id !== 0) {
                     /** @var ilObjItemGroup $newObj */
                     $newObj = ilObjectFactory::getInstanceByObjId($new_id, false);
                 } else {
                     $newObj = new ilObjItemGroup();
-                    $newObj->setType("itgr");
+                    $newObj->setType('itgr');
                     $newObj->create(true);
                 }
 
-                $newObj->setTitle($a_rec["Title"]);
-                $newObj->setDescription($a_rec["Description"]);
-                $newObj->setDisplay(
-                    $a_rec["HideTitle"] === "1"
-                        ? ilItemGroupAR::DISPLAY_WITHOUT_TITLE
-                        : ilItemGroupAR::DISPLAY_WITH_TITLE
-                );
-                $newObj->setDisplayWithTitleAndToggleableInitially(
-                    $a_rec["Behaviour"] === "1"
-                        ? ilItemGroupAR::DISPLAY_WITH_TITLE_AND_TOGGLEABLE_INITIALLY_OPEN
-                        : ilItemGroupAR::DISPLAY_WITH_TITLE_AND_TOGGLEABLE_INITIALLY_CLOSED
-                );
+                $newObj->setTitle($a_rec['Title']);
+                $newObj->setDescription($a_rec['Description']);
+                $newObj->setListPresentation((string) ($a_rec['ListPresentation'] ?? ''));
+                $newObj->setTileSize((int) ($a_rec['TileSize'] ?? 0));
+                $newObj->setDisplay($this->resolveDisplay($a_rec));
+                $newObj->setDisplayWithTitleAndToggleableInitially($this->resolveBehaviour($a_rec));
                 $newObj->update();
                 $this->current_obj = $newObj;
-                $a_mapping->addMapping("components/ILIAS/ItemGroup", "itgr", $a_rec["Id"], $newObj->getId());
-
+                $a_mapping->addMapping('components/ILIAS/ItemGroup', 'itgr', $a_rec['Id'], (string) $newObj->getId());
                 break;
 
-            case "itgr_item":
-                if ($obj_id = $a_mapping->getMapping('components/ILIAS/Container', 'objs', $a_rec['ItemId'])) {
-                    $ref_id = current(ilObject::_getAllReferences($obj_id));
-                    $itgri = new ilItemGroupItems();
-                    $itgri->setItemGroupId($this->current_obj->getId());
-                    $itgri->read();
-                    $itgri->addItem($ref_id);
-                    $itgri->update();
+            case 'itgr_item':
+                $obj_id = (int) ($a_mapping->getMapping('components/ILIAS/Container', 'objs', $a_rec['ItemId']) ?? 0);
+
+                if ($obj_id === 0) {
+                    break;
                 }
+
+                $ref_id = current(ilObject::_getAllReferences($obj_id));
+                $itgri = new ilItemGroupItems();
+                $itgri->setItemGroupId($this->current_obj->getId());
+                $itgri->read();
+                $itgri->addItem($ref_id);
+                $itgri->update();
                 break;
         }
+    }
+
+    private function resolveDisplay(array $a_set): string
+    {
+        if (isset($a_set['HideTitle'])) {
+            return match ($a_set['HideTitle']) {
+                '1' => ilItemGroupAR::DISPLAY_WITHOUT_TITLE,
+                default => ilItemGroupAR::DISPLAY_WITH_TITLE,
+            };
+        }
+
+        $display = $a_set['Display'] ?? '';
+        return match ($display) {
+            ilItemGroupAR::DISPLAY_WITHOUT_TITLE, ilItemGroupAR::DISPLAY_WITH_TITLE_AND_TOGGLEABLE => $display,
+            default => ilItemGroupAR::DISPLAY_WITH_TITLE,
+        };
+    }
+
+    private function resolveBehaviour(array $a_set): string
+    {
+        if (isset($a_set['Behaviour'])) {
+            return match ($a_set['Behaviour']) {
+                '1' => ilItemGroupAR::DISPLAY_WITH_TITLE_AND_TOGGLEABLE_INITIALLY_OPEN,
+                default => ilItemGroupAR::DISPLAY_WITH_TITLE_AND_TOGGLEABLE_INITIALLY_CLOSED,
+            };
+        }
+
+        $toggleable_initially = $a_set['ToggleableInitially'] ?? '';
+        return match ($toggleable_initially) {
+            ilItemGroupAR::DISPLAY_WITH_TITLE_AND_TOGGLEABLE_INITIALLY_OPEN => $toggleable_initially,
+            default => ilItemGroupAR::DISPLAY_WITH_TITLE_AND_TOGGLEABLE_INITIALLY_CLOSED,
+        };
     }
 }
