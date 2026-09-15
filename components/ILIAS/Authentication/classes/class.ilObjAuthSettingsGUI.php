cf8f3693bcee231108744217f1929c9178225911
mjansen <mjansen@databay.de>
2026-09-15 13:20:41 +0200

    [IMPROVEMENT] Auth: Tidy authentication settings sub-screens
    (cherry picked from commit 53f630d280f7f86d73045bf68628a25296f11e99)

--- components/ILIAS/Authentication/classes/class.ilObjAuthSettingsGUI.php
+++ components/ILIAS/Authentication/classes/class.ilObjAuthSettingsGUI.php
@@ -975,6 +975,7 @@ class ilObjAuthSettingsGUI extends ilObjectGUI
                 break;
 
             case strtolower(ilObjectContentStyleSettingsGUI::class):
+                $this->checkPermission('write');
                 $this->setTitleAndDescription();
                 $this->setSubTabs('authSettings');
                 $this->tabs_gui->activateTab('authentication_settings');
@@ -1122,11 +1123,13 @@ class ilObjAuthSettingsGUI extends ilObjectGUI
                 ''
             );
 
-            $this->tabs_gui->addSubTab(
-                'style',
-                $this->lng->txt('cont_style'),
-                $this->ctrl->getLinkTargetByClass(ilObjectContentStyleSettingsGUI::class)
-            );
+            if ($this->access->checkAccess('write', '', $this->object->getRefId())) {
+                $this->tabs_gui->addSubTab(
+                    'style',
+                    $this->lng->txt('cont_style'),
+                    $this->ctrl->getLinkTargetByClass(ilObjectContentStyleSettingsGUI::class)
+                );
+            }
         }
     }
 
