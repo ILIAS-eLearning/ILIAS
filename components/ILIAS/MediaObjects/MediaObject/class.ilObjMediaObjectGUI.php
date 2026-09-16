40a02a70f9bfc5f8fd76ac328d7bcb1d503c7aaa
Alexander Killing <killing@leifos.de>
2026-09-16 23:13:20 +0200

    media objects: 30866: Media objects - size confusion or wrong expactation?

--- components/ILIAS/MediaObjects/MediaObject/class.ilObjMediaObjectGUI.php
+++ components/ILIAS/MediaObjects/MediaObject/class.ilObjMediaObjectGUI.php
@@ -1037,11 +1037,15 @@ class ilObjMediaObjectGUI extends ilObjectGUI
             $this->object->setDescription($format);
             // determine width and height of known image types
             $wh_input = $form->getInput("standard_width_height");
+            $source = $std_item->getOriginalSource();
+            if ($source === "" && $std_item->getLocationType() === "LocalFile") {
+                $source = $mob_dir . "/" . $location;
+            }
             $wh = ilObjMediaObject::_determineWidthHeight(
                 $format,
                 $form->getInput("standard_type"),
-                $mob_dir . "/" . $location,
-                $std_item->getLocation(),
+                $source,
+                $source,
                 (bool) ($wh_input["constr_prop"] ?? false),
                 ($form->getInput("standard_size") == "original"),
                 ($wh_input["width"] == "") ? null : (int) $wh_input["width"],
@@ -1161,11 +1165,15 @@ class ilObjMediaObjectGUI extends ilObjectGUI
 
                 // determine width and height of known image types
                 $wh_input = $form->getInput("full_width_height");
+                $source = $full_item->getOriginalSource();
+                if ($source === "" && $full_item->getLocationType() === "LocalFile") {
+                    $source = $mob_dir . "/" . $location;
+                }
                 $wh = ilObjMediaObject::_determineWidthHeight(
                     $format,
                     $type,
-                    $mob_dir . "/" . $location,
-                    $full_item->getLocation(),
+                    $source,
+                    $source,
                     (bool) ($wh_input["constr_prop"] ?? false),
                     ($form->getInput("full_size") == "original"),
                     ($wh_input["width"] == "") ? null : (int) $wh_input["width"],
