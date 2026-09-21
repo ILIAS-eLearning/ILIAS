cacf6c61bdcf7952b62548e7fe6c5c0be311979e
Alexander Killing <killing@leifos.de>
2026-09-21 12:55:44 +0200

    media objects: 27940: Fehlende Sprachvariablen bei Verlinkungen in Medienobjekten auf Plugins

--- components/ILIAS/MediaObjects/ImageMap/class.ilImageMapEditorGUI.php
+++ components/ILIAS/MediaObjects/ImageMap/class.ilImageMapEditorGUI.php
@@ -27,6 +27,7 @@ use ILIAS\MediaObjects\ImageMap\ImageMapGUIRequest;
 class ilImageMapEditorGUI
 {
     protected \ILIAS\COPage\Xsl\XslManager $xsl;
+    protected ilObjectDefinition $obj_definition;
     protected ilObjMediaObject $media_object;
     protected ImageMapGUIRequest $request;
     protected ImageMapManager $map;
@@ -44,6 +45,7 @@ class ilImageMapEditorGUI
         $this->ctrl = $DIC->ctrl();
         $this->main_tpl = $DIC->ui()->mainTemplate();
         $this->lng = $DIC->language();
+        $this->obj_definition = $DIC["objDefinition"];
         $this->toolbar = $DIC->toolbar();
         $this->media_object = $a_media_object;
 
@@ -663,7 +665,11 @@ class ilImageMapEditorGUI
                         $title = ilObject::_lookupTitle(
                             $obj_id
                         );
-                        $link_str = $lng->txt("obj_" . $t_arr[count($t_arr) - 2]) .
+                        $type = $t_arr[count($t_arr) - 2];
+                        $type_title = $this->obj_definition->isPlugin($type)
+                            ? ilObjectPlugin::lookupTxtById($type, "obj_" . $type)
+                            : $lng->txt("obj_" . $type);
+                        $link_str = $type_title .
                             ": " . $title . " [" . $t_arr[count($t_arr) - 1] . "]" . $frame_str;
                     }
                 } else {
