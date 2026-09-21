09ce709d7d1c44b396a1d90e8a12bd225288a227
Stephan Kergomard <webmaster@kergomard.ch>
2026-09-21 11:14:12 +0200

    Test: Redirect on Question Copy
    See: https://mantis.ilias.de/view.php?id=48353

--- components/ILIAS/Test/src/Questions/Presentation/QuestionsTableActions.php
+++ components/ILIAS/Test/src/Questions/Presentation/QuestionsTableActions.php
@@ -280,6 +280,7 @@ class QuestionsTableActions
                 $protect_by_write_protection();
                 $this->test_obj->copyQuestions($row_ids);
                 $this->tpl->setOnScreenMessage('success', $this->lng->txt('copy_questions_success'), true);
+                $this->ctrl->redirectByClass(\ilObjTestGUI::class, 'showQuestions');
                 return true;
 
             case self::ACTION_ADD_TO_POOL:
