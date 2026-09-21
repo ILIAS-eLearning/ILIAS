d4afe939e7b272a1e6059c52fa26c30585afc410
Alexander Killing <killing@leifos.de>
2026-09-21 20:15:09 +0200

    copage: array access fix

--- components/ILIAS/Style/Content/classes/class.ilObjStyleSheet.php
+++ components/ILIAS/Style/Content/classes/class.ilObjStyleSheet.php
@@ -1933,7 +1933,7 @@ class ilObjStyleSheet extends ilObject
         if ($a_template_type == "") {
             return self::$templates;
         }
-        return self::$templates[$a_template_type];
+        return self::$templates[$a_template_type] ?? [];
     }
 
     public static function _getPseudoClasses(string $tag): array
